cc3f4fe3bbfc47c5853eb7361250561484afd1e5
ismael faro <ismael.faro1@ibm.com>
2017-05-02 20:21:11 -0400

    Merged qiskit/unroll/

--- qiskit/unroll/_unrollerexception.py
+++ qiskit/unroll/_unrollerexception.py
@@ -8,10 +8,10 @@ Author: Andrew Cross
 class UnrollerException(Exception):
     """Base class for errors raised by unroller."""
 
-    def __init__(self, *msg):
+    def __init__(self, *message):
         """Set the error message."""
-        self.msg = ' '.join(msg)
+        self.message = ' '.join(message)
 
     def __str__(self):
         """Return the message."""
-        return repr(self.msg)
+        return repr(self.message)
