5e0904afa39c65ad8cc2689e31f050cacf3811fd
Andrew Cross <awcross@us.ibm.com>
2017-05-04 13:13:22 -0400

    rippleadd example updated.

--- rippleadd.py
+++ rippleadd.py
@@ -15,7 +15,6 @@ def print_2x8(layout):
     """Print a 2x8 layout."""
     rev_layout = {b: a for a, b in layout.items()}
     print("")
-    print("2x8 layout:")
     for i in range(8):
         qubit = ("q", i)
         if qubit in rev_layout:
@@ -75,21 +74,24 @@ for j in range(n):
 qc.measure(cout[0], ans[n])
 
 ######################################################################
-# Work in progress
+# Work in progress to map the QuantumCircuit to the 2x8 device
 ######################################################################
 
 print("QuantumCircuit OPENQASM")
 print("-----------------------")
 print(qc.qasm())
 
+######################################################################
+# First pass: expand subroutines to a basis of 1 and 2 qubit gates.
+######################################################################
+
 u = unroll.Unroller(Qasm(data=qc.qasm()).parse(),
                     unroll.CircuitBackend(["u1", "u2", "u3", "cx"]))
 u.execute()
-C = u.be.C  # circuit directed graph object
+C = u.backend.circuit  # circuit directed graph object
 
-print("")
-print("Unrolled OPENQASM")
-print("-----------------------")
+print("Unrolled OPENQASM to [u1, u2, u3, cx] basis")
+print("-------------------------------------------")
 print(C.qasm(qeflag=True))
 
 print("")
@@ -99,35 +101,48 @@ print("width   = %d" % C.width())
 print("bits    = %d" % C.num_cbits())
 print("factors = %d" % C.num_tensor_factors())
 
-# This is the 2 by 8
-# Rewrite to use dict with integers (see quantum_optimization.py uder tools)
+######################################################################
+# Second pass: choose a layout on the coupling graph and add SWAPs.
+######################################################################
+
+# This is the 2 by 8 array of 16 qubits
 couplingdict = {0: [1, 8], 1: [2, 9], 2: [3, 10], 3: [4, 11], 4: [5, 12],
                 5: [6, 13], 6: [7, 14], 7: [15], 8: [9], 9: [10], 10: [11],
                 11: [12], 12: [13], 13: [14], 14: [15]}
 
 coupling = mapper.Coupling(couplingdict)
-print("")
-print("2x8 coupling graph = \n%s" % coupling)
-
 C_mapped, layout = mapper.swap_mapper(C, coupling)
 
+print("")
+print("Initial qubit positions on 2x8 layout")
+print("-------------------------------------------")
 print_2x8(layout)
 
 print("")
-print("SWAP mapped OPENQASM")
-print("-----------------------")
+print("Inserted SWAP gates for 2x8 layout")
+print("-------------------------------------------")
 print(C_mapped.qasm(qeflag=True))
+
+print("")
+print("size    = %d" % C_mapped.size())
+print("depth   = %d" % C_mapped.depth())
+print("width   = %d" % C_mapped.width())
+print("bits    = %d" % C_mapped.num_cbits())
+print("factors = %d" % C_mapped.num_tensor_factors())
+
+######################################################################
+# Third pass: expand SWAP subroutines and adjust cx gate orientations.
+######################################################################
 
 u = unroll.Unroller(Qasm(data=C_mapped.qasm(qeflag=True)).parse(),
                     unroll.CircuitBackend(["u1", "u2", "u3", "cx"]))
 u.execute()
-C_mapped_unrolled = u.be.C
-
+C_mapped_unrolled = u.backend.circuit
 C_directions = mapper.direction_mapper(C_mapped_unrolled, coupling)
 
 print("")
-print("Direction mapped OPENQASM")
-print("-----------------------")
+print("Changed CNOT directions")
+print("-------------------------------------------")
 print(C_directions.qasm(qeflag=True))
 
 print("")
@@ -137,5 +152,46 @@ print("width   = %d" % C_directions.width())
 print("bits    = %d" % C_directions.num_cbits())
 print("factors = %d" % C_directions.num_tensor_factors())
 
-# TODO: pass to remove CX.CX
-# TODO: pass to simplify adjacent single qubit gate strings
+######################################################################
+# Fourth pass: collect runs of cx gates and cancel them.
+######################################################################
+# We assume there are no double edges in the connectivity graph, so
+# we don't need to check the direction of the cx gates in a run.
+runs = C_directions.collect_runs(["cx"])
+for r in runs:
+    if len(r) % 2 == 0:
+        for n in r:
+            C_directions._remove_op_node(n)
+    else:
+        for j in range(len(r)-1):
+            C_directions._remove_op_node(r[j])
+
+print("")
+print("Cancelled redundant CNOT gates")
+print("-------------------------------------------")
+print(C_directions.qasm(qeflag=True))
+
+print("")
+print("size    = %d" % C_directions.size())
+print("depth   = %d" % C_directions.depth())
+print("width   = %d" % C_directions.width())
+print("bits    = %d" % C_directions.num_cbits())
+print("factors = %d" % C_directions.num_tensor_factors())
+
+######################################################################
+# Fifth pass: expand single qubit gates to u1, u2, u3 and simplify.
+######################################################################
+
+u = unroll.Unroller(Qasm(data=C_directions.qasm(qeflag=True)).parse(),
+                    unroll.CircuitBackend(["u1", "u2", "u3", "cx"]))
+u.execute()
+C_directions_unrolled = u.backend.circuit
+
+runs = C_directions_unrolled.collect_runs(["u1", "u2", "u3"])
+print(runs)
+
+# TODO: complete this pass
+
+# TODO: add Circuit method to tally each type of gate
+# TODO: incorporate these passes into QuantumProgram
+# TODO: test on examples using simulator
