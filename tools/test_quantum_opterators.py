9b8ad039bd0d9ca5934bb7d7dbeac7e7793c1e54
Jay M. Gambetta <jay.gambetta@us.ibm.com>
2017-05-08 16:12:27 -0400

    trying to merge with only part done

--- tools/test_quantum_opterators.py
+++ tools/test_quantum_opterators.py
@@ -1,8 +1,14 @@
 """Test the cost function for different basis functions."""
 import numpy as np
-from operators import gate_qij
+from operators import operator_qi, operator_qij
 
+# single qubit Test
+gate = np.array([[1, 0], [0, -1]])
 
-gate = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]])
-gate_qij(gate, 0, 1, 2)
-print(gate_qij)
+print(operator_qi(gate, 0, 3))
+print(operator_qi(gate, 1, 3))
+print(operator_qi(gate, 2, 3))
+
+cnot = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 1], [0, 0, 1, 0]])
+print(" --------------------")
+print(operator_qij(gate, 0, 1, 2))
