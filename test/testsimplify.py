df239e835885e4a1a898ec83b90de9f38a592892
Jesús Pérez <jesusprubio@gmail.com>
2017-05-11 12:31:05 +0200

    Some constant renaming in tests to make the linter happy.

--- test/testsimplify.py
+++ test/testsimplify.py
@@ -52,8 +52,8 @@ def testu2u2(phi1, lamb1, phi2, lamb2):
                                         lamb2 + math.pi / 2.0)
 
 
-minv = 2.0
-maxv = 0.0
+MIN_V = 2.0
+MAX_V = 0.0
 samples = 50000
 for samp in range(samples):
     phi1 = random.uniform(0.0, 2.0 * math.pi)
@@ -61,14 +61,14 @@ for samp in range(samples):
     lamb1 = random.uniform(0.0, 2.0 * math.pi)
     lamb2 = random.uniform(0.0, 2.0 * math.pi)
     nrm = np.linalg.norm(testu2u2(phi1, lamb1, phi2, lamb2))
-    if nrm < minv:
-        minv = nrm
-    if nrm > maxv:
-        maxv = nrm
-print("u2.u2: samp=%d min=%.15f max=%.15f" % (samples, minv, maxv))
+    if nrm < MIN_V:
+        MIN_V = nrm
+    if nrm > MAX_V:
+        MAX_V = nrm
+print("u2.u2: samp=%d min=%.15f max=%.15f" % (samples, MIN_V, MAX_V))
 
-minv = 2.0
-maxv = 0.0
+MIN_V = 2.0
+MAX_V = 0.0
 for samp in range(samples):
     theta1 = random.uniform(0.0, 2.0 * math.pi)
     theta2 = random.uniform(0.0, 2.0 * math.pi)
@@ -80,14 +80,14 @@ for samp in range(samples):
     nrm = np.linalg.norm(np.dot(u3(theta1, phi1, lamb1),
                                 u3(theta2, phi2, lamb2)) -
                          u3(t, p, l))
-    if nrm < minv:
-        minv = nrm
-    if nrm > maxv:
-        maxv = nrm
-print("u3.u3: samp=%d min=%.15f max=%.15f" % (samples, minv, maxv))
+    if nrm < MIN_V:
+        MIN_V = nrm
+    if nrm > MAX_V:
+        MAX_V = nrm
+print("u3.u3: samp=%d min=%.15f max=%.15f" % (samples, MIN_V, MAX_V))
 
-minv = 2.0
-maxv = 0.0
+MIN_V = 2.0
+MAX_V = 0.0
 for samp in range(samples):
     theta1 = random.uniform(0.0, 2.0 * math.pi)
     theta2 = random.uniform(0.0, 2.0 * math.pi)
@@ -99,14 +99,14 @@ for samp in range(samples):
     nrm = np.linalg.norm(np.dot(u3(theta1, phi1, lamb1),
                                 u3(theta2, phi2, lamb2)) -
                          u3(t, p, l))
-    if nrm < minv:
-        minv = nrm
-    if nrm > maxv:
-        maxv = nrm
-print("u3.u3 (case 1): samp=%d min=%.15f max=%.15f" % (samples, minv, maxv))
+    if nrm < MIN_V:
+        MIN_V = nrm
+    if nrm > MAX_V:
+        MAX_V = nrm
+print("u3.u3 (case 1): samp=%d min=%.15f max=%.15f" % (samples, MIN_V, MAX_V))
 
-minv = 2.0
-maxv = 0.0
+MIN_V = 2.0
+MAX_V = 0.0
 for samp in range(samples):
     theta1 = random.uniform(0.0, 2.0 * math.pi)
     theta2 = -theta1
@@ -118,14 +118,14 @@ for samp in range(samples):
     nrm = np.linalg.norm(np.dot(u3(theta1, phi1, lamb1),
                                 u3(theta2, phi2, lamb2)) -
                          u3(t, p, l))
-    if nrm < minv:
-        minv = nrm
-    if nrm > maxv:
-        maxv = nrm
-print("u3.u3 (case 2, 1): samp=%d min=%.15f max=%.15f" % (samples, minv, maxv))
+    if nrm < MIN_V:
+        MIN_V = nrm
+    if nrm > MAX_V:
+        MAX_V = nrm
+print("u3.u3 (case 2, 1): samp=%d min=%.15f max=%.15f" % (samples, MIN_V, MAX_V))
 
-minv = 2.0
-maxv = 0.0
+MIN_V = 2.0
+MAX_V = 0.0
 for samp in range(samples):
     theta2 = random.uniform(0.0, 2.0 * math.pi)
     theta1 = -theta2
@@ -137,14 +137,14 @@ for samp in range(samples):
     nrm = np.linalg.norm(np.dot(u3(theta1, phi1, lamb1),
                                 u3(theta2, phi2, lamb2)) -
                          u3(t, p, l))
-    if nrm < minv:
-        minv = nrm
-    if nrm > maxv:
-        maxv = nrm
-print("u3.u3 (case 2, 2): samp=%d min=%.15f max=%.15f" % (samples, minv, maxv))
+    if nrm < MIN_V:
+        MIN_V = nrm
+    if nrm > MAX_V:
+        MAX_V = nrm
+print("u3.u3 (case 2, 2): samp=%d min=%.15f max=%.15f" % (samples, MIN_V, MAX_V))
 
-minv = 2.0
-maxv = 0.0
+MIN_V = 2.0
+MAX_V = 0.0
 for samp in range(samples):
     theta1 = random.uniform(0.0, 2.0 * math.pi)
     theta2 = -theta1 + 2 * math.pi
@@ -156,14 +156,14 @@ for samp in range(samples):
     nrm = np.linalg.norm(np.dot(u3(theta1, phi1, lamb1),
                                 u3(theta2, phi2, lamb2)) -
                          u3(t, p, l))
-    if nrm < minv:
-        minv = nrm
-    if nrm > maxv:
-        maxv = nrm
-print("u3.u3 (case 2, 3): samp=%d min=%.15f max=%.15f" % (samples, minv, maxv))
+    if nrm < MIN_V:
+        MIN_V = nrm
+    if nrm > MAX_V:
+        MAX_V = nrm
+print("u3.u3 (case 2, 3): samp=%d min=%.15f max=%.15f" % (samples, MIN_V, MAX_V))
 
-minv = 2.0
-maxv = 0.0
+MIN_V = 2.0
+MAX_V = 0.0
 for samp in range(samples):
     theta2 = random.uniform(0.0, 2.0 * math.pi)
     theta1 = -theta2 + 2.0 * math.pi
@@ -175,14 +175,14 @@ for samp in range(samples):
     nrm = np.linalg.norm(np.dot(u3(theta1, phi1, lamb1),
                                 u3(theta2, phi2, lamb2)) -
                          u3(t, p, l))
-    if nrm < minv:
-        minv = nrm
-    if nrm > maxv:
-        maxv = nrm
-print("u3.u3 (case 2, 4): samp=%d min=%.15f max=%.15f" % (samples, minv, maxv))
+    if nrm < MIN_V:
+        MIN_V = nrm
+    if nrm > MAX_V:
+        MAX_V = nrm
+print("u3.u3 (case 2, 4): samp=%d min=%.15f max=%.15f" % (samples, MIN_V, MAX_V))
 
-minv = 2.0
-maxv = 0.0
+MIN_V = 2.0
+MAX_V = 0.0
 for samp in range(samples):
     theta1 = random.uniform(0.0, 2.0 * math.pi)
     theta2 = -theta1 + math.pi
@@ -194,14 +194,14 @@ for samp in range(samples):
     nrm = np.linalg.norm(np.dot(u3(theta1, phi1, lamb1),
                                 u3(theta2, phi2, lamb2)) -
                          u3(t, p, l))
-    if nrm < minv:
-        minv = nrm
-    if nrm > maxv:
-        maxv = nrm
-print("u3.u3 (case 3, 1): samp=%d min=%.15f max=%.15f" % (samples, minv, maxv))
+    if nrm < MIN_V:
+        MIN_V = nrm
+    if nrm > MAX_V:
+        MAX_V = nrm
+print("u3.u3 (case 3, 1): samp=%d min=%.15f max=%.15f" % (samples, MIN_V, MAX_V))
 
-minv = 2.0
-maxv = 0.0
+MIN_V = 2.0
+MAX_V = 0.0
 for samp in range(samples):
     theta2 = random.uniform(0.0, 2.0 * math.pi)
     theta1 = -theta2 + math.pi
@@ -213,14 +213,14 @@ for samp in range(samples):
     nrm = np.linalg.norm(np.dot(u3(theta1, phi1, lamb1),
                                 u3(theta2, phi2, lamb2)) -
                          u3(t, p, l))
-    if nrm < minv:
-        minv = nrm
-    if nrm > maxv:
-        maxv = nrm
-print("u3.u3 (case 3, 2): samp=%d min=%.15f max=%.15f" % (samples, minv, maxv))
+    if nrm < MIN_V:
+        MIN_V = nrm
+    if nrm > MAX_V:
+        MAX_V = nrm
+print("u3.u3 (case 3, 2): samp=%d min=%.15f max=%.15f" % (samples, MIN_V, MAX_V))
 
-minv = 2.0
-maxv = 0.0
+MIN_V = 2.0
+MAX_V = 0.0
 for samp in range(samples):
     theta1 = random.uniform(0.0, 2.0 * math.pi)
     theta2 = -theta1 + 3 * math.pi
@@ -232,14 +232,14 @@ for samp in range(samples):
     nrm = np.linalg.norm(np.dot(u3(theta1, phi1, lamb1),
                                 u3(theta2, phi2, lamb2)) -
                          u3(t, p, l))
-    if nrm < minv:
-        minv = nrm
-    if nrm > maxv:
-        maxv = nrm
-print("u3.u3 (case 3, 3): samp=%d min=%.15f max=%.15f" % (samples, minv, maxv))
+    if nrm < MIN_V:
+        MIN_V = nrm
+    if nrm > MAX_V:
+        MAX_V = nrm
+print("u3.u3 (case 3, 3): samp=%d min=%.15f max=%.15f" % (samples, MIN_V, MAX_V))
 
-minv = 2.0
-maxv = 0.0
+MIN_V = 2.0
+MAX_V = 0.0
 for samp in range(samples):
     theta2 = random.uniform(0.0, 2.0 * math.pi)
     theta1 = -theta2 + 3.0 * math.pi
@@ -251,8 +251,8 @@ for samp in range(samples):
     nrm = np.linalg.norm(np.dot(u3(theta1, phi1, lamb1),
                                 u3(theta2, phi2, lamb2)) -
                          u3(t, p, l))
-    if nrm < minv:
-        minv = nrm
-    if nrm > maxv:
-        maxv = nrm
-print("u3.u3 (case 3, 4): samp=%d min=%.15f max=%.15f" % (samples, minv, maxv))
+    if nrm < MIN_V:
+        MIN_V = nrm
+    if nrm > MAX_V:
+        MAX_V = nrm
+print("u3.u3 (case 3, 4): samp=%d min=%.15f max=%.15f" % (samples, MIN_V, MAX_V))
