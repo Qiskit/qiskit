a6eb1be826400ff84b84a8c1c3416103749d479e
Jay M. Gambetta <jay.gambetta@us.ibm.com>
2017-05-18 16:38:56 -0400

    adding license info

--- examples/python/teleport.py
+++ examples/python/teleport.py
@@ -1,21 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2017 IBM RESEARCH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
+
 """
 Quantum teleportation example based on an OpenQASM example.
 
 Author: Andrew Cross
         Jesus Perez <jesusper@us.ibm.com>
 """
-
 import sys
 import os
-
 # We don't know from where the user is running the example,
 # so we need a relative position from this file path.
 # TODO: Relative imports for intra-package imports are highly discouraged.
 # http://stackoverflow.com/a/7506006
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 from qiskit import QuantumProgram
-
 import Qconfig
+
 
 ###############################################################
 # Set the device name and coupling map.
