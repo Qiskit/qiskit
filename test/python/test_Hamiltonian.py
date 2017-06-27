d6ad4e34902be134e6a227ec5df47ac636583549
Jay M. Gambetta <jay.gambetta@us.ibm.com>
2017-06-27 07:51:30 -0400

    moving some examples out of tutorial
    cleaning up and moving some examples to their own folder

--- test/python/test_Hamiltonian.py
+++ test/python/test_Hamiltonian.py
@@ -20,16 +20,40 @@
 
 Author: Jay Gambetta
 
-    python test_simulators.py ../qasm/example.qasm qasm_simulator
 
 """
 import numpy as np
 from scipy import linalg as la
 import sys
 sys.path.append("../..")
-from tools.optimizationtools import cost_function, make_Hamiltonian
+from tools.optimizationtools import Energy_Estimate, make_Hamiltonian, Hamiltonian_from_file
+from tools.pauli import Pauli
 
+# printing an example from a H2 file
+print(make_Hamiltonian(Hamiltonian_from_file("H2Equilibrium.txt")))
+
+# printing an example from a graph input
 n = 3
+v0 = np.zeros(n)
+v0[2] = 1
+v1 = np.zeros(n)
+v1[0] = 1
+v1[1] = 1
+v2 = np.zeros(n)
+v2[0] = 1
+v2[2] = 1
+v3 = np.zeros(n)
+v3[1] = 1
+v3[2] = 1
+
+pauli_list = [(1, Pauli(v0, np.zeros(n))), (1, Pauli(v1, np.zeros(n))), (1, Pauli(v2, np.zeros(n))), (1, Pauli(v3, np.zeros(n)))]
+a = make_Hamiltonian(pauli_list)
+print(a)
+
+w, v = la.eigh(a, eigvals=(0, 0))
+print(w)
+print(v)
+
 # cost function
 alpha = np.zeros(n)
 alpha[2] = 1
@@ -38,27 +62,24 @@ beta = np.zeros((n, n))
 beta[0, 1] = 1
 beta[0, 2] = 1
 beta[1, 2] = 1
-a = make_Hamiltonian(n, alpha, beta)
-print(a)
-w, v = la.eigh(a, eigvals=(0, 0))
-print(w)
-print(v)
 data = {'000': 10}
-print(cost_function(data, n, alpha, beta))
+print(Energy_Estimate(data, n, alpha, beta))
 data = {'001': 10}
-print(cost_function(data, n, alpha, beta))
+print(Energy_Estimate(data, n, alpha, beta))
 data = {'010': 10}
-print(cost_function(data, n, alpha, beta))
+print(Energy_Estimate(data, n, alpha, beta))
 data = {'011': 10}
-print(cost_function(data, n, alpha, beta))
+print(Energy_Estimate(data, n, alpha, beta))
 data = {'100': 10}
-print(cost_function(data, n, alpha, beta))
+print(Energy_Estimate(data, n, alpha, beta))
 data = {'101': 10}
-print(cost_function(data, n, alpha, beta))
+print(Energy_Estimate(data, n, alpha, beta))
 data = {'110': 10}
-print(cost_function(data, n, alpha, beta))
+print(Energy_Estimate(data, n, alpha, beta))
 data = {'111': 10}
-print(cost_function(data, n, alpha, beta))
+print(Energy_Estimate(data, n, alpha, beta))
+
+
 """
 [[ 4.+0.j  0.+0.j  0.+0.j  0.+0.j  0.+0.j  0.+0.j  0.+0.j  0.+0.j]
  [ 0.+0.j  0.+0.j  0.+0.j  0.+0.j  0.+0.j  0.+0.j  0.+0.j  0.+0.j]
