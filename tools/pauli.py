7613f3e83e1705052fa178f60b988889a731c3af
ANTONIO MEZZACAPO <amezzac@us.ibm.com>
2017-07-01 02:27:57 -0400

    track phase in product of pauli

--- tools/pauli.py
+++ tools/pauli.py
@@ -101,15 +101,29 @@ def random_pauli(numberofqubits):
     return Pauli(v, w)
 
 def sgn_prod(P1, P2):
-    """Multiply two Paulis P1*P2 and track the sign"""
+    """Multiply two Paulis P1*P2 and track the phase"""
 
     if P1.numberofqubits != P2.numberofqubits:
         print('Paulis cannot be multiplied - different number of qubits')
     vnew = (P1.v + P2.v) % 2
     wnew = (P1.w + P2.w) % 2
     paulinew = Pauli(vnew, wnew)
-    sgn = np.inner(P1.w, P2.v) % 2
-    return paulinew, sgn
+    phase=1
+    for i in range(len(P1.v)):
+        if P1.v[i]==1 and P1.w[i]==0 and P2.v[i]==0 and P2.w[i]==1:  # Z*X
+            phase=1j*phase
+        if P1.v[i]==0 and P1.w[i]==1 and P2.v[i]==1 and P2.w[i]==0:  # X*Z
+            phase=-1j*phase
+        if P1.v[i]==0 and P1.w[i]==1 and P2.v[i]==1 and P2.w[i]==1:  # X*Y    
+            phase=1j*phase
+        if P1.v[i]==1 and P1.w[i]==1 and P2.v[i]==0 and P2.w[i]==1:  # Y*X
+            phase=-1j*phase
+        if P1.v[i]==1 and P1.w[i]==1 and P2.v[i]==1 and P2.w[i]==0:  # Y*Z
+            phase=1j*phase
+        if P1.v[i]==1 and P1.w[i]==0 and P2.v[i]==1 and P2.w[i]==1:  # Z*Y  
+            phase=-1j*phase
+            
+    return paulinew, phase
 
 def inverse_pauli(other):
     """Return the inverse of a Pauli."""
