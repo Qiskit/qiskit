01aecbfbf91305ec976151e87ce7287703c8bc40
Jay M. Gambetta <jay.gambetta@us.ibm.com>
2017-07-12 22:48:13 -0400

    adding visualization and tomo temp for antonio

--- tools/tomography.py
+++ tools/tomography.py
@@ -21,6 +21,7 @@ Author: Christopher J. Wood <cjwood@us.ibm.com>
 """
 import numpy as np
 from functools import reduce
+from scipy import linalg as la
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import proj3d
 from matplotlib.patches import FancyArrowPatch
@@ -77,100 +78,6 @@ def bit_string_index(s):
     assert s.count("0") == n - k, "s must be a string of 0 and 1"
     ones = [pos for pos, char in enumerate(s) if char == "1"]
     return lex_index(n, k, ones)
-
-
-def plot_qsphere(state, number_of_qubits):
-    """Plot the qsphere of data."""
-    # default setting for qsphere
-    fig = plt.figure(figsize=(10, 10))
-    ax = fig.add_subplot(111, projection='3d')
-    ax.axes.set_xlim3d(-1.0, 1.0)
-    ax.axes.set_ylim3d(-1.0, 1.0)
-    ax.axes.set_zlim3d(-1.0, 1.0)
-    ax.set_aspect("equal")
-    ax.axes.grid(False)
-    # Plot semi-transparent sphere
-    u = np.linspace(0, 2 * np.pi, 25)
-    v = np.linspace(0, np.pi, 25)
-    x = np.outer(np.cos(u), np.sin(v))
-    y = np.outer(np.sin(u), np.sin(v))
-    z = np.outer(np.ones(np.size(u)), np.cos(v))
-    ax.plot_surface(x, y, z, rstride=1, cstride=1, color='k', alpha=0.05,
-                    linewidth=0)
-    # wireframe
-    # Get rid of the panes
-    ax.w_xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-    ax.w_yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-    ax.w_zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
-
-    # Get rid of the spines
-    ax.w_xaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
-    ax.w_yaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
-    ax.w_zaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
-    # Get rid of the ticks
-    ax.set_xticks([])
-    ax.set_yticks([])
-    ax.set_zticks([])
-
-    d = number_of_qubits
-    for i in range(2**number_of_qubits):
-        # get x,y,z points
-        element = bin(i)[2:].zfill(number_of_qubits)
-        weight = element.count("1")
-        zvalue = -2 * weight / d + 1
-        number_of_divisions = n_choose_k(d, weight)
-        weight_order = bit_string_index(element)
-        #if weight_order >= number_of_divisions / 2:
-        #    com_key = compliment(element)
-        #    weight_order_temp = bit_string_index(com_key)
-        #    weight_order = np.floor(
-        #        number_of_divisions / 2) + weight_order_temp + 1
-        angle = (weight_order) * 2 * np.pi / number_of_divisions
-        xvalue = np.sqrt(1 - zvalue**2) * np.cos(angle)
-        yvalue = np.sqrt(1 - zvalue**2) * np.sin(angle)
-        ax.plot([xvalue], [yvalue], [zvalue], markerfacecolor=(.5,.5,.5), markeredgecolor=(.5,.5,.5), marker='o', markersize=10, alpha=1)
-        # get prob and angle - prob will be shade and angle color
-        prob = np.real(np.dot(state[i], state[i].conj()))
-        angleround = int(np.angle(state[i])/(2*np.pi)*12)
-        if angleround == 4:
-            colorstate = (1, 0, 0)
-        elif angleround == 5:
-            colorstate = (1, 0.5, 0)
-        elif angleround == 6:
-            colorstate = (1, 1, 0)
-        elif angleround == 7:
-            colorstate = (0.5, 1, 0)
-        elif angleround == 8:
-            colorstate = (0, 1, 0)
-        elif angleround == 9:
-            colorstate = (0, 1, 0.5)
-        elif angleround == 10:
-            colorstate = (0, 1, 1)
-        elif angleround == 11:
-            colorstate = (0, 0.5, 1)
-        elif angleround == 0:
-            colorstate = (0, 0, 1)
-        elif angleround == 1:
-            colorstate = (0.5, 0, 1)
-        elif angleround == 2:
-            colorstate = (1, 0, 1)
-        elif angleround == 3:
-            colorstate = (1, 0, 0.5)
-        # print("outcome = " + element + " weight " + str(weight) + " angle " + str(angle) + " amp " + str(linewidth))
-        a = Arrow3D([0, xvalue], [0, yvalue], [0, zvalue], mutation_scale=20,
-                    alpha=prob, arrowstyle="-", color=colorstate, lw = 10)
-        ax.add_artist(a)
-    # add weight lines
-    for weight in range(d + 1):
-        theta = np.linspace(-2 * np.pi, 2 * np.pi, 100)
-        z = -2 * weight / d + 1
-        r = np.sqrt(1 - z**2)
-        x = r * np.cos(theta)
-        y = r * np.sin(theta)
-        ax.plot(x, y, z, color=(.5,.5,.5))
-    # add center point
-    ax.plot([0], [0], [0], markerfacecolor=(.5,.5,.5), markeredgecolor=(.5,.5,.5), marker='o', markersize=10, alpha=1)
-    plt.show()
 
 
 def plot_bloch_vector(bloch, number_of_qubits, title=""):
@@ -294,7 +201,332 @@ def plot_state(rho, number_of_qubits, method='city'):
             ax.set_xlabel('Pauli', fontsize=12)
             ax.set_ylim([-1, 1])
             plt.show()
+        elif method == "qsphere":
+            """Plot the qsphere."""
+            # default setting for qsphere
+            we, state = la.eigh(rho, eigvals=(2**number_of_qubits-1, 2**number_of_qubits-1))
+            # print(we)
+            state.flatten()
+            fig = plt.figure(figsize=(10, 10))
+            ax = fig.add_subplot(111, projection='3d')
+            ax.axes.set_xlim3d(-1.0, 1.0)
+            ax.axes.set_ylim3d(-1.0, 1.0)
+            ax.axes.set_zlim3d(-1.0, 1.0)
+            ax.set_aspect("equal")
+            ax.axes.grid(False)
+            # Plot semi-transparent sphere
+            u = np.linspace(0, 2 * np.pi, 25)
+            v = np.linspace(0, np.pi, 25)
+            x = np.outer(np.cos(u), np.sin(v))
+            y = np.outer(np.sin(u), np.sin(v))
+            z = np.outer(np.ones(np.size(u)), np.cos(v))
+            ax.plot_surface(x, y, z, rstride=1, cstride=1, color='k', alpha=0.05,
+                            linewidth=0)
+            # wireframe
+            # Get rid of the panes
+            ax.w_xaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+            ax.w_yaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+            ax.w_zaxis.set_pane_color((1.0, 1.0, 1.0, 0.0))
+
+            # Get rid of the spines
+            ax.w_xaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
+            ax.w_yaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
+            ax.w_zaxis.line.set_color((1.0, 1.0, 1.0, 0.0))
+            # Get rid of the ticks
+            ax.set_xticks([])
+            ax.set_yticks([])
+            ax.set_zticks([])
+
+            d = number_of_qubits
+            for i in range(2**number_of_qubits):
+                # get x,y,z points
+                element = bin(i)[2:].zfill(number_of_qubits)
+                weight = element.count("1")
+                zvalue = -2 * weight / d + 1
+                number_of_divisions = n_choose_k(d, weight)
+                weight_order = bit_string_index(element)
+                #if weight_order >= number_of_divisions / 2:
+                #    com_key = compliment(element)
+                #    weight_order_temp = bit_string_index(com_key)
+                #    weight_order = np.floor(
+                #        number_of_divisions / 2) + weight_order_temp + 1
+                angle = (weight_order) * 2 * np.pi / number_of_divisions
+                xvalue = np.sqrt(1 - zvalue**2) * np.cos(angle)
+                yvalue = np.sqrt(1 - zvalue**2) * np.sin(angle)
+                ax.plot([xvalue], [yvalue], [zvalue], markerfacecolor=(.5,.5,.5), markeredgecolor=(.5,.5,.5), marker='o', markersize=10, alpha=1)
+                # get prob and angle - prob will be shade and angle color
+                prob = np.real(np.dot(state[i], state[i].conj()))
+                angleround = int(((np.angle(state[i])-np.angle(state[0])) % 2*np.pi)/(2*np.pi)*12)
+                if angleround == 4:
+                    colorstate = (1, 0, 0)
+                elif angleround == 5:
+                    colorstate = (1, 0.5, 0)
+                elif angleround == 6:
+                    colorstate = (1, 1, 0)
+                elif angleround == 7:
+                    colorstate = (0.5, 1, 0)
+                elif angleround == 8:
+                    colorstate = (0, 1, 0)
+                elif angleround == 9:
+                    colorstate = (0, 1, 0.5)
+                elif angleround == 10:
+                    colorstate = (0, 1, 1)
+                elif angleround == 11:
+                    colorstate = (0, 0.5, 1)
+                elif angleround == 0:
+                    colorstate = (0, 0, 1)
+                elif angleround == 1:
+                    colorstate = (0.5, 0, 1)
+                elif angleround == 2:
+                    colorstate = (1, 0, 1)
+                elif angleround == 3:
+                    colorstate = (1, 0, 0.5)
+                # print("outcome = " + element + " weight " + str(weight) + " angle " + str(angle) + " amp " + str(linewidth))
+                a = Arrow3D([0, xvalue], [0, yvalue], [0, zvalue], mutation_scale=20,
+                            alpha=prob, arrowstyle="-", color=colorstate, lw = 10)
+                ax.add_artist(a)
+            # add weight lines
+            for weight in range(d + 1):
+                theta = np.linspace(-2 * np.pi, 2 * np.pi, 100)
+                z = -2 * weight / d + 1
+                r = np.sqrt(1 - z**2)
+                x = r * np.cos(theta)
+                y = r * np.sin(theta)
+                ax.plot(x, y, z, color=(.5,.5,.5))
+            # add center point
+            ax.plot([0], [0], [0], markerfacecolor=(.5,.5,.5), markeredgecolor=(.5,.5,.5), marker='o', markersize=10, alpha=1)
+            plt.show()
         else:
             print("No method given")
     else:
         print("size of rho is not equal to number of qubits")
+
+
+###############################################################
+# Constructing tomographic measurement circuits
+###############################################################
+def build_keys_helper(keys, qubit):
+    """
+    Returns array of measurement strings ['Xj', 'Yj', 'Zj'] for qubit=j.
+    """
+    tmp = []
+    for k in keys:
+        for b in ["X","Y","Z"]:
+            tmp.append(k + b + str(qubit))
+    return tmp
+
+def build_tomo_keys(circuit, qubit_list):
+    """
+    For input circuit string returns an array of all measurement circits orded in
+    lexicographic order from last qubit to first qubit.
+    Example:
+    qubit_list = [0]: [circuitX0, circuitY0, circuitZ0].
+    qubit_list = [0,1]: [circuitX1X0, circuitX1Y0, circuitX1Z0, circuitY1X0,..., circuitZ1Z0].
+    """
+    keys = [circuit]
+    for j in sorted(qubit_list,reverse=True):
+        keys = build_keys_helper(keys, j)
+    return keys
+
+# We need to build circuits in QASM lexical order, not standard!
+
+def build_tomo_circuit_helper(Q_program, circuits, qreg: str, creg: str, qubit: int):
+    """
+    Adds measurements for the qubit=j to the input circuits, so if circuits = [c0, c1,...]
+    circuits-> [c0Xj, c0Yj, c0Zj, c1Xj,...]
+    """
+    for c in circuits:
+        circ = Q_program.get_circuit(c)
+        for b in ["X","Y","Z"]:
+            meas = b+str(qubit)
+            tmp = Q_program.create_circuit(meas, [qreg],[creg])
+            qr = Q_program.get_quantum_registers(qreg)
+            cr = Q_program.get_classical_registers(creg)
+            if b == "X":
+                tmp.u2(0., np.pi, qr[qubit])
+            if b == "Y":
+                tmp.u2(0., np.pi / 2., qr[qubit])
+            tmp.measure(qr[qubit], cr[qubit])
+            Q_program.add_circuit(c+meas, circ + tmp)
+
+def build_tomo_circuits(Q_program, circuit, qreg: str, creg: str, qubit_list):
+    """
+    Generates circuits in the input QuantumProgram for implementing complete quantum
+    state tomography of the state of the qubits in qubit_list prepared by circuit.
+    """
+    circ = [circuit]
+    for j in sorted(qubit_list, reverse=True):
+        build_tomo_circuit_helper(Q_program, circ, qreg, creg, j)
+        circ = build_keys_helper(circ, j)
+
+
+###############################################################
+# Tomographic Reconstruction functions.
+###############################################################
+
+def vectorize(op):
+    """
+    Flatten an operator to a column-major vector.
+    """
+    return op.flatten(order='F')
+
+def devectorize(v):
+    """
+    Devectorize a column-major vectorized square matrix.
+    """
+    d = int(np.sqrt(v.size));
+    return v.reshape(d,d, order='F')
+
+def meas_basis_matrix(meas_basis):
+    """
+    Returns a matrix of vectorized measurement operators S = sum_j |j><M_j|.
+    """
+    n = len(meas_basis)
+    d = meas_basis[0].size
+    S = np.array([vectorize(m).conj() for m in meas_basis])
+    return S.reshape(n,d)
+
+def is_pos_def(x):
+    return np.all(np.linalg.eigvals(x) > 0)
+
+def outer(v1, v2=None):
+    """
+    Returns the matrix |v1><v2| resulting from the outer product of two vectors.
+    """
+    if v2 is None:
+        u = v1.conj()
+    else:
+        u = v2.conj()
+    return np.outer(v1, u)
+
+def wizard(rho, normalize_flag = True, epsilon = 0.):
+    """
+    Maps an operator to the nearest positive semidefinite operator
+    by setting negative eigenvalues to zero and rescaling the positive
+    eigenvalues.
+    See arXiv:1106.5458 [quant-ph]
+    """
+    #print("Using wizard method to constrain positivity")
+    if normalize_flag:
+        rho = rho / np.trace(rho)
+    dim = len(rho)
+    rho_wizard = np.zeros([dim, dim])
+    v, w = np.linalg.eigh(rho) # v eigenvecrors v[0] < v[1] <...
+    for j in range(dim):
+        if v[j] < epsilon:
+            tmp = v[j]
+            v[j] = 0.
+            # redistribute loop
+            x = 0.
+            for k in range(j+1,dim):
+                x += tmp / (dim-(j+1))
+                v[k] = v[k] + tmp / (dim -(j+1))
+    for j in range(dim):
+        rho_wizard = rho_wizard + v[j] * outer(w[:,j])
+    return rho_wizard
+
+def fit_state(freqs, meas_basis, weights=None, normalize_flag = True, wizard_flag = False):
+    """
+    Returns a matrix reconstructed by unconstrained least-squares fitting.
+    """
+    if weights is None:
+        W = np.eye(len(freqs)) # use uniform weights
+    else:
+        W = np.array(np.diag(weights))
+    S = np.dot(W, meas_basis_matrix(meas_basis)) # actually W.S
+    v = np.dot(W, freqs) # W|f>
+    v = np.array(np.dot(S.T.conj(), v)) # S^*.W^*W.|f>
+    inv = np.linalg.pinv(np.dot(S.T.conj(), S)) # pseudo inverse of  S^*.W^*.W.S
+    v = np.dot(inv, v) # |rho>
+    rho = devectorize(v)
+    if normalize_flag:
+        rho = rho / np.trace(rho)
+    if wizard_flag:
+        #rho_wizard = wizard(rho,normalize_flag)
+        rho = wizard(rho, normalize_flag=normalize_flag)
+    return rho
+
+
+
+###############################################################
+# Parsing measurement data for reconstruction
+###############################################################
+
+def nqubit_basis(n):
+    """
+    Returns the measurement basis for n-qubits in the correct order for the
+    meas_outcome_strings function.
+    """
+    b1 =  np.array([
+            np.array([[0.5, 0.5], [0.5, 0.5]]), np.array([[0.5, -0.5], [-0.5, 0.5]]), # Xp, Xm
+            np.array([[0.5, -0.5j], [0.5j, 0.5]]), np.array([[0.5, 0.5j], [-0.5j, 0.5]]), # Yp, Ym
+            np.array([[1, 0], [0, 0]]), np.array([[0, 0], [0, 1]]), # Zp, Zm
+            ])
+    if n == 1:
+        return b1
+    else:
+        bnm1 = nqubit_basis(n-1)
+        m = 2**(n-1)
+        d = bnm1.size // m**3
+        return np.kron( bnm1.reshape(d, m, m, m), b1.reshape(3, 2, 2, 2)).reshape(3*d * 2*m, 2*m, 2*m)
+
+def meas_outcome_strings(nq):
+    """
+    Returns a list of the measurement outcome strings for nq qubits.
+    """
+    return [bin(j)[2:].zfill(nq) for j in range(2**nq)]
+
+def tomo_outcome_strings(meas_qubits,nq=None):
+    """
+    Returns a list of the measurement outcome strings for meas_qubits qubits in an
+    nq qubit system.
+    """
+    if nq is None:
+        nq = len(meas_qubits)
+    qs = sorted(meas_qubits, reverse=True)
+    outs = meas_outcome_strings(len(qs))
+    res = [];
+    for s in outs:
+        label = ""
+        for j in range(nq):
+            if j in qs:
+                label = s[qs.index(j)] + label
+            else:
+                label = str(0) + label
+        res.append(label)
+    return res
+
+def none_to_zero(val):
+    """
+    Returns 0 if the input argument is None, else it returns the input.
+    """
+    if val is None:
+        return 0
+    else:
+        return val
+
+###############################################################
+# Putting it all together
+###############################################################
+def state_tomography(Q_program, tomo_circuits, shots, nq,
+                                    meas_qubits, method='leastsq_wizard'):
+    """
+    Returns the reconstructed density matrix.
+    """
+    m = len(meas_qubits)
+    counts = np.array([none_to_zero(Q_program.get_counts(c).get(s))
+                        for c in tomo_circuits
+                        for s in tomo_outcome_strings(meas_qubits, nq)])
+    if method == 'leastsq_wizard':
+        return fit_state(counts / shots, nqubit_basis(m), normalize_flag=True, wizard_flag=True)
+    elif method == 'least_sq':
+        return fit_state(counts / shots, nqubit_basis(m), normalize_flag=True, wizard_flag=False)
+    else:
+        print("error: unknown reconstruction method")
+
+def fidelity(rho, psi):
+    """
+    Returns the state fidelity F between a density matrix rho
+    and a target pure state psi.
+    """
+    return np.sqrt(np.abs(np.dot(psi, np.dot(rho, psi))))
