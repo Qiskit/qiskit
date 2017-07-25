f4000a629d97c291541de9f73c31d49d303b87fe
Andrew Cross <awcross@us.ibm.com>
2017-07-24 20:44:36 -0400

    Update __init__ to remove oneregisterbackend.

--- qiskit/unroll/__init__.py
+++ qiskit/unroll/__init__.py
@@ -1,6 +1,5 @@
 from ._unroller import Unroller
 from ._dagbackend import DAGBackend
-from ._oneregisterbackend import OneRegisterBackend
 from ._printerbackend import PrinterBackend
 from ._unrollerbackend import UnrollerBackend
 from ._backendexception import BackendException
