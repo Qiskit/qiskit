8344f6b21e30acb9f4752378c8515dc66409d353
Diego M. Rodriguez <diego.plan9@gmail.com>
2017-10-03 16:21:30 +0200

    Add register_backend(), reorganize utils, tests
    Add "qiskit.backends._backendutils.register_backend()" as the main
    function for registering a local backend, including in turn:
    * revise the autodiscovery function "discover_sdk_backends()", making
      use of "register_backend()" and dropping compatibility with old-style
      definition of backends (ie. using a "__configuration" module
      variable".
    * merge the module-level variables "_backend_classes" and
      "_backend_configurations" into a single "_REGISTERED_BACKENDS" dict
      that uses namedtuples, for consistency.
    * update "local_backends()" and "remote_backends()" to make use of the
      new merged variable.
    * reorganize the "qiskit.backend.__init__" imports to expose only the
      methods needed by the user.
    * update the tests and add tests for "register_backend()".

--- qiskit/backends/__init__.py
+++ qiskit/backends/__init__.py
@@ -1,8 +1,6 @@
-from ._backendutils import (update_implemented_backends,
-                            _backend_classes,
-                            find_runnable_backends,
-                            get_backend_class,
+from ._basebackend import BaseBackend
+from ._backendutils import (get_backend_class,
                             get_backend_configuration,
-                            local_backends)
-_backend_classes = update_implemented_backends()
-runnable_backends = find_runnable_backends(_backend_classes)
+                            local_backends,
+                            register_backend,
+                            remote_backends)
