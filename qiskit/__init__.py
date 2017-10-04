b1e5672ecdc05e9fd5aa91f838b09a98dc6ad4f1
Jessica Wilson <jessicalostinspace@gmail.com>
2017-10-04 08:27:04 -0700

    Removing init file changes since build broken

--- qiskit/__init__.py
+++ qiskit/__init__.py
@@ -1,9 +1,5 @@
-"""
-Imports file dependencies
-"""
 from IBMQuantumExperience import RegisterSizeError
 
-import qiskit.extensions.standard
 from ._qiskiterror import QISKitError
 from ._classicalregister import ClassicalRegister
 from ._quantumregister import QuantumRegister
@@ -12,6 +8,7 @@ from ._gate import Gate
 from ._compositegate import CompositeGate
 from ._instruction import Instruction
 from ._instructionset import InstructionSet
+import qiskit.extensions.standard
 from ._jobprocessor import JobProcessor
 from ._quantumjob import QuantumJob
 from ._quantumprogram import QuantumProgram
