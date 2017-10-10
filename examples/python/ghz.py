768580dfe4666d713b2d29f292e50a77aa875288
Jessica Wilson <jessicalostinspace@gmail.com>
2017-10-09 22:45:06 -0700

    Changed docstring layout

--- examples/python/ghz.py
+++ examples/python/ghz.py
@@ -22,9 +22,9 @@ GHZ state example illustrating mapping onto the backend.
 import sys
 import os
 
-# We don't know from where the user is running the example, so we need a
-# relative position from this file path. TODO: Relative imports for intra-
-# package imports are highly discouraged. http://stackoverflow.com/a/7506006
+ -# We don't know from where the user is running the example,
+ -# so we need a relative position from this file path.
+ -# TODO: Relative imports for intra-package imports are highly discouraged.
 sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
 from qiskit import QuantumProgram
 
