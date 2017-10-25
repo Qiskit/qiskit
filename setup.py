b2d6e702292d5a0313d65ab7f3ac5ccdd58e0249
Diego M. Rodríguez <diego.plan9@gmail.com>
2017-10-25 16:12:04 +0200

    Check IBMQuantumExperience version at runtime (#115)
    * Add runtime check for IBMQuantumExperience version
    
    Add `qiskit._util._check_ibmqe_version()` (invoked on the main import)
    for emitting a warning if the installed version of IBMQuantumExperience
    does not match the version defined on `setup.py`.
    
    * Rename requires.txt and requires-dev.txt
    
    Rename to "requirements.txt" and "requirements-dev.txt", in order to
    conform better to the de-facto practices.
    Update the requirements to match the versions specified in setup.py.
    
    * setup.py: OS classifiers, backend, IBMQE version
    
    * Explicitly require IBMQuantumExperience>=1.8.13, as older versions
      might not contain all the functionality needed.
    * Include "qiskit.backends" as a package instead of "qiskit.simulators".
    * Add trove classifiers for the operative systems supported.
    * Fix PEP8 warnings for long description.
    
    * Avoid using __version__ in setup.py due to imports
    
    Revert the change of using "__version__" imported from "__init__.py"
    directly on "setup.py", as the current importing of the modules from
    "__init__.py" causes the building to fail due to the dependencies not
    being available at the time of invocation.

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
 from setuptools import setup
-from qiskit import __version__
-
 
 packages = ["qiskit",
+            "qiskit.backends",
             "qiskit.dagcircuit",
             "qiskit.extensions",
             "qiskit.extensions.standard",
             "qiskit.mapper",
             "qiskit.qasm",
             "qiskit.qasm._node",
-            "qiskit.simulators",
             "qiskit.unroll",
             "qiskit.tools",
             "qiskit.tools.apps",
             "qiskit.tools.qcvv",
             "qiskit.tools.qi"]
 
-requirements = ["IBMQuantumExperience>=1.8",
+requirements = ["IBMQuantumExperience>=1.8.13",
                 "requests>=2.18,<2.19",
                 "networkx>=1.11,<1.12",
                 "ply==3.10",
@@ -27,10 +25,11 @@ requirements = ["IBMQuantumExperience>=1.8",
 
 setup(
     name="qiskit",
-    version=__version__,
+    version="0.4.0",
     description="Software for developing quantum computing programs",
-    long_description="""QISKit is a software development kit for writing quantum
-computing experiments, programs, and applications. Works with Python 3.5 and 3.6""",
+    long_description="""QISKit is a software development kit for writing
+        quantum computing experiments, programs, and applications. Works with
+        Python 3.5 and 3.6""",
     url="https://github.com/QISKit/qiskit-sdk-py",
     author="QISKit Development Team",
     author_email="qiskit@us.ibm.com",
@@ -40,6 +39,9 @@ computing experiments, programs, and applications. Works with Python 3.5 and 3.6
         "License :: OSI Approved :: Apache Software License",
         "Intended Audience :: Developers",
         "Intended Audience :: Science/Research",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX :: Linux",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Topic :: Scientific/Engineering",
