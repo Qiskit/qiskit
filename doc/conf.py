a4bf8293eb3e3ecab7ec15a87fd2188c58e77178
Diego M. Rodriguez <diego.plan9@gmail.com>
2017-11-08 17:16:02 +0100

    Update Make and structure to allow multiple langs
    Update the Makefile "doc" target to build both the English and the
    Japanese versions, in separate directories, and the "clean" target to
    cleanup the autodoc documentation.
    Add a "conf.py" file to the "doc/ja" folder that modifies the relevant
    variables, as Sphinx uses that folder as the root document folder when
    building the Japanese version.

--- doc/conf.py
+++ doc/conf.py
@@ -99,7 +99,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '_autodoc/modules.rst']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '_autodoc/modules.rst', 'ja']
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
