f5eb32922c12711fc02ba2e8552f1e454e8bd438
Luciano Bello <luciano.bello@ibm.com>
2017-11-17 11:50:06 -0500

    test description

--- test/python/test_mapper.py
+++ test/python/test_mapper.py
@@ -21,6 +21,7 @@ import unittest
 from qiskit import QuantumProgram
 from .common import QiskitTestCase
 
+
 class MapperTest(QiskitTestCase):
     """Test the mapper."""
 
@@ -31,22 +32,30 @@ class MapperTest(QiskitTestCase):
     def tearDown(self):
         pass
 
-    def test_issue_81(self):
-        # https://github.com/QISKit/qiskit-sdk-py/issues/81
+    def test_mapper_overoptimization(self):
+        """
+        The mapper should not change the semantics of the input. An overoptimization introduced
+        the issue #81: https://github.com/QISKit/qiskit-sdk-py/issues/81 
+        """
         self.qp.load_qasm_file(self._get_resource_path('qasm/issue_81.qasm'), name='test')
         coupling_map = {0: [2], 1: [2], 2: [3], 3: []}
         result1 = self.qp.execute(["test"], backend="local_qasm_simulator", coupling_map=coupling_map)
         count1 = result1.get_counts("test")
         result2 = self.qp.execute(["test"], backend="local_qasm_simulator", coupling_map=None)
         count2 = result2.get_counts("test")
-        self.assertEqual(count1.keys(), count2.keys(),)
+        self.assertEqual(count1.keys(), count2.keys(), )
 
-    def test_issue_111(self):
+    def test_math_domain_error(self):
+        """
+        The math library operates over floats and introduce floating point errors that should be avoid
+        See: https://github.com/QISKit/qiskit-sdk-py/issues/111
+        """
         # https://github.com/QISKit/qiskit-sdk-py/issues/111
         self.qp.load_qasm_file(self._get_resource_path('qasm/issue_111.qasm'), name='test')
         coupling_map = {0: [2], 1: [2], 2: [3], 3: []}
         result1 = self.qp.execute(["test"], backend="local_qasm_simulator", coupling_map=coupling_map, seed=self.seed)
         self.assertEqual(result1.get_counts("test"), {'0001': 507, '0101': 517})
+
 
 if __name__ == '__main__':
     unittest.main()
