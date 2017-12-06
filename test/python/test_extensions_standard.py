1b6c7e81be1a954dc057d87c09b9ee7276ef19b2
Luciano Bello <luciano.bello@ibm.com>
2017-12-06 18:19:30 -0500

    testing ToffoliGate, CHGate, CrzGate, Barrier, HGate, XGate, YGate, ZGate

---
+++ test/python/test_extensions_standard.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=invalid-name,missing-docstring
+
+# Copyright 2017 IBM RESEARCH. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# =============================================================================
+
+import unittest
+
+from qiskit import ClassicalRegister, QuantumCircuit, QuantumRegister
+from qiskit._qiskiterror import QISKitError
+from qiskit.extensions.standard.ccx import ToffoliGate
+from qiskit.extensions.standard.ch import CHGate
+from qiskit.extensions.standard.crz import CrzGate
+from qiskit.extensions.standard.barrier import Barrier
+from qiskit.extensions.standard.h import HGate
+from qiskit.extensions.standard.x import XGate
+from qiskit.extensions.standard.y import YGate
+from qiskit.extensions.standard.z import ZGate
+
+from .common import QiskitTestCase
+
+
+class TestStandard(QiskitTestCase):
+    """Standard Extension Test."""
+
+    def setUp(self):
+        self.q = QuantumRegister("q", 3)
+        self.c = ClassicalRegister("c", 3)
+        self.circuit = QuantumCircuit(self.q, self.c)
+
+
+    def test_barrier(self):
+        c = self.circuit
+        self.assertRaises(QISKitError, c.barrier, self.c[0])
+        c.barrier(self.q[1])
+        self.assertEqual(type(c[0]), Barrier)
+        self.assertIn('barrier q[1];', c.qasm())
+        self.assertNotIn('barrier q[0];', c.qasm())
+        c[0].reapply(c)
+        self.assertIn('barrier q[1];\nbarrier q[1];', c.qasm())
+        self.assertEqual(c[0].inverse(), c[0])  
+        self.assertIn('barrier q[1];\nbarrier q[1];', c.qasm())
+
+
+    def test_ccx(self):
+        c = self.circuit
+        self.assertRaises(QISKitError, c.ccx, self.c[0],self.c[1],self.c[2])
+        self.assertRaises(QISKitError, c.ccx, self.q[0], self.q[0], self.q[2])
+        c.ccx(self.q[0],self.q[1],self.q[2])
+        self.assertEqual(type(c[0]), ToffoliGate)
+        self.assertIn('ccx q[0],q[1],q[2];', c.qasm())
+        c[0].reapply(c)
+        self.assertIn('ccx q[0],q[1],q[2];\nccx q[0],q[1],q[2];', c.qasm())
+        self.assertEqual(c[0].inverse(), c[0])  
+        self.assertIn('ccx q[0],q[1],q[2];\nccx q[0],q[1],q[2];', c.qasm())
+
+
+    def test_ch(self):
+        c = self.circuit
+        self.assertRaises(QISKitError, c.ch, self.c[0],self.c[1])
+        self.assertRaises(QISKitError, c.ch, self.q[0], self.q[0])
+        c.ch(self.q[0],self.q[1])
+        self.assertEqual(type(c[0]), CHGate)
+        self.assertIn('ch q[0],q[1];', c.qasm())
+        c[0].reapply(c)
+        self.assertIn('ch q[0],q[1];\nch q[0],q[1];', c.qasm())
+        self.assertEqual(c[0].inverse(), c[0])  
+        self.assertIn('ch q[0],q[1];\nch q[0],q[1];', c.qasm())
+
+    def test_crz(self):
+        c = self.circuit
+        self.assertRaises(QISKitError, c.crz, 0, self.c[0],self.c[1])
+        self.assertRaises(QISKitError, c.crz, 0, self.q[0], self.q[0])
+        #TODO self.assertRaises(QISKitError, c.crz, 0, 0, self.q[0])
+        c.crz(1,self.q[0],self.q[1])
+        self.assertEqual(type(c[0]), CrzGate)
+        print(c.qasm())
+        self.assertIn('crz(1.000000000000000) q[0],q[1];', c.qasm())
+        c[0].reapply(c)
+        self.assertIn('crz(1.000000000000000) q[0],q[1];\ncrz(1.000000000000000) q[0],q[1];', c.qasm())
+        self.assertEqual(c[0].inverse(), c[0])  
+        self.assertIn('crz(-1.000000000000000) q[0],q[1];\ncrz(1.000000000000000) q[0],q[1];', c.qasm())
+
+    def test_h(self):
+        c = self.circuit
+        self.assertRaises(QISKitError, c.h, self.c[0])
+        c.h(self.q[1])
+        self.assertEqual(type(c[0]), HGate)
+        self.assertIn('h q[1];', c.qasm())
+        self.assertNotIn('h q[0];', c.qasm())
+        c[0].reapply(c)
+        self.assertIn('h q[1];\nh q[1];', c.qasm())
+        self.assertEqual(c[0].inverse(), c[0])  
+        self.assertIn('h q[1];\nh q[1];', c.qasm())
+
+    def test_x(self):
+        c = self.circuit
+        self.assertRaises(QISKitError, c.x, self.c[0])
+        c.x(self.q[1])
+        self.assertEqual(type(c[0]), XGate)
+        self.assertIn('x q[1];', c.qasm())
+        self.assertNotIn('x q[0];', c.qasm())
+        c[0].reapply(c)
+        self.assertIn('x q[1];\nx q[1];', c.qasm())
+        self.assertEqual(c[0].inverse(), c[0])  
+        self.assertIn('x q[1];\nx q[1];', c.qasm())
+
+    def test_y(self):
+        c = self.circuit
+        self.assertRaises(QISKitError, c.y, self.c[0])
+        c.y(self.q[1])
+        self.assertEqual(type(c[0]), YGate)
+        self.assertIn('y q[1];', c.qasm())
+        self.assertNotIn('y q[0];', c.qasm())
+        c[0].reapply(c)
+        self.assertIn('y q[1];\ny q[1];', c.qasm())
+        self.assertEqual(c[0].inverse(), c[0])  
+        self.assertIn('y q[1];\ny q[1];', c.qasm())
+
+
+    def test_z(self):
+        c = self.circuit
+        c.z(self.q[1])
+        self.assertEqual(type(c[0]), ZGate)
+        self.assertIn('z q[1];', c.qasm())
+        self.assertNotIn('z q[0];', c.qasm())
+        c[0].reapply(c)
+        self.assertIn('z q[1];\nz q[1];', c.qasm())
+        self.assertEqual(c[0].inverse(), c[0])  
+        self.assertIn('z q[1];\nz q[1];', c.qasm())
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
