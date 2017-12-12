e4d2ec6019b5077fe2105f572a714fe0c8c6d348
Peng Liu <liup@us.ibm.com>
2017-12-12 15:14:02 -0500

    equivalence checker added

---
+++ qiskit/tools/equivalence_checker/circuit_builder.py
@@ -0,0 +1,18 @@
+
+from qiskit import qasm, unroll
+from DAGBACKEND_FIX import DAGBACKEND_FIX
+
+
+def buildAST(qasm_file):
+    if not qasm_file:
+        print('"Not filename provided')
+        return {"status": "Error", "result": "Not filename provided"}
+    ast = qasm.Qasm(filename=qasm_file).parse()  # Node (AST)
+    return ast
+
+
+def buildCircuit(ast, basis_gates=None):
+    unrolled_circuit = unroll.Unroller(ast=ast, backend=DAGBACKEND_FIX(basis_gates)) #CircuitBackend
+    unrolled_circuit.execute()
+    circuit_unrolled = unrolled_circuit.backend.circuit  # circuit DAG
+    return circuit_unrolled
