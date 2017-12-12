83d651b6ad6649bdec8d0a68a09faa0aed7fba69
Andrew Cross <awcross@us.ibm.com>
2017-12-12 11:49:46 -0500

    Mapper issue 159 (#171)
    * optimize_1q_gates behavior change: remove u1(2*pi).
    
    Address issue #159.
    Modifications to use of sympy in optimize_1q_gates.
    Updated tests.
    
    * Remove comment.
    
    * Add sym() method to Prefix().
    
    Fixes a problem with sympy integration for negated parameters.
    
    * Remove print().
    
    * Add sym() to binaryop.
    
    * new test to check the bug in #172
    
    * doc
    
    * Linting and style fixes
    
    * Fix test_mapper test that depends on Python vers.
    
    Modify `test_math_domain_error` in order to expect different results
    depending on the version of Python. This is a work-around to allow the
    test to pass, but should be handled higher up.
    
    * moving toffoli_gate test to test_quantumprogram
    
    * removing not necessary N () to improve performance
    
    * add new tests.
    
    * add corresponding test qasm files.
    
    * tests and corrections.
    
    * Restyle test_mapper tests, update example qasms
    
    * Allow test_mapper to compare QASM for Python 3.5
    
    Revise "test_optimize_1q_gates_issue159" comparing the output of the
    compilation with two similar expected outputs.

--- test/python/test_mapper.py
+++ test/python/test_mapper.py
@@ -16,9 +16,11 @@
 # limitations under the License.
 # =============================================================================
 
+from sys import version_info
 import unittest
 
 from qiskit import QuantumProgram
+from qiskit import qasm, unroll, mapper
 from .common import QiskitTestCase
 
 
@@ -29,17 +31,15 @@ class MapperTest(QiskitTestCase):
         self.seed = 42
         self.qp = QuantumProgram()
 
-    def tearDown(self):
-        pass
-
     def test_mapper_overoptimization(self):
         """
         The mapper should not change the semantics of the input. An overoptimization introduced
-        the issue #81: https://github.com/QISKit/qiskit-sdk-py/issues/81 
+        the issue #81: https://github.com/QISKit/qiskit-sdk-py/issues/81
         """
         self.qp.load_qasm_file(self._get_resource_path('qasm/overoptimization.qasm'), name='test')
         coupling_map = {0: [2], 1: [2], 2: [3], 3: []}
-        result1 = self.qp.execute(["test"], backend="local_qasm_simulator", coupling_map=coupling_map)
+        result1 = self.qp.execute(["test"], backend="local_qasm_simulator",
+                                  coupling_map=coupling_map)
         count1 = result1.get_counts("test")
         result2 = self.qp.execute(["test"], backend="local_qasm_simulator", coupling_map=None)
         count2 = result2.get_counts("test")
@@ -47,13 +47,161 @@ class MapperTest(QiskitTestCase):
 
     def test_math_domain_error(self):
         """
-        The math library operates over floats and introduce floating point errors that should be avoid
+        The math library operates over floats and introduce floating point errors that should be
+        avoided.
         See: https://github.com/QISKit/qiskit-sdk-py/issues/111
         """
         self.qp.load_qasm_file(self._get_resource_path('qasm/math_domain_error.qasm'), name='test')
         coupling_map = {0: [2], 1: [2], 2: [3], 3: []}
-        result1 = self.qp.execute(["test"], backend="local_qasm_simulator", coupling_map=coupling_map, seed=self.seed)
-        self.assertEqual(result1.get_counts("test"), {'0001': 507, '0101': 517})
+        result1 = self.qp.execute(["test"], backend="local_qasm_simulator",
+                                  coupling_map=coupling_map, seed=self.seed)
+
+        # TODO: the circuit produces different results under different versions
+        # of Python, which defeats the purpose of the "seed" parameter. A proper
+        # fix should be issued - this is a workaround for this particular test.
+        if version_info.minor == 5:  # Python 3.5
+            self.assertEqual(result1.get_counts("test"), {'0001': 507, '0101': 517})
+        else:  # Python 3.6 and higher
+            self.assertEqual(result1.get_counts("test"), {'0001': 480, '0101': 544})
+
+    def test_optimize_1q_gates_issue159(self):
+        """Test change in behavior for optimize_1q_gates that removes u1(2*pi) rotations.
+
+        See: https://github.com/QISKit/qiskit-sdk-py/issues/159
+        """
+        self.qp = QuantumProgram()
+        qr = self.qp.create_quantum_register('qr', 2)
+        cr = self.qp.create_classical_register('cr', 2)
+        qc = self.qp.create_circuit('Bell', [qr], [cr])
+        qc.h(qr[0])
+        qc.cx(qr[1], qr[0])
+        qc.cx(qr[1], qr[0])
+        qc.cx(qr[1], qr[0])
+        qc.measure(qr[0], cr[0])
+        qc.measure(qr[1], cr[1])
+        backend = 'ibmqx4'
+        cmap = {1: [0], 2: [0, 1, 4], 3: [2, 4]}
+        qobj = self.qp.compile(["Bell"], backend=backend, coupling_map=cmap)
+
+        # TODO: Python 3.5 produces an equivalent but different QASM, with the
+        # last lines swapped. This assertion compares the output with the two
+        # expected programs, but proper revision should be done.
+        self.assertIn(self.qp.get_compiled_qasm(qobj, "Bell"),
+                      (EXPECTED_QASM_1Q_GATES, EXPECTED_QASM_1Q_GATES_3_5))
+
+    def test_symbolic_unary(self):
+        """Test symbolic math in DAGBackend and optimizer with a prefix.
+
+        See: https://github.com/QISKit/qiskit-sdk-py/issues/172
+        """
+        ast = qasm.Qasm(filename=self._get_resource_path(
+            'qasm/issue172_unary.qasm')).parse()
+        unr = unroll.Unroller(ast, backend=unroll.DAGBackend(["cx", "u1", "u2", "u3"]))
+        unr.execute()
+        circ = mapper.optimize_1q_gates(unr.backend.circuit)
+        self.assertEqual(circ.qasm(qeflag=True), EXPECTED_QASM_SYMBOLIC_UNARY)
+
+    def test_symbolic_binary(self):
+        """Test symbolic math in DAGBackend and optimizer with a binary operation.
+
+        See: https://github.com/QISKit/qiskit-sdk-py/issues/172
+        """
+        ast = qasm.Qasm(filename=self._get_resource_path(
+            'qasm/issue172_binary.qasm')).parse()
+
+        unr = unroll.Unroller(ast, backend=unroll.DAGBackend(["cx", "u1", "u2", "u3"]))
+        unr.execute()
+        circ = mapper.optimize_1q_gates(unr.backend.circuit)
+        self.assertEqual(circ.qasm(qeflag=True), EXPECTED_QASM_SYMBOLIC_BINARY)
+
+    def test_symbolic_extern(self):
+        """Test symbolic math in DAGBackend and optimizer with an external function.
+
+        See: https://github.com/QISKit/qiskit-sdk-py/issues/172
+        """
+        ast = qasm.Qasm(filename=self._get_resource_path(
+            'qasm/issue172_extern.qasm')).parse()
+        unr = unroll.Unroller(ast, backend=unroll.DAGBackend(["cx", "u1", "u2", "u3"]))
+        unr.execute()
+        circ = mapper.optimize_1q_gates(unr.backend.circuit)
+        self.assertEqual(circ.qasm(qeflag=True), EXPECTED_QASM_SYMBOLIC_EXTERN)
+
+    def test_symbolic_power(self):
+        """Test symbolic math in DAGBackend and optimizer with a power (^).
+
+        See: https://github.com/QISKit/qiskit-sdk-py/issues/172
+        """
+        ast = qasm.Qasm(data=QASM_SYMBOLIC_POWER).parse()
+        unr = unroll.Unroller(ast, backend=unroll.DAGBackend(["cx", "u1", "u2", "u3"]))
+        unr.execute()
+        circ = mapper.optimize_1q_gates(unr.backend.circuit)
+        self.assertEqual(circ.qasm(qeflag=True), EXPECTED_QASM_SYMBOLIC_POWER)
+
+
+# QASMs expected by the tests.
+EXPECTED_QASM_SYMBOLIC_BINARY = """OPENQASM 2.0;
+include "qelib1.inc";
+qreg qr[1];
+creg cr[1];
+u1(-0.1 + 0.55*pi) qr[0];
+measure qr[0] -> cr[0];\n"""
+
+EXPECTED_QASM_SYMBOLIC_EXTERN = """OPENQASM 2.0;
+include "qelib1.inc";
+qreg qr[1];
+creg cr[1];
+u1(-0.479425538604203) qr[0];
+measure qr[0] -> cr[0];\n"""
+
+EXPECTED_QASM_SYMBOLIC_POWER = """OPENQASM 2.0;
+include "qelib1.inc";
+qreg qr[1];
+creg cr[1];
+u1(pi + (-pi + 0.3)^2.0) qr[0];
+measure qr[0] -> cr[0];\n"""
+
+EXPECTED_QASM_SYMBOLIC_UNARY = """OPENQASM 2.0;
+include "qelib1.inc";
+qreg qr[1];
+creg cr[1];
+u1(-1.5*pi) qr[0];
+measure qr[0] -> cr[0];\n"""
+
+EXPECTED_QASM_1Q_GATES = """OPENQASM 2.0;
+include "qelib1.inc";
+qreg q[2];
+creg cr[2];
+u2(0,pi) q[0];
+cx q[1],q[0];
+cx q[1],q[0];
+cx q[1],q[0];
+u2(0,pi) q[0];
+measure q[0] -> cr[1];
+u2(0,pi) q[1];
+measure q[1] -> cr[0];\n"""
+
+# This QASM is the same as EXPECTED_QASM_1Q_GATES, with the u2-measure lines
+# swapped.
+EXPECTED_QASM_1Q_GATES_3_5 = """OPENQASM 2.0;
+include "qelib1.inc";
+qreg q[2];
+creg cr[2];
+u2(0,pi) q[0];
+cx q[1],q[0];
+cx q[1],q[0];
+cx q[1],q[0];
+u2(0,pi) q[1];
+measure q[1] -> cr[0];
+u2(0,pi) q[0];
+measure q[0] -> cr[1];\n"""
+
+QASM_SYMBOLIC_POWER = """OPENQASM 2.0;
+include "qelib1.inc";
+qreg qr[1];
+creg cr[1];
+u1(pi) qr[0];
+u1((0.3+(-pi))^2) qr[0];
+measure qr[0] -> cr[0];"""
 
 
 if __name__ == '__main__':
