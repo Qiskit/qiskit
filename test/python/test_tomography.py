b8bbf89719555ae30092f5f8649580ed90df96b6
cjwood <cjwood@us.ibm.com>
2017-12-21 23:56:05 +1100

    Attempting to fix unit tests for travis python 3.5 build

--- test/python/test_tomography.py
+++ test/python/test_tomography.py
@@ -33,206 +33,78 @@ class TestTomography(QiskitTestCase):
         self.assertEqual(tomo.marginal_counts(counts, [0]), {"0": 41, "1": 59})
         self.assertEqual(tomo.marginal_counts(counts, [1]), {"0": 49, "1": 51})
 
-    def test_state_tomography_sets_1qubit(self):
-        tomoset1 = tomo.state_tomography_set([0], meas_basis='Pauli')
-        tomoset1_default = tomo.state_tomography_set([0])
-        self.assertEqual(self.state_refset1['qubits'], tomoset1['qubits'])
-        self.assertEqual(self.state_refset1['circuits'], tomoset1['circuits'])
-        self.assertEqual(self.state_refset1['qubits'],
-                         tomoset1_default['qubits'])
-        self.assertEqual(self.state_refset1['circuits'],
-                         tomoset1_default['circuits'])
+    def test_state_tomography_set_default(self):
+        pauli_set = tomo.state_tomography_set([0], meas_basis='Pauli')
+        default_set = tomo.state_tomography_set([0])
+        self.assertEqual(pauli_set['circuits'], default_set['circuits'])
 
-    def test_state_tomography_sets_2qubit(self):
-        tomoset2 = tomo.state_tomography_set([0, 1], meas_basis='Pauli')
-        self.assertEqual(self.state_refset2['qubits'], tomoset2['qubits'])
-        self.assertEqual(self.state_refset2['circuits'], tomoset2['circuits'])
-
-    def test_state_tomography_circuit_names_1qubit(self):
-        self.assertEqual(
-            tomo.tomography_circuit_names(self.state_refset1, 'test'),
-            ['test_meas_X(0)', 'test_meas_Y(0)', 'test_meas_Z(0)'])
-
-    def test_state_tomography_circuit_names_2qubit(self):
-        self.assertEqual(
-            tomo.tomography_circuit_names(self.state_refset2, 'test'),
-            ['test_meas_X(0)X(1)', 'test_meas_X(0)Y(1)',
-             'test_meas_X(0)Z(1)', 'test_meas_Y(0)X(1)',
-             'test_meas_Y(0)Y(1)', 'test_meas_Y(0)Z(1)',
-             'test_meas_Z(0)X(1)', 'test_meas_Z(0)Y(1)',
-             'test_meas_Z(0)Z(1)'])
+    def test_process_tomography_set_default(self):
+        tomo_set = tomo.process_tomography_set([0])
+        default_set = tomo.process_tomography_set(
+            [0], meas_basis='Pauli', prep_basis='SIC')
+        self.assertEqual(tomo_set['circuits'],
+                         default_set['circuits'])
 
     def test_state_tomography_1qubit(self):
+        # Tomography set
+        tomo_set = tomo.state_tomography_set([0])
         # Get test circuits
         qp, qr, cr = _test_circuits_1qubit()
         # Test simulation and fitting
-        shots = 1000
+        shots = 2500
         threshold = 1e-2
         rho = _tomography_test_data(
-            qp, 'Zp', qr, cr, self.state_refset1, shots)
+            qp, 'Zp', qr, cr, tomo_set, shots)
         self.assertTrue(_tomography_test_fit(rho, [1, 0], threshold))
         rho = _tomography_test_data(
-            qp, 'Zm', qr, cr, self.state_refset1, shots)
+            qp, 'Zm', qr, cr, tomo_set, shots)
         self.assertTrue(_tomography_test_fit(rho, [0, 1], threshold))
         rho = _tomography_test_data(
-            qp, 'Xp', qr, cr, self.state_refset1, shots)
+            qp, 'Xp', qr, cr, tomo_set, shots)
         self.assertTrue(_tomography_test_fit(
             rho, [1 / np.sqrt(2), 1 / np.sqrt(2)], threshold))
         rho = _tomography_test_data(
-            qp, 'Xm', qr, cr, self.state_refset1, shots)
+            qp, 'Xm', qr, cr, tomo_set, shots)
         self.assertTrue(_tomography_test_fit(
             rho, [1 / np.sqrt(2), -1 / np.sqrt(2)], threshold))
         rho = _tomography_test_data(
-            qp, 'Yp', qr, cr, self.state_refset1, shots)
+            qp, 'Yp', qr, cr, tomo_set, shots)
         self.assertTrue(_tomography_test_fit(
             rho, [1 / np.sqrt(2), 1j / np.sqrt(2)], threshold))
         rho = _tomography_test_data(
-            qp, 'Ym', qr, cr, self.state_refset1, shots)
+            qp, 'Ym', qr, cr, tomo_set, shots)
         self.assertTrue(_tomography_test_fit(
             rho, [1 / np.sqrt(2), -1j / np.sqrt(2)], threshold))
 
     def test_state_tomography_2qubit(self):
+        # Tomography set
+        tomo_set = tomo.state_tomography_set([0, 1])
         # Get test circuits
         qp, qr, cr = _test_circuits_2qubit()
-        shots = 1000
+        shots = 2500
         threshold = 1e-2
         # Test simulation and fitting
         rho = _tomography_test_data(
-            qp, 'Bell', qr, cr, self.state_refset2, shots)
+            qp, 'Bell', qr, cr, tomo_set, shots)
         self.assertTrue(_tomography_test_fit(
             rho, [1 / np.sqrt(2), 0, 0, 1 / np.sqrt(2)], threshold))
 
-    def test_process_tomography_sets_1qubit(self):
-        tomoset1_default = tomo.process_tomography_set([0])
-        tomoset1_sic = tomo.process_tomography_set(
-            [0], meas_basis='Pauli', prep_basis='SIC')
-        tomoset1_pauli = tomo.process_tomography_set(
-            [0], meas_basis='Pauli', prep_basis='Pauli')
-        self.assertEqual(self.process_refset1_sic['qubits'],
-                         tomoset1_default['qubits'])
-        self.assertEqual(self.process_refset1_sic['circuits'],
-                         tomoset1_default['circuits'])
-        self.assertEqual(self.process_refset1_sic['qubits'],
-                         tomoset1_sic['qubits'])
-        self.assertEqual(self.process_refset1_sic['circuits'],
-                         tomoset1_sic['circuits'])
-        self.assertEqual(self.process_refset1_pauli['qubits'],
-                         tomoset1_pauli['qubits'])
-        self.assertEqual(self.process_refset1_pauli['circuits'],
-                         tomoset1_pauli['circuits'])
-
-    def test_process_tomography_circuit_names_1qubit(self):
-        self.assertEqual(
-            tomo.tomography_circuit_names(self.process_refset1_sic, 'test'),
-            ['test_prep_S0(0)_meas_X(0)', 'test_prep_S0(0)_meas_Y(0)',
-             'test_prep_S0(0)_meas_Z(0)', 'test_prep_S1(0)_meas_X(0)',
-             'test_prep_S1(0)_meas_Y(0)', 'test_prep_S1(0)_meas_Z(0)',
-             'test_prep_S2(0)_meas_X(0)', 'test_prep_S2(0)_meas_Y(0)',
-             'test_prep_S2(0)_meas_Z(0)', 'test_prep_S3(0)_meas_X(0)',
-             'test_prep_S3(0)_meas_Y(0)', 'test_prep_S3(0)_meas_Z(0)'])
-        self.assertEqual(
-            tomo.tomography_circuit_names(self.process_refset1_pauli, 'test'),
-            ['test_prep_X0(0)_meas_X(0)', 'test_prep_X0(0)_meas_Y(0)',
-             'test_prep_X0(0)_meas_Z(0)', 'test_prep_X1(0)_meas_X(0)',
-             'test_prep_X1(0)_meas_Y(0)', 'test_prep_X1(0)_meas_Z(0)',
-             'test_prep_Y0(0)_meas_X(0)', 'test_prep_Y0(0)_meas_Y(0)',
-             'test_prep_Y0(0)_meas_Z(0)', 'test_prep_Y1(0)_meas_X(0)',
-             'test_prep_Y1(0)_meas_Y(0)', 'test_prep_Y1(0)_meas_Z(0)',
-             'test_prep_Z0(0)_meas_X(0)', 'test_prep_Z0(0)_meas_Y(0)',
-             'test_prep_Z0(0)_meas_Z(0)', 'test_prep_Z1(0)_meas_X(0)',
-             'test_prep_Z1(0)_meas_Y(0)', 'test_prep_Z1(0)_meas_Z(0)'])
-
     def test_process_tomography_1qubit(self):
+        # Tomography set
+        tomo_set = tomo.process_tomography_set([0])
         # Get test circuits
         qp, qr, cr = _test_circuits_1qubit()
         # Test simulation and fitting
-        shots = 1000
+        shots = 2500
         threshold = 1e-2
         choi = _tomography_test_data(
-            qp, 'Zp', qr, cr, self.process_refset1_sic, shots)
+            qp, 'Zp', qr, cr, tomo_set, shots)
         self.assertTrue(_tomography_test_fit(
             choi, [1 / np.sqrt(2), 0, 0, 1 / np.sqrt(2)], threshold))
         choi = _tomography_test_data(
-            qp, 'Zm', qr, cr, self.process_refset1_sic, shots)
-        self.assertTrue(_tomography_test_fit(
-            choi, [0, 1 / np.sqrt(2), 1 / np.sqrt(2), 0], threshold))
-        choi = _tomography_test_data(
-            qp, 'Xp', qr, cr, self.process_refset1_sic, shots)
+            qp, 'Xp', qr, cr, tomo_set, shots)
         self.assertTrue(_tomography_test_fit(
             choi, [0.5, 0.5, 0.5, -0.5], threshold))
-
-    # Class member variables
-    state_refset1 = {
-        'qubits': [0],
-        'circuits': [
-            {'meas': {0: 'X'}},
-            {'meas': {0: 'Y'}},
-            {'meas': {0: 'Z'}}
-        ],
-        'meas_basis': tomo.PAULI_BASIS
-    }
-
-    state_refset2 = {
-        'qubits': [0, 1],
-        'circuits': [
-            {'meas': {0: 'X', 1: 'X'}},
-            {'meas': {0: 'X', 1: 'Y'}},
-            {'meas': {0: 'X', 1: 'Z'}},
-            {'meas': {0: 'Y', 1: 'X'}},
-            {'meas': {0: 'Y', 1: 'Y'}},
-            {'meas': {0: 'Y', 1: 'Z'}},
-            {'meas': {0: 'Z', 1: 'X'}},
-            {'meas': {0: 'Z', 1: 'Y'}},
-            {'meas': {0: 'Z', 1: 'Z'}}
-        ],
-        'meas_basis': tomo.PAULI_BASIS
-    }
-
-    process_refset1_pauli = {
-        'qubits': [0],
-        'meas_basis': tomo.PAULI_BASIS,
-        'prep_basis': tomo.PAULI_BASIS,
-        'circuits': [
-            {'meas': {0: 'X'}, 'prep': {0: ('X', 0)}},
-            {'meas': {0: 'Y'}, 'prep': {0: ('X', 0)}},
-            {'meas': {0: 'Z'}, 'prep': {0: ('X', 0)}},
-            {'meas': {0: 'X'}, 'prep': {0: ('X', 1)}},
-            {'meas': {0: 'Y'}, 'prep': {0: ('X', 1)}},
-            {'meas': {0: 'Z'}, 'prep': {0: ('X', 1)}},
-            {'meas': {0: 'X'}, 'prep': {0: ('Y', 0)}},
-            {'meas': {0: 'Y'}, 'prep': {0: ('Y', 0)}},
-            {'meas': {0: 'Z'}, 'prep': {0: ('Y', 0)}},
-            {'meas': {0: 'X'}, 'prep': {0: ('Y', 1)}},
-            {'meas': {0: 'Y'}, 'prep': {0: ('Y', 1)}},
-            {'meas': {0: 'Z'}, 'prep': {0: ('Y', 1)}},
-            {'meas': {0: 'X'}, 'prep': {0: ('Z', 0)}},
-            {'meas': {0: 'Y'}, 'prep': {0: ('Z', 0)}},
-            {'meas': {0: 'Z'}, 'prep': {0: ('Z', 0)}},
-            {'meas': {0: 'X'}, 'prep': {0: ('Z', 1)}},
-            {'meas': {0: 'Y'}, 'prep': {0: ('Z', 1)}},
-            {'meas': {0: 'Z'}, 'prep': {0: ('Z', 1)}}
-        ]
-    }
-
-    process_refset1_sic = {
-        'qubits': [0],
-        'meas_basis': tomo.PAULI_BASIS,
-        'prep_basis': tomo.SIC_BASIS,
-        'circuits': [
-            {'meas': {0: 'X'}, 'prep': {0: ('S', 0)}},
-            {'meas': {0: 'Y'}, 'prep': {0: ('S', 0)}},
-            {'meas': {0: 'Z'}, 'prep': {0: ('S', 0)}},
-            {'meas': {0: 'X'}, 'prep': {0: ('S', 1)}},
-            {'meas': {0: 'Y'}, 'prep': {0: ('S', 1)}},
-            {'meas': {0: 'Z'}, 'prep': {0: ('S', 1)}},
-            {'meas': {0: 'X'}, 'prep': {0: ('S', 2)}},
-            {'meas': {0: 'Y'}, 'prep': {0: ('S', 2)}},
-            {'meas': {0: 'Z'}, 'prep': {0: ('S', 2)}},
-            {'meas': {0: 'X'}, 'prep': {0: ('S', 3)}},
-            {'meas': {0: 'Y'}, 'prep': {0: ('S', 3)}},
-            {'meas': {0: 'Z'}, 'prep': {0: ('S', 3)}}
-        ]
-    }
 
 
 def _tomography_test_data(qp, name, qr, cr, tomoset, shots):
