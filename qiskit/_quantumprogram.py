0ceb09381c2dc022475de7279e193a9922d9f82f
Diego M. Rodriguez <diego.plan9@gmail.com>
2017-12-22 21:17:02 +0100

    Add execute() mention to hpc documentation

--- qiskit/_quantumprogram.py
+++ qiskit/_quantumprogram.py
@@ -1366,7 +1366,7 @@ class QuantumProgram(object):
             shots (int): the number of shots
             max_credits (int): the max credits to use 3, or 5
             seed (int): the intial seed the simulatros use
-            hpc (json): This will setup some parameter for
+            hpc (dict): This will setup some parameter for
                         ibmqx_hpc_qasm_simulator, using a JSON-like format like:
                         {
                             'multi_shot_optimization': Boolean,
