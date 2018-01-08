d3a4e399cbb1ac8ff17a9732b3bb5f574ecaccdd
Juan Gomez <atilag@gmail.com>
2018-01-08 12:53:02 +0100

    * Bug fix: qasm libs were not included because of a typo in MANIFEST * Bumped to version 0.4.1

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ class QiskitSimulatorBuild(build):
 
 setup(
     name="qiskit",
-    version="0.4.0",
+    version="0.4.1",
     description="Software for developing quantum computing programs",
     long_description="""QISKit is a software development kit for writing
         quantum computing experiments, programs, and applications. Works with
