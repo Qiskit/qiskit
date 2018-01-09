abc25dcef9c2d666cf09056f59b2e6dc148651cd
Diego M. Rodriguez <diego.plan9@gmail.com>
2018-01-09 19:37:46 +0100

    pip release: 0.4.4

--- setup.py
+++ setup.py
@@ -79,7 +79,7 @@ class QiskitSimulatorBuild(build):
 
 setup(
     name="qiskit",
-    version="0.4.3",
+    version="0.4.4",
     description="Software for developing quantum computing programs",
     long_description="""QISKit is a software development kit for writing
         quantum computing experiments, programs, and applications. Works with
