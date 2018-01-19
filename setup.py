d2075fcb9aa6f4c5fd2c37aa01e270856efeef3c
Diego M. Rodriguez <diego.plan9@gmail.com>
2018-01-19 17:47:54 +0100

    Revise minimal versions of deps, fix typo

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ import platform
 requirements = [
     "IBMQuantumExperience>=1.8.26",
     "matplotlib>=2.1,<2.2",
-    "networkx>=2.0,<2.1",
-    "numpy>=1.14,<1.15",
+    "networkx>=1.11,<2.1",
+    "numpy>=1.13,<1.15",
     "ply==3.10",
-    "scipy>=1.0,<1.1",
+    "scipy>=0.19,<1.1",
     "Sphinx>=1.6,<1.7",
     "sympy>=1.0"
 ]
