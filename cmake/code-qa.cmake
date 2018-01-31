f547a6f3cf15a744de6b7c1388ba3c7b4d83fc96
Juan Gomez <atilag@gmail.com>
2018-01-31 18:57:01 +0100

    CMake integration: * Added make "doc" target for Sphinx documentation * Added both sdist and bdist_wheel distribution packages builds

--- cmake/code-qa.cmake
+++ cmake/code-qa.cmake
@@ -3,7 +3,7 @@ enable_testing()
 # Create Python distrubution package
 find_program(PYTHON "python")
 if (NOT PYTHON)
-message(FATAL_ERROR "We can't find Python in your system. Please, install it and try again.")
+message(FATAL_ERROR "Couldn't find Python in your system. Please, install it and try again.")
 endif()
 
 add_test(NAME qiskit_python
@@ -13,7 +13,7 @@ add_test(NAME qiskit_python
 function(add_linter_target)
     find_program(PYLINT "pylint")
     if (NOT PYLINT)
-        message(FATAL_ERROR "We can't find pylint in yout system. Please, install it and try again.")
+        message(FATAL_ERROR "Couldn't find pylint in your system. Please, install it and try again.")
     endif()
 
     add_custom_target(linter)
@@ -25,7 +25,7 @@ endfunction()
 function(add_code_style_target)
     find_program(PYCODESTYLE "pycodestyle")
     if (NOT PYCODESTYLE)
-        message(FATAL_ERROR "We can't find pycodestyle in yout system. Please, install it and try again.")
+        message(SEND_ERROR "Couldn't find pycodestyle in your system. Please, install it and try again.")
     endif()
 
     add_custom_target(style)
@@ -35,3 +35,48 @@ function(add_code_style_target)
         WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
 endfunction()
 
+function(add_doc_target DOC_FORMAT SOURCE_DIR BUILD_DIR)
+    find_program(PYTHON python)
+    if(NOT PYTHON)
+        message(SEND_ERROR "Couldn't find python in your system. Please, install it and try again.")
+    endif()
+    find_program(BETTER_APIDOC "better-apidoc")
+    if(NOT BETTER_APIDOC)
+        message(SEND_ERROR "Couldn't find better-apidoc in your system. Please, install it and try again.")
+    endif()
+    find_program(SPHINX_AUTOGEN "sphinx-autogen")
+    if(NOT SPHINX_AUTOGEN)
+        message(SEND_ERROR "Couldn't find sphinx-autogen in your system. Please, install it and try again.")
+    endif()
+
+    set(SPHINX ${PYTHON} -msphinx)
+
+     # Set param defaults
+    if(NOT DOC_FORMAT)
+        set(DOC_FORMAT "html")
+    endif()
+    if(NOT SOURCE_DIR)
+        set(SOURCE_DIR "./")
+    endif()
+    if(NOT BUILD_DIR)
+        set(BUILD_DIR "_build/")
+    endif()
+
+    add_custom_target(doc)
+    foreach(lang_dir "." "ja")
+        add_custom_command(TARGET doc
+            COMMAND ${CMAKE_COMMAND} -E env PYTHONPATH=${PROJECT_SOURCE_DIR}
+                ${BETTER_APIDOC} -f -o doc/_templates doc/${lang_dir}
+                -d 5 -e -t doc/_templates/better-apidoc qiskit qiskit/tools
+                "qiskit/extensions/standard/[a-z]*"
+            WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
+        add_custom_command(TARGET doc
+            COMMAND ${CMAKE_COMMAND} -E env PYTHONPATH=${PROJECT_SOURCE_DIR}
+                ${SPHINX_AUTOGEN} -t doc/_templates doc/${lang_dir}/_autodoc/*
+                WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
+        add_custom_command(TARGET doc
+            COMMAND ${CMAKE_COMMAND} -E env PYTHONPATH=${PROJECT_SOURCE_DIR}
+                ${SPHINX} -M ${DOC_FORMAT} "${SOURCE_DIR}${lang_dir}" "${BUILD_DIR}${lang_dir}"
+                WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}/doc)
+    endforeach()
+endfunction()
