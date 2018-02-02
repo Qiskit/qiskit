847a23049d0fe246497e0e3df5a73e3610feb01d
Juan Gomez <atilag@gmail.com>
2018-02-02 12:27:33 +0100

    CMake integration: * 'make clean' target now removes all custom generated files and   directories. * Fxied 'make pypi_package' to not include binaries in the source   distribution package (sdist)

--- cmake/python-build.cmake
+++ cmake/python-build.cmake
@@ -1,4 +1,3 @@
-
 # Creates a TARGET_NAME target for creating Pypi distributable package
 # Args:
 #    TARGET_NAME    The name of the target to be invoked in the Makefile
@@ -19,46 +18,95 @@ function(add_pypi_package_target TARGET_NAME PACKAGE_TYPE)
 
 	message("QISKIT_VERSION = ${QISKIT_VERSION}")
 	configure_file(${SETUP_PY_IN} ${SETUP_PY})
+	# For ' make clean' target
+	set_property(DIRECTORY APPEND PROPERTY ADDITIONAL_MAKE_CLEAN_FILES ${SETUP_PY})
 
 	if(PACKAGE_TYPE STREQUAL "both")
-		set(PIP_PACKAGE_TYPES sdist --dist-dir ${CMAKE_CURRENT_BINARY_DIR}/dist
-			bdist_wheel --dist-dir ${CMAKE_CURRENT_BINARY_DIR}/dist)
+		set(PIP_PACKAGE_SOURCE_DIST sdist --dist-dir ${CMAKE_CURRENT_BINARY_DIR}/dist)
+		set(PIP_PACKAGE_PLATFORM_WHEELS bdist_wheel --dist-dir ${CMAKE_CURRENT_BINARY_DIR}/dist)
 	elseif(PACKAGE_TYPE STREQUAL "sdist")
-		set(PIP_PACKAGE_TYPES sdist --dist-dir ${CMAKE_CURRENT_BINARY_DIR}/dist)
+		set(PIP_PACKAGE_SOURCE_DIST sdist --dist-dir ${CMAKE_CURRENT_BINARY_DIR}/dist)
 	elseif(PACKAGE_TYPE STREQUAL "bdist_wheel")
-		set(PIP_PACKAGE_TYPES bdist_wheel --dist-dir ${CMAKE_CURRENT_BINARY_DIR}/dist)
+		set(PIP_PACKAGE_PLATFORM_WHEELS bdist_wheel --dist-dir ${CMAKE_CURRENT_BINARY_DIR}/dist)
 	endif()
 
+	# For ' make clean' target
+	set_property(DIRECTORY APPEND PROPERTY ADDITIONAL_MAKE_CLEAN_FILES
+		${CMAKE_CURRENT_BINARY_DIR}/dist)
 
-	if(UNIX AND NOT APPLE)
-		#set(PIP_WHEEL_PLATFORM ${PACKAGE_TYPE} -p manylinux1_x86_64)
-	elseif(MINGW)
-		set(EXECUTABLE_FILE_EXTENSION ".exe")
-		#set(PIP_WHEEL_PLATFORM "bdist_wheel") # TODO: Find the correct Tag
-	elseif(APPLE)
-		#set(PIP_WHEEL_PLATFORM "bdist_wheel") # TODO: Find the correct Tag
-	endif()
-
-	set(COPY_QISKIT_SIM_TARGET ${TARGET_NAME}_copy_qiskit_simulator)
-	add_custom_target(${COPY_QISKIT_SIM_TARGET})
-    add_custom_command(TARGET ${COPY_QISKIT_SIM_TARGET}
-		COMMAND ${CMAKE_COMMAND} -E copy
-			${QISKIT_SIMULATOR_OUTPUT_DIR}/qiskit_simulator${EXECUTABLE_FILE_EXTENSION}
-			${CMAKE_CURRENT_SOURCE_DIR}/qiskit/backends)
 	if(MINGW)
-		foreach(dll_file ${QISKIT_SIMULATOR_THIRD_PARTY_DLLS})
-			add_custom_command(TARGET ${COPY_QISKIT_SIM_TARGET}
-				COMMAND ${CMAKE_COMMAND} -E copy
-					${dll_file}
-					${CMAKE_CURRENT_SOURCE_DIR}/qiskit/backends)
-		endforeach()
+		set(EXECUTABLE_FILE_EXTENSION ".exe")
 	endif()
 
+	# The main package target, all othe targets will depend on it
 	add_custom_target(${TARGET_NAME})
-	add_custom_command(TARGET ${TARGET_NAME}
-		COMMAND ${PYTHON} ${SETUP_PY} ${PIP_PACKAGE_TYPES}
-		WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
-	add_dependencies(${TARGET_NAME} ${COPY_QISKIT_SIM_TARGET})
-	add_dependencies(${COPY_QISKIT_SIM_TARGET} qiskit_simulator)
+
+	# For source distributions, we don't want any binary in the final package
+	if(PIP_PACKAGE_SOURCE_DIST)
+		set(TARGET_NAME_SDIST ${TARGET_NAME}_SDIST)
+		add_custom_target(${TARGET_NAME_SDIST})
+		add_custom_command(TARGET ${TARGET_NAME_SDIST}
+			COMMAND ${PYTHON} ${SETUP_PY} ${PIP_PACKAGE_SOURCE_DIST}
+			WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
+	endif()
+
+	if(PIP_PACKAGE_PLATFORM_WHEELS)
+		set(COPY_QISKIT_SIM_TARGET ${TARGET_NAME}_COPY_QISKIT_SIMULATOR)
+		# We create a target which will depend on TARGET_NAME_WHEELS for
+		# copying all the binaries to their final locations
+		add_custom_target(${COPY_QISKIT_SIM_TARGET})
+    	add_custom_command(TARGET ${COPY_QISKIT_SIM_TARGET}
+			COMMAND ${CMAKE_COMMAND} -E copy
+				${QISKIT_SIMULATOR_OUTPUT_DIR}/qiskit_simulator${EXECUTABLE_FILE_EXTENSION}
+				${CMAKE_CURRENT_SOURCE_DIR}/qiskit/backends)
+		# For ' make clean' target
+		set_property(DIRECTORY APPEND PROPERTY
+			ADDITIONAL_MAKE_CLEAN_FILES
+				${CMAKE_CURRENT_SOURCE_DIR}/qiskit/backends/qiskit_simulator${EXECUTABLE_FILE_EXTENSION})
+		# For Windows, we need to copy external .dll dependencies too
+		if(MINGW)
+			foreach(dll_file ${QISKIT_SIMULATOR_THIRD_PARTY_DLLS})
+				add_custom_command(TARGET ${COPY_QISKIT_SIM_TARGET}
+					COMMAND ${CMAKE_COMMAND} -E copy
+						${dll_file}
+						${CMAKE_CURRENT_SOURCE_DIR}/qiskit/backends)
+				# For 'make clean' target
+				get_filename_component(FINAL_FILE ${dll_file} NAME)
+				set_property(DIRECTORY APPEND PROPERTY
+					ADDITIONAL_MAKE_CLEAN_FILES
+						${CMAKE_CURRENT_SOURCE_DIR}/qiskit/backends/${FINAL_FILE})
+			endforeach()
+		endif()
+
+		set(TARGET_NAME_WHEELS ${TARGET_NAME}_WHEELS)
+		add_custom_target(${TARGET_NAME_WHEELS})
+		add_custom_command(TARGET ${TARGET_NAME_WHEELS}
+			COMMAND ${PYTHON} ${SETUP_PY} ${PIP_PACKAGE_PLATFORM_WHEELS}
+			WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
+	endif()
+
+	# Create our dependency graph
+	if(PIP_PACKAGE_PLATFORM_WHEELS)
+		add_dependencies(${TARGET_NAME} ${TARGET_NAME_WHEELS})
+		add_dependencies(${TARGET_NAME_WHEELS} ${COPY_QISKIT_SIM_TARGET})
+		add_dependencies(${COPY_QISKIT_SIM_TARGET} qiskit_simulator)
+		# If we have to build the source distribution as well, then we
+		# need to build and package source distribution package first, and the
+		# way to express this is by depending on the source distribution target.
+		# Otherwise the binaries of the wheel package will be added to the
+		# source distribution too, and we don't want that.
+		if(PIP_PACKAGE_SOURCE_DIST)
+			add_dependencies(${COPY_QISKIT_SIM_TARGET} ${TARGET_NAME_SDIST})
+		endif()
+	endif()
+
+	if(PIP_PACKAGE_SOURCE_DIST)
+		# if we have to build wheels package too, we already have a depdency
+		# with TARGET_NAME, but if we haven't, we need to depend on TARGET_NAME
+		if(NOT PIP_PACKAGE_PLATFORM_WHEELS)
+			add_dependencies(${TARGET_NAME} ${TARGET_NAME_SDIST})
+		endif()
+		add_dependencies(${TARGET_NAME_SDIST} qiskit_simulator)
+	endif()
 endfunction()
 
