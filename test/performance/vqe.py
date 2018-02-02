22666711b844f3ba58468e5298657105734b6d90
Ali Javadi <ajavadia@princeton.edu>
2018-02-02 09:49:32 -0500

    fixing linter issues

--- test/performance/vqe.py
+++ test/performance/vqe.py
@@ -26,11 +26,7 @@ import numpy as np
 from scipy import linalg as la
 from functools import partial
 
-# We don't know from where the user is running the example,
-# so we need a relative position from this file path.
-# TODO: Relative imports for intra-package imports are highly discouraged.
-# http://stackoverflow.com/a/7506006
-sys.path.append(os.path.join(os.path.dirname(__file__), '../..'))
+# import qiskit modules
 from qiskit import QuantumProgram, QuantumCircuit
 from qiskit import QuantumProgram
 import Qconfig
@@ -40,53 +36,60 @@ from qiskit.tools.apps.optimization import trial_circuit_ryrz, SPSA_optimization
 from qiskit.tools.apps.optimization import Hamiltonian_from_file, make_Hamiltonian
 from qiskit.tools.apps.optimization import eval_hamiltonian, group_paulis
 
-# Ignore warnings due to chopping of small imaginary part of the energy 
+# Ignore warnings due to chopping of small imaginary part of the energy
 import warnings
 warnings.filterwarnings('ignore')
 
-n=2
-m=6
-device='local_qiskit_simulator'
+n = 2
+m = 6
+device = 'local_qiskit_simulator'
 
-initial_theta=np.random.randn(2*n*m)
-entangler_map={1: [0]} # the map of two-qubit gates with control at key and target at values
-shots=1024
-max_trials=100
-ham_name=os.path.join(os.path.dirname(__file__),'H2/H2Equilibrium.txt')
+initial_theta = np.random.randn(2 * n * m)
+entangler_map = {1: [0]}    # map of two-qubit gates (key: control, values: target)
+shots = 1024
+max_trials = 100
+ham_name = os.path.join(os.path.dirname(__file__), 'H2/H2Equilibrium.txt')
 
 # Exact Energy
-pauli_list=Hamiltonian_from_file(ham_name)
-H=make_Hamiltonian(pauli_list)
-exact=np.amin(la.eig(H)[0]).real
+pauli_list = Hamiltonian_from_file(ham_name)
+H = make_Hamiltonian(pauli_list)
+exact = np.amin(la.eig(H)[0]).real
 print('The exact ground state energy is:')
 print(exact)
-pauli_list_grouped=group_paulis(pauli_list)
+pauli_list_grouped = group_paulis(pauli_list)
 
 # Optimization
 Q_program = QuantumProgram()
-Q_program.set_api(Qconfig.APItoken,Qconfig.config["url"])
-print (Q_program.get_backend_status(device))
+Q_program.set_api(Qconfig.APItoken, Qconfig.config["url"])
+print(Q_program.get_backend_status(device))
 
 
-def cost_function(Q_program,H,n,m,entangler_map,shots,device,theta):
-    
-    return eval_hamiltonian(Q_program,H,trial_circuit_ryrz(n,m,theta,entangler_map,None,False),shots,device).real
+def cost_function(Q_program, H, n, m, entangler_map, shots, device, theta):
+    return eval_hamiltonian(Q_program, H,
+                            trial_circuit_ryrz(n, m, theta, entangler_map,
+                                               None, False), shots, device).real
+
 
 def optimize():
-    initial_c=0.01
-    target_update=2*np.pi*0.1
+    initial_c = 0.01
+    target_update = 2 * np.pi * 0.1
     save_step = 20
-    
-    if shots ==1:
-        SPSA_params=SPSA_calibration(partial(cost_function,Q_program,H,n,m,entangler_map,
-                                             shots,device),initial_theta,initial_c,target_update,25)
-        output=SPSA_optimization(partial(cost_function,Q_program,H,n,m,entangler_map,shots,device),
-                                 initial_theta,SPSA_params,max_trials,save_step,1);
+
+    if shots == 1:
+        SPSA_params = SPSA_calibration(partial(cost_function, Q_program, H, n, m,
+                                               entangler_map, shots, device),
+                                       initial_theta, initial_c, target_update, 25)
+        output = SPSA_optimization(partial(cost_function, Q_program, H, n, m,
+                                           entangler_map, shots, device),
+                                   initial_theta, SPSA_params, max_trials, save_step, 1)
 
     else:
-        SPSA_params=SPSA_calibration(partial(cost_function,Q_program,pauli_list_grouped,n,m,entangler_map,
-                                             shots,device),initial_theta,initial_c,target_update,25)
-        output=SPSA_optimization(partial(cost_function,Q_program,pauli_list_grouped,n,m,entangler_map,shots,device),
-                                 initial_theta,SPSA_params,max_trials,save_step,1);
+        SPSA_params = SPSA_calibration(partial(cost_function, Q_program, pauli_list_grouped, n, m,
+                                               entangler_map, shots, device),
+                                       initial_theta, initial_c, target_update, 25)
+        output = SPSA_optimization(partial(cost_function, Q_program, pauli_list_grouped, n, m,
+                                           entangler_map, shots, device),
+                                   initial_theta, SPSA_params, max_trials, save_step, 1)
+
 
 optimize()
