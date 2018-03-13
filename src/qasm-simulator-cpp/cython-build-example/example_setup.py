69f763326dd8b57d132d4033b2275b6903b39534
cjwood <cjwood@us.ibm.com>
2018-03-13 18:54:42 -0400

    added example of cython building and linking of simulator

---
+++ src/qasm-simulator-cpp/cython-build-example/example_setup.py
@@ -0,0 +1,59 @@
+import sys
+import os
+from distutils.core import setup, Extension
+from distutils.spawn import find_executable
+from Cython.Build import cythonize
+
+# Build options
+include = ['-I../src',
+           '-I../src/backends',
+           '-I../src/engines',
+           '-I../src/utilities',
+           '-I../src/third-party',
+           '-I../src/third-party/headers']
+warnings = ['-pedantic', '-Wall', '-Wextra', '-Wfloat-equal', '-Wundef',
+            '-Wcast-align', '-Wwrite-strings', '-Wmissing-declarations',
+            '-Wshadow', '-Woverloaded-virtual']
+opt = ['-ffast-math', '-O3', '-march=native']
+compile_args = ['-g', '-std=c++11'] + opt + include + warnings
+link_args = []
+
+# MacOS Specific build instructions
+if sys.platform == 'darwin':
+
+    # Set minimum os version to support C++11 headers
+    min_macos_version = '-mmacosx-version-min=10.9'
+    compile_args.append(min_macos_version)
+    link_args.append(min_macos_version)
+
+    # Link to Apple Accelerate framework for BLAS
+    link_args += ['-Wl,-framework', '-Wl,Accelerate']
+
+    # Check for OpenMP compatible GCC compiler
+    for gcc in ['g++-7', 'g++-6', 'g++-5']:
+        path = find_executable(gcc)
+        if path is not None:
+            # Use most recent GCC compiler
+            print(path)
+            os.environ['CC'] = path
+            os.environ['CXX'] = path
+            compile_args.append('-fopenmp')
+            link_args.append('-fopenmp')
+            break
+else:
+    link_args += ['-llapack', '-lblas']
+    # TODO Check BLAS linkage for Windows and Linux
+    # TODO Link to Anaconda MKL if available
+
+# Simulator extension
+qasm_simulator = Extension('qasm_simulator',
+                           sources=['qasm_simulator.pyx'],
+                           extra_link_args=link_args,
+                           extra_compile_args=compile_args,
+                           language='c++')
+
+setup(
+    name='qasm_simulator',
+    packages=['qasm_simulator'],
+    ext_modules=cythonize(qasm_simulator)
+)
---
+++ src/qasm-simulator-cpp/cython-build-example/qasm_simulator.pyx
@@ -0,0 +1,26 @@
+from libcpp.string cimport string
+
+cdef extern from "simulator.hpp" namespace "QISKIT":
+    int addOne(int)
+
+    cdef cppclass Simulator:
+        Simulator()
+
+        string execute(int indent)
+        void load_string(string qobj)
+
+
+cdef class SimulatorWrapper:
+    cdef Simulator *thisptr
+
+    def __cinit__(self):
+        self.thisptr = new Simulator()
+
+    def __dealloc__(self):
+        del self.thisptr
+
+    def run(self, qobj_str, indent=4):
+        ""
+        # serialize qobj as json byte string
+        self.thisptr.load_string(qobj_str.encode())
+        return self.thisptr.execute(indent).decode()
