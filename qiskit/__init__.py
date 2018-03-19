a32e30f71d05304979081ab422e840e92e9f230b
Paul Nation <nonhermitian@gmail.com>
2018-03-19 15:27:29 -0400

    Do Python version check in init (#349)
    * Do Python version check in init
    
    This does the Python version check in a similar manner as NumPy and SciPy equivilent checks.
    
    * Fix pylint issues
    
    * Pylint 10/10

--- qiskit/__init__.py
+++ qiskit/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # pylint: disable=wrong-import-order
+# pylint: disable=wrong-import-position
 
 # Copyright 2017 IBM RESEARCH. All Rights Reserved.
 #
@@ -17,6 +18,16 @@
 # =============================================================================
 
 """Main QISKit public functionality."""
+
+import sys
+# Check for Python version 3.5+
+if sys.version_info < (3, 5):
+    raise Exception('QISKit requires Python version 3.5 or greater.')
+
+# Check for required ibmqe version
+from ._util import _check_ibmqe_version
+_check_ibmqe_version()
+
 from IBMQuantumExperience import RegisterSizeError
 
 from ._qiskiterror import QISKitError
@@ -39,8 +50,5 @@ from ._jobprocessor import JobProcessor
 from ._quantumjob import QuantumJob
 from ._quantumprogram import QuantumProgram
 from ._result import Result
-from ._util import _check_ibmqe_version
 
 __version__ = '0.5.0'
-
-_check_ibmqe_version()
