6be7f0d43b34cefcd85bc6318c9df5ed19b9da1f
Christopher Wood <cjwood@us.ibm.com>
2018-04-10 15:08:24 -0400

    Allow combining circuits with different unique registers (#389)
    * modify circuit addition rules to allow combining circuits with different but unique registers
    
    * fixed bug in extend circuit, added unit tests for extend

--- test/python/test_quantumprogram.py
+++ test/python/test_quantumprogram.py
@@ -1569,8 +1569,8 @@ class TestQuantumProgram(QiskitTestCase):
     # More test cases for interesting examples
     ###############################################################
 
-    def test_add_circuit(self):
-        """Test add two circuits.
+    def test_combine_circuit_common(self):
+        """Test combining two circuits with same registers.
 
         If all correct should return the data
         """
@@ -1583,35 +1583,122 @@ class TestQuantumProgram(QiskitTestCase):
         qc1.measure(qr[0], cr[0])
         qc2.measure(qr[1], cr[1])
         new_circuit = qc1 + qc2
-        q_program.add_circuit('new_circuit', new_circuit)
-        # new_circuit.measure(qr[0], cr[0])
-        circuits = ['new_circuit']
+        name = 'test_circuit'
+        q_program.add_circuit(name, new_circuit)
         backend = 'local_qasm_simulator'  # the backend to run on
         shots = 1024  # the number of shots in the experiment.
-        result = q_program.execute(circuits, backend=backend, shots=shots,
+        result = q_program.execute(name, backend=backend, shots=shots,
                                    seed=78)
-        counts = result.get_counts('new_circuit')
+        counts = result.get_counts(name)
         target = {'00': shots / 2, '01': shots / 2}
         threshold = 0.025 * shots
         self.assertDictAlmostEqual(counts, target, threshold)
 
-    def test_add_circuit_fail(self):
-        """Test add two circuits fail.
+    def test_combine_circuit_different(self):
+        """Test combinging two circuits with different registers.
 
-        If the circuits have different registers it should return a QISKitError
+        If all correct should return the data
+        """
+
+        qr = QuantumRegister("qr", 2)
+        cr = ClassicalRegister("cr", 2)
+        qc1 = QuantumCircuit(qr)
+        qc1.x(qr)
+        qc2 = QuantumCircuit(qr, cr)
+        qc2.measure(qr, cr)
+
+        qp = QuantumProgram()
+        name = 'test'
+        qp.add_circuit(name, qc1 + qc2)
+        backend = 'local_qasm_simulator'  # the backend to run on
+        shots = 1024  # the number of shots in the experiment.
+        result = qp.execute(name, backend=backend, shots=shots, seed=78)
+        counts = result.get_counts(name)
+        target = {'11': shots}
+        threshold = 0.0
+        self.assertDictAlmostEqual(counts, target, threshold)
+
+    def test_combine_circuit_fail(self):
+        """Test combining two circuits fails if registers incompatible.
+
+        If two circuits have samed name register of different size or type
+        it should raise a QISKitError.
+        """
+        q1 = QuantumRegister("q", 1)
+        q2 = QuantumRegister("q", 2)
+        c1 = QuantumRegister("q", 1)
+        qc1 = QuantumCircuit(q1)
+        qc2 = QuantumCircuit(q2)
+        qc3 = QuantumCircuit(c1)
+
+        self.assertRaises(QISKitError, qc1.__add__, qc2)
+        self.assertRaises(QISKitError, qc1.__add__, qc3)
+
+    def test_extend_circuit(self):
+        """Test extending a circuit with same registers.
+
+        If all correct should return the data
         """
         q_program = QuantumProgram()
-        qr = q_program.create_quantum_register("qr", 1)
-        cr = q_program.create_classical_register("cr", 1)
-        q = q_program.create_quantum_register("q", 1)
-        c = q_program.create_classical_register("c", 1)
+        qr = q_program.create_quantum_register("qr", 2)
+        cr = q_program.create_classical_register("cr", 2)
         qc1 = q_program.create_circuit("qc1", [qr], [cr])
-        qc2 = q_program.create_circuit("qc2", [q], [c])
+        qc2 = q_program.create_circuit("qc2", [qr], [cr])
         qc1.h(qr[0])
         qc1.measure(qr[0], cr[0])
-        qc2.measure(q[0], c[0])
-        # new_circuit = qc1 + qc2
-        self.assertRaises(QISKitError, qc1.__add__, qc2)
+        qc2.measure(qr[1], cr[1])
+        qc1 += qc2
+        name = 'test_circuit'
+        q_program.add_circuit(name, qc1)
+        # new_circuit.measure(qr[0], cr[0])
+        backend = 'local_qasm_simulator'  # the backend to run on
+        shots = 1024  # the number of shots in the experiment.
+        result = q_program.execute(name, backend=backend, shots=shots,
+                                   seed=78)
+        counts = result.get_counts(name)
+        target = {'00': shots / 2, '01': shots / 2}
+        threshold = 0.025 * shots
+        self.assertDictAlmostEqual(counts, target, threshold)
+
+    def test_extend_circuit_different_registers(self):
+        """Test extending a circuit with different registers.
+
+        If all correct should return the data
+        """
+
+        qr = QuantumRegister("qr", 2)
+        cr = ClassicalRegister("cr", 2)
+        qc1 = QuantumCircuit(qr)
+        qc1.x(qr)
+        qc2 = QuantumCircuit(qr, cr)
+        qc2.measure(qr, cr)
+        qc1 += qc2
+        qp = QuantumProgram()
+        name = 'test_circuit'
+        qp.add_circuit(name, qc1)
+        backend = 'local_qasm_simulator'  # the backend to run on
+        shots = 1024  # the number of shots in the experiment.
+        result = qp.execute(name, backend=backend, shots=shots, seed=78)
+        counts = result.get_counts(name)
+        target = {'11': shots}
+        threshold = 0.0
+        self.assertDictAlmostEqual(counts, target, threshold)
+
+    def test_extend_circuit_fail(self):
+        """Test extending a circuits fails if registers incompatible.
+
+        If two circuits have samed name register of different size or type
+        it should raise a QISKitError.
+        """
+        q1 = QuantumRegister("q", 1)
+        q2 = QuantumRegister("q", 2)
+        c1 = QuantumRegister("q", 1)
+        qc1 = QuantumCircuit(q1)
+        qc2 = QuantumCircuit(q2)
+        qc3 = QuantumCircuit(c1)
+
+        self.assertRaises(QISKitError, qc1.__iadd__, qc2)
+        self.assertRaises(QISKitError, qc1.__iadd__, qc3)
 
     def test_example_multiple_compile(self):
         """Test a toy example compiling multiple circuits.
