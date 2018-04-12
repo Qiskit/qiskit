295a3ba50aa14d954dc50501640d6d0a67e728f8
Ali Javadi <ajavadia@princeton.edu>
2018-04-12 12:13:54 -0400

    copying some fixes from erick's branch to QuantumJob to make jobprocessor test pass

--- qiskit/_quantumjob.py
+++ qiskit/_quantumjob.py
@@ -24,6 +24,7 @@ from qiskit.unroll import Unroller, DagUnroller, JsonBackend
 from qiskit.dagcircuit import DAGCircuit
 from qiskit import QuantumCircuit
 from qiskit.qasm import Qasm
+from qiskit import _openquantumcompiler as openquantumcompiler
 
 
 class QuantumJob():
@@ -33,7 +34,7 @@ class QuantumJob():
     # TODO We need to create more tests for checking all possible inputs.
     # TODO Make this interface clearer -- circuits could be many things!
     def __init__(self, circuits, backend='local_qasm_simulator',
-                 circuit_config=None, seed=None,
+                 config=None, circuit_config=None, seed=None,
                  resources=None,
                  shots=1024, names=None,
                  do_compile=False, preformatted=False):
@@ -76,14 +77,14 @@ class QuantumJob():
             # circuits is actually a qobj...validate (not ideal but convenient)
             self.qobj = circuits
         else:
-            self.qobj = self._create_qobj(circuits, circuit_config, backend,
+            self.qobj = self._create_qobj(circuits, config, circuit_config, backend,
                                           seed, resources, shots, do_compile)
         self.backend = self.qobj['config']['backend']
         self.resources = resources
         self.seed = seed
         self.result = None
 
-    def _create_qobj(self, circuits, circuit_config, backend, seed,
+    def _create_qobj(self, circuits, config, circuit_config, backend, seed,
                      resources, shots, do_compile):
         # local and remote backends currently need different
         # compilied circuit formats
@@ -91,6 +92,16 @@ class QuantumJob():
         if do_compile:
             for circuit in circuits:
                 formatted_circuits.append(None)
+        else:
+            for circuit in self.circuits:
+                formatted_circuits.append(
+                    openquantumcompiler.dag2json(circuit))
+        required_config = {
+            'max_credits': resources['max_credits'],
+            'shots': shots,
+            'backend': backend
+        }
+        """
         else:
             if backend in backends.local_backends():
                 for circuit in self.circuits:
@@ -109,15 +120,22 @@ class QuantumJob():
             else:
                 for circuit in self.circuits:
                     formatted_circuits.append(circuit.qasm(qeflag=True))
-
+                    """
+        # merge dicts, second overrides first. Used if user supplies a
+        # circuit_config formatted config for the top level for backends which
+        # use the top level config for backend specific defaults.
+        if config is None:
+            config = required_config
+        else:
+            config = {**config, **required_config}
         # create circuit component of qobj
         circuit_records = []
         if circuit_config is None:
-            config = {'coupling_map': None,
-                      'basis_gates': 'u1,u2,u3,cx,id',
-                      'layout': None,
-                      'seed': seed}
-            circuit_config = [config] * len(self.circuits)
+            circuit_config = {'coupling_map': None,
+                              'basis_gates': 'u1,u2,u3,cx,id',
+                              'layout': None,
+                              'seed': seed}
+            circuit_config = [circuit_config] * len(self.circuits)
 
         for circuit, fcircuit, name, config in zip(self.circuits,
                                                    formatted_circuits,
@@ -133,11 +151,7 @@ class QuantumJob():
             circuit_records.append(record)
 
         return {'id': self._generate_job_id(length=10),
-                'config': {
-                    'max_credits': resources['max_credits'],
-                    'shots': shots,
-                    'backend': backend
-                },
+                'config': config,
                 'circuits': circuit_records}
 
     def _generate_job_id(self, length=10):
