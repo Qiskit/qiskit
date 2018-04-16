d2d697d525251cece88136485420a9e18957df00
Erick Winston <ewinston@us.ibm.com>
2018-04-16 14:30:28 -0400

    updated dummybackend to use BaseJob

--- test/python/_dummybackend.py
+++ test/python/_dummybackend.py
@@ -24,9 +24,11 @@ purposes. Testing local timeouts, arbitrary responses or behavior, etc.
 import uuid
 import logging
 from threading import Event
+from concurrent import futures
 
 from qiskit import Result, QISKitError
 from qiskit.backends import BaseBackend
+from qiskit.backends import BaseJob
 from qiskit.backends.baseprovider import BaseProvider
 
 logger = logging.getLogger(__name__)
@@ -59,6 +61,9 @@ class DummySimulator(BaseBackend):
             self._configuration = configuration
 
     def run(self, q_job):
+        return DummyJob(self.run_job, q_job)
+    
+    def run_job(self, q_job):
         """ Main dummy simulator loop """
         job_id = str(uuid.uuid4())
         qobj = q_job.qobj
@@ -73,3 +78,40 @@ class DummySimulator(BaseBackend):
                 raise QISKitError('Dummy backend has timed out!')
 
         return Result({'job_id': job_id, 'result': [], 'status': 'COMPLETED'}, qobj)
+
+class DummyJob(BaseJob):
+    """dummy simulator job"""
+    _executor = futures.ProcessPoolExecutor()
+    
+    def __init__(self, fn, qobj):
+        self._qobj = qobj
+        self._future = self._executor.submit(fn, qobj)
+
+    def result(self, timeout=None):
+        return self._future.result(timeout=timeout)
+
+    def cancelled(self):
+        return self._future.cancelled()
+
+    def done(self):
+        return self._future.done()
+
+    def status(self):
+        if self.running():
+            return "running"
+        elif self.cancelled():
+            return "cancelled"
+        elif self.done():
+            return "done"
+        else:
+            return "unknown"
+
+    def abort(self):
+        return self._future.cancel()
+
+    def running(self):
+        return self._future.running()
+
+    def add_done_callback(self, fn):
+        self._future.add_done_callback(fn)
+    
