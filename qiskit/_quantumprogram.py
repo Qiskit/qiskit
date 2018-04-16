c474af40d9a461bd3597b84837c392bed988f3cc
Ali Javadi <ajavadia@princeton.edu>
2018-04-16 10:50:29 -0400

    remove commented lines

--- qiskit/_quantumprogram.py
+++ qiskit/_quantumprogram.py
@@ -1167,20 +1167,6 @@ class QuantumProgram(object):
                 coupling_map = None
             if coupling_map == 'all-to-all':
                 coupling_map = None
-            # if the backend is a real chip, insert barrier before measurements
-            if not backend_conf['simulator']:
-                measured_qubits = []
-                qasm_idx = []
-                for i, instruction in enumerate(circuit.data):
-                    if isinstance(instruction, Measure):
-                        measured_qubits.append(instruction.arg[0])
-                        qasm_idx.append(i)
-                    elif isinstance(instruction, Gate) and bool(set(instruction.arg) &
-                                                                set(measured_qubits)):
-                        raise QISKitError('backend "{0}" rejects gate after '
-                                          'measurement in circuit "{1}"'.format(backend, name))
-                #for i, qubit in zip(qasm_idx, measured_qubits):
-                #    circuit.data.insert(i, Barrier([qubit], circuit))
             dag_circuit, final_layout = openquantumcompiler.compile(
                 circuit,
                 basis_gates=basis_gates,
