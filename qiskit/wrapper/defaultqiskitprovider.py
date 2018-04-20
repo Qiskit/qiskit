05dbd651834b100bf89f70c6d5739bf53e6165a6
Ali Javadi <ajavadia@princeton.edu>
2018-04-20 15:12:48 -0400

    compact flag. resolve_backend_name method in default provider. assume name in all configs.

--- qiskit/wrapper/defaultqiskitprovider.py
+++ qiskit/wrapper/defaultqiskitprovider.py
@@ -44,7 +44,7 @@ class DefaultQISKitProvider(BaseProvider):
                 pass
         raise KeyError(name)
 
-    def available_backends(self, filters=None):
+    def available_backends(self, filters=None, compact=True):
         """
         Args:
             filters (dict): dictionary of filtering conditions.
