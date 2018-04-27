0a754e3637ff33d69a5c49faf41092ca34bb8511
Diego M. Rodriguez <diego.plan9@gmail.com>
2018-04-27 17:33:39 +0200

    Update QObj.as_dict, fixes, add test
    Revise the `QObjStructure.as_dict()` function in order to make it
    generic (at the base class level) and recursive: the public attributes
    of a structure are traversed, handling lists and delegating into each
    subobject.
    Use `SimpleNameSpace` for simplifying the handling of `kwargs` on the
    constructors via standard Python facilities.
    Update several QObj structures to reflect the current schema.
    Add a test for QObj instantation and `as_dict()` comparison.
    Make the `QObjStructures` pickable via `__reduce__`, in order to allow
    passing them to the futures.

--- qiskit/qobj/_qobj.py
+++ qiskit/qobj/_qobj.py
@@ -1,32 +1,48 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2018 IBM RESEARCH. All Rights Reserved.
+# Copyright 2018, IBM.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# =============================================================================
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree.
 
 """Models for QObj and its related components."""
+
+from types import SimpleNamespace
 
 from ._utils import QObjType
 
 
-class QObjStructure(object):
+class QObjStructure(SimpleNamespace):
     """
     General QObj structure.
     """
-    def __init__(self, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+    REQUIRED_ARGS = ()
+
+    def as_dict(self):
+        """Return a dictionary representation of the QObjStructure, recursively
+        converting its public attributes.
+
+        Returns:
+            dict: a dictionary.
+        """
+        def expand_item(obj):
+            """
+            Return a valid representation of `obj` depending on its type.
+            """
+            if isinstance(obj, list):
+                return [expand_item(item) for item in obj]
+            if isinstance(obj, QObjStructure):
+                return obj.as_dict()
+            return obj
+
+        return {key: expand_item(value) for key, value
+                in self.__dict__.items() if not key.startswith('_')}
+
+    def __reduce__(self):
+        init_args = tuple(getattr(self, key) for key in self.REQUIRED_ARGS)
+        extra_args = {key: value for key, value in self.__dict__.items()
+                      if key not in self.REQUIRED_ARGS}
+        return self.__class__, init_args, extra_args
 
 
 class QObj(QObjStructure):
@@ -36,31 +52,21 @@ class QObj(QObjStructure):
         id (str): QObj identifier.
         config (QObjConfig): config settings for the QObj.
         experiments (list[QObjExperiment]): list of experiments.
-        headers (QObjStructure): headers.
+        header (QObjStructure): headers.
         type (str): experiment type (QASM/PULSE).
     """
-    def __init__(self, id, config, experiments, headers, **kwargs):
+
+    REQUIRED_ARGS = ['id', 'config', 'experiments', 'header']
+
+    def __init__(self, id, config, experiments, header, **kwargs):
         # pylint: disable=redefined-builtin,invalid-name
         self.id = id
         self.config = config
         self.experiments = experiments
-        self.headers = headers
-        self.type = QObjType.PULSE.value
+        self.header = header
+        self.type = QObjType.QASM.value
 
         super().__init__(**kwargs)
-
-    def as_dict(self):
-        """
-        Returns:
-            dict: a dictionary representation of the QObj.
-        """
-        return {
-            'id': self.id,
-            'config': self.config.as_dict(),
-            'experiments': [experiment.as_dict() for experiment
-                            in self.experiments],
-            'headers': self.headers.as_dict()
-        }
 
 
 class QObjConfig(QObjStructure):
@@ -70,15 +76,13 @@ class QObjConfig(QObjStructure):
         shots (int): number of shots.
         register_slots (int): number of classical register slots.
     """
+    REQUIRED_ARGS = ['shots', 'register_slots']
+
     def __init__(self, shots, register_slots, **kwargs):
         self.shots = shots
         self.register_slots = register_slots
-        super().__init__(**kwargs)
 
-    def __eq__(self, other):
-        attrs = ['max_credits', 'shots', 'backend']
-        return all(getattr(self, attr) == getattr(other, attr)
-                   for attr in attrs)
+        super().__init__(**kwargs)
 
 
 class QObjExperiment(QObjStructure):
@@ -87,6 +91,8 @@ class QObjExperiment(QObjStructure):
     Attributes:
         instructions(list[QObjInstruction]): list of instructions
     """
+    REQUIRED_ARGS = ['instructions']
+
     def __init__(self, instructions, **kwargs):
         self.instructions = instructions
 
@@ -97,9 +103,13 @@ class QObjInstruction(QObjStructure):
     """Quantum Instruction.
 
     Attributes:
-        instructions(list[QObjInstruction]): list of instructions
+        name(str): name of the gate.
+        qubits(list): list of qubits to apply to the gate.
     """
-    def __init__(self, instructions, **kwargs):
-        self.instructions = instructions
+    REQUIRED_ARGS = ['name', 'qubits']
+
+    def __init__(self, name, qubits, **kwargs):
+        self.name = name
+        self.qubits = qubits
 
         super().__init__(**kwargs)
