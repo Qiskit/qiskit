dd9b0a95011c0c4df53ec25be85e673870b733d0
Luciano <luciano@debian.org>
2018-05-03 13:52:21 -0400

    Support for QuantumRegister in gates supporing qubits (#369)
    * no name in the registers means no name :-/
    
    * slip assertResult
    
    * simpler
    
    * 352 fix, test_rz_reg and test_s_reg
    
    * test_sdg_reg
    
    * swap behaves as CX when called with registers as args.
    
    * test_swap_reg_reg_inv
    
    * useless =
    
    * cx
    
    * cx_base
    
    * test_rz_reg_inv
    
    * ry and rx
    
    * test_s_reg_inv
    
    * cy and cz
    
    * barrier
    
    * keeping previous behaivor
    
    * ch
    
    * cu3
    
    * cu1
    
    * crz
    
    * crz and h
    
    * iden
    
    * sdg
    
    * t
    
    * tdg
    
    * u1
    
    * u2
    
    * u3
    
    * ubase
    
    * x
    
    * y
    
    * z
    
    * code organization
    
    * barrier for a list of qbits (and formating)
    
    * adding backwards compatibility to barrier
    
    * format
    
    * ccx and cswap
    
    * format
    
    * this extension of instructionset is not needed
    
    * format
    
    * ordered get_{c,q}regs
    
    * invalid tests
    
    * -from qiskit import InstructionSet
    
    * clarification in barrier usage
    
    * do not allow, swap q,r[1]
    
    * qasm stmt should be in new lines
    
    * docstring

--- qiskit/extensions/standard/ch.py
+++ qiskit/extensions/standard/ch.py
@@ -59,6 +59,18 @@ def ch(self, ctl, tgt):
             instructions.add(self.ch((ctl, i), (tgt, i)))
         return instructions
 
+    if isinstance(ctl, QuantumRegister):
+        gs = InstructionSet()
+        for j in range(ctl.size):
+            gs.add(self.ch((ctl, j), tgt))
+        return gs
+
+    if isinstance(tgt, QuantumRegister):
+        gs = InstructionSet()
+        for j in range(tgt.size):
+            gs.add(self.ch(ctl, (tgt, j)))
+        return gs
+
     self._check_qubit(ctl)
     self._check_qubit(tgt)
     self._check_dups([ctl, tgt])
--- qiskit/extensions/standard/cx.py
+++ qiskit/extensions/standard/cx.py
@@ -51,13 +51,25 @@ class CnotGate(Gate):
 
 
 def cx(self, ctl, tgt):
-    """Apply CNOT from ctl to tgt."""
+    """Apply CX from ctl to tgt."""
     if isinstance(ctl, QuantumRegister) and \
        isinstance(tgt, QuantumRegister) and len(ctl) == len(tgt):
         instructions = InstructionSet()
         for i in range(ctl.size):
             instructions.add(self.cx((ctl, i), (tgt, i)))
         return instructions
+
+    if isinstance(ctl, QuantumRegister):
+        gs = InstructionSet()
+        for j in range(ctl.size):
+            gs.add(self.cx((ctl, j), tgt))
+        return gs
+
+    if isinstance(tgt, QuantumRegister):
+        gs = InstructionSet()
+        for j in range(tgt.size):
+            gs.add(self.cx(ctl, (tgt, j)))
+        return gs
 
     self._check_qubit(ctl)
     self._check_qubit(tgt)
