00c64cd8a6c4ed8ced1e1a8369641085141700e3
Diego M. Rodriguez <diego.plan9@gmail.com>
2018-05-23 13:01:00 +0200

    Add __iter__ for Register
    Add an `__iter__` method for providing cleaner access to iterating over
    the elements of a `Register` and its subclasses via an iterator instead
    of relying on `__getitem__`.

--- qiskit/_register.py
+++ qiskit/_register.py
@@ -84,8 +84,27 @@ class Register(object):
             raise QISKitIndexError("register index out of range")
 
     def __getitem__(self, key):
-        """Return tuple (self, key) if key is valid."""
+        """
+        Arg:
+            key (int): index of the bit/qubit to be retrieved.
+
+        Returns:
+            tuple[Register, int]: a tuple in the form `(self, key)`.
+
+        Raises:
+            QISKitError: if the `key` is not an integer.
+            QISKitIndexError: if the `key` is not in the range
+                `(0, self.size)`.
+        """
         if not isinstance(key, int):
             raise QISKitError("expected integer index into register")
         self.check_range(key)
         return self, key
+
+    def __iter__(self):
+        """
+        Returns:
+            iterator: an iterator over the bits/qubits of the register, in the
+                form `tuple (Register, int)`.
+        """
+        return zip([self]*self.size, range(self.size))
