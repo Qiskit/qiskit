d9c4849aa85aa800f31000a387b678692fd564c7
Paul Nation <nonhermitian@gmail.com>
2018-05-23 04:26:19 -0400

    Fix mapping error for multiple qregs raised in #492 (#494)
    * Fix mapping error for multipl qreg.
    
    * better unittest

--- test/python/test_compiler.py
+++ test/python/test_compiler.py
@@ -346,6 +346,29 @@ class TestCompiler(QiskitTestCase):
             qobj = None
         self.assertIsInstance(qobj, dict)
 
+    @requires_qe_access
+    def test_mapping_multi_qreg(self, QE_TOKEN, QE_URL, hub=None, group=None, project=None):
+        """Test mapping works for multiple qregs.
+        """
+        provider = IBMQProvider(QE_TOKEN, QE_URL, hub, group, project)
+        backend = provider.get_backend('ibmqx5')
+
+        q = qiskit.QuantumRegister(3, name='qr')
+        q2 = qiskit.QuantumRegister(1, name='qr2')
+        q3 = qiskit.QuantumRegister(4, name='qr3')
+        c = qiskit.ClassicalRegister(3, name='cr')
+        qc = qiskit.QuantumCircuit(q, q2, q3, c)
+        qc.h(q[0])
+        qc.cx(q[0], q2[0])
+        qc.cx(q[1], q3[2])
+        qc.measure(q, c)
+
+        try:
+            qobj = qiskit._compiler.compile(qc, backend)
+        except QISKitError:
+            qobj = None
+        self.assertIsInstance(qobj, dict)
+
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
