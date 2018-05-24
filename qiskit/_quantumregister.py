6f76c88bdb67e3ecfdeeaa7c02d1be89d155d56c
Diego M. Rodríguez <diego.plan9@gmail.com>
2018-05-24 13:30:11 +0200

    Fix #479: Raise IndexError during Register.__getitem__ (#491)
    * Raise IndexError during Register.__getitem__
    
    Update `Register.__getitem__` to conform to the Python convention and
    raise a subclass of `IndexError` for identifying the end of the sequence
    when using it in a `for` loop. The exception raised is also a subclass
    of `QISKitError`, for consistency with our current exception policy.
    
    * Add __repr__ to Register, trim subclasses
    
    Add a `__repr__` method to `Register` that extends the previously
    existing `__str__` method, providing a valid Python string that can
    be used for recreating the object (fixing the argument order in the
    process).
    Remove methods from subclasses that are already provided by the
    parent class.
    
    * Add __iter__ for Register
    
    Add an `__iter__` method for providing cleaner access to iterating over
    the elements of a `Register` and its subclasses via an iterator instead
    of relying on `__getitem__`.

--- qiskit/_quantumregister.py
+++ qiskit/_quantumregister.py
@@ -33,11 +33,3 @@ class QuantumRegister(Register):
     def qasm(self):
         """Return OPENQASM string for this register."""
         return "qreg %s[%d];" % (self.name, self.size)
-
-    def __str__(self):
-        """Return a string representing the register."""
-        return "QuantumRegister(%s,%d)" % (self.name, self.size)
-
-    def __len__(self):
-        """Return a int representing the size."""
-        return self.size
