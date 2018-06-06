5f84d0e874af96109788315d06019ff22f1d33e1
Salvador de la Puente González <neo.salvador@gmail.com>
2018-06-06 17:52:21 +0200

    Convert LocalJob tests into unit-tests. (#526)
    * Convert LocalJob tests into unit-tests.
    
    It uses the unittest.mock module to patch external dependencies. It also uses auto-speccing [1] to ensure the mocks expose the same API as the objects being mocked.
    
    [1] https://docs.python.org/3/library/unittest.mock.html#autospeccing
    
    * Make code compatible with Python 3.5 mock API
    
    * Adding subtest, fixing names and clarifying some tests.
    
    * Fixing variable names
    
    * Fix lint, remove sympy simulators

--- test/python/test_localjob.py
+++ test/python/test_localjob.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# pylint: disable=invalid-name,missing-docstring,broad-except
+# pylint: disable=invalid-name,missing-docstring
 
 # Copyright 2017 IBM RESEARCH. All Rights Reserved.
 #
@@ -15,188 +15,154 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # =============================================================================
+"""LocalJob creation and test suite."""
 
-"""LocalJob Test."""
-
-import sys
+from os import path
 import unittest
-from concurrent import futures
-import time
-
-import numpy
-from scipy.stats import chi2_contingency
-
-import qiskit._compiler
-from qiskit import (ClassicalRegister, QuantumCircuit, QuantumRegister,
-                    QuantumJob)
-from qiskit.backends.local import LocalProvider, LocalJob
-from .common import requires_qe_access, QiskitTestCase, slow_test
+from unittest.mock import patch
+from contextlib import contextmanager
+from qiskit.backends.local import LocalJob
+from qiskit.backends.local import QasmSimulatorCpp, QasmSimulatorProjectQ
+from qiskit.backends.local import QasmSimulatorPy
+from qiskit.backends.local import StatevectorSimulatorCpp
+from qiskit.backends.local import StatevectorSimulatorPy
+from qiskit.backends.local import UnitarySimulatorPy
+from .common import QiskitTestCase
 
 
 class TestLocalJob(QiskitTestCase):
-    """
-    Test localjob module.
-    """
+    """Test how backends create LocalJob objects and the LocalJob class."""
 
-    @classmethod
-    @requires_qe_access
-    def setUpClass(cls, QE_TOKEN, QE_URL, hub=None, group=None, project=None):
-        # pylint: disable=arguments-differ
-        super().setUpClass()
-        # create QuantumCircuit
-        qr = QuantumRegister(2, 'q')
-        cr = ClassicalRegister(2, 'c')
-        qc = QuantumCircuit(qr, cr)
-        qc.h(qr[0])
-        qc.cx(qr[0], qr[1])
-        qc.measure(qr, cr)
-        cls._qc = qc
-        cls._provider = LocalProvider(QE_TOKEN, QE_URL, hub, group, project)
-
-    def setUp(self):
-        # Store the original executor, for restoring later.
-        self._original_executor = LocalJob._executor
-
-    def tearDown(self):
-        # Restore the original executor.
-        LocalJob._executor = self._original_executor
+    _backends = [
+        QasmSimulatorCpp,
+        QasmSimulatorProjectQ,
+        QasmSimulatorPy,
+        StatevectorSimulatorCpp,
+        StatevectorSimulatorPy,
+        UnitarySimulatorPy
+    ]
 
     def test_run(self):
-        backend = self._provider.get_backend('local_qasm_simulator_py')
-        qobj = qiskit._compiler.compile(self._qc, backend)
-        quantum_job = QuantumJob(qobj, backend, preformatted=True)
-        job = backend.run(quantum_job)
-        result = job.result()
-        counts_qx = result.get_counts(result.get_names()[0])
-        counts_ex = {'00': 512, '11': 512}
-        states = counts_qx.keys() | counts_ex.keys()
-        # contingency table
-        ctable = numpy.array([[counts_qx.get(key, 0) for key in states],
-                              [counts_ex.get(key, 0) for key in states]])
-        contingency = chi2_contingency(ctable)
-        self.log.info('chi2_contingency: %s', str(contingency))
-        self.assertGreater(contingency[1], 0.01)
+        with mocked_simulator_binaries(),\
+             patch.object(LocalJob, '__init__', return_value=None,
+                          autospec=True):
 
-    @slow_test
-    def test_run_async(self):
-        if sys.platform == 'darwin':
-            LocalJob._executor = futures.ThreadPoolExecutor(max_workers=2)
-        else:
-            LocalJob._executor = futures.ProcessPoolExecutor(max_workers=2)
-        try:
-            backend = self._provider.get_backend('local_qasm_simulator_cpp')
-        except KeyError:
-            backend = self._provider.get_backend('local_qasm_simulator_py')
-        num_qubits = 15
-        qr = QuantumRegister(num_qubits, 'q')
-        cr = ClassicalRegister(num_qubits, 'c')
-        qc = QuantumCircuit(qr, cr)
-        for i in range(num_qubits-1):
-            qc.cx(qr[i], qr[i+1])
-        qc.measure(qr, cr)
-        qobj = qiskit._compiler.compile(qc, backend)
-        quantum_job = QuantumJob(qobj, backend, preformatted=True)
-        num_jobs = 5
-        job_array = [backend.run(quantum_job) for _ in range(num_jobs)]
-        found_async_jobs = False
-        timeout = 30
-        start_time = time.time()
-        self.log.info('testing with simulator: %s', backend.name)
-        while not found_async_jobs:
-            check = sum([job.running for job in job_array])
-            if check >= 2:
-                self.log.info('found %d simultaneous jobs', check)
-                found_async_jobs = True
-            if all([job.done for job in job_array]):
-                self.log.warning('all jobs completed before simultaneous jobs '
-                                 'could be detected')
-                break
-            for job in job_array:
-                self.log.info('%s %s %s', job.status['status'],
-                              job.running, check)
-            self.log.info('%s %.4f', '-'*20, time.time()-start_time)
-            if time.time() - start_time > timeout:
-                raise TimeoutError('failed to see multiple running jobs after '
-                                   '{0} s'.format(timeout))
-            time.sleep(1)
+            for backend_constructor in self._backends:
+                with self.subTest(backend=backend_constructor):
+                    self.log.info('Backend under test: %s', backend_constructor)
+                    backend = backend_constructor()
+                    job = backend.run(FakeQJob())
+                    self.assertIsInstance(job, LocalJob)
 
-        # Wait for all the jobs to finish.
-        # TODO: this causes the test to wait until the 15 qubit jobs are
-        # finished, which might take long (hence the @slow_test). Waiting for
-        # the result is needed as otherwise the jobs would still be running
-        # once the test is completed, causing failures in subsequent tests as
-        # the executor's queue might be overloaded.
+    def test_multiple_execution(self):
+        # Notice that it is Python responsibility to test the executors
+        # can run several tasks at the same time. It is our responsibility to
+        # use the executor correctly. That is what this test checks.
 
-        _ = [job.result() for job in job_array]
+        taskcount = 10
+        target_tasks = [lambda: None for _ in range(taskcount)]
+
+        # pylint: disable=redefined-outer-name
+        with intercepted_executor_for_localjob() as (LocalJob, executor):
+            for index in range(taskcount):
+                LocalJob(target_tasks[index], FakeQJob())
+
+        self.assertEqual(executor.submit.call_count, taskcount)
+        for index in range(taskcount):
+            _, callargs, _ = executor.submit.mock_calls[index]
+            submitted_task = callargs[0]
+            target_task = target_tasks[index]
+            self.assertEqual(submitted_task, target_task)
 
     def test_cancel(self):
-        """Test the cancelation of jobs.
+        # Again, cancelling jobs is beyond our responsibility. In this test
+        # we only check if we delegate on the proper method of the underlaying
+        # future object.
 
-        Since only Jobs that are still in the executor queue pending to be
-        executed can be cancelled, this test launches a lot of jobs, passing
-        if some of them can be cancelled.
-        """
-        # Force the number of workers to 1, as only Jobs that are still in
-        # the executor queue can be canceled.
-        if sys.platform == 'darwin':
-            LocalJob._executor = futures.ThreadPoolExecutor(max_workers=1)
-        else:
-            LocalJob._executor = futures.ProcessPoolExecutor(max_workers=1)
-
-        backend = self._provider.get_backend('local_qasm_simulator_py')
-        num_qubits = 5
-        qr = QuantumRegister(num_qubits, 'q')
-        cr = ClassicalRegister(num_qubits, 'c')
-        qc = QuantumCircuit(qr, cr)
-        for i in range(num_qubits-1):
-            qc.cx(qr[i], qr[i+1])
-        qc.measure(qr, cr)
-        qobj = qiskit._compiler.compile(qc, backend)
-        quantum_job = QuantumJob(qobj, backend, preformatted=True)
-        num_jobs = 10
-        timeout = 10
-        start_time = time.time()
-        self.log.info('testing with simulator: %s', backend.name)
-        job_array = [backend.run(quantum_job) for _ in range(num_jobs)]
-        for job in job_array:
+        # pylint: disable=redefined-outer-name
+        with intercepted_executor_for_localjob() as (LocalJob, executor):
+            job = LocalJob(lambda: None, FakeQJob())
             job.cancel()
-        found_cancelled = False
-        while not found_cancelled:
-            check = sum([job.cancelled for job in job_array])
-            if check >= 1:
-                self.log.info('found %d cancelled jobs', check)
-                found_cancelled = True
-            if all([job.done for job in job_array]):
-                self.log.warning('all jobs completed before simultaneous jobs '
-                                 'could be detected')
-                break
-            for job in job_array:
-                self.log.info('%s %s %s', job.status['status'], job.cancelled,
-                              check)
-            self.log.info('{0} {1:0.2f}'.format('-'*20, time.time()-start_time))
-            if time.time() - start_time > timeout:
-                raise TimeoutError('failed to see multiple running jobs after '
-                                   '{0} s'.format(timeout))
-            time.sleep(1)
 
-        # Wait for all the jobs to finish.
-        _ = [job.result() for job in job_array if not job.cancelled]
+        self.assertCalledOnce(executor.submit)
+        mocked_future = executor.submit.return_value
+        self.assertCalledOnce(mocked_future.cancel)
 
     def test_done(self):
-        backend = self._provider.get_backend('local_qasm_simulator_py')
-        qobj = qiskit._compiler.compile(self._qc, backend)
-        quantum_job = QuantumJob(qobj, backend, preformatted=True)
-        job = backend.run(quantum_job)
-        job.result()
-        self.assertTrue(job.done)
+        # Once more, testing that reading the `done` property delegates into
+        # the proper future API.
 
-    def test_get_backend_name(self):
-        backend_name = 'local_qasm_simulator_py'
-        backend = self._provider.get_backend(backend_name)
-        qobj = qiskit._compiler.compile(self._qc, backend)
-        quantum_job = QuantumJob(qobj, backend, preformatted=True)
-        job = backend.run(quantum_job)
-        self.assertTrue(job.backend_name == backend_name)
+        # pylint: disable=redefined-outer-name
+        with intercepted_executor_for_localjob() as (LocalJob, executor):
+            job = LocalJob(lambda: None, FakeQJob())
+            _ = job.done
+
+        self.assertCalledOnce(executor.submit)
+        mocked_future = executor.submit.return_value
+        self.assertCalledOnce(mocked_future.done)
+
+    def assertCalledOnce(self, mocked_callable):
+        """Assert a mocked callable has been called once."""
+        call_count = mocked_callable.call_count
+        self.assertEqual(
+            call_count, 1,
+            'Callable object has been called more than once ({})'.format(
+                call_count))
+
+
+class FakeQJob():
+    def __init__(self):
+        self.backend = FakeBackend()
+        self.qobj = {
+            'id': 'test-id',
+            'config': {
+                'backend_name': self.backend.name,
+                'shots': 1024,
+                'max_credits': 100
+            },
+            'circuits': [{
+                'compiled_circuit_qasm': 'fake-code',
+                'config': {
+                    'seed': 123456
+                }
+            }]
+        }
+
+
+class FakeBackend():
+
+    def __init__(self):
+        self.name = 'test-backend'
+
+
+@contextmanager
+def intercepted_executor_for_localjob():
+    """Context that patches the derived executor classes to return the same
+    executor object. Also patches the future object returned by executor's
+    submit()."""
+
+    import importlib
+    import concurrent.futures as futures
+    import qiskit.backends.local.localjob as localjob
+
+    executor = unittest.mock.MagicMock(spec=futures.Executor)
+    executor.submit.return_value = unittest.mock.MagicMock(spec=futures.Future)
+    mock_options = {'return_value': executor, 'autospec': True}
+    with patch.object(futures, 'ProcessPoolExecutor', **mock_options),\
+            patch.object(futures, 'ThreadPoolExecutor', **mock_options):
+        importlib.reload(localjob)
+        yield localjob.LocalJob, executor
+
+
+@contextmanager
+def mocked_simulator_binaries():
+    """Context to force binary-based simulators to think the simulators exist.
+    """
+    from qiskit.backends.local import qasm_simulator_projectq
+    with patch.object(path, 'exists', return_value=True, autospec=True),\
+            patch.object(path, 'getsize', return_value=1000, autospec=True),\
+            patch.object(qasm_simulator_projectq, 'CppSim', {}):
+        yield
 
 
 if __name__ == '__main__':
