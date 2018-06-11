b063f46d009f645b2dc9e05b95bc9271ff454883
Ali Javadi-Abhari <ajavadia@users.noreply.github.com>
2018-06-11 06:31:00 -0400

    Fix incorrect unrolling of t to tdg in CircuitBackend (#557)

--- qiskit/unroll/_circuitbackend.py
+++ qiskit/unroll/_circuitbackend.py
@@ -235,7 +235,7 @@ class CircuitBackend(UnrollerBackend):
                    "rz": [(1, 1), lambda x: self.circuit.rz(x[0][0], x[1][0])],
                    "s": [(0, 1), lambda x: self.circuit.s(x[1][0])],
                    "sdg": [(0, 1), lambda x: self.circuit.s(x[1][0]).inverse()],
-                   "t": [(0, 1), lambda x: self.circuit.t(x[1][0]).inverse()],
+                   "t": [(0, 1), lambda x: self.circuit.t(x[1][0])],
                    "tdg": [(0, 1), lambda x: self.circuit.t(x[1][0]).inverse()],
                    "u1": [(1, 1), lambda x: self.circuit.u1(x[0][0], x[1][0])],
                    "u2": [(2, 1), lambda x: self.circuit.u2(x[0][0], x[0][1],
