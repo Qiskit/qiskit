7b598e1459cbd406e95354d5fc663c870891ec42
Eddie Schoute <eddieschoute@gmail.com>
2018-06-11 12:28:59 -0400

    Change get_named_nodes to set

--- qiskit/dagcircuit/_dagcircuit.py
+++ qiskit/dagcircuit/_dagcircuit.py
@@ -1072,15 +1072,15 @@ class DAGCircuit:
                 self.multi_graph.remove_edge(p[0], self.output_map[w])
 
     def get_named_nodes(self, name):
-        """Returns a list of "op" node ids with the given name in arbitrary order."""
+        """Get the set of "op" node ids with the given name."""
         if name not in self.basis:
             raise DAGCircuitError("%s is not in the list of basis operations"
                                   % name)
 
         # We need to instantiate the full list now because the underlying multi_graph
         # may change when users iterate over the named nodes.
-        return [node_id for node_id, data in self.multi_graph.nodes(data=True)
-                if data["type"] == "op" and data["name"] == name]
+        return {node_id for node_id, data in self.multi_graph.nodes(data=True)
+                if data["type"] == "op" and data["name"] == name}
 
     def _remove_op_node(self, n):
         """Remove an operation node n.
