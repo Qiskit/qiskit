243f38ddb4f8e3b84ec37c0b7594992ca8231ce3
Salvador de la Puente González <salva@unoyunodiez.com>
2018-07-01 19:19:27 +0200

    Disabling output tests when matplotlib drawer is unavailable

--- test/python/test_visualization_output.py
+++ test/python/test_visualization_output.py
@@ -14,9 +14,18 @@ import os
 import unittest
 from math import pi
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
-from qiskit.tools.visualization import (latex_circuit_drawer,
-                                        matplotlib_circuit_drawer)
 from .common import QiskitTestCase
+
+try:
+    from qiskit.tools.visualization import (latex_circuit_drawer,
+                                            matplotlib_circuit_drawer)
+    VALID_MATPLOTLIB = True
+except RuntimeError:
+    # Under some combinations (travis osx vms, or headless configurations)
+    # matplotlib might not be fully, raising:
+    # RuntimeError: Python is not installed as a framework.
+    # when importing. If that is the case, the full test is skipped.
+    VALID_MATPLOTLIB = False
 
 
 def _path_to_diagram_reference(filename):
@@ -30,6 +39,7 @@ def _this_directory():
 # TODO: Enable for refactoring purposes and enable by default when we can
 # decide if the backend is available or not.
 @unittest.skip('Useful for refactoring purposes, skipping by default.')
+@unittest.skipIf(not VALID_MATPLOTLIB, 'matplotlib not available.')
 class TestVisualizationImplementation(QiskitTestCase):
     """Visual accuracy of visualization tools outputs tests."""
 
