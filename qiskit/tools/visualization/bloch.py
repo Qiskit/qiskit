0e9520fa8d2fa3a7eb2d2b1832de34c163b0965f
Paul Nation <nonhermitian@gmail.com>
2018-07-02 11:23:46 -0400

    add back import-error override

--- qiskit/tools/visualization/bloch.py
+++ qiskit/tools/visualization/bloch.py
@@ -39,6 +39,7 @@
 #    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ###############################################################################
 
+# pylint: import-error
 
 """Bloch sphere"""
 
