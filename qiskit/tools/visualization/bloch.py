711f9e5b2f579f6f886b7baaaa749f022f1cea77
Paul Nation <nonhermitian@gmail.com>
2018-07-03 15:20:57 -0400

    fix doc ticks

--- qiskit/tools/visualization/bloch.py
+++ qiskit/tools/visualization/bloch.py
@@ -617,8 +617,8 @@ class Bloch():
 
 
 def _hide_tick_lines_and_labels(axis):
-    '''
+    """
     Set visible property of ticklines and ticklabels of an axis to False
-    '''
+    """
     for item in axis.get_ticklines() + axis.get_ticklabels():
         item.set_visible(False)
