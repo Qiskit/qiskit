a286c2e1f32d348bdebe42e443b8e58d0539d74e
Paul Nation <nonhermitian@gmail.com>
2018-07-27 11:07:08 -0400

    Fix #674

--- qiskit/mapper/__init__.py
+++ qiskit/mapper/__init__.py
@@ -10,4 +10,6 @@
 from ._compiling import two_qubit_kak, euler_angles_1q
 from ._coupling import Coupling, coupling_dict2list, coupling_list2dict
 from ._mappererror import MapperError
-from ._mapping import swap_mapper, direction_mapper, cx_cancellation, optimize_1q_gates
+from ._mapping import (swap_mapper, direction_mapper, cx_cancellation,
+                       optimize_1q_gates, remove_last_measurements,
+                       return_last_measurements)
