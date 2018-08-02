7598d3face47efdf89fd1f237362042e01cb4917
Diego M. Rodriguez <diego.plan9@gmail.com>
2018-08-02 11:26:53 +0200

    `if` style fix

--- test/python/test_filter_backends.py
+++ test/python/test_filter_backends.py
@@ -19,10 +19,7 @@ class TestBackendFilters(QiskitTestCase):
     @requires_qe_access
     def test_filter_config_dict(self, QE_TOKEN, QE_URL, hub=None, group=None, project=None):
         """Test filtering by dictionary of configuration properties"""
-        if not self.using_ibmq_credentials:
-            n_qubits = 5
-        else:
-            n_qubits = 20
+        n_qubits = 20 if self.using_ibmq_credentials else 5
 
         register(QE_TOKEN, QE_URL, hub, group, project)
         filter_ = {'n_qubits': n_qubits, 'local': False}
