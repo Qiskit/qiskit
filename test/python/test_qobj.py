afee57379cdae8860542b6c1dbdab4f1ab8e30fd
Max Reuter <max.reuter@ibm.com>
2018-08-08 10:53:41 -0400

    Clean up Qobj from_dict test for styling and clarity

--- test/python/test_qobj.py
+++ test/python/test_qobj.py
@@ -60,26 +60,31 @@ class TestQobj(QiskitTestCase):
         except jsonschema.ValidationError as validation_error:
             self.fail(str(validation_error))
 
-    @unittest.expectedFailure  # expected to fail until _qobjectify_item is updated
+    @unittest.expectedFailure
+    # expected to fail until _qobjectify_item is updated (see TODO_ on line 77 of _qobj.py)
     def test_from_dict_per_class(self):
         """Test Qobj and its subclass representations given a dictionary."""
-        qobj_config_dict = {'shots': 1, 'memory_slots': 2}
-        qobj_experiment_dict = {'instructions':
-                                [QobjInstruction(name='u1', qubits=[1], params=[0.4])]}
-        qobj_instruction_dict = {'name': 'u1', 'qubits': [1], 'params': [0.4]}
+        test_parameters = {
+            Qobj: (
+                self.valid_qobj,
+                self.valid_dict
+            ),
+            QobjConfig: (
+                QobjConfig(shots=1, memory_slots=2, max_credits=10),
+                {'shots': 1, 'memory_slots': 2}
+            ),
+            QobjExperiment: (
+                QobjExperiment(
+                    instructions=[QobjInstruction(name='u1', qubits=[1], params=[0.4])]),
+                {'instructions': [QobjInstruction(name='u1', qubits=[1], params=[0.4])]}
+            ),
+            QobjInstruction: (
+                QobjInstruction(name='u1', qubits=[1], params=[0.4]),
+                {'name': 'u1', 'qubits': [1], 'params': [0.4]}
+            )
+        }
 
-        valid_qobj_config = QobjConfig(shots=1024, memory_slots=2, max_credits=10)
-        valid_qobj_experiment = QobjExperiment(
-            instructions=[QobjInstruction(name='u1', qubits=[1], params=[0.4])]
-        )
-        valid_qobj_instruction = QobjInstruction(name='u1', qubits=[1], params=[0.4])
-
-        dicts = [self.valid_dict, qobj_config_dict, qobj_experiment_dict, qobj_instruction_dict]
-        objects = [self.valid_qobj, valid_qobj_config,
-                   valid_qobj_experiment, valid_qobj_instruction]
-        classes = [Qobj, QobjConfig, QobjExperiment, QobjInstruction]
-
-        for objekt, dikt, klass in zip(objects, dicts, classes):
-            with self.subTest(msg=str(klass) + ' failed from_dict test.'):
-                if objekt != klass.from_dict(dikt):
-                    self.assertEqual(objekt, klass.from_dict(dikt))
+        for qobj_class, (qobj, expected_dict) in test_parameters.items():
+            with self.subTest(msg=str(qobj_class)):
+                if qobj != qobj_class.from_dict(expected_dict):
+                    self.assertEqual(qobj, qobj_class.from_dict(expected_dict))
