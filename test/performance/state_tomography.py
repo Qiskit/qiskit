d6aafa06f3aee93073640e5ffa8ea16b2013cd33
Ali Javadi <ajavadia@princeton.edu>
2018-08-24 14:46:53 -0400

    update performance benchmarking based on tomography

--- test/performance/state_tomography.py
+++ test/performance/state_tomography.py
@@ -18,8 +18,7 @@ import time
 
 # import qiskit modules
 from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
-from qiskit import execute
-from qiskit import QISKitError
+from qiskit import QISKitError, execute
 
 # import tomography libary and other useful tools
 import qiskit.tools.qcvv.tomography as tomo
@@ -32,32 +31,34 @@ def target_prep(state, target):
     # quantum circuit to make an entangled cat state
     if state == 'cat':
         n_qubits = int(np.log2(target.size))
-        qr = QuantumRegister(n_qubits)
-        cr = ClassicalRegister(n_qubits)
-        circ = QuantumCircuit(qr, cr)
+        qr = QuantumRegister(n_qubits, 'qr')
+        cr = ClassicalRegister(n_qubits, 'cr')
+        circ = QuantumCircuit(qr, cr, name='cat')
         circ.h(qr[0])
         for i in range(1, n_qubits):
-            cat.cx(qr[0], qr[i])
+            circ.cx(qr[0], qr[i])
 
     # quantum circuit to prepare arbitrary given state
     elif state == 'random':
         n_qubits = int(np.log2(target.size))
-        qr = QuantumRegister(n_qubits)
-        cr = ClassicalRegister(n_qubits)
-        circ = QuantumCircuit(qr, cr)
+        qr = QuantumRegister(n_qubits, 'qr')
+        cr = ClassicalRegister(n_qubits, 'cr')
+        circ = QuantumCircuit(qr, cr, name='random')
         circ.initialize("Qinit", target, [qr[i] for i in range(n_qubits)])
 
     return circ
 
 
-# add basis measurements for tomography
+# add basis measurements to the circuit for tomography
 # XX..X, XX..Y, .., ZZ..Z
 def add_tomo_circuits(circ):
     # Construct state tomography set for measurement of qubits in the register
+    qr = next(iter(circ.get_qregs().values()))
+    cr = next(iter(circ.get_cregs().values()))
     tomo_set = tomo.state_tomography_set(list(range(qr.size)))
 
     # Add the state tomography measurement circuits
-    tomo_circuits = tomo.create_tomography_circuits(circ, tomo_set)
+    tomo_circuits = tomo.create_tomography_circuits(circ, qr, cr, tomo_set)
 
     return tomo_set, tomo_circuits
 
@@ -81,17 +82,17 @@ def state_tomography(state, n_qubits, shots):
     # Use the local qasm simulator
     backend = 'local_qasm_simulator'
 
-    # Prepare target state and assess quality
-    circ = target_prep(state, target)
-    prep_result = execute(circ, backend='local_statevector_simulator')
-    prep_state = prep_result.get_statevector()
+    # Prepared target state and assess quality
+    prep_circ = target_prep(state, target)
+    prep_result = execute(prep_circ, backend='local_statevector_simulator').result()
+    prep_state = prep_result.get_statevector(prep_circ)
     F_prep = state_fidelity(prep_state, target)
     print('Prepared state fidelity =', F_prep)
 
-    # Run state tomography simulation and fit data to reconstruct state
-    tomo_set, tomo_circuits = add_tomo_circuits(circ)
-    tomo_result = execute(tomo_circuits, backend=backend, shots=shots)
-    tomo_data = tomo.tomography_data(tomo_result, circ, tomo_set)
+    # Run state tomography simulation and fit data to reconstruct circuit
+    tomo_set, tomo_circuits = add_tomo_circuits(prep_circ)
+    tomo_result = execute(tomo_circuits, backend=backend, shots=shots).result()
+    tomo_data = tomo.tomography_data(tomo_result, prep_circ.name, tomo_set)
     rho_fit = tomo.fit_tomography_data(tomo_data)
 
     # calculate fidelity and purity of fitted state
@@ -112,11 +113,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     tstart = time.time()
-    circuits = state_tomography(args.state, args.n_qubits, args.shots)
+    tomo_circuits = state_tomography(args.state, args.n_qubits, args.shots)
     tend = time.time()
 
-    avg = sum(c.qasm().count("\n") for c in circuits) / len(circuits)
+    avg = sum(c.qasm().count("\n") for c in tomo_circuits) / len(tomo_circuits)
 
-    print("---- Number of circuits: {}".format(len(all_circuits)))
+    print("---- Number of circuits: {}".format(len(tomo_circuits)))
     print("---- Avg circuit size: {}".format(avg))
     print("---- Elapsed time: {}".format(tend - tstart))
