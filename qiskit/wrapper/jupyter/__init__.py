4c5f23e21cca52d7a529931556430d6f235c312e
Paul Nation <nonhermitian@gmail.com>
2018-08-30 06:34:15 -0400

    requested updates

--- qiskit/wrapper/jupyter/__init__.py
+++ qiskit/wrapper/jupyter/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# pylint: disable=import-error
+
 # Copyright 2018, IBM.
 #
 # This source code is licensed under the Apache License, Version 2.0 found in
@@ -8,8 +8,8 @@
 """Initilize the Jupyter routines.
 """
 
-from IPython import get_ipython
+from IPython import get_ipython          # pylint: disable=import-error
 from .jupyter_magics import StatusMagic
 
-_ip = get_ipython()  # pylint: disable=C0103
-_ip.register_magics(StatusMagic)
+_IP = get_ipython()
+_IP.register_magics(StatusMagic)
--- qiskit/wrapper/jupyter/jupyter_magics.py
+++ qiskit/wrapper/jupyter/jupyter_magics.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# pylint: disable=import-error
+
 # Copyright 2018, IBM.
 #
 # This source code is licensed under the Apache License, Version 2.0 found in
@@ -10,10 +10,10 @@
 import sys
 import time
 import threading
-from IPython.display import display
-from IPython.core import magic_arguments
-from IPython.core.magic import cell_magic, Magics, magics_class
-import ipywidgets as widgets
+from IPython.display import display                              # pylint: disable=import-error
+from IPython.core import magic_arguments                         # pylint: disable=import-error
+from IPython.core.magic import cell_magic, Magics, magics_class  # pylint: disable=import-error
+import ipywidgets as widgets                                     # pylint: disable=import-error
 import qiskit
 
 
@@ -30,74 +30,74 @@ class StatusMagic(Magics):
         default=2,
         help='Interval for status check.'
     )
-    def qiskit_job_status(self, line='', cell=None):  # pylint: disable=W0613
+    def qiskit_job_status(self, line='', cell=None):
         """A Jupyter magic function to check the status of a Qiskit job instance.
         """
         args = magic_arguments.parse_argstring(self.qiskit_job_status, line)
         # Split cell lines to get LHS variables
-        _cell_lines = cell.split('\n')
-        _split_lines = []
-        for _line in _cell_lines:
-            _split_lines.append(_line.replace(' ', '').split('='))
+        cell_lines = cell.split('\n')
+        split_lines = []
+        for cline in cell_lines:
+            split_lines.append(cline.replace(' ', '').split('='))
 
         # Execute the cell
         self.shell.ex(cell)
 
         # Look for all vars that are BaseJob instances
-        _jobs = []
-        for _line in _split_lines:
-            if len(_line) == 2:
-                _line_var = _line[0]
+        jobs = []
+        for spline in split_lines:
+            if len(spline) == 2:
+                line_var = spline[0]
                 # Check corner case where expression with equals is commented out
-                if '#' not in _line_var:
-                    if isinstance(self.shell.user_ns[_line_var], qiskit.backends.basejob.BaseJob):
-                        _jobs.append(_line_var)
+                if '#' not in line_var:
+                    if isinstance(self.shell.user_ns[line_var], qiskit.backends.basejob.BaseJob):
+                        jobs.append(line_var)
         # Must have one job class
-        if not any(_jobs):
+        if not any(jobs):
             raise Exception(
                 "Cell just contain at least one 'job=qiskit.execute(...)' expression")
 
         def _checker(job_var, status, header):
-            _status_name = job_var.status().name
-            while _status_name not in ['DONE', 'CANCELLED']:
+            job_status_name = job_var.status().name
+            while job_status_name not in ['DONE', 'CANCELLED']:
                 time.sleep(args.interval)
-                _status = job_var.status()
-                _status_name = _status.name
-                _status_msg = _status.value
-                if _status_name == 'ERROR':
+                job_status = job_var.status()
+                job_status_name = job_status.name
+                job_status_msg = job_status.value
+                if job_status_name == 'ERROR':
                     break
                 else:
-                    if _status_name == 'QUEUED':
-                        _status_msg += ' (%s)' % job_var._queue_position
-                    status.value = header % (_status_msg)
+                    if job_status_name == 'QUEUED':
+                        job_status_msg += ' (%s)' % job_var._queue_position
+                    status.value = header % (job_status_msg)
 
-            status.value = header % (_status_msg)
+            status.value = header % (job_status_msg)
             # Explicitly stop the thread just to be safe.
             sys.exit()
 
         # List index of job if checking status of multiple jobs.
-        _multi_job = False
-        if len(_jobs) > 1:
-            _multi_job = True
+        multi_job = False
+        if len(jobs) > 1:
+            multi_job = True
 
-        _job_checkers = []
+        job_checkers = []
         # Loop over every BaseJob that was found.
-        for idx, job in enumerate(_jobs):
+        for idx, job in enumerate(jobs):
             job_var = self.shell.user_ns[job]
-            _style = "font-size:16px;"
-            if _multi_job:
+            style = "font-size:16px;"
+            if multi_job:
                 idx_str = '[%s]' % idx
             else:
                 idx_str = ''
-            _header = "<p style='{style}'>Job Status {id}: %s </p>".format(id=idx_str,
-                                                                           style=_style)
-            _status = widgets.HTML(
-                value=_header % job_var.status().value)
+            header = "<p style='{style}'>Job Status {id}: %s </p>".format(id=idx_str,
+                                                                          style=style)
+            status = widgets.HTML(
+                value=header % job_var.status().value)
 
-            thread = threading.Thread(target=_checker, args=(job_var, _status, _header))
+            thread = threading.Thread(target=_checker, args=(job_var, status, header))
             thread.start()
-            _job_checkers.append(_status)
+            job_checkers.append(status)
 
         # Group all HTML widgets into single vertical layout
-        box = widgets.VBox(_job_checkers)
+        box = widgets.VBox(job_checkers)
         display(box)
