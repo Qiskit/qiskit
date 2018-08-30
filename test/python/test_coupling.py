e1094811f810eb86e3fe75cc5b97c6b4e0dada4b
Ali Javadi <ajavadia@princeton.edu>
2018-08-30 13:25:17 -0400

    remove 3 more pylint ignores

--- test/python/test_coupling.py
+++ test/python/test_coupling.py
@@ -5,7 +5,6 @@
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
 
-# pylint: disable=invalid-name
 
 """Coupling Graph Test: Directed graph object for representing coupling between qubits."""
 
