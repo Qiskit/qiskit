39ca802491f7e3ccf90e243b7a32a9818eb236d9
Matthew Treinish <mtreinish@kortar.org>
2018-09-05 16:19:47 -0400

    Conform iplot_histogram and plot_histogram
    This commit fixes the inconsistencies between the 2 histogram plot
    functions iplot_histogram (for an interactive js plot) and
    plot_histogram (for a matplotlib generated plot). Both functions perform
    the same function just using different tools. To make them
    interchangeable this commit addresses the differences between the 2 and
    makes the interface the same between the 2. However, because of
    backwards compat concerns with plot_histogram (which has been present in
    several releases already) there is a some duplication of number_to_keep
    kwarg and the options dict. This deprecates the number_to_keep parameter
    so users can migrate off of it and then we can remove it from both
    plot_histogram and iplot_histogram.
    
    With the interfaces to both functions being the same this commit also
    updates the __init__ module to leverage iplot_histogram by default if
    qiskit.tools.visualization.plot_histogram() is called and we're running
    under jupyter and have a network connection to download the necessary
    js.
    
    Fixes #864

--- qiskit/tools/visualization/__init__.py
+++ qiskit/tools/visualization/__init__.py
@@ -11,14 +11,18 @@ import sys
 
 from ._circuit_visualization import circuit_drawer, plot_circuit, generate_latex_source,\
     latex_circuit_drawer, matplotlib_circuit_drawer, qx_color_scheme
-from ._counts_visualization import plot_histogram
 
 if ('ipykernel' in sys.modules) and ('spyder' not in sys.modules):
     import requests
     if requests.get(
             'https://qvisualization.mybluemix.net/').status_code == 200:
         from .interactive._iplot_state import iplot_state as plot_state
+        from .interactive._iplot_histogram import iplot_histogram as \
+            plot_histogram
     else:
         from ._state_visualization import plot_state
+        from ._counts_visualization import plot_histogram
+
 else:
     from ._state_visualization import plot_state
+    from ._counts_visualization import plot_histogram
