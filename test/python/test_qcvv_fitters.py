1260301e99c3464cac7e6b74ac0e38b93e471744
Matthew Treinish <mtreinish@kortar.org>
2018-09-14 11:19:13 -0400

    Add docstrings
    This commit adds docstrings to all the tests and removes the pylint
    ignore to enforce the presence of docstrings.

--- test/python/test_qcvv_fitters.py
+++ test/python/test_qcvv_fitters.py
@@ -5,7 +5,7 @@
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
 
-# pylint: disable=missing-docstring
+"""Test qiskit.tools.qcvv.fitters."""
 
 import unittest
 
@@ -26,8 +26,10 @@ except RuntimeError:
 
 @unittest.skipUnless(VALID_MATPLOTLIB, 'osx matplotlib backend not avaiable')
 class TestQCVVFitters(QiskitTestCase):
+    """Tests for functions in qiskit.tools.qcvv.fitters."""
 
     def test_exp_fit_fun(self):
+        """Test exponential decay fit function."""
         a_in = 2
         x_in = 3
         tau_in = 4
@@ -36,6 +38,7 @@ class TestQCVVFitters(QiskitTestCase):
         self.assertAlmostEqual(1.9447331054820294, result)
 
     def test_osc_fit_fun(self):
+        """Test decay cosine fit function."""
         a_in = 2
         x_in = 3
         tau_in = 4
@@ -46,6 +49,7 @@ class TestQCVVFitters(QiskitTestCase):
         self.assertAlmostEqual(0.5766900211497354, result)
 
     def test_rb_fit_fun(self):
+        """Test randomized benchmark fit function."""
         a_in = 2
         x_in = 3
         alpha_in = 4
@@ -54,6 +58,7 @@ class TestQCVVFitters(QiskitTestCase):
         self.assertAlmostEqual(129, result)
 
     def test_shape_rb_data(self):
+        """Test randomized benchmark data shaping function."""
         raw_data = np.zeros((2, 2, 2))
         # TODO(mtreinish): Come up with a more realistic input data set instead
         # of this synthetic example
@@ -68,6 +73,7 @@ class TestQCVVFitters(QiskitTestCase):
             self.assertEqual(expec.all(), result[i].all())
 
     def test_rb_epc(self):
+        """Test error per clifford for randomized benchmark fit data."""
         rb_pattern = [[1], [0, 2]]
         fit = {'q2': {'fit': [2, 0.5, 0.8], 'fiterr': [0, 0, 0]},
                'q1': {'fit': [1, 0.2, 0.9], 'fiterr': [0, 0, 0]},
