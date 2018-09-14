ae441e2869481011ad860abeb5841505d61da040
Luciano Bello <luciano.bello@ibm.com>
2018-09-14 13:03:21 -0400

    qiskit/qasm/_node/_comment.py

---
+++ qiskit/qasm/_node/_comment.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2018, IBM.
+#
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree.
+
+"""
+Node for an OPENQASM comment.
+"""
+import re
+
+from ._node import Node
+
+
+class Comment(Node):
+    """Node for an OPENQASM comment.
+    """
+
+    def __init__(self, value):
+        """Create the version node."""
+        Node.__init__(self, "comment", None, None)
+        self.comment_text = re.match(r'//(.*)$', value)[1]
+
+    def qasm(self, prec=None):
+        """Return the corresponding comment string."""
+        # pylint: disable=unused-argument
+        return "//%s" % (self.comment_text)
