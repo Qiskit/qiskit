766dd42c7c89d055b129f1ffae6994e589d7d481
Ali Javadi <ajavadia@princeton.edu>
2018-10-05 14:22:07 -0400

    make test_create more clear

--- test/python/test_dagcircuit.py
+++ test/python/test_dagcircuit.py
@@ -16,29 +16,29 @@ from .common import QiskitTestCase
 
 
 class TestDagCircuit(QiskitTestCase):
-    """QasmParser"""
+    """Testing the dag circuit representation"""
 
     def test_create(self):
         qubit0 = ('qr', 0)
         qubit1 = ('qr', 1)
         clbit0 = ('cr', 0)
         clbit1 = ('cr', 1)
-        condition = None
+        condition = ('cr', 3)
         dag = DAGCircuit()
         dag.add_basis_element('h', 1, number_classical=0, number_parameters=0)
         dag.add_basis_element('cx', 2)
         dag.add_basis_element('x', 1)
-        dag.add_basis_element('measure', 1, number_classical=1,
-                              number_parameters=0)
+        dag.add_basis_element('measure', 1, number_classical=1, number_parameters=0)
         dag.add_qreg('qr', 2)
         dag.add_creg('cr', 2)
-        dag.apply_operation_back('h', [qubit0], [], [], condition)
-        dag.apply_operation_back('cx', [qubit0, qubit1], [],
-                                 [], condition)
-        dag.apply_operation_back('measure', [qubit1], [clbit1], [], condition)
-        dag.apply_operation_back('x', [qubit1], [], [], ('cr', 1))
-        dag.apply_operation_back('measure', [qubit0], [clbit0], [], condition)
-        dag.apply_operation_back('measure', [qubit1], [clbit1], [], condition)
+        dag.apply_operation_back('h', [qubit0], [], [], condition=None)
+        dag.apply_operation_back('cx', [qubit0, qubit1], [], [], condition=None)
+        dag.apply_operation_back('measure', [qubit1], [clbit1], [], condition=None)
+        dag.apply_operation_back('x', [qubit1], [], [], condition=condition)
+        dag.apply_operation_back('measure', [qubit0], [clbit0], [], condition=None)
+        dag.apply_operation_back('measure', [qubit1], [clbit1], [], condition=None)
+        self.assertEqual(len(dag.multi_graph.nodes), 14)
+        self.assertEqual(len(dag.multi_graph.edges), 16)
 
     def test_get_named_nodes(self):
         dag = DAGCircuit()
