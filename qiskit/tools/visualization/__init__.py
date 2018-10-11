8d235d2edea09d5f19058d2eeeb919314e960286
Luciano <luciano@debian.org>
2018-10-11 09:50:48 -0400

    Text drawer (#1054)
    `circuit_drawer` is extended with output `text`, which draws the circuit in the console or a file using ASCII art (using the extended ASCII charset)

--- qiskit/tools/visualization/__init__.py
+++ qiskit/tools/visualization/__init__.py
@@ -10,7 +10,7 @@
 import sys
 from qiskit._util import _has_connection
 from ._circuit_visualization import circuit_drawer, plot_circuit, generate_latex_source,\
-    latex_circuit_drawer, matplotlib_circuit_drawer, qx_color_scheme
+    latex_circuit_drawer, matplotlib_circuit_drawer, _text_circuit_drawer, qx_color_scheme
 from ._error import VisualizationError
 from ._state_visualization import plot_bloch_vector
 from ._dag_visualization import dag_drawer
