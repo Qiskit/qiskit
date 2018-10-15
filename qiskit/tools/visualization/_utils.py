d46b84f832573ec9bce7b02031ef93658ac96bc1
Matthew Treinish <mtreinish@kortar.org>
2018-10-15 14:36:10 -0400

    Split out matplotlib and qcstyle as separate modules
    This commit starts the decomposition of the _circuit_visualization
    module into having separate modules per output backend. The
    _circuit_visualization module has is quite bulky containing the
    implementation of 2 different visualization backends. This starts by
    splitting out matplotlib and QCStyle. The latex backend will be done
    soon, but it currently in flux with multiple open pull requests. Once
    development calms on that backend it too will be spit into a separate
    module.
    
    Starts #1104

---
+++ qiskit/tools/visualization/_utils.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2018, IBM.
+#
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree
+
+"""Common visualization utilties."""
+
+import PIL
+
+
+def _trim(image):
+    """Trim a PIL image and remove white space."""
+    background = PIL.Image.new(image.mode, image.size, image.getpixel((0, 0)))
+    diff = PIL.ImageChops.difference(image, background)
+    diff = PIL.ImageChops.add(diff, diff, 2.0, -100)
+    bbox = diff.getbbox()
+    if bbox:
+        image = image.crop(bbox)
+    return image
