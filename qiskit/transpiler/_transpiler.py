fa5f9965c2203631f04c8f8b8680bd27a6eabe5c
Matthew Treinish <mtreinish@kortar.org>
2018-10-29 07:12:22 -0400

    Avoid circuilar imports on qiskit.unroll
    The __init__.py module for qiskit.unroll becomes a ball of yarn filled
    with ciruclar imports when trying to leverage unrollers from inside
    call paths from inside fundamental circuit elements. (this was
    encountered trying to add a draw() function to the QuantumCircuit
    class). Because we're importing individual classes in the __init__.py
    from unroll submodules it can force a circular import if any of those
    submodules are using a module in the call path. To avoid this but still
    maintain backwards compatibility for any users importing from
    qiskit.unroll we move all the qiskit.unroll submodules into a new subdir
    qiskit.unrollers which doesn't have anything in __init__.py which will
    address any potential circular imports when those are used directly.
    But, the classes exposed off of qiskit.unroll remain unchanged
    preserving backwards compatibility.

--- qiskit/transpiler/_transpiler.py
+++ qiskit/transpiler/_transpiler.py
@@ -13,9 +13,10 @@ import scipy.sparse.csgraph as cs
 
 from qiskit.transpiler._transpilererror import TranspilerError
 from qiskit._qiskiterror import QISKitError
-from qiskit import QuantumCircuit
 from qiskit.dagcircuit import DAGCircuit
-from qiskit.unroll import DagUnroller, DAGBackend, JsonBackend
+from qiskit.unrollers import _dagunroller
+from qiskit.unrollers import _dagbackend
+from qiskit.unrollers import _jsonbackend
 from qiskit.mapper import (Coupling, optimize_1q_gates, coupling_list2dict, swap_mapper,
                            cx_cancellation, direction_mapper,
                            remove_last_measurements, return_last_measurements)
@@ -44,7 +45,7 @@ def transpile(circuits, backend, basis_gates=None, coupling_map=None, initial_la
     Raises:
         TranspilerError: in case of bad compile options, e.g. the hpc options.
     """
-    if isinstance(circuits, QuantumCircuit):
+    if not isinstance(circuits, list):
         circuits = [circuits]
 
     # FIXME: THIS NEEDS TO BE CLEANED UP -- some things to decide for list of circuits:
@@ -227,7 +228,8 @@ def transpile_dag(dag, basis_gates='u1,u2,u3,cx,id', coupling_map=None,
         # default set of passes
         # TODO: move each step here to a pass, and use a default passmanager below
         basis = basis_gates.split(',') if basis_gates else []
-        dag_unroller = DagUnroller(dag, DAGBackend(basis))
+        dag_unroller = _dagunroller.DagUnroller(
+            dag, _dagbackend.DAGBackend(basis))
         dag = dag_unroller.expand_gates()
         # if a coupling map is given compile to the map
         if coupling_map:
@@ -242,7 +244,8 @@ def transpile_dag(dag, basis_gates='u1,u2,u3,cx,id', coupling_map=None,
                 dag, coupling, initial_layout, trials=20, seed=seed_mapper)
             logger.info("final layout: %s", final_layout)
             # Expand swaps
-            dag_unroller = DagUnroller(dag, DAGBackend(basis))
+            dag_unroller = _dagunroller.DagUnroller(
+                dag, _dagbackend.DAGBackend(basis))
             dag = dag_unroller.expand_gates()
             # Change cx directions
             dag = direction_mapper(dag, coupling)
@@ -261,7 +264,8 @@ def transpile_dag(dag, basis_gates='u1,u2,u3,cx,id', coupling_map=None,
         compiled_circuit = dag
     elif format == 'json':
         # FIXME: JsonBackend is wrongly taking an ordered dict as basis, not list
-        dag_unroller = DagUnroller(dag, JsonBackend(dag.basis))
+        dag_unroller = _dagunroller.DagUnroller(
+            dag, _jsonbackend.JsonBackend(dag.basis))
         compiled_circuit = dag_unroller.execute()
     elif format == 'qasm':
         compiled_circuit = dag.qasm()
