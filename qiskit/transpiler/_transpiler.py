0eff32a8ce51f8ecfbfd853769ba449f56b75c44
Matthew Treinish <mtreinish@kortar.org>
2018-10-30 06:17:13 -0400

    Revert QuantumCircuit import removal
    As part of #911 we need to avoid circular _quantum_circuit imports.
    However this PR is a prerequisite for #911 and shouldn't implement
    pieces of that. This commit removes a fix which is part of #911 but not
    actual part of refactoring the unrollers.

--- qiskit/transpiler/_transpiler.py
+++ qiskit/transpiler/_transpiler.py
@@ -13,6 +13,7 @@ import scipy.sparse.csgraph as cs
 
 from qiskit.transpiler._transpilererror import TranspilerError
 from qiskit._qiskiterror import QISKitError
+from qiskit import QuantumCircuit
 from qiskit.dagcircuit import DAGCircuit
 from qiskit.unrollers import _dagunroller
 from qiskit.unrollers import _dagbackend
@@ -45,7 +46,7 @@ def transpile(circuits, backend, basis_gates=None, coupling_map=None, initial_la
     Raises:
         TranspilerError: in case of bad compile options, e.g. the hpc options.
     """
-    if not isinstance(circuits, list):
+    if isinstance(circuits, QuantumCircuit):
         circuits = [circuits]
 
     # FIXME: THIS NEEDS TO BE CLEANED UP -- some things to decide for list of circuits:
