a214451ee00bb010c4806b0abfb6477b8faf77e5
Richard Chen <chenrich@us.ibm.com>
2018-10-30 12:09:57 -0400

    add more methods

--- qiskit/tools/qi/pauli.py
+++ qiskit/tools/qi/pauli.py
@@ -48,15 +48,19 @@ class Pauli:
             v = np.asarray(v).astype(np.bool)
             w = np.asarray(w).astype(np.bool)
 
+        if v.dtype != np.bool or w.dtype != np.bool:
+            v = v.astype(np.bool)
+            w = w.astype(np.bool)
+
         self._v = v
         self._w = w
 
     @classmethod
-    def from_pauli_string(cls, pauli_string):
+    def from_label(cls, labels):
         """Take pauli string to construct pauli."""
-        v = np.zeros(len(pauli_string), dtype=np.bool)
-        w = np.zeros(len(pauli_string), dtype=np.bool)
-        for i, label in enumerate(pauli_string):
+        v = np.zeros(len(labels), dtype=np.bool)
+        w = np.zeros(len(labels), dtype=np.bool)
+        for i, label in enumerate(labels):
             if label == 'X':
                 w[i] = True
             elif label == 'Z':
@@ -69,15 +73,38 @@ class Pauli:
                                   "'Y' or 'Z' but you have {}.".format(label))
         return cls(v, w)
 
+    def __len__(self):
+        """Return number of qubits."""
+        return len(self._v)
+
+    def __repr__(self):
+        """Return the representation of self."""
+        v = [x for x in self._v.astype(np.int)]
+        w = [x for x in self._w.astype(np.int)]
+
+        ret = "Pauli(v={}, w={})".format(v, w)
+        return ret
+
     def __str__(self):
         """Output the Pauli label."""
-        return self.to_label()
+        label = ''
+        for v, w in zip(self._v, self._w):
+            if not v and not w:
+                label += 'I'
+            elif not v and w:
+                label += 'X'
+            elif v and not w:
+                label += 'Z'
+            else:
+                label += 'Y'
+
+        return label
 
     def __eq__(self, other):
         """Return True if all Pauli terms are equal."""
         res = False
-        if self.numberofqubits == other.numberofqubits:
-            if np.all(self.v == other.v) and np.all(self.w == other.w):
+        if len(self) == len(other):
+            if np.all(self._v == other.v) and np.all(self._w == other.w):
                 res = True
         return res
 
@@ -87,12 +114,22 @@ class Pauli:
 
     def __mul__(self, other):
         """Multiply two Paulis."""
-        if self.numberofqubits != other.numberofqubits:
+        if len(self) != len(other):
             raise QISKitError('These Paulis cannot be multiplied - different number '
                               'of qubits')
-        v_new = np.logical_xor(self._v, other._v)
-        w_new = np.logical_xor(self._w, other._w)
+        v_new = np.logical_xor(self._v, other.v)
+        w_new = np.logical_xor(self._w, other.w)
         return Pauli(v_new, w_new)
+
+    @property
+    def v(self):
+        """Getter of v."""
+        return self._v
+
+    @property
+    def w(self):
+        """Getter of w."""
+        return self._w
 
     @staticmethod
     def sgn_prod(p1, p2):
@@ -129,26 +166,16 @@ class Pauli:
     @property
     def numberofqubits(self):
         """Number of qubits."""
-        return len(self._v)
+        return len(self)
 
     def to_label(self):
+        # TODO: the order of qubits.
         """Print out the labels in X, Y, Z format.
 
         Returns:
             str: pauli label
         """
-        label = ''
-        for v, w in zip(self._v, self._w):
-            if not v and not w:
-                label += 'I'
-            elif not v and w:
-                label += 'X'
-            elif v and not w:
-                label += 'Z'
-            else:
-                label += 'Y'
-
-        return label
+        return str(self)
 
     def to_matrix(self):
         """
@@ -163,6 +190,7 @@ class Pauli:
         return mat.toarray()
 
     def to_spmatrix(self):
+        # TODO: the order of qubits.
         """
         Convert Pauli to a sparse matrix representation (CSR format).
 
@@ -187,6 +215,7 @@ class Pauli:
         return mat.tocsr()
 
     def update_v(self, v, pos=None):
+        # TODO: the order of qubits.
         """
         Update partial of entire v.
 
@@ -202,7 +231,10 @@ class Pauli:
             for p, idx in enumerate(pos):
                 self._v[idx] = v[p]
 
+        return self
+
     def update_w(self, w, pos=None):
+        # TODO: the order of qubits.
         """
         Update partial of entire w.
 
@@ -218,7 +250,10 @@ class Pauli:
             for p, idx in enumerate(pos):
                 self._w[idx] = w[p]
 
+        return self
+
     def insert_qubits(self, pos, pauli_labels):
+        # TODO: the order of qubits.
         """
         Insert pauli to the targeted positions.
 
@@ -242,10 +277,13 @@ class Pauli:
 
         new_v = self._v
         new_w = self._w
-        self._v = np.insert(new_v, pos, tmp._v)
-        self._w = np.insert(new_w, pos, tmp._w)
+        self._v = np.insert(new_v, pos, tmp.v)
+        self._w = np.insert(new_w, pos, tmp.w)
+
+        return self
 
     def append_qubits(self, pauli_labels):
+        # TODO: the order of qubits.
         """
         Append pauli to the end.
 
@@ -257,10 +295,13 @@ class Pauli:
 
         tmp = Pauli.from_pauli_string(pauli_labels)
 
-        self._v = np.concatenate((self._v, tmp._v))
-        self._w = np.concatenate((self._w, tmp._w))
+        self._v = np.concatenate((self._v, tmp.v))
+        self._w = np.concatenate((self._w, tmp.w))
+
+        return self
 
     def delete_qubits(self, pos):
+        # TODO: the order of qubits.
         """
         Delete pauli at the position.
 
@@ -273,6 +314,8 @@ class Pauli:
         self._v = np.delete(self._v, pos)
         self._w = np.delete(self._w, pos)
 
+        return self
+
     @staticmethod
     def generate_random_pauli(num_qubits):
         """Return a random Pauli on numberofqubits.
@@ -283,12 +326,13 @@ class Pauli:
         Returns:
             Pauli: the random pauli
         """
-        v = np.random.randint(2, size=num_qubits).astype(bool)
-        w = np.random.randint(2, size=num_qubits).astype(bool)
+        v = np.random.randint(2, size=num_qubits).astype(np.bool)
+        w = np.random.randint(2, size=num_qubits).astype(np.bool)
         return Pauli(v, w)
 
     @staticmethod
     def generate_single_qubit_pauli(pos, pauli_label, num_qubits):
+        # TODO: the order of qubits.
         """
         Generate single qubit pauli at pos with pauli_label with length num_qubits.
 
@@ -306,6 +350,15 @@ class Pauli:
         w[pos] = tmp._w[0]
 
         return Pauli(v, w)
+
+    def kron(self, pauli):
+        # TODO: the order of qubits.
+        """kron product of two paulis"""
+
+        self._v = np.concatenate((self._v, pauli.v))
+        self._w = np.concatenate((self._w, pauli.w))
+
+        return self
 
 
 def pauli_group(number_of_qubits, case='weight'):
@@ -379,7 +432,12 @@ def pauli_singles(j_index, number_qubits):
 
 
 if __name__ == '__main__':
-    p = Pauli.from_pauli_string('IIII')
+    p = Pauli.from_label('IIII')
     # p.insert_qubits([0, 2], ['Z', 'X'])
-    p.append_qubits(list('ZX'))
+    # p.append_qubits(list('ZX'))
     print(p)
+    print(repr(p))
+    a = np.zeros(4)
+    b = np.ones(4)
+    c = Pauli(a, b)
+    print(c._v.dtype)
