da0c347e4d769edf02a08e83e3d4f8a0734e1b82
Luciano Bello <luciano.bello@ibm.com>
2018-11-01 11:30:50 -0400

    proto _get_instructions

--- qiskit/tools/visualization/_utils.py
+++ qiskit/tools/visualization/_utils.py
@@ -19,3 +19,22 @@ def _trim(image):
     if bbox:
         image = image.crop(bbox)
     return image
+
+def _get_instructions(dag, reversebits=False):
+    """
+    Given a dag, return a tuple (qregs, cregs, ops) where
+    qregs and cregs are the name of the quantum and classical
+    registers in order (based on reversebits) and ops is a list
+    of DAG nodes which type is "operation".
+    Args:
+        dag (DAGCircuit): From where the information is extracted.
+        reversebits (bool): If true the order of the bits in the registers is reversed.
+    Returns:
+        Tuple(list,list,list): To be consumed by the visualizer directly.
+    """
+    ops = []
+    for node_no in dag.node_nums_in_topological_order:
+        node = dag.multi_graph.node[node_no]
+        if node['type'] == 'op':
+            ops.append(node)
+    return dag.qregs, dag.cregs, ops
