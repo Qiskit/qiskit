b0a3abe508a5d1875977682ee021ddc769f08096
Jay Gambetta <jay.gambetta@us.ibm.com>
2018-11-03 10:31:34 -0400

    Update __init__.py

--- qiskit/quantum_info/states/__init__.py
+++ qiskit/quantum_info/states/__init__.py
@@ -5,6 +5,6 @@
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
 
-"""Module for States Circuits."""
+"""Module for States."""
 from ._states import basis_state, random_state, projector
 from ._measures import state_fidelity
