3a64a4abff813fe038d0002a748ab3955e6b0c5c
Juan Gomez <atilag@gmail.com>
2018-11-07 22:38:18 +0100

    Publisher/Subscriber (with Broker) implementation (#1117)
    * Added pub/sub using message broker and some tests
    
    * * Decouple ProgressBar from the transpiler by using events (Pub/Sub)
    
    * * Remove receiver/channel code in favor of the Pub/Sub.
    * ProgressBar has been totally decoupled
    
    * * Better in-code documentation
    * Fixing linter
    
    * * Reordering files location to remove any trace of progressbar
      from the transpiler component.
    
    * * Moving all progressbars to a single file
    
    * * Exporting progressbars through the Jupyter module
    
    * * Remove old progressbar file remains
    
    * * parallel_map() function emmits the correct set of events
    * transpiler_dag() function subsribe to parallel_map() events to
      know when a dag has been sucessfully transpiled, and emmits the
      corresponding event
    * Added a clear() method to the Subscriber class so we can reset
      every subscriber/event from the Broker.
    * Added tests for unsubscribing
    * Better in-code doc
    
    * * Happy linter
    
    * * test_parallel_progbar_used() removed
    
    * * Jupyter tools importing don't crash if you are importing them
      in a non-jupyter context
    * Removed TextProgressBar test from the Parallel tests
    
    * * Lintering

--- qiskit/wrapper/jupyter/jupyter_magics.py
+++ qiskit/wrapper/jupyter/jupyter_magics.py
@@ -15,9 +15,7 @@ from IPython.core import magic_arguments                         # pylint: disab
 from IPython.core.magic import cell_magic, Magics, magics_class  # pylint: disable=import-error
 import ipywidgets as widgets                                     # pylint: disable=import-error
 import qiskit
-from qiskit.transpiler._receiver import receiver as rec
-from qiskit.transpiler._progressbar import TextProgressBar
-from qiskit.wrapper.jupyter.progressbar import HTMLProgressBar
+from .progressbar import TextProgressBar, HTMLProgressBar
 
 
 @magics_class
@@ -143,12 +141,10 @@ class ProgressBarMagic(Magics):
         """
         args = magic_arguments.parse_argstring(self.qiskit_progress_bar, line)
         if args.type == 'html':
-            progress_bar = HTMLProgressBar()
+            HTMLProgressBar()
         elif args.type == 'text':
-            progress_bar = TextProgressBar()
+            TextProgressBar()
         else:
             raise qiskit.QISKitError('Invalid progress bar type.')
+
         self.shell.ex(cell)
-        # Remove progress bar from receiver if not used in cell
-        if progress_bar.channel_id in rec.channels.keys():
-            rec.remove_channel(progress_bar.channel_id)
