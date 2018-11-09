5379d2dec06ffeed698ddd9517bb334783570e31
Charles Taylor <charlesastaylor@gmail.com>
2018-11-09 10:24:25 +0000

    Add fallback for when CPU count can't be determined (#1214) (#1214)
    *Fallback to 1 when cpu_count returns None
    
    *Create test_util.py and add test
    
    Fixes #1212

---
+++ test/python/test_util.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2018, IBM.
+#
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree.
+
+"""Tests for qiskit/_util.py"""
+
+from unittest import mock
+
+from qiskit import _util
+from .common import QiskitTestCase
+
+
+class TestUtil(QiskitTestCase):
+    """Tests for qiskit/_util.py"""
+
+    @mock.patch('platform.system', return_value='Linux')
+    @mock.patch('psutil.virtual_memory')
+    @mock.patch('psutil.cpu_count', return_value=None)
+    def test_local_hardware_none_cpu_count(self, cpu_count_mock, vmem_mock,
+                                           platform_mock):
+        """Test cpu count fallback to 1 when true value can't be determined"""
+        # pylint: disable=unused-argument
+        result = _util.local_hardware_info()
+        self.assertEqual(1, result['cpus'])
