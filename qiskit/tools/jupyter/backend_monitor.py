c2a85fdc4286ef6dc3571e81c6eee0f8a772824c
Paul Nation <nonhermitian@gmail.com>
2018-11-12 09:21:43 -0500

    fix module docs

--- qiskit/tools/jupyter/backend_monitor.py
+++ qiskit/tools/jupyter/backend_monitor.py
@@ -5,7 +5,7 @@
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
 
-"""A module of magic functions"""
+"""A module for monitoring backends."""
 
 import time
 import threading
