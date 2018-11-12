ee4e0d39cec36c98eb2b7c5d95967646943a4685
Borja Godoy Gago <borja.godoy@barrabesmeaning.com>
2018-11-12 17:58:59 +0100

    replace logo for terra logo

--- doc/conf.py
+++ doc/conf.py
@@ -178,7 +178,7 @@ html_static_path = ['./theme/static/']
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = 'theme/static/qiskit-logo-white-no-margin.png'
+html_logo = 'theme/static/qiskit-terra-logo.png'
 
 html_favicon = 'theme/static/favicon.ico'
 
