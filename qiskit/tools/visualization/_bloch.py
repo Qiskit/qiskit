d0295d3dd673d5a1cbfe2dbb5905a9c53a15024c
Matthew Treinish <mtreinish@kortar.org>
2018-11-14 09:42:12 -0500

    Revert "Turn off pylint import goruping check"
    This reverts commit 8ab663099001170b7486825af727a1678d65b5f4.

--- qiskit/tools/visualization/_bloch.py
+++ qiskit/tools/visualization/_bloch.py
@@ -39,7 +39,6 @@
 #    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ###############################################################################
 
-# pylint: disable=ungrouped-imports
 
 """Bloch sphere"""
 
