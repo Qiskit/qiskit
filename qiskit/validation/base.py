606ca560604d53697b5dac9979b3d3046237d406
Diego M. Rodriguez <diego.plan9@gmail.com>
2018-11-14 15:50:27 +0100

    Allow "many" during post_load, add base Obj
    Fix the post_load hook for adding additional fields to a BaseModel in
    order to account for `many` fields.
    Add `Obj` and `ObjSchema` for replacing the usage of fields.Dict,
    allowing extra control over their properties.

--- qiskit/validation/base.py
+++ qiskit/validation/base.py
@@ -69,8 +69,8 @@ class BaseSchema(Schema):
             valid_data[key] = getattr(original_data, key)
         return valid_data
 
-    @post_load(pass_original=True)
-    def load_additional_data(self, valid_data, original_data):
+    @post_load(pass_original=True, pass_many=True)
+    def load_additional_data(self, valid_data, many, original_data):
         """Include unknown fields after load.
 
         Unknown fields are added with no processing at all.
@@ -84,9 +84,16 @@ class BaseSchema(Schema):
 
         Inspired by https://github.com/marshmallow-code/marshmallow/pull/595.
         """
-        additional_keys = set(original_data) - set(valid_data)
-        for key in additional_keys:
-            valid_data[key] = original_data[key]
+        if many:
+            for i, _ in enumerate(valid_data):
+                additional_keys = set(original_data[i]) - set(valid_data[i])
+                for key in additional_keys:
+                    valid_data[i][key] = original_data[i][key]
+        else:
+            additional_keys = set(original_data) - set(valid_data)
+            for key in additional_keys:
+                valid_data[key] = original_data[key]
+
         return valid_data
 
     @post_load
@@ -290,4 +297,15 @@ def bind_schema(schema):
 
 class BaseModel(SimpleNamespace):
     """Base class for Models for validated Qiskit classes."""
+    pass
+
+
+class ObjSchema(BaseSchema):
+    """Generic object schema."""
+    pass
+
+
+@bind_schema(ObjSchema)
+class Obj(BaseModel):
+    """Generic object in a Model."""
     pass
