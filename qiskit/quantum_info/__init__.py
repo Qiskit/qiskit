39901cc082169ba49a8fd4d6257b22ed2f9040cd
Jay Gambetta <jay.gambetta@us.ibm.com>
2018-11-14 20:09:18 -0500

    linting

--- qiskit/quantum_info/__init__.py
+++ qiskit/quantum_info/__init__.py
@@ -5,6 +5,8 @@
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
 
+"""Quantum Information  methods."""
+
 from .operators.pauli import Pauli, pauli_group
 from .states._states import basis_state, random_state, projector
 from .states._measures import state_fidelity
