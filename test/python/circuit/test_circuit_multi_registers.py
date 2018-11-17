8a474d30c6edc4bc5fbd1e27f201999c3cb89bcc
Jay Gambetta <jay.gambetta@us.ibm.com>
2018-11-17 14:58:41 -0500

    Linting

--- test/python/circuit/test_circuit_multi_registers.py
+++ test/python/circuit/test_circuit_multi_registers.py
@@ -5,7 +5,7 @@
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
 
-# pylint: disable=unused-import
+# pylint: disable=redefined-builtin
 
 """Test Qiskit's QuantumCircuit class for multiple registers."""
 
