3f7f00fa5cdb7d2687b36b3ce1c6fca5a0bad6cd
Luciano Bello <luciano.bello@ibm.com>
2018-11-19 11:38:15 -0500

    create_dag as staticmethod

--- test/python/transpiler/test_swap_mapper.py
+++ test/python/transpiler/test_swap_mapper.py
@@ -16,36 +16,36 @@ from qiskit import QuantumRegister
 from ..common import QiskitTestCase
 
 
-def _create_dag(instructions):
-    dag = DAGCircuit()
-    qregs = {}
-    basic_elements = {}
-
-    for instruction in instructions:
-        basic_elements[instruction[0]] = len(instruction[1])
-
-        for arg in instruction[1]:
-            if qregs.get(arg[0]) is None:
-                qregs[arg[0]] = []
-            qregs[arg[0]].append(arg[1])
-
-    # dag.add_qreg
-    for name, regs in qregs.items():
-        dag.add_qreg(QuantumRegister(max(regs) + 1, name))
-
-    # dag.add_basis_element
-    for name, amount in basic_elements.items():
-        dag.add_basis_element(name, amount)
-
-    # apply_operation_back
-    for instruction, args in instructions:
-        dag.apply_operation_back(instruction, args)
-
-    return dag
-
-
 class TestSwapMapper(QiskitTestCase):
     """ Tests the SwapMapper pass."""
+
+    @staticmethod
+    def create_dag(instructions):
+        dag = DAGCircuit()
+        qregs = {}
+        basic_elements = {}
+
+        for instruction in instructions:
+            basic_elements[instruction[0]] = len(instruction[1])
+
+            for arg in instruction[1]:
+                if qregs.get(arg[0]) is None:
+                    qregs[arg[0]] = []
+                qregs[arg[0]].append(arg[1])
+
+        # dag.add_qreg
+        for name, regs in qregs.items():
+            dag.add_qreg(QuantumRegister(max(regs) + 1, name))
+
+        # dag.add_basis_element
+        for name, amount in basic_elements.items():
+            dag.add_basis_element(name, amount)
+
+        # apply_operation_back
+        for instruction, args in instructions:
+            dag.apply_operation_back(instruction, args)
+
+        return dag
 
     def test_trivial_case(self):
         """No need to have any swap, the CX are distance 1 to each other
@@ -58,9 +58,9 @@ class TestSwapMapper(QiskitTestCase):
          Coupling map: [1]--[0]--[2]
         """
         coupling = Coupling({0: [1, 2]})
-        dag = _create_dag([('CX', [('q', 0), ('q', 1)]),
-                           ('U', [('q', 0)]),
-                           ('CX', [('q', 0), ('q', 2)])])
+        dag = TestSwapMapper.create_dag([('CX', [('q', 0), ('q', 1)]),
+                                         ('U', [('q', 0)]),
+                                         ('CX', [('q', 0), ('q', 2)])])
 
         before = dag.qasm()
         pass_ = SwapMapper(coupling)
@@ -81,10 +81,10 @@ class TestSwapMapper(QiskitTestCase):
          Coupling map: [0]--[1]--[2]--[3]
         """
         coupling = Coupling({0: [1], 1: [2], 2: [3]})
-        dag = _create_dag([('CX', [('q', 2), ('q', 3)]),
-                           ('CX', [('q', 0), ('q', 1)])])
-
+        dag = TestSwapMapper.create_dag([('CX', [('q', 2), ('q', 3)]),
+                                         ('CX', [('q', 0), ('q', 1)])])
         before = dag.qasm()
+
         pass_ = SwapMapper(coupling)
         after_dag = pass_.run(dag)
 
@@ -101,8 +101,8 @@ class TestSwapMapper(QiskitTestCase):
          Coupling map: [1]--[0]--[2]
         """
         coupling = Coupling({0: [1, 2]})
-        dag = _create_dag([('CX', [('q', 0), ('q', 1)]),
-                           ('CX', [('q', 1), ('q', 2)])])
+        dag = TestSwapMapper.create_dag([('CX', [('q', 0), ('q', 1)]),
+                                         ('CX', [('q', 1), ('q', 2)])])
         expected = '\n'.join(["OPENQASM 2.0;",
                               "qreg q[3];",
                               "opaque swap a,b;",
