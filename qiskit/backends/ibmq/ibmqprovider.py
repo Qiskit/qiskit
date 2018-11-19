5a48644396d45b42a7d515519087915b0c6e600c
Ali Javadi-Abhari <ajavadia@users.noreply.github.com>
2018-11-19 07:57:00 -0500

    overwrite argument for IBMQ.save_account (#1295)
    * overwrite argument for IBMQ.save_account
    
    * fix test
    
    * fix documentation. do NOT overwrite when overwrite=False

--- qiskit/backends/ibmq/ibmqprovider.py
+++ qiskit/backends/ibmq/ibmqprovider.py
@@ -116,7 +116,7 @@ class IBMQProvider(BaseProvider):
 
         self._append_account(credentials)
 
-    def save_account(self, token, url=QE_URL, **kwargs):
+    def save_account(self, token, url=QE_URL, overwrite=False, **kwargs):
         """Save the account to disk for future use.
 
         Login into Quantum Experience or IBMQ using the provided credentials,
@@ -127,20 +127,13 @@ class IBMQProvider(BaseProvider):
             token (str): Quantum Experience or IBM Q API token.
             url (str): URL for Quantum Experience or IBM Q (for IBM Q,
                 including the hub, group and project in the URL).
+            overwrite (bool): overwrite existing credentials.
             **kwargs (dict):
                 * proxies (dict): Proxy configuration for the API.
                 * verify (bool): If False, ignores SSL certificates errors
         """
         credentials = Credentials(token, url, **kwargs)
-
-        # Check if duplicated credentials are already stored. By convention,
-        # we assume (hub, group, project) is always unique.
-        stored_credentials = read_credentials_from_qiskitrc()
-
-        if credentials.unique_id() in stored_credentials.keys():
-            warnings.warn('Credentials are already stored.')
-        else:
-            store_credentials(credentials)
+        store_credentials(credentials, overwrite=overwrite)
 
     def active_accounts(self):
         """List all accounts currently in the session.
