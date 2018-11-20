8256624651fe4c28873a0b14d15b31d6d8ef629e
Luciano Bello <luciano.bello@ibm.com>
2018-11-20 18:10:03 -0500

    docstrings in test

--- test/python/test_layout.py
+++ test/python/test_layout.py
@@ -5,6 +5,8 @@
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
 
+""" Tests the layout object"""
+
 import unittest
 
 from qiskit.mapper import Layout
@@ -12,8 +14,10 @@ from .common import QiskitTestCase
 
 
 class LayoutTest(QiskitTestCase):
+    """Test the methods in the layout object."""
 
     def test_layout_from_dict(self):
+        """Constructor from a dict"""
         layout = Layout({('qr', 0): 0,
                          ('qr', 1): 1,
                          ('qr', 2): 2})
@@ -26,6 +30,7 @@ class LayoutTest(QiskitTestCase):
         self.assertEqual(layout[2], ('qr', 2))
 
     def test_layout_from_list(self):
+        """Constructor from a list"""
         layout = Layout([('q0', 0), ('q1', 0), None, ('q1', 1), ('q0', 1)])
 
         self.assertEqual(layout[('q0', 0)], 0)
@@ -41,32 +46,37 @@ class LayoutTest(QiskitTestCase):
         self.assertEqual(layout[4], ('q0', 1))
 
     def test_layout_set(self):
+        """Setter"""
         layout = Layout()
         layout[('qr', 0)] = 0
         self.assertEqual(layout[('qr', 0)], 0)
         self.assertEqual(layout[0], ('qr', 0))
 
     def test_layout_len(self):
+        """Length of the layout is the amount of wires"""
         layout = Layout()
         self.assertEqual(len(layout), 0)
         layout.add(('qr', 0))
         self.assertEqual(len(layout), 1)
-        layout.add(('qr', 1),3)
+        layout.add(('qr', 1), 3)
         self.assertEqual(len(layout), 4)
 
     def test_layout_set_len(self):
+        """Length setter"""
         layout = Layout()
         layout.add(('qr', 1), 3)
         layout.length(4)
         self.assertEqual(len(layout), 4)
 
     def test_layout_idle_wires(self):
+        """Get wires that are not mapped"""
         layout = Layout()
         layout.add(('qr', 1), 2)
         layout.length(4)
         self.assertEqual(layout.idle_wires(), [0, 1, 3])
 
     def test_layout_get_logical(self):
+        """Get the map from the logical (qu)bits view"""
         layout_dict = {('qr', 0): 0,
                        ('qr', 1): 1,
                        ('qr', 2): 2}
@@ -74,10 +84,12 @@ class LayoutTest(QiskitTestCase):
         self.assertDictEqual(layout_dict, layout.get_logical())
 
     def test_layout_get_physical(self):
+        """Get the map from the physical wires view"""
         layout = Layout({('qr', 0): 0, ('qr', 1): 1, ('qr', 2): 2})
         self.assertDictEqual(layout.get_physical(), {0: ('qr', 0), 1: ('qr', 1), 2: ('qr', 2)})
 
-    def test_layout_add_logical(self):
+    def test_layout_add(self):
+        """add() method"""
         layout = Layout()
         layout[('qr,0')] = 0
         layout.add(('qr', 1))
@@ -86,6 +98,7 @@ class LayoutTest(QiskitTestCase):
         self.assertEqual(layout[1], ('qr', 1))
 
     def test_layout_swap(self):
+        """swap() method"""
         layout = Layout()
         layout.add(('qr', 0))
         layout.add(('qr', 1))
