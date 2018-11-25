d0d87087bab186ff48ed548127fe664572ca9ab1
lukasz <lukasz>
2018-11-25 08:32:19 +0100

    Move assignment of QuantumCircuit.measure to its class. Better object design, code readability and  IDE support.

--- qiskit/_measure.py
+++ qiskit/_measure.py
@@ -10,7 +10,6 @@ Quantum measurement in the computational basis.
 """
 from ._instruction import Instruction
 from ._instructionset import InstructionSet
-from ._quantumcircuit import QuantumCircuit
 from ._quantumregister import QuantumRegister
 from ._classicalregister import ClassicalRegister
 
@@ -59,4 +58,3 @@ def measure(self, qubit, cbit):
     return self._attach(Measure(qubit, cbit, self))
 
 
-QuantumCircuit.measure = measure
