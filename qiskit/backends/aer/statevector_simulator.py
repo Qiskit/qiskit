a97153dc71fe58387a3d7c33c013641d463ad0a7
Diego M. Rodriguez <diego.plan9@gmail.com>
2018-11-27 20:41:59 +0100

    Update backends for new-style results
    Update the backends for the new-style results:
    * revise usage of `result_from_old_style_dict`, dropping the second
      parameter.
    * adjust statevector handling of snapshots.
    * adjust unitary creation of the vector, converting complex numbers to
      pairs.

--- qiskit/backends/aer/statevector_simulator.py
+++ qiskit/backends/aer/statevector_simulator.py
@@ -86,18 +86,18 @@ class StatevectorSimulator(QasmSimulator):
         for experiment in qobj.experiments:
             del experiment.instructions[-1]
         # Extract final state snapshot and move to 'statevector' data field
-        for experiment_result in result.results.values():
-            snapshots = experiment_result.snapshots
+        for experiment_result in result.results:
+            snapshots = experiment_result.data.snapshots.to_dict()
             if str(final_state_key) in snapshots:
                 final_state_key = str(final_state_key)
             # Pop off final snapshot added above
             final_state = snapshots.pop(final_state_key, None)
             final_state = final_state['statevector'][0]
             # Add final state to results data
-            experiment_result.data['statevector'] = final_state
+            experiment_result.data.statevector = final_state
             # Remove snapshot dict if empty
             if snapshots == {}:
-                experiment_result.data.pop('snapshots', None)
+                delattr(experiment_result.data, 'snapshots')
         return result
 
     def _validate(self, qobj):
