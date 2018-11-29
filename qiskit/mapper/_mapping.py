0816cf76b5f647539fb2a892c8abdaaa40373bfb
Ali Javadi <ajavadia@princeton.edu>
2018-11-29 10:05:53 -0500

    update check on qargs

--- qiskit/mapper/_mapping.py
+++ qiskit/mapper/_mapping.py
@@ -703,7 +703,7 @@ def optimize_1q_gates(circuit):
             left_name = nd["name"]
             if (nd["condition"] is not None
                     or len(nd["op"].qargs) != 1
-                    or nd["op"].qargs[0] != run_qarg
+                    or nd["qargs"][0] != run_qarg
                     or left_name not in ["u1", "u2", "u3", "id"]):
                 raise MapperError("internal error")
             if left_name == "u1":
