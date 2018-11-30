c24c0cadf4fb8ce38cba87e083478cf69d1667e4
Ali Javadi <ajavadia@princeton.edu>
2018-11-30 00:32:16 -0500

    update qobj examples

--- qiskit/tools/_compiler.py
+++ qiskit/tools/_compiler.py
@@ -90,11 +90,6 @@ def dags_2_qobj(dags, backend_name, config=None, shots=None,
     # Step 1: create the Qobj, with empty experiments.
     # Copy the configuration: the values in `config` have preference
     qobj_config = deepcopy(config or {})
-    # TODO: "memory_slots" is required by the qobj schema in the top-level
-    # qobj.config, and is user-defined. At the moment is set to the maximum
-    # number of *register* slots for the circuits, in order to have `measure`
-    # behave properly until the transition is over; and each circuit stores
-    # its memory_slots in its configuration.
     qobj_config.update({'shots': shots,
                         'max_credits': max_credits,
                         'memory_slots': 0})
@@ -111,14 +106,10 @@ def dags_2_qobj(dags, backend_name, config=None, shots=None,
                                                  'config': config,
                                                  'coupling_map': coupling_map})
 
-    # Update the `memory_slots` value.
-    # TODO: remove when `memory_slots` can be provided by the user.
+    # Update the global `memory_slots` and `n_qubits` values.
     qobj.config.memory_slots = max(experiment.config.memory_slots for
                                    experiment in qobj.experiments)
 
-    # Update the `n_qubits` global value.
-    # TODO: num_qubits is not part of the qobj specification, but needed
-    # for the simulator.
     qobj.config.n_qubits = max(experiment.config.n_qubits for
                                experiment in qobj.experiments)
 
@@ -142,14 +133,9 @@ def _dags_2_qobj_parallel(dag, config=None, basis_gates=None, coupling_map=None)
     experiment = QobjExperiment.from_dict(json_circuit)
     # Step 3b: populate the Experiment configuration and header
     experiment.header.name = dag.name
-    # TODO: place in header or config?
     experiment_config = deepcopy(config or {})
     experiment_config.update({
-        'coupling_map': coupling_map,
-        'basis_gates': basis_gates,
-        'layout': dag.layout,
         'memory_slots': sum([creg.size for creg in dag.cregs.values()]),
-        # TODO: `n_qubits` is not part of the qobj spec, but needed for the simulator.
         'n_qubits': sum([qreg.size for qreg in dag.qregs.values()])
         })
     experiment.config = QobjItem(**experiment_config)
