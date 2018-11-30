c28b7e8759199960ea69e6f0dabee123c7642c71
Luciano Bello <luciano.bello@ibm.com>
2018-11-30 15:39:25 -0500

    lint

--- qiskit/transpiler/passes/check_map.py
+++ qiskit/transpiler/passes/check_map.py
@@ -46,8 +46,9 @@ class CheckMap(AnalysisPass):
             subdag = layer['graph']
 
             for a_cx in subdag.get_cnot_nodes():
-                physical_q0 = (QuantumRegister(self.coupling_map.node_counter, 'q'), self.layout[a_cx['op'].qargs[0]])
-                physical_q1 = (QuantumRegister(self.coupling_map.node_counter, 'q'), self.layout[a_cx['op'].qargs[1]])
+                q = QuantumRegister(self.coupling_map.node_counter, 'q')
+                physical_q0 = (q, self.layout[a_cx['op'].qargs[0]])
+                physical_q1 = (q, self.layout[a_cx['op'].qargs[1]])
                 if self.coupling_map.distance(physical_q0, physical_q1) != 1:
                     self.property_set['is_mapped'] = False
                     return
