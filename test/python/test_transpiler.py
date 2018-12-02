15a373d24df57d1d2001fffdb96c500695be3579
Jay Gambetta <jay.gambetta@us.ibm.com>
2018-12-01 22:19:18 -0500

    Lint

--- test/python/test_transpiler.py
+++ test/python/test_transpiler.py
@@ -15,7 +15,6 @@ from qiskit.transpiler import PassManager, transpile_dag, transpile
 from qiskit.tools._compiler import circuits_to_qobj
 from qiskit.transpiler.passes import CXCancellation
 from qiskit.dagcircuit import DAGCircuit
-from qiskit.unroll import DagUnroller, JsonBackend
 from .common import QiskitTestCase
 
 
