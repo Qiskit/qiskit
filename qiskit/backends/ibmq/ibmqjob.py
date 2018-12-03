5e82ba7b4673386f2afd493868158dddaeaedba3
Ali Javadi <ajavadia@princeton.edu>
2018-12-03 16:26:59 -0500

    restore ibmq _reorder_bits

--- qiskit/backends/ibmq/ibmqjob.py
+++ qiskit/backends/ibmq/ibmqjob.py
@@ -470,8 +470,7 @@ class IBMQJobPreQobj(IBMQJob):
 
     def _result_from_job_response(self, job_response):
         if self._is_device:
-            # TODO: temporarily disabled for #1373, reenable before 0.7.
-            # _reorder_bits(job_response)
+            _reorder_bits(job_response)
             pass
 
         experiment_results = []
@@ -528,7 +527,7 @@ def _reorder_bits(job_data):
                            ' bits: bits may be out of order')
             return
         # device_qubit -> device_clbit (how it should have been)
-        measure_dict = {op['qubits'][0]: op['clbits'][0]
+        measure_dict = {op['qubits'][0]: op['memory'][0]
                         for op in circ['operations']
                         if op['name'] == 'measure'}
         counts_dict_new = {}
