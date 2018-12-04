6f05e12ac48d96ea03855559235300468edba3c1
Matthew Treinish <mtreinish@kortar.org>
2018-12-04 17:51:13 -0500

    Add support to qobj_to_circuits for qobj without qasm
    This commit adds support to the helper function in
    qiskit.tools.qobj_to_circuits for creating circuits from a qobj without
    compiled qasm in the header. It does this by looping over the Qobj
    object to run the operations needed to construct the circuit objects
    directly instead of just parsing qasm.
    
    Fixes #1407

--- qiskit/converters/qobj_to_circuits.py
+++ qiskit/converters/qobj_to_circuits.py
@@ -6,7 +6,22 @@
 # the LICENSE.txt file in the root directory of this source tree.
 
 """Helper function for converting qobj to a list of circuits"""
-from qiskit._quantumcircuit import QuantumCircuit
+
+from qiskit import _classicalregister as cr
+from qiskit import _quantumcircuit as qc
+from qiskit import _quantumregister as qr
+
+def _get_registers(reg_list, constructor):
+    registers = {}
+    for bits in reg_list:
+        if bits[0] not in registers:
+            registers[bits[0]] = [bits[1]]
+        else:
+            registers[bits[0]].append(bits[1])
+    out_list = []
+    for reg in registers:
+        out_list.append(constructor(len(registers[reg]), name=reg))
+    return out_list
 
 
 def qobj_to_circuits(qobj):
@@ -23,8 +38,35 @@ def qobj_to_circuits(qobj):
         for x in qobj.experiments:
             if hasattr(x.header, 'compiled_circuit_qasm'):
                 circuits.append(
-                    QuantumCircuit.from_qasm_str(x.header.compiled_circuit_qasm))
+                    qc.QuantumCircuit.from_qasm_str(
+                        x.header.compiled_circuit_qasm))
+            else:
+                quantum_registers = _get_registers(x.header.qubit_labels,
+                                                   qr.QuantumRegister)
+                classical_registers = _get_registers(x.header.clbit_labels,
+                                                     cr.ClassicalRegister)
+                circuit = qc.QuantumCircuit(*quantum_registers,
+                                            *classical_registers,
+                                            name=x.header.name)
+                qreg_dict = {}
+                creg_dict = {}
+                for reg in quantum_registers:
+                    qreg_dict[reg.name] = reg
+                for reg in classical_registers:
+                    creg_dict[reg.name] = reg
+                for i in x.instructions:
+                    instr_method = getattr(circuit, i.name)
+                    qubits = []
+                    for qubit in i.qubits:
+                        qubit_label = x.header.qubit_labels[qubit]
+                        qubits.append(
+                            qreg_dict[qubit_label[0]][qubit_label[1]])
+                    clbits = []
+                    for clbit in i.memory:
+                        clbit_label = x.header.clbit_labels[clbit]
+                        clbits.append(
+                            creg_dict[clbit_label[0]][clbit_label[1]])
+                    instr_method(*qubits, *clbits, *i.params)
+                circuits.append(circuit)
         return circuits
-    # TODO(mtreinish): add support for converting a qobj if the qasm isn't
-    # embedded in the header
     return None
