57e733a6cd15612dd7800e7948384b5c8b3f2bca
Diego M. Rodriguez <diego.plan9@gmail.com>
2018-12-04 12:40:43 +0100

    Remove unneeded pylint disable

--- test/python/tools/visualization/test_visualization.py
+++ test/python/tools/visualization/test_visualization.py
@@ -5,8 +5,6 @@
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
 
-# pylint: disable=missing-docstring
-
 """Tests for visualization tools."""
 
 import os
