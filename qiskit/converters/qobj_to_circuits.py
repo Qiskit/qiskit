ffab8602a0f42a5e869bbf64d1f85ed59a3bcd14
Matthew Treinish <mtreinish@kortar.org>
2018-12-05 10:05:43 -0500

    Changes from review comments

--- qiskit/converters/qobj_to_circuits.py
+++ qiskit/converters/qobj_to_circuits.py
@@ -12,19 +12,6 @@ from qiskit import _quantumcircuit as qc
 from qiskit import _quantumregister as qr
 
 
-def _get_registers(reg_list, constructor):
-    registers = {}
-    for bits in reg_list:
-        if bits[0] not in registers:
-            registers[bits[0]] = [bits[1]]
-        else:
-            registers[bits[0]].append(bits[1])
-    out_list = []
-    for reg in registers:
-        out_list.append(constructor(len(registers[reg]), name=reg))
-    return out_list
-
-
 def qobj_to_circuits(qobj):
     """Return a list of QuantumCircuit object(s) from a qobj
 
@@ -37,37 +24,34 @@ def qobj_to_circuits(qobj):
     if qobj.experiments:
         circuits = []
         for x in qobj.experiments:
-            if hasattr(x.header, 'compiled_circuit_qasm'):
-                circuits.append(
-                    qc.QuantumCircuit.from_qasm_str(
-                        x.header.compiled_circuit_qasm))
-            else:
-                quantum_registers = _get_registers(x.header.qubit_labels,
-                                                   qr.QuantumRegister)
-                classical_registers = _get_registers(x.header.clbit_labels,
-                                                     cr.ClassicalRegister)
-                circuit = qc.QuantumCircuit(*quantum_registers,
-                                            *classical_registers,
-                                            name=x.header.name)
-                qreg_dict = {}
-                creg_dict = {}
-                for reg in quantum_registers:
-                    qreg_dict[reg.name] = reg
-                for reg in classical_registers:
-                    creg_dict[reg.name] = reg
-                for i in x.instructions:
-                    instr_method = getattr(circuit, i.name)
-                    qubits = []
-                    for qubit in i.qubits:
-                        qubit_label = x.header.qubit_labels[qubit]
-                        qubits.append(
-                            qreg_dict[qubit_label[0]][qubit_label[1]])
-                    clbits = []
-                    for clbit in i.memory:
-                        clbit_label = x.header.clbit_labels[clbit]
-                        clbits.append(
-                            creg_dict[clbit_label[0]][clbit_label[1]])
-                    instr_method(*qubits, *clbits, *i.params)
-                circuits.append(circuit)
+            quantum_registers = [
+                qr.QuantumRegister(
+                    i[1], name=i[0]) for i in x.header.qreg_sizes]
+            classical_registers = [
+                cr.ClassicalRegister(
+                    i[1], name=i[0]) for i in x.header.creg_sizes]
+            circuit = qc.QuantumCircuit(*quantum_registers,
+                                        *classical_registers,
+                                        name=x.header.name)
+            qreg_dict = {}
+            creg_dict = {}
+            for reg in quantum_registers:
+                qreg_dict[reg.name] = reg
+            for reg in classical_registers:
+                creg_dict[reg.name] = reg
+            for i in x.instructions:
+                instr_method = getattr(circuit, i.name)
+                qubits = []
+                for qubit in i.qubits:
+                    qubit_label = x.header.qubit_labels[qubit]
+                    qubits.append(
+                        qreg_dict[qubit_label[0]][qubit_label[1]])
+                clbits = []
+                for clbit in i.memory:
+                    clbit_label = x.header.clbit_labels[clbit]
+                    clbits.append(
+                        creg_dict[clbit_label[0]][clbit_label[1]])
+                instr_method(*qubits, *clbits, *i.params)
+            circuits.append(circuit)
         return circuits
     return None
