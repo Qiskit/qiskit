94e001f586028a89538ef573edc34f00c6e1198b
Matthew Treinish <mtreinish@kortar.org>
2018-12-05 11:20:42 -0500

    Remove deepcopy usage in dag_to_circuit
    This commit attempts to improve the performance of dag_to_circuit by
    removing the deepcopy usage. Instead this switches to  creating a
    circuit object using the normal constructors while iterating over the
    dag.

--- qiskit/converters/dag_to_circuit.py
+++ qiskit/converters/dag_to_circuit.py
@@ -6,12 +6,14 @@
 # the LICENSE.txt file in the root directory of this source tree.
 
 """Helper function for converting a dag to a circuit"""
-from copy import deepcopy
+import collections
 import random
 import string
 import networkx as nx
 
 from qiskit._quantumcircuit import QuantumCircuit
+from qiskit._classicalregister import ClassicalRegister
+from qiskit._quantumregister import QuantumRegister
 
 
 def dag_to_circuit(dag):
@@ -23,24 +25,35 @@ def dag_to_circuit(dag):
     Return:
         QuantumCircuit: the circuit representing the input dag.
     """
-    circuit = QuantumCircuit()
+    qregs = collections.OrderedDict()
+    for qreg in dag.qregs.values():
+        qreg_tmp = QuantumRegister(qreg.size, name=qreg.name)
+        qregs[qreg.name] = qreg_tmp
+    cregs = collections.OrderedDict()
+    for creg in dag.cregs.values():
+        creg_tmp = ClassicalRegister(creg.size, name=creg.name)
+        cregs[creg.name] = creg_tmp
+
     random_name = QuantumCircuit.cls_prefix() + \
         str(''.join(random.choice(string.ascii_lowercase) for i in range(8)))
-    circuit.name = dag.name or random_name
-    for qreg in dag.qregs.values():
-        circuit.add_register(qreg)
-    for creg in dag.cregs.values():
-        circuit.add_register(creg)
+    name = dag.name or random_name
+    circuit = QuantumCircuit(*qregs.values(), *cregs.values(), name=name)
+
     graph = dag.multi_graph
     for node in nx.topological_sort(graph):
         n = graph.nodes[node]
         if n['type'] == 'op':
-            op = deepcopy(n['op'])
-            op.qargs = n['qargs']
-            op.cargs = n['cargs']
-            op.circuit = circuit
+            instr_method = getattr(circuit, n['op'].name)
+            qubits = []
+            for qubit in n['op'].qargs:
+                qubits.append(qregs[qubit[0].name][qubit[1]])
+
+            clbits = []
+            for clbit in n['op'].cargs:
+                clbits.append(cregs[clbit[0].name][clbit[1]])
+            params = n['op'].param
+            result = instr_method(*params, *qubits, *clbits)
             if 'condition' in n and n['condition']:
-                op = op.c_if(*n['condition'])
-            circuit._attach(op)
+                result.c_if(*n['condition'])
 
     return circuit
