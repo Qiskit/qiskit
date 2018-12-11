acaa7a79e492f0cec3cfa9953ccd18df5277b62e
cjwood <cjwood@us.ibm.com>
2018-12-11 15:22:51 -0500

    update python simulators to match qiskit-aer
    * update qasm_simulator_py to use numpy.einsum
    * add measure sampling to qasm_simulator_py
    * implements statevector_simulator_py without snapshot hack
    * removes snapshots from python simulators
    * allows measure, reset, conditionals in single shot statevector simulator
    * if circuit contains no creg qasm_simulator_py returns empty count dictionary
    * if circuit contains no creg memory option returns empty memory list
    * adds basic backend_options from qiskit-aer C++ to terra aer_py simulators
    * set initial statevector for qasm_simualtor_py and statevector_simulator_py with "initial_statevector"
    * set initial unitary for unitary_simulator_py with "initial_unitary
    * set chop threshold for output statevector and unitaries with "chop_threshold"
    * sampling is activated if flag `"allows_measure_sampling": True in an experiment config
    * add basic test for measure sampling if flag not found in config
    * check max_qubits based on system memory and einsum max in experiment validation

--- qiskit/backends/builtinsimulators/_simulatortools.py
+++ qiskit/backends/builtinsimulators/_simulatortools.py
@@ -15,50 +15,8 @@ Functions
 """
 
 from string import ascii_uppercase, ascii_lowercase
-
 import numpy as np
-
 from qiskit import QiskitError
-
-
-def index1(b, i, k):
-    """Magic index1 function.
-
-    Takes a bitstring k and inserts bit b as the ith bit,
-    shifting bits >= i over to make room.
-    """
-    retval = k
-    lowbits = k & ((1 << i) - 1)  # get the low i bits
-
-    retval >>= i
-    retval <<= 1
-
-    retval |= b
-
-    retval <<= i
-    retval |= lowbits
-
-    return retval
-
-
-def index2(b1, i1, b2, i2, k):
-    """Magic index2 function.
-
-    Takes a bitstring k and inserts bits b1 as the i1th bit
-    and b2 as the i2th bit
-    """
-    if i1 == i2:
-        raise QiskitError("can't insert two bits to same place")
-
-    if i1 > i2:
-        # insert as (i1-1)th bit, will be shifted left 1 by next line
-        retval = index1(b1, i1-1, k)
-        retval = index1(b2, i2, retval)
-    else:  # i2>i1
-        # insert as (i2-1)th bit, will be shifted left 1 by next line
-        retval = index1(b2, i2-1, k)
-        retval = index1(b1, i1, retval)
-    return retval
 
 
 def single_gate_params(gate, params=None):
@@ -75,7 +33,7 @@ def single_gate_params(gate, params=None):
     if gate == 'U' or gate == 'u3':
         return params[0], params[1], params[2]
     elif gate == 'u2':
-        return np.pi/2, params[0], params[1]
+        return np.pi / 2, params[0], params[1]
     elif gate == 'u1':
         return 0, 0, params[0]
     elif gate == 'id':
@@ -97,14 +55,22 @@ def single_gate_matrix(gate, params=None):
     # This a is a probable a FIXME since it might show bugs in the simulator.
     (theta, phi, lam) = map(float, single_gate_params(gate, params))
 
-    return np.array([[np.cos(theta/2),
-                      -np.exp(1j*lam)*np.sin(theta/2)],
-                     [np.exp(1j*phi)*np.sin(theta/2),
-                      np.exp(1j*phi+1j*lam)*np.cos(theta/2)]])
+    return np.array([[np.cos(theta / 2),
+                      -np.exp(1j * lam) * np.sin(theta / 2)],
+                     [np.exp(1j * phi) * np.sin(theta / 2),
+                      np.exp(1j * phi + 1j * lam) * np.cos(theta / 2)]])
+
+
+def cx_gate_matrix():
+    """Get the matrix for a controlled-NOT gate."""
+    return np.array([[1, 0, 0, 0],
+                     [0, 0, 0, 1],
+                     [0, 0, 1, 0],
+                     [0, 1, 0, 0]], dtype=complex)
 
 
 def einsum_matmul_index(gate_indices, number_of_qubits):
-    """Return the index string for Numpy.eignsum matrix multiplication.
+    """Return the index string for Numpy.eignsum matrix-matrix multiplication.
 
     The returned indices are to perform a matrix multiplication A.B where
     the matrix A is an M-qubit matrix, matrix B is an N-qubit matrix, and
@@ -118,6 +84,65 @@ def einsum_matmul_index(gate_indices, number_of_qubits):
 
     Returns:
         str: An indices string for the Numpy.einsum function.
+    """
+
+    mat_l, mat_r, tens_lin, tens_lout = _einsum_matmul_index_helper(gate_indices,
+                                                                    number_of_qubits)
+
+    # Right indices for the N-qubit input and output tensor
+    tens_r = ascii_uppercase[:number_of_qubits]
+
+    # Combine indices into matrix multiplication string format
+    # for numpy.einsum function
+    return "{mat_l}{mat_r}, ".format(mat_l=mat_l, mat_r=mat_r) + \
+           "{tens_lin}{tens_r}->{tens_lout}{tens_r}".format(tens_lin=tens_lin,
+                                                            tens_lout=tens_lout,
+                                                            tens_r=tens_r)
+
+
+def einsum_vecmul_index(gate_indices, number_of_qubits):
+    """Return the index string for Numpy.eignsum matrix-vector multiplication.
+
+    The returned indices are to perform a matrix multiplication A.v where
+    the matrix A is an M-qubit matrix, vector v is an N-qubit vector, and
+    M <= N, and identity matrices are implied on the subsystems where A has no
+    support on v.
+
+    Args:
+        gate_indices (list[int]): the indices of the right matrix subsystems
+                                  to contract with the left matrix.
+        number_of_qubits (int): the total number of qubits for the right matrix.
+
+    Returns:
+        str: An indices string for the Numpy.einsum function.
+    """
+
+    mat_l, mat_r, tens_lin, tens_lout = _einsum_matmul_index_helper(gate_indices,
+                                                                    number_of_qubits)
+
+    # Combine indices into matrix multiplication string format
+    # for numpy.einsum function
+    return "{mat_l}{mat_r}, ".format(mat_l=mat_l, mat_r=mat_r) + \
+           "{tens_lin}->{tens_lout}".format(tens_lin=tens_lin,
+                                            tens_lout=tens_lout)
+
+
+def _einsum_matmul_index_helper(gate_indices, number_of_qubits):
+    """Return the index string for Numpy.eignsum matrix multiplication.
+
+    The returned indices are to perform a matrix multiplication A.v where
+    the matrix A is an M-qubit matrix, matrix v is an N-qubit vector, and
+    M <= N, and identity matrices are implied on the subsystems where A has no
+    support on v.
+
+    Args:
+        gate_indices (list[int]): the indices of the right matrix subsystems
+                                   to contract with the left matrix.
+        number_of_qubits (int): the total number of qubits for the right matrix.
+
+    Returns:
+        tuple: (mat_left, mat_right, tens_in, tens_out) of index strings for
+        that may be combined into a Numpy.einsum function string.
 
     Raises:
         QiskitError: if the total number of qubits plus the number of
@@ -131,14 +156,11 @@ def einsum_matmul_index(gate_indices, number_of_qubits):
     if len(gate_indices) + number_of_qubits > 26:
         raise QiskitError("Total number of free indexes limited to 26")
 
-    # Right indices for the N-qubit input and output tensor
-    idx_right = ascii_uppercase[:number_of_qubits]
+    # Indicies for N-qubit input tensor
+    tens_in = ascii_lowercase[:number_of_qubits]
 
-    # Left ndicies for N-qubit input tensor
-    idx_left_in = ascii_lowercase[:number_of_qubits]
-
-    # Left indices for the N-qubit output tensor
-    idx_left_out = list(idx_left_in)
+    # Indices for the N-qubit output tensor
+    tens_out = list(tens_in)
 
     # Left and right indices for the M-qubit multiplying tensor
     mat_left = ""
@@ -147,13 +169,10 @@ def einsum_matmul_index(gate_indices, number_of_qubits):
     # Update left indices for mat and output
     for pos, idx in enumerate(reversed(gate_indices)):
         mat_left += ascii_lowercase[-1 - pos]
-        mat_right += idx_left_in[-1 - idx]
-        idx_left_out[-1 - idx] = ascii_lowercase[-1 - pos]
-    idx_left_out = "".join(idx_left_out)
+        mat_right += tens_in[-1 - idx]
+        tens_out[-1 - idx] = ascii_lowercase[-1 - pos]
+    tens_out = "".join(tens_out)
 
     # Combine indices into matrix multiplication string format
     # for numpy.einsum function
-    return "{mat_l}{mat_r}, ".format(mat_l=mat_left, mat_r=mat_right) + \
-           "{tens_lin}{tens_r}->{tens_lout}{tens_r}".format(tens_lin=idx_left_in,
-                                                            tens_lout=idx_left_out,
-                                                            tens_r=idx_right)
+    return mat_left, mat_right, tens_in, tens_out
