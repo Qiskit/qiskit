a6b8a78bad2d7d964c58094b2e4b4a961debb0c8
Andrew Cross <awcross@us.ibm.com>
2018-12-15 00:42:41 -0500

    start tests.

--- qiskit/transpiler/passes/__init__.py
+++ qiskit/transpiler/passes/__init__.py
@@ -13,6 +13,7 @@ from .optimize_1q_gates import Optimize1qGates
 from .decompose import Decompose
 from .mapping.check_map import CheckMap
 from .mapping.basic_mapper import BasicMapper
+from .mapping.stochastic_mapper import StochasticMapper
 from .mapping.direction_mapper import DirectionMapper
 from .mapping.unroller import Unroller
 from .mapping.lookahead_mapper import LookaheadMapper
