82f43bd9f7e706ab732fa8657b4acaa693a4c4d7
Diego M. Rodriguez <diego.plan9@gmail.com>
2018-12-17 19:31:03 +0100

    Enable job monitor tests

--- test/python/tools/monitor/test_backend_monitor.py
+++ test/python/tools/monitor/test_backend_monitor.py
@@ -5,7 +5,6 @@
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
 
-# pylint: disable=redefined-builtin
 
 """Tests for the wrapper functionality."""
 
@@ -17,12 +16,14 @@ from ...common import QiskitTestCase, requires_qe_access
 
 class TestBackendOverview(QiskitTestCase):
     """Tools test case."""
+
     @requires_qe_access
     def test_backend_overview(self, qe_token, qe_url):
         """Test backend_overview"""
         IBMQ.enable_account(qe_token, qe_url)
         backend_overview()
 
+    @requires_qe_access
     def test_backend_monitor(self, qe_token, qe_url):
         """Test backend_monitor"""
         IBMQ.enable_account(qe_token, qe_url)
