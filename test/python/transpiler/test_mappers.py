86efad41848f0374e2cd2617a086ddaec2d4a530
Luciano Bello <luciano.bello@ibm.com>
2018-12-20 10:24:36 -0500

    introduce pickles

--- test/python/transpiler/test_mappers.py
+++ test/python/transpiler/test_mappers.py
@@ -5,9 +5,15 @@
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
 
-"""Meta Tests for mappers"""
+"""Meta Tests for mappers
+
+Regenerate ground truth by running this on the root directory:
+python -m  test.python.transpiler.test_mappers
+
+"""
 
 import unittest
+import pickle
 
 from qiskit import QuantumRegister, QuantumCircuit, BasicAer
 from qiskit.transpiler import PassManager, transpile
@@ -17,12 +23,24 @@ from qiskit.mapper import CouplingMap
 from ..common import QiskitTestCase
 
 class CommonUtilities():
+    regenerate_expected = False
 
     def create_passmanager(self, coupling_map):
-        return PassManager(self.swap_pass(CouplingMap(coupling_map)))
+        return PassManager(self.pass_class(CouplingMap(coupling_map)))
 
-    def assertResult(self, result, expected):
-        self.assertEqual(result, expected)
+    def create_backend(self):
+        return BasicAer.get_backend('qasm_simulator')
+
+    def assertResult(self, result, test_name):
+        filename = "%s_%s.pickle" % (type(self).__name__, test_name)
+        if self.regenerate_expected:
+            # Run result in backend to test that is valid.
+            with open(filename, "wb") as output_file:
+                pickle.dump(result, output_file)
+        else:
+            with open(filename, "rb") as input_file:
+                expected=pickle.load(input_file)
+            self.assertEqual(result, expected)
 
 class CommonTestCases(CommonUtilities):
 
@@ -43,39 +61,24 @@ class CommonTestCases(CommonUtilities):
         circuit.h(qr[1])
         circuit.cx(qr[1], qr[2])
 
-        backend = BasicAer.get_backend('qasm_simulator')
-        result = transpile(circuit, backend, coupling_map=coupling_map,
+        result = transpile(circuit, self.create_backend(), coupling_map=coupling_map,
                              pass_manager=self.create_passmanager(coupling_map))
-        self.assertResult(result, self.expected['a_cx_to_map'])
+        self.assertResult(result, 'a_cx_to_map')
 
 class TestsBasicMapper(CommonTestCases, QiskitTestCase):
-
-    def setUp(self):
-        self.swap_pass = BasicSwap
-        qr = QuantumRegister(3, 'q')
-        self.expected = {}
-        self.expected["a_cx_to_map"] = QuantumCircuit(qr)
-        self.expected["a_cx_to_map"].h(qr[1])
-        self.expected["a_cx_to_map"].swap(qr[1], qr[0])
-        self.expected["a_cx_to_map"].cx(qr[0], qr[2])
+    pass_class = BasicSwap
 
     def test_specific_input(self):
         """Some specific test"""
         self.assertTrue(True)
 
 class TestsLookaheadSwap(CommonTestCases, QiskitTestCase):
-    def setUp(self):
-        self.swap_pass = LookaheadSwap
-        qr = QuantumRegister(3, 'q')
-        self.expected = {}
-        self.expected["a_cx_to_map"] = QuantumCircuit(qr)
-        self.expected["a_cx_to_map"].h(qr[1])
-        self.expected["a_cx_to_map"].swap(qr[0], qr[1])
-        self.expected["a_cx_to_map"].cx(qr[0], qr[2])
+    pass_class = LookaheadSwap
 
     def test_specific_input(self):
         """Some specific test"""
         self.assertTrue(True)
 
 if __name__ == '__main__':
+    CommonUtilities.regenerate_expected = True
     unittest.main()
