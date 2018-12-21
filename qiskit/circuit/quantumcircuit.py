84a77e0e0f3368ee152af0d4b3690ebdc23a1cf3
Diego M. Rodriguez <diego.plan9@gmail.com>
2018-12-21 18:02:11 +0100

    Update cyclic import in QuantumCircuit
    Move the pylint disable to an inner-most level for QuantumCircuit. There
    is still an import cycle (qiskit.circuit.quantumcircuit ->
    qiskit.converters.ast_to_dag) due to circuit.Measure and circuit.Reset
    being part of the module and the dynamic extension mechanism.

--- qiskit/circuit/quantumcircuit.py
+++ qiskit/circuit/quantumcircuit.py
@@ -5,11 +5,10 @@
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
 
-# pylint: disable=cyclic-import,invalid-name
-
 """
 Quantum circuit object.
 """
+
 from collections import OrderedDict
 from copy import deepcopy
 import itertools
@@ -413,6 +412,7 @@ class QuantumCircuit:
 
 
 def _circuit_from_qasm(qasm):
+    # pylint: disable=cyclic-import
     from qiskit.converters import ast_to_dag
     from qiskit.converters import dag_to_circuit
     ast = qasm.parse()
