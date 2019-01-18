3121c572a22669f235e5c4bc0729284d456ef70f
knzwnao <knzwnao@jp.ibm.com>
2019-01-18 13:26:55 +0900

    reformat code

--- qiskit/pulse/pulse.py
+++ qiskit/pulse/pulse.py
@@ -12,37 +12,38 @@ Pulse envelope generation.
 import ast
 import cmath
 import inspect
-import matplotlib.pyplot as plt
+
 import numpy as np
-from matplotlib import get_backend as get_matplotlib_backend
+from matplotlib import get_backend as get_matplotlib_backend, pyplot as plt
 from scipy.interpolate import CubicSpline
 
 from qiskit.exceptions import QiskitError
 
-builtin = ['exp', 'log', 'log10', 'sqrt', 'acos', 'asin', 'atan', 'cos', 'sin', 'tan',
-           'acosh', 'asinh', 'atanh', 'cosh', 'sinh', 'tanh', 'pi', 'e', 'tau']
+MATH = ['exp', 'log', 'log10', 'sqrt', 'acos', 'asin', 'atan', 'cos', 'sin', 'tan',
+        'acosh', 'asinh', 'atanh', 'cosh', 'sinh', 'tanh', 'pi', 'e', 'tau']
 
 
-class Pulse(object):
+class Pulse:
     """Pulse specification."""
 
-    def __init__(self, envfunc, t_gate, dt, **kwargs):
+    def __init__(self, envfunc, t_gate, t_intv, **kwargs):
         """Create a new pulse envelope.
         Args:
             envfunc (str): equation describing pulse envelope
             t_gate (float): gate time
-            dt (float): time resolution
+            t_intv (float): time interval of sampling points
+            **kwargs: initial values of pulse parameter
         Raises:
             QiskitError: when pulse envelope is not in the correct format.
         """
         self._param = {}
         self._ast = None
-        self.dt = dt
+        self.t_intv = t_intv
         self.t_gate = t_gate
 
         if isinstance(envfunc, str):
             self._ast, pars = Pulse.eval_expr(envfunc)
-            self._param = dict([(par, kwargs.get(par, 0)) for par in pars])
+            self._param = {par: kwargs.get(par, 0) for par in pars}
         else:
             raise QiskitError('Incorrect specification of pulse envelope function.')
 
@@ -51,7 +52,7 @@ class Pulse(object):
         """Get parameters for describing pulse envelope
 
         Returns:
-            param (dict): Dictionary of parameters
+            dict: pulse parameters
         """
         return self._param
 
@@ -60,7 +61,9 @@ class Pulse(object):
         """Set parameters for describing pulse envelope
 
         Args:
-            param_new (dict): Dictionary of parameters
+            param_new (dict): dictionary of parameters
+        Raises:
+            QiskitError: when pulse parameter is not in the correct format.
         """
         if isinstance(param_new, dict):
             for key, val in self._param.items():
@@ -72,21 +75,23 @@ class Pulse(object):
         """Calculate and output pulse envelope as a list of complex values [re, im]
 
         Returns:
-            envelope (list): List of pulse envelope at each time interval dt
+            list: complex pulse envelope at each sampling point
+        Raises:
+            QiskitError: when envelope function is not initialized.
         """
         if self._ast:
             math_funcs = dict(inspect.getmembers(cmath))
-            _dict = dict([(k, math_funcs.get(k, None)) for k in builtin])
+            _dict = {k: math_funcs.get(k, None) for k in MATH}
             _dict.update(self._param)
             envelope = []
 
-            _t = 0
-            while _t <= self.t_gate:
-                _dict['t'] = _t
+            _time = 0
+            while _time <= self.t_gate:
+                _dict['t'] = _time
                 value = eval(compile(self._ast, filename='', mode='eval'),
                              {"__builtins__": None}, _dict)
                 envelope.append([np.real(value), np.imag(value)])
-                _t += self.dt
+                _time += self.t_intv
         else:
             raise QiskitError('Envelope function is not defined.')
 
@@ -98,35 +103,37 @@ class Pulse(object):
         Args:
             nop (int): number of points for interpolation
         Returns:
-            im (matplotlib.figure): a matplotlib figure object for the pulse envelope
+            matplotlib.figure: a matplotlib figure object for the pulse envelope
         """
-        _v = np.array(self.tolist())
-        ry = np.array(_v[:, 0])
-        iy = np.array(_v[:, 1])
-        x = self.dt * np.linspace(0, len(ry) - 1, len(ry))
+        pulse_samp = np.array(self.tolist())
+        re_y = np.array(pulse_samp[:, 0])
+        im_y = np.array(pulse_samp[:, 1])
+        x = self.t_intv * np.linspace(0, len(re_y) - 1, len(re_y))
 
         # spline interpolation
-        cs_ry = CubicSpline(x, ry)
-        cs_iy = CubicSpline(x, iy)
-        xs = np.linspace(0, max(x), nop)
+        cs_ry = CubicSpline(x, re_y)
+        cs_iy = CubicSpline(x, im_y)
+        x_interp = np.linspace(0, max(x), nop)
 
         figure = plt.figure(figsize=(6, 5))
-        ax = figure.add_subplot(111)
-        ax.scatter(x=x, y=ry, c='red')
-        ax.scatter(x=x, y=iy, c='blue')
-        ax.fill_between(x=xs, y1=cs_ry(xs), y2=np.zeros_like(xs), facecolors='r', alpha=0.5)
-        ax.fill_between(x=xs, y1=cs_iy(xs), y2=np.zeros_like(xs), facecolors='b', alpha=0.5)
-        ax.set_xlim([0, self.t_gate])
-        ax.grid(b=True, linestyle='-')
+        ax0 = figure.add_subplot(111)
+        ax0.scatter(x=x, y=re_y, c='red')
+        ax0.scatter(x=x, y=im_y, c='blue')
+        ax0.fill_between(x=x_interp, y1=cs_ry(x_interp), y2=np.zeros_like(x_interp),
+                         facecolors='r', alpha=0.5)
+        ax0.fill_between(x=x_interp, y1=cs_iy(x_interp), y2=np.zeros_like(x_interp),
+                         facecolors='b', alpha=0.5)
+        ax0.set_xlim([0, self.t_gate])
+        ax0.grid(b=True, linestyle='-')
 
         if get_matplotlib_backend() == 'module://ipykernel.pylab.backend_inline':
             # returns None when matplotlib is inline mode to prevent Jupyter
             # with matplotlib inlining enabled to draw the diagram twice.
-            im = None
+            img = None
         else:
-            im = figure
+            img = figure
 
-        return im
+        return img
 
     @staticmethod
     def eval_expr(expr_str):
@@ -136,15 +143,16 @@ class Pulse(object):
             expr_str (str): equation string to be evaluated
 
         Returns:
-            tree (AST): abstract syntax tree of input equation string
-            param (list): list of parameters
+            tuple[AST, list]: a tuple in the form (AST, list)
+        Raises:
+            QiskitError: when unintentional code is injected.
         """
         tree = ast.parse(expr_str, mode='eval')
         param = []
 
         for node in ast.walk(tree):
             if isinstance(node, ast.Name):
-                if node.id not in builtin and node.id not in param and node.id != 't':
+                if node.id not in MATH and node.id not in param and node.id != 't':
                     param.append(node.id)
             elif isinstance(node, (ast.Expression, ast.Call, ast.Load,
                                    ast.BinOp, ast.UnaryOp, ast.operator,
