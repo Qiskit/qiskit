0360b9d4b14edf6183a778027ea0f90528c87758
Manoel Marques <manoel@us.ibm.com>
2019-02-04 12:25:25 -0500

    Each Pluggable calls init_params on its dependencies

--- qiskit/aqua/components/variational_forms/variational_form.py
+++ qiskit/aqua/components/variational_forms/variational_form.py
@@ -19,7 +19,7 @@ This module contains the definition of a base class for
 variational forms. Several types of commonly used ansatz.
 """
 
-from qiskit.aqua import Pluggable
+from qiskit.aqua import Pluggable, PluggableType, get_pluggable_class
 from abc import abstractmethod
 from qiskit.aqua.utils import get_entangler_map, validate_entangler_map
 
@@ -45,7 +45,16 @@ class VariationalForm(Pluggable):
 
     @classmethod
     def init_params(cls, params):
-        args = {k: v for k, v in params.items() if k != 'name'}
+        var_form_params = params.get(Pluggable.SECTION_KEY_VAR_FORM)
+        args = {k: v for k, v in var_form_params.items() if k != 'name'}
+
+        # We pass on num_qubits to initial state since we know our dependent needs this
+        init_state_params = params.get(Pluggable.SECTION_KEY_INITIAL_STATE)
+        if init_state_params is not None:
+            init_state_params['num_qubits'] = var_form_params['num_qubits']
+            args['initial_state'] = get_pluggable_class(PluggableType.INITIAL_STATE,
+                                                        init_state_params['name']).init_params(params)
+
         return cls(**args)
 
     @abstractmethod
