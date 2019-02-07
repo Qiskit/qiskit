8c807d06a562a3aed689b02fa0acb596c8f7e74c
Erick Winston <ewinston@us.ibm.com>
2019-02-07 10:32:15 -0500

    add unitary_matrix.py!

---
+++ qiskit/extensions/standard/unitary_matrix.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2017, IBM.
+#
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree.
+
+# pylint: disable=invalid-name
+
+"""
+Matrix gate
+"""
+import numpy
+import sympy
+from qiskit.circuit import Gate
+from qiskit.circuit import OpaqueGate
+from qiskit.circuit import QuantumCircuit
+from qiskit.circuit import QuantumRegister
+from qiskit.dagcircuit import DAGCircuit
+from qiskit.exceptions import QiskitError
+
+
+class UnitaryMatrixGate(Gate):
+
+    def __init__(self, unitary_matrix, *qargs, circuit=None):
+        umatrix = numpy.array(unitary_matrix).astype(numpy.complex64)
+        if not numpy.allclose(umatrix.dot(umatrix.T.conj()),
+                              numpy.identity(2**len(qargs))):
+            raise QiskitError('non-unitary matrix')
+        super().__init__('unitary', [sympy.Matrix(umatrix)], list(qargs), circuit=circuit)
+        self.representation = umatrix
+
+    def _define_decompositions(self):
+        self._decompositions = None
+
+    def reapply(self, circ):
+        self._modifiers(circ.unitary(self.params[0], *self.qargs))
+
+    def qasm(self):
+        """Return a default OpenQASM string for the instruction.
+        """
+        name_param = self.name
+        if self.params:
+            name_param = "%s(%s)" % (name_param,
+                                     repr(self.params[0].tolist()))
+
+        name_param_arg = "%s %s;" % (name_param,
+                                     ",".join(["%s[%d]" % (j[0].name, j[1])
+                                               for j in self.qargs + self.cargs]))
+        return self._qasmif(name_param_arg)
+        
+
+def unitary(self, unitary_matrix, *qargs):
+    for qubit in qargs:
+        self._check_qubit(qubit)
+    return self._attach(UnitaryMatrixGate(unitary_matrix, *qargs, circuit=self))
+
+QuantumCircuit.unitary = unitary
