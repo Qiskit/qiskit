f3ef9d71b6dfaeb6de8d7577ef2850d1ac90666e
Erick Winston <ewinston@us.ibm.com>
2019-02-08 16:18:23 -0500

    minor

--- qiskit/converters/circuits_to_qobj.py
+++ qiskit/converters/circuits_to_qobj.py
@@ -123,10 +123,10 @@ def circuits_to_qobj(circuits, user_qobj_header=None, run_config=None,
                 params = [sympy.matrix2numpy(x, dtype=complex)
                           if isinstance(x, sympy.Matrix) else x for x in params]
                 if len(params) == 1 and isinstance(params[0], numpy.ndarray):
-                    # Aer expects list of rows for unitary instruction params
+                    # TODO: Aer expects list of rows for unitary instruction params;
+                    # change to matrix in Aer.
                     params = params[0]
                 current_instruction.params = params
-
             # TODO: I really dont like this for snapshot. I also think we should change
             # type to snap_type
             if opt.name == "snapshot":
