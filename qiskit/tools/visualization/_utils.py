9a156a8e1dfed0a3574f52fb620a53d691ec8755
Maddy Tod <40489777+maddy-tod@users.noreply.github.com>
2019-02-12 15:13:30 +0000

    Choose how to justify circuit with text drawer (#1725)
    * Fixed #1685 by adding a dictionary to the DAGCircuit class which keeps track of where a node is in the circuit, by mapping the number of a node to its position from the circuit.data object
    
    * Done some further testing and it seems that all that is needed is the lexicographical topological sort - the key doesn't make a difference
    
    * Removed all code relating to the key function
    
    * Updated the tests to reflect the changes to the order the nodes are output in
    
    * Updating based on lint
    
    * Start of work on having the text circuits be left justified - seems to work, more testing needed
    
    * Updated to get around gates that would overlap
    
    * Refactoring
    
    * Changes to respect order gates were declared in
    
    * Fixed a bug where the lines sometimes didn't appear
    
    * Added in parameter to say how you want the circuit to be justified
    
    * Left and None justify working
    
    * Right justify now working
    
    * Fixed bug where reverse bits didn't work
    
    * Fixed a bug where gates were inlined even though they overlapped
    
    * Fixed bug where resets didn't draw
    
    * Updated the testing to reflect the new outputs
    
    * Fixed up lint and comments
    
    * Removed unintentional changes from other files
    
    * Removed an old image file
    
    * Moved the justify code into a method in _utils
    
    * Fixed merge conflict
    
    * Added in more testing
    
    * Updated CHANGELOG
    
    * tidying up
    
    * fixed review comments - removed unused code, updated one output, removed soon to be deprecated method
    
    * Changed the test for the circuit drawers to allow for new justify parameter, and set line_length to be -1 so that text outputs will always be the same. Updated the reference images accordingly
    
    * Removed old comment
    
    * Linting
    
    * Removed an accidental parameter

--- qiskit/tools/visualization/_utils.py
+++ qiskit/tools/visualization/_utils.py
@@ -48,6 +48,135 @@ def _trim(image):
     return image
 
 
+def _get_layered_instructions(circuit, reversebits=False, justify=None):
+    """
+    Given a circuit, return a tuple (qregs, cregs, ops) where
+    qregs and cregs are the quantum and classical registers
+    in order (based on reversebits) and ops is a list
+    of DAG nodes which type is "operation".
+    Args:
+        circuit (QuantumCircuit): From where the information is extracted.
+        reversebits (bool): If true the order of the bits in the registers is
+            reversed.
+        justify (str) : `left`, `right` or `none`. Defaults to `left`. Says how
+            the circuit should be justified.
+    Returns:
+        Tuple(list,list,list): To be consumed by the visualizer directly.
+    """
+
+    if justify:
+        justify.lower()
+    # default to left
+    justify = justify if justify in ('right', 'none') else 'left'
+
+    dag = circuit_to_dag(circuit)
+    ops = []
+    qregs = []
+    cregs = []
+
+    for qreg in dag.qregs.values():
+        qregs += [(qreg, bitno) for bitno in range(qreg.size)]
+
+    for creg in dag.cregs.values():
+        cregs += [(creg, bitno) for bitno in range(creg.size)]
+
+    if justify == 'none':
+        for node_no in dag.node_nums_in_topological_order():
+            node = dag.multi_graph.node[node_no]
+            if node['type'] == 'op':
+                ops.append([node])
+
+    if justify == 'left':
+        for dag_layer in dag.layers():
+            layers = []
+            current_layer = []
+
+            dag_nodes = dag_layer['graph'].op_nodes(data=True)
+            dag_nodes.sort(key=lambda tup: tup[0])
+            dag_nodes = [v for k, v in dag_nodes]
+
+            for node in dag_nodes:
+                multibit_gate = len(node['qargs']) + len(node['cargs']) > 1
+
+                if multibit_gate:
+                    # need to see if it crosses over any other nodes
+                    gate_span = _get_gate_span(qregs, node)
+
+                    all_indices = []
+                    for ins in dag_nodes:
+                        if ins != node:
+                            all_indices += _get_gate_span(qregs, ins)
+
+                    if any(i in gate_span for i in all_indices):
+                        # needs to be a new layer
+                        layers.append([node])
+                    else:
+                        # can be added
+                        current_layer.append(node)
+                else:
+                    current_layer.append(node)
+
+            layers.append(current_layer)
+            ops += layers
+
+    if justify == 'right':
+        dag_layers = []
+
+        for dag_layer in dag.layers():
+            dag_layers.append(dag_layer)
+
+        # Have to work from the end of the circuit
+        dag_layers.reverse()
+
+        # Dict per layer, keys are qubits and values are the gate
+        layer_dicts = [{}]
+
+        for dag_layer in dag_layers:
+
+            dag_instructions = dag_layer['graph'].get_op_nodes(data=True)
+
+            # sort into the order they were input
+            dag_instructions.sort(key=lambda tup: tup[0])
+            for (_, instruction) in dag_instructions:
+
+                gate_span = _get_gate_span(qregs, instruction)
+
+                added = False
+                for i in range(len(layer_dicts)):
+                    # iterate from the end
+                    curr_dict = layer_dicts[-1 - i]
+
+                    if any(index in curr_dict for index in gate_span):
+                        added = True
+
+                        if i == 0:
+                            new_dict = {}
+
+                            for index in gate_span:
+                                new_dict[index] = instruction
+                            layer_dicts.append(new_dict)
+                        else:
+                            curr_dict = layer_dicts[-i]
+                            for index in gate_span:
+                                curr_dict[index] = instruction
+
+                        break
+
+                if not added:
+                    for index in gate_span:
+                        layer_dicts[0][index] = instruction
+
+        # need to convert from dict format to layers
+        layer_dicts.reverse()
+        ops = [list(layer.values()) for layer in layer_dicts]
+
+    if reversebits:
+        qregs.reverse()
+        cregs.reverse()
+
+    return qregs, cregs, ops
+
+
 def _get_instructions(circuit, reversebits=False):
     """
     Given a circuit, return a tuple (qregs, cregs, ops) where
@@ -81,3 +210,22 @@ def _get_instructions(circuit, reversebits=False):
         cregs.reverse()
 
     return qregs, cregs, ops
+
+
+def _get_gate_span(qregs, instruction):
+    """Get the list of qubits drawing this gate would cover"""
+
+    min_index = len(qregs)
+    max_index = 0
+    for qreg in instruction['qargs']:
+        index = qregs.index(qreg)
+
+        if index < min_index:
+            min_index = index
+        if index > max_index:
+            max_index = index
+
+    if instruction['cargs']:
+        return qregs[min_index:]
+
+    return qregs[min_index:max_index + 1]
