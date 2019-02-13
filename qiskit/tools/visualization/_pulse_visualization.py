75afd1b48b52943426e5bf1f9245c9dcbcbd42c8
knzwnao <knzwnao@jp.ibm.com>
2019-02-13 10:44:27 +0900

    update pulse_drawer

--- qiskit/tools/visualization/_pulse_visualization.py
+++ qiskit/tools/visualization/_pulse_visualization.py
@@ -14,8 +14,10 @@ Visualization function for pulse envelope.
 import numpy as np
 from scipy.interpolate import CubicSpline
 
+from qiskit.exceptions import QiskitError
 
-def pulse_drawer(samples, duration, dt=None, interp_method='CubicSpline',
+
+def pulse_drawer(samples, duration, dt=None, interp_method='None',
                  filename=None, interactive=False,
                  dpi=150, nop=1000, size=(6, 5)):
     """Plot the interpolated envelope of pulse
@@ -37,6 +39,7 @@ def pulse_drawer(samples, duration, dt=None, interp_method='CubicSpline',
         matplotlib.figure: a matplotlib figure object for the pulse envelope
     Raises:
         ImportError: when the output methods requieres non-installed libraries.
+        QiskitError: when invalid interpolation method is specified.
     """
 
     try:
@@ -52,35 +55,38 @@ def pulse_drawer(samples, duration, dt=None, interp_method='CubicSpline',
 
     re_y = np.real(samples)
     im_y = np.imag(samples)
-    time = np.arange(0, duration + 1) * _dt
-
-    # pseudo-AWG output
-    _re_y = np.repeat(re_y, 2)
-    _im_y = np.repeat(im_y, 2)
-    _time = np.r_[time[0], np.repeat(time[1:-1], 2), time[-1]]
 
     image = plt.figure(figsize=size)
     ax0 = image.add_subplot(111)
 
-    ax0.plot(_time, _re_y, color='red',
-             linewidth=1.5, label='real part')
-    ax0.plot(_time, _im_y, color='blue',
-             linewidth=1.5, label='imaginary part')
-    ax0.scatter(x=time[:-1], y=re_y, color='red', marker='o')
-    ax0.scatter(x=time[:-1], y=im_y, color='blue', marker='o')
-
     if interp_method == 'CubicSpline':
-        # spline interpolation
+        # spline interpolation, use mid-point of dt
+        time = np.arange(0, duration + 1) * _dt + 0.5 * _dt
         cs_ry = CubicSpline(time[:-1], re_y)
         cs_iy = CubicSpline(time[:-1], im_y)
-        time_interp = np.linspace(0, duration * _dt, nop)
-        # plot
-        ax0.fill_between(x=time_interp, y1=cs_ry(time_interp),
-                         y2=np.zeros_like(time_interp),
-                         facecolors='red', alpha=0.1)
-        ax0.fill_between(x=time_interp, y1=cs_iy(time_interp),
-                         y2=np.zeros_like(time_interp),
-                         facecolors='blue', alpha=0.1)
+
+        _time = np.linspace(0, duration * _dt, nop)
+        _re_y = cs_ry(_time)
+        _im_y = cs_iy(_time)
+    elif interp_method == 'None':
+        # pseudo-DAC output
+        time = np.arange(0, duration + 1) * _dt
+
+        _time = np.r_[time[0], np.repeat(time[1:-1], 2), time[-1]]
+        _re_y = np.repeat(re_y, 2)
+        _im_y = np.repeat(im_y, 2)
+    else:
+        raise QiskitError('Invalid interpolation method "%s"' % interp_method)
+
+    # plot
+    ax0.fill_between(x=_time, y1=_re_y, y2=np.zeros_like(_time),
+                     facecolor='red', alpha=0.3,
+                     edgecolor='red', linewidth=1.5,
+                     label='real part')
+    ax0.fill_between(x=_time, y1=_im_y, y2=np.zeros_like(_time),
+                     facecolor='blue', alpha=0.3,
+                     edgecolor='blue', linewidth=1.5,
+                     label='imaginary part')
 
     ax0.set_xlim(0, max(time))
     ax0.grid(b=True, linestyle='-')
