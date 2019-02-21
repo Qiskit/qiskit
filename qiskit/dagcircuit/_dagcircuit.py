74a44227c01212118bb21d18e6e5402262df3f5d
Luciano <luciano@debian.org>
2019-02-21 11:54:47 -0500

    Revert #1806 (#1843)

--- qiskit/dagcircuit/_dagcircuit.py
+++ qiskit/dagcircuit/_dagcircuit.py
@@ -941,7 +941,7 @@ class DAGCircuit:
         # Now that we know the connections, delete node
         self.multi_graph.remove_node(node)
         # Iterate over nodes of input_circuit
-        for m in input_dag.multi_graph:
+        for m in nx.topological_sort(input_dag.multi_graph):
             md = input_dag.multi_graph.node[m]
             if md["type"] == "op":
                 # Insert a new node
