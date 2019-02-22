a2a47278f700ee157dddac8eb11ece38e7a0d64a
cameron <cameron@cmcgarry.co.uk>
2019-02-22 11:07:44 +0000

    Docs and setup for dict construction fn

--- qiskit/quantum_info/analyzation/average.py
+++ qiskit/quantum_info/analyzation/average.py
@@ -11,23 +11,38 @@
 def average_data(counts, observable):
     """Compute the mean value of an diagonal observable.
 
-    Takes in an observable in dictionary format and then
-    calculates the sum_i value(i) P(i) where value(i) is the value of
-    the observable for state i.
-
-    TODO: make the observable also include a matrix
+    Takes in a diagonal observable in dictionary, list or matrix format and then
+    calculates the sum_i value(i) P(i) where value(i) is the value of the
+    observable for state i.
 
     Args:
         counts (dict): a dict of outcomes from an experiment
-        observable (dict): The observable to be averaged over. As an example
-        ZZ on qubits equals {"00": 1, "11": 1, "01": -1, "10": -1}
+        observable (dict or matrix): The observable to be averaged over. As an
+        example ZZ on qubits equals {"00": 1, "11": 1, "01": -1, "10": -1}
 
     Returns:
         Double: Average of the observable
     """
+    if not isinstance(observable, dict):
+        observable = make_dict_observable(observable)
     temp = 0
     tot = sum(counts.values())
     for key in counts:
         if key in observable:
             temp += counts[key] * observable[key] / tot
     return temp
+
+def make_dict_observable(matrix_observable):
+    """Convert an observable in matrix form to dictionary form.
+
+    Takes in a diagonal observable as a matrix and converts it to a dictionary
+    form. Can also handle a list sorted of the diagonal elements.
+
+    Args:
+        observable (list): The observable to be converted to dicitonary form. As
+
+    Results:
+        A dictionary with all observable states as keys, and corresponding
+        values being the observed value for that state. 
+    """
+    raise NotImplementedError
