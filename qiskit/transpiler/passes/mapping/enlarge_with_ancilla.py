136f138a7ea29877c9c295ea35cffda085d6aa2d
Luciano Bello <luciano.bello@ibm.com>
2019-02-25 12:56:05 -0500

    enlarge with ancilla should update the layout

--- qiskit/transpiler/passes/mapping/enlarge_with_ancilla.py
+++ qiskit/transpiler/passes/mapping/enlarge_with_ancilla.py
@@ -57,8 +57,13 @@ class EnlargeWithAncilla(TransformationPass):
             if self.ancilla_name in dag.qregs:
                 save_prefix = QuantumRegister.prefix
                 QuantumRegister.prefix = self.ancilla_name
-                dag.add_qreg(QuantumRegister(num_idle_physical_qubits))
+                qreg = QuantumRegister(num_idle_physical_qubits)
+                dag.add_qreg(qreg)
                 QuantumRegister.prefix = save_prefix
             else:
-                dag.add_qreg(QuantumRegister(num_idle_physical_qubits, name=self.ancilla_name))
+                qreg = QuantumRegister(num_idle_physical_qubits, name=self.ancilla_name)
+                dag.add_qreg(qreg)
+        for index, idle_physical_bit in enumerate(self.layout.idle_physical_bits()):
+            self.layout[idle_physical_bit] = (qreg, index)
+        self.property_set['layout'] = self.layout
         return dag
