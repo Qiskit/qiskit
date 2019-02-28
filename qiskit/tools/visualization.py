598605e646d5dd7bb80276a32ce8f85569576667
Matthew Treinish <mtreinish@kortar.org>
2019-02-28 09:10:39 -0500

    Fix lint
    Sigh pylint, raising an error for both using a wildcard import and not
    using all the imports from the wild card.

--- qiskit/tools/visualization.py
+++ qiskit/tools/visualization.py
@@ -5,4 +5,7 @@
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
 
+# pylint: disable=unused-wildcard-import,missing-docstring,wildcard-import
+
+# NOTE(mtreinish): Import public api here to maintain backwards compat
 from qiskit.visualization import *
