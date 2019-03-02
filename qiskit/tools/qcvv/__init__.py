fbbafdd015d9bf6707e8d2524c4bf98f40d12d61
Ali Javadi <ajavadia@princeton.edu>
2019-03-02 16:19:31 -0500

    deprecate qcvv package

---
+++ qiskit/tools/qcvv/__init__.py
@@ -0,0 +1,5 @@
+import warnings
+
+warnings.warn('The qiskit.tools.qcvv package is deprecated. '
+              'Please use qiskit-ignis available on PIP for similar '
+              'functionality and more.', DeprecationWarning)
