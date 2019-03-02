bf4aeb35cf843cb66704abc4bf8aa7d517898663
Toshinari Itoko <itoko@jp.ibm.com>
2019-03-02 07:15:54 -0500

    wip: look for better memory interface

--- qiskit/pulse/channels/__init__.py
+++ qiskit/pulse/channels/__init__.py
@@ -9,8 +9,8 @@
 
 from .backend_channels import OutputChannel, AcquireChannel, SnapshotChannel
 from .channel_factory import create_channel
-from .channel_register import ChannelRegister, OutputChannelRegister
 from .channel_memory import ChannelMemory
+from .channel_register import ChannelRegister, OutputChannelRegister
 from .output_channels import DriveChannel, ControlChannel, MeasureChannel
 from .pulse_channel import PulseChannel
 
--- qiskit/pulse/channels/channel_memory.py
+++ qiskit/pulse/channels/channel_memory.py
@@ -9,17 +9,54 @@
 Memory of channel registers.
 """
 from qiskit.providers import BaseBackend
+from qiskit.pulse import ChannelsError
+from .backend_channels import AcquireChannel, SnapshotChannel
+from .channel_register import ChannelRegister, OutputChannelRegister
+from .output_channels import DriveChannel, ControlChannel, MeasureChannel
 
 
 class ChannelMemory:    # TODO: better name?
     """Implement a channel memory."""
 
     def __init__(self, backend: BaseBackend = None):
-        self.drive = None
-        self.control = None
-        self.measure = None
-        self.acquire = None
-        self.snapshot = None
+        self._drive = None
+        self._control = None
+        self._measure = None
+        self._acquire = None
+        self._snapshot = None
         if backend:
             # TODO create default channel registers
             pass
+
+    def register(self, reg: ChannelRegister):
+        """
+        Overwrite the register of the same channel as `reg`.
+        Args:
+            reg:
+
+        Returns:
+
+        """
+        cls = reg.channel_cls
+        if cls == DriveChannel:
+            self._drive = reg
+        elif cls == ControlChannel:
+            self._control = reg
+        elif cls == MeasureChannel:
+            self._measure = reg
+        elif cls == AcquireChannel:
+            self._acquire = reg
+        elif cls == SnapshotChannel:
+            self._snapshot = reg
+        else:
+            raise ChannelsError("Unknown channel: %s", cls.__name__)
+
+    @property
+    def drive(self) -> OutputChannelRegister:
+        return self._drive
+
+    @drive.setter
+    def drive(self, reg):
+        raise ChannelsError("Cannot set directly, use 'register()' to change")
+
+
