07bf54b124da9a635207c7e5f3c6f98b01dc6295
Matthew Treinish <mtreinish@kortar.org>
2019-03-08 13:43:23 -0500

    Disable method name length enforcement for test code

--- test/python/transpiler/test_transpile.py
+++ test/python/transpiler/test_transpile.py
@@ -5,7 +5,7 @@
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
 
-# pylint: disable=redefined-builtin,invalid-name
+# pylint: disable=redefined-builtin
 
 """Tests basic functionality of the transpile function"""
 
