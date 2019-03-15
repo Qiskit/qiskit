3da34e4624ed25f5b73345c142cd0a991db2898c
Matthew Treinish <mtreinish@kortar.org>
2019-03-15 11:41:34 +0800

    Add __qiskit_version__ to list all package versions (#1885)
    * Add __qiskit_version__ to list all package versions
    
    This commit adds a new top level attribute to the qiskit package,
    __qiskit_version__ which stores a dict of all the qiskit package
    versions that are installed on the system. This is an alternative to the
    standard __version__ attribute which will only return the value of the
    qiskit-terra package because it is the source of the qiskit namespace.
    
    Fixes Qiskit/qiskit#46
    
    * Add changelog

--- qiskit/version.py
+++ qiskit/version.py
@@ -8,8 +8,46 @@
 """Contains the terra version."""
 
 import os
+import subprocess
+import sys
 
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 with open(os.path.join(ROOT_DIR, "VERSION.txt"), "r") as version_file:
     __version__ = version_file.read().strip()
+
+
+def _get_qiskit_versions():
+    cmd = [sys.executable, '-m', 'pip', 'freeze']
+    reqs = subprocess.check_output(cmd)
+    reqs_dict = {}
+    for req in reqs.split():
+        req_parts = req.decode().split('==')
+        if len(req_parts) == 1 and req_parts[0].startswith('git'):
+            if 'qiskit' in req_parts[0]:
+                package = req_parts[0].split('#egg=')[1]
+                sha = req_parts[0].split('@')[-1].split('#')[0]
+                reqs_dict[package] = 'dev-' + sha
+            continue
+        elif len(req_parts) == 1:
+            continue
+        reqs_dict[req_parts[0]] = req_parts[1]
+    out_dict = {}
+    # Dev/Egg _ to - conversion
+    for package in ['qiskit_terra', 'qiskit_ignis', 'qiskit_aer',
+                    'qiskit_ibmq_provider', 'qiskit_aqua']:
+        if package in reqs_dict:
+            out_dict[package.replace('_', '-')] = reqs_dict[package]
+
+    for package in ['qiskit', 'qiskit-terra', 'qiskit-ignis', 'qiskit-aer',
+                    'qiskit-ibmq-provider', 'qiskit-aqua']:
+        if package in out_dict:
+            continue
+        if package in reqs_dict:
+            out_dict[package] = reqs_dict[package]
+        else:
+            out_dict[package] = None
+    return out_dict
+
+
+__qiskit_version__ = _get_qiskit_versions()
