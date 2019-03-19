08b076170a9cdc5dd6cc101f8971aed4bf3b9b47
Luciano Bello <luciano.bello@ibm.com>
2019-03-19 13:29:19 -0400

    size pass

---
+++ qiskit/transpiler/passes/width.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2019, IBM.
+#
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree.
+
+""" An analysis pass for calculated the width of a DAG circuit.
+"""
+from qiskit.transpiler.basepasses import AnalysisPass
+
+
+class Width(AnalysisPass):
+    """ An analysis pass for calculated the width of a DAG circuit.
+    """
+    def run(self, dag):
+        dag.property_set['width'] = dag.width()
