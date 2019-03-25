16db24c59a23670b70ffc9fe5bcc8f2d4fd8b4fb
Paul Nation <nonhermitian@gmail.com>
2019-03-25 10:40:18 -0400

    update size computation

--- qiskit/circuit/quantumcircuit.py
+++ qiskit/circuit/quantumcircuit.py
@@ -354,12 +354,16 @@ class QuantumCircuit:
                                             justify=justify)
 
     def size(self):
-        """Returns total number of operations in circuit.
+        """Returns total number of gate operations in circuit.
 
         Returns:
-            int: Total number of operations.
+            int: Total number of gate operations.
         """
-        return len(self.data)
+        gate_ops = 0
+        for item in self.data:
+            if item.name not in ['barrier', 'snapshot']:
+                gate_ops += 1
+        return gate_ops
 
     def depth(self):
         """Return circuit depth (i.e. length of critical path).
