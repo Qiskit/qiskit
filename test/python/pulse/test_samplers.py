9d6144d96ea3754b3c2cd49c8bac3233ba4d7aee
Thomas Alexander <talexander@ibm.com>
2019-03-28 14:40:47 -0400

    Added test module and case for samplers.

---
+++ test/python/pulse/test_samplers.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2019, IBM.
+#
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree.
+
+# pylint: disable=invalid-name,unexpected-keyword-arg
+
+"""Tests pulse function samplers."""
+
+import unittest
+import numpy as np
+
+from qiskit.test import QiskitTestCase
+
+
+class TestSampler(QiskitTestCase):
+    pass
