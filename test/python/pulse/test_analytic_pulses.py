1ff482b2c9259277757e38e363bd71eea1eeac5e
Thomas Alexander <talexander@ibm.com>
2019-03-29 17:44:32 -0400

    Added test pulse function module.

---
+++ test/python/pulse/test_analytic_pulses.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2019, IBM.
+#
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree.
+
+# pylint: disable=invalid-name, unexpected-keyword-arg, no-value-for-parameter
+
+"""Tests analytic pulse functions."""
+
+import numpy as np
+
+from qiskit.test import QiskitTestCase
+import qiskit.pulse.pulses.analytic as analytic
+
+
+class TestAnalyticPulses(QiskitTestCase):
+    pass
