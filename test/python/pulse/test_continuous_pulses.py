77bb4becec020e1ef6ebaaab0e6f7e9725ca21ba
Thomas Alexander <talexander@ibm.com>
2019-04-01 13:25:25 -0400

    Update tests.

--- test/python/pulse/test_continuous_pulses.py
+++ test/python/pulse/test_continuous_pulses.py
@@ -37,6 +37,7 @@ class TestContinuousPulses(QiskitTestCase):
         square_arr_phased = continuous.square(np.linspace(0, 10, 100), amp=0.5,
                                               period=5, phase=np.pi/2)
         self.assertAlmostEqual(square_arr[0], 0.5)
+        self.assertAlmostEqual(square_arr[1]-square_arr[0], 0.0)
         self.assertAlmostEqual(square_arr[25], -0.5)
         self.assertAlmostEqual(square_arr_phased[0], -0.5)
         # Assert bounded between -amp and amp
@@ -45,53 +46,71 @@ class TestContinuousPulses(QiskitTestCase):
 
     def test_sawtooth(self):
         """Test sawtooth wave."""
-        sawtooth_arr = continuous.sawtooth(np.linspace(0, 10, 101), amp=0.5, period=5)
+        amp = 0.5
+        period = 5
+        times, dt = np.linspace(0, 10, 101, retstep=True)
+        sawtooth_arr = continuous.sawtooth(times, amp=amp, period=period)
         # with new phase
-        sawtooth_arr_phased = continuous.sawtooth(np.linspace(0, 10, 101), amp=0.5,
-                                                  period=5, phase=np.pi/2)
+        sawtooth_arr_phased = continuous.sawtooth(times, amp=amp,
+                                                  period=period, phase=np.pi/2)
         self.assertAlmostEqual(sawtooth_arr[0], 0.0)
+        # test slope
+        self.assertAlmostEqual((sawtooth_arr[1]-sawtooth_arr[0])/dt, 2*amp/period)
         self.assertAlmostEqual(sawtooth_arr[24], 0.48)
         self.assertAlmostEqual(sawtooth_arr[50], 0.)
-        self.assertAlmostEqual(sawtooth_arr[75], -0.5)
-        self.assertAlmostEqual(sawtooth_arr_phased[0], -0.5)
+        self.assertAlmostEqual(sawtooth_arr[75], -amp)
+        self.assertAlmostEqual(sawtooth_arr_phased[0], -amp)
         # Assert bounded between -amp and amp
-        self.assertTrue(np.all((-0.5 <= sawtooth_arr) & (sawtooth_arr <= 0.5)))
+        self.assertTrue(np.all((-amp <= sawtooth_arr) & (sawtooth_arr <= amp)))
         self.assertEqual(len(sawtooth_arr), 101)
 
     def test_triangle(self):
         """Test triangle wave."""
-        triangle_arr = continuous.triangle(np.linspace(0, 10, 101), amp=0.5, period=5)
+        amp = 0.5
+        period = 5
+        times, dt = np.linspace(0, 10, 101, retstep=True)
+        triangle_arr = continuous.triangle(times, amp=amp, period=period)
         # with new phase
-        triangle_arr_phased = continuous.triangle(np.linspace(0, 10, 101), amp=0.5,
-                                                  period=5, phase=np.pi/2)
+        triangle_arr_phased = continuous.triangle(times, amp=amp,
+                                                  period=period, phase=np.pi/2)
         self.assertAlmostEqual(triangle_arr[0], 0.0)
+        # test slope
+        self.assertAlmostEqual((triangle_arr[1]-triangle_arr[0])/dt, 4*amp/period)
         self.assertAlmostEqual(triangle_arr[12], 0.48)
         self.assertAlmostEqual(triangle_arr[13], 0.48)
         self.assertAlmostEqual(triangle_arr[50], 0.)
-        self.assertAlmostEqual(triangle_arr_phased[0], 0.5)
+        self.assertAlmostEqual(triangle_arr_phased[0], amp)
         # Assert bounded between -amp and amp
-        self.assertTrue(np.all((-0.5 <= triangle_arr) & (triangle_arr <= 0.5)))
+        self.assertTrue(np.all((-amp <= triangle_arr) & (triangle_arr <= amp)))
         self.assertEqual(len(triangle_arr), 101)
 
     def test_cos(self):
         """Test cosine wave."""
-        cos_arr = continuous.cos(np.linspace(0, 10, 101), amp=0.5, freq=1/5)
+        amp = 0.5
+        period = 5
+        freq = 1/period
+        times, dt = np.linspace(0, 10, 101, retstep=True)
+        cos_arr = continuous.cos(np.linspace(0, 10, 101), amp=amp, freq=freq)
         # with new phase
-        cos_arr_phased = continuous.cos(np.linspace(0, 10, 101), amp=0.5,
-                                        freq=1/5, phase=np.pi/2)
+        cos_arr_phased = continuous.cos(times, amp=amp,
+                                        freq=freq, phase=np.pi/2)
         # Assert starts at 1
-        self.assertAlmostEqual(cos_arr[0], 0.5)
+        self.assertAlmostEqual(cos_arr[0], amp)
         self.assertAlmostEqual(cos_arr[6], 0.3644, places=2)
-        self.assertAlmostEqual(cos_arr[25], -0.5)
-        self.assertAlmostEqual(cos_arr[50], 0.5)
+        self.assertAlmostEqual(cos_arr[25], -amp)
+        self.assertAlmostEqual(cos_arr[50], amp)
         self.assertAlmostEqual(cos_arr_phased[0], 0.0)
         # Assert bounded between -amp and amp
-        self.assertTrue(np.all((-0.5 <= cos_arr) & (cos_arr <= 0.5)))
+        self.assertTrue(np.all((-amp <= cos_arr) & (cos_arr <= amp)))
         self.assertEqual(len(cos_arr), 101)
 
     def test_sin(self):
         """Test sine wave."""
-        sin_arr = continuous.sin(np.linspace(0, 10, 101), amp=0.5, freq=1/5)
+        amp = 0.5
+        period = 5
+        freq = 1/period
+        times, dt = np.linspace(0, 10, 101, retstep=True)
+        sin_arr = continuous.sin(times, amp=amp, freq=freq)
         # with new phase
         sin_arr_phased = continuous.sin(np.linspace(0, 10, 101), amp=0.5,
                                         freq=1/5, phase=np.pi/2)
