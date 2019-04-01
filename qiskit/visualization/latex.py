e381e5c985c5c3c0170c78cb292429f8d87b9569
Matthew Treinish <mtreinish@kortar.org>
2019-04-01 08:45:48 -0400

    Fix lint

--- qiskit/visualization/latex.py
+++ qiskit/visualization/latex.py
@@ -6,6 +6,7 @@
 # the LICENSE.txt file in the root directory of this source tree.
 
 # pylint: disable=invalid-name,anomalous-backslash-in-string,missing-docstring
+# pylint: disable=consider-using-enumerate
 
 """latex circuit visualization backends."""
 
