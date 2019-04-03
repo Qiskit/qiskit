8caefcf244f3f4d8b95d7b2d72bc5cdd1c621232
Erick Winston <ewinston@us.ibm.com>
2019-04-03 12:24:55 -0400

    start variable parameters

---
+++ qiskit/circuit/variableparameter.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2019, IBM.
+#
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree.
+"""
+Object for inserting varaible parameters into circuits so the variable may be
+changed without recompiling.
+"""
+
+class VariableParameter
+    def __init__(self, name):
+        self._name = name
+    
