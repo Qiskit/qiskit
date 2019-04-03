1ucian0/qiskit-terra-fuzz

use a list of Gate for a Gate.definition, decompose based on that

qiskit/transpiler/passes/decompose.py
@@ -5,9 +5,10 @@
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
 
-"""Pass for decompose a gate in a circuit."""
+"""Pass for one layer of decomposing the gates in a circuit."""
 
 from qiskit.transpiler.basepasses import TransformationPass
+from qiskit.dagcircuit import DAGCircuit
 
 
 class Decompose(TransformationPass):
@@ -33,10 +34,13 @@ class Decompose(TransformationPass):
         """
         # Walk through the DAG and expand each non-basis node
         for node_id, current_node in dag.op_nodes(self.gate, data=True):
-            decomposition_rules = current_node["op"].decompositions()
-
-            # TODO: allow choosing other possible decompositions
-            decomposition_dag = decomposition_rules[0]
-
-            dag.substitute_node_with_dag(node_id, decomposition_dag)
+            # TODO: allow choosing among multiple decomposition rules
+            rule = current_node["op"].definition
+            # hacky way to build a dag on the same register as the rule is defined
+            # TODO: need anonymous rules to address wires by index
+            decomposition = DAGCircuit()
+            decomposition.add_qreg(rule[0][1][0][0])
+            for inst in rule:
+                decomposition.apply_operation_back(*inst)
+            dag.substitute_node_with_dag(node_id, decomposition)
         return dag

qiskit/transpiler/passes/unroll_3q_or_more.py
@@ -8,6 +8,7 @@
 """Pass for decomposing 3q (or more) gates into 2q or 1q gates."""
 
 from qiskit.transpiler.basepasses import TransformationPass
+from qiskit.dagcircuit import DAGCircuit
 
 
 class Unroll3qOrMore(TransformationPass):
@@ -25,10 +26,15 @@ class Unroll3qOrMore(TransformationPass):
             DAGCircuit: output dag with maximum node degrees of 2
         """
         for node_id, node_data in dag.threeQ_or_more_nodes():
-            decomposition_rules = node_data["op"].decompositions()
-
             # TODO: allow choosing other possible decompositions
-            decomposition_dag = self.run(decomposition_rules[0])  # recursively unroll
+            rule = node_data["op"].definition
+            # hacky way to build a dag on the same register as the rule is defined
+            # TODO: need anonymous rules to address wires by index
+            decomposition = DAGCircuit()
+            decomposition.add_qreg(rule[0][1][0][0])
+            for inst in rule:
+                decomposition.apply_operation_back(*inst)
+            decomposition = self.run(decomposition)  # recursively unroll
 
-            dag.substitute_node_with_dag(node_id, decomposition_dag)
+            dag.substitute_node_with_dag(node_id, decomposition)
         return dag

qiskit/transpiler/passes/unroller.py
@@ -8,6 +8,7 @@
 """Pass for unrolling a circuit to a given basis."""
 
 from qiskit.transpiler.basepasses import TransformationPass
+from qiskit.dagcircuit import DAGCircuit
 from qiskit.exceptions import QiskitError
 
 
@@ -45,14 +46,20 @@ class Unroller(TransformationPass):
                 continue
 
             # TODO: allow choosing other possible decompositions
-            decomposition_rules = current_node["op"].decompositions()
-
-            if not decomposition_rules:
+            rule = current_node["op"].definition
+            if not rule:
                 raise QiskitError("Cannot unroll the circuit to the given basis, %s. "
                                   "The current node being expanded, %s, "
                                   "is defined in terms of an invalid basis." %
                                   (str(self.basis), current_node["op"].name))
 
-            decomposition_dag = self.run(decomposition_rules[0])  # recursively unroll gates
-            dag.substitute_node_with_dag(node_id, decomposition_dag)
+            # hacky way to build a dag on the same register as the rule is defined
+            # TODO: need anonymous rules to address wires by index
+            decomposition = DAGCircuit()
+            decomposition.add_qreg(rule[0][1][0][0])
+            for inst in rule:
+                decomposition.apply_operation_back(*inst)
+
+            unrolled_dag = self.run(decomposition)  # recursively unroll gates
+            dag.substitute_node_with_dag(node_id, unrolled_dag)
         return dag

Qiskit/qiskit

qiskit/transpiler/passes/decompose.py
@@ -5,9 +5,10 @@
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
 
-"""Pass for decompose a gate in a circuit."""
+"""Pass for one layer of decomposing the gates in a circuit."""
 
 from qiskit.transpiler.basepasses import TransformationPass
+from qiskit.dagcircuit import DAGCircuit
 
 
 class Decompose(TransformationPass):
@@ -32,11 +33,14 @@ class Decompose(TransformationPass):
             DAGCircuit: output dag where gate was expanded.
         """
         # Walk through the DAG and expand each non-basis node
-
         for node in dag.op_nodes(self.gate):
-            decomposition_rules = node.op.decompositions()
-            # TODO: allow choosing other possible decompositions
-            decomposition_dag = decomposition_rules[0]
-
-            dag.substitute_node_with_dag(node, decomposition_dag)
+            # TODO: allow choosing among multiple decomposition rules
+            rule = current_node["op"].definition
+            # hacky way to build a dag on the same register as the rule is defined
+            # TODO: need anonymous rules to address wires by index
+            decomposition = DAGCircuit()
+            decomposition.add_qreg(rule[0][1][0][0])
+            for inst in rule:
+                decomposition.apply_operation_back(*inst)
+            dag.substitute_node_with_dag(node, decomposition)
         return dag

qiskit/transpiler/passes/unroll_3q_or_more.py
@@ -8,6 +8,7 @@
 """Pass for decomposing 3q (or more) gates into 2q or 1q gates."""
 
 from qiskit.transpiler.basepasses import TransformationPass
+from qiskit.dagcircuit import DAGCircuit
 
 
 class Unroll3qOrMore(TransformationPass):
@@ -25,10 +26,14 @@ class Unroll3qOrMore(TransformationPass):
             DAGCircuit: output dag with maximum node degrees of 2
         """
         for node in dag.threeQ_or_more_nodes():
-            decomposition_rules = node.op.decompositions()
-
             # TODO: allow choosing other possible decompositions
-            decomposition_dag = self.run(decomposition_rules[0])  # recursively unroll
-
-            dag.substitute_node_with_dag(node, decomposition_dag)
+            rule = node_data["op"].definition
+            # hacky way to build a dag on the same register as the rule is defined
+            # TODO: need anonymous rules to address wires by index
+            decomposition = DAGCircuit()
+            decomposition.add_qreg(rule[0][1][0][0])
+            for inst in rule:
+                decomposition.apply_operation_back(*inst)
+            decomposition = self.run(decomposition)  # recursively unroll
+            dag.substitute_node_with_dag(node, decomposition)
         return dag

qiskit/transpiler/passes/unroller.py
@@ -8,6 +8,7 @@
 """Pass for unrolling a circuit to a given basis."""
 
 from qiskit.transpiler.basepasses import TransformationPass
+from qiskit.dagcircuit import DAGCircuit
 from qiskit.exceptions import QiskitError
 
 
@@ -45,15 +46,20 @@ class Unroller(TransformationPass):
                 continue
 
             # TODO: allow choosing other possible decompositions
-            decomposition_rules = node.op.decompositions()
-
-            if not decomposition_rules:
+            rule = current_node["op"].definition
+            if not rule:
                 raise QiskitError("Cannot unroll the circuit to the given basis, %s. "
                                   "The current node being expanded, %s, "
                                   "is defined in terms of an invalid basis." %
                                   (str(self.basis), node.op.name))
 
-            decomposition_dag = self.run(decomposition_rules[0])  # recursively unroll gates
-            dag.substitute_node_with_dag(node=node, input_dag=decomposition_dag)
+            # hacky way to build a dag on the same register as the rule is defined
+            # TODO: need anonymous rules to address wires by index
+            decomposition = DAGCircuit()
+            decomposition.add_qreg(rule[0][1][0][0])
+            for inst in rule:
+                decomposition.apply_operation_back(*inst)
 
+            unrolled_dag = self.run(decomposition)  # recursively unroll gates
+            dag.substitute_node_with_dag(node, decomposition)
         return dag