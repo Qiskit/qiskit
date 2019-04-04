8b1c08be2f0deda494fd90fab80f475f2ea2ccc5
Ali Javadi-Abhari <ajavadia@users.noreply.github.com>
2019-04-04 13:16:16 -0400

    Update qiskit/transpiler/passes/size.py
    Co-Authored-By: 1ucian0 <luciano@debian.org>

--- qiskit/transpiler/passes/size.py
+++ qiskit/transpiler/passes/size.py
@@ -11,7 +11,7 @@ from qiskit.transpiler.basepasses import AnalysisPass
 
 
 class Size(AnalysisPass):
-    """ An analysis pass for calculated the size of a DAG circuit.
+    """ An analysis pass for calculating the size of a DAG circuit.
     """
 
     def run(self, dag):
