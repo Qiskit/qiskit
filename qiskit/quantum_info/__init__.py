2c966a51671305923dca4b63dea86cd683e85e8d
Ali Javadi <ajavadia@princeton.edu>
2019-04-10 19:17:45 -0400

    add to __init__ of quantum_info sub packages

--- qiskit/quantum_info/__init__.py
+++ qiskit/quantum_info/__init__.py
@@ -5,7 +5,7 @@
 # This source code is licensed under the Apache License, Version 2.0 found in
 # the LICENSE.txt file in the root directory of this source tree.
 
-"""Quantum Information  methods."""
+"""Quantum Information methods."""
 
 from .operators.operator import Operator
 from .operators.unitary import Unitary
