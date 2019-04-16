78752933b987bdf61bb3efc61a5529d200359f27
Matthew Treinish <mtreinish@kortar.org>
2019-04-16 13:58:19 -0400

    Add dissasembler to dissassemble a compiled qobj
    This commit adds a new dissassembler module that has a single
    dissasemble function. This is used to take an input qobj and return the
    circuits, run_config, and user headers which are stored in it.
    
    Fixes #1952

---
+++ qiskit/compiler/dissasembler.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2019, IBM.
+#
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree.
+
+"""Disassemble function for a qobj into a list of circuits and it's config"""
+
+from qiskit.converters import qobj_to_circuits
+
+
+def disassemble(qobj):
+    """Dissasemble a qobj and return the circuits, run_config, and user header
+
+    Args:
+        qobj (Qobj): The input qobj object to dissasemble
+    Returns:
+        circuits list: A list of quantum circuits
+        run_config (dict): The dist of the run config
+        user_qobj_header (dict): The dict of any user headers in the qobj
+
+    """
+    run_config = qobj.config.to_dict()
+    user_qobj_header = qobj.header.to_dict()
+    circuits = qobj_to_circuits(qobj)
+
+    return circuits, run_config, user_qobj_header
