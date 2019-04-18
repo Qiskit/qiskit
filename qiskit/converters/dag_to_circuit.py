de2328a32c75b05df1a0389ae11eda1cd2c0310b
Matthew Treinish <mtreinish@kortar.org>
2019-04-18 09:52:28 -0400

    Remove node type check that's not needed with topological_op_nodes()

--- qiskit/converters/dag_to_circuit.py
+++ qiskit/converters/dag_to_circuit.py
@@ -37,54 +37,52 @@ def dag_to_circuit(dag):
     circuit = QuantumCircuit(*qregs.values(), *cregs.values(), name=name)
 
     for node in dag.topological_op_nodes():
-        if node.type == 'op':
-            qubits = []
-            for qubit in node.qargs:
-                qubits.append(qregs[qubit[0].name][qubit[1]])
+        qubits = []
+        for qubit in node.qargs:
+            qubits.append(qregs[qubit[0].name][qubit[1]])
 
-            clbits = []
-            for clbit in node.cargs:
-                clbits.append(cregs[clbit[0].name][clbit[1]])
+        clbits = []
+        for clbit in node.cargs:
+            clbits.append(cregs[clbit[0].name][clbit[1]])
 
-            # Get arguments for classical control (if any)
-            if node.condition is None:
-                control = None
-            else:
-                control = (node.condition[0], node.condition[1])
+        # Get arguments for classical control (if any)
+        if node.condition is None:
+            control = None
+        else:
+            control = (node.condition[0], node.condition[1])
 
-            def duplicate_instruction(inst):
-                """Create a fresh instruction from an input instruction."""
-                if issubclass(inst.__class__,
-                              Instruction) and inst.__class__ not in [
-                                  Instruction, Gate]:
-                    if inst.name == 'barrier':
-                        new_inst = inst.__class__(inst.num_qubits)
-                    elif inst.name == 'initialize':
-                        params = getattr(inst, 'params', [])
-                        new_inst = inst.__class__(params)
-                    elif inst.name == 'snapshot':
-                        label = inst.params[0]
-                        snap_type = inst.params[1]
-                        new_inst = inst.__class__(inst.num_qubits,
-                                                  inst.num_clbits,
-                                                  label, snap_type)
-                    else:
-                        params = getattr(inst, 'params', [])
-                        new_inst = inst.__class__(*params)
+        def duplicate_instruction(inst):
+            """Create a fresh instruction from an input instruction."""
+            if issubclass(inst.__class__,
+                          Instruction) and inst.__class__ not in [
+                              Instruction, Gate]:
+                if inst.name == 'barrier':
+                    new_inst = inst.__class__(inst.num_qubits)
+                elif inst.name == 'initialize':
+                    params = getattr(inst, 'params', [])
+                    new_inst = inst.__class__(params)
+                elif inst.name == 'snapshot':
+                    label = inst.params[0]
+                    snap_type = inst.params[1]
+                    new_inst = inst.__class__(inst.num_qubits,
+                                              inst.num_clbits,
+                                              label, snap_type)
                 else:
-                    if isinstance(inst, Gate):
-                        new_inst = Gate(inst.name, inst.num_qubits,
-                                        inst.params)
-                    else:
-                        new_inst = Instruction(name=inst.name,
-                                               num_qubits=inst.num_qubits,
-                                               num_clbits=inst.num_clbits,
-                                               params=inst.params)
-                    new_inst.definition = inst.definition
-                return new_inst
+                    params = getattr(inst, 'params', [])
+                    new_inst = inst.__class__(*params)
+            else:
+                if isinstance(inst, Gate):
+                    new_inst = Gate(inst.name, inst.num_qubits,
+                                    inst.params)
+                else:
+                    new_inst = Instruction(name=inst.name,
+                                           num_qubits=inst.num_qubits,
+                                           num_clbits=inst.num_clbits,
+                                           params=inst.params)
+                new_inst.definition = inst.definition
+            return new_inst
 
-            inst = duplicate_instruction(node.op)
-            inst.control = control
-            circuit.append(inst, qubits, clbits)
-
+        inst = duplicate_instruction(node.op)
+        inst.control = control
+        circuit.append(inst, qubits, clbits)
     return circuit
