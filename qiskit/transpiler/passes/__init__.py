0f755e4dea115bc02968aa340eb7e9a846616c46
Ali Javadi-Abhari <ajavadia@users.noreply.github.com>
2019-04-18 11:40:27 -0400

    remove consolidate blocks pass (#2153)
    * remove consolidate blocks pass
    
    * remove test too
    
    * adding chriseclectic as reviewer

--- qiskit/transpiler/passes/__init__.py
+++ qiskit/transpiler/passes/__init__.py
@@ -26,7 +26,6 @@ from .optimize_swap_before_measure import OptimizeSwapBeforeMeasure
 from .commutative_cancellation import CommutativeCancellation
 from .remove_reset_in_zero_state import RemoveResetInZeroState
 from .collect_2q_blocks import Collect2qBlocks
-from .consolidate_blocks import ConsolidateBlocks
 from .mapping.barrier_before_final_measurements import BarrierBeforeFinalMeasurements
 from .mapping.check_map import CheckMap
 from .mapping.check_cnot_direction import CheckCnotDirection
