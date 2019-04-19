9072d61399108a358fc0ad6e74bda55b78ac68cb
Matthew Treinish <mtreinish@kortar.org>
2019-04-19 09:33:27 -0400

    Deprecate qobj_to_circuits

--- qiskit/compiler/dissasembler.py
+++ qiskit/compiler/dissasembler.py
@@ -12,6 +12,66 @@
 from qiskit.converters import qobj_to_circuits
 
 
+# TODO: This is broken for conditionals. Will fix after circuits_2_qobj pr
+def _ experiments_to_ciruits(qobj):
+    """Return a list of QuantumCircuit object(s) from a qobj
+
+    Args:
+        qobj (Qobj): The Qobj object to convert to QuantumCircuits
+    Returns:
+        list: A list of QuantumCircuit objects from the qobj
+
+    """
+    if qobj.experiments:
+        circuits = []
+        for x in qobj.experiments:
+            quantum_registers = [QuantumRegister(i[1], name=i[0])
+                                 for i in x.header.qreg_sizes]
+            classical_registers = [ClassicalRegister(i[1], name=i[0])
+                                   for i in x.header.creg_sizes]
+            circuit = QuantumCircuit(*quantum_registers,
+                                     *classical_registers,
+                                     name=x.header.name)
+            qreg_dict = {}
+            creg_dict = {}
+            for reg in quantum_registers:
+                qreg_dict[reg.name] = reg
+            for reg in classical_registers:
+                creg_dict[reg.name] = reg
+            for i in x.instructions:
+                instr_method = getattr(circuit, i.name)
+                qubits = []
+                try:
+                    for qubit in i.qubits:
+                        qubit_label = x.header.qubit_labels[qubit]
+                        qubits.append(
+                            qreg_dict[qubit_label[0]][qubit_label[1]])
+                except Exception:  # pylint: disable=broad-except
+                    pass
+                clbits = []
+                try:
+                    for clbit in i.memory:
+                        clbit_label = x.header.clbit_labels[clbit]
+                        clbits.append(
+                            creg_dict[clbit_label[0]][clbit_label[1]])
+                except Exception:  # pylint: disable=broad-except
+                    pass
+                params = []
+                try:
+                    params = i.params
+                except Exception:  # pylint: disable=broad-except
+                    pass
+                if i.name in ['snapshot']:
+                    instr_method(*params)
+                elif i.name == 'initialize':
+                    instr_method(params, qubits)
+                else:
+                    instr_method(*params, *qubits, *clbits)
+            circuits.append(circuit)
+        return circuits
+    return None
+
+
 def disassemble(qobj):
     """Dissasemble a qobj and return the circuits, run_config, and user header
 
@@ -25,6 +85,6 @@ def disassemble(qobj):
     """
     run_config = qobj.config.to_dict()
     user_qobj_header = qobj.header.to_dict()
-    circuits = qobj_to_circuits(qobj)
+    circuits = _experiments_to_circuits(qobj)
 
     return circuits, run_config, user_qobj_header
