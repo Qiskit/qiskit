edc96e5f0581ab6aee40013e7a8e3c6c50feda8e
Luciano <luciano@debian.org>
2019-04-19 15:54:42 -0400

    Preset passmanagers (#2163)
    * dynamic layout
    
    * _
    
    * move the default pass manager to its own file
    
    * docstrings

---
+++ qiskit/transpiler/preset_passmanagers/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2019, IBM.
+#
+# This source code is licensed under the Apache License, Version 2.0 found in
+# the LICENSE.txt file in the root directory of this source tree.
+
+"""Preset PassManager flows."""
+
+from .default import default_pass_manager, default_pass_manager_simulator
