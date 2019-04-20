5ecb33ff7afa0d5146450e0151f7c4ac0ad0895d
Ali Javadi <ajavadia@princeton.edu>
2019-04-20 15:47:22 -0400

    parse transpile options from kwarg or transpile_config or backend

--- qiskit/compiler/transpiler.py
+++ qiskit/compiler/transpiler.py
@@ -6,33 +6,27 @@
 # the LICENSE.txt file in the root directory of this source tree.
 
 """Circuit transpile function"""
+import warnings
 
-import logging
+from qiskit.circuit import QuantumCircuit
+from qiskit.converters import circuit_to_dag, dag_to_circuit
+from qiskit.mapper import Layout, CouplingMap
+from qiskit.tools.parallel import parallel_map
+from qiskit.transpiler.exceptions import TranspilerError
+from qiskit.transpiler.preset_passmanagers import (default_pass_manager_simulator,
+                                                   default_pass_manager)
 
-from qiskit import transpiler
-from qiskit.mapper import Layout
-
-
-logger = logging.getLogger(__name__)
-
-
-def transpile(circuits, backend=None, basis_gates=None, coupling_map=None,
-              backend_properties=None, initial_layout=None,
-              seed_transpiler=None, seed_mapper=None):
+def transpile(circuits,
+              basis_gates=None, coupling_map=None, backend_properties=None,
+              initial_layout=None, seed_transpiler=None, optimization_level=None,
+              transpile_config=None, backend=None,
+              seed_mapper=None, pass_manager=None):
     """transpile one or more circuits, according to some desired
     transpilation targets.
 
     Args:
         circuits (QuantumCircuit or list[QuantumCircuit]):
             Circuit(s) to transpile
-
-        backend (BaseBackend):
-            If set, transpiler options are automatically grabbed from
-            backend.configuration() and backend.properties().
-            If any other option is explicitly set (e.g. coupling_map), it
-            will override the backend's.
-            Note: the backend arg is purely for convenience. The resulting
-                circuit may be run on any backend as long as it is compatible.
 
         basis_gates (list[str]):
             List of basis gate names to unroll to.
@@ -53,23 +47,59 @@ def transpile(circuits, backend=None, basis_gates=None, coupling_map=None,
             that provides this information, it can be obtained with:
             ``backend.properties()``
 
-        initial_layout (list or dict):
-            Initial layout of virtual qubits onto the physical qubits
+        initial_layout (Layout or dict or list):
+            Initial position of virtual qubits on physical qubits.
             If this layout makes the circuit compatible with the coupling_map
             constraints, it will be used.
-            The final layout will not necessarily be the same, since swaps may
-            permute the location of qubits.
-            e.g:
-                [q[0], None, None, q[1], None]
-                {q[0]: 0, q[1]: 3}
-            N.B. These layouts are not identical. In the first case, it is
-            signaled to the transpiler that three unspecified virtual qubits
-            must exist on physical qubits 1, 2 and 4. The transpiler will
-            expand the original circuit with extra virtual ancilla qubits
-            to accomodate this layout.
+            The final layout is not guaranteed to be the same, as the transpiler
+            may permute qubits through swaps or other means.
+
+            Multiple formats are supported:
+            a. Layout instance
+
+            b. dict
+                virtual to physical:
+                    {qr[0]: 0,
+                     qr[1]: 3,
+                     qr[2]: 5}
+
+                physical to virtual:
+                    {0: qr[0],
+                     3: qr[1],
+                     5: qr[2]}
+
+            c. list
+                virtual to physical:
+                    [0, 3, 5]  # virtual qubits are ordered (in addition to named)
+
+                physical to virtual:
+                    [qr[0], None, None, qr[1], None, qr[2]]
 
         seed_transpiler (int):
             sets random seed for the stochastic parts of the transpiler
+
+        optimization_level (int):
+            How much optimization to perform on the circuits.
+            Higher levels generate more optimized circuits,
+            at the expense of longer transpilation time.
+                0: no optimization
+                1: light optimization
+                2: heavy optimization
+
+        transpile_config (TranspileConfig):
+            transpiler configuration, containing some or all of the above options.
+            If any other options is explicitly set (e.g. coupling_map), it
+            will override the transpile_config's.
+
+        backend (BaseBackend):
+            If set, transpiler options are automatically grabbed from
+            backend.configuration() and backend.properties().
+            If any other option is explicitly set (e.g. coupling_map), it
+            will override the backend's.
+            If any other options is set in the transpile_config, it will
+            also override the backend's.
+            Note: the backend arg is purely for convenience. The resulting
+                circuit may be run on any backend as long as it is compatible.
 
         seed_mapper (int):
             DEPRECATED in 0.8: use ``seed_transpiler`` kwarg instead
@@ -79,27 +109,175 @@ def transpile(circuits, backend=None, basis_gates=None, coupling_map=None,
 
     Returns:
         QuantumCircuit or list[QuantumCircuit]: transpiled circuit(s).
+
+    Raises:
+        TranspilerError: in case of bad inputs to transpiler or errors in passes
     """
+    # Deprecated matter
+    if seed_mapper:
+        warnings.warn("seed_mapper has been deprecated and will be removed in the "
+                      "0.9 release. Instead use seed_transpiler to set the seed "
+                      "for all stochastic parts of the.", DeprecationWarning)
+        seed_transpiler = seed_mapper
 
-    # ------------
-    # TODO: This is a hack while we are still using the old transpiler.
-    initial_layout = getattr(transpile_config, 'initial_layout', None)
-    basis_gates = getattr(transpile_config, 'basis_gates', None)
-    coupling_map = getattr(transpile_config, 'coupling_map', None)
-    seed_mapper = getattr(transpile_config, 'seed_mapper', None)
+    if pass_manager:
+        warnings.warn("After the 0.9 release, the transpile() function no longer accepts "
+                      "a pass_manager arg. Instead use pass_manager.run(circuits) directly "
+                      "to run a custom pipeline of passes.", DeprecationWarning)
+        return pass_manager.run(circuits)
 
-    if initial_layout is not None and not isinstance(initial_layout, Layout):
+    # Parse transpile options. the priority is:
+    # 1. kwarg    2. transpile_config    3. backend
+    if basis_gates is None:
+        basis_gate = getattr(transpile_config, 'basis_gates', None) or \
+                     getattr(backend.configuration(), 'basis_gates', None)
+
+    if coupling_map is None:
+        coupling_map = getattr(transpile_config, 'coupling_map', None) or \
+                       getattr(backend.configuration(), 'coupling_map', None)
+
+    if backend_properties is None:
+        backend_properties = getattr(transpile_config, 'backend_properties', None) or \
+                             backend.properties()
+
+    # TODO: (Ali) allow partial layout specifications
+    initial_layout = initial_layout or getattr(transpile_config, 'initial_layout', None)
+    if isinstance(initial_layout, list):
+        if all(isinstance(elem, int) for elem in initial_layout):
+            # FIXME: (Ali) must allow a list of initial layouts
+            initial_layout = Layout.from_intlist(initial_layout, *circuits[0].qregs)
+        elif all(elem is None or isinstance(elem, tuple) for elem in initial_layout):
+            initial_layout = Layout.from_tuplelist(initial_layout)
+        else:
+            raise TranspilerError("bad format for initial_layout")
+    elif isinstance(initial_layout, dict):
         initial_layout = Layout(initial_layout)
 
-    pass_manager = None
-    backend = getattr(transpile_config, 'backend', None)
-    new_circuits = transpiler.transpile(circuits, backend, basis_gates, coupling_map,
-                                        initial_layout, seed_mapper, pass_manager)
-    # ---------
+    seed_transpiler = seed_transpiler or getattr(transpile_config, 'seed_transpiler', None)
 
-    # THE IDEAL CODE HERE WILL BE.
-    # 1 set up the pass_manager using transpile_config options
-    # pass_manager = PassManager(TranspileConig)
-    # run the passes
-    # new_circuits = pass_manager.run(circuits)
-    return new_circuits
+    return_form_is_single = False
+    if isinstance(circuits, QuantumCircuit):
+        circuits = [circuits]
+        return_form_is_single = True
+
+    # pass manager overrides explicit transpile options (basis_gates, coupling_map)
+    # explicit transpile options override options gotten from a backend
+    if not pass_manager and backend:
+        basis_gates = basis_gates or getattr(backend.configuration(), 'basis_gates', None)
+        # This needs to be removed once Aer 0.2 is out
+        coupling_map = coupling_map or getattr(backend.configuration(), 'coupling_map', None)
+
+    circuits = parallel_map(_transpilation, circuits,
+                            task_kwargs={'basis_gates': basis_gates,
+                                         'coupling_map': coupling_map,
+                                         'initial_layout': initial_layout,
+                                         'seed_mapper': seed_mapper,
+                                         'pass_manager': pass_manager})
+    if return_form_is_single:
+        return circuits[0]
+    return circuits
+
+
+def _transpilation(circuit, basis_gates=None, coupling_map=None,
+                   initial_layout=None, seed_mapper=None,
+                   pass_manager=None):
+    """Perform transpilation of a single circuit.
+
+    Args:
+        circuit (QuantumCircuit): A circuit to transpile.
+        basis_gates (list[str]): list of basis gate names supported by the
+            target. Default: ['u1','u2','u3','cx','id']
+        coupling_map (CouplingMap): coupling map (perhaps custom) to target in mapping
+        initial_layout (Layout): initial layout of qubits in mapping
+        seed_mapper (int): random seed for the swap_mapper
+        pass_manager (PassManager): a pass_manager for the transpiler stage
+
+    Returns:
+        QuantumCircuit: A transpiled circuit.
+
+    Raises:
+        TranspilerError: If the Layout does not matches the circuit
+    """
+    if initial_layout is not None and set(circuit.qregs) != initial_layout.get_registers():
+        raise TranspilerError('The provided initial layout does not match the registers in '
+                              'the circuit "%s"' % circuit.name)
+
+    if pass_manager and not pass_manager.working_list:
+        return circuit
+
+    is_parametric_circuit = bool(circuit.variables)
+
+    dag = circuit_to_dag(circuit)
+    del circuit
+
+    final_dag = _transpile_dag(dag, basis_gates=basis_gates,
+                               coupling_map=coupling_map,
+                               initial_layout=initial_layout,
+                               skip_numeric_passes=is_parametric_circuit,
+                               seed_mapper=seed_mapper,
+                               pass_manager=pass_manager)
+
+    out_circuit = dag_to_circuit(final_dag)
+
+    return out_circuit
+
+
+def _transpile_dag(dag, basis_gates=None, coupling_map=None,
+                   initial_layout=None, skip_numeric_passes=None,
+                   seed_mapper=None, pass_manager=None):
+    """Transform a dag circuit into another dag circuit (transpile), through
+    consecutive passes on the dag.
+
+    Args:
+        dag (DAGCircuit): dag circuit to transform via transpilation
+        basis_gates (list[str]): list of basis gate names supported by the
+            target. Default: ['u1','u2','u3','cx','id']
+        coupling_map (list): A graph of coupling::
+
+            [
+             [control0(int), target0(int)],
+             [control1(int), target1(int)],
+            ]
+
+            eg. [[0, 2], [1, 2], [1, 3], [3, 4]}
+
+        initial_layout (Layout or None): A layout object
+        skip_numeric_passes (bool): If true, skip passes which require fixed parameter values
+        seed_mapper (int): random seed_mapper for the swap mapper
+        pass_manager (PassManager): pass manager instance for the transpilation process
+            If None, a default set of passes are run.
+            Otherwise, the passes defined in it will run.
+            If contains no passes in it, no dag transformations occur.
+
+    Returns:
+        DAGCircuit: transformed dag
+    """
+    if basis_gates is None:
+        basis_gates = ['u1', 'u2', 'u3', 'cx', 'id']
+    if isinstance(basis_gates, str):
+        warnings.warn("The parameter basis_gates is now a list of strings. "
+                      "For example, this basis ['u1','u2','u3','cx'] should be used "
+                      "instead of 'u1,u2,u3,cx'. The string format will be "
+                      "removed after 0.9", DeprecationWarning, 2)
+        basis_gates = basis_gates.split(',')
+
+    if pass_manager is None:
+        # default set of passes
+
+        # if a coupling map is given compile to the map
+        if coupling_map:
+            pass_manager = default_pass_manager(basis_gates,
+                                                CouplingMap(coupling_map),
+                                                initial_layout,
+                                                skip_numeric_passes,
+                                                seed_mapper=seed_mapper)
+        else:
+            pass_manager = default_pass_manager_simulator(basis_gates)
+
+    # run the passes specified by the pass manager
+    # TODO return the property set too. See #1086
+    name = dag.name
+    dag = pass_manager.run_passes(dag)
+    dag.name = name
+
+    return dag
