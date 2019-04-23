b962d031c59c0ea8c67ddadfb77ed6273c2f6e63
CZ <ouf@zurich.ibm.com>
2019-04-23 13:31:36 +0200

    adding qgans and variational distributions to init files

--- qiskit/aqua/algorithms/__init__.py
+++ qiskit/aqua/algorithms/__init__.py
@@ -16,7 +16,7 @@
 # =============================================================================
 
 from .quantum_algorithm import QuantumAlgorithm
-from .adaptive import VQE, QAOA, QSVMVariational
+from .adaptive import VQE, QAOA, QSVMVariational, QGAN
 from .classical import ExactEigensolver, ExactLSsolver, SVM_Classical
 from .many_sample import EOH, QSVMKernel
 from .single_sample import Grover, IQPE, QPE, AmplitudeEstimation, Simon, \
@@ -29,7 +29,7 @@ __all__ = ['QuantumAlgorithm',
            'QSVMVariational',
            'QGAN',
            'ExactEigensolver',
-           'ExactLPsolver',
+           'ExactLSsolver',
            'SVM_Classical',
            'EOH',
            'QSVMKernel',
