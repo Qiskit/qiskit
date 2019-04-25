d9fce14675c47aca75bec730990656372daadc7a
Thomas Alexander <talexander@ibm.com>
2019-04-25 17:41:38 -0400

    Delete extra interface methods.

--- qiskit/pulse/interfaces.py
+++ qiskit/pulse/interfaces.py
@@ -9,7 +9,7 @@
 ScheduleComponent, a common interface for components of schedule (Instruction and Schedule).
 """
 from abc import ABCMeta, abstractmethod
-from typing import Tuple, List, Iterable, Union
+from typing import Tuple, List, Union
 
 from qiskit.pulse.channels import Channel
 
@@ -74,65 +74,4 @@ class ScheduleComponent(metaclass=ABCMeta):
     @abstractmethod
     def children(self) -> Tuple[Union[int, 'ScheduleComponent']]:
         """Child nodes of this schedule component. """
-        pass
-
-    @abstractmethod
-    def union(self, *schedules: List['ScheduleComponent']) -> 'ScheduleComponent':
-        """Return a new schedule which is the union of the parent `Schedule` and `schedule`.
-
-        Args:
-            schedules: Schedules to be take the union with the parent `Schedule`.
-        """
-        pass
-
-    @abstractmethod
-    def shift(self: 'ScheduleComponent', time: int) -> 'ScheduleComponent':
-        """Return a new schedule shifted forward by `time`.
-
-        Args:
-            time: Time to shift by
-        """
-        pass
-
-    @abstractmethod
-    def insert(self, start_time: int, schedule: 'ScheduleComponent') -> 'ScheduleComponent':
-        """Return a new schedule with `schedule` inserted at `start_time` of `self`.
-
-        Args:
-            start_time: time to be inserted
-            schedule: schedule to be inserted
-        """
-        pass
-
-    @abstractmethod
-    def append(self, schedule: 'ScheduleComponent') -> 'ScheduleComponent':
-        """Return a new schedule with `schedule` inserted at the maximum time over
-        all channels shared between `self` and `schedule`.
-
-        Args:
-            schedule: schedule to be appended
-        """
-        pass
-
-    def flatten(self, time: int = 0) -> Iterable[Tuple[int, 'ScheduleComponent']]:
-        """Iterable for flattening Schedule tree.
-
-        Args:
-            time: Initial time of this node
-        """
-        pass
-
-    @abstractmethod
-    def __add__(self, schedule: 'ScheduleComponent') -> 'ScheduleComponent':
-        """Return a new schedule with `schedule` inserted within `self` at `start_time`."""
-        pass
-
-    @abstractmethod
-    def __or__(self, schedule: 'ScheduleComponent') -> 'ScheduleComponent':
-        """Return a new schedule which is the union of `self` and `schedule`."""
-        pass
-
-    @abstractmethod
-    def __lshift__(self, time: int) -> 'ScheduleComponent':
-        """Return a new schedule which is shifted forward by `time`."""
         pass
