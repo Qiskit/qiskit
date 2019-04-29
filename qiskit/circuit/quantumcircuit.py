87188be0dc09189319db1000c0cdbbada4a5a7cc
Kevin Krsulich <kevin.krsulich@ibm.com>
2019-04-29 17:15:24 -0400

    Raise if appending different Parameters of same name to circuit.

--- qiskit/circuit/quantumcircuit.py
+++ qiskit/circuit/quantumcircuit.py
@@ -285,6 +285,9 @@ class QuantumCircuit:
                 if param in current_symbols:
                     self._parameter_table[param].append((instruction, param_index))
                 else:
+                    if param.name in set(p.name for p in current_symbols):
+                        raise QiskitError(
+                            'Name conflict on adding parameter: {}'.format(param.name))
                     self._parameter_table[param] = [(instruction, param_index)]
 
         return instruction
