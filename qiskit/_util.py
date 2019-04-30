6c80c302830227177dae39fccaddbfd84c05b3d2
Matthew Treinish <mtreinish@kortar.org>
2019-04-30 11:41:37 -0400

    Remove warning from qiskit._util and rely on a comment
    Since the warning was too noisy and gets emitted on every qiskit import
    this commit just removes it. The _util module is still deprecated, and
    listed as such in the module and in the changelog (and the future
    release notes). But, since it was marked as private previously weighing
    the noise of advertising the deprecation with a warning against the
    potential of an external user using it removing the warning seems the
    better option.

--- qiskit/_util.py
+++ qiskit/_util.py
@@ -16,17 +16,8 @@
 
 """Compat shim for backwards compatability with qiskit.util."""
 
-import warnings
+# The 'qiskit._util' module is deprecated and has been renamed
+# 'qiskit.util'. Please update your imports as 'qiskit._util'
+# will be removed in Qiskit Terra 0.9.
 
-from qiskit import util
-
-def __getattr__(name):
-    warnings.warn('The qiskit._util module is deprecated and has been renamed '
-                  'qiskit.util. Please update your imports as qiskit._util '
-                  'will be removed in Qiskit Terra 0.9.', DeprecationWarning)
-    res = getattr(util, name, None)
-    if res is None:
-        raise AttributeError("module: 'qiskit._util' has no attribute: '%s'"
-                             % name)
-    return res
-
+from qiskit.util import *
