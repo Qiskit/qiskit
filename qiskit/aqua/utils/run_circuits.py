9c469139c07994374c97f67a6f13f33cd1f4b4dc
Richard Chen <chenrich@us.ibm.com>
2019-05-01 09:15:05 -0400

    1. remove the usage `has_shared_circuits` 2. clean up the redundant as it is split into multiple functions.

--- qiskit/aqua/utils/run_circuits.py
+++ qiskit/aqua/utils/run_circuits.py
@@ -60,7 +60,6 @@ def find_regs_by_name(circuit, name, qreg=True):
 
 
 def _avoid_empty_circuits(circuits):
-
     new_circuits = []
     for qc in circuits:
         if len(qc) == 0:
@@ -73,42 +72,6 @@ def _avoid_empty_circuits(circuits):
             qc.iden(tmp_q[0])
         new_circuits.append(qc)
     return new_circuits
-
-
-def _reuse_shared_circuits(circuits, backend, backend_config, compile_config, run_config,
-                           qjob_config=None, backend_options=None, show_circuit_summary=False):
-    """Reuse the circuits with the shared head.
-
-    We assume the 0-th circuit is the shared_circuit, so we execute it first
-    and then use it as initial state for simulation.
-
-    Note that all circuits should have the exact the same shared parts.
-    """
-    qjob_config = qjob_config or {}
-    backend_options = backend_options or {}
-
-    shared_circuit = circuits[0]
-    shared_result = compile_and_run_circuits(shared_circuit, backend, backend_config,
-                                             compile_config, run_config, qjob_config,
-                                             show_circuit_summary=show_circuit_summary)
-
-    if len(circuits) == 1:
-        return shared_result
-    shared_quantum_state = np.asarray(shared_result.get_statevector(shared_circuit))
-    # extract different of circuits
-    for circuit in circuits[1:]:
-        circuit.data = circuit.data[len(shared_circuit):]
-
-    temp_backend_options = copy.deepcopy(backend_options)
-    if 'backend_options' not in temp_backend_options:
-        temp_backend_options['backend_options'] = {}
-    temp_backend_options['backend_options']['initial_statevector'] = shared_quantum_state
-    diff_result = compile_and_run_circuits(circuits[1:], backend, backend_config,
-                                           compile_config, run_config, qjob_config,
-                                           backend_options=temp_backend_options,
-                                           show_circuit_summary=show_circuit_summary)
-    result = _combine_result_objects([shared_result, diff_result])
-    return result
 
 
 def _combine_result_objects(results):
@@ -129,7 +92,6 @@ def _combine_result_objects(results):
 
 
 def _maybe_add_aer_expectation_instruction(qobj, options):
-
     if 'expectation' in options:
         from qiskit.providers.aer.utils.qobj_utils import snapshot_instr, append_instr, get_instr_pos
         # add others, how to derive the correct used number of qubits?
@@ -144,7 +106,7 @@ def _maybe_add_aer_expectation_instruction(qobj, options):
             snapshot_pos = get_instr_pos(qobj, idx, 'snapshot')
             if len(snapshot_pos) == 0:  # does not append the instruction yet.
                 new_ins = snapshot_instr('expectation_value_pauli', 'test',
-                                         range(num_qubits), params=params[param_idx])
+                                         list(range(num_qubits)), params=params[param_idx])
                 qobj = append_instr(qobj, idx, new_ins)
             else:
                 for i in snapshot_pos:  # update all expectation_value_snapshot
@@ -158,7 +120,8 @@ def _compile_wrapper(circuits, backend, backend_config, compile_config, run_conf
     if not isinstance(transpiled_circuits, list):
         transpiled_circuits = [transpiled_circuits]
 
-    qobj = assemble_circuits(transpiled_circuits, qobj_id=str(uuid.uuid4()), qobj_header=QobjHeader(), run_config=run_config)
+    qobj = assemble_circuits(transpiled_circuits, qobj_id=str(uuid.uuid4()), qobj_header=QobjHeader(),
+                             run_config=run_config)
     return qobj, transpiled_circuits
 
 
@@ -214,7 +177,8 @@ def compile_cirucits(circuits, backend, backend_config=None, compile_config=None
         else:  # Try setting up the reusable qobj
             # Compile and cache first circuit if cache is empty. The load method will try to reuse it
             if circuit_cache.qobjs is None:
-                qobj, transpiled_circuits = _compile_wrapper([circuits[0]], backend, backend_config, compile_config, run_config)
+                qobj, transpiled_circuits = _compile_wrapper([circuits[0]], backend, backend_config,
+                                                             compile_config, run_config)
 
                 if is_aer_provider(backend):
                     qobj = _maybe_add_aer_expectation_instruction(qobj, kwargs)
@@ -244,7 +208,8 @@ def compile_cirucits(circuits, backend, backend_config=None, compile_config=None
                 else:
                     logger.info('Circuit cache is empty, compiling from scratch.')
                 circuit_cache.clear_cache()
-                qobj, transpiled_sub_circuits = _compile_wrapper(sub_circuits, backend, backend_config, compile_config, run_config)
+                qobj, transpiled_sub_circuits = _compile_wrapper(sub_circuits, backend, backend_config,
+                                                                 compile_config, run_config)
                 transpiled_circuits.extend(transpiled_sub_circuits)
                 if is_aer_provider(backend):
                     qobj = _maybe_add_aer_expectation_instruction(qobj, kwargs)
@@ -259,7 +224,8 @@ def compile_cirucits(circuits, backend, backend_config=None, compile_config=None
                         logger.info('Transpilation may be too aggressive. Try skipping transpiler.')
 
         else:
-            qobj, transpiled_sub_circuits = _compile_wrapper(sub_circuits, backend, backend_config, compile_config, run_config)
+            qobj, transpiled_sub_circuits = _compile_wrapper(sub_circuits, backend, backend_config, compile_config,
+                                                             run_config)
             transpiled_circuits.extend(transpiled_sub_circuits)
             if is_aer_provider(backend):
                 qobj = _maybe_add_aer_expectation_instruction(qobj, kwargs)
@@ -292,8 +258,9 @@ def _safe_submit_qobj(qobj, backend, backend_options, noise_config, skip_qobj_va
 
     return job, job_id
 
+
 def run_qobjs(qobjs, backend, qjob_config=None, backend_options=None,
-                 noise_config=None, skip_qobj_validation=False):
+              noise_config=None, skip_qobj_validation=False):
     """
     An execution wrapper with Qiskit-Terra, with job auto recover capability.
 
@@ -402,13 +369,12 @@ def run_qobjs(qobjs, backend, qjob_config=None, backend_options=None,
 
     return result
 
+
 def compile_and_run_circuits(circuits, backend, backend_config=None,
                              compile_config=None, run_config=None,
                              qjob_config=None, backend_options=None,
                              noise_config=None, show_circuit_summary=False,
-                             has_shared_circuits=False, circuit_cache=None,
-                             skip_qobj_validation=False, measurement_mitigation=None,
-                             **kwargs):
+                             circuit_cache=None, skip_qobj_validation=False, **kwargs):
     """
     An execution wrapper with Qiskit-Terra, with job auto recover capability.
 
@@ -425,11 +391,8 @@ def compile_and_run_circuits(circuits, backend, backend_config=None,
         backend_options (dict, optional): configuration for simulator
         noise_config (dict, optional): configuration for noise model
         show_circuit_summary (bool, optional): showing the summary of submitted circuits.
-        has_shared_circuits (bool, optional): use the 0-th circuits as initial state for other circuits.
         circuit_cache (CircuitCache, optional): A CircuitCache to use when calling compile_and_run_circuits
         skip_qobj_validation (bool, optional): Bypass Qobj validation to decrease submission time
-        measurement_mitigation (callable, optional): the approach to mitigate measurement error,
-                                                     CompleteMeasFitter or TensoredMeasFitter
 
     Returns:
         Result: Result object
@@ -437,208 +400,9 @@ def compile_and_run_circuits(circuits, backend, backend_config=None,
     Raises:
         AquaError: Any error except for JobError raised by Qiskit Terra
     """
-    backend_config = backend_config or {}
-    compile_config = compile_config or {}
-    run_config = run_config or {}
-    qjob_config = qjob_config or {}
-    backend_options = backend_options or {}
-    noise_config = noise_config or {}
-
-    if backend is None or not isinstance(backend, BaseBackend):
-        raise ValueError('Backend is missing or not an instance of BaseBackend')
-
-    if not isinstance(circuits, list):
-        circuits = [circuits]
-
-    if is_simulator_backend(backend):
-        circuits = _avoid_empty_circuits(circuits)
-
-    if has_shared_circuits:
-        return _reuse_shared_circuits(circuits, backend, backend_config, compile_config,
-                                      run_config, qjob_config, backend_options)
-
-    with_autorecover = False if is_simulator_backend(backend) else True
-
-    if MAX_CIRCUITS_PER_JOB is not None:
-        max_circuits_per_job = int(MAX_CIRCUITS_PER_JOB)
-    else:
-        if is_local_backend(backend):
-            max_circuits_per_job = sys.maxsize
-        else:
-            max_circuits_per_job = backend.configuration().max_experiments
-
-    if circuit_cache is not None and circuit_cache.try_reusing_qobjs:
-        # Check if all circuits are the same length.
-        # If not, don't try to use the same qobj.experiment for all of them.
-        if len(set([len(circ.data) for circ in circuits])) > 1:
-            circuit_cache.try_reusing_qobjs = False
-        else:  # Try setting up the reusable qobj
-            # Compile and cache first circuit if cache is empty. The load method will try to reuse it
-            if circuit_cache.qobjs is None:
-                qobj, transpiled_circuits = _compile_wrapper([circuits[0]], backend, backend_config, compile_config, run_config)
-
-                if is_aer_provider(backend):
-                    qobj = _maybe_add_aer_expectation_instruction(qobj, kwargs)
-                circuit_cache.cache_circuit(qobj, [circuits[0]], 0)
-
-    qobjs = []
-    jobs = []
-    job_ids = []
-    transpiled_circuits = []
-    chunks = int(np.ceil(len(circuits) / max_circuits_per_job))
-    for i in range(chunks):
-        sub_circuits = circuits[i * max_circuits_per_job:(i + 1) * max_circuits_per_job]
-        if circuit_cache is not None and circuit_cache.misses < circuit_cache.allowed_misses:
-            try:
-                if circuit_cache.cache_transpiled_circuits:
-                    transpiled_sub_circuits = compiler.transpile(sub_circuits, backend, **backend_config,
-                                                                 **compile_config)
-                    qobj = circuit_cache.load_qobj_from_cache(transpiled_sub_circuits, i, run_config=run_config)
-                else:
-                    qobj = circuit_cache.load_qobj_from_cache(sub_circuits, i, run_config=run_config)
-                if is_aer_provider(backend):
-                    qobj = _maybe_add_aer_expectation_instruction(qobj, kwargs)
-            # cache miss, fail gracefully
-            except (TypeError, IndexError, FileNotFoundError, EOFError, AquaError, AttributeError) as e:
-                circuit_cache.try_reusing_qobjs = False  # Reusing Qobj didn't work
-                if len(circuit_cache.qobjs) > 0:
-                    logger.info('Circuit cache miss, recompiling. Cache miss reason: ' + repr(e))
-                    circuit_cache.misses += 1
-                else:
-                    logger.info('Circuit cache is empty, compiling from scratch.')
-                circuit_cache.clear_cache()
-                qobj, transpiled_sub_circuits = _compile_wrapper(sub_circuits, backend, backend_config, compile_config, run_config)
-                transpiled_circuits.extend(transpiled_sub_circuits)
-                if is_aer_provider(backend):
-                    qobj = _maybe_add_aer_expectation_instruction(qobj, kwargs)
-                try:
-                    circuit_cache.cache_circuit(qobj, sub_circuits, i)
-                except (TypeError, IndexError, AquaError, AttributeError, KeyError) as e:
-                    try:
-                        circuit_cache.cache_transpiled_circuits = True
-                        circuit_cache.cache_circuit(qobj, transpiled_sub_circuits, i)
-                    except (TypeError, IndexError, AquaError, AttributeError, KeyError) as e:
-                        logger.info('Circuit could not be cached for reason: ' + repr(e))
-                        logger.info('Transpilation may be too aggressive. Try skipping transpiler.')
-
-        else:
-            qobj, transpiled_sub_circuits = _compile_wrapper(sub_circuits, backend, backend_config, compile_config, run_config)
-            transpiled_circuits.extend(transpiled_sub_circuits)
-            if is_aer_provider(backend):
-                qobj = _maybe_add_aer_expectation_instruction(qobj, kwargs)
-
-        # assure get job ids
-        while True:
-            job = run_on_backend(backend, qobj, backend_options=backend_options, noise_config=noise_config,
-                                 skip_qobj_validation=skip_qobj_validation)
-            try:
-                job_id = job.job_id()
-                break
-            except JobError as e:
-                logger.warning("FAILURE: the {}-th chunk of circuits, can not get job id, "
-                               "Resubmit the qobj to get job id. "
-                               "Terra job error: {} ".format(i, e))
-            except Exception as e:
-                logger.warning("FAILURE: the {}-th chunk of circuits, can not get job id, "
-                               "Resubmit the qobj to get job id. "
-                               "Error: {} ".format(i, e))
-        job_ids.append(job_id)
-        jobs.append(job)
-        qobjs.append(qobj)
-
-    if logger.isEnabledFor(logging.DEBUG) and show_circuit_summary:
-        logger.debug("==== Before transpiler ====")
-        logger.debug(summarize_circuits(circuits))
-        logger.debug("====  After transpiler ====")
-        logger.debug(summarize_circuits(transpiled_circuits))
-
-    results = []
-    if with_autorecover:
-        logger.info("Backend status: {}".format(backend.status()))
-        logger.info("There are {} circuits and they are chunked into {} chunks, "
-                    "each with {} circutis (max.).".format(len(circuits), chunks,
-                                                           max_circuits_per_job))
-        logger.info("All job ids:\n{}".format(job_ids))
-        for idx in range(len(jobs)):
-            while True:
-                job = jobs[idx]
-                job_id = job_ids[idx]
-                logger.info("Running {}-th chunk circuits, job id: {}".format(idx, job_id))
-                # try to get result if possible
-                try:
-                    result = job.result(**qjob_config)
-                    if result.success:
-                        results.append(result)
-                        logger.info("COMPLETED the {}-th chunk of circuits, "
-                                    "job id: {}".format(idx, job_id))
-                        break
-                    else:
-                        logger.warning("FAILURE: the {}-th chunk of circuits, "
-                                       "job id: {}".format(idx, job_id))
-                except JobError as e:
-                    # if terra raise any error, which means something wrong, re-run it
-                    logger.warning("FAILURE: the {}-th chunk of circuits, job id: {} "
-                                   "Terra job error: {} ".format(idx, job_id, e))
-                except Exception as e:
-                    raise AquaError("FAILURE: the {}-th chunk of circuits, job id: {} "
-                                    "Unknown error: {} ".format(idx, job_id, e)) from e
-
-                # something wrong here, querying the status to check how to handle it.
-                # keep qeurying it until getting the status.
-                while True:
-                    try:
-                        job_status = job.status()
-                        break
-                    except JobError as e:
-                        logger.warning("FAILURE: job id: {}, "
-                                       "status: 'FAIL_TO_GET_STATUS' "
-                                       "Terra job error: {}".format(job_id, e))
-                        time.sleep(5)
-                    except Exception as e:
-                        raise AquaError("FAILURE: job id: {}, "
-                                        "status: 'FAIL_TO_GET_STATUS' "
-                                        "Unknown error: ({})".format(job_id, e)) from e
-
-                logger.info("Job status: {}".format(job_status))
-
-                # handle the failure job based on job status
-                if job_status == JobStatus.DONE:
-                    logger.info("Job ({}) is completed anyway, retrieve result "
-                                "from backend.".format(job_id))
-                    job = backend.retrieve_job(job_id)
-                elif job_status == JobStatus.RUNNING or job_status == JobStatus.QUEUED:
-                    logger.info("Job ({}) is {}, but encounter an exception, "
-                                "recover it from backend.".format(job_id, job_status))
-                    job = backend.retrieve_job(job_id)
-                else:
-                    logger.info("Fail to run Job ({}), resubmit it.".format(job_id))
-                    qobj = qobjs[idx]
-                    #  assure job get its id
-                    while True:
-                        job = run_on_backend(backend, qobj,
-                                             backend_options=backend_options,
-                                             noise_config=noise_config,
-                                             skip_qobj_validation=skip_qobj_validation)
-                        try:
-                            job_id = job.job_id()
-                            break
-                        except JobError as e:
-                            logger.warning("FAILURE: the {}-th chunk of circuits, "
-                                           "can not get job id. Resubmit the qobj to get job id. "
-                                           "Terra job error: {} ".format(idx, e))
-                        except Exception as e:
-                            logger.warning("FAILURE: the {}-th chunk of circuits, "
-                                           "can not get job id, Resubmit the qobj to get job id. "
-                                           "Unknown error: {} ".format(idx, e))
-                    jobs[idx] = job
-                    job_ids[idx] = job_id
-    else:
-        results = []
-        for job in jobs:
-            results.append(job.result(**qjob_config))
-
-    result = _combine_result_objects(results) if len(results) != 0 else None
-
+    qobjs = compile_cirucits(circuits, backend, backend_config, compile_config, run_config,
+                             show_circuit_summary, circuit_cache, **kwargs)
+    result = run_qobjs(qobjs, backend, qjob_config, backend_options, noise_config, skip_qobj_validation)
     return result
 
 
