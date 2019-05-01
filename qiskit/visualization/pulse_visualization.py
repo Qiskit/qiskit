b37d58a36902bbfd6d3ad7c37a504ae14999f976
Thomas Alexander <talexander@ibm.com>
2019-05-01 01:18:20 -0400

    Remove output visualization option.

--- qiskit/visualization/pulse_visualization.py
+++ qiskit/visualization/pulse_visualization.py
@@ -27,7 +27,7 @@ from qiskit.visualization import matplotlib as _matplotlib
 logger = logging.getLogger(__name__)
 
 
-def pulse_drawer(data, dt=1, style=None, filename=None, output='mpl',
+def pulse_drawer(data, dt=1, style=None, filename=None,
                  interp_method=None, scaling=None, channels_to_plot=None,
                  plot_all=False, plot_range=None, interactive=False,
                  table=True, label=False, framechange=True):
@@ -38,8 +38,6 @@ def pulse_drawer(data, dt=1, style=None, filename=None, output='mpl',
         dt (float): Time interval of samples
         style (OPStylePulse or OPStyleSched): A style sheet to configure plot appearance
         filename (str): Name required to save pulse image
-        output (str): Select the output method to use for drawing the pulse object.
-            Valid choices are `mpl`.
         interp_method (Callable): interpolation function
             See `qiskit.visualization.interpolation` for more information
         scaling (float): scaling of waveform amplitude
@@ -56,22 +54,17 @@ def pulse_drawer(data, dt=1, style=None, filename=None, output='mpl',
     Raises:
         VisualizationError: when invalid data is given or lack of information
     """
-    if output == 'mpl':
-        if isinstance(data, SamplePulse):
-            drawer = _matplotlib.SamplePulseDrawer(style=style)
-            image = drawer.draw(data, dt=dt, interp_method=interp_method, scaling=scaling)
-        elif isinstance(data, (Schedule, Instruction)):
-            drawer = _matplotlib.ScheduleDrawer(style=style)
-            image = drawer.draw(data, dt=dt, interp_method=interp_method, scaling=scaling,
-                                plot_range=plot_range, channels_to_plot=channels_to_plot,
-                                plot_all=plot_all, table=table, label=label,
-                                framechange=framechange)
-        else:
-            raise VisualizationError('This data cannot be visualized.')
+    if isinstance(data, SamplePulse):
+        drawer = _matplotlib.SamplePulseDrawer(style=style)
+        image = drawer.draw(data, dt=dt, interp_method=interp_method, scaling=scaling)
+    elif isinstance(data, (Schedule, Instruction)):
+        drawer = _matplotlib.ScheduleDrawer(style=style)
+        image = drawer.draw(data, dt=dt, interp_method=interp_method, scaling=scaling,
+                            plot_range=plot_range, channels_to_plot=channels_to_plot,
+                            plot_all=plot_all, table=table, label=label,
+                            framechange=framechange)
     else:
-        raise VisualizationError(
-            'Invalid output type %s selected. The only valid choice '
-            'is mpl' % output)
+        raise VisualizationError('This data cannot be visualized.')
 
     if filename:
         image.savefig(filename, dpi=drawer.style.dpi, bbox_inches='tight')
