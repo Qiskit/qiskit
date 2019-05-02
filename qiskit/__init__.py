0fdfc49398dbf669164db08b074bd41cd06cd0ca
Paul Nation <nonhermitian@gmail.com>
2019-05-02 11:30:47 -0400

    fid to new location

--- qiskit/__init__.py
+++ qiskit/__init__.py
@@ -32,7 +32,7 @@ from qiskit.circuit import QuantumCircuit
 # pylint: disable=redefined-builtin
 from qiskit.tools.compiler import compile  # TODO remove after 0.8
 from qiskit.execute import execute
-from qiskit.transpiler.transpiler import transpile
+from qiskit.compiler import transpile
 
 # The qiskit.extensions.x imports needs to be placed here due to the
 # mechanism for adding gates dynamically.
