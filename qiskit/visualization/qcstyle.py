b1280b390e529933c0441471790ff3e6e9f9a9f0
Luciano Bello <luciano.bello@ibm.com>
2019-05-08 15:51:45 -0400

    clean up!

--- qiskit/visualization/qcstyle.py
+++ qiskit/visualization/qcstyle.py
@@ -15,7 +15,104 @@
 # pylint: disable=invalid-name,anomalous-backslash-in-string,missing-docstring
 
 
-class QCStyle:
+class DefaultStyle:
+    """A colorblind friendly gate styling modelled
+    after:
+        B. Wang, “Points of view: Color blindness“,
+        Nat. Methods 8, 441 (2011).
+    """
+    def __init__(self):
+        self.tc = '#000000'
+        self.sc = '#000000'
+        self.lc = '#000000'
+        self.cc = '#778899'
+        self.gc = '#ffffff'
+        self.gt = '#000000'
+        self.bc = '#bdbdbd'
+        self.bg = '#ffffff'
+        self.fs = 13
+        self.sfs = 8
+        self.disptex = {
+            'id': 'id',
+            'u0': 'U_0',
+            'u1': 'U_1',
+            'u2': 'U_2',
+            'u3': 'U_3',
+            'x': 'X',
+            'y': 'Y',
+            'z': 'Z',
+            'h': 'H',
+            's': 'S',
+            'sdg': 'S^\\dagger',
+            't': 'T',
+            'tdg': 'T^\\dagger',
+            'rx': 'R_x',
+            'ry': 'R_y',
+            'rz': 'R_z',
+            'reset': '\\left|0\\right\\rangle'
+        }
+        self.dispcol = {
+            'id': '#F0E442',
+            'u0': '#E7AB3B',
+            'u1': '#E7AB3B',
+            'u2': '#E7AB3B',
+            'u3': '#E7AB3B',
+            'x': '#58C698',
+            'y': '#58C698',
+            'z': '#58C698',
+            'h': '#70B7EB',
+            's': '#E0722D',
+            'sdg': '#E0722D',
+            't': '#E0722D',
+            'tdg': '#E0722D',
+            'rx': '#ffffff',
+            'ry': '#ffffff',
+            'rz': '#ffffff',
+            'reset': '#D188B4',
+            'target': '#70B7EB',
+            'meas': '#D188B4'
+        }
+        self.latexmode = True
+        self.pimode = False
+        self.fold = 20
+        self.bundle = False
+        self.barrier = True
+        self.index = False
+        self.figwidth = -1
+        self.dpi = 150
+        self.margin = [2.0, 0.0, 0.0, 0.3]
+        self.cline = 'doublet'
+
+    def set_style(self, dic):
+        self.tc = dic.get('textcolor', self.tc)
+        self.sc = dic.get('subtextcolor', self.sc)
+        self.lc = dic.get('linecolor', self.lc)
+        self.cc = dic.get('creglinecolor', self.cc)
+        self.gt = dic.get('gatetextcolor', self.tc)
+        self.gc = dic.get('gatefacecolor', self.gc)
+        self.bc = dic.get('barrierfacecolor', self.bc)
+        self.bg = dic.get('backgroundcolor', self.bg)
+        self.fs = dic.get('fontsize', self.fs)
+        self.sfs = dic.get('subfontsize', self.sfs)
+        self.disptex = dic.get('displaytext', self.disptex)
+        for key in self.dispcol.keys():
+            self.dispcol[key] = self.gc
+        self.dispcol = dic.get('displaycolor', self.dispcol)
+        self.latexmode = dic.get('latexdrawerstyle', self.latexmode)
+        self.pimode = dic.get('usepiformat', self.pimode)
+        self.fold = dic.get('fold', self.fold)
+        if self.fold < 2:
+            self.fold = -1
+        self.bundle = dic.get('cregbundle', self.bundle)
+        self.barrier = dic.get('plotbarrier', self.barrier)
+        self.index = dic.get('showindex', self.index)
+        self.figwidth = dic.get('figwidth', self.figwidth)
+        self.dpi = dic.get('dpi', self.dpi)
+        self.margin = dic.get('margin', self.margin)
+        self.cline = dic.get('creglinestyle', self.cline)
+
+
+class BWStyle:
     def __init__(self):
         self.tc = '#000000'
         self.sc = '#000000'
