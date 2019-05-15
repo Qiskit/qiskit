e01fe342b328d92aeaa63c37cae83da54838d714
Diego M. Rodríguez <diego.plan9@gmail.com>
2019-05-15 09:33:54 +0200

    Update style for no-else-raise (#2374)

--- qiskit/visualization/counts_visualization.py
+++ qiskit/visualization/counts_visualization.py
@@ -83,7 +83,7 @@ def plot_histogram(data, figsize=(7, 5), color=None, number_to_keep=None,
         raise VisualizationError("Value of sort option, %s, isn't a "
                                  "valid choice. Must be 'asc', "
                                  "'desc', or 'hamming'")
-    elif sort in DIST_MEAS.keys() and target_string is None:
+    if sort in DIST_MEAS.keys() and target_string is None:
         err_msg = 'Must define target_string when using distance measure.'
         raise VisualizationError(err_msg)
 
