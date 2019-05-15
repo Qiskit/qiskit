a61da373852acd3757dd733736b0cb02612b314f
madeleine-tod <madeleine.tod@ibm.com>
2019-05-15 12:37:17 +0100

    Tidied up the logic

--- qiskit/visualization/matplotlib.py
+++ qiskit/visualization/matplotlib.py
@@ -135,11 +135,9 @@ class MatplotlibDrawer:
                 self._style = DefaultStyle()
             elif config_style == 'bw':
                 self._style = BWStyle()
-        if not config and style is None:
-            self._style = DefaultStyle()
         elif style is False:
             self._style = BWStyle()
-        else :
+        else:
             self._style = DefaultStyle()
 
         self.plot_barriers = plot_barriers
