b41653dce602bcf7e2b435bc67ce194ec106c18e
Kevin Krsulich <kevin.krsulich@ibm.com>
2019-05-17 10:24:03 -0400

    Add transpilation and execution to transpiler hypothesis tests.

--- test/python/transpiler/test_hypothesis.py
+++ test/python/transpiler/test_hypothesis.py
@@ -2,6 +2,8 @@
 # Compile at each optimization level, for each device (with more than n-qubits)
 # Simulate and verify results of every transpiled circuit match that of initial circuit
 
+import numpy as np
+
 from hypothesis import given
 from hypothesis.stateful import multiple, rule, precondition, invariant, Bundle, RuleBasedStateMachine
 import hypothesis.strategies as st
@@ -19,11 +21,11 @@ from qiskit.extensions.standard import *
 
 # TBD, conditionals, Parameters
 
-oneQ_gates = [ HGate, IdGate, SGate, SdgGate, TGate, TdgGate, U0Gate, XGate, YGate, ZGate, Reset ]
+oneQ_gates = [ HGate, IdGate, SGate, SdgGate, TGate, TdgGate, XGate, YGate, ZGate, Reset ]
 twoQ_gates = [ CnotGate, CyGate, CzGate, SwapGate, CHGate ]
 threeQ_gates = [ ToffoliGate, FredkinGate ]
 
-oneQ_oneP_gates = [ U1Gate, RXGate, RYGate, RZGate ]
+oneQ_oneP_gates = [ U0Gate, U1Gate, RXGate, RYGate, RZGate ]
 # oneQ_twoP_gates = [ U2Gate ]
 # oneQ_threeP_gates = [ U3Gate ]
 
@@ -42,75 +44,87 @@ class QCircuitMachine(RuleBasedStateMachine):
         super().__init__()
         self.qc = QuantumCircuit()
 
-    @precondition(lambda self: len(self.qc.qubits) <= 5)
+    @precondition(lambda self: len(self.qc.qubits) < 5)
     @rule(target=qubits, n=st.integers(min_value=1, max_value=5))
     def add_qreg(self, n):
         n = max(n, 5 - len(self.qc.qubits))
         qreg = QuantumRegister(n)
         self.qc.add_register(qreg)
-        return multiple(list(qreg))
+        return multiple(*list(qreg))
 
     @rule(target=clbits, n=st.integers(1, 5))
     def add_creg(self, n):
         creg = ClassicalRegister(n)
         self.qc.add_register(creg)
-        return multiple(list(creg))
+        return multiple(*list(creg))
 
     ### Gates of various shapes
 
-    #@precondition(_at_least_n_qubits(self, 1))
-    @rule(gate=st.one_of(oneQ_gates), qarg=st.lists(qubits))
+    @rule(gate=st.sampled_from(oneQ_gates), qarg=qubits)
     def add_1q_gate(self, gate, qarg):
         self.qc.append(gate(), [qarg], [])
     
-    # @precondition(lambda self: len(self.qc.qubits) > 1)
-    # @rule(gate=st.sampled_from(2q_gates), ctrl=st.integers(), tgt=st.integers())
-    # def add_2q_gate(self, gate, ctrl, tgt):
-    #     qubit_count = len(self.qc.qubits)
-    #     if ctrl % qubit_count != tgt % qubit_count:
-    #         self.qc.append(gate(), ctrl % qubit_count, tgt % qubit_count)
+    @rule(gate=st.sampled_from(twoQ_gates),
+          qargs=st.lists(qubits, max_size=2, min_size=2, unique=True))
+    def add_2q_gate(self, gate, qargs):
+        self.qc.append(gate(), qargs)
 
-    # def add_3q_gate():
-    #     pass
+    @rule(gate=st.sampled_from(threeQ_gates),
+          qargs=st.lists(qubits, max_size=3, min_size=3, unique=True))
+    def add_3q_gate(self, gate, qargs):
+        self.qc.append(gate(), qargs)
 
-    # def add_1q1p_gate():
-    #     pass
+    @rule(gate=st.sampled_from(oneQ_oneP_gates),
+          qarg=qubits,
+          param=st.floats(allow_nan=False, allow_infinity=False))
+    def add_1q1p_gate(self, gate, qarg, param):
+        self.qc.append(gate(param), [qarg])
 
-    # @precondition(lambda self: len(self.qc.qubits) > 1 and len(self.qc.clbits) > 1)
-    # @rule(qubit=st.integers(), clbit=st.integers())
-    # def add_1q1c_gate(self, qubit, cbit):
-    #     self.qc.measure(qubit % len(self.qc.qubits), clbit % len(self.qc.clbits))
+    @rule(gate=st.sampled_from(oneQ_oneC_gates), qarg=qubits, carg=clbits)
+    def add_1q1c_gate(self, gate, qarg, carg):
+        self.qc.append(gate(), [qarg], [carg])
 
-    # @precondition(_at_least_n_qubits(self, 1))
-    # @rule(gate=st.sampled_from(variadic_gates, data=st.data())
-    # def add_variQ_gate(self, gate, data):
-    #     qubits = data.draw(st.lists(self.qc.qubits, min_size=1, unique=True)
-    #     self.qc.append(gate(), qubits, [])
+    @rule(gate=st.sampled_from(variadic_gates),
+          qargs=st.lists(qubits, min_size=1, unique=True))
+    def add_variQ_gate(self, gate, qargs):
+        self.qc.append(gate(len(qargs)), qargs)
 
     # Properties to check
 
     @invariant()
     def qasm(self):
+        # At every step in the process, it should be possible to generate QASM
         self.qc.qasm()
 
-    # Could be an invariant, but then it's run at ~every step
-    @precondition(lambda self: len(self.qc.qubits) > 1)
+    @precondition(lambda self: any(isinstance(d[0], Measure) for d in self.qc.data))
     @rule()
-    def xpile(self):
+    def equivalent_transpile(self):
         aer_qasm_simulator = Aer.get_backend('qasm_simulator')
         basicaer_qasm_simulator = Aer.get_backend('qasm_simulator')
 
-        # If measure/reset => qasm => keys eq, values np.allclose
-        # Else => statevecector? np.allclose
-        execute(self.qc, backend = aer_qasm_simulator).result().get_counts()
+        aer_counts = execute(self.qc, backend = aer_qasm_simulator).result().get_counts()
+        basicaer_counts = execute(self.qc, backend = aer_qasm_simulator).result().get_counts()
+
+        assert counts_equivalent(aer_counts, basicaer_counts), (aer_counts, basicaer_counts)
 
         levels = [0,1,2,3]
         backends = [FakeTenerife(), FakeMelbourne(), FakeRueschlikon(), FakeTokyo()]
-        
-        assertDictAlmostEqual(None, {}, {})
-        
-        
-    
 
+        for level in levels:
+            for backend in backends:
+                xpiled_qc = transpile(self.qc, backend=backend, optimization_level=level)
+
+                xpiled_aer_counts = execute(xpiled_qc, backend = aer_qasm_simulator).result().get_counts()
+                xpiled_basicaer_counts = execute(xpiled_qc, backend = basicaer_qasm_simulator).result().get_counts()
+
+                assert counts_equivalent(aer_counts, xpiled_aer_counts)
+                assert counts_equivalent(basicaer_counts, xpiled_basicaer_counts)
+        
+        
+def counts_equivalent(c1, c2):
+    sc1 = np.array(sorted(c1.items(), key=lambda c: c[0]))
+    sc2 = np.array(sorted(c2.items(), key=lambda c: c[0]))
+
+    return all(sc1[:,0] == sc2[:,0]) and np.allclose(sc1[:,1].astype(int), sc2[:,1].astype(int))
 
 TestQuantumCircuit = QCircuitMachine.TestCase
