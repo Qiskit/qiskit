79c44e5615ddc66d0d083f8715c709f9d3371cd6
Luciano <luciano.bello@ibm.com>
2019-05-21 13:09:39 -0400

    Introducing Qubits and Clbits classes (#2414)
    * bit class
    
    * compare to tuple
    
    * hashable bits
    
    * iter
    
    * getitem
    
    * avoid duplication of code
    
    * test_barrier_none
    
    * allow direct access to index and register
    
    * layout
    
    * snapshot
    
    * conditional
    
    * defintions
    
    * layout
    
    * test.python.circuit.test_circuit_registers.TestCircuitRegisters.test_measure_slice
    
    * removing some tuple representation
    
    * removing some tuple representation
    
    * repr
    
    * None
    
    * test.python.transpiler.test_layout
    
    * dag
    
    * test.python.compiler.test_compiler.TestCompiler.test_compile_single_qubit
    
    * test.python.compiler.test_compiler
    
    * order
    
    * StochasticSwap
    
    * lint
    
    * register have order
    
    * lint
    
    * test.python.visualization.test_visualization
    
    * test.python.visualization.test_circuit_text_drawer
    
    * test.python.basicaer.test_basicaer_integration
    
    * test.python.compiler.test_transpiler
    
    * test.python.transpiler.test_remove_diagonal_gates_before_measure
    
    * test.python.transpiler.test_stochastic_swap.TestStochasticSwap
    
    * test.python.transpiler.test_unroller
    
    * test.python.circuit.test_circuit_registers
    
    * test.python.transpiler.test_noise_adaptive_layout
    
    * test.python.quantum_info.operators.test_operator.TestOperator
    
    * mappers
    
    * pickles
    
    * latex lint
    
    * test.python.transpiler.test_full_ancilla_allocation
    
    * test.python.transpiler.test_consolidate_blocks
    
    * test.python.transpiler.test_commutative_cancellation
    
    * test.python.transpiler.test_basic_swap
    
    * test.python.quantum_info.operators.channel.test_superop
    
    * lint qiskit/converters/ast_to_dag.py
    
    * test/python/test_dagcircuit.py
    
    * test/python/test_dagcircuit.py
    
    * test.python.compiler.test_assembler
    
    * test.python.circuit.test_unitary
    
    * condition
    
    * test.python.circuit.test_circuit_properties
    
    * instruction.control
    
    * lint
    
    * latex condition
    
    * qiskit/dagcircuit/dagcircuit.py
    
    * qiskit/circuit/quantumcircuit.py
    
    * qiskit/assembler/assemble_circuits.py
    
    * test for 1898
    
    * https://github.com/Qiskit/qiskit-terra/pull/2414#discussion_r284383842
    
    * docstrings
    
    * removing order by comparing sets in Collect2qBlocks
    
    * QuBit-> Qubit and ClBit-> Clbit
    
    * errors
    
    * lint
    
    * QuBit -> Qubit
    
    * ctrl_reg, ctrl_val = instruction._control
    
    * https://github.com/Qiskit/qiskit-terra/pull/2414/files/ff8770f7b195f66997a45f01943059d1c6ffaf13#r284695050
    
    * explicit globals
    
    * lint
    
    * __getitem__ for deprecation
    
    * from_tuple
    
    * new pickles
    
    * docstring
    
    * accept gate arguments as tuples and mark them as deprecated
    
    * this fix contradicts whats expected from a negative key lookup
    
    * promote __iter__
    
    * simpler negative index check...
    
    * https://github.com/Qiskit/qiskit-terra/pull/2414/files#r284542889
    
    * docstring lint
    
    * getitem
    
    * layout backwards compatibility
    
    * lint
    
    * lint!
    
    * deprecated messager only when _is_bit
    
    * changelog
    
    * expose less the contructor
    
    * remove prints and raised text
    
    * simplifications
    
    * lint

--- qiskit/transpiler/passes/mapping/set_layout.py
+++ qiskit/transpiler/passes/mapping/set_layout.py
@@ -16,7 +16,7 @@
 Sets property_set['layout'] to layout.
 
 This pass associates a physical qubit (int) to each virtual qubit
-of the circuit (tuple(QuantumRegister, int)) in increasing order.
+of the circuit (Qubit) in increasing order.
 """
 
 from qiskit.transpiler.basepasses import AnalysisPass
