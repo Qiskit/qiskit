66283e446837dbe93fef75dd254a5b6f1aaa8478
godott <synisnot@gmail.com>
2019-05-20 23:42:31 -0400

    License updated; code refactored

--- qiskit/quantum_info/operators/operator.py
+++ qiskit/quantum_info/operators/operator.py
@@ -1,9 +1,17 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2019, IBM.
+# This code is part of Qiskit.
 #
-# This source code is licensed under the Apache License, Version 2.0 found in
-# the LICENSE.txt file in the root directory of this source tree.
+# (C) Copyright IBM 2017, 2019.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
 """
 Matrix Operator class.
 """
@@ -277,12 +285,13 @@ class Operator(BaseOperator):
             QiskitError: if other is not an operators, or has incompatible
             dimensions.
         """
+
         if not isinstance(other, Operator):
             other = Operator(other)
         if self.dim != other.dim:
             raise QiskitError("other operator has different dimensions.")
-        return Operator(self.data * other.data - other.data * self.data, self.input_dims(),
-                        self.output_dims())
+
+	return self @ other - other @ self
 
     @property
     def _shape(self):
--- test/python/transpiler/test_commutative_cancellation.py
+++ test/python/transpiler/test_commutative_cancellation.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2019, IBM.
+# This code is part of Qiskit.
 #
-# This source code is licensed under the Apache License, Version 2.0 found in
-# the LICENSE.txt file in the root directory of this source tree.
+# (C) Copyright IBM 2017, 2019.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
 
 """Gate cancellation pass testing"""
 
@@ -101,9 +108,9 @@ class TestCommutativeCancellation(QiskitTestCase):
     def test_consecutive_cnots(self):
         """A simple circuit equals identity
 
-        qr0:----.- ----.--       qr0:----.-----.--
-                |      |                 |     |
-        qr1:---(+)----(+)-   =   qr1:---(+)---(+)-
+        qr0:----.- ----.--       qr0:------------
+                |      |
+        qr1:---(+)----(+)-   =   qr1:------------
         """
 
         qr = QuantumRegister(2, 'qr')
