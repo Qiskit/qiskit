3a7d583d55eefc93cf06151ecceb3a6b0534fa26
Raban Iten <ite@beatnix.zurich.ibm.com>
2019-05-23 15:46:53 +0200

    Updated test to use UnitarySimulator

--- test/python/circuit/test_diag.py
+++ test/python/circuit/test_diag.py
@@ -17,6 +17,9 @@ from qiskit import BasicAer
 import numpy as np
 from qiskit import execute
 from qiskit.test import QiskitTestCase
+import math
+
+_EPS = 1e-10  # global variable used to chop very small numbers to zero
 
 
 class TestDiagGate(QiskitTestCase):
@@ -28,50 +31,29 @@ class TestDiagGate(QiskitTestCase):
                 diag = [np.exp(1j * ph) for ph in phases]
                 num_qubits = int(np.log2(len(diag)))
                 q = QuantumRegister(num_qubits)
-                # test the diagonal gate for all possible basis states.
-                for i in range(2 ** (num_qubits)):
-                    qc = _prepare_basis_state(q, i)
-                    qc.diag(diag, q[0:num_qubits])
-                    job = execute(qc, BasicAer.get_backend('statevector_simulator'))
-                    result = job.result()
-                    vec_out = result.get_statevector()
-                    vec_desired = _apply_diag_gate_to_basis_state(phases, i)
-                    if i == 0:
-                        global_phase = vec_out[0] / vec_desired[0]
-                    vec_desired = (global_phase * vec_desired).tolist()
-                    dist = np.linalg.norm(np.array(vec_desired - vec_out))
-                    self.assertAlmostEqual(dist, 0)
+                qc = QuantumCircuit(q)
+                qc.diag(diag, q[0:num_qubits])
+                simulator = BasicAer.get_backend('unitary_simulator')
+                result = execute(qc, simulator).result()
+                unitary = result.get_unitary(qc)
+                unitary_desired = _get_diag_gate_matrix(diag)
+                self.assertTrue(is_identity_up_to_global_phase(np.dot(unitary,ct(unitary_desired))))
 
 
-def _apply_diag_gate_to_basis_state(phases, basis_state):
-    # ToDo: improve efficiency here by implementing a simulation for diagonal gates
-    num_qubits = int(np.log2(len(phases)))
-    ph = phases[basis_state]
-    state = np.zeros(2 ** num_qubits, dtype=complex)
-    state[basis_state] = np.exp(1j * ph)
-    return state
+def _get_diag_gate_matrix(diag):
+    return np.diagflat(diag)
 
 
-def _get_binary_rep_as_list(n, num_digits):
-    binary_string = np.binary_repr(n).zfill(num_digits)
-    binary = []
-    for line in binary_string:
-        for c in line:
-            binary.append(int(c))
-    return binary
+def is_identity_up_to_global_phase(m):
+    if not abs(abs(m[0, 0])-1) < _EPS:
+        return False
+    phase = m[0, 0]
+    err = np.linalg.norm(1/phase * m - np.eye(m.shape[1], m.shape[1]))
+    return math.isclose(err, 0, abs_tol=_EPS)
 
 
-def _prepare_basis_state(q, i):
-    num_qubits = len(q)
-    qc = QuantumCircuit(q)
-    # ToDo: Remove this work around after the state vector simulator is fixed (it can't simulate the empty
-    # ToDo: circuit at the moment)
-    qc.iden(q[0])
-    binary_rep = _get_binary_rep_as_list(i, num_qubits)
-    for j in range(len(binary_rep)):
-        if binary_rep[j] == 1:
-            qc.x(q[- (j + 1)])
-    return qc
+def ct(m):
+    return np.transpose(np.conjugate(m))
 
 
 if __name__ == '__main__':
