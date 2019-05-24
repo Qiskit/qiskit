47b573bd32ee668847b0b87c2b222e52458e6102
Raban Iten <ite@beatnix.zrl-ch.ibmmobiledemo.com>
2019-05-24 16:51:49 +0200

    Updated test for ucr

--- test/python/circuit/test_ucy_and_ucz.py
+++ test/python/circuit/test_ucy_and_ucz.py
@@ -21,6 +21,8 @@ from qiskit import QuantumCircuit
 from qiskit import QuantumRegister
 from qiskit import execute
 from qiskit.test import QiskitTestCase
+from scipy.linalg import block_diag
+import math
 
 angles_list = [[0], [0.4], [0, 0], [0, 0.8], [0, 0, 1, 1], [0, 1, 0.5, 1],
           (2 * np.pi * np.random.rand(2 ** 3)).tolist(), (2 * np.pi * np.random.rand(2 ** 4)).tolist(),
@@ -36,73 +38,36 @@ class TestUCY(QiskitTestCase):
             with self.subTest(angles=angles, rot_axis=rot_axis):
                 num_contr = int(np.log2(len(angles)))
                 q = QuantumRegister(num_contr + 1)
-                # test the UC R_y gate for all possible basis states.
-                for i in range(2 ** (num_contr + 1)):
-                    qc = _prepare_basis_state(q, i, num_contr + 1)
-                    if rot_axis == "Y":
-                        qc.ucy(angles, q[1:num_contr + 1], q[0])
-                    else:
-                        qc.ucz(angles, q[1:num_contr + 1], q[0])
-                    # ToDo: improve efficiency here by allowing to execute circuit on several states in parallel (this would
-                    # ToDo: in particular allow to get out the isometry the circuit is implementing by applying it to the first
-                    # ToDo: few basis vectors
-                    job = execute(qc, BasicAer.get_backend('statevector_simulator'))
-                    result = job.result()
-                    vec_out = result.get_statevector()
-                    vec_desired = _apply_ucr_to_basis_state(angles, i, rot_axis)
-                    # It is fine if the gate is implemented up to a global phase (however, the phases between the different
-                    # outputs for different bases states must be correct!)
-                    if i == 0:
-                        global_phase = _get_global_phase(vec_out, vec_desired)
-                    vec_desired = (global_phase * vec_desired).tolist()
-                    dist = np.linalg.norm(np.array(vec_desired - vec_out))
-                    self.assertAlmostEqual(dist, 0)
+                qc = QuantumCircuit(q)
+                if rot_axis == "Y":
+                    qc.ucy(angles, q[1:num_contr + 1], q[0])
+                else:
+                    qc.ucz(angles, q[1:num_contr + 1], q[0])
+                simulator = BasicAer.get_backend('unitary_simulator')
+                result = execute(qc, simulator).result()
+                unitary = result.get_unitary(qc)
+                unitary_desired = _get_ucr_matrix(angles, rot_axis)
+                self.assertTrue(is_identity_up_to_global_phase(np.dot(ct(unitary), unitary_desired)))
 
 
-def _apply_ucr_to_basis_state(angle_list, basis_state, rot_axis):
-    num_qubits = int(np.log2(len(angle_list)) + 1)
-    angle = angle_list[basis_state // 2]
+def _get_ucr_matrix(angles, rot_axis):
     if rot_axis == "Y":
-        r = np.array([[np.cos(angle / 2), - np.sin(angle / 2)], [np.sin(angle / 2), np.cos(angle / 2)]])
+        gates = [np.array([[np.cos(angle / 2), - np.sin(angle / 2)], [np.sin(angle / 2), np.cos(angle / 2)]]) for angle in angles]
     else:
-        r = np.array([[np.exp(-1.j * angle / 2), 0], [0, np.exp(1.j * angle / 2)]])
-    state = np.zeros(2 ** num_qubits, dtype=complex)
-    if basis_state / 2. == float(basis_state // 2):
-        target_state = np.dot(r, np.array([[1], [0]]))
-        state[basis_state] = target_state[0, 0]
-        state[basis_state + 1] = target_state[1, 0]
-    else:
-        target_state = np.dot(r, np.array([[0], [1]]))
-        state[basis_state - 1] = target_state[0, 0]
-        state[basis_state] = target_state[1, 0]
-    return state
+        gates = [np.array([[np.exp(-1.j * angle / 2), 0], [0, np.exp(1.j * angle / 2)]]) for angle in angles]
+    return block_diag(*gates)
 
 
-def _prepare_basis_state(q, i, num_qubits):
-    qc = QuantumCircuit(q)
-    # ToDo: Remove this work around after the state vector simulator is fixed (it can't simulate the empty
-    # ToDo: circuit at the moment)
-    qc.iden(q[0])
-    binary_rep = _get_binary_rep_as_list(i, num_qubits)
-    for j in range(len(binary_rep)):
-        if binary_rep[j] == 1:
-            qc.x(q[- (j + 1)])
-    return qc
+def is_identity_up_to_global_phase(m):
+    if not abs(abs(m[0, 0])-1) < _EPS:
+        return False
+    phase = m[0, 0]
+    err = np.linalg.norm(1/phase * m - np.eye(m.shape[1], m.shape[1]))
+    return math.isclose(err, 0, abs_tol=_EPS)
 
 
-def _get_global_phase(a, b):
-    for i in range(len(b)):
-        if abs(b[i]) > _EPS:
-            return a[i] / b[i]
-
-
-def _get_binary_rep_as_list(n, num_digits):
-    binary_string = np.binary_repr(n).zfill(num_digits)
-    binary = []
-    for line in binary_string:
-        for c in line:
-            binary.append(int(c))
-    return binary
+def ct(m):
+    return np.transpose(np.conjugate(m))
 
 
 if __name__ == '__main__':
