66c10fce9838c1601fb9c94850199f381cf1c28b
Raban Iten <ite@beatnix.zrl-ch.ibmmobiledemo.com>
2019-05-24 15:45:41 +0200

    Updated test for isometries

--- test/python/circuit/test_isometry.py
+++ test/python/circuit/test_isometry.py
@@ -24,6 +24,8 @@ from qiskit import QuantumRegister
 from qiskit import execute
 from qiskit.test import QiskitTestCase
 
+import math
+
 _EPS = 1e-10  # global variable used to chop very small numbers to zero
 
 
@@ -37,65 +39,31 @@ class TestUCG(QiskitTestCase):
                     unitary_group.rvs(16)[:, 0:8]]:
             with self.subTest(iso=iso):
                 if len(iso.shape) == 1:
-                    num_q_input = 0
-                else:
-                    num_q_input = int(np.log2(iso.shape[1]))
+                    iso = iso.reshape((len(iso), 1))
+                num_q_input = int(np.log2(iso.shape[1]))
                 num_q_ancilla_for_output = int(np.log2(iso.shape[0])) - num_q_input
                 n = num_q_input + num_q_ancilla_for_output
                 q = QuantumRegister(n)
-                # test the isometry for all possible input basis states.
-                for i in range(2 ** num_q_input):
-                    qc = _prepare_basis_state(q, i)
-                    qc.iso(iso, q[:num_q_input], q[num_q_input:])
-                    # ToDo: improve efficiency here by allowing to execute circuit on several states in parallel (this would
-                    # ToDo: in particular allow to get out the isometry the circuit is implementing by applying it to the first
-                    # ToDo: few basis vectors)
-                    job = execute(qc, BasicAer.get_backend('statevector_simulator'))
-                    result = job.result()
-                    vec_out = result.get_statevector()
-                    vec_desired = _apply_isometry_to_basis_state(iso, i)
-                    # It is fine if the gate is implemented up to a global phase (however, the phases between the different
-                    # outputs for different bases states must be correct!
-                    if i == 0:
-                        global_phase = _get_global_phase(vec_out, vec_desired)
-                    vec_desired = (global_phase * vec_desired).tolist()
-                    dist = np.linalg.norm(np.array(vec_desired - vec_out))
-                    self.assertAlmostEqual(dist, 0)
+                qc = QuantumCircuit(q)
+                qc.iso(iso, q[:num_q_input], q[num_q_input:])
+                simulator = BasicAer.get_backend('unitary_simulator')
+                result = execute(qc, simulator).result()
+                unitary = result.get_unitary(qc)
+                iso_from_circuit = unitary[::, 0:2**num_q_input]
+                iso_desired = iso
+                self.assertTrue(is_identity_up_to_global_phase(np.dot(ct(iso_from_circuit), iso_desired)))
 
 
-def _prepare_basis_state(q, i):
-    num_qubits = len(q)
-    qc = QuantumCircuit(q)
-    # ToDo: Remove this work around after the state vector simulator is fixed (it can't simulate the empty
-    # ToDo: circuit at the moment)
-    qc.iden(q[0])
-    binary_rep = _get_binary_rep_as_list(i, num_qubits)
-    for j in range(len(binary_rep)):
-        if binary_rep[j] == 1:
-            qc.x(q[- (j + 1)])
-    return qc
+def is_identity_up_to_global_phase(m):
+    if not abs(abs(m[0, 0])-1) < _EPS:
+        return False
+    phase = m[0, 0]
+    err = np.linalg.norm(1/phase * m - np.eye(m.shape[1], m.shape[1]))
+    return math.isclose(err, 0, abs_tol=_EPS)
 
 
-def _apply_isometry_to_basis_state(iso, basis_state):
-    if len(iso.shape) == 1:
-        return iso.reshape(iso.shape[0], 1)[:, basis_state]
-    else:
-        return iso[:, basis_state]
-
-
-def _get_binary_rep_as_list(n, num_digits):
-    binary_string = np.binary_repr(n).zfill(num_digits)
-    binary = []
-    for line in binary_string:
-        for c in line:
-            binary.append(int(c))
-    return binary
-
-
-def _get_global_phase(a, b):
-    for i in range(len(b)):
-        if abs(b[i]) > _EPS:
-            return a[i] / b[i]
+def ct(m):
+    return np.transpose(np.conjugate(m))
 
 
 if __name__ == '__main__':
