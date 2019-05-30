a7b0a3c22f0e7721caa4f6522a72663e2d79b313
Paul Nation <nonhermitian@gmail.com>
2019-05-30 16:09:23 -0400

    fix module header

--- test/python/transpiler/test_kak_over_optimization.py
+++ test/python/transpiler/test_kak_over_optimization.py
@@ -12,7 +12,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Width pass testing"""
+"""Test KAK over optimization"""
 
 import unittest
 
