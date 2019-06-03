a7fdf3a41c1f05f78bec7a61e1f6d4a7f5933e81
Matthew Stypulkoski <Matthew.Stypulkoski@ibm.com>
2019-06-03 15:51:57 -0400

    Add test for checking circuit width before transpiling

--- test/python/compiler/test_transpiler.py
+++ test/python/compiler/test_transpiler.py
@@ -596,8 +596,8 @@ class TestTranspile(QiskitTestCase):
         cmap = [[1, 0], [1, 2], [2, 3], [4, 3], [4, 10], [5, 4],
                 [5, 6], [5, 9], [6, 8], [7, 8], [9, 8], [9, 10],
                 [11, 3], [11, 10], [11, 12], [12, 2], [13, 1], [13, 12]]
-        
-        qc = QuantumCircuit(17, 17)
+
+        qc = QuantumCircuit(15, 15)
 
         with self.assertRaises(TranspilerError):
             transpile(qc, coupling_map=cmap)
