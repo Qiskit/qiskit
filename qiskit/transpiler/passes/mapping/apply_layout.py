f47531563dfee04238a5c17d728d3f802e8cf667
Toshinari Itoko <itoko@jp.ibm.com>
2019-06-07 02:52:54 +0900

    lint

--- qiskit/transpiler/passes/mapping/apply_layout.py
+++ qiskit/transpiler/passes/mapping/apply_layout.py
@@ -1,17 +1,25 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2019, IBM.
+# This code is part of Qiskit.
 #
-# This source code is licensed under the Apache License, Version 2.0 found in
-# the LICENSE.txt file in the root directory of this source tree.
+# (C) Copyright IBM 2019.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
 
 """
 A pass for transforming a circuit with virtual qubits into a circuit with physical qubits.
 """
-from qiskit import QuantumRegister
+from qiskit.circuit import QuantumRegister
 from qiskit.dagcircuit import DAGCircuit
-from qiskit.transpiler import Layout, CouplingMap
-from qiskit.transpiler import TransformationPass
+from qiskit.transpiler.basepasses import TransformationPass
+from qiskit.transpiler.coupling import CouplingMap
+from qiskit.transpiler.layout import Layout
 
 
 class ApplyLayout(TransformationPass):
--- test/python/transpiler/test_apply_layout.py
+++ test/python/transpiler/test_apply_layout.py
@@ -1,18 +1,26 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2018, IBM.
+# This code is part of Qiskit.
 #
-# This source code is licensed under the Apache License, Version 2.0 found in
-# the LICENSE.txt file in the root directory of this source tree.
+# (C) Copyright IBM 2019.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
 
 """Test the ApplyLayout pass"""
 
 import unittest
 
-from qiskit import QuantumRegister, QuantumCircuit, ClassicalRegister
+from qiskit.circuit import QuantumRegister, QuantumCircuit, ClassicalRegister
 from qiskit.converters import circuit_to_dag
-from qiskit.transpiler import CouplingMap, Layout
 from qiskit.test import QiskitTestCase
+from qiskit.transpiler.coupling import CouplingMap
+from qiskit.transpiler.layout import Layout
 from qiskit.transpiler.passes import ApplyLayout
 
 
