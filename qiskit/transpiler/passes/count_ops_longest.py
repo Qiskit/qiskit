96e17a70605f0b82481b14ebe100a477b1ddfff6
Albert Frisch <alfr@de.ibm.com>
2019-07-03 11:21:13 +0200

    update doc string

--- qiskit/transpiler/passes/count_ops_longest.py
+++ qiskit/transpiler/passes/count_ops_longest.py
@@ -12,13 +12,15 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-""" An analysis pass for counting operations in a DAG circuit.
+""" An analysis pass for counting operations on the longest path in a DAG
+circuit.
 """
 from qiskit.transpiler.basepasses import AnalysisPass
 
 
 class CountOpsLongest(AnalysisPass):
-    """ An analysis pass for counting operations in a DAG circuit.
+    """ An analysis pass for counting operations on the longest path in a DAG
+    circuit.
     """
 
     def run(self, dag):
