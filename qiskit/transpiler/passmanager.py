1f95361032c72443fa940eec4614de78f79d3304
Luciano <luciano.bello@ibm.com>
2019-07-03 21:42:15 -0400

    Saving circuit.layout after transpiling (#2736)
    * from unittest.mock import patch
    
    * .
    
    * alternative to 2721
    
    * test
    
    * remove subtest

--- qiskit/transpiler/passmanager.py
+++ qiskit/transpiler/passmanager.py
@@ -139,6 +139,7 @@ class PassManager():
 
         circuit = dag_to_circuit(dag)
         circuit.name = name
+        circuit.layout = self.property_set['layout']
         return circuit
 
     def draw(self, filename, style=None, raw=False):
