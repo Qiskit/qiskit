854509177204e34dc491d70f5aed17d38eddc1b3
Maddy Tod <40489777+maddy-tod@users.noreply.github.com>
2019-07-10 11:49:36 -0400

    Fixed DAG.layers() to be deterministic (#2740)
    * Updated the layers method to only add the registers once
    
    * Added in a test and made it so the nodes are added properly
    
    * Added in a sort to the order of the nodes so that the order is maintained between DAGs
    
    * Improved redundant code and added a more detailed comment
    
    * Added another comment to clarify the return
    
    * Removed some redundant code

--- qiskit/dagcircuit/dagcircuit.py
+++ qiskit/dagcircuit/dagcircuit.py
@@ -1015,6 +1015,10 @@ class DAGCircuit:
         greedy algorithm. Each returned layer is a dict containing
         {"graph": circuit graph, "partition": list of qubit lists}.
 
+        New but semantically equivalent DAGNodes will be included in the returned layers,
+        NOT the DAGNodes from the original DAG. The original vs new nodes can be compared using
+        DAGNode.semantic_eq(node1, node2)
+
         TODO: Gates that use the same cbits will end up in different
         layers as this is currently implemented. This may not be
         the desired behavior.
@@ -1025,15 +1029,17 @@ class DAGCircuit:
         except StopIteration:
             return
 
-        def add_nodes_from(layer, nodes):
-            """ Convert DAGNodes into a format that can be added to a
-             multigraph and then add to graph"""
-            layer._multi_graph.add_nodes_from(nodes)
-
         for graph_layer in graph_layers:
 
             # Get the op nodes from the layer, removing any input and output nodes.
             op_nodes = [node for node in graph_layer if node.type == "op"]
+
+            # Sort to make sure they are in the order they were added to the original DAG
+            # It has to be done by node_id as graph_layer is just a list of nodes
+            # with no implied topology
+            # Drawing tools that rely on _node_id to infer order of node creation
+            # so we need this to be preserved by layers()
+            op_nodes.sort(key=lambda nd: nd._node_id)
 
             # Stop yielding once there are no more op_nodes in a layer.
             if not op_nodes:
@@ -1043,36 +1049,26 @@ class DAGCircuit:
             new_layer = DAGCircuit()
             new_layer.name = self.name
 
+            # add in the registers - this adds the input/output nodes
             for creg in self.cregs.values():
                 new_layer.add_creg(creg)
             for qreg in self.qregs.values():
                 new_layer.add_qreg(qreg)
 
-            add_nodes_from(new_layer, self.input_map.values())
-            add_nodes_from(new_layer, self.output_map.values())
-            add_nodes_from(new_layer, op_nodes)
+            for node in op_nodes:
+                # this creates new DAGNodes in the new_layer
+                new_layer.apply_operation_back(node.op,
+                                               node.qargs,
+                                               node.cargs,
+                                               node.condition)
 
             # The quantum registers that have an operation in this layer.
             support_list = [
                 op_node.qargs
-                for op_node in op_nodes
+                for op_node in new_layer.op_nodes()
                 if op_node.name not in {"barrier", "snapshot", "save", "load", "noise"}
             ]
 
-            # Now add the edges to the multi_graph
-            # By default we just wire inputs to the outputs.
-            wires = {self.input_map[wire]: self.output_map[wire]
-                     for wire in self.wires}
-            # Wire inputs to op nodes, and op nodes to outputs.
-            for op_node in op_nodes:
-                args = self._bits_in_condition(op_node.condition) \
-                       + op_node.cargs + op_node.qargs
-                arg_ids = (self.input_map[(arg.register, arg.index)] for arg in args)
-                for arg_id in arg_ids:
-                    wires[arg_id], wires[op_node] = op_node, wires[arg_id]
-
-            # Add wiring to/from the operations and between unused inputs & outputs.
-            new_layer._multi_graph.add_edges_from(wires.items())
             yield {"graph": new_layer, "partition": support_list}
 
     def serial_layers(self):
