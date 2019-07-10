ccb0a3dbbe853296ea03070a3db25ea8e8c24958
Toshinari Itoko <itoko@jp.ibm.com>
2019-07-10 11:47:07 +0900

    add more tests

--- test/python/transpiler/test_flexlayer_heuristics.py
+++ test/python/transpiler/test_flexlayer_heuristics.py
@@ -17,17 +17,15 @@
 import unittest
 
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
-from qiskit.converters import circuit_to_dag, dag_to_circuit
+from qiskit.converters import circuit_to_dag
 from qiskit.transpiler import CouplingMap, Layout
 from qiskit.transpiler.passes.mapping.algorithm.dependency_graph import DependencyGraph
 from qiskit.transpiler.passes.mapping.algorithm.flexlayer_heuristics import FlexlayerHeuristics
-from qiskit.transpiler.passes.mapping.algorithm.flexlayer_heuristics import remove_head_swaps
 
 
 class TestFlexlayerHeuristics(unittest.TestCase):
     """Tests for FlexlayerHeuristics."""
 
-    @unittest.skip("due to a bug in DAGCircuit.__eq__()")
     def test_search_4qcx4h1(self):
         """Test for 4 cx gates and 1 h gate in a 4q circuit.
         """
@@ -40,13 +38,13 @@ class TestFlexlayerHeuristics(unittest.TestCase):
         circuit.h(qr[1])
         circuit.cx(qr[1], qr[0])
         circuit.barrier()
-        for i in range(4):
-            circuit.measure(qr[i], cr[i])
+        circuit.measure(qr, cr)
+
         dep_graph = DependencyGraph(circuit, graph_type="basic")
         coupling = CouplingMap([[0, 1], [1, 2], [1, 3]])  # {0: [1], 1: [2, 3]}
         initial_layout = Layout.generate_trivial_layout(qr)
         algo = FlexlayerHeuristics(circuit, dep_graph, coupling, initial_layout)
-        actual_dag, layout = algo.search()
+        actual_dag, actual_last_layout = algo.search()
 
         expected = QuantumCircuit(qr, cr)
         expected.cx(qr[0], qr[1])
@@ -61,16 +59,10 @@ class TestFlexlayerHeuristics(unittest.TestCase):
         expected.measure(qr[2], cr[1])
         expected.measure(qr[0], cr[2])
         expected.measure(qr[3], cr[3])
-        expected_layout = initial_layout
-        from qiskit.tools.visualization import dag_drawer
-        dag_drawer(actual_dag)
-        dag_drawer(circuit_to_dag(expected))
-        print(dag_to_circuit(actual_dag).draw())
-        print(expected.draw())
+        expected_last_layout = Layout.from_qubit_list([qr[2], qr[0], qr[1], qr[3]])
         self.assertEqual(actual_dag, circuit_to_dag(expected))
-        self.assertEqual(layout, expected_layout)
+        self.assertEqual(str(actual_last_layout), str(expected_last_layout))
 
-    @unittest.skip("TODO: Change to use DAGCircuit and Layout")
     def test_search_multi_creg(self):
         """Test for multiple ClassicalRegisters.
         """
@@ -88,95 +80,29 @@ class TestFlexlayerHeuristics(unittest.TestCase):
         circuit.measure(qr[1], cr1[1])
         circuit.measure(qr[2], cr2[0])
         circuit.measure(qr[3], cr2[1])
+
         dep_graph = DependencyGraph(circuit, graph_type="basic")
         coupling = CouplingMap([[0, 1], [1, 2], [1, 3]])  # {0: [1], 1: [2, 3]}
         initial_layout = Layout.generate_trivial_layout(qr)
         algo = FlexlayerHeuristics(circuit, dep_graph, coupling, initial_layout)
-        qc, layout = algo.search()
-        actual_measures = [s for s in qc.qasm().split('\n') if s.startswith("measure")]
-        expected_measures = []
-        expected_measures.append("measure q[0] -> d[0];")
-        expected_measures.append("measure q[1] -> c[0];")
-        expected_measures.append("measure q[2] -> c[1];")
-        expected_measures.append("measure q[3] -> d[1];")
-        expected_layout = initial_layout
-        self.assertEqual(sorted(actual_measures), expected_measures)
-        self.assertEqual(layout, expected_layout)
+        actual_dag, actual_last_layout = algo.search()
 
-    @unittest.skip("TODO: Change to use DAGCircuit and Layout")
-    def test_remove_head_swaps(self):
-        """Test for removing unnecessary swap gates from qc by changing initial_layout.
-        """
-        qr = QuantumRegister(4, 'q')
-        cr = ClassicalRegister(4, 'c')
-        circuit = QuantumCircuit(qr, cr)
-        circuit.cx(qr[0], qr[1])
-        circuit.u1(1, qr[2])
-        circuit.swap(qr[2], qr[3])
-        circuit.cx(qr[1], qr[2])
-        for i in range(4):
-            circuit.measure(qr[i], cr[i])
-        initial_layout = {('q', i): ('q', i) for i in range(4)}
-        resqc, layout = remove_head_swaps(circuit, initial_layout)
-        actual_qasm = ''.join([s for s in resqc.qasm().split('\n') if not s.startswith("measure")])
-        actual_measure = sorted([s for s in resqc.qasm().split('\n') if s.startswith("measure")])
-        header = 'OPENQASM 2.0;include "qelib1.inc";qreg q[4];creg c[4];'
-        expected_qasm = header + "cx q[0],q[1];u1(1) q[3];cx q[1],q[2];"
-        expected_measure = ["measure q[%d] -> c[%d];" % (i, i) for i in range(4)]
-        expected_layout = {('q', 0): ('q', 0), ('q', 1): ('q', 1), ('q', 3): ('q', 2),
-                           ('q', 2): ('q', 3)}
-        self.assertEqual(actual_qasm, expected_qasm)
-        self.assertEqual(actual_measure, expected_measure)
-        self.assertEqual(layout, expected_layout)
-
-    # @unittest.skip("WIP")
-    # def test_search_rd32_v0_67(self):
-    #     qp = QuantumProgram()
-    #     qasm_text = 'OPENQASM 2.0;\
-    #         include "qelib1.inc";\
-    #         gate peres a,b,c {ccx a, b, c; cx a, b;}\
-    #         qreg q[4];\
-    #         creg c[4];\
-    #         peres q[0], q[1], q[3];\
-    #         peres q[1], q[2], q[3];\
-    #         barrier q;\
-    #         measure q->c;'
-    #     qc = load_qasm_string(qasm_text, basis_gates="u1,u2,u3,cx,id")
-    #     dg = DependencyGraph(qc, graph_type="basic")
-    #     coupling = Coupling({1: [0], 2: [0, 1, 4], 3: [2, 4]})
-    #     initial_layout = {('q', 0): ('q', 1), ('q', 1): ('q', 0), ('q', 2): ('q', 2),
-    #                       ('q', 3): ('q', 4)}
-    #     algo = FlexlayerHeuristics(qc, dg, coupling, initial_layout)
-    #     dag, layout = algo.search()
-
-    @unittest.skip("TODO: Change to use DAGCircuit and Layout")
-    def test_remove_head_swaps_and_remain_middle_swaps(self):
-        """Test for only head swaps are removed, and middle swaps are remained properly.
-        """
-        qr = QuantumRegister(6, 'q')
-        cr = ClassicalRegister(6, 'c')
-        circuit = QuantumCircuit(qr, cr)
-        circuit.u1(1, qr[4])
-        circuit.h(qr[3])
-        circuit.swap(qr[4], qr[5])
-        circuit.cx(qr[3], qr[4])
-        circuit.swap(qr[3], qr[4])
-        circuit.u1(1, qr[1])
-        circuit.h(qr[0])
-        circuit.swap(qr[0], qr[1])
-        circuit.swap(qr[1], qr[2])
-        circuit.swap(qr[2], qr[3])
-        circuit.cx(qr[3], qr[4])
-        initial_layout = {('b', i): ('q', i) for i in range(6)}
-        resqc, layout = remove_head_swaps(circuit, initial_layout)
-        actual_qasm = ''.join([s for s in resqc.qasm().split('\n')])
-        header = 'OPENQASM 2.0;include "qelib1.inc";qreg q[6];creg c[6];'
-        expected_qasm = header + "u1(1) q[5];h q[3];cx q[3],q[4];swap q[3],q[4];u1(1) q[0];" \
-                                 "h q[2];swap q[2],q[3];cx q[3],q[4];"
-        expected_layout = {('b', 0): ('q', 2), ('b', 1): ('q', 0), ('b', 2): ('q', 1),
-                           ('b', 3): ('q', 3), ('b', 4): ('q', 5), ('b', 5): ('q', 4)}
-        self.assertEqual(actual_qasm, expected_qasm)
-        self.assertEqual(layout, expected_layout)
+        expected = QuantumCircuit(qr, cr1, cr2)
+        expected.cx(qr[0], qr[1])
+        expected.swap(qr[1], qr[2])
+        expected.cx(qr[1], qr[3])
+        expected.cx(qr[2], qr[1])
+        expected.h(qr[2])
+        expected.swap(qr[0], qr[1])
+        expected.cx(qr[2], qr[1])
+        expected.barrier()
+        expected.measure(qr[0], cr2[0])
+        expected.measure(qr[1], cr1[0])
+        expected.measure(qr[2], cr1[1])
+        expected.measure(qr[3], cr2[1])
+        expected_last_layout = Layout.from_qubit_list([qr[2], qr[0], qr[1], qr[3]])
+        self.assertEqual(actual_dag, circuit_to_dag(expected))
+        self.assertEqual(str(actual_last_layout), str(expected_last_layout))
 
 
 if __name__ == "__main__":
--- test/python/transpiler/test_flexlayer_swap.py
+++ test/python/transpiler/test_flexlayer_swap.py
@@ -16,7 +16,7 @@
 
 import unittest
 
-from qiskit import QuantumRegister, QuantumCircuit
+from qiskit import QuantumRegister, QuantumCircuit, ClassicalRegister
 from qiskit.converters import circuit_to_dag
 from qiskit.test import QiskitTestCase
 from qiskit.transpiler import CouplingMap
@@ -28,7 +28,7 @@ class TestFlexlayerSwap(QiskitTestCase):
 
     def test_trivial_case(self):
         """No need to have any swap, the CX are distance 1 to each other
-         q0:--(+)-[U]-(+)-
+         q0:--(+)-[H]-(+)-
                |       |
          q1:---.-------|--
                        |
@@ -83,40 +83,90 @@ class TestFlexlayerSwap(QiskitTestCase):
 
         self.assertEqual(circuit_to_dag(expected), after)
 
+    def test_flexlayer_swap_maps_measurements(self):
+        """Verify measurement nodes are updated to map correct cregs to re-mapped qregs.
+
+        Create a circuit with measures on q1 and q2, following a cx between q1 and q2.
+        Since (1, 2) is not in the coupling, one of the two will be required to move.
+        Verify that the mapped measure corresponds to one of the two possible layouts following
+        the swap.
+        """
+        coupling = CouplingMap([[0, 1], [0, 2]])
+
+        qr = QuantumRegister(3, 'q')
+        cr = ClassicalRegister(2)
+        circuit = QuantumCircuit(qr, cr)
+        circuit.cx(qr[1], qr[2])
+        circuit.measure(qr[1], cr[0])
+        circuit.measure(qr[2], cr[1])
+        dag = circuit_to_dag(circuit)
+
+        expected = QuantumCircuit(qr, cr)
+        expected.swap(qr[0], qr[2])
+        expected.cx(qr[1], qr[0])
+        expected.measure(qr[1], cr[0])
+        expected.measure(qr[0], cr[1])  # <- changed due to swap insertion
+
+        pass_ = FlexlayerSwap(coupling)
+        after = pass_.run(dag)
+
+        self.assertEqual(circuit_to_dag(expected), after)
+
+    def test_flexlayer_swap_doesnt_modify_mapped_circuit(self):
+        """Test that flexlayer swap is idempotent.
+
+        It should not modify a circuit which is already compatible with the
+        coupling map, and can be applied repeatedly without modifying the circuit.
+        """
+        coupling = CouplingMap([[0, 1], [0, 2]])
+
+        qr = QuantumRegister(3, 'q')
+        cr = ClassicalRegister(2)
+        circuit = QuantumCircuit(qr, cr)
+        circuit.cx(qr[1], qr[2])
+        circuit.measure(qr[1], cr[0])
+        circuit.measure(qr[2], cr[1])
+        dag = circuit_to_dag(circuit)
+
+        mapped_dag = FlexlayerSwap(coupling).run(dag)
+        remapped_dag = FlexlayerSwap(coupling).run(mapped_dag)
+
+        self.assertEqual(mapped_dag, remapped_dag)
+
     def test_far_swap(self):
         """ A far swap that affects coming CXs.
          qr0:--(+)---.--
                 |    |
-         qr1:---|----|--
-                |    |
-         qr2:---|----|--
-                |    |
-         qr3:---.---(+)-
+         qr1:---.----|--
+                     |
+         qr2:--------|--
+                     |
+         qr3:-------(+)-
 
          CouplingMap map: [0]--[1]--[2]--[3]
 
-         qr0:--X--------------
-               |
-         qr1:--X--X-----------
-                  |
-         qr2:-----X--(+)---.--
-                      |    |
-         qr3:---------.---(+)-
+         qr0:-(+)-X----------
+               |  |
+         qr1:--.--X--X-------
+                     |
+         qr2:--------X--.----
+                        |
+         qr3:----------(+)---
 
         """
         coupling = CouplingMap([[0, 1], [1, 2], [2, 3]])
 
         qr = QuantumRegister(4, name='q')
         circuit = QuantumCircuit(qr)
+        circuit.cx(qr[1], qr[0])
         circuit.cx(qr[0], qr[3])
-        circuit.cx(qr[3], qr[0])
         dag = circuit_to_dag(circuit)
 
         expected = QuantumCircuit(qr)
+        expected.cx(qr[1], qr[0])
         expected.swap(qr[0], qr[1])
         expected.swap(qr[1], qr[2])
         expected.cx(qr[2], qr[3])
-        expected.cx(qr[3], qr[2])
 
         pass_ = FlexlayerSwap(coupling)
         after = pass_.run(dag)
