f5802684e94dd5487c4000e1443869de6471703a
Luciano Bello <luciano.bello@ibm.com>
2019-07-11 14:50:08 -0400

    font-family: SFMono-Regular,Consolas,Liberation Mono,Menlo,Courier,monospace

--- qiskit/visualization/text.py
+++ qiskit/visualization/text.py
@@ -459,7 +459,9 @@ class TextDrawing():
     def _repr_html_(self):
         return '<pre style="word-wrap: normal;' \
                'white-space: pre;' \
-               'line-height: 15px;">%s</pre>' % self.single_string()
+               'line-height: 15px;' \
+               'font-family: SFMono-Regular,Consolas,Liberation Mono,Menlo,Courier,monospace">' \
+               '%s</pre>' % self.single_string()
 
     def _get_qubit_labels(self):
         qubits = []
