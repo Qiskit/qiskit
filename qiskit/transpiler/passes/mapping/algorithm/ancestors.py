504c8a6f30508ab8603ae2862a3203aca09b4414
Toshinari Itoko <itoko@jp.ibm.com>
2019-07-12 19:41:03 +0900

    simplify cache algorithm

--- qiskit/transpiler/passes/mapping/algorithm/ancestors.py
+++ qiskit/transpiler/passes/mapping/algorithm/ancestors.py
@@ -28,11 +28,10 @@ class Ancestors:
     Utility class for speeding up a function to find ancestors in DAG.
     """
 
-    def __init__(self, G: nx.DiGraph, max_depth: int = 5):
+    def __init__(self, G: nx.DiGraph):
         self._graph = G.reverse()
-        self._max_depth = max_depth
-        self._depth = 0
 
+    @lru_cache(maxsize=CACHE_SIZE)
     def ancestors(self, n: int) -> set:
         """
         Ancestor nodes of the node `n`
@@ -41,31 +40,10 @@ class Ancestors:
         Returns:
             Set of indices of the ancestor nodes.
         """
-        self._depth = 0
-        return self._ancestors_rec(n)
-
-    @lru_cache(maxsize=CACHE_SIZE)
-    def _ancestors_rec(self, n: int) -> set:
-        if self._depth >= self._max_depth:
-            return self._ancestors_loop(n)
-        self._depth += 1
         ret = set()
         for n_succ in self._graph.successors(n):
+            if n_succ in ret:
+                continue
             ret.add(n_succ)
-            ret.update(self._ancestors_rec(n_succ))
-        self._depth -= 1
-        return ret
-
-    @lru_cache(maxsize=CACHE_SIZE)
-    def _ancestors_loop(self, n: int) -> set:
-        ret = set()
-        done = set()
-        cands = [n]
-        while cands:
-            node = cands.pop()
-            for v in self._graph.successors(node):
-                if v not in done:
-                    ret.add(v)
-                    cands.append(v)
-            done.add(node)
+            ret.update(self.ancestors(n_succ))
         return ret
