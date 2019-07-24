87d4104ba6cd7c58beac3fdc4d259dec5f53f350
Yotam Vaknin <Yotam.Vaknin@Ibm.com>
2019-07-24 17:23:29 +0300

    Fixed is complex function

--- qiskit/validation/fields/custom.py
+++ qiskit/validation/fields/custom.py
@@ -95,7 +95,7 @@ class InstructionParameter(ModelTypeValidator):
         if isinstance(value, sympy.Symbol):
             return str(value)
         if isinstance(value, sympy.Basic):
-            if sympy.im(value) != 0:
+            if sympy.is_complex:
                 return [float(sympy.re(value)), float(sympy.im(value))]
             if value.is_Integer:
                 return int(value.evalf())
