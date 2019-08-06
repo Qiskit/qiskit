a9583f0149f6ff5a986fa085f1f8b324ef1e4f66
Luciano Bello <luciano.bello@ibm.com>
2019-08-06 16:45:57 -0400

    add a test for plot_circuit_layout

--- test/python/visualization/test_gate_map.py
+++ test/python/visualization/test_gate_map.py
@@ -18,7 +18,9 @@ import os
 import matplotlib.pyplot as plt
 from qiskit.test.mock import FakeProvider
 from qiskit.test import QiskitTestCase
-from qiskit.visualization.gate_map import _GraphDist, plot_gate_map
+from qiskit.visualization.gate_map import _GraphDist, plot_gate_map, plot_circuit_layout
+from qiskit import QuantumRegister, QuantumCircuit
+from qiskit.transpiler import Layout
 from .visualization import path_to_diagram_reference, QiskitVisualizationTestCase
 
 
@@ -44,9 +46,24 @@ class TestGateMap(QiskitVisualizationTestCase):
             self.assertImagesAreEqual(filename, img_ref, 0.1)
             os.remove(filename)
 
+    def test_plot_circuit_layout(self):
+        """ tests plot_circuit_layout for each device (20 qubit, 16 qubit, 14 qubit and 5 qubit)"""
+        qr = QuantumRegister(3, 'qr')
+        circuit = QuantumCircuit(qr)
+        circuit._layout = Layout({qr[0]: 4, qr[1]: 0, qr[2]: 2})
+        for backend in self.backends:
+            n = backend.configuration().n_qubits
+            img_ref = path_to_diagram_reference(str(n) + "_plot_circuit_layout.png")
+            filename = "temp.png"
+            fig = plot_circuit_layout(circuit, backend)
+            fig.savefig(filename, bbox_inches="tight")
+            self.assertImagesAreEqual(filename, img_ref, 0.1)
+            os.remove(filename)
+
 
 class TestGraphDist(QiskitTestCase):
     """ tests _GraphdDist functions """
+
     def setUp(self):
         """ setup plots for _GraphDist """
         ax1 = plt.subplots(figsize=(5, 5))[1]
@@ -62,12 +79,12 @@ class TestGraphDist(QiskitTestCase):
         self.ax1_bounds_x, self.ax1_bounds_y = ax1.get_xlim(), ax1.get_ylim()
         self.ax2_bounds_x, self.ax2_bounds_y = ax2.get_xlim(), ax2.get_ylim()
         self.size = 4
-        self.real_values = [self.ax1_x1-self.ax1_x0, self.ax1_y1-self.ax1_y0,
-                            self.ax2_x1-self.ax2_x0, self.ax2_y1-self.ax2_y0]
-        self.abs_values = [self.ax1_bounds_x[0]-self.ax1_bounds_x[1],
-                           self.ax1_bounds_y[0]-self.ax1_bounds_y[1],
-                           self.ax2_bounds_x[0]-self.ax2_bounds_x[1],
-                           self.ax2_bounds_y[0]-self.ax2_bounds_y[1]]
+        self.real_values = [self.ax1_x1 - self.ax1_x0, self.ax1_y1 - self.ax1_y0,
+                            self.ax2_x1 - self.ax2_x0, self.ax2_y1 - self.ax2_y0]
+        self.abs_values = [self.ax1_bounds_x[0] - self.ax1_bounds_x[1],
+                           self.ax1_bounds_y[0] - self.ax1_bounds_y[1],
+                           self.ax2_bounds_x[0] - self.ax2_bounds_x[1],
+                           self.ax2_bounds_y[0] - self.ax2_bounds_y[1]]
         self.val = []
         for i in range(4):
             val = (self.size / self.real_values[i]) * self.abs_values[i]
