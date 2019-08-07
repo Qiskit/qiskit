64516ed2955ed29b643b67c072b5b1b8243ccf03
Luciano Bello <luciano.bello@ibm.com>
2019-08-07 11:11:22 -0400

    keep the file

--- test/python/visualization/test_gate_map.py
+++ test/python/visualization/test_gate_map.py
@@ -58,7 +58,8 @@ class TestGateMap(QiskitVisualizationTestCase):
             fig = plot_circuit_layout(circuit, backend)
             fig.savefig(filename, bbox_inches="tight")
             self.assertImagesAreEqual(filename, img_ref, 0.1)
-            os.remove(filename)
+            break
+            # os.remove(filename)
 
 
 class TestGraphDist(QiskitTestCase):
