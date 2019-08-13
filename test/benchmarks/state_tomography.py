052b3402774e46a0a4660025d414db4fa8514ffc
Matthew Treinish <mtreinish@kortar.org>
2019-08-13 16:53:51 -0400

    Update state tomography benchmarks to use ignis (Qiskit/qiskit-metapackage#400)
    * Update state tomography benchmarks to use ignis
    
    The state tomography benchmarks have been skipped since terra deprecated
    and then removed it's support for doing tomography. The new way to run
    tomography is by using ignis. This commit does just that and rewrites
    the tomography benchmarks to use ignis instead of terra so that we can
    now run them on the current versions of terra.
    
    * Fix lint
    
    * Add a 6 qubit benchmark
    
    * Fix lint again
    
    * Remove setting timer it's already the default
    
    * Increase timeout for state tomography benchmarks
    
    The cat state tomography benchmark for 5 qubits typically takes about 55
    seconds for my local desktop. However, sometimes it can take slightly
    longer and with a default timeout of 60 seconds it doesn't give much of
    a margin.
    
    * Remove 6 qubit benchmark
    
    The 6 qubit cat state tomography benchmark is a bit too slow to be
    valuable. On my local desktop it's consistently more than the current
    timeout of the state tomography benchmark which is 2min (the bell state
    tomography does not have an duration issues, it's minimally slower with
    6 qubits). We already have a ton of very slow benchmarks and the
    additional value for running tomography with 6 qubits vs 5 is minimal.
    So in the interest of time this commit removes the 6 qubit cases.
    
    * Fix lint yet again

--- test/benchmarks/state_tomography.py
+++ test/benchmarks/state_tomography.py
@@ -1,190 +1,63 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2017, IBM.
+# This code is part of Qiskit.
 #
-# This source code is licensed under the Apache License, Version 2.0 found in
-# the LICENSE.txt file in the root directory of this source tree.
+# (C) Copyright IBM 2017, 2019.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
 
 # pylint: disable=missing-docstring,invalid-name,no-member,broad-except
-# pylint: disable=no-else-return
+# pylint: disable=no-else-return, attribute-defined-outside-init
 
-"""
-Quantum State Tomography.
-Generates many small circuits, thus good for profiling compiler overhead.
-Number of circuits grows like 3^n_qubits
-"""
-
-import numpy as np
+from qiskit.ignis.verification import tomography as tomo
 
 import qiskit
-# import tomography libary and other useful tools
-try:
-    import qiskit.tools.qcvv.tomography as tomo
-    NO_TOMO = False
-except ImportError:
-    NO_TOMO = True
 from qiskit.quantum_info import state_fidelity
-try:
-    from qiskit.tools.qi.qi import random_unitary_matrix, purity
-except ImportError:
-    NO_TOMO = True
 
 
 class StateTomographyBench:
     params = [2, 3, 4, 5]
-    timeout = 360.0
-    use_quantum_program = False
+    param_names = ['n_qubits']
+    version = '0.1.1'
+    timeout = 120.0
 
     def setup(self, _):
-        if hasattr(qiskit, 'QuantumProgram'):
-            self.use_quantum_program = True
-        else:
-            self.use_quantum_program = False
-        if NO_TOMO:
-            raise NotImplementedError
+        self.sv_backend = qiskit.BasicAer.get_backend('statevector_simulator')
+        self.qasm_backend = qiskit.BasicAer.get_backend('qasm_simulator')
 
-    # circuit that outputs the target state
-    def target_prep(self, state, target, n_qubits, qp=None):
-        if not self.use_quantum_program:
-            # quantum circuit to make an entangled cat state
-            if state == 'cat':
-                n_qubits = int(np.log2(target.size))
-                qr = qiskit.QuantumRegister(n_qubits, 'qr')
-                cr = qiskit.ClassicalRegister(n_qubits, 'cr')
-                circ = qiskit.QuantumCircuit(qr, cr, name='cat')
-                circ.h(qr[0])
-                for i in range(1, n_qubits):
-                    circ.cx(qr[0], qr[i])
-            # quantum circuit to prepare arbitrary given state
-            elif state == 'random':
-                n_qubits = int(np.log2(target.size))
-                qr = qiskit.QuantumRegister(n_qubits, 'qr')
-                cr = qiskit.ClassicalRegister(n_qubits, 'cr')
-                circ = qiskit.QuantumCircuit(qr, cr, name='random')
-                circ.initialize(target, [qr[i] for i in range(n_qubits)])
-            return circ
-        else:
-            if state == 'cat':
-                n_qubits = int(np.log2(target.size))
-                qr = qp.create_quantum_register('qr', n_qubits)
-                cr = qp.create_classical_register('cr', n_qubits)
-                cat = qp.create_circuit('prep', [qr], [cr])
-                cat.h(qr[0])
-                for i in range(1, n_qubits):
-                    cat.cx(qr[0], qr[i])
-            # quantum circuit to prepare arbitrary given state
-            elif state == 'random':
-                n_qubits = int(np.log2(target.size))
-                qr = qp.create_quantum_register('qr', n_qubits)
-                cr = qp.create_classical_register('cr', n_qubits)
-                random = qp.create_circuit('prep', [qr], [cr])
-                try:
-                    random.initialize(
-                        "Qinit", target, [qr[i] for i in range(n_qubits)])
-                except Exception:
-                    random.initialize(
-                        target, [qr[i] for i in range(n_qubits)])
-            return qp
-
-    # add basis measurements to the circuit for tomography
-    # XX..X, XX..Y, .., ZZ..Z
-    def add_tomo_circuits(self, circ):
-        if not self.use_quantum_program:
-            # Construct state tomography set for measurement of qubits in the
-            # register
-            qr = next(iter(circ.qregs))
-            cr = next(iter(circ.cregs))
-            tomo_set = tomo.state_tomography_set(list(range(qr.size)))
-            # Add the state tomography measurement circuits
-            tomo_circuits = tomo.create_tomography_circuits(
-                circ, qr, cr, tomo_set)
-            return tomo_set, tomo_circuits
-        if self.use_quantum_program:
-            # Construct state tomography set for measurement of qubits in the
-            # register
-            qr_name = list(circ.get_quantum_register_names())[0]
-            cr_name = list(circ.get_classical_register_names())[0]
-            qr = circ.get_quantum_register(qr_name)
-            cr = circ.get_classical_register(cr_name)
-            tomo_set = tomo.state_tomography_set(list(range(qr.size)))
-            # Add the state tomography measurement circuits to the Quantum
-            # Program
-            tomo_circuits = tomo.create_tomography_circuits(
-                circ, qr, cr, tomo_set)
-            return circ, tomo_set, tomo_circuits
-        raise Exception
+    def time_state_tomography_bell(self, n_qubits):
+        qr = qiskit.QuantumRegister(2)
+        bell = qiskit.QuantumCircuit(qr)
+        bell.h(qr[0])
+        bell.cx(qr[0], qr[1])
+        psi_bell = qiskit.execute(
+            bell, self.sv_backend).result().get_statevector(bell)
+        qr_full = qiskit.QuantumRegister(n_qubits)
+        bell = qiskit.QuantumCircuit(qr_full)
+        bell.h(qr_full[n_qubits - 2])
+        bell.cx(qr_full[n_qubits - 2], qr_full[n_qubits - 1])
+        qst_bell = tomo.state_tomography_circuits(bell,
+                                                  [qr_full[n_qubits - 2],
+                                                   qr_full[n_qubits - 1]])
+        job = qiskit.execute(qst_bell, self.qasm_backend, shots=5000)
+        rho_bell = tomo.StateTomographyFitter(job.result(), qst_bell).fit()
+        state_fidelity(psi_bell, rho_bell)
 
     def time_state_tomography_cat(self, n_qubits):
-        # cat target state: [1. 0. 0. ... 0. 0. 1.]/sqrt(2.)
-        target = np.zeros(pow(2, n_qubits))
-        target[0] = 1
-        target[pow(2, n_qubits)-1] = 1.0
-        target /= np.sqrt(2.0)
-        if not self.use_quantum_program:
-            self._state_tomography(target, 'cat', n_qubits)
-        else:
-            self._state_tomography_quantum_program(target, 'cat', n_qubits)
-
-    def time_state_tomography_random(self, n_qubits):
-        # random target state: first column of a random unitary
-        target = random_unitary_matrix(pow(2, n_qubits))[0]
-        if not self.use_quantum_program:
-            self._state_tomography(target, 'random', n_qubits)
-        else:
-            self._state_tomography_quantum_program(target, 'random', n_qubits)
-
-    def _state_tomography_quantum_program(self, target, state, n_qubits,
-                                          shots=1):
-        qp = qiskit.QuantumProgram()
-        try:
-            backend = 'local_qiskit_simulator'
-            qp.get_backend_configuration(backend)
-        except LookupError:
-            backend = 'local_qasm_simulator'
-
-        # Prepared target state and assess quality
-        qp = self.target_prep(state, target, n_qubits, qp=qp)
-        prep_result = qp.execute(['prep'],
-                                 backend=backend, shots=1)
-        prep_state = prep_result.get_data('prep')['quantum_state']
-        F_prep = state_fidelity(prep_state, target)
-        print('Prepared state fidelity =', F_prep)
-
-        # Run state tomography simulation and fit data to reconstruct circuit
-        qp, tomo_set, tomo_circuits = self.add_tomo_circuits(qp)
-        tomo_result = qp.execute(tomo_circuits, backend=backend, shots=shots)
-        tomo_data = tomo.tomography_data(tomo_result, 'prep', tomo_set)
-        rho_fit = tomo.fit_tomography_data(tomo_data)
-
-        # calculate fidelity and purity of fitted state
-        F_fit = state_fidelity(rho_fit, target)
-        pur = purity(rho_fit)
-        print('Fitted state fidelity =', F_fit)
-        print('Fitted state purity =', str(pur))
-
-    # perform quantum state tomography and assess quality of reconstructed
-    # vector
-    def _state_tomography(self, target, state, n_qubits, shots=1):
-        # Use the local qasm simulator
-        backend = qiskit.BasicAer.get_backend('statevector_simulator')
-
-        # Prepared target state and assess quality
-        prep_circ = self.target_prep(state, target, n_qubits)
-        prep_result = qiskit.execute(
-            prep_circ, backend=backend).result()
-        prep_state = prep_result.get_statevector(prep_circ)
-        F_prep = state_fidelity(prep_state, target)
-        print('Prepared state fidelity =', F_prep)
-
-        # Run state tomography simulation and fit data to reconstruct circuit
-        tomo_set, tomo_circuits = self.add_tomo_circuits(prep_circ)
+        qr = qiskit.QuantumRegister(n_qubits, 'qr')
+        circ = qiskit.QuantumCircuit(qr, name='cat')
+        circ.h(qr[0])
+        for i in range(1, n_qubits):
+            circ.cx(qr[0], qr[i])
+        psi = qiskit.execute(circ, self.sv_backend).result().get_statevector()
+        qst_circ = tomo.state_tomography_circuits(circ, qr)
         tomo_result = qiskit.execute(
-            tomo_circuits, backend=backend, shots=shots).result()
-        tomo_data = tomo.tomography_data(tomo_result, prep_circ.name, tomo_set)
-        rho_fit = tomo.fit_tomography_data(tomo_data)
-
-        # calculate fidelity and purity of fitted state
-        F_fit = state_fidelity(rho_fit, target)
-        pur = purity(rho_fit)
-        print('Fitted state fidelity =', F_fit)
-        print('Fitted state purity =', str(pur))
+            qst_circ, self.qasm_backend, shots=5000).result()
+        rho = tomo.StateTomographyFitter(tomo_result, qst_circ).fit()
+        state_fidelity(psi, rho)
