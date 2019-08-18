cff8b7519120eb114d9615831f1d6c453c02bc5b
Luciano Bello <luciano.bello@ibm.com>
2019-08-18 14:12:12 +0200

    the other test

--- test/python/visualization/test_circuit_matplotlib_drawer.py
+++ test/python/visualization/test_circuit_matplotlib_drawer.py
@@ -86,7 +86,7 @@ class TestMatplotlibDrawer(QiskitVisualizationTestCase):
         # check the barriers plot properly when plot_barriers= True
         filename = self._get_resource_path('visualization/references/current_matplotlib_ref.png')
         visualization.circuit_drawer(qc, output='mpl', plot_barriers=True, filename=filename)
-        self.addCleanup(os.remove, filename)
+        # self.addCleanup(os.remove, filename)
 
         ref_filename = self._get_resource_path(
             'visualization/references/matplotlib_barriers_ref.png')
@@ -132,7 +132,7 @@ class TestMatplotlibDrawer(QiskitVisualizationTestCase):
 
         filename = self._get_resource_path('current_%s_long_name_matplotlib.png' % os.name)
         visualization.circuit_drawer(circuit, output='mpl', filename=filename)
-        # self.addCleanup(os.remove, filename)
+        self.addCleanup(os.remove, filename)
 
         ref_filename = self._get_resource_path(
             'visualization/references/%s_long_name_matplotlib.png' % os.name)
