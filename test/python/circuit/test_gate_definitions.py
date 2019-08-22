684f3e44793181ef852a35b88e42ab402944c385
Ali Javadi-Abhari <ajavadia@users.noreply.github.com>
2019-08-21 22:19:53 -0400

    Shorter controlled-hadamard definition (#2837)
    * shorter CH gate definition
    
    * changelog
    
    * add ch matrix
    
    * import numpy
    
    * add test
    
    * fix broken test

---
+++ test/python/circuit/test_gate_definitions.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2017, 2018.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+
+"""Test hardcoded decomposition rules and matrix definitions for standard gates."""
+
+from qiskit import QuantumCircuit
+from qiskit.quantum_info import Operator
+from qiskit.test import QiskitTestCase
+
+
+class TestGateDefinitions(QiskitTestCase):
+    """Test the decomposition of a gate in terms of other gates
+    yields the same matrix as the hardcoded matrix definition."""
+
+    def test_ch_definition(self):  # TODO: expand this to all gates
+        """Test ch gate matrix and definition.
+        """
+        circ = QuantumCircuit(2)
+        circ.ch(0, 1)
+        decomposed_circ = circ.decompose()
+        self.assertTrue(Operator(circ).equiv(Operator(decomposed_circ)))
