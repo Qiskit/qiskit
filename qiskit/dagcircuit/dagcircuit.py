dd39811ae566963a415624e21f44b6062136de30
Luciano Bello <luciano.bello@ibm.com>
2019-08-24 14:20:49 +0200

    tuples for bits!

--- qiskit/dagcircuit/dagcircuit.py
+++ qiskit/dagcircuit/dagcircuit.py
@@ -364,9 +364,9 @@ class DAGCircuit:
                     # If mapping to a register not in valregs, add it.
                     # (k,0) exists in edge_map because edge_map doesn't
                     # fragment k
-                    if not edge_map[(k, 0)].register.name in valregs:
+                    if not edge_map[k[0]].register.name in valregs:
                         size = max(map(lambda x: x.index,
-                                       filter(lambda x: x.register == edge_map[(k, 0)].register,
+                                       filter(lambda x: x.register == edge_map[k[0]].register,
                                               edge_map.values())))
                         qreg = QuantumRegister(size + 1, edge_map[(k, 0)].register.name)
                         add_regs.add(qreg)
@@ -718,8 +718,7 @@ class DAGCircuit:
         Returns:
             generator(DAGNode): node in topological order
         """
-        return nx.lexicographical_topological_sort(self._multi_graph,
-                                                   key=lambda x: str(x.qargs))
+        return nx.lexicographical_topological_sort(self._multi_graph, key=lambda x: x._node_id)
 
     def topological_op_nodes(self):
         """
