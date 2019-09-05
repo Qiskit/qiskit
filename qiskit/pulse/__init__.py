1be1808ce1d33cdf2db06d90a71e46daa2fe6d9b
Thomas Alexander <talexander@ibm.com>
2019-09-05 18:00:23 -0400

    Remove init modificiations of pulse.

--- qiskit/pulse/__init__.py
+++ qiskit/pulse/__init__.py
@@ -26,6 +26,3 @@ from .exceptions import PulseError
 from .interfaces import ScheduleComponent
 # from .parser import parse_string_expr
 from .schedule import Schedule
-
-from . import pulse_lib
-from . import utils
