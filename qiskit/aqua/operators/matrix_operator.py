d4c13977e1b4a12dec172a2c0a52f049463ff0cd
Manoel Marques <manoel@us.ibm.com>
2019-09-06 12:40:28 -0400

    additional docstring fixes

--- qiskit/aqua/operators/matrix_operator.py
+++ qiskit/aqua/operators/matrix_operator.py
@@ -307,7 +307,8 @@ class MatrixOperator(BaseOperator):
         Carry out the eoh evolution for the operator under supplied specifications.
 
         Args:
-            state_in (InitialState): The initial state for the evolution
+            state_in (Union(list,numpy.array)): A vector representing the initial state
+                                            for the evolution
             evo_time (int): The evolution time
             num_time_slices (int): The number of time slices for the expansion
             expansion_mode (str): The mode under which the expansion is to be done.
