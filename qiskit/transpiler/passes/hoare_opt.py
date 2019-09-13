f9856c8b9d0226cc60c91e1201d232e9f395456f
dime10 <dime10@users.noreply.github.com>
2019-09-13 19:16:38 +0200

    Added implementation of traverse_dag function.

--- qiskit/transpiler/passes/hoare_opt.py
+++ qiskit/transpiler/passes/hoare_opt.py
@@ -3,6 +3,7 @@
 """Pass for hoare circuit optimization.
 """
 from qiskit.transpiler.basepasses import TransformationPass
+from qiskit.circuit import ControlledGate
 from z3 import And, Not, Implies, Solver, Bool
 
 
@@ -14,38 +15,64 @@ class HoareOptimizer(TransformationPass):
     def __init__(self):
         self.solver = Solver()
         self.variables = dict()
-	self.gatenum = dict()
+        self.gatenum = dict()
 
     def _gen_variable(self, qb):
         """ After each gate generate a new unique variable name for each of the
             qubits, using scheme: 'q[id]_[gatenum]', e.g. q1_0 -> q1_1 -> q1_2,
                                                           q2_0 -> q2_1
         """
-        str = "q"+qb +"_"+self.gatenum[qb]
+        str = "q" + qb + "_" + self.gatenum[qb]
         self.gatenum[qb] += 1
-	return Bool(str)
+        return Bool(str)
 
     def _initialize(self, dag):
         """ create boolean variables for each qubit and apply qb == 0 condition
         """
-        pass
+        for qb in dag.qubits():
+            i = qb[1]  # id: Qubit(QuantumRegister(3, 'q'), 0)
+            x = self._gen_variable(i)
+            self.solver.add(x == 0)
+            self.variables[i] = x
 
-    def _add_postconditions(self, gate):
+    def _add_postconditions(self, gate, ctrl_ones, trgtvar):
         """ create boolean variables for each qubit the gate is applied to
             and apply the relevant post conditions
         """
         pass
 
+    def _test_gate(self, gate, ctrl_ones, trgtvar):
+        pass
+
     def _traverse_dag(self, dag):
         """ traverse DAG
-            - initialize boolean variables for each entry node
-            - traverse gates:
-                for each gate check: if any control is 0
-                                     if triviality conditions are satisfied
-                if yes remove gate from dag
-                apply post conditions of gate
+            for each gate check: if any control is 0
+                                 if triviality conditions are satisfied
+            if yes remove gate from dag
+            apply postconditions of gate
         """
-        pass
+        for l in dag.serial_layers():
+            nodes = l['graph'].gate_nodes()
+            if len(nodes) != 1:
+                continue
+            gate = nodes[0]
+
+            if isinstance(gate, ControlledGate):
+                numctrl = gate.num_ctrl_qubits
+            else:
+                numctrl = 0
+            ctrlqb = gate.qargs[:numctrl]
+            trgtqb = gate.qargs[numctrl:]
+            ctrlvar = [qb[1] for qb in ctrlqb]
+            trgtvar = [qb[1] for qb in trgtqb]
+
+            ctrl_ones = And(*ctrlvar)
+
+            trivial = self._test_gate(gate, ctrl_ones, trgtvar)
+            if trivial:
+                dag.remove_op_node(gate)
+
+            self._add_postconditions(gate, ctrl_ones, trgtvar)
 
     def run(self, dag):
         """
