027b06a443ca1c1b6e8f149224d3e1ebb6f06de5
dime10 <dime10@users.noreply.github.com>
2019-09-13 15:05:20 +0200

    Initial HoareOptimizer pass class structure.

---
+++ qiskit/transpiler/passes/hoare_opt.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+"""Pass for hoare circuit optimization.
+"""
+from qiskit.transpiler.basepasses import TransformationPass
+from z3 import And, Not, Implies, Solver
+
+
+class HoareOptimizer(TransformationPass):
+    """ The inner workings of this are detailed in:
+        https://arxiv.org/abs/1810.00375
+    """
+
+    def __init__(self):
+        self.solver = Solver()
+        self.variables = dict()
+
+    def _gen_var_name(self, qb):
+        """ After each gate generate a new unique variable name for each of the
+            qubits, using scheme: 'q[id]_[gatenum]', e.g. q1_0 -> q1_1 -> q1_2,
+                                                          q2_0 -> q2_1
+        """
+        pass
+
+    def _initialize(self, dag):
+        """ create boolean variables for each qubit and apply qb == 0 condition
+        """
+        pass
+
+    def _add_postconditions(self, gate):
+        """ create boolean variables for each qubit the gate is applied to
+            and apply the relevant post conditions
+        """
+        pass
+
+    def _traverse_dag(self, dag):
+        """ traverse DAG
+            - initialize boolean variables for each entry node
+            - traverse gates:
+                for each gate check: if any control is 0
+                                     if triviality conditions are satisfied
+                if yes remove gate from dag
+                apply post conditions of gate
+        """
+        pass
+
+    def run(self, dag):
+        """
+        Args:
+            dag (DAGCircuit): the directed acyclic graph to run on.
+        Returns:
+            DAGCircuit: Transformed DAG.
+        """
+        self._initialize(dag)
+        self._traverse_dag(dag)
+
+        return dag
