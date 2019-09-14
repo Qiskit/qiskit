9ac6038b639961d4b58f09d4c66f975dd8a59be8
Shaojun Sun <shaojun.sun@cern.ch>
2019-09-14 12:43:49 +0200

    update gate extension

--- qiskit/transpiler/passes/_gate_extension.py
+++ qiskit/transpiler/passes/_gate_extension.py
@@ -3,20 +3,14 @@
 """
 adding extension function to Gate
 """
-from qiskit.extensions.standard import XGate, YGate, ZGate, SwapGate, CnotGate, ToffoliGate
+from qiskit.extensions.standard import HGate, XGate, YGate, ZGate, SwapGate, CnotGate, ToffoliGate
 from z3 import Not, And
 
 # HGate
-XGate.postconditions = lambda self, x1, y1: True
+HGate.postconditions = lambda self, x1, y1: True
 
 # XGate
 XGate.postconditions = lambda self, x1, y1: y1 == Not(x1)
-
-# XGate
-CnotGate.postconditions = lambda self, x1, y1: y1 == Not(x1)
-
-# XGate
-ToffoliGate.postconditions = lambda self, x1, y1: y1 == Not(x1)
 
 # YGate
 YGate.postconditions = lambda self, x1, y1: y1 == Not(x1)
@@ -24,6 +18,12 @@ YGate.postconditions = lambda self, x1, y1: y1 == Not(x1)
 # ZGate
 ZGate.trivial_if = lambda self, x1: True
 ZGate.postconditions = lambda self, x1, y1: y1 == x1
+
+# CnotGate
+CnotGate.postconditions = lambda self, x1, y1: y1 == Not(x1)
+
+# ToffoliGate
+ToffoliGate.postconditions = lambda self, x1, y1: y1 == Not(x1)
 
 # SwapGate
 SwapGate.trivial_if = lambda self, x1, x2: x1 == x2
