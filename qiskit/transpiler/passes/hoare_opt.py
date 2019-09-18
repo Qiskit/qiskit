e58b162ea36ebbd839b0f0ee8b65f0bbb18e89dd
dime10 <dime10@users.noreply.github.com>
2019-09-18 10:39:22 +0200

    Switched to using 'topological_op_nodes' from 'serial_layers' for direct references to nodes in original DAG.

--- qiskit/transpiler/passes/hoare_opt.py
+++ qiskit/transpiler/passes/hoare_opt.py
@@ -4,7 +4,6 @@
 """
 from qiskit.transpiler.basepasses import TransformationPass
 from qiskit.circuit import ControlledGate
-from qiskit.dagcircuit import DAGCircuit
 from z3 import And, Or, Not, Implies, Solver, Bool, sat, unsat
 import qiskit.transpiler.passes._gate_extension
 
@@ -102,24 +101,16 @@ class HoareOptimizer(TransformationPass):
             if yes remove gate from dag
             apply postconditions of gate
         """
-        new_dag = DAGCircuit()
-        for l in dag.serial_layers():
-            layer_dag = l['graph']
-            nodes = layer_dag.gate_nodes()
-            if len(nodes) != 1:
-                continue
-            node, gate = nodes[0], nodes[0].op
-
-            var = self._seperate_ctrl_trgt(node)
-            ctrlvar = var[1]
-            trgtqb, trgtvar = var[2], var[3]
+        for node in dag.topological_op_nodes():
+            gate = node.op
+            _, ctrlvar, trgtqb, trgtvar = self._seperate_ctrl_trgt(node)
 
             ctrl_ones = And(*ctrlvar)
 
             trivial = self._test_gate(gate, ctrl_ones, trgtvar)
             if trivial:
-                layer_dag.remove_op_node(node)
-            elif self.l > 1:
+                dag.remove_op_node(node)
+            elif self.l > 1 and not trivial:
                 for qb in node.qargs:
                     self.gatecache[qb.index].append(node)
                     self.varnum[qb.index][node] = self.gatenum[qb.index]-1
@@ -127,8 +118,6 @@ class HoareOptimizer(TransformationPass):
                         self._multigate_opt(qb.index)
 
             self._add_postconditions(gate, ctrl_ones, trgtqb, trgtvar)
-            new_dag.extend_back(layer_dag)
-        return new_dag
 
     def _target_successive_seq(self, dag, qb, max_idx):
         """ gates are target successive if they have the same set of target
@@ -180,15 +169,15 @@ class HoareOptimizer(TransformationPass):
     def _multigate_opt(self, dag, qb, rec=False, max_idx=None):
         """
         """
-        rem = False
+        removed = False
         for seq in self._target_successive_seq(dag, qb, max_idx):
             if self._is_identity(seq) and self._seq_as_one(seq):
                 for node in seq:
                     dag.remove_op_node(node)
                     for qb in node.qargs:
                         self.gatecache[qb.index].remove(node)
-                rem = True
-        if not rem and not rec:
+                removed = True
+        if not removed and not rec:
             # need to remove at least one gate from cache, so remove oldest
             first_gate = self.gatecache[qb.index][0]
             for qb in first_gate.qargs:
@@ -225,9 +214,8 @@ class HoareOptimizer(TransformationPass):
             DAGCircuit: Transformed DAG.
         """
         self._initialize(dag)
-        new_dag = self._traverse_dag(dag)
+        self._traverse_dag(dag)
         if self.l > 1:
             for qb in dag.qubits():
                 self._multigate_opt(dag, qb)
-
-        return new_dag
+        return dag
