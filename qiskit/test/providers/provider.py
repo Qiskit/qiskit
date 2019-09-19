57cff7c98dbfefebcb16ce6dff63efd683db84e5
SooluThomas <soolu.elto@gmail.com>
2019-09-19 13:35:49 -0400

    Removed the backend properties test

--- qiskit/test/providers/provider.py
+++ qiskit/test/providers/provider.py
@@ -58,32 +58,3 @@ class ProviderTestCase(QiskitTestCase):
         """Test getting a backend from the provider."""
         backend = self.provider.get_backend(name=self.backend_name)
         self.assertEqual(backend.name(), self.backend_name)
-
-    def test_gate_error(self):
-        """Test getting the gate errors."""
-        self.assertEqual(self.backend.properties().gate_error('u1', 0),
-                         1.0)
-        self.assertEqual(self.backend.properties().gate_error('u1', [0]),
-                         1.0)
-        self.assertEqual(self.backend.properties().gate_error('cx', [0, 1]),
-                         1.0)
-
-    def test_gate_length(self):
-        """Test getting the gate duration."""
-        self.assertEqual(self.backend.properties().gate_length('u1', 0),
-                         0.)
-        self.assertEqual(self.backend.properties().gate_length('u3', qubits=[0]),
-                         2 * 1.3333 * 1e-9)
-
-    def test_t1(self):
-        self.assertEqual(self.backend.properties().test_t1(0),
-                         (7.195004210055389e-05, datetime.datetime(2019, 9, 17, 11, 59, 48, 147267)))
-
-    def test_get_gate_property(self):
-        self.assertEqual(self.backend.properties().get_gate_property('cx', (0,1), 'gate_error'), 1.0)
-
-    def test_get_qubit_property(self):
-        self.assertEqual(self.backend.properties().get_qubit_property(0,'T1'),
-                         (7.195004210055389e-05, datetime.datetime(2019, 9, 17, 11, 59, 48, 147267)))
-        self.assertEqual(self.backend.properties().get_qubit_property(0,'frequency'),
-                         (4919968006.92, datetime.datetime(2019, 9, 17, 11, 59, 48, 147267)))
