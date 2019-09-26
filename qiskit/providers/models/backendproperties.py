839a1d1854cff71a22c7a66d1f2008cf13e7afeb
SooluThomas <soolu.elto@gmail.com>
2019-09-26 14:25:16 -0400

    More Changes to docstrings and dict logic

--- qiskit/providers/models/backendproperties.py
+++ qiskit/providers/models/backendproperties.py
@@ -20,7 +20,6 @@ from marshmallow.validate import Length, Regexp
 from qiskit.util import _to_tuple
 from qiskit.validation.fields import DateTime, List, Nested, Number, String, Integer
 from qiskit.validation import BaseModel, BaseSchema, bind_schema
-
 from qiskit.pulse.exceptions import PulseError
 
 
@@ -139,36 +138,58 @@ class BackendProperties(BaseModel):
                 self._qubits[qubit] = formatted_props
 
         self._gates = {}
-        _temp = {}  # To add nested dictionaries
-        _temp_name = str  # To check for list of dictionaries
         for gate in gates:
-            qubits = _to_tuple(gate.qubits)
+            if gate.gate not in self._gates:
+                self._gates[gate.gate] = {}
             formatted_props = {}
-            formatted_props['name'] = gate.name
             for param in gate.parameters:
                 value = self._apply_prefix(param.value, param.unit)
                 formatted_props[param.name] = (value, param.date)
-            if _temp_name != gate.gate:
-                _temp = {}
-            _temp_name = gate.gate
-            _temp[qubits] = formatted_props
-            self._gates[gate.gate] = _temp
+            self._gates[gate.gate][_to_tuple(gate.qubits)] = formatted_props
 
         super().__init__(**kwargs)
+
+    def gate_property(self,
+                      operation: str,
+                      qubits: Union[int, Iterable[int]] = None,
+                      name: str = None) -> Tuple[Any, datetime.datetime]:
+        """
+        Return the gate property of the given operation.
+
+        Args:
+            operation: Name of the gate.
+            qubits: The qubit to find the property for.
+            name: Optionally used to specify within the heirarchy which
+                  property to return.
+
+        Return:
+            Gate property.
+
+        Raises:
+	        PulseError: If the property is not found or name is specified but qubit is not.
+        """
+        try:
+            result = self._gates[operation]
+            if qubits is not None:
+                result = result[_to_tuple(qubits)]
+                if name:
+                    result = result[name]
+            elif name:
+                raise PulseError("Provide qubits to get {n} of '{o}'.".format(n=name, o=operation))
+        except KeyError:
+            raise PulseError("Could not find the desired property for {o}.".format(o=operation))
+        return result
 
     def gate_error(self, operation: str, qubits: Union[int, Iterable[int]]) -> float:
         """
         Return gate error estimates from backend properties.
 
         Args:
-            operation (str): The operation for which to get the error.
-            qubits (Union[int, Iterable[int]]): The specific qubits for the operation.
+            operation: The operation for which to get the error.
+            qubits: The specific qubits for the operation.
 
         Return:
-            'Gate error' of the given operation and qubit(s).
-
-        Raises:
-            PulseError: If error is True and the property is not found.
+            Gate error of the given operation and qubit(s).
         """
         return self.gate_property(operation, qubits,
                                   'gate_error')[0]  # Throw away datetime at index 1
@@ -178,100 +199,74 @@ class BackendProperties(BaseModel):
         Return the duration of the gate in units of seconds.
 
         Args:
-            operation (str): The operation for which to get the duration.
-            qubits (Union[int, Iterable[int]]): The specific qubits for the operation.
+            operation: The operation for which to get the duration.
+            qubits: The specific qubits for the operation.
 
         Return:
-            'Gate length' of the given operation and qubit(s).
-
-        Raises:
-	        PulseError: If error is True and the property is not found.
+            Gate length of the given operation and qubit(s).
         """
         return self.gate_property(operation, qubits,
                                   'gate_length')[0]  # Throw away datetime at index 1
 
-    def gate_property(self,
-                      operation: str,
-                      qubits: Union[int, Iterable[int]] = None,
-                      name: str = None) -> Tuple[Any, datetime.datetime]:
-        """
-        Return the gate properties of the given qubit and property.
-
-        Args:
-            operation (str) : Name of the gate
-            qubits (Union[int, Iterable[int]]): The qubit to find properties for.
-            name (str): Optionally used to specify within the heirarchy which
-                        property to return.
-
-        Return:
-            Gate properties of the given qubit and property, if it was given by the
-            backend, otherwise, return `None`.
-
-        Raises:
-	        PulseError: If `qubits` is `None` and `name` is `not None` or
-            if the property is not found.
-        """
-        result = self._gates
-        try:
-            result = result[operation]
-            if qubits is not None:
-                result = result[_to_tuple(qubits)]
-                if name:
-                    result = result[name]
-            elif name:
-                raise PulseError("Provide qubits to get {n} of '{o}'.".format(n=name, o=operation))
-        except KeyError:
-            raise PulseError("Could not find the desired property.")
-        return result
-
-    def qubit_property(self, qubit: int,
+    def qubit_property(self,
+                       qubit: int,
                        name: str = None) -> Tuple[Any, datetime.datetime]:
         """
-        Return the qubit properties of the given qubit and name.
+        Return the property of the given qubit.
 
         Args:
-            qubit (int): The property to look for.
-            name (str): Optionally used to specify within the heirarchy which property to return.
+            qubit: The property to look for.
+            name: Optionally used to specify within the heirarchy which property to return.
 
         Return:
-            Qubit properties of the given qubit and name, if it was given by
-            the backend, otherwise, return `None`.
+            Qubit property.
 
         Raises:
-	        PulseError: If `qubit` is `None` and `name` is `not None` or
-            if the property is not found.
+	        PulseError: If the property is not found.
         """
-        result = self._qubits
         try:
-            result = result[qubit]
+            result = self._qubits[qubit]
             if name is not None:
                 result = result[name]
         except KeyError:
-            raise PulseError("Please check arguments again. Couldn't find the desired property.")
+            raise PulseError("Couldn't find the desired property for {q}.".format(q=qubit))
         return result
 
     def t1(self, qubit: int) -> Tuple[Any, datetime.datetime]:
         """
-        Return the properties of T1 of the given qubit.
+        Return the T1 time of the given qubit.
 
         Args:
-            qubit (int): The property to look for.
+            qubit: Qubit for which to return the T1 time of.
 
         Return:
-            Properties of T1 of the given qubit, if it was given by the backend, otherwise,
-            return `None`.
-
-        Raises:
-	        PulseError: If error is True and the property is not found.
+            T1 time of the given qubit.
         """
         return self.qubit_property(qubit, 'T1')
 
-    def _apply_prefix(self, value, unit):
+    def _apply_prefix(self, value: float, unit: str) -> float:
+        """
+        Given a SI unit prefix and value, apply the prefix to convert to standard SI unit.
+
+        Args:
+            value: The number to apply prefix to.
+            unit: String prefix.
+
+        Return:
+            Converted value.
+
+        Raises:
+            PulseError: If the units aren't recognized.
+        """
         prefixes = {
-            'p': 1e-12, 'n': 1e-9,
-            'u': 1e-6, 'µ': 1e-6,
-            'm': 1e-3, 'k': 1e3,
-            'M': 1e6, 'G': 1e9,
+            'p': 1e-12,
+            'n': 1e-9,
+            'u': 1e-6,
+            'µ': 1e-6,
+            'm': 1e-3,
+            'k': 1e3,
+            'M': 1e6,
+            'G': 1e9
         }
         if not unit:
             return value
