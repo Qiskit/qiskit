749021902aa186fafe9bba020267e6e0ef8e0950
Matthew Treinish <mtreinish@kortar.org>
2019-10-14 15:15:16 -0400

    Add class's __init__() docstring too (Qiskit/qiskit-metapackage#630)
    Right now many classes document class constructors and they're arguments
    in the docstring for the __init__() methods of the class. However, by
    default autodoc does not show these for the classes' documentation and
    instead relies on only the class docstring. This commit sets the sphinx
    autodoc config option [1] to use both the class and init docstring
    together.
    
    [1] https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html#confval-autoclass_content

--- docs/conf.py
+++ docs/conf.py
@@ -277,6 +277,7 @@ epub_title = project
 # A list of files that should not be packed into the epub file.
 epub_exclude_files = ['search.html']
 
+autoclass_content = 'both'
 
 # -- Extension configuration -------------------------------------------------
 
