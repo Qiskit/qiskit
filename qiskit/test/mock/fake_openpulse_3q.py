0468f7c314ba1fa571ff1ddba3b1ec150ff06b23
Lauren Capelluto <laurencapelluto@gmail.com>
2019-10-18 17:28:01 -0400

    Basic pulse scheduler (#2650)
    * Initial commit for issue #1921: basic pulse scheduler
    
    Consolidate language regarding greedy/late scheduling. Small time updating fixup. Do input checking sooner and outside of greedy scheduling
    
    Min early schedule works but needs to be rewritten
    
    Update structure to reflect transpiler/
    
    Update mock CmdDef to work with previous cmd_def tests. Add tests, fixup a couple docstrings
    
    Update mock to use u1, u2, u3. Update tests accordingly and make an expected schedule to compare to
    
    Allow a list of circuits rather than only one circuit for top level `scheduler.schedule`
    
    Update gregs->qubits for accuracy
    
    Wrap cmd_def.get error in helpful message
    
    Allow 'asap' and 'alap' method aliases. Add some comments for alap scheduler. move variables that are used nonlocally in helper functions before the helper function to introduce them to the reader
    
    Allow more input options for schedule (cmd_def and meas_map, all optional)
    
    Change name of cx-parametrized to ParametrizedGate
    
    Use insert rather than union when we probably want to respect the buffer
    
    Add documentation around Measure bug, where qubits aren't measured into the requested memory slot
    
    Update tests for doing insertion schedule building rather than union
    
    Add schedule to top level (from qiskit import schedule)
    
    Fixup imports and move schedule from scheduler to compiler
    
    Fixup tests and changelog (after rebase)
    
    Be more specific in QiskitError import to fix cyclic import warning
    
    Keep circuit name in newly scheduled schedule
    
    ALAP now aligns ends of schedules. Additionally fix bug where qubits not yet seen (in reverse order) wouldn't have their times properly updated when a barrier with that qubit is encountered.
    
    * Make docstring improvements
    
    * Move two scheduling structures to better locations
    
    * Add 3Q test, and 3Q mock to enable such a test. 3Q Pulse mock backend is essentially the same as the 2Q one.
    
    * Style

--- qiskit/test/mock/fake_openpulse_3q.py
+++ qiskit/test/mock/fake_openpulse_3q.py
@@ -22,14 +22,14 @@ from qiskit.qobj import PulseLibraryItem, PulseQobjInstruction
 from .fake_backend import FakeBackend
 
 
-class FakeOpenPulse2Q(FakeBackend):
-    """A fake 2 qubit backend for pulse test."""
+class FakeOpenPulse3Q(FakeBackend):
+    """Trivial extension of the FakeOpenPulse2Q."""
 
     def __init__(self):
         configuration = PulseBackendConfiguration(
-            backend_name='fake_openpulse_2q',
+            backend_name='fake_openpulse_3q',
             backend_version='0.0.0',
-            n_qubits=2,
+            n_qubits=3,
             meas_levels=[0, 1, 2],
             basis_gates=['u1', 'u2', 'u3', 'cx', 'id'],
             simulator=False,
@@ -39,36 +39,39 @@ class FakeOpenPulse2Q(FakeBackend):
             memory=False,
             max_shots=65536,
             gates=[GateConfig(name='TODO', parameters=[], qasm_def='TODO')],
-            coupling_map=[[1, 0]],
-            n_registers=2,
-            n_uchannels=2,
+            coupling_map=[[0, 1], [1, 2]],
+            n_registers=3,
+            n_uchannels=3,
             u_channel_lo=[
                 [UchannelLO(q=0, scale=1. + 0.j)],
-                [UchannelLO(q=0, scale=-1. + 0.j), UchannelLO(q=1, scale=1. + 0.j)]
+                [UchannelLO(q=0, scale=-1. + 0.j), UchannelLO(q=1, scale=1. + 0.j)],
+                [UchannelLO(q=0, scale=1. + 0.j)]
             ],
             meas_level=[1, 2],
-            qubit_lo_range=[[4.5, 5.5], [4.5, 5.5]],
-            meas_lo_range=[[6.0, 7.0], [6.0, 7.0]],
+            qubit_lo_range=[[4.5, 5.5], [4.5, 5.5], [4.5, 5.5]],
+            meas_lo_range=[[6.0, 7.0], [6.0, 7.0], [6.0, 7.0]],
             dt=1.3333,
             dtm=10.5,
             rep_times=[100, 250, 500, 1000],
             meas_map=[[0, 1]],
             channel_bandwidth=[
                 [-0.2, 0.4], [-0.3, 0.3], [-0.3, 0.3],
-                [-0.02, 0.02], [-0.02, 0.02], [-0.02, 0.02]
+                [-0.02, 0.02], [-0.02, 0.02], [-0.02, 0.02],
+                [-0.2, 0.4], [-0.3, 0.3], [-0.3, 0.3]
             ],
             meas_kernels=['kernel1'],
             discriminators=['max_1Q_fidelity'],
-            acquisition_latency=[[100, 100], [100, 100]],
+            acquisition_latency=[[100, 100], [100, 100], [100, 100]],
             conditional_latency=[
+                [100, 1000], [1000, 100], [100, 1000],
                 [100, 1000], [1000, 100], [100, 1000],
                 [1000, 100], [100, 1000], [1000, 100]
             ]
         )
 
         self._defaults = PulseDefaults(
-            qubit_freq_est=[4.9, 5.0],
-            meas_freq_est=[6.5, 6.6],
+            qubit_freq_est=[4.9, 5.0, 4.8],
+            meas_freq_est=[6.5, 6.6, 6.4],
             buffer=10,
             pulse_library=[PulseLibraryItem(name='test_pulse_1', samples=[0.j, 0.1j]),
                            PulseLibraryItem(name='test_pulse_2', samples=[0.j, 0.1j, 1j]),
@@ -82,6 +85,9 @@ class FakeOpenPulse2Q(FakeBackend):
                      Command(name='u1', qubits=[1],
                              sequence=[PulseQobjInstruction(name='fc', ch='d1',
                                                             t0=0, phase='-P1*np.pi')]),
+                     Command(name='u1', qubits=[2],
+                             sequence=[PulseQobjInstruction(name='fc', ch='d2',
+                                                            t0=0, phase='-P1*np.pi')]),
                      Command(name='u2', qubits=[0],
                              sequence=[PulseQobjInstruction(name='fc', ch='d0',
                                                             t0=0, phase='-P0*np.pi'),
@@ -94,6 +100,10 @@ class FakeOpenPulse2Q(FakeBackend):
                                        PulseQobjInstruction(name='test_pulse_4', ch='d1', t0=0),
                                        PulseQobjInstruction(name='fc', ch='d1',
                                                             t0=0, phase='-P0*np.pi')]),
+                     Command(name='u2', qubits=[2],
+                             sequence=[PulseQobjInstruction(name='test_pulse_3', ch='d2', t0=0),
+                                       PulseQobjInstruction(name='fc', ch='d2',
+                                                            t0=0, phase='-P0*np.pi')]),
                      Command(name='u3', qubits=[0],
                              sequence=[PulseQobjInstruction(name='test_pulse_1', ch='d0', t0=0)]),
                      Command(name='u3', qubits=[1],
@@ -104,6 +114,12 @@ class FakeOpenPulse2Q(FakeBackend):
                                        PulseQobjInstruction(name='test_pulse_1', ch='d1', t0=20),
                                        PulseQobjInstruction(name='fc', ch='d1',
                                                             t0=20, phase=2.1)]),
+                     Command(name='cx', qubits=[1, 2],
+                             sequence=[PulseQobjInstruction(name='test_pulse_1', ch='d1', t0=0),
+                                       PulseQobjInstruction(name='test_pulse_2', ch='u1', t0=10),
+                                       PulseQobjInstruction(name='test_pulse_1', ch='d2', t0=20),
+                                       PulseQobjInstruction(name='fc', ch='d2',
+                                                            t0=20, phase=2.1)]),
                      Command(name='ParametrizedGate', qubits=[0, 1],
                              sequence=[PulseQobjInstruction(name='test_pulse_1', ch='d0', t0=0),
                                        PulseQobjInstruction(name='test_pulse_2', ch='u0', t0=10),
@@ -112,11 +128,13 @@ class FakeOpenPulse2Q(FakeBackend):
                                        PulseQobjInstruction(name='test_pulse_1', ch='d1', t0=20),
                                        PulseQobjInstruction(name='fc', ch='d1',
                                                             t0=20, phase=2.1)]),
-                     Command(name='measure', qubits=[0, 1],
+                     Command(name='measure', qubits=[0, 1, 2],
                              sequence=[PulseQobjInstruction(name='test_pulse_1', ch='m0', t0=0),
                                        PulseQobjInstruction(name='test_pulse_1', ch='m1', t0=0),
+                                       PulseQobjInstruction(name='test_pulse_1', ch='m2', t0=0),
                                        PulseQobjInstruction(name='acquire', duration=10, t0=0,
-                                                            qubits=[0, 1], memory_slot=[0, 1])])]
+                                                            qubits=[0, 1, 2],
+                                                            memory_slot=[0, 1, 2])])]
         )
 
         super().__init__(configuration)
