78f68ca8566038d28bae2c26419c506461397f9c
Luciano Bello <luciano.bello@ibm.com>
2019-10-21 17:02:01 -0400

    fix the drawer with the new format from passes

--- qiskit/visualization/pass_manager_visualization.py
+++ qiskit/visualization/pass_manager_visualization.py
@@ -105,8 +105,8 @@ def pass_manager_drawer(pass_manager, filename, style=None, raw=False):
 
     for index, controller_group in enumerate(passes):
 
-        # label is the name of the flow controller (without the word controller)
-        label = "[%s] %s" % (index, controller_group['type'].__name__.replace('Controller', ''))
+        # label is the name the flow controller parameter
+        label = "[%s] %s" % (index, ', '.join(controller_group['flow_controllers']))
 
         # create the subgraph for this controller
         subgraph = pydot.Cluster(str(component_id), label=label, fontname='helvetica')
