65541f98328c48d6345ffa6c98168ebce42c5c20
Luciano Bello <luciano.bello@ibm.com>
2019-10-22 16:13:02 -0400

    prec in customunitary

--- qiskit/qasm/node/customunitary.py
+++ qiskit/qasm/node/customunitary.py
@@ -13,6 +13,7 @@
 # that they have been altered from the originals.
 
 """Node for an OPENQASM custom gate statement."""
+import warnings
 
 from .node import Node
 
@@ -43,8 +44,11 @@ class CustomUnitary(Node):
             self.arguments = None
             self.bitlist = children[1]
 
-    def qasm(self):
+    def qasm(self, prec=None):
         """Return the corresponding OPENQASM string."""
+        if prec is not None:
+            warnings.warn('Parameter \'prec\' is no longer used and is being deprecated.',
+                          DeprecationWarning)
         string = self.name
         if self.arguments is not None:
             string += "(" + self.arguments.qasm() + ")"
