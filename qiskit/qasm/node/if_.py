26ea0d85f4471bbfaf90dd0236aa6ebddcf58c41
Luciano Bello <luciano.bello@ibm.com>
2019-10-22 16:09:30 -0400

    prec in if_.py

--- qiskit/qasm/node/if_.py
+++ qiskit/qasm/node/if_.py
@@ -13,6 +13,7 @@
 # that they have been altered from the originals.
 
 """Node for an OPENQASM if statement."""
+import warnings
 
 from .node import Node
 
@@ -30,8 +31,12 @@ class If(Node):
         """Create the if node."""
         super().__init__('if', children, None)
 
-    def qasm(self):
+    def qasm(self, prec=None):
         """Return the corresponding OPENQASM string."""
+        if prec is not None:
+            warnings.warn('Parameter \'prec\' is no longer used and is being deprecated.',
+                          DeprecationWarning)
+
         return "if(" + self.children[0].qasm() + "==" \
                + str(self.children[1].value) + ") " + \
                self.children[2].qasm()
