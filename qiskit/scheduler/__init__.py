0aff5098841ea639c7a1a24ea2e14b7f82774e8a
SooluThomas <soolu.elto@gmail.com>
2019-10-22 10:26:16 -0400

    Add stubs for scheduler

--- qiskit/scheduler/__init__.py
+++ qiskit/scheduler/__init__.py
@@ -12,7 +12,37 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Module for scheduling pulse `Schedule`s from `QuantumCircuit`s."""
+"""
+===================================
+Scheduler (:mod:`qiskit.scheduler`)
+===================================
+
+Module for scheduling pulse `Schedule`/s from `QuantumCircuit`/s.
+
+.. currentmodule:: qiskit.scheduler
+
+.. autosummary::
+   :toctree: ../stubs/
+
+   ScheduleConfig
+   schedule_circuit
+   format_meas_map
+
+Scheduling policies
+===================
+
+.. autosummary::
+   :toctree: ../stubs/
+
+   as_soon_as_possible
+   as_late_as_possible
+   translate_gates_to_pulse_defs
+
+"""
 
 from qiskit.scheduler.config import ScheduleConfig
 from qiskit.scheduler.schedule_circuit import schedule_circuit
+from qiskit.scheduler.utils import format_meas_map
+from qiskit.scheduler.methods.basic import (as_late_as_possible,
+                                            as_soon_as_possible,
+                                            translate_gates_to_pulse_defs)
