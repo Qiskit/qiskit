278e4c99356ca29be10f9f119c186316523182f4
Luciano Bello <luciano.bello@ibm.com>
2019-10-24 15:45:59 -0400

    undo level1

--- qiskit/transpiler/preset_passmanagers/level1.py
+++ qiskit/transpiler/preset_passmanagers/level1.py
@@ -38,7 +38,6 @@ from qiskit.transpiler.passes import RemoveResetInZeroState
 from qiskit.transpiler.passes import Optimize1qGates
 from qiskit.transpiler.passes import ApplyLayout
 from qiskit.transpiler.passes import CheckCXDirection
-from qiskit.transpiler.passes import DenseLayout
 
 
 def level_1_pass_manager(transpile_config):
@@ -116,10 +115,8 @@ def level_1_pass_manager(transpile_config):
     pm1 = PassManager()
     if coupling_map:
         pm1.append(_given_layout)
-        pm1.append([TrivialLayout(coupling_map)], condition=_choose_layout_condition)
+        pm1.append(_choose_layout, condition=_choose_layout_condition)
         pm1.append(_layout_check)
-        pm1.append([DenseLayout(coupling_map)], condition=_choose_layout_condition,
-                   rollback_if=lambda x: False)
         pm1.append(_embed)
     pm1.append(_unroll)
     if coupling_map:
