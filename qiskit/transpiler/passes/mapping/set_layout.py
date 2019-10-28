5405cb97726cb775f64a5bc0f935d2fe9ab33643
Diego M. Rodriguez <diego.plan9@gmail.com>
2019-10-28 12:39:08 +0100

    Misc tweaks to passes.mapping docstrings

--- qiskit/transpiler/passes/mapping/set_layout.py
+++ qiskit/transpiler/passes/mapping/set_layout.py
@@ -12,22 +12,21 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""
-Sets property_set['layout'] to layout.
+"""Set the ``layout`` property to the given layout."""
 
-This pass associates a physical qubit (int) to each virtual qubit
-of the circuit (Qubit) in increasing order.
-"""
 
 from qiskit.transpiler.basepasses import AnalysisPass
 
 
 class SetLayout(AnalysisPass):
-    """Sets property_set['layout'] to layout."""
+    """Set the ``layout`` property to the given layout.
+
+    This pass associates a physical qubit (int) to each virtual qubit
+    of the circuit (Qubit) in increasing order.
+    """
 
     def __init__(self, layout):
-        """
-        Sets property_set['layout'] to layout.
+        """SetLayout initializer.
 
         Args:
             layout (Layout): the layout to set.
