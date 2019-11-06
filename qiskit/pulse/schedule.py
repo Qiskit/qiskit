81bce4cf8630439e06368f52e4db2e55ef222bd5
Daniel Puzzuoli <dan.puzzuoli@gmail.com>
2019-11-06 10:10:42 -0500

    Update qiskit/pulse/schedule.py
    Co-Authored-By: Lauren Capelluto <laurencapelluto@gmail.com>

--- qiskit/pulse/schedule.py
+++ qiskit/pulse/schedule.py
@@ -263,7 +263,8 @@ class Schedule(ScheduleComponent):
                 intervals: Optional[Iterable[Interval]] = None) -> 'Schedule':
         """
         Return a Schedule with only the instructions from this Schedule *failing* at least one of
-        the provided filters. That is, this function has the same inputs as self.filter, but returns
+        the provided filters. This method is the complement of `self.filter`, so that
+            self.filter(args) | self.exclude(args) == self
 
         Args:
             filter_funcs: A list of Callables which take a (int, ScheduleComponent) tuple and
