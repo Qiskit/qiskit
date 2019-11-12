302bf7924ea198f65074a1743ca627d194156c19
Ali Javadi-Abhari <ajavadia@users.noreply.github.com>
2019-11-12 10:30:31 -0500

    Update base.py

--- qiskit/validation/base.py
+++ qiskit/validation/base.py
@@ -147,6 +147,8 @@ class BaseSchema(Schema):
             for i, _ in enumerate(valid_data):
                 additional_keys = set(original_data[i].__dict__) - set(valid_data[i])
                 for key in additional_keys:
+                    if key.startswith('_'):
+                        continue
                     valid_data[i][key] = getattr(original_data[i], key)
         else:
             additional_keys = set(original_data.__dict__) - set(valid_data)
