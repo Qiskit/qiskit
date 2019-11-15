cba1ad01e220c376e31e42d019c6d0f4a55741eb
Ali Javadi <ajavadia@princeton.edu>
2019-11-15 08:30:59 -0600

    fake almaden enhancement

--- qiskit/test/mock/backends/almaden/fake_almaden.py
+++ qiskit/test/mock/backends/almaden/fake_almaden.py
@@ -19,8 +19,8 @@ Fake Almaden device (20 qubit).
 import os
 import json
 
-from qiskit.providers.models import (GateConfig, QasmBackendConfiguration,
-                                     BackendProperties)
+from qiskit.providers.models import (PulseBackendConfiguration,
+                                     BackendProperties, PulseDefaults)
 from qiskit.test.mock.fake_backend import FakeBackend
 
 
@@ -37,37 +37,29 @@ class FakeAlmaden(FakeBackend):
                 ↕         ↕
           15 ↔ 16 ↔ 17 ↔ 18 ↔ 19
         """
-        cmap = [[0, 1], [1, 0], [1, 2], [1, 6], [2, 1], [2, 3], [3, 2],
-                [3, 4], [3, 8], [4, 3], [5, 6], [5, 10], [6, 1], [6, 5],
-                [6, 7], [7, 6], [7, 8], [7, 12], [8, 3], [8, 7], [8, 9],
-                [9, 8], [9, 14], [10, 5], [10, 11], [11, 10], [11, 12],
-                [11, 16], [12, 7], [12, 11], [12, 13], [13, 12], [13, 14],
-                [13, 18], [14, 9], [14, 13], [15, 16], [16, 11], [16, 15],
-                [16, 17], [17, 16], [17, 18], [18, 13], [18, 17], [18, 19],
-                [19, 18]]
+        dirname = os.path.dirname(__file__)
+        filename = "conf_almaden.json"
+        with open(os.path.join(dirname, filename), "r") as f_conf:
+            conf = json.load(f_conf)
 
-        configuration = QasmBackendConfiguration(
-            backend_name='fake_almaden',
-            backend_version='0.0.0',
-            n_qubits=20,
-            basis_gates=['u1', 'u2', 'u3', 'cx', 'id'],
-            simulator=False,
-            local=True,
-            conditional=False,
-            open_pulse=False,
-            memory=True,
-            max_shots=8192,
-            gates=[GateConfig(name='TODO', parameters=[], qasm_def='TODO')],
-            coupling_map=cmap,
-        )
-
+        configuration = PulseBackendConfiguration.from_dict(conf)
+        configuration.backend_name = 'fake_almaden'
         super().__init__(configuration)
 
     def properties(self):
-        """Returns a snapshot of device properties as recorded on 10/08/19.
+        """Returns a snapshot of device properties as recorded on 10/24/19.
         """
         dirname = os.path.dirname(__file__)
         filename = "props_almaden.json"
         with open(os.path.join(dirname, filename), "r") as f_prop:
             props = json.load(f_prop)
         return BackendProperties.from_dict(props)
+
+    def defaults(self):
+        """Returns a snapshot of device defaults as recorded on 10/21/19.
+        """
+        dirname = os.path.dirname(__file__)
+        filename = "defs_almaden.json"
+        with open(os.path.join(dirname, filename), "r") as f_defs:
+            defs = json.load(f_defs)
+        return PulseDefaults.from_dict(defs)
