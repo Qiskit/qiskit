04cc7a12148a16005d286b1927b504e62a3dce98
Diego M. Rodríguez <diego.plan9@gmail.com>
2019-11-15 15:52:37 +0100

    Docstrings updates for qiskit.transpiler.passes.[n-z] (#3477)
    * Revise qiskit.transpiler.passes docstrings
    
    * Add property_set notes, misc tweaks
    
    * Add Raises

--- qiskit/transpiler/passes/num_tensor_factors.py
+++ qiskit/transpiler/passes/num_tensor_factors.py
@@ -12,14 +12,17 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-""" An analysis pass for calculating the number of tensor factors of a DAG circuit.
-"""
+"""Calculate the number of tensor factors of a DAG circuit."""
+
 from qiskit.transpiler.basepasses import AnalysisPass
 
 
 class NumTensorFactors(AnalysisPass):
-    """ An analysis pass for calculating the number of tensor factors of a DAG circuit.
+    """Calculate the number of tensor factors of a DAG circuit.
+
+    The result is saved in ``property_set['num_tensor_factors']`` as an integer.
     """
 
     def run(self, dag):
+        """Run the NumTensorFactors pass on `dag`."""
         self.property_set['num_tensor_factors'] = dag.num_tensor_factors()
