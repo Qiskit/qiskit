0f7a7d8f85f51d061e828212e8733f064ff28065
Soolu Thomas <soolu.thomas@ibm.com>
2019-11-18 12:11:47 -0500

    Set logo-only to False (Qiskit/qiskit-metapackage#693)

--- docs/conf.py
+++ docs/conf.py
@@ -141,7 +141,7 @@ html_theme_path = ['.', sphinx_rtd_theme.get_html_theme_path()]
 # documentation.
 #
 html_theme_options = {
-    'logo_only': True,
+    'logo_only': False,
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
