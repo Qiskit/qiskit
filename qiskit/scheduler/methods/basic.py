55d0dc81325e6b940007c751384b9cd8be3aea41
SooluThomas <soolu.elto@gmail.com>
2019-11-20 15:19:22 -0500

    Fixed review suggestions

--- qiskit/scheduler/methods/basic.py
+++ qiskit/scheduler/methods/basic.py
@@ -147,46 +147,46 @@ def translate_gates_to_pulse_defs(circuit: QuantumCircuit,
     circ_pulse_defs = []
 
     cmd_def = schedule_config.cmd_def
-    m_slots = {}  # Map measured qubit index to clbit
+    qubit_mem_slots = {}  # Map measured qubit index to clbit
 
     def get_measure_schedule() -> CircuitPulseDef:
         """Create a schedule to measure the qubits queued for measuring."""
         measures = set()
         all_qubits = set()
         sched = Schedule()
-        for qubit in m_slots.keys():
+        for qubit in qubit_mem_slots.keys():
             measures.add(tuple(schedule_config.meas_map[qubit]))
         for qubits in measures:
-            unused_mem_slots = set(qubits) - set(m_slots.values())
             all_qubits.update(qubits)
+            unused_mem_slots = set(qubits) - set(qubit_mem_slots.values())
             default_sched = cmd_def.get('measure', qubits)
             for time, inst in default_sched.instructions:
                 if isinstance(inst, AcquireInstruction):
                     mem_slots = []
                     for channel in inst.acquires:
-                        if channel.index in m_slots.keys():
-                            mem_slots.append(MemorySlot(m_slots[channel.index]))
+                        if channel.index in qubit_mem_slots.keys():
+                            mem_slots.append(MemorySlot(qubit_mem_slots[channel.index]))
                         else:
                             mem_slots.append(MemorySlot(unused_mem_slots.pop()))
                     new_acquire = AcquireInstruction(command=inst.command,
                                                      acquires=inst.acquires,
                                                      mem_slots=mem_slots)
                     sched |= new_acquire << time
-                else:
-                    if inst.channels[0].index in m_slots.keys():
-                        sched |= inst << time
-        m_slots.clear()
+                # Measurement pulses should only be added if its qubit was measured by the user
+                elif inst.channels[0].index in qubit_mem_slots.keys():
+                    sched |= inst << time
+        qubit_mem_slots.clear()
         return CircuitPulseDef(schedule=sched, qubits=list(all_qubits))
 
     for inst, qubits, clbits in circuit.data:
         inst_qubits = [qubit.index for qubit in qubits]  # We want only the indices of the qubits
-        if any(q in m_slots for q in inst_qubits):
+        if any(q in qubit_mem_slots for q in inst_qubits):
             # If we are operating on a qubit that was scheduled to be measured, process that first
             circ_pulse_defs.append(get_measure_schedule())
         if isinstance(inst, Barrier):
             circ_pulse_defs.append(CircuitPulseDef(schedule=inst, qubits=inst_qubits))
         elif isinstance(inst, Measure):
-            m_slots[inst_qubits[0]] = clbits[0].index
+            qubit_mem_slots[inst_qubits[0]] = clbits[0].index
         else:
             try:
                 circ_pulse_defs.append(
@@ -196,7 +196,7 @@ def translate_gates_to_pulse_defs(circuit: QuantumCircuit,
                 raise QiskitError("Operation '{0}' on qubit(s) {1} not supported by the backend "
                                   "command definition. Did you remember to transpile your input "
                                   "circuit for the same backend?".format(inst.name, inst_qubits))
-    if m_slots:
+    if qubit_mem_slots:
         circ_pulse_defs.append(get_measure_schedule())
 
     return circ_pulse_defs
