fed8b8f74feb7bbfaa52564e6c941484abf682cb
Paul Nation <nonhermitian@gmail.com>
2019-11-23 05:05:26 -0500

    example missing circuit

--- qiskit/quantum_info/analysis/distance.py
+++ qiskit/quantum_info/analysis/distance.py
@@ -35,8 +35,16 @@ def hellinger_fidelity(dist_p, dist_q):
 
         .. jupyter-execute::
 
-            from qiskit import QuantumCircuit, execute, IBMQ
+            from qiskit import QuantumCircuit, execute, BasicAer
             from qiskit.quantum_info.analysis import hellinger_fidelity
+
+            qc = QuantumCircuit(5, 5)
+            qc.h(2)
+            qc.cx(2, 1)
+            qc.cx(2, 3)
+            qc.cx(3, 4)
+            qc.cx(1, 0)
+            qc.measure(range(5), range(5))
 
             sim = BasicAer.get_backend('qasm_simulator')
             res1 = execute(qc, sim).result()
