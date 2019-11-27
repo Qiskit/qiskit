925a2087d6a135057f7cac461f660b3c7c99cfd6
Paul Nation <nonhermitian@gmail.com>
2019-11-26 23:02:58 -0500

    Add ability to get counts from statevectors and density matrices (#3514)
    * add state to counts
    
    * add to result and tests
    
    * style
    
    * cleanup
    
    * add DM version and tests
    
    * lint

--- qiskit/result/result.py
+++ qiskit/result/result.py
@@ -17,6 +17,7 @@
 from qiskit.circuit.quantumcircuit import QuantumCircuit
 from qiskit.pulse.schedule import Schedule
 from qiskit.exceptions import QiskitError
+from qiskit.quantum_info.states import state_to_counts
 
 from qiskit.validation.base import BaseModel, bind_schema
 from qiskit.result import postprocess
@@ -171,16 +172,19 @@ class Result(BaseModel):
         Raises:
             QiskitError: if there are no counts for the experiment.
         """
+        exp = self._get_experiment(experiment)
         try:
-            exp = self._get_experiment(experiment)
-            try:
-                header = exp.header.to_dict()
-            except (AttributeError, QiskitError):  # header is not available
-                header = None
+            header = exp.header.to_dict()
+        except (AttributeError, QiskitError):  # header is not available
+            header = None
 
+        if 'counts' in self.data(experiment).keys():
             return postprocess.format_counts(self.data(experiment)['counts'],
                                              header)
-        except KeyError:
+        elif 'statevector' in self.data(experiment).keys():
+            vec = postprocess.format_statevector(self.data(experiment)['statevector'])
+            return state_to_counts(vec)
+        else:
             raise QiskitError('No counts for experiment "{0}"'.format(experiment))
 
     def get_statevector(self, experiment=None, decimals=None):
