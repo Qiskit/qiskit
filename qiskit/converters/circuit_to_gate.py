3a901a326ec1c12ae9b53bcb44ed437f62bda1e0
Matthew Treinish <mtreinish@kortar.org>
2019-12-05 11:37:11 -0500

    Handle cregs in circuit_to_gate() converter
    Prior to this commit the circuit_to_get converter function was not
    handling cregs at all. This led to an output definition with
    instruction tuples of length 2 (inst object, [qbits]) instead of length
    3 (inst object, [qbits], [cbits]). This would then cause potential
    issues for operations that were assuming the standardized data format
    with length three. This includes the inverse() method of a gate. This
    commit fixes this issue by adding creg handling to the output gate
    definition generation. While the only instructions with cregs set don't
    have to_gate() methods and the converter won't work with them this is
    still necessary to maintain compatibility with all gate methods, it will
    just mean that cregs will always end up with [] except in the case of
    custom gate defintions. The logic was also reworked to be slightly easier
    to read since having multiple layers of nested map() calls gets hard to
    debug.
    
    Fixes #3545

--- qiskit/converters/circuit_to_gate.py
+++ qiskit/converters/circuit_to_gate.py
@@ -15,6 +15,7 @@
 """Helper function for converting a circuit to a gate"""
 
 from qiskit.circuit.gate import Gate
+from qiskit.circuit.classicalregister import ClassicalRegister
 from qiskit.circuit.quantumregister import QuantumRegister, Qubit
 from qiskit.exceptions import QiskitError
 
@@ -80,10 +81,15 @@ def circuit_to_gate(circuit, parameter_map=None):
 
     if gate.num_qubits > 0:
         q = QuantumRegister(gate.num_qubits, 'q')
+    if gate.num_clbits > 0:
+        c = ClassicalRegister(gate.num_clbits, 'c')
 
-    definition = list(map(lambda x:
-                          (x[0], list(map(lambda y: q[find_bit_position(y)], x[1]))),
-                          definition))
-    gate.definition = definition
+    out_definition = []
+    for x in definition:
+        x_def = (x[0], list(map(lambda y: q[find_bit_position(y)], x[1])),
+                 list(map(lambda z: c[find_bit_position(z)], x[2])))
+        out_definition.append(x_def)
+
+    gate.definition = out_definition
 
     return gate
