4dd50ae6603d923dbbe545dc62b8463372ff1a16
Thomas Alexander <talexander@ibm.com>
2019-12-09 22:26:02 -0500

    Fix bug where too many framechanges in table would cause plotting to fail.

--- qiskit/visualization/pulse/matplotlib.py
+++ qiskit/visualization/pulse/matplotlib.py
@@ -393,7 +393,12 @@ class ScheduleDrawer:
             # table area size
             ncols = self.style.table_columns
             nrows = int(np.ceil(len(table_data)/ncols))
-
+            full_size = nrows * self.style.fig_unit_h_table
+            max_size = self.style.max_table_ratio * self.style.figsize[1]
+            max_rows = np.floor(max_size/self.style.fig_unit_h_table/ncols)
+            nrows = int(min(nrows, max_rows))
+            # don't overflow plot with table data
+            table_data = table_data[:int(nrows*ncols)]
             # fig size
             h_table = nrows * self.style.fig_unit_h_table
             h_waves = (self.style.figsize[1] - h_table)
