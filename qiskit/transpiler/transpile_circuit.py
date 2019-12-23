9ec86794377450ade7be97d5ac66b8aa191908a9
Luciano Bello <luciano.bello@ibm.com>
2019-12-23 14:30:55 -0500

    rollback transpile_circuit

--- qiskit/transpiler/transpile_circuit.py
+++ qiskit/transpiler/transpile_circuit.py
@@ -22,7 +22,7 @@ from qiskit.transpiler.passes.ms_basis_decomposer import MSBasisDecomposer
 from qiskit.transpiler.exceptions import TranspilerError
 
 
-def transpile_circuit(circuit, transpile_args):
+def transpile_circuit(circuit, transpile_config):
     """Select a PassManager and run a single circuit through it.
     Args:
         circuit (QuantumCircuit): circuit to transpile
@@ -38,44 +38,44 @@ def transpile_circuit(circuit, transpile_args):
         QuantumCircuit: transpiled circuit
     """
     # either the pass manager is already selected...
-    if transpile_args['pass_manager'] is not None:
-        pass_manager = transpile_args['pass_manager']
+    if transpile_config['pass_manager'] is not None:
+        pass_manager = transpile_config['pass_manager']
     # or we choose an appropriate one based on desired optimization level (default: level 1)
     else:
-        level = transpile_args['optimization_level']
+        level = transpile_config['optimization_level']
 
         # Workaround for ion trap support: If basis gates includes
         # Mølmer-Sørensen (rxx) and the circuit includes gates outside the basis,
         # first unroll to u3, cx, then run MSBasisDecomposer to target basis.
         basic_insts = ['measure', 'reset', 'barrier', 'snapshot']
-        device_insts = set(transpile_args['pass_manager_config'].basis_gates).union(basic_insts)
+        device_insts = set(transpile_config['pass_manager_config'].basis_gates).union(basic_insts)
 
         ms_basis_swap = None
-        if 'rxx' in transpile_args['pass_manager_config'].basis_gates and \
-                not device_insts >= transpile_args['circuit'].count_ops().keys():
-            ms_basis_swap = transpile_args['pass_manager_config'].basis_gates
-            transpile_args['pass_manager_config'].basis_gates = list(set(['u3', 'cx']).union(
-                transpile_args['pass_manager_config'].basis_gates))
+        if 'rxx' in transpile_config['pass_manager_config'].basis_gates and \
+                not device_insts >= transpile_config['circuit'].count_ops().keys():
+            ms_basis_swap = transpile_config['pass_manager_config'].basis_gates
+            transpile_config['pass_manager_config'].basis_gates = list(set(['u3', 'cx']).union(
+                transpile_config['pass_manager_config'].basis_gates))
 
         if level is None:
             level = 1
 
         if level == 0:
-            pass_manager = level_0_pass_manager(transpile_args['pass_manager_config'])
+            pass_manager = level_0_pass_manager(transpile_config['pass_manager_config'])
         elif level == 1:
-            pass_manager = level_1_pass_manager(transpile_args['pass_manager_config'])
+            pass_manager = level_1_pass_manager(transpile_config['pass_manager_config'])
         elif level == 2:
-            pass_manager = level_2_pass_manager(transpile_args['pass_manager_config'])
+            pass_manager = level_2_pass_manager(transpile_config['pass_manager_config'])
         elif level == 3:
-            pass_manager = level_3_pass_manager(transpile_args['pass_manager_config'])
+            pass_manager = level_3_pass_manager(transpile_config['pass_manager_config'])
         else:
             raise TranspilerError("optimization_level can range from 0 to 3.")
 
         if ms_basis_swap is not None:
             pass_manager.append(MSBasisDecomposer(ms_basis_swap))
 
-    out_circuit = pass_manager.run(transpile_args['circuit'],
-                                   callback=transpile_args['callback'],
-                                   output_name=transpile_args['output_name'])
+    out_circuit = pass_manager.run(circuit,
+                                   callback=transpile_config['callback'],
+                                   output_name=transpile_config['output_name'])
 
     return out_circuit
