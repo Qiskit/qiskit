0dc5ce84ee6e4d717b3bc4be381f09f66cac56be
Matthew Treinish <mtreinish@kortar.org>
2020-01-08 17:08:12 -0500

    Raise instead of warn when shots>max_shots
    This commit updates the logic to no longer warn if shots>max_shots and
    then change the value to making that a hard exception. To address the
    default behavior if no shots kwarg is specified either 1024 or max_shots
    will be used, whichever is smaller.

--- qiskit/compiler/assemble.py
+++ qiskit/compiler/assemble.py
@@ -31,7 +31,7 @@ from qiskit.qobj.utils import MeasLevel, MeasReturnType
 def assemble(experiments,
              backend=None,
              qobj_id=None, qobj_header=None,
-             shots=1024, memory=False, max_credits=None, seed_simulator=None,
+             shots=None, memory=False, max_credits=None, seed_simulator=None,
              qubit_lo_freq=None, meas_lo_freq=None,
              qubit_lo_range=None, meas_lo_range=None,
              schedule_los=None, meas_level=MeasLevel.CLASSIFIED,
@@ -65,6 +65,7 @@ def assemble(experiments,
 
         shots (int):
             Number of repetitions of each circuit, for sampling. Default: 1024
+            or max_shots from the backend configuration, whichever is smaller
 
         memory (bool):
             If True, per-shot measurement bitstrings are returned as well
@@ -184,7 +185,7 @@ def _parse_common_args(backend, qobj_id, qobj_header, shots,
 
     Raises:
         QiskitError: if the memory arg is True and the backend does not support
-        memory.
+        memory. Also if shots exceeds max_shots for the configured backend.
     """
     # grab relevant info from backend if it exists
     backend_config = None
@@ -210,12 +211,15 @@ def _parse_common_args(backend, qobj_id, qobj_header, shots,
     qobj_header = QobjHeader(**{k: v for k, v in qobj_header.items() if v is not None})
 
     max_shots = getattr(backend_config, 'max_shots', None)
-    if max_shots and max_shots < shots:
-        warnings.warn(
+    if shots is None:
+        if max_shots:
+            shots = min(1024, max_shots)
+        else:
+            shots = 1024
+    elif max_shots and max_shots < shots:
+        raise QiskitError(
             'Number of shots specified: %s exceeds max_shots property of the '
-            'backend: %s. Reducing shots to max_shots' % (shots, max_shots),
-            RuntimeWarning)
-        shots = max_shots
+            'backend: %s. Reducing shots to max_shots' % (shots, max_shots))
 
     # create run configuration and populate
     run_config_dict = dict(shots=shots,
