b02a0e551b63b4394ba0b121d712df6e623b70a2
Erick Winston <ewinston@us.ibm.com>
2020-01-17 13:33:12 -0500

    avoid multiple definitions

---
+++ qiskit/extensions/standard/cu1.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2017.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+controlled-u1 gate.
+"""
+import warnings
+# pylint: disable=unused-import
+from qiskit.extensions.standard.u1 import Cu1Gate, cu1
+
+warnings.warn('This module is deprecated. The Cu1Gate can now be found in u1.py')
---
+++ qiskit/extensions/standard/cu3.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2017.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+controlled-u3 gate.
+"""
+import warnings
+# pylint: disable=unused-import
+from qiskit.extensions.standard.u3 import Cu3Gate, cu3
+
+warnings.warn('This module is deprecated. The Cu3Gate can now be found in u3.py')
