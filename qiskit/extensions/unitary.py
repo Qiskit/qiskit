61517ef11d57e597f057583f6d4a6861f2bcdd2e
Erick Winston <ewinston@us.ibm.com>
2020-01-22 17:40:59 -0500

    expected blank space

--- qiskit/extensions/unitary.py
+++ qiskit/extensions/unitary.py
@@ -211,6 +211,7 @@ def _compute_control_matrix(base_mat, num_ctrl_qubits, ctrl_state=None):
                 + numpy.kron(base_mat, ctrl_proj))
     return full_mat
 
+
 def unitary(self, obj, qubits, label=None):
     """Apply u2 to q."""
     if isinstance(qubits, QuantumRegister):
