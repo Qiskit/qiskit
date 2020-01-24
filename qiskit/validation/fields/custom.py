47eeb173bd00144db0e1e5a1e7404690dec4ca8f
Eric Eastman <ereastman96@gmail.com>
2020-01-24 15:12:38 -0500

    Fixed deserialization logic in Complex number definition (#3747)
    Adds the ability to deserialize from a dictionary containing actual complex numbers too.
    
    * Fixed deserialization logic error in Complex number definition
    
    * Readded try/except, removed else, updated docs for Complex _deserialize method
    
    Fixed linter errors

--- qiskit/validation/fields/custom.py
+++ qiskit/validation/fields/custom.py
@@ -29,7 +29,7 @@ class Complex(ModelTypeValidator):
     """Field for complex numbers.
 
     Field for parsing complex numbers:
-    * deserializes to Python's `complex`.
+    * deserializes from either a Python `complex` or 2-element collection.
     * serializes to a tuple of 2 decimals `(real, imaginary)`
     """
 
@@ -47,13 +47,15 @@ class Complex(ModelTypeValidator):
             raise self.make_error_serialize('format', input=value)
 
     def _deserialize(self, value, attr, data, **_):
-        if not is_collection(value) or len(value) != 2:
-            raise self.make_error('invalid', input=value)
+        if is_collection(value) and (len(value) == 2):
+            try:
+                return complex(value[0], value[1])
+            except (ValueError, TypeError):
+                raise self.make_error('invalid', input=value)
+        elif isinstance(value, complex):
+            return value
 
-        try:
-            return complex(*value)
-        except (ValueError, TypeError):
-            raise self.make_error('invalid', input=value)
+        raise self.make_error('invalid', input=value)
 
 
 class InstructionParameter(ModelTypeValidator):
