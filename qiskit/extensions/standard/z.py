2ceb42af079071afb03cae6a802e0f89869f81e7
sumitpuri <er.sumitpuri@gmail.com>
2020-01-25 10:46:45 -0600

    Update qiskit/extensions/standard/z.py
    Co-Authored-By: Kevin Krsulich <kevin@krsulich.net>

--- qiskit/extensions/standard/z.py
+++ qiskit/extensions/standard/z.py
@@ -54,7 +54,7 @@ def z(self, q):
     """Apply Z gate to a specified qubit (q).
     A Z-gate implements a pi rotation of the qubit state vector about the
     z-axis of the Bloch sphere.
-    This gate is canonically used to rotate the qubit state from |+⟩ to |-⟩, or vise versa.
+    This gate is canonically used to rotate the qubit state from |+⟩ to |-⟩, or vice versa.
 
     Examples:
 
