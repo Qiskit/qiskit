fbc3f00ed948d94b399e1044f958f8770a467ba0
Iskandar Sitdikov <IceKhan13@users.noreply.github.com>
2020-01-28 08:05:11 -0800

    #2262 | Acquire commands on a single qubit (#3574)
    * Acquire: AcquireInstruction change to sigle qbit, mem_slot, reg_slot
    
    * Acquire: change usage of AcquireInstruction
    
    * Tests: partial test fixing
    
    * Assembler: tweak scheduler assembler to laverage new acquire + tests fixes
    
    * Scheduler: fix scheduler to use acquire on single qubit + tests fix
    
    * Style: fix lint errors
    
    * Acquire: remove unecessary properties
    
    * Style: fix lint errors
    
    * Acuqire: back compatibility on acquire multiple qubits
    
    * Acuqire: fix linter
    
    * Acquire: refactor
    
    * Acquire: fix implicit acquires; todo: revisit _validate_meas_map method
    
    * Acquire: pylint fix
    
    * Acquire: minor fixes
    
    * Acquire: fix add implicits acquires function
    
    * Acquire: instruction properties fix
    
    * Acquire: remove test for validating meas map
    
    * Linter: fix errors
    
    * Acquire: style fix
    
    * Acquire: remove deprecation in acquires property + fix positional arguments
    
    * Acquire: back and forward compatibility
    
    * Acquire: remove deprecation warning (hm, I thought I removed it before... weird ;) )
    
    * Acquire: grammar fixes + split schedule acquire test
    
    * Acquire: add release note
    
    * Acquire: release notes remove issues
    
    Co-authored-by: Lauren Capelluto <laurencapelluto@gmail.com>

--- qiskit/assembler/assemble_schedules.py
+++ qiskit/assembler/assemble_schedules.py
@@ -81,6 +81,7 @@ def assemble_schedules(schedules, qobj_id, qobj_header, run_config):
         # instructions
         max_memory_slot = 0
         qobj_instructions = []
+        acquire_instructions = []
 
         # Instructions are returned as tuple of shifted time and instruction
         for shift, instruction in schedule.instructions:
@@ -111,12 +112,15 @@ def assemble_schedules(schedules, qobj_id, qobj_header, run_config):
             elif isinstance(instruction, AcquireInstruction):
                 max_memory_slot = max(max_memory_slot,
                                       *[slot.index for slot in instruction.mem_slots])
-                if meas_map:
-                    # verify all acquires satisfy meas_map
-                    _validate_meas_map(instruction, meas_map)
+
+                acquire_instructions.append(instruction)
 
             converted_instruction = instruction_converter(shift, instruction)
             qobj_instructions.append(converted_instruction)
+
+        if meas_map:
+            # verify all acquires satisfy meas_map
+            _validate_meas_map(acquire_instructions, meas_map)
 
         # memory slot size is memory slot index + 1 because index starts from zero
         exp_memory_slot_size = max_memory_slot + 1
@@ -199,11 +203,16 @@ def assemble_schedules(schedules, qobj_id, qobj_header, run_config):
                      header=qobj_header)
 
 
-def _validate_meas_map(acquire, meas_map):
+def _validate_meas_map(instructions, meas_map):
     """Validate all qubits tied in meas_map are to be acquired."""
     meas_map_set = [set(m) for m in meas_map]
     # Verify that each qubit is listed once in measurement map
-    measured_qubits = {acq_ch.index for acq_ch in acquire.acquires}
+
+    acquires = []
+    for inst in instructions:
+        acquires += inst.acquires
+    measured_qubits = {acq_ch.index for acq_ch in acquires}
+
     tied_qubits = set()
     for meas_qubit in measured_qubits:
         for map_inst in meas_map_set:
