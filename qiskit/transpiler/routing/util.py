f9f5301a7334cb4282fba60651f4d29c8f9a1624
Eddie Schoute <eddieschoute@gmail.com>
2020-01-29 13:48:47 -0500

    Add files for implementing general routing
    This implements the Token Swapping approximation algorithm

---
+++ qiskit/transpiler/routing/util.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2017, 2019.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+# Copyright 2019 Andrew M. Childs, Eddie Schoute, Cem M. Unsal
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Utility functions shared between permutation functionality."""
+
+from typing import Tuple, TypeVar, Iterable, MutableMapping, Optional
+
+import qiskit.transpiler.routing as rt
+
+_K = TypeVar('_K')
+_V = TypeVar('_V')
+Reg = Tuple[str, int]
+
+
+def swap_permutation(swaps: Iterable[Iterable[rt.Swap[_K]]],
+                     mapping: MutableMapping[_K, _V],
+                     allow_missing_keys: bool = False) -> None:
+    """Given a circuit of swaps, apply them to the permutation (in-place).
+
+    Args:
+      swaps: param mapping: A mapping of Keys to Values, where the Keys are being swapped.
+      mapping: The permutation to have swaps applied to.
+      allow_missing_keys: Whether to allow swaps of missing keys in mapping.
+    """
+    for swap_step in swaps:
+        for sw1, sw2 in swap_step:
+            # Take into account non-existent keys.
+            val1 = None  # type: Optional[_V]
+            val2 = None  # type: Optional[_V]
+            if allow_missing_keys:
+                val1 = mapping.pop(sw1, None)
+                val2 = mapping.pop(sw2, None)
+            else:
+                # Asserts that both keys exist
+                val1, val2 = mapping.pop(sw1), mapping.pop(sw2)
+
+            if val1 is not None:
+                mapping[sw2] = val1
+            if val2 is not None:
+                mapping[sw1] = val2
