a9c947472752e02b5db80cf2996426282ccc2179
Matthew Treinish <mtreinish@kortar.org>
2020-02-03 19:16:19 -0500

    Fix tests

--- test/python/compiler/test_assembler.py
+++ test/python/compiler/test_assembler.py
@@ -632,7 +632,7 @@ class TestPulseAssembler(QiskitTestCase):
         self.assertDictEqual(qobj_insts[2].parameters, {'duration': 25, 'amp': 1})
         self.assertDictEqual(qobj_insts[3].parameters,
                              {'duration': 150, 'sigma': 8, 'amp': 0.2, 'width': 140})
-        self.assertListEqual(
+        self.assertEqual(
             qobj.to_dict()['experiments'][0]['instructions'][0]['parameters']['amp'],
             0.0+0.5j)
 
