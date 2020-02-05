77161672a93f87b2a3fb3e9fdd041c801a36700c
IceKhan13 <iskandar.sitdikov@gmail.com>
2020-02-04 17:05:17 -0800

    LargePulseBackendMock: skeleton of backend

---
+++ qiskit/test/mock/backends/fake_100q/fake_100q.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2020.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+Fake 100 qubit device.
+"""
+import os
+import json
+
+from qiskit.providers.models import (PulseBackendConfiguration,
+                                     BackendProperties, PulseDefaults)
+from qiskit.test.mock.fake_backend import FakeBackend
+
+
+class FakeOpenPulse100Q(FakeBackend):
+    """A fake 100Q backend."""
+
+    def __init__(self):
+        """
+        TODO: architecture
+        """
+        dirname = os.path.dirname(__file__)
+        filename = "conf_100q.json"
+        with open(os.path.join(dirname, filename), "r") as f_conf:
+            conf = json.load(f_conf)
+
+        configuration = PulseBackendConfiguration.from_dict(conf)
+        configuration.backend_name = 'fake_openpulse_100q'
+        super().__init__(configuration)
+
+    def properties(self):
+        """Returns a snapshot of device properties."""
+        dirname = os.path.dirname(__file__)
+        filename = "props_100q.json"
+        with open(os.path.join(dirname, filename), "r") as f_prop:
+            props = json.load(f_prop)
+        return BackendProperties.from_dict(props)
+
+    def defaults(self):
+        """Returns a snapshot of device defaults."""
+        dirname = os.path.dirname(__file__)
+        filename = "defs_100q.json"
+        with open(os.path.join(dirname, filename), "r") as f_defs:
+            defs = json.load(f_defs)
+        return PulseDefaults.from_dict(defs)
