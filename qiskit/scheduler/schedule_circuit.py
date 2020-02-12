cc34c49dc9fed4a0373e343f62b70045ca905a6d
SooluThomas <soolu.elto@gmail.com>
2020-02-12 14:18:30 -0500

    Update qiskit/scheduler/schedule_circuit.py
    Co-Authored-By: Lauren Capelluto <laurencapelluto@gmail.com>

--- qiskit/scheduler/schedule_circuit.py
+++ qiskit/scheduler/schedule_circuit.py
@@ -33,7 +33,7 @@ def schedule_circuit(circuit: QuantumCircuit,
 
     Supported methods:
 
-        * ``as_soon_as_possible``: Schedule pulses greedily, as early as possible on a qubit resource.
+        * ``'as_soon_as_possible'``: Schedule pulses greedily, as early as possible on a qubit resource.
           (alias: ``asap``)
         * ``as_late_as_possible``: Schedule pulses late-- keep qubits in the ground state when possible.
           (alias: ``alap``)
