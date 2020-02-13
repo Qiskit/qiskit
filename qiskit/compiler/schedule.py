5a0b77e88d08ee0459e483a1ee795c0ba9c4b66d
Lauren Capelluto <laurencapelluto@gmail.com>
2020-02-13 13:44:43 -0500

    Update the docstrings for qiskit.compiler.schedule for the docathon.

--- qiskit/compiler/schedule.py
+++ qiskit/compiler/schedule.py
@@ -12,9 +12,8 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""
-Convenience entry point into pulse scheduling, requiring only a circuit and a backend. For more
-control over pulse scheduling, look at `qiskit.scheduler.schedule_circuit`.
+"""Convenience entry point into pulse scheduling, requiring only a circuit and a backend.
+For more control over pulse scheduling, look at :py:func:`qiskit.scheduler.schedule_circuit`.
 """
 from typing import List, Optional, Union
 
@@ -31,24 +30,25 @@ def schedule(circuits: Union[QuantumCircuit, List[QuantumCircuit]],
              cmd_def: Optional[CmdDef] = None,
              meas_map: Optional[List[List[int]]] = None,
              method: Optional[Union[str, List[str]]] = None) -> Union[Schedule, List[Schedule]]:
-    """
-    Schedule a circuit to a pulse Schedule, using the backend, according to any specified methods.
-    Supported methods are documented in
+    """Schedule a circuit to a pulse Schedule, using the backend, according to any specified
+    methods. Supported methods are documented in
     :py:func:`qiskit.pulse.scheduler.schedule_circuit.schedule_circuit`.
 
     Args:
-        circuits: The quantum circuit or circuits to translate
-        backend: A backend instance, which contains hardware-specific data required for scheduling
-        inst_map: Mapping of circuit operations to pulse schedules. If None, defaults to the
-                  `backend` `instruction_schedule_map`
+        circuits: The quantum circuit or circuits to translate.
+        backend: A backend instance, which contains hardware-specific data required for scheduling.
+        inst_map: Mapping of circuit operations to pulse schedules. If ``None``, defaults to the
+                  ``backend`` ``instruction_schedule_map``.
         cmd_def: Deprecated
-        meas_map: List of sets of qubits that must be measured together. If `None`, defaults to
-                  the `backend` `meas_map`
-        method: Optionally specify a particular scheduling method
+        meas_map: List of sets of qubits that must be measured together. If ``None``, defaults to
+                  the ``backend``'s ``meas_map``.
+        method: Optionally specify a particular scheduling method.
+
     Returns:
-        A pulse `Schedule` that implements the input circuit
+        A pulse ``Schedule`` that implements the input circuit.
+
     Raises:
-        QiskitError: If `inst_map` and `meas_map` are not passed and `backend` is not passed
+        QiskitError: If ``inst_map`` and ``meas_map`` are not passed and ``backend`` is not passed.
     """
     if inst_map is None:
         if cmd_def is not None:
