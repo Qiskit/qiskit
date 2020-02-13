2780a7eb3b069ef6867c49b1a3933cc3f3d952fe
Ali Javadi <ajavadia@princeton.edu>
2020-02-13 15:50:01 -0500

    add tests

---
+++ test/python/circuit/test_library.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2017, 2019.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Test library of quantum circuits."""
+
+from qiskit.test import QiskitTestCase
+from qiskit.circuit import QuantumCircuit
+from qiskit.circuit.library import permutation, shift, inner_product
+
+
+class TestBooleanLogicLibrary(QiskitTestCase):
+    """Test library of boolean logic quantum circuits."""
+
+    def test_permutation(self):
+        """Test permutation circuit."""
+        circuit = permutation(n_qubits=4, pattern=[1, 0, 3, 2])
+        expected = QuantumCircuit(4)
+        expected.swap(0, 1)
+        expected.swap(2, 3)
+        self.assertEqual(circuit, expected)
