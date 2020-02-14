beae2c2ad255ef2bffeee825c497378b640b3efc
Paul Nation <nonhermitian@gmail.com>
2020-02-14 14:34:11 -0500

    unused import

--- test/python/visualization/test_gate_map.py
+++ test/python/visualization/test_gate_map.py
@@ -17,7 +17,6 @@ from io import BytesIO
 from PIL import Image
 from ddt import ddt, data
 from qiskit.test.mock import FakeProvider
-from qiskit.test import QiskitTestCase
 from qiskit.visualization.gate_map import plot_gate_map, plot_circuit_layout
 from qiskit.tools.visualization import HAS_MATPLOTLIB
 from qiskit import QuantumRegister, QuantumCircuit
