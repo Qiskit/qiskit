deba21303de41921f80b598e433947cda36956af
Matthew Treinish <mtreinish@kortar.org>
2020-02-14 10:27:35 -0500

    Disable broken lint rule

--- test/python/tools/jupyter/test_notebooks.py
+++ test/python/tools/jupyter/test_notebooks.py
@@ -12,6 +12,8 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
+# pylint: disable=bad-docstring-quotes
+
 """Tests for the wrapper functionality."""
 
 import os
