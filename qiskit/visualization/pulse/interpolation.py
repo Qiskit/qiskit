a866860ce14b4aa8135ddbdc4aa00b62c0c2a079
faraimazh <faraimazh@gmail.com>
2020-02-16 21:24:51 +0200

    fixed issue 3427 by updating interpolation module functions

--- qiskit/visualization/pulse/interpolation.py
+++ qiskit/visualization/pulse/interpolation.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 
 # This code is part of Qiskit.
 #
@@ -41,6 +41,7 @@ def interp1d(time, samples, nop, kind='linear'):
     dt = time[1] - time[0]
 
     time += 0.5 * dt
+
     cs_ry = interpolate.interp1d(time[:-1], re_y, kind=kind, bounds_error=False)
     cs_iy = interpolate.interp1d(time[:-1], im_y, kind=kind, bounds_error=False)
 
@@ -48,9 +49,34 @@ def interp1d(time, samples, nop, kind='linear'):
 
     return time_, cs_ry(time_), cs_iy(time_)
 
+def step_wise(time, samples, nop, kind='nearest'):
+    """Scipy interpolation wrapper.
+​
+    Args:
+        time (ndarray): time.
+        samples (ndarray): complex pulse envelope.
+        nop (int): data points for interpolation.
+        kind (str): Scipy interpolation type. See `scipy.interpolate.interp1d` documentation
+            for more information.
+    Returns:
+        ndarray: interpolated waveform.
+    """
+
+    samples_ = np.repeat(samples, 2)
+    re_y_ = np.real(samples_)
+    im_y_ = np.imag(samples_)
+
+    time__ = np.concatenate(([time[0]], np.repeat(time[1:-1], 2), [time[-1]]))
+
+    cs_ry_ = interpolate.interp1d(time__, re_y_, bounds_error=False)
+    cs_iy_ = interpolate.interp1d(time__, im_y_, bounds_error=False)
+
+    time_ = np.linspace(time[0], time[-1], nop)
+
+    return time_, cs_ry_(time_), cs_iy_(time_)
 
 linear = partial(interp1d, kind='linear')
 
 cubic_spline = partial(interp1d, kind='cubic')
 
-step_wise = partial(interp1d, kind='nearest')
+step_wise = partial(step_wise, kind='nearest')
