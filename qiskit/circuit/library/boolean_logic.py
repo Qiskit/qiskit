0035d1fa1f3640707d114a69ed0334b99201c8ae
Ali Javadi <ajavadia@princeton.edu>
2020-02-18 16:07:48 -0500

    pylint

--- qiskit/circuit/library/boolean_logic.py
+++ qiskit/circuit/library/boolean_logic.py
@@ -12,6 +12,8 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
+# pylint: disable=no-member
+
 """Implementations of boolean logic quantum circuits.
 """
 
