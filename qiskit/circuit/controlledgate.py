31e044bd47b515b44d21dc0e4d56ed59efa7d1d3
ewinston <ewinston@us.ibm.com>
2020-02-19 09:30:30 -0500

    add ability to specify open controls. (#3739)
    * add ability specify open controls.
    
    This PR adds the ability to control on '0' qubit states.
    
    * add release notes
    
    * expected blank space
    
    * allow string control state specification.
    
    * linting
    
    * linting
    
    * remove f-string
    
    * add ctrl_state keyword to standard extensions
    
    * fix comments. add ctrl_state attribute to ControlledGate.
    
    * add ctrl_state and test of open controlled unitary matrix generation
    
    * minor bug fixes
    
    * linting
    
    * linting
    
    Co-authored-by: Ali Javadi-Abhari <ajavadia@users.noreply.github.com>

--- qiskit/circuit/controlledgate.py
+++ qiskit/circuit/controlledgate.py
@@ -15,17 +15,21 @@
 """
 Controlled unitary gate.
 """
-
 from qiskit.circuit.exceptions import CircuitError
 from .gate import Gate
+from . import QuantumRegister
 
 
 class ControlledGate(Gate):
     """Controlled unitary gate."""
 
     def __init__(self, name, num_qubits, params, label=None, num_ctrl_qubits=1,
-                 definition=None):
-        """Create a new gate.
+                 definition=None, ctrl_state=None):
+        """Create a controlled gate.
+
+        Attributes:
+            num_ctrl_qubits (int): The number of control qubits.
+            ctrl_state (int): The control state in decimal notation.
 
         Args:
             name (str): The Qobj name of the gate.
@@ -34,8 +38,13 @@ class ControlledGate(Gate):
             label (str or None): An optional label for the gate [Default: None]
             num_ctrl_qubits (int): Number of control qubits.
             definition (list): list of gate rules for implementing this gate.
+            ctrl_state (int or str or None): The control state in decimal or as
+                a bitstring (e.g. '111'). If specified as a bitstring the length
+                must equal num_ctrl_qubits, MSB on left. If None, use
+                2**num_ctrl_qubits-1.
         Raises:
             CircuitError: num_ctrl_qubits >= num_qubits
+            CircuitError: ctrl_state < 0 or ctrl_state > 2**num_ctrl_qubits.
         """
         super().__init__(name, num_qubits, params, label=label)
         if num_ctrl_qubits < num_qubits:
@@ -50,14 +59,76 @@ class ControlledGate(Gate):
                     self.base_gate = base_gate.base_gate
                 else:
                     self.base_gate = base_gate
+        self._ctrl_state = None
+        self.ctrl_state = ctrl_state
+
+    @property
+    def definition(self):
+        """Return definition in terms of other basic gates. If the gate has
+        open controls, as determined from `self.ctrl_state`, the returned
+        definition is conjugated with X."""
+        if not self._definition:
+            self._define()
+        # pylint: disable=cyclic-import
+        from qiskit.extensions.standard import XGate, CnotGate
+        bit_ctrl_state = bin(self.ctrl_state)[2:].zfill(self.num_ctrl_qubits)
+        # hacky way to get register assuming single register
+        if self._definition:
+            qreg = self._definition[0][1][0].register
+        elif isinstance(self, CnotGate):
+            qreg = QuantumRegister(self.num_qubits, 'q')
+            self._definition = [(self, [qreg[0], qreg[1]], [])]
+        open_rules = []
+        for qind, val in enumerate(bit_ctrl_state[::-1]):
+            if val == '0':
+                open_rules.append([XGate(), [qreg[qind]], []])
+        return open_rules + self._definition + open_rules
+
+    @definition.setter
+    def definition(self, excited_def):
+        """Set controlled gate definition with closed controls."""
+        super(Gate, self.__class__).definition.fset(self, excited_def)
+
+    @property
+    def ctrl_state(self):
+        """Return the control state of the gate as a decimal integer."""
+        return self._ctrl_state
+
+    @ctrl_state.setter
+    def ctrl_state(self, ctrl_state):
+        """Set the control state of this gate.
+
+        Args:
+            ctrl_state (int or str or None): The control state of the gate.
+
+        Raises:
+            CircuitError: ctrl_state is invalid.
+        """
+        if isinstance(ctrl_state, str):
+            try:
+                assert len(ctrl_state) == self.num_ctrl_qubits
+                ctrl_state = int(ctrl_state, 2)
+            except ValueError:
+                raise CircuitError('invalid control bit string: ' + ctrl_state)
+            except AssertionError:
+                raise CircuitError('invalid control bit string: length != '
+                                   'num_ctrl_qubits')
+        if isinstance(ctrl_state, int):
+            if 0 <= ctrl_state < 2**self.num_ctrl_qubits:
+                self._ctrl_state = ctrl_state
+            else:
+                raise CircuitError('invalid control state specification')
+        elif ctrl_state is None:
+            self._ctrl_state = 2**self.num_ctrl_qubits - 1
+        else:
+            raise CircuitError('invalid control state specification')
 
     def __eq__(self, other):
         if not isinstance(other, ControlledGate):
             return False
         else:
             return (other.num_ctrl_qubits == self.num_ctrl_qubits and
-                    self.base_gate == other.base_gate and
-                    super().__eq__(other))
+                    self.base_gate == other.base_gate)
 
     def inverse(self):
         """Invert this gate by calling inverse on the base gate."""
