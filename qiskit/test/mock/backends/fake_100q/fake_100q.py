a5f4873b7fef01a83558bf25937a1e8de1c748cb
IceKhan13 <iskandar.sitdikov@gmail.com>
2020-02-23 23:09:46 -0800

    FakePulseBackend: defaults initial implementation

--- qiskit/test/mock/backends/fake_100q/fake_100q.py
+++ qiskit/test/mock/backends/fake_100q/fake_100q.py
@@ -15,16 +15,18 @@
 """
 Fake 100 qubit device.
 """
+import itertools
 import os
 import json
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
+import numpy as np
 
 from qiskit.providers.models import (PulseBackendConfiguration,
                                      BackendProperties, PulseDefaults, BackendConfiguration, GateConfig, UchannelLO,
                                      Command)
 from qiskit.providers.models.backendproperties import Nduv, Gate
-from qiskit.qobj import PulseQobjInstruction
+from qiskit.qobj import PulseQobjInstruction, PulseLibraryItem
 from qiskit.test.mock.fake_backend import FakeBackend
 
 
@@ -87,9 +89,19 @@ class FakeBackendBuilder(object):
         self.grid_size = grid_size
         self.version = version
         self.now = datetime.now()
-        # TODO: add CNOT
-        self.basis_gates = ['id', 'u1', 'u2', 'u3']
+        self.basis_gates = ['id', 'u1', 'u2', 'u3', 'cx']
         self.n_qubits = grid_size**2
+
+    def _generate_cmap(self) -> List[List[int]]:
+        """Generate Almaden like connectivity map."""
+        cmap = []
+        for i in range(self.n_qubits):
+            if i % self.grid_size != 0:
+                cmap.append([i, i + 1])
+            if i % self.grid_size < self.grid_size and i % 2 == 0:
+                cmap.append([i, i + self.grid_size])
+
+        return cmap
 
     def build_props(self) -> BackendProperties:
         """Build properties for backend."""
@@ -126,15 +138,6 @@ class FakeBackendBuilder(object):
     def build_conf(self) -> PulseBackendConfiguration:
         """Build configuration for backend."""
         # TODO: correct values
-
-        # generate Almaden like connectivity map
-        cmap = []
-        for i in range(self.n_qubits):
-            if i % self.grid_size != 0:
-                cmap.append([i, i + 1])
-            if i % self.grid_size < self.grid_size and i % 2 == 0:
-                cmap.append([i, i + self.grid_size])
-
         meas_map = [list(range(self.n_qubits))]
         hamiltonian = {'h_str': [], 'description': "", 'qub': {}, 'vars': {}}
         conditional_latency = []
@@ -161,7 +164,7 @@ class FakeBackendBuilder(object):
             memory=False,
             max_shots=65536,
             gates=[GateConfig(name='TODO', parameters=[], qasm_def='TODO')],
-            coupling_map=cmap,
+            coupling_map=self._generate_cmap(),
             n_registers=self.n_qubits,
             n_uchannels=self.n_qubits,
             u_channel_lo=u_channel_lo,
@@ -182,20 +185,59 @@ class FakeBackendBuilder(object):
 
     def build_defaults(self) -> PulseDefaults:
         """Build backend defaults."""
-        # TODO: correct values
-        qubit_freq_est = [4.9] * self.n_qubits
-        meas_freq_est = [6.5] * self.n_qubits
-        buffer = 10
-        pulse_library = []
 
-        cmd_def = []
-        # TODO: fix parameters, instructions, phases
+        qubit_freq_est = np.linspace(4.9, 5.1, self.n_qubits).tolist()
+        meas_freq_est = np.linspace(6.4, 6.6, self.n_qubits).tolist()
+        buffer = 10
+        pulse_library = [PulseLibraryItem(name='test_pulse_1', samples=[0.j, 0.1j]),
+                         PulseLibraryItem(name='test_pulse_2', samples=[0.j, 0.1j, 1j]),
+                         PulseLibraryItem(name='test_pulse_3',
+                                          samples=[0.j, 0.1j, 1j, 0.5 + 0j]),
+                         PulseLibraryItem(name='test_pulse_4',
+                                          samples=7*[0.j, 0.1j, 1j, 0.5 + 0j])]
+
+        measure_command_sequence = [PulseQobjInstruction(name='acquire', duration=10, t0=0,
+                                                         qubits=range(self.n_qubits),
+                                                         memory_slot=range(self.n_qubits))]
+        measure_command_sequence += [PulseQobjInstruction(name='test_pulse_1',
+                                                          ch='m{}'.format(i), t0=0)
+                                     for i in range(self.n_qubits)]
+
+        measure_command = Command(name='measure', qubits=range(self.n_qubits),
+                                  sequence=measure_command_sequence)
+
+        cmd_def = [measure_command]
+
         for i in range(self.n_qubits):
-            for gate in self.basis_gates:
-                cmd_def.append(Command(name=gate, qubits=[i],
-                                       sequence=[PulseQobjInstruction(name='fc',
-                                                                      ch='d{}'.format(i),
-                                                                      t0=0, phase=1.0)]))
+            cmd_def += [
+                Command(name='u1', qubits=[i],
+                        sequence=[PulseQobjInstruction(name='fc', ch='d{}'.format(i),
+                                                       t0=0, phase='-P0')]),
+                Command(name='u2', qubits=[i],
+                        sequence=[PulseQobjInstruction(name='fc', ch='d{}'.format(i),
+                                                       t0=0, phase='-P1'),
+                                  PulseQobjInstruction(name='test_pulse_4',
+                                                       ch='d{}'.format(i), t0=0),
+                                  PulseQobjInstruction(name='fc', ch='d{}'.format(i),
+                                                       t0=0, phase='-P0')]),
+                Command(name='u3', qubits=[i],
+                        sequence=[PulseQobjInstruction(name='test_pulse_3',
+                                                       ch='d{}'.format(i), t0=0)])
+            ]
+
+        for connected_pair in self._generate_cmap():
+            q1, q2 = connected_pair
+            cmd_def += [
+                Command(name='cx', qubits=[q1, q2],
+                        sequence=[PulseQobjInstruction(name='test_pulse_1',
+                                                       ch='d{}'.format(q1), t0=0),
+                                  PulseQobjInstruction(name='test_pulse_2',
+                                                       ch='u{}'.format(q1), t0=10),
+                                  PulseQobjInstruction(name='test_pulse_1',
+                                                       ch='d{}'.format(q2), t0=20),
+                                  PulseQobjInstruction(name='fc', ch='d{}'.format(q2),
+                                                       t0=20, phase=2.1)])
+            ]
 
         return PulseDefaults(
             qubit_freq_est=qubit_freq_est,
