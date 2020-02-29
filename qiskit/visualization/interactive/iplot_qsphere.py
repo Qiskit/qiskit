ecd45d62dc2ee3ceea768a724be1b5636d8a1cdc
michelle4654 <michelle4654@gmail.com>
2020-02-29 15:00:58 -0800

    Update iplot_qsphere.py

--- qiskit/visualization/interactive/iplot_qsphere.py
+++ qiskit/visualization/interactive/iplot_qsphere.py
@@ -33,7 +33,7 @@ if ('ipykernel' in sys.modules) and ('spyder' not in sys.modules):
         from IPython.core.display import display, HTML
     except ImportError:
         print("Error importing IPython.core.display. To install, run "
-              '"pip install ipython".')
+              '"pip install ipython". ')
 
 
 def iplot_state_qsphere(rho, figsize=None):
