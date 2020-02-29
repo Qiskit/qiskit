5759ca18c55f937cda833ba7d57a25fee543778d
michelle4654 <michelle4654@gmail.com>
2020-02-29 14:51:58 -0800

    Update iplot_blochsphere.py

--- qiskit/visualization/interactive/iplot_blochsphere.py
+++ qiskit/visualization/interactive/iplot_blochsphere.py
@@ -27,7 +27,7 @@ if ('ipykernel' in sys.modules) and ('spyder' not in sys.modules):
         from IPython.core.display import display, HTML
     except ImportError:
         print("Error importing IPython.core.display. To install, run "
-              '"pip install ipython".')
+              '"pip install ipython". ')
 
 
 def iplot_bloch_multivector(rho, figsize=None):
