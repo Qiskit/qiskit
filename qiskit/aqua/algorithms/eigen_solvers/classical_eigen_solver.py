bc70e47e2c636cc56ae03bf282d07e7a9b8ef08d
woodsp <woodsp@us.ibm.com>
2020-03-02 23:18:29 -0500

    Add minimum eigen solver interface

--- qiskit/aqua/algorithms/eigen_solvers/classical_eigen_solver.py
+++ qiskit/aqua/algorithms/eigen_solvers/classical_eigen_solver.py
@@ -21,6 +21,7 @@ import warnings
 import numpy as np
 from scipy import sparse as scisparse
 
+from qiskit.aqua import AquaError
 from qiskit.aqua.algorithms import ClassicalAlgorithm
 from qiskit.aqua.operators import op_converter
 from qiskit.aqua.operators import BaseOperator
@@ -35,9 +36,9 @@ logger = logging.getLogger(__name__)
 
 class ClassicalEigensolver(ClassicalAlgorithm):
     r"""
-    The Exact Eigensolver algorithm.
+    The Classical Eigensolver algorithm.
 
-    Exact Eigensolver computes up to the first :math:`k` eigenvalues of a complex-valued square
+    Classical Eigensolver computes up to the first :math:`k` eigenvalues of a complex-valued square
     matrix of dimension :math:`n \times n`, with :math:`k \leq n`.
 
     Note:
@@ -46,18 +47,56 @@ class ClassicalEigensolver(ClassicalAlgorithm):
         operator size, mostly in terms of number of qubits it represents, gets larger.
     """
 
-    def __init__(self, operator: BaseOperator, k: int = 1,
+    def __init__(self, operator: Optional[BaseOperator] = None, k: int = 1,
                  aux_operators: Optional[List[BaseOperator]] = None) -> None:
         """
         Args:
-            operator: Operator instance
+            operator: Operator instance. If None is supplied it must be provided later before
+                run() is called. Allowing None here permits the algorithm to be configured
+                and used later when operator is available, say creating an instance an letting
+                application stack use this algorithm with an operator it creates.
             k: How many eigenvalues are to be computed, has a min. value of 1.
             aux_operators: Auxiliary operators to be evaluated at each eigenvalue
         """
         validate_min('k', k, 1)
         super().__init__()
 
-        self._operator = op_converter.to_matrix_operator(operator)
+        self._in_operator = None
+        self._in_aux_operators = None
+        self._operator = None
+        self._aux_operators = None
+        self._in_k = k
+        self._k = k
+
+        self.operator = operator
+        self.aux_operators = aux_operators
+
+        self._ret = {}
+
+    @property
+    def operator(self) -> BaseOperator:
+        """ returns operator """
+        return self._in_operator
+
+    @operator.setter
+    def operator(self, operator: BaseOperator) -> None:
+        """ set operator """
+        self._in_operator = operator
+        if operator is None:
+            self._operator = None
+        else:
+            self._operator = op_converter.to_matrix_operator(operator)
+            self._check_set_k()
+
+    @property
+    def aux_operators(self) -> List[BaseOperator]:
+        """ returns aux operators """
+        return self._in_aux_operators
+
+    @aux_operators.setter
+    def aux_operators(self, aux_operators: List[BaseOperator]) -> None:
+        """ set aux operators """
+        self._in_aux_operators = aux_operators
         if aux_operators is None:
             self._aux_operators = []
         else:
@@ -65,16 +104,32 @@ class ClassicalEigensolver(ClassicalAlgorithm):
                 [aux_operators] if not isinstance(aux_operators, list) else aux_operators
             self._aux_operators = \
                 [op_converter.to_matrix_operator(aux_op) for aux_op in aux_operators]
-        self._k = k
-        if self._k > self._operator.matrix.shape[0]:
-            self._k = self._operator.matrix.shape[0]
-            logger.debug("WARNING: Asked for %s eigenvalues but max possible is %s.", k, self._k)
-        self._ret = {}
+
+    @property
+    def k(self) -> int:
+        """ returns k (number of eigenvalues requested) """
+        return self._in_k
+
+    @k.setter
+    def k(self, k: int) -> int:
+        """ set k (number of eigenvalues requested) """
+        validate_min('k', k, 1)
+        self._in_k = k
+        self._check_set_k()
+
+    def _check_set_k(self):
+        if self._operator is not None:
+            if self._in_k > self._operator.matrix.shape[0]:
+                self._k = self._operator.matrix.shape[0]
+                logger.debug("WARNING: Asked for %s eigenvalues but max possible is %s.",
+                             self._in_k, self._k)
+            else:
+                self._k = self._in_k
 
     def _solve(self):
         if self._operator.dia_matrix is None:
             if self._k >= self._operator.matrix.shape[0] - 1:
-                logger.debug("Scipy doesn't support to get all eigenvalues, using numpy instead.")
+                logger.debug("SciPy doesn't support to get all eigenvalues, using NumPy instead.")
                 eigval, eigvec = np.linalg.eig(self._operator.matrix.toarray())
             else:
                 eigval, eigvec = scisparse.linalg.eigs(self._operator.matrix, k=self._k, which='SR')
@@ -126,6 +181,10 @@ class ClassicalEigensolver(ClassicalAlgorithm):
         Returns:
             dict: Dictionary of results
         """
+        if self._operator is None:
+            raise AquaError("Operator was never provided")
+
+        self._ret = {}
         self._solve()
         self._get_ground_state_energy()
         self._get_energies()
---
+++ qiskit/aqua/algorithms/minimum_eigen_solvers/minimum_eigen_solver.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2020.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""The Minimum Eigensolver interface"""
+
+
+from abc import ABC, abstractmethod
+from typing import List, Optional
+
+from .minimum_eigen_solver_result import MinimumEigensolverResult
+from qiskit.aqua.operators import BaseOperator
+
+
+class MinimumEigensolver(ABC):
+    """
+    The Minimum Eigensolver Interface.
+
+    Algorithms that can compute a minimum eigenvalue for an operator
+    may implement this interface to allow different algorithms to be
+    used interchangeably.
+    """
+
+    @abstractmethod
+    def compute_minimum_eigenvalue(
+            self, operator: Optional[BaseOperator] = None,
+            aux_operators: Optional[List[BaseOperator]] = None) -> MinimumEigensolverResult:
+        """
+        Computes minimum eigenvalue. Operator and aux_operators can be supplied here and
+        if not None will override any already set into algorithm so it can be reused with
+        different operators. While an operator is required by algorithms, aux_operators
+        are optional. To 'remove' a previous aux_operators array use an empty list here.
+
+        Args:
+            operator: If not None replaces operator in algorithm
+            aux_operators:  If not None replaces aux_operators in algorithm
+
+        Returns:
+            MinimumEigensolverResult
+        """
+        if operator is not None:
+            self.operator = operator
+        if aux_operators is not None:
+            self.aux_operators = aux_operators if aux_operators else None
+        pass
+
+    def supports_aux_operators(self) -> bool:
+        """
+        If the minimum eigensolver computes an eigenstate of the main operator then it
+        can compute the expectation value of the aux_operators for that state. Otherwise
+        they will be ignored
+
+        Returns:
+            True if aux_operator expectations can be evaluated, False otherwise
+        """
+        return False
+
+    @property
+    @abstractmethod
+    def operator(self) -> BaseOperator:
+        """ returns operator """
+        pass
+
+    @operator.setter
+    @abstractmethod
+    def operator(self, operator: BaseOperator) -> None:
+        """ set operator """
+        pass
+
+    @property
+    @abstractmethod
+    def aux_operators(self) -> List[BaseOperator]:
+        """ returns aux operators """
+        pass
+
+    @aux_operators.setter
+    @abstractmethod
+    def aux_operators(self, aux_operators: List[BaseOperator]) -> None:
+        """ set aux operators """
+        pass
