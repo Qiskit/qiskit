9a09ae8b367393a252e936b933eab2e4dbf02c9b
Matthew Treinish <mtreinish@kortar.org>
2020-03-04 09:34:37 -0500

    Revert "Ignore cyclic import in scheduler"
    This reverts commit e3cb2f40e0812c6415a61225a83446963ca1b687.

--- qiskit/scheduler/__init__.py
+++ qiskit/scheduler/__init__.py
@@ -12,8 +12,6 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-# pylint: disable=cyclic-import
-
 """
 ===========================================
 Circuit Scheduler (:mod:`qiskit.scheduler`)
