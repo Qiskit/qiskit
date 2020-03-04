e3cb2f40e0812c6415a61225a83446963ca1b687
Matthew Treinish <mtreinish@kortar.org>
2020-03-04 09:22:21 -0500

    Ignore cyclic import in scheduler

--- qiskit/scheduler/__init__.py
+++ qiskit/scheduler/__init__.py
@@ -12,6 +12,8 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
+# pylint: disable=cyclic-import
+
 """
 ===========================================
 Circuit Scheduler (:mod:`qiskit.scheduler`)
