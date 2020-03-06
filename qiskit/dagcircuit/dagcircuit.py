28cb807c01bba91c6c8c11ecdcc1100067121fe8
Matthew Treinish <mtreinish@kortar.org>
2020-03-06 12:56:52 -0500

    Switch from ordered set back to set() with a property
    This commit stop using OrderedSet because we can achieve similar
    performance without an external dependency. Instead it changes wires to
    be a private attribute and makes a property wires() which will return a
    sorted bit list. This way we get the benefits of lookup time inside
    of a dagcircuit object but when something outside needs an ordered list
    of bits they can continue using the property as today.

--- qiskit/dagcircuit/dagcircuit.py
+++ qiskit/dagcircuit/dagcircuit.py
@@ -27,8 +27,6 @@ import copy
 import itertools
 import networkx as nx
 
-from ordered_set import OrderedSet
-
 from qiskit.circuit.quantumregister import QuantumRegister, Qubit
 from qiskit.circuit.classicalregister import ClassicalRegister, Clbit
 from qiskit.circuit.gate import Gate
@@ -55,7 +53,7 @@ class DAGCircuit:
         self.name = None
 
         # Set of wires (Register,idx) in the dag
-        self.wires = OrderedSet()
+        self._wires = set()
 
         # Map from wire (Register,idx) to input nodes of the graph
         self.input_map = OrderedDict()
@@ -95,6 +93,11 @@ class DAGCircuit:
     def clbits(self):
         """Return a list of classical bits (as a list of Clbit instances)."""
         return [clbit for creg in self.cregs.values() for clbit in creg]
+
+    @property
+    def wires(self):
+        """Return a generator that returns wires in order."""
+        return sorted(self._wires, key=lambda bit: (bit.register.name, bit.index))
 
     @property
     def node_counter(self):
@@ -138,8 +141,8 @@ class DAGCircuit:
         Raises:
             DAGCircuitError: if trying to add duplicate wire
         """
-        if wire not in self.wires:
-            self.wires.add(wire)
+        if wire not in self._wires:
+            self._wires.add(wire)
             self._max_node_id += 1
             input_map_wire = self.input_map[wire] = self._max_node_id
 
@@ -482,7 +485,7 @@ class DAGCircuit:
                     raise DAGCircuitError("wire %s[%d] not in self" % (
                         m_wire.register.name, m_wire.index))
 
-                if nd.wire not in input_circuit.wires:
+                if nd.wire not in input_circuit._wires:
                     raise DAGCircuitError("inconsistent wire type for %s[%d] in input_circuit"
                                           % (nd.register.name, nd.wire.index))
 
@@ -546,7 +549,7 @@ class DAGCircuit:
                     raise DAGCircuitError("wire %s[%d] not in self" % (
                         m_name.register.name, m_name.index))
 
-                if nd.wire not in input_circuit.wires:
+                if nd.wire not in input_circuit._wires:
                     raise DAGCircuitError(
                         "inconsistent wire for %s[%d] in input_circuit"
                         % (nd.wire.register.name, nd.wire.index))
@@ -569,14 +572,14 @@ class DAGCircuit:
         Yields:
             Bit: Bit in idle wire.
         """
-        for wire in self.wires:
+        for wire in self._wires:
             nodes = self.nodes_on_wire(wire, only_ops=False)
             if len(list(nodes)) == 2:
                 yield wire
 
     def size(self):
         """Return the number of operations."""
-        return self._multi_graph.order() - 2 * len(self.wires)
+        return self._multi_graph.order() - 2 * len(self._wires)
 
     def depth(self):
         """Return the circuit depth.
@@ -594,12 +597,12 @@ class DAGCircuit:
     def width(self):
         """Return the total number of qubits + clbits used by the circuit.
            This function formerly returned the number of qubits by the calculation
-           return len(self.wires) - self.num_clbits()
+           return len(self._wires) - self.num_clbits()
            but was changed by issue #2564 to return number of qubits + clbits
            with the new function DAGCircuit.num_qubits replacing the former
            semantic of DAGCircuit.width().
         """
-        return len(self.wires)
+        return len(self._wires)
 
     def num_qubits(self):
         """Return the total number of qubits used by the circuit.
@@ -607,7 +610,7 @@ class DAGCircuit:
            DAGCircuit.width() now returns qubits + clbits for
            consistency with Circuit.width() [qiskit-terra #2564].
         """
-        return len(self.wires) - self.num_clbits()
+        return len(self._wires) - self.num_clbits()
 
     def num_clbits(self):
         """Return the total number of classical bits used by the circuit."""
