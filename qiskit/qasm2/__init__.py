c48b9d96c5245bfb66ee1036ef731b7292ffc91d
Jack J. Woehr <jwoehr@softwoehr.com>
2020-03-10 11:07:21 -0600

    Begin abstraction of Qasm translation into functional interface

---
+++ qiskit/qasm2/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2017.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+=========================
+Qasm (:mod:`qiskit.qasm`)
+=========================
+
+.. currentmodule:: qiskit.qasm2
+
+.. autosummary::
+   :toctree: ../stubs/
+
+   Qasm
+   QasmError
+"""
+
+# from numpy import pi
+
+# from .qasm import Qasm
+# from .exceptions import QasmError
