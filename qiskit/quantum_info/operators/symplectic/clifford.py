8a6eb76eac7431a0665144c8d6f03e6d72953064
sethmerkel <49210444+sethmerkel@users.noreply.github.com>
2020-03-11 11:19:32 -0400

    Add compose, tensor, expand, and is_unitary
    Co-Authored-By: Christopher J. Wood <cjwood@us.ibm.com>

--- qiskit/quantum_info/operators/symplectic/clifford.py
+++ qiskit/quantum_info/operators/symplectic/clifford.py
@@ -134,10 +134,16 @@ class Clifford(BaseOperator):
         # checking that the underlying Stabilizer table array is a valid
         # Clifford array.
 
-        # TODO: IMPLEMENT ME!
+        # Condition is
+        # table.T * [[0, 1], [1, 0]] * table = [[0, 1], [1, 0]]
+        # where we are block matrix multiplying using symplectic product
 
-        raise NotImplementedError(
-            'This method has not been implemented for Clifford operators yet.')
+        one = np.eye(self.n_qubits, dtype=int)
+        zero = np.zeros((self.n_qubits, self.n_qubits), dtype=int)
+        seye = np.block([[zero, one], [one, zero]])
+        arr = self.table.array.astype(int)
+
+        return np.array_equal(arr.T.dot(seye).dot(arr) % 2, seye)
 
     def to_matrix(self):
         """Convert operator to Numpy matrix."""
@@ -161,19 +167,27 @@ class Clifford(BaseOperator):
 
     def conjugate(self):
         """Return the conjugate of the Clifford."""
-
-        # TODO: IMPLEMENT ME!
-
-        raise NotImplementedError(
-            'This method has not been implemented for Clifford operators yet.')
+        # TODO: Needs testing to see if correct
+        x = self.table.X
+        z = self.table.Z
+        ret = self.copy()
+        ret.table.phase = self.table.phase ^ (np.sum(x & z, axis=1) % 2)
+        return ret
 
     def transpose(self):
         """Return the transpose of the Clifford."""
 
-        # TODO: IMPLEMENT ME!
+        # TODO: Needs testing to see if correct
+        # This is done using block matrix multiplication
+        # [[0, 1], [1, 0]] * table.T * [[0, 1], [1, 0]]
 
-        raise NotImplementedError(
-            'This method has not been implemented for Clifford operators yet.')
+        ret = self.copy()
+        tmp = ret.destabilizer.X.copy()
+        ret.destabilizer.X = ret.stabilizer.Z
+        ret.destabilizer.Z = ret.destabilizer.Z.T
+        ret.stabilizer.X = ret.stabilizer.X.T
+        ret.stabilizer.Z = tmp
+        return ret
 
     def compose(self, other, qargs=None, front=False):
         """Return the composed operator.
@@ -210,10 +224,11 @@ class Clifford(BaseOperator):
         # Clifford object can't be changed by composition
         self._get_compose_dims(other, qargs, front)
 
-        # TODO: IMPLEMENT ME!
+        if qargs is None or (
+                len(qargs) == self.n_qubits and sorted(qargs) == qargs):
+            return self._compose_clifford(other, front=front)
 
-        raise NotImplementedError(
-            'This method has not been implemented for Clifford operators yet.')
+        return self._compose_subsystem(other, qargs, front=front)
 
     def dot(self, other, qargs=None):
         """Return the right multiplied operator self * other.
@@ -242,13 +257,7 @@ class Clifford(BaseOperator):
         Returns:
             Clifford: the tensor product operator self ⊗ other.
         """
-        if not isinstance(other, Clifford):
-            other = Clifford(other)
-
-        # TODO: IMPLEMENT ME!
-
-        raise NotImplementedError(
-            'This method has not been implemented for Clifford operators yet.')
+        return self._tensor_product(other, reverse=False)
 
     def expand(self, other):
         """Return the tensor product operator other ⊗ self.
@@ -259,13 +268,7 @@ class Clifford(BaseOperator):
         Returns:
             Clifford: the tensor product operator other ⊗ self.
         """
-        if not isinstance(other, Clifford):
-            other = Clifford(other)
-
-        # TODO: IMPLEMENT ME!
-
-        raise NotImplementedError(
-            'This method has not been implemented for Clifford operators yet.')
+        return self._tensor_product(other, reverse=True)
 
     # ---------------------------------------------------------------------
     # Representation conversions
@@ -311,3 +314,161 @@ class Clifford(BaseOperator):
         clifford = Clifford(np.eye(2 * instruction.num_qubits))
         append_gate(clifford, instruction)
         return clifford
+
+    # ---------------------------------------------------------------------
+    # Internal tensor produce
+    # ---------------------------------------------------------------------
+    def _tensor_product(self, other, reverse=False):
+        """Return the tensor product operator.
+
+        Args:
+            other (Clifford): another Clifford operator.
+            reverse (bool): If False return self ⊗ other, if True return
+                            if True return (other ⊗ self) [Default: False
+        Returns:
+            Clifford: the tensor product operator.
+
+        Raises:
+            QiskitError: if other cannot be converted into an Clifford.
+        """
+        if not isinstance(other, Clifford):
+            other = Clifford(other)
+
+        if reverse:
+            first = other
+            second = self
+        else:
+            first = self
+            second = other
+        n_first = first.n_qubits
+        n_second = second.n_qubits
+
+        # Pad stabilizers and destabilizers
+        destab = (first.destabilizer.tensor(n_second * 'I') +
+                  second.destabilizer.expand(n_first * 'I'))
+        stab = (first.stabilizer.tensor(n_second * 'I') +
+                second.stabilizer.expand(n_first * 'I'))
+
+        # Add the padded table
+        table = destab + stab
+        return Clifford(table)
+
+    # ---------------------------------------------------------------------
+    # Internal composition methods
+    # ---------------------------------------------------------------------
+    def _compose_subsystem(self, other, qargs, front=False):
+        """Return the composition channel."""
+        # Create Clifford on full system from subsystem and compose
+        nq = self.n_qubits
+        no = other.n_qubits
+        fullother = self.copy()
+        fullother.table.array = np.eye(2 * self.n_qubits, dtype=np.bool)
+        for inda, qinda in enumerate(qargs):
+            for indb, qindb in enumerate(qargs):
+                fullother.table._array[nq - 1 - qinda, nq - 1 - qindb] = other.table._array[
+                    no - 1 - inda, no - 1 - indb]
+                fullother.table._array[nq - 1 - qinda, 2*nq - 1 - qindb] = other.table._array[
+                    no - 1 - inda, 2*no - 1 - indb]
+                fullother.table._array[2*nq - 1 - qinda, nq - 1 - qindb] = other.table._array[
+                    2*no - 1 - inda, no - 1 - indb]
+                fullother.table._array[2*nq - 1 - qinda, 2*nq - 1 - qindb] = other.table._array[
+                    2*no - 1 - inda, 2*no - 1 - indb]
+                fullother.table._phase[nq - 1 - qinda] = other.table._phase[no - 1 - inda]
+                fullother.table._phase[2*nq - 1 - qinda] = other.table._phase[2*no - 1 - inda]
+        return self._compose_clifford(fullother, front=front)
+
+    def _compose_clifford(self, other, front=False):
+        """Return the composition channel assume other is Clifford of same size as self."""
+        if front:
+            table1 = self.table
+            table2 = other.table
+        else:
+            table1 = other.table
+            table2 = self.table
+
+        # PREVIOUS METHOD:
+        # This one isn't currently getting phases correct
+
+        # ret_table = table2.copy()
+        #
+        # Zero the return array, leave the phases in place
+        # ret_table.array *= False
+        # for i in range(ret_table.size):
+        #     for j in range(table1.size):
+        #         if table2.array[i, j]:
+        #             ret_table[i] = self._rowsum(ret_table[i], table1[j])
+        #
+        # return Clifford(ret_table)
+
+        # ALT METHOD:
+        # This one is correct but needs to be optimized
+
+        num_qubits = self.n_qubits
+
+        array1 = table1.array.astype(int)
+        phase1 = table1.phase.astype(int)
+
+        array2 = table2.array.astype(int)
+        phase2 = table2.phase.astype(int)
+
+        # Update Pauli table
+        pauli = StabilizerTable(array2.dot(array1) % 2)
+
+        # Add phases
+        phase = np.mod(array2.dot(phase1) + phase2, 2)
+
+        # Correcting for phase due to Pauli multiplicatio
+        ifacts = np.zeros(2 * num_qubits, dtype=np.int)
+
+        for r2 in range(2 * num_qubits):
+
+            row2 = array2[r2]
+            x2 = table2.X[r2]
+            z2 = table2.Z[r2]
+
+            # Adding a factor of i for each Y in the image of an operator under the
+            # first operation, since Y=iXZ
+
+            ifacts[r2] += np.sum(x2 & z2)
+
+            # Adding factors of i due to qubit-wise Pauli multiplication
+
+            x = np.zeros(num_qubits, dtype=int)
+            z = np.zeros(num_qubits, dtype=int)
+
+            for i, r1 in enumerate(table1):
+
+                x1 = r1.X[0].astype(int)
+                z1 = r1.Z[0].astype(int)
+
+                val = np.mod(abs(3 * z1 - x1) - abs(3 * z - x) - 1, 3)
+                shift = 1 * (val == 0) - 1 * (val == 1)
+                shift = row2[i] * (x1 | z1) * (x | z) * shift
+
+                x = (x + row2[i] * x1) % 2
+                z = (z + row2[i] * z1) % 2
+
+                ifacts[r2] += np.sum(shift)
+
+        p = np.mod(ifacts, 4) // 2
+
+        phase = np.mod(phase + p, 2)
+
+        return Clifford(StabilizerTable(pauli, phase))
+
+    @staticmethod
+    def _rowsum(row1, row2):
+        """Rowsum from AG paper"""
+        x1, z1 = row1.X, row1.Z
+        x2, z2 = row2.X, row2.Z
+
+        # Phase update (g function in AG paper)
+        phase = row1.phase ^ row2.phase ^ np.array(
+            np.sum((~x1 & z1 & x2 & ~z2) |
+                   (x1 & ~z1 & x2 & z2) |
+                   (x1 & z1 & ~x2 & z2), axis=1) % 2, dtype=np.bool)
+
+        # Pauli update
+        pauli = row1.array ^ row2.array
+
+        return StabilizerTable(pauli, phase)
