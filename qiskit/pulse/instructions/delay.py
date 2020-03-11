aedf2f719d1b328578937a088c8becb4ceff294b
Lauren Capelluto <laurencapelluto@gmail.com>
2020-03-11 16:14:35 -0400

    Command/Inst refactor: Delay (#3924)
    * Migrate Delay from a Command to an Instruction, and deprecate DelayInstruction, while maintaining support for the previous API
    
    * Scattering of bugfixes
    
    * Remove extra line
    
    * Add reno note update
    
    * Add new test
    
    * Update qiskit/pulse/instructions/delay.py
    
    * Fix up releasenote, single : to double ::
    
    * Give instructions a default name
    
    * Update releasenote as a deprecation
    
    * Fixup release notes typo: deprecation needs to be plural
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

---
+++ qiskit/pulse/instructions/delay.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2020.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""An instruction for blocking time on a channel; useful for scheduling alignment."""
+import warnings
+
+from typing import List, Optional, Union
+
+from qiskit.pulse.channels import PulseChannel
+from qiskit.pulse.exceptions import PulseError
+from .instruction import Instruction
+
+
+class Delay(Instruction):
+    """A blocking instruction with no other effect. The delay is used for aligning and scheduling
+    other instructions.
+
+    Example:
+
+        To schedule an instruction at time = 10, on a channel assigned to the variable ``channel``,
+        the following could be used::
+
+            sched = Schedule(name="Delay instruction example")
+            sched += Delay(10, channel)
+            sched += Gaussian(duration, amp, sigma, channel)
+
+        The ``channel`` will output no signal from time=0 up until time=10.
+    """
+
+    def __init__(self, duration: int,
+                 channel: Optional[PulseChannel] = None,
+                 name: Optional[str] = None):
+        """Create a new delay instruction.
+
+        No other instruction may be scheduled within a ``Delay``.
+
+        Args:
+            duration: Length of time of the delay in terms of dt.
+            channel: The channel that will have the delay.
+            name: Name of the delay for display purposes.
+        """
+        if channel is None:
+            warnings.warn("Usage of Delay without specifying a channel is deprecated. For "
+                          "example, Delay(5)(DriveChannel(0)) should be replaced by "
+                          "Delay(5, DriveChannel(0)).", DeprecationWarning)
+        self._channel = channel
+        super().__init__(duration, channel, name=name)
+
+    @property
+    def operands(self) -> List[Union[int, PulseChannel]]:
+        """Return a list of instruction operands."""
+        return [self.duration, self.channel]
+
+    @property
+    def channel(self) -> PulseChannel:
+        """Return the :py:class:`~qiskit.pulse.channels.Channel` that this instruction is
+        scheduled on.
+        """
+        return self._channel
+
+    def __call__(self, channel: PulseChannel) -> 'Delay':
+        """Return new ``Delay`` that is fully instantiated with both ``duration`` and a ``channel``.
+
+        Args:
+            channel: The channel that will have the delay.
+
+        Return:
+            Complete and ready to schedule ``Delay``.
+
+        Raises:
+            PulseError: If ``channel`` has already been set.
+        """
+        warnings.warn("Calling Delay with a channel is deprecated. Instantiate the delay with "
+                      "a channel instead.", DeprecationWarning)
+        if self._channel is not None:
+            raise PulseError("The channel has already been assigned as {}.".format(self.channel))
+        return Delay(self.duration, channel)
+
+    def __repr__(self):
+        return "{}({}, {})".format(self.__class__.__name__,
+                                   self.duration,
+                                   self.channel)
