b6efb76ce820e5d4edcf7e8a6ec28bf13a8fa51d
Jack J. Woehr <jwoehr@softwoehr.com>
2020-03-14 01:14:31 -0600

    more on unit test

--- test/python/test_qasm2.py
+++ test/python/test_qasm2.py
@@ -25,46 +25,68 @@ class TestQasm2(QiskitTestCase):
     """Test Qasm2 functional interface to QASM loading/unloading"""
 
     def setUp(self):
+
+        # Source file path
         self.qasm_file_path = self._get_resource_path('yiqing.qasm',
                                                       Path.QASM2)
-        self.yiqing_circ_draw_path = self._get_resource_path('output/yiqing_circ_draw.txt',
-                                                             Path.QASM2)
-        y_f = open(self.yiqing_circ_draw_path, 'r')
-        self.yiqing_circ_draw = y_f.read()
+        # Captured circuit draw
+        self._circ_draw_path = self._get_resource_path('output/yiqing_circ_draw.txt',
+                                                       Path.QASM2)
+        y_f = open(self._circ_draw_path, 'r')
+        self._circ_draw = y_f.read()
         y_f.close()
 
-    def test_load_unload(self):
-        """Test Qasm2 load() and unload()"""
-        ffile = open(self.qasm_file_path, 'r')
-        lines = ffile.read()
-        ffile.close()
+        # The file with unload output from QuantumCircuit
+        # It differs in order from the original source file.
+        self._circ_unload_path = self._get_resource_path('output/yiqing_circ_unload.txt',
+                                                         Path.QASM2)
+        y_f = open(self._circ_unload_path, 'r')
+        self._circ_unload = y_f.read()
+        y_f.close()
+
+        y_f = open(self.qasm_file_path, 'r')
+        lines = y_f.read()
+        y_f.close()
         lines_list = lines.split(os.linesep)
 
         # qasm string loaded via qiskit qasm
-        c_0 = load(data=lines)
+        self.c_0 = load(data=lines)
         # print(c0.draw())
 
         # qasm list of string loaded via qiskit qasm'
-        c_0a = load(data=lines_list)
+        self.c_0a = load(data=lines_list)
         # print(c0a.draw())
 
-        c_0_unloaded = unload(c_0)
-        c_0a_unloaded = unload(c_0a)
+        self.c_0_unloaded = unload(self.c_0)
+        self.c_0a_unloaded = unload(self.c_0a)
 
-        self.assertTrue(c_0, "Error: Circuit c_0 was not generated.")
-        self.assertTrue(c_0a, "Error: Circuit c_0a was not generated.")
+    def test_load(self):
+        """Test Qasm2 load() functional interface"""
 
-        self.assertEqual(c_0, c_0a,
+        self.assertTrue(self.c_0, "Error: Circuit c_0 was not generated.")
+        self.assertTrue(self.c_0a, "Error: Circuit c_0a was not generated.")
+
+        self.assertEqual(self.c_0, self.c_0a,
                          "Error: Circuits c_0 and c_0a are not the same.")
 
-        self.assertEqual(str(c_0.draw()),
-                         self.yiqing_circ_draw,
-                         "Error: Circuit c_0 draw doesn't match {}".format(self.yiqing_circ_draw_path))  #pylint: disable=line-too-long
+        s_draw_no_match = "Error: Circuit c_0 draw doesn't match {}"
+        self.assertEqual(str(self.c_0.draw()),
+                         self._circ_draw,
+                         s_draw_no_match.format(self._circ_draw_path))
 
-        self.assertEqual(c_0_unloaded, c_0a_unloaded,
+    def test_unload(self):
+        """Test Qasm2 unload() functional interface"""
+
+        self.assertEqual(self.c_0_unloaded, self.c_0a_unloaded,
                          "Error: Circuits c_0 and c_0a don't unload the same.")
-        # self.assertEqual(c_0_unloaded, lines,
-        #                  "Error: Circuit c_0 unloaded doesn't match{}".format(self.qasm_file_path))   #pylint: disable=line-too-long
+
+        # This fails to match, have not identified the problem
+        # s_unload_unmatch = "Error:\n{}\ndoesn't match\n{}"
+        # self.assertEqual(self.c_0_unloaded,
+        #                  self._circ_unload,
+        #                  s_unload_unmatch.format(self.c_0_unloaded,
+        #                                          self._circ_unload))
+
 
 if __name__ == '__main__':
     unittest.main()
