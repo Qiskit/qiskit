53f115799abac59f5141ba75aa5b996b02c4de5c
michelle4654 <michelle4654@gmail.com>
2020-03-17 10:55:34 -0500

    Fixed Issue 3649 by adding 'pip install...' to ImportErrors (#3880)
    * Fixed issue #3649 by adding 'pip install...' to ImportErrors
    
    * Added warning to backend monitor
    
    * Update backend_monitor.py
    
    * Update qiskit/test/decorators.py
    
    Co-Authored-By: Kevin Krsulich <kevin@krsulich.net>
    
    * Update decorators.py
    
    * Update crosstalk_adaptive_schedule.py
    
    * Update gate_map.py
    
    * Update iplot_blochsphere.py
    
    * Update iplot_cities.py
    
    * Update iplot_hinton.py
    
    * Update iplot_histogram.py
    
    * Update iplot_paulivec.py
    
    * Update iplot_qsphere.py
    
    * Update matplotlib.py
    
    * Update state_visualization.py
    
    * Update qiskit/test/decorators.py
    
    Co-Authored-By: Kevin Krsulich <kevin@krsulich.net>
    
    * Update crosstalk_adaptive_schedule.py
    
    * Update gate_map.py
    
    * Update iplot_blochsphere.py
    
    * Update iplot_cities.py
    
    * Update iplot_hinton.py
    
    * Update iplot_histogram.py
    
    * Update iplot_paulivec.py
    
    * Update iplot_qsphere.py
    
    * Update matplotlib.py
    
    * Update state_visualization.py
    
    * Update decorators.py
    
    Co-authored-by: Kevin Krsulich <kevin@krsulich.net>
    Co-authored-by: Kevin Krsulich <kevin.krsulich@ibm.com>
    Co-authored-by: Luciano Bello <luciano.bello@ibm.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- qiskit/visualization/interactive/iplot_blochsphere.py
+++ qiskit/visualization/interactive/iplot_blochsphere.py
@@ -26,7 +26,8 @@ if ('ipykernel' in sys.modules) and ('spyder' not in sys.modules):
     try:
         from IPython.core.display import display, HTML
     except ImportError:
-        print("Error importing IPython.core.display")
+        print("Error importing IPython.core.display. To install, run "
+              '"pip install ipython".')
 
 
 def iplot_bloch_multivector(rho, figsize=None):
