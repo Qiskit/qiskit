4116f0f9ad8a73ada0acc343f8cb809bfffb8ce7
Lauren Capelluto <laurencapelluto@gmail.com>
2020-03-18 15:22:42 -0400

    Command/Inst refactor: Acquire (and Kernel, Discriminator) (#3935)
    * Make new Acquire instruction and update path to Acquire command to point to the new one. Deprecate AcquireInstruction and make it return an Acquire instead
    
    * Fix some style and continue implementation
    
    * Fill in docs and fix tests
    
    * Fill in __repr__ and __eq__ as necessary for Kernel, Discriminator, Acqurie. Fix up some more tests. Fill in pulse_instructions converter for Acquire
    
    * Style fixes
    
    * Fix bug missing inclusion of Acquire type along with AcquireInstruction
    
    * Fixup some kernel instantiation in pulse instructions converter
    
    * Fixup style
    
    * Try to fix the docs build
    
    * Update qiskit/pulse/instructions/acquire.py
    
    Improve description of Acquire
    
    Co-Authored-By: Thomas Alexander <thomasalexander2718@gmail.com>
    
    * Respond to review
    
    * Update repr for Kernel and Discriminator to reflect **params. Update a test to also match
    
    * style
    
    * Complete release note
    
    * Complete reno note (was missing one line)
    
    * discriminator and kernel shouldn't be treated as instruction operands
    
    * Add link in releasenote
    
    * Update releasenotes/notes/unify-instructions-and-commands-aaa6d8724b8a29d3.yaml
    
    Co-Authored-By: Matthew Treinish <mtreinish@kortar.org>
    
    Co-authored-by: Thomas Alexander <thomasalexander2718@gmail.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    Co-authored-by: Matthew Treinish <mtreinish@kortar.org>

--- qiskit/pulse/commands/meas_opts.py
+++ qiskit/pulse/commands/meas_opts.py
@@ -12,77 +12,9 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-# pylint: disable=missing-param-doc,useless-super-delegation
+"""Measurement options. Deprecated path."""
 
-"""
-Measurement options.
-"""
-
-from typing import Optional
+# pylint: disable=unused-import
 
 
-class MeasOpts:
-    """Measurement options."""
-
-    def __init__(self, name: Optional[str] = None, **params):
-        """Create new measurement options.
-
-        Parameters:
-            name: Name of measurement option to be used.
-        """
-        self._name = name
-        self._params = params
-
-    @property
-    def name(self):
-        """Return parameter name."""
-        return self._name
-
-    @property
-    def params(self):
-        """Return parameter dict."""
-        return self._params
-
-    def __eq__(self, other: 'MeasOpts'):
-        """Two measurement options are the same if they are of the same type
-        and have the same name and params.
-
-        Args:
-            other: Other Discriminator/Kernel
-
-        Returns:
-            bool: are self and other equal
-        """
-        return (type(self) is type(other) and
-                self.name == other.name and
-                self.params == other.params)
-
-    def __hash__(self):
-        return hash((super().__hash__(), self.name, self.params))
-
-    def __repr__(self):
-        return '%s(%s)' % (self.__class__.__name__, self.name)
-
-
-class Discriminator(MeasOpts):
-    """Discriminator."""
-
-    def __init__(self, name: Optional[str] = None, **params):
-        """Create new discriminator.
-
-        Parameters:
-            name: Name of discriminator to be used
-        """
-        super().__init__(name, **params)
-
-
-class Kernel(MeasOpts):
-    """Kernel."""
-
-    def __init__(self, name: Optional[str] = None, **params):
-        """Create new kernel.
-
-        Parameters:
-            name: Name of kernel to be used
-        """
-        super().__init__(name, **params)
+from qiskit.pulse.configuration import Kernel, Discriminator
