dfddb295113750f92cc5d42f52e384f790297fa4
Paul Nation <nonhermitian@gmail.com>
2020-03-20 09:39:40 -0400

    disable import position

--- qiskit/__init__.py
+++ qiskit/__init__.py
@@ -12,7 +12,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-# pylint: disable=wrong-import-order
+# pylint: disable=wrong-import-order, wrong-import-position
 
 
 """Main Qiskit public functionality."""
