e1a887c99f9899b1c829bbb954792db6ef95ee57
Christopher J. Wood <cjwood@us.ibm.com>
2020-03-20 13:19:34 -0400

    Add ``PauliTable`` symplectic operator class (#3889)
    * Add PauliTable class
    
    * Efficient symplectic representation of a list of N-qubit Pauli operators
    
    * Add PauliTable unit tests
    
    * Add `argsort` method to `PauliTable`, rename `delete_duplicates` to `unique`
    
    * Improve documentation of PauliTable
    
    * add _block_stack method to PauliTable
    
    * Make PauliTable a BaseOperator
    
    * Add qargs to @ compose
    
    * Add release note
    
    * Add array setter method to Pauli table
    
    * Swap qubit ordering in PauliTable columns
    
    After this change qubit ording matches the array index structure, so qubit-0 is column 0 of the X and Z array blocks.
    
    * add more structure to test file
    
    * Improve repr for PauliTable
    
    * Add qargs addition to PauliTable
    
    * Rename `n_qubits` to `num_qubits`
    
    * Allow PauliTable init from Pauli object
    
    * Remove duplicate test
    
    * Update to use BaseOperator num_qubits
    
    * Fix API doc links in release note

--- test/python/quantum_info/operators/symplectic/__init__.py
+++ test/python/quantum_info/operators/symplectic/__init__.py
@@ -2,7 +2,7 @@
 
 # This code is part of Qiskit.
 #
-# (C) Copyright IBM 2017, 2019.
+# (C) Copyright IBM 2017, 2020.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -12,14 +12,4 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Quantum Operators."""
-
-from .operator import Operator
-from .scalar_op import ScalarOp
-from .pauli import Pauli, pauli_group
-from .channel import Choi, SuperOp, Kraus, Stinespring, Chi, PTM
-from .quaternion import Quaternion
-from .measures import process_fidelity, average_gate_fidelity, gate_error
-
-from .symplectic import PauliTable
-from .symplectic import pauli_basis
+"""Qiskit quantum information symplectic operator tests."""
