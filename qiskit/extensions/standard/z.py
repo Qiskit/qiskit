0469f3c4dbe457f4da7ae692923509da42add275
Ali Javadi-Abhari <ajavadia@users.noreply.github.com>
2020-03-26 13:49:55 -0400

    Update qiskit/extensions/standard/z.py
    Co-Authored-By: Julien Gacon <gaconju@gmail.com>

--- qiskit/extensions/standard/z.py
+++ qiskit/extensions/standard/z.py
@@ -149,7 +149,7 @@ class CZGate(ControlledGate, metaclass=CZMeta):
     .. math::
 
         CZ\ q_1, q_0 =
-            |0\rangle\langle0| \otimes I + |1\rangle\langle1| \otimes Z =
+            |0\rangle\langle 0| \otimes I + |1\rangle\langle 1| \otimes Z =
             \begin{pmatrix}
                 1 & 0 & 0 & 0 \\
                 0 & 1 & 0 & 0 \\
