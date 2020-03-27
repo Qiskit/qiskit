7b92deb23043cf00c4d25de9fbd60caf22575e7d
knzwnao <knzwnao@jp.ibm.com>
2020-03-27 12:58:49 +0900

    update example

--- qiskit/visualization/state_visualization.py
+++ qiskit/visualization/state_visualization.py
@@ -527,7 +527,6 @@ def plot_state_city(
            qc = QuantumCircuit(2, 2)
            qc.h(0)
            qc.cx(0, 1)
-           qc.measure([0, 1], [0, 1])
 
            backend = BasicAer.get_backend('statevector_simulator')
            job = execute(qc, backend).result()
