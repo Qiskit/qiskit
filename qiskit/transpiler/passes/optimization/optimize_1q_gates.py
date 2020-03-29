1381f4d892a462a7c7c9c4807c6abadf0d333a2b
Adenilton Silva <ajsilva@cin.ufpe.br>
2020-03-29 08:21:12 -0300

    refactor optimize_1q_gates

--- qiskit/transpiler/passes/optimization/optimize_1q_gates.py
+++ qiskit/transpiler/passes/optimization/optimize_1q_gates.py
@@ -28,9 +28,162 @@ from qiskit.quantum_info.operators import Quaternion
 
 _CHOP_THRESHOLD = 1e-15
 
+class node:
+    def __init__(self, name, parameters):
+        '''
+        # right.name = "gate name in (u1, u2, u3)"
+        # right.parameters = (theta, phi, lambda)
+        '''
+        self.name = name
+        self.parameters = parameters
 
 class Optimize1qGates(TransformationPass):
     """Optimize chains of single-qubit u1, u2, u3 gates by combining them into a single gate."""
+
+    def define_left_node(self, current_node):
+        left_name = current_node.name
+        if (current_node.condition is not None
+                or len(current_node.qargs) != 1
+                or left_name not in ["u1", "u2", "u3", "id"]):
+            raise TranspilerError("internal error")
+        if left_name == "u1":
+            left_parameters = (0, 0, current_node.op.params[0])
+        elif left_name == "u2":
+            left_parameters = (np.pi / 2, current_node.op.params[0],
+                               current_node.op.params[1])
+        elif left_name == "u3":
+            left_parameters = tuple(current_node.op.params)
+        else:
+            left_name = "u1"  # replace id with u1
+            left_parameters = (0, 0, 0)
+        # If there are any sympy objects coming from the gate convert
+        # to numpy.
+        left_parameters = tuple([float(x) for x in left_parameters])
+        return node(left_name, left_parameters)
+
+    def combine_u1u1_into_right_node(self, left_node, right_node):
+        '''
+        u1(lambda1) * u1(lambda2) = u1(lambda1 + lambda2)
+        '''
+        right_node.parameters = (0, 0, right_node.parameters[2] +
+                                 left_node.parameters[2])
+
+    def combine_u1u2_into_right_node(self, left_node, right_node):
+        '''
+        u1(lambda1) * u2(phi2, lambda2) = u2(phi2 + lambda1, lambda2)
+        '''
+        right_node.parameters = (np.pi / 2, right_node.parameters[1] +
+                                 left_node.parameters[2], right_node.parameters[2])
+
+    def combine_u1u3_into_right_node(self, left_node, right_node):
+        '''
+        u1(lambda1) * u3(theta2, phi2, lambda2) =
+             u3(theta2, phi2 + lambda1, lambda2)
+        '''
+        right_node.parameters = (right_node.parameters[0], right_node.parameters[1] +
+                                 left_node.parameters[2], right_node.parameters[2])
+
+    def combine_u2u1_into_right_node(self, left_node, right_node):
+        '''
+        u2(phi1, lambda1) * u1(lambda2) = u2(phi1, lambda1 + lambda2)
+        '''
+        right_node.name = "u2"
+        right_node.parameters = (np.pi / 2, left_node.parameters[1],
+                                 right_node.parameters[2] + left_node.parameters[2])
+
+    def combine_u2u2_into_right_node(self, left_node, right_node):
+        '''
+        Using Ry(pi/2).Rz(2*lambda).Ry(pi/2) =
+           Rz(pi/2).Ry(pi-2*lambda).Rz(pi/2),
+        u2(phi1, lambda1) * u2(phi2, lambda2) =
+            u3(pi - lambda1 - phi2, phi1 + pi/2, lambda2 + pi/2)
+        '''
+        right_node.name = "u3"
+        right_node.parameters = (np.pi - left_node.parameters[2] -
+                                 right_node.parameters[1], left_node.parameters[1] +
+                                 np.pi / 2, right_node.parameters[2] +
+                                 np.pi / 2)
+
+    def combine_u3u1_into_right_node(self, left_node, right_node):
+        '''
+        u3(theta1, phi1, lambda1) * u1(lambda2) =
+            u3(theta1, phi1, lambda1 + lambda2)
+        '''
+        right_node.name = "u3"
+        right_node.parameters = (left_node.parameters[0], left_node.parameters[1],
+                                 right_node.parameters[2] + left_node.parameters[2])
+
+    def combine_u2oru3u3_into_right_node(self, left_node, right_node):
+        '''
+        For composing u3's or u2's with u3's, use
+        u2(phi, lambda) = u3(pi/2, phi, lambda)
+        together with the qiskit.mapper.compose_u3 method.
+        '''
+        right_node.name = "u3"
+        # Evaluate the symbolic expressions for efficiency
+        right_node.parameters = Optimize1qGates.compose_u3(left_node.parameters[0],
+                                                           left_node.parameters[1],
+                                                           left_node.parameters[2],
+                                                           right_node.parameters[0],
+                                                           right_node.parameters[1],
+                                                           right_node.parameters[2])
+
+    def combine_gates_into_right_node(self, left_node, right_node):
+        '''
+        Combine left_node and right_node into right_node
+        '''
+        name_tuple = (left_node.name, right_node.name)
+        if name_tuple == ("u1", "u1"):
+            self.combine_u1u1_into_right_node(left_node, right_node)
+
+        elif name_tuple == ("u1", "u2"):
+            self.combine_u1u2_into_right_node(left_node, right_node)
+
+        elif name_tuple == ("u2", "u1"):
+            self.combine_u2u1_into_right_node(left_node, right_node)
+
+        elif name_tuple == ("u1", "u3"):
+            self.combine_u1u3_into_right_node(left_node, right_node)
+
+        elif name_tuple == ("u3", "u1"):
+            self.combine_u3u1_into_right_node(left_node, right_node)
+
+        elif name_tuple == ("u2", "u2"):
+            self.combine_u2u2_into_right_node(left_node, right_node)
+
+        elif name_tuple[1] == "nop":
+            right_node.name = left_node.name
+            right_node.parameters = left_node.parameters
+        else:
+            self.combine_u2oru3u3_into_right_node(left_node, right_node)
+
+    def generate_gate(self, right_node):
+        new_op = Gate(name="", num_qubits=1, params=[])
+        if right_node.name == "u1":
+            new_op = U1Gate(right_node.parameters[2])
+        if right_node.name == "u2":
+            new_op = U2Gate(right_node.parameters[1], right_node.parameters[2])
+        if right_node.name == "u3":
+            new_op = U3Gate(*right_node.parameters)
+        return new_op
+
+    def add_right_node_to_dag(self, dag, new_op, right_node, run):
+        if right_node.name != 'nop':
+            dag.substitute_node(run[0], new_op, inplace=True)
+
+    def extract_simplified_gates(self, dag, right_node, run):
+        '''
+        Delete the other nodes in the run
+        '''
+        for current_node in run[1:]:
+            dag.remove_op_node(current_node)
+        if right_node.name == "nop":
+            dag.remove_op_node(run[0])
+
+    def can_cambine(self, left_node, right_node):
+        # TODO
+        return True
+        
 
     def run(self, dag):
         """Run the Optimize1qGates pass on `dag`.
@@ -47,147 +200,62 @@ class Optimize1qGates(TransformationPass):
         runs = dag.collect_runs(["u1", "u2", "u3"])
         runs = _split_runs_on_parameters(runs)
         for run in runs:
-            right_name = "u1"
-            right_parameters = (0, 0, 0)  # (theta, phi, lambda)
+            right_node = node("u1", (0, 0, 0))
+
 
             for current_node in run:
-                left_name = current_node.name
-                if (current_node.condition is not None
-                        or len(current_node.qargs) != 1
-                        or left_name not in ["u1", "u2", "u3", "id"]):
-                    raise TranspilerError("internal error")
-                if left_name == "u1":
-                    left_parameters = (0, 0, current_node.op.params[0])
-                elif left_name == "u2":
-                    left_parameters = (np.pi / 2, current_node.op.params[0],
-                                       current_node.op.params[1])
-                elif left_name == "u3":
-                    left_parameters = tuple(current_node.op.params)
-                else:
-                    left_name = "u1"  # replace id with u1
-                    left_parameters = (0, 0, 0)
-                # If there are any sympy objects coming from the gate convert
-                # to numpy.
-                left_parameters = tuple([float(x) for x in left_parameters])
-                # Compose gates
-                name_tuple = (left_name, right_name)
-                if name_tuple == ("u1", "u1"):
-                    # u1(lambda1) * u1(lambda2) = u1(lambda1 + lambda2)
-                    right_parameters = (0, 0, right_parameters[2] +
-                                        left_parameters[2])
-                elif name_tuple == ("u1", "u2"):
-                    # u1(lambda1) * u2(phi2, lambda2) = u2(phi2 + lambda1, lambda2)
-                    right_parameters = (np.pi / 2, right_parameters[1] +
-                                        left_parameters[2], right_parameters[2])
-                elif name_tuple == ("u2", "u1"):
-                    # u2(phi1, lambda1) * u1(lambda2) = u2(phi1, lambda1 + lambda2)
-                    right_name = "u2"
-                    right_parameters = (np.pi / 2, left_parameters[1],
-                                        right_parameters[2] + left_parameters[2])
-                elif name_tuple == ("u1", "u3"):
-                    # u1(lambda1) * u3(theta2, phi2, lambda2) =
-                    #     u3(theta2, phi2 + lambda1, lambda2)
-                    right_parameters = (right_parameters[0], right_parameters[1] +
-                                        left_parameters[2], right_parameters[2])
-                elif name_tuple == ("u3", "u1"):
-                    # u3(theta1, phi1, lambda1) * u1(lambda2) =
-                    #     u3(theta1, phi1, lambda1 + lambda2)
-                    right_name = "u3"
-                    right_parameters = (left_parameters[0], left_parameters[1],
-                                        right_parameters[2] + left_parameters[2])
-                elif name_tuple == ("u2", "u2"):
-                    # Using Ry(pi/2).Rz(2*lambda).Ry(pi/2) =
-                    #    Rz(pi/2).Ry(pi-2*lambda).Rz(pi/2),
-                    # u2(phi1, lambda1) * u2(phi2, lambda2) =
-                    #    u3(pi - lambda1 - phi2, phi1 + pi/2, lambda2 + pi/2)
-                    right_name = "u3"
-                    right_parameters = (np.pi - left_parameters[2] -
-                                        right_parameters[1], left_parameters[1] +
-                                        np.pi / 2, right_parameters[2] +
-                                        np.pi / 2)
-                elif name_tuple[1] == "nop":
-                    right_name = left_name
-                    right_parameters = left_parameters
-                else:
-                    # For composing u3's or u2's with u3's, use
-                    # u2(phi, lambda) = u3(pi/2, phi, lambda)
-                    # together with the qiskit.mapper.compose_u3 method.
-                    right_name = "u3"
-                    # Evaluate the symbolic expressions for efficiency
-                    right_parameters = Optimize1qGates.compose_u3(left_parameters[0],
-                                                                  left_parameters[1],
-                                                                  left_parameters[2],
-                                                                  right_parameters[0],
-                                                                  right_parameters[1],
-                                                                  right_parameters[2])
-                    # Why evalf()? This program:
-                    #   OPENQASM 2.0;
-                    #   include "qelib1.inc";
-                    #   qreg q[2];
-                    #   creg c[2];
-                    #   u3(0.518016983430947*pi,1.37051598592907*pi,1.36816383603222*pi) q[0];
-                    #   u3(1.69867232277986*pi,0.371448347747471*pi,0.461117217930936*pi) q[0];
-                    #   u3(0.294319836336836*pi,0.450325871124225*pi,1.46804720442555*pi) q[0];
-                    #   measure q -> c;
-                    # took >630 seconds (did not complete) to optimize without
-                    # calling evalf() at all, 19 seconds to optimize calling
-                    # evalf() AFTER compose_u3, and 1 second to optimize
-                    # calling evalf() BEFORE compose_u3.
-                # 1. Here down, when we simplify, we add f(theta) to lambda to
-                # correct the global phase when f(theta) is 2*pi. This isn't
-                # necessary but the other steps preserve the global phase, so
-                # we continue in that manner.
-                # 2. The final step will remove Z rotations by 2*pi.
-                # 3. Note that is_zero is true only if the expression is exactly
-                # zero. If the input expressions have already been evaluated
-                # then these final simplifications will not occur.
-                # TODO After we refactor, we should have separate passes for
-                # exact and approximate rewriting.
+                left_node = self.define_left_node(current_node)
+                a = self.can_cambine(left_node, right_node)
+                self.combine_gates_into_right_node(left_node, right_node)
+                self.simplify_right_node(right_node)
 
-                # Y rotation is 0 mod 2*pi, so the gate is a u1
-                if np.mod(right_parameters[0], (2 * np.pi)) == 0 \
-                        and right_name != "u1":
-                    right_name = "u1"
-                    right_parameters = (0, 0, right_parameters[1] +
-                                        right_parameters[2] +
-                                        right_parameters[0])
-                # Y rotation is pi/2 or -pi/2 mod 2*pi, so the gate is a u2
-                if right_name == "u3":
-                    # theta = pi/2 + 2*k*pi
-                    if np.mod((right_parameters[0] - np.pi / 2), (2 * np.pi)) == 0:
-                        right_name = "u2"
-                        right_parameters = (np.pi / 2, right_parameters[1],
-                                            right_parameters[2] +
-                                            (right_parameters[0] - np.pi / 2))
-                    # theta = -pi/2 + 2*k*pi
-                    if np.mod((right_parameters[0] + np.pi / 2), (2 * np.pi)) == 0:
-                        right_name = "u2"
-                        right_parameters = (np.pi / 2, right_parameters[1] +
-                                            np.pi, right_parameters[2] -
-                                            np.pi + (right_parameters[0] +
-                                                     np.pi / 2))
-                # u1 and lambda is 0 mod 2*pi so gate is nop (up to a global phase)
-                if right_name == "u1" and np.mod(right_parameters[2], (2 * np.pi)) == 0:
-                    right_name = "nop"
+            new_op = self.generate_gate(right_node)
 
-            new_op = Gate(name="", num_qubits=1, params=[])
-            if right_name == "u1":
-                new_op = U1Gate(right_parameters[2])
-            if right_name == "u2":
-                new_op = U2Gate(right_parameters[1], right_parameters[2])
-            if right_name == "u3":
-                new_op = U3Gate(*right_parameters)
+            self.add_right_node_to_dag(dag, new_op, right_node, run)
 
-            if right_name != 'nop':
-                dag.substitute_node(run[0], new_op, inplace=True)
-
-            # Delete the other nodes in the run
-            for current_node in run[1:]:
-                dag.remove_op_node(current_node)
-            if right_name == "nop":
-                dag.remove_op_node(run[0])
+            self.extract_simplified_gates(dag, right_node, run)
 
         return dag
+
+
+    def simplify_right_node(self, right_node):
+        '''
+        1. Here down, when we simplify, we add f(theta) to lambda to
+        correct the global phase when f(theta) is 2*pi. This isn't
+        necessary but the other steps preserve the global phase, so
+        we continue in that manner.
+        2. The final step will remove Z rotations by 2*pi.
+        3. Note that is_zero is true only if the expression is exactly
+        zero. If the input expressions have already been evaluated
+        then these final simplifications will not occur.
+        TODO After we refactor, we should have separate passes for
+        exact and approximate rewriting.
+        Y rotation is 0 mod 2*pi, so the gate is a u1
+        '''
+        if np.mod(right_node.parameters[0], (2 * np.pi)) == 0 \
+                and right_node.name != "u1":
+            right_node.name = "u1"
+            right_node.parameters = (0, 0, right_node.parameters[1] +
+                                     right_node.parameters[2] +
+                                     right_node.parameters[0])
+        # Y rotation is pi/2 or -pi/2 mod 2*pi, so the gate is a u2
+        if right_node.name == "u3":
+            # theta = pi/2 + 2*k*pi
+            if np.mod((right_node.parameters[0] - np.pi / 2), (2 * np.pi)) == 0:
+                right_node.name = "u2"
+                right_node.parameters = (np.pi / 2, right_node.parameters[1],
+                                         right_node.parameters[2] +
+                                         (right_node.parameters[0] - np.pi / 2))
+            # theta = -pi/2 + 2*k*pi
+            if np.mod((right_node.parameters[0] + np.pi / 2), (2 * np.pi)) == 0:
+                right_node.name = "u2"
+                right_node.parameters = (np.pi / 2, right_node.parameters[1] +
+                                         np.pi, right_node.parameters[2] -
+                                         np.pi + (right_node.parameters[0] +
+                                                  np.pi / 2))
+        # u1 and lambda is 0 mod 2*pi so gate is nop (up to a global phase)
+        if right_node.name == "u1" and np.mod(right_node.parameters[2], (2 * np.pi)) == 0:
+            right_node.name = "nop"
 
     @staticmethod
     def compose_u3(theta1, phi1, lambda1, theta2, phi2, lambda2):
