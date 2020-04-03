2c408d1ead036d3ea52d55021285f411096ec43c
Donny Greenberg <dongreenberg2@gmail.com>
2020-04-02 22:41:28 -0400

    Introducing HamiltonianGate, and tests. Tests pass. Allow complex ParameterExpression.

---
+++ qiskit/extensions/hamiltonian_gate.py
@@ -0,0 +1,150 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2017, 2019.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+Gate described by the time evolution of a Hermitian Hamiltonian operator.
+"""
+
+from numbers import Number
+import numpy
+import scipy.linalg
+
+from qiskit.circuit import Gate, ParameterExpression
+from qiskit.circuit import QuantumCircuit
+from qiskit.circuit import QuantumRegister
+from qiskit.quantum_info.operators.predicates import matrix_equal
+from qiskit.quantum_info.operators.predicates import is_hermitian_matrix
+from qiskit.extensions.exceptions import ExtensionError
+
+from .unitary import UnitaryGate
+
+
+class HamiltonianGate(Gate):
+    """Class for representing evolution by a Hermitian Hamiltonian operator as a gate. This gate
+    resolves to a UnitaryGate, which can be decomposed into basis gates if it is 2 qubits or
+    less, or simulated directly in Aer for more qubits. """
+
+    def __init__(self, data, time, label=None):
+        """Create a gate from a hamiltonian operator and evolution time parameter t
+
+        Args:
+            data (matrix or Operator): unitary operator.
+            time (float or complex): time evolution parameter.
+            label (str): unitary name for backend [Default: None].
+
+        Raises:
+            ExtensionError: if input data is not an N-qubit unitary operator.
+        """
+        if hasattr(data, 'to_matrix'):
+            # If input is Gate subclass or some other class object that has
+            # a to_matrix method this will call that method.
+            data = data.to_matrix()
+        elif hasattr(data, 'to_operator'):
+            # If input is a BaseOperator subclass this attempts to convert
+            # the object to an Operator so that we can extract the underlying
+            # numpy matrix from `Operator.data`.
+            data = data.to_operator().data
+        # Convert to numpy array in case not already an array
+        data = numpy.array(data, dtype=complex)
+        # Check input is unitary
+        if not is_hermitian_matrix(data):
+            raise ExtensionError("Input matrix is not Hermitian.")
+        # Check input is N-qubit matrix
+        input_dim, output_dim = data.shape
+        num_qubits = int(numpy.log2(input_dim))
+        if input_dim != output_dim or 2**num_qubits != input_dim:
+            raise ExtensionError(
+                "Input matrix is not an N-qubit operator.")
+
+        self._qasm_name = None
+        self._qasm_definition = None
+        self._qasm_def_written = False
+        # Store instruction params
+        super().__init__('hamiltonian', num_qubits, [data, time], label=label)
+
+    def __eq__(self, other):
+        if not isinstance(other, HamiltonianGate):
+            return False
+        if self.label != other.label:
+            return False
+        # Should we match unitaries as equal if they are equal
+        # up to global phase?
+        operators_eq = matrix_equal(self.params[0], other.params[0], ignore_phase=True)
+        times_eq = self.params[1] == other.params[1]
+        return operators_eq and times_eq
+
+    def to_matrix(self):
+        """Return matrix for the unitary."""
+        unbound_t = False
+        if isinstance(self.params[1], ParameterExpression):
+            unbound_t = len(self.params[1].parameters) > 0
+        if isinstance(self.params[1], Number) or not unbound_t:
+            #pylint: disable=no-member
+            return scipy.linalg.expm(1j * self.params[0] * self.params[1])
+        else:
+            raise NotImplementedError("Unable to generate Unitary matrix for "
+                                      "unbound t parameter {}".format(self.params[1]))
+
+    def inverse(self):
+        """Return the adjoint of the unitary."""
+        return self.adjoint()
+
+    def conjugate(self):
+        """Return the conjugate of the Hamiltonian."""
+        return HamiltonianGate(numpy.transpose(self.params[0]), -self.params[1])
+
+    def adjoint(self):
+        """Return the adjoint of the unitary."""
+        return HamiltonianGate(self.params[0], -self.params[1])
+
+    def transpose(self):
+        """Return the transpose of the Hamiltonian."""
+        return HamiltonianGate(numpy.transpose(self.params[0]), self.params[1])
+
+    def _define(self):
+        """Calculate a subcircuit that implements this unitary."""
+        return UnitaryGate(self.to_matrix())
+
+    def control(self, num_ctrl_qubits=1, label=None, ctrl_state=None):
+        r"""Return controlled version of gate, relying on control implementation in Unitary.
+
+        Args:
+            num_ctrl_qubits (int): number of controls to add to gate (default=1)
+            label (str): optional gate label
+            ctrl_state (int or str or None): The control state in decimal or as a
+                bit string (e.g. '1011'). If None, use 2**num_ctrl_qubits-1.
+
+        Returns:
+            UnitaryGate: controlled version of gate.
+
+        Raises:
+            QiskitError: invalid ctrl_state
+        """
+        return self._define().control(num_ctrl_qubits=num_ctrl_qubits,
+                                      label=label,
+                                      ctrl_state=ctrl_state)
+
+    def qasm(self):
+        """ The qasm for the UnitaryGate defining this HamiltonianGate
+        """
+        return self._define().qasm()
+
+def hamiltonian(self, matrix, time, qubits, label=None):
+    """Apply u2 to q."""
+    if isinstance(qubits, QuantumRegister):
+        qubits = qubits[:]
+    return self.append(HamiltonianGate(data=matrix, time=time, label=label), qubits, [])
+
+
+QuantumCircuit.hamiltonian = hamiltonian
---
+++ test/python/circuit/test_hamiltonian_gate.py
@@ -0,0 +1,188 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2017, 2019.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+# pylint: disable=arguments-differ,method-hidden
+
+"""Quick program to test the qi tools modules."""
+
+import json
+import numpy as np
+from numpy.testing import assert_allclose
+
+import qiskit
+from qiskit.extensions.hamiltonian_gate import HamiltonianGate
+from qiskit.test import QiskitTestCase
+from qiskit import BasicAer
+from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from qiskit.circuit import Parameter
+from qiskit.quantum_info import Operator
+from qiskit.transpiler import PassManager
+from qiskit.compiler import transpile
+from qiskit.converters import circuit_to_dag, dag_to_circuit
+from qiskit.transpiler.passes import CXCancellation
+
+
+class TestHamiltonianGate(QiskitTestCase):
+    """Tests for the HamiltonianGate class."""
+
+    def setUp(self):
+        """Setup."""
+        pass
+
+    def test_set_matrix(self):
+        """Test instantiation"""
+        try:
+            HamiltonianGate([[0, 1], [1, 0]], 1)
+        # pylint: disable=broad-except
+        except Exception as err:
+            self.fail('unexpected exception in init of HamiltonianGate: {0}'.format(err))
+
+    def test_set_matrix_raises(self):
+        """test non-unitary"""
+        try:
+            HamiltonianGate([[1, 0], [1, 1]], 1)
+        # pylint: disable=broad-except
+        except Exception:
+            pass
+        else:
+            self.fail('setting Unitary with non-hermitian matrix did not raise')
+
+    def test_conjugate(self):
+        """test conjugate"""
+        ymat = np.conj(np.array([[0, -1j], [1j, 0]]))
+        ham = HamiltonianGate([[1, 1j], [-1j, 1]], np.pi / 2)
+        np.testing.assert_array_almost_equal(ham.conjugate().to_matrix(),
+                                             ymat)
+
+    def test_adjoint(self):
+        """test adjoint operation"""
+        uni = HamiltonianGate([[0, 1j], [-1j, 0]], np.pi)
+        np.testing.assert_array_almost_equal(uni.adjoint().to_matrix(),
+                                             uni.to_matrix())
+
+
+class TestHamiltonianCircuit(QiskitTestCase):
+    """Hamiltonian gate circuit tests."""
+
+    def test_1q_hamiltonian(self):
+        """test 1 qubit hamiltonian"""
+        qr = QuantumRegister(1)
+        cr = ClassicalRegister(1)
+        qc = QuantumCircuit(qr, cr)
+        matrix = np.zeros((2, 2))
+        qc.x(qr[0])
+        theta = Parameter('theta')
+        qc.append(HamiltonianGate(matrix, theta), [qr[0]])
+        qc = qc.bind_parameters({theta: 1})
+        # test of qasm output
+        self.log.info(qc.qasm())
+        # test of text drawer
+        self.log.info(qc)
+        dag = circuit_to_dag(qc)
+        dag_nodes = dag.named_nodes('hamiltonian')
+        self.assertTrue(len(dag_nodes) == 1)
+        dnode = dag_nodes[0]
+        self.assertIsInstance(dnode.op, HamiltonianGate)
+        for qubit in dnode.qargs:
+            self.assertIn(qubit.index, [0, 1])
+        assert_allclose(dnode.op.to_matrix(), np.eye(2))
+
+    def test_2q_hamiltonian(self):
+        """test 2 qubit hamiltonian """
+        backend = BasicAer.get_backend('qasm_simulator')
+        qr = QuantumRegister(2)
+        cr = ClassicalRegister(2)
+        qc = QuantumCircuit(qr, cr)
+        matrix = Operator.from_label('XY')
+        qc.x(qr[0])
+        theta = Parameter('theta')
+        uni2q = HamiltonianGate(matrix, theta)
+        qc.append(uni2q, [qr[0], qr[1]])
+        passman = PassManager()
+        passman.append(CXCancellation())
+        qc2 = transpile(qc, backend, pass_manager=passman)
+
+        # Testing bind after transpile
+        qc2 = qc2.bind_parameters({theta: np.pi / 2})
+        # test of qasm output
+        self.log.info(qc2.qasm())
+        # test of text drawer
+        self.log.info(qc2)
+        dag = circuit_to_dag(qc2)
+        nodes = dag.two_qubit_ops()
+        self.assertEqual(len(nodes), 1)
+        dnode = nodes[0]
+        self.assertIsInstance(dnode.op, HamiltonianGate)
+        for qubit in dnode.qargs:
+            self.assertIn(qubit.index, [0, 1])
+        # Equality based on Pauli exponential identity
+        np.testing.assert_array_almost_equal(dnode.op.to_matrix(), 1j*matrix.data)
+        qc3 = dag_to_circuit(dag)
+        self.assertEqual(qc2, qc3)
+
+    def test_3q_hamiltonian(self):
+        """test 3 qubit hamiltonian on non-consecutive bits"""
+        qr = QuantumRegister(4)
+        qc = QuantumCircuit(qr)
+        qc.x(qr[0])
+        matrix = Operator.from_label('XZY')
+        theta = Parameter('theta')
+        uni3q = HamiltonianGate(matrix, theta)
+        qc.append(uni3q, [qr[0], qr[1], qr[3]])
+        qc.cx(qr[3], qr[2])
+        # test of text drawer
+        self.log.info(qc)
+        qc = qc.bind_parameters({theta: np.pi / 2})
+        dag = circuit_to_dag(qc)
+        nodes = dag.multi_qubit_ops()
+        self.assertEqual(len(nodes), 1)
+        dnode = nodes[0]
+        self.assertIsInstance(dnode.op, HamiltonianGate)
+        for qubit in dnode.qargs:
+            self.assertIn(qubit.index, [0, 1, 3])
+        np.testing.assert_almost_equal(dnode.op.to_matrix(), 1j*matrix.data)
+
+    def test_qobj_with_hamiltonian(self):
+        """test qobj output with hamiltonian"""
+        qr = QuantumRegister(4)
+        qc = QuantumCircuit(qr)
+        qc.rx(np.pi / 4, qr[0])
+        matrix = Operator.from_label('XIZ')
+        theta = Parameter('theta')
+        uni = HamiltonianGate(matrix, theta, label='XIZ')
+        qc.append(uni, [qr[0], qr[1], qr[3]])
+        qc.cx(qr[3], qr[2])
+        qc = qc.bind_parameters({theta: np.pi / 2})
+        qobj = qiskit.compiler.assemble(qc)
+        instr = qobj.experiments[0].instructions[1]
+        self.assertEqual(instr.name, 'hamiltonian')
+        # Also test label
+        self.assertEqual(instr.label, 'XIZ')
+        np.testing.assert_array_almost_equal(np.array(instr.params[0]).astype(np.complex64),
+                                             matrix.data)
+        # check conversion to dict
+        qobj_dict = qobj.to_dict(validate=True)
+
+        class NumpyEncoder(json.JSONEncoder):
+            """Class for encoding json str with complex and np arrays."""
+            def default(self, obj):
+                if isinstance(obj, np.ndarray):
+                    return obj.tolist()
+                if isinstance(obj, complex):
+                    return (obj.real, obj.imag)
+                return json.JSONEncoder.default(self, obj)
+
+        # check json serialization
+        self.assertTrue(isinstance(json.dumps(qobj_dict, cls=NumpyEncoder),
+                                   str))
