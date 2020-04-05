b49fbe16f1bf5bf653f463245073282d7586882a
Edwin Navarro <enavarro@comcast.net>
2020-04-05 05:26:57 -0700

    Fixed issue #4064 corrected indent spacing

--- qiskit/converters/circuit_to_gate.py
+++ qiskit/converters/circuit_to_gate.py
@@ -49,8 +49,8 @@ def circuit_to_gate(circuit, parameter_map=None):
     for inst, _, _ in circuit.data:
         if not isinstance(inst, Gate):
             raise QiskitError(('One or more instructions cannot be converted to'
-                              ' a gate. "{}" is not a gate instruction').format(
-                              inst.name))
+                               ' a gate. "{}" is not a gate instruction').format(
+                                   inst.name))
 
     if parameter_map is None:
         parameter_dict = {p: p for p in circuit.parameters}
