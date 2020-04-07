886be62ea0467d611ea6a486e57eeb90d6ec82ba
Christopher J. Wood <cjwood@us.ibm.com>
2020-04-07 16:17:56 -0400

    Add random Clifford and pauli table functions (#4062)
    * Add random_clifford function
    
    * Add random_pauli_table, random_stabilizer_table functions
    
    * optimize random clifford
    
    * Add tests for random symplectic ops
    
    * Add release note
    
    * lint
    
    * remove accidently added file
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- qiskit/quantum_info/random.py
+++ qiskit/quantum_info/random.py
@@ -17,7 +17,12 @@
 # pylint: disable=unused-import
 from qiskit.quantum_info.operators.random import (random_unitary,
                                                   random_quantum_channel,
-                                                  random_hermitian)
+                                                  random_hermitian,
+                                                  random_clifford,
+                                                  random_pauli_table,
+                                                  random_stabilizer_table)
+
 from qiskit.quantum_info.states.random import (random_statevector,
                                                random_density_matrix)
+
 from qiskit.quantum_info.states.random import random_state  # DEPRECATED in 0.13
