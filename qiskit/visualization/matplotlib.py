a1fda408da4f07e4e7c7902058c55baff6061916
Frederik Flöther <ffbcmy@gmail.com>
2020-04-07 14:03:31 +0200

    trailing whitespace

--- qiskit/visualization/matplotlib.py
+++ qiskit/visualization/matplotlib.py
@@ -588,7 +588,7 @@ class MatplotlibDrawer:
                 self._cond['n_lines'] += 1
                 idx += 1
 
-        # 7 is the length of the smallest possible label        
+        # 7 is the length of the smallest possible label
         self.x_offset = -.5 + 0.3 * (len_longest_label - 7)
 
     def _draw_regs_sub(self, n_fold, feedline_l=False, feedline_r=False):
