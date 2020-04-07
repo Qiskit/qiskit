5571747d2a109f3c9f5c3dcfa0cacd8f97d77802
Julien Gacon <jules.gacon@googlemail.com>
2020-04-07 05:02:53 +0200

    Kickoff the arithmetics library (#4061)
    * add functional rotations base class
    
    * refactor first arithmetic circuits
    
    * fixed value comparator (now integer comparator)
    * linear rotation (now linear Pauli rotations)
    * piecewise linear rotation (now piecewise linear pauli rotations)
    
    Co-authored-by: Stefan Woerner <wor@zurich.ibm.com>
    
    * integrate polynomial rotations
    
    Co-authored-by: Almudena Carrera Vazquez <acv@zurich.ibm.com>
    
    * update inits
    
    * add tests
    
    * skip test depending on Aqua
    
    * fix imports: no weighted_adder, add functional_rot
    
    * move circ diag in parsed-literal block
    
    * adjust formatting of linear circ diag
    
    * update rendering of docstrings
    
    * move AND/OR gates from Aqua
    
    Co-authored-by: Shaohan Hu <shaohan.hu@ibm.com>
    Co-authored-by: Manoel Marques <manoel@us.ibm.com>
    
    * use data getter not _data in copy
    
    * add missing gate inits
    
    * fix imports
    
    * fix lint
    
    * move WSO from aqua
    
    Co-authored-by: Stefan Woerner <wor@zurich.ibm.com>
    Co-authored-by: Manoel Marques <manoel@us.ibm.com>
    Co-authored-by: Shaohan Hu <shaohan.hu@ibm.com>
    
    * rename to weighted adder
    
    * refactor weighted adder and make a circuit
    
    * updates inits with weighted adder
    
    * add tests for weighted adder
    
    * fix outdated mcu1 import
    
    * add functional pauli rotations import
    
    * Update test/python/circuit/test_library.py
    
    Co-Authored-By: Ali Javadi-Abhari <ajavadia@users.noreply.github.com>
    
    * Update qiskit/circuit/library/arithmetic/weighted_adder.py
    
    Co-Authored-By: Ali Javadi-Abhari <ajavadia@users.noreply.github.com>
    
    * add comment on default behaviour of the weights
    
    * fix rst rendering
    
    * include arithmetic circs in sphinx
    
    * fix copyright
    
    * add _invalidate() method
    
    Co-authored-by: Stefan Woerner <wor@zurich.ibm.com>
    Co-authored-by: Almudena Carrera Vazquez <acv@zurich.ibm.com>
    Co-authored-by: Shaohan Hu <shaohan.hu@ibm.com>
    Co-authored-by: Manoel Marques <manoel@us.ibm.com>
    Co-authored-by: Ali Javadi-Abhari <ajavadia@users.noreply.github.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- qiskit/circuit/quantumcircuit.py
+++ qiskit/circuit/quantumcircuit.py
@@ -1144,7 +1144,7 @@ class QuantumCircuit:
         cpy = copy.copy(self)
 
         instr_instances = {id(instr): instr
-                           for instr, _, __ in self._data}
+                           for instr, _, __ in self.data}
 
         instr_copies = {id_: instr.copy()
                         for id_, instr in instr_instances.items()}
@@ -1157,7 +1157,7 @@ class QuantumCircuit:
         }
 
         cpy._data = [(instr_copies[id(inst)], qargs.copy(), cargs.copy())
-                     for inst, qargs, cargs in self._data]
+                     for inst, qargs, cargs in self.data]
 
         if name:
             cpy.name = name
