36e0472c3cdb4f530581b85993eedae335735156
Matthew Treinish <mtreinish@kortar.org>
2020-04-09 11:38:32 -0400

    Revert "Moving up some parameter checks in the class hierarchy (#3668)"
    This change has some unforseen backwards compatibility implications and
    is causing downstream projects like qiskit-aer to fail CI now. Given the
    pending release and this not being critical, this commit reverts this
    commit and we can revisit it after the 0.13.0 release.
    
    This reverts commit 5afe87ce1e5279ca716fdf5a3c478f2e27a6519f.

--- qiskit/converters/ast_to_dag.py
+++ qiskit/converters/ast_to_dag.py
@@ -23,6 +23,7 @@ from qiskit.exceptions import QiskitError
 
 from qiskit.circuit import QuantumRegister, ClassicalRegister, Gate
 from qiskit.qasm.node.real import Real
+from qiskit.circuit.instruction import Instruction
 from qiskit.circuit.measure import Measure
 from qiskit.circuit.reset import Reset
 from qiskit.extensions.standard.barrier import Barrier
@@ -401,9 +402,15 @@ class AstInterpreter:
         if name in self.standard_extension:
             op = self.standard_extension[name](*params)
         elif name in self.gates:
-            op = Gate(name=name, num_qubits=self.gates[name]['n_bits'], params=params)
-            if not self.gates[name]['opaque']:
-                # call a custom gate (otherwise, opaque)
+            if self.gates[name]['opaque']:
+                # call an opaque gate
+                op = Gate(name=name, num_qubits=self.gates[name]['n_bits'], params=params)
+            else:
+                # call a custom gate
+                op = Instruction(name=name,
+                                 num_qubits=self.gates[name]['n_bits'],
+                                 num_clbits=0,
+                                 params=params)
                 op.definition = self._gate_definition_to_qiskit_definition(self.gates[name],
                                                                            params=params)
         else:
