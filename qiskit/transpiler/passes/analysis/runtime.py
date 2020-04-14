be6108cdb2f2a92436b2a6b786feb277373960c9
Albert Frisch <albert.frisch@aqt.eu>
2020-04-14 11:37:21 +0200

    minor formatting

--- qiskit/transpiler/passes/analysis/runtime.py
+++ qiskit/transpiler/passes/analysis/runtime.py
@@ -32,7 +32,7 @@ class Runtime(AnalysisPass):
                 e.g.::
 
                 {'h': 1,
-                'cx': 4}
+                 'cx': 4}
         """
         super().__init__()
         self.requires.append(DAGLongestPath(op_times))
