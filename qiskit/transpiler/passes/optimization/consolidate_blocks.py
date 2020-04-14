1ba3d7c155376ec6d5bbb76c787853d5338744da
Luciano Bello <luciano.bello@ibm.com>
2020-04-14 12:27:16 -0400

    undo

--- qiskit/transpiler/passes/optimization/consolidate_blocks.py
+++ qiskit/transpiler/passes/optimization/consolidate_blocks.py
@@ -117,12 +117,8 @@ class ConsolidateBlocks(TransformationPass):
                         basis_count += 1
                     subcirc.append(nd.op, [q[block_index_map[i]] for i in nd.qargs])
                 unitary = UnitaryGate(Operator(subcirc))  # simulates the circuit
-                try:
-                    consolidate = self.force_consolidate or unitary.num_qubits > 2 or \
-                                  self.decomposer.num_basis_gates(unitary) != basis_count
-                except QiskitError:
-                    consolidate = False
-                if consolidate:
+                if self.force_consolidate or unitary.num_qubits > 2 or \
+                        self.decomposer.num_basis_gates(unitary) != basis_count:
                     new_dag.apply_operation_back(
                         unitary, sorted(block_qargs, key=lambda x: block_index_map[x]))
                 else:
