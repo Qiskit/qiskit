14e791b6a2804ad91933e170d71c95a70640b71e
Cryoris <jules.gacon@googlemail.com>
2020-04-17 12:59:11 +0200

    add featmaps description from terra
    Co-authored-by: Stephen Wood <woodsp@us.ibm.com>

--- qiskit/circuit/library/data_encoding/__init__.py
+++ qiskit/circuit/library/data_encoding/__init__.py
@@ -12,7 +12,33 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""The circuit library submodule for data-encoding circuits."""
+"""
+Data-encoding circuits
+======================
+
+In machine learning, pattern recognition and image processing, a **data-encoding circuit**
+starts from an initial set of measured data and builds derived values (also known as
+**features**) intended to be informative and non-redundant, facilitating the subsequent
+learning and generalization steps, and in some cases leading to better human
+interpretations.
+
+A feature map is related to **dimensionality reduction**; it involves reducing the amount of
+resources required to describe a large set of data. When performing analysis of complex data,
+one of the major problems stems from the number of variables involved. Analysis with a large
+number of variables generally requires a large amount of memory and computation power, and may
+even cause a classification algorithm to overfit to training samples and generalize poorly to new
+samples.
+
+When the input data to an algorithm is too large to be processed and is suspected to be redundant
+(for example, the same measurement is provided in both pounds and kilograms), then it can be
+transformed into a reduced set of features, named a **feature vector**.
+
+The process of determining a subset of the initial features is called **feature selection**.
+The selected features are expected to contain the relevant information from the input data,
+so that the desired task can be performed by using the reduced representation instead
+of the complete initial data.
+
+"""
 
 from .pauli_expansion import PauliExpansion
 from .first_order_expansion import FirstOrderExpansion
