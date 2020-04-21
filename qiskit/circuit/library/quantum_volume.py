08e42c83a3fee2e1708d5f27929835c4441b680e
Paul Nation <nonhermitian@gmail.com>
2020-04-20 21:00:25 -0400

    update qv lib entires

--- qiskit/circuit/library/quantum_volume.py
+++ qiskit/circuit/library/quantum_volume.py
@@ -20,16 +20,22 @@ import numpy as np
 from qiskit.quantum_info.random import random_unitary
 from qiskit.circuit import QuantumCircuit
 
+MAX_INT = np.iinfo(np.int32).max-1
+
 
 class QuantumVolume(QuantumCircuit):
-    """A quantum volume model circuit.
+    """A model quantum volume circuit [1].
 
-    The model circuits are random instances of circuits used to measure
-    the Quantum Volume metric, as introduced in [1].
+    The model circuits are random instances of circuits
+    consisting of layers of Haar random elements of SU(4)
+    applied between corresponding pairs of qubits in a random
+    bipartition.
 
-    The model circuits consist of layers of Haar random
-    elements of SU(4) applied between corresponding pairs
-    of qubits in a random bipartition.
+    If depth == num_qubits (default), the circuit name includes
+    the Quantum Volume that the circuit corresponds to else it is
+    the width x depth of the circuit, the seed used in the random
+    number generator, and the offset from this seed (here always
+    equal to zero.). This completely specifies the circuit.
 
     **References:**
 
@@ -42,7 +48,8 @@ class QuantumVolume(QuantumCircuit):
                  num_qubits: int,
                  depth: Optional[int] = None,
                  seed: Optional[int] = None) -> QuantumCircuit:
-        """Create quantum volume model circuit of size num_qubits x depth.
+        """Create a model quantum volume circuit of
+        size num_qubits x depth.
 
         Args:
             num_qubits: number of active qubits in model circuit.
@@ -61,20 +68,96 @@ class QuantumVolume(QuantumCircuit):
                 circuit = QuantumVolume(5, seed=42)
                 %circuit_library_info circuit
         """
-        super().__init__(num_qubits, name="volume")
-
         depth = depth or num_qubits  # how many layers of SU(4)
         width = int(np.floor(num_qubits/2))  # how many SU(4)s fit in each layer
-        rng = np.random.RandomState(seed)
+        if seed is None:
+            seed = np.random.randint(MAX_INT)
 
-        unitary_seeds = rng.randint(low=1, high=1000, size=[depth, width])
+        if depth == num_qubits:
+            name = 'QV{}_{}+{}'.format(2**num_qubits, seed, 0)
+        else:
+            name = "genQV_{}x{}_{}+{}".format(num_qubits, depth, seed, 0)
+        super().__init__(num_qubits, name=name)
+
+        rng = np.random.RandomState(seed)
 
         # For each layer, generate a permutation of qubits
         # Then generate and apply a Haar-random SU(4) to each pair
         perm_0 = list(range(num_qubits))
-        for d in range(depth):
+        for _ in range(depth):
             perm = rng.permutation(perm_0)
             for w in range(width):
                 physical_qubits = int(perm[2*w]), int(perm[2*w+1])
-                su4 = random_unitary(4, seed=unitary_seeds[d][w])
+                su4 = random_unitary(4, seed=rng.randint(MAX_INT))
                 self.append(su4, [physical_qubits[0], physical_qubits[1]])
+
+
+class QuantumVolumeGenerator():
+    """A generator for quantum volume circuits.
+
+    If depth == num_qubits (default), the circuit names include
+    the Quantum Volume that the circuit corresponds to else it is
+    the width x depth of the circuit, the seed used in the random
+    number generator, and the offset from this seed, i.e. the
+    number of times the random number generator has been called.
+
+    """
+
+    def __init__(self, num_qubits: int,
+                 depth: Optional[int] = None,
+                 seed: Optional[int] = None):
+        """A generator for quantum volume circuits.
+
+        Parameters:
+            num_qubits: Number of qubits in QV circuit.
+            depth: Number of layers of random pairwise unitaries.
+            seed: Optional seed at which to start generator
+
+        Example:
+        .. jupyter-execute::
+            from alexandria.quantum_volume import QuantumVolumeGenerator
+            qv_gen = QuantumVolumeGenerator(4, seed=9876)
+            qv16_circs = qv_gen(5)
+            for circ in qv16_circs:
+                print(circ.name)
+        """
+        if seed is None:
+            seed = np.random.randint(MAX_INT)
+        self.seed = seed
+        self.rnd = np.random.RandomState(self.seed) # pylint: disable=no-member
+        self.num_qubits = num_qubits
+        self.depth = depth or num_qubits
+        if self.num_qubits == self.depth:
+            self.circ_name = 'QV{}_{}'.format(2**self.num_qubits,
+                                              self.seed)
+        else:
+            self.circ_name = "genQV_{}x{}_{}".format(num_qubits, depth, seed)
+        self.count = 0
+
+    def __call__(self, samples: int = None):
+        """Creates a collection of Quantum Volume circuits.
+        Parameters:
+            samples: Number of circuits to generate.
+        Returns:
+            list: A list of QuantumCircuits.
+        """
+        if samples is None:
+            samples = 1
+        out = []
+        for _ in range(self.depth):
+            qc_name = self.circ_name + '+{}'.format(self.count)
+            qc = QuantumCircuit(self.num_qubits, name=qc_name)
+            for _ in range(samples):
+                # Generate uniformly random permutation Pj of [0...n-1]
+                perm = self.rnd.permutation(self.num_qubits)
+                # For each pair p in Pj, generate Haar random SU(4)
+                for k in range(int(self.num_qubits/2)):
+                    su4 = random_unitary(4, seed=self.rnd.randint(MAX_INT))
+                    pair = int(perm[2*k]), int(perm[2*k+1])
+                    qc.append(su4, [pair[0], pair[1]])
+            out.append(qc)
+            self.count += 1
+        return out
+
+    def __next__(self):
+        return self.__call__()[0]
