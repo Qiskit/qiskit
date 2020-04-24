BackOnTrackIce/qiskit-terra

Add support for substituting a Parameter with a ParameterExpression.

test/python/circuit/test_parameters.py
@@ -1005,6 +1005,62 @@ class TestParameterExpressions(QiskitTestCase):
             self.assertTrue(all(float(gate.params[0]) == theta_val
                                 for gate in rz_gates))
 
+    def test_substituting_parameter_with_simple_expression(self):
+        """Substitute a simple parameter expression for a parameter."""
+        x = Parameter('x')
+
+        y = Parameter('y')
+        sub = y / 2
+
+        updated_expr = x.subs({x: sub})
+
+        expected = y / 2
+
+        self.assertEqual(updated_expr, expected)
+
+    def test_substituting_parameter_with_simple_expression(self):
+        """Substitute a simple parameter expression for a parameter."""
+        x = Parameter('x')
+
+        y = Parameter('y')
+        z = Parameter('z')
+        sub = y * z
+
+        updated_expr = x.subs({x: sub})
+
+        expected = y * z
+
+        self.assertEqual(updated_expr, expected)
+        
+    def test_substituting_simple_with_simple_expression(self):
+        """Substitute a simple parameter expression in a parameter expression."""
+        x = Parameter('x')
+        expr = x * x
+
+        y = Parameter('y')
+        sub = y / 2
+
+        updated_expr = expr.subs({x: sub})
+
+        expected = y*y / 4
+
+        self.assertEqual(updated_expr, expected)
+
+    def test_substituting_compound_expression(self):
+        """Substitute a compound parameter expression in a parameter expression."""
+        x = Parameter('x')
+        expr = x*x
+
+        y = Parameter('y')
+        z = Parameter('z')
+        sub = y + z
+
+        updated_expr = expr.subs({x: sub})
+
+        expected = (y + z) * (y + z)
+
+        self.assertEqual(updated_expr, expected)
+
 
 class TestParameterEquality(QiskitTestCase):
     """Test equality of Parameters and ParameterExpressions."""

Qiskit/qiskit

test/python/circuit/test_parameters.py
@@ -1003,6 +1003,62 @@ class TestParameterExpressions(QiskitTestCase):
             self.assertTrue(all(float(gate.params[0]) == theta_val
                                 for gate in rz_gates))
 
+    def test_substituting_parameter_with_simple_expression(self):
+        """Substitute a simple parameter expression for a parameter."""
+        x = Parameter('x')
+
+        y = Parameter('y')
+        sub_ = y / 2
+
+        updated_expr = x.subs({x: sub_})
+
+        expected = y / 2
+
+        self.assertEqual(updated_expr, expected)
+
+    def test_substituting_parameter_with_compound_expression(self):
+        """Substitute a simple parameter expression for a parameter."""
+        x = Parameter('x')
+
+        y = Parameter('y')
+        z = Parameter('z')
+        sub_ = y * z
+
+        updated_expr = x.subs({x: sub_})
+
+        expected = y * z
+
+        self.assertEqual(updated_expr, expected)
+
+    def test_substituting_simple_with_simple_expression(self):
+        """Substitute a simple parameter expression in a parameter expression."""
+        x = Parameter('x')
+        expr = x * x
+
+        y = Parameter('y')
+        sub_ = y / 2
+
+        updated_expr = expr.subs({x: sub_})
+
+        expected = y*y / 4
+
+        self.assertEqual(updated_expr, expected)
+
+    def test_substituting_compound_expression(self):
+        """Substitute a compound parameter expression in a parameter expression."""
+        x = Parameter('x')
+        expr = x*x
+
+        y = Parameter('y')
+        z = Parameter('z')
+        sub_ = y + z
+
+        updated_expr = expr.subs({x: sub_})
+
+        expected = (y + z) * (y + z)
+
+        self.assertEqual(updated_expr, expected)
+
 
 class TestParameterEquality(QiskitTestCase):
     """Test equality of Parameters and ParameterExpressions."""