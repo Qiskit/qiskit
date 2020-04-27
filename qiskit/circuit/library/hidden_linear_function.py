07a3bbeca8eaa8da07d8ef0e6e1178d748b9e8be
Cryoris <jules.gacon@googlemail.com>
2020-04-27 19:21:01 +0200

    math format and h(range(num_q))

--- qiskit/circuit/library/hidden_linear_function.py
+++ qiskit/circuit/library/hidden_linear_function.py
@@ -2,7 +2,7 @@
 
 # This code is part of Qiskit.
 #
-# (C) Copyright IBM 2020.
+# (C) Copyright IBM 2017, 2020.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -36,17 +36,17 @@ class HiddenLinearFunction(QuantumCircuit):
 
     .. math::
 
-        q(x) = \sum_{i,j=1}^{n}{x_i.x_j} (mod 4)
+        q(x) = \sum_{i,j=1}^{n}{x_i x_j} ~(\mathrm{mod}~ 4)
 
-    and restrict q(x) onto the nullspace of A. This results in a linear
+    and restrict :math:`q(x)` onto the nullspace of A. This results in a linear
     function.
 
     .. math::
 
-        2 \sum_{i=1}^{n}{z_i.x_i} (mod 4)  \forall  x \in Ker(A)
+        2 \sum_{i=1}^{n}{z_i x_i} ~(\mathrm{mod}~ 4)  \forall  x \in \mathrm{Ker}(A)
 
     and the goal is to recover this linear function (equivalently a vector
-    :math:`[z[0], ..., z[n-1]]`). There can be multiple solutions.
+    :math:`[z_0, ..., z_{n-1}]`). There can be multiple solutions.
 
     In [1] it is shown that the present circuit solves this problem
     on a quantum computer in constant depth, whereas any corresponding
@@ -73,7 +73,7 @@ class HiddenLinearFunction(QuantumCircuit):
     """
 
     def __init__(self,
-                 adjacency_matrix: Union[List, np.array]) -> None:
+                 adjacency_matrix: Union[List[List[int]], np.ndarray]) -> None:
         """Create new HLF circuit.
 
         Args:
@@ -84,8 +84,7 @@ class HiddenLinearFunction(QuantumCircuit):
         super().__init__(num_qubits,
                          name=f"hlf: %s" % adjacency_matrix)
 
-        for i in range(num_qubits):
-            self.h(i)
+        self.h(range(num_qubits))
         for i in range(num_qubits):
             for j in range(i+1, num_qubits):
                 if adjacency_matrix[i][j]:
@@ -93,5 +92,4 @@ class HiddenLinearFunction(QuantumCircuit):
         for i in range(num_qubits):
             if adjacency_matrix[i][i]:
                 self.s(i)
-        for i in range(num_qubits):
-            self.h(i)
+        self.h(range(num_qubits))
