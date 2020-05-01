7af8eb1064e3bdc6f833e906ad8f4a11e4c5749a
Thomas Alexander <talexander@ibm.com>
2020-05-01 13:14:38 -0400

    Change deprecation warning to emit warning at proper stack location.

--- qiskit/util.py
+++ qiskit/util.py
@@ -123,7 +123,7 @@ def deprecate_function(msg):
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            warnings.warn(msg, DeprecationWarning)
+            warnings.warn(msg, DeprecationWarning, stacklevel=3)
             return func(*args, **kwargs)
         return wrapper
     return decorator
