a9fdba6d0fdbc034d59162b64494db3108da0c31
Edwin Navarro <enavarro@comcast.net>
2020-05-02 13:44:34 -0700

    Lint fix

--- qiskit/visualization/circuit_visualization.py
+++ qiskit/visualization/circuit_visualization.py
@@ -636,7 +636,7 @@ def _matplotlib_circuit_drawer(circuit,
         idle_wires (bool): Include idle wires. Default is True.
         with_layout (bool): Include layout information, with labels on the physical
             layout. Default: True.
-        initial_state (bool): Optional. Adds ``|0>`` in the beginning of the line. 
+        initial_state (bool): Optional. Adds ``|0>`` in the beginning of the line.
             Default: ``False``.
         fold (int): amount ops allowed before folding. Default is 25.
         ax (matplotlib.axes.Axes): An optional Axes object to be used for
