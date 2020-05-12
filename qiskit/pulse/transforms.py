15a1166139d82eb84a1b236b2fddbc18f20b375d
Thomas Alexander <thomasalexander2718@gmail.com>
2020-05-12 14:43:26 -0400

    Update qiskit/pulse/transforms.py
    Co-authored-by: Lauren Capelluto <laurencapelluto@gmail.com>

--- qiskit/pulse/transforms.py
+++ qiskit/pulse/transforms.py
@@ -421,9 +421,7 @@ def remove_directives(schedule: Schedule) -> Schedule:
 def remove_trivial_barriers(schedule: Schedule) -> Schedule:
     """Remove trivial barriers with 0 or 1 channels."""
     def filter_func(inst):
-        if (isinstance(inst[1], directives.RelativeBarrier) and
-                len(inst[1].channels) < 2):
-            return True
-        return False
+        return (isinstance(inst[1], directives.RelativeBarrier) and
+                len(inst[1].channels) < 2)
 
     return schedule.exclude(filter_func)
