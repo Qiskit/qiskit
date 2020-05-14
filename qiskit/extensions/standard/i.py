52417c2e6ed476c044d5a67dd03d7775a7e40637
Erick Winston <ewinston@us.ibm.com>
2020-05-14 03:14:02 -0400

    update i an x from merge

--- qiskit/extensions/standard/i.py
+++ qiskit/extensions/standard/i.py
@@ -13,31 +13,6 @@
 # that they have been altered from the originals.
 
 """The standard gates moved to qiskit/circuit/library."""
-    def control(self, num_ctrl_qubits=1, label=None, ctrl_state=None):
-        """Controlled version of this gate.
-
-        Args:
-            num_ctrl_qubits (int): number of control qubits.
-            label (str or None): An optional label for the gate [Default: None]
-            ctrl_state (int or str or None): control state expressed as integer,
-                string (e.g. '110'), or None. If None, use all 1s.
-
-        Returns:
-            Gate: Generic gate with all identities.
-        """
-        # pylint: disable=cyclic-import
-        from qiskit.circuit import QuantumRegister
-        if num_ctrl_qubits > 2:
-            ctrl_substr = 'c{0:d}'.format(num_ctrl_qubits)
-        else:
-            ctrl_substr = ('{0}' * num_ctrl_qubits).format('c')
-        new_name = '{0}{1}'.format(ctrl_substr, self.name)
-        num_qubits = 1 + num_ctrl_qubits
-        idgate = Gate(new_name, num_qubits, [], label=label)
-        qr = QuantumRegister(num_qubits, 'q')
-        idgate._definition = [(IGate(), [qr[i]], []) for i in range(num_qubits)]
-        return idgate
-
 
 from qiskit.circuit.library.standard_gates.i import IGate, IdGate
 
--- qiskit/extensions/standard/x.py
+++ qiskit/extensions/standard/x.py
@@ -18,22 +18,6 @@ from qiskit.circuit.library.standard_gates.x import (
     XGate, CXGate, CCXGate, C3XGate, C4XGate, RCCXGate, RC3XGate, CnotGate, ToffoliGate,
     MCXGate, MCXGrayCode, MCXVChain, MCXRecursive
 )
-    def control(self, num_ctrl_qubits=1, label=None, ctrl_state=None):
-        """Controlled version of this gate. This is needed for distinguishing CnotGate
-        from an opaque gate.
-
-        Args:
-            num_ctrl_qubits (int): number of control qubits.
-            label (str or None): An optional label for the gate [Default: None]
-            ctrl_state (int or str or None): control state expressed as integer,
-                string (e.g. '110'), or None. If None, use all 1s.
-
-        Returns:
-            ControlledGate: controlled version of this gate.
-        """
-        return super().control(num_ctrl_qubits=num_ctrl_qubits, label=label,
-                               ctrl_state=ctrl_state)
-
 
 __all__ = ['XGate', 'CXGate', 'CCXGate', 'C3XGate', 'C4XGate', 'RCCXGate', 'RC3XGate',
            'CnotGate', 'ToffoliGate', 'MCXGate', 'MCXGrayCode', 'MCXVChain', 'MCXRecursive']
