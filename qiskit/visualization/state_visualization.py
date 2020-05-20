524a6af6118423c68576ddacf4af8999437a71ff
Paul Nation <nonhermitian@gmail.com>
2020-05-19 22:21:56 -0400

    fix example

--- qiskit/visualization/state_visualization.py
+++ qiskit/visualization/state_visualization.py
@@ -666,10 +666,10 @@ def plot_state_qsphere(rho, figsize=None, ax=None, show_state_labels=True,
            from qiskit.visualization import plot_state_qsphere
            %matplotlib inline
 
-           qc = QuantumCircuit(2, 2)
+           qc = QuantumCircuit(2)
+           qc.x(0)
            qc.h(0)
            qc.cx(0, 1)
-           qc.measure([0, 1], [0, 1])
 
            backend = BasicAer.get_backend('statevector_simulator')
            job = execute(qc, backend).result()
