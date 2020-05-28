cc489b0b8b7798a21161cbfb165929378510b7b4
Starfish <65141281+metacubestormerxii12@users.noreply.github.com>
2020-05-28 18:48:18 -0500

    Update test_circuit_matplotlib_drawer.py

--- test/python/visualization/test_circuit_matplotlib_drawer.py
+++ test/python/visualization/test_circuit_matplotlib_drawer.py
@@ -85,7 +85,8 @@ class TestMatplotlibDrawer(QiskitVisualizationTestCase):
 
         # check the barriers plot properly when plot_barriers= True
         filename = self._get_resource_path('visualization/references/current_matplotlib_ref.png')
-        visualization.circuit_drawer(qc, output='mpl', plot_barriers=True, filename=filename)
+        visualization.circuit_drawer(qc, output='mpl', scale=1.02,
+                                     plot_barriers=True, filename=filename)
         self.addCleanup(os.remove, filename)
 
         ref_filename = self._get_resource_path(
@@ -155,7 +156,8 @@ class TestMatplotlibDrawer(QiskitVisualizationTestCase):
         circuit.h(qr[0]).c_if(cr, 2)
 
         conditional_filename = self._get_resource_path('current_conditional_matplotlib_ref.png')
-        visualization.circuit_drawer(circuit, output='mpl', filename=conditional_filename)
+        visualization.circuit_drawer(circuit, output='mpl', scale=1.02
+                                     filename=conditional_filename)
         self.addCleanup(os.remove, conditional_filename)
 
         ref_filename = self._get_resource_path(
