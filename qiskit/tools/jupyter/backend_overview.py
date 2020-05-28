106c61e3cf74abbd935df84021d35436411c3f1d
Matthew Treinish <mtreinish@kortar.org>
2020-05-28 10:57:53 -0400

    Use FakeProvider for jupyter and monitor tests (#4296) (#4482)
    * Use FakeProvider for jupyter and monitor tests
    
    Right now the jupyter widget tests and backend monitor tests are the
    only place in the qiskit-terra unittests that require talking to IQX.
    This commit removes that dependency by leveraging the FakeProvider and
    FakeBackends which contain snapshots of the responses from the IBMQ api.
    
    To accomplish mocking out the qiskit-ibmq-provider usage the
    backend_overview module had to be renamed because it conflicted with the
    backend_overview function and was not importable via an absolute import.
    
    * Fix tests and lint
    
    * Remove online tests from contributing documentation
    
    There are no longer any online tests in the suite now that the provider
    interactions have been mocked out. This commit removes the documentation
    about how to configure the online tests.
    
    * Fixes from review comments
    
    (cherry picked from commit a87fe61992003e7e7e541f41194c5f0b3e458346)

--- qiskit/tools/jupyter/backend_overview.py
+++ qiskit/tools/jupyter/backend_overview.py
@@ -22,7 +22,7 @@ from IPython.core.magic import line_magic, Magics, magics_class  # pylint: disab
 from IPython.core import magic_arguments                         # pylint: disable=import-error
 import matplotlib.pyplot as plt                                  # pylint: disable=import-error
 import ipywidgets as widgets                                     # pylint: disable=import-error
-from qiskit.tools.monitor.backend_overview import get_unique_backends
+from qiskit.tools.monitor.overview import get_unique_backends
 from qiskit.visualization.gate_map import plot_gate_map
 
 
