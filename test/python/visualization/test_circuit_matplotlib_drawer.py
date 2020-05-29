2c2999710a253d58a6a61b21238225e7b164a7a1
Starfish <65141281+metacubestormerxii12@users.noreply.github.com>
2020-05-28 19:46:21 -0500

    Update test_circuit_matplotlib_drawer.py

--- test/python/visualization/test_circuit_matplotlib_drawer.py
+++ test/python/visualization/test_circuit_matplotlib_drawer.py
@@ -156,7 +156,7 @@ class TestMatplotlibDrawer(QiskitVisualizationTestCase):
         circuit.h(qr[0]).c_if(cr, 2)
 
         conditional_filename = self._get_resource_path('current_conditional_matplotlib_ref.png')
-        visualization.circuit_drawer(circuit, output='mpl', scale=1.02
+        visualization.circuit_drawer(circuit, output='mpl', scale=1.02,
                                      filename=conditional_filename)
         self.addCleanup(os.remove, conditional_filename)
 
