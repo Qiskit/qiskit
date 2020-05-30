a1bb87f42cfa8250900c5487a6a4998162fad204
Luciano Bello <luciano.bello@ibm.com>
2020-05-30 12:54:46 -0400

    better test_synthesis.py

--- test/python/circuit/oracle_compiler/test_synthesis.py
+++ test/python/circuit/oracle_compiler/test_synthesis.py
@@ -17,21 +17,39 @@
 import unittest
 
 from qiskit.circuit.oracle_compiler import compile_oracle
-from qiskit import QuantumCircuit
+
+from qiskit import QuantumCircuit, QuantumRegister
+from qiskit.circuit.library.standard_gates import XGate
+
 from . import examples
 
 
 class TestSynthesis(unittest.TestCase):
     """Tests LogicNetwork.synth method."""
+
     def test_grover_oracle(self):
         network = compile_oracle(examples.grover_oracle)
         quantum_circuit = network.synth()
-        self.assertIsInstance(quantum_circuit, QuantumCircuit)
+
+        expected = QuantumCircuit(5)
+        expected.append(XGate().control(4, ctrl_state='0101'), [0, 1, 2, 3, 4])
+
+        self.assertEqual(quantum_circuit, expected)
 
     def test_grover_oracle_arg_regs(self):
         network = compile_oracle(examples.grover_oracle)
         quantum_circuit = network.synth(arg_regs=True)
-        self.assertIsInstance(quantum_circuit, QuantumCircuit)
+
+        qr_a = QuantumRegister(1, 'a')
+        qr_b = QuantumRegister(1, 'b')
+        qr_c = QuantumRegister(1, 'c')
+        qr_d = QuantumRegister(1, 'd')
+        qr_return = QuantumRegister(1, 'return')
+        expected = QuantumCircuit(qr_d, qr_c, qr_b, qr_a, qr_return)
+        expected.append(XGate().control(4, ctrl_state='0101'),
+                        [qr_d[0], qr_c[0], qr_b[0], qr_a[0], qr_return[0]])
+
+        self.assertEqual(quantum_circuit, expected)
 
 
 if __name__ == '__main__':
