c595e247e6e54f031f393caed0f268730bb63b3c
Starfish <65141281+metacubestormerxii12@users.noreply.github.com>
2020-06-05 00:58:01 -0500

    Update dagcircuit.py

--- qiskit/dagcircuit/dagcircuit.py
+++ qiskit/dagcircuit/dagcircuit.py
@@ -609,7 +609,7 @@ class DAGCircuit:
         else:
             return None
 
-    def idle_wires(self, barriers_is_idle=False):
+    def idle_wires(self, barrier_is_idle):
         """Return idle wires (wires which only contain barriers are idle).
 
         Yields:
