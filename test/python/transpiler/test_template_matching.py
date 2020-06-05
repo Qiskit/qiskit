1be933be04a9de00e5b807b36ea6115b905d683c
romain <rmoyard@gmail.com>
2020-06-05 05:34:01 +0200

    Linting test_template_matching.

--- test/python/transpiler/test_template_matching.py
+++ test/python/transpiler/test_template_matching.py
@@ -12,22 +12,25 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Tests for pass cancelling 2 consecutive CNOTs on the same qubits."""
+"""Test the TemplateOptimization pass."""
 
+import unittest
 from qiskit import QuantumRegister, QuantumCircuit
+from qiskit.circuit.library.template_circuits.toffoli import template_2a_2, template_5a_3
 from qiskit.converters.circuit_to_dag import circuit_to_dag
 from qiskit.transpiler import PassManager
 from qiskit.transpiler.passes import TemplateOptimization
 from qiskit.test import QiskitTestCase
+from qiskit.transpiler.exceptions import TranspilerError
 
 
 class TestTemplateMatching(QiskitTestCase):
-    """Test the CXCancellation pass."""
+    """Test the TemplateOptimization pass."""
 
-    def test_pass_cx_cancellation(self):
-        """Test the cx cancellation pass.
-
-        It should cancel consecutive cx pairs on same qubits.
+    def test_pass_cx_cancellation_no_template_given(self):
+        """
+        Check the cancellation of CX gates for the apply of the three basic
+        template x-x, cx-cx. ccx-ccx.
         """
         qr = QuantumRegister(2)
         circuit_in = QuantumCircuit(qr)
@@ -50,21 +53,189 @@ class TestTemplateMatching(QiskitTestCase):
 
         self.assertEqual(circuit_in_opt, circuit_out)
 
+    def test_pass_cx_cancellation_own_template(self):
+        """
+        Check the cancellation of CX gates for the apply of a self made template cx-cx.
+        """
+        qr = QuantumRegister(2, 'qr')
+        circuit_in = QuantumCircuit(qr)
+        circuit_in.h(qr[0])
+        circuit_in.h(qr[0])
+        circuit_in.cx(qr[0], qr[1])
+        circuit_in.cx(qr[0], qr[1])
+        circuit_in.cx(qr[0], qr[1])
+        circuit_in.cx(qr[0], qr[1])
+        circuit_in.cx(qr[1], qr[0])
+        circuit_in.cx(qr[1], qr[0])
+        dag_in = circuit_to_dag(circuit_in)
 
-'''    def test_optimize_h_gates_pass_manager(self):
-        """Transpile: qr:--[H]-[H]-[H]-- == qr:--[u2]-- """
-        qr = QuantumRegister(1, 'qr')
-        circuit = QuantumCircuit(qr)
-        circuit.h(qr[0])
-        circuit.h(qr[0])
-        circuit.h(qr[0])
+        qrt = QuantumRegister(2, 'qrc')
+        qct = QuantumCircuit(qrt)
+        qct.cx(0, 1)
+        qct.cx(0, 1)
 
-        expected = QuantumCircuit(qr)
-        expected.u2(0, np.pi, qr[0])
+        template_list = [qct]
+        pass_ = TemplateOptimization(template_list)
+        dag_opt = pass_.run(dag_in)
 
-        passmanager = PassManager()
-        passmanager.append(Unroller(['u2']))
-        passmanager.append(Optimize1qGates())
-        result = passmanager.run(circuit)
+        circuit_expected = QuantumCircuit(qr)
+        circuit_expected.h(qr[0])
+        circuit_expected.h(qr[0])
+        dag_expected = circuit_to_dag(circuit_expected)
 
-        self.assertEqual(expected, result)'''
+        self.assertEqual(dag_opt, dag_expected)
+
+    def test_pass_cx_cancellation_template_from_library(self):
+        """
+        Check the cancellation of CX gates for the apply of the library template cx-cx (2a_2).
+        """
+        qr = QuantumRegister(2, 'qr')
+        circuit_in = QuantumCircuit(qr)
+        circuit_in.h(qr[0])
+        circuit_in.h(qr[0])
+        circuit_in.cx(qr[0], qr[1])
+        circuit_in.cx(qr[0], qr[1])
+        circuit_in.cx(qr[0], qr[1])
+        circuit_in.cx(qr[0], qr[1])
+        circuit_in.cx(qr[1], qr[0])
+        circuit_in.cx(qr[1], qr[0])
+        dag_in = circuit_to_dag(circuit_in)
+
+        template_list = [template_2a_2()]
+        pass_ = TemplateOptimization(template_list)
+        dag_opt = pass_.run(dag_in)
+
+        circuit_expected = QuantumCircuit(qr)
+        circuit_expected.h(qr[0])
+        circuit_expected.h(qr[0])
+        dag_expected = circuit_to_dag(circuit_expected)
+
+        self.assertEqual(dag_opt, dag_expected)
+
+    def test_pass_template_5a(self):
+        """
+        Verify the result of template matching and substitution with the template 5a_3.
+        q_0: ───────■─────────■────■──
+                  ┌─┴─┐     ┌─┴─┐  │
+        q_1: ──■──┤ X ├──■──┤ X ├──┼──
+             ┌─┴─┐└───┘┌─┴─┐└───┘┌─┴─┐
+        q_2: ┤ X ├─────┤ X ├─────┤ X ├
+             └───┘     └───┘     └───┘
+        The circuit before optimization is:
+              ┌───┐               ┌───┐
+        qr_0: ┤ X ├───────────────┤ X ├─────
+              └─┬─┘     ┌───┐┌───┐└─┬─┘
+        qr_1: ──┼────■──┤ X ├┤ Z ├──┼────■──
+                │    │  └─┬─┘└───┘  │    │
+        qr_2: ──┼────┼────■────■────■────┼──
+                │    │  ┌───┐┌─┴─┐  │    │
+        qr_3: ──■────┼──┤ H ├┤ X ├──■────┼──
+                │  ┌─┴─┐└───┘└───┘     ┌─┴─┐
+        qr_4: ──■──┤ X ├───────────────┤ X ├
+                   └───┘               └───┘
+
+        The match is given by [0,1][1,2][2,7], after substitution the circuit becomes:
+
+              ┌───┐               ┌───┐
+        qr_0: ┤ X ├───────────────┤ X ├
+              └─┬─┘     ┌───┐┌───┐└─┬─┘
+        qr_1: ──┼───────┤ X ├┤ Z ├──┼──
+                │       └─┬─┘└───┘  │
+        qr_2: ──┼────■────■────■────■──
+                │    │  ┌───┐┌─┴─┐  │
+        qr_3: ──■────┼──┤ H ├┤ X ├──■──
+                │  ┌─┴─┐└───┘└───┘
+        qr_4: ──■──┤ X ├───────────────
+                   └───┘
+        """
+        qr = QuantumRegister(5, 'qr')
+        circuit_in = QuantumCircuit(qr)
+        circuit_in.ccx(qr[3], qr[4], qr[0])
+        circuit_in.cx(qr[1], qr[4])
+        circuit_in.cx(qr[2], qr[1])
+        circuit_in.h(qr[3])
+        circuit_in.z(qr[1])
+        circuit_in.cx(qr[2], qr[3])
+        circuit_in.ccx(qr[2], qr[3], qr[0])
+        circuit_in.cx(qr[1], qr[4])
+        dag_in = circuit_to_dag(circuit_in)
+
+        template_list = [template_5a_3()]
+        pass_ = TemplateOptimization(template_list)
+        dag_opt = pass_.run(dag_in)
+
+        circuit_expected = QuantumCircuit(qr)
+        circuit_expected.ccx(qr[3], qr[4], qr[0])
+        circuit_expected.cx(qr[2], qr[4])
+        circuit_expected.cx(qr[2], qr[1])
+        circuit_expected.z(qr[1])
+        circuit_expected.h(qr[3])
+        circuit_expected.cx(qr[2], qr[3])
+        circuit_expected.ccx(qr[2], qr[3], qr[0])
+        print(circuit_expected)
+        dag_expected = circuit_to_dag(circuit_expected)
+
+        self.assertEqual(dag_opt, dag_expected)
+
+    def test_pass_template_wrong_type(self):
+        """
+        If a template is not equivalent to the identity, it raises an erro.
+        """
+        qr = QuantumRegister(2, 'qr')
+        circuit_in = QuantumCircuit(qr)
+        circuit_in.h(qr[0])
+        circuit_in.h(qr[0])
+        circuit_in.cx(qr[0], qr[1])
+        circuit_in.cx(qr[0], qr[1])
+        circuit_in.cx(qr[0], qr[1])
+        circuit_in.cx(qr[0], qr[1])
+        circuit_in.cx(qr[1], qr[0])
+        circuit_in.cx(qr[1], qr[0])
+        dag_in = circuit_to_dag(circuit_in)
+
+        qrt = QuantumRegister(2, 'qrc')
+        qct = QuantumCircuit(qrt)
+        qct.cx(0, 1)
+        qct.x(0)
+        qct.h(1)
+
+        template_list = [qct]
+        pass_ = TemplateOptimization(template_list)
+
+        self.assertRaises(TranspilerError, pass_.run, dag_in)
+
+    def test_pass_template_too_many_qubits(self):
+        """
+        If the the template has more qubits than the circuit, it raises an error.
+        """
+        qr = QuantumRegister(2, 'qr')
+        circuit_in = QuantumCircuit(qr)
+        circuit_in.h(qr[0])
+        circuit_in.h(qr[0])
+        circuit_in.cx(qr[0], qr[1])
+        circuit_in.cx(qr[0], qr[1])
+        circuit_in.cx(qr[0], qr[1])
+        circuit_in.cx(qr[0], qr[1])
+        circuit_in.cx(qr[1], qr[0])
+        circuit_in.cx(qr[1], qr[0])
+        dag_in = circuit_to_dag(circuit_in)
+
+        qrt = QuantumRegister(10, 'qrc')
+        qct = QuantumCircuit(qrt)
+        qct.mcx(control_qubits=[0, 1, 2, 3], target_qubit=[8])
+        qct.mcx(control_qubits=[0, 1, 2, 3], target_qubit=[8])
+        qct.x(0)
+        qct.x(0)
+        qct.h(1)
+        qct.h(1)
+        qct.t(8)
+        qct.t(8)
+
+        template_list = [qct]
+        pass_ = TemplateOptimization(template_list)
+
+        self.assertRaises(TranspilerError, pass_.run, dag_in)
+
+
+if __name__ == '__main__':
+    unittest.main()
