9a53cc8c41aa0be6fe57cb0368c2c30412b334bb
SooluThomas <soolu.elto@gmail.com>
2020-06-08 16:58:50 -0400

    Separate set phase and shift phase in visualization

--- qiskit/visualization/pulse/matplotlib.py
+++ qiskit/visualization/pulse/matplotlib.py
@@ -57,6 +57,7 @@ class EventsOutputChannels:
 
         self._waveform = None
         self._framechanges = None
+        self._setphase = None
         self._frequencychanges = None
         self._conditionals = None
         self._snapshots = None
@@ -96,6 +97,14 @@ class EventsOutputChannels:
             self._build_waveform()
 
         return self._trim(self._framechanges)
+
+    @property
+    def setphase(self) -> Dict[int, SetPhase]:
+        """Get the SetPhase phase values."""
+        if self._setphase is None:
+            self._build_waveform()
+
+        return self._trim(self._setphase)
 
     @property
     def frequencychanges(self) -> Dict[int, SetFrequency]:
@@ -143,7 +152,10 @@ class EventsOutputChannels:
         Returns:
             bool: if the channel has nothing to plot
         """
-        if any(self.waveform) or self.framechanges or self.conditionals or self.snapshots:
+        if (
+            any(self.waveform) or self.framechanges
+            or self.setphase or self.conditionals or self.snapshots
+        ):
             return False
 
         return True
@@ -160,12 +172,16 @@ class EventsOutputChannels:
         time_event = []
 
         framechanges = self.framechanges
+        setphase = self.setphase
         conditionals = self.conditionals
         snapshots = self.snapshots
         frequencychanges = self.frequencychanges
 
         for key, val in framechanges.items():
-            data_str = 'phase: %.2f' % val
+            data_str = 'shift phase: %.2f' % val
+            time_event.append((key, name, data_str))
+        for key, val in setphase.items():
+            data_str = 'set phase: %.2f' % val
             time_event.append((key, name, data_str))
         for key, val in conditionals.items():
             data_str = 'conditional, %s' % val
@@ -183,6 +199,7 @@ class EventsOutputChannels:
         """Create waveform from stored pulses.
         """
         self._framechanges = {}
+        self._setphase = {}
         self._frequencychanges = {}
         self._conditionals = {}
         self._snapshots = {}
@@ -195,13 +212,14 @@ class EventsOutputChannels:
             if time > self.tf:
                 break
             tmp_fc = 0
+            tmp_set_phase = 0
             tmp_sf = None
             for command in commands:
                 if isinstance(command, (FrameChange, ShiftPhase)):
                     tmp_fc += command.phase
                     pv[time:] = 0
                 elif isinstance(command, SetPhase):
-                    tmp_fc = command.phase
+                    tmp_set_phase = command.phase
                     pv[time:] = 0
                 elif isinstance(command, SetFrequency):
                     tmp_sf = command.frequency
@@ -212,6 +230,9 @@ class EventsOutputChannels:
             if tmp_fc != 0:
                 self._framechanges[time] = tmp_fc
                 fc += tmp_fc
+            if tmp_set_phase !=0:
+                self._setphase[time] = tmp_set_phase
+                fc = tmp_set_phase
             if tmp_sf is not None:
                 self._frequencychanges[time] = tmp_sf
             for command in commands:
