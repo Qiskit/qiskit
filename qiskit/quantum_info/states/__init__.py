d706935e77e49e94c7db1c3c054bf77b00b3c9c6
Ali Javadi <ajavadia@princeton.edu>
2020-06-15 00:39:25 -0400

    remove deprecated qi functions

--- qiskit/quantum_info/states/__init__.py
+++ qiskit/quantum_info/states/__init__.py
@@ -19,5 +19,3 @@ from .densitymatrix import DensityMatrix
 from .utils import partial_trace, shannon_entropy
 from .measures import (state_fidelity, purity, entropy, concurrence,
                        mutual_information, entanglement_of_formation)
-from .states import basis_state, projector  # DERECATED in 0.12.0
-from .counts import state_to_counts  # DEPRECATED in 0.13.0
