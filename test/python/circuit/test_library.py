ed9eb5e3856b22ecded9124d6f34e4e6bad104ff
Mantas Cepulkovskis <mantas.cepulkovskis@gmail.com>
2020-06-26 22:47:19 +0300

    Add test for FourierChecking circuit

--- test/python/circuit/test_library.py
+++ test/python/circuit/test_library.py
@@ -31,7 +31,7 @@ from qiskit.circuit.library import (BlueprintCircuit, Permutation, QuantumVolume
                                     WeightedAdder, Diagonal, NLocal, TwoLocal, RealAmplitudes,
                                     EfficientSU2, ExcitationPreserving, PauliFeatureMap,
                                     ZFeatureMap, ZZFeatureMap, MCMT, MCMTVChain, GMS,
-                                    HiddenLinearFunction, GraphState)
+                                    HiddenLinearFunction, GraphState, FourierChecking)
 from qiskit.circuit.random.utils import random_circuit
 from qiskit.converters.circuit_to_dag import circuit_to_dag
 from qiskit.exceptions import QiskitError
@@ -228,6 +228,52 @@ class TestGraphStateLibrary(QiskitTestCase):
         """Test that adjacency matrix is required to be symmetric."""
         with self.assertRaises(CircuitError):
             GraphState(adjacency_matrix)
+
+
+@ddt
+class TestFourierCheckingLibrary(QiskitTestCase):
+    """Test the Fourier Checking circuit."""
+
+    def assertFourierCheckingIsCorrect(self, f_truth_table, g_truth_table, fc_circuit):
+        """Assert that the Fourier Checking circuit produces the correct matrix."""
+
+        simulated = Operator(fc_circuit)
+
+        num_qubits = int(np.log2(len(f_truth_table)))
+
+        # create Hadamard matrix, re-use approach from TestMCMT
+        h_i = 1 / np.sqrt(2) * np.array([[1, 1], [1, -1]])
+        h_tot = np.array([1])
+        for _ in range(num_qubits):
+            h_tot = np.kron(h_tot, h_i)
+
+        f_mat = np.diag(f_truth_table)
+        g_mat = np.diag(g_truth_table)
+
+        expected = np.linalg.multi_dot([h_tot, g_mat, h_tot, f_mat, h_tot])
+        expected = Operator(expected)
+
+        self.assertTrue(expected.equiv(simulated))
+
+    @data(
+        ([1, -1, -1, -1], [1, 1, -1, -1])
+    )
+    @unpack
+    def test_fourier_checking(self, f_truth_table, g_truth_table):
+        """Test if the Fourier Checking circuit produces the correct matrix."""
+        fc_circuit = FourierChecking(f_truth_table, g_truth_table)
+        self.assertFourierCheckingIsCorrect(f_truth_table, g_truth_table, fc_circuit)
+
+    @data(
+        ([1, -1, -1, -1], [1, 1, -1]),
+        ([1], [-1]),
+        ([1, -1, -1, -1, 1], [1, 1, -1, -1, 1])
+    )
+    @unpack
+    def test_invalid_input_raises(self, f_truth_table, g_truth_table):
+        """Test that invalid input truth tables raise an error."""
+        with self.assertRaises(CircuitError):
+            FourierChecking(f_truth_table, g_truth_table)
 
 
 class TestIQPLibrary(QiskitTestCase):
