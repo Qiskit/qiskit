6c95b6511e4fe0937edbacdd41820993369e307e
Matthew Treinish <mtreinish@kortar.org>
2020-06-29 08:57:30 -0400

    Fix tests

--- test/python/result/test_counts.py
+++ test/python/result/test_counts.py
@@ -34,7 +34,7 @@ class TestCounts(unittest.TestCase):
     def test_counts_with_exta_formatting_data(self):
         raw_counts = {'0x0': 4, '0x2': 10}
         expected = {'0 0 00': 4, '0 0 10': 10}
-        result = counts.Counts(raw_counts, 'test_counts', shots=14,
+        result = counts.Counts(raw_counts, 'test_counts',
                                creg_sizes=[['c0', 2], ['c0', 1], ['c1', 1]],
                                memory_slots=4)
         self.assertEqual(result, expected)
@@ -43,7 +43,7 @@ class TestCounts(unittest.TestCase):
         raw_counts = {'0x0': 4, '0x1': 7, '0x2': 10, '0x6': 5, '0x9': 11,
                       '0xD': 9, '0xE': 8}
         expected = {'00': 4, '01': 27, '10': 23}
-        counts_obj = counts.Counts(raw_counts, shots=54, creg_sizes=[['c0', 4]],
+        counts_obj = counts.Counts(raw_counts, creg_sizes=[['c0', 4]],
                                    memory_slots=4)
         result = utils.marginal_counts(counts_obj, [0, 1])
         self.assertEqual(expected, result)
@@ -84,7 +84,7 @@ class TestCounts(unittest.TestCase):
     def test_int_counts_with_exta_formatting_data(self):
         raw_counts = {0: 4, 2: 10}
         expected = {'0 0 00': 4, '0 0 10': 10}
-        result = counts.Counts(raw_counts, 'test_counts', shots=14,
+        result = counts.Counts(raw_counts, 'test_counts',
                                creg_sizes=[['c0', 2], ['c0', 1], ['c1', 1]],
                                memory_slots=4)
         self.assertEqual(result, expected)
@@ -92,7 +92,7 @@ class TestCounts(unittest.TestCase):
     def test_marginal_int_counts(self):
         raw_counts = {0: 4, 1: 7, 2: 10, 6: 5, 9: 11, 13: 9, 14: 8}
         expected = {'00': 4, '01': 27, '10': 23}
-        counts_obj = counts.Counts(raw_counts, shots=54, creg_sizes=[['c0', 4]],
+        counts_obj = counts.Counts(raw_counts, creg_sizes=[['c0', 4]],
                                    memory_slots=4)
         result = utils.marginal_counts(counts_obj, [0, 1])
         self.assertEqual(expected, result)
@@ -135,7 +135,7 @@ class TestCounts(unittest.TestCase):
     def test_bistring_counts_with_exta_formatting_data(self):
         raw_counts = {'0': 4, '10': 10}
         expected = {'0 0 00': 4, '0 0 10': 10}
-        result = counts.Counts(raw_counts, 'test_counts', shots=14,
+        result = counts.Counts(raw_counts, 'test_counts',
                                creg_sizes=[['c0', 2], ['c0', 1], ['c1', 1]],
                                memory_slots=4)
         self.assertEqual(result, expected)
@@ -143,7 +143,7 @@ class TestCounts(unittest.TestCase):
     def test_marginal_bitstring_counts(self):
         raw_counts = {'0': 4, '1': 7, '10': 10, '110': 5, '1001': 11, '1101': 9, '1110': 8}
         expected = {'00': 4, '01': 27, '10': 23}
-        counts_obj = counts.Counts(raw_counts, shots=54, creg_sizes=[['c0', 4]],
+        counts_obj = counts.Counts(raw_counts, creg_sizes=[['c0', 4]],
                                    memory_slots=4)
         result = utils.marginal_counts(counts_obj, [0, 1])
         self.assertEqual(expected, result)
@@ -214,7 +214,7 @@ class TestCounts(unittest.TestCase):
     def test_0b_bistring_counts_with_exta_formatting_data(self):
         raw_counts = {'0b0': 4, '0b10': 10}
         expected = {'0 0 00': 4, '0 0 10': 10}
-        result = counts.Counts(raw_counts, 'test_counts', shots=14,
+        result = counts.Counts(raw_counts, 'test_counts',
                                creg_sizes=[['c0', 2], ['c0', 1], ['c1', 1]],
                                memory_slots=4)
         self.assertEqual(result, expected)
@@ -223,7 +223,7 @@ class TestCounts(unittest.TestCase):
         raw_counts = {'0b0': 4, '0b1': 7, '0b10': 10, '0b110': 5, '0b1001': 11,
                       '0b1101': 9, '0b1110': 8}
         expected = {'00': 4, '01': 27, '10': 23}
-        counts_obj = counts.Counts(raw_counts, shots=54, creg_sizes=[['c0', 4]],
+        counts_obj = counts.Counts(raw_counts, creg_sizes=[['c0', 4]],
                                    memory_slots=4)
         result = utils.marginal_counts(counts_obj, [0, 1])
         self.assertEqual(expected, result)
