6f8a8284c3fdaf96dda9fb878d26e171671ddec6
Thomas Alexander <talexander@ibm.com>
2020-06-30 18:56:00 -0400

    Add analysis module.

---
+++ qiskit/pulse/analysis.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2020.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Analysis passes and functions for pulse programs."""
+
+import numpy as np
+
+from qiskit import pulse
+from qiskit.pulse import instructions, commands
+from qiskit.pulse.basepasses import AnalysisPass
+from qiskit.pulse import InstructionScheduleMap
+
+
+class MeasurementAlignmentAnalysis(AnalysisPass):
+    def __init__(
+        self,
+        inst_map: InstructionScheduleMap,
+        cal_gate: str = 'u3',
+        max_calibration_duration: int = None,
+    ):
+        """Calculate the latest time to align all measures in a program."""
+        super().__init__()
+        self.inst_map = inst_map
+        self.cal_gate = cal_gate
+        self.max_calibration_duration = max_calibration_duration
+
+    def run(
+        self,
+        program: pulse.Program,
+    ) -> pulse.Program:
+        """Return the the max between the duration of the calibration time and
+        the absolute time of the latest scheduled acquire."""
+        if self.max_calibration_duration is None:
+            align_time = self.get_max_calibration_duration()
+        else:
+            align_time = self.max_calibration_duration
+        for schedule in program.schedules:
+            last_acquire = 0
+            acquire_times = [time for time, inst in schedule.instructions
+                             if isinstance(inst, (instructions.Acquire,
+                                                  commands.AcquireInstruction))]
+            if acquire_times:
+                last_acquire = max(acquire_times)
+            align_time = max(align_time, last_acquire)
+        self.analysis.meas_align_time = align_time
+        return program
+
+    def get_max_calibration_duration(self):
+        """Return the time needed to allow for readout discrimination calibration pulses."""
+        max_calibration_duration = 0
+        for qubits in self.inst_map.qubits_with_instruction(self.cal_gate):
+            cmd = self.inst_map.get(self.cal_gate, qubits, np.pi, 0, np.pi)
+            max_calibration_duration = max(cmd.duration, max_calibration_duration)
+        return max_calibration_duration
--- qiskit/pulse/transforms.py
+++ qiskit/pulse/transforms.py
@@ -12,64 +12,19 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Basic transformation functions which take a Schedule (and possibly some arguments) and return
-a new Schedule.
-"""
+"""Transformation passes and functions for pulse programs."""
+
 import warnings
 
 from typing import List, Optional, Iterable
 
-import numpy as np
-
 from qiskit import pulse
-from qiskit.pulse import (Acquire, AcquireInstruction, Delay, Play,
+from qiskit.pulse import (analysis, Acquire, AcquireInstruction, Delay, Play,
                           InstructionScheduleMap, ScheduleComponent, Schedule)
 from qiskit.pulse.passmanager import PassManager
-from qiskit.pulse.basepasses import (AnalysisPass, TransformationPass)
+from qiskit.pulse.basepasses import TransformationPass
 from .channels import Channel, AcquireChannel, MeasureChannel, MemorySlot
 from .exceptions import PulseError, CompilerError
-
-
-class AlignMeasuresAnalysis(AnalysisPass):
-    def __init__(
-        self,
-        inst_map: InstructionScheduleMap,
-        cal_gate: str = 'u3',
-        max_calibration_duration: int = None,
-    ):
-        """Calculate the latest time to align all measures in a program."""
-        super().__init__()
-        self.inst_map = inst_map
-        self.cal_gate = cal_gate
-        self.max_calibration_duration = max_calibration_duration
-
-    def run(
-        self,
-        program: pulse.Program,
-    ) -> pulse.Program:
-        """Return the the max between the duration of the calibration time and
-        the absolute time of the latest scheduled acquire."""
-        if self.max_calibration_duration is None:
-            align_time = self.get_max_calibration_duration()
-        else:
-            align_time = self.max_calibration_duration
-        for schedule in program.schedules:
-            last_acquire = 0
-            acquire_times = [time for time, inst in schedule.instructions
-                             if isinstance(inst, (Acquire, AcquireInstruction))]
-            if acquire_times:
-                last_acquire = max(acquire_times)
-            align_time = max(align_time, last_acquire)
-        self.analysis.meas_align_time = align_time
-        return program
-
-    def get_max_calibration_duration(self):
-        """Return the time needed to allow for readout discrimination calibration pulses."""
-        max_calibration_duration = 0
-        for qubits in self.inst_map.qubits_with_instruction(self.cal_gate):
-            cmd = self.inst_map.get(self.cal_gate, qubits, np.pi, 0, np.pi)
-            max_calibration_duration = max(cmd.duration, max_calibration_duration)
-        return max_calibration_duration
 
 
 class AlignMeasures(TransformationPass):
@@ -99,8 +54,13 @@ class AlignMeasures(TransformationPass):
         if align_time is not None and align_time < 0:
             raise CompilerError("Align time cannot be negative.")
         if align_time is None:
-            self.requires.append(AlignMeasuresAnalysis(
-                inst_map, cal_gate, max_calibration_duration=max_calibration_duration))
+            self.requires.append(
+                analysis.MeasurementAlignmentAnalysis(
+                    inst_map,
+                    cal_gate,
+                    max_calibration_duration=max_calibration_duration,
+                    ),
+                )
 
         self.align_time = align_time
 
