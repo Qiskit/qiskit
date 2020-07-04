18216f37ec7c29f9be4ac9ef26290929e67aa6c7
Toshinari Itoko <itoko@jp.ibm.com>
2020-07-04 17:05:25 +0900

    Add instruction_durations argument to schedule()

--- qiskit/compiler/schedule.py
+++ qiskit/compiler/schedule.py
@@ -25,6 +25,7 @@ from qiskit.circuit.quantumcircuit import QuantumCircuit
 from qiskit.exceptions import QiskitError
 from qiskit.pulse import InstructionScheduleMap, Schedule
 from qiskit.providers import BaseBackend
+from qiskit.transpiler.instruction_durations import InstructionDurations, InstructionDurationsType
 from qiskit.scheduler import schedule_circuit, ScheduleConfig
 
 LOG = logging.getLogger(__name__)
@@ -39,6 +40,7 @@ def schedule(circuits: Union[QuantumCircuit, List[QuantumCircuit]],
              backend: Optional[BaseBackend] = None,
              inst_map: Optional[InstructionScheduleMap] = None,
              meas_map: Optional[List[List[int]]] = None,
+             instruction_durations: Optional[InstructionDurationsType] = None,
              dt: Optional[float] = None,
              method: Optional[Union[str, List[str]]] = None) -> Union[Schedule, List[Schedule]]:
     """
@@ -52,6 +54,13 @@ def schedule(circuits: Union[QuantumCircuit, List[QuantumCircuit]],
                   ``backend``\'s ``instruction_schedule_map``
         meas_map: List of sets of qubits that must be measured together. If ``None``, defaults to
                   the ``backend``\'s ``meas_map``
+        instruction_durations: Durations of instructions. Duration must be the number of ``dt``s.
+            Note that ``dt`` depends on backend configuration. Instruction is specified by
+            a pair of basic instruction name and its acting physical qubits.
+            E.g. [('cx', [0, 1], 1000), ('u3', [0], 300)]
+            Durations defined in ``backend.properties`` are used as default and
+            they are overwritten with the instruction_durations.
+            Note: The durations must coincide with durations of schedules defined in inst_map.
         dt: For scheduled circuits which contain time information, dt is required. If not provided,
             it will be obtained from the backend configuration
         method: Optionally specify a particular scheduling method
@@ -72,12 +81,19 @@ def schedule(circuits: Union[QuantumCircuit, List[QuantumCircuit]],
         if backend is None:
             raise QiskitError("Must supply either a backend or a meas_map for scheduling passes.")
         meas_map = backend.configuration().meas_map
+    if instruction_durations is None:
+        if backend is None:
+            raise QiskitError("Must supply either a backend or a instruction_durations for "
+                              "scheduling passes.")
+        instruction_durations = InstructionDurations.from_backend(backend)
     if dt is None:
         if backend is not None:
             dt = backend.configuration().dt
 
     schedule_config = ScheduleConfig(inst_map=inst_map, meas_map=meas_map, dt=dt)
     circuits = circuits if isinstance(circuits, list) else [circuits]
+    for circ in circuits:
+        circ.instruction_durations = instruction_durations
     schedules = [schedule_circuit(circuit, schedule_config, method) for circuit in circuits]
     end_time = time()
     _log_schedule_time(start_time, end_time)
