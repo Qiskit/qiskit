697758c11e8b590f3e6431293e14ff33b7ccbd4e
Matthew Treinish <mtreinish@kortar.org>
2020-07-07 13:31:21 -0400

    Remove the interactive visualizations (#4438)
    * Remove the interactive visualizations
    
    The interactive visualization functions with each of the iplot functions
    will stop working in the near future. The hard coded url that these functions rely on (see #805) where the static compile js is hosted from is going to
    disapear in the near future. This will break these functions from
    working on released versions of qiskit, with no ability to fix it. This
    commit just deletes the functions because they won't work anymore once
    https://qvisualization.mybluemix.net goes away. So we can't even
    maintain compatbility with a deprecation window once that happens. Even
    if we did rehost the underlying js code somewhere else we can't rely on a
    static url like this in the future and the application is too large to
    self host in terra itself.
    
    * Add backwards compat shims to call mpl functions
    
    While the interactive visualization functions will not work anymore to
    ease the transition of anyone using them this commit adds back the
    functions removed in the previous commit and converts them to be shims
    that call the matplotlib based visualization functions. The functions
    are also deprecated, this should at least give users a method to
    transition off of the iplot functions.
    
    * Fix release note section typo
    
    * Return figures from matplotlib funcs
    
    * Fix lint failures
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- qiskit/visualization/interactive/iplot_histogram.py
+++ qiskit/visualization/interactive/iplot_histogram.py
@@ -15,56 +15,17 @@
 """
 Histogram visualization
 """
-from string import Template
-from collections import Counter
-import sys
-import time
-import re
-import numpy as np
-from ..exceptions import VisualizationError
 
-if ('ipykernel' in sys.modules) and ('spyder' not in sys.modules):
-    try:
-        from IPython.core.display import display, HTML
-    except ImportError:
-        print("Error importing IPython.core.display. To install, run "
-              '"pip install ipython".')
+import warnings
 
-
-def process_data(data, number_to_keep):
-    """ Prepare received data for representation.
-
-        Args:
-            data (dict): values to represent (ex. {'001' : 130})
-            number_to_keep (int): number of elements to show individually.
-
-        Returns:
-            dict: processed data to show.
-    """
-
-    result = dict()
-
-    if number_to_keep != 0:
-        data_temp = dict(Counter(data).most_common(number_to_keep))
-        data_temp['rest'] = sum(data.values()) - sum(data_temp.values())
-        data = data_temp
-
-    labels = data
-    values = np.array([data[key] for key in labels], dtype=float)
-    pvalues = values / sum(values)
-    for position, label in enumerate(labels):
-        result[label] = round(pvalues[position], 5)
-
-    return result
+from qiskit.visualization.counts_visualization import plot_histogram
 
 
 def iplot_histogram(data, figsize=None, number_to_keep=None,
                     sort='asc', legend=None):
     """ Create a histogram representation.
-
         Graphical representation of the input array using a vertical bars
         style graph.
-
         Args:
             data (list or dict):  This is either a list of dicts or a single
                 dict containing the values to represent (ex. {'001' : 130})
@@ -74,93 +35,31 @@ def iplot_histogram(data, figsize=None, number_to_keep=None,
             sort (string): Could be 'asc' or 'desc'
             legend (list): A list of strings to use for labels of the data.
                 The number of entries must match the length of data.
-
         Raises:
             VisualizationError: When legend is provided and the length doesn't
                 match the input data.
-
+        Returns:
+            Figure: A matplotlib figure for the visualization
         Example:
             .. code-block::
 
                 from qiskit import QuantumCircuit, BasicAer, execute
                 from qiskit.visualization import iplot_histogram
                 %matplotlib inline
-
                 qc = QuantumCircuit(2, 2)
                 qc.h(0)
                 qc.cx(0, 1)
                 qc.measure([0, 1], [0, 1])
-
                 backend = BasicAer.get_backend('qasm_simulator')
                 job = execute(qc, backend)
                 iplot_histogram(job.result().get_counts())
     """
-
-    # HTML
-    html_template = Template("""
-    <p>
-        <div id="histogram_$divNumber"></div>
-    </p>
-    """)
-
-    # JavaScript
-    javascript_template = Template("""
-    <script>
-        requirejs.config({
-            paths: {
-                qVisualization: "https://qvisualization.mybluemix.net/q-visualizations"
-            }
-        });
-
-        require(["qVisualization"], function(qVisualizations) {
-            qVisualizations.plotState("histogram_$divNumber",
-                                      "histogram",
-                                      $executions,
-                                      $options);
-        });
-    </script>
-    """)
-
-    # Process data and execute
-    div_number = str(time.time())
-    div_number = re.sub('[.]', '', div_number)
-
-    # set default figure size if none provided
-    if figsize is None:
-        figsize = (7, 5)
-
-    options = {'number_to_keep': 0 if number_to_keep is None else number_to_keep,
-               'sort': sort,
-               'show_legend': 0,
-               'width': int(figsize[0]),
-               'height': int(figsize[1])}
-    if legend:
-        options['show_legend'] = 1
-
-    data_to_plot = []
-    if isinstance(data, dict):
-        data = [data]
-
-    if legend and len(legend) != len(data):
-        raise VisualizationError("Length of legendL (%s) doesn't match number "
-                                 "of input executions: %s" %
-                                 (len(legend), len(data)))
-
-    for item, execution in enumerate(data):
-        exec_data = process_data(execution, options['number_to_keep'])
-        out_dict = {'data': exec_data}
-        if legend:
-            out_dict['name'] = legend[item]
-        data_to_plot.append(out_dict)
-
-    html = html_template.substitute({
-        'divNumber': div_number
-    })
-
-    javascript = javascript_template.substitute({
-        'divNumber': div_number,
-        'executions': data_to_plot,
-        'options': options
-    })
-
-    display(HTML(html + javascript))
+    warnings.warn(
+        "The iplot_histogram function is deprecated and will be "
+        "removed in a future release. The hosted code this depended on no "
+        "longer exists so this is falling back to use the matplotlib "
+        "visualizations. qiskit.visualization.plot_histogram should be "
+        "used directly moving forward.", DeprecationWarning, stacklevel=2)
+    fig = plot_histogram(data, figsize=figsize, number_to_keep=number_to_keep,
+                         sort=sort, legend=legend)
+    return fig
