bd151fa022c7605d41c63d606b518428fad95273
Thomas Alexander <talexander@ibm.com>
2020-07-09 16:51:20 -0400

    Fix bug in the builder measure_all macro. (#4674)
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- qiskit/pulse/macros.py
+++ qiskit/pulse/macros.py
@@ -75,7 +75,6 @@ def measure(qubits: List[int],
                 "Please provide another name using the 'measure_name' keyword "
                 "argument. For assistance, the instructions which are defined are: "
                 "{}".format(measure_name, inst_map.instructions))
-
         for time, inst in default_sched.instructions:
             if qubit_mem_slots and isinstance(
                     inst, (instructions.Acquire, commands.AcquireInstruction)):
