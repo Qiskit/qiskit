e5f99fd6ba59357ef661602c2d6a5040e5804eff
Naoki Kanazawa <39517270+nkanazawa1989@users.noreply.github.com>
2020-07-10 23:00:26 +0900

    Pulse drawer v2, drawing IRs (#4665)
    * add channel event
    
    * refactor event and add unittest
    
    * update unittest
    
    * add unittest for sample pulse
    
    * fix unittest
    
    * update docstring
    
    * update behavior of get_framechange
    
    * update docstring
    
    * add drawing objects
    
    * update docstring and remove sample pulse
    
    * update docstring
    
    * lint
    
    * lint
    
    * Update qiskit/visualization/pulse_v2/events.py
    
    Co-authored-by: Thomas Alexander <thomasalexander2718@gmail.com>
    
    * Update qiskit/visualization/pulse_v2/events.py
    
    Co-authored-by: Thomas Alexander <thomasalexander2718@gmail.com>
    
    * feedback from Thomas
    
    * feedback from Thomas
    
    * fix bug
    
    * add time sort for waveforms
    
    * update docstring
    
    * add unittests
    
    * update docstring
    
    * update docstring
    
    * update docstring
    
    * update docstring
    
    * update name of argument
    
    * update unittest
    
    * lint
    
    * Update qiskit/visualization/pulse_v2/drawing_objects.py
    
    Co-authored-by: Thomas Alexander <thomasalexander2718@gmail.com>
    
    * Update qiskit/visualization/pulse_v2/drawing_objects.py
    
    Co-authored-by: Thomas Alexander <thomasalexander2718@gmail.com>
    
    * Update qiskit/visualization/pulse_v2/drawing_objects.py
    
    Co-authored-by: Thomas Alexander <thomasalexander2718@gmail.com>
    
    * Update qiskit/visualization/pulse_v2/drawing_objects.py
    
    Co-authored-by: Thomas Alexander <thomasalexander2718@gmail.com>
    
    * Update qiskit/visualization/pulse_v2/drawing_objects.py
    
    Co-authored-by: Thomas Alexander <thomasalexander2718@gmail.com>
    
    * Update qiskit/visualization/pulse_v2/drawing_objects.py
    
    Co-authored-by: Thomas Alexander <thomasalexander2718@gmail.com>
    
    * Update qiskit/visualization/pulse_v2/drawing_objects.py
    
    Co-authored-by: Thomas Alexander <thomasalexander2718@gmail.com>
    
    * Update qiskit/visualization/pulse_v2/drawing_objects.py
    
    Co-authored-by: Thomas Alexander <thomasalexander2718@gmail.com>
    
    * feedback from Thomas
    
    * minor format change
    
    * add space before module docstring
    
    Co-authored-by: Thomas Alexander <thomasalexander2718@gmail.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- qiskit/visualization/pulse_v2/events.py
+++ qiskit/visualization/pulse_v2/events.py
@@ -11,6 +11,7 @@
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
+
 r"""
 Channel event manager for pulse schedules.
 
