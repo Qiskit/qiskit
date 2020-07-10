708c33e4db5e774ff7a6adcc902bfdc4d49880a7
Paul Nation <nonhermitian@gmail.com>
2020-07-10 17:16:14 -0400

    Trigger docs build for updated optimization tutorials (Qiskit/qiskit-metapackage#961)
    * worthless change
    
    * add optim images

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,6 @@ sys.path.insert(0, os.path.abspath('.'))
 
 import sphinx_rtd_theme
 
-
 # -- Project information -----------------------------------------------------
 from distutils import dir_util
 import os
@@ -77,6 +76,19 @@ nbsphinx_execute = 'never'
 nbsphinx_widgets_path = ''
 html_sourcelink_suffix = ''
 exclude_patterns = ['_build', '**.ipynb_checkpoints']
+
+nbsphinx_thumbnails = {
+    'tutorials/optimization/1_quadratic_program': 
+    '_static/optimization/1_quadratic_program.png',
+    'tutorials/optimization/2_converters_for_quadratic_programs': 
+    '_static/optimization/2_converters.png',
+    'tutorials/optimization/3_minimum_eigen_optimizer': 
+    '_static/optimization/3_min_eig_opt.png',
+    'tutorials/optimization/4_grover_optimizer': 
+    '_static/optimization/4_grover.png',
+    'tutorials/optimization/5_admm_optimizer': 
+    '_static/optimization/5_ADMM.png',
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['theme/']
