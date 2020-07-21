c1a639cb8a123410c6a08d4538fa7b8d605ac8de
Thomas Alexander <talexander@ibm.com>
2020-07-20 23:10:31 -0400

    Fix broadcast acquire config (#4654)
    * Properly handle multiple kernels and discriminators in acquires.
    
    * Remove dead function.
    
    * Added a reno.
    
    * Fix reno
    
    * Fix instruction bundle code.
    
    * Change assemble errors to QiskitError.
    
    * Fix error message.
    
    * Fix double error call in assemble.
    
    * Split up discriminator/kernel unit tests.
    
    * Add more informative message.
    
    * Uglified my indentation.
    
    * Removed bundled from acquire instruction comment.
    
    * linting.
    
    * Fix bad merge
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- test/python/compiler/test_assembler.py
+++ test/python/compiler/test_assembler.py
@@ -27,6 +27,7 @@ from qiskit.compiler.assemble import assemble
 from qiskit.exceptions import QiskitError
 from qiskit.pulse import Schedule, Acquire, Play
 from qiskit.pulse.channels import MemorySlot, AcquireChannel, DriveChannel, MeasureChannel
+from qiskit.pulse.configuration import Kernel, Discriminator
 from qiskit.pulse.library import gaussian
 from qiskit.qobj import QasmQobj, validate_qobj_against_schema
 from qiskit.qobj.utils import MeasLevel, MeasReturnType
@@ -768,6 +769,138 @@ class TestPulseAssembler(QiskitTestCase):
         qobj = assemble(self.schedule, self.backend, **self.config)
         self.assertEqual(qobj.config.rep_time, int(rep_times[0]*1e6))
         self.assertEqual(qobj.config.rep_delay, rep_delay*1e6)
+
+    def test_assemble_with_individual_discriminators(self):
+        """Test that assembly works with individual discriminators."""
+        disc_one = Discriminator('disc_one', test_params=True)
+        disc_two = Discriminator('disc_two', test_params=False)
+
+        schedule = Schedule()
+        schedule = schedule.append(
+            Acquire(5, AcquireChannel(0), MemorySlot(0), discriminator=disc_one),
+        )
+        schedule = schedule.append(
+            Acquire(5, AcquireChannel(1), MemorySlot(1), discriminator=disc_two),
+        )
+
+        qobj = assemble(schedule,
+                        qubit_lo_freq=self.default_qubit_lo_freq,
+                        meas_lo_freq=self.default_meas_lo_freq,
+                        meas_map=[[0, 1]])
+        validate_qobj_against_schema(qobj)
+
+        qobj_discriminators = qobj.experiments[0].instructions[0].discriminators
+        self.assertEqual(len(qobj_discriminators), 2)
+        self.assertEqual(qobj_discriminators[0].name, 'disc_one')
+        self.assertEqual(qobj_discriminators[0].params['test_params'], True)
+        self.assertEqual(qobj_discriminators[1].name, 'disc_two')
+        self.assertEqual(qobj_discriminators[1].params['test_params'], False)
+
+    def test_assemble_with_single_discriminators(self):
+        """Test that assembly works with both a single discriminator."""
+        disc_one = Discriminator('disc_one', test_params=True)
+
+        schedule = Schedule()
+        schedule = schedule.append(
+            Acquire(5, AcquireChannel(0), MemorySlot(0), discriminator=disc_one),
+        )
+        schedule = schedule.append(
+            Acquire(5, AcquireChannel(1), MemorySlot(1)),
+        )
+
+        qobj = assemble(schedule,
+                        qubit_lo_freq=self.default_qubit_lo_freq,
+                        meas_lo_freq=self.default_meas_lo_freq,
+                        meas_map=[[0, 1]])
+        validate_qobj_against_schema(qobj)
+
+        qobj_discriminators = qobj.experiments[0].instructions[0].discriminators
+        self.assertEqual(len(qobj_discriminators), 1)
+        self.assertEqual(qobj_discriminators[0].name, 'disc_one')
+        self.assertEqual(qobj_discriminators[0].params['test_params'], True)
+
+    def test_assemble_with_unequal_discriminators(self):
+        """Test that assembly works with incorrect number of discriminators for
+        number of qubits."""
+        disc_one = Discriminator('disc_one', test_params=True)
+        disc_two = Discriminator('disc_two', test_params=False)
+
+        schedule = Schedule()
+        schedule += Acquire(5, AcquireChannel(0), MemorySlot(0), discriminator=disc_one)
+        schedule += Acquire(5, AcquireChannel(1), MemorySlot(1), discriminator=disc_two)
+        schedule += Acquire(5, AcquireChannel(2), MemorySlot(2))
+
+        with self.assertRaises(QiskitError):
+            assemble(schedule,
+                     qubit_lo_freq=self.default_qubit_lo_freq,
+                     meas_lo_freq=self.default_meas_lo_freq,
+                     meas_map=[[0, 1, 2]])
+
+    def test_assemble_with_individual_kernels(self):
+        """Test that assembly works with individual kernels."""
+        disc_one = Kernel('disc_one', test_params=True)
+        disc_two = Kernel('disc_two', test_params=False)
+
+        schedule = Schedule()
+        schedule = schedule.append(
+            Acquire(5, AcquireChannel(0), MemorySlot(0), kernel=disc_one),
+        )
+        schedule = schedule.append(
+            Acquire(5, AcquireChannel(1), MemorySlot(1), kernel=disc_two),
+        )
+
+        qobj = assemble(schedule,
+                        qubit_lo_freq=self.default_qubit_lo_freq,
+                        meas_lo_freq=self.default_meas_lo_freq,
+                        meas_map=[[0, 1]])
+        validate_qobj_against_schema(qobj)
+
+        qobj_kernels = qobj.experiments[0].instructions[0].kernels
+        self.assertEqual(len(qobj_kernels), 2)
+        self.assertEqual(qobj_kernels[0].name, 'disc_one')
+        self.assertEqual(qobj_kernels[0].params['test_params'], True)
+        self.assertEqual(qobj_kernels[1].name, 'disc_two')
+        self.assertEqual(qobj_kernels[1].params['test_params'], False)
+
+    def test_assemble_with_single_kernels(self):
+        """Test that assembly works with both a single kernel."""
+        disc_one = Kernel('disc_one', test_params=True)
+
+        schedule = Schedule()
+        schedule = schedule.append(
+            Acquire(5, AcquireChannel(0), MemorySlot(0), kernel=disc_one),
+        )
+        schedule = schedule.append(
+            Acquire(5, AcquireChannel(1), MemorySlot(1)),
+        )
+
+        qobj = assemble(schedule,
+                        qubit_lo_freq=self.default_qubit_lo_freq,
+                        meas_lo_freq=self.default_meas_lo_freq,
+                        meas_map=[[0, 1]])
+        validate_qobj_against_schema(qobj)
+
+        qobj_kernels = qobj.experiments[0].instructions[0].kernels
+        self.assertEqual(len(qobj_kernels), 1)
+        self.assertEqual(qobj_kernels[0].name, 'disc_one')
+        self.assertEqual(qobj_kernels[0].params['test_params'], True)
+
+    def test_assemble_with_unequal_kernels(self):
+        """Test that assembly works with incorrect number of discriminators for
+        number of qubits."""
+        disc_one = Kernel('disc_one', test_params=True)
+        disc_two = Kernel('disc_two', test_params=False)
+
+        schedule = Schedule()
+        schedule += Acquire(5, AcquireChannel(0), MemorySlot(0), kernel=disc_one)
+        schedule += Acquire(5, AcquireChannel(1), MemorySlot(1), kernel=disc_two)
+        schedule += Acquire(5, AcquireChannel(2), MemorySlot(2))
+
+        with self.assertRaises(QiskitError):
+            assemble(schedule,
+                     qubit_lo_freq=self.default_qubit_lo_freq,
+                     meas_lo_freq=self.default_meas_lo_freq,
+                     meas_map=[[0, 1, 2]])
 
 
 class TestPulseAssemblerMissingKwargs(QiskitTestCase):
