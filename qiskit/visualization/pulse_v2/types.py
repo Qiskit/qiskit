2129555f037ebb59a89369408ddcf7581cb63236
knzwnao <knzwnao@jp.ibm.com>
2020-07-23 12:29:04 +0900

    conform to py3.5 syntax

--- qiskit/visualization/pulse_v2/types.py
+++ qiskit/visualization/pulse_v2/types.py
@@ -16,26 +16,17 @@
 
 """
 Special data types.
-"""
-
-from typing import NamedTuple, Union, List, Optional
-
-from qiskit import pulse
 
 
-class PhaseFreqTuple(NamedTuple):
-    """Data set to represent a pair of floating values associated with a
+- PhaseFreqTuple:
+    Data set to represent a pair of floating values associated with a
     phase and frequency of the frame of channel.
 
     phase: Floating value associated with phase.
     freq: Floating value associated with frequency.
-    """
-    phase: float
-    freq: float
 
-
-class InstructionTuple(NamedTuple):
-    """Type for representing instruction objects to draw.
+- InstructionTuple:
+    Type for representing instruction objects to draw.
 
     Waveform and Frame type instructions are internally converted into this data format and
     fed to the object generators.
@@ -44,15 +35,9 @@ class InstructionTuple(NamedTuple):
     dt: Time resolution of this system.
     frame: `PhaseFreqTuple` object to represent the frame of this instruction.
     inst: Pulse instruction object.
-    """
-    t0: int
-    dt: Optional[float]
-    frame: PhaseFreqTuple
-    inst: Union[pulse.Instruction, List[pulse.Instruction]]
 
-
-class NonPulseTuple(NamedTuple):
-    """Data set to generate drawing objects.
+- NonPulseTuple:
+    Data set to generate drawing objects.
 
     Special instructions such as snapshot and relative barriers are internally converted
     into this data format and fed to the object generators.
@@ -60,30 +45,59 @@ class NonPulseTuple(NamedTuple):
     t0: The time when this instruction is issued. This value is in units of cycle time dt.
     dt: Time resolution of this system.
     inst: Pulse instruction object.
-    """
-    t0: int
-    dt: float
-    inst: Union[pulse.Instruction, List[pulse.Instruction]]
 
-
-class ChannelTuple(NamedTuple):
-    """Data set to generate drawing objects.
+- ChannelTuple:
+    Data set to generate drawing objects.
 
     Channel information is internally represented in this data format.
 
     channel: Pulse channel object.
     scaling: Vertical scaling factor of the channel.
-    """
-    channel: pulse.channels.Channel
-    scaling: float
 
-
-class ComplexColors(NamedTuple):
-    """Data set to represent a pair of color code associated with a real and
+- ComplexColors:
+    Data set to represent a pair of color code associated with a real and
     an imaginary part of filled waveform colors.
 
     real: Color code for the real part of waveform.
     imaginary: Color code for the imaginary part of waveform.
-    """
-    real: str
-    imaginary: str
+"""
+
+from typing import NamedTuple, Union, List, Optional
+
+from qiskit import pulse
+
+
+# TODO: replace with dataclass when py3.5 support is removed.
+
+
+PhaseFreqTuple = NamedTuple(
+    'PhaseFreqTuple',
+    [('phase', float),
+     ('freq', float)])
+
+
+InstructionTuple = NamedTuple(
+    'InstructionTuple',
+    [('t0', int),
+     ('dt', Optional[float]),
+     ('frame', PhaseFreqTuple),
+     ('inst', Union[pulse.Instruction, List[pulse.Instruction]])])
+
+
+NonPulseTuple = NamedTuple(
+    'NonPulseTuple',
+    [('t0', int),
+     ('dt', Optional[float]),
+     ('inst', Union[pulse.Instruction, List[pulse.Instruction]])])
+
+
+ChannelTuple = NamedTuple(
+    'ChannelTuple',
+    [('channel', pulse.channels.Channel),
+     ('scaling', float)])
+
+
+ComplexColors = NamedTuple(
+    'ComplexColors',
+    [('real', str),
+     ('imaginary', str)])
