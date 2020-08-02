a12ee88b03befb58f7e62f225114fb6a4aec2d23
romain <rmoyard@gmail.com>
2020-08-02 23:46:37 +0200

    Linting.

--- qiskit/transpiler/passes/optimization/template_optimization.py
+++ qiskit/transpiler/passes/optimization/template_optimization.py
@@ -51,16 +51,18 @@ class TemplateOptimization(TransformationPass):
             template_list (list[QuantumCircuit()]): list of the different template circuit to apply.
             heuristics_backward_param (list[int]): [length, survivor] Those are the parameters for
             applying heuristics on the backward part of the algorithm. This part of the algorithm
-            creates a tree of matching scenario. This tree grows exponentially. The heuristics evaluates
-            which scenarios have the longest match and keep only those. The length is the
-            interval in the tree for cutting it and surviror is the number of scenarios that are kept. We advice
-            to use l=3 and s=1 to have serious time advantage. We remind that the heuristics implies losing
-            a part of the maximal matches. Check reference for more details.
-            heuristics_qubits_param (list[int]): [length] The heuristics for the qubit choice make guesses from
-            the dag dependency of the circuit in order to limit the number of qubit configurations to explore.
-            The length is the number of successors or not predecessors that will be explored in the dag dependency
-            of the circuit, each qubits of the nodes are added to the set of authorized qubits. We advice to use
-            length=1. Check reference for more details.
+            creates a tree of matching scenario. This tree grows exponentially. The heuristics
+            evaluates which scenarios have the longest match and keep only those. The length is the
+            interval in the tree for cutting it and surviror is the number of scenarios that are
+            kept. We advice to use l=3 and s=1 to have serious time advantage. We remind that the
+            heuristics implies losing a part of the maximal matches. Check reference for more
+            details.
+            heuristics_qubits_param (list[int]): [length] The heuristics for the qubit choice make
+            guesses from the dag dependency of the circuit in order to limit the number of qubit
+            configurations to explore. The length is the number of successors or not predecessors
+            that will be explored in the dag dependency of the circuit, each qubits of the nodes
+            are added to the set of authorized qubits. We advice to use length=1. Check reference
+            for more details.
         """
         super().__init__()
         # If no template is given; the template are set as x-x, cx-cx, ccx-ccx.
@@ -98,7 +100,8 @@ class TemplateOptimization(TransformationPass):
                 raise TranspilerError('A template is a Quantumciruit() that performs the identity.')
 
             if template.num_qubits > len(circuit_dag_dep.qubits):
-                continue
+                raise TranspilerError('A template should have equal or less number of qubit'
+                                      ' compared to the circuit.')
 
             template_dag_dep = circuit_to_dagdependency(template)
 
