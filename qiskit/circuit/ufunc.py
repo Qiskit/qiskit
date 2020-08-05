2f245df1b52ece8e357a58b2a070e97437b0b384
georgios-ts <ts.giorgos@yahoo.gr>
2020-08-05 20:22:46 +0300

    missing docs and header added

--- qiskit/circuit/ufunc.py
+++ qiskit/circuit/ufunc.py
@@ -1,3 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2020.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
 """
 A collection of universal functions applied to Parameters.
 """
@@ -20,43 +34,51 @@ def _call(ufunc, args) -> ParameterExpression:
 
 
 def sin(x):
+    """Sine of a ParameterExpression"""
     from sympy import sin as _sin
     return _call(_sin, x)
 
 
 def cos(x):
+    """Cosine of a ParameterExpression"""
     from sympy import cos as _cos
     return _call(_cos, x)
 
 
 def tan(x):
+    """Tangent of a ParameterExpression"""
     from sympy import tan as _tan
     return _call(_tan, x)
 
 
 def asin(x):
+    """Arcsin of a ParameterExpression"""
     from sympy import asin as _asin
     return _call(_asin, x)
 
 
 def acos(x):
+    """Arccos of a ParameterExpression"""
     from sympy import acos as _acos
     return _call(_acos, x)
 
 
 def atan(x):
+    """Arctan of a ParameterExpression"""
     from sympy import atan as _atan
     return _call(_atan, x)
 
 
 # proper exponential
 def exp(x):
+    """Exponential of a ParameterExpression"""
     from sympy import exp as _exp
     return _call(_exp, x)
 
 
 # complex exponential
 def cexp(x):
+    """Complex exponential of a ParameterExpression"""
     from sympy import I
     from sympy import exp as _exp
     return _call(
@@ -65,5 +87,6 @@ def cexp(x):
 
 
 def log(x):
+    """Logarithm of a ParameterExpression"""
     from sympy import log as _log
     return _call(_log, x)
