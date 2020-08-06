8159b02f87afb773a15a422a1dc8b04d91ce9439
Siddharth <dangwal.sid6@gmail.com>
2020-08-06 13:07:44 +0530

    added a message to notify when shots isn't of type int

--- qiskit/compiler/assemble.py
+++ qiskit/compiler/assemble.py
@@ -220,10 +220,12 @@ def _parse_common_args(backend, qobj_id, qobj_header, shots,
             shots = min(1024, max_shots)
         else:
             shots = 1024
-    elif max_shots and max_shots < shots:
+    elif isinstance(shots, int) and max_shots and max_shots < shots:
         raise QiskitError(
             'Number of shots specified: %s exceeds max_shots property of the '
             'backend: %s.' % (shots, max_shots))
+    elif not isinstance(shots, int):
+        raise TypeError('shots must be of type int')
 
     # create run configuration and populate
     run_config_dict = dict(shots=shots,
