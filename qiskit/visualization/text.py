1ea56d412a5140c160092ef0046dfdba95145286
Luciano Bello <luciano.bello@ibm.com>
2020-08-10 15:39:38 -0400

    decide encoding at init time

--- qiskit/visualization/text.py
+++ qiskit/visualization/text.py
@@ -519,13 +519,12 @@ class TextDrawing():
 
     def __init__(self, qregs, cregs, instructions, plotbarriers=True,
                  line_length=None, vertical_compression='high', layout=None, initial_state=True,
-                 cregbundle=False, global_phase=None):
+                 cregbundle=False, global_phase=None, encoding=None):
         self.qregs = qregs
         self.cregs = cregs
         self.instructions = instructions
         self.layout = layout
         self.initial_state = initial_state
-
         self.cregbundle = cregbundle
         self.global_phase = global_phase
         self.plotbarriers = plotbarriers
@@ -533,6 +532,7 @@ class TextDrawing():
         if vertical_compression not in ['high', 'medium', 'low']:
             raise ValueError("Vertical compression can only be 'high', 'medium', or 'low'")
         self.vertical_compression = vertical_compression
+        self.encoding = encoding if encoding else sys.stdout.encoding
 
     def __str__(self):
         return self.single_string()
@@ -554,8 +554,7 @@ class TextDrawing():
         Returns:
             str: The lines joined by a newline (``\\n``)
         """
-        import sys
-        return "\n".join(self.lines()).encode().decode(sys.stdout.encoding, errors='ignore')
+        return "\n".join(self.lines()).encode().decode(self.encoding, errors='ignore')
 
     def dump(self, filename, encoding="utf8"):
         """Dumps the ascii art in the file.
