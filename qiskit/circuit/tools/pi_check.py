6d45aed43ad4c9f65b7b1b4da26fc42b2bda43ea
Edwin Navarro <enavarro@comcast.net>
2020-08-11 11:16:48 -0700

    Add powers and remove unused code

--- qiskit/circuit/tools/pi_check.py
+++ qiskit/circuit/tools/pi_check.py
@@ -21,8 +21,10 @@ import numpy as np
 from qiskit.circuit.parameterexpression import ParameterExpression
 from qiskit.exceptions import QiskitError
 
-N, D = np.meshgrid(np.arange(1, 17), np.arange(1, 17))
+MAX_FRAC = 16
+N, D = np.meshgrid(np.arange(1, MAX_FRAC+1), np.arange(1, MAX_FRAC+1))
 FRAC_MESH = N / D * np.pi
+POW_LIST = np.pi ** np.arange(2, 5)
 
 
 def pi_check(inpt, eps=1e-6, output='text', ndigits=5):
@@ -63,7 +65,7 @@ def pi_check(inpt, eps=1e-6, output='text', ndigits=5):
     def normalize(single_inpt):
         if abs(single_inpt) < 1e-14:
             return '0'
-        val = single_inpt / np.pi
+
         if output in ['text', 'qasm']:
             pi = 'pi'
         elif output == 'latex':
@@ -73,81 +75,82 @@ def pi_check(inpt, eps=1e-6, output='text', ndigits=5):
         else:
             raise QiskitError('pi_check parameter output should be text, '
                               'latex, mpl, or qasm.')
+
+        negstr = '-' if single_inpt < 0 else ''
+
+        # First check is for whole multiples of pi
+        val = single_inpt / np.pi
         if abs(val) >= 1 - eps:
             if abs(abs(val) - abs(round(val))) < eps:
-                val = int(round(val))
-                if val == 1:
-                    str_out = '{}'.format(pi)
-                elif val == -1:
-                    str_out = '-{}'.format(pi)
+                val = int(abs(round(val)))
+                if abs(val) == 1:
+                    str_out = '{}{}'.format(negstr, pi)
                 else:
                     if output == 'qasm':
-                        str_out = '{}*{}'.format(val, pi)
+                        str_out = '{}{}*{}'.format(negstr, val, pi)
                     else:
-                        str_out = '{}{}'.format(val, pi)
+                        str_out = '{}{}{}'.format(negstr, val, pi)
                 return str_out
 
-        val = np.pi / single_inpt
-        if abs(abs(val) - abs(round(val))) < eps:
-            val = int(round(val))
-            if val > 0:
-                if output == 'latex':
-                    str_out = '\\frac{%s}{%s}' % (pi, abs(val))
+        # Second is a check for powers of pi
+        if abs(single_inpt) > np.pi:
+            power = np.where(abs(abs(single_inpt) - POW_LIST) < eps)
+            if power[0].shape[0]:
+                if output == 'qasm':
+                    str_out = '{:.{}g}'.format(single_inpt, ndigits)
+                elif output == 'latex':
+                    str_out = '{}{}^{}'.format(negstr, pi, power[0][0] + 2)
+                elif output == 'mpl':
+                    str_out = '{}{}$^{}$'.format(negstr, pi, power[0][0] + 2)
                 else:
-                    str_out = '{}/{}'.format(pi, val)
-            else:
-                if output == 'latex':
-                    str_out = '\\frac{-%s}{%s}' % (pi, abs(val))
-                else:
-                    str_out = '-{}/{}'.format(pi, abs(val))
+                    str_out = '{}{}**{}'.format(negstr, pi, power[0][0] + 2)
+                return str_out
+
+        # Third is a check for a number larger than MAX_FRAC, since no
+        # fractions will exceed that value
+        if abs(single_inpt) >= MAX_FRAC:
+            str_out = '{:.{}g}'.format(single_inpt, ndigits)
             return str_out
 
-        # Look for all fracs in 8
-        abs_val = abs(single_inpt)
-        frac = np.where(np.abs(abs_val - FRAC_MESH) < 1e-8)
+        # Fourth check is for fractions for 1*pi in the numer and any
+        # number in the denom.
+        val = np.pi / single_inpt
+        if abs(abs(val) - abs(round(val))) < eps:
+            val = int(abs(round(val)))
+            if output == 'latex':
+                str_out = '\\frac{%s%s}{%s}' % (negstr, pi, val)
+            else:
+                str_out = '{}{}/{}'.format(negstr, pi, val)
+            return str_out
+
+        # Finally check for fractions where the numer > 1*pi and numer
+        # is up to MAX_FRAC*pi and denom is up to MAX_FRAC and all
+        # fractions are reduced. Ex. 15pi/16, 2pi/5, 15pi/2, 8pi/3, 16pi/9.
+        frac = np.where(np.abs(abs(single_inpt) - FRAC_MESH) < eps)
         if frac[0].shape[0]:
             numer = int(frac[1][0]) + 1
             denom = int(frac[0][0]) + 1
-            if single_inpt < 0:
-                numer *= -1
-
-            if numer == 1 and denom == 1:
-                str_out = '{}'.format(pi)
-            elif numer == -1 and denom == 1:
-                str_out = '-{}'.format(pi)
-            elif numer == 1:
-                if output == 'latex':
-                    str_out = '\\frac{%s}{%s}' % (pi, denom)
-                else:
-                    str_out = '{}/{}'.format(pi, denom)
-            elif numer == -1:
-                if output == 'latex':
-                    str_out = '\\frac{-%s}{%s}' % (pi, denom)
-                else:
-                    str_out = '-{}/{}'.format(pi, denom)
-            elif denom == 1:
-                if output == 'latex':
-                    str_out = '\\frac{%s}{%s}' % (numer, pi)
-                else:
-                    str_out = '{}/{}'.format(numer, pi)
+            if output == 'latex':
+                str_out = '\\frac{%s%s%s}{%s}' % (negstr, numer, pi, denom)
+            elif output == 'qasm':
+                str_out = '{}{}*{}/{}'.format(negstr, numer, pi, denom)
             else:
-                if output == 'latex':
-                    str_out = '\\frac{%s%s}{%s}' % (numer, pi, denom)
-                elif output == 'qasm':
-                    str_out = '{}*{}/{}'.format(numer, pi, denom)
-                else:
-                    str_out = '{}{}/{}'.format(numer, pi, denom)
-
+                str_out = '{}{}{}/{}'.format(negstr, numer, pi, denom)
             return str_out
-        # nothing found
-        str_out = '%.{}g'.format(ndigits) % single_inpt
+
+        # Nothing found
+        str_out = '{:.{}g}'.format(single_inpt, ndigits)
         return str_out
 
     complex_inpt = complex(inpt)
     real, imag = map(normalize, [complex_inpt.real, complex_inpt.imag])
 
+    posstr = '+' if complex_inpt.imag >= 0 else ''
+    if output == 'mpl':
+        real = real.replace('-', '$-$')
+        imag = imag.replace('-', '$-$')
     if real == '0' and imag != '0':
         return imag + 'j'
     elif real != 0 and imag != '0':
-        return '{}+{}j'.format(real, imag)
+        return '{}{}{}j'.format(real, posstr, imag)
     return real
--- test/python/circuit/test_tools.py
+++ test/python/circuit/test_tools.py
@@ -19,6 +19,7 @@ from test import combine
 from ddt import ddt
 from qiskit.test import QiskitTestCase
 from qiskit.circuit.tools.pi_check import pi_check
+from numpy import pi
 
 
 @ddt
@@ -31,7 +32,31 @@ class TestPiCheck(QiskitTestCase):
                    (2.99, '2.99'),
                    (2.999999999999999, '3'),
                    (0.99, '0.99'),
-                   (0.999999999999999, '1')])
+                   (0.999999999999999, '1'),
+                   (pi, 'pi'),
+                   (-pi, '-pi'),
+                   (3*pi, '3pi'),
+                   (-3*pi, '-3pi'),
+                   (pi/35, 'pi/35'),
+                   (-pi/35, '-pi/35'),
+                   (3*pi/35, '0.26928'),
+                   (-3*pi/35, '-0.26928'),
+                   (pi**2, 'pi**2'),
+                   (-pi**2, '-pi**2'),
+                   (1e9, '1e+09'),
+                   (-1e9, '-1e+09'),
+                   (1e-9, '1e-09'),
+                   (-1e-9, '-1e-09'),
+                   (6*pi/11, '6pi/11'),
+                   (-6*pi/11, '-6pi/11'),
+                   (6*pi/1, '6pi'),
+                   (-6*pi/1, '-6pi'), 
+                   (6*pi/2, '3pi'),
+                   (-6*pi/2, '-3pi'),
+                   (6*pi/5+1j*3*pi/7, '6pi/5+3pi/7j'),
+                   (-6*pi/5+1j*3*pi/7, '-6pi/5+3pi/7j'),
+                   (6*pi/5-1j*3*pi/7, '6pi/5-3pi/7j'),
+                   (-6*pi/5-1j*3*pi/7, '-6pi/5-3pi/7j')])
     def test_default(self, case):
         """Default pi_check({case[0]})='{case[1]}'"""
         input_number = case[0]
