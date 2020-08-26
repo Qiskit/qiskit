22b8313004afa88c908e34a7aee17add1fd762a9
Luciano Bello <luciano.bello@ibm.com>
2020-08-26 18:32:37 -0400

    encoding to files

--- qiskit/visualization/text.py
+++ qiskit/visualization/text.py
@@ -565,14 +565,14 @@ class TextDrawing():
             raise TextDrawerEncodingError('The encoding %s has a limited charset. Consider a '
                                           'different encoding in your environment.' % self.encoding)
 
-    def dump(self, filename, encoding="utf8"):
+    def dump(self, filename, encoding=None):
         """Dumps the ascii art in the file.
 
         Args:
             filename (str): File to dump the ascii art.
-            encoding (str): Optional. Default "utf-8".
+            encoding (str): Optional. Force encoding, instead of self.encoding.
         """
-        with open(filename, mode='w', encoding=encoding) as text_file:
+        with open(filename, mode='w', encoding=encoding or self.encoding) as text_file:
             text_file.write(self.single_string())
 
     def lines(self, line_length=None):
