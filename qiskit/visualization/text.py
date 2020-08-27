798550097decb8487dc1c7e0a5e7649e16b43ad3
Luciano Bello <luciano.bello@ibm.com>
2020-08-27 13:48:46 -0400

    many chars have a valid (but wrong) representation on cp1252. So first encode, then decode

--- qiskit/visualization/text.py
+++ qiskit/visualization/text.py
@@ -553,18 +553,16 @@ class TextDrawing():
 
     def single_string(self):
         """Creates a long string with the ascii art.
-
         Returns:
             str: The lines joined by a newline (``\\n``)
         """
         try:
-            ret = "\n".join(self.lines()).encode().decode(self.encoding)
-        except UnicodeDecodeError:
+            return "\n".join(self.lines()).encode(self.encoding).decode(self.encoding)
+        except (UnicodeEncodeError, UnicodeDecodeError):
             warn('The encoding %s has a limited charset. Consider a different encoding in your '
                  'environment. UTF-8 is being used instead' % self.encoding, RuntimeWarning)
-            self.encoding = 'utf-8'
-            ret = "\n".join(self.lines()).encode().decode(self.encoding, errors='ignore')
-        return ret
+            self.encoding='utf-8'
+            return "\n".join(self.lines()).encode(self.encoding).decode(self.encoding)
 
     def dump(self, filename, encoding=None):
         """Dumps the ascii art in the file.
