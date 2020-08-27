27425acb90627a5a2db554029919ee7fe0c43abd
Luciano Bello <luciano.bello@ibm.com>
2020-08-27 15:40:17 -0400

    lint

--- qiskit/visualization/text.py
+++ qiskit/visualization/text.py
@@ -561,7 +561,7 @@ class TextDrawing():
         except (UnicodeEncodeError, UnicodeDecodeError):
             warn('The encoding %s has a limited charset. Consider a different encoding in your '
                  'environment. UTF-8 is being used instead' % self.encoding, RuntimeWarning)
-            self.encoding='utf-8'
+            self.encoding = 'utf-8'
             return "\n".join(self.lines()).encode(self.encoding).decode(self.encoding)
 
     def dump(self, filename, encoding=None):
