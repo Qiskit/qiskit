e0afd02e7d4095375cf89f6418a085fcb471d427
Lauren Capelluto <laurencapelluto@gmail.com>
2020-08-31 11:12:39 -0400

    Update qiskit/scheduler/lowering.py
    Co-authored-by: Thomas Alexander <thomasalexander2718@gmail.com>

--- qiskit/scheduler/lowering.py
+++ qiskit/scheduler/lowering.py
@@ -64,7 +64,7 @@ def lower_gates(circuit: QuantumCircuit, schedule_config: ScheduleConfig) -> Lis
         # Exclude acquisition on these qubits, since they are handled by the user calibrations
         acquire_excludes = {}
         if Measure().name in circuit.calibrations.keys():
-            qubits = tuple(sorted([*qubit_mem_slots]))
+            qubits = tuple(sorted(qubit_mem_slots.keys()))
             params = ()
             for qubit in qubits:
                 try:
