0976359f7d80c6a6baa2bf287d92e76f0ddef50f
Cryoris <jules.gacon@googlemail.com>
2020-09-14 12:54:19 +0200

    make normal and lognormal support multivariate

--- qiskit/circuit/library/probability_distributions/lognormal.py
+++ qiskit/circuit/library/probability_distributions/lognormal.py
@@ -12,8 +12,9 @@
 
 """The log-normal probability distribution circuit."""
 
-import numpy
-import scipy
+from typing import Tuple
+import numpy as np
+from scipy.stats import multivariate_normal
 from qiskit.circuit import QuantumCircuit
 
 
@@ -28,19 +29,51 @@ class LogNormalDistribution(QuantumCircuit):
 
     """
 
-    def __init__(self, num_qubits: int, mu: float = 0, sigma: float = 1, 
-                 bounds: Tuple[float, float] = (0, 1), name: str = 'P(X)') -> None:
-        """
+    def __init__(self, num_qubits: int, mu: float = 0, sigma: float = 1,
+                 bounds: Tuple[float, float] = None, name: str = 'P(X)') -> None:
+        r"""
         Args:
-            num_qubits: The number of qubits in the circuit, the distribution is uniform over 
-                ``2 ** num_qubits`` values.
+            num_qubits: The number of qubits in the circuit.
+            mu: The parameter :math:`\mu`.
+            sigma: The parameter :math:`\sigma`.
+            bounds: The truncation bounds of the distribution.
+            name: The name of the circuit.
         """
-        super().__init__(num_qubits, name=name)
-        
-        # compute the normalized, truncated probabilities 
-        x = numpy.linspace(bounds[0], bounds[1], num=2**num_qubits)
-        probabilities = [scipy.stats.lognorm(x_i, s=sigma, scale=numpy.exp(mu)) for x_i in x]
-        normalized_probabilites = probabilities / numpy.sum(probabilities)
+        if not isinstance(num_qubits, list):  # univariate case
+            super().__init__(num_qubits, name=name)
+
+            if bounds is None:
+                bounds = (0, 1)
+
+            x = np.linspace(bounds[0], bounds[1], num=2**num_qubits)
+
+        else:  # multivariate case
+            super().__init__(sum(num_qubits), name=name)
+
+            if bounds is None:
+                bounds = [(0, 1)] * len(num_qubits)
+
+            # compute the evaluation points using numpy's meshgrid
+            # indexing 'ij' yields the "column-based" indexing
+            meshgrid = np.meshgrid(*[np.linspace(bound[0], bound[1], num=2**num_qubits[i])
+                                     for i, bound in enumerate(bounds)], indexing='ij')
+            # flatten into a list of points
+            x = list(zip(*[grid.flatten() for grid in meshgrid]))
+
+        # compute the normalized, truncated probabilities
+        probabilities = []
+        for x_i in x:
+            # pylint: disable=line-too-long
+            # map probabilities from normal to log-normal reference:
+            # https://stats.stackexchange.com/questions/214997/multivariate-log-normal-probabiltiy-density-function-pdf
+            if np.min(x_i) > 0:
+                det = 1 / np.prod(x_i)
+                probability = multivariate_normal.pdf(np.log(x_i), mu, sigma) * det
+            else:
+                probability = 0
+            probabilities += [probability]
+        print(probabilities)
+        normalized_probabilities = probabilities / np.sum(probabilities)
 
         # use default synthesis to construct the circuit
-        self.initialize(normalized_probabilities)
+        self.initialize(np.sqrt(normalized_probabilities), self.qubits)  # pylint: disable=no-member
--- qiskit/circuit/library/probability_distributions/normal.py
+++ qiskit/circuit/library/probability_distributions/normal.py
@@ -12,14 +12,15 @@
 
 """The normal probability distribution circuit."""
 
-import numpy
-import scipy
+from typing import Tuple, Union, List, Optional
+import numpy as np
+from scipy.stats import multivariate_normal
 from qiskit.circuit import QuantumCircuit
 
 
 class NormalDistribution(QuantumCircuit):
     r"""The normal distribution circuit.
-    
+
     The probability density function of the normal distribution is defined as
 
     .. math::
@@ -28,24 +29,45 @@ class NormalDistribution(QuantumCircuit):
 
     """
 
-    def __init__(self, num_qubits: int, mu: float = 0, sigma: float = 1, 
-                 bounds: Tuple[float, float] = (0, 1), name: str = 'P(X)') -> None:
-        """
+    def __init__(self,
+                 num_qubits: Union[int, List[int]],
+                 mu: Union[float, List[float]] = 0,
+                 sigma: Union[float, List[float]] = 1,
+                 bounds: Optional[Union[Tuple[float, float], List[Tuple[float, float]]]] = None,
+                 name: str = 'P(X)') -> None:
+        r"""
         Args:
-            num_qubits: The number of qubits in the circuit, the distribution is uniform over 
+            num_qubits: The number of qubits in the circuit, the distribution is uniform over
                 ``2 ** num_qubits`` values.
             mu: The parameter :math:`\mu`, which is the expected value of the distribution.
             sigma: The parameter :math:`\sigma`, which is the standard deviation.
             bounds: The truncation bounds of the distribution.
             name: The name of the circuit.
         """
-        super().__init__(num_qubits, name=name)
-        
-        # compute the normalized, truncated probabilities 
-        x = numpy.linspace(bounds[0], bounds[1], num=2**num_qubits)
-        probabilities = [scipy.stats.norm(x_i, mu, sigma) for x_i in x]
-        normalized_probabilites = probabilities / numpy.sum(probabilities)
+        if not isinstance(num_qubits, list):  # univariate case
+            super().__init__(num_qubits, name=name)
+
+            if bounds is None:
+                bounds = (-1, 1)
+
+            x = np.linspace(bounds[0], bounds[1], num=2**num_qubits)
+
+        else:  # multivariate case
+            super().__init__(sum(num_qubits), name=name)
+
+            if bounds is None:
+                bounds = [(-1, 1)] * len(num_qubits)
+
+            # compute the evaluation points using numpy's meshgrid
+            # indexing 'ij' yields the "column-based" indexing
+            meshgrid = np.meshgrid(*[np.linspace(bound[0], bound[1], num=2**num_qubits[i])
+                                     for i, bound in enumerate(bounds)], indexing='ij')
+            # flatten into a list of points
+            x = list(zip(*[grid.flatten() for grid in meshgrid]))
+
+        # compute the normalized, truncated probabilities
+        probabilities = multivariate_normal.pdf(x, mu, sigma)
+        normalized_probabilities = probabilities / np.sum(probabilities)
 
         # use default synthesis to construct the circuit
-        self.initialize(normalized_probabilities)
-
+        self.initialize(np.sqrt(normalized_probabilities), self.qubits)  # pylint: disable=no-member
