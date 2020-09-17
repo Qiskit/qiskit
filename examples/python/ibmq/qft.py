202bf946cc948d16403d20270d73b8bd3f7fd2dc
Cryoris <jules.gacon@googlemail.com>
2020-09-17 19:19:38 +0200

    update examples

--- examples/python/ibmq/qft.py
+++ examples/python/ibmq/qft.py
@@ -27,7 +27,7 @@ def input_state(circ, n):
     """n-qubit input state for QFT that produces output 1."""
     for j in range(n):
         circ.h(j)
-        circ.u1(-math.pi/float(2**(j)), j)
+        circ.p(-math.pi/float(2**(j)), j)
 
 
 def qft(circ, n):
