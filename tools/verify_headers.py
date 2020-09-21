647865adbeceba08c8d020c4574fafc375e15b2e
Luciano Bello <luciano.bello@ibm.com>
2020-09-20 22:14:09 -0400

    Remove "coding: utf-8" (PEP 3120) and add the verify_header checker

--- tools/verify_headers.py
+++ tools/verify_headers.py
@@ -15,7 +15,9 @@ import argparse
 import multiprocessing
 import os
 import sys
+import re
 
+pep3120 = re.compile("^[ \t\f]*#.*?coding[:=][ \t]*([-_.a-zA-Z0-9]+)")
 
 def discover_files(code_paths):
     out_paths = []
@@ -51,7 +53,9 @@ def validate_header(file_path):
     for index, line in enumerate(lines):
         count += 1
         if count > 5:
-            return (file_path, False, "Header not found in first 5 lines")
+            return file_path, False, "Header not found in first 5 lines"
+        if pep3120.match(line):
+            return file_path, False, "Unnecessary encoding specification (PEP 263, 3120)"
         if line == "# This code is part of Qiskit.\n":
             start = index
             break
@@ -82,9 +86,9 @@ def main():
     res = pool.map(validate_header, files)
     failed_files = [x for x in res if x[1] is False]
     if len(failed_files) > 0:
-        for i in failed_files:
-            sys.stderr.write("%s failed header check because:\n\n" % i[0])
-            sys.stderr.write("%s\n\n" % i[2])
+        for failed_file in failed_files:
+            sys.stderr.write("%s failed header check because:\n" % failed_file[0])
+            sys.stderr.write("%s\n\n" % failed_file[2])
         sys.exit(1)
     sys.exit(0)
 
