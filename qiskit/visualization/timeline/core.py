2953144ec708043c07bd948691093dc0fb16e7e6
knzwnao <knzwnao@jp.ibm.com>
2020-09-24 08:27:41 +0900

    minor update

--- qiskit/visualization/timeline/core.py
+++ qiskit/visualization/timeline/core.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-r"""
+"""
 Core module of the timeline drawer.
 
 This module provides `DrawDataContainer` which is a collection of drawing objects
