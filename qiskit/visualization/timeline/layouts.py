d5a2ba0b29f045751e5d981e5e4a7da014295477
knzwnao <knzwnao@jp.ibm.com>
2020-09-24 08:16:47 +0900

    code cleanup for pulse drawer consistency

--- qiskit/visualization/timeline/layouts.py
+++ qiskit/visualization/timeline/layouts.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-
 # This code is part of Qiskit.
 #
 # (C) Copyright IBM 2020.
@@ -19,75 +17,11 @@ Those functions are assigned to the `layout` key of the stylesheet.
 User can change the layout of the output image by writing own function.
 """
 
-from typing import List
-
+from typing import List, Tuple
+import numpy as np
 from qiskit import circuit
 from qiskit.visualization.exceptions import VisualizationError
 from qiskit.visualization.timeline import types
-
-
-def default_color_table(gate_name: str) -> str:
-    """Color table that returns color code associated with the gate name."""
-    _color_code_name_table = {
-        'u0': '#FA74A6',
-        'u1': '#000000',
-        'u2': '#FA74A6',
-        'u3': '#FA74A6',
-        'id': '#05BAB6',
-        'x': '#05BAB6',
-        'y': '#05BAB6',
-        'z': '#05BAB6',
-        'h': '#6FA4FF',
-        'cx': '#6FA4FF',
-        'cy': '#6FA4FF',
-        'cz': '#6FA4FF',
-        'swap': '#6FA4FF',
-        's': '#6FA4FF',
-        'sdg': '#6FA4FF',
-        'dcx': '#6FA4FF',
-        'iswap': '#6FA4FF',
-        't': '#BB8BFF',
-        'tdg': '#BB8BFF',
-        'r': '#BB8BFF',
-        'rx': '#BB8BFF',
-        'ry': '#BB8BFF',
-        'rz': '#BB8BFF',
-        'reset': '#808080',
-        'measure': '#808080'
-    }
-    return _color_code_name_table.get(gate_name, '#BB8BFF')
-
-
-def default_latex_gate_name(gate_name: str) -> str:
-    """Conversion table that returns latex syntax gate name."""
-    _latex_name_table = {
-        'u0': r'{\rm U}_0',
-        'u1': r'{\rm U}_1',
-        'u2': r'{\rm U}_2',
-        'u3': r'{\rm U}_3',
-        'id': r'{\rm Id}',
-        'x': r'{\rm X}',
-        'y': r'{\rm Y}',
-        'z': r'{\rm Z}',
-        'h': r'{\rm H}',
-        'cx': r'{\rm Cx}',
-        'cy': r'{\rm Cy}',
-        'cz': r'{\rm Cz}',
-        'swap': r'{\rm SWAP}',
-        's': r'{\rm S}',
-        'sdg': r'{\rm S}^\dagger',
-        'dcx': r'{\rm DCX}',
-        'iswap': r'{\rm iSWAP}',
-        't': r'{\rm T}',
-        'tdg': r'{\rm T}^\dagger',
-        'r': r'{\rm R}',
-        'rx': r'{\rm R}_x',
-        'ry': r'{\rm R}_y',
-        'rz': r'{\rm R}_z',
-        'reset': r'|0\rangle',
-        'measure': r'{\rm Measure}'
-    }
-    return _latex_name_table.get(gate_name, r'{{\rm {name}}}'.format(name=gate_name))
 
 
 def qreg_creg_ascending(bits: List[types.Bits]) -> List[types.Bits]:
@@ -138,3 +72,33 @@ def qreg_creg_descending(bits: List[types.Bits]) -> List[types.Bits]:
     cregs = sorted(cregs, key=lambda x: x.index, reverse=True)
 
     return qregs + cregs
+
+
+def time_map_in_dt(time_window: Tuple[int, int]) -> types.HorizontalAxis:
+    """Layout function for the horizontal axis formatting.
+
+    Generate equispaced 6 horizontal axis ticks.
+
+    Args:
+        time_window: Left and right edge of this graph.
+
+    Returns:
+        Axis formatter object.
+    """
+    # shift time axis
+    t0, t1 = time_window
+
+    # axis label
+    axis_loc = np.linspace(max(t0, 0), t1, 6)
+    axis_label = axis_loc.copy()
+
+    # consider time resolution
+    label = 'System cycle time (dt)'
+
+    formatted_label = ['{val:.0f}'.format(val=val) for val in axis_label]
+
+    return types.HorizontalAxis(
+        window=(t0, t1),
+        axis_map=dict(zip(axis_loc, formatted_label)),
+        label=label
+    )
