432844143bd79e12cb941de6b6f64932b9122db2
Ali Javadi <ajavadia@princeton.edu>
2020-09-29 09:25:33 -0400

    template_circuits -> templates

--- qiskit/transpiler/passes/optimization/template_optimization.py
+++ qiskit/transpiler/passes/optimization/template_optimization.py
@@ -27,7 +27,7 @@ from qiskit.converters.circuit_to_dagdependency import circuit_to_dagdependency
 from qiskit.converters.dag_to_dagdependency import dag_to_dagdependency
 from qiskit.converters.dagdependency_to_dag import dagdependency_to_dag
 from qiskit.transpiler.basepasses import TransformationPass
-from qiskit.circuit.library.templates import template_nct_2a_1, template_nct_2a_2, template_nct_2a_3
+from qiskit.circuit.library.templates.toffoli import template_2a_1, template_2a_2, template_2a_3
 from qiskit.quantum_info.operators.operator import Operator
 from qiskit.transpiler.exceptions import TranspilerError
 from qiskit.transpiler.passes.optimization.template_matching import (TemplateMatching,
