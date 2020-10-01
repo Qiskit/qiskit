4512e6d6251d83d48e5df509b1a941be639916fc
Ali Javadi <ajavadia@princeton.edu>
2020-10-01 00:28:02 -0400

    instruction_durations overwrites backend durations, not extend them

--- qiskit/transpiler/instruction_durations.py
+++ qiskit/transpiler/instruction_durations.py
@@ -82,9 +82,6 @@ class InstructionDurations:
         # TODO: backend.properties() should tell us durations of measurements
         # TODO: Remove the following lines after that
         try:
-            dtm = backend.configuration().dtm
-            if dtm != dt:
-                raise TranspilerError("dtm != dt case is not supported.")
             inst_map = backend.defaults().instruction_schedule_map
             all_qubits = tuple(range(backend.configuration().num_qubits))
             meas_duration = inst_map.get('measure', all_qubits).duration
