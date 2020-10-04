693c7e5d0a10487dcc70509ab99b486860c4c796
Edwin Navarro <enavarro@comcast.net>
2020-10-04 13:51:36 -0700

    Convert styles to all dict and begin file loads

--- qiskit/visualization/qcstyle.py
+++ qiskit/visualization/qcstyle.py
@@ -21,30 +21,30 @@ class DefaultStyle:
     """
     def __init__(self):
         colors = {
-            # Default Colors
+            '### Default Colors': 'Default Colors',
             'basis': '#FA74A6',         # Red
             'clifford': '#6FA4FF',      # Light Blue
             'pauli': '#05BAB6',         # Green
             'def_other': '#BB8BFF',     # Purple
-            # IQX Colors
+            '### IQX Colors': 'IQX Colors',
             'classical': '#002D9C',     # Dark Blue
             'phase': '#33B1FF',         # Cyan
             'hadamard': '#FA4D56',      # Light Red
             'non_unitary': '#A8A8A8',   # Medium Gray
             'iqx_other': '#9F1853',     # Dark Red
-            # B/W
+            '### B/W': 'B/W',
             'black': '#000000',
             'white': '#FFFFFF',
             'light_gray': '#778899',
             'dark_gray': '#BDBDBD'
         }
         self.style = {
-
             'name': 'default',
             'tc': colors['black'],          # Non-gate Text Color
             'gt': colors['black'],          # Gate Text Color
             'sc': colors['black'],          # Gate Subtext Color
             'lc': colors['black'],          # Line Color
+            'mc': colors['white'],          # Measure Arcs Color
             'cc': colors['light_gray'],     # creg Line Color
             'gc': colors['def_other'],      # Default Gate Color
             'bc': colors['dark_gray'],      # Barrier Color
@@ -52,7 +52,6 @@ class DefaultStyle:
             'ec': None,                     # Edge Color (B/W only)
             'fs': 13,                       # Gate Font Size
             'sfs': 8,                       # Subtext Font Size
-            'latexmode': False,
             'index': False,
             'figwidth': -1,
             'dpi': 150,
@@ -60,12 +59,12 @@ class DefaultStyle:
             'cline': 'doublet',
 
             'disptex': {
-                'id': 'I',
-                'p': 'P',
-                'u': 'U',
                 'u1': '$\\mathrm{U}_1$',
                 'u2': '$\\mathrm{U}_2$',
                 'u3': '$\\mathrm{U}_3$',
+                'u': 'U',
+                'p': 'P',
+                'id': 'I',
                 'x': 'X',
                 'y': 'Y',
                 'z': 'Z',
@@ -76,11 +75,9 @@ class DefaultStyle:
                 'sxdg': '$\\sqrt{\\mathrm{X}}^\\dagger$',
                 't': 'T',
                 'tdg': '$\\mathrm{T}^\\dagger$',
-                'iswap': 'Iswap',
                 'dcx': 'Dcx',
+                'iswap': 'Iswap',
                 'ms': 'MS',
-                'diagonal': 'Diagonal',
-                'unitary': 'Unitary',
                 'r': 'R',
                 'rx': '$\\mathrm{R}_\\mathrm{X}$',
                 'ry': '$\\mathrm{R}_\\mathrm{Y}$',
@@ -107,10 +104,10 @@ class DefaultStyle:
                 'swap': (colors['clifford'], colors['black']),
                 's': (colors['clifford'], colors['black']),
                 'sdg': (colors['clifford'], colors['black']),
-                'dcx': (colors['clifford'], colors['black']),
-                'iswap': (colors['clifford'], colors['black']),
                 't': (colors['def_other'], colors['black']),
                 'tdg': (colors['def_other'], colors['black']),
+                'dcx': (colors['clifford'], colors['black']),
+                'iswap': (colors['clifford'], colors['black']),
                 'r': (colors['def_other'], colors['black']),
                 'rx': (colors['def_other'], colors['black']),
                 'ry': (colors['def_other'], colors['black']),
@@ -135,33 +132,35 @@ class DefaultStyle:
             }
         }
 
-def set_style(base_style, update_style):
-    upd_dict = copy(update_style)
-    base_style['name'] = upd_dict.pop('name', base_style['name'])
-    base_style['tc'] = upd_dict.pop('textcolor', base_style['tc'])
-    base_style['sc'] = upd_dict.pop('subtextcolor', base_style['sc'])
-    base_style['lc'] = upd_dict.pop('linecolor', base_style['lc'])
-    base_style['cc'] = upd_dict.pop('creglinecolor', base_style['cc'])
-    base_style['gt'] = upd_dict.pop('gatetextcolor', base_style['gt'])
-    base_style['gc'] = upd_dict.pop('gatefacecolor', base_style['gc'])
-    base_style['bc'] = upd_dict.pop('barrierfacecolor', base_style['bc'])
-    base_style['bg'] = upd_dict.pop('backgroundcolor', base_style['bg'])
-    base_style['fs'] = upd_dict.pop('fontsize', base_style['fs'])
-    base_style['sfs'] = upd_dict.pop('subfontsize', base_style['sfs'])
-    base_style['disptex'] = upd_dict.pop('displaytext', base_style['disptex'])
-    dcol = upd_dict.pop('displaycolor', base_style['dispcol'])
+def set_style(def_style, json_style):
+    json_dict = copy(json_style)
+    def_style['name'] = json_dict.pop('name', def_style['name'])
+    def_style['tc'] = json_dict.pop('textcolor', def_style['tc'])
+    def_style['sc'] = json_dict.pop('subtextcolor', def_style['sc'])
+    def_style['lc'] = json_dict.pop('linecolor', def_style['lc'])
+    def_style['cc'] = json_dict.pop('creglinecolor', def_style['cc'])
+    def_style['gt'] = json_dict.pop('gatetextcolor', def_style['gt'])
+    def_style['gc'] = json_dict.pop('gatefacecolor', def_style['gc'])
+    def_style['bc'] = json_dict.pop('barrierfacecolor', def_style['bc'])
+    def_style['bg'] = json_dict.pop('backgroundcolor', def_style['bg'])
+    def_style['fs'] = json_dict.pop('fontsize', def_style['fs'])
+    def_style['sfs'] = json_dict.pop('subfontsize', def_style['sfs'])
+    dtex = json_dict.pop('displaytext', def_style['disptex'])
+    for tex in dtex.keys():
+        if tex in def_style['disptex'].keys():
+            def_style['disptex'][tex] = dtex[tex]
+    dcol = json_dict.pop('displaycolor', def_style['dispcol'])
     for col in dcol.keys():
-        if col in base_style['dispcol'].keys():
-            base_style['dispcol'][col] = dcol[col]
-    base_style['latexmode'] = upd_dict.pop('latexdrawerstyle', base_style['latexmode'])
-    base_style['index'] = upd_dict.pop('showindex', base_style['index'])
-    base_style['figwidth'] = upd_dict.pop('figwidth', base_style['figwidth'])
-    base_style['dpi'] = upd_dict.pop('dpi', base_style['dpi'])
-    base_style['margin'] = upd_dict.pop('margin', base_style['margin'])
-    base_style['cline'] = upd_dict.pop('creglinestyle', base_style['cline'])
+        if col in def_style['dispcol'].keys():
+            def_style['dispcol'][col] = dcol[col]
+    def_style['index'] = json_dict.pop('showindex', def_style['index'])
+    def_style['figwidth'] = json_dict.pop('figwidth', def_style['figwidth'])
+    def_style['dpi'] = json_dict.pop('dpi', def_style['dpi'])
+    def_style['margin'] = json_dict.pop('margin', def_style['margin'])
+    def_style['cline'] = json_dict.pop('creglinestyle', def_style['cline'])
 
-    if upd_dict:
-        warn('style option/s ({}) is/are not supported'.format(', '.join(upd_dict.keys())),
+    if json_dict:
+        warn('style option/s ({}) is/are not supported'.format(', '.join(json_dict.keys())),
              DeprecationWarning, 2)
 
 
