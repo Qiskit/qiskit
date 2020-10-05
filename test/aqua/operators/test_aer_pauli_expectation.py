6f14cd6705d78342bf9b5d2e0e0e2ec3e5fe3132
hhorii <hhorii@users.noreply.github.com>
2020-10-05 23:30:30 +0900

    Reuse a circuit for multiple parameter sets in aer (Qiskit/qiskit-aqua#1222)
    * optimize parameter binding for Aer
    
    * add a test for aer's paremter binding
    
    * use parameter-repeat in aer
    
    * refactor for aer backend to use existing parameter-repeat
    
    Co-authored-by: Steve Wood <40241007+woodsp-ibm@users.noreply.github.com>

--- test/aqua/operators/test_aer_pauli_expectation.py
+++ test/aqua/operators/test_aer_pauli_expectation.py
@@ -167,37 +167,31 @@ class TestAerPauliExpectation(QiskitAquaTestCase):
             actual_sampled = sut.convert(expect_op, params=param_bindings).eval()
             self.assertAlmostEqual(actual_sampled, expect_sampled, delta=.1)
 
-        def get_binding_status(sampler):
-            # sampler._transpiled_circ_templates:
-            #     set only if aer's binding was used.
-            #     renewed if var_form is renewed,
-            # sampler._last_ready_circ:
-            #     set only if aer's binding was used.
-            #     renewed if var_form or # of parameter sets are changed
+        def get_circuit_templates(sampler):
             return sampler._transpiled_circ_templates
 
-        def validate_aer_binding_used(status):
-            self.assertIsNotNone(status)
+        def validate_aer_binding_used(templates):
+            self.assertIsNotNone(templates)
 
-        def validate_aer_templates_reused(prev_status, cur_status):
-            self.assertIs(prev_status, cur_status)
+        def validate_aer_templates_reused(prev_templates, cur_templates):
+            self.assertIs(prev_templates, cur_templates)
 
         validate_sampler(self.sampler, aer_sampler, generate_parameters(1))
-        cur_status = get_binding_status(aer_sampler)
+        cur_templates = get_circuit_templates(aer_sampler)
 
-        validate_aer_binding_used(cur_status)
+        validate_aer_binding_used(cur_templates)
 
-        prev_status = cur_status
+        prev_templates = cur_templates
         validate_sampler(self.sampler, aer_sampler, generate_parameters(2))
-        cur_status = get_binding_status(aer_sampler)
+        cur_templates = get_circuit_templates(aer_sampler)
 
-        validate_aer_templates_reused(prev_status, cur_status)
+        validate_aer_templates_reused(prev_templates, cur_templates)
 
-        prev_status = cur_status
+        prev_templates = cur_templates
         validate_sampler(self.sampler, aer_sampler, generate_parameters(2))  # same num of params
-        cur_status = get_binding_status(aer_sampler)
+        cur_templates = get_circuit_templates(aer_sampler)
 
-        validate_aer_templates_reused(prev_status, cur_status)
+        validate_aer_templates_reused(prev_templates, cur_templates)
 
     def test_pauli_expectation_param_qobj(self):
         """ Test PauliExpectation with param_qobj """
