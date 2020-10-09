b0d31257972f2c87cb4d549429372192d0e159e7
Brandon <56101718+BKGodd@users.noreply.github.com>
2020-10-09 18:21:35 -0400

    Fixed issue 4697 by updating append method (#4887)
    * Fixed issue 4697 by updating append method
    
    * Fixed issue 4697 by updating append method
    
    * Fixed issue 4697 by updating append method
    
    * Update quantumcircuit.py conditional
    
    * Update conditional statement
    
    * Add unit test and releasenotes
    
    * Add unit test and releasenotes
    
    * Update logical check and comments
    
    * Update logical check and comments
    
    * Update logical check and comments
    
    * Update append method and releasenotes
    
    Co-authored-by: Kevin Krsulich <kevin.krsulich@ibm.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- test/python/circuit/test_parameters.py
+++ test/python/circuit/test_parameters.py
@@ -136,7 +136,7 @@ class TestParameters(QiskitTestCase):
         vparams = qc._parameter_table
         self.assertEqual(len(vparams), 1)
         self.assertIs(theta, next(iter(vparams)))
-        self.assertIs(rxg, vparams[theta][0][0])
+        self.assertEqual(rxg, vparams[theta][0][0])
 
     def test_is_parameterized(self):
         """Test checking if a gate is parameterized (bound/unbound)"""
