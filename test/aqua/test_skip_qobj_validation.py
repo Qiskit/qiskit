d40e493d1449caa53f46c36f6aaf8d24f5c73743
Manoel Marques <Manoel.Marques@ibm.com>
2020-10-23 17:44:31 -0400

    Allow Noise Simulation for simulator backends (Qiskit/qiskit-aqua#1384)

--- test/aqua/test_skip_qobj_validation.py
+++ test/aqua/test_skip_qobj_validation.py
@@ -16,7 +16,7 @@ import unittest
 from test.aqua import QiskitAquaTestCase
 from qiskit import QuantumCircuit, QuantumRegister, ClassicalRegister
 from qiskit import BasicAer
-from qiskit.aqua import QuantumInstance
+from qiskit.aqua import QuantumInstance, AquaError
 
 
 def _compare_dict(dict1, dict2):
@@ -102,6 +102,15 @@ class TestSkipQobjValidation(QiskitAquaTestCase):
         quantum_instance.skip_qobj_validation = True
         res_w_noise_skip_validation = quantum_instance.execute(self.qc).get_counts(self.qc)
         self.assertTrue(_compare_dict(res_w_noise, res_w_noise_skip_validation))
+
+        # BasicAer should fail:
+        with self.assertRaises(AquaError):
+            _ = QuantumInstance(BasicAer.get_backend('qasm_simulator'),
+                                noise_model=noise_model)
+
+        with self.assertRaises(AquaError):
+            quantum_instance = QuantumInstance(BasicAer.get_backend('qasm_simulator'))
+            quantum_instance.set_config(noise_model=noise_model)
 
 
 if __name__ == '__main__':
