4da057d2c4af8802a8a6675c65db3ad1b9348b7f
Matthew Treinish <mtreinish@kortar.org>
2020-11-03 19:43:35 -0500

    Add user config file options to control parallel execution (#5324)
    * Add user config file options to control parallel execution
    
    This commit adds new options to the user config file, 'parallel' and
    'num_processes' which when set will override the default behavior of
    terra's parallel_map function (which is used internally by transpile()
    and assemble()). Along with these new options the default behavior on
    macOS is changed to disable parallel execution by default.
    
    Fixes #5323
    
    * Fix lint and osx tests
    
    * lint
    
    Co-authored-by: Luciano Bello <luciano.bello@ibm.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- qiskit/user_config.py
+++ qiskit/user_config.py
@@ -14,12 +14,21 @@
 
 import configparser
 import os
+import sys
 from warnings import warn
 
 from qiskit import exceptions
 
 DEFAULT_FILENAME = os.path.join(os.path.expanduser("~"),
                                 '.qiskit', 'settings.conf')
+
+if os.getenv('QISKIT_PARALLEL', None) is not None:
+    PARALLEL_DEFAULT = os.getenv('QISKIT_PARALLEL', None).lower() == 'true'
+else:
+    if sys.platform in {'darwin', 'win32'}:
+        PARALLEL_DEFAULT = False
+    else:
+        PARALLEL_DEFAULT = True
 
 
 class UserConfig:
@@ -33,6 +42,8 @@ class UserConfig:
     circuit_mpl_style_path = ~/.qiskit:<default location>
     transpile_optimization_level = 1
     suppress_packaging_warnings = False
+    parallel = False
+    num_processes = 4
 
     """
     def __init__(self, filename=None):
@@ -111,6 +122,21 @@ class UserConfig:
             if package_warnings:
                 self.settings['suppress_packaging_warnings'] = package_warnings
 
+            # Parse parallel
+            parallel_enabled = self.config_parser.getboolean(
+                'default', 'parallel', fallback=PARALLEL_DEFAULT)
+            self.settings['parallel_enabled'] = parallel_enabled
+
+            # Parse num_processes
+            num_processes = self.config_parser.getint(
+                'default', 'num_processes', fallback=-1)
+            if not num_processes == -1:
+                if num_processes <= 0:
+                    raise exceptions.QiskitUserConfigError(
+                        "%s is not a valid number of processes. Must be "
+                        "greater than 0")
+                self.settings['num_processes'] = num_processes
+
 
 def get_config():
     """Read the config file from the default location or env var
@@ -124,7 +150,7 @@ def get_config():
     """
     filename = os.getenv('QISKIT_SETTINGS', DEFAULT_FILENAME)
     if not os.path.isfile(filename):
-        return {}
+        return {'parallel_enabled': PARALLEL_DEFAULT}
     user_config = UserConfig(filename)
     user_config.read_config_file()
     return user_config.settings
