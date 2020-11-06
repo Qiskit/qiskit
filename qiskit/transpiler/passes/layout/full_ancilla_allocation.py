9b7a963c9727e911d7239bfaaf525123f38da7fa
Luciano Bello <luciano.bello@ibm.com>
2020-11-06 17:40:38 -0500

    extend circuit with ancillas from layout

--- qiskit/transpiler/passes/layout/full_ancilla_allocation.py
+++ qiskit/transpiler/passes/layout/full_ancilla_allocation.py
@@ -65,12 +65,13 @@ class FullAncillaAllocation(AnalysisPass):
         if layout is None:
             raise TranspilerError('FullAncillaAllocation pass requires property_set["layout"].')
 
-        layout_physical_qubits = layout.get_physical_bits().keys()
-        idle_physical_qubits = []
+        layout_physical_qubits = list(range(max(layout.get_physical_bits())+1))
+        idle_physical_qubits = [q for q in layout_physical_qubits
+                                if q not in layout.get_physical_bits()]
 
         if self.coupling_map:
             idle_physical_qubits = [q for q in self.coupling_map.physical_qubits
-                                    if q not in layout_physical_qubits]
+                                    if q not in layout.get_physical_bits()]
 
         if idle_physical_qubits:
             if self.ancilla_name in dag.qregs:
