aa6a07a698f9ef47529b059875b1b9e9288b96ac
Luciano Bello <luciano.bello@ibm.com>
2020-11-07 13:57:17 -0500

    spacing

--- qiskit/extensions/quantum_initializer/initializer.py
+++ qiskit/extensions/quantum_initializer/initializer.py
@@ -44,6 +44,7 @@ class Initialize(Instruction):
 
     def __init__(self, params):
         """Create new initialize composite.
+
         params (str or list):
           * Statevector: Statevector to initialize to.
           * list: vector of complex amplitudes to initialize to.
