2a695ec96c19e7420d0e0289c0456231f033c5ef
Matthew Treinish <mtreinish@kortar.org>
2020-11-10 08:27:56 -0500

    Fix lint

--- qiskit/circuit/quantumcircuit.py
+++ qiskit/circuit/quantumcircuit.py
@@ -10,6 +10,8 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
+# pylint: disable=bad-docstring-quotes,invalid-name
+
 """Quantum circuit object."""
 
 import copy
