8405fea4217a26ac229448bdcbc84e11664ebd57
Matthew Treinish <mtreinish@kortar.org>
2020-11-12 07:51:45 -0500

    Prepare 0.16.1 release
    This commit prepares for the 0.16.1 release by bumping the version
    strings to reflect the new version. This is just a bugfix release
    that several fixes for bugs introduced in 0.16.0.

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ with open(README_PATH) as readme_file:
 
 setup(
     name="qiskit-terra",
-    version="0.16.0",
+    version="0.16.1",
     description="Software for developing quantum computing programs",
     long_description=README,
     long_description_content_type='text/markdown',
