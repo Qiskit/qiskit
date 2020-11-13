a769e4e185d8dc44fa974a1f2ad83da98725ef28
Ashish Panigrahi <ashish.panigrahi@protonmail.com>
2020-11-13 10:00:44 +0530

    implemented suggestions

--- qiskit/visualization/matplotlib.py
+++ qiskit/visualization/matplotlib.py
@@ -366,11 +366,9 @@ class MatplotlibDrawer:
 
         if gate_text in self._style['disptex']:
             gate_text = "{}".format(self._style['disptex'][gate_text])
-        else:
-            gate_text = "{}".format(gate_text)
+        elif gate_text == op.name or gate_text == base_name:
+            gate_text = gate_text.capitalize()
 
-        if ctrl_text:
-            ctrl_text = "{}".format(ctrl_text)
         return gate_text, ctrl_text
 
     def _get_colors(self, op):
