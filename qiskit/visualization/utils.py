b94c50c9eb2a558a1197bc8bc7b3515fc6faad6c
Edwin Navarro <enavarro@comcast.net>
2020-11-13 18:49:04 -0800

    measure_map for conditionals

--- qiskit/visualization/utils.py
+++ qiskit/visualization/utils.py
@@ -15,12 +15,14 @@
 """Common visualization utilities."""
 
 import re
+from collections import OrderedDict
 
 import numpy as np
 from qiskit.converters import circuit_to_dag
 from qiskit.quantum_info.states import DensityMatrix
 from qiskit.quantum_info.operators import PauliTable, SparsePauliOp
 from qiskit.visualization.exceptions import VisualizationError
+from qiskit.circuit import Measure
 
 try:
     import PIL
@@ -92,7 +94,6 @@ def _get_layered_instructions(circuit, reverse_bits=False,
     Returns:
         Tuple(list,list,list): To be consumed by the visualizer directly.
     """
-    print("Justify", justify)
     if justify:
         justify = justify.lower()
 
@@ -100,18 +101,35 @@ def _get_layered_instructions(circuit, reverse_bits=False,
     justify = justify if justify in ('right', 'none') else 'left'
 
     dag = circuit_to_dag(circuit)
-    print("DAG**********", dag.layers())
+
+    for x in dag.nodes():
+        print('Node', x._op)
+    for x in dag.layers():
+        print('\npart', x['partition'])
+        print('layer', x['graph'])
+        print("DAG**********", x['graph'].node_counter, '\n')
+        for y in x['graph'].op_nodes():
+            print('Node', y.type, y._op)
+    print()
+
     ops = []
     qregs = dag.qubits
     cregs = dag.clbits
+
+    print(cregs)
+    measure_map = OrderedDict([(i.index, -1) for i in cregs])
+    print(measure_map)
+    print(measure_map[1])
 
     if justify == 'none':
         for node in dag.topological_op_nodes():
             ops.append([node])
 
     else:
-        ops = _LayerSpooler(dag, justify)
+        ops = _LayerSpooler(dag, justify, measure_map)
 
+    print(ops)
+    print(measure_map)
     if reverse_bits:
         qregs.reverse()
         cregs.reverse()
@@ -133,7 +151,7 @@ def _sorted_nodes(dag_layer):
     dag_instructions = dag_layer['graph'].op_nodes()
     # sort into the order they were input
     for x in dag_instructions:
-        print(x.op.name, x._node_id)
+        print('Sorted nodes', x.op.name, x._node_id)
     dag_instructions.sort(key=lambda nd: nd._node_id)
     for x in dag_instructions:
         print(x.op.name, x._node_id)
@@ -156,9 +174,9 @@ def _get_gate_span(qregs, instruction):
 
     if instruction.cargs:
         return qregs[min_index:]
-    #if instruction.condition:
-    #    return qregs[min_index:]
-    print('INSTRUCTION-gate span', instruction.op, min_index, max_index)
+    if instruction.condition:
+        return qregs[min_index:]
+    #print('INSTRUCTION-gate span', instruction.op, min_index, max_index)
 
     return qregs[min_index:max_index + 1]
 
@@ -166,24 +184,25 @@ def _get_gate_span(qregs, instruction):
 def _any_crossover(qregs, node, nodes):
     """Return True .IFF. 'node' crosses over any in 'nodes',"""
     gate_span = _get_gate_span(qregs, node)
-    print('crossover', node.op.name, gate_span)
+    #print('crossover', node.op.name, gate_span)
     all_indices = []
     for check_node in nodes:
         if check_node != node:
             all_indices += _get_gate_span(qregs, check_node)
-            print('cross check', check_node.op.name, _get_gate_span(qregs, check_node))
+            #print('cross check', check_node.op.name, _get_gate_span(qregs, check_node))
     return any(i in gate_span for i in all_indices)
 
 
 class _LayerSpooler(list):
     """Manipulate list of layer dicts for _get_layered_instructions."""
 
-    def __init__(self, dag, justification):
+    def __init__(self, dag, justification, measure_map):
         """Create spool"""
         super().__init__()
         self.dag = dag
         self.qregs = dag.qubits
         self.justification = justification
+        self.measure_map = measure_map
 
         print('len self', len(self))
         if self.justification == 'left':
@@ -195,7 +214,11 @@ class _LayerSpooler(list):
                 for node in dag_nodes:
                     print('curr_index', current_index, node.op.name)
                     self.add(node, current_index)
-                    print('LEN SelF', len(self))
+                    #print('LEN SelF', len(self))
+                    for i, s in enumerate(self):
+                        print('Layer', i)
+                        for ss in s:
+                            print('List object', ss._op)
 
         else:
             dag_layers = []
@@ -227,31 +250,44 @@ class _LayerSpooler(list):
 
     def insertable(self, node, nodes):
         """True .IFF. we can add 'node' to layer 'nodes'"""
-        print('insertable', node.op.name, nodes, _any_crossover(self.qregs, node, nodes))
+        print('insertable', node.op.name, nodes, not _any_crossover(self.qregs, node, nodes))
         return not _any_crossover(self.qregs, node, nodes)
 
     def slide_from_left(self, node, index):
         """Insert node into first layer where there is no conflict going l > r"""
+        print("Sliding node", node.op, node.cargs)
+        measure_pos = None
+        if isinstance(node.op, Measure):
+            print("FOUND MEASURE ********************", node.op, node.cargs[0].index)
+            measure_index = node.cargs[0].index
+            #self.measure_map[measure_index][0] = True
         if not self:
+            print('First insert', node._op)
             self.append([node])
             inserted = True
 
         else:
             inserted = False
             curr_index = index
+            index_stop = -1
+            if node.condition:
+                print('NODE CONDITION ************', int(node.condition[1] / 2))
+                index_stop = self.measure_map[int(node.condition[1] / 2)]
             last_insertable_index = None
             print('in slide current_index', curr_index)
-            while curr_index > -1:
-                print('In while', node.op.name, curr_index)
+            while curr_index > index_stop:
+                print('In while before is found in', node.op.name, curr_index)
                 if self.is_found_in(node, self[curr_index]):
                     break
+                print("Before insertable 1")
                 if self.insertable(node, self[curr_index]):
                     last_insertable_index = curr_index
                 curr_index = curr_index - 1
 
-            if last_insertable_index:
+            if last_insertable_index is not None:
                 print('LAST INS True', last_insertable_index)
                 self[last_insertable_index].append(node)
+                measure_pos = last_insertable_index
                 inserted = True
 
             else:
@@ -259,14 +295,22 @@ class _LayerSpooler(list):
                 inserted = False
                 curr_index = index
                 while curr_index < len(self):
+                    print('curr, len', curr_index, len(self))
+                    print("Before insertable 2")
                     if self.insertable(node, self[curr_index]):
                         self[curr_index].append(node)
+                        measure_pos = curr_index
                         inserted = True
+                        print('Pre-break')
                         break
                     curr_index = curr_index + 1
 
         if not inserted:
+            print("Appended", node._op)
             self.append([node])
+        if isinstance(node.op, Measure):
+            measure_pos = measure_pos if measure_pos else len(self) - 1
+            self.measure_map[measure_index] = measure_pos
 
     def slide_from_right(self, node, index):
         """Insert node into rightmost layer as long there is no conflict."""
