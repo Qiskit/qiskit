8f97742ba7f0f04d829aa73b099e366cdf9458b4
Mohamed El Mandouh <73552047+elmandouh@users.noreply.github.com>
2020-11-15 00:40:56 -0500

    Update lowering.py

--- qiskit/scheduler/lowering.py
+++ qiskit/scheduler/lowering.py
@@ -124,11 +124,7 @@ def lower_gates(circuit: QuantumCircuit, schedule_config: ScheduleConfig) -> Lis
         else:
             try:
                 gate_cals = circuit.calibrations[inst.name]
-                params = []
-                for p in inst.params:
-                    if not p.paramerter:
-                        params.append(float(p))
-                schedule = gate_cals[(tuple(inst_qubits), tuple(params))]
+                schedule = gate_cals[(tuple(inst_qubits), tuple(float(p) for p in inst.params)]
                 circ_pulse_defs.append(CircuitPulseDef(schedule=schedule, qubits=inst_qubits))
                 continue
             except KeyError:
