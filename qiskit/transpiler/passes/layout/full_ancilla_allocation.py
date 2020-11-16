8ae0e7c1403d35914d6c88d3980d88d0014b4f9f
Kevin Krsulich <kevin.krsulich@ibm.com>
2020-11-16 18:04:07 -0500

    Update transpiler passes to prefer inspecting Bits over Registers.

--- qiskit/transpiler/passes/layout/full_ancilla_allocation.py
+++ qiskit/transpiler/passes/layout/full_ancilla_allocation.py
@@ -87,6 +87,7 @@ class FullAncillaAllocation(AnalysisPass):
             else:
                 qreg = QuantumRegister(len(idle_physical_qubits), name=self.ancilla_name)
 
+        self.property_set['layout_ancilla_register'] = qreg
         for idx, idle_q in enumerate(idle_physical_qubits):
             self.property_set['layout'][idle_q] = qreg[idx]
 
