Fix gradient chain rule (Qiskit/qiskit-aqua#1449)
* Update qgan.py

Raise error if the number of the training data items is smaller than the batch size, e.g. due to truncation to bounds.

* Update qgan.py

* Update qgan.py

* add possibility to choose an optimizer for the quantum generator in the qGAN algorithm

* Update quantum_generator.py

* Add optimizer instances to test_qgan.py

* Update test_qgan.py

* remove forced maxiter and t=0 for optimizer

* Update quantum_generator.py

* fix lint

* fix linting and brakets

* fix optimizer argument

* Change maxiter check, fix tests to set maxiter to 1

* fix lint

* fixes chain rule

* update unittest and fix lin comb prob grad

* fix lint

* update numbers test

Co-authored-by: Steve Wood <40241007+woodsp-ibm@users.noreply.github.com>
Co-authored-by: woodsp <woodsp@us.ibm.com>
Co-authored-by: Manoel Marques <Manoel.Marques@ibm.com>

qiskit/aqua/operators/gradients/circuit_gradients/lin_comb.py
@@ -589,10 +589,11 @@ class LinComb(CircuitGradient):
                 return prob_dict
             elif isinstance(item, Iterable):
                 # Generate the operator which computes the linear combination
-                lin_comb_op = 2 * (I ^ state_op.num_qubits) ^ Z
+                lin_comb_op = 2 * Z ^ (I ^ state_op.num_qubits)
                 lin_comb_op = lin_comb_op.to_matrix()
                 return list(np.diag(
-                    partial_trace(lin_comb_op.dot(np.outer(item, np.conj(item))), [0]).data))
+                    partial_trace(lin_comb_op.dot(np.outer(item, np.conj(item))),
+                                  [state_op.num_qubits]).data))
             else:
                 raise TypeError(
                     'The state result should be either a DictStateFn or a VectorStateFn.')

qiskit/aqua/operators/gradients/gradient.py
@@ -194,6 +194,11 @@ class Gradient(GradientBase):
                         'jax and use `import jax.numpy as jnp` instead of `import numpy as np` when'
                         'defining a combo_fn.')
 
-            # f(g_1(x), g_2(x)) --> df/dx = df/dg_1 dg_1/dx + df/dg_2 dg_2/dx
+            def chain_rule_combo_fn(x):
+                result = np.dot(x[1], x[0])
+                if isinstance(result, np.ndarray):
+                    result = list(result)
+                return result
+
             return ListOp([ListOp(operator.oplist, combo_fn=grad_combo_fn), ListOp(grad_ops)],
-                          combo_fn=lambda x: np.dot(x[0], x[1]))
+                          combo_fn=chain_rule_combo_fn)

qiskit/aqua/operators/gradients/hessian.py
@@ -232,10 +232,10 @@ class Hessian(HessianBase):
 
             # term1 = (∂F/∂g_i)•(d g_i/ d θ0,θ1)
             term1 = ListOp([ListOp(operator.oplist, combo_fn=first_partial_combo_fn),
-                            ListOp(dd_ops)], combo_fn=lambda x: np.dot(x[0], x[1]))
+                            ListOp(dd_ops)], combo_fn=lambda x: np.dot(x[1], x[0]))
             # term2 = (∂F/∂^2 g_i)•(d g_i/d θ0)•(d g_i/d θ1)
             term2 = ListOp([ListOp(operator.oplist, combo_fn=second_partial_combo_fn), d1d0_ops],
-                           combo_fn=lambda x: np.dot(x[0], x[1]))
+                           combo_fn=lambda x: np.dot(x[1], x[0]))
 
             return SummedOp([term1, term2])
 