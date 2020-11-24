fbeac71553240d27795bbe26aec1e8ad052f225f
knzwnao <knzwnao@jp.ibm.com>
2020-11-24 13:05:37 +0900

    fix complex cast

--- qiskit/visualization/pulse_v2/device_info.py
+++ qiskit/visualization/pulse_v2/device_info.py
@@ -124,7 +124,11 @@ class OpenPulseBackendInfo(DrawerBackendInfo):
         for qind, u_lo_mappers in enumerate(configuration.u_channel_lo):
             temp_val = .0 + .0j
             for u_lo_mapper in u_lo_mappers:
-                temp_val += defaults.qubit_freq_est[u_lo_mapper.q] * complex(*u_lo_mapper.scale)
+                if isinstance(u_lo_mapper.scale, complex):
+                    scale = u_lo_mapper.scale
+                else:
+                    scale = complex(*u_lo_mapper.scale)
+                temp_val += defaults.qubit_freq_est[u_lo_mapper.q] * scale
             chan_freqs[pulse.ControlChannel(qind)] = temp_val.real
 
         # load qubit channel mapping
