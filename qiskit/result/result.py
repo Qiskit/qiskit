80ccdb3f5863fd3f79d265de4cfb3bce12c7e1b1
Thomas Alexander <thomas.a.alexander@ibm.com>
2020-11-24 08:07:36 -0400

    Remove extra paraentheses

--- qiskit/result/result.py
+++ qiskit/result/result.py
@@ -232,7 +232,7 @@ class Result:
         except KeyError:
             raise QiskitError(
                 'No memory for experiment "{}". Please verify that you either ran a measurement level 2 job '
-                'with the memory flag set, eg., "memory=True", or a measurement level 0/1 job."'.format(experiment)
+                'with the memory flag set, eg., "memory=True", or a measurement level 0/1 job.'.format(experiment)
             )
 
     def get_counts(self, experiment=None):
