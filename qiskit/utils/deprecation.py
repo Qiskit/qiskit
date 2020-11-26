39c9b136dad20773bc7cc416a18dfe5574bb947b
Matthew Treinish <mtreinish@kortar.org>
2020-11-26 11:36:20 -0500

    Add stacklevel kwarg to deprecate function decorator
    The deprecate_function decorator is used to emite a deprecation
    warning only once for a function or method. However one piece that was
    missing was to make the stacklevel configurable. There are some cases
    where we could want to use this decorator on an internal function and
    show the proper caller. To do this we need to specify the stacklevel
    kwarg on the warn() call. This commit adds a stacklevel kwarg for this
    use case. It defaults to the current level 2 but enables using the
    decorator in places where the stacklevel can be something other than 2.

--- qiskit/utils/deprecation.py
+++ qiskit/utils/deprecation.py
@@ -58,7 +58,7 @@ def deprecate_arguments(kwarg_map):
     return decorator
 
 
-def deprecate_function(msg):
+def deprecate_function(msg, stacklevel=2):
     """Emit a warning prior to calling decorated function.
 
     Args:
@@ -66,13 +66,14 @@ def deprecate_function(msg):
 
     Returns:
         Callable: The decorated, deprecated callable.
+        stacklevel (int): The warning stackevel to use, defaults to 2.
     """
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             # warn only once
             if not wrapper._warned:
-                warnings.warn(msg, DeprecationWarning, stacklevel=2)
+                warnings.warn(msg, DeprecationWarning, stacklevel=stacklevel)
                 wrapper._warned = True
             return func(*args, **kwargs)
         wrapper._warned = False
