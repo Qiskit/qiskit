8f208749f408bfa8fc6d794ff23728851d653e01
Christopher Wood <cjwood@us.ibm.com>
2020-12-03 13:12:48 -0500

    Add `reverse_qargs` to `Statevector` and `DensityMatrix`

--- qiskit/quantum_info/states/statevector.py
+++ qiskit/quantum_info/states/statevector.py
@@ -293,6 +293,26 @@ class Statevector(QuantumState, TolerancesMixin):
         return matrix_equal(self.data, other.data, ignore_phase=True,
                             rtol=rtol, atol=atol)
 
+    def reverse_qargs(self):
+        r"""Return a Statevector with reversed subsystem ordering.
+
+        For a tensor product state this is equivalent to reversing the order
+        of tensor product subsystems. For a statevector
+        :math:`|\psi \rangle = |\psi_{n-1} \rangle \otimes ... \otimes |\psi_0 \rangle`
+        the returned statevector will be
+        :math:`|\psi_{0} \rangle \otimes ... \otimes |\psi_{n-1} \rangle`.
+
+        Returns:
+            Statevector: the Statevector with reversed subsystem order.
+        """
+        ret = copy.copy(self)
+        axes = tuple(range(self._op_shape._num_qargs_l - 1, -1, -1))
+        ret._data = np.reshape(np.transpose(
+            np.reshape(self.data, self._op_shape.tensor_shape), axes),
+                               self._op_shape.shape)
+        ret._op_shape = self._op_shape.reverse()
+        return ret
+
     def expectation_value(self, oper, qargs=None):
         """Compute the expectation value of an operator.
 
