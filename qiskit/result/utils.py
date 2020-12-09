f6f9fd2a257798e438453c0918e9a25513908421
Luciano Bello <luciano.bello@ibm.com>
2020-12-09 15:20:50 +0100

    add aeddins-ibm as co-author
    Co-Authored-By: aeddins-ibm <60495383+aeddins-ibm@users.noreply.github.com>

--- qiskit/result/utils.py
+++ qiskit/result/utils.py
@@ -111,7 +111,7 @@ def _marginalize(counts, indices=None):
     indices = sorted(indices, reverse=True)
 
     # Build the return list
-    new_counts = Counter({})
+    new_counts = Counter()
     for key, val in counts.items():
         new_key = ''.join([key.replace(' ', '').replace('_', '')[-idx-1] for idx in indices])
         new_counts[new_key] += val
