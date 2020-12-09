6dce749800f734b5c86fbf85555278ec94509149
Matthew Treinish <mtreinish@kortar.org>
2020-12-09 15:41:29 -0500

    Ensure collect_1q_runs doesn't collect measurements

--- qiskit/dagcircuit/dagcircuit.py
+++ qiskit/dagcircuit/dagcircuit.py
@@ -1378,7 +1378,8 @@ class DAGCircuit:
 
         def filter_fn(node):
             return node.type == 'op' and len(node.qargs) == 1 \
-                and node.condition is None and not node.op.is_parameterized()
+                and len(node.cargs) == 0 and node.condition is None \
+                and not node.op.is_parameterized() \
 
         group_list = rx.collect_runs(self._multi_graph, filter_fn)
         return set(tuple(x) for x in group_list)
