f909affc24a23694f9f1e172d00580c62b72b091
Luciano Bello <luciano.bello@ibm.com>
2020-12-10 16:37:50 +0100

    Allow passes to be called directly

--- qiskit/transpiler/basepasses.py
+++ qiskit/transpiler/basepasses.py
@@ -95,6 +95,31 @@ class BasePass(metaclass=MetaPass):
         """
         return isinstance(self, AnalysisPass)
 
+    @classmethod
+    def on(cls, circuit, property_set=None, conf=None):
+        """Runs the pass on circuit.
+
+        Args:
+            circuit (QuantumCircuit): the dag on which the pass is run.
+            property_set (PropertySet, dict, or None): input/output property set.
+            conf (dict): kwargs to the pass constructor. See cls.__init__() for details.
+        Returns:
+            circuit (QuantumCircuit or None): when this is left unimplemented for a pass.
+        """
+        from qiskit.converters import circuit_to_dag, dag_to_circuit
+        from qiskit.dagcircuit.dagcircuit import DAGCircuit
+
+        if conf is None:
+            conf = {}
+
+        cls_pass = cls(**conf)
+        result = cls_pass.run(circuit_to_dag(circuit))
+        if isinstance(property_set, (dict, PropertySet)):
+            property_set.update(cls_pass.property_set)
+        if isinstance(result, DAGCircuit):
+            result = dag_to_circuit(result)
+        return result
+
 
 class AnalysisPass(BasePass):  # pylint: disable=abstract-method
     """An analysis pass: change property set, not DAG."""
