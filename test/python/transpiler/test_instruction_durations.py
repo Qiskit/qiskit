713a1c3105b65b3aade749ae30f8a2a0ce318667
Jessie Yu <jessieyu@us.ibm.com>
2021-01-06 13:08:57 -0500

    Use actual gate name in backend monitor (#5546)
    * don't hard code gates
    
    * fix lint
    
    * add release note
    
    * match old format
    
    * fix instruction duration test
    
    * check gate name in title
    
    * Update qiskit/tools/jupyter/backend_monitor.py
    
    Co-authored-by: Thomas Alexander <thomasalexander2718@gmail.com>
    
    * Update qiskit/tools/monitor/overview.py
    
    Co-authored-by: Thomas Alexander <thomasalexander2718@gmail.com>
    
    * fix title
    
    * use props.gate_error
    
    * remove unused code
    
    Co-authored-by: Thomas Alexander <thomasalexander2718@gmail.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    (cherry picked from commit cf7433a62fdeab9014f4b46abee9099ac1086be8)
    
    # Conflicts:
    #	test/python/transpiler/test_instruction_durations.py

--- test/python/transpiler/test_instruction_durations.py
+++ test/python/transpiler/test_instruction_durations.py
@@ -36,13 +36,32 @@ class TestInstructionDurationsClass(QiskitTestCase):
             InstructionDurations(invalid_dic)
 
     def test_from_backend_for_backend_with_dt(self):
-        durations = InstructionDurations.from_backend(FakeParis())
+        backend = FakeParis()
+        gate = self._find_gate_length_gate(backend)
+        durations = InstructionDurations.from_backend(backend)
         self.assertGreater(durations.dt, 0)
-        self.assertGreater(durations.get('u2', 0), 0)
+        self.assertGreater(durations.get(gate, 0), 0)
 
     def test_from_backend_for_backend_without_dt(self):
+<<<<<<< HEAD
         durations = InstructionDurations.from_backend(FakeVigo())
+=======
+        backend = FakeTokyo()
+        gate = self._find_gate_length_gate(backend)
+        durations = InstructionDurations.from_backend(backend)
+>>>>>>> cf7433a62... Use actual gate name in backend monitor (#5546)
         self.assertIsNone(durations.dt)
-        self.assertGreater(durations.get('u2', 0, 's'), 0)
+        self.assertGreater(durations.get(gate, 0, 's'), 0)
         with self.assertRaises(TranspilerError):
-            durations.get('u2', 0)
+            durations.get(gate, 0)
+
+    def _find_gate_length_gate(self, backend):
+        """Find a gate that has gate length."""
+        props = backend.properties()
+        for gate in props.gates:
+            try:
+                if props.gate_length(gate.gate, 0):
+                    return gate.gate
+            except Exception:  # pylint: disable=broad-except
+                pass
+        raise ValueError("Unable to find a gate with gate length.")
