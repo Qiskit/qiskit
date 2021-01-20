cc256ff927c33fa2b3baeaafdea2df5c7a3009fd
Paul Nation <nonhermitian@gmail.com>
2021-01-20 06:01:46 -0500

    Updates based on review

--- qiskit/quantum_info/analysis/distance.py
+++ qiskit/quantum_info/analysis/distance.py
@@ -14,6 +14,45 @@
 import numpy as np
 
 
+def hellinger_distance(dist_p, dist_q):
+    """Computes the Hellinger distance between
+    two counts distributions.
+
+    Parameters:
+        dist_p (dict): First dict of counts.
+        dist_q (dict): Second dict of counts.
+
+    Returns:
+        float: Distance
+
+    References:
+        `Hellinger Distance @ wikipedia <https://en.wikipedia.org/wiki/Hellinger_distance>`_
+    """
+    p_sum = sum(dist_p.values())
+    q_sum = sum(dist_q.values())
+
+    p_normed = {}
+    for key, val in dist_p.items():
+        p_normed[key] = val/p_sum
+
+    q_normed = {}
+    for key, val in dist_q.items():
+        q_normed[key] = val/q_sum
+
+    total = 0
+    for key, val in p_normed.items():
+        if key in q_normed.keys():
+            total += (np.sqrt(val) - np.sqrt(q_normed[key]))**2
+            del q_normed[key]
+        else:
+            total += val
+    total += sum(q_normed.values())
+
+    dist = np.sqrt(total)/np.sqrt(2)
+
+    return dist
+
+
 def hellinger_fidelity(dist_p, dist_q):
     """Computes the Hellinger fidelity between
     two counts distributions.
@@ -49,27 +88,9 @@ def hellinger_fidelity(dist_p, dist_q):
             res2 = execute(qc, sim).result()
 
             hellinger_fidelity(res1.get_counts(), res2.get_counts())
+
+    References:
+        `Hellinger Distance @ wikipedia <https://en.wikipedia.org/wiki/Hellinger_distance>`_
     """
-    p_sum = sum(dist_p.values())
-    q_sum = sum(dist_q.values())
-
-    p_normed = {}
-    for key, val in dist_p.items():
-        p_normed[key] = val/p_sum
-
-    q_normed = {}
-    for key, val in dist_q.items():
-        q_normed[key] = val/q_sum
-
-    total = 0
-    for key, val in p_normed.items():
-        if key in q_normed.keys():
-            total += (np.sqrt(val) - np.sqrt(q_normed[key]))**2
-            del q_normed[key]
-        else:
-            total += val
-    total += sum(q_normed.values())
-
-    dist = np.sqrt(total)/np.sqrt(2)
-
+    dist = hellinger_distance(dist_p, dist_q)
     return (1-dist**2)**2
