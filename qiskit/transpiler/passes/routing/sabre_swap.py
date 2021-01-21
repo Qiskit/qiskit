1a9b83826575c54c3a4d4e1be891ada4416e2a4b
Luciano Bello <bel@zurich.ibm.com>
2021-01-21 22:10:24 +0100

    do not copy if it is symmetric

--- qiskit/transpiler/passes/routing/sabre_swap.py
+++ qiskit/transpiler/passes/routing/sabre_swap.py
@@ -118,7 +118,7 @@ class SabreSwap(TransformationPass):
         """
 
         super().__init__()
-        self.coupling_map = deepcopy(coupling_map)
+        self.coupling_map = coupling_map if coupling_map.is_symmetric else deepcopy(coupling_map)
         self.heuristic = heuristic
         self.seed = seed
         self.applied_gates = None
