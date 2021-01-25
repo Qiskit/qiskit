acc92910a345af162b343b3bcf09182c177f1487
yaelbh <yaelbh@il.ibm.com>
2021-01-25 17:48:35 +0200

    equivalence checker

---
+++ qiskit/circuit/tools/equivalence_checker.py
@@ -0,0 +1,26 @@
+from time import time
+from qiskit.quantum_info.operators import Operator
+
+class EquivalenceCheckerResult():
+    def __init__(self, success, is_equivalent, time_taken,
+                 circname1, circname2):
+        self._success = success
+        self._time_taken = time_taken
+        self._is_equivalent = is_equivalent
+        self._circname1 = circname1
+        self._circname2 = circname2
+
+class EquivalenceChecker():
+    def run(circ1, circ2):
+        is_equivalent = None
+        success = True
+
+        start = time()
+        try:
+            is_equivalent = Operator(circ1).equiv(Operator(circ2))
+        except:
+            success = False
+
+        time_taken = time() - start
+        return EquivalenceCheckerResult(success, is_equivalent, time_taken,
+                                        circ1.name, circ2.name)
