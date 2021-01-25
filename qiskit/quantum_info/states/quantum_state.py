cb152578f8c35028b670508154dfb36b8b02d7f8
yaelbh <yaelbh@il.ibm.com>
2021-01-25 17:48:14 +0200

    typo fixes

--- qiskit/quantum_info/states/quantum_state.py
+++ qiskit/quantum_info/states/quantum_state.py
@@ -34,7 +34,7 @@ class QuantumState:
 
         .. note::
 
-            If `op_shape`` is specified it will take precedence over other
+            If ``op_shape`` is specified it will take precedence over other
             kwargs.
         """
         self._op_shape = op_shape
