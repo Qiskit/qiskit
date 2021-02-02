f591a0f1148b45d8c54bdf394961dfb1f2c19d39
Naoki Kanazawa <39517270+nkanazawa1989@users.noreply.github.com>
2021-02-02 13:58:21 +0900

    ScheduleBlock 1/4 - remove timeslot dependency from instruction (#5681)
    * remove timeslot dependency from instruction
    
    * update type hint
    
    * update unittest
    
    * update validation logic
    
    * fix lint and test
    
    * Update qiskit/pulse/utils.py
    
    Co-authored-by: Thomas Alexander <thomasalexander2718@gmail.com>
    
    * Update qiskit/pulse/instructions/instruction.py
    
    Co-authored-by: Thomas Alexander <thomasalexander2718@gmail.com>
    
    * Update qiskit/pulse/schedule.py
    
    Co-authored-by: Lauren Capelluto <laurencapelluto@gmail.com>
    
    * minor updates
    
    * add reno
    
    Co-authored-by: Thomas Alexander <thomasalexander2718@gmail.com>
    Co-authored-by: Lauren Capelluto <laurencapelluto@gmail.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- qiskit/pulse/instructions/instruction.py
+++ qiskit/pulse/instructions/instruction.py
@@ -22,17 +22,16 @@ For example::
     sched += Delay(duration, channel)  # Delay is a specific subclass of Instruction
 """
 import warnings
-
 from abc import ABC
 from collections import defaultdict
 from typing import Callable, Dict, Iterable, List, Optional, Set, Tuple
-
-import numpy as np
 
 from qiskit.circuit.parameterexpression import ParameterExpression, ParameterValueType
 from qiskit.pulse.channels import Channel
 from qiskit.pulse.exceptions import PulseError
 from qiskit.pulse.utils import format_parameter_value
+
+
 # pylint: disable=missing-return-doc
 
 
@@ -50,7 +49,7 @@ class Instruction(ABC):
 
         Args:
             operands: The argument list.
-            duration: Length of time taken by the instruction in terms of dt.
+            duration: Deprecated.
             channels: Tuple of pulse channels that this instruction operates on.
             name: Optional display name for this instruction.
 
@@ -59,20 +58,18 @@ class Instruction(ABC):
             PulseError: If the input ``channels`` are not all of
                 type :class:`Channel`.
         """
-        if not isinstance(duration, (int, np.integer)):
-            raise PulseError("Instruction duration must be an integer, "
-                             "got {} instead.".format(duration))
-        if duration < 0:
-            raise PulseError("{} duration of {} is invalid: must be nonnegative."
-                             "".format(self.__class__.__name__, duration))
-
         for channel in channels:
             if not isinstance(channel, Channel):
                 raise PulseError("Expected a channel, got {} instead.".format(channel))
 
-        self._duration = duration
+        if duration is not None:
+            warnings.warn('Specifying duration in the constructor is deprecated. '
+                          'Now duration is an abstract property rather than class variable. '
+                          'All subclasses should implement ``duration`` accordingly. '
+                          'See Qiskit-Terra #5679 for more information.',
+                          DeprecationWarning)
+
         self._channels = channels
-        self._timeslots = {channel: [(0, self.duration)] for channel in channels}
         self._operands = operands
         self._name = name
         self._hash = None
@@ -107,12 +104,6 @@ class Instruction(ABC):
         return self._channels
 
     @property
-    def timeslots(self) -> Dict[Channel, List[Tuple[int, int]]]:
-        """Occupied time slots by this instruction."""
-        warnings.warn("Access to Instruction timeslots is deprecated.")
-        return self._timeslots
-
-    @property
     def start_time(self) -> int:
         """Relative begin time of this instruction."""
         return 0
@@ -125,7 +116,7 @@ class Instruction(ABC):
     @property
     def duration(self) -> int:
         """Duration of this instruction."""
-        return self._duration
+        raise NotImplementedError
 
     @property
     def _children(self) -> Tuple['Instruction']:
