3e48acf281b5e2a786c53581bf561d02b213319d
Matthew Treinish <mtreinish@kortar.org>
2021-02-08 11:44:26 -0500

    Prepare 0.16.4 release (#5801)
    * Prepare 0.16.4 release
    
    This commit prepares for the 0.16.4 release by bumping the version
    strings to reflect the new version. This is just a bugfix release
    that fixes bugs since the 0.16.3 release.
    
    * Update releasenotes/notes/prepare-0.16.4-721822aa11ee7d60.yaml
    
    * Update releasenotes/notes/prepare-0.16.4-721822aa11ee7d60.yaml
    
    Co-authored-by: Luciano Bello <bel@zurich.ibm.com>
    
    Co-authored-by: Luciano Bello <bel@zurich.ibm.com>

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ with open(README_PATH) as readme_file:
 
 setup(
     name="qiskit-terra",
-    version="0.16.3",
+    version="0.16.4",
     description="Software for developing quantum computing programs",
     long_description=README,
     long_description_content_type='text/markdown',
