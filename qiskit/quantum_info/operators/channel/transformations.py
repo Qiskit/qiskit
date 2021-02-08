d29d691d31a311e4876bcd7507b8520a24d588f8
Christopher J. Wood <cjwood@us.ibm.com>
2021-02-08 17:26:47 -0500

    quantum_info.operator mixin interface refactor (#5617)
    * Add operator mixin classes
    
    Move tolerances to mixin folder
    
    * Update BaseOperator to use mixins
    
    * Update Operator and ScalarOp to use mixins
    
    * Update Pauli, BasePauli with mixins
    
    * Update Clifford, SparsePauliOp, PauliTable, StabilizerTable to use mixins
    
    * Update QuantumChannels to use mixins
    
    * Linting
    
    * Add release note
    
    * Split GateOpMixin into GroupMixin and AdjointMixin
    
    * Add LinearOp base class
    
    Subclass of BaseOperator that includes LinearMixin and TolerancesMixin
    
    * Remove deprecated np.bool
    
    * Remove _tensor, _compose from GroupMixin
    
    * Fix generation of API docs from Mixin base classes
    
    * Fix lint
    
    * Remove AdjointMixin from base operator
    
    * Forgot to add AdjointMixing to LinearOp
    
    * Fix qarg from removing _compose
    
    * fix typos
    
    * Remove trying to set doc of property
    
    * Linting

--- qiskit/quantum_info/operators/channel/transformations.py
+++ qiskit/quantum_info/operators/channel/transformations.py
@@ -25,6 +25,27 @@ from qiskit.quantum_info.operators.predicates import is_hermitian_matrix
 from qiskit.quantum_info.operators.predicates import ATOL_DEFAULT
 
 
+def _transform_rep(input_rep, output_rep, data, input_dim, output_dim):
+    """Transform a QuantumChannel between representation."""
+    if input_rep == output_rep:
+        return data
+    if output_rep == 'Choi':
+        return _to_choi(input_rep, data, input_dim, output_dim)
+    if output_rep == 'Operator':
+        return _to_operator(input_rep, data, input_dim, output_dim)
+    if output_rep == 'SuperOp':
+        return _to_superop(input_rep, data, input_dim, output_dim)
+    if output_rep == 'Kraus':
+        return _to_kraus(input_rep, data, input_dim, output_dim)
+    if output_rep == 'Chi':
+        return _to_chi(input_rep, data, input_dim, output_dim)
+    if output_rep == 'PTM':
+        return _to_ptm(input_rep, data, input_dim, output_dim)
+    if output_rep == 'Stinespring':
+        return _to_stinespring(input_rep, data, input_dim, output_dim)
+    raise QiskitError('Invalid QuantumChannel {}'.format(output_rep))
+
+
 def _to_choi(rep, data, input_dim, output_dim):
     """Transform a QuantumChannel to the Choi representation."""
     if rep == 'Choi':
