22eb83fca3bbbc3104085e6a1d23f76614aa996e
knzwnao <knzwnao@jp.ibm.com>
2021-02-15 01:13:57 +0900

    forward reference

--- qiskit/assembler/assemble_schedules.py
+++ qiskit/assembler/assemble_schedules.py
@@ -25,7 +25,8 @@ from qiskit.qobj.converters.pulse_instruction import ParametricPulseShapes
 
 
 def assemble_schedules(
-        schedules: List[Union[schedule.ScheduleComponent, Tuple[int, schedule.ScheduleComponent]]],
+        schedules: List[Union['schedule.ScheduleComponent',
+                              Tuple[int, 'schedule.ScheduleComponent']]],
         qobj_id: int,
         qobj_header: qobj.QobjHeader,
         run_config: RunConfig) -> qobj.PulseQobj:
@@ -62,7 +63,8 @@ def assemble_schedules(
 
 
 def _assemble_experiments(
-        schedules: List[Union[schedule.ScheduleComponent, Tuple[int, schedule.ScheduleComponent]]],
+        schedules: List[Union['schedule.ScheduleComponent',
+                              Tuple[int, 'schedule.ScheduleComponent']]],
         lo_converter: converters.LoConfigConverter,
         run_config: RunConfig
 ) -> Tuple[List[qobj.PulseQobjExperiment], Dict[str, Any]]:
