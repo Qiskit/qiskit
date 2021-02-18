5cca805073d087b0914f0c9aaa132c4e1e30808b
Luciano Bello <bel@zurich.ibm.com>
2021-02-18 10:28:59 +0100

    reverting to cleaning up some changes

--- qiskit/quantum_info/__init__.py
+++ qiskit/quantum_info/__init__.py
@@ -122,9 +122,7 @@ from .operators.measures import (process_fidelity,
                                  average_gate_fidelity,
                                  gate_error,
                                  diamond_norm)
-
-from .states.statevector import Statevector
-from .states.densitymatrix import DensityMatrix
+from .states import Statevector, DensityMatrix
 from .states import (partial_trace, state_fidelity, purity, entropy,
                      concurrence, entanglement_of_formation,
                      mutual_information, shannon_entropy)
