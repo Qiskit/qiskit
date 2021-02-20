af783782fb681a70e5164e0db522d36e3dadc0ff
Omar Costa Hamido <omarcostinha@gmail.com>
2021-02-20 01:16:16 -0800

    fix: 11:Block quote ends without a blank line

--- qiskit/tools/monitor/job_monitor.py
+++ qiskit/tools/monitor/job_monitor.py
@@ -9,6 +9,7 @@
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
+#
 
 
 """A module for monitoring various qiskit functionality
