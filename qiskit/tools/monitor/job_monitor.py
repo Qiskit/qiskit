05c9fafda1a60d8e135f09f2bf53cc6f673338d5
Omar Costa Hamido <omarcostinha@gmail.com>
2021-02-20 10:38:31 -0800

    deleting line 12

--- qiskit/tools/monitor/job_monitor.py
+++ qiskit/tools/monitor/job_monitor.py
@@ -9,7 +9,6 @@
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
-#
 
 
 """A module for monitoring various qiskit functionality
