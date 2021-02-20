4fd3ccf9b696a1e3907f5db0ef5552146b9e17c5
Omar Costa Hamido <omarcostinha@gmail.com>
2021-02-20 00:19:57 -0800

    fixed: missing whitespace after ','

--- qiskit/tools/monitor/job_monitor.py
+++ qiskit/tools/monitor/job_monitor.py
@@ -37,7 +37,7 @@ def _text_checker(job, interval, _interval_set=False, quiet=False, output=sys.st
     msg_len = len(msg)
 
     if not quiet:
-        print('%s%s: %s' % (line_discipline,'Job Status', msg), end='', file=output)
+        print('%s%s: %s' % (line_discipline, 'Job Status', msg), end='', file=output)
     while status.name not in ['DONE', 'CANCELLED', 'ERROR']:
         time.sleep(interval)
         status = job.status()
@@ -60,7 +60,7 @@ def _text_checker(job, interval, _interval_set=False, quiet=False, output=sys.st
             msg_len = len(msg)
 
         if msg != prev_msg and not quiet:
-            print('%s%s: %s' % (line_discipline,'Job Status', msg), end='', file=output)
+            print('%s%s: %s' % (line_discipline, 'Job Status', msg), end='', file=output)
             prev_msg = msg
     if not quiet:
         print('', file=output)
