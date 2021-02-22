f8e222e108bb471bb0f216298b3c0c698f1bb0fd
Luciano Bello <bel@zurich.ibm.com>
2021-02-22 13:12:38 +0100

    moving docs

--- qiskit/circuit/classicalfunction/boolean_expression.py
+++ qiskit/circuit/classicalfunction/boolean_expression.py
@@ -1,4 +1,4 @@
-# This code is part of Qiskit.
+    # This code is part of Qiskit.
 #
 # (C) Copyright IBM 2021.
 #
@@ -19,69 +19,7 @@ from .classicalfunction import HAS_TWEEDLEDUM
 
 
 class BooleanExpression(Gate):
-    r"""The Logical Expression Quantum Oracle.
-
-    The Logical Expression Oracle, as its name suggests, constructs circuits for any arbitrary
-    input logical expressions. A logical expression is composed of logical operators
-    `&` (`AND`), `|` (`OR`) and `~` (`NOT`),
-    as well as symbols for literals (variables).
-    For example, `'a & b'`, and `(v0 | ~v1) & (~v2 & v3)`
-    are both valid string representation of boolean logical expressions.
-
-    For convenience, this oracle, in addition to parsing arbitrary logical expressions,
-    also supports input strings in the `DIMACS CNF format
-    <http://www.satcompetition.org/2009/format-benchmarks2009.html>`__,
-    which is the standard format for specifying SATisfiability (SAT) problem instances in
-    `Conjunctive Normal Form (CNF) <https://en.wikipedia.org/wiki/Conjunctive_normal_form>`__,
-    which is a conjunction of one or more clauses, where a clause is a disjunction of one
-    or more literals.
-
-    The following is an example of a CNF expressed in the DIMACS format:
-
-    .. code:: text
-
-      c This is an example DIMACS CNF file with 3 satisfying assignments: 1 -2 3, -1 -2 -3, 1 2 -3.
-      p cnf 3 5
-      -1 -2 -3 0
-      1 -2 3 0
-      1 2 -3 0
-      1 -2 -3 0
-      -1 2 3 0
-
-    The first line, following the `c` character, is a comment. The second line specifies that the
-    CNF is over three boolean variables --- let us call them  :math:`x_1, x_2, x_3`, and contains
-    five clauses.  The five clauses, listed afterwards, are implicitly joined by the logical `AND`
-    operator, :math:`\land`, while the variables in each clause, represented by their indices,
-    are implicitly disjoined by the logical `OR` operator, :math:`lor`. The :math:`-` symbol
-    preceding a boolean variable index corresponds to the logical `NOT` operator, :math:`lnot`.
-    Character `0` (zero) marks the end of each clause.  Essentially, the code above corresponds
-    to the following CNF:
-
-    :math:`(\lnot x_1 \lor \lnot x_2 \lor \lnot x_3)
-    \land (x_1 \lor \lnot x_2 \lor x_3)
-    \land (x_1 \lor x_2 \lor \lnot x_3)
-    \land (x_1 \lor \lnot x_2 \lor \lnot x_3)
-    \land (\lnot x_1 \lor x_2 \lor x_3)`.
-
-    This is an example showing how to search for a satisfying assignment to an SAT problem encoded
-    in DIMACS using the `Logical Expression oracle with the Grover algorithm.
-    <https://github.com/Qiskit/qiskit-tutorials-community/blob/master/optimization/grover.ipynb>`__
-
-    Logic expressions, regardless of the input formats, are parsed and stored as a source string,
-    from which the corresponding circuits are constructed by
-    :class:`~qiskit.circuit.classicalfunction.ClassicalFunction`.
-    The oracle circuits
-    can then be used with any oracle-oriented algorithms when appropriate. For example, an oracle
-    built from a DIMACS input can be used with the Grover's algorithm to search for a satisfying
-    assignment to the encoded SAT instance.
-
-    Examples:
-        >>> from qiskit.circuit import BooleanExpression
-        >>> from qiskit.algorithms.amplitude_amplifiers import Grover
-        >>> expr = "(x0 & x1 | ~x2) ^ x4"
-        >>> oracle = BooleanExpression(expr)
-        >>> grover = Grover(oracle=oracle, good_state=oracle.evaluate_bitstring)
-    """
+    """The Boolean Expression gate."""
 
     def __init__(self, expression: str, name: str = None) -> None:
         """
@@ -160,7 +98,7 @@ class BooleanExpression(Gate):
             filename (str): A file in DIMACS format.
 
         Returns:
-            BooleanExpression: A quantum circuit for the input string
+            BooleanExpression: A gate for the input string
 
         Raises:
             ImportError: If tweedledum is not installed. Tweedledum is required.
--- qiskit/circuit/library/phase_oracle.py
+++ qiskit/circuit/library/phase_oracle.py
@@ -17,12 +17,32 @@ from qiskit.circuit.classicalfunction.boolean_expression import BooleanExpressio
 
 
 class PhaseOracle(QuantumCircuit):
-    """Phase Oracle."""
+    r"""Phase Oracle.
+
+    The Phase Oracle object constructs circuits for any arbitrary
+    input logical expressions. A logical expression is composed of logical operators
+    `&` (`AND`), `|` (`OR`), `~` (`NOT`), and `^` (`XOR`).
+    as well as symbols for literals (variables).
+    For example, `'a & b'`, and `(v0 | ~v1) & (~v2 & v3)`
+    are both valid string representation of boolean logical expressions.
+
+    For convenience, this oracle, in addition to parsing arbitrary logical expressions,
+    also supports input strings in the `DIMACS CNF format
+    <http://www.satcompetition.org/2009/format-benchmarks2009.html>`__,
+    which is the standard format for specifying SATisfiability (SAT) problem instances in
+    `Conjunctive Normal Form (CNF) <https://en.wikipedia.org/wiki/Conjunctive_normal_form>`__,
+    which is a conjunction of one or more clauses, where a clause is a disjunction of one
+    or more literals. See :meth:`qiskit.circuit.library.phase_oracle.PhaseOracle.from_dimacs_file`.
+    """
 
     def __init__(self, expression: str) -> None:
-        self.bit_oracle = BooleanExpression(expression)
-        self.state_qubits = range(self.bit_oracle.num_qubits - 1)   # input qubits for the oracle
+        self.boolean_expression = BooleanExpression(expression)
+        # input qubits for the oracle
+        self.state_qubits = range(self.boolean_expression.num_qubits - 1)
 
+        self.compose(self._build_from_boolean_expression(), inplace=True)
+
+    def _build_from_boolean_expression(self):
         # initialize the quantumcircuit
         qr_state = QuantumRegister(len(self.state_qubits), 'state')
 
@@ -32,7 +52,7 @@ class PhaseOracle(QuantumCircuit):
         synthesizer = lambda logic_network: pkrm_synth(logic_network,
                                                        {"pkrm_synth": {"phase_esop": True}})
 
-        self.compose(self.bit_oracle.synth(synthesizer=synthesizer), inplace=True)
+        return self.boolean_expression.synth(synthesizer=synthesizer)
 
     def evaluate_bitstring(self, bitstring: str) -> bool:
         """Evaluate the oracle on a bitstring.
@@ -42,4 +62,59 @@ class PhaseOracle(QuantumCircuit):
         Returns:
             True if the bitstring is a good state, False otherwise.
         """
-        return self.bit_oracle.simulate(bitstring)
+        return self.boolean_expression.simulate(bitstring)
+
+    @classmethod
+    def from_dimacs_file(cls, filename: str):
+        """Create a PhaseOracle from the string in the DIMACS format.
+
+        It is possible to build a PhaseOracle from a file in `DIMACS CNF format
+        <http://www.satcompetition.org/2009/format-benchmarks2009.html>`__,
+        which is the standard format for specifying SATisfiability (SAT) problem instances in
+        `Conjunctive Normal Form (CNF) <https://en.wikipedia.org/wiki/Conjunctive_normal_form>`__,
+        which is a conjunction of one or more clauses, where a clause is a disjunction of one
+        or more literals.
+
+        The following is an example of a CNF expressed in the DIMACS format:
+
+        .. code:: text
+
+          c This is an example DIMACS CNF file with 3 satisfying assignments: 1 -2 3, -1 -2 -3, 1 2 -3.
+          p cnf 3 5
+          -1 -2 -3 0
+          1 -2 3 0
+          1 2 -3 0
+          1 -2 -3 0
+          -1 2 3 0
+
+        The first line, following the `c` character, is a comment. The second line specifies that the
+        CNF is over three boolean variables --- let us call them  :math:`x_1, x_2, x_3`, and contains
+        five clauses.  The five clauses, listed afterwards, are implicitly joined by the logical `AND`
+        operator, :math:`\land`, while the variables in each clause, represented by their indices,
+        are implicitly disjoined by the logical `OR` operator, :math:`lor`. The :math:`-` symbol
+        preceding a boolean variable index corresponds to the logical `NOT` operator, :math:`lnot`.
+        Character `0` (zero) marks the end of each clause.  Essentially, the code above corresponds
+        to the following CNF:
+
+        :math:`(\lnot x_1 \lor \lnot x_2 \lor \lnot x_3)
+        \land (x_1 \lor \lnot x_2 \lor x_3)
+        \land (x_1 \lor x_2 \lor \lnot x_3)
+        \land (x_1 \lor \lnot x_2 \lor \lnot x_3)
+        \land (\lnot x_1 \lor x_2 \lor x_3)`.
+
+
+        Args:
+            filename (str): A file in DIMACS format.
+
+        Returns:
+            PhaseOracle: A quantum circuit with a phase oracle.
+        """
+        phase_oracle = cls.__new__(cls)
+        phase_oracle.boolean_expression = BooleanExpression.from_dimacs_file(filename)
+
+        # input qubits for the oracle
+        phase_oracle.state_qubits = range(phase_oracle.boolean_expression.num_qubits - 1)
+
+        phase_oracle.compose(phase_oracle._build_from_boolean_expression(), inplace=True)
+
+        return phase_oracle
