09bb59b029f79f64fb215809a4f34486a0f6e745
Daniel Egger <38065505+eggerdj@users.noreply.github.com>
2021-03-01 12:33:09 +0100

    Fix to carry over calibrations with __iadd__ and __add__ (#5910)
    * * Added fix to carry over calibrations with __iadd__ and __add__.
    * Added corresponding test.
    
    * * Added reno.
    
    * * Lint fix.
    
    * * Switched to add_calibration.
    
    * * Removed the redundent comments.

--- qiskit/circuit/quantumcircuit.py
+++ qiskit/circuit/quantumcircuit.py
@@ -566,6 +566,11 @@ class QuantumCircuit:
         for instruction_context in itertools.chain(self.data, rhs.data):
             circuit._append(*instruction_context)
         circuit.global_phase = self.global_phase + rhs.global_phase
+
+        for gate, cals in rhs.calibrations.items():
+            for key, sched in cals.items():
+                circuit.add_calibration(gate, qubits=key[0], schedule=sched, params=key[1])
+
         return circuit
 
     def extend(self, rhs):
@@ -610,6 +615,11 @@ class QuantumCircuit:
         for instruction_context in data:
             self._append(*instruction_context)
         self.global_phase += rhs.global_phase
+
+        for gate, cals in rhs.calibrations.items():
+            for key, sched in cals.items():
+                self.add_calibration(gate, qubits=key[0], schedule=sched, params=key[1])
+
         return self
 
     def compose(self, other, qubits=None, clbits=None, front=False, inplace=False):
