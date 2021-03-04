18ec52d8bd4b4faa5d0f19d1173f8f5d5e3cbce2
Miroslav Tomasik <mirotomasik@gmail.com>
2021-03-04 16:51:27 +0100

    Update qiskit/opflow/gradients/hessian.py
    Co-authored-by: Julien Gacon <gaconju@gmail.com>

--- qiskit/opflow/gradients/hessian.py
+++ qiskit/opflow/gradients/hessian.py
@@ -56,8 +56,9 @@ class Hessian(HessianBase):
             params: The parameters we are computing the Hessian with respect to
                     Either give directly the tuples/list of tuples for which the second order
                     derivative is to be computed or give a list of parameters to build the
-                    full Hessian for those parameters. If not explicitly passed, params are inferred
-                    from operator and sorted by name.
+                    full Hessian for those parameters. If not explicitly passed, the full Hessian
+                    is constructed. The parameters are then inferred from the operator and 
+                    sorted by name.
 
         Returns:
             OperatorBase: An operator whose evaluation yields the Hessian
