fc30007de6d0866997e6eeb753080c419a51d0dd
Daniel Egger <deg@zurich.ibm.com>
2021-03-10 18:51:33 +0100

    * Fix lint.

--- test/python/pulse/test_frames.py
+++ test/python/pulse/test_frames.py
@@ -23,6 +23,7 @@ from qiskit.pulse.resolved_frame import ResolvedFrame
 
 
 class TestFrame(QiskitTestCase):
+    """Basic frame tests."""
 
     def basic(self):
         """Test basic functionality of frames."""
@@ -44,9 +45,9 @@ class TestResolvedFrames(QiskitTestCase):
 
     def setUp(self):
         super().setUp()
-        self.dt = 2.2222222222222221e-10
-        self.f0 = 0.093e9  # Frequency of frame 0
-        self.f1 = 0.125e9  # Frequency of frame 1
+        self.dt_ = 2.2222222222222221e-10
+        self.freq0 = 0.093e9  # Frequency of frame 0
+        self.freq1 = 0.125e9  # Frequency of frame 1
 
     def test_phase_advance(self):
         """Test that phases are properly set when frames are resolved."""
@@ -61,26 +62,27 @@ class TestResolvedFrames(QiskitTestCase):
             pulse.play(sig0, d0)
             pulse.play(sig1, d0)
 
-        frames_config = frames_configuration([[d0], [d0]], [self.f0, self.f1], self.dt)
+        frames_config = frames_configuration([[d0], [d0]], [self.freq0, self.freq1], self.dt_)
 
-        frame0_ = ResolvedFrame(pulse.Frame(1), self.f0, 0.0, self.dt, [])
-        frame1_ = ResolvedFrame(pulse.Frame(2), self.f1, 0.0, self.dt, [])
+        frame0_ = ResolvedFrame(pulse.Frame(1), self.freq0, 0.0, self.dt_, [])
+        frame1_ = ResolvedFrame(pulse.Frame(2), self.freq1, 0.0, self.dt_, [])
 
         # Check that the resolved frames are tracking phases properly
         for time in [0, 160, 320, 480]:
-            phase = np.angle(np.exp(2.0j*np.pi*self.f0*time*self.dt)) % (2*np.pi)
+            phase = np.angle(np.exp(2.0j * np.pi * self.freq0 * time * self.dt_)) % (2 * np.pi)
             self.assertAlmostEqual(frame0_.phase(time), phase, places=8)
 
-            phase = np.angle(np.exp(2.0j * np.pi * self.f1 * time * self.dt)) % (2 * np.pi)
+            phase = np.angle(np.exp(2.0j * np.pi * self.freq1 * time * self.dt_)) % (2 * np.pi)
             self.assertAlmostEqual(frame1_.phase(time), phase, places=8)
 
         # Check that the proper phase instructions are added to the frame resolved schedules.
         resolved = resolve_frames(sched, frames_config).instructions
 
-        params = [(0, self.f0, 1), (160, self.f1, 4), (320, self.f0, 7), (480, self.f1, 10)]
+        params = [(0, self.freq0, 1), (160, self.freq1, 4),
+                  (320, self.freq0, 7), (480, self.freq1, 10)]
 
         for time, frame_frequency, index in params:
-            phase = np.angle(np.exp(2.0j*np.pi*frame_frequency*time*self.dt)) % (2*np.pi)
+            phase = np.angle(np.exp(2.0j * np.pi * frame_frequency * time * self.dt_)) % (2 * np.pi)
             self.assertEqual(resolved[index][0], time)
             self.assertAlmostEqual(resolved[index][1].phase, phase, places=8)
 
@@ -93,34 +95,34 @@ class TestResolvedFrames(QiskitTestCase):
             pulse.play(sig, pulse.DriveChannel(0))
             pulse.shift_phase(1.0, pulse.Frame(0))
 
-        frame = ResolvedFrame(pulse.Frame(0), self.f0, 0.0, self.dt, [])
+        frame = ResolvedFrame(pulse.Frame(0), self.freq0, 0.0, self.dt_, [])
         frame.set_frame_instructions(sched)
 
         self.assertAlmostEqual(frame.phase(0), 0.0, places=8)
 
         # Test the phase right before the shift phase instruction
-        phase = np.angle(np.exp(2.0j*np.pi*self.f0*159*self.dt)) % (2*np.pi)
+        phase = np.angle(np.exp(2.0j * np.pi * self.freq0 * 159 * self.dt_)) % (2 * np.pi)
         self.assertAlmostEqual(frame.phase(159), phase, places=8)
 
         # Test the phase at and after the shift phase instruction
-        phase = (np.angle(np.exp(2.0j * np.pi * self.f0 * 160 * self.dt)) + 1.0) % (2 * np.pi)
+        phase = (np.angle(np.exp(2.0j * np.pi * self.freq0 * 160 * self.dt_)) + 1.0) % (2 * np.pi)
         self.assertAlmostEqual(frame.phase(160), phase, places=8)
 
-        phase = (np.angle(np.exp(2.0j * np.pi * self.f0 * 161 * self.dt)) + 1.0) % (2 * np.pi)
+        phase = (np.angle(np.exp(2.0j * np.pi * self.freq0 * 161 * self.dt_)) + 1.0) % (2 * np.pi)
         self.assertAlmostEqual(frame.phase(161), phase, places=8)
 
     def test_set_frequency(self):
         """Test setting the frequency of the resolved frame."""
 
-        frame = ResolvedFrame(pulse.Frame(0), self.f1, 0.0, self.dt, [])
-        frame.set_frequency(16, self.f0)
-        frame.set_frequency(10, self.f1)
-        frame.set_frequency(10, self.f1)
+        frame = ResolvedFrame(pulse.Frame(0), self.freq1, 0.0, self.dt_, [])
+        frame.set_frequency(16, self.freq0)
+        frame.set_frequency(10, self.freq1)
+        frame.set_frequency(10, self.freq1)
 
-        self.assertAlmostEqual(frame.frequency(0), self.f1, places=8)
-        self.assertAlmostEqual(frame.frequency(10), self.f1, places=8)
-        self.assertAlmostEqual(frame.frequency(15), self.f1, places=8)
-        self.assertAlmostEqual(frame.frequency(16), self.f0, places=8)
+        self.assertAlmostEqual(frame.frequency(0), self.freq1, places=8)
+        self.assertAlmostEqual(frame.frequency(10), self.freq1, places=8)
+        self.assertAlmostEqual(frame.frequency(15), self.freq1, places=8)
+        self.assertAlmostEqual(frame.frequency(16), self.freq0, places=8)
 
     def test_broadcasting(self):
         """Test that resolved frames broadcast to control channels."""
@@ -136,11 +138,11 @@ class TestResolvedFrames(QiskitTestCase):
                     pulse.play(sig, pulse.ControlChannel(0))
 
         frames_config = frames_configuration([[pulse.DriveChannel(3), pulse.ControlChannel(0)]],
-                                             [self.f0], self.dt)
+                                             [self.freq0], self.dt_)
 
         resolved = resolve_frames(sched, frames_config).instructions
 
-        phase = (np.angle(np.exp(2.0j * np.pi * self.f0 * 160 * self.dt)) + 1.23) % (2 * np.pi)
+        phase = (np.angle(np.exp(2.0j * np.pi * self.freq0 * 160 * self.dt_)) + 1.23) % (2 * np.pi)
 
         self.assertAlmostEqual(resolved[1][1].phase, 0.0, places=8)
         self.assertAlmostEqual(resolved[4][1].phase, phase, places=8)
