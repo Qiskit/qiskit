d5cf9fd85f5d887cf33fb47796455cbfaa093553
Jesus Sistos <jesussistos.josb@gmail.com>
2021-03-11 23:52:44 -0500

    Reversing Bloch_multivector to accomodate qiskit ordering (#5959)

--- qiskit/visualization/state_visualization.py
+++ qiskit/visualization/state_visualization.py
@@ -257,14 +257,15 @@ def plot_bloch_multivector(state, title='', figsize=None, *, rho=None):
     from matplotlib import get_backend
     from matplotlib import pyplot as plt
 
-    # Data
-    bloch_data = _bloch_multivector_data(state)
+    # Data gets reversed to account for qubit ordering
+    bloch_data = _bloch_multivector_data(state)[::-1]
     num = len(bloch_data)
     width, height = plt.figaspect(1/num)
     fig = plt.figure(figsize=(width, height))
     for i in range(num):
+        pos = num - 1 - i
         ax = fig.add_subplot(1, num, i + 1, projection='3d')
-        plot_bloch_vector(bloch_data[i], "qubit " + str(i), ax=ax,
+        plot_bloch_vector(bloch_data[i], "qubit " + str(pos), ax=ax,
                           figsize=figsize)
     fig.suptitle(title, fontsize=16)
     if get_backend() in ['module://ipykernel.pylab.backend_inline',
