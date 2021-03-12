3a97c76c286e5b5df9999f5a643a439c32483097
Jesus Sistos <jesussistos.josb@gmail.com>
2021-03-12 18:08:24 -0500

    Changed reverse_order to reverse_bits

--- qiskit/visualization/state_visualization.py
+++ qiskit/visualization/state_visualization.py
@@ -218,7 +218,7 @@ def plot_bloch_vector(bloch, title="", ax=None, figsize=None, coord_type="cartes
 
 
 @deprecate_arguments({'rho': 'state'})
-def plot_bloch_multivector(state, title='', figsize=None, *, rho=None, reverse_order=False):
+def plot_bloch_multivector(state, title='', figsize=None, *, rho=None, reverse_bits=False):
     """Plot the Bloch sphere.
 
     Plot a sphere, axes, the Bloch vector, and its projections onto each axis.
@@ -227,7 +227,7 @@ def plot_bloch_multivector(state, title='', figsize=None, *, rho=None, reverse_o
         state (Statevector or DensityMatrix or ndarray): an N-qubit quantum state.
         title (str): a string that represents the plot title
         figsize (tuple): Has no effect, here for compatibility only.
-        reverse_order (bool): If True, plots qubits following Qiskit's convention [Default:False].
+        reverse_bits (bool): If True, plots qubits following Qiskit's convention [Default:False].
 
     Returns:
         matplotlib.Figure:
@@ -250,7 +250,7 @@ def plot_bloch_multivector(state, title='', figsize=None, *, rho=None, reverse_o
             qc.cx(0, 1)
 
             state = Statevector.from_instruction(qc)
-            plot_bloch_multivector(state, title="New Bloch Multivector", reverse_order=False)
+            plot_bloch_multivector(state, title="New Bloch Multivector", reverse_bits=False)
     """
     if not HAS_MATPLOTLIB:
         raise ImportError('Must have Matplotlib installed. To install, run '
@@ -260,13 +260,13 @@ def plot_bloch_multivector(state, title='', figsize=None, *, rho=None, reverse_o
 
     # Data
     bloch_data = (_bloch_multivector_data(state)[::-1]
-                  if reverse_order
+                  if reverse_bits
                   else _bloch_multivector_data(state))
     num = len(bloch_data)
     width, height = plt.figaspect(1/num)
     fig = plt.figure(figsize=(width, height))
     for i in range(num):
-        pos = num - 1 - i if reverse_order else i
+        pos = num - 1 - i if reverse_bits else i
         ax = fig.add_subplot(1, num, i + 1, projection='3d')
         plot_bloch_vector(bloch_data[i], "qubit " + str(pos), ax=ax,
                           figsize=figsize)
