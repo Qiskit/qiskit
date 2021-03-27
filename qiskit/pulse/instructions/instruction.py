73a332e2bea590da38dd425e48ad8baf586b8e72
knzwnao <knzwnao@jp.ibm.com>
2021-03-27 12:29:11 +0900

    channels become abstract

--- qiskit/pulse/instructions/instruction.py
+++ qiskit/pulse/instructions/instruction.py
@@ -22,7 +22,7 @@ For example::
     sched += Delay(duration, channel)  # Delay is a specific subclass of Instruction
 """
 import warnings
-from abc import ABC
+from abc import ABC, abstractproperty
 from collections import defaultdict
 from typing import Callable, Dict, Iterable, List, Optional, Set, Tuple, Any
 
@@ -96,7 +96,7 @@ class Instruction(ABC):
         """Return instruction operands."""
         return self._operands
 
-    @property
+    @abstractproperty
     def channels(self) -> Tuple[Channel]:
         """Returns channels that this schedule uses."""
         raise NotImplementedError
