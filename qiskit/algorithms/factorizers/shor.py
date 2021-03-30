24fb5ef5ced5a124de6e303da8d7b11840cf62f3
Julien Gacon <gaconju@gmail.com>
2021-03-30 13:33:48 +0200

    rm unnecessary f-string

--- qiskit/algorithms/factorizers/shor.py
+++ qiskit/algorithms/factorizers/shor.py
@@ -108,7 +108,7 @@ class Shor:
                                          angles: Union[np.ndarray, ParameterVector]
                                          ) -> QuantumCircuit:
         """Creates a circuit which implements double-controlled modular addition by a."""
-        circuit = QuantumCircuit(num_qubits, name=f"ccphi_add_mod_N")
+        circuit = QuantumCircuit(num_qubits, name="ccphi_add_mod_N")
 
         ctl_up = 0
         ctl_down = 1
