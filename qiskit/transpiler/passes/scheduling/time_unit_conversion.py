ba1ae3c651a844c851522760b95b86e919a30723
Ali Javadi-Abhari <ajavadia@users.noreply.github.com>
2021-03-31 22:38:55 +0100

    no full scheduling of circuits with delay unless explicitly requested (#6084)
    * Skip auto scheduling of circuits with delay when transpiling for backends without dt
    
    * remove implicit scheduling, instead do a pass for unit conversion
    
    * simplify alap/asap schedulers
    
    * ALAP/ASAP scheduling now require TimeUnitConversion
    
    * lint
    
    * ..
    
    * review comments
    
    * remove accidental print
    
    * lint
    
    * not use bit.index
    
    * remove bit.index again
    
    Co-authored-by: Toshinari Itoko <itoko@jp.ibm.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- qiskit/transpiler/passes/scheduling/time_unit_conversion.py
+++ qiskit/transpiler/passes/scheduling/time_unit_conversion.py
@@ -1,6 +1,6 @@
 # This code is part of Qiskit.
 #
-# (C) Copyright IBM 2020.
+# (C) Copyright IBM 2021.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -10,19 +10,22 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Choose a time unit to be used in the scheduling and its following passes."""
+"""Unify time unit in circuit for scheduling and following passes."""
 from typing import Set
 
 from qiskit.circuit import Delay
 from qiskit.dagcircuit import DAGCircuit
-from qiskit.transpiler.basepasses import AnalysisPass
+from qiskit.transpiler.basepasses import TransformationPass
 from qiskit.transpiler.exceptions import TranspilerError
 from qiskit.transpiler.instruction_durations import InstructionDurations
 
 
-class TimeUnitAnalysis(AnalysisPass):
-    """Choose a time unit to be used in the following passes
-    (e.g. scheduling pass and dynamical decoupling pass).
+class TimeUnitConversion(TransformationPass):
+    """Choose a time unit to be used in the following time-aware passes,
+    and make all circuit time units consistent with that.
+
+    This pass will add a .duration metadata to each op whose duration is known,
+    which will be used by subsequent scheduling passes for scheduling.
 
     If dt (dt in seconds) is known to transpiler, the unit 'dt' is chosen. Otherwise,
     the unit to be selected depends on what units are used in delays and instruction durations:
@@ -38,7 +41,7 @@ class TimeUnitAnalysis(AnalysisPass):
             inst_durations (InstructionDurations): A dictionary of durations of instructions.
         """
         super().__init__()
-        self.inst_durations = inst_durations
+        self.inst_durations = inst_durations or InstructionDurations()
 
     def run(self, dag: DAGCircuit):
         """Run the TimeUnitAnalysis pass on `dag`.
@@ -46,11 +49,15 @@ class TimeUnitAnalysis(AnalysisPass):
         Args:
             dag (DAGCircuit): DAG to be checked.
 
+        Returns:
+            DAGCircuit: DAG with consistent timing and op nodes annotated with duration.
+
         Raises:
             TranspilerError: if the units are not unifiable
         """
+        # Choose unit
         if self.inst_durations.dt is not None:
-            self.property_set['time_unit'] = 'dt'
+            time_unit = 'dt'
         else:
             # Check what units are used in delays and other instructions: dt or SI or mixed
             units_delay = self._units_used_in_delays(dag)
@@ -64,12 +71,28 @@ class TimeUnitAnalysis(AnalysisPass):
 
             unified_unit = self._unified(units_delay | units_other)
             if unified_unit == "SI":
-                self.property_set['time_unit'] = 's'
+                time_unit = 's'
             elif unified_unit == "dt":
-                self.property_set['time_unit'] = 'dt'
+                time_unit = 'dt'
             else:
                 raise TranspilerError("Fail to unify time units. SI units "
                                       "and dt unit must not be mixed when dt is not supplied.")
+
+        # Make units consistent
+        bit_indices = {bit: index for index, bit in enumerate(dag.qubits)}
+        for node in dag.op_nodes():
+            try:
+                node.op = node.op.copy()
+                node.op.duration = self.inst_durations.get(
+                        node.op,
+                        [bit_indices[qarg] for qarg in node.qargs],
+                        unit=time_unit)
+                node.op.unit = time_unit
+            except TranspilerError:
+                pass
+
+        self.property_set['time_unit'] = time_unit
+        return dag
 
     @staticmethod
     def _units_used_in_delays(dag: DAGCircuit) -> Set[str]:
