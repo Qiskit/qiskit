fd9bd8fc0ca4f20b02cfe5daa16c2a8ddef5dfd9
Soham Bopardikar <bopardikarsoham@gmail.com>
2021-04-06 15:31:38 +0530

    Fixed Typo in docstring for MCX gate
    (cherry picked from commit b7a9c953d875d0bd352f550a9050f5926d8aa1ce)

--- qiskit/circuit/quantumcircuit.py
+++ qiskit/circuit/quantumcircuit.py
@@ -2530,7 +2530,7 @@ class QuantumCircuit:
 
         The multi-cX gate can be implemented using different techniques, which use different numbers
         of ancilla qubits and have varying circuit depth. These modes are:
-        - 'no-ancilla': Requires 0 ancilla qubits.
+        - 'noancilla': Requires 0 ancilla qubits.
         - 'recursion': Requires 1 ancilla qubit if more than 4 controls are used, otherwise 0.
         - 'v-chain': Requires 2 less ancillas than the number of control qubits.
         - 'v-chain-dirty': Same as for the clean ancillas (but the circuit will be longer).
