03b3011bc7dc8f1ee311fb6869e2304ca59169cb
knzwnao <knzwnao@jp.ibm.com>
2021-04-06 15:59:25 +0900

    update jupyter execute (remove builder syntax)

--- qiskit/pulse/builder.py
+++ qiskit/pulse/builder.py
@@ -1151,29 +1151,9 @@ def pad(*chs: chans.Channel) -> ContextManager[None]:  # pylint: disable=unused-
 
     Yields:
         None
-
-    Examples:
-
-    .. jupyter-execute::
-
-        from qiskit import pulse
-
-        d0 = pulse.DriveChannel(0)
-        d1 = pulse.DriveChannel(1)
-
-        with pulse.build() as pulse_prog:
-            with pulse.pad():
-                with pulse.align_right():
-                    # this pulse will start at t=0
-                    pulse.play(pulse.Constant(100, 1.0), d0)
-                    # this pulse will start at t=80
-                    # a delay will be inserted from t=0 to t=80
-                    pulse.play(pulse.Constant(20, 1.0), d1)
-        assert pulse_prog.ch_start_time(d0) == pulse_prog.ch_start_time(d1)
-        assert pulse_prog.ch_stop_time(d0) == pulse_prog.ch_stop_time(d1)
     """
     warnings.warn('Context-wise padding is being deprecated. This padding is just ignored. '
-                  'Set the padding process in the pulse transformation pass. ',
+                  'Set the padding process in the pulse transformation pass. '
                   'This transform pass feature will be supported soon.', DeprecationWarning)
     try:
         yield
