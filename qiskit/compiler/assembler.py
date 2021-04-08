b365e30feedfbb4c403e84deb754320471cde24a
Daniel Egger <deg@zurich.ibm.com>
2021-04-08 11:58:24 +0200

    * Renamed sample_duration to dt.

--- qiskit/compiler/assembler.py
+++ qiskit/compiler/assembler.py
@@ -478,7 +478,7 @@ def _expand_parameters(circuits, run_config):
 
 def frames_configuration(frame_channels: List[List[PulseChannel]],
                          frame_frequencies: List[float],
-                         sample_duration: float,
+                         dt: float,
                          frame_indices: List[int] = None) -> Union[dict, None]:
     """
     Ties together the frames of the backend and the frequencies of the frames.
@@ -487,7 +487,7 @@ def frames_configuration(frame_channels: List[List[PulseChannel]],
         frame_channels: A List of lists. Sublist i is a list of channel names
             that frame i will broadcast on.
         frame_frequencies: A list of starting frequencies for each frame.
-        sample_duration: time of a sample.
+        dt: time of a sample.
         frame_indices: The indices of the frames. If None is given these will be
             in ascending order starting from 0.
 
@@ -515,7 +515,7 @@ def frames_configuration(frame_channels: List[List[PulseChannel]],
             'phase': 0.0,
             'frequency': frame_frequencies[idx],
             'channels': channels,
-            'sample_duration': sample_duration
+            'sample_duration': dt
         }
 
     return frames_config
