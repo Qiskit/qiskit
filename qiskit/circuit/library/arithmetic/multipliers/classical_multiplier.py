89cc44f9d09846930b1c2cea1e220d41a2e3f7b5
Mantas Cepulkovskis <mantas.cepulkovskis@gmail.com>
2021-04-18 18:39:55 +0300

    Update docstring

--- qiskit/circuit/library/arithmetic/multipliers/classical_multiplier.py
+++ qiskit/circuit/library/arithmetic/multipliers/classical_multiplier.py
@@ -16,12 +16,58 @@ from qiskit.circuit import QuantumCircuit, QuantumRegister, AncillaRegister
 
 
 class ClassicalMultiplier(QuantumCircuit):
-    r"""A circuit that uses classical multiplication approach to store product of two input registers out-of-place.
+    r"""A multiplication circuit to store product of two input registers out-of-place.
+
+    Circuit to compute the product of two qubit registers using the approach from [1].
+    Given two equally sized input registers that store quantum states
+    :math:`|a\rangle` and :math:`|b\rangle`, performs multiplication of numbers that
+    can be represented by the states, storing the resulting state out-of-place
+    in a third output register:
+
+    .. math::
+
+        |a\rangle |b\rangle |0\rangle \mapsto |a\rangle |b\rangle |a \cdot b\rangle
+
+    Here :math:`|a\rangle` (and correspondingly :math:`|b\rangle`) stands for the direct product
+    :math:`|a_n\rangle \otimes |a_{n-1}\rangle \ldots |a_{1}\rangle \otimes |a_{0}\rangle`
+    which denotes a quantum register prepared with the value :math:`a = 2^{0}a_{0} + 2^{1}a_{1} +
+    \ldots 2^{n}a_{n}` [2].
+
+    As an example, a multiplier circuit that performs multiplication on two 2-qubit sized
+    registers is as follows:
+
+    .. parsed-literal::
+
+          a_0: ──────────■───────────────────────────────
+                         │
+          a_1: ──────────┼────────────────────■──────────
+               ┌─────────┴─────────┐┌─────────┴─────────┐
+          b_0: ┤0                  ├┤0                  ├
+               │                   ││                   │
+          b_1: ┤1                  ├┤1                  ├
+               │                   ││                   │
+        out_0: ┤2                  ├┤                   ├
+               │                   ││                   │
+        out_1: ┤3 RippleCarryAdder ├┤2 RippleCarryAdder ├
+               │                   ││                   │
+        out_2: ┤4                  ├┤3                  ├
+               │                   ││                   │
+        out_3: ┤                   ├┤4                  ├
+               │                   ││                   │
+        aux_0: ┤5                  ├┤5                  ├
+               └───────────────────┘└───────────────────┘
+
+    Multiplication in this circuit is implemented in a classical approach by performing
+    a series of shifted additions using one of the input registers while the qubits
+    from the other input register act as control qubits for the adders.
 
     **References:**
 
     [1] Häner et al., Optimizing Quantum Circuits for Arithmetic, 2018.
     `arXiv:1805.12445 <https://arxiv.org/pdf/1805.12445.pdf>`_
+
+    [2] Vedral et al., Quantum Networks for Elementary Arithmetic Operations, 1995.
+    `arXiv:quant-ph/9511018 <https://arxiv.org/pdf/quant-ph/9511018.pdf>`_
 
     """
 
