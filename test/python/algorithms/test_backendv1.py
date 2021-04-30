09a8c849bad08c696952b9899a4a0311c3a8ec9d
Manoel Marques <manoel.marques@ibm.com>
2021-04-30 17:03:13 -0400

    Add error mitigation test

--- test/python/algorithms/test_backendv1.py
+++ test/python/algorithms/test_backendv1.py
@@ -16,12 +16,12 @@ import unittest
 from test.python.algorithms import QiskitAlgorithmsTestCase
 from qiskit import QuantumCircuit
 from qiskit.test.mock import FakeProvider
-from qiskit.utils import QuantumInstance
+from qiskit.utils import QuantumInstance, algorithm_globals
 from qiskit.algorithms import (Shor, VQE,
                                Grover, AmplificationProblem)
 from qiskit.opflow import X, Z, I
 from qiskit.algorithms.optimizers import SPSA
-from qiskit.circuit.library import TwoLocal
+from qiskit.circuit.library import TwoLocal, EfficientSU2
 
 
 class TestBackendV1(QiskitAlgorithmsTestCase):
@@ -78,6 +78,50 @@ class TestBackendV1(QiskitAlgorithmsTestCase):
         grover = Grover(quantum_instance=qi)
         result = grover.amplify(problem)
         self.assertIn(result.top_measurement, ['11'])
+
+    def test_measurement_error_mitigation_with_vqe(self):
+        """ measurement error mitigation test with vqe """
+        try:
+            from qiskit.ignis.mitigation.measurement import CompleteMeasFitter
+            from qiskit.providers.aer import noise
+        except ImportError as ex:
+            self.skipTest("Package doesn't appear to be installed. Error: '{}'".format(str(ex)))
+            return
+
+        algorithm_globals.random_seed = 0
+
+        # build noise model
+        noise_model = noise.NoiseModel()
+        read_err = noise.errors.readout_error.ReadoutError([[0.9, 0.1], [0.25, 0.75]])
+        noise_model.add_all_qubit_readout_error(read_err)
+
+        backend = self._qasm
+
+        quantum_instance = QuantumInstance(
+            backend=backend,
+            seed_simulator=167,
+            seed_transpiler=167,
+            noise_model=noise_model,
+            measurement_error_mitigation_cls=CompleteMeasFitter
+        )
+
+        h2_hamiltonian = -1.052373245772859 * (I ^ I) \
+            + 0.39793742484318045 * (I ^ Z) \
+            - 0.39793742484318045 * (Z ^ I) \
+            - 0.01128010425623538 * (Z ^ Z) \
+            + 0.18093119978423156 * (X ^ X)
+        optimizer = SPSA(maxiter=200)
+        ansatz = EfficientSU2(2, reps=1)
+
+        vqe = VQE(
+            ansatz=ansatz,
+            optimizer=optimizer,
+            quantum_instance=quantum_instance
+        )
+        result = vqe.compute_minimum_eigenvalue(operator=h2_hamiltonian)
+        self.assertGreater(quantum_instance.time_taken, 0.)
+        quantum_instance.reset_execution_results()
+        self.assertAlmostEqual(result.eigenvalue.real, -1.86, delta=0.05)
 
 
 if __name__ == '__main__':
