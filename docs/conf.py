68ec83e64763b8bdbb860ea75b1b7106e4629d9b
Max Rossmannek <oss@zurich.ibm.com>
2021-05-07 18:42:14 +0200

    Insert actual path to source when generating docs

--- docs/conf.py
+++ docs/conf.py
@@ -22,16 +22,15 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
 
 """
 Sphinx documentation builder
 """
 
-import os
 # Set env flag so that we can doc functions that may otherwise not be loaded
 # see for example interactive visualizations in qiskit.visualization.
 os.environ['QISKIT_DOCS'] = 'TRUE'
