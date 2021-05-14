db47902195b33b0ab0c3bfd8ea722ee181e8a2a7
Matthew Treinish <mtreinish@kortar.org>
2021-05-14 18:04:13 -0400

    Replace doc footnote with link

--- qiskit/circuit/quantumcircuit.py
+++ qiskit/circuit/quantumcircuit.py
@@ -1341,9 +1341,8 @@ class QuantumCircuit:
                 output file if ``filename`` is specified. By default this is
                 set to the system's default encoding (ie whatever
                 ``locale.getpreferredencoding()`` returns) and can be set to
-                any valid codec or alias from stdlib's ``codec`` module [#f1].
-
-        .. [#f1] https://docs.python.org/3/library/codecs.html#standard-encodings
+                any valid codec or alias from stdlib's
+                `codec module <https://docs.python.org/3/library/codecs.html#standard-encodings>`__
 
         Returns:
             str: If formatted=False.
