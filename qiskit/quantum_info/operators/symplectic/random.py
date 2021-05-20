052bd08b00f7c1e4222da8f8ac5b585bc2e542d6
Christopher Wood <cjwood@us.ibm.com>
2021-05-20 12:58:52 -0400

    Fix type in random_pauli_list

--- qiskit/quantum_info/operators/symplectic/random.py
+++ qiskit/quantum_info/operators/symplectic/random.py
@@ -18,7 +18,7 @@ from numpy.random import default_rng
 
 from .clifford import Clifford
 from .pauli import Pauli
-from .pauli_list import PauliList
+from .pauli_list import PauliList, BasePauli
 from .pauli_table import PauliTable
 from .stabilizer_table import StabilizerTable
 
@@ -55,9 +55,9 @@ def random_pauli_list(num_qubits, size=1, seed=None, phase=True):
 
     Args:
         num_qubits (int): the number of qubits.
-        size (int): Optional. The number of rows of the table (Default: 1).
+        size (int): Optional. The length of the Pauli list (Default: 1).
         seed (int or np.random.Generator): Optional. Set a fixed seed or generator for RNG.
-        phase (bool): If True the phases are randomized, otherwise the phases are fixed to 0.
+        phase (bool): If True the Pauli phases are randomized, otherwise the phases are fixed to 0.
                      [Default: True]
 
     Returns:
@@ -74,7 +74,7 @@ def random_pauli_list(num_qubits, size=1, seed=None, phase=True):
     x = rng.integers(2, size=(size, num_qubits)).astype(bool)
     if phase:
         _phase = rng.integers(4, size=(size))
-        return PauliList((z, x, _phase))
+        return PauliList(BasePauli(z, x, _phase))
     return PauliList((z, x))
 
 
