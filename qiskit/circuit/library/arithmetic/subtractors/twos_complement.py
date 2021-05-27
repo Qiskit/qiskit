27870919812b117f72bbba136c8dd4f29cd0ad61
ManjulaGandhi <manjulagandhi.cta.cit@gmail.com>
2021-05-27 12:46:03 +0530

    Black

--- qiskit/circuit/library/arithmetic/subtractors/twos_complement.py
+++ qiskit/circuit/library/arithmetic/subtractors/twos_complement.py
@@ -15,10 +15,11 @@
 from qiskit.circuit import QuantumCircuit, QuantumRegister, AncillaRegister
 from qiskit.circuit.library.arithmetic import DraperQFTAdder
 
+
 class TwosComplement(QuantumCircuit):
     r"""A circuit that obtains Two's Complement on one qubit register.
     Circuit to compute the two's complement of one qubit register Part from [1].
-    
+
     As an example, a Two's Complement circuit that performs two's complement on a 3-qubit sized
     register is as follows:
     .. parsed-literal::
@@ -35,18 +36,14 @@ class TwosComplement(QuantumCircuit):
                          ░       ░      │           │
             cin_2: ──────░───────░──────┤2          ├─────
                          ░       ░      └───────────┘
-  
-   
+
+
     **Reference**
     [1] Thomas G.Draper, 2000. "Addition on a Quantum Computer"
     `Journal https://arxiv.org/pdf/quant-ph/0008033.pdf`_
     """
 
-    def __init__(self, 
-                 num_state_qubits: int, 
-                 adder=None,
-                 name: str = 'TwosComplement'
-                 ) -> None:
+    def __init__(self, num_state_qubits: int, adder=None, name: str = "TwosComplement") -> None:
         """
         Args:
             num_state_qubits: The size of the register.
@@ -55,44 +52,42 @@ class TwosComplement(QuantumCircuit):
         Raises:
             ValueError: If ``num_state_qubits`` is lower than 1.
         """
-        
+
         if num_state_qubits < 1:
-            raise ValueError('The number of qubits must be at least 1.')
+            raise ValueError("The number of qubits must be at least 1.")
         if adder is None:
-            adder = DraperQFTAdder(num_state_qubits,modular=False)
+            adder = DraperQFTAdder(num_state_qubits, modular=False)
         # get the number of qubits needed
         num_qubits = adder.num_qubits
         num_helper_qubits = adder.num_ancillas
-        
+
         # define the registers
-        b_qr = QuantumRegister(num_state_qubits, name='input_b')
-        carry_qr=QuantumRegister(1, name='carry')
-        one_qr = AncillaRegister(num_state_qubits, name='cin')
+        b_qr = QuantumRegister(num_state_qubits, name="input_b")
+        carry_qr = QuantumRegister(1, name="carry")
+        one_qr = AncillaRegister(num_state_qubits, name="cin")
         if num_helper_qubits != 0:
             qr_h = AncillaRegister(num_helper_qubits)
 
-        
         # initialize the circuit
-        if num_helper_qubits != 0 :
-            super().__init__(b_qr, carry_qr, one_qr,qr_h, name=name)
+        if num_helper_qubits != 0:
+            super().__init__(b_qr, carry_qr, one_qr, qr_h, name=name)
         else:
             super().__init__(b_qr, carry_qr, one_qr, name=name)
-        #if num_carry_qubits > 0:
+        # if num_carry_qubits > 0:
         #    qr_c = QuantumRegister(num_carry_qubits)
         #    self.add_register(qr_c)
         # adder helper qubits if required
-        #if num_helper_qubits > 0:
+        # if num_helper_qubits > 0:
         #    qr_h = AncillaRegister(num_helper_qubits)  # helper/ancilla qubits
         #    self.add_register(qr_h)
-        
+
         # Build a temporary subcircuit that obtains two's complement of b,
 
-
-    #flippling circuit and adding 1
+        # flippling circuit and adding 1
         self.barrier()
         self.x(one_qr[0])
         self.barrier()
         for j in range(num_state_qubits):
             self.x(b_qr[j])
-        self.append(adder,one_qr[:]+b_qr[:]+carry_qr[:])
+        self.append(adder, one_qr[:] + b_qr[:] + carry_qr[:])
         self.x(one_qr[0])
--- qiskit/circuit/library/arithmetic/subtractors/twos_complement_subtractor.py
+++ qiskit/circuit/library/arithmetic/subtractors/twos_complement_subtractor.py
@@ -12,68 +12,82 @@
 
 """Compute the difference of two qubit registers using Two's Complement Subtraction."""
 from qiskit.circuit import QuantumCircuit, QuantumRegister, AncillaRegister
-#from qiskit.circuit.library.arithmetic.adders.adder import Adder
-from qiskit.circuit.library.arithmetic.adders import CDKMRippleCarryAdder,DraperQFTAdder,VBERippleCarryAdder
+
+# from qiskit.circuit.library.arithmetic.adders.adder import Adder
+from qiskit.circuit.library.arithmetic.adders import (
+    CDKMRippleCarryAdder,
+    DraperQFTAdder,
+    VBERippleCarryAdder,
+)
 from qiskit.circuit.library.arithmetic.subtractor import TwosComplement
+
+
 class Subtractor(QuantumCircuit):
     r"""A circuit that uses Two's Complement Subtraction to perform in-place subtraction on two qubit registers.
-     Circuit to compute the difference of two qubit registers.
-     Given two equally sized input registers that store quantum states
-    :math:`|a\rangle` and :math:`|b\rangle`, performs subtraction of numbers that
-    can be represented by the states, storing the resulting state in-place in the second register:
-    .. math::
-        |a\rangle |b\rangle \mapsto |a\rangle |a-b\rangle
-    Here :math:`|a\rangle` (and correspondingly :math:`|b\rangle`) stands for the direct product
-    :math:`|a_n\rangle \otimes |a_{n-1}\rangle \ldots |a_{1}\rangle \otimes |a_{0}\rangle`
-    which denotes a quantum register prepared with the value :math:`a = 2^{0}a_{0} + 2^{1}a_{1} +
-    \ldots 2^{n}a_{n}`[1].
-    As an example, a subtractor circuit that performs two's complement on :math:`|b\rangle`and 
-    performs addition on two 3-qubit sized registers is as follows:
-    .. parsed-literal::
-         a_0: ──────────────────────────────────────■──────■────────■──────────────────────────────────────
-                                                    │      │        │
-         a_1: ──────────────────────────────────────┼──────┼────────┼────────■──────■──────────────────────
-               ░       ░ ┌───┐┌───────────┐┌──────┐ │P(π)  │        │        │      │              ┌──────┐
-         b_0: ─░───────░─┤ X ├┤2          ├┤0     ├─■──────┼────────┼────────┼──────┼──────────────┤0     ├
-               ░       ░ ├───┤│           ││      │        │P(π/2)  │        │P(π)  │              │      │
-         b_1: ─░───────░─┤ X ├┤3          ├┤1 qft ├────────■────────┼────────■──────┼──────────────┤1 qft ├
-               ░       ░ └───┘│           ││      │                 │P(π/4)         │P(π/2)        │      │
-   carry_b_0: ─░───────░──────┤4          ├┤2     ├─────────────────■───────────────■────────■─────┤2     ├
-               ░       ░      │  QFTAdder │└──────┘                                          │P(π) └──────┘
-   carry_a_0: ────────────────┤           ├──────────────────────────────────────────────────■─────────────
-               ░ ┌───┐ ░      │           │ ┌───┐
-        a0_0: ─░─┤ X ├─░──────┤0          ├─┤ X ├──────────────────────────────────────────────────────────
-               ░ └───┘ ░      │           │ └───┘
-        a0_1: ─░───────░──────┤1          ├────────────────────────────────────────────────────────────────
-               ░       ░      └───────────┘
-           
-    **References**
-    
-    [1] Vedral et al., Quantum Networks for Elementary Arithmetic Operations, 1995.
-    `arXiv:quant-ph/9511018 <https://arxiv.org/pdf/quant-ph/9511018.pdf>`_
+      Circuit to compute the difference of two qubit registers.
+      Given two equally sized input registers that store quantum states
+     :math:`|a\rangle` and :math:`|b\rangle`, performs subtraction of numbers that
+     can be represented by the states, storing the resulting state in-place in the second register:
+     .. math::
+         |a\rangle |b\rangle \mapsto |a\rangle |a-b\rangle
+     Here :math:`|a\rangle` (and correspondingly :math:`|b\rangle`) stands for the direct product
+     :math:`|a_n\rangle \otimes |a_{n-1}\rangle \ldots |a_{1}\rangle \otimes |a_{0}\rangle`
+     which denotes a quantum register prepared with the value :math:`a = 2^{0}a_{0} + 2^{1}a_{1} +
+     \ldots 2^{n}a_{n}`[1].
+     As an example, a subtractor circuit that performs two's complement on :math:`|b\rangle`and
+     performs addition on two 3-qubit sized registers is as follows:
+     .. parsed-literal::
+          a_0: ──────────────────────────────────────■──────■────────■──────────────────────────────────────
+                                                     │      │        │
+          a_1: ──────────────────────────────────────┼──────┼────────┼────────■──────■──────────────────────
+                ░       ░ ┌───┐┌───────────┐┌──────┐ │P(π)  │        │        │      │              ┌──────┐
+          b_0: ─░───────░─┤ X ├┤2          ├┤0     ├─■──────┼────────┼────────┼──────┼──────────────┤0     ├
+                ░       ░ ├───┤│           ││      │        │P(π/2)  │        │P(π)  │              │      │
+          b_1: ─░───────░─┤ X ├┤3          ├┤1 qft ├────────■────────┼────────■──────┼──────────────┤1 qft ├
+                ░       ░ └───┘│           ││      │                 │P(π/4)         │P(π/2)        │      │
+    carry_b_0: ─░───────░──────┤4          ├┤2     ├─────────────────■───────────────■────────■─────┤2     ├
+                ░       ░      │  QFTAdder │└──────┘                                          │P(π) └──────┘
+    carry_a_0: ────────────────┤           ├──────────────────────────────────────────────────■─────────────
+                ░ ┌───┐ ░      │           │ ┌───┐
+         a0_0: ─░─┤ X ├─░──────┤0          ├─┤ X ├──────────────────────────────────────────────────────────
+                ░ └───┘ ░      │           │ └───┘
+         a0_1: ─░───────░──────┤1          ├────────────────────────────────────────────────────────────────
+                ░       ░      └───────────┘
+
+     **References**
+
+     [1] Vedral et al., Quantum Networks for Elementary Arithmetic Operations, 1995.
+     `arXiv:quant-ph/9511018 <https://arxiv.org/pdf/quant-ph/9511018.pdf>`_
     """
 
-    #def __init__(self, num_state_qubits: int, adder: Optional[adder] = None):
-    def __init__(self, num_state_qubits: int, adder=None, name: str = 'Subtractor'):
+    # def __init__(self, num_state_qubits: int, adder: Optional[adder] = None):
+    def __init__(self, num_state_qubits: int, adder=None, name: str = "Subtractor"):
         if adder is None:
-            adder = DraperQFTAdder(num_state_qubits+1,modular=True)
+            adder = DraperQFTAdder(num_state_qubits + 1, modular=True)
             ##adder = RippleCarryAdder(num_state_qubits+1,modular=True)
         twos_complement = TwosComplement(num_state_qubits)
         # get the number of qubits needed
         num_qubits = adder.num_qubits
-        num_helper_qubits = max(adder.num_ancillas,twos_complement.num_ancillas)
-        
+        num_helper_qubits = max(adder.num_ancillas, twos_complement.num_ancillas)
+
         # construct the registers
-        qr_a = QuantumRegister(num_state_qubits, 'a')  # input a
-        qr_b = QuantumRegister(num_state_qubits, 'b')  # input b
-        carry_b=QuantumRegister(1,'carry_b')
-        carry_a=AncillaRegister(1,'carry_a')
-        qr_h=AncillaRegister(num_helper_qubits)
-        
-        super().__init__(qr_a,qr_b,carry_b,carry_a,qr_h, name=name)
-        
-        
-        self.compose(twos_complement, qubits=qr_b[:]+carry_b[:]+qr_h[:twos_complement.num_ancillas],inplace=True)
-        
+        qr_a = QuantumRegister(num_state_qubits, "a")  # input a
+        qr_b = QuantumRegister(num_state_qubits, "b")  # input b
+        carry_b = QuantumRegister(1, "carry_b")
+        carry_a = AncillaRegister(1, "carry_a")
+        qr_h = AncillaRegister(num_helper_qubits)
+
+        super().__init__(qr_a, qr_b, carry_b, carry_a, qr_h, name=name)
+
+        self.compose(
+            twos_complement,
+            qubits=qr_b[:] + carry_b[:] + qr_h[: twos_complement.num_ancillas],
+            inplace=True,
+        )
+
         # adder
-        self.compose(adder,qubits=qr_a[:]+carry_a[:]+qr_b[:]+carry_b[:]+qr_h[:adder.num_ancillas],inplace=True)
+        self.compose(
+            adder,
+            qubits=qr_a[:] + carry_a[:] + qr_b[:] + carry_b[:] + qr_h[: adder.num_ancillas],
+            inplace=True,
+        )
