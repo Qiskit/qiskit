7ec386a4cb0501d41f611b2e86bc4d12beadf7f1
Pieter Eendebak <pieter.eendebak@gmail.com>
2021-05-27 16:45:09 +0200

    add pass to remove small rotations

---
+++ qiskit/transpiler/passes/optimization/optimize_small_rotations.py
@@ -0,0 +1,50 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2017, 2018.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Optimize by removing small rotations """
+
+from qiskit.circuit import QuantumCircuit
+from qiskit.circuit.library import CRXGate, CRYGate, CRZGate, PhaseGate, RXGate, RYGate, RZGate
+
+from qiskit.transpiler.basepasses import TransformationPass
+
+
+class RemoveZeroRotations(TransformationPass):
+    """Return a circuit with any zero rotation removed."""
+
+    def __init__(self, epsilon=0):
+        """Remove all zero rotations from a circuit"""
+        super().__init__()
+
+        self.epsilon = 0
+        self._empty_dag1 = circuit_to_dag(QuantumCircuit(1))
+        self._empty_dag2 = circuit_to_dag(QuantumCircuit(2))
+
+    def run(self, dag: DAGCircuit) -> DAGCircuit:
+        """Run the Decompose pass on `dag`.
+
+        Args:
+            dag: input dag.
+
+        Returns:
+            Output dag with zero rotations removed
+        """
+        for node in dag.op_nodes():
+            if isinstance(node.op, (PhaseGate, RXGate, RYGate, RZGate)):
+                phi = node.op.params[0]
+                if np.abs(phi) <= self.epsilon:
+                    dag.substitute_node_with_dag(node, self._empty_dag1)
+            elif isinstance(node.op, (CRXGate, CRYGate, CRZGate)):
+                phi = node.op.params[0]
+                if np.abs(phi) <= self.epsilon:
+                    dag.substitute_node_with_dag(node, self._empty_dag2)
+        return dag
