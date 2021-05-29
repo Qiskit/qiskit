9e2afe5c6856f0f30a266798cd85badc4c9ff5a7
Julien Gacon <gaconju@gmail.com>
2021-05-29 15:12:12 +0200

    fix SPSA -> Gradient descent in docstrings

--- test/python/algorithms/optimizers/test_gradient_descent.py
+++ test/python/algorithms/optimizers/test_gradient_descent.py
@@ -23,7 +23,7 @@ from qiskit.test.decorators import slow_test
 
 
 class TestGradientDescent(QiskitAlgorithmsTestCase):
-    """Tests for the SPSA optimizer."""
+    """Tests for the gradient descent optimizer."""
 
     def setUp(self):
         super().setUp()
@@ -31,7 +31,7 @@ class TestGradientDescent(QiskitAlgorithmsTestCase):
 
     @slow_test
     def test_pauli_two_design(self):
-        """Test SPSA on the Pauli two-design example."""
+        """Test standard gradient descent on the Pauli two-design example."""
         circuit = PauliTwoDesign(3, reps=3, seed=2)
         parameters = list(circuit.parameters)
         obs = Z ^ Z ^ I
