a45f2f246853eb9076feff2319db21b95fff34b4
Mantas Čepulkovskis <60826723+mantcep@users.noreply.github.com>
2021-05-29 15:05:02 +0300

    Update qiskit/circuit/library/arithmetic/multipliers/hrs_cumulative_multiplier.py
    Co-authored-by: Julien Gacon <gaconju@gmail.com>

--- qiskit/circuit/library/arithmetic/multipliers/hrs_cumulative_multiplier.py
+++ qiskit/circuit/library/arithmetic/multipliers/hrs_cumulative_multiplier.py
@@ -81,7 +81,8 @@ class HRSCumulativeMultiplier(Multiplier):
             num_result_qubits: The number of result qubits to limit the output to.
                 If number of result qubits is :math:`n`, multiplication modulo :math:`2^n` is performed
                 to limit the output to the specified number of qubits. Default
-                value is ``2 * num_state_qubits``.
+                value is ``2 * num_state_qubits`` to represent any possible
+                result from the multiplication of the two inputs.
             adder: Half adder circuit to be used for performing multiplication. The
                 CDKMRippleCarryAdder is used as default if no adder is provided.
             name: The name of the circuit object.
