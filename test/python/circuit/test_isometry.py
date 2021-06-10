7dd63643df970730e83032dac36ffb8cff465e77
Mohit Deshpande <mohitd2000@gmail.com>
2021-06-10 13:39:28 -0700

    Allow users to optionally specify Isometry tolerance (#6482)
    * Allow users to optionally specify Isometry tolerance
    
    Instead of hard-coding a fixed value for floating-point epsilon
    tolerances, allow users to optionally specify what value of the
    tolerance they would like to use instead.
    
    Fixes #3789
    
    * Added unit tests for custom isometry tolerance
    
    * Added release notes for custom isometry tolerance
    
    * Fixed lint errors
    
    * Update test/python/circuit/test_isometry.py
    
    Co-authored-by: Abby Mitchell <abby.mitchell@btinternet.com>
    Co-authored-by: Luciano Bello <bel@zurich.ibm.com>

--- test/python/circuit/test_isometry.py
+++ test/python/circuit/test_isometry.py
@@ -85,6 +85,45 @@ class TestIsometry(QiskitTestCase):
         random_unitary(16).data,
         random_unitary(16).data[:, 0:8],
     )
+    def test_isometry_tolerance(self, iso):
+        """Tests for the decomposition of isometries from m to n qubits with a custom tolerance"""
+        if len(iso.shape) == 1:
+            iso = iso.reshape((len(iso), 1))
+        num_q_output = int(np.log2(iso.shape[0]))
+        num_q_input = int(np.log2(iso.shape[1]))
+        q = QuantumRegister(num_q_output)
+        qc = QuantumCircuit(q)
+
+        # Compute isometry with custom tolerance
+        qc.isometry(iso, q[:num_q_input], q[num_q_input:], epsilon=1e-3)
+
+        # Verify the circuit can be decomposed
+        self.assertIsInstance(qc.decompose(), QuantumCircuit)
+
+        # Decompose the gate
+        qc = transpile(qc, basis_gates=["u1", "u3", "u2", "cx", "id"])
+
+        # Simulate the decomposed gate
+        simulator = BasicAer.get_backend("unitary_simulator")
+        result = execute(qc, simulator).result()
+        unitary = result.get_unitary(qc)
+        iso_from_circuit = unitary[::, 0 : 2 ** num_q_input]
+        self.assertTrue(matrix_equal(iso_from_circuit, iso, ignore_phase=True))
+
+    @data(
+        np.eye(2, 2),
+        random_unitary(2).data,
+        np.eye(4, 4),
+        random_unitary(4).data[:, 0],
+        np.eye(4, 4)[:, 0:2],
+        random_unitary(4).data,
+        np.eye(4, 4)[:, np.random.permutation(np.eye(4, 4).shape[1])][:, 0:2],
+        np.eye(8, 8)[:, np.random.permutation(np.eye(8, 8).shape[1])],
+        random_unitary(8).data[:, 0:4],
+        random_unitary(8).data,
+        random_unitary(16).data,
+        random_unitary(16).data[:, 0:8],
+    )
     def test_isometry_inverse(self, iso):
         """Tests for the inverse of isometries from m to n qubits"""
         if len(iso.shape) == 1:
