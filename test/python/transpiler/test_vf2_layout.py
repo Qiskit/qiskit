13319cc1db43c6c9cef7e6fd5f88473074cace15
Luciano Bello <bel@zurich.ibm.com>
2021-06-23 12:12:59 +0200

    done with test

--- test/python/transpiler/test_vf2_layout.py
+++ test/python/transpiler/test_vf2_layout.py
@@ -48,11 +48,11 @@ class TestVF2Layout(QiskitTestCase):
     def test_3q_circuit_5q_coupling(self):
         """3 qubits in Tenerife, without considering the direction
 
-              1                    1
-            ↙ ↑                 /  |
-          0 ← 2 ← 3           0 - qr1 - qr2
-              ↑ ↙                 |   /
-              4                   qr0
+            1                    1
+          ↙ ↑                 /  |
+        0 ← 2 ← 3           0 - qr1 - qr2
+            ↑ ↙                 |   /
+            4                   qr0
         """
         cmap5 = FakeTenerife().configuration().coupling_map
 
@@ -128,11 +128,11 @@ class TestVF2Layout(QiskitTestCase):
 
     def test_3q_circuit_5q_coupling_sd(self):
         """3 qubits in Tenerife, considering the direction
-                    1                       1
-                  ↙ ↑                    ↙  ↑
-                0 ← 2 ← 3              0 ← qr2 ← qr1
-                    ↑ ↙                     ↑  ↙
-                    4                      qr0
+            1                       1
+          ↙ ↑                    ↙  ↑
+        0 ← 2 ← 3              0 ← qr2 ← qr1
+            ↑ ↙                     ↑  ↙
+            4                      qr0
         """
         cmap5 = FakeTenerife().configuration().coupling_map
 
@@ -188,89 +188,51 @@ class TestVF2Layout(QiskitTestCase):
         self.assertEqual(layout[qr1[4]], 1)
         self.assertEqual(pass_.property_set["VF2Layout_stop_reason"], "solution found")
 
-    # def test_5q_circuit_16q_coupling_no_solution(self):
-    #     """5 qubits in Rueschlikon, no solution
-    #
-    #     q0[1] ↖     ↗ q0[2]
-    #            q0[0]
-    #     q0[3] ↙     ↘ q0[4]
-    #     """
-    #     cmap16 = FakeRueschlikon().configuration().coupling_map
-    #
-    #     qr = QuantumRegister(5, "q")
-    #     circuit = QuantumCircuit(qr)
-    #     circuit.cx(qr[0], qr[1])
-    #     circuit.cx(qr[0], qr[2])
-    #     circuit.cx(qr[0], qr[3])
-    #     circuit.cx(qr[0], qr[4])
-    #     dag = circuit_to_dag(circuit)
-    #     pass_ = CSPLayout(CouplingMap(cmap16), seed=self.seed)
-    #     pass_.run(dag)
-    #     layout = pass_.property_set["layout"]
-    #     self.assertIsNone(layout)
-    #     self.assertEqual(pass_.property_set["CSPLayout_stop_reason"], "nonexistent solution")
+    def test_5q_circuit_16q_coupling_no_solution(self):
+        """5 qubits in Rueschlikon, no solution
 
-    # @staticmethod
-    # def create_hard_dag():
-    #     """Creates a particularly hard circuit (returns its dag) for Tokyo"""
-    #     circuit = QuantumCircuit(20)
-    #     circuit.cx(13, 12)
-    #     circuit.cx(6, 0)
-    #     circuit.cx(5, 10)
-    #     circuit.cx(10, 7)
-    #     circuit.cx(5, 12)
-    #     circuit.cx(2, 15)
-    #     circuit.cx(16, 18)
-    #     circuit.cx(6, 4)
-    #     circuit.cx(10, 3)
-    #     circuit.cx(11, 10)
-    #     circuit.cx(18, 16)
-    #     circuit.cx(5, 12)
-    #     circuit.cx(4, 0)
-    #     circuit.cx(18, 16)
-    #     circuit.cx(2, 15)
-    #     circuit.cx(7, 8)
-    #     circuit.cx(9, 6)
-    #     circuit.cx(16, 17)
-    #     circuit.cx(9, 3)
-    #     circuit.cx(14, 12)
-    #     circuit.cx(2, 15)
-    #     circuit.cx(1, 16)
-    #     circuit.cx(5, 3)
-    #     circuit.cx(8, 12)
-    #     circuit.cx(2, 1)
-    #     circuit.cx(5, 3)
-    #     circuit.cx(13, 5)
-    #     circuit.cx(12, 14)
-    #     circuit.cx(12, 13)
-    #     circuit.cx(6, 4)
-    #     circuit.cx(15, 18)
-    #     circuit.cx(15, 18)
-    #     return circuit_to_dag(circuit)
+        q0[1] ↖     ↗ q0[2]
+               q0[0]
+        q0[3] ↙     ↘ q0[4]
+        """
+        cmap16 = FakeRueschlikon().configuration().coupling_map
 
-    # def test_seed(self):
-    #     """Different seeds yield different results"""
-    #     seed_1 = 42
-    #     seed_2 = 43
-    #
-    #     cmap5 = FakeTenerife().configuration().coupling_map
-    #
-    #     qr = QuantumRegister(3, "qr")
-    #     circuit = QuantumCircuit(qr)
-    #     circuit.cx(qr[1], qr[0])  # qr1 -> qr0
-    #     circuit.cx(qr[0], qr[2])  # qr0 -> qr2
-    #     circuit.cx(qr[1], qr[2])  # qr1 -> qr2
-    #     dag = circuit_to_dag(circuit)
-    #
-    #     pass_1 = CSPLayout(CouplingMap(cmap5), seed=seed_1)
-    #     pass_1.run(dag)
-    #     layout_1 = pass_1.property_set["layout"]
-    #
-    #     pass_2 = CSPLayout(CouplingMap(cmap5), seed=seed_2)
-    #     pass_2.run(dag)
-    #     layout_2 = pass_2.property_set["layout"]
-    #
-    #     self.assertNotEqual(layout_1, layout_2)
+        qr = QuantumRegister(5, "q")
+        circuit = QuantumCircuit(qr)
+        circuit.cx(qr[0], qr[1])
+        circuit.cx(qr[0], qr[2])
+        circuit.cx(qr[0], qr[3])
+        circuit.cx(qr[0], qr[4])
+        dag = circuit_to_dag(circuit)
+        pass_ = VF2Layout(CouplingMap(cmap16), seed=self.seed)
+        pass_.run(dag)
+        layout = pass_.property_set["layout"]
+        self.assertIsNone(layout)
+        self.assertEqual(pass_.property_set["VF2Layout_stop_reason"], "nonexistent solution")
+
+    def test_seed(self):
+        """Different seeds yield different results"""
+        seed_1 = 42
+        seed_2 = 43
+
+        cmap5 = FakeTenerife().configuration().coupling_map
+
+        qr = QuantumRegister(3, "qr")
+        circuit = QuantumCircuit(qr)
+        circuit.cx(qr[1], qr[0])  # qr1 -> qr0
+        circuit.cx(qr[0], qr[2])  # qr0 -> qr2
+        circuit.cx(qr[1], qr[2])  # qr1 -> qr2
+        dag = circuit_to_dag(circuit)
+
+        pass_1 = VF2Layout(CouplingMap(cmap5), seed=seed_1)
+        pass_1.run(dag)
+        layout_1 = pass_1.property_set["layout"]
+
+        pass_2 = VF2Layout(CouplingMap(cmap5), seed=seed_2)
+        pass_2.run(dag)
+        layout_2 = pass_2.property_set["layout"]
+
+        self.assertNotEqual(layout_1, layout_2)
 
 
 if __name__ == "__main__":
