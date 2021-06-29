e384fb72611a41c18fbaceb20dfddf3b45a025e1
Ali Javadi <ajavadia@princeton.edu>
2021-06-29 01:01:42 -0400

    move pass under passes/scheduling

--- qiskit/transpiler/passes/scheduling/insert_dd.py
+++ qiskit/transpiler/passes/scheduling/insert_dd.py
@@ -17,7 +17,7 @@ import numpy as np
 
 from qiskit.dagcircuit import DAGCircuit
 from qiskit.circuit import Delay
-from qiskit.circuit.library import UGate, XGate
+from qiskit.circuit.library import UGate
 from qiskit.transpiler.passes import Optimize1qGates
 from qiskit.transpiler.basepasses import TransformationPass
 from qiskit.transpiler.exceptions import TranspilerError
@@ -26,21 +26,30 @@ from qiskit.transpiler.exceptions import TranspilerError
 class InsertDD(TransformationPass):
     """Dynamical decoupling insertion pass.
 
-    This pass works on a scheduled, physical circuit. It scans the circuit for idle periods
-    of time (i.e. those containing delay instructions) and inserts a DD sequence of
-    gates in those spots. These gates amount to the identity, so do not alter the logical
-    action of the circuit, but have the effect of mitigating decoherence in those idle periods.
+    This pass works on a scheduled, physical circuit. It scans the circuit for
+    idle periods of time (i.e. those containing delay instructions) and inserts
+    a DD sequence of gates in those spots. These gates amount to the identity,
+    so do not alter the logical action of the circuit, but have the effect of
+    mitigating decoherence in those idle periods.
 
-    The pass does not insert DD on idle periods that immediately follow qubit reset,
-    as qubits in the ground state are less susceptile to decoherence.
+    The pass does not insert DD on idle periods that immediately follow qubit
+    reset, as qubits in the ground state are less susceptile to decoherence.
     """
 
-    def __init__(self, durations, dd_sequence):
+    def __init__(self, durations, dd_sequence, qubits=None, spacing=None):
         """Dynamical decoupling initializer.
 
         Args:
-            durations (InstructionDurations): Durations of instructions to be used in scheduling
-            dd_sequence (list[str]): sequence of gates to apply in idle spots
+            durations (InstructionDurations): Durations of instructions to be
+                used in scheduling.
+            dd_sequence (list[str]): sequence of gates (by name) to apply
+                in idle spots.
+            qubits (list[int]): physical qubits on which to apply DD.
+                If None, all qubits will undergo DD.
+            spacing (callable): a function that specifies spacing between DD
+                gates. It maps natural numbers, i, to the fraction of the total
+                slack that must be allocated to the i'th delay window. If None,
+                equal spacing will be used.
         """
         super().__init__()
         self._durations = durations
@@ -58,7 +67,7 @@ class InsertDD(TransformationPass):
         Raises:
             TranspilerError: if the circuit is not mapped on physical qubits.
         """
-        if len(dag.qregs) != 1 or dag.qregs.get('q', None) is None:
+        if len(dag.qregs) != 1 or dag.qregs.get("q", None) is None:
             raise TranspilerError('DD runs on physical circuits only.')
 
         new_dag = dag._copy_circuit_metadata()
@@ -72,11 +81,11 @@ class InsertDD(TransformationPass):
                     new_dag.apply_operation_back(nd.op, nd.qargs, nd.cargs)
                 else:
                     dd_sequence = []
+                    dd_sequence_duration = 0
                     for g in self._dd_sequence:
-                        x = XGate()
-                        x.duration = self._durations.get(g, qubit)
-                        dd_sequence.append(x)
-                    dd_sequence_duration = sum([self._durations.get(g, qubit) for g in self._dd_sequence])
+                        g.duration = self._durations.get(g, qubit)
+                        dd_sequence.append(g)
+                        dd_sequence_duration += g.duration
                     slack = nd.op.duration - dd_sequence_duration
                     if slack <= 0:     # dd doesn't fit
                         new_dag.apply_operation_back(nd.op, nd.qargs, nd.cargs)
@@ -94,7 +103,7 @@ class InsertDD(TransformationPass):
                                 new_dag.apply_operation_back(Delay(begin), [qubit])
                                 new_dag.apply_operation_back(dd_sequence[0], [qubit])
                                 new_dag.apply_operation_back(Delay(slack-begin), [qubit])
-                                # absorb an X gate into the successor (from left in circuit)
+                                # absorb the gate into the successor (from left in circuit)
                                 theta, phi, lam = succ.op.params
                                 succ.op.params = Optimize1qGates.compose_u3(theta, phi, lam, np.pi, 0, np.pi)
                             else:
