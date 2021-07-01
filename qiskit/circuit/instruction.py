836a05ce4560bec658e5176c61dbfbf8eeef59a8
Kevin Krsulich <kevin@krsulich.net>
2021-07-01 10:13:53 -0400

    Update qiskit/circuit/instruction.py
    Co-authored-by: Julien Gacon <gaconju@gmail.com>

--- qiskit/circuit/instruction.py
+++ qiskit/circuit/instruction.py
@@ -64,7 +64,7 @@ class Instruction:
                 list of parameters
             duration (int or float): instruction's duration. it must be integer if ``unit`` is 'dt'
             unit (str): time unit of duration
-            label: An optional label for identifying the instruction.
+            label (str or None): An optional label for identifying the instruction.
 
         Raises:
             CircuitError: when the register is not in the correct format.
