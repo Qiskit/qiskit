9d11a3a850ed5a24f7165878f7cd2b1267848c8f
Naoki Kanazawa <nkanazawa1989@gmail.com>
2021-07-02 02:09:46 +0900

    Update test/python/transpiler/test_align_measure.py
    Co-authored-by: Ali Javadi-Abhari <ajavadia@users.noreply.github.com>

--- test/python/transpiler/test_align_measure.py
+++ test/python/transpiler/test_align_measure.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Transpiler testing"""
+"""Testing measurement alignment pass."""
 
 from qiskit import QuantumCircuit
 from qiskit.test import QiskitTestCase
