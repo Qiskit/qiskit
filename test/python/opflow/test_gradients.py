fix unittest gradients

test/python/opflow/test_gradients.py
@@ -979,7 +979,7 @@ class TestGradients(QiskitOpflowTestCase):
         shots = 10000 if backend_type == "qasm_simulator" else 1
 
         values = [[0, np.pi / 2], [np.pi / 4, np.pi / 4], [np.pi / 3, np.pi / 9]]
-        correct_values = [[-4.0, 0], [-1.0, -2.41421356], [-0.34202014, -3.5069806]]
+        correct_values = [[-4.0, 0], [-2.0, -4.82842712], [-0.68404029, -7.01396121]]
         for i, value in enumerate(values):
             backend = BasicAer.get_backend(backend_type)
             q_instance = QuantumInstance(backend=backend, shots=shots)