037b9a432e4d0196d7504097fc4107c39e889ca0
Adam Carriker <adamcarriker@Adams-MacBook-Pro.local>
2021-07-07 17:03:19 -0400

    Fixing GaussianSquare Docstring
    Fix #6691
    
    Fixes docstring of GaussianSquare class which incorrectly divides the mean (risefall) in half twice instead of once.

--- qiskit/pulse/library/parametric_pulses.py
+++ qiskit/pulse/library/parametric_pulses.py
@@ -215,7 +215,7 @@ class GaussianSquare(ParametricPulse):
 
         0 <= x < risefall
 
-        f(x) = amp * exp( -(1/2) * (x - risefall/2)^2 / sigma^2) )
+        f(x) = amp * exp( -(1/2) * (x - risefall)^2 / sigma^2) )
 
         risefall <= x < risefall + width
 
@@ -223,7 +223,7 @@ class GaussianSquare(ParametricPulse):
 
         risefall + width <= x < duration
 
-        f(x) = amp * exp( -(1/2) * (x - (risefall + width)/2)^2 / sigma^2) )
+        f(x) = amp * exp( -(1/2) * (x - (risefall + width))^2 / sigma^2) )
 
     """
 
