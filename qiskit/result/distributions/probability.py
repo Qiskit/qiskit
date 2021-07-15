1ee02f9ecfb5b4187644b164d803791262d1354a
Paul Nation <nonhermitian@gmail.com>
2021-07-15 09:14:48 -0400

    fix probabilities

--- qiskit/result/distributions/probability.py
+++ qiskit/result/distributions/probability.py
@@ -38,56 +38,6 @@ class ProbDistribution(dict):
                     * An integer
 
             shots (int): Number of shots the distribution was derived from.
-
-        Raises:
-            TypeError: If the input keys are not a string or int
-            ValueError: If the string format of the keys is incorrect
         """
         self.shots = shots
-        if data:
-            first_key = next(iter(data.keys()))
-            if isinstance(first_key, int):
-                pass
-            elif isinstance(first_key, str):
-                if first_key.startswith("0x"):
-                    hex_raw = data
-                    data = {int(key, 0): value for key, value in hex_raw.items()}
-                elif first_key.startswith("0b"):
-                    bin_raw = data
-                    data = {int(key, 0): value for key, value in bin_raw.items()}
-                elif self._bitstring_regex.search(first_key):
-                    bin_raw = data
-                    data = {int("0b" + key, 0): value for key, value in bin_raw.items()}
-                else:
-                    raise ValueError(
-                        "The input keys are not a valid string format, must either "
-                        "be a hex string prefixed by '0x' or a binary string "
-                        "optionally prefixed with 0b"
-                    )
-            else:
-                raise TypeError("Input data's keys are of invalid type, must be str or int")
         super().__init__(data)
-
-    def binary_probabilities(self, num_bits=None):
-        """Build a probabilities dictionary with binary string keys
-
-        Parameters:
-            num_bits (int): number of bits in the binary bitstrings (leading
-                zeros will be padded). If None, the length will be derived
-                from the largest key present.
-
-        Returns:
-            dict: A dictionary where the keys are binary strings in the format
-                ``"0110"``
-        """
-        n = len(bin(max(self.keys(), default=0))) - 2 if num_bits is None else num_bits
-        return {format(key, "b").zfill(n): value for key, value in self.items()}
-
-    def hex_probabilities(self):
-        """Build a probabilities dictionary with hexadecimal string keys
-
-        Returns:
-            dict: A dictionary where the keys are hexadecimal strings in the
-                format ``"0x1a"``
-        """
-        return {hex(key): value for key, value in self.items()}
