351b2c2a7f0550486cfa7c81523d13306bafa687
Paul Nation <nonhermitian@gmail.com>
2021-07-15 10:18:53 -0400

    black again

--- qiskit/mitigation/mthree/mitigation.py
+++ qiskit/mitigation/mthree/mitigation.py
@@ -390,8 +390,16 @@ class M3Mitigation:
 
         P = spla.LinearOperator((M.num_elems, M.num_elems), precond_matvec)
         vec = counts_to_vector(M.sorted_counts)
-        out, error = spla.gmres(L, vec, tol=tol, atol=tol, maxiter=max_iter, M=P,
-                                callback=callback, callback_type='legacy')
+        out, error = spla.gmres(
+            L,
+            vec,
+            tol=tol,
+            atol=tol,
+            maxiter=max_iter,
+            M=P,
+            callback=callback,
+            callback_type="legacy",
+        )
         if error:
             raise QiskitError("GMRES did not converge: {}".format(error))
 
