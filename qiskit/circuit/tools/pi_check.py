85ced446bf7290a82bd8357ecbd654e44bc3a98a
Edwin Navarro <enavarro@comcast.net>
2021-07-18 09:31:34 -0700

    Use sstr in pi_check ParameterExpression comparison

--- qiskit/circuit/tools/pi_check.py
+++ qiskit/circuit/tools/pi_check.py
@@ -56,12 +56,13 @@ def pi_check(inpt, eps=1e-6, output="text", ndigits=5):
         for sym in syms:
             if not sym.is_number:
                 continue
-            sym_float = abs(float(sym))
-            sym_str = str(sym_float)
-            pi = pi_check(sym_float, eps=eps, output=output, ndigits=ndigits)
+            pi = pi_check(abs(float(sym)), eps=eps, output=output, ndigits=ndigits)
             try:
                 _ = float(pi)
             except (ValueError, TypeError):
+                from sympy import sstr
+
+                sym_str = sstr(abs(sym), full_prec=False)
                 param_str = param_str.replace(sym_str, pi)
         return param_str
     elif isinstance(inpt, str):
