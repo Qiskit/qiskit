2666ad87b027282a185ae4dc457357aeaa9d2758
Julien Gacon <gaconju@gmail.com>
2021-07-23 17:02:14 +0200

    only cast if parameter expr

--- qiskit/circuit/library/evolved_operator_ansatz.py
+++ qiskit/circuit/library/evolved_operator_ansatz.py
@@ -240,11 +240,12 @@ class EvolvedOperatorAnsatz(BlueprintCircuit):
             evolution.compose(self.initial_state, front=True, inplace=True)
 
         # cast global phase to float if it has no free parameters
-        try:
-            evolution.global_phase = float(evolution.global_phase._symbol_expr)
-        except RuntimeError:
-            # expression contains free parameters
-            pass
+        if isinstance(evolution.global_phase, ParameterExpression):
+            try:
+                evolution.global_phase = float(evolution.global_phase._symbol_expr)
+            except RuntimeError:
+                # expression contains free parameters
+                pass
 
         try:
             instr = evolution.to_gate()
