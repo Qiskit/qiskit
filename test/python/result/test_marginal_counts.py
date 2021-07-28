1ad5b9ffd62a4cf6bfe2283780a7adf949655358
Matt Wright <16mpw1@queensu.ca>
2021-07-28 12:08:38 -0400

    Ignore pylint errors in unittests

--- test/python/result/test_marginal_counts.py
+++ test/python/result/test_marginal_counts.py
@@ -10,6 +10,8 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
+# pylint: disable=missing-class-docstring,missing-function-docstring
+
 """Test marginal_counts function."""
 
 import unittest
