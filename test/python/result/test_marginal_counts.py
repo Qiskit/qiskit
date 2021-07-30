85f479006184f9341d3e45533d0b506aab97f789
Matt Wright <16mpw1@queensu.ca>
2021-07-30 13:04:54 -0400

    Fix default edge case test

--- test/python/result/test_marginal_counts.py
+++ test/python/result/test_marginal_counts.py
@@ -14,16 +14,29 @@
 
 """Test marginal_counts function."""
 
+from qiskit.result import models, Result
 from qiskit.result.utils import marginal_counts
 from qiskit.test import QiskitTestCase
 
 
 class TestMarginalCounts(QiskitTestCase):
+    def setUp(self):
+        self.base_result_args = dict(
+            backend_name="test_backend",
+            backend_version="1.0.0",
+            qobj_id="id-123",
+            job_id="job-123",
+            success=True,
+        )
+        super().setUp()
+
     def test_default(self):
         raw_counts = {"00": 11, "11": 12}
-        expected = raw_counts.copy()
-        result = marginal_counts(raw_counts)
-        self.assertEqual(expected, result)
+        data = models.ExperimentResultData(counts=dict(**raw_counts))
+        exp_result = models.ExperimentResult(shots=14, success=True, meas_level=2, data=data)
+        result = Result(results=[exp_result], **self.base_result_args)
+        marginalized_counts = marginal_counts(result).get_counts()
+        self.assertEqual(raw_counts, marginalized_counts)
 
     def test_1qubit(self):
         raw_counts = {"00": 11, "11": 12}
