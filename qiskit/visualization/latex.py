207bc43f541bdfceb5919bf757089ffab83bcb8a
Edwin Navarro <enavarro@comcast.net>
2021-08-05 09:10:39 -0700

    Lint

--- qiskit/visualization/latex.py
+++ qiskit/visualization/latex.py
@@ -530,10 +530,11 @@ class QCircuitImage:
         self._latex[wire_last][col] = "\\control \\qw"
         # Put side text to the right between bottom wire in wire_list and the one above it
         # \settowidth allows redefining \glen to the width of the gate. This avoids overlapping.
-        self._latex[wire_max][col] += (" \\cds{-1}{\\settowidth{\\glen}{\\ensuremath{%s}}"
-                                       " \\hspace{0.5em}\\hspace{\\glen}\\ensuremath{%s}}"
-                                       % (gate_text, gate_text))
-        return 5    # num_cols for side text gates
+        self._latex[wire_max][col] += (
+            " \\cds{-1}{\\settowidth{\\glen}{\\ensuremath{%s}}"
+            " \\hspace{0.5em}\\hspace{\\glen}\\ensuremath{%s}}" % (gate_text, gate_text)
+        )
+        return 5  # num_cols for side text gates
 
     def _build_measure(self, node, col):
         """Build a meter and the lines to the creg"""
