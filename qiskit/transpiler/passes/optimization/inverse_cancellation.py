2fd283d8233d3e7373cc2b77c7ea1661f283a7aa
Victoria Adebayo <vadebayo49@gmail.com>
2021-08-10 19:45:18 -0400

    update self and inverse gates

--- qiskit/transpiler/passes/optimization/inverse_cancellation.py
+++ qiskit/transpiler/passes/optimization/inverse_cancellation.py
@@ -34,84 +34,56 @@ class Cancellation(TransformationPass):
         Returns:
             DAGCircuit: Transformed DAG.
         """
-        # EXAMPLE gates_to_cancel = ["h", (RXGate(theta), RXGate(-theta)), "cx"]
         self_inverse_gates = []
         inverse_gate_pairs = []
-        # TODO your code here...
-        # loop through each item in self.gates_to_cancel:
-        #     if it's a single item (not a tuple):  <== check the type of the input "type checking"
-        #         add to self_inverse
-        #     otherwise add to inverse pairs. Check that there are exactly two items
-        # EXAMPLE self_inverse_gates = ["h", "cx"]
-        # EXAMPLE inverse_gate_pairs = [(RXGate(theta), RXGate(-theta))]
+  
+        for gates in self.gates_to_cancel:
+            if isinstance(gates, str):
+                self_inverse_gates.append(gates)
+            else:
+                inverse_gate_pairs.append(gates)
+
         dag = self._run_on_self_inverse(dag, self_inverse_gates)
         return self._run_on_inverse_pairs(dag, inverse_gate_pairs)
 
     def _run_on_self_inverse(self, dag, self_inverse_gates):
         """
-        TODO
+        Run self-inverse gates on `dag`.
+
+        Args:
+            dag (DAGCircuit): the directed acyclic graph to run on.
+
+        Returns:
+            DAGCircuit: Transformed DAG.
         """
-        # EXAMPLE ["h", "jfdlsj", "fjdsjd", "fjdsf", "jkdjsk"]
         gate_cancel_runs = dag.collect_runs(self.gates_to_cancel)
-        # Generalize input using self.gates_to_cancel
         for gate_cancel_run in gate_cancel_runs:
-            # Partition the gate_cancel_run into chunks with equal gate arguments
-            partition = []
-            chunk = []
-            for i in range(len(gate_cancel_run) - 1):
-                chunk.append(gate_cancel_run[i])
-
-                qargs0 = gate_cancel_run[i].qargs
-                qargs1 = gate_cancel_run[i + 1].qargs
-
-                if qargs0 != qargs1:
-                    partition.append(chunk)
-                    chunk = []
-            chunk.append(gate_cancel_run[-1])
-            partition.append(chunk)
-            # Simplify each chunk in the partition
-            for chunk in partition:
-                if len(chunk) % 2 == 0:
-                    for n in chunk:
-                        dag.remove_op_node(n)
-                else:
-                    for n in chunk[1:]:
-                        dag.remove_op_node(n)
+            if len(gate_cancel_run) % 2 == 0:
+                dag.remove_op_node(gate_cancel_run[0])
+            for node in gate_cancel_run[1:]:
+                dag.remove_op_node(node)
         return dag
 
     def _run_on_inverse_pairs(self, dag, inverse_gate_pairs):
         """
-        TODO
+        Run inverse gate pairs on `dag`.
+        
+        Args:
+            dag (DAGCircuit): the directed acyclic graph to run on.
+
+        Returns:
+            DAGCircuit: Transformed DAG.
         """
-        # EXAMPLE DAG RX(pi/4) 0, RX(pi/4) 0, RX(-pi/4) 0, ...
-        # EXAMPLE DAG RX(pi/4) 0, RX(pi/4) 1, RX(-pi/4) 0, RX(-pi/4) 1, ... 
-        # EXAMPLE [(RXGate(pi/4), RXGate(-pi/4)), (RZ(theta), RZ(-theta))]
         for pair in inverse_gate_pairs:
-            # EXAMPLE pair = (RXGate(pi/4), RXGate(-pi/4))
-            # EXAMPLE pair[0] = RXGate(pi/4)
-            gate_cancel_runs = dag.collect_runs([pair[0].name])  # "rx"
+            gate_cancel_runs = dag.collect_runs([pair[0].name]) 
             for gate_cancel_run in gate_cancel_runs:
-                partition = []
-                chunk = []
                 for i in range(len(gate_cancel_run) - 1):
-                    chunk.append(gate_cancel_run[i])
-
-                    qargs0 = gate_cancel_run[i].qargs
-                    qargs1 = gate_cancel_run[i + 1].qargs
-
-                    if qargs0 != qargs1:
-                        partition.append(chunk)
-                        chunk = []
-                chunk.append(gate_cancel_run[-1])
-                partition.append(chunk)
-                # Simplify each chunk in the partition
-                for chunk in partition:
-                    # EXAMPLE
-                    # RX(pi/4) 0, RX(pi/4) 0, RX(-pi/4) 0
-                    # TODO: this only checks the first two items, we want to check every pair
-                    if chunk[0] == pair[0] and chunk[1] == pair[1]:
-                        dag.remove_op_node(chunk[0])
-                        dag.remove_op_node(chunk[1])
-                    # TODO: check also the inverse order
+                    #TODO Change i & i+1 from a dag node to a gate node
+                    if gate_cancel_run[i] == pair[0] and gate_cancel_run[i+1] == pair[1]:
+                        dag.remove_op_node(gate_cancel_run[i])
+                        dag.remove_op_node(gate_cancel_run[i+1])
+                    elif gate_cancel_run[i] == pair[1] and gate_cancel_run[i+1] == pair[0]:
+                        dag.remove_op_node(gate_cancel_run[i])
+                        dag.remove_op_node(gate_cancel_run[i+1])
 
         return dag
