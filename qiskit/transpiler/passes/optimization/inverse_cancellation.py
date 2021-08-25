77e86631ccc5ed1cfce5f710a30e304075ab881d
Victoria Adebayo <vadebayo49@gmail.com>
2021-08-25 15:38:10 -0400

    PR update

--- qiskit/transpiler/passes/optimization/inverse_cancellation.py
+++ qiskit/transpiler/passes/optimization/inverse_cancellation.py
@@ -91,23 +91,25 @@ class InverseCancellation(TransformationPass):
         Returns:
             DAGCircuit: Transformed DAG.
         """
-        gate_cancel_runs = [dag.collect_runs([gate.name]) for gate in self_inverse_gates]
-        for gate_cancel_run in gate_cancel_runs:
-            partitions = []
-            chunk = []
-            for i in range(len(gate_cancel_run) - 1):
-                chunk.append(gate_cancel_run[i])
-                if gate_cancel_run[i].qargs != gate_cancel_run[i + 1].qargs:
-                    partitions.append(chunk)
-                    chunk = []
-            chunk.append(gate_cancel_run[-1])
-            partitions.append(chunk)
-            # Remove an even number of gates from each chunk
-            for chunk in partitions:
-                if len(chunk) % 2 == 0:
-                    dag.remove_op_node(chunk[0])
-                for node in chunk[1:]:
-                    dag.remove_op_node(node)
+        # Sets of gate runs by name, for instance: [{(H 0, H 0), (H 1, H 1)}, {(X 0, X 0}]
+        gate_runs_sets = [dag.collect_runs([gate.name]) for gate in self_inverse_gates]
+        for gate_runs in gate_runs_sets:
+            for gate_cancel_run in gate_runs:
+                partitions = []
+                chunk = []
+                for i in range(len(gate_cancel_run) - 1):
+                    chunk.append(gate_cancel_run[i])
+                    if gate_cancel_run[i].qargs != gate_cancel_run[i + 1].qargs:
+                        partitions.append(chunk)
+                        chunk = []
+                chunk.append(gate_cancel_run[-1])
+                partitions.append(chunk)
+                # Remove an even number of gates from each chunk
+                for chunk in partitions:
+                    if len(chunk) % 2 == 0:
+                        dag.remove_op_node(chunk[0])
+                    for node in chunk[1:]:
+                        dag.remove_op_node(node)
         return dag
 
     def _run_on_inverse_pairs(self, dag: DAGCircuit, inverse_gate_pairs: List[Tuple[Gate, Gate]]):
