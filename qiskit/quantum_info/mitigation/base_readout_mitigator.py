c5daa3b045078413787b621eeb95048d63e0adfc
Shelly Garion <shelly@il.ibm.com>
2021-08-26 11:26:56 +0300

    remove unnecessary methods from base_readout_mitigator

--- qiskit/quantum_info/mitigation/base_readout_mitigator.py
+++ qiskit/quantum_info/mitigation/base_readout_mitigator.py
@@ -29,10 +29,8 @@ except ImportError:
 class BaseReadoutMitigator(ABC):
     """Base readout error mitigator class."""
 
-    def quasi_probabilities(self,
-                            data: Counts,
-                            qubits: Iterable[int] = None,
-                            shots: Optional[int] = None
+    def quasi_probabilities(
+        self, data: Counts, qubits: Iterable[int] = None, shots: Optional[int] = None
     ) -> (Dict[str, float], Dict[str, float]):
         """Convert counts to a dictionary of quasi-probabilities
 
@@ -59,12 +57,13 @@ class BaseReadoutMitigator(ABC):
             QiskitError: if qubits is not None and does not match the number of count clbits.
         """
 
-    def expectation_value(self,
-                          data: Counts,
-                          diagonal: np.ndarray,
-                          qubits: Iterable[int] = None,
-                          shots: Optional[int] = None,
-                          ) -> Tuple[float, float]:
+    def expectation_value(
+        self,
+        data: Counts,
+        diagonal: np.ndarray,
+        qubits: Iterable[int] = None,
+        shots: Optional[int] = None,
+    ) -> Tuple[float, float]:
         """Calculate the expectation value of a diagonal Hermitian operator.
 
         Args:
@@ -85,72 +84,4 @@ class BaseReadoutMitigator(ABC):
         Raises:
             QiskitError: if the diagonal does not match the number of count clbits.
             QiskitError: if qubits is not None and does not match the number of count clbits.
-        """
-
-    @abstractmethod
-    def mitigation_matrix(self, qubits: List[int] = None) -> np.ndarray:
-        r"""Return the measurement mitigation matrix for the specified qubits.
-        The mitigation matrix :math:`A^{-1}` is defined as the inverse of the
-        :meth:`assignment_matrix` :math:`A`.
-
-        Args:
-            qubits: Optional, qubits being measured for operator expval.
-
-        Returns:
-            np.ndarray: the measurement error mitigation matrix :math:`A^{-1}`.
-        """
-
-    @abstractmethod
-    def assignment_matrix(self, qubits: List[int] = None) -> np.ndarray:
-        r"""Return the measurement assignment matrix for specified qubits.
-        The assignment matrix is the stochastic matrix :math:`A` which assigns
-        a noisy measurement probability distribution to an ideal input
-        measurement distribution: :math:`P(i|j) = \langle i | A | j \rangle`.
-        Args:
-            qubits: Optional, qubits being measured for operator expval.
-        Returns:
-            np.ndarray: the assignment matrix A.
-        """
-
-    def assignment_fidelity(self, qubits: Optional[List[int]] = None) -> float:
-        r"""Return the measurement assignment fidelity on the specified qubits.
-        The assignment fidelity on N-qubits is defined as
-        :math:`\sum_{x\in\{0, 1\}^n} P(x|x) / 2^n`, where
-        :math:`P(x|x) = \rangle x|A|x\langle`, and :math:`A` is the
-        :meth:`assignment_matrix`.
-
-        Args:
-            qubits: Optional, qubits being measured for operator expval.
-
-        Returns:
-            float: the assignment fidelity.
-        """
-        return self.assignment_matrix(qubits=qubits).diagonal().mean()
-
-    def plot_assignment_matrix(self, qubits=None, ax=None):
-        """Matrix plot of the readout error assignment matrix.
-
-        Args:
-            qubits (list(int)): Optional, qubits being measured for operator expval.
-            ax (axes): Optional. Axes object to add plot to.
-
-        Returns:
-            plt.axes: the figure axes object.
-
-        Raises:
-            ImportError: if matplotlib is not installed.
-        """
-
-    def plot_mitigation_matrix(self, qubits=None, ax=None):
-        """Matrix plot of the readout error mitigation matrix.
-
-        Args:
-            qubits (list(int)): Optional, qubits being measured for operator expval.
-            ax (plt.axes): Optional. Axes object to add plot to.
-
-        Returns:
-            plt.axes: the figure axes object.
-
-        Raises:
-            ImportError: if matplotlib is not installed.
         """
