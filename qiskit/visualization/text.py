808e66d06021e850a9287e70a89e3df46aafdf7f
Edwin Navarro <enavarro@comcast.net>
2021-09-03 03:19:10 -0700

    Fix measure with condition display in mpl and latex drawers (#6822)
    * Fix measure with condition display in mpl and latex drawers
    
    * Remove exceptions import
    
    * Adjust indicator spacing for measure and condition
    
    * Add measure arrow
    
    * Fix ref images
    
    * Fix arrow
    
    * Fix refs
    
    * Fix measure cregbundle creg and condition value displays
    
    * Lint
    
    * Clean up condition display
    
    * Fix some condition displays
    
    * Add latex and mpl tests
    
    * Fix mpl tests
    
    * Lint
    
    * Adjust user_ax test
    
    * Switch text and latex drawers to hex conditionals
    
    * Release note
    
    * Final merge
    
    * Change hex display
    
    Co-authored-by: Luciano Bello <bel@zurich.ibm.com>

--- qiskit/visualization/text.py
+++ qiskit/visualization/text.py
@@ -891,11 +891,6 @@ class TextDrawing:
         return lines
 
     @staticmethod
-    def label_for_conditional(node):
-        """Creates the label for a conditional instruction."""
-        return "= %s" % node.op.condition[1]
-
-    @staticmethod
     def special_label(node):
         """Some instructions have special labels"""
         labels = {IGate: "I", SXGate: "√X", SXdgGate: "√Xdg"}
@@ -1384,7 +1379,7 @@ class Layer:
                 label = f"{bit_reg.name}_{bit_index} {label_bool}"
                 self.set_clbit(creg, BoxOnClWire(label=label, top_connect=top_connect))
             else:
-                label = "= %s" % val
+                label = "%s" % str(hex(val))
                 self.set_clbit(creg[0], BoxOnClWire(label=label, top_connect=top_connect))
         else:
             if isinstance(creg, Clbit):
@@ -1410,7 +1405,7 @@ class Layer:
         for i, bit in enumerate(clbits):
             bot_connect = " "
             if bit == clbits[-1]:
-                bot_connect = "=%s" % str(int(val, 2))
+                bot_connect = "%s" % str(hex(int(val, 2)))
             if vlist[i] == "1":
                 self.set_clbit(bit, ClBullet(top_connect="║", bot_connect=bot_connect))
             elif vlist[i] == "0":
