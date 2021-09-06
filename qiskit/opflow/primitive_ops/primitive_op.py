a72c19603a7999db8168edc2782825852797a9ba
dlasecki <dal@zurich.ibm.com>
2021-09-06 08:38:02 +0200

    Update qiskit/opflow/primitive_ops/primitive_op.py
    Co-authored-by: Jake Lishman <jake@binhbar.com>

--- qiskit/opflow/primitive_ops/primitive_op.py
+++ qiskit/opflow/primitive_ops/primitive_op.py
@@ -246,11 +246,9 @@ class PrimitiveOp(OperatorBase):
                 return ListOp([self.assign_parameters(param_dict) for param_dict in unrolled_dict])
             if self.coeff.parameters <= set(unrolled_dict.keys()):
                 binds = {param: unrolled_dict[param] for param in self.coeff.parameters}
-                param_value = self.coeff.bind(binds)
-                if np.imag(complex(param_value)) == 0:
-                    param_value = float(param_value)
-                else:
-                    param_value = complex(param_value)
+                param_value = complex(self.coeff.bind(binds))
+                if abs(param_value.imag) == 0:
+                    param_value = param_value.real
         return self.__class__(self.primitive, coeff=param_value)
 
     # Nothing to collapse here.
