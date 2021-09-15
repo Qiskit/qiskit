d77d73ee70ddf5f7fedcb59268e4f5841b67b29b
wmurphy-collabstar <56129494+wmurphy-collabstar@users.noreply.github.com>
2021-09-15 11:10:01 -0400

    Fixed issue 5920 (#6294)
    Co-authored-by: Abby Mitchell abby.s.mitchell@gmail.com
    Co-authored-by: Winona Murphy 56129494+wmurphy-collabstar@users.noreply.github.com
    Co-authored-by: Luciano Bello <bel@zurich.ibm.com>
    Co-authored-by: Abby-Mitchell <abby.mitchell@btinternet.com>

--- test/ipynb/mpl/circuit/test_circuit_matplotlib_drawer.py
+++ test/ipynb/mpl/circuit/test_circuit_matplotlib_drawer.py
@@ -98,6 +98,105 @@ class TestMatplotlibDrawer(QiskitTestCase):
 
         self.circuit_drawer(circuit, filename="empty_circut.png")
 
+    def test_calibrations(self):
+        """Test calibrations annotations
+        See https://github.com/Qiskit/qiskit-terra/issues/5920
+        """
+
+        circuit = QuantumCircuit(2, 2)
+        circuit.h(0)
+
+        from qiskit import pulse
+
+        with pulse.build(name="hadamard") as h_q0:
+            pulse.play(
+                pulse.library.Gaussian(duration=128, amp=0.1, sigma=16), pulse.DriveChannel(0)
+            )
+
+        circuit.add_calibration("h", [0], h_q0)
+
+        self.circuit_drawer(circuit, filename="calibrations.png")
+
+    def test_calibrations_with_control_gates(self):
+        """Test calibrations annotations
+        See https://github.com/Qiskit/qiskit-terra/issues/5920
+        """
+
+        circuit = QuantumCircuit(2, 2)
+        circuit.cx(0, 1)
+        circuit.ch(0, 1)
+
+        from qiskit import pulse
+
+        with pulse.build(name="cnot") as cx_q01:
+            pulse.play(
+                pulse.library.Gaussian(duration=128, amp=0.1, sigma=16), pulse.DriveChannel(1)
+            )
+
+        circuit.add_calibration("cx", [0, 1], cx_q01)
+
+        with pulse.build(name="ch") as ch_q01:
+            pulse.play(
+                pulse.library.Gaussian(duration=128, amp=0.1, sigma=16), pulse.DriveChannel(1)
+            )
+
+        circuit.add_calibration("ch", [0, 1], ch_q01)
+
+        self.circuit_drawer(circuit, filename="calibrations_with_control_gates.png")
+
+    def test_calibrations_with_swap_and_reset(self):
+        """Test calibrations annotations
+        See https://github.com/Qiskit/qiskit-terra/issues/5920
+        """
+
+        circuit = QuantumCircuit(2, 2)
+        circuit.swap(0, 1)
+        circuit.reset(0)
+
+        from qiskit import pulse
+
+        with pulse.build(name="swap") as swap_q01:
+            pulse.play(
+                pulse.library.Gaussian(duration=128, amp=0.1, sigma=16), pulse.DriveChannel(1)
+            )
+
+        circuit.add_calibration("swap", [0, 1], swap_q01)
+
+        with pulse.build(name="reset") as reset_q0:
+            pulse.play(
+                pulse.library.Gaussian(duration=128, amp=0.1, sigma=16), pulse.DriveChannel(1)
+            )
+
+        circuit.add_calibration("reset", [0], reset_q0)
+
+        self.circuit_drawer(circuit, filename="calibrations_with_swap_and_reset.png")
+
+    def test_calibrations_with_rzz_and_rxx(self):
+        """Test calibrations annotations
+        See https://github.com/Qiskit/qiskit-terra/issues/5920
+        """
+        circuit = QuantumCircuit(2, 2)
+        circuit.rzz(pi, 0, 1)
+        circuit.rxx(pi, 0, 1)
+
+        from qiskit import pulse
+
+        with pulse.build(name="rzz") as rzz_q01:
+            pulse.play(
+                pulse.library.Gaussian(duration=128, amp=0.1, sigma=16), pulse.DriveChannel(1)
+            )
+
+        circuit.add_calibration("rzz", [0, 1], rzz_q01)
+
+        with pulse.build(name="rxx") as rxx_q01:
+            pulse.play(
+                pulse.library.Gaussian(duration=128, amp=0.1, sigma=16), pulse.DriveChannel(1)
+            )
+
+        circuit.add_calibration("rxx", [0, 1], rxx_q01)
+
+        self.circuit_drawer(circuit, filename="calibrations_with_rzz_and_rxx.png")
+
     def test_no_ops(self):
         """Test circuit with no ops.
         See https://github.com/Qiskit/qiskit-terra/issues/5393"""
