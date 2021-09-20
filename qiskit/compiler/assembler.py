Qiskit/qiskit

commit 7df731095b810ade0893565c4f3a516fa10f1386

Enable lo frequencies to be passed in circuits (#6167)
* Add lo freqs for circuits

* Update execute and disassemble

* Add checks on qub/meas lo ranges and list sizes

* Refactor freq check

* Minor stylistic changes

* Remove e from ykt conf json

* Formatting change

* Use f strings and update schemas

* Add host of tests; need to check a few more edge cases including equivalent job and expt level los

* Add additional tests for expt level los

* Update note and add expt level range check

* Add release note

* Fix pulse backends not having defaults

* Rm extra pdb

* Fix long line

* Capitalize and remove abbr

* Add default try/except

* Fix line len

* Lint fix

* Loosen lo checks to work for simulators

* Fix lint on lo freq check

* Further black reformatting

* docstring

* paragraph

* simpler type

Co-authored-by: Luciano Bello <bel@zurich.ibm.com>
Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

qiskit/compiler/assembler.py
@@ -56,10 +56,10 @@ def assemble(
     memory: Optional[bool] = False,
     max_credits: Optional[int] = None,
     seed_simulator: Optional[int] = None,
-    qubit_lo_freq: Optional[List[int]] = None,
-    meas_lo_freq: Optional[List[int]] = None,
-    qubit_lo_range: Optional[List[int]] = None,
-    meas_lo_range: Optional[List[int]] = None,
+    qubit_lo_freq: Optional[List[float]] = None,
+    meas_lo_freq: Optional[List[float]] = None,
+    qubit_lo_range: Optional[List[float]] = None,
+    meas_lo_range: Optional[List[float]] = None,
     schedule_los: Optional[
         Union[
             List[Union[Dict[PulseChannel, float], LoConfig]],
@@ -101,15 +101,19 @@ def assemble(
             measurement level 2 supports this option.
         max_credits: Maximum credits to spend on job. Default: 10
         seed_simulator: Random seed to control sampling, for when backend is a simulator
-        qubit_lo_freq: List of default qubit LO frequencies in Hz. Will be overridden by
-            ``schedule_los`` if set.
-        meas_lo_freq: List of default measurement LO frequencies in Hz. Will be overridden
-            by ``schedule_los`` if set.
-        qubit_lo_range: List of drive LO ranges each of form ``[range_min, range_max]`` in Hz.
-            Used to validate the supplied qubit frequencies.
-        meas_lo_range: List of measurement LO ranges each of form ``[range_min, range_max]`` in Hz.
-            Used to validate the supplied qubit frequencies.
-        schedule_los: Experiment LO configurations, frequencies are given in Hz.
+        qubit_lo_freq: List of job level qubit drive LO frequencies in Hz. Overridden by
+            ``schedule_los`` if specified. Must have length ``n_qubits.``
+        meas_lo_freq: List of measurement LO frequencies in Hz. Overridden by ``schedule_los`` if
+            specified. Must have length ``n_qubits.``
+        qubit_lo_range: List of job level drive LO ranges each of form ``[range_min, range_max]``
+            in Hz. Used to validate ``qubit_lo_freq``. Must have length ``n_qubits.``
+        meas_lo_range: List of job level measurement LO ranges each of form
+            ``[range_min, range_max]`` in Hz. Used to validate ``meas_lo_freq``. Must have length
+            ``n_qubits.``
+        schedule_los: Experiment level (ie circuit or schedule) LO frequency configurations for
+            qubit drive and measurement channels. These values override the job level values from
+            ``default_qubit_los`` and ``default_meas_los``. Frequencies are in Hz. Settable for qasm
+            and pulse jobs.
         meas_level: Set the appropriate level of the measurement output for pulse experiments.
         meas_return: Level of measurement data for the backend to return.
 
@@ -160,6 +164,11 @@ def assemble(
         seed_simulator,
         init_qubits,
         rep_delay,
+        qubit_lo_freq,
+        meas_lo_freq,
+        qubit_lo_range,
+        meas_lo_range,
+        schedule_los,
         **run_config,
     )
 
@@ -190,11 +199,6 @@ def assemble(
     elif all(isinstance(exp, (ScheduleBlock, Schedule, Instruction)) for exp in experiments):
         run_config = _parse_pulse_args(
             backend,
-            qubit_lo_freq,
-            meas_lo_freq,
-            qubit_lo_range,
-            meas_lo_range,
-            schedule_los,
             meas_level,
             meas_return,
             meas_map,
@@ -227,6 +231,11 @@ def _parse_common_args(
     seed_simulator,
     init_qubits,
     rep_delay,
+    qubit_lo_freq,
+    meas_lo_freq,
+    qubit_lo_range,
+    meas_lo_range,
+    schedule_los,
     **run_config,
 ):
     """Resolve the various types of args allowed to the assemble() function through
@@ -242,19 +251,33 @@ def _parse_common_args(
             and determines the runtime environment.
 
     Raises:
-        QiskitError: if the memory arg is True and the backend does not support
-            memory. Also if shots exceeds max_shots for the configured backend. Also if
-            the type of shots is not int.
+        QiskitError:
+            - If the memory arg is True and the backend does not support memory.
+            - If ``shots`` exceeds ``max_shots`` for the configured backend.
+            - If ``shots`` are not int type.
+            - If any of qubit or meas lo's, or associated ranges do not have length equal to
+            ``n_qubits``.
+            - If qubit or meas lo's do not fit into perscribed ranges.
     """
     # grab relevant info from backend if it exists
     backend_config = None
+    backend_defaults = None
+    n_qubits = None
     if backend:
         backend_config = backend.configuration()
+        n_qubits = backend_config.n_qubits
         # check for memory flag applied to backend that does not support memory
         if memory and not backend_config.memory:
             raise QiskitError(
                 "memory not supported by backend {}".format(backend_config.backend_name)
             )
+
+        # try to set defaults for pulse, other leave as None
+        if backend_config.open_pulse:
+            try:
+                backend_defaults = backend.defaults()
+            except AttributeError:
+                pass
 
     # an identifier for the Qobj
     qobj_id = qobj_id or str(uuid.uuid4())
@@ -299,6 +322,27 @@ def _parse_common_args(
                 RuntimeWarning,
             )
 
+    qubit_lo_freq = qubit_lo_freq or getattr(backend_defaults, "qubit_freq_est", None)
+    meas_lo_freq = meas_lo_freq or getattr(backend_defaults, "meas_freq_est", None)
+
+    qubit_lo_range = qubit_lo_range or getattr(backend_config, "qubit_lo_range", None)
+    meas_lo_range = meas_lo_range or getattr(backend_config, "meas_lo_range", None)
+
+    # check that LO frequencies are in the perscribed range
+    _check_lo_freqs(qubit_lo_freq, qubit_lo_range, "qubit")
+    _check_lo_freqs(meas_lo_freq, meas_lo_range, "meas")
+
+    # configure experiment level LO frequencies
+    schedule_los = schedule_los or []
+    if isinstance(schedule_los, (LoConfig, dict)):
+        schedule_los = [schedule_los]
+
+    # Convert to LoConfig if LO configuration supplied as dictionary
+    schedule_los = [
+        lo_config if isinstance(lo_config, LoConfig) else LoConfig(lo_config)
+        for lo_config in schedule_los
+    ]
+
     # create run configuration and populate
     run_config_dict = dict(
         shots=shots,
@@ -307,19 +351,55 @@ def _parse_common_args(
         seed_simulator=seed_simulator,
         init_qubits=init_qubits,
         rep_delay=rep_delay,
+        qubit_lo_freq=qubit_lo_freq,
+        meas_lo_freq=meas_lo_freq,
+        qubit_lo_range=qubit_lo_range,
+        meas_lo_range=meas_lo_range,
+        schedule_los=schedule_los,
+        n_qubits=n_qubits,
         **run_config,
     )
 
     return qobj_id, qobj_header, run_config_dict
 
 
+def _check_lo_freqs(
+    lo_freq: Union[List[float], None],
+    lo_range: Union[List[float], None],
+    lo_type: str,
+):
+    """Check that LO frequencies are within the perscribed LO range.
+
+    NOTE: Only checks if frequency/range lists have equal length. And does not check that the lists
+    have length ``n_qubits``. This is because some backends, like simulator backends, do not
+    require these constraints. For real hardware, these parameters will be validated on the backend.
+
+    Args:
+        lo_freq: List of LO frequencies.
+        lo_range: Nested list of LO frequency ranges. Inner list is of the form
+            ``[lo_min, lo_max]``.
+        lo_type: The type of LO value--"qubit" or "meas".
+
+    Raises:
+        QiskitError:
+            - If each element of the LO range is not a 2d list.
+            - If the LO frequency is not in the LO range for a given qubit.
+    """
+    if lo_freq and lo_range and len(lo_freq) == len(lo_range):
+        for i, freq in enumerate(lo_freq):
+            freq_range = lo_range[i]
+            if not (isinstance(freq_range, list) and len(freq_range) == 2):
+                raise QiskitError("Each element of {} LO range must be a 2d list.".format(lo_type))
+            if freq < freq_range[0] or freq > freq_range[1]:
+                raise QiskitError(
+                    "Qubit {} {} LO frequency is {}. The range is [{}, {}].".format(
+                        i, lo_type, freq, freq_range[0], freq_range[1]
+                    )
+                )
+
+
 def _parse_pulse_args(
     backend,
-    qubit_lo_freq,
-    meas_lo_freq,
-    qubit_lo_range,
-    meas_lo_range,
-    schedule_los,
     meas_level,
     meas_return,
     meas_map,
@@ -339,9 +419,7 @@ def _parse_pulse_args(
     """
     # grab relevant info from backend if it exists
     backend_config = None
-    backend_default = None
     if backend:
-        backend_default = backend.defaults()
         backend_config = backend.configuration()
 
         if meas_level not in getattr(backend_config, "meas_levels", [MeasLevel.CLASSIFIED]):
@@ -352,25 +430,6 @@ def _parse_pulse_args(
             )
 
     meas_map = meas_map or getattr(backend_config, "meas_map", None)
-
-    schedule_los = schedule_los or []
-    if isinstance(schedule_los, (LoConfig, dict)):
-        schedule_los = [schedule_los]
-
-    # Convert to LoConfig if LO configuration supplied as dictionary
-    schedule_los = [
-        lo_config if isinstance(lo_config, LoConfig) else LoConfig(lo_config)
-        for lo_config in schedule_los
-    ]
-
-    if not qubit_lo_freq and hasattr(backend_default, "qubit_freq_est"):
-        qubit_lo_freq = backend_default.qubit_freq_est
-    if not meas_lo_freq and hasattr(backend_default, "meas_freq_est"):
-        meas_lo_freq = backend_default.meas_freq_est
-
-    qubit_lo_range = qubit_lo_range or getattr(backend_config, "qubit_lo_range", None)
-    meas_lo_range = meas_lo_range or getattr(backend_config, "meas_lo_range", None)
-
     dynamic_reprate_enabled = getattr(backend_config, "dynamic_reprate_enabled", False)
 
     rep_time = rep_time or getattr(backend_config, "rep_times", None)
@@ -389,11 +448,6 @@ def _parse_pulse_args(
 
     # create run configuration and populate
     run_config_dict = dict(
-        qubit_lo_freq=qubit_lo_freq,
-        meas_lo_freq=meas_lo_freq,
-        qubit_lo_range=qubit_lo_range,
-        meas_lo_range=meas_lo_range,
-        schedule_los=schedule_los,
         meas_level=meas_level,
         meas_return=meas_return,
         meas_map=meas_map,

qiskit/assembler/assemble_circuits.py
@@ -11,6 +11,7 @@
 # that they have been altered from the originals.
 
 """Assemble function for converting a list of circuits into a qobj."""
+import copy
 from collections import defaultdict
 from typing import Dict, List, Optional, Tuple
 
@@ -260,6 +261,43 @@ def _extract_common_calibrations(
     return experiments, QasmExperimentCalibrations(gates=common_calibrations)
 
 
+def _configure_experiment_los(
+    experiments: List[QasmQobjExperiment],
+    lo_converter: converters.LoConfigConverter,
+    run_config: RunConfig,
+):
+    # get per experiment los
+    freq_configs = [lo_converter(lo_dict) for lo_dict in getattr(run_config, "schedule_los", [])]
+
+    if len(experiments) > 1 and len(freq_configs) not in [0, 1, len(experiments)]:
+        raise QiskitError(
+            "Invalid 'schedule_los' setting specified. If specified, it should be "
+            "either have a single entry to apply the same LOs for each experiment or "
+            "have length equal to the number of experiments."
+        )
+
+    if len(freq_configs) > 1:
+        if len(experiments) > 1:
+            for idx, expt in enumerate(experiments):
+                freq_config = freq_configs[idx]
+                expt.config.qubit_lo_freq = freq_config.qubit_lo_freq
+                expt.config.meas_lo_freq = freq_config.meas_lo_freq
+        elif len(experiments) == 1:
+            expt = experiments[0]
+            experiments = []
+            for freq_config in freq_configs:
+                expt_config = copy.deepcopy(expt.config)
+                expt_config.qubit_lo_freq = freq_config.qubit_lo_freq
+                expt_config.meas_lo_freq = freq_config.meas_lo_freq
+                experiments.append(
+                    QasmQobjExperiment(
+                        header=expt.header, instructions=expt.instructions, config=expt_config
+                    )
+                )
+
+    return experiments
+
+
 def assemble_circuits(
     circuits: List[QuantumCircuit], run_config: RunConfig, qobj_id: int, qobj_header: QobjHeader
 ) -> QasmQobj:
@@ -274,9 +312,54 @@ def assemble_circuits(
     Returns:
         The qobj to be run on the backends
     """
+    # assemble the circuit experiments
+    experiments_and_pulse_libs = parallel_map(_assemble_circuit, circuits, [run_config])
+    experiments = []
+    pulse_library = {}
+    for exp, lib in experiments_and_pulse_libs:
+        experiments.append(exp)
+        if lib:
+            pulse_library.update(lib)
+
+    # extract common calibrations
+    experiments, calibrations = _extract_common_calibrations(experiments)
+
+    # configure LO freqs per circuit
+    lo_converter = converters.LoConfigConverter(QasmQobjExperimentConfig, **run_config.to_dict())
+    experiments = _configure_experiment_los(experiments, lo_converter, run_config)
+
     qobj_config = QasmQobjConfig()
     if run_config:
-        qobj_config = QasmQobjConfig(**run_config.to_dict())
+        qobj_config_dict = run_config.to_dict()
+
+        # remove LO ranges, not needed in qobj
+        qobj_config_dict.pop("qubit_lo_range", None)
+        qobj_config_dict.pop("meas_lo_range", None)
+
+        # convert LO frequencies to GHz, if they exist
+        if "qubit_lo_freq" in qobj_config_dict:
+            qobj_config_dict["qubit_lo_freq"] = [
+                freq / 1e9 for freq in qobj_config_dict["qubit_lo_freq"]
+            ]
+        if "meas_lo_freq" in qobj_config_dict:
+            qobj_config_dict["meas_lo_freq"] = [
+                freq / 1e9 for freq in qobj_config_dict["meas_lo_freq"]
+            ]
+
+        # override default los if single ``schedule_los`` entry set
+        schedule_los = qobj_config_dict.pop("schedule_los", [])
+        if len(schedule_los) == 1:
+            lo_dict = schedule_los[0]
+            q_los = lo_converter.get_qubit_los(lo_dict)
+            # Hz -> GHz
+            if q_los:
+                qobj_config_dict["qubit_lo_freq"] = [freq / 1e9 for freq in q_los]
+            m_los = lo_converter.get_meas_los(lo_dict)
+            if m_los:
+                qobj_config_dict["meas_lo_freq"] = [freq / 1e9 for freq in m_los]
+
+        qobj_config = QasmQobjConfig(**qobj_config_dict)
+
     qubit_sizes = []
     memory_slot_sizes = []
     for circ in circuits:
@@ -291,18 +374,11 @@ def assemble_circuits(
     qobj_config.memory_slots = max(memory_slot_sizes)
     qobj_config.n_qubits = max(qubit_sizes)
 
-    experiments_and_pulse_libs = parallel_map(_assemble_circuit, circuits, [run_config])
-    experiments = []
-    pulse_library = {}
-    for exp, lib in experiments_and_pulse_libs:
-        experiments.append(exp)
-        if lib:
-            pulse_library.update(lib)
     if pulse_library:
         qobj_config.pulse_library = [
             PulseLibraryItem(name=name, samples=samples) for name, samples in pulse_library.items()
         ]
-    experiments, calibrations = _extract_common_calibrations(experiments)
+
     if calibrations and calibrations.gates:
         qobj_config.calibrations = calibrations
 

qiskit/assembler/assemble_schedules.py
@@ -90,10 +90,9 @@ def _assemble_experiments(
 
     if len(schedules) > 1 and len(freq_configs) not in [0, 1, len(schedules)]:
         raise QiskitError(
-            "Invalid frequency setting is specified. If the frequency is specified, "
-            "it should be configured the same for all schedules, configured for each "
-            "schedule, or a list of frequencies should be provided for a single "
-            "frequency sweep schedule."
+            "Invalid 'schedule_los' setting specified. If specified, it should be "
+            "either have a single entry to apply the same LOs for each schedule or "
+            "have length equal to the number of schedules."
         )
 
     instruction_converter = getattr(
@@ -313,11 +312,11 @@ def _assemble_config(
     if isinstance(meas_level, qobj_utils.MeasLevel):
         qobj_config["meas_level"] = meas_level.value
 
-    # convert lo frequencies to Hz
+    # convert LO frequencies to GHz
     qobj_config["qubit_lo_freq"] = [freq / 1e9 for freq in qobj_config["qubit_lo_freq"]]
     qobj_config["meas_lo_freq"] = [freq / 1e9 for freq in qobj_config["meas_lo_freq"]]
 
-    # frequency sweep config
+    # override defaults if single entry for ``schedule_los``
     schedule_los = qobj_config.pop("schedule_los", [])
     if len(schedule_los) == 1:
         lo_dict = schedule_los[0]

qiskit/assembler/disassemble.py
@@ -55,6 +55,16 @@ def disassemble(qobj) -> Union[CircuitModule, PulseModule]:
 
 def _disassemble_circuit(qobj) -> CircuitModule:
     run_config = qobj.config.to_dict()
+
+    # convert lo freq back to Hz
+    qubit_lo_freq = run_config.get("qubit_lo_freq", [])
+    if qubit_lo_freq:
+        run_config["qubit_lo_freq"] = [freq * 1e9 for freq in qubit_lo_freq]
+
+    meas_lo_freq = run_config.get("meas_lo_freq", [])
+    if meas_lo_freq:
+        run_config["meas_lo_freq"] = [freq * 1e9 for freq in meas_lo_freq]
+
     user_qobj_header = qobj.header.to_dict()
     return CircuitModule((_experiments_to_circuits(qobj), run_config, user_qobj_header))
 

qiskit/execute_function.py
@@ -54,6 +54,8 @@ def execute(
     seed_simulator=None,
     default_qubit_los=None,
     default_meas_los=None,  # schedule run options
+    qubit_lo_range=None,
+    meas_lo_range=None,
     schedule_los=None,
     meas_level=None,
     meas_return=None,
@@ -168,15 +170,33 @@ def execute(
 
         seed_simulator (int): Random seed to control sampling, for when backend is a simulator
 
-        default_qubit_los (list): List of default qubit LO frequencies in Hz
+        default_qubit_los (Optional[List[float]]): List of job level qubit drive LO frequencies
+            in Hz. Overridden by ``schedule_los`` if specified. Must have length ``n_qubits``.
 
-        default_meas_los (list): List of default meas LO frequencies in Hz
+        default_meas_los (Optional[List[float]]): List of job level measurement LO frequencies in
+            Hz. Overridden by ``schedule_los`` if specified. Must have length ``n_qubits``.
 
-        schedule_los (None or list or dict or LoConfig): Experiment LO
-            configurations, if specified the list is in the format::
+        qubit_lo_range (Optional[List[List[float]]]): List of job level drive LO ranges each of form
+            ``[range_min, range_max]`` in Hz. Used to validate ``qubit_lo_freq``. Must have length
+            ``n_qubits``.
 
-                list[Union[Dict[PulseChannel, float], LoConfig]] or
-                     Union[Dict[PulseChannel, float], LoConfig]
+        meas_lo_range (Optional[List[List[float]]]): List of job level measurement LO ranges each of
+            form ``[range_min, range_max]`` in Hz. Used to validate ``meas_lo_freq``. Must have
+            length ``n_qubits``.
+
+        schedule_los (list):
+            Experiment level (ie circuit or schedule) LO frequency configurations for qubit drive
+            and measurement channels. These values override the job level values from
+            ``default_qubit_los`` and ``default_meas_los``. Frequencies are in Hz. Settable for qasm
+            and pulse jobs.
+
+            If a single LO config or dict is used, the values are set at job level. If a list is
+            used, the list must be the size of the number of experiments in the job, except in the
+            case of a single experiment. In this case, a frequency sweep will be assumed and one
+            experiment will be created for every list entry.
+
+            Not every channel is required to be specified. If not specified, the backend default
+            value will be used.
 
         meas_level (int or MeasLevel): Set the appropriate level of the
             measurement output for pulse experiments.
@@ -312,8 +332,10 @@ def execute(
             memory=memory,
             max_credits=max_credits,
             seed_simulator=seed_simulator,
-            default_qubit_los=default_qubit_los,
-            default_meas_los=default_meas_los,
+            qubit_lo_freq=default_qubit_los,
+            meas_lo_freq=default_meas_los,
+            qubit_lo_range=qubit_lo_range,
+            meas_lo_range=meas_lo_range,
             schedule_los=schedule_los,
             meas_level=meas_level,
             meas_return=meas_return,
@@ -338,8 +360,10 @@ def execute(
             "shots": shots,
             "memory": memory,
             "seed_simulator": seed_simulator,
-            "default_qubit_los": default_qubit_los,
-            "default_meas_los": default_meas_los,
+            "qubit_lo_freq": default_qubit_los,
+            "meas_lo_freq": default_meas_los,
+            "qubit_lo_range": qubit_lo_range,
+            "meas_lo_range": meas_lo_range,
             "schedule_los": schedule_los,
             "meas_level": meas_level,
             "meas_return": meas_return,

qiskit/providers/models/backendconfiguration.py
@@ -355,6 +355,7 @@ class QasmBackendConfiguration:
         if processor_type is not None:
             self.processor_type = processor_type
 
+        # convert lo range from GHz to Hz
         if "qubit_lo_range" in kwargs.keys():
             kwargs["qubit_lo_range"] = [
                 [min_range * 1e9, max_range * 1e9]
@@ -450,15 +451,16 @@ class QasmBackendConfiguration:
         if "dtm" in out_dict:
             out_dict["dtm"] *= 1e9
 
+        # Use GHz in dict
         if "qubit_lo_range" in out_dict:
             out_dict["qubit_lo_range"] = [
-                [min_range * 1e9, max_range * 1e9]
+                [min_range * 1e-9, max_range * 1e-9]
                 for (min_range, max_range) in out_dict["qubit_lo_range"]
             ]
 
         if "meas_lo_range" in out_dict:
             out_dict["meas_lo_range"] = [
-                [min_range * 1e9, max_range * 1e9]
+                [min_range * 1e-9, max_range * 1e-9]
                 for (min_range, max_range) in out_dict["meas_lo_range"]
             ]
 
@@ -596,12 +598,15 @@ class PulseBackendConfiguration(QasmBackendConfiguration):
         self.n_uchannels = n_uchannels
         self.u_channel_lo = u_channel_lo
         self.meas_levels = meas_levels
+
+        # convert from GHz to Hz
         self.qubit_lo_range = [
             [min_range * 1e9, max_range * 1e9] for (min_range, max_range) in qubit_lo_range
         ]
         self.meas_lo_range = [
             [min_range * 1e9, max_range * 1e9] for (min_range, max_range) in meas_lo_range
         ]
+
         self.meas_kernels = meas_kernels
         self.discriminators = discriminators
         self.hamiltonian = hamiltonian
@@ -726,6 +731,7 @@ class PulseBackendConfiguration(QasmBackendConfiguration):
             out_dict.pop("_channel_qubit_map")
             out_dict.pop("_control_channels")
 
+        # Use GHz in dict
         if self.qubit_lo_range:
             out_dict["qubit_lo_range"] = [
                 [min_range * 1e-9, max_range * 1e-9]

commit 9ce4e0b1ac06bd5d1253fc9d6418e9bbd67e8d05

Avoid configuring pulse defaults in circuit assembly unless needed
This commit addresses a performance regression in assemble() introduced
in #6167. That PR added the ability for users to configure pulse
properties via assemble kwargs (such as lo frequencies). While this
should really only be exposed via a Backend's options object and terra
shouldn't have to deal with it, for legacy/backwards compat reasons we
need to expose these options via assemble(). However, that PR was a bit
aggressive in when it needed to parse the pulse defaults and properties
to determine if input was valid. The intent there was for circuits jobs
that users were configuring pulse configuration we'd be providing the
same checks as with pulse jobs. However for circuits jobs this
additional validation is only needed if the user has specified a pulse
configuration options and we need to check that they're valid. If a user
doesn't we shouldn't be touching anything pulse, especially as just
loading pulse defaults can result in a large performance regression
(roughly 2 orders of magnitude over assembly without it). To fix this is
commit adjusts the logic when we parse the pulse defaults (and all the
subsequent logic that uses them) to be if it's a pulse job, or if it's a
circuit job and one of the named pulse paramters has been set. This
returns the performance for pure circuits job to what it was prior to
PR #6167 but still retains the functionality that #6167 provided when
it's needed.

qiskit/compiler/assembler.py
@@ -157,6 +157,7 @@ def assemble(
     """
     start_time = time()
     experiments = experiments if isinstance(experiments, list) else [experiments]
+    pulse_qobj = any(isinstance(exp, (ScheduleBlock, Schedule, Instruction)) for exp in experiments)
     qobj_id, qobj_header, run_config_common_dict = _parse_common_args(
         backend,
         qobj_id,
@@ -172,6 +173,7 @@ def assemble(
         qubit_lo_range,
         meas_lo_range,
         schedule_los,
+        pulse_qobj=pulse_qobj,
         **run_config,
     )
 
@@ -237,6 +239,7 @@ def _parse_common_args(
     qubit_lo_range,
     meas_lo_range,
     schedule_los,
+    pulse_qobj=False,
     **run_config,
 ):
     """Resolve the various types of args allowed to the assemble() function through
@@ -272,7 +275,14 @@ def _parse_common_args(
             raise QiskitError(f"memory not supported by backend {backend_config.backend_name}")
 
         # try to set defaults for pulse, other leave as None
-        if backend_config.open_pulse:
+        pulse_param_set = (
+            qubit_lo_freq is not None
+            or meas_lo_freq is not None
+            or qubit_lo_range is not None
+            or meas_lo_range is not None
+            or schedule_los is not None
+        )
+        if pulse_qobj or backend_config.open_pulse and pulse_param_set:
             try:
                 backend_defaults = backend.defaults()
             except AttributeError: