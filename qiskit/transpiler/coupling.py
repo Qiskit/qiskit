37c6a632c43c9a50053b467fe04898b8d63fe219
Matthew Treinish <mtreinish@kortar.org>
2021-09-21 13:37:40 -0400

    Expand docstring

--- qiskit/transpiler/coupling.py
+++ qiskit/transpiler/coupling.py
@@ -322,14 +322,48 @@ class CouplingMap:
 
     @classmethod
     def from_heavy_hex(cls, distance, bidirectional=True):
-        """Return a connected and directed heavy hex map."""
+        """Return a heavy hexagon graph coupling map
+
+        A heavy hexagon graph is described in:
+
+        https://journals.aps.org/prx/abstract/10.1103/PhysRevX.10.011022
+
+        Args:
+            distance (int): The code distance for the generated heavy hex
+                graph. The value for distance can be any odd positive integer.
+                The distance relates to the number of qubits by:
+                :math:`n = \\frac{5d^2 - 2d - 1}{2}` where :math:`n` is the
+                number of qubits and :math:`d` is the ``distance`` parameter.
+            bidirectional (bool): Whether the edges in the output coupling
+                graph are bidirectional or not. By default this is set to
+                ``True``
+        Returns:
+            CouplingMap: A heavy hex coupling graph
+        """
         cmap = cls(description="heavy-hex")
         cmap.graph = rx.generators.directed_heavy_hex_graph(distance, bidirectional=bidirectional)
         return cmap
 
     @classmethod
     def from_heavy_square(cls, distance, bidirectional=True):
-        """Return a connected and directed heavy square graph."""
+        """Return a heavy square graph coupling map.
+
+        A heavy square graph is described in:
+
+        https://journals.aps.org/prx/abstract/10.1103/PhysRevX.10.011022
+
+        Args:
+            distance (int): The code distance for the generated heavy hex
+                graph. The value for distance can be any odd positive integer.
+                The distance relates to the number of qubits by:
+                :math:`n = 3d^2 - 2d` where :math:`n` is the
+                number of qubits and :math:`d` is the ``distance`` parameter.
+            bidirectional (bool): Whether the edges in the output coupling
+                graph are bidirectional or not. By default this is set to
+                ``True``
+        Returns:
+            CouplingMap: A heavy hex coupling graph
+        """
         cmap = cls(description="heavy-square")
         cmap.graph = rx.generators.directed_heavy_square_graph(
             distance, bidirectional=bidirectional
