1c6db0926fae26cba92b92a359f7db587d062e1f
Nahum Rosa Cruz Sa <49600259+nahumsa@users.noreply.github.com>
2021-09-27 13:41:49 -0300

    Add hexagonal lattice CouplingMap (#7062)
    * add hexagonal couplingmap
    
    * lint
    
    * Fix release note jupyter
    
    Co-authored-by: Matthew Treinish <mtreinish@kortar.org>
    
    Co-authored-by: Matthew Treinish <mtreinish@kortar.org>

--- qiskit/transpiler/coupling.py
+++ qiskit/transpiler/coupling.py
@@ -370,6 +370,25 @@ class CouplingMap:
         )
         return cmap
 
+    @classmethod
+    def from_hexagonal_lattice(cls, rows, cols, bidirectional=True):
+        """Return a hexagonal lattice graph coupling map.
+
+        Args:
+            rows (int): The number of rows to generate the graph with.
+            cols (int): The number of columns to generate the graph with.
+            bidirectional (bool): Whether the edges in the output coupling
+                graph are bidirectional or not. By default this is set to
+                ``True``
+        Returns:
+            CouplingMap: A hexagonal lattice coupling graph
+        """
+        cmap = cls(description="hexagonal-lattice")
+        cmap.graph = rx.generators.directed_hexagonal_lattice_graph(
+            rows, cols, bidirectional=bidirectional
+        )
+        return cmap
+
     def largest_connected_component(self):
         """Return a set of qubits in the largest connected component."""
         return max(rx.weakly_connected_components(self.graph), key=len)
