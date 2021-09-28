d57351e5396cce560093336b239c9a2438fa5475
Matthew Treinish <mtreinish@kortar.org>
2021-09-28 16:35:08 -0400

    Fix docstring typos
    Co-authored-by: Eric Peterson <peterson.eric.c@gmail.com>

--- qiskit/transpiler/passes/synthesis/plugin.py
+++ qiskit/transpiler/passes/synthesis/plugin.py
@@ -257,7 +257,7 @@ class UnitarySynthesisPlugin(abc.ABC):
             'cx': {(0, 1): 0.012012477900732316, (1, 0): 5.191111111111111e-07}
             }
 
-        Where the ``length`` value is in unites of seconds.
+        where the ``length`` value is in units of seconds.
 
         Do note that this dictionary might not be complete or could be empty
         as it depends on the target backend reporting gate lengths on every
