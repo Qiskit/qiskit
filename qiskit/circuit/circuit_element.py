00c1030da2317ab1b3b30b9e1c83704a93094ea4
Shelly Garion <shelly@il.ibm.com>
2021-09-30 16:03:25 +0300

    add CircuitElement mixin class

---
+++ qiskit/circuit/circuit_element.py
@@ -0,0 +1,43 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2017.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Circuit Element Mixin."""
+
+from abc import ABC, abstractmethod
+
+
+class CircuitElement(ABC):
+    @property
+    @abstractmethod
+    def name(self):
+        """Unique string identifier for operation type."""
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def num_qubits(self):
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def num_clbits(self):
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def num_params(self):
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def params(self):
+        raise NotImplementedError
