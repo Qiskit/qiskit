03023b140f715cf755774f4179c0f0ba4ac2e3ca
Luciano Bello <bel@zurich.ibm.com>
2021-10-11 10:46:35 +0200

    deprecated sympy's expr.expr_free_symbols, move to atoms

--- qiskit/circuit/tools/pi_check.py
+++ qiskit/circuit/tools/pi_check.py
@@ -46,13 +46,13 @@ def pi_check(inpt, eps=1e-6, output="text", ndigits=5):
     """
     if isinstance(inpt, ParameterExpression):
         param_str = str(inpt)
-        if not hasattr(inpt._symbol_expr, "expr_free_symbols"):
+        if not hasattr(inpt._symbol_expr, "kind"):
             from sympy import sympify
 
             expr = sympify(inpt._symbol_expr)
         else:
             expr = inpt._symbol_expr
-        syms = expr.expr_free_symbols
+        syms = expr.atoms()
         for sym in syms:
             if not sym.is_number:
                 continue
