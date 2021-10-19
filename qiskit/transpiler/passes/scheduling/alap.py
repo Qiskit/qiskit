1472d3792a53f495a3f4fa8950e27bba874feb77
itoko <15028342+itoko@users.noreply.github.com>
2021-10-20 00:14:18 +0900

    Fix #7006 by tracking availability of clbits in scheduling (#7085)
    * Add condition_bits to Instruction
    
    * Fix a bug in schduling of circuits using clbits
    
    * Fix small bugs, style and lint
    
    * Update docstrings to tell the limitation of schedulers
    
    * Add release note
    
    * Update docstrings
    
    * Replace with more robust assertions
    
    * Update test/python/transpiler/test_instruction_alignments.py
    
    Co-authored-by: Luciano Bello <bel@zurich.ibm.com>
    Co-authored-by: Kevin Krsulich <kevin.krsulich@ibm.com>
    Co-authored-by: Kevin Krsulich <kevin@krsulich.net>

--- qiskit/transpiler/passes/scheduling/alap.py
+++ qiskit/transpiler/passes/scheduling/alap.py
@@ -11,18 +11,29 @@
 # that they have been altered from the originals.
 
 """ALAP Scheduling."""
+import itertools
 from collections import defaultdict
 from typing import List
-from qiskit.transpiler.passes.scheduling.time_unit_conversion import TimeUnitConversion
-from qiskit.circuit.delay import Delay
+
+from qiskit.circuit import Delay, Measure
 from qiskit.circuit.parameterexpression import ParameterExpression
 from qiskit.dagcircuit import DAGCircuit
 from qiskit.transpiler.basepasses import TransformationPass
 from qiskit.transpiler.exceptions import TranspilerError
+from qiskit.transpiler.passes.scheduling.time_unit_conversion import TimeUnitConversion
 
 
 class ALAPSchedule(TransformationPass):
-    """ALAP Scheduling."""
+    """ALAP Scheduling pass, which schedules the **stop** time of instructions as late as possible.
+
+    For circuits with instructions writing or reading clbits (e.g. measurements, conditional gates),
+    the scheduler assumes clbits I/O operations take no time, ``measure`` locks clbits to be written
+    at its end and ``c_if`` locks clbits to be read at its beginning.
+
+    Notes:
+        The ALAP scheduler may not schedule a circuit exactly the same as any real backend does
+        when the circuit contains control flows (e.g. conditional instructions).
+    """
 
     def __init__(self, durations):
         """ALAPSchedule initializer.
@@ -58,6 +69,8 @@ class ALAPSchedule(TransformationPass):
             new_dag.add_creg(creg)
 
         qubit_time_available = defaultdict(int)
+        clbit_readable = defaultdict(int)
+        clbit_writeable = defaultdict(int)
 
         def pad_with_delays(qubits: List[int], until, unit) -> None:
             """Pad idle time-slots in ``qubits`` with delays in ``unit`` until ``until``."""
@@ -68,11 +81,6 @@ class ALAPSchedule(TransformationPass):
 
         bit_indices = {bit: index for index, bit in enumerate(dag.qubits)}
         for node in reversed(list(dag.topological_op_nodes())):
-            start_time = max(qubit_time_available[q] for q in node.qargs)
-            pad_with_delays(node.qargs, until=start_time, unit=time_unit)
-
-            new_dag.apply_operation_front(node.op, node.qargs, node.cargs)
-
             # validate node.op.duration
             if node.op.duration is None:
                 indices = [bit_indices[qarg] for qarg in node.qargs]
@@ -85,11 +93,32 @@ class ALAPSchedule(TransformationPass):
                     f"Parameterized duration ({node.op.duration}) "
                     f"of {node.op.name} on qubits {indices} is not bounded."
                 )
+            # choose appropriate clbit available time depending on op
+            clbit_time_available = (
+                clbit_writeable if isinstance(node.op, Measure) else clbit_readable
+            )
+            # correction to change clbit start time to qubit start time
+            delta = 0 if isinstance(node.op, Measure) else node.op.duration
+            # must wait for op.condition_bits as well as node.cargs
+            start_time = max(
+                itertools.chain(
+                    (qubit_time_available[q] for q in node.qargs),
+                    (clbit_time_available[c] - delta for c in node.cargs + node.op.condition_bits),
+                )
+            )
+
+            pad_with_delays(node.qargs, until=start_time, unit=time_unit)
+
+            new_dag.apply_operation_front(node.op, node.qargs, node.cargs)
 
             stop_time = start_time + node.op.duration
             # update time table
             for q in node.qargs:
                 qubit_time_available[q] = stop_time
+            for c in node.cargs:  # measure
+                clbit_writeable[c] = clbit_readable[c] = start_time
+            for c in node.op.condition_bits:  # conditional op
+                clbit_writeable[c] = max(stop_time, clbit_writeable[c])
 
         working_qubits = qubit_time_available.keys()
         circuit_duration = max(qubit_time_available[q] for q in working_qubits)
