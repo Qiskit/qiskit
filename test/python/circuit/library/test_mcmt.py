6c28966ac8416a5b5385092bdb599def0c98cef1
Jake Lishman <jake.lishman@ibm.com>
2021-10-21 14:53:50 +0100

    Fix MCMT typo in test

--- test/python/circuit/library/test_mcmt.py
+++ test/python/circuit/library/test_mcmt.py
@@ -29,26 +29,26 @@ class TestMCMT(QiskitTestCase):
     """Test the multi-controlled multi-target circuit."""
 
     @data(MCMT, MCMTVChain)
-    def test_mcmt_label(self, mtmc_class):
+    def test_mcmt_label(self, mcmt_class):
         """Test MCMT label remains functional but is deprecated."""
         custom_label = "abc"
         with self.subTest(msg="init with label and get"):
             with self.assertWarns(DeprecationWarning):
-                mcmt = mtmc_class(
+                mcmt = mcmt_class(
                     XGate(), num_ctrl_qubits=1, num_target_qubits=1, label=custom_label
                 )
             with self.assertWarns(DeprecationWarning):
                 self.assertEqual(mcmt.label, custom_label)
 
         with self.subTest(msg="label set and get"):
-            mcmt = mtmc_class(XGate(), num_ctrl_qubits=1, num_target_qubits=1)
+            mcmt = mcmt_class(XGate(), num_ctrl_qubits=1, num_target_qubits=1)
             with self.assertWarns(DeprecationWarning):
                 mcmt.label = custom_label
             with self.assertWarns(DeprecationWarning):
                 self.assertEqual(mcmt.label, custom_label)
 
         with self.subTest(msg="control gate label"):
-            mcmt = mtmc_class(XGate(), num_ctrl_qubits=1, num_target_qubits=1)
+            mcmt = mcmt_class(XGate(), num_ctrl_qubits=1, num_target_qubits=1)
             c_mcmt = mcmt.control()
             with self.assertWarns(DeprecationWarning):
                 c_mcmt = mcmt.control(label=custom_label)
