be6a1143733cf1dbf1220a10c090633803c417fa
Dariusz Lasecki <dal@zurich.ibm.com>
2021-10-26 11:33:08 +0200

    Added passing and correct VarQite unit tests.

--- test/python/algorithms/quantum_time_evolution/variational/test_var_qite.py
+++ test/python/algorithms/quantum_time_evolution/variational/test_var_qite.py
@@ -14,6 +14,7 @@ import unittest
 
 import numpy as np
 
+from qiskit import Aer
 from qiskit.algorithms.quantum_time_evolution.variational.principles.imaginary.implementations.imaginary_mc_lachlan_variational_principle import (
     ImaginaryMcLachlanVariationalPrinciple,
 )
@@ -28,9 +29,121 @@ from qiskit.opflow import (
 )
 from test.python.algorithms import QiskitAlgorithmsTestCase
 
+np.random.seed = 11
+
 
 class TestVarQite(QiskitAlgorithmsTestCase):
-    def test_run(self):
+    def test_run_d_1(self):
+        observable = SummedOp(
+            [
+                0.2252 * (I ^ I),
+                0.5716 * (Z ^ Z),
+                0.3435 * (I ^ Z),
+                -0.4347 * (Z ^ I),
+                0.091 * (Y ^ Y),
+                0.091 * (X ^ X),
+            ]
+        ).reduce()
+
+        d = 1
+        ansatz = EfficientSU2(observable.num_qubits, reps=d)
+
+        parameters = ansatz.ordered_parameters
+        init_param_values = np.zeros(len(ansatz.ordered_parameters))
+        for i in range(len(ansatz.ordered_parameters)):
+            init_param_values[i] = np.pi / 2
+        init_param_values[0] = 1
+        var_principle = ImaginaryMcLachlanVariationalPrinciple()
+
+        param_dict = dict(zip(parameters, init_param_values))
+
+        reg = None
+        backend = Aer.get_backend("statevector_simulator")
+
+        var_qite = VarQite(
+            var_principle, regularization=reg, backend=backend, error_based_ode=False
+        )
+        time = 1
+
+        evolution_result = var_qite.evolve(
+            observable,
+            time,
+            ansatz,  # ansatz is a state in this case
+            hamiltonian_value_dict=param_dict,
+        )
+
+        # values from the prototype
+        thetas_expected = [
+            0.905901128153194,
+            2.00336012271211,
+            2.69398302961789,
+            2.74463415808318,
+            2.33136908953802,
+            1.7469421463254,
+            2.10770301585949,
+            1.92775891861825,
+        ]
+
+        parameter_values = evolution_result.data[0][0].params
+        for i, parameter_value in enumerate(parameter_values):
+            np.testing.assert_almost_equal(float(parameter_value), thetas_expected[i], decimal=3)
+
+    def test_run_d_1_t_7(self):
+        observable = SummedOp(
+            [
+                0.2252 * (I ^ I),
+                0.5716 * (Z ^ Z),
+                0.3435 * (I ^ Z),
+                -0.4347 * (Z ^ I),
+                0.091 * (Y ^ Y),
+                0.091 * (X ^ X),
+            ]
+        ).reduce()
+
+        d = 1
+        ansatz = EfficientSU2(observable.num_qubits, reps=d)
+
+        parameters = ansatz.ordered_parameters
+        init_param_values = np.zeros(len(ansatz.ordered_parameters))
+        for i in range(len(ansatz.ordered_parameters)):
+            init_param_values[i] = np.pi / 2
+        init_param_values[0] = 1
+        var_principle = ImaginaryMcLachlanVariationalPrinciple()
+
+        param_dict = dict(zip(parameters, init_param_values))
+
+        reg = None
+        backend = Aer.get_backend("statevector_simulator")
+
+        var_qite = VarQite(
+            var_principle, regularization=reg, backend=backend, error_based_ode=False
+        )
+        time = 7
+
+        evolution_result = var_qite.evolve(
+            observable,
+            time,
+            ansatz,  # ansatz is a state in this case
+            hamiltonian_value_dict=param_dict,
+        )
+
+        # values from the prototype
+        thetas_expected = [
+            0.828917365718767,
+            1.88481074798033,
+            3.14111335991238,
+            3.14125849601269,
+            2.33768562678401,
+            1.78670990729437,
+            2.04214275514208,
+            2.04009918594422,
+        ]
+
+        parameter_values = evolution_result.data[0][0].params
+        for i, parameter_value in enumerate(parameter_values):
+            np.testing.assert_almost_equal(float(parameter_value), thetas_expected[i], decimal=2)
+
+    def test_run_d_2(self):
         observable = SummedOp(
             [
                 0.2252 * (I ^ I),
@@ -54,56 +167,40 @@ class TestVarQite(QiskitAlgorithmsTestCase):
 
         param_dict = dict(zip(parameters, init_param_values))
 
-        reg = "ridge"
+        reg = None
+        backend = Aer.get_backend("statevector_simulator")
 
-        var_qite = VarQite(var_principle, regularization=reg, backend=None, error_based_ode=False)
+        var_qite = VarQite(
+            var_principle, regularization=reg, backend=backend, error_based_ode=False
+        )
         time = 1
 
         evolution_result = var_qite.evolve(
             observable,
             time,
-            # TODO design api assumes it is provided here, currently calculated within VarQte
             ansatz,  # ansatz is a state in this case
             hamiltonian_value_dict=param_dict,
         )
 
-        print(evolution_result)
+        # values from the prototype
+        thetas_expected = [
+            1.29495364023786,
+            1.08970061333559,
+            0.667488228710748,
+            0.500122687902944,
+            1.4377736672043,
+            1.22881086103085,
+            0.729773048146251,
+            1.01698854755226,
+            0.050807780587492,
+            0.294828474947149,
+            0.839305697704923,
+            0.663689581255428,
+        ]
 
-    # def test_compare(self):
-    #     observable = SummedOp([(Z ^ X), 3.0 * (Y ^ Y), (Z ^ X), (I ^ Z), (Z ^ I)]).reduce()
-    #
-    #     d = 1
-    #     ansatz = EfficientSU2(observable.num_qubits, reps=d)
-    #
-    #     parameters = ansatz.ordered_parameters
-    #     init_param_values = np.zeros(len(ansatz.ordered_parameters))
-    #     for i in range(ansatz.num_qubits):
-    #         init_param_values[-(ansatz.num_qubits + i + 1)] = np.pi / 2
-    #
-    #     param_dict = dict(zip(parameters, init_param_values))
-    #
-    #     var_principle = ImaginaryMcLachlanVariationalPrinciple(
-    #         grad_method="lin_comb", qfi_method="lin_comb_full"
-    #     )
-    #
-    #     reg = None
-    #
-    #     var_qite = VarQite(
-    #         var_principle,
-    #         regularization=reg,
-    #         backend=Aer.get_backend("statevector_simulator"),
-    #         error_based_ode=False,
-    #     )
-    #     time = 1
-    #
-    #     evolution_result = var_qite.evolve(
-    #         observable,
-    #         time,
-    #         initial_state=ansatz,  # ansatz is a state in this case
-    #         hamiltonian_value_dict=param_dict,
-    #     )
-    #
-    #     print(evolution_result)
+        parameter_values = evolution_result.data[0][0].params
+        for i, parameter_value in enumerate(parameter_values):
+            np.testing.assert_almost_equal(float(parameter_value), thetas_expected[i], decimal=4)
 
 
 if __name__ == "__main__":
