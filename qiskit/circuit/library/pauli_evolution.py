184fe199c6bc08455b8481d90e6faef2c73a4504
Julien Gacon <gaconju@gmail.com>
2021-10-30 00:55:48 +0200

    PauliEvolution gate (#6975)
    * first draft of evolution gates
    
    * fix format
    
    * use PauliEvoGate for evolutions
    
    - add synthesis classes to imports
    - implement lie-trotter w/o opflow
    - PauliEvo shows as "exp(it <term>)"
    
    * black
    
    * fix tests
    
    * implement suzuki trotter
    
    * move Pauli evo logic here
    
    * black
    
    * fix single pauli evolutions
    
    and use HamiltonianGate instead of direct matrix evo
    
    * handle ops that are just the identity
    
    * fix tests
    
    * add more tests
    
    * let Opflow use evolution gate
    
    * Apply suggestions from code review
    
    Co-authored-by: Steve Wood <40241007+woodsp-ibm@users.noreply.github.com>
    
    * fix tests
    
    * fix QPY test
    
    * use 2q rotations where possible
    
    * move evo synth to new directory
    
    * move pauli evolution to synthesis, add docs
    
    * cosmetic updates
    
    - name of evo gate
    - make single pauli evos not wrapped
    
    * black
    
    * make qpy work on content serialization
    
    .. but it doens't yet recreate an EvolutionGate instance
    
    * make LieTrotter the default
    
    * lint and docs
    
    * docs!
    
    * rename to PauliEvolutionGate
    
    * sphinx?
    
    * add reno
    
    * Update qiskit/circuit/synthesis/evolution/suzuki_trotter.py
    
    Co-authored-by: Ali Javadi-Abhari <ajavadia@users.noreply.github.com>
    
    * don't capitalize drawing of PauliEvolutionGate
    
    * apply review comments
    
    - synthesize(self)
    - don't show `t` in evo-gate draw
    - delete pauli_evolution.py
    
    * fix typo in release notes, add ref, move files to different folders
    
    * use SparsePauliOp in releasenotes
    
    * remove circular import situation in n_local
    
    * fix release notes
    
    * docs again
    
    Co-authored-by: Steve Wood <40241007+woodsp-ibm@users.noreply.github.com>
    Co-authored-by: Ali Javadi-Abhari <ajavadia@users.noreply.github.com>
    Co-authored-by: Ali Javadi <ajavadia@princeton.edu>

---
+++ qiskit/circuit/library/pauli_evolution.py
@@ -0,0 +1,117 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""A gate to implement time-evolution of operators."""
+
+from typing import Union, Optional
+
+from qiskit.circuit.gate import Gate
+from qiskit.circuit.parameterexpression import ParameterExpression
+from qiskit.synthesis import EvolutionSynthesis, LieTrotter
+from qiskit.quantum_info import Pauli, SparsePauliOp
+
+
+class PauliEvolutionGate(Gate):
+    r"""Time-evolution of an operator consisting of Paulis.
+
+    For an operator :math:`H` consisting of Pauli terms and (real) evolution time :math:`t`
+    this gate implements
+
+    .. math::
+
+        U(t) = e^{-itH}.
+
+    This gate serves as a high-level definition of the evolution and can be synthesized into
+    a circuit using different algorithms.
+
+    The evolution gates are related to the Pauli rotation gates by a factor of 2. For example
+    the time evolution of the Pauli :math:`X` operator is connected to the Pauli :math:`X` rotation
+    :math:`R_X` by
+
+    .. math::
+
+        U(t) = e^{-itX} = R_X(2t).
+
+    **References:**
+
+    [1] G. Li et al. Paulihedral: A Generalized Block-Wise Compiler Optimization
+    Framework For Quantum Simulation Kernels (2021).
+    [`arXiv:2109.03371 <https://arxiv.org/abs/2109.03371>`_]
+    """
+
+    def __init__(
+        self,
+        operator,
+        time: Union[float, ParameterExpression] = 1.0,
+        label: Optional[str] = None,
+        synthesis: Optional[EvolutionSynthesis] = None,
+    ) -> None:
+        """
+        Args:
+            operator (Pauli | PauliOp | SparsePauliOp | PauliSumOp | list):
+                The operator to evolve. Can also be provided as list of non-commuting
+                operators where the elements are sums of commuting operators.
+                For example: ``[XY + YX, ZZ + ZI + IZ, YY]``.
+            time: The evolution time.
+            label: A label for the gate to display in visualizations.
+            synthesis: A synthesis strategy. If None, the default synthesis is the Lie-Trotter
+                product formula with a single repetition.
+        """
+        if isinstance(operator, list):
+            operator = [_to_sparse_pauli_op(op) for op in operator]
+            name = f"exp(-i {[' + '.join(op.paulis.to_labels()) for op in operator]})"
+        else:
+            operator = _to_sparse_pauli_op(operator)
+            name = f"exp(-i {' + '.join(operator.paulis.to_labels())})"
+
+        if synthesis is None:
+            synthesis = LieTrotter()
+
+        num_qubits = operator[0].num_qubits if isinstance(operator, list) else operator.num_qubits
+        super().__init__(name=name, num_qubits=num_qubits, params=[time], label=label)
+
+        self.time = time
+        self.operator = operator
+        self.synthesis = synthesis
+
+    def _define(self):
+        """Unroll, where the default synthesis is matrix based."""
+        self.definition = self.synthesis.synthesize(self)
+
+    def inverse(self) -> "PauliEvolutionGate":
+        return PauliEvolutionGate(operator=self.operator, time=-self.time, synthesis=self.synthesis)
+
+
+def _to_sparse_pauli_op(operator):
+    """Cast the operator to a SparsePauliOp.
+
+    For Opflow objects, return a global coefficient that must be multiplied to the evolution time.
+    Since this coefficient might contain unbound parameters it cannot be absorbed into the
+    coefficients of the SparsePauliOp.
+    """
+    # pylint: disable=cyclic-import
+    from qiskit.opflow import PauliSumOp, PauliOp
+
+    if isinstance(operator, PauliSumOp):
+        sparse_pauli = operator.primitive
+        sparse_pauli._coeffs *= operator.coeff
+        return sparse_pauli
+    if isinstance(operator, PauliOp):
+        sparse_pauli = SparsePauliOp(operator.primitive)
+        sparse_pauli._coeffs *= operator.coeff
+        return sparse_pauli
+    if isinstance(operator, Pauli):
+        return SparsePauliOp(operator)
+    if isinstance(operator, SparsePauliOp):
+        return operator
+
+    raise ValueError(f"Unsupported operator type for evolution: {type(operator)}.")
