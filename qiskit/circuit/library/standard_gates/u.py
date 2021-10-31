22a44adc51246b4fa8bcff2b3c3dc6ce7025449f
sg495 <sg495@users.noreply.github.com>
2021-10-31 01:20:51 +0100

    Update u.py
    Fixes #7197

--- qiskit/circuit/library/standard_gates/u.py
+++ qiskit/circuit/library/standard_gates/u.py
@@ -26,7 +26,13 @@ class UGate(Gate):
 
     .. math::
         U(\theta, \phi, \lambda) =
-            RZ(\phi - \pi/2) RX(\pi/2) RZ(\pi - \theta) RX(\pi/2) RZ(\lambda - \pi/2)
+            RZ(\phi) RX(\pi/2) RZ(\pi - \theta) RX(\pi/2) RZ(\lambda - \pi)
+
+    Equivalent simplified form:
+
+    .. math::
+        U(\theta, \phi, \lambda) =
+            RZ(\phi + \pi/2) RZ(\theta) RZ(\lambda - \pi/2)
 
     **Circuit symbol:**
 
