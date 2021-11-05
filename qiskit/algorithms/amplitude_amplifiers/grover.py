b3afa1927b1316675200ea041869fc5fc4489338
Jake Lishman <jake.lishman@ibm.com>
2021-11-05 12:00:10 +0000

    Remove excess whitespace

--- qiskit/algorithms/amplitude_amplifiers/grover.py
+++ qiskit/algorithms/amplitude_amplifiers/grover.py
@@ -60,7 +60,7 @@ class Grover(AmplitudeAmplifier):
 
     This class supports oracles in form of :class:`~qiskit.QuantumCircuit`.
 
-    With the oracle at hand, Grover's Search constructs the Grover operator to amplify the 
+    With the oracle at hand, Grover's Search constructs the Grover operator to amplify the
     amplitudes of the good states:
 
     .. math::
