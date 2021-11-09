3bc293349c05dbf267a9df4ad6e0ac632ccd9907
Jake Lishman <jake.lishman@ibm.com>
2021-11-09 14:28:37 +0000

    Fix decomposition of C3SXGate (#7230)
    * Fix decomposition of C3SXGate
    
    The phases used in the decomposition were inverted, so that the gate was
    actually equivalent to `SdgXGate().control(3)` rather than the intended
    `SXGate().control(3)`.  Note that both gates are valid square roots of
    `C3XGate()`, which is likely how this went undetected, but the names and
    value of `ControlledGate.base_gate` did not match.
    
    The decomposition of `C4XGate` had a similar negative-phase error, but
    the prelude cancelled with the use of the inverse `C3SXGate`, resulting
    in a correct gate in the end.  This commit updates `C4XGate` to work
    with the new `C3SXGate`.
    
    * Add tests of controlled-gate equivalences
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- qiskit/circuit/library/standard_gates/x.py
+++ qiskit/circuit/library/standard_gates/x.py
@@ -511,19 +511,19 @@ class C3SXGate(ControlledGate):
         """
         gate c3sqrtx a,b,c,d
         {
-            h d; cu1(-pi/8) a,d; h d;
-            cx a,b;
-            h d; cu1(pi/8) b,d; h d;
+            h d; cu1(pi/8) a,d; h d;
             cx a,b;
             h d; cu1(-pi/8) b,d; h d;
+            cx a,b;
+            h d; cu1(pi/8) b,d; h d;
             cx b,c;
-            h d; cu1(pi/8) c,d; h d;
-            cx a,c;
             h d; cu1(-pi/8) c,d; h d;
+            cx a,c;
+            h d; cu1(pi/8) c,d; h d;
             cx b,c;
-            h d; cu1(pi/8) c,d; h d;
-            cx a,c;
             h d; cu1(-pi/8) c,d; h d;
+            cx a,c;
+            h d; cu1(pi/8) c,d; h d;
         }
         """
         # pylint: disable=cyclic-import
@@ -534,31 +534,31 @@ class C3SXGate(ControlledGate):
         # pylint: disable=invalid-unary-operand-type
         rules = [
             (HGate(), [q[3]], []),
-            (CU1Gate(-self._angle), [q[0], q[3]], []),
-            (HGate(), [q[3]], []),
-            (CXGate(), [q[0], q[1]], []),
-            (HGate(), [q[3]], []),
-            (CU1Gate(self._angle), [q[1], q[3]], []),
+            (CU1Gate(self._angle), [q[0], q[3]], []),
             (HGate(), [q[3]], []),
             (CXGate(), [q[0], q[1]], []),
             (HGate(), [q[3]], []),
             (CU1Gate(-self._angle), [q[1], q[3]], []),
             (HGate(), [q[3]], []),
-            (CXGate(), [q[1], q[2]], []),
+            (CXGate(), [q[0], q[1]], []),
             (HGate(), [q[3]], []),
-            (CU1Gate(self._angle), [q[2], q[3]], []),
-            (HGate(), [q[3]], []),
-            (CXGate(), [q[0], q[2]], []),
-            (HGate(), [q[3]], []),
-            (CU1Gate(-self._angle), [q[2], q[3]], []),
+            (CU1Gate(self._angle), [q[1], q[3]], []),
             (HGate(), [q[3]], []),
             (CXGate(), [q[1], q[2]], []),
             (HGate(), [q[3]], []),
-            (CU1Gate(self._angle), [q[2], q[3]], []),
+            (CU1Gate(-self._angle), [q[2], q[3]], []),
             (HGate(), [q[3]], []),
             (CXGate(), [q[0], q[2]], []),
             (HGate(), [q[3]], []),
+            (CU1Gate(self._angle), [q[2], q[3]], []),
+            (HGate(), [q[3]], []),
+            (CXGate(), [q[1], q[2]], []),
+            (HGate(), [q[3]], []),
             (CU1Gate(-self._angle), [q[2], q[3]], []),
+            (HGate(), [q[3]], []),
+            (CXGate(), [q[0], q[2]], []),
+            (HGate(), [q[3]], []),
+            (CU1Gate(self._angle), [q[2], q[3]], []),
             (HGate(), [q[3]], []),
         ]
         qc = QuantumCircuit(q)
@@ -834,25 +834,25 @@ class C4XGate(ControlledGate):
         """
         gate c3sqrtx a,b,c,d
         {
-            h d; cu1(-pi/8) a,d; h d;
-            cx a,b;
-            h d; cu1(pi/8) b,d; h d;
+            h d; cu1(pi/8) a,d; h d;
             cx a,b;
             h d; cu1(-pi/8) b,d; h d;
+            cx a,b;
+            h d; cu1(pi/8) b,d; h d;
             cx b,c;
-            h d; cu1(pi/8) c,d; h d;
-            cx a,c;
             h d; cu1(-pi/8) c,d; h d;
+            cx a,c;
+            h d; cu1(pi/8) c,d; h d;
             cx b,c;
-            h d; cu1(pi/8) c,d; h d;
-            cx a,c;
             h d; cu1(-pi/8) c,d; h d;
+            cx a,c;
+            h d; cu1(pi/8) c,d; h d;
         }
         gate c4x a,b,c,d,e
         {
-            h e; cu1(-pi/2) d,e; h e;
+            h e; cu1(pi/2) d,e; h e;
             rc3x a,b,c,d;
-            h e; cu1(pi/4) d,e; h e;
+            h e; cu1(-pi/2) d,e; h e;
             rc3x a,b,c,d;
             c3sqrtx a,b,c,e;
         }
@@ -865,11 +865,11 @@ class C4XGate(ControlledGate):
         qc = QuantumCircuit(q, name=self.name)
         rules = [
             (HGate(), [q[4]], []),
-            (CU1Gate(-numpy.pi / 2), [q[3], q[4]], []),
+            (CU1Gate(numpy.pi / 2), [q[3], q[4]], []),
             (HGate(), [q[4]], []),
             (RC3XGate(), [q[0], q[1], q[2], q[3]], []),
             (HGate(), [q[4]], []),
-            (CU1Gate(numpy.pi / 2), [q[3], q[4]], []),
+            (CU1Gate(-numpy.pi / 2), [q[3], q[4]], []),
             (HGate(), [q[4]], []),
             (RC3XGate().inverse(), [q[0], q[1], q[2], q[3]], []),
             (C3SXGate(), [q[0], q[1], q[2], q[4]], []),
