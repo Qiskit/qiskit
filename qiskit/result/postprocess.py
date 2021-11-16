34d245d526826005295469217659a5dcb30c1eee
Christopher Wood <cjwood@us.ibm.com>
2021-11-16 16:24:18 -0500

    Handle non-qubit dimensions

--- qiskit/result/postprocess.py
+++ qiskit/result/postprocess.py
@@ -187,7 +187,7 @@ def format_statevector(vec, decimals=None):
 
     if isinstance(vec, Statevector):
         if decimals:
-            return Statevector(np.around(vec.data, decimals=decimals))
+            return Statevector(np.around(vec.data, decimals=decimals), dims=vec.dims())
         return vec
     if isinstance(vec, np.ndarray):
         if decimals:
@@ -222,7 +222,11 @@ def format_unitary(mat, decimals=None):
 
     if isinstance(mat, Operator):
         if decimals:
-            return Operator(np.around(mat.data, decimals=decimals))
+            return Operator(
+                np.around(mat.data, decimals=decimals),
+                input_dims=mat.input_dims(),
+                output_dims=mat.output_dims(),
+            )
         return mat
     if isinstance(mat, np.ndarray):
         if decimals:
