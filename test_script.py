91ad61019e2980e7c91c2066fc44a9922d5fb6e9
Elliot <elliot.coupe@maxkelsen.com>
2021-11-19 16:06:56 +1000

    Now constructs the required ansatz solution for each layer. Work still needs to be done on construct_adapt_ansatz loop to update variables (e.g. optimal hyperparams)

---
+++ test_script.py
@@ -0,0 +1,40 @@
+from functools import reduce
+from qiskit.opflow import I, X, Y, Z
+from qiskit.algorithms import QAOA
+from qiskit import Aer
+from qiskit.utils import QuantumInstance
+from qiskit.algorithms import AdaptQAOA
+
+
+QISKIT_DICT = {"I": I, "X": X, "Y": Y, "Z": Z}
+
+def string_to_qiskit(qstring):
+    if is_all_same(qstring):
+        # case where its all X's or Y's
+        gate = qstring[0]
+        list_string = [
+            i * "I" + gate + (len(qstring) - i - 1) * "I" for i in range(len(qstring))]
+        return sum([reduce(lambda a, b: a ^ b, [QISKIT_DICT[char.upper()] for char in x]) for x in list_string])
+
+    return reduce(lambda a, b: a ^ b, [QISKIT_DICT[char.upper()] for char in qstring])
+def is_all_same(items):
+    return all(x == items[0] for x in items)
+
+
+mixer_list = ["XXIII","XIIX","IXXII"]
+cost_op = string_to_qiskit("ZZZZZ")
+mixer_list = [string_to_qiskit(x) for x in mixer_list]
+
+quantum_instance = QuantumInstance(Aer.get_backend('qasm_simulator'), shots=1024)
+
+'''
+adapt = AdaptQAOA(mixer_pool_type='singular', max_reps=5, quantum_instance=quantum_instance)
+#adapt.optimal_mixer_list = mixer_list
+cme = adapt.compute_minimum_eigenvalue(cost_op)
+print(cme)
+'''
+
+
+qaoa = QAOA(reps=5, quantum_instance=quantum_instance)
+out = qaoa.compute_minimum_eigenvalue(cost_op)
+print(out)
