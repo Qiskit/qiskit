f1dc8ec49a4c10db476155dd7a550a3b658995d0
Julien Gacon <gaconju@gmail.com>
2021-11-23 18:45:02 +0100

    update as theta + difference

--- qiskit/algorithms/pvqd.py
+++ qiskit/algorithms/pvqd.py
@@ -17,10 +17,12 @@ from typing import Optional, Union, List, Tuple, Callable
 import numpy as np
 
 from qiskit.algorithms.optimizers import Optimizer
-from qiskit.circuit import QuantumCircuit
+from qiskit.circuit import QuantumCircuit, ParameterVector
 from qiskit.circuit.library import PauliEvolutionGate
 from qiskit.providers import Backend
-from qiskit.opflow import OperatorBase, CircuitSampler, ExpectationBase, ListOp, StateFn
+from qiskit.opflow import (
+    OperatorBase, CircuitSampler, ExpectationBase, ListOp, StateFn, GradientBase
+)
 from qiskit.synthesis import EvolutionSynthesis, LieTrotter
 from qiskit.utils import QuantumInstance
 
@@ -47,6 +49,7 @@ class PVQD:
         optimizer: Optimizer,
         quantum_instance: Union[Backend, QuantumInstance],
         expectation: ExpectationBase,
+        gradient: Optional[GradientBase] = None,
         evolution: Optional[EvolutionSynthesis] = None,
     ) -> None:
         """
@@ -67,13 +70,14 @@ class PVQD:
         self.ansatz = ansatz
         self.initial_parameters = initial_parameters
         self.optimizer = optimizer
+        self.gradient = gradient
         self.expectation = expectation
         self.evolution = evolution
 
         self._sampler = CircuitSampler(quantum_instance)
 
     def step(
-        self, hamiltonian: OperatorBase, theta: np.ndarray, dt: float
+        self, hamiltonian: OperatorBase, theta: np.ndarray, dt: float, initial_guess: np.ndarray
     ) -> Tuple[np.ndarray, float]:
         """Perform a single time step.
 
@@ -86,12 +90,12 @@ class PVQD:
             A tuple consisting of the next parameters and the fidelity of the optimization.
         """
         # construct cost function
-        overlap = self.get_overlap(hamiltonian, dt, theta)
+        overlap, gradient = self.get_overlap(hamiltonian, dt, theta)
 
         # call optimizer
-        optimizer_result = self.optimizer.minimize(lambda x: -overlap(x), self.initial_parameters)
+        optimizer_result = self.optimizer.minimize(lambda x: -overlap(x), initial_guess, gradient)
 
-        return optimizer_result.x, -optimizer_result.fun
+        return theta + optimizer_result.x, -optimizer_result.fun
 
     def get_overlap(
         self, hamiltonian: OperatorBase, dt: float, current_parameters: np.ndarray
@@ -113,28 +117,42 @@ class PVQD:
         )
 
         # define the overlap of the Trotterized state and the ansatz
-        overlap = StateFn(self.ansatz).adjoint() @ StateFn(trotterized)
+        x = ParameterVector("w", self.ansatz.num_parameters)
+        shifted = self.ansatz.assign_parameters(current_parameters + x)
+        overlap = StateFn(trotterized).adjoint() @ StateFn(shifted)
 
         # apply the expectation converter
         converted = self.expectation.convert(overlap)
 
         ansatz_parameters = self.ansatz.parameters
 
-        def evaluate_overlap(theta: np.ndarray) -> float:
+        def evaluate_overlap(displacement: np.ndarray) -> float:
             """Evaluate the overlap of the ansatz with the Trotterized evolution.
 
             Args:
-                theta: The parameters for the ansatz.
+                displacement: The parameters for the ansatz.
 
             Returns:
                 The fidelity of the ansatz with parameters ``theta`` and the Trotterized evolution.
             """
             # evaluate with the circuit sampler
-            value_dict = dict(zip(ansatz_parameters, theta))
+            value_dict = dict(zip(x, displacement))
             sampled = self._sampler.convert(converted, params=value_dict)
             return np.abs(sampled.eval()) ** 2
 
-        return evaluate_overlap
+        if self.gradient is not None:
+            gradient = self.gradient.convert(overlap)
+
+            def evaluate_gradient(displacement: np.ndarray) -> np.ndarray:
+                """Evaluate the gradient."""
+                # evaluate with the circuit sampler
+                value_dict = dict(zip(ansatz_parameters, current_parameters + displacement))
+                sampled = self._sampler.convert(gradient, params=value_dict)
+                return 2 * sampled.eval()
+
+            return evaluate_overlap, evaluate_gradient
+
+        return evaluate_overlap, None
 
     def _get_observable_evaluator(self, observables):
         if isinstance(observables, list):
@@ -197,9 +215,14 @@ class PVQD:
         parameters = [self.initial_parameters]
 
         current_time = 0
+        initial_guess = np.random.random(self.initial_parameters.size) * 0.01
+        # initial_guess = np.zeros(self.initial_parameters.size)
         while current_time < time:
             # perform VQE to find the next parameters
-            next_parameters, fidelity = self.step(hamiltonian, parameters[-1], dt)
+            next_parameters, fidelity = self.step(hamiltonian, parameters[-1], dt, initial_guess)
+
+            # set initial guess to last parameter update
+            initial_guess = next_parameters - parameters[-1]
 
             # store parameters
             parameters.append(next_parameters)
--- test/python/algorithms/test_pvqd.py
+++ test/python/algorithms/test_pvqd.py
@@ -13,12 +13,14 @@
 from qiskit.test import QiskitTestCase
 
 import numpy as np
+import scipy as sc
 
 from qiskit import Aer
 from qiskit.algorithms import PVQD
-from qiskit.algorithms.optimizers import SPSA
+from qiskit.algorithms.optimizers import SPSA, L_BFGS_B, GradientDescent, COBYLA
 from qiskit.circuit.library import EfficientSU2
-from qiskit.opflow import X, Z, MatrixExpectation
+from qiskit.opflow import X, Z, I, MatrixExpectation, Gradient
+from qiskit.quantum_info import Statevector
 
 import matplotlib.pyplot as plt
 
@@ -26,28 +28,64 @@ import matplotlib.pyplot as plt
 class TestPVQD(QiskitTestCase):
     """Tests for the pVQD algorithm."""
 
+    def exact(self, final_time, dt, hamiltonian, observable, initial_state):
+        """Get the exact values for energy and the observable."""
+        energies = []  # list of energies evaluated at timesteps dt
+        obs = []  # list of observables
+        ts = []  # list of timepoints at which energy/obs are evaluated
+        t = 0
+        while t <= final_time:
+            # get exact state at time t
+            exact_state = initial_state.evolve(sc.linalg.expm(-1j * t * hamiltonian.to_matrix()))
+
+            # store observables and time
+            ts.append(t)
+            energies.append(exact_state.expectation_value(hamiltonian.to_matrix()))
+            obs.append(exact_state.expectation_value(observable.to_matrix()))
+
+            # next timestep
+            t += dt
+
+        return ts, energies, obs
+
     def test_pvqd(self):
         """Test a simple evolution."""
 
         backend = Aer.get_backend("statevector_simulator")
         expectation = MatrixExpectation()
-        hamiltonian = X ^ X
+        hamiltonian = 0.1 * (Z ^ Z) + (I ^ X) + (X ^ I)
         observable = Z ^ Z
 
-        time = 0.1
+        time = 1
         dt = 0.01
 
-        ansatz = EfficientSU2(2, reps=3)
-        optimizer = SPSA(maxiter=100, learning_rate=0.01, perturbation=0.01)
+        ansatz = EfficientSU2(2, reps=2)
+        optimizer = SPSA(maxiter=300, learning_rate=0.1, perturbation=0.01)
+        # optimizer = SPSA()
+        # optimizer = COBYLA()
+        # optimizer = GradientDescent(learning_rate=0.01)
+        # optimizer = L_BFGS_B()
         initial_parameters = np.zeros(ansatz.num_parameters)
         initial_parameters[-2] = np.pi / 2
         initial_parameters[-4] = np.pi / 2
 
-        pvqd = PVQD(ansatz, initial_parameters, optimizer, backend, expectation)
+        # run pVQD keeping track of the energy and the magnetization
+        pvqd = PVQD(ansatz, initial_parameters, optimizer,
+                    quantum_instance=backend, expectation=expectation)
         result = pvqd.evolve(hamiltonian, time, dt, observables=[hamiltonian, observable])
 
-        fig, (ax1, ax2) = plt.subplots(2, 1)
-        ax1.plot(result.times, result.observables[:, 0])
-        ax2.plot(result.times, result.observables[:, 1])
+        # get reference results
+        initial_state = Statevector(ansatz.bind_parameters(initial_parameters))
+        ref_t, ref_energy, ref_magn = self.exact(time, dt, hamiltonian, observable, initial_state)
+
+        _, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
+        ax1.set_title("Energy")
+        ax1.plot(result.times, result.observables[:, 0], label="pVQD")
+        ax1.plot(ref_t, ref_energy, label="exact")
+        ax2.set_title("Magnetization")
+        ax2.plot(result.times, result.observables[:, 1], label="pVQD")
+        ax2.plot(ref_t, ref_magn, label="exact")
+        ax2.set_xlabel(r"time $t$")
+        plt.tight_layout()
         plt.show()
         print(result)
