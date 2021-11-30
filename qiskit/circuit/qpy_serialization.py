20fd39339d199ebbf36fe141e79da8128645acd3
knzwnao <nkanazawa1989@gmail.com>
2021-11-30 21:04:03 +0900

    keep import path

---
+++ qiskit/circuit/qpy_serialization.py
@@ -0,0 +1,24 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+# pylint: disable=unused-import
+
+"""Old import path for the QPY serialization module."""
+
+import warnings
+from qiskit.qpy import dump, load
+
+warnings.warn(
+    "Importing from qiskit.circuits.qpy_serialization has been deprecated and this will be removed soon."
+    "Use qiskit.qpy instead.",
+    DeprecationWarning,
+)
