4bb889f95be2c7e1b6f0ca0c983ed0c2c6f57971
Matthew Treinish <mtreinish@kortar.org>
2021-12-01 08:27:23 -0500

    Fix arguments for RZXCalibrationBuilder and EchoRZXWeylDecomposition
    The RZXCalibrationBuilder and EchoRZXWeylDecomposition transpiler passes
    were previously taking BaseBackend instances as arguments. Besides that
    being a deprecated class which will be removed soon, it also is
    incorrect because backend objects are not guaranteed to be pickleable so
    when a pass manager runs in parallel processes this will cause an error.
    This was never caught because these passes aren't part of any default
    pass managers and their tests don't include running them as part of
    transpile().
    
    To fix this passes typically take the properties of a backend they
    require. This is being reworked for BackendV2 in #5885 so a target
    object can be used to encapsulate the model of a backend so we have a
    single data structure to pass around, but until that is the minimum
    backend version we need to also support taking the individual components
    for BackendV1 and BaseBackend.
    
    This commit changes the pass constructors to take only use the parameters
    from the backend object used in the pass internals and stop requiring a
    backend object be used to construct an instance of either pass.

--- qiskit/transpiler/passes/calibration/builders.py
+++ qiskit/transpiler/passes/calibration/builders.py
@@ -22,7 +22,6 @@ from qiskit.circuit import Instruction as CircuitInst
 from qiskit.circuit.library.standard_gates import RZXGate
 from qiskit.dagcircuit import DAGCircuit
 from qiskit.exceptions import QiskitError
-from qiskit.providers import basebackend
 from qiskit.pulse import (
     Play,
     Delay,
@@ -103,26 +102,29 @@ class RZXCalibrationBuilder(CalibrationBuilder):
     angle. Additional details can be found in https://arxiv.org/abs/2012.11660.
     """
 
-    def __init__(self, backend: basebackend):
+    def __init__(
+        self,
+        instruction_schedule_map: InstructionScheduleMap,
+        qubit_channel_mapping: List[List[str]],
+    ):
         """
         Initializes a RZXGate calibration builder.
 
         Args:
-            backend: Backend for which to construct the gates.
+            instruction_schedule_map: The :obj:`InstructionScheduleMap` object representing the
+                default pulse calibrations for the target backend
+            qubit_channel_mapping: The list mapping qubit indices to the list of
+                channel names that apply on that qubit.
 
         Raises:
             QiskitError: if open pulse is not supported by the backend.
         """
         super().__init__()
-        if not backend.configuration().open_pulse:
-            raise QiskitError(
-                "Calibrations can only be added to Pulse-enabled backends, "
-                "but {} is not enabled with Pulse.".format(backend.name())
-            )
+        if instruction_schedule_map is None or qubit_channel_mapping is None:
+            raise QiskitError("Calibrations can only be added to Pulse-enabled backends")
 
-        self._inst_map = backend.defaults().instruction_schedule_map
-        self._config = backend.configuration()
-        self._channel_map = backend.configuration().qubit_channel_mapping
+        self._inst_map = instruction_schedule_map
+        self._channel_map = qubit_channel_mapping
 
     def supported(self, node_op: CircuitInst, qubits: List) -> bool:
         """Determine if a given node supports the calibration.
