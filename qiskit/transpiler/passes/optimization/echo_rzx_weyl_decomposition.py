e55d0f85c34751694bb56b4da499bbc994199e43
Jintaoyu <yjt98765@gmail.com>
2021-12-01 11:56:51 +0800

    Add circuit diagram to merge_adjacent_barriers.py

--- qiskit/transpiler/passes/optimization/echo_rzx_weyl_decomposition.py
+++ qiskit/transpiler/passes/optimization/echo_rzx_weyl_decomposition.py
@@ -51,7 +51,7 @@ class EchoRZXWeylDecomposition(TransformationPass):
 
     @staticmethod
     def _echo_rzx_dag(theta):
-        """Return the following circuit:
+        """Return the following circuit::
 
              ┌───────────────┐┌───┐┌────────────────┐┌───┐
         q_0: ┤0              ├┤ X ├┤0               ├┤ X ├
@@ -70,7 +70,7 @@ class EchoRZXWeylDecomposition(TransformationPass):
 
     @staticmethod
     def _reverse_echo_rzx_dag(theta):
-        """Return the following circuit:
+        """Return the following circuit::
         
              ┌───┐┌───────────────┐     ┌────────────────┐┌───┐
         q_0: ┤ H ├┤1              ├─────┤1               ├┤ H ├─────
