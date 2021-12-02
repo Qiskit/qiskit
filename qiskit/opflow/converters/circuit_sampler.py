Qiskit/qiskit

commit 019675463592b498d3cd6e8a84f7ae35b7e3f1b5

Two-step transpilation for variational algorithms (#7217)
* first working version

* use additional kwarg, not dict

* add test

* add reno

* black & type hint in tests

* add more docs

* log based testing

* avoid enum and pass pass managers in transpile

* docs

* Apply suggestions from code review

Co-authored-by: Matthew Treinish <mtreinish@kortar.org>

* update docs

Co-authored-by: Luciano Bello <bel@zurich.ibm.com>
Co-authored-by: Matthew Treinish <mtreinish@kortar.org>
Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

qiskit/opflow/converters/circuit_sampler.py
@@ -296,7 +296,9 @@ class CircuitSampler(ConverterBase):
                 circuits = [op_c.to_circuit(meas=True) for op_c in circuit_sfns]
 
             try:
-                self._transpiled_circ_cache = self.quantum_instance.transpile(circuits)
+                self._transpiled_circ_cache = self.quantum_instance.transpile(
+                    circuits, pass_manager=self.quantum_instance.unbound_pass_manager
+                )
             except QiskitError:
                 logger.debug(
                     r"CircuitSampler failed to transpile circuits with unbound "
@@ -325,6 +327,12 @@ class CircuitSampler(ConverterBase):
                 logger.debug("Parameter binding %.5f (ms)", (end_time - start_time) * 1000)
         else:
             ready_circs = self._transpiled_circ_cache
+
+        # run transpiler passes on bound circuits
+        if self._transpile_before_bind:
+            ready_circs = self.quantum_instance.transpile(
+                ready_circs, pass_manager=self.quantum_instance.bound_pass_manager
+            )
 
         results = self.quantum_instance.execute(
             ready_circs, had_transpiled=self._transpile_before_bind

qiskit/utils/quantum_instance.py
@@ -155,6 +155,7 @@ class QuantumInstance:
         # transpile
         initial_layout=None,
         pass_manager=None,
+        bound_pass_manager=None,
         seed_transpiler: Optional[int] = None,
         optimization_level: Optional[int] = None,
         # simulation
@@ -184,13 +185,21 @@ class QuantumInstance:
             seed_simulator: Random seed for simulators
             max_credits: Maximum credits to use
             basis_gates: List of basis gate names supported by the
-                                               target. Defaults to basis gates of the backend.
+                target. Defaults to basis gates of the backend.
             coupling_map (Optional[Union['CouplingMap', List[List]]]):
-                        Coupling map (perhaps custom) to target in mapping
+                Coupling map (perhaps custom) to target in mapping
             initial_layout (Optional[Union['Layout', Dict, List]]):
-                        Initial layout of qubits in mapping
-            pass_manager (Optional['PassManager']):
-                        Pass manager to handle how to compile the circuits
+                Initial layout of qubits in mapping
+            pass_manager (Optional['PassManager']): Pass manager to handle how to compile the circuits.
+                To run only this pass manager and not the ``bound_pass_manager``, call the
+                :meth:`~qiskit.utils.QuantumInstance.transpile` method with the argument
+                ``pass_manager=quantum_instance.unbound_pass_manager``.
+            bound_pass_manager (Optional['PassManager']): A second pass manager to apply on bound
+                circuits only, that is, circuits without any free parameters. To only run this pass
+                manager and not ``pass_manager`` call the
+                :meth:`~qiskit.utils.QuantumInstance.transpile` method with the argument
+                ``pass_manager=quantum_instance.bound_pass_manager``.
+                manager should also be run.
             seed_transpiler: The random seed for circuit mapper
             optimization_level: How much optimization to perform on the circuits.
                 Higher levels generate more optimized circuits, at the expense of longer
@@ -207,7 +216,6 @@ class QuantumInstance:
                 or :class:`~qiskit.utils.mitigation.TensoredMeasFitter` from the
                 :mod:`qiskit.utils.mitigation` module can be used here as exact values, not
                 instances. ``TensoredMeasFitter`` doesn't support the ``subset_fitter`` method.
-
             cals_matrix_refresh_period: How often to refresh the calibration
                 matrix in measurement mitigation. in minutes
             measurement_error_mitigation_shots: The number of shots number for
@@ -230,6 +238,7 @@ class QuantumInstance:
         """
         self._backend = backend
         self._pass_manager = pass_manager
+        self._bound_pass_manager = bound_pass_manager
 
         # if the shots are none, try to get them from the backend
         if shots is None:
@@ -367,24 +376,62 @@ class QuantumInstance:
 
         return info
 
-    def transpile(self, circuits):
+    @property
+    def unbound_pass_manager(self):
+        """Return the pass manager for designated for unbound circuits.
+
+        Returns:
+            Optional['PassManager']: The pass manager for unbound circuits, if it has been set.
         """
-        A wrapper to transpile circuits to allow algorithm access the transpiled circuits.
+        return self._pass_manager
+
+    @property
+    def bound_pass_manager(self):
+        """Return the pass manager for designated for bound circuits.
+
+        Returns:
+            Optional['PassManager']: The pass manager for bound circuits, if it has been set.
+        """
+        return self._bound_pass_manager
+
+    def transpile(self, circuits, pass_manager=None):
+        """A wrapper to transpile circuits to allow algorithm access the transpiled circuits.
+
         Args:
             circuits (Union['QuantumCircuit', List['QuantumCircuit']]): circuits to transpile
+            pass_manager (Optional['PassManager']): A pass manager to transpile the circuits. If
+                none is given, but either ``pass_manager`` or ``bound_pass_manager`` has been set
+                in the initializer, these are run. If none has been provided there either, the
+                backend and compile configs from the initializer are used.
+
         Returns:
             List['QuantumCircuit']: The transpiled circuits, it is always a list even though
-                                    the length is one.
+                the length is one.
         """
         # pylint: disable=cyclic-import
         from qiskit import compiler
+        from qiskit.transpiler import PassManager
 
-        if self._pass_manager is not None:
-            transpiled_circuits = self._pass_manager.run(circuits)
+        # if no pass manager here is given, check if they have been set in the init
+        if pass_manager is None:
+            # if they haven't been set in the init, use the transpile args from the init
+            if self._pass_manager is None and self._bound_pass_manager is None:
+                transpiled_circuits = compiler.transpile(
+                    circuits, self._backend, **self._backend_config, **self._compile_config
+                )
+            # it they have been set, run them
+            else:
+                pass_manager = PassManager()
+                if self._pass_manager is not None:
+                    pass_manager += self._pass_manager  # check if None
+                if self._bound_pass_manager is not None:
+                    pass_manager += self._bound_pass_manager
+
+                transpiled_circuits = pass_manager.run(circuits)
+        # custom pass manager set by user
         else:
-            transpiled_circuits = compiler.transpile(
-                circuits, self._backend, **self._backend_config, **self._compile_config
-            )
+            transpiled_circuits = pass_manager.run(circuits)
+
         if not isinstance(transpiled_circuits, list):
             transpiled_circuits = [transpiled_circuits]
 

releasenotes/notes/two-step-transpile-f20d709a7a0c42dd.yaml
@@ -1,0 +1,66 @@
+---
+features:
+  - |
+    Allow two transpiler stages in the :class:`~qiskit.utils.QuantumInstance`, one for
+    parameterized circuits and a second one for bound circuits (i.e. no free parameters) only.
+    If a quantum instance with passes for unbound and bound circuits is passed into a
+    :class:`~qiskit.opflow.CircuitSampler`, the sampler will attempt to apply the unbound pass
+    once on the parameterized circuit, cache it, and only apply the bound pass for all future
+    evaluations.
+
+    This enables variational algorithms like the :class:`~qiskit.algorithms.VQE` to run a
+    custom pass manager for parameterized circuits once and, additionally, another the transpiler
+    again with a different custom pass manager on the bound circuits in each iteration. Being able
+    to run different pass managers is important because not all passes support parameterized
+    circuits (for example :class:`~qiskit.transpiler.passes.Optimize1qGatesDecomposition` only
+    works with bound circuit parameters).
+
+    For example, this feature allows using the pulse-efficient CX decomposition in the VQE, as
+
+    .. code-block::python
+
+        from qiskit.algorithms import VQE
+        from qiskit.opflow import Z
+        from qiskit.circuit.library.standard_gates.equivalence_library import StandardEquivalenceLibrary as std_eqlib
+        from qiskit.transpiler import PassManager, PassManagerConfig, CouplingMap
+        from qiskit.transpiler.preset_passmanagers import level_1_pass_manager
+        from qiskit.transpiler.passes import (
+            Collect2qBlocks, ConsolidateBlocks, Optimize1qGatesDecomposition,
+            RZXCalibrationBuilderNoEcho, UnrollCustomDefinitions, BasisTranslator
+        )
+        from qiskit.transpiler.passes.optimization.echo_rzx_weyl_decomposition import EchoRZXWeylDecomposition
+        from qiskit.test.mock import FakeBelem
+        from qiskit.utils import QuantumInstance
+
+        # Replace by a real backend! If not ensure qiskit-aer is installed to simulate the backend
+        backend = FakeBelem()
+
+        # Build the pass manager for the parameterized circuit
+        rzx_basis = ['rzx', 'rz', 'x', 'sx']
+        coupling_map = CouplingMap(backend.configuration().coupling_map)
+        config = PassManagerConfig(basis_gates=rzx_basis, coupling_map=coupling_map)
+        pre = level_1_pass_manager(config)
+
+        # Build a pass manager for the CX decomposition (works only on bound circuits)
+        post = PassManager([
+            # Consolidate consecutive two-qubit operations.
+            Collect2qBlocks(),
+            ConsolidateBlocks(basis_gates=['rz', 'sx', 'x', 'rxx']),
+
+            # Rewrite circuit in terms of Weyl-decomposed echoed RZX gates.
+            EchoRZXWeylDecomposition(backend),
+
+            # Attach scaled CR pulse schedules to the RZX gates.
+            RZXCalibrationBuilderNoEcho(backend),
+
+            # Simplify single-qubit gates.
+            UnrollCustomDefinitions(std_eqlib, rzx_basis),
+            BasisTranslator(std_eqlib, rzx_basis),
+            Optimize1qGatesDecomposition(rzx_basis),
+        ])
+
+        quantum_instance = QuantumInstance(backend, pass_manager=pre, bound_pass_manager=post)
+
+        vqe = VQE(quantum_instance=quantum_instance)
+        result = vqe.compute_minimum_eigenvalue(Z ^ Z)
+

test/python/algorithms/test_vqe.py
@@ -12,6 +12,7 @@
 
 """ Test VQE """
 
+import logging
 import unittest
 from test.python.algorithms import QiskitAlgorithmsTestCase
 
@@ -45,10 +46,26 @@ from qiskit.opflow import (
     X,
     Z,
 )
+from qiskit.transpiler import PassManager, PassManagerConfig
+from qiskit.transpiler.preset_passmanagers import level_1_pass_manager
 from qiskit.utils import QuantumInstance, algorithm_globals, has_aer
+from ..transpiler._dummy_passes import DummyAP
 
 if has_aer():
     from qiskit import Aer
+
+logger = "LocalLogger"
+
+
+class LogPass(DummyAP):
+    """A dummy analysis pass that logs when executed"""
+
+    def __init__(self, message):
+        super().__init__()
+        self.message = message
+
+    def run(self, dag):
+        logging.getLogger(logger).info(self.message)
 
 
 @ddt
@@ -651,6 +668,51 @@ class TestVQE(QiskitAlgorithmsTestCase):
         self.assertAlmostEqual(result.aux_operator_eigenvalues[1][1], 0.0, places=6)
         self.assertAlmostEqual(result.aux_operator_eigenvalues[2][1], 0.0)
         self.assertAlmostEqual(result.aux_operator_eigenvalues[3][1], 0.0)
+
+    def test_2step_transpile(self):
+        """Test the two-step transpiler pass."""
+        # count how often the pass for parameterized circuits is called
+        pre_counter = LogPass("pre_passmanager")
+        pre_pass = PassManager(pre_counter)
+        config = PassManagerConfig(basis_gates=["u3", "cx"])
+        pre_pass += level_1_pass_manager(config)
+
+        # ... and the pass for bound circuits
+        bound_counter = LogPass("bound_pass_manager")
+        bound_pass = PassManager(bound_counter)
+
+        quantum_instance = QuantumInstance(
+            backend=BasicAer.get_backend("statevector_simulator"),
+            basis_gates=["u3", "cx"],
+            pass_manager=pre_pass,
+            bound_pass_manager=bound_pass,
+        )
+
+        optimizer = SPSA(maxiter=5, learning_rate=0.01, perturbation=0.01)
+
+        vqe = VQE(optimizer=optimizer, quantum_instance=quantum_instance)
+        _ = vqe.compute_minimum_eigenvalue(Z)
+
+        with self.assertLogs(logger, level="INFO") as cm:
+            _ = vqe.compute_minimum_eigenvalue(Z)
+
+        expected = [
+            "pre_passmanager",
+            "bound_pass_manager",
+            "bound_pass_manager",
+            "bound_pass_manager",
+            "bound_pass_manager",
+            "bound_pass_manager",
+            "bound_pass_manager",
+            "bound_pass_manager",
+            "bound_pass_manager",
+            "bound_pass_manager",
+            "bound_pass_manager",
+            "bound_pass_manager",
+            "pre_passmanager",
+            "bound_pass_manager",
+        ]
+        self.assertEqual([record.message for record in cm.records], expected)
 
 
 if __name__ == "__main__":

commit 1ef11877c97baa9cb06c51680d1e6ae5c58900bd

Fix performance regression of VQE by #7217 (#7338)
* Fix performance regression of VQE

* Update qiskit/opflow/converters/circuit_sampler.py

Co-authored-by: Julien Gacon <gaconju@gmail.com>

Co-authored-by: Julien Gacon <gaconju@gmail.com>

qiskit/opflow/converters/circuit_sampler.py
@@ -329,7 +329,7 @@ class CircuitSampler(ConverterBase):
             ready_circs = self._transpiled_circ_cache
 
         # run transpiler passes on bound circuits
-        if self._transpile_before_bind:
+        if self._transpile_before_bind and self.quantum_instance.bound_pass_manager is not None:
             ready_circs = self.quantum_instance.transpile(
                 ready_circs, pass_manager=self.quantum_instance.bound_pass_manager
             )