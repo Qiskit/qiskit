bb6f3a959b777ec9e39fd21bc0df5dc5f1d2f7cb
Jake Lishman <jake.lishman@ibm.com>
2021-12-06 17:14:12 +0000

    Revert default import of rzx_templates
    This was made in order to simplify some import paths, but as a side
    effect it caused ten `QuantumCircuit`s to be created on import of
    `qiskit`, which spoiled the QPY backwards compatibility tests and likely
    had import performance implications.
    
    The import of this likely should not need to create circuits, but for
    now, we revert its import rather than making a change this close to
    release.

--- qiskit/transpiler/passes/calibration/__init__.py
+++ qiskit/transpiler/passes/calibration/__init__.py
@@ -13,4 +13,3 @@
 """Module containing transpiler calibration passes."""
 
 from .builders import RZXCalibrationBuilder, RZXCalibrationBuilderNoEcho, PulseGates
-from .rzx_templates import rzx_templates
