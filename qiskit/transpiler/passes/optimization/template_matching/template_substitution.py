f50c4e62865915a5fc2f0e4af4ec979f501ecbf7
Nick Bronn <nick.bronn@gmail.com>
2021-12-15 20:08:36 -0400

    removed real=True in two symbol Symbol expressions, which was messing up the solver for some reason.

--- qiskit/transpiler/passes/optimization/template_matching/template_substitution.py
+++ qiskit/transpiler/passes/optimization/template_matching/template_substitution.py
@@ -505,11 +505,11 @@ class TemplateSubstitution:
                 equations.append(sym.Eq(template_params.get_sympy_expr(), circ_param_sym))
 
                 for param in template_params.parameters:
-                    temp_symbols[param] = sym.Symbol(str(param), real=True)
+                    temp_symbols[param] = sym.Symbol(str(param))
 
                 if isinstance(circuit_params[t_idx], ParameterExpression):
                     for param in circuit_params[t_idx].parameters:
-                        circ_dict[param] = sym.Symbol(str(param), real=True)
+                        circ_dict[param] = sym.Symbol(str(param))
 
         if not temp_symbols:
             return template_dag_dep
