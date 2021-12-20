5f860494ae5162a4674d3dad131c327c77617cc0
Nick Bronn <nick.bronn@gmail.com>
2021-12-20 15:56:44 -0400

    simplified the parameter handling for adding calibrations to gates

--- qiskit/dagcircuit/dagcircuit.py
+++ qiskit/dagcircuit/dagcircuit.py
@@ -215,23 +215,9 @@ class DAGCircuit:
         Raises:
             Exception: if the gate is of type string and params is None.
         """
-        # import pdb; pdb.set_trace()
-        if isinstance(gate, Gate):
-            if gate.params:
-                for param in gate.params:
-                    try:
-                        gate_has_param_exp = isinstance(param, ParameterExpression)
-                    except IndexError:
-                        gate_has_param_exp = False
 
-                    if gate_has_param_exp:
-                        self._calibrations[gate.name][
-                            (tuple(qubits), tuple([float(param)]))
-                        ] = schedule
-                    else:
-                        self._calibrations[gate.name][(tuple(qubits), tuple(param))] = schedule
-            else:
-                self._calibrations[gate.name][(tuple(qubits), tuple(gate.params))] = schedule
+        if isinstance(gate, Gate):
+            self._calibrations[gate.name][(tuple(qubits), tuple(gate.params))] = schedule
         else:
             self._calibrations[gate][(tuple(qubits), tuple(params or []))] = schedule
 
