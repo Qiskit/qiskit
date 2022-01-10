54ec96e4758814d85fe03d782a351c10701ec018
Shelly Garion <shelly@il.ibm.com>
2022-01-10 09:07:59 +0200

    revert changes in gate_utils in test

--- test/python/circuit/gate_utils.py
+++ test/python/circuit/gate_utils.py
@@ -12,7 +12,7 @@
 
 """Utils for testing the standard gates."""
 
-from inspect import signature, Parameter, isclass
+from inspect import signature, Parameter
 
 
 def _get_free_params(fun, ignore=None):
@@ -27,14 +27,8 @@ def _get_free_params(fun, ignore=None):
     """
     ignore = ignore or []
     free_params = []
-    if isclass(fun):
-        fun = fun.__init__
     for name, param in signature(fun).parameters.items():
-        if (
-            param.default == Parameter.empty
-            and param.kind != Parameter.VAR_POSITIONAL
-            and name != "self"
-        ):
+        if param.default == Parameter.empty and param.kind != Parameter.VAR_POSITIONAL:
             if name not in ignore:
                 free_params.append(name)
     return free_params
