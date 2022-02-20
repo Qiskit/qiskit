efb89d69b5692da2c69ace1b0fe8937d57ebee01
knzwnao <nkanazawa1989@gmail.com>
2022-02-20 14:09:51 +0900

    private functions import

--- qiskit/circuit/qpy_serialization.py
+++ qiskit/circuit/qpy_serialization.py
@@ -16,3 +16,12 @@
 
 # TODO deprecate this in 0.21.0
 from qiskit.qpy import dump, load
+
+# For backward compatibility. Provide, Runtime, Experiment call these private functions.
+from qiskit.qpy import (
+    _write_instruction,
+    _read_instruction,
+    _write_parameter_expression,
+    _read_parameter_expression,
+    _read_parameter_expression_v3,
+)
