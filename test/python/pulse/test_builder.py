7fddb5e61695fbeb4d5fa756363936d1750b00f6
Junye Huang <h.jun.ye@gmail.com>
2022-02-22 12:53:45 +0100

    Fix configurable_backend circular import problem

--- test/python/pulse/test_builder.py
+++ test/python/pulse/test_builder.py
@@ -22,7 +22,7 @@ from qiskit.pulse.instructions import directives
 from qiskit.pulse.transforms import target_qobj_transform
 from qiskit.test import QiskitTestCase
 from qiskit.test.mock import FakeOpenPulse2Q
-from qiskit.test.mock.utils.backend_converter import ConfigurableFakeBackend as ConfigurableBackend
+from qiskit.test.mock.utils.configurable_backend import ConfigurableFakeBackend as ConfigurableBackend
 from qiskit.pulse import library, instructions
 
 
