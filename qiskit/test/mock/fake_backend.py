a29d58ef0260bae941ca1b0b69bbd8fcb4dc645d
Junye Huang <h.jun.ye@gmail.com>
2022-03-07 13:02:56 +0100

    modify NoiseModel.from_backend method to support V2 backend

--- qiskit/test/mock/fake_backend.py
+++ qiskit/test/mock/fake_backend.py
@@ -445,131 +445,37 @@ class FakeBackendV2(BackendV2):
             job = sim.run(circuits, **kwargs)
         return job
 
-    def from_backend(cls, backend,
-                     gate_error=True,
-                     readout_error=True,
-                     thermal_relaxation=True,
-                     temperature=0,
-                     gate_lengths=None,
-                     gate_length_units='ns',
-                     standard_gates=None,
-                     warnings=True):
-        """Return a noise model derived from a devices backend properties.
-
-        This function generates a noise model based on:
-
-        * 1 and 2 qubit gate errors consisting of a
-          :func:`depolarizing_error` followed
-          by a :func:`thermal_relaxation_error`.
-
-        * Single qubit :class:`ReadoutError` on all measurements.
-
-        The Error error parameters are tuned for each individual qubit based on
-        the :math:`T_1`, :math:`T_2`, frequency and readout error parameters for
-        each qubit, and the gate error and gate time parameters for each gate
-        obtained from the device backend properties.
-
-        **Additional Information**
-
-        The noise model includes the following errors:
-
-        * If ``readout_error=True`` include single qubit readout
-          errors on measurements.
-
-        * If ``gate_error=True`` and ``thermal_relaxation=True`` include:
-
-            * Single-qubit gate errors consisting of a :func:`depolarizing_error`
-              followed by a :func:`thermal_relaxation_error` for the qubit the
-              gate acts on.
-
-            * Two-qubit gate errors consisting of a 2-qubit
-              :func:`depolarizing_error` followed by single qubit
-              :func:`thermal_relaxation_error` on each qubit participating in
-              the gate.
-
-        * If ``gate_error=True`` is ``True`` and ``thermal_relaxation=False``:
-
-            * An N-qubit :func:`depolarizing_error` on each N-qubit gate.
-
-        * If ``gate_error=False`` and ``thermal_relaxation=True`` include
-          single-qubit :func:`thermal_relaxation_errors` on each qubits
-          participating in a multi-qubit gate.
-
-        For best practice in simulating a backend make sure that the
-        circuit is compiled using the set of basis gates in the noise
-        module by setting ``basis_gates=noise_model.basis_gates``
-        and using the device coupling map with
-        ``coupling_map=backend.configuration().coupling_map``
-
-        **Specifying custom gate times**
-
-        The ``gate_lengths`` kwarg can be used to specify custom gate times
-        to add gate errors using the :math:`T_1` and :math:`T_2` values from
-        the backend properties. This should be passed as a list of tuples
-        ``gate_lengths=[(name, value), ...]``
-        where ``name`` is the gate name string, and ``value`` is the gate time
-        in nanoseconds.
-
-        If a custom gate is specified that already exists in
-        the backend properties, the ``gate_lengths`` value will override the
-        gate time value from the backend properties.
-        If non-default values are used gate_lengths should be a list
-
-        Args:
-            backend (BackendV1): backend.
-            gate_error (bool): Include depolarizing gate errors (Default: True).
-            readout_error (Bool): Include readout errors in model
-                                  (Default: True).
-            thermal_relaxation (Bool): Include thermal relaxation errors
-                                       (Default: True).
-            temperature (double): qubit temperature in milli-Kelvin (mK) for
-                                  thermal relaxation errors (Default: 0).
-            gate_lengths (list): Custom gate times for thermal relaxation errors.
-                                  Used to extend or override the gate times in
-                                  the backend properties (Default: None))
-            gate_length_units (str): Time units for gate length values in
-                                     gate_lengths. Can be 'ns', 'ms', 'us',
-                                     or 's' (Default: 'ns').
-            standard_gates (bool): DEPRECATED, If true return errors as standard
-                                   qobj gates. If false return as unitary
-                                   qobj instructions (Default: None)
-            warnings (bool): Display warnings (Default: True).
-
-        Returns:
-            NoiseModel: An approximate noise model for the device backend.
-
-        Raises:
-            NoiseError: If the input backend is not valid.
+    def _get_noise_model_from_backend_v2(
+        self,
+        gate_error=True,
+        readout_error=True,
+        thermal_relaxation=True,
+        temperature=0,
+        gate_lengths=None,
+        gate_length_units='ns',
+        standard_gates=None,
+        warnings=True
+    ):
+        """Build noise model from BackendV2.
+        This is a temporary fix until Aer supports V2 backends.
         """
-        if isinstance(backend, BackendV2):
-            raise NoiseError(
-                "NoiseModel.from_backend does not currently support V2 Backends.")
-        if isinstance(backend, (BaseBackend, BackendV1)):
-            properties = backend.properties()
-            configuration = backend.configuration()
-            basis_gates = configuration.basis_gates
-            num_qubits = configuration.num_qubits
-            dt = getattr(configuration, "dt", 0)
-            if not properties:
-                raise NoiseError('Qiskit backend {} does not have a '
-                                 'BackendProperties'.format(backend))
-        elif isinstance(backend, BackendProperties):
-            warn(
-                'Passing BackendProperties instead of a "backend" object '
-                'has been deprecated as of qiskit-aer 0.10.0 and will be '
-                'removed no earlier than 3 months from that release date. '
-                'Duration dependent delay relaxation noise requires a '
-                'backend object.', DeprecationWarning, stacklevel=2)
-            properties = backend
-            basis_gates = set()
-            for prop in properties.gates:
-                basis_gates.add(prop.gate)
-            basis_gates = list(basis_gates)
-            num_qubits = len(properties.qubits)
-            dt = 0  # disable delay noise if dt is unknown
-        else:
-            raise NoiseError('{} is not a Qiskit backend or'
-                             ' BackendProperties'.format(backend))
+
+        from warnings import warn, catch_warnings, filterwarnings
+        from qiskit.circuit import Delay
+        from qiskit.providers.exceptions import BackendPropertyError
+        from qiskit.providers.aer.noise import NoiseModel
+        from qiskit.providers.aer.noise.device.models import (
+            _excited_population,
+            basic_device_gate_errors,
+            basic_device_readout_errors
+        )
+        from qiskit.providers.aer.noise.passes import RelaxationNoisePass
+
+        properties = self.properties()
+        basis_gates = self.operation_names
+        num_qubits = self.num_qubits
+        dt = self.dt
+
         noise_model = NoiseModel(basis_gates=basis_gates)
 
         # Add single-qubit readout errors
