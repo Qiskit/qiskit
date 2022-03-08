a3da72d89db16d1009c9a9cc1885d9f85b89985e
Dariusz Lasecki <dal@zurich.ibm.com>
2022-03-08 10:45:17 +0100

    Lint fix.

--- test/python/algorithms/quantum_time_evolution/real/implementations/trotterization/test_trotter_qrte.py
+++ test/python/algorithms/quantum_time_evolution/real/implementations/trotterization/test_trotter_qrte.py
@@ -14,6 +14,7 @@
 
 import unittest
 
+from test.python.opflow import QiskitOpflowTestCase
 from ddt import ddt, data
 import numpy as np
 from numpy.testing import assert_raises
@@ -36,7 +37,6 @@ from qiskit.opflow import (
     Y,
 )
 from qiskit.synthesis import SuzukiTrotter, QDrift
-from test.python.opflow import QiskitOpflowTestCase
 
 
 @ddt
