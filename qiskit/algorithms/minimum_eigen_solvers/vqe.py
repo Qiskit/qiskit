de8bcbb79336daae450be86d74803b1ff206020d
vishnu <vishnuajith@gmail.com>
2022-03-09 10:24:16 +0530

    lint fixing releasenote and vqe.py

--- qiskit/algorithms/minimum_eigen_solvers/vqe.py
+++ qiskit/algorithms/minimum_eigen_solvers/vqe.py
@@ -526,7 +526,6 @@ class VQE(VariationalAlgorithm, MinimumEigensolver):
         else:
             gradient = self._gradient
 
-
         self._eval_count = 0
         energy_evaluation, expectation = self.get_energy_evaluation(
             operator, return_expectation=True
