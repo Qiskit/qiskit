13e63a86b80ea638818668ff4c9e222f13344127
knzwnao <nkanazawa1989@gmail.com>
2022-03-11 00:13:28 +0900

    convert DD into padding pass.
    Now DD pass is a subclass of padding pass. DD pass takes pulse alignment constraints to execute circuit on recent RTA systems.
    
    There are small changes to the base padding pass to host this pass as a subclass. .pad method is now called with (t0, t1) instead of interval. In addition, pre_runhook method is added which is called before running the padding protocol.

--- qiskit/transpiler/passes/scheduling/__init__.py
+++ qiskit/transpiler/passes/scheduling/__init__.py
@@ -15,6 +15,5 @@
 from .alap import ALAPSchedule
 from .asap import ASAPSchedule
 from .time_unit_conversion import TimeUnitConversion
-from .dynamical_decoupling import DynamicalDecoupling
 from .instruction_alignment import AlignMeasures, ValidatePulseGates
-from .padding import PadDelay
+from .padding import PadDelay, DynamicalDecoupling
