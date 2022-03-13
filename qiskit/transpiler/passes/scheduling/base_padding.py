095274ba6df421bc63308f5a9eec0543d984f189
knzwnao <nkanazawa1989@gmail.com>
2022-03-13 11:25:37 +0900

    add comment for invalid interval

--- qiskit/transpiler/passes/scheduling/base_padding.py
+++ qiskit/transpiler/passes/scheduling/base_padding.py
@@ -203,6 +203,16 @@ class BasePadding(TransformationPass):
             in the property set so that the added node is also scheduled.
             This is achieved by adding operation via :meth:`_apply_scheduled_op`.
 
+        .. note::
+
+            This method doesn't check if the total duration of new DAGOpNode added here
+            is identical to the interval (``t_end - t_start``).
+            A developer of the pass must guarantee this is satisfied.
+            If the duration is greater than the interval, your circuit may be
+            compiled down to the target code with extra duration on the backend compiler,
+            which is then played normally without error. However, the outcome of your circuit
+            might be unexpected due to erroneous scheduling.
+
         Args:
             dag: DAG circuit that sequence is applied.
             qubit: The wire that the sequence is applied on.
