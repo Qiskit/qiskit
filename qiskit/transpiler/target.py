7467249067cc98c55b4a64e2dd2024a1678cc828
Matthew Treinish <mtreinish@kortar.org>
2022-03-17 18:05:32 -0400

    Make ConsolidateBlocks transpiler pass target aware (#7778)
    * Make ConsolidateBlocks transpiler pass target aware
    
    This commit updates the consolidate blocks pass target aware so that at
    its instantiation a Target object can be specified to define the target
    device for compilation. When specified this target will be used for
    all device aware operations in the pass. When a target is specified the
    consolidate blocks pass will only consolidate blocks with instructions
    outside the target (both operation and qubits).
    
    Part of #7113
    
    * Fix typo
    
    * Add release note
    
    * Add tests for target instruction_supported() method
    
    * Add Target tests for consolidate block
    
    * Apply suggestions from code review
    
    Co-authored-by: Jake Lishman <jake@binhbar.com>
    
    * Check number of arguments in ideal operation case
    
    This commit adds a check in the ideal operation case that the number of
    qargs is equal to the number of qubits the operation object supports.
    This way if you call instruction_supported() with a qargs parameter that
    has the wrong number of arguments even an ideally implemented gate will
    return false. For example, target.instruction_supported(cx, (0, 1, 2))
    will return False since a CXGate object only supports 2 qubits.
    
    * Cast qargs to tuple to prevent issues with qarg list lookups
    
    * Deduplicate qubit index map creation
    
    Co-authored-by: Jake Lishman <jake@binhbar.com>

--- qiskit/transpiler/target.py
+++ qiskit/transpiler/target.py
@@ -484,6 +484,28 @@ class Target(Mapping):
             raise KeyError(f"{qargs} not in target.")
         return [self._gate_name_map[x] for x in self._qarg_gate_map[qargs]]
 
+    def instruction_supported(self, operation_name, qargs):
+        """Return whether the instruction (operation + qubits) is supported by the target
+
+        Args:
+            operation_name (str): The name of the operation for the instruction
+            qargs (tuple): The tuple of qubit indices for the instruction
+
+        Returns:
+            bool: Returns ``True`` if the instruction is supported and ``False`` if it isn't.
+
+        """
+        # Case a list if passed in by mistake
+        qargs = tuple(qargs)
+        if operation_name in self._gate_map:
+            if qargs in self._gate_map[operation_name]:
+                return True
+            if self._gate_map[operation_name] is None or None in self._gate_map[operation_name]:
+                return self._gate_name_map[operation_name].num_qubits == len(qargs) and all(
+                    x < self.num_qubits for x in qargs
+                )
+        return False
+
     @property
     def operation_names(self):
         """Get the operation names in the target."""
