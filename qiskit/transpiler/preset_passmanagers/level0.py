14e292e319e7220c29a23dc4eb80f8b6e46e60cb
Matthew Treinish <mtreinish@kortar.org>
2022-03-19 02:24:18 -0400

    Make UnitarySynthesis transpiler pass Target aware (#7775)
    * Make UnitarySynthesis transpiler pass Target aware
    
    This commit updates the unitary synthesis pass target aware so that at
    its insantiation a Target object can be specified to define the target
    device for compilation. When specified this target will be used for
    all device aware operations in the pass.
    
    This also requires adding an optional supports_target attribute to the
    unitary synthesis plugin interface to specified whether a plugin
    supports working natively with a target.
    
    * Use kwargs for arguments on _build_gate_*() functions
    
    * Remove redundant target parsing in default synthesis plugin
    
    If a target is present we're populating the gate_lengths and gate_errors
    payload based on the instruction properties in the target. So when we
    pass that to the default plugin along with the target we don't need to
    traverse the target again to extract the errors and lengths as we can
    access the dict payload to do this in the same manner as if a target
    wasn't present. This removes some duplicated code and simplifies the
    logic in the default synthesis plugin.
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- qiskit/transpiler/preset_passmanagers/level0.py
+++ qiskit/transpiler/preset_passmanagers/level0.py
@@ -105,6 +105,7 @@ def level_0_pass_manager(pass_manager_config: PassManagerConfig) -> PassManager:
             method=unitary_synthesis_method,
             min_qubits=3,
             plugin_config=unitary_synthesis_plugin_config,
+            target=target,
         ),
         Unroll3qOrMore(),
     ]
@@ -171,6 +172,7 @@ def level_0_pass_manager(pass_manager_config: PassManagerConfig) -> PassManager:
                 backend_props=backend_properties,
                 method=unitary_synthesis_method,
                 plugin_config=unitary_synthesis_plugin_config,
+                target=target,
             ),
             UnrollCustomDefinitions(sel, basis_gates),
             BasisTranslator(sel, basis_gates, target),
@@ -185,6 +187,7 @@ def level_0_pass_manager(pass_manager_config: PassManagerConfig) -> PassManager:
                 method=unitary_synthesis_method,
                 min_qubits=3,
                 plugin_config=unitary_synthesis_plugin_config,
+                target=target,
             ),
             Unroll3qOrMore(),
             Collect2qBlocks(),
@@ -197,6 +200,7 @@ def level_0_pass_manager(pass_manager_config: PassManagerConfig) -> PassManager:
                 backend_props=backend_properties,
                 method=unitary_synthesis_method,
                 plugin_config=unitary_synthesis_plugin_config,
+                target=target,
             ),
         ]
     else:
