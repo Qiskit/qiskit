6e412abc1122e611444dcf40704c0d0ad6b2e9ed
Matthew Treinish <mtreinish@kortar.org>
2022-03-18 21:26:10 -0400

    Make DenseLayout transpiler pass target aware (#7776)
    * Make DenseLayout transpiler pass target aware
    
    This commit updates the dense layout pass target aware so that at
    its instantiation a Target object can be specified to define the target
    device for compilation. When specified this target will be used for
    all device aware operations in the pass. When a target is specified the
    error matrix used is the average of all 1q and 2q operations while
    before the matrix was composed of just cx and readout error rates.
    
    Part of #7113
    
    * Adjust error heuristic for target case
    
    This commit adjusts the error heuristic used in dense layout. It now
    uses the max() error rate (so worst fidelity) operation on qubits instead
    of the average fidelity which was used in the previous commits. It also
    removes the weighting in the code based on the number of 1q ops and 2q
    ops. Previously the error rate for a layout was calculated as:
    
        ((sum(1q error for qubits in layout) / num_dag_qubits) * num_1q_ops - avg_1q_err) + ((sum(2q error for edges in layout subgraph) / num_dag_qubits) * num_2q_ops)
    
    however since we're considering all 2q and 1q ops now knowing exactly
    what's going to end up in the circuit is not exact so the weighing based
    on the number of ops is removed. So when a target is set the value of
    `num_1q_ops` and `num_2q_ops` is hard coded to 1.
    
    * Add comment explaining why we use max error rate
    
    * Make coupling_map kwarg optional
    
    * Expand DenseLayout tests
    
    * Apply suggestions from code review
    
    Co-authored-by: Kevin Hartman <kevin@hart.mn>
    
    Co-authored-by: Kevin Hartman <kevin@hart.mn>

--- qiskit/transpiler/passes/layout/dense_layout.py
+++ qiskit/transpiler/passes/layout/dense_layout.py
@@ -35,39 +35,33 @@ class DenseLayout(AnalysisPass):
         by being set in `property_set`.
     """
 
-    def __init__(self, coupling_map, backend_prop=None):
+    def __init__(self, coupling_map=None, backend_prop=None, target=None):
         """DenseLayout initializer.
 
         Args:
             coupling_map (Coupling): directed graph representing a coupling map.
             backend_prop (BackendProperties): backend properties object
+            target (Target): A target representing the target backend.
         """
         super().__init__()
         self.coupling_map = coupling_map
         self.backend_prop = backend_prop
+        self.target = target
         num_qubits = 0
         self.adjacency_matrix = None
-        if self.coupling_map:
-            num_qubits = self.coupling_map.size()
-            self.adjacency_matrix = retworkx.adjacency_matrix(self.coupling_map.graph)
-        self.error_mat = np.zeros((num_qubits, num_qubits))
-        if self.backend_prop and self.coupling_map:
-            error_dict = {
-                tuple(gate.qubits): gate.parameters[0].value
-                for gate in self.backend_prop.gates
-                if len(gate.qubits) == 2
-            }
-            for edge in self.coupling_map.get_edges():
-                gate_error = error_dict.get(edge)
-                if gate_error is not None:
-                    self.error_mat[edge[0]][edge[1]] = gate_error
-            for index, qubit_data in enumerate(self.backend_prop.qubits):
-                # Handle faulty qubits edge case
-                if index >= num_qubits:
-                    break
-                for item in qubit_data:
-                    if item.name == "readout_error":
-                        self.error_mat[index][index] = item.value
+        if target is not None:
+            num_qubits = target.num_qubits
+            self.coupling_map = target.build_coupling_map()
+            if self.coupling_map is not None:
+                self.adjacency_matrix = retworkx.adjacency_matrix(self.coupling_map.graph)
+            self.error_mat, self._use_error = _build_error_matrix(num_qubits, target=target)
+        else:
+            if self.coupling_map:
+                num_qubits = self.coupling_map.size()
+                self.adjacency_matrix = retworkx.adjacency_matrix(self.coupling_map.graph)
+            self.error_mat, self._use_error = _build_error_matrix(
+                num_qubits, backend_prop=self.backend_prop, coupling_map=self.coupling_map
+            )
 
     def run(self, dag):
         """Run the DenseLayout pass on `dag`.
@@ -81,18 +75,26 @@ class DenseLayout(AnalysisPass):
         Raises:
             TranspilerError: if dag wider than self.coupling_map
         """
+        if self.coupling_map is None:
+            raise TranspilerError(
+                "A coupling_map or target with constrained qargs is necessary to run the pass."
+            )
         num_dag_qubits = len(dag.qubits)
         if num_dag_qubits > self.coupling_map.size():
             raise TranspilerError("Number of qubits greater than device.")
         num_cx = 0
         num_meas = 0
 
-        # Get avg number of cx and meas per qubit
-        ops = dag.count_ops()
-        if "cx" in ops.keys():
-            num_cx = ops["cx"]
-        if "measure" in ops.keys():
-            num_meas = ops["measure"]
+        if self.target is not None:
+            num_cx = 1
+            num_meas = 1
+        else:
+            # Get avg number of cx and meas per qubit
+            ops = dag.count_ops()
+            if "cx" in ops.keys():
+                num_cx = ops["cx"]
+            if "measure" in ops.keys():
+                num_meas = ops["measure"]
 
         best_sub = self._best_subset(num_dag_qubits, num_meas, num_cx)
         layout = Layout()
@@ -125,7 +127,7 @@ class DenseLayout(AnalysisPass):
             self.adjacency_matrix,
             num_meas,
             num_cx,
-            bool(self.backend_prop),
+            self._use_error,
             self.coupling_map.is_symmetric,
             self.error_mat,
         )
@@ -134,3 +136,53 @@ class DenseLayout(AnalysisPass):
         perm = csgraph.reverse_cuthill_mckee(sp_sub_graph)
         best_map = best_map[perm]
         return best_map
+
+
+def _build_error_matrix(num_qubits, target=None, coupling_map=None, backend_prop=None):
+    error_mat = np.zeros((num_qubits, num_qubits))
+    use_error = False
+    if target is not None and target.qargs is not None:
+        for qargs in target.qargs:
+            # Ignore gates over 2q DenseLayout only works with 2q
+            if len(qargs) > 2:
+                continue
+            error = 0.0
+            ops = target.operation_names_for_qargs(qargs)
+            for op in ops:
+                props = target[op][qargs]
+                if props is not None and props.error is not None:
+                    # Use max error rate to represent operation error
+                    # on a qubit(s). If there is more than 1 operation available
+                    # we don't know what will be used on the qubits eventually
+                    # so we take the highest error operation as a proxy for
+                    # the possible worst case.
+                    error = max(error, props.error)
+            max_error = error
+            # TODO: Factor in T1 and T2 to error matrix after #7736
+            if len(qargs) == 1:
+                qubit = qargs[0]
+                error_mat[qubit][qubit] = max_error
+                use_error = True
+            elif len(qargs) == 2:
+                error_mat[qargs[0]][qargs[1]] = max_error
+                use_error = True
+    elif backend_prop and coupling_map:
+        error_dict = {
+            tuple(gate.qubits): gate.parameters[0].value
+            for gate in backend_prop.gates
+            if len(gate.qubits) == 2
+        }
+        for edge in coupling_map.get_edges():
+            gate_error = error_dict.get(edge)
+            if gate_error is not None:
+                error_mat[edge[0]][edge[1]] = gate_error
+                use_error = True
+        for index, qubit_data in enumerate(backend_prop.qubits):
+            # Handle faulty qubits edge case
+            if index >= num_qubits:
+                break
+            for item in qubit_data:
+                if item.name == "readout_error":
+                    error_mat[index][index] = item.value
+                    use_error = True
+    return error_mat, use_error
