9a29cfc31e990cedc4f8e060165b7a7caee16418
Luciano Bello <bel@zurich.ibm.com>
2022-03-20 10:24:08 +0100

    testing: less than 2 ^ number of bits

--- test/python/circuit/test_instructions.py
+++ test/python/circuit/test_instructions.py
@@ -562,6 +562,14 @@ class TestInstructions(QiskitTestCase):
             instructions.add(instruction, [Qubit()], [])
             with self.assertRaisesRegex(CircuitError, r"condition value should be 0/1 or True/False"):
                 instructions.c_if(Clbit(), 2)
+            with self.subTest("less than 2 ^ number of bits"):
+                instruction = HGate()
+                instructions = InstructionSet()
+                instructions.add(instruction, [Qubit()], [])
+                register = ClassicalRegister(2)
+                with self.assertRaisesRegex(CircuitError,
+                                            r"condition value should be less than 2 \^ number of bits"):
+                    instructions.c_if(register, 10)
 
     def test_instructionset_c_if_deprecated_resolution(self):
         r"""Test that the deprecated path of passing an iterable of :obj:`.ClassicalRegister`\ s to
