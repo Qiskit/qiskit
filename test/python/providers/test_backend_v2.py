f2a2d2247da5cb5ce46b446dc4f54ced8637a775
Matthew Treinish <mtreinish@kortar.org>
2022-03-31 04:26:31 -0400

    Add qubit_properties to Target class (#7736)
    * Add qubit_properties to Target class
    
    This commit adds a new attribute to the target class for defining qubit
    properties. This was originally not included in the target because
    nothing in the transpiler supports using any qubit properties. However,
    in a future PR I'm planning to modify the default heuristic layout score
    used by VF2Layout to factor in T1 and/or T2 in addition to the
    measurement error rate it's using now. To do this though we need to
    ensure that we have these values available via the target. This commit
    also is the first time we've had to expand the set of information that
    can optionally be pecified in a target. I opted to just make a straight
    addition here rather than versioning it. There should be no backwards
    compatibility issue for users or provider maintainers on upgrade because
    not specifying a qubit properties payload will not change any behavior
    the Target and BackendV2 objects will behave exactly as before.
    
    I was thinking we'd only use a version if we needed to make a backwards
    incompatible change in the future. That way we can support both versions
    of the class a the same time while we migrated to a newer interface. But
    for simple additions like this that don't effect the behavior or cause
    any issues with compatibility we can simply add them to the class.
    
    * Remove unecessary getter function from target
    
    This commit removes an unecessary getter method to access the
    qubit_properties attribute of the target. The attribute was already
    public and we could get the qubit properties directly from the list
    stored in the target.
    
    * Add missing release note
    
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- test/python/providers/test_backend_v2.py
+++ test/python/providers/test_backend_v2.py
@@ -24,7 +24,12 @@ from qiskit.compiler import transpile
 from qiskit.compiler.transpiler import _parse_inst_map
 from qiskit.pulse.instruction_schedule_map import InstructionScheduleMap
 from qiskit.test.base import QiskitTestCase
-from qiskit.test.mock.fake_backend_v2 import FakeBackendV2, FakeBackend5QV2
+from qiskit.test.mock.fake_backend_v2 import (
+    FakeBackendV2,
+    FakeBackend5QV2,
+    FakeBackendSimple,
+    FakeBackendV2LegacyQubitProps,
+)
 from qiskit.test.mock.fake_mumbai_v2 import FakeMumbaiFractionalCX
 from qiskit.quantum_info import Operator
 
@@ -52,6 +57,18 @@ class TestBackendV2(QiskitTestCase):
         self.assertEqual([73.09352e-6, 63.48783e-6], [x.t1 for x in props])
         self.assertEqual([126.83382e-6, 112.23246e-6], [x.t2 for x in props])
         self.assertEqual([5.26722e9, 5.17538e9], [x.frequency for x in props])
+
+    def test_legacy_qubit_properties(self):
+        """Test that qubit props work for backends not using properties in target."""
+        props = FakeBackendV2LegacyQubitProps().qubit_properties([1, 0])
+        self.assertEqual([73.09352e-6, 63.48783e-6], [x.t1 for x in props])
+        self.assertEqual([126.83382e-6, 112.23246e-6], [x.t2 for x in props])
+        self.assertEqual([5.26722e9, 5.17538e9], [x.frequency for x in props])
+
+    def test_no_qubit_properties_raises(self):
+        """Ensure that if no qubit properties are defined we raise correctly."""
+        with self.assertRaises(NotImplementedError):
+            FakeBackendSimple().qubit_properties(0)
 
     def test_option_bounds(self):
         """Test that option bounds are enforced."""
