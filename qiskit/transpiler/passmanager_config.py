ad8ff03a01ebf0a7646f91bf89fc6d4a416e71eb
Matthew Treinish <mtreinish@kortar.org>
2022-05-18 16:37:14 -0400

    Add string representation for PassManagerConfig
    This commit adds a new string representation to the PassManagerConfig
    class. When str() is run on a PassManagerConfig object it will return a
    string listing the values for all of the attributes in the class.

--- qiskit/transpiler/passmanager_config.py
+++ qiskit/transpiler/passmanager_config.py
@@ -124,3 +124,24 @@ class PassManagerConfig:
                 res.target = backend.target
 
         return res
+
+    def __str__(self):
+        return (
+            "Pass Manager Config:\n"
+            f"\tinitial_layout: {self.initial_layout}\n"
+            f"\tbasis_gates: {self.basis_gates}\n"
+            f"\tinst_map: {self.inst_map}\n"
+            f"\tcoupling_map: {self.coupling_map}\n"
+            f"\tlayout_method: {self.layout_method}\n"
+            f"\trouting_method: {self.routing_method}\n"
+            f"\ttranslation_method: {self.translation_method}\n"
+            f"\tscheduling_method: {self.scheduling_method}\n"
+            f"\tinstruction_durations: {self.instruction_durations}\n"
+            f"\tbackend_properties: {self.backend_properties}\n"
+            f"\tapproximation_degree: {self.approximation_degree}\n"
+            f"\tseed_transpiler: {self.seed_transpiler}\n"
+            f"\ttiming_constraints: {self.timing_constraints}\n"
+            f"\tunitary_synthesis_method: {self.unitary_synthesis_method}\n"
+            f"\tunitary_synthesis_plugin_config: {self.unitary_synthesis_plugin_config}\n"
+            f"\ttarget: {self.target}\n"
+        )
