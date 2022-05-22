d64770d3e16cd8d0495a0567378383b70ae8dd87
Pedro Rivero <pedro.rivero.ramirez@gmail.com>
2022-05-22 14:37:13 +0200

    Update docstring

--- qiskit/primitives/base_result.py
+++ qiskit/primitives/base_result.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 """
-Primitive result abstract class
+Primitive result abstract base class
 """
 
 from abc import ABC
@@ -19,24 +19,24 @@ from typing import Sized
 
 
 class BaseResult(ABC):
-    """Primitive result base class.
+    """Primitive result abstract base class.
 
     Base class for Primitive results meant to provide common functionality to all inheriting result dataclasses.
     """
 
     def __post_init__(self) -> None:
-        """Verify that all fields are consistent with the number of experiments represented."""
+        """Verify that all fields in any inheriting result dataclass are consistent, after instantiation, with the number of experiments being represented."""
         for val in self._field_values:  # type: Sized
             if len(val) != self.num_experiments:
                 raise ValueError("Inconsistent number of experiments across data fields.")
 
     @property
     def num_experiments(self) -> int:
-        """Number of experiments."""
+        """Number of experiments in any inheriting result dataclass."""
         field_value: Sized = self._field_values.pop()
         return len(field_value)
 
     @property
     def _field_values(self) -> list:
-        """Returns list of field values in any inheriting dataclass."""
+        """List of field values in any inheriting dataclass."""
         return [getattr(self, field.name) for field in fields(self)]
