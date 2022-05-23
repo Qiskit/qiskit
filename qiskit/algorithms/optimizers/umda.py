6e7be93523aa0276603841d9704c04769fec13f1
Vicente P. Soloviev <vicente.perez.soloviev@gmail.com>
2022-05-23 13:32:27 +0200

    remove pandas

--- qiskit/algorithms/optimizers/umda.py
+++ qiskit/algorithms/optimizers/umda.py
@@ -13,10 +13,10 @@
 """Univariate Marginal Distribution Algorithm (Estimation-of-Distribution-Algorithm)."""
 
 from typing import Callable, List, Optional, Tuple
-import pandas as pd
 import numpy as np
 from scipy.stats import norm
 from .optimizer import OptimizerResult, POINT
+from qiskit.utils import algorithm_globals
 
 from .scipy_optimizer import SciPyOptimizer
 
@@ -32,6 +32,7 @@ class UMDA(SciPyOptimizer):
     best_mae_global = 9999999
     best_ind_global = 9999999
     history = []
+    evaluations = 0
 
     elite_factor = 0.4
     std_bound = 0.3
@@ -63,28 +64,23 @@ class UMDA(SciPyOptimizer):
         self.vector = self._initialization()
         self.dead_iter = self.size_gen / 5
 
-        self.variables = list(self.vector.columns)
-
         self.best_mae_global = 999999999999
         self.truncation_length = int(size_gen * alpha)
 
         # initialization of generation
-        mus = self.vector[self.variables].loc['mu'].to_list()
-        stds = self.vector[self.variables].loc['std'].to_list()
-        self.generation = pd.DataFrame(np.random.normal(mus, stds, [self.size_gen, len(self.variables)]),
-                                       columns=self.variables, dtype='float_')
+        self.generation = algorithm_globals.random.normal(self.vector[0, :],
+                                                          self.vector[1, :],
+                                                          [self.size_gen, self.n_variables])
 
         super().__init__(method="UMDA")
 
     def _initialization(self):
-        vector = pd.DataFrame(columns=list(range(0, self.n_variables)))
-        vector['data'] = ['mu', 'std', 'min', 'max']
-        vector = vector.set_index('data')
-        vector.loc['mu'] = np.pi
-        vector.loc['std'] = 0.5
+        vector = np.zeros((4, self.n_variables))
 
-        vector.loc['min'] = 0  # optional
-        vector.loc['max'] = np.pi * 2  # optional
+        vector[0, :] = np.pi  # mu
+        vector[1, :] = 0.5  # std
+        vector[2, :] = 0  # min
+        vector[3, :] = np.pi * 2  # max
 
         return vector
 
@@ -94,28 +90,27 @@ class UMDA(SciPyOptimizer):
         Updates the generation pandas dataframe
         """
 
-        mus = self.vector[self.variables].loc['mu'].to_list()
-        stds = self.vector[self.variables].loc['std'].to_list()
-        gen = pd.DataFrame(np.random.normal(mus, stds, [self.size_gen, len(self.variables)]),
-                           columns=self.variables, dtype='float_')
+        gen = algorithm_globals.random.normal(self.vector[0, :],
+                                              self.vector[1, :],
+                                              [self.size_gen, self.n_variables])
 
-        self.generation = self.generation.nsmallest(int(self.elite_factor*len(self.generation)), 'cost')
-        # self.generation = self.generation.append(gen).reset_index(drop=True)
-        self.generation = pd.concat([self.generation, gen]).reset_index(drop=True)
+        self.generation = self.generation[:int(self.elite_factor*len(self.generation))]
+        self.generation = np.vstack((self.generation, gen))
 
     # truncate the generation at alpha percent
     def _truncation(self):
         """Selection of the best individuals of the actual generation.
         Updates the generation by selecting the best individuals.
         """
-        self.generation = self.generation.nsmallest(self.truncation_length, 'cost')
+        best_indices = self.evaluations.argsort()[:self.truncation_length]
+        self.generation = self.generation[best_indices, :]
+        self.evaluations = np.take(self.evaluations, best_indices)
 
     # check each individual of the generation
     def _check_generation(self, objective_function):
         """Check the cost of each individual in the cost function implemented by the user.
         """
-        self.generation['cost'] = self.generation.apply(lambda row: objective_function(
-            row[self.variables].to_list()), axis=1)
+        self.evaluations = np.apply_along_axis(objective_function, 1, self.generation)
 
     # update the probability vector
     def _update_vector(self):
@@ -123,10 +118,10 @@ class UMDA(SciPyOptimizer):
         generation can sample from it. Update the vector of normal distributions
         """
 
-        for var in self.variables:
-            self.vector.at['mu', var], self.vector.at['std', var] = norm.fit(self.generation[var].values)
-            if self.vector.at['std', var] < self.std_bound:
-                self.vector.at['std', var] = self.std_bound
+        for i in range(self.n_variables):
+            self.vector[0, i], self.vector[1, i] = norm.fit(self.generation[:, i])
+            if self.vector[1, i] < self.std_bound:
+                self.vector[1, i] = self.std_bound
 
     def minimize(
         self,
@@ -145,10 +140,11 @@ class UMDA(SciPyOptimizer):
             self._truncation()
             self._update_vector()
 
-            best_mae_local = self.generation['cost'].min()
+            best_mae_local = min(self.evaluations)
 
             self.history.append(best_mae_local)
-            best_ind_local = self.generation[self.generation['cost'] == best_mae_local]
+            best_ind_local = np.where(self.evaluations == best_mae_local)[0][0]
+            best_ind_local = self.generation[best_ind_local]
 
             # update the best values ever
             if best_mae_local < self.best_mae_global:
@@ -159,21 +155,13 @@ class UMDA(SciPyOptimizer):
             else:
                 not_better += 1
                 if not_better == self.dead_iter:
-                    result.x = self.best_ind_global.reset_index(drop=True).loc[0].to_list()[:-1]
-                    result.fun = self.best_mae_global
-                    result.nfev = len(self.history)
-
-                    if self.disp:
-                        print('\tNFVALS = ' + str(result.nfev) + ' F = ' + str(result.fun))
-                        print('\tX = ' + str(result.x))
-
-                    return result
+                    break
 
             self._new_generation()
 
-        result.x = self.best_ind_global.reset_index(drop=True).loc[0].to_list()[:-1]
+        result.x = self.best_ind_global
         result.fun = self.best_mae_global
-        result.nfev = len(self.history)
+        result.nfev = len(self.history) * self.size_gen
 
         if self.disp:
             print('\tNFVALS = ' + str(result.nfev) + ' F = ' + str(result.fun))
