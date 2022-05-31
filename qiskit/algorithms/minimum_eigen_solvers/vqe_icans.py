61362cb629e2d5da69f8d21135d593676d438e77
Julien Gacon <gaconju@gmail.com>
2022-05-31 16:51:59 +0200

    formatting, auxops, callback

--- qiskit/algorithms/minimum_eigen_solvers/vqe_icans.py
+++ qiskit/algorithms/minimum_eigen_solvers/vqe_icans.py
@@ -1,42 +1,116 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
 
-from typing import Optional, List, Callable, Union, Dict, Tuple
+"""VQE optimization with dynamic shot scheduling – ICANS optimizer."""
 
-from qiskit import Aer
-from qiskit.algorithms import VQE, VQEResult
-from qiskit.algorithms.optimizers import SPSA, ADAM
-from qiskit.circuit.library import EfficientSU2
-from qiskit.opflow import X, Y, Z, I, StateFn
-from qiskit import Aer, transpile, assemble
-from qiskit.circuit.library import RealAmplitudes
-from qiskit.opflow import PauliSumOp
-from qiskit.circuit import QuantumCircuit, Parameter, ParameterVector
-from qiskit.opflow import *
-from qiskit.utils import QuantumInstance
-from qiskit.providers import Backend
-import numpy as np
+from typing import Optional, Callable, Union
 import copy
 from time import time
+import numpy as np
+
+from qiskit.algorithms.list_or_dict import ListOrDict
+from qiskit.algorithms.aux_ops_evaluator import eval_observables
+from qiskit.circuit import QuantumCircuit
+from qiskit.opflow import OperatorBase, PauliExpectation, SummedOp
+from qiskit.utils import QuantumInstance
+from qiskit.providers import Backend
+
+from .vqe import VQE, VQEResult
+from .minimum_eigen_solver import MinimumEigensolverResult
 
 
-##----icans
 class ICANS(VQE):
-    # Init is the same as standard VQE
-    # New parameters will appear at compute_minimum_eigenvalue function
+    """
+    The iCANS algorithm (individual coupled adaptive number of shots) [1], adaptively selects the
+    number of shots in each iteration to reach the target accuracy with minimal number of quantum
+    computational resources, both for a given iteration and for a given partial derivative in a
+    stochastic gradient descent. As an optimization method, it is appropriately suited to noisy
+    situations, when the cost of each shot is expensive.
+
+    ICANS is implemented as a subclass of VQE. VQE [2] is a quantum algorithm that uses a
+    variational technique to find the minimum eigenvalue of the Hamiltonian of a given system.
+    According to Ref. [1], iCANS may perform comparably or better than other state-of-the-art
+    optimizers, and especially well in the presence of realistic hardware noise.
+    There are two variations, iCANS1 and iCANS2. The first is the more aggressive version,
+    in relation to the learning rate, while iCANS2 is more cautious and limits the learning rate
+    so that the expected gain is always guaranteed to be positive.
+
+    Examples::
+
+    This example runs ICANS with some specified parameters and limited learning rate.
+
+    .. code-block::python
+
+        import numpy as np
+
+        from qiskit.algorithms import ICANS
+        from qiskit.circuit.library import EfficientSU2
+        from qiskit import Aer
+
+        backend = Aer.get_backend("qasm_simulator")
+
+        hamiltonian = PauliSumOp.from_list(
+            [
+                ("XXI", 1),
+                ("XIX", 1),
+                ("IXX", 1),
+                ("YYI", 1),
+                ("YIY", 1),
+                ("IYY", 1),
+                ("ZZI", 1),
+                ("ZIZ", 1),
+                ("IZZ", 1),
+                ("IIZ", 3),
+                ("IZI", 3),
+                ("ZII", 3),
+            ]
+        )
+
+        # parameters for iCANS
+        maxiter = 500
+        min_shots = 100
+        alpha = 0.05
+
+        icans = ICANS(ansatz, quantum_instance=backend, min_shots=min_shots, alpha=alpha,
+                      maxiter=maxiter, limit_learning_rate=True)
+
+        result_icans = icans1.compute_minimum_eigenvalue(hamiltonian)
+        print(result_icans)
+
+
+
+    References::
+
+        [1] Jonas M. Kübler, Andrew Arrasmith, Lukasz Cincio, Patrick J. Coles (2019).
+            An Adaptive Optimizer for Measurement-Frugal Variational Algorithms.
+            `arXiv:1909.09082 <https://arxiv.org/abs/1909.09083>`_
+        [2] Alberto Peruzzo, Et al. (2013). A variational eigenvalue solver on a quantum processor.
+            `arXiv:1304.3061 <https://arxiv.org/abs/1304.3061>`_
+
+    """
+
     def __init__(
         self,
         ansatz: Optional[QuantumCircuit] = None,
         initial_point: Optional[np.ndarray] = None,
-        # gradient: Optional[Union[GradientBase, Callable]] = None,
-        max_evals_grouped: int = 1,
         callback: Optional[Callable[[int, np.ndarray, float, float], None]] = None,
-        quantum_instance: Optional[Union[QuantumInstance, Backend]] = None,        
-        minShots: int =100,
+        quantum_instance: Optional[Union[QuantumInstance, Backend]] = None,
+        min_shots: int = 100,
         alpha: float = 0.01,
-        max_iterations: Optional[int] = 500,
-        L: Optional[float] = None,
-        mu: Optional[float]= 0.99,
-        b: Optional[float] = 0.000001,
-        limit_learning_rate: Optional[bool] = False
+        maxiter: int = 500,
+        lipschitz: Optional[float] = None,
+        mu: float = 0.99,
+        b: float = 1e-6,
+        limit_learning_rate: bool = False,
     ) -> None:
         super().__init__(
             ansatz=ansatz,
@@ -47,10 +121,10 @@ class ICANS(VQE):
             callback=callback,
             quantum_instance=quantum_instance,
         )
-        self.minShots = minShots
+        self.min_shots = min_shots
         self.alpha = alpha
-        self.max_iterations = max_iterations
-        self.L = L
+        self.maxiter = maxiter
+        self.lipschitz = lipschitz
         self.mu = mu
         self.b = b
         self.limit_learning_rate = limit_learning_rate
@@ -58,91 +132,80 @@ class ICANS(VQE):
     def single_shots(
         self, expectation: OperatorBase, values: np.ndarray, num_shots: int
     ) -> np.ndarray:
-        # set the number of shots to the current value
+        """Do single shots"""
+        # set the number of shots in the quantum instance
         self._circuit_sampler.quantum_instance.run_config.shots = num_shots
+
+        # evaluate the circuits
         param_dict = dict(zip(self.ansatz.parameters, values))
-        
+
         sampled = self._circuit_sampler.convert(expectation, params=param_dict)
+
         if not isinstance(sampled, SummedOp):
             sampled = SummedOp([sampled])
 
-        n_components = len(sampled.oplist)
-        out_Result = []
-        for comp in range(n_components):
+        # evaluate each Pauli group
+        out = []
+        for component in sampled.oplist:
             read_outs = []
-            composed = sampled.oplist[comp]
-            meas2 = composed[0]
-            samples2 = composed[1]
-            
-            for state, amplitude in samples2.primitive.items():
+            measurement, samples = component
+
+            for state, amplitude in samples.primitive.items():
                 occurence = int(np.round(num_shots * np.abs(amplitude) ** 2))
-                value = np.real(meas2.eval(state))
+                value = np.real(measurement.eval(state))
                 read_outs += occurence * [value]
-                
-            out_Result.append(read_outs)
-            
-        # Sum the components and return
-        out = np.array(out_Result)
-        
-        return np.array(out.sum(axis=0))
-    
+
+            out.append(read_outs)
+
+        return np.array(out).sum(axis=0)
+
     def compute_minimum_eigenvalue(
-        self,
-        operator
-    ):
-        # Execute optimization by iCans method
-        # limit_learning_rate = Falsefor iCans1, True for iCans2
+        self, operator: OperatorBase, aux_operators: Optional[ListOrDict[OperatorBase]] = None
+    ) -> MinimumEigensolverResult:
         start_time = time()
 
-        epsilon = np.pi / 2  # Parameter shift rule
-        
+        epsilon = np.pi / 2  # Parameter shift rule shifts the parameter values by pi/2
         n_param = self.ansatz.num_parameters
-        
+
         if self.initial_point is None:
             lst_param = 2 * np.pi * np.random.rand(n_param)
         else:
             lst_param = self.initial_point
 
-        # Define Lipschitz constant
-        if self.L is None:
-            # H = a*X + b*Z,
-            # where X and Z are Pauli operators. The eigenvalues of X and Z are both {1, -1}, so the eigenvalues of a*X are {a, -a} and of b*Z are {b, -b}. Then we compute the upper bound for the Lipschitz constant from Eq. (5) as
-            # L <= |a| + |b|.
-            # :: self.L = np.sum(np.abs(self.operator.coeffs))
-            self.L = np.sum(np.abs(operator.coeffs))
-        
+        if self.lipschitz is None:
+            lipschitz = np.sum(np.abs(operator.coeffs))
+        else:
+            lipschitz = self.lipschitz
+
         expectation = self.construct_expectation(self.ansatz.parameters, operator)
-        
-        shots = np.zeros(n_param) + self.minShots  # Initial vector with number of shots
-        self.hist_shots = np.zeros(self.max_iterations)  # historic total shots
-        self.hist_results = np.zeros(self.max_iterations)  # historic partial results        
-        xi = np.zeros(n_param)  # Variances
-        chi = np.zeros(n_param)  # gradients
-        gamma = np.zeros(n_param)  # Expected gain
-        
-        for k in range(self.max_iterations):
+        energy_evaluation = self.get_energy_evaluation(operator)
+
+        # algorithm variables (number of shots, estimated average, variance, ...)
+        shots = np.zeros(n_param) + self.min_shots
+        xi = np.zeros(n_param)
+        chi = np.zeros(n_param)
+        gamma = np.zeros(n_param)
+
+        # keep track of the total number of shots taken
+        shots_history = np.empty(self.maxiter)
+
+        for k in range(self.maxiter):
             grad = np.zeros(n_param)
             variances = np.zeros(n_param)
-            
+
             # Calculation of gradients
             for param in range(n_param):
                 parameters_plus = copy.deepcopy(lst_param)
                 parameters_plus[param] += epsilon
-                
-                results_plus = self.single_shots(
-                    expectation, parameters_plus, shots[param]
-                )
-                
+
+                results_plus = self.single_shots(expectation, parameters_plus, shots[param])
+
                 parameters_minus = copy.deepcopy(lst_param)
                 parameters_minus[param] -= epsilon
-                
-                results_minus = self.single_shots(
-                    expectation, parameters_minus, shots[param]
-                )
-                
-                grad[param] = (
-                    np.average(results_plus) - np.average(results_minus)
-                ) / 2
+
+                results_minus = self.single_shots(expectation, parameters_minus, shots[param])
+
+                grad[param] = (np.average(results_plus) - np.average(results_minus)) / 2
                 variances[param] = np.var(results_plus + results_minus)
 
             # Update gradient
@@ -150,70 +213,70 @@ class ICANS(VQE):
                 lst_param = lst_param - self.alpha * grad
             else:
                 alpha_bound = grad[param] ** 2 / (
-                        self.L
-                        * (
-                            grad[param] ** 2
-                            + variances[param] / shots[param]
-                            + self.b * self.mu**k
-                        )
-                    )
-                if self.alpha <=alpha_bound:
-                    lst_param = lst_param - self.alpha * grad
-                else:
-                    lst_param = lst_param - alpha_bound * grad                    
-            
-            # Saves the historic of results
-            self.hist_results[k] = np.average(results_plus)
+                    lipschitz
+                    * (grad[param] ** 2 + variances[param] / shots[param] + self.b * self.mu**k)
+                )
+                lst_param = lst_param - min(self.alpha, alpha_bound) * grad
+
+            if self.callback is not None:
+                # the energy evaluation function forwards the information to the callback
+                _ = energy_evaluation(lst_param)
+
             # Saves the accumulated shots
             if k >= 1:
-                self.hist_shots[k] = self.hist_shots[k - 1] + 2 * shots.sum()
+                shots_history[k] = shots_history[k - 1] + 2 * shots.sum()
             else:
-                self.hist_shots[k] = 2 * shots.sum()
+                shots_history[k] = 2 * shots.sum()
+
             # Update number of shots
-            for param, s in enumerate(shots):
+            for param, _ in enumerate(shots):
                 xi_aux = self.mu * xi[param] + (1 - self.mu) * variances[param]
                 chi_aux = self.mu * chi[param] + (1 - self.mu) * grad[param]
-                
+
                 xi[param] = xi_aux / (1 - self.mu ** (k + 1))
                 chi[param] = chi_aux / (1 - self.mu ** (k + 1))
-                
+
                 shots[param] = np.ceil(
                     2
-                    * self.L
+                    * lipschitz
                     * self.alpha
-                    / (2 - self.L * self.alpha)
+                    / (2 - lipschitz * self.alpha)
                     * xi[param]
                     / (chi[param] ** 2 + self.b * self.mu**k)
                 )
-                if shots[param] < self.minShots:
-                    shots[param] = self.minShots
-                    
+                if shots[param] < self.min_shots:
+                    shots[param] = self.min_shots
+
             # Expected return
             for i in range(n_param):
                 gamma[i] = (
-                    (self.alpha - self.L * self.alpha**2 / 2) * chi[i] ** 2
-                    - self.L * self.alpha**2 * xi[i] / (2 * shots[i])
+                    (self.alpha - lipschitz * self.alpha**2 / 2) * chi[i] ** 2
+                    - lipschitz * self.alpha**2 * xi[i] / (2 * shots[i])
                 ) / shots[i]
             idx = gamma.argmax(axis=0)
             smax = shots[idx]
-            
+
             # Clip shots
             for param in range(n_param):
                 if shots[param] > smax:
                     shots[param] = smax
-        # print("Historic number shots: ", hist_shots)
-        # print("Total shots: ", hist_shots.sum())
-        
+
         eval_time = time() - start_time
 
         result = VQEResult()
         result.optimal_point = lst_param
         result.optimal_parameters = dict(zip(self.ansatz.parameters, lst_param))
-        result.optimal_value = self.hist_results[self.max_iterations-1]
-        result.cost_function_evals = 2*n_param*self.max_iterations
+        result.optimal_value = energy_evaluation(lst_param)
+        result.cost_function_evals = 2 * n_param * self.maxiter
         result.optimizer_time = eval_time
-        result.eigenvalue =self.hist_results[self.max_iterations-1] + 0j
+        result.eigenvalue = result.optimal_value + 0j
         result.eigenstate = self._get_eigenstate(result.optimal_parameters)
-        result.total_shots = self.hist_shots[self.max_iterations-1]
+        result.total_shots = shots_history[-1]
+
+        # calculate the expectation values of the aux operators if they are provided
+        if aux_operators is not None:
+            bound = self.ansatz.assign_parameters(result.optimal_point)
+            aux_values = eval_observables(self.quantum_instance, bound, aux_operators, expectation)
+            result.aux_operator_eigenvalues = aux_values
 
         return result
