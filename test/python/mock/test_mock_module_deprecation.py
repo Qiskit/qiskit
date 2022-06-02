af0f11053e75e2a6e51b93a398a364ce773f9364
Junye Huang <h.jun.ye@gmail.com>
2022-06-02 11:09:46 +0200

    add deprecation test

---
+++ test/python/mock/test_mock_module_deprecation.py
@@ -0,0 +1,22 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Test for deprecation of qiskit.test.mock module."""
+from qiskit.test import QiskitTestCase
+
+
+class MockModuleDeprecationTest(QiskitTestCase):
+    """Test for deprecation of qiskit.test.mock module."""
+
+    def test_deprecated_mock_module(self):
+        with self.assertWarns(DeprecationWarning):
+            import qiskit.test.mock
