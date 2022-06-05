086661cf6e0539062327944bcb45407ae0c7ded2
John Lapeyre <jlapeyre@users.noreply.github.com>
2022-06-05 17:11:12 -0400

    Add copyright headers

--- qiskit/numba/maybe_numba.py
+++ qiskit/numba/maybe_numba.py
@@ -1,3 +1,20 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+Import numba if available, otherwise, dummy functions.
+"""
+
+
 def _noop_jit(f, *args, **kwargs):
     """A a decorator that does nothing"""
     return f
