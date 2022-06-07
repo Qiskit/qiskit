589531082a4baa8dd8d984401be015fe5e86da65
Frank Harkins <frankharkins@hotmail.co.uk>
2022-06-07 17:26:32 +0100

    Update docstring

--- qiskit/visualization/qcstyle.py
+++ qiskit/visualization/qcstyle.py
@@ -30,8 +30,8 @@ class DefaultStyle:
     `latex` output. The options available in the style dict are defined below:
 
     name (str): the name of the style. The name can be set to ``iqx``,
-        ``bw``, ``default``, or the name of a user-created json file. This
-        overrides the setting in the user config file (usually
+        ``textbook``, ``bw``, ``default``, or the name of a user-created json
+        file. This overrides the setting in the user config file (usually
         ``~/.qiskit/settings.conf``).
 
     textcolor (str): the color code to use for all text not inside a gate.
