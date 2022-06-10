ddbbf57f8ca7147f1c89ff7263bd0a8661c581e8
poig <43441314+poig@users.noreply.github.com>
2022-06-11 05:47:25 +0800

    Update decompose.py

--- qiskit/transpiler/passes/basis/decompose.py
+++ qiskit/transpiler/passes/basis/decompose.py
@@ -75,7 +75,7 @@ class Decompose(TransformationPass):
         )
         self.gates_to_decompose = value
 
-    def run(self, dag: DAGCircuit = None, reps: int = 1) -> DAGCircuit:
+    def run(self, dag: DAGCircuit, reps: int) -> DAGCircuit:
         """Run the Decompose pass on `dag`.
 
         Args:
@@ -86,7 +86,6 @@ class Decompose(TransformationPass):
             output dag where ``gate`` was expanded.
         """
         # Walk through the DAG and expand each non-basis node
-        global node
         for _ in range(reps):
             gates = []
             for node in dag.op_nodes():
