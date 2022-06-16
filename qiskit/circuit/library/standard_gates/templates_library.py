e8b67a98a44a699e0aab0abd7980e0894884ac8c
Maria Gragera <m.gragera.garces@gmail.com>
2022-06-16 20:16:34 +0100

    template lib clifford templates

---
+++ qiskit/circuit/library/standard_gates/templates_library.py
@@ -0,0 +1,510 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2017.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""Standard gates."""
+from qiskit.qasm import pi
+from qiskit.circuit.quantumcircuit import QuantumCircuit
+from qiskit.quantum_info.synthesis.ion_decompose import cnot_rxx_decompose
+
+from . import (
+    clifford_2_1,
+    clifford_2_2,
+    clifford_2_3,
+
+    clifford_6_5,
+
+)
+
+_sel = StandardTemplateLibrary = TemplateLibrary()
+
+#Clifford templates
+
+"""
+Clifford template 2_1:
+.. parsed-literal::
+
+        q_0: ─■──■─
+              │  │
+        q_1: ─■──■─
+"""
+
+def clifford_2_1():
+    """
+    Returns:
+        QuantumCircuit: template as a quantum circuit.
+    """
+    qc = QuantumCircuit(2)
+    qc.cz(0, 1)
+    qc.cz(0, 1)
+    return qc
+
+def_Clifford2_1 = clifford_2_1()
+
+_sel.add_template(clifford_2_1(), def_Clifford2_1)
+
+
+
+"""
+Clifford template 2_2:
+.. parsed-literal::
+
+        q_0: ──■────■──
+             ┌─┴─┐┌─┴─┐
+        q_1: ┤ X ├┤ X ├
+             └───┘└───┘
+"""
+
+def clifford_2_2():
+    """
+    Returns:
+        QuantumCircuit: template as a quantum circuit.
+    """
+    qc = QuantumCircuit(2)
+    qc.cx(0, 1)
+    qc.cx(0, 1)
+    return qc
+
+def_Clifford2_2 = clifford_2_2()
+
+_sel.add_template(clifford_2_2(), def_Clifford2_2)
+
+
+
+"""
+Clifford template 2_3:
+.. parsed-literal::
+             ┌───┐┌───┐
+        q_0: ┤ H ├┤ H ├
+             └───┘└───┘
+"""
+
+def clifford_2_3():
+    """
+    Returns:
+        QuantumCircuit: template as a quantum circuit.
+    """
+    qc = QuantumCircuit(1)
+    qc.h(0)
+    qc.h(0)
+    return qc
+
+def_Clifford2_3 = clifford_2_3()
+
+_sel.add_template(clifford_2_3(), def_Clifford2_3)
+
+
+
+"""
+Clifford template 2_4:
+.. parsed-literal::
+
+        q_0: ─X──X─
+              │  │
+        q_1: ─X──X─
+"""
+
+def clifford_2_4():
+    """
+    Returns:
+        QuantumCircuit: template as a quantum circuit.
+    """
+    qc = QuantumCircuit(2)
+    qc.swap(0, 1)
+    qc.swap(1, 0)
+    return qc
+
+def_Clifford2_4 = clifford_2_4()
+
+_sel.add_template(clifford_2_4(), def_Clifford2_4)
+
+
+
+"""
+Clifford template 3_1:
+.. parsed-literal::
+
+             ┌───┐┌───┐┌───┐
+        q_0: ┤ S ├┤ S ├┤ Z ├
+             └───┘└───┘└───┘
+"""
+
+def clifford_3_1():
+    """
+    Returns:
+        QuantumCircuit: template as a quantum circuit.
+    """
+    qc = QuantumCircuit(1)
+    qc.s(0)
+    qc.s(0)
+    qc.z(0)
+    return qc
+
+def_Clifford3_1 = clifford_3_1()
+
+_sel.add_template(clifford_3_1(), def_Clifford3_1)
+
+
+
+"""
+Clifford template 4_1:
+.. parsed-literal::
+
+                  ┌───┐
+        q_0: ──■──┤ X ├──■───X─
+             ┌─┴─┐└─┬─┘┌─┴─┐ │
+        q_1: ┤ X ├──■──┤ X ├─X─
+             └───┘     └───┘
+"""
+def clifford_4_1():
+    """
+    Returns:
+        QuantumCircuit: template as a quantum circuit.
+    """
+    qc = QuantumCircuit(2)
+    qc.cx(0, 1)
+    qc.cx(1, 0)
+    qc.cx(0, 1)
+    qc.swap(0, 1)
+    return qc
+
+def_Clifford4_1 = clifford_4_1()
+
+_sel.add_template(clifford_4_1(), def_Clifford4_1)
+
+
+
+"""
+Clifford template 4_2:
+.. parsed-literal::
+
+        q_0: ───────■────────■─
+             ┌───┐┌─┴─┐┌───┐ │
+        q_1: ┤ H ├┤ X ├┤ H ├─■─
+             └───┘└───┘└───┘
+"""
+
+def clifford_4_2():
+    """
+    Returns:
+        QuantumCircuit: template as a quantum circuit.
+    """
+    qc = QuantumCircuit(2)
+    qc.h(1)
+    qc.cx(0, 1)
+    qc.h(1)
+    qc.cz(0, 1)
+    return qc
+
+def_Clifford4_2 = clifford_4_2()
+
+_sel.add_template(clifford_4_2(), def_Clifford4_2)
+
+
+
+"""
+Clifford template 4_3:
+.. parsed-literal::
+
+             ┌───┐     ┌─────┐
+        q_0: ┤ S ├──■──┤ SDG ├──■──
+             └───┘┌─┴─┐└─────┘┌─┴─┐
+        q_1: ─────┤ X ├───────┤ X ├
+                  └───┘       └───┘
+"""
+def clifford_4_3():
+    """
+    Returns:
+        QuantumCircuit: template as a quantum circuit.
+    """
+    qc = QuantumCircuit(2)
+    qc.s(0)
+    qc.cx(0, 1)
+    qc.sdg(0)
+    qc.cx(0, 1)
+    return qc
+
+def_Clifford4_3 = clifford_4_3()
+
+_sel.add_template(clifford_4_3(), def_Clifford4_3)
+
+
+
+"""
+Clifford template 4_4:
+.. parsed-literal::
+
+             ┌───┐   ┌─────┐
+        q_0: ┤ S ├─■─┤ SDG ├─■─
+             └───┘ │ └─────┘ │
+        q_1: ──────■─────────■─
+"""
+def clifford_4_4():
+    """
+    Returns:
+        QuantumCircuit: template as a quantum circuit.
+    """
+    qc = QuantumCircuit(2)
+    qc.s(0)
+    qc.cz(0, 1)
+    qc.sdg(0)
+    qc.cz(0, 1)
+    return qc
+
+def_Clifford4_4 = clifford_4_4()
+
+_sel.add_template(clifford_4_4(), def_Clifford4_4)
+
+
+"""
+Clifford template 5_1:
+.. parsed-literal::
+
+        q_0: ──■─────────■─────────■──
+             ┌─┴─┐     ┌─┴─┐       │
+        q_1: ┤ X ├──■──┤ X ├──■────┼──
+             └───┘┌─┴─┐└───┘┌─┴─┐┌─┴─┐
+        q_2: ─────┤ X ├─────┤ X ├┤ X ├
+                  └───┘     └───┘└───┘
+"""
+
+def clifford_5_1():
+    """
+    Returns:
+        QuantumCircuit: template as a quantum circuit.
+    """
+    qc = QuantumCircuit(3)
+    qc.cx(0, 1)
+    qc.cx(1, 2)
+    qc.cx(0, 1)
+    qc.cx(1, 2)
+    qc.cx(0, 2)
+    return qc
+
+def_Clifford5_1 = clifford_5_1()
+
+_sel.add_template(clifford_5_1(), def_Clifford5_1)
+
+
+
+"""
+Clifford template 6_2:
+.. parsed-literal::
+
+             ┌───┐
+        q_0: ┤ S ├──■───────────■───■─
+             ├───┤┌─┴─┐┌─────┐┌─┴─┐ │
+        q_1: ┤ S ├┤ X ├┤ SDG ├┤ X ├─■─
+             └───┘└───┘└─────┘└───┘
+"""
+
+def clifford_6_2():
+    """
+    Returns:
+        QuantumCircuit: template as a quantum circuit.
+    """
+    qc = QuantumCircuit(2)
+    qc.s(0)
+    qc.s(1)
+    qc.cx(0, 1)
+    qc.sdg(1)
+    qc.cx(0, 1)
+    qc.cz(0, 1)
+    return qc
+
+def_Clifford6_2 = clifford_6_2()
+
+_sel.add_template(clifford_6_2(), def_Clifford6_2)
+
+
+
+"""
+Clifford template 6_3:
+.. parsed-literal::
+
+                   ┌───┐     ┌───┐
+        q_0: ─X──■─┤ H ├──■──┤ X ├─────
+              │  │ └───┘┌─┴─┐└─┬─┘┌───┐
+        q_1: ─X──■──────┤ X ├──■──┤ H ├
+                        └───┘     └───┘
+"""
+def clifford_6_3():
+    """
+    Returns:
+        QuantumCircuit: template as a quantum circuit.
+    """
+    qc = QuantumCircuit(2)
+    qc.swap(0, 1)
+    qc.cz(0, 1)
+    qc.h(0)
+    qc.cx(0, 1)
+    qc.cx(1, 0)
+    qc.h(1)
+    return qc
+
+def_Clifford6_3 = clifford_6_3()
+
+_sel.add_template(clifford_6_3(), def_Clifford6_3)
+
+
+
+"""
+Clifford template 6_4:
+.. parsed-literal::
+
+             ┌───┐┌───┐┌───┐┌───┐┌───┐┌───┐
+        q_0: ┤ S ├┤ H ├┤ S ├┤ H ├┤ S ├┤ H ├
+             └───┘└───┘└───┘└───┘└───┘└───┘
+"""
+
+def clifford_6_4():
+    """
+    Returns:
+        QuantumCircuit: template as a quantum circuit.
+    """
+    qc = QuantumCircuit(1)
+    qc.s(0)
+    qc.h(0)
+    qc.s(0)
+    qc.h(0)
+    qc.s(0)
+    qc.h(0)
+    return qc
+
+def_Clifford6_4 = clifford_6_4()
+
+_sel.add_template(clifford_6_4(), def_Clifford6_4)
+
+
+"""
+Clifford template 6_5:
+.. parsed-literal::
+
+                      ┌───┐
+        q_0: ─■───■───┤ S ├───■───────
+              │ ┌─┴─┐┌┴───┴┐┌─┴─┐┌───┐
+        q_1: ─■─┤ X ├┤ SDG ├┤ X ├┤ S ├
+                └───┘└─────┘└───┘└───┘
+"""
+
+def clifford_6_5():
+    """
+    Returns:
+        QuantumCircuit: template as a quantum circuit.
+    """
+    qc = QuantumCircuit(2)
+    qc.cz(0, 1)
+    qc.cx(0, 1)
+    qc.s(0)
+    qc.sdg(1)
+    qc.cx(0, 1)
+    qc.s(1)
+    return qc
+
+def_Clifford6_5 = clifford_6_5()
+
+_sel.add_template(clifford_6_5(), def_Clifford6_5)
+
+
+
+"""
+Clifford template 8_1:
+.. parsed-literal::
+
+                       ┌───┐ ┌───┐ ┌───┐┌─────┐
+        q_0: ──■───────┤ X ├─┤ S ├─┤ X ├┤ SDG ├
+             ┌─┴─┐┌───┐└─┬─┘┌┴───┴┐└─┬─┘└┬───┬┘
+        q_1: ┤ X ├┤ H ├──■──┤ SDG ├──■───┤ H ├─
+             └───┘└───┘     └─────┘      └───┘
+"""
+
+def clifford_8_1():
+    """
+    Returns:
+        QuantumCircuit: template as a quantum circuit.
+    """
+    qc = QuantumCircuit(2)
+    qc.cx(0, 1)
+    qc.h(1)
+    qc.cx(1, 0)
+    qc.s(0)
+    qc.sdg(1)
+    qc.cx(1, 0)
+    qc.sdg(0)
+    qc.h(1)
+    return qc
+
+def_Clifford8_1 = clifford_8_1()
+
+_sel.add_template(clifford_8_1(), def_Clifford8_1)
+
+
+
+"""
+Clifford template 8_2:
+.. parsed-literal::
+
+                             ┌───┐
+        q_0: ──■─────────■───┤ S ├───■────────────
+             ┌─┴─┐┌───┐┌─┴─┐┌┴───┴┐┌─┴─┐┌───┐┌───┐
+        q_1: ┤ X ├┤ H ├┤ X ├┤ SDG ├┤ X ├┤ S ├┤ H ├
+             └───┘└───┘└───┘└─────┘└───┘└───┘└───┘
+"""
+
+def clifford_8_2():
+    """
+    Returns:
+        QuantumCircuit: template as a quantum circuit.
+    """
+    qc = QuantumCircuit(2)
+    qc.cx(0, 1)
+    qc.h(1)
+    qc.cx(0, 1)
+    qc.s(0)
+    qc.sdg(1)
+    qc.cx(0, 1)
+    qc.s(1)
+    qc.h(1)
+    return qc
+
+def_Clifford8_2 = clifford_8_2()
+
+_sel.add_template(clifford_8_2(), def_Clifford8_2)
+
+"""
+Clifford template 8_3:
+.. parsed-literal::
+
+        q_0: ─────────────────■───────────────────────■──
+             ┌───┐┌───┐┌───┐┌─┴─┐┌─────┐┌───┐┌─────┐┌─┴─┐
+        q_1: ┤ S ├┤ H ├┤ S ├┤ X ├┤ SDG ├┤ H ├┤ SDG ├┤ X ├
+             └───┘└───┘└───┘└───┘└─────┘└───┘└─────┘└───┘
+"""
+
+def clifford_8_3():
+    """
+    Returns:
+        QuantumCircuit: template as a quantum circuit.
+    """
+    qc = QuantumCircuit(2)
+    qc.s(1)
+    qc.h(1)
+    qc.s(1)
+    qc.cx(0, 1)
+    qc.sdg(1)
+    qc.h(1)
+    qc.sdg(1)
+    qc.cx(0, 1)
+    return qc
+
+def_Clifford8_3 = clifford_8_3()
+
+_sel.add_template(clifford_8_3(), def_Clifford8_3)
