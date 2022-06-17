027805516c12f1e9ff997540a058f056339bcdc6
Matthew Treinish <mtreinish@kortar.org>
2022-06-17 15:07:23 -0400

    Set version number to 0.21.0rc1 for first release candidate
    For the 0.21.0 release we're going to start pushing release candidates
    prior to the release to enable testing before we cut the final release.
    In preparation for tagging the first release candidate this commit
    updates the version string to indicate it's a release candidate.

--- docs/conf.py
+++ docs/conf.py
@@ -21,9 +21,9 @@ copyright = "2019, Qiskit Development Team"  # pylint: disable=redefined-builtin
 author = "Qiskit Development Team"
 
 # The short X.Y version
-version = ""
+version = "0.21"
 # The full version, including alpha/beta/rc tags
-release = "0.21.0"
+release = "0.21.0rc1"
 
 extensions = [
     "sphinx.ext.napoleon",
