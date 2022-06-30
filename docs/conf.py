0f377f7a2cdbd7eaa46e8e2b5de974c8c22b9612
Matthew Treinish <mtreinish@kortar.org>
2022-06-30 12:22:21 -0400

    Prepare 0.21.0 release (#8229)
    * Prepare 0.21.0 release
    
    This commit prepares the 0.21.0 release. We've released a release
    candidate already. This commit moves the release notes to a 0.21
    directory to organize the release notes directory and sets the version
    number to 0.21.0 final to prepare for release.
    
    * Add prelude to release notes
    
    * Apply suggestions from code review
    
    Co-authored-by: Jake Lishman <jake@binhbar.com>
    
    Co-authored-by: Jake Lishman <jake@binhbar.com>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = "Qiskit Development Team"
 # The short X.Y version
 version = "0.21"
 # The full version, including alpha/beta/rc tags
-release = "0.21.0rc1"
+release = "0.21.0"
 
 extensions = [
     "sphinx.ext.napoleon",
