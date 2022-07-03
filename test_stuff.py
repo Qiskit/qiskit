0924d627b3f53413cbd13c07ecd2fb26e8dafb17
AlexanderIvrii <alexi@il.ibm.com>
2022-07-03 16:40:22 +0300

    Improving external test file

--- test_stuff.py
+++ test_stuff.py
@@ -1,32 +1,28 @@
 import cProfile, pstats
 import time
-import numpy as np
-from functools import lru_cache
-
 import qiskit.circuit.library
 from qiskit import QuantumCircuit, transpile
 from qiskit.converters import circuit_to_dagdependency
 from qiskit.dagcircuit import DAGCircuit, DAGDependency
 from qiskit.transpiler import PassManager
-from qiskit.transpiler.passes import CommutationAnalysis, CommutativeCancellation, InverseCancellation
+from qiskit.transpiler.passes import CommutationAnalysis, CommutativeCancellation, InverseCancellation, \
+    TemplateOptimization
 from qiskit.transpiler.passes.optimization.commutative_inverse_cancellation import CommutativeInverseCancellation
 from test.python.quantum_info.operators.symplectic.test_clifford import random_clifford_circuit
-
 from qiskit.dagcircuit.dagdependency import _does_commute
 from qiskit.circuit.library import Permutation, LinearFunction, CXGate, HGate, SGate, SdgGate, SwapGate, CZGate, ZGate, XGate, YGate
 from qiskit.quantum_info import Operator
 from qiskit.transpiler.passes.optimization.commutation_analysis import _commute
 from qiskit.quantum_info.operators import Clifford
+from qiskit.circuit.library.templates import *
 
 
-def construct_dd(qc):
-    dd = circuit_to_dagdependency(qc)
-    return dd
+# =====================================
+# CIRCUIT OPTIMIZATION
+# =====================================
 
-    # gates = [ "s", "sdg", "]
-
-def run_experiment(qc, is_cliff=True):
-    # print("=====> RUNNING EXPERIMENT <=====")
+def optimize_circuit_aux(qc, is_cliff=True):
+    # print("=====> RUNNING CIRCUIT OPTIMIZATION  <=====")
 
     gates_to_cancel = [CXGate(), HGate(), SwapGate(), CZGate(), ZGate(), XGate(), YGate()]
 
@@ -114,14 +110,21 @@ def run_experiment(qc, is_cliff=True):
     print("")
 
 
-def test_clifford(num_qubits, num_gates):
+def optimize_aux(num_qubits, num_gates, seed):
     gates = ["x", "y", "z", "h", "s", "sdg", "cx", "cz", "swap"]
-    for seed in range(10):
-        qc = random_clifford_circuit(num_qubits, num_gates, gates=gates, seed=seed)
-        run_experiment(qc)
+    qc = random_clifford_circuit(num_qubits, num_gates, gates=gates, seed=seed)
+    optimize_circuit_aux(qc)
 
 
-def test_circ1():
+def optimize():
+    optimize_aux(10, 1000, 0)
+    optimize_aux(10, 5000, 0)
+    optimize_aux(50, 1000, 0)
+    optimize_aux(50, 5000, 0)
+
+
+
+def optimize_circ1():
     print(f"==========RUNNING EXPERIMENT 1")
     qc = QuantumCircuit(2)
     qc.z(0)
@@ -130,11 +133,10 @@ def test_circ1():
     qc.z(0)
     qc.x(1)
     print(qc)
-    run_experiment(qc)
+    optimize_circuit_aux(qc)
 
 
-
-def test_circ2():
+def optimize_circ2():
     print(f"========RUNNING EXPERIMENT 2")
     qc = QuantumCircuit(2)
     qc.h(0)
@@ -143,17 +145,103 @@ def test_circ2():
     qc.h(0)
     qc.h(1)
     print(qc)
-    run_experiment(qc)
+    optimize_circuit_aux(qc)
+
+
+# =====================================
+# DAG DEPENDENCY
+# =====================================
+
+def time_construct_dag_dependency_aux(num_qubits, num_gates, seed):
+    gates = ["x", "y", "z", "h", "s", "sdg", "cx", "cz", "swap"]
+    qc = random_clifford_circuit(num_qubits, num_gates, gates=gates, seed=seed)
+    start_time = time.time()
+    dag_dependency = circuit_to_dagdependency(qc)
+    end_time = time.time()
+    print(f"DagDependency: #qubits = {num_qubits}, #gates = {num_gates}, seed = {seed}, #edges = {len(dag_dependency.get_all_edges())}, depth = {dag_dependency.depth()}, time = {end_time - start_time:.4f}")
+
+
+def time_construct_dag_dependency():
+    time_construct_dag_dependency_aux(5, 100, seed=0)
+    time_construct_dag_dependency_aux(5, 1000, seed=0)
+    time_construct_dag_dependency_aux(5, 5000, seed=0)
+    time_construct_dag_dependency_aux(10, 100, seed=0)
+    time_construct_dag_dependency_aux(10, 1000, seed=0)
+    time_construct_dag_dependency_aux(10, 5000, seed=0)
+
+
+from memory_profiler import profile
+
+@profile
+def memory_construct_dag_dependency_aux(num_qubits, num_gates, seed):
+    gates = ["x", "y", "z", "h", "s", "sdg", "cx", "cz", "swap"]
+    qc = random_clifford_circuit(num_qubits, num_gates, gates=gates, seed=seed)
+    dag_dependency = circuit_to_dagdependency(qc)
+    print(f"DagDependency: #qubits = {num_qubits}, #gates = {num_gates}, seed = {seed}, #edges = {len(dag_dependency.get_all_edges())}")
+
+
+def memory_construct_dag_dependency():
+    memory_construct_dag_dependency_aux(5, 100, seed=0)
+    memory_construct_dag_dependency_aux(5, 1000, seed=0)
+    memory_construct_dag_dependency_aux(5, 5000, seed=0)
+    memory_construct_dag_dependency_aux(10, 100, seed=0)
+    memory_construct_dag_dependency_aux(10, 1000, seed=0)
+    memory_construct_dag_dependency_aux(10, 5000, seed=0)
+
+
+# =====================================
+# TEMPLATE OPTIMIZATION
+# =====================================
+
+def time_template_optimization_aux(num_qubits, num_gates, seed):
+    gates = ["x", "y", "z", "h", "s", "sdg", "cx", "cz", "swap"]
+    qc = random_clifford_circuit(num_qubits, num_gates, gates=gates, seed=seed)
+    # ops = qc.count_ops()
+    # print(f"#gates = {sum([ops[x] for x in ops])}")
+
+    # All Clifford templates without S*H*S*H*S*H = Id, since this only holds *up to a phase*.
+    template_list = [clifford_2_1(),
+                     clifford_2_2(),
+                     clifford_2_3(),
+                     clifford_2_4(),
+                     clifford_3_1(),
+                     clifford_4_1(),
+                     clifford_4_2(),
+                     clifford_4_3(),
+                     clifford_4_4(),
+                     clifford_5_1(),
+                     clifford_6_1(),
+                     clifford_6_2(),
+                     clifford_6_3(),
+                     # clifford_6_4(),
+                     clifford_6_5(),
+                     clifford_8_1(),
+                     clifford_8_2(),
+                     clifford_8_3()]
+
+    pm = PassManager(TemplateOptimization(template_list=template_list))
+    start_time = time.time()
+    qc2 = pm.run(qc)
+    end_time = time.time()
+    ops = qc2.count_ops()
+    # print(f"#gates = {sum([ops[x] for x in ops])}")
+    print(f"Templates: #qubits = {num_qubits}, #gates = {num_gates}, seed = {seed}, #gates_opt = {sum([ops[x] for x in ops])}, time = {end_time - start_time:.4f}")
+
+
+def time_template_optimization():
+    time_template_optimization_aux(5, 100, seed=0)
+    time_template_optimization_aux(5, 1000, seed=0)
+    time_template_optimization_aux(10, 100, seed=0)
+    time_template_optimization_aux(10, 1000, seed=0)
+
 
 
 if __name__ == "__main__":
-    test_clifford(10, 5000)
-    print("")
+    # time_construct_dag_dependency()
+    # memory_construct_dag_dependency()
+    # time_template_optimization()
+    optimize()
 
-    # test_clifford(10, 5000)
-    # test_clifford(50, 1000)
-    # test_clifford(10, 1000)
-    #test_circ1()
-    # test_circ2()
+
 
 
