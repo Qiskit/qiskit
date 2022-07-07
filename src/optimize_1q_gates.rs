c73c812c0841aedbaa366e72a13ad699a7609f99
aeddins-ibm <60495383+aeddins-ibm@users.noreply.github.com>
2022-07-07 09:53:51 -0700

    rust docstring formatting
    Co-authored-by: Matthew Treinish <mtreinish@kortar.org>

--- src/optimize_1q_gates.rs
+++ src/optimize_1q_gates.rs
@@ -16,15 +16,15 @@ use pyo3::Python;
 
 const PI: f64 = std::f64::consts::PI;
 
-//     Return a triple theta, phi, lambda for the product.
-//
-//         u3(theta, phi, lambda)
-//            = u3(theta1, phi1, lambda1).u3(theta2, phi2, lambda2)
-//            = Rz(phi1).Ry(theta1).Rz(lambda1+phi2).Ry(theta2).Rz(lambda2)
-//            = Rz(phi1).Rz(phi').Ry(theta').Rz(lambda').Rz(lambda2)
-//            = u3(theta', phi1 + phi', lambda2 + lambda')
-//
-//         Return theta, phi, lambda.
+///     Return a triple theta, phi, lambda for the product.
+///
+///         u3(theta, phi, lambda)
+///            = u3(theta1, phi1, lambda1).u3(theta2, phi2, lambda2)
+///            = Rz(phi1).Ry(theta1).Rz(lambda1+phi2).Ry(theta2).Rz(lambda2)
+///            = Rz(phi1).Rz(phi').Ry(theta').Rz(lambda').Rz(lambda2)
+///            = u3(theta', phi1 + phi', lambda2 + lambda')
+///
+///         Return theta, phi, lambda.
 #[pyfunction]
 #[pyo3(text_signature = "(theta1, phi1, lambda1, theta2, phi2, lambda2, /)")]
 pub fn compose_u3_rust(
