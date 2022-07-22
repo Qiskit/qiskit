3f03011f2bce1c711a807ffc418b446662e94486
MarcDrudis <MarcSanzDrudis@outlook.com>
2022-07-22 16:34:24 +0200

    changes

--- qiskit/algorithms/evolvers/classical_real_evolver.py
+++ qiskit/algorithms/evolvers/classical_real_evolver.py
@@ -12,13 +12,12 @@
 
 """Interface for Classical Quantum Real Time Evolution."""
 
+from multiprocessing.sharedctypes import Value
 import scipy.sparse as sp
-from scipy.sparse.linalg import bicg,norm
+from scipy.sparse.linalg import bicg, norm
 import numpy as np
-import time
+from typing import Tuple, Optional
 
-from qiskit.quantum_info import Pauli
-from qiskit.opflow.primitive_ops.pauli_op import PauliOp
 from qiskit.opflow import StateFn
 
 
@@ -30,13 +29,21 @@ from .real_evolver import RealEvolver
 class ClassicalRealEvolver(RealEvolver):
     """Interface for Quantum Real Time Evolution."""
 
-    def __init__(self, timesteps: int = 100):
+    def __init__(self, timesteps: Optional[int]  = None, threshold: Optional[float] = 1e-4):
         """
         Args:
             timesteps: The number of timesteps to perform for the time evolution.
-        """
-        self.timesteps = timesteps
+            threshold: The threshold for the error. If timesteps is `None` this will be used
+            to estimate the necessary number of timesteps to reach a threshold error.
 
+        Raises:
+            ValueError: If timesteps and threshold are `None`.
+        """
+        if timesteps is None and threshold is None:
+            raise ValueError("Either timesteps or threshold must be specified.")
+
+        self.timesteps = timesteps
+        self.threshold = threshold
 
     def evolve(self, evolution_problem: EvolutionProblem) -> EvolutionResult:
         r"""Perform real time evolution :math:`\exp(-i t H)|\Psi\rangle`.
@@ -71,45 +78,71 @@ class ClassicalRealEvolver(RealEvolver):
             Evolution result which includes an evolved quantum state.
         """
 
-        state, lhs_operator, rhs_operator = self._start(evolution_problem=evolution_problem)
+        state, lhs_operator, rhs_operator, timesteps = self._start(evolution_problem=evolution_problem)
 
-        for ts in range(self.timesteps):
+        for _ in range(timesteps):
             state = self._step(state, lhs_operator, rhs_operator)
 
-        return EvolutionResult(
-            evolved_state=StateFn(state),
-            aux_ops_evaluated = None
-        )
+        return EvolutionResult(evolved_state=StateFn(state), aux_ops_evaluated=None)
 
-    def _start(self,evolution_problem: EvolutionProblem):
+    def _start(
+        self, evolution_problem: EvolutionProblem
+    ) -> Tuple[np.ndarray, sp.csr_matrix, sp.csr_matrix]:
         """Returns a tuple with the initial state as an array and the operators needed for time
-         evolution as sparse matrices."""
+         evolution as sparse matrices.
+
+        Args:
+            evolution_problem: The definition of the evolution problem.
+
+        Returns:
+            A tuple with the initial state as an array and the operators needed for time
+            evolution as sparse matrices.
+        """
         state = evolution_problem.initial_state.to_matrix(massive=True).transpose()
         hamiltonian = evolution_problem.hamiltonian.to_spmatrix()
+        if self.timesteps is None:
+            timesteps = self._ntimesteps(time = evolution_problem.time, hamiltonian = hamiltonian, threshold = self.threshold)
+            print(timesteps)
+        else:
+            timesteps = self.timesteps
+        timestep = evolution_problem.time / timesteps
 
-        timestep = evolution_problem.time / self.timesteps
-
-        idnty = sp.identity(
-            hamiltonian.shape[0], format="csr"
-        )  # What would be the best format for this?
+        idnty = sp.identity(hamiltonian.shape[0], format="csr")
 
         lhs_operator = idnty + 1j * timestep / 2 * hamiltonian
         rhs_operator = idnty - 1j * timestep / 2 * hamiltonian
 
-        return state, lhs_operator, rhs_operator
+        return state, lhs_operator, rhs_operator, timesteps
 
-
-    def _ntimesteps(self, time, hamiltonian, threshold):
+    def minimal_number_steps(self, norm_hamiltonian: float, time: float, threshold = 1e-4) -> int:
         """Calculate the timestep for the given time and threshold.
 
         we use the fact that the taylor expansion term of third order in our expansion would be
         :math:`\frac{(-i H \delta t)^3}{12} ` to compute an approximation for how many timesteps
         we need to reach a certain precision.
-        """
-        hnorm = norm(hamiltonian, ord = np.inf)
-        return int(np.power(hnorm * time , 3/2) * np.power(12 * threshold, -1/2))
 
-    def _step(self, state, lhs_operator, rhs_operator):
+        Args:
+            time: The time to evolve.
+            norm_hamiltonian: The operator norm of the Hamiltonian if known or an estimation of
+                             it (For example the infinity norm of the Hamiltonian). This value
+                             will be associated with the error of the Hamiltonian.
+            threshold: The threshold for the error.
+
+        Returns:
+            The number of timesteps needed to reach the error threshold.
+        """
+        return int(np.power(norm_hamiltonian * time, 3 / 2) * np.power(12 * threshold, -1 / 2)) + 1
+
+    def _ntimesteps(self, time: float, hamiltonian: sp.csr_matrix, threshold: float = 1e-4) -> int:
+        """Calculate the number of timesteps needed to reach the threshold error if the user doesn't
+        indicate the number of timesteps.
+
+        Uses the infinity norm to estimate the operator norm of the Hamiltonian.
+        """
+        hnorm = norm(hamiltonian, ord=np.inf)
+        return self.minimal_number_steps(norm_hamiltonian=hnorm, time=time, threshold=threshold)
+
+    def _step(self, state: np.ndarray , lhs_operator: sp.csr_matrix, rhs_operator: sp.csr_matrix) -> np.ndarray:
         """ "Perform one timestep of the evolution.
 
         Args:
@@ -121,9 +154,8 @@ class ClassicalRealEvolver(RealEvolver):
             The evolved state.
         """
 
-
         rhs = rhs_operator.dot(state)
-        ev_state, exitcode = bicg(A=lhs_operator, b=rhs, atol=1e-8,x0 = state)
+        ev_state, exitcode = bicg(A=lhs_operator, b=rhs, atol=1e-8, x0=state)
         if exitcode != 0:
             raise RuntimeError("Failure!")
         return ev_state
--- test/python/algorithms/evolvers/test_classical_real_evolver.py
+++ test/python/algorithms/evolvers/test_classical_real_evolver.py
@@ -11,19 +11,13 @@
 # that they have been altered from the originals.
 
 """Test Classical Real Evolver."""
-from math import trunc
 import unittest
 import numpy as np
-import scipy.sparse as sp
 from qiskit.algorithms.evolvers.classical_real_evolver import ClassicalRealEvolver
 from test.python.algorithms import QiskitAlgorithmsTestCase
 from ddt import data, ddt, unpack
-from numpy.testing import assert_raises
-
-from qiskit.quantum_info import Statevector
 from qiskit.algorithms.evolvers.evolution_problem import EvolutionProblem
-from qiskit.circuit import Parameter
-from qiskit.opflow import Y, Z, I, One, X, Zero, VectorStateFn, StateFn, SummedOp, PauliSumOp
+from qiskit.opflow import Y, Z, I, One, X, Zero
 import time
 
 
@@ -33,94 +27,31 @@ class TestClassicalRealEvolver(QiskitAlgorithmsTestCase):
 
     @data(
         [One, np.pi / 2, X, Zero, -1.0j],
-        [One ^ Zero, np.pi/2 , ((X ^ X) + (Y^Y)) / 2, Zero ^ One, -1.0j],
+        [One ^ Zero, np.pi / 2, ((X ^ X) + (Y ^ Y)) / 2, Zero ^ One, -1.0j],
     )
-
     @unpack
     def test_evolve(self, initial_state, time, hamiltonian, expected_state, phase):
-        evolution_problem = EvolutionProblem(
-            hamiltonian, time, initial_state, truncation_threshold=1e-8
-        )
-        classic_evolver = ClassicalRealEvolver()
+        evolution_problem = EvolutionProblem(hamiltonian, time, initial_state)
+        classic_evolver = ClassicalRealEvolver(timesteps=None, threshold = 1e-5)
+        # recomended_steps = classic_evolver.minimal_number_steps(1.0,time, 1e-4)
+        # print(recomended_steps)
+        # classic_evolver.timesteps = recomended_steps
         result = classic_evolver.evolve(evolution_problem)
-        np.testing.assert_almost_equal(
-            result.evolved_state.to_matrix(), phase * expected_state.to_matrix(), decimal=5
-        )
 
-    def test_ising_quick(self):
-        num_sites = 25
-        J = 0.1
-        g = -1.0
-        t = 1.0
-
-        zz = ["I" * i + "ZZ" + "I" * (num_sites - i - 2) for i in range(num_sites - 1)]
-        x = ["I" * i + "X" + "I" * (num_sites - i - 1) for i in range(num_sites)]
-        hamiltonian = PauliSumOp.from_list(list(zip(zz, len(zz) * [J])) + list(zip(x, len(x) * [g])))
-        initial_state = np.zeros(2 ** num_sites, dtype=np.complex128)
-        initial_state[0] = 1.0
-        initial_state = VectorStateFn(initial_state)
-
-        evolution_problem = EvolutionProblem(hamiltonian,t,initial_state,truncation_threshold=1e-5)
-        classic_evolver = ClassicalRealEvolver()
-
-        start_time_s = time.time()
-        state = evolution_problem.initial_state.to_matrix(massive=True).transpose()
-        start_time = time.time()
-        print(f"Time to create the state: {start_time - start_time_s:.3}")
-
-        hamiltonian = hamiltonian.primitive.to_matrix(sparse=True)
-
-        end_time = time.time()
-
-        print("Time to convert to matrix: ", end_time - start_time)
-
-        ntimesteps = 10
-        timestep = evolution_problem.time / ntimesteps
-
-        start_time = time.time()
-        idnty = sp.identity(
-            hamiltonian.shape[0], format="csr"
-        )  # What would be the best format for this?
-        end_time = time.time()
-        print("Identity matrix creation time: ", end_time - start_time)
-
-        lhs_operator = idnty + 1j * timestep / 2 * hamiltonian
-        rhs_operator = idnty - 1j * timestep / 2 * hamiltonian
-
-
-        end_time_s = time.time()
-        print(f"It takes {end_time_s- start_time_s :.3} seconds to initialize the system.")
-
-        start_time = time.time()
-        classic_evolver._step(state, lhs_operator, rhs_operator)
-        end_time = time.time()
-        print(f"It takes {end_time- start_time :.3} seconds to make a step.")
-
-
-    def test_ising(self):
-        num_sites = 18
-        J = 0.1
-        g = -1.0
-        time = 1.0
-
-        zz = ["I" * i + "ZZ" + "I" * (num_sites - i - 2) for i in range(num_sites - 1)]
-        x = ["I" * i + "X" + "I" * (num_sites - i - 1) for i in range(num_sites)]
-        hamiltonian = PauliSumOp.from_list(list(zip(zz, len(zz) * [J])) + list(zip(x, len(x) * [g])))
-        initial_state = np.zeros(2 ** num_sites, dtype=np.complex128)
-        initial_state[0] = 1.0
-        initial_state = VectorStateFn(initial_state)
-
-        evolution_problem = EvolutionProblem(hamiltonian,time,initial_state,truncation_threshold=1e-5)
-        classic_evolver = ClassicalRealEvolver()
-        result = classic_evolver.evolve(evolution_problem)
-        print(result.evolved_state)
-
-        # #Verify with exact solution.
-        # sp_hamiltonian = hamiltonian.to_spmatrix()
-        # print(sp_hamiltonian.shape)
-        # evol_operator = sp.linalg.expm(-1j * time * sp_hamiltonian)
-        # evol_state = evol_operator.dot( initial_state.to_matrix())
-        # np.testing.assert_allclose(result.evolved_state.to_matrix(), evol_state, rtol = 1e-5, atol = 0)
+        with self.subTest("Amplitudes"):
+            np.testing.assert_allclose(
+                np.absolute(result.evolved_state.to_matrix()),
+                np.absolute(phase * expected_state.to_matrix()),
+                atol=1e-7,
+                rtol=0,
+            )
+        with self.subTest("Phases"):
+            np.testing.assert_allclose(
+                np.angle(result.evolved_state.to_matrix()),
+                np.angle(phase * expected_state.to_matrix()),
+                atol=1e-7,
+                rtol=0,
+            )
 
 
 if __name__ == "__main__":
