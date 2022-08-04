12b00c43c4fe139731c38de3037dce316187eee4
Junye Huang <h.jun.ye@gmail.com>
2022-08-04 10:55:39 +0200

    unwrap comments and codes, remove spaces around = in function arguments

--- qiskit/visualization/state_visualization.py
+++ qiskit/visualization/state_visualization.py
@@ -714,8 +714,7 @@ def plot_state_qsphere(
 
         .. jupyter-execute::
 
-           # You can show the phase of each state and use
-           # degrees instead of radians
+           # You can show the phase of each state and use degrees instead of radians
 
            from qiskit.quantum_info import DensityMatrix
            import numpy as np
@@ -728,8 +727,7 @@ def plot_state_qsphere(
            qc.z(1)
 
            matrix = DensityMatrix(qc)
-           plot_state_qsphere(matrix,
-                show_state_phases = True, use_degrees = True)
+           plot_state_qsphere(matrix, show_state_phases=True, use_degrees=True)
     """
     from matplotlib import gridspec
     from matplotlib import pyplot as plt
