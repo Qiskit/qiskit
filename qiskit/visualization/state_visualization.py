92396903eba0ed2a99a50deb4f23e9c2225f2979
Guillermo-Mijares-Vilarino <106545082+Guillermo-Mijares-Vilarino@users.noreply.github.com>
2022-08-04 10:55:45 +0200

    Added plot state qsphere code example to API reference in order to better showcase the different arguments (#8355)
    * changed plot_state_qsphere code examples in API reference to better showcase the different arguments
    
    * run tests
    
    * removed reference to Aer and made changes to circuit
    
    * simplified state commands
    
    * Update qiskit/visualization/state_visualization.py
    
    Co-authored-by: Matthew Treinish <mtreinish@kortar.org>
    
    * removed repeated imports and matplotlib inline
    
    * added comment and removed figsize
    
    Co-authored-by: Junye Huang <h.jun.ye@gmail.com>
    Co-authored-by: Matthew Treinish <mtreinish@kortar.org>
    Co-authored-by: mergify[bot] <37929162+mergify[bot]@users.noreply.github.com>
    (cherry picked from commit f206accc270ff2144387c8f25833b31ad8e87572)

--- qiskit/visualization/state_visualization.py
+++ qiskit/visualization/state_visualization.py
@@ -698,20 +698,38 @@ def plot_state_qsphere(
 
         QiskitError: Input statevector does not have valid dimensions.
 
-    Example:
+    Examples:
         .. jupyter-execute::
 
            from qiskit import QuantumCircuit
            from qiskit.quantum_info import Statevector
            from qiskit.visualization import plot_state_qsphere
-           %matplotlib inline
 
            qc = QuantumCircuit(2)
            qc.h(0)
            qc.cx(0, 1)
 
-           state = Statevector.from_instruction(qc)
+           state = Statevector(qc)
            plot_state_qsphere(state)
+
+        .. jupyter-execute::
+
+           # You can show the phase of each state and use
+           # degrees instead of radians
+
+           from qiskit.quantum_info import DensityMatrix
+           import numpy as np
+
+           qc = QuantumCircuit(2)
+           qc.h([0, 1])
+           qc.cz(0,1)
+           qc.ry(np.pi/3, 0)
+           qc.rx(np.pi/5, 1)
+           qc.z(1)
+
+           matrix = DensityMatrix(qc)
+           plot_state_qsphere(matrix,
+                show_state_phases = True, use_degrees = True)
     """
     from matplotlib import gridspec
     from matplotlib import pyplot as plt
