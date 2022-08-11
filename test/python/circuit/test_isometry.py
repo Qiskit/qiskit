7bd53ec9357913f5838f8d659423d0dccf6294cc
israelferrazaraujo <israelferrazaraujo@hotmail.com>
2022-08-11 10:37:02 -0300

    Test the phase of isometry decomposition

--- test/python/circuit/test_isometry.py
+++ test/python/circuit/test_isometry.py
@@ -69,7 +69,7 @@ class TestIsometry(QiskitTestCase):
         unitary = result.get_unitary(qc)
         iso_from_circuit = unitary[::, 0 : 2**num_q_input]
         iso_desired = iso
-        self.assertTrue(matrix_equal(iso_from_circuit, iso_desired, ignore_phase=True))
+        self.assertTrue(matrix_equal(iso_from_circuit, iso_desired, ignore_phase=False))
 
     @data(
         np.eye(2, 2),
@@ -108,7 +108,7 @@ class TestIsometry(QiskitTestCase):
         result = execute(qc, simulator).result()
         unitary = result.get_unitary(qc)
         iso_from_circuit = unitary[::, 0 : 2**num_q_input]
-        self.assertTrue(matrix_equal(iso_from_circuit, iso, ignore_phase=True))
+        self.assertTrue(matrix_equal(iso_from_circuit, iso, ignore_phase=False))
 
     @data(
         np.eye(2, 2),
