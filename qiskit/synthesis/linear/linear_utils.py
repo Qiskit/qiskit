b0f7fbac5c772c81892c93e511c49728d2360dbe
Shelly Garion <shelly@il.ibm.com>
2022-08-17 16:21:39 +0300

    update variable names

--- qiskit/synthesis/linear/linear_utils.py
+++ qiskit/synthesis/linear/linear_utils.py
@@ -16,26 +16,26 @@ import numpy as np
 from qiskit.exceptions import QiskitError
 
 
-def _calc_rank(A):
-    # This function takes as input a binary square matrix A
-    # Returns the rank of A over the binary field F_2
-    n = A.shape[1]
-    X = np.identity(n, dtype=int)
+def _calc_rank(mat):
+    # This function takes as input a binary square matrix mat
+    # Returns the rank of mat over the binary field F_2
+    n = mat.shape[1]
+    xmat = np.identity(n, dtype=int)
 
     for i in range(n):
-        y = np.dot(A[i, :], X) % 2
+        y = np.dot(mat[i, :], xmat) % 2
         not_y = (y + 1) % 2
-        good = X[:, np.nonzero(not_y)]
+        good = xmat[:, np.nonzero(not_y)]
         good = good[:, 0, :]
-        bad = X[:, np.nonzero(y)]
+        bad = xmat[:, np.nonzero(y)]
         bad = bad[:, 0, :]
         if bad.shape[1] > 0:
             bad = np.add(bad, np.roll(bad, 1, axis=1))
             bad = bad % 2
             bad = np.delete(bad, 0, axis=1)
-            X = np.concatenate((good, bad), axis=1)
+            xmat = np.concatenate((good, bad), axis=1)
     # now columns of X span the binary null-space of A
-    return n - X.shape[1]
+    return n - xmat.shape[1]
 
 
 def random_invertible_binary_matrix(num_qubits, seed=None):
@@ -53,17 +53,17 @@ def random_invertible_binary_matrix(num_qubits, seed=None):
     return mat
 
 
-def _gauss_elimination(A, ncols=None, full_elim=False):
-    """Gauss elimination of a matrix A with m rows and n columns.
-    If full_elim = True, it allows full elimination of A[:, 0 : ncols]
-    Returns the matrix A and the permutation perm that was done on the rows
+def _gauss_elimination(mat, ncols=None, full_elim=False):
+    """Gauss elimination of a matrix mat with m rows and n columns.
+    If full_elim = True, it allows full elimination of mat[:, 0 : ncols]
+    Returns the matrix mat and the permutation perm that was done on the rows
     during the process."""
 
     # Treat the matrix A as containing integer values
-    A = np.array(A, dtype=int, copy=False)
+    mat = np.array(mat, dtype=int, copy=False)
 
-    m = A.shape[0]  # no. of rows
-    n = A.shape[1]  # no. of columns
+    m = mat.shape[0]  # no. of rows
+    n = mat.shape[1]  # no. of columns
     if ncols is not None:
         n = min(n, ncols)  # no. of active columns
 
@@ -72,66 +72,66 @@ def _gauss_elimination(A, ncols=None, full_elim=False):
     r = 0  # current rank
     k = 0  # current pivot column
     while (r < m) and (k < n):
-        isNZ = False
+        is_non_zero = False
         new_r = r
         for j in range(k, n):
             for i in range(r, m):
-                if A[i][j]:
-                    isNZ = True
+                if mat[i][j]:
+                    is_non_zero = True
                     k = j
                     new_r = i
                     break
-            if isNZ:
+            if is_non_zero:
                 break
-        if not isNZ:
-            return A, perm  # A is in the canonical form
+        if not is_non_zero:
+            return mat, perm  # A is in the canonical form
 
         if new_r != r:
-            tmp = A[new_r].copy()
-            A[new_r] = A[r]
-            A[r] = tmp
+            tmp = mat[new_r].copy()
+            mat[new_r] = mat[r]
+            mat[r] = tmp
             perm[r], perm[new_r] = perm[new_r], perm[r]
 
         if full_elim:
             for i in range(0, r):
-                if A[i][k]:
-                    A[i] = A[i] ^ A[r]
+                if mat[i][k]:
+                    mat[i] = mat[i] ^ mat[r]
 
         for i in range(r + 1, m):
-            if A[i][k]:
-                A[i] = A[i] ^ A[r]
+            if mat[i][k]:
+                mat[i] = mat[i] ^ mat[r]
         r += 1
 
-    return A, perm
+    return mat, perm
 
 
-def calc_inverse_matrix(A, verify=False):
-    """Given a square numpy(dtype=int) matrix A, tries to compute its inverse.
+def calc_inverse_matrix(mat, verify=False):
+    """Given a square numpy(dtype=int) matrix mat, tries to compute its inverse.
     Returns None if the matrix is not invertible, and the inverse otherwise."""
 
-    if A.shape[0] != A.shape[1]:
+    if mat.shape[0] != mat.shape[1]:
         raise QiskitError("Matrix to invert is a non-square matrix.")
 
-    n = A.shape[0]
+    n = mat.shape[0]
     # concatenate the matrix and identity
-    B = np.concatenate((A, np.eye(n, dtype=int)), axis=1)
-    B, _ = _gauss_elimination(B, None, full_elim=True)
+    mat1 = np.concatenate((mat, np.eye(n, dtype=int)), axis=1)
+    mat1, _ = _gauss_elimination(mat1, None, full_elim=True)
 
-    r = _compute_rank_after_gauss_elim(B[:, 0:n])
+    r = _compute_rank_after_gauss_elim(mat1[:, 0:n])
 
     if r < n:
         raise QiskitError("The matrix is not invertible")
 
-    else:
-        C = B[:, n : 2 * n]
+    matinv = mat1[:, n : 2 * n]
 
-        if verify:
-            D = np.dot(A, C) % 2
-            assert np.array_equal(D, np.eye(n))
-        return C
+    if verify:
+        mat2 = np.dot(mat, matinv) % 2
+        assert np.array_equal(mat2, np.eye(n))
+
+    return matinv
 
 
-def _compute_rank_after_gauss_elim(A):
+def _compute_rank_after_gauss_elim(mat):
     """Given a matrix A after Gaussian elimination, computes its rank
     (i.e. simply the number of nonzero rows)"""
-    return np.sum(A.any(axis=1))
+    return np.sum(mat.any(axis=1))
