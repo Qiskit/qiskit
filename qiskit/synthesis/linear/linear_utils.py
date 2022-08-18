48ac9c2005703a605042b7821a4c952578e04ad8
Shelly Garion <shelly@il.ibm.com>
2022-08-18 13:08:19 +0300

    add a function with 4 options to synthesize a CX circuit

--- qiskit/synthesis/linear/linear_utils.py
+++ qiskit/synthesis/linear/linear_utils.py
@@ -12,6 +12,7 @@
 
 """Utility functions for handling binary matrices."""
 
+import copy
 import numpy as np
 from qiskit.exceptions import QiskitError
 
@@ -135,3 +136,50 @@ def _compute_rank_after_gauss_elim(mat):
     """Given a matrix A after Gaussian elimination, computes its rank
     (i.e. simply the number of nonzero rows)"""
     return np.sum(mat.any(axis=1))
+
+
+def transpose_cx_circ(qc):
+    """Transpose all cx gates in a circuit."""
+    data = qc.data
+    for i in range(len(data)):
+        if data[i][0].name == "cx" and data[i].operation.num_qubits == 2:
+            data[i][1][0], data[i][1][1] = data[i][1][1], data[i][1][0]
+
+
+def optimize_cx_4_options(function, mat, optimize_count=True):
+    # Get best implementation of CX, implementing M,M^(-1),M^T,M^(-1)^T
+    # if not choose_depth, get best cost
+    for i in range(4):
+        mat_cpy = copy.deepcopy(mat)
+        # i=1 inverse, i=2 transpose, i=3 transpose and inverse
+        if i == 0:
+            qc = function(mat)
+        if i == 1:
+            mat_cpy = calc_inverse_matrix(mat_cpy)
+            qc = function(mat_cpy)
+            qc = qc.inverse()
+        elif i == 2:
+            mat_cpy = np.transpose(mat_cpy)
+            qc = function(mat_cpy)
+            transpose_cx_circ(qc)
+            qc = qc.inverse()
+        elif i == 3:
+            mat_cpy = calc_inverse_matrix(np.transpose(mat_cpy))
+            qc = function(mat_cpy)
+            transpose_cx_circ(qc)
+
+        new_depth = qc.depth()
+        new_count = qc.count_ops()["cx"]
+
+        if (
+            i == 0
+            or (optimize_count and best_count > new_count)
+            or (not optimize_count and best_depth > new_depth)
+            or (optimize_count and best_count == new_count and best_depth > new_depth)
+            or (not optimize_count and best_depth == new_depth and best_count > new_count)
+        ):
+            best_count = new_count
+            best_depth = new_depth
+            best_qc = qc
+
+    return best_qc
