fix test for suffix averaging.

qiskit/algorithms/optimizers/suffix_averaging.py
@@ -17,7 +17,8 @@ class SuffixAveragingOptimizer(Optimizer):
 
         References:
             [1] S. Tamiya and H. Yamasaki. 2021.
-            Stochastic Gradient Line Bayesian Optimization for Efficient Noise-Robust Optimization of Parameterized Quantum Circuits.
+            Stochastic Gradient Line Bayesian Optimization
+            for Efficient Noise-Robust Optimization of Parameterized Quantum Circuits.
             arXiv preprint arXiv:2111.07952.
         """
 
@@ -63,9 +64,7 @@ class SuffixAveragingOptimizer(Optimizer):
             n_iterates = int(len(self._circ_params))
             n_repitition = int(len(self._circ_params) / n_params)
             if n_repitition < self._n_params_suffix:
-                warnings.warn(
-                    "The total number of iterations is less than the number of parameters for taking suffix averaging."
-                )
+                warnings.warn("The total number of iterations is less than n_params_suffix.")
                 averaged_param = np.zeros_like(self._circ_params[0])
                 for j in range(n_repitition):
                     averaged_param += self._circ_params[n_iterates - n_params * j - 1]
@@ -81,9 +80,7 @@ class SuffixAveragingOptimizer(Optimizer):
             n_iterates = len(self._circ_params)
 
             if n_iterates < self._n_params_suffix:
-                warnings.warn(
-                    "The total number of iterations is less than the number of parameters for taking suffix averaging."
-                )
+                warnings.warn("The total number of iterations is less than n_params_suffix.")
                 return np.average(self._circ_params, axis=0)
 
             averaged_param = np.zeros_like(self._circ_params[0])

test/python/algorithms/optimizers/test_suffix_averaging.py
@@ -1,6 +1,7 @@
 """ Test of Suffix averaging """
 
 import unittest
+import numpy as np
 from qiskit.algorithms.optimizers.suffix_averaging import SuffixAveragingOptimizer
 from test.python.algorithms import QiskitAlgorithmsTestCase
 from qiskit import BasicAer
@@ -9,7 +10,6 @@ from qiskit.utils import QuantumInstance, algorithm_globals
 from qiskit.opflow import PauliSumOp
 from qiskit.algorithms.optimizers import ADAM
 from qiskit.algorithms import VQE
-import numpy as np
 
 
 class TestOptimizerSA(QiskitAlgorithmsTestCase):
@@ -33,7 +33,7 @@ class TestOptimizerSA(QiskitAlgorithmsTestCase):
         """Test suffix averaging."""
         circ_params = []
         n_params_suffix = 5
-        maxiter = 5
+        maxiter = 7
 
         def store_intermediate_result(eval_count, parameters, mean, std):
             circ_params.append(parameters)
@@ -55,12 +55,14 @@ class TestOptimizerSA(QiskitAlgorithmsTestCase):
 
         result = vqe.compute_minimum_eigenvalue(operator=self.qubit_op)
 
+        n_param = len(circ_params[0])
         average_params = np.zeros_like(circ_params[0])
+
         for i in range(n_params_suffix):
-            average_params += circ_params[maxiter - i - 2]
+            average_params += circ_params[-i * (n_param + 1) - 2]
         average_params /= n_params_suffix
 
-        self.assertListEqual(result.optimal_point, average_params)
+        self.assertListEqual(result.optimal_point.tolist(), average_params.tolist())
 
 
 if __name__ == "__main__":