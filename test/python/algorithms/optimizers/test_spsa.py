7b966858595742d202d9869e54c3da0a003ea9d4
Luciano Bello <bel@zurich.ibm.com>
2022-08-30 11:28:58 +0200

    Use np.array_split instead of np.split to support uneven splits in spsa._batch_evaluate

--- test/python/algorithms/optimizers/test_spsa.py
+++ test/python/algorithms/optimizers/test_spsa.py
@@ -181,3 +181,48 @@ class TestSPSA(QiskitAlgorithmsTestCase):
         for i, (key, values) in enumerate(history.items()):
             self.assertTrue(all(isinstance(value, expected_types[i]) for value in values))
             self.assertEqual(len(history[key]), maxiter)
+
+    def test_estimate_stddev(self):
+        """Test the estimate_stddev"""
+
+        def objective(x):
+            ret = 0.0
+            for array in x:
+                ret += sum(array)
+            return ret
+
+        points = np.arange(8.0)
+        result = SPSA.estimate_stddev(objective, [points, points * 0.8])
+
+        self.assertEqual(result, 0.0)
+
+    def test_estimate_stddev_split(self):
+        """Test the estimate_stddev with max_evals_grouped"""
+
+        def objective(x):
+            ret = 0.0
+            for array in x:
+                ret += sum(array)
+            return ret
+
+        points = np.arange(8.0)
+        result = SPSA.estimate_stddev(objective, [points, points * 0.8], max_evals_grouped=2)
+
+        self.assertEqual(result, 8.18124930781885)
+
+    def test_estimate_stddev_uneven_split(self):
+        """Test the estimate_stddev with uneven split
+        See https://github.com/Qiskit/qiskit-nature/issues/797"""
+
+        def objective(x):
+            ret = 0.0
+            for array in x:
+                ret += sum(array)
+            return ret
+
+        points = np.arange(8.0)
+        result = SPSA.estimate_stddev(
+            objective, [points, points * 0.8], avg=13, max_evals_grouped=3
+        )
+
+        self.assertEqual(result, 11.340000000000002)
