c139421753587167c48f591a1c117e032f22dc15
AlexanderIvrii <alexi@il.ibm.com>
2022-09-04 08:01:53 +0300

    fix merge error

--- setup.py
+++ setup.py
@@ -108,6 +108,7 @@ setup(
             "default = qiskit.transpiler.passes.synthesis.high_level_synthesis_plugins:DefaultSynthesisLinearFunction",
             "depth_opt = test_stuff:LinearFunctionSynthesisPluginForDepth",
             "count_opt = test_stuff:LinearFunctionSynthesisPluginForCount",
+        ],
         "qiskit.transpiler.routing": [
             "basic = qiskit.transpiler.preset_passmanagers.builtin_plugins:BasicSwapPassManager",
             "stochastic = qiskit.transpiler.preset_passmanagers.builtin_plugins:StochasticSwapPassManager",
