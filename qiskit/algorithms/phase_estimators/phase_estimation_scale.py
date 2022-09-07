Fixed unitary calculation.

qiskit/algorithms/phase_estimators/phase_estimation_scale.py
@@ -116,7 +116,9 @@ class PhaseEstimationScale:
         return phases
 
     @classmethod
-    def from_pauli_sum(cls, pauli_sum: SummedOp | PauliSumOp | SparsePauliOp | Operator) -> "PhaseEstimationScale" | float:
+    def from_pauli_sum(
+        cls, pauli_sum: SummedOp | PauliSumOp | SparsePauliOp | Operator
+    ) -> "PhaseEstimationScale" | float:
         """Create a PhaseEstimationScale from a `SummedOp` representing a sum of Pauli Operators.
 
         It is assumed that the ``pauli_sum`` is the sum of ``PauliOp`` objects. The bound on
@@ -134,7 +136,7 @@ class PhaseEstimationScale:
             A ``PhaseEstimationScale`` object
         """
         if isinstance(pauli_sum, (PauliSumOp, SparsePauliOp)):
-            bound = abs(pauli_sum.coeff) * sum(abs(pauli_sum.coeffs))
+            bound = abs(pauli_sum.coeff) * sum(abs(coeff) for coeff in pauli_sum.coeffs)
             return PhaseEstimationScale(bound)
         elif isinstance(pauli_sum, Operator):
             bound = np.sum(np.abs(np.eigvalsh(pauli_sum)))