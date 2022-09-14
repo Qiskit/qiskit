b4f93313a65e51de7f9c366c702fd7002f270e70
Ikko Hamamura <ikkoham@users.noreply.github.com>
2022-09-14 16:25:19 +0900

    Update qiskit/primitives/utils.py

--- qiskit/primitives/utils.py
+++ qiskit/primitives/utils.py
@@ -126,7 +126,6 @@ def _circuit_key(circuit: QuantumCircuit) -> tuple:
         Key for directory.
     """
     return (
-        circuit.name,
         circuit.num_qubits,
         circuit.num_clbits,
         circuit.num_parameters,
