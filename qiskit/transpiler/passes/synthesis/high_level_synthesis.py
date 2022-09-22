4f6c95c91daa84521673a223323a089797976880
AlexanderIvrii <alexi@il.ibm.com>
2022-09-22 12:32:23 +0300

    Adding example from release notes to HighLevelSynthesis documentation

--- qiskit/transpiler/passes/synthesis/high_level_synthesis.py
+++ qiskit/transpiler/passes/synthesis/high_level_synthesis.py
@@ -72,7 +72,20 @@ class HLSConfig:
 
 class HighLevelSynthesis(TransformationPass):
     """Synthesize higher-level objects by choosing the appropriate synthesis method
-    based on the node's name and the HLS-config (if provided).
+    based on the object's name and the HLS-config (if provided).
+
+    As an example, let us assume that ``op_a`` and ``op_b`` are names of two higher-level objects,
+    that ``op_a``-objects have two synthesis methods ``default`` which does require any additional
+    parameters and ``other`` with two optional integer parameters ``option_1`` and ``option_2``,
+    that ``op_b``-objects have a single synthesis method ``default``, and ``qc`` is a quantum
+    circuit containing ``op_a`` and ``op_b`` objects. The following code snippet::
+
+        hls_config = HLSConfig(op_b=[("other", {"option_1": 7, "option_2": 4})])
+        pm = PassManager([HighLevelSynthesis(hls_config=hls_config)])
+        transpiled_qc = pm.run(qc)
+
+    shows how to run the alternative synthesis method ``other`` for ``op_b``-objects, while using the
+    ``default`` methods for all other high-level objects, including ``op_a``-objects.
     """
 
     def __init__(self, hls_config=None):
