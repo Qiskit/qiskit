222e50a6e3aa9c0a5a3747a8e594e3efc54bc5df
MarcDrudis <MarcSanzDrudis@outlook.com>
2022-09-26 11:38:18 +0200

    restore evolvers

--- qiskit/algorithms/evolvers/evolution_result.py
+++ qiskit/algorithms/evolvers/evolution_result.py
@@ -14,8 +14,6 @@
 
 from typing import Optional, Union, Tuple
 
-import numpy as np
-
 from qiskit import QuantumCircuit
 from qiskit.algorithms.list_or_dict import ListOrDict
 from qiskit.opflow import StateFn, OperatorBase
@@ -44,7 +42,6 @@ class EvolutionResult(AlgorithmResult):
         self,
         evolved_state: Union[StateFn, QuantumCircuit, OperatorBase],
         aux_ops_evaluated: Optional[ListOrDict[Tuple[complex, complex]]] = None,
-        observables: Optional[ListOrDict[Tuple[np.ndarray, np.ndarray]]] = None,
     ):
         """
         Args:
@@ -52,10 +49,7 @@ class EvolutionResult(AlgorithmResult):
             aux_ops_evaluated: Optional list of observables for which expected values on an evolved
                 state are calculated. These values are in fact tuples formatted as (mean, standard
                 deviation).
-            observables: Optional list of observables for which expected values for each timestep.
-             These values are in fact tuples formatted as (mean, standard deviation).
         """
 
         self.evolved_state = evolved_state
         self.aux_ops_evaluated = aux_ops_evaluated
-        self.observables = observables
