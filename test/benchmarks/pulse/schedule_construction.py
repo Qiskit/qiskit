79ff6ecbf28f2cc0d1470824c3e426f72fb438ce
Naoki Kanazawa <nkanazawa1989@gmail.com>
2022-09-27 00:14:42 +0900

    Update pulse benchmarking suite (Qiskit/qiskit-metapackage#1607)
    - Add pulse defaults loading test. This mainly measures instmap construction.
    - Add lowering test. This measures conversion of block -> schedule.
    - Add ECR building tests with three major approaches.
    - Add parameterized block test with parameter scan, assuming calibration

--- test/benchmarks/pulse/schedule_construction.py
+++ test/benchmarks/pulse/schedule_construction.py
@@ -17,64 +17,163 @@
 
 import numpy as np
 
-from qiskit.pulse import Schedule, Gaussian, DriveChannel, Play, Waveform
+from qiskit.circuit import Parameter, QuantumCircuit, Gate
+from qiskit.pulse import builder, library, channels
 
 
-def build_sample_pulse_schedule(number_of_unique_pulses, number_of_channels):
-    rng = np.random.RandomState(42)
-    sched = Schedule()
-    for _ in range(number_of_unique_pulses):
-        for channel in range(number_of_channels):
-            sched.append(
-                Play(Waveform(rng.random(50)), DriveChannel(channel)),
-                inplace=True,
+class EchoedCrossResonanceConstructionBench:
+
+    def setup(self):
+
+        with builder.build() as x_ctrl:
+            builder.play(
+                library.Drag(160, 0.2, 40, 1.5),
+                channels.DriveChannel(0),
             )
-    return sched
+        self.x_ctrl = x_ctrl
 
-
-def build_parametric_pulse_schedule(number_of_unique_pulses,
-                                    number_of_channels):
-    sched = Schedule()
-    for _ in range(number_of_unique_pulses):
-        for channel in range(number_of_channels):
-            sched.append(
-                Play(
-                    Gaussian(duration=25, sigma=4, amp=0.5j),
-                    DriveChannel(channel),
-                ),
-                inplace=True,
+        with builder.build() as cr45p:
+            builder.play(
+                library.GaussianSquare(800, 0.4, 64, 544),
+                channels.ControlChannel(0),
             )
-    return sched
+            builder.play(
+                library.GaussianSquare(800, 0.1, 64, 544),
+                channels.DriveChannel(1),
+            )
+        self.cr45p = cr45p
 
+    def time_full_scratch(self):
+        # Full scratch in a single builder context
+        with builder.build():
+            with builder.align_sequential():
+                with builder.align_left():
+                    builder.play(
+                        library.GaussianSquare(800, 0.4, 64, 544),
+                        channels.ControlChannel(0),
+                    )
+                    builder.play(
+                        library.GaussianSquare(800, 0.1, 64, 544),
+                        channels.DriveChannel(1),
+                    )
+                builder.play(
+                    library.Drag(160, 0.2, 40, 1.5),
+                    channels.DriveChannel(0),
+                )
+                with builder.phase_offset(
+                        np.pi,
+                        channels.ControlChannel(0),
+                        channels.DriveChannel(1),
+                ):
+                    with builder.align_left():
+                        builder.play(
+                            library.GaussianSquare(800, 0.4, 64, 544),
+                            channels.ControlChannel(0),
+                        )
+                        builder.play(
+                            library.GaussianSquare(800, 0.1, 64, 544),
+                            channels.DriveChannel(1),
+                        )
+                builder.play(
+                    library.Drag(160, 0.2, 40, 1.5),
+                    channels.DriveChannel(0),
+                )
 
-class ScheduleConstructionBench:
-    params = ([1, 2, 5], [8, 128, 2048])
-    param_names = ['number_of_unique_pulses', 'number_of_channels']
-    timeout = 600
+    def time_with_call(self):
+        # Call subroutine, internally creates reference and assign immediately
+        with builder.build():
+            with builder.align_sequential():
+                builder.call(self.cr45p)
+                builder.call(self.x_ctrl)
+                with builder.phase_offset(
+                        np.pi,
+                        channels.ControlChannel(0),
+                        channels.DriveChannel(1),
+                ):
+                    builder.call(self.cr45p)
+                builder.call(self.x_ctrl)
 
-    def setup(self, unique_pulses, channels):
-        self.sample_sched = build_sample_pulse_schedule(unique_pulses,
-                                                        channels)
-        self.parametric_sched = build_parametric_pulse_schedule(unique_pulses,
-                                                                channels)
+    def time_assign_later(self):
+        # Create placeholder and assign subroutine at a later time
+        with builder.build() as temp_sched:
+            with builder.align_sequential():
+                builder.reference("cr45p", "q0", "q1")
+                builder.reference("x", "q0")
+                with builder.phase_offset(
+                        np.pi,
+                        channels.ControlChannel(0),
+                        channels.DriveChannel(1),
+                ):
+                    builder.reference("cr45p", "q0", "q1")
+                builder.reference("x", "q0")
 
-    def time_sample_pulse_schedule_construction(self,
-                                                unique_pulses,
-                                                channels):
-        build_sample_pulse_schedule(unique_pulses, channels)
-
-    def time_parametric_pulse_schedule_construction(self,
-                                                    unique_pulses,
-                                                    channels):
-        build_parametric_pulse_schedule(unique_pulses, channels)
-
-    def time_append_instruction(self, _, __):
-        self.sample_sched.append(self.parametric_sched, inplace=True)
-
-    def time_insert_instruction_left_to_right(self, _, __):
-        sched = self.sample_sched.shift(self.parametric_sched.stop_time)
-        sched.insert(
-            self.parametric_sched.start_time,
-            self.parametric_sched,
+        temp_sched.assign_references(
+            {
+                ("cr45p", "q0", "q1"): self.cr45p,
+                ("x", "q0"): self.x_ctrl,
+            },
             inplace=True,
         )
+
+
+class ParameterizedScheduleBench:
+
+    params = [3, 11, 31, 51]
+
+    def setup(self, nscan):
+        self.p0 = Parameter("P0")
+        self.p1 = Parameter("P1")
+        self.p2 = Parameter("P2")
+
+        with builder.build() as schedule:
+            builder.play(
+                library.Constant(self.p0, self.p1),
+                channels.DriveChannel(self.p2),
+            )
+        self.schedule = schedule
+
+        with builder.build() as outer_schedule:
+            builder.reference("subroutine")
+        outer_schedule.assign_references(
+            {("subroutine", ): schedule}, inplace=True
+        )
+        self.outer_schedule = outer_schedule
+
+        gate = Gate("my_gate", 1, [self.p0, self.p1, self.p2])
+        qc = QuantumCircuit(1)
+        qc.append(gate, [0])
+        qc.add_calibration(gate, (0,), schedule)
+        self.qc = qc
+
+        # list of parameters
+        self.amps = np.linspace(-1, 1, nscan)
+
+    def time_assign_single_schedule(self, _):
+
+        out = []
+        for amp in self.amps:
+            assigned = self.schedule.assign_parameters(
+                {self.p0: 100, self.p1: amp, self.p2: 0},
+                inplace=False,
+            )
+            out.append(assigned)
+
+    def time_assign_parameterized_subroutine(self, _):
+
+        out = []
+        for amp in self.amps:
+            assigned = self.outer_schedule.assign_parameters(
+                {self.p0: 100, self.p1: amp, self.p2: 0},
+                inplace=False,
+            )
+            out.append(assigned)
+
+    def time_assign_through_pulse_gate(self, _):
+
+        out = []
+        for amp in self.amps:
+            assigned = self.qc.assign_parameters(
+                {self.p0: 100, self.p1: amp, self.p2: 0},
+                inplace=False,
+            )
+            out.append(assigned)
