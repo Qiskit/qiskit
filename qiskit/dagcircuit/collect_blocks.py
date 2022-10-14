a42a3ab1c1224da9d20b6cfe43f2664c1445c1d4
AlexanderIvrii <alexi@il.ibm.com>
2022-10-14 12:19:01 +0300

    Collecting and refining blocks of nodes from DAGCircuit and DAGDependency

---
+++ qiskit/dagcircuit/collect_blocks.py
@@ -0,0 +1,208 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+
+"""Various ways to divide a DAG into blocks of nodes and to split blocks of nodes
+into smaller sub-blocks."""
+
+from . import DAGOpNode, DAGCircuit, DAGDependency
+from .exceptions import DAGCircuitError
+
+
+class BlockCollector:
+    """This class implements various strategies of dividing a DAG (direct acyclic graph)
+    into blocks of nodes that satisfy certain criteria. It works both with the
+    :class:`~qiskit.dagcircuit.DAGCircuit` and
+    :class:`~qiskit.dagcircuit.DAGDependency` representations of a DAG, where
+    DagDependency takes into account commutativity between nodes.
+
+    Collecting nodes from DAGDependency generally leads to more optimal results, but is
+    slower, as it requires to construct a DAGDependency beforehand. Thus, DAGCircuit should
+    be used with lower transpiler settings, and DAGDependency should be used with higher
+    transpiler settings.
+
+    In general, there are multiple ways to collect maximal blocks. The approaches used
+    here are of the form 'starting from the input nodes of a DAG, greedily collect
+    the largest block of nodes that match certain criteria'. For additional details,
+    see https://github.com/Qiskit/qiskit-terra/issues/5775.
+    """
+
+    def __init__(self, dag):
+        """
+        Args:
+            dag (Union[DAGCircuit, DAGDependency]): The input DAG.
+
+        Raises:
+            DAGCircuitError: the input object is not a DAG.
+        """
+
+        self.dag = dag
+
+        if isinstance(dag, DAGCircuit):
+            self.is_dag_dependency = False
+
+        elif isinstance(dag, DAGDependency):
+            self.is_dag_dependency = True
+
+        else:
+            raise DAGCircuitError("not a DAG.")
+
+        # For an efficient implementation, we compute and keep updating the in_degree
+        # for every node, that is the number of node's immediate predecessors.
+        # A node is a leaf (aka input) node iff its in_degree is 0.
+        # When a node is (marked as) collected, the in_degrees of its immediate
+        # successors are updated by subtracting 1.
+        # Additionally, pending_nodes explicitly keeps the list of nodes with in_degree 0.
+        self.pending_nodes = []
+        self.in_degree = dict()
+        for node in self._op_nodes():
+            deg = len(self._direct_preds(node))
+            self.in_degree[node] = deg
+            if deg == 0:
+                self.pending_nodes.append(node)
+
+    def _op_nodes(self):
+        """Returns DAG nodes."""
+        if not self.is_dag_dependency:
+            return self.dag.op_nodes()
+        else:
+            return self.dag.get_nodes()
+
+    def _direct_preds(self, node):
+        """Returns direct predecessors of a node."""
+        if not self.is_dag_dependency:
+            return [pred for pred in self.dag.predecessors(node) if isinstance(pred, DAGOpNode)]
+        else:
+            return [
+                self.dag.get_node(pred_id) for pred_id in self.dag.direct_predecessors(node.node_id)
+            ]
+
+    def _direct_succs(self, node):
+        """Returns direct successors of a node."""
+        if not self.is_dag_dependency:
+            return [succ for succ in self.dag.successors(node) if isinstance(succ, DAGOpNode)]
+        else:
+            return [
+                self.dag.get_node(succ_id) for succ_id in self.dag.direct_successors(node.node_id)
+            ]
+
+    def have_uncollected_nodes(self):
+        """Returns whether there are uncollected (pending) nodes"""
+        return len(self.pending_nodes) > 0
+
+    def collect_matching_block(self, filter_fn):
+        """Iteratively collects the largest block of input (aka in_degree=0) nodes that match a
+        given filtering function. Examples of this include collecting blocks of swap gates,
+        blocks of linear gates (CXs and SWAPs), blocks of Clifford gates, blocks of single-qubit gates,
+        blocks of two-qubit gates, etc.  Here 'iteratively' means that once a node is collected,
+        the in_degrees of its immediate successors are decreased by 1, allowing more nodes to become
+        input and to be eligible for collecting into the current block.
+        Returns the block of collected nodes.
+        """
+        current_block = []
+        unprocessed_pending_nodes = self.pending_nodes
+        self.pending_nodes = []
+
+        # Iteratively process unprocessed_pending_nodes:
+        # - any node that does not match filter_fn is added to pending_nodes
+        # - any node that match filter_fn is added to the current_block,
+        #   and some of its successors may be moved to unprocessed_pending_nodes.
+        while unprocessed_pending_nodes:
+            new_pending_nodes = []
+            for node in unprocessed_pending_nodes:
+                if filter_fn(node):
+                    current_block.append(node)
+
+                    # update the in_degree of node's successors
+                    for suc in self._direct_succs(node):
+                        self.in_degree[suc] -= 1
+                        if self.in_degree[suc] == 0:
+                            new_pending_nodes.append(suc)
+                else:
+                    self.pending_nodes.append(node)
+            unprocessed_pending_nodes = new_pending_nodes
+
+        return current_block
+
+    def collect_all_matching_blocks(self, filter_fn):
+        """Collects all blocks that match a given filtering function filter_fn.
+        This iteratively finds the largest block that does not match filter_fn,
+        then the largest block that matches filter_fn, and so on, until no more uncollected
+        nodes remain. Intuitively, finding larger blocks of non-matching nodes helps to
+        find larger blocks of matching nodes later on.
+        Returns the list of matching blocks only.
+        """
+
+        def not_filter_fn(node):
+            """Returns the opposite of filter_fn."""
+            return not filter_fn(node)
+
+        # Iteratively collect non-matching and matching blocks.
+        matching_blocks = []
+        while self.have_uncollected_nodes():
+            self.collect_matching_block(not_filter_fn)
+            matching_block = self.collect_matching_block(filter_fn)
+            if matching_block:
+                matching_blocks.append(matching_block)
+
+        return matching_blocks
+
+
+class BlockSplitter:
+    """Splits a block of nodes into sub-blocks over disjoint qubits.
+    The implementation is based on the Disjoint Set Union data structure."""
+
+    def __init__(self):
+        self.leader = {}  # qubit's group leader
+        self.group = {}  # qubit's group
+
+    def find_leader(self, index):
+        """Find in DSU."""
+        if index not in self.leader:
+            self.leader[index] = index
+            self.group[index] = []
+            return index
+        if self.leader[index] == index:
+            return index
+        self.leader[index] = self.find_leader(self.leader[index])
+        return self.leader[index]
+
+    def union_leaders(self, index1, index2):
+        """Union in DSU."""
+        leader1 = self.find_leader(index1)
+        leader2 = self.find_leader(index2)
+        if leader1 == leader2:
+            return
+        if len(self.group[leader1]) < len(self.group[leader2]):
+            leader1, leader2 = leader2, leader1
+
+        self.leader[leader2] = leader1
+        self.group[leader1].extend(self.group[leader2])
+        self.group[leader2].clear()
+
+    def run(self, block):
+        """Splits block of nodes into sub-blocks over disjoint qubits."""
+        for node in block:
+            indices = node.qargs
+            if not indices:
+                continue
+            first = indices[0]
+            for index in indices[1:]:
+                self.union_leaders(first, index)
+            self.group[self.find_leader(first)].append(node)
+
+        blocks = []
+        for index in self.leader:
+            if self.leader[index] == index:
+                blocks.append(self.group[index])
+
+        return blocks
