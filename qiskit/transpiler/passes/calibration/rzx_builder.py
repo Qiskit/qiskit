357d222a62da548525ccaf2b623dc3890d2ba671
Matthew Treinish <mtreinish@kortar.org>
2022-10-25 13:53:52 -0400

    Remove redundant float cast

--- qiskit/transpiler/passes/calibration/rzx_builder.py
+++ qiskit/transpiler/passes/calibration/rzx_builder.py
@@ -178,7 +178,7 @@ class RZXCalibrationBuilder(CalibrationBuilder):
         rzx_theta = Schedule(name="rzx(%.3f)" % theta)
         rzx_theta.metadata["publisher"] = CalibrationPublisher.QISKIT
 
-        if np.isclose(float(theta), 0.0):
+        if np.isclose(theta, 0.0):
             return rzx_theta
 
         cx_sched = self._inst_map.get("cx", qubits=qubits)
