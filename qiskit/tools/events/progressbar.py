df41d6e11868b03e4adc1a085fe8d1c71618295e
cheewei <second_comet@yahoo.com>
2022-11-01 14:38:34 +0800

    fix docstring

--- qiskit/tools/events/progressbar.py
+++ qiskit/tools/events/progressbar.py
@@ -121,9 +121,13 @@ class TextProgressBar(BaseProgressBar):
 
     .. jupyter-execute::
 
+       import numpy as np
+       import qiskit.tools.jupyter
+       from qiskit.tools.parallel import parallel_map
        from qiskit.tools.events import TextProgressBar
        TextProgressBar()
        %qiskit_progress_bar -t text
+       parallel_map(np.sin, np.linspace(0,10,100));
     """
 
     def __init__(self, output_handler=None):
