40e494c43557529125775f8007e88fcc12227486
AlexanderIvrii <alexi@il.ibm.com>
2022-11-14 08:07:05 +0200

    Restoring docstring

--- qiskit/synthesis/permutation/permutation_lnn.py
+++ qiskit/synthesis/permutation/permutation_lnn.py
@@ -20,9 +20,20 @@ def synth_permutation_depth_lnn_kms(pattern):
     """Synthesize a permutation circuit for a linear nearest-neighbor
     architecture using the Kutin, Moulton, Smithline method.
 
+    This is the permutation synthesis algorithm from
+    https://arxiv.org/abs/quant-ph/0701194, Chapter 6.
+    It synthesizes any permutation of n qubits over linear nearest-neighbor
+    architecture using SWAP gates with depth at most n and size at most
+    n(n-1)/2 (where both depth and size are measured with respect to SWAPs).
+
+
     Args:
-        pattern (Union[list[int], np.ndarray]): permutation pattern, describing which qubits
-            occupy the positions 0, 1, 2, etc. after applying the permutation.
+        pattern (Union[list[int], np.ndarray]): permutation pattern, describing
+            which qubits occupy the positions 0, 1, 2, etc. after applying the
+            permutation. That is, ``pattern[k] = m`` when the permutation maps
+            qubit ``m`` to position ``k``. As an example, the pattern ``[2, 4, 3, 0, 1]``
+            means that qubit ``2`` goes to position ``0``, qubit ``4`` goes to
+            position ``1``, etc.
 
     Returns:
         QuantumCircuit: the synthesized quantum circuit.
