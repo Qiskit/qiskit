8164b67adb6b10eca03ebf36ea066b33918f1b75
Jake Lishman <jake.lishman@ibm.com>
2022-11-23 17:22:00 +0000

    Prepare 0.22.3

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ author = "Qiskit Development Team"
 # The short X.Y version
 version = "0.22"
 # The full version, including alpha/beta/rc tags
-release = "0.22.2"
+release = "0.22.3"
 
 extensions = [
     "sphinx.ext.napoleon",
