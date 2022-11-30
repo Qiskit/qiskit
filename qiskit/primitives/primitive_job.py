ca97f8ac3f95c7b7bf611a1b1e24b44f94d781ce
ikkoham <ikkoham@users.noreply.github.com>
2022-11-30 23:15:28 +0900

    remove context manager

--- qiskit/primitives/primitive_job.py
+++ qiskit/primitives/primitive_job.py
@@ -40,9 +40,8 @@ class PrimitiveJob(JobV1):
         if self._future is not None:
             raise JobError("Primitive job has already been submitted.")
 
-        with ThreadPoolExecutor(max_workers=1) as executor:
-            future = executor.submit(self._function, *self._args, **self._kwargs)
-        self._future = future
+        executor = ThreadPoolExecutor(max_workers=1)
+        self._future executor.submit(self._function, *self._args, **self._kwargs)
 
     def result(self):
         """Return the results of the job."""
