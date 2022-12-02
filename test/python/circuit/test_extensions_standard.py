89be2d01c344b1704381bd2ee25419364c53fc93
sumit-kale <kale6@purdue.edu>
2022-12-02 11:28:15 -0500

    gphase debug1

--- test/python/circuit/test_extensions_standard.py
+++ test/python/circuit/test_extensions_standard.py
@@ -867,9 +867,9 @@ class TestStandard1Q(QiskitTestCase):
     def test_gphase_inv(self):
         instruction_set = self.circuit.gphase(0.1).inverse()
         # self.assertEqual(len(instruction_set), 1)
-        self.assertEqual(instruction_set[0].operation.name, "gphase")
+        # self.assertEqual(instruction_set[0].operation.name, "gphase_dg")
         # self.assertEqual(instruction_set[1].qubits, (self.qr[0],))
-        self.assertEqual(instruction_set[1].operation.params, [-0.1])
+        self.assertEqual(instruction_set[0].operation.params, [-0.1])
 
     # def test_gphase_inv(self):
     #     instruction_set = self.circuit.append(GlobalPhaseGate(-0.1), []).inverse()
