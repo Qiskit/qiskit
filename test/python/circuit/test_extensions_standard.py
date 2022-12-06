e1fa6dcb31a1f48250b6542e6c777718ed65f2fb
sumit-kale <kale6@purdue.edu>
2022-12-05 21:57:29 -0500

    removed extra comma l871

--- test/python/circuit/test_extensions_standard.py
+++ test/python/circuit/test_extensions_standard.py
@@ -868,11 +868,7 @@ class TestStandard1Q(QiskitTestCase):
     def test_global_phase_matrix(self):  # , theta: float, expected: np.ndarray
         """Test global_phase matrix."""
         theta = 0.1
-        np.testing.assert_allclose(
-            GlobalPhaseGate(theta).__array__(),
-            np.array([[np.exp(1j * theta)]], dtype=complex),
-            atol=1e-7,
-        )
+        np.testing.assert_allclose(GlobalPhaseGate(theta).__array__(),np.array([[np.exp(1j * theta)]], dtype=complex),atol=1e-7)
 
 
 @ddt
