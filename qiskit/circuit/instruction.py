593910d111febf6b5f9c8cca4632bcc517f5976d
Julien Gacon <gaconju@gmail.com>
2022-12-20 18:49:55 +0100

    Fix NumPy 1.24.0 compatibility and pin `coverage<7.0` (#9305)
    * fix Kraus from (array, None)
    
    * fix triu_to_dense test
    
    * fix instruction comparison
    
    * skip snobfit if numpy 1.24.0 or above is installed
    
    Co-authored-by: ElePT <57907331+ElePT@users.noreply.github.com>
    
    * pin coverage <7.0
    
    * add links to Kraus and snobfit issues
    
    * retrigger CI
    
    Co-authored-by: ElePT <57907331+ElePT@users.noreply.github.com>
    (cherry picked from commit 9733fc0ca9e548da976e369116c857653c03db4f)
    
    # Conflicts:
    #	constraints.txt

--- qiskit/circuit/instruction.py
+++ qiskit/circuit/instruction.py
@@ -131,11 +131,13 @@ class Instruction(Operation):
                 pass
 
             try:
-                if numpy.shape(self_param) == numpy.shape(other_param) and numpy.allclose(
+                self_asarray = numpy.asarray(self_param)
+                other_asarray = numpy.asarray(other_param)
+                if numpy.shape(self_asarray) == numpy.shape(other_asarray) and numpy.allclose(
                     self_param, other_param, atol=_CUTOFF_PRECISION, rtol=0
                 ):
                     continue
-            except TypeError:
+            except (ValueError, TypeError):
                 pass
 
             try:
