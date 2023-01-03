5484e3c0a2ec78d4cee43f81b31795dd5b13265c
Matthew Treinish <mtreinish@kortar.org>
2023-01-03 10:34:17 -0500

    Fix lint

--- qiskit/transpiler/preset_passmanagers/level3.py
+++ qiskit/transpiler/preset_passmanagers/level3.py
@@ -142,7 +142,7 @@ def level_3_pass_manager(pass_manager_config: PassManagerConfig) -> StagedPassMa
             layout_trials=20,
             skip_routing=pass_manager_config.routing_method is not None
             and routing_method != "sabre",
-            target=target
+            target=target,
         )
 
     # Choose routing pass
