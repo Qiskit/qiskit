6876995ed5ca6cf596601857acab6ad7bf9f74eb
Matthew Treinish <mtreinish@kortar.org>
2023-01-06 12:11:56 -0500

    Run QPY backwards compatibility tests in parallel
    The QPY backwards compoatibility testing is now taking a significant
    amount of time. The tests are designed to ensure that the backwards
    compatibility guarantees the QPY format makes are upheld and validates
    that a QPY payload generated from any earlier release of Qiskit (since
    terra 0.18.0 when QPY was first introduced) can be loaded as an
    equivalent circuit with the current development branch of Qiskit. While
    when we first started running these tests there were only a handful of
    versions we're now validating 19 earlier versions of Qiskit and that
    number will only grow over time. This portion of test jobs in CI are
    taking an increasing amount of time. To help offset this in the short
    term this commit migrates the qpy tests to run in parallel, by
    leveraging GNU Parallel. This includes building the virtualenv and
    running the qpy generator (which typically is much faster than the
    installation time). Locally this results in a huge speedup, although
    this will be more modest in CI because of limited resources available
    to the CI worker. Longer term when the QPY portion of the job gets too
    slow we'll likely have to migrate the qpy tests to a dedicated CI job
    that runs in parallel to the unit tests. But hopefully, running the
    tests in parallel will provide a sufficient speedup to offset another
    job for some time.

---
+++ test/qpy_compat/process_version.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2023.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+set -e
+set -x
+
+version=$1
+parts=( ${version//./ } )
+if [[ ${parts[1]} -lt 18 ]] ; then
+    exit 0
+fi
+echo "Building venv for qiskit-terra $version"
+python -m venv $version
+./$version/bin/pip install "qiskit-terra==$version"
+mkdir qpy_$version
+pushd qpy_$version
+echo "Generating qpy files with qiskit-terra $version"
+../$version/bin/python ../test_qpy.py generate --version=$version
+echo "Loading qpy files from $version with dev qiskit-terra"
+../qiskit_venv/bin/python ../test_qpy.py load --version=$version
+popd
