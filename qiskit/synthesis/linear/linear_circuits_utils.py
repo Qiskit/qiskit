83b0ba31aaf7ef63507255687502d33723140601
Shelly Garion <shelly@il.ibm.com>
2023-01-29 08:18:07 +0000

    Add utils needed for LNN tests

--- qiskit/synthesis/linear/linear_circuits_utils.py
+++ qiskit/synthesis/linear/linear_circuits_utils.py
@@ -100,3 +100,51 @@ def optimize_cx_4_options(function: Callable, mat: np.ndarray, optimize_count: b
             best_qc = qc
 
     return best_qc
+
+
+def get_circ_cx_depth(qc: QuantumCircuit) -> int:
+    """Calculate the circuit depth only for CX gates, removing single qubit gates.
+
+    Args:
+        qc: a QuantumCircuit containing only CX and single qubit gates.
+
+    Returns:
+        int: the circuit depth only for CX gates.
+
+    Raises:
+        CircuitError: if qc has a non-CX two-qubit gate.
+    """
+    num_qubits = qc.num_qubits
+    qc2 = QuantumCircuit(num_qubits)
+    for instruction in reversed(qc.data):
+        if instruction.operation.num_qubits > 1:
+            if instruction.operation.name == "cx":
+                qc2._append(instruction)
+            else:
+                raise CircuitError("The circuit has two-qubits gates different than CX.")
+    return qc2.depth()
+
+
+def check_lnn_connectivity(qc: QuantumCircuit) -> bool:
+    """Check that the synthesized circuit qc fits linear nearest neighbor connectivity.
+
+    Args:
+        qc: a QuantumCircuit containing only CX and single qubit gates.
+
+    Returns:
+        bool: True if the circuit has linear nearest neighbor connectivity.
+
+    Raises:
+        CircuitError: if qc has a non-CX two-qubit gate.
+    """
+    for instruction in qc.data:
+        if instruction.operation.num_qubits > 1:
+            if instruction.operation.name == "cx":
+                q0 = qc.find_bit(instruction.qubits[0]).index
+                q1 = qc.find_bit(instruction.qubits[1]).index
+                dist = abs(q0 - q1)
+                if dist != 1:
+                    return False
+            else:
+                raise CircuitError("The circuit has two-qubits gates different than CX.")
+    return True
