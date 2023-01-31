fd79142eba14086adb9e8463870774a70849723e
Jake Lishman <jake.lishman@ibm.com>
2023-01-31 14:30:57 +0000

    Use custom descriptor instead of classmethod/property
    The positive interaction between `classmethod` and `property` was
    apparently only added in Python 3.9.

--- qiskit/circuit/register.py
+++ qiskit/circuit/register.py
@@ -22,8 +22,19 @@ import numpy as np
 
 from qiskit.circuit.exceptions import CircuitError
 
-# Over-specific import to avoid cyclic imports.
-from qiskit.utils.deprecation import deprecate_function
+
+class _NameFormat:
+    REGEX = re.compile("[a-z][a-zA-Z0-9_]*")
+
+    def __get__(self, obj, objtype=None):
+        warnings.warn(
+            "Register.name_format is deprecated as of Qiskit Terra 0.23, and will be removed in a"
+            " future release. There is no longer a restriction on the names of registers, so the"
+            " attribute has no meaning any more.",
+            DeprecationWarning,
+            stacklevel=2,
+        )
+        return self.REGEX
 
 
 class Register:
@@ -31,21 +42,10 @@ class Register:
 
     __slots__ = ["_name", "_size", "_bits", "_bit_indices", "_hash", "_repr"]
 
-    _name_format = re.compile("[a-z][a-zA-Z0-9_]*")
-
     # In historical version of Terra, registers' name had to conform to the OpenQASM 2 specification
     # (see appendix A of https://arxiv.org/pdf/1707.03429v2.pdf), and this regex enforced it.  That
     # restriction has been relaxed, so this is no longer necessary.
-    @classmethod
-    @property
-    @deprecate_function(
-        "Register.name_format is deprecated as of Qiskit Terra 0.23, and will be removed in a"
-        " future release. There is no longer a restriction on the names of registers, so the"
-        " attribute has no meaning any more."
-    )
-    def name_format(cls):
-        # pylint: disable=missing-function-docstring
-        return cls._name_format
+    name_format = _NameFormat()
 
     # Counter for the number of instances in this class.
     instances_counter = itertools.count()
