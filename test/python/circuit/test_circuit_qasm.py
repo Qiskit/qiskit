cdc0abdd54c15c752c4167413e2cb653e4a2ede6
John Lapeyre <jlapeyre@users.noreply.github.com>
2023-02-20 19:20:26 -0500

    test/python/circuit/test_circuit_qasm.py:295:106: E501 Line too long (363 > 105 characters)
    And similar errors

--- test/python/circuit/test_circuit_qasm.py
+++ test/python/circuit/test_circuit_qasm.py
@@ -294,7 +294,7 @@ c3sqrtx q[0],q[1],q[2],q[3];
 include "qelib1.inc";
 gate mcx q0,q1,q2,q3 { h q3; p(pi/8) q0; p(pi/8) q1; p(pi/8) q2; p(pi/8) q3; cx q0,q1; p(-pi/8) q1; cx q0,q1; cx q1,q2; p(-pi/8) q2; cx q0,q2; p(pi/8) q2; cx q1,q2; p(-pi/8) q2; cx q0,q2; cx q2,q3; p(-pi/8) q3; cx q1,q3; p(pi/8) q3; cx q2,q3; p(-pi/8) q3; cx q0,q3; p(pi/8) q3; cx q2,q3; p(-pi/8) q3; cx q1,q3; p(pi/8) q3; cx q2,q3; p(-pi/8) q3; cx q0,q3; h q3; }
 qreg q[4];
-mcx q[0],q[1],q[2],q[3];\n"""
+mcx q[0],q[1],q[2],q[3];\n"""  # noqa: E501
         self.assertEqual(qc.qasm(), expected_qasm)
 
     def test_circuit_qasm_with_mcx_gate_variants(self):
@@ -319,7 +319,7 @@ gate mcx_vchain q0,q1,q2,q3,q4,q5,q6,q7,q8 { rccx q0,q1,q6; rccx q2,q6,q7; rccx 
 qreg q[9];
 mcx_gray q[0],q[1],q[2],q[3],q[4],q[5];
 mcx_recursive q[0],q[1],q[2],q[3],q[4],q[5],q[6];
-mcx_vchain q[0],q[1],q[2],q[3],q[4],q[5],q[6],q[7],q[8];\n"""
+mcx_vchain q[0],q[1],q[2],q[3],q[4],q[5],q[6],q[7],q[8];\n"""  # noqa: E501
 
         self.assertEqual(qc.qasm(), expected_qasm)
 
