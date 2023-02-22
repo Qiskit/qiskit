fcd646efa2180e3edd099157559dc50c407edee6
ikkoham <ikkoham@users.noreply.github.com>
2023-02-23 00:47:13 +0900

    remove private tests

--- test/python/primitives/test_primitive.py
+++ test/python/primitives/test_primitive.py
@@ -14,106 +14,10 @@
 
 import json
 
-from ddt import data, ddt, unpack
-from numpy import array, float32, float64, int32, int64
-
 from qiskit import QuantumCircuit, pulse, transpile
-from qiskit.circuit.random import random_circuit
-from qiskit.primitives.base.base_primitive import BasePrimitive
 from qiskit.primitives.utils import _circuit_key
 from qiskit.providers.fake_provider import FakeAlmaden
 from qiskit.test import QiskitTestCase
-
-
-@ddt
-class TestCircuitValidation(QiskitTestCase):
-    """Test circuits validation logic."""
-
-    @data(
-        (random_circuit(2, 2, seed=0), (random_circuit(2, 2, seed=0),)),
-        (
-            [random_circuit(2, 2, seed=0), random_circuit(2, 2, seed=1)],
-            (random_circuit(2, 2, seed=0), random_circuit(2, 2, seed=1)),
-        ),
-    )
-    @unpack
-    def test_validate_circuits(self, circuits, expected):
-        """Test circuits standardization."""
-        self.assertEqual(BasePrimitive._validate_circuits(circuits), expected)
-
-    @data(None, "ERROR", True, 0, 1.0, 1j, [0.0])
-    def test_type_error(self, circuits):
-        """Test type error if invalid input."""
-        with self.assertRaises(TypeError):
-            BasePrimitive._validate_circuits(circuits)
-
-    @data((), [], "")
-    def test_value_error(self, circuits):
-        """Test value error if no circuits are provided."""
-        with self.assertRaises(ValueError):
-            BasePrimitive._validate_circuits(circuits)
-
-
-@ddt
-class TestParameterValuesValidation(QiskitTestCase):
-    """Test parameter_values validation logic."""
-
-    @data(
-        ((), ((),)),
-        ([], ((),)),
-        (0, ((0,),)),
-        (1.2, ((1.2,),)),
-        ((0,), ((0,),)),
-        ([0], ((0,),)),
-        ([1.2], ((1.2,),)),
-        ((0, 1), ((0, 1),)),
-        ([0, 1], ((0, 1),)),
-        ([0, 1.2], ((0, 1.2),)),
-        ([0.3, 1.2], ((0.3, 1.2),)),
-        (((0, 1)), ((0, 1),)),
-        (([0, 1]), ((0, 1),)),
-        ([(0, 1)], ((0, 1),)),
-        ([[0, 1]], ((0, 1),)),
-        ([[0, 1.2]], ((0, 1.2),)),
-        ([[0.3, 1.2]], ((0.3, 1.2),)),
-        # Test for numpy dtypes
-        (int32(5), ((float(int32(5)),),)),
-        (int64(6), ((float(int64(6)),),)),
-        (float32(3.2), ((float(float32(3.2)),),)),
-        (float64(6.4), ((float(float64(6.4)),),)),
-        ([int32(5), float32(3.2)], ((float(int32(5)), float(float32(3.2))),)),
-    )
-    @unpack
-    def test_validate_parameter_values(self, _parameter_values, expected):
-        """Test parameter_values standardization."""
-        for parameter_values in [_parameter_values, array(_parameter_values)]:  # Numpy
-            self.assertEqual(BasePrimitive._validate_parameter_values(parameter_values), expected)
-            self.assertEqual(
-                BasePrimitive._validate_parameter_values(None, default=parameter_values), expected
-            )
-
-    @data(
-        "ERROR",
-        ("E", "R", "R", "O", "R"),
-        (["E", "R", "R"], ["O", "R"]),
-        1j,
-        (1j,),
-        ((1j,),),
-        True,
-        False,
-        float("inf"),
-        float("-inf"),
-        float("nan"),
-    )
-    def test_type_error(self, parameter_values):
-        """Test type error if invalid input."""
-        with self.assertRaises(TypeError):
-            BasePrimitive._validate_parameter_values(parameter_values)
-
-    def test_value_error(self):
-        """Test value error if no parameter_values or default are provided."""
-        with self.assertRaises(ValueError):
-            BasePrimitive._validate_parameter_values(None)
 
 
 class TestCircuitKey(QiskitTestCase):
