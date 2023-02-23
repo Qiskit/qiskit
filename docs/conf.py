09f904a03c056abb5ed80030e4d1f75108943502
Julien Gacon <gaconju@gmail.com>
2023-02-23 12:18:44 +0100

    Prepare 0.23.2 (#9643)
    * update version numbers
    
    * fix renos
    
    * add reno
    
    * fix typo in the reno
    
    * Minimize changes in Cargo.lock
    
    Due to an old minium supported rust version
    on stable, only build the the rust package
    without updating all the packages.
    
    * Fix minor grammar bugs
    
    ---------
    
    Co-authored-by: Jake Lishman <jake.lishman@ibm.com>

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ author = "Qiskit Development Team"
 # The short X.Y version
 version = "0.23"
 # The full version, including alpha/beta/rc tags
-release = "0.23.1"
+release = "0.23.2"
 
 extensions = [
     "sphinx.ext.napoleon",
