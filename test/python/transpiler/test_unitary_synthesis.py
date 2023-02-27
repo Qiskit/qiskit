2004c1a923ff4ed6ae293135f31d050c5fc37a3b
Matthew Treinish <mtreinish@kortar.org>
2023-02-27 18:40:13 -0500

    Fix lint

--- test/python/transpiler/test_unitary_synthesis.py
+++ test/python/transpiler/test_unitary_synthesis.py
@@ -836,17 +836,21 @@ class TestUnitarySynthesis(QiskitTestCase):
         result_qc = dag_to_circuit(result_dag)
         self.assertEqual(result_qc, QuantumCircuit(2))
 
-    def test_unitary_synthesis_opaqua_gate_target(self):
+    def test_unitary_synthesis_custom_gate_target(self):
         qc = QuantumCircuit(2)
         qc.unitary(np.eye(4), [0, 1])
         dag = circuit_to_dag(qc)
 
         class CustomGate(Gate):
+            """Custom Opaque Gate"""
+
             def __init__(self):
                 super().__init__("custom", 2, [])
 
         target = Target(num_qubits=2)
-        target.add_instruction(UGate(Parameter('t'), Parameter('p'), Parameter('l')), {(0,): None, (1,): None})
+        target.add_instruction(
+            UGate(Parameter("t"), Parameter("p"), Parameter("l")), {(0,): None, (1,): None}
+        )
         target.add_instruction(CustomGate(), {(0, 1): None, (1, 0): None})
         unitary_synth_pass = UnitarySynthesis(target=target)
         result_dag = unitary_synth_pass.run(dag)
