53a65c3b64f0d6eea8122e4d525a75724be23952
Soolu Thomas <soolu.thomas@ibm.com>
2023-03-02 16:01:28 -0500

    Update conf.py (Qiskit/qiskit-metapackage#1682)

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ import sys
 
 sys.path.insert(0, os.path.abspath('.'))
 
-from docs import custom_extensions
+import custom_extensions
 from custom_directives import (IncludeDirective, GalleryItemDirective,
                                CustomGalleryItemDirective, CustomCalloutItemDirective,
                                CustomCardItemDirective)
