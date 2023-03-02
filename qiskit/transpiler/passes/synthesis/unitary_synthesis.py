b8992a7dc3f22c1cb27e475c82165d41ddee511a
Matthew Treinish <mtreinish@kortar.org>
2023-03-02 16:10:58 -0500

    Remove out of date comment

--- qiskit/transpiler/passes/synthesis/unitary_synthesis.py
+++ qiskit/transpiler/passes/synthesis/unitary_synthesis.py
@@ -727,7 +727,7 @@ class DefaultUnitarySynthesis(plugin.UnitarySynthesisPlugin):
             _decomposer1q = Optimize1qGatesDecomposition(basis_gates, target)
             return _decomposer1q._gate_sequence_to_dag(
                 _decomposer1q._resynthesize_run(unitary, qubits[0])
-            )  # already in dag format
+            )
         elif unitary.shape == (4, 4):
             # select synthesizers that can lower to the target
             if target is not None:
