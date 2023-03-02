6b565170acd687fb8d3adba75f9097537281f768
Matthew Treinish <mtreinish@kortar.org>
2023-03-02 17:00:55 -0500

    Change DenseLayout guard to only prevent running when it won't work

--- qiskit/transpiler/passes/layout/dense_layout.py
+++ qiskit/transpiler/passes/layout/dense_layout.py
@@ -79,10 +79,10 @@ class DenseLayout(AnalysisPass):
             raise TranspilerError(
                 "A coupling_map or target with constrained qargs is necessary to run the pass."
             )
-        if not self.coupling_map.is_connected():
+        if dag.num_qubits() > self.coupling_map.largest_connected_component():
             raise TranspilerError(
                 "Coupling Map is disjoint, this pass can't be used with a disconnected coupling "
-                "map."
+                "map for a circuit this wide."
             )
         num_dag_qubits = len(dag.qubits)
         if num_dag_qubits > self.coupling_map.size():
