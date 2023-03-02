6fc2d25921a90fc72098431def1714b8b44f6864
John Lapeyre <jlapeyre@users.noreply.github.com>
2023-03-02 09:52:41 -0500

    Prevent black from adding trailing space in docstring

--- qiskit/qasm/qasmlexer.py
+++ qiskit/qasm/qasmlexer.py
@@ -124,7 +124,7 @@ class QasmLexer:
         return t
 
     def t_STRING(self, t):
-        r"\"([^\\\"]|\\.)*\""
+        r"\"([^\\\"]|\\.)*\""  # fmt: skip
         return t
 
     def t_INCLUDE(self, _):
