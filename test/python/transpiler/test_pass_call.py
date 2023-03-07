e782105f0acff3c6e064083edc5c759aec452ce7
John Lapeyre <jlapeyre@users.noreply.github.com>
2023-03-06 19:36:54 -0500

    Fix test broken by previous commit in this branch

--- test/python/transpiler/test_pass_call.py
+++ test/python/transpiler/test_pass_call.py
@@ -102,8 +102,5 @@ class TestPassCall(QiskitTestCase):
         unroller = Unroller(basis)
         with self.assertRaises(QiskitError) as cm:
             unroller(circuit)
-        exp_msg = (
-            "Error decomposing node of instruction 'p': 'NoneType' object has no"
-            " attribute 'global_phase'. Unable to define instruction 'u' in the basis."
-        )
+        exp_msg = "Error decomposing node of instruction 'p'. Unable to define instruction 'u' in the basis."
         self.assertEqual(exp_msg, cm.exception.message)
