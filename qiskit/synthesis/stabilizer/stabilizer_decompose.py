03caa3fc7b0f6fa92b98d6e574aea1f2ae7a30e7
Shelly Garion <shelly@il.ibm.com>
2023-04-02 10:06:20 +0000

    improved stabilizer synthesis.
    Co-authored-by: alexi@il.ibm.com

--- qiskit/synthesis/stabilizer/stabilizer_decompose.py
+++ qiskit/synthesis/stabilizer/stabilizer_decompose.py
@@ -14,14 +14,19 @@ Circuit synthesis for a stabilizer state preparation circuit.
 """
 # pylint: disable=invalid-name
 
+import numpy as np
 from qiskit.circuit import QuantumCircuit
 from qiskit.exceptions import QiskitError
 from qiskit.quantum_info.states import StabilizerState
+from qiskit.synthesis.linear.linear_matrix_utils import (
+    calc_inverse_matrix,
+)
 from qiskit.synthesis.linear_phase import synth_cz_depth_line_mr
 from qiskit.synthesis.clifford.clifford_decompose_layers import (
-    synth_clifford_layers,
     _default_cz_synth_func,
-    _default_cx_synth_func,
+    _reverse_clifford,
+    _create_graph_state,
+    _decompose_graph_state,
 )
 
 
@@ -76,29 +81,84 @@ def synth_stabilizer_layers(
 
     cliff = stab.clifford
     num_qubits = cliff.num_qubits
-    qubit_list = list(range(num_qubits))
 
-    circ = synth_clifford_layers(
-        cliff,
-        cz_synth_func=cz_synth_func,
-        cz_func_reverse_qubits=cz_func_reverse_qubits,
-        cx_synth_func=_default_cx_synth_func,
-        validate=validate,
+    if cz_func_reverse_qubits:
+        cliff0 = _reverse_clifford(cliff)
+    else:
+        cliff0 = cliff
+
+    H1_circ, cliff1 = _create_graph_state(cliff0, validate=validate)
+
+    H2_circ, CZ1_circ, S1_circ, _ = _decompose_graph_state(
+        cliff1, validate=validate, cz_synth_func=cz_synth_func
     )
-    H2_circ = circ.data[-5]
-    S1_circ = circ.data[-4]
-    CZ1_circ = circ.data[-3]
-    H1_circ = circ.data[-2]
-    pauli_circ = circ.data[-1]
 
-    stab_circuit = QuantumCircuit(num_qubits)
-    stab_circuit.append(H2_circ, qubit_list)
-    stab_circuit.append(S1_circ, qubit_list)
-    stab_circuit.append(CZ1_circ, qubit_list)
-    stab_circuit.append(H1_circ, qubit_list)
-    stab_circuit.append(pauli_circ, qubit_list)
+    qubit_list = list(range(num_qubits))
+    layeredCircuit = QuantumCircuit(num_qubits)
 
-    return stab_circuit
+    layeredCircuit.append(H2_circ, qubit_list)
+    layeredCircuit.append(S1_circ, qubit_list)
+    layeredCircuit.append(CZ1_circ, qubit_list)
+
+    if cz_func_reverse_qubits:
+        H1_circ = H1_circ.reverse_bits()
+    layeredCircuit.append(H1_circ, qubit_list)
+
+    # Add Pauli layer to fix the Clifford phase signs
+    # pylint: disable=cyclic-import
+    from qiskit.quantum_info.operators.symplectic import Clifford
+
+    clifford_target = Clifford(layeredCircuit)
+    pauli_circ = _calc_pauli_diff_stabilizer(cliff, clifford_target)
+    layeredCircuit.append(pauli_circ, qubit_list)
+
+    return layeredCircuit
+
+
+def _calc_pauli_diff_stabilizer(cliff, cliff_target):
+    """Given two Cliffords whose stabilizers differ by a Pauli, we find this Pauli."""
+
+    # pylint: disable=cyclic-import
+    from qiskit.quantum_info.operators.symplectic import Pauli
+
+    num_qubits = cliff.num_qubits
+    if cliff.num_qubits != cliff_target.num_qubits:
+        raise QiskitError("num_qubits is not the same for the original clifford and the target.")
+
+    # stabilizer generators of the original clifford
+    stab_gen = StabilizerState(cliff).clifford.to_dict()["stabilizer"]
+
+    # stabilizer state of the target clifford
+    ts = StabilizerState(cliff_target)
+
+    phase_destab = [False] * num_qubits
+    phase_stab = [ts.expectation_value(Pauli(stab_gen[i])) == -1 for i in range(num_qubits)]
+
+    phase = []
+    phase.extend(phase_destab)
+    phase.extend(phase_stab)
+    phase = np.array(phase, dtype=int)
+
+    A = cliff.symplectic_matrix.astype(int)
+    Ainv = calc_inverse_matrix(A)
+
+    # By carefully writing how X, Y, Z gates affect each qubit, all we need to compute
+    # is A^{-1} * (phase)
+    C = np.matmul(Ainv, phase) % 2
+
+    # Create the Pauli
+    pauli_circ = QuantumCircuit(num_qubits, name="Pauli")
+    for k in range(num_qubits):
+        destab = C[k]
+        stab = C[k + num_qubits]
+        if stab and destab:
+            pauli_circ.y(k)
+        elif stab:
+            pauli_circ.x(k)
+        elif destab:
+            pauli_circ.z(k)
+
+    return pauli_circ
 
 
 def synth_stabilizer_depth_lnn(stab):
