af823bb3bd4781c15feeb78164e919a76a9b5a64
diemilio <diemilio@live.com>
2023-04-09 09:44:09 -0400

    fix: modify get_param_str to prevent circuit drawer from displaying params of type QuantumCircuit

--- qiskit/visualization/circuit/_utils.py
+++ qiskit/visualization/circuit/_utils.py
@@ -28,7 +28,7 @@ from qiskit.circuit import (
     ControlFlowOp,
 )
 from qiskit.circuit.library import PauliEvolutionGate
-from qiskit.circuit import ClassicalRegister
+from qiskit.circuit import ClassicalRegister, QuantumCircuit
 from qiskit.circuit.tools import pi_check
 from qiskit.converters import circuit_to_dag
 from qiskit.utils import optionals as _optionals
@@ -119,10 +119,7 @@ def get_gate_ctrl_text(op, drawer, style=None, calibrations=None):
 
 def get_param_str(op, drawer, ndigits=3):
     """Get the params as a string to add to the gate text display"""
-    if not hasattr(op, "params") or any(isinstance(param, np.ndarray) for param in op.params):
-        return ""
-
-    if isinstance(op, ControlFlowOp):
+    if not hasattr(op, "params") or any(isinstance(param, np.ndarray) for param in op.params) or any(isinstance(param, QuantumCircuit) for param in op.params):
         return ""
 
     if isinstance(op, Delay):
