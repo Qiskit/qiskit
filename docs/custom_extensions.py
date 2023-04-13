d7c41364b655d5ae0d8a135c189439da0cd414a6
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2023-04-13 08:56:42 -0600

    Replace pycodestyle with Black and Isort (Qiskit/qiskit-metapackage#1689)
    * Replace pycodestyle with Black and Isort
    
    * Get rid of wheel in pyproject.toml
    
    * Also format `tools/`
    
    * Exclude benchmarks
    
    * Review feedback
    
    * Use isort 5.11 because 5.12 drops Py37 support
    
    * Review feedback

--- docs/custom_extensions.py
+++ docs/custom_extensions.py
@@ -20,18 +20,18 @@ import warnings
 from distutils import dir_util
 
 # Elements with api doc sources
-qiskit_elements = ['qiskit-terra', 'qiskit-ibmq-provider']
+qiskit_elements = ["qiskit-terra", "qiskit-ibmq-provider"]
 apidocs_exists = False
 apidocs_master = None
 
 
 def _get_current_versions(app):
     versions = {}
-    setup_py_path = os.path.join(os.path.dirname(app.srcdir), 'setup.py')
-    with open(setup_py_path, 'r') as fd:
+    setup_py_path = os.path.join(os.path.dirname(app.srcdir), "setup.py")
+    with open(setup_py_path, "r") as fd:
         setup_py = fd.read()
         for package in qiskit_elements:
-            version_regex = re.compile(package + '[=|>]=(.*)\"')
+            version_regex = re.compile(package + '[=|>]=(.*)"')
             match = version_regex.search(setup_py)
             if match:
                 ver = match[1]
@@ -40,44 +40,41 @@ def _get_current_versions(app):
 
 
 def _install_from_master():
-    for package in qiskit_elements + ['qiskit-ignis']:
-        github_url = 'git+https://github.com/Qiskit/%s' % package
-        cmd = [sys.executable, '-m', 'pip', 'install', '-U', github_url]
+    for package in qiskit_elements + ["qiskit-ignis"]:
+        github_url = "git+https://github.com/Qiskit/%s" % package
+        cmd = [sys.executable, "-m", "pip", "install", "-U", github_url]
         subprocess.run(cmd)
 
 
 def _git_copy(package, sha1, api_docs_dir):
     try:
         with tempfile.TemporaryDirectory() as temp_dir:
-            github_source = 'https://github.com/Qiskit/%s' % package
-            subprocess.run(['git', 'clone', github_source, temp_dir],
-                           capture_output=True)
-            subprocess.run(['git', 'checkout', sha1], cwd=temp_dir,
-                           capture_output=True)
-            dir_util.copy_tree(
-                os.path.join(temp_dir, 'docs', 'apidocs'),
-                api_docs_dir)
+            github_source = "https://github.com/Qiskit/%s" % package
+            subprocess.run(["git", "clone", github_source, temp_dir], capture_output=True)
+            subprocess.run(["git", "checkout", sha1], cwd=temp_dir, capture_output=True)
+            dir_util.copy_tree(os.path.join(temp_dir, "docs", "apidocs"), api_docs_dir)
 
     except FileNotFoundError:
-        warnings.warn('Copy from git failed for %s at %s, skipping...' %
-                      (package, sha1), RuntimeWarning)
+        warnings.warn(
+            "Copy from git failed for %s at %s, skipping..." % (package, sha1), RuntimeWarning
+        )
 
 
 def load_api_sources(app):
     """Git clones and sets up Qiskit repos so that we can generate their API docs."""
-    api_docs_dir = os.path.join(app.srcdir, 'apidoc')
-    if os.getenv('DOCS_FROM_MASTER'):
+    api_docs_dir = os.path.join(app.srcdir, "apidoc")
+    if os.getenv("DOCS_FROM_MASTER"):
         global apidocs_master
         apidocs_master = tempfile.mkdtemp()
         shutil.move(api_docs_dir, apidocs_master)
         _install_from_master()
         for package in qiskit_elements:
-            _git_copy(package, 'HEAD', api_docs_dir)
+            _git_copy(package, "HEAD", api_docs_dir)
         return
     elif os.path.isdir(api_docs_dir):
         global apidocs_exists
         apidocs_exists = True
-        warnings.warn('docs/apidocs already exists skipping source clone')
+        warnings.warn("docs/apidocs already exists skipping source clone")
         return
     meta_versions = _get_current_versions(app)
     for package in qiskit_elements:
@@ -86,39 +83,36 @@ def load_api_sources(app):
 
 def load_tutorials(app):
     """Git clones the tutorials repo so that we can generate their docs."""
-    tutorials_dir = os.path.join(app.srcdir, 'tutorials')
+    tutorials_dir = os.path.join(app.srcdir, "tutorials")
     try:
         with tempfile.TemporaryDirectory() as temp_dir:
-            github_source = 'https://github.com/Qiskit/qiskit-tutorials'
-            subprocess.run(['git', 'clone', github_source, temp_dir],
-                           capture_output=True)
-            dir_util.copy_tree(
-                os.path.join(temp_dir, 'tutorials'),
-                tutorials_dir)
+            github_source = "https://github.com/Qiskit/qiskit-tutorials"
+            subprocess.run(["git", "clone", github_source, temp_dir], capture_output=True)
+            dir_util.copy_tree(os.path.join(temp_dir, "tutorials"), tutorials_dir)
     except FileNotFoundError:
         warnings.warn(
-            'Copy from git failed for qiskit-tutorials, skipping...',
+            "Copy from git failed for qiskit-tutorials, skipping...",
             RuntimeWarning,
         )
 
 
 def clean_api_source(app, exc):
     """Deletes the Git cloned repos used for API doc generation."""
-    api_docs_dir = os.path.join(app.srcdir, 'apidoc')
+    api_docs_dir = os.path.join(app.srcdir, "apidoc")
     global apidocs_exists
     global apidocs_master
     if apidocs_exists:
         return
     elif apidocs_master:
         shutil.rmtree(api_docs_dir)
-        shutil.move(os.path.join(apidocs_master, 'apidoc'), api_docs_dir)
+        shutil.move(os.path.join(apidocs_master, "apidoc"), api_docs_dir)
         return
     shutil.rmtree(api_docs_dir)
 
 
 def clean_tutorials(app, exc):
     """Deletes the Git cloned tutorials repo used for doc generation."""
-    tutorials_dir = os.path.join(app.srcdir, 'tutorials')
+    tutorials_dir = os.path.join(app.srcdir, "tutorials")
     shutil.rmtree(tutorials_dir)
 
 
@@ -128,5 +122,5 @@ def deprecate_ibmq_provider(app, docname, source):
        The package ``qiskit-ibmq-provider`` is being deprecated and its repo is going to be
        archived soon. Please transition to the new packages. More information in
        https://ibm.biz/provider_migration_guide\n\n"""
-    if 'apidoc/ibmq' in docname or 'qiskit.providers.ibmq' in docname:
+    if "apidoc/ibmq" in docname or "qiskit.providers.ibmq" in docname:
         source[0] = message + source[0]
