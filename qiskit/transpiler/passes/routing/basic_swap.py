QuantumTeaching/qiskit-core

Filter/ignore qubits in Target without any operations
Building off #9840 which adds full path support in all the preset pass
managers for targetting backends with a disconnected coupling graph,
this commit adds support for ignoring qubits that do not support any
operations. When a Target is generated from #9911 with `filter_faulty`
set to `True` this will potentially result in qubits being present in
the `Target` without any supported operations. In these cases the
layout passes in the transpiler might inadvertently use these qubits
only to fail in the basis translator because there are no instructions
available. This commit adds filtering of connected components from the
list of output connected components if the `Target` does have any
supported instructions on a qubit.

This works by building a copy of the coupling map's internal graph
that removes the nodes which do not have any supported operations.
Then when we compute the connected components of this graph it will
exclude any components of isolated qubits without any operations
supported. A similar change is made to the coupling graph we pass to
rustworkx.vf2_mapping() inside the vf2 layout family of passes.

qiskit/transpiler/passes/routing/basic_swap.py
@@ -72,7 +72,9 @@ class BasicSwap(TransformationPass):
 
         if len(dag.qubits) > len(self.coupling_map.physical_qubits):
             raise TranspilerError("The layout does not match the amount of qubits in the DAG")
-        disjoint_utils.check_layout_isolated_to_component(dag, self.coupling_map)
+        disjoint_utils.check_layout_isolated_to_component(
+            dag, self.coupling_map, target=self.target
+        )
 
         canonical_register = dag.qregs["q"]
         trivial_layout = Layout.generate_trivial_layout(canonical_register)

qiskit/transpiler/passes/routing/bip_mapping.py
@@ -167,7 +167,9 @@ class BIPMapping(TransformationPass):
                 "BIPMapping requires the number of virtual and physical qubits to be the same. "
                 "Supply 'qubit_subset' to specify physical qubits to use."
             )
-        disjoint_utils.check_layout_isolated_to_component(dag, self.coupling_map)
+        disjoint_utils.check_layout_isolated_to_component(
+            dag, self.coupling_map, target=self.target
+        )
 
         original_dag = dag
 

qiskit/transpiler/passes/routing/lookahead_swap.py
@@ -130,7 +130,9 @@ class LookaheadSwap(TransformationPass):
                 f"The number of DAG qubits ({len(dag.qubits)}) is greater than the number of "
                 f"available device qubits ({number_of_available_qubits})."
             )
-        disjoint_utils.check_layout_isolated_to_component(dag, self.coupling_map)
+        disjoint_utils.check_layout_isolated_to_component(
+            dag, self.coupling_map, target=self.target
+        )
 
         register = dag.qregs["q"]
         current_state = _SystemState(

qiskit/transpiler/passes/routing/sabre_swap.py
@@ -213,7 +213,9 @@ class SabreSwap(TransformationPass):
             heuristic = Heuristic.Decay
         else:
             raise TranspilerError("Heuristic %s not recognized." % self.heuristic)
-        disjoint_utils.check_layout_isolated_to_component(dag, self.coupling_map)
+        disjoint_utils.check_layout_isolated_to_component(
+            dag, self.coupling_map, target=self.target
+        )
 
         self.dist_matrix = self.coupling_map.distance_matrix
 

qiskit/transpiler/passes/routing/stochastic_swap.py
@@ -105,7 +105,9 @@ class StochasticSwap(TransformationPass):
         if len(dag.qubits) > len(self.coupling_map.physical_qubits):
             raise TranspilerError("The layout does not match the amount of qubits in the DAG")
 
-        disjoint_utils.check_layout_isolated_to_component(dag, self.coupling_map)
+        disjoint_utils.check_layout_isolated_to_component(
+            dag, self.coupling_map, target=self.target
+        )
 
         self.rng = np.random.default_rng(self.seed)
 

Qiskit/qiskit

qiskit/transpiler/passes/routing/basic_swap.py
@@ -72,8 +72,9 @@ class BasicSwap(TransformationPass):
 
         if len(dag.qubits) > len(self.coupling_map.physical_qubits):
             raise TranspilerError("The layout does not match the amount of qubits in the DAG")
-        disjoint_utils.require_layout_isolated_to_component(dag, self.coupling_map)
-
+        disjoint_utils.require_layout_isolated_to_component(
+            dag, self.coupling_map, target=self.target
+        )
         canonical_register = dag.qregs["q"]
         trivial_layout = Layout.generate_trivial_layout(canonical_register)
         current_layout = trivial_layout.copy()

qiskit/transpiler/passes/routing/bip_mapping.py
@@ -167,7 +167,9 @@ class BIPMapping(TransformationPass):
                 "BIPMapping requires the number of virtual and physical qubits to be the same. "
                 "Supply 'qubit_subset' to specify physical qubits to use."
             )
-        disjoint_utils.require_layout_isolated_to_component(dag, self.coupling_map)
+        disjoint_utils.require_layout_isolated_to_component(
+            dag, self.coupling_map, target=self.target
+        )
 
         original_dag = dag
 

qiskit/transpiler/passes/routing/lookahead_swap.py
@@ -130,7 +130,9 @@ class LookaheadSwap(TransformationPass):
                 f"The number of DAG qubits ({len(dag.qubits)}) is greater than the number of "
                 f"available device qubits ({number_of_available_qubits})."
             )
-        disjoint_utils.require_layout_isolated_to_component(dag, self.coupling_map)
+        disjoint_utils.require_layout_isolated_to_component(
+            dag, self.coupling_map, target=self.target
+        )
 
         register = dag.qregs["q"]
         current_state = _SystemState(

qiskit/transpiler/passes/routing/sabre_swap.py
@@ -213,7 +213,9 @@ class SabreSwap(TransformationPass):
             heuristic = Heuristic.Decay
         else:
             raise TranspilerError("Heuristic %s not recognized." % self.heuristic)
-        disjoint_utils.require_layout_isolated_to_component(dag, self.coupling_map)
+        disjoint_utils.require_layout_isolated_to_component(
+            dag, self.coupling_map, target=self.target
+        )
 
         self.dist_matrix = self.coupling_map.distance_matrix
 

qiskit/transpiler/passes/routing/stochastic_swap.py
@@ -105,7 +105,9 @@ class StochasticSwap(TransformationPass):
         if len(dag.qubits) > len(self.coupling_map.physical_qubits):
             raise TranspilerError("The layout does not match the amount of qubits in the DAG")
 
-        disjoint_utils.require_layout_isolated_to_component(dag, self.coupling_map)
+        disjoint_utils.require_layout_isolated_to_component(
+            dag, self.coupling_map, target=self.target
+        )
 
         self.rng = np.random.default_rng(self.seed)
 