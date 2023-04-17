541cb252cddfbcf87369c152d37771bf6f23f560
ElePT <epenatap@gmail.com>
2023-04-17 14:27:34 +0200

    Remove warning filter

--- test/python/opflow/opflow_test_case.py
+++ test/python/opflow/opflow_test_case.py
@@ -19,12 +19,4 @@ from qiskit.test import QiskitTestCase
 class QiskitOpflowTestCase(QiskitTestCase):
     """Opflow test Case"""
 
-    def setUp(self):
-        super().setUp()
-        # ignore opflow msgs
-        warnings.filterwarnings("ignore", category=DeprecationWarning, message=r".*opflow.*")
-
-    def tearDown(self):
-        super().tearDown()
-        # restore opflow msgs
-        warnings.filterwarnings("error", category=DeprecationWarning, message=r".*opflow.*")
+    pass
--- test/python/opflow/test_matrix_expectation.py
+++ test/python/opflow/test_matrix_expectation.py
@@ -14,7 +14,6 @@
 
 import unittest
 from test.python.opflow import QiskitOpflowTestCase
-import warnings
 import itertools
 import numpy as np
 
@@ -46,51 +45,34 @@ class TestMatrixExpectation(QiskitOpflowTestCase):
         super().setUp()
         self.seed = 97
         backend = BasicAer.get_backend("statevector_simulator")
-        with warnings.catch_warnings(record=True) as caught_warnings:
-            warnings.filterwarnings(
-                "always",
-                category=DeprecationWarning,
-            )
+
+        with self.assertWarns(DeprecationWarning):
             q_instance = QuantumInstance(
                 backend, seed_simulator=self.seed, seed_transpiler=self.seed
             )
-        self.assertTrue(len(caught_warnings) > 0)
-        with warnings.catch_warnings(record=True) as caught_warnings:
-            warnings.filterwarnings(
-                "always",
-                category=DeprecationWarning,
-            )
             self.sampler = CircuitSampler(q_instance, attach_results=True)
 
-        self.expect = MatrixExpectation()
-        self.assertTrue(len(caught_warnings) > 0)
+        with self.assertWarns(DeprecationWarning):
+            self.expect = MatrixExpectation()
 
     def test_pauli_expect_pair(self):
         """pauli expect pair test"""
-        op = Z ^ Z
-        # wf = (Pl^Pl) + (Ze^Ze)
-        wf = CX @ (H ^ I) @ Zero
 
-        converted_meas = self.expect.convert(~StateFn(op) @ wf)
-        self.assertAlmostEqual(converted_meas.eval(), 0, delta=0.1)
-        with warnings.catch_warnings(record=True) as caught_warnings:
-            warnings.filterwarnings(
-                "always",
-                category=DeprecationWarning,
-            )
+        with self.assertWarns(DeprecationWarning):
+            op = Z ^ Z
+            # wf = (Pl^Pl) + (Ze^Ze)
+            wf = CX @ (H ^ I) @ Zero
+            converted_meas = self.expect.convert(~StateFn(op) @ wf)
+            self.assertAlmostEqual(converted_meas.eval(), 0, delta=0.1)
             sampled = self.sampler.convert(converted_meas)
-        self.assertTrue(len(caught_warnings) > 0)
-        self.assertAlmostEqual(sampled.eval(), 0, delta=0.1)
+            self.assertAlmostEqual(sampled.eval(), 0, delta=0.1)
 
     def test_pauli_expect_single(self):
         """pauli expect single test"""
-        paulis = [Z, X, Y, I]
-        states = [Zero, One, Plus, Minus, S @ Plus, S @ Minus]
-        with warnings.catch_warnings(record=True) as caught_warnings:
-            warnings.filterwarnings(
-                "always",
-                category=DeprecationWarning,
-            )
+
+        with self.assertWarns(DeprecationWarning):
+            paulis = [Z, X, Y, I]
+            states = [Zero, One, Plus, Minus, S @ Plus, S @ Minus]
             for pauli, state in itertools.product(paulis, states):
                 converted_meas = self.expect.convert(~StateFn(pauli) @ state)
                 matmulmean = state.adjoint().to_matrix() @ pauli.to_matrix() @ state.to_matrix()
@@ -98,18 +80,14 @@ class TestMatrixExpectation(QiskitOpflowTestCase):
 
                 sampled = self.sampler.convert(converted_meas)
                 self.assertAlmostEqual(sampled.eval(), matmulmean, delta=0.1)
-        self.assertTrue(len(caught_warnings) > 0)
 
     def test_pauli_expect_op_vector(self):
         """pauli expect op vector test"""
-        paulis_op = ListOp([X, Y, Z, I])
-        converted_meas = self.expect.convert(~StateFn(paulis_op))
 
-        with warnings.catch_warnings(record=True) as caught_warnings:
-            warnings.filterwarnings(
-                "always",
-                category=DeprecationWarning,
-            )
+        with self.assertWarns(DeprecationWarning):
+            paulis_op = ListOp([X, Y, Z, I])
+            converted_meas = self.expect.convert(~StateFn(paulis_op))
+
             plus_mean = converted_meas @ Plus
             np.testing.assert_array_almost_equal(plus_mean.eval(), [1, 0, 0, 1], decimal=1)
             sampled_plus = self.sampler.convert(plus_mean)
@@ -129,90 +107,76 @@ class TestMatrixExpectation(QiskitOpflowTestCase):
             sum_zero_mean = converted_meas @ sum_zero
             np.testing.assert_array_almost_equal(sum_zero_mean.eval(), [0, 0, 1, 1], decimal=1)
             sampled_zero = self.sampler.convert(sum_zero)
-        self.assertTrue(len(caught_warnings) > 0)
 
-        np.testing.assert_array_almost_equal(
-            (converted_meas @ sampled_zero).eval(), [0, 0, 1, 1], decimal=1
-        )
+            np.testing.assert_array_almost_equal(
+                (converted_meas @ sampled_zero).eval(), [0, 0, 1, 1], decimal=1
+            )
 
-        for i, op in enumerate(paulis_op.oplist):
-            mat_op = op.to_matrix()
-            np.testing.assert_array_almost_equal(
-                zero_mean.eval()[i],
-                Zero.adjoint().to_matrix() @ mat_op @ Zero.to_matrix(),
-                decimal=1,
-            )
-            np.testing.assert_array_almost_equal(
-                plus_mean.eval()[i],
-                Plus.adjoint().to_matrix() @ mat_op @ Plus.to_matrix(),
-                decimal=1,
-            )
-            np.testing.assert_array_almost_equal(
-                minus_mean.eval()[i],
-                Minus.adjoint().to_matrix() @ mat_op @ Minus.to_matrix(),
-                decimal=1,
-            )
+            for i, op in enumerate(paulis_op.oplist):
+                mat_op = op.to_matrix()
+                np.testing.assert_array_almost_equal(
+                    zero_mean.eval()[i],
+                    Zero.adjoint().to_matrix() @ mat_op @ Zero.to_matrix(),
+                    decimal=1,
+                )
+                np.testing.assert_array_almost_equal(
+                    plus_mean.eval()[i],
+                    Plus.adjoint().to_matrix() @ mat_op @ Plus.to_matrix(),
+                    decimal=1,
+                )
+                np.testing.assert_array_almost_equal(
+                    minus_mean.eval()[i],
+                    Minus.adjoint().to_matrix() @ mat_op @ Minus.to_matrix(),
+                    decimal=1,
+                )
 
     def test_pauli_expect_state_vector(self):
         """pauli expect state vector test"""
-        states_op = ListOp([One, Zero, Plus, Minus])
 
-        paulis_op = X
-        converted_meas = self.expect.convert(~StateFn(paulis_op) @ states_op)
-        np.testing.assert_array_almost_equal(converted_meas.eval(), [0, 0, 1, -1], decimal=1)
-
-        with warnings.catch_warnings(record=True) as caught_warnings:
-            warnings.filterwarnings(
-                "always",
-                category=DeprecationWarning,
-            )
+        with self.assertWarns(DeprecationWarning):
+            states_op = ListOp([One, Zero, Plus, Minus])
+            paulis_op = X
+            converted_meas = self.expect.convert(~StateFn(paulis_op) @ states_op)
+            np.testing.assert_array_almost_equal(converted_meas.eval(), [0, 0, 1, -1], decimal=1)
             sampled = self.sampler.convert(converted_meas)
-        self.assertTrue(len(caught_warnings) > 0)
-        np.testing.assert_array_almost_equal(sampled.eval(), [0, 0, 1, -1], decimal=1)
+            np.testing.assert_array_almost_equal(sampled.eval(), [0, 0, 1, -1], decimal=1)
 
-        # Small test to see if execution results are accessible
-        for composed_op in sampled:
-            self.assertIn("statevector", composed_op[1].execution_results)
+            # Small test to see if execution results are accessible
+            for composed_op in sampled:
+                self.assertIn("statevector", composed_op[1].execution_results)
 
     def test_pauli_expect_op_vector_state_vector(self):
         """pauli expect op vector state vector test"""
-        paulis_op = ListOp([X, Y, Z, I])
-        states_op = ListOp([One, Zero, Plus, Minus])
 
-        valids = [[+0, 0, 1, -1], [+0, 0, 0, 0], [-1, 1, 0, -0], [+1, 1, 1, 1]]
-        converted_meas = self.expect.convert(~StateFn(paulis_op))
-        np.testing.assert_array_almost_equal((converted_meas @ states_op).eval(), valids, decimal=1)
+        with self.assertWarns(DeprecationWarning):
+            paulis_op = ListOp([X, Y, Z, I])
+            states_op = ListOp([One, Zero, Plus, Minus])
 
-        with warnings.catch_warnings(record=True) as caught_warnings:
-            warnings.filterwarnings(
-                "always",
-                category=DeprecationWarning,
-            )
+            valids = [[+0, 0, 1, -1], [+0, 0, 0, 0], [-1, 1, 0, -0], [+1, 1, 1, 1]]
+            converted_meas = self.expect.convert(~StateFn(paulis_op))
+            np.testing.assert_array_almost_equal((converted_meas @ states_op).eval(), valids, decimal=1)
+
             sampled = self.sampler.convert(states_op)
-        self.assertTrue(len(caught_warnings) > 0)
-        np.testing.assert_array_almost_equal((converted_meas @ sampled).eval(), valids, decimal=1)
+            np.testing.assert_array_almost_equal((converted_meas @ sampled).eval(), valids, decimal=1)
 
     def test_multi_representation_ops(self):
         """Test observables with mixed representations"""
-        mixed_ops = ListOp([X.to_matrix_op(), H, H + I, X])
-        converted_meas = self.expect.convert(~StateFn(mixed_ops))
 
-        plus_mean = converted_meas @ Plus
-        with warnings.catch_warnings(record=True) as caught_warnings:
-            warnings.filterwarnings(
-                "always",
-                category=DeprecationWarning,
-            )
+        with self.assertWarns(DeprecationWarning):
+            mixed_ops = ListOp([X.to_matrix_op(), H, H + I, X])
+            converted_meas = self.expect.convert(~StateFn(mixed_ops))
+
+            plus_mean = converted_meas @ Plus
             sampled_plus = self.sampler.convert(plus_mean)
-        self.assertTrue(len(caught_warnings) > 0)
-        np.testing.assert_array_almost_equal(
-            sampled_plus.eval(), [1, 0.5**0.5, (1 + 0.5**0.5), 1], decimal=1
-        )
+            np.testing.assert_array_almost_equal(
+                sampled_plus.eval(), [1, 0.5**0.5, (1 + 0.5**0.5), 1], decimal=1
+            )
 
     def test_matrix_expectation_non_hermite_op(self):
         """Test MatrixExpectation for non hermitian operator"""
-        exp = ~StateFn(1j * Z) @ One
-        self.assertEqual(self.expect.convert(exp).eval(), 1j)
+        with self.assertWarns(DeprecationWarning):
+            exp = ~StateFn(1j * Z) @ One
+            self.assertEqual(self.expect.convert(exp).eval(), 1j)
 
 
 if __name__ == "__main__":
