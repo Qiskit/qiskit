1203a3b18551f06325354208274e61d02d8bd584
Kento Ueda <38037695+to24toro@users.noreply.github.com>
2023-04-20 19:04:44 +0900

    Bug fix macros.measure with backendv2 (#9987)
    * create measuregrouping class
    
    * add meas_map.setter in MeasureGrouping class
    
    * macros.measure
    
    * get_qubit_groups
    
    * generate_schedule
    
    * target.add_measuregrouping in backend_compat
    
    * target.add_measuregrouping in backend_converter
    
    * reformat and add docs
    
    * on the way of working on generate_schedule_in_measure
    
    * split measure into measure_v1 and measure_v2
    
    macros.py
    
    target
    
    delete raise statement in measure_v2
    
    modify instructions.Acquire to Acquire
    
    * macros.py
    
    * test_measuregrouping
    
    * bug fix schedule with backendV2 for 0.25.0
    
    * modify comments
    
    * fix name of schedule in test_macros
    
    * delete meas_map as a Target attribute
    
    * minor changes in macros.py
    
    * add test to test_macros.py
    
    * make schedule_remapping_memory_slot private
    
    * delete since field from deprecate_arg
    
    * delete deprecate depcorator
    
    * black macros.py
    
    * revert about target
    
    * modify implementation of qubit_mem_slots
    
    * change the definition of meas_group_set
    
    * black macros.py
    
    * fix meas_group_set
    
    * fix qubit_mem_slots
    
    * reno
    
    * modify unassigned_qubit_indices
    
    * remove list() from unassigned_qubit_indices and unassigned_reg_indices

--- test/python/pulse/test_macros.py
+++ test/python/pulse/test_macros.py
@@ -24,7 +24,7 @@ from qiskit.pulse import (
 )
 from qiskit.pulse import macros
 from qiskit.pulse.exceptions import PulseError
-from qiskit.providers.fake_provider import FakeOpenPulse2Q
+from qiskit.providers.fake_provider import FakeOpenPulse2Q, FakeHanoiV2
 from qiskit.test import QiskitTestCase
 
 
@@ -34,6 +34,7 @@ class TestMeasure(QiskitTestCase):
     def setUp(self):
         super().setUp()
         self.backend = FakeOpenPulse2Q()
+        self.backend_v2 = FakeHanoiV2()
         self.inst_map = self.backend.defaults().instruction_schedule_map
 
     def test_measure(self):
@@ -43,7 +44,6 @@ class TestMeasure(QiskitTestCase):
             self.inst_map.get("measure", [0, 1]).filter(channels=[MeasureChannel(0)]),
             Acquire(10, AcquireChannel(0), MemorySlot(0)),
         )
-
         self.assertEqual(sched.instructions, expected.instructions)
 
     def test_measure_sched_with_qubit_mem_slots(self):
@@ -90,6 +90,78 @@ class TestMeasure(QiskitTestCase):
             macros.measure(qubits=[0], meas_map=self.backend.configuration().meas_map)
         with self.assertRaises(PulseError):
             macros.measure(qubits=[0], inst_map=self.inst_map)
+
+    def test_measure_v2(self):
+        """Test macro - measure with backendV2."""
+        sched = macros.measure(qubits=[0], backend=self.backend_v2)
+        expected = self.backend_v2.target.get_calibration("measure", (0,)).filter(
+            channels=[
+                MeasureChannel(0),
+            ]
+        )
+        measure_duration = expected.filter(instruction_types=[Play]).duration
+        for qubit in range(self.backend_v2.num_qubits):
+            expected += Acquire(measure_duration, AcquireChannel(qubit), MemorySlot(qubit))
+        self.assertEqual(sched.instructions, expected.instructions)
+
+    def test_measure_v2_sched_with_qubit_mem_slots(self):
+        """Test measure with backendV2 and custom qubit_mem_slots."""
+        sched = macros.measure(qubits=[0], backend=self.backend_v2, qubit_mem_slots={0: 2})
+        expected = self.backend_v2.target.get_calibration("measure", (0,)).filter(
+            channels=[
+                MeasureChannel(0),
+            ]
+        )
+        measure_duration = expected.filter(instruction_types=[Play]).duration
+        for qubit in range(self.backend_v2.num_qubits):
+            if qubit == 0:
+                expected += Acquire(measure_duration, AcquireChannel(qubit), MemorySlot(2))
+            elif qubit == 1:
+                expected += Acquire(measure_duration, AcquireChannel(qubit), MemorySlot(0))
+            elif qubit == 2:
+                expected += Acquire(measure_duration, AcquireChannel(qubit), MemorySlot(1))
+            else:
+                expected += Acquire(measure_duration, AcquireChannel(qubit), MemorySlot(qubit))
+        self.assertEqual(sched.instructions, expected.instructions)
+
+    def test_measure_v2_sched_with_meas_map(self):
+        """Test measure with backendV2 custom meas_map as list and dict."""
+        sched_with_meas_map_list = macros.measure(
+            qubits=[0], backend=self.backend_v2, meas_map=[[0, 1]]
+        )
+        sched_with_meas_map_dict = macros.measure(
+            qubits=[0], backend=self.backend_v2, meas_map={0: [0, 1], 1: [0, 1]}
+        )
+        expected = self.backend_v2.target.get_calibration("measure", (0,)).filter(
+            channels=[
+                MeasureChannel(0),
+            ]
+        )
+        measure_duration = expected.filter(instruction_types=[Play]).duration
+        for qubit in range(self.backend_v2.num_qubits):
+            expected += Acquire(measure_duration, AcquireChannel(qubit), MemorySlot(qubit))
+        self.assertEqual(sched_with_meas_map_list.instructions, expected.instructions)
+        self.assertEqual(sched_with_meas_map_dict.instructions, expected.instructions)
+
+    def test_multiple_measure_v2(self):
+        """Test macro - multiple qubit measure with backendV2."""
+        sched = macros.measure(qubits=[0, 1], backend=self.backend_v2)
+        expected = self.backend_v2.target.get_calibration("measure", (0,)).filter(
+            channels=[
+                MeasureChannel(0),
+            ]
+        )
+        expected += self.backend_v2.target.get_calibration("measure", (1,)).filter(
+            channels=[
+                MeasureChannel(1),
+            ]
+        )
+        measure_duration = expected.filter(instruction_types=[Play]).duration
+        expected += Acquire(measure_duration, AcquireChannel(0), MemorySlot(0))
+        expected += Acquire(measure_duration, AcquireChannel(1), MemorySlot(1))
+        for qubit in range(2, self.backend_v2.num_qubits):
+            expected += Acquire(measure_duration, AcquireChannel(qubit), MemorySlot(qubit))
+        self.assertEqual(sched.instructions, expected.instructions)
 
 
 class TestMeasureAll(QiskitTestCase):
