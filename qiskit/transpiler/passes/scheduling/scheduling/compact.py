a88da1f35bff5cd5a7551345ad7ce7e84ec0ea79
Toshinari Itoko <itoko@jp.ibm.com>
2023-04-20 11:36:03 +0900

    Fix a bug in algorithm

--- qiskit/transpiler/passes/scheduling/scheduling/compact.py
+++ qiskit/transpiler/passes/scheduling/scheduling/compact.py
@@ -20,9 +20,40 @@ from qiskit.transpiler.passes.scheduling.scheduling.alap import ALAPScheduleAnal
 
 
 class CompactScheduleAnalysis(BaseScheduler):
-    """Compact scheduling pass, which schedules the head non-conditional gates on each qubit
-    are scheduled as late as possible while the tail non-conditional gates on each qubit
-    are scheduled as soon as possible.
+    """Compact scheduling pass, in which the head non-conditional gates are scheduled
+    as late as possible (ALAP) while the tail non-conditional gates are scheduled
+    as soon as possible (ASAP).
+
+    For example, given a below circuit,
+                    ┌───┐               ┌───┐ ░ ┌─┐
+               q_0: ┤ H ├──■─────────■──┤ H ├─░─┤M├──────
+                    └───┘┌─┴─┐     ┌─┴─┐└───┘ ░ └╥┘┌─┐
+               q_1: ─────┤ X ├──■──┤ X ├──────░──╫─┤M├───
+                    ┌───┐└───┘┌─┴─┐├───┤      ░  ║ └╥┘┌─┐
+               q_2: ┤ H ├─────┤ X ├┤ H ├──────░──╫──╫─┤M├
+                    └───┘     └───┘└───┘      ░  ║  ║ └╥┘
+            meas: 3/═════════════════════════════╩══╩══╩═
+                                                 0  1  2
+    Compact scheduling algorithm schedules it as below.
+                          ┌───┐            ┌────────────────┐           ┌───┐        ░ ┌─┐
+               q_0: ──────┤ H ├─────────■──┤ Delay(900[dt]) ├──■────────┤ H ├────────░─┤M├──────
+                    ┌─────┴───┴──────┐┌─┴─┐└────────────────┘┌─┴─┐┌─────┴───┴──────┐ ░ └╥┘┌─┐
+               q_1: ┤ Delay(200[dt]) ├┤ X ├────────■─────────┤ X ├┤ Delay(200[dt]) ├─░──╫─┤M├───
+                    ├────────────────┤├───┤      ┌─┴─┐       ├───┤├────────────────┤ ░  ║ └╥┘┌─┐
+               q_2: ┤ Delay(700[dt]) ├┤ H ├──────┤ X ├───────┤ H ├┤ Delay(700[dt]) ├─░──╫──╫─┤M├
+                    └────────────────┘└───┘      └───┘       └───┘└────────────────┘ ░  ║  ║ └╥┘
+            meas: 3/════════════════════════════════════════════════════════════════════╩══╩══╩═
+                                                                                        0  1  2
+    Notice that the first Hadamard on qubit 2 ALAP while the second Hadamard on qubit 2 ASAP.
+
+    Note that the specification of Compact scheduling defines only how to schedule the head and
+    the tail gates. That means there is no specification for how to schedule gates in the middle
+    (e.g. if there is one more Hadamard on qubit 0 between two CNOT gates, its position may vary
+    depending on the implementation).
+
+    Also note that conditional gates and non-``Gate`` operations are not taken into accounts in
+    Compact scheduling. That means they are scheduled (without unnecessary delays) but may not
+    in the compact manner.
     """
 
     def run(self, dag):
@@ -46,17 +77,17 @@ class CompactScheduleAnalysis(BaseScheduler):
         alap_schedule.run(dag)
         node_start_time = alap_schedule.property_set["node_start_time"]
 
-        # move floating non-conditional gate towards front as possible
+        # move floating non-conditional gate forward as possible
         bit_indices = {bit: index for index, bit in enumerate(dag.qubits)}
-        for node in reversed(list(dag.topological_op_nodes())):
+        for node in dag.topological_op_nodes():
             if not isinstance(node.op, Gate) or node.op.condition_bits:
                 continue
 
-            start_time = node_start_time[node]
             asap_time = 0
+            start_time = node_start_time[node]
             for prev in dag.predecessors(node):
                 if isinstance(prev, DAGInNode):
-                    asap_time = start_time
+                    asap_time = start_time  # no update of node_start_time[node]
                     break
 
                 prev_duration = self._get_node_duration(prev, bit_indices, dag)
