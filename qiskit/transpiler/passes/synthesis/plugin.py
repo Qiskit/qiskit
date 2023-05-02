Qiskit/qiskit

commit 6da214185414bc82d42a3a70027fc541c2dc894c

two-qubit unitary synthesis adapted to errors and over-complete gatesets (#9175)
* make Optimize1qGatesDecomposition target aware

* choose decomopsition based on fidelity

* tests for the target path

* choosing XXDecomposition

* add known embodiments as a library

* choosing between controlled and supercontrolled decomposers

* add known embodiments. add basis_fidelity to init of XXDecomposer

* set basis_fidelity at init of all decomposers, not per call

* choose euler_basis based on 1q error rates

* make backup_optimizer in XXDecomposer pulse_efficient

* revert changes to one_qubit_euler_decomposer.py

* use Optimize1qGatesDecomposition when UnitarySynthesis hits a 1-qubit unitary

* build all the decomposers, then choose after running them

* add a function for computing circuit error from target

* run all available decomposers, fix synth direction if needed, choose min cost

* xx embodiment for itself

* add ECR embodiment and fix some tests

* add ECR embodiment and fix some tests

* fix preferred_direction

* no decomposition if no basis

* release notes

* remove unnecessary GateDirection from level 3. Add a test for targeting a single fractional gate, no full entangler

* change gate_lengths and gate_errors format, and update _preferred_direction and _error

* remove accidental test duplicates

* small lint fixes

* approximation degree

* rebase

* add approximate=True/False arg to TwoQubitBasisDecomposer

* black

* review comments

* approximation_degree should default to 1, not None

* type hints

* fix a test that was wrong in test_optimize_1q_commutation

* one qubit unitary corner case is decomposed with UnitarySynthesis but not with Optimize1qGatesDecomposition

* one more test for approx

* lint

* release note format

qiskit/transpiler/passes/synthesis/plugin.py
@@ -346,11 +346,11 @@ class UnitarySynthesisPlugin(abc.ABC):
         """Return whether the plugin supports taking ``gate_lengths``
 
         ``gate_lengths`` will be a dictionary in the form of
-        ``{gate_name: {(qubit_1, qubit_2): length}}``. For example::
+        ``{(qubits,): [Gate, length]}``. For example::
 
             {
-            'sx': {(0,): 0.0006149355812506126, (1,): 0.0006149355812506126},
-            'cx': {(0, 1): 0.012012477900732316, (1, 0): 5.191111111111111e-07}
+            (0,): [SXGate(): 0.0006149355812506126, RZGate(): 0.0],
+            (0, 1): [CXGate(): 0.012012477900732316]
             }
 
         where the ``length`` value is in units of seconds.
@@ -367,11 +367,11 @@ class UnitarySynthesisPlugin(abc.ABC):
         """Return whether the plugin supports taking ``gate_errors``
 
         ``gate_errors`` will be a dictionary in the form of
-        ``{gate_name: {(qubit_1, qubit_2): error}}``. For example::
+        ``{(qubits,): [Gate, error]}``. For example::
 
             {
-            'sx': {(0,): 0.0006149355812506126, (1,): 0.0006149355812506126},
-            'cx': {(0, 1): 0.012012477900732316, (1, 0): 5.191111111111111e-07}
+            (0,): [SXGate(): 0.0006149355812506126, RZGate(): 0.0],
+            (0, 1): [CXGate(): 0.012012477900732316]
             }
 
         Do note that this dictionary might not be complete or could be empty

releasenotes/notes/unitary-synthesis-based-on-errors-8041fcc9584f5db2.yaml
@@ -1,0 +1,19 @@
+---
+features:
+  - |
+    The :class:`~.DefaultUnitarySynthesis` transpiler pass now chooses
+    one and two-qubit unitary synthesis based on the basis errors, if they
+    are reported in the target. In particular, it runs all possible synthesis
+    and chooses the cheapest option.
+    For a one-qubit decomposition, it can target Pauli basis
+    (e.g. RZ-RX-RZ or RZ-RY-RZ), generic unitary basis (e.g. U), and a
+    few others.
+    For a two-qubit decomposition, it can target any supercontrolled basis
+    (e.g. CNOT, iSWAP, B) or multiple controlled basis 
+    (e.g. CZ, CH, ZZ^.5, ZX^.2, etc.).
+upgrade:
+  - |
+    The interface for :class:`~.UnitarySynthesisPlugin` has been changed so
+    now `gate_lengths` and `gate_errors` have the form
+    ``{(qubits,): [Gate, length]}``. This allows gates that have the same
+    name but different parameters to be represented separately.

qiskit/quantum_info/synthesis/__init__.py
@@ -16,4 +16,4 @@ from .two_qubit_decompose import TwoQubitBasisDecomposer, two_qubit_cnot_decompo
 from .one_qubit_decompose import OneQubitEulerDecomposer
 from .quaternion import Quaternion
 from .clifford_decompose import decompose_clifford
-from .xx_decompose import XXDecomposer
+from .xx_decompose.decomposer import XXDecomposer

qiskit/quantum_info/synthesis/two_qubit_decompose.py
@@ -28,14 +28,13 @@ import math
 import io
 import base64
 import warnings
-from typing import ClassVar, Optional, Type
+from typing import ClassVar, Optional, Type, Union
 
 import logging
 
 import numpy as np
 
-from qiskit.circuit.quantumregister import QuantumRegister
-from qiskit.circuit.quantumcircuit import QuantumCircuit, Gate
+from qiskit.circuit import QuantumRegister, QuantumCircuit, Gate
 from qiskit.circuit.library.standard_gates import CXGate, RXGate, RYGate, RZGate
 from qiskit.exceptions import QiskitError
 from qiskit.quantum_info.operators import Operator
@@ -44,6 +43,8 @@ from qiskit.quantum_info.synthesis.one_qubit_decompose import (
     OneQubitEulerDecomposer,
     DEFAULT_ATOL,
 )
+from qiskit.utils.deprecation import deprecate_arguments
+
 
 logger = logging.getLogger(__name__)
 
@@ -854,7 +855,6 @@ class TwoQubitBasisDecomposer:
         basis_fidelity (float): Fidelity to be assumed for applications of KAK Gate. Default 1.0.
         euler_basis (str): Basis string to be provided to OneQubitEulerDecomposer for 1Q synthesis.
             Valid options are ['ZYZ', 'ZXZ', 'XYX', 'U', 'U3', 'U1X', 'PSX', 'ZSX', 'RR'].
-            Default 'U3'.
         pulse_optimize (None or bool): If True, try to do decomposition which minimizes
             local unitaries in between entangling gates. This will raise an exception if an
             optimal decomposition is not implemented. Currently, only [{CX, SX, RZ}] is known.
@@ -862,16 +862,19 @@ class TwoQubitBasisDecomposer:
             if unknown.
     """
 
-    def __init__(self, gate, basis_fidelity=1.0, euler_basis=None, pulse_optimize=None):
+    def __init__(
+        self,
+        gate: Gate,
+        basis_fidelity: float = 1.0,
+        euler_basis: str = "U",
+        pulse_optimize: Optional[bool] = None,
+    ):
         self.gate = gate
         self.basis_fidelity = basis_fidelity
         self.pulse_optimize = pulse_optimize
 
         basis = self.basis = TwoQubitWeylDecomposition(Operator(gate).data)
-        if euler_basis is not None:
-            self._decomposer1q = OneQubitEulerDecomposer(euler_basis)
-        else:
-            self._decomposer1q = OneQubitEulerDecomposer("U3")
+        self._decomposer1q = OneQubitEulerDecomposer(euler_basis)
 
         # FIXME: find good tolerances
         self.is_supercontrolled = math.isclose(basis.a, np.pi / 4) and math.isclose(basis.c, 0.0)
@@ -1082,16 +1085,35 @@ class TwoQubitBasisDecomposer:
 
         return U3r, U3l, U2r, U2l, U1r, U1l, U0r, U0l
 
-    def __call__(self, target, basis_fidelity=None, *, _num_basis_uses=None) -> QuantumCircuit:
-        """Decompose a two-qubit unitary over fixed basis + SU(2) using the best approximation given
-        that each basis application has a finite fidelity.
+    @deprecate_arguments({"target": "unitary"})
+    def __call__(
+        self,
+        unitary: Union[Operator, np.ndarray],
+        basis_fidelity: Optional[float] = None,
+        approximate: bool = True,
+        *,
+        _num_basis_uses: int = None,
+    ) -> QuantumCircuit:
+        """Decompose a two-qubit `unitary` over fixed basis + SU(2) using the best approximation given
+        that each basis application has a finite `basis_fidelity`.
 
-        You can force a particular approximation by passing _num_basis_uses.
+        Args:
+            unitary (Operator or ndarray): 4x4 unitary to synthesize.
+            basis_fidelity (float or None): Fidelity to be assumed for applications of KAK Gate.
+                If given, overrides basis_fidelity given at init.
+            approximate (bool): Approximates if basis fidelities are less than 1.0.
+            _num_basis_uses (int): force a particular approximation by passing a number in [0, 3].
+        Returns:
+            QuantumCircuit: Synthesized circuit.
+        Raises:
+            QiskitError: if pulse_optimize is True but we don't know how to do it.
         """
         basis_fidelity = basis_fidelity or self.basis_fidelity
-        target = np.asarray(target, dtype=complex)
+        if approximate is False:
+            basis_fidelity = 1.0
+        unitary = np.asarray(unitary, dtype=complex)
 
-        target_decomposed = TwoQubitWeylDecomposition(target)
+        target_decomposed = TwoQubitWeylDecomposition(unitary)
         traces = self.traces(target_decomposed)
         expected_fidelities = [trace_to_fid(traces[i]) * basis_fidelity**i for i in range(4)]
 

qiskit/quantum_info/synthesis/xx_decompose/__init__.py
@@ -15,3 +15,4 @@ Submodule symbol exports for XX decomposition.
 """
 
 from .decomposer import XXDecomposer
+from .embodiments import XXEmbodiments

qiskit/quantum_info/synthesis/xx_decompose/decomposer.py
@@ -17,7 +17,7 @@ Driver for a synthesis routine which emits optimal XX-based circuits.
 import heapq
 import math
 from operator import itemgetter
-from typing import Callable, Optional
+from typing import Callable, Optional, Union
 
 import numpy as np
 
@@ -58,6 +58,9 @@ class XXDecomposer:
     (i.e., each locally equivalent to CAN(alpha, 0, 0) for a possibly varying alpha).
 
     Args:
+        basis_fidelity: available strengths and fidelity of each.
+            Can be either (1) a dictionary mapping XX angle values to fidelity at that angle; or
+            (2) a single float f, interpreted as {pi: f, pi/2: f/2, pi/3: f/3}.
         euler_basis: Basis string provided to OneQubitEulerDecomposer for 1Q synthesis.
             Defaults to "U".
         embodiments: A dictionary mapping interaction strengths alpha to native circuits which
@@ -75,6 +78,7 @@ class XXDecomposer:
 
     def __init__(
         self,
+        basis_fidelity: Union[dict, float] = 1.0,
         euler_basis: str = "U",
         embodiments: Optional[dict] = None,
         backup_optimizer: Optional[Callable] = None,
@@ -84,9 +88,18 @@ class XXDecomposer:
         )
 
         self._decomposer1q = Optimize1qGatesDecomposition(ONE_QUBIT_EULER_BASIS_GATES[euler_basis])
-        self.gate = RZXGate(np.pi / 2)
         self.embodiments = embodiments if embodiments is not None else {}
         self.backup_optimizer = backup_optimizer
+        self.basis_fidelity = basis_fidelity
+
+        # expose one of the basis gates so others can know what this decomposer targets
+        embodiment_circuit = next(iter(self.embodiments.items()), ([], []))[1]
+        for instruction in embodiment_circuit:
+            if len(instruction.qubits) == 2:
+                self.gate = instruction.operation
+                break
+        else:
+            self.gate = RZXGate(np.pi / 2)
 
         self._check_embodiments()
 
@@ -180,7 +193,7 @@ class XXDecomposer:
     @staticmethod
     def _strength_to_infidelity(basis_fidelity, approximate=False):
         """
-        Converts a dictionary mapping ZX strengths to fidelities to a dictionary mapping ZX
+        Converts a dictionary mapping XX strengths to fidelities to a dictionary mapping XX
         strengths to infidelities. Also supports one of the other formats Qiskit uses: if only a
         lone float is supplied, it extends it from CX over CX/2 and CX/3 by linear decay.
         """
@@ -202,7 +215,12 @@ class XXDecomposer:
 
         raise TypeError("Unknown basis_fidelity payload.")
 
-    def __call__(self, unitary, basis_fidelity=1.0, approximate=True):
+    def __call__(
+        self,
+        unitary: Union[Operator, np.ndarray],
+        basis_fidelity: Optional[Union[dict, float]] = None,
+        approximate: bool = True,
+    ):
         """
         Fashions a circuit which (perhaps `approximate`ly) models the special unitary operation
         `unitary`, using the circuit templates supplied at initialization as `embodiments`.  The
@@ -215,14 +233,17 @@ class XXDecomposer:
             unitary (Operator or ndarray): 4x4 unitary to synthesize.
             basis_fidelity (dict or float): Fidelity of basis gates. Can be either (1) a dictionary
                 mapping XX angle values to fidelity at that angle; or (2) a single float f,
-                interpreted as {pi: f, pi/2: f/2, pi/3: f/3} .
+                interpreted as {pi: f, pi/2: f/2, pi/3: f/3}.
+                If given, overrides the basis_fidelity given at init.
             approximate (bool): Approximates if basis fidelities are less than 1.0 .
         Returns:
             QuantumCircuit: Synthesized circuit.
         """
+        basis_fidelity = basis_fidelity or self.basis_fidelity
         strength_to_infidelity = self._strength_to_infidelity(
             basis_fidelity, approximate=approximate
         )
+
         from qiskit.extensions import UnitaryGate  # pylint: disable=cyclic-import
 
         # get the associated _positive_ canonical coordinate
@@ -242,8 +263,12 @@ class XXDecomposer:
         }
         circuit = canonical_xx_circuit(best_point, best_sequence, embodiments)
 
-        if best_sequence == [np.pi / 2, np.pi / 2, np.pi / 2] and self.backup_optimizer is not None:
-            return self.backup_optimizer(unitary, basis_fidelity=basis_fidelity)
+        if (
+            best_sequence in ([np.pi / 2, np.pi / 2, np.pi / 2], [np.pi / 2, np.pi / 2])
+            and self.backup_optimizer is not None
+        ):
+            pi2_fidelity = 1 - strength_to_infidelity[np.pi / 2]
+            return self.backup_optimizer(unitary, basis_fidelity=pi2_fidelity)
 
         # change to positive canonical coordinates
         if weyl_decomposition.c >= -EPSILON:

qiskit/quantum_info/synthesis/xx_decompose/embodiments.py
@@ -1,0 +1,162 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2022
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+A library of known embodiments of RXXGate in terms of other gates,
+for some generic or specific angles.
+
+TODO: discover these automatically from the gates' algebraic definition
+"""
+
+import numpy as np
+
+from qiskit.circuit import QuantumCircuit, Parameter
+from qiskit.circuit.library import (
+    RXXGate,
+    RZZGate,
+    RZXGate,
+    RYYGate,
+    CRZGate,
+    CRXGate,
+    CRYGate,
+    CPhaseGate,
+    CZGate,
+    CXGate,
+    CYGate,
+    CHGate,
+    ECRGate,
+)
+
+
+rxx_circuit = QuantumCircuit(2)
+theta = Parameter("θ")
+rxx_circuit.rxx(theta, 0, 1)
+
+rzz_circuit = QuantumCircuit(2)
+theta = Parameter("θ")
+rzz_circuit.h(0)
+rzz_circuit.h(1)
+rzz_circuit.rzz(theta, 0, 1)
+rzz_circuit.h(0)
+rzz_circuit.h(1)
+
+rzx_circuit = QuantumCircuit(2)
+rzx_circuit.h(0)
+rzx_circuit.rzx(theta, 0, 1)
+rzx_circuit.h(0)
+
+ryy_circuit = QuantumCircuit(2)
+ryy_circuit.s(0)
+ryy_circuit.s(1)
+ryy_circuit.ryy(theta, 0, 1)
+ryy_circuit.sdg(0)
+ryy_circuit.sdg(1)
+
+cphase_circuit = QuantumCircuit(2)
+cphase_circuit.h(0)
+cphase_circuit.h(1)
+cphase_circuit.cp(-2 * theta, 0, 1)
+cphase_circuit.rz(theta, 0)
+cphase_circuit.rz(theta, 1)
+cphase_circuit.h(0)
+cphase_circuit.h(1)
+cphase_circuit.global_phase += theta / 2
+
+crz_circuit = QuantumCircuit(2)
+crz_circuit.h(0)
+crz_circuit.h(1)
+crz_circuit.crz(-2 * theta, 0, 1)
+crz_circuit.rz(theta, 1)
+crz_circuit.h(0)
+crz_circuit.h(1)
+
+crx_circuit = QuantumCircuit(2)
+crx_circuit.h(0)
+crx_circuit.crx(-2 * theta, 0, 1)
+crx_circuit.rx(theta, 1)
+crx_circuit.h(0)
+
+cry_circuit = QuantumCircuit(2)
+cry_circuit.h(0)
+cry_circuit.s(1)
+cry_circuit.cry(-2 * theta, 0, 1)
+cry_circuit.ry(theta, 1)
+cry_circuit.h(0)
+cry_circuit.sdg(1)
+
+cz_circuit = QuantumCircuit(2)
+cz_circuit.h(0)
+cz_circuit.h(1)
+cz_circuit.cz(0, 1)
+cz_circuit.s(0)
+cz_circuit.s(1)
+cz_circuit.h(0)
+cz_circuit.h(1)
+cz_circuit.global_phase -= np.pi / 4
+
+cx_circuit = QuantumCircuit(2)
+cx_circuit.h(0)
+cx_circuit.cx(0, 1)
+cx_circuit.s(0)
+cx_circuit.sx(1)
+cx_circuit.h(0)
+cx_circuit.global_phase -= np.pi / 4
+
+cy_circuit = QuantumCircuit(2)
+cy_circuit.h(0)
+cy_circuit.s(1)
+cy_circuit.cy(0, 1)
+cy_circuit.s(0)
+cy_circuit.sdg(1)
+cy_circuit.sx(1)
+cy_circuit.h(0)
+cy_circuit.global_phase -= np.pi / 4
+
+ch_circuit = QuantumCircuit(2)
+ch_circuit.h(0)
+ch_circuit.tdg(1)
+ch_circuit.h(1)
+ch_circuit.sdg(1)
+ch_circuit.ch(0, 1)
+ch_circuit.s(0)
+ch_circuit.s(1)
+ch_circuit.h(1)
+ch_circuit.t(1)
+ch_circuit.sx(1)
+ch_circuit.h(0)
+ch_circuit.global_phase -= np.pi / 4
+
+ecr_circuit = QuantumCircuit(2)
+ecr_circuit.h(0)
+ecr_circuit.s(0)
+ecr_circuit.x(0)
+ecr_circuit.x(1)
+ecr_circuit.ecr(0, 1)
+ecr_circuit.s(0)
+ecr_circuit.h(0)
+ecr_circuit.global_phase -= np.pi / 2
+
+XXEmbodiments = {
+    RXXGate: rxx_circuit,
+    RYYGate: ryy_circuit,
+    RZZGate: rzz_circuit,
+    RZXGate: rzx_circuit,
+    CRXGate: crx_circuit,
+    CRYGate: cry_circuit,
+    CRZGate: crz_circuit,
+    CPhaseGate: cphase_circuit,
+    CXGate: cx_circuit,
+    CYGate: cy_circuit,
+    CZGate: cz_circuit,
+    CHGate: ch_circuit,
+    ECRGate: ecr_circuit,
+}

commit 77323ade32b45d91266a94790cf0f34f20ff11ae

Undo breaking API change in unitary synthesis plugin interface
This commit reverts an unecessary breaking API change that was
introduced in #9175. In that PR the data type for two arguments,
gate_lengths and gate_errors, that was potentially passed to
plugins was changed. This change was well intentioned as it was
used by the default plugin as part of the changes to the default
plugin made in that PR. But this neglected the downstream effects
for any existing plugin interface users relying on the input to
those fields. Making this change to existing fields would break
any plugins that were using the old data format and is a violation
of the Qiskit stability and deprecation policy. This commit reverts
that change so that gate_lengths and gate_errors contain the same data
format as in previous releases. Instead to facilitate the new
workflow a new fields, gate_lengths_by_qubit and gate_errors_by_qubit,
were added that leverage the new format introduced by #9175. By adding
this as new optional fields existing users can continue to work as
before, but the default plugin which requires the different data
format can use the new data type.

qiskit/transpiler/passes/synthesis/plugin.py
@@ -85,6 +85,14 @@ something like::
             return False
 
         @property
+        def supports_gate_lengths_by_qubit(self):
+            return False
+
+        @property
+        def supports_gate_errors_by_qubit(self):
+            return False
+
+        @property
         def min_qubits(self):
             return None
 
@@ -341,8 +349,7 @@ class UnitarySynthesisPlugin(abc.ABC):
         pass
 
     @property
-    @abc.abstractmethod
-    def supports_gate_lengths(self):
+    def supports_gate_lengths_by_qubit(self):
         """Return whether the plugin supports taking ``gate_lengths``
 
         ``gate_lengths`` will be a dictionary in the form of
@@ -359,11 +366,10 @@ class UnitarySynthesisPlugin(abc.ABC):
         as it depends on the target backend reporting gate lengths on every
         gate for each qubit.
         """
-        pass
+        return False
 
     @property
-    @abc.abstractmethod
-    def supports_gate_errors(self):
+    def supports_gate_errors_by_qubit(self):
         """Return whether the plugin supports taking ``gate_errors``
 
         ``gate_errors`` will be a dictionary in the form of
@@ -372,6 +378,48 @@ class UnitarySynthesisPlugin(abc.ABC):
             {
             (0,): [SXGate(): 0.0006149355812506126, RZGate(): 0.0],
             (0, 1): [CXGate(): 0.012012477900732316]
+            }
+
+        Do note that this dictionary might not be complete or could be empty
+        as it depends on the target backend reporting gate errors on every
+        gate for each qubit. The gate error rates reported in ``gate_errors``
+        are provided by the target device ``Backend`` object and the exact
+        meaning might be different depending on the backend.
+        """
+        return False
+
+    @property
+    @abc.abstractmethod
+    def supports_gate_lengths(self):
+        """Return whether the plugin supports taking ``gate_lengths``
+
+        ``gate_lengths`` will be a dictionary in the form of
+        ``{gate_name: {(qubit_1, qubit_2): length}}``. For example::
+
+            {
+            'sx': {(0,): 0.0006149355812506126, (1,): 0.0006149355812506126},
+            'cx': {(0, 1): 0.012012477900732316, (1, 0): 5.191111111111111e-07}
+            }
+
+        where the ``length`` value is in units of seconds.
+
+        Do note that this dictionary might not be complete or could be empty
+        as it depends on the target backend reporting gate lengths on every
+        gate for each qubit.
+        """
+        pass
+
+    @property
+    @abc.abstractmethod
+    def supports_gate_errors(self):
+        """Return whether the plugin supports taking ``gate_errors``
+
+        ``gate_errors`` will be a dictionary in the form of
+        ``{gate_name: {(qubit_1, qubit_2): error}}``. For example::
+
+            {
+            'sx': {(0,): 0.0006149355812506126, (1,): 0.0006149355812506126},
+            'cx': {(0, 1): 0.012012477900732316, (1, 0): 5.191111111111111e-07}
             }
 
         Do note that this dictionary might not be complete or could be empty

qiskit/transpiler/passes/synthesis/unitary_synthesis.py
@@ -386,6 +386,7 @@ class UnitarySynthesis(TransformationPass):
             plugin_method = DefaultUnitarySynthesis()
         plugin_kwargs = {"config": self._plugin_config}
         _gate_lengths = _gate_errors = None
+        _gate_lengths_by_qubit = _gate_errors_by_qubit = None
 
         if self.method == "default":
             # If the method is the default, we only need to evaluate one set of keyword arguments.
@@ -417,6 +418,16 @@ class UnitarySynthesis(TransformationPass):
             if method.supports_gate_errors:
                 _gate_errors = _gate_errors or _build_gate_errors(self._backend_props, self._target)
                 kwargs["gate_errors"] = _gate_errors
+            if method.supports_gate_lengths_by_qubit:
+                _gate_lengths_by_qubit = _gate_lengths_by_qubit or _build_gate_lengths_by_qubit(
+                    self._backend_props, self._target
+                )
+                kwargs["gate_lengths_by_qubit"] = _gate_lengths_by_qubit
+            if method.supports_gate_errors_by_qubit:
+                _gate_errors_by_qubit = _gate_errors_by_qubit or _build_gate_errors_by_qubit(
+                    self._backend_props, self._target
+                )
+                kwargs["gate_errors_by_qubit"] = _gate_errors_by_qubit
             supported_bases = method.supported_bases
             if supported_bases is not None:
                 kwargs["matched_basis"] = _choose_bases(self._basis_gates, supported_bases)
@@ -479,6 +490,46 @@ class UnitarySynthesis(TransformationPass):
 
 
 def _build_gate_lengths(props=None, target=None):
+    gate_lengths = {}
+    if target is not None:
+        for gate, prop_dict in target.items():
+            gate_lengths[gate] = {}
+            for qubit, gate_props in prop_dict.items():
+                if gate_props is not None and gate_props.duration is not None:
+                    gate_lengths[gate][qubit] = gate_props.duration
+    elif props is not None:
+        for gate in props._gates:
+            gate_lengths[gate] = {}
+            for k, v in props._gates[gate].items():
+                length = v.get("gate_length")
+                if length:
+                    gate_lengths[gate][k] = length[0]
+            if not gate_lengths[gate]:
+                del gate_lengths[gate]
+    return gate_lengths
+
+
+def _build_gate_errors(props=None, target=None):
+    gate_errors = {}
+    if target is not None:
+        for gate, prop_dict in target.items():
+            gate_errors[gate] = {}
+            for qubit, gate_props in prop_dict.items():
+                if gate_props is not None and gate_props.error is not None:
+                    gate_errors[gate][qubit] = gate_props.error
+    if props is not None:
+        for gate in props._gates:
+            gate_errors[gate] = {}
+            for k, v in props._gates[gate].items():
+                error = v.get("gate_error")
+                if error:
+                    gate_errors[gate][k] = error[0]
+            if not gate_errors[gate]:
+                del gate_errors[gate]
+    return gate_errors
+
+
+def _build_gate_lengths_by_qubit(props=None, target=None):
     """
     Builds a `gate_lengths` dictionary from either `props` (BackendV1)
     or `target (BackendV2)`.
@@ -511,7 +562,7 @@ def _build_gate_lengths(props=None, target=None):
     return gate_lengths
 
 
-def _build_gate_errors(props=None, target=None):
+def _build_gate_errors_by_qubit(props=None, target=None):
     """
     Builds a `gate_error` dictionary from either `props` (BackendV1)
     or `target (BackendV2)`.
@@ -565,10 +616,18 @@ class DefaultUnitarySynthesis(plugin.UnitarySynthesisPlugin):
 
     @property
     def supports_gate_lengths(self):
-        return True
+        return False
 
     @property
     def supports_gate_errors(self):
+        return False
+
+    @property
+    def supports_gate_lengths_by_qubit(self):
+        return True
+
+    @property
+    def supports_gate_errors_by_qubit(self):
         return True
 
     @property
@@ -733,8 +792,8 @@ class DefaultUnitarySynthesis(plugin.UnitarySynthesisPlugin):
         coupling_map = options["coupling_map"][0]
         natural_direction = options["natural_direction"]
         pulse_optimize = options["pulse_optimize"]
-        gate_lengths = options["gate_lengths"]
-        gate_errors = options["gate_errors"]
+        gate_lengths = options["gate_lengths_by_qubit"]
+        gate_errors = options["gate_errors_by_qubit"]
         qubits = options["coupling_map"][1]
         target = options["target"]
 

releasenotes/notes/0.24/unitary-synthesis-based-on-errors-8041fcc9584f5db2.yaml
@@ -9,11 +9,17 @@ features:
     (e.g. RZ-RX-RZ or RZ-RY-RZ), generic unitary basis (e.g. U), and a
     few others.
     For a two-qubit decomposition, it can target any supercontrolled basis
-    (e.g. CNOT, iSWAP, B) or multiple controlled basis 
+    (e.g. CNOT, iSWAP, B) or multiple controlled basis
     (e.g. CZ, CH, ZZ^.5, ZX^.2, etc.).
-upgrade:
   - |
-    The interface for :class:`~.UnitarySynthesisPlugin` has been changed so
-    now `gate_lengths` and `gate_errors` have the form
-    ``{(qubits,): [Gate, length]}``. This allows gates that have the same
-    name but different parameters to be represented separately.
+    The interface for :class:`~.UnitarySynthesisPlugin` has two new
+    optional properties ``supports_gate_lengths_by_qubit`` and
+    ``supports_gate_errors_by_qubit`` which when set will add the
+    fields ``gate_lengths_by_qubit`` and ``gate_errors_by_qubit``
+    respectively to the input options to the plugin's ``run()`` method.
+    These new fields are an alternative view of the data provided by
+    ``gate_lengths`` and ``gate_errors`` but instead have the form:
+    ``{(qubits,): [Gate, length]}`` (where ``Gate`` is the instance
+    of :class:`~.Gate` for that definition). This allows plugins to
+    reason about working with gates of the same type but but that
+    have different parameters set.

test/python/transpiler/test_unitary_synthesis_plugin.py
@@ -226,8 +226,8 @@ class TestUnitarySynthesisPlugin(QiskitTestCase):
         expected_kwargs = [
             "basis_gates",
             "coupling_map",
-            "gate_errors",
-            "gate_lengths",
+            "gate_errors_by_qubit",
+            "gate_lengths_by_qubit",
             "natural_direction",
             "pulse_optimize",
         ]
@@ -294,8 +294,8 @@ class TestUnitarySynthesisPlugin(QiskitTestCase):
         expected_kwargs = [
             "basis_gates",
             "coupling_map",
-            "gate_errors",
-            "gate_lengths",
+            "gate_errors_by_qubit",
+            "gate_lengths_by_qubit",
             "natural_direction",
             "pulse_optimize",
         ]