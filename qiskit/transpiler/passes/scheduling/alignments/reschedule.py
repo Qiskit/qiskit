b1f9c7c7c63943265e86ca37ffebca1616dde9bb
AlexanderIvrii <alexi@il.ibm.com>
2023-05-07 09:59:46 +0300

    clarifying docstring

--- qiskit/transpiler/passes/scheduling/alignments/reschedule.py
+++ qiskit/transpiler/passes/scheduling/alignments/reschedule.py
@@ -182,9 +182,9 @@ class ConstrainedReschedule(AnalysisPass):
 
         This pass should perform rescheduling to satisfy:
 
-            - All DAGOpNode are placed at start time satisfying hardware alignment constraints.
-            - The end time of current does not overlap with the start time of successor nodes.
-            - Compiler directives are not necessary satisfying the constraints.
+            - All DAGOpNode nodes (except for compiler directives) are placed at start time
+              satisfying hardware alignment constraints.
+            - The end time of a node does not overlap with the start time of successor nodes.
 
         Assumptions:
 
@@ -192,19 +192,19 @@ class ConstrainedReschedule(AnalysisPass):
             - All bits in either qargs or cargs associated with node synchronously start.
             - Start time of qargs and cargs may different due to I/O latency.
 
-        Based on the configurations above, rescheduler pass takes following strategy.
+        Based on the configurations above, the rescheduler pass takes the following strategy:
 
-        1. Scan node from the beginning, i.e. from left of the circuit. The rescheduler
-            calls ``node_start_time`` from the property set,
-            and retrieves the scheduled start time of current node.
-        2. If the start time of the node violates the alignment constraints,
-            the scheduler increases the start time until it satisfies the constraint.
-        3. Check overlap with successor nodes. If any overlap occurs, the rescheduler
-            pushes the successor nodes backward towards the end of the wire.
-            Note that shifted location doesn't need to satisfy the constraints,
-            thus it will be a minimum delay to resolve the overlap with the ancestor node.
-        4. Repeat 1-3 until the node at the end of the wire. This will resolve
-            all misalignment without creating overlap between the nodes.
+        1. The nodes are processed in the topological order, from the beginning of
+            the circuit (i.e. from left to right). For every node (including compiler
+            directives), the function ``_push_node_back`` performs steps 2 and 3.
+        2. If the start time of the node violates the alignment constraint,
+            the start time is increased to satisfy the constraint.
+        3. Each immediate successor whose start_time overlaps the node's end_time is
+            pushed backwards (towards the end of the wire). Note that at this point
+            the shifted successor does not need to satisfy the constraints, but this
+            will be taken care of when that successor node itself is processed.
+        4. After every node is processed, all misalignment constraints will be resolved,
+            and there will be no overlap between the nodes.
 
         Args:
             dag: DAG circuit to be rescheduled with constraints.
