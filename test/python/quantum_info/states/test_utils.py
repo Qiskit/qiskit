b92f0d213e10af6d94ad0df02e56f5986ddd6a0c
diemilio <diemilio@live.com>
2023-05-16 17:20:15 -0400

    tox lint test

--- test/python/quantum_info/states/test_utils.py
+++ test/python/quantum_info/states/test_utils.py
@@ -57,30 +57,33 @@ class TestStateUtils(QiskitTestCase):
 
     def test_schmidt_decomposition(self):
         """Test schmidt_decomposition function"""
-        
+
         # separable 2-level system without subsystem permutation
         target = Statevector.from_label("10l")
-        schmidt_comps = schmidt_decomposition(Statevector.from_label("10l"),[0])
-        state = Statevector(sum([suv[0]*np.kron(suv[1],suv[2]) for suv in schmidt_comps]))
+        schmidt_comps = schmidt_decomposition(Statevector.from_label("10l"), [0])
+        state = Statevector(sum(suv[0] * np.kron(suv[1], suv[2]) for suv in schmidt_comps))
         self.assertEqual(state, target)
 
         # separable 2-level system with subsystem permutation
         target = Statevector.from_label("0l1")
-        schmidt_comps = schmidt_decomposition(Statevector.from_label("l10"),[2,1])
-        state = Statevector(sum([suv[0]*np.kron(suv[1],suv[2]) for suv in schmidt_comps]))
+        schmidt_comps = schmidt_decomposition(Statevector.from_label("l10"), [2, 1])
+        state = Statevector(sum(suv[0] * np.kron(suv[1], suv[2]) for suv in schmidt_comps))
         self.assertEqual(state, target)
 
         # entangled 2-level system
-        target = 1/np.sqrt(2)*(Statevector.from_label("00") + Statevector.from_label("11"))
-        schmidt_comps = schmidt_decomposition(target,[0])
-        state = Statevector(sum([suv[0]*np.kron(suv[1],suv[2]) for suv in schmidt_comps]))
+        target = 1 / np.sqrt(2) * (Statevector.from_label("00") + Statevector.from_label("11"))
+        schmidt_comps = schmidt_decomposition(target, [0])
+        state = Statevector(sum(suv[0] * np.kron(suv[1], suv[2]) for suv in schmidt_comps))
         self.assertEqual(state, target)
 
         # entangled 3-level system
-        target = Statevector(np.array([1,0,0,0,1,0,0,0,1])*1/np.sqrt(3),dims=(3,3))
-        schmidt_comps = schmidt_decomposition(target,[0])
-        state = Statevector(sum([suv[0]*np.kron(suv[1],suv[2]) for suv in schmidt_comps]),dims=(3,3))
+        target = Statevector(np.array([1, 0, 0, 0, 1, 0, 0, 0, 1]) * 1 / np.sqrt(3), dims=(3, 3))
+        schmidt_comps = schmidt_decomposition(target, [0])
+        state = Statevector(
+            sum(suv[0] * np.kron(suv[1], suv[2]) for suv in schmidt_comps), dims=(3, 3)
+        )
         self.assertEqual(state, target)
+
 
 if __name__ == "__main__":
     unittest.main()
