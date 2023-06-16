0e006ef593f627008e2c5a60118bd0284ed902d1
Mahnoor Fatima <59219220+maxwell04-wq@users.noreply.github.com>
2023-06-17 01:33:54 +0500

    Add return type to `test_from_gate_with_cyclic_definition ` in `test_clifford.py`

--- test/python/quantum_info/operators/symplectic/test_clifford.py
+++ test/python/quantum_info/operators/symplectic/test_clifford.py
@@ -586,7 +586,7 @@ class TestCliffordGates(QiskitTestCase):
                 qc.sdg(0)
                 self.definition = qc
 
-        Clifford(MyHGate())
+        print(Clifford(MyHGate()))
 
 
 @ddt
