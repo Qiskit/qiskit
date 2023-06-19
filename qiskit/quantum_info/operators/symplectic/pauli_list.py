1b0db35c96488e7b948df7528b6c8a0dd9a7879e
Preetham Tikkireddi <thspreetham@gmail.com>
2023-06-19 22:09:53 +0530

    fix pauli_list docstring

--- qiskit/quantum_info/operators/symplectic/pauli_list.py
+++ qiskit/quantum_info/operators/symplectic/pauli_list.py
@@ -915,8 +915,8 @@ class PauliList(BasePauli, LinearMixin, GroupMixin):
             frame (string): ``'h'`` for Heisenberg (default) or ``'s'`` for Schrödinger framework.
 
         Returns:
-            Pauli: the Pauli :math:`C^\dagger.P.C` (Schrödinger framework)
-            or the Pauli :math:`C.P.C^\dagger` (Heisenberg framework).
+            Pauli: the Pauli :math:`C.P.C^\dagger` (Schrödinger framework)
+            or the Pauli :math:`C^\dagger.P.C` (Heisenberg framework).
 
         Raises:
             QiskitError: if the Clifford number of qubits and qargs don't match.
