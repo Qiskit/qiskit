7b4e79179d5d146568542653b38e77fa5b3b1516
Jake Lishman <jake.lishman@ibm.com>
2023-07-18 21:11:37 +0100

    Add checking for bad `{qu,cl}bit_order` arguments
    Co-authored-by: Kevin Hartman <kevin@hart.mn>

--- qiskit/converters/circuit_to_dag.py
+++ qiskit/converters/circuit_to_dag.py
@@ -28,13 +28,17 @@ def circuit_to_dag(circuit, copy_operations=True, *, qubit_order=None, clbit_ord
             :class:`~.DAGCircuit` will be shared instances and modifications to
             operations in the :class:`~.DAGCircuit` will be reflected in the
             :class:`~.QuantumCircuit` (and vice versa).
-        qubit_order (Iterable[Qubit] or None): the ordered that the qubits should be indexed in the
+        qubit_order (Iterable[Qubit] or None): the order that the qubits should be indexed in the
             output DAG.  Defaults to the same order as in the circuit.
-        clbit_order (Iterable[Clbit] or None): the ordered that the clbits should be indexed in the
+        clbit_order (Iterable[Clbit] or None): the order that the clbits should be indexed in the
             output DAG.  Defaults to the same order as in the circuit.
 
     Return:
         DAGCircuit: the DAG representing the input circuit.
+
+    Raises:
+        ValueError: if the ``qubit_order`` or ``clbit_order`` parameters do not match the bits in
+            the circuit.
 
     Example:
         .. code-block::
@@ -58,8 +62,22 @@ def circuit_to_dag(circuit, copy_operations=True, *, qubit_order=None, clbit_ord
     dagcircuit.calibrations = circuit.calibrations
     dagcircuit.metadata = circuit.metadata
 
-    dagcircuit.add_qubits(circuit.qubits if qubit_order is None else qubit_order)
-    dagcircuit.add_clbits(circuit.clbits if clbit_order is None else clbit_order)
+    if qubit_order is None:
+        qubits = circuit.qubits
+    elif len(qubit_order) != circuit.num_qubits or set(qubit_order) != set(circuit.qubits):
+        raise ValueError("'qubit_order' does not contain exactly the same qubits as the circuit")
+    else:
+        qubits = qubit_order
+
+    if clbit_order is None:
+        clbits = circuit.clbits
+    elif len(clbit_order) != circuit.num_clbits or set(clbit_order) != set(circuit.clbits):
+        raise ValueError("'clbit_order' does not contain exactly the same clbits as the circuit")
+    else:
+        clbits = clbit_order
+
+    dagcircuit.add_qubits(qubits)
+    dagcircuit.add_clbits(clbits)
 
     for register in circuit.qregs:
         dagcircuit.add_qreg(register)
