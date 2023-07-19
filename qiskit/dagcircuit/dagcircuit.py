3a69d2444d962e61695956c743174cf16e2f7f4c
Caleb Johnson <calebj1524@outlook.com>
2023-07-19 01:18:48 -0500

    Add separable_circuits method to DAGCircuit class (#9471)
    * Add decompose_dag method to DAGCircuit class
    
    * Don't clean up idle wires within function
    
    * Add tests for DAGCircuit.separable_circuits
    
    * Add ordering and QuantumCircuit equivalency tests. Add release note.
    
    * Code style changes
    
    * Update release note
    
    * Clean up documentation
    
    * Return a DAG for any partition with at least one qubit, even if empty
    
    * Adjust tests to new flag
    
    * peer review
    
    * peer review
    
    * fix var name
    
    * test against dagcircuits
    
    * lint
    
    * test remove_idle_wires
    
    * improve release note
    
    * Update separable_circuits docstring
    
    * Sort the dag circuits in tests to prevent them breaking in future
    
    * minor cleanup
    
    * minor cleanup
    
    * Fix sorting
    
    * Fix type-hint resolution
    
    * black
    
    * unused import
    
    ---------
    
    Co-authored-by: Jake Lishman <jake.lishman@ibm.com>

--- qiskit/dagcircuit/dagcircuit.py
+++ qiskit/dagcircuit/dagcircuit.py
@@ -1402,6 +1402,58 @@ class DAGCircuit:
             self._decrement_op(node.op)
         return new_node
 
+    def separable_circuits(self, remove_idle_qubits=False) -> List["DAGCircuit"]:
+        """Decompose the circuit into sets of qubits with no gates connecting them.
+
+        Args:
+            remove_idle_qubits (bool): Flag denoting whether to remove idle qubits from
+                the separated circuits. If ``False``, each output circuit will contain the
+                same number of qubits as ``self``.
+
+        Returns:
+            List[DAGCircuit]: The circuits resulting from separating ``self`` into sets
+                of disconnected qubits
+
+        Each :class:`~.DAGCircuit` instance returned by this method will contain the same number of
+        clbits as ``self``. The global phase information in ``self`` will not be maintained
+        in the subcircuits returned by this method.
+        """
+        connected_components = rx.weakly_connected_components(self._multi_graph)
+
+        # Collect each disconnected subgraph
+        disconnected_subgraphs = []
+        for components in connected_components:
+            disconnected_subgraphs.append(self._multi_graph.subgraph(list(components)))
+
+        # Helper function for ensuring rustworkx nodes are returned in lexicographical,
+        # topological order
+        def _key(x):
+            return x.sort_key
+
+        # Create new DAGCircuit objects from each of the rustworkx subgraph objects
+        decomposed_dags = []
+        for subgraph in disconnected_subgraphs:
+            new_dag = self.copy_empty_like()
+            new_dag.global_phase = 0
+            subgraph_is_classical = True
+            for node in rx.lexicographical_topological_sort(subgraph, key=_key):
+                if isinstance(node, DAGInNode):
+                    if isinstance(node.wire, Qubit):
+                        subgraph_is_classical = False
+                if not isinstance(node, DAGOpNode):
+                    continue
+                new_dag.apply_operation_back(node.op, node.qargs, node.cargs)
+
+            # Ignore DAGs created for empty clbits
+            if not subgraph_is_classical:
+                decomposed_dags.append(new_dag)
+
+        if remove_idle_qubits:
+            for dag in decomposed_dags:
+                dag.remove_qubits(*(bit for bit in dag.idle_wires() if isinstance(bit, Qubit)))
+
+        return decomposed_dags
+
     def swap_nodes(self, node1, node2):
         """Swap connected nodes e.g. due to commutation.
 
