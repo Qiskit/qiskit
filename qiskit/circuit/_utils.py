332353ac2797b9e0a58e808c753aa7d66ae47cec
Jake Lishman <jake.lishman@ibm.com>
2023-07-20 15:46:47 +0100

    Improve parameter-binding performance of large instructions (#10284)
    * Improve parameter-binding performance of large instructions
    
    Previously, the parameter-assignment methods of `QuantumCircuit` had
    poor performance when an instruction had a complex definition that
    involved many of the parameters being bound.  The strategy of binding
    each parameter separately led to each definition being copied and
    rebound multiple times, with each rebinding being recursive all the way
    down.
    
    This commit makes the definition rebinding happen only once per
    instruction, and updates the data model used to make it a complete
    recursion through `QuantumCircuit.assign_parameters`.  This has the side
    effect of fixing an issue where internal global phases would not be
    updated.
    
    The algorithmic change that enables this (just rebind the definition at
    the end) is rather simpler than the length of this patch suggests.  This
    is just because the previous structure of separating out a single
    `_assign_parameter` method made it harder to restructure the logic
    without introducing unpleasant stateful coupling between the driver and
    helper methods.  Instead, I inlined most of the helper functions into
    the driver body, so we can treat some components of the binding in a
    per-parameter way and some in a per-operation way, in whatever way is
    better.
    
    * Fix lint
    
    * Reduce overhead from input normalisation
    
    This reduces several costs in the input normalisation, and makes the
    calculation of some properties lazy for the sequence-like inputs; for
    many close-to-hardware circuits such as those used in PEC, the sequence
    form of parameter input is more natural, and almost no instructions will
    have internally parametrised definitions, nor will there be a
    parametrised global phase or calibrations.  In these cases, we can avoid
    overhead from eagerly normalising the input into the forms that's easier
    for these less-common assignment operations to use.
    
    As a side-effect of the abstraction, we can also avoid making several
    dictionary copies, and just use the mapping abstraction to filter the
    dictionary during iteration on the fly.
    
    This also takes the opportunity to improve the performance of sorting
    large vectors of parameters.  In practice, I don't think this had a huge
    impact on performance, but in principle it's rather more efficient now
    and results in many fewer Python function calls during a sort.
    
    * Address Ruff's generator concern
    
    * Add comment on new keyword arguments

--- qiskit/circuit/_utils.py
+++ qiskit/circuit/_utils.py
@@ -16,6 +16,19 @@ This module contains utility functions for circuits.
 import numpy
 from qiskit.exceptions import QiskitError
 from qiskit.circuit.exceptions import CircuitError
+from .parametervector import ParameterVectorElement
+
+
+def sort_parameters(parameters):
+    """Sort an iterable of :class:`.Parameter` instances into a canonical order, respecting the
+    ordering relationships between elements of :class:`.ParameterVector`\\ s."""
+
+    def key(parameter):
+        if isinstance(parameter, ParameterVectorElement):
+            return (parameter.vector.name, parameter.index)
+        return (parameter.name,)
+
+    return sorted(parameters, key=key)
 
 
 def _compute_control_matrix(base_mat, num_ctrl_qubits, ctrl_state=None):
