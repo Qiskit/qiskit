8d8466cfd8b9ee87ea3dabadfb10761a9dc3c633
Jake Lishman <jake.lishman@ibm.com>
2023-07-21 18:49:47 +0100

    Bump version numbers post 0.25.0rc1 (#10464)
    This opens development on `main` for the next minor version of *Qiskit*
    (no longer to be called Terra).  The version number is jumping to 0.45
    to bring the version of `qiskit-terra` in line with `qiskit` (currently
    the metapackge); starting from "Qiskit 0.45", this repository will be
    called `qiskit` only, and the packages `qiskit` and `qiskit-terra` will
    be unified, at least as far as Python packaging allows us to do.

--- docs/conf.py
+++ docs/conf.py
@@ -23,9 +23,9 @@ copyright = f"2017-{datetime.date.today().year}, Qiskit Development Team"  # pyl
 author = "Qiskit Development Team"
 
 # The short X.Y version
-version = "0.25"
+version = "0.45"
 # The full version, including alpha/beta/rc tags
-release = "0.25.0"
+release = "0.45.0"
 
 extensions = [
     "sphinx.ext.napoleon",
