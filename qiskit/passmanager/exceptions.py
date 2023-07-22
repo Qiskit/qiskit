3e1e73aad1e4fe6e8c0e47cafe571a5d10a740c4
Matthew Treinish <mtreinish@kortar.org>
2023-07-22 08:55:07 -0400

    Revert "Revert "Pass manager refactoring: add passmanager module (#10124)" (#10454)" (#10474)
    This reverts commit c03e4c78bc2338d5411e4b4518a394c2e32f1fe3.

--- qiskit/passmanager/exceptions.py
+++ qiskit/passmanager/exceptions.py
@@ -1,6 +1,6 @@
 # This code is part of Qiskit.
 #
-# (C) Copyright IBM 2017, 2018.
+# (C) Copyright IBM 2023.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -10,13 +10,10 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""A property set is maintained by the PassManager to keep information
-about the current state of the circuit """
+"""Qiskit pass manager exceptions."""
+
+from qiskit.exceptions import QiskitError
 
 
-from qiskit.passmanager import propertyset as passmanager_propertyset
-
-
-def __getattr__(name):
-    # Just redirect to new module. This will be deprecated.
-    return getattr(passmanager_propertyset, name)
+class PassManagerError(QiskitError):
+    """Pass manager error."""
