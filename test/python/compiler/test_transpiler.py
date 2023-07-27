618c661a0f807166a86602ff71a58cc1041167bf
Jake Lishman <jake.lishman@ibm.com>
2023-07-27 11:02:56 +0100

    Add test skip for O3 on Windows
    This is the same skip in place as in the related QPY round-trip
    serialisation, while we work out the issues with eigensystem solver
    stability, especially on Windows.

--- test/python/compiler/test_transpiler.py
+++ test/python/compiler/test_transpiler.py
@@ -1849,6 +1849,11 @@ class TestPostTranspileIntegration(QiskitTestCase):
     def test_qpy_roundtrip_control_flow_expr(self, optimization_level):
         """Test that the output of a transpiled circuit with control flow including `Expr` nodes can
         be round-tripped through QPY."""
+        if optimization_level == 3 and sys.platform == "win32":
+            self.skipTest(
+                "This test case triggers a bug in the eigensolver routine on windows. "
+                "See #10345 for more details."
+            )
         backend = FakeMelbourne()
         transpiled = transpile(
             self._control_flow_expr_circuit(),
