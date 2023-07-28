f3d3be94800c2d6ec459dfc0696329c5d57f0b39
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2023-07-28 07:25:20 -0600

    Switch to new Furo docs theme (Qiskit/qiskit-metapackage#1784)
    See Qiskit/qiskit-terra#10498 for the
    motivation.
    
    Note that in Terra, we distinguish between dev/CI builds vs. prod
    builds. We don't make that distinguishment here because we don't get
    much dev activity in this repo.

--- docs/conf.py
+++ docs/conf.py
@@ -149,19 +149,7 @@ intersphinx_mapping = {
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = "qiskit_sphinx_theme"
-html_theme_options = {
-    "logo_only": False,
-    "display_version": True,
-    "prev_next_buttons_location": "bottom",
-    "style_external_links": True,
-    # Toc options
-    "collapse_navigation": True,
-    "sticky_navigation": True,
-    "navigation_depth": 4,
-    "includehidden": True,
-    "titles_only": False,
-}
+html_theme = "qiskit"
 html_favicon = "images/favicon.ico"
 html_last_updated_fmt = "%Y/%m/%d"
 html_context = {
