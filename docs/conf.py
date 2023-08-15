ba996e077c72c42b6e668b61e2d5834ad5abaef2
Eric Arellano <14852634+Eric-Arellano@users.noreply.github.com>
2023-08-15 13:23:12 -0700

    Refer to IBM docs

--- docs/conf.py
+++ docs/conf.py
@@ -127,8 +127,12 @@ html_theme = "qiskit"
 html_favicon = "images/favicon.ico"
 html_last_updated_fmt = "%Y/%m/%d"
 html_context = {
-    "analytics_enabled": bool(os.getenv("QISKIT_ENABLE_ANALYTICS", ""))
-}  # enable segment analytics for qiskit.org/documentation
+    # Enable segment analytics for qiskit.org/documentation
+    "analytics_enabled": bool(os.getenv("QISKIT_ENABLE_ANALYTICS", "")),
+    "theme_announcement": "Qiskit is getting a new documentation experience on IBM Quantum!",
+    "announcement_url": "https://quantum-computing.ibm.com/",
+    "announcement_url_text": "Check it out",
+}
 html_static_path = ["_static"]
 
 # This speeds up the docs build because it works around the Furo theme's slowdown from the left
