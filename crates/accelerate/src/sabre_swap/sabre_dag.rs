ab282f35f96244b3dbfd9308c9b9bf88cb38df89
Jake Lishman <jake.lishman@ibm.com>
2023-08-29 13:02:30 +0100

    Fail gracefully on bad `SabreDAG` construction
    This is a private, internal Rust type, but it doesn't cost us anything
    (meaningful) to bounds-check the accessors and ensure we fail gracefully
    rather than panicking if we ever make a mistake in Python space. This
    more faithfully fulfills the return value of `SabreDAG::new`, which
    previously was an effectively infallible `Result`.

--- crates/accelerate/src/sabre_swap/sabre_dag.rs
+++ crates/accelerate/src/sabre_swap/sabre_dag.rs
@@ -12,6 +12,7 @@
 
 use hashbrown::HashMap;
 use hashbrown::HashSet;
+use pyo3::exceptions::PyIndexError;
 use pyo3::prelude::*;
 use rustworkx_core::petgraph::prelude::*;
 
@@ -51,7 +52,7 @@ impl SabreDAG {
         num_clbits: usize,
         nodes: Vec<(usize, Vec<usize>, HashSet<usize>)>,
         node_blocks: HashMap<usize, Vec<SabreDAG>>,
-    ) -> Self {
+    ) -> PyResult<Self> {
         let mut qubit_pos: Vec<Option<NodeIndex>> = vec![None; num_qubits];
         let mut clbit_pos: Vec<Option<NodeIndex>> = vec![None; num_clbits];
         let mut dag = DiGraph::with_capacity(nodes.len(), 2 * nodes.len());
@@ -65,30 +66,58 @@ impl SabreDAG {
             });
             let mut is_front = true;
             for x in qargs {
-                if let Some(predecessor) = qubit_pos[*x] {
+                let pos = qubit_pos.get_mut(*x).ok_or_else(|| {
+                    PyIndexError::new_err(format!(
+                        "qubit index {} is out of range for {} qubits",
+                        *x, num_qubits
+                    ))
+                })?;
+                if let Some(predecessor) = *pos {
                     is_front = false;
                     dag.add_edge(predecessor, gate_index, ());
                 }
-                qubit_pos[*x] = Some(gate_index);
+                *pos = Some(gate_index);
             }
             for x in cargs {
-                if let Some(predecessor) = clbit_pos[*x] {
+                let pos = clbit_pos.get_mut(*x).ok_or_else(|| {
+                    PyIndexError::new_err(format!(
+                        "clbit index {} is out of range for {} clbits",
+                        *x, num_qubits
+                    ))
+                })?;
+                if let Some(predecessor) = *pos {
                     is_front = false;
                     dag.add_edge(predecessor, gate_index, ());
                 }
-                clbit_pos[*x] = Some(gate_index);
+                *pos = Some(gate_index);
             }
             if is_front {
                 first_layer.push(gate_index);
             }
         }
-        SabreDAG {
+        Ok(SabreDAG {
             num_qubits,
             num_clbits,
             dag,
             first_layer,
             nodes,
             node_blocks,
-        }
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::SabreDAG;
+    use hashbrown::{HashMap, HashSet};
+
+    #[test]
+    fn no_panic_on_bad_qubits() {
+        assert!(SabreDAG::new(2, 0, vec![(0, vec![0, 2], HashSet::new())], HashMap::new()).is_err())
+    }
+
+    #[test]
+    fn no_panic_on_bad_clbits() {
+        assert!(SabreDAG::new(2, 1, vec![(0, vec![0, 1], [0, 1].into())], HashMap::new()).is_err())
     }
 }
