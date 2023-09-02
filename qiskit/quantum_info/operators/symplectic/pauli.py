bf9f6f4862d5c34d9b2e235448eb797e496b5c69
Luciano Bello <bel@zurich.ibm.com>
2023-09-02 22:55:52 +0200

    black

--- qiskit/quantum_info/operators/symplectic/pauli.py
+++ qiskit/quantum_info/operators/symplectic/pauli.py
@@ -155,9 +155,7 @@ class Pauli(BasePauli):
     _VALID_LABEL_PATTERN = re.compile(r"(?P<coeff>[+-]?1?[ij]?)(?P<pauli>[IXYZ]*)")
     _CANONICAL_PHASE_LABEL = {"": 0, "-i": 1, "-": 2, "i": 3}
 
-    def __init__(
-        self,
-        data: str | tuple | Pauli | ScalarOp | QuantumCircuit | None = None):
+    def __init__(self, data: str | tuple | Pauli | ScalarOp | QuantumCircuit | None = None):
         """Initialize the Pauli.
 
         When using the symplectic array input data both z and x arguments must
