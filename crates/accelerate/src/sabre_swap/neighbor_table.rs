9ff0af91405ba90f9db4b40bbb41b37c3339cb2d
Jake Lishman <jake.lishman@ibm.com>
2023-09-05 19:28:45 +0100

    Use qubit-index newtypes in Rust space (#10761)
    * Use qubit-index newtypes in Rust space
    
    This converts all of our Rust-space components that are concerned with
    virtual and physical qubits (via the `NLayout` class) to represent those
    integers with newtypes wrapping them as half-size indices, and changes
    the `NLayout` API to only allow access via these types and not by raw
    integers.
    
    The way this is done should have no overhead in time usage from Rust,
    and is actually a reduction in memory usage because all the qubits are
    stored at half the width they were previously (for most systems).  This
    is done to add type safety to all our components that were concerned
    with the mixing of these two qubits.  The implementation of this commit
    already turned up the logical bug fixed by gh-10756.
    
    * Separate out type-safe enforcement in `NeighborTable` to another commit
    
    This reverts the changes to the coupling-map generation and the indexing
    into `NeighborTable` that were designed to make it more type safe in
    favour of doing that in a separate commit.

--- crates/accelerate/src/sabre_swap/neighbor_table.rs
+++ crates/accelerate/src/sabre_swap/neighbor_table.rs
@@ -16,6 +16,8 @@ use numpy::PyReadonlyArray2;
 use pyo3::prelude::*;
 use rayon::prelude::*;
 
+use crate::nlayout::PhysicalQubit;
+
 /// A simple container that contains a vector of vectors representing
 /// neighbors of each node in the coupling map
 ///
@@ -29,30 +31,31 @@ use rayon::prelude::*;
 #[pyclass(module = "qiskit._accelerate.sabre_swap")]
 #[derive(Clone, Debug)]
 pub struct NeighborTable {
-    pub neighbors: Vec<Vec<usize>>,
+    pub neighbors: Vec<Vec<PhysicalQubit>>,
 }
 
 #[pymethods]
 impl NeighborTable {
     #[new]
     #[pyo3(text_signature = "(/, adjacency_matrix=None)")]
-    pub fn new(adjacency_matrix: Option<PyReadonlyArray2<f64>>) -> Self {
+    pub fn new(adjacency_matrix: Option<PyReadonlyArray2<f64>>) -> PyResult<Self> {
         let run_in_parallel = getenv_use_multiple_threads();
         let neighbors = match adjacency_matrix {
             Some(adjacency_matrix) => {
                 let adj_mat = adjacency_matrix.as_array();
-                let build_neighbors = |row: ArrayView1<f64>| -> Vec<usize> {
+                let build_neighbors = |row: ArrayView1<f64>| -> PyResult<Vec<PhysicalQubit>> {
                     row.iter()
                         .enumerate()
-                        .filter_map(
-                            |(row_index, value)| {
-                                if *value == 0. {
-                                    None
-                                } else {
-                                    Some(row_index)
-                                }
-                            },
-                        )
+                        .filter_map(|(row_index, value)| {
+                            if *value == 0. {
+                                None
+                            } else {
+                                Some(match row_index.try_into() {
+                                    Ok(index) => Ok(PhysicalQubit::new(index)),
+                                    Err(err) => Err(err.into()),
+                                })
+                            }
+                        })
                         .collect()
                 };
                 if run_in_parallel {
@@ -60,24 +63,24 @@ impl NeighborTable {
                         .axis_iter(Axis(0))
                         .into_par_iter()
                         .map(|row| build_neighbors(row))
-                        .collect()
+                        .collect::<PyResult<_>>()?
                 } else {
                     adj_mat
                         .axis_iter(Axis(0))
                         .map(|row| build_neighbors(row))
-                        .collect()
+                        .collect::<PyResult<_>>()?
                 }
             }
             None => Vec::new(),
         };
-        NeighborTable { neighbors }
+        Ok(NeighborTable { neighbors })
     }
 
-    fn __getstate__(&self) -> Vec<Vec<usize>> {
+    fn __getstate__(&self) -> Vec<Vec<PhysicalQubit>> {
         self.neighbors.clone()
     }
 
-    fn __setstate__(&mut self, state: Vec<Vec<usize>>) {
+    fn __setstate__(&mut self, state: Vec<Vec<PhysicalQubit>>) {
         self.neighbors = state
     }
 }
