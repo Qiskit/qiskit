a02e0a3dc6efcbe69cc86718be9b527c73367395
Matthew Treinish <mtreinish@kortar.org>
2023-09-07 14:32:39 -0400

    Speed up synthesis passes when there is nothing to synthesize (#10788)
    * Speed up synthesis passes when there is nothing to synthesize
    
    This commit makes a small optimization to the UnitarySynthesis and
    HighLevelSynthesis synthesis passes when there are no gates to
    synthesize. Previously these passes would iterate over the entire DAG
    and check every gate to determine that None needed to be synthesized.
    While this is relatively quick for very large circuits this can take
    100s of ms. However, this was wasted work because the DAG carries a
    dictionary of op names in the circuit so we can determine in constant
    time whether any of the operations to synthesize are present up front
    and skip the bulk of the iteration. To improve the impact here the
    DAGCircuit.count_ops() method is updated to skip the recursion if no
    control flow operations are present. Otherwise we'd still be iterating
    over the DAG (in a faster manner) which would limit the improvement
    from this commit. This isn't the best approach for that as it uses a
    hardcoded list of control flow operations which doesn't seem super
    general purpose, but as this hard coded set of instructions is
    already used elsewhere in the DAGCircuit this isn't making the
    situation any worse. If/when we expand recursive objects this can be
    revisited (and we'll likely not be able to avoid the iteration in
    count ops). With these changes the time these passes take when there are
    no operations to synthesize only takes ~5-15 microseconds.
    
    A similar optimization should be considered for UnrollCustomDefinitions
    as this takes an even longer amount of time to iterate when there is
    nothing to translate. However, it wasn't done as part of this PR as to
    check the equivalence library has an entry we need to work with gate
    objects (well actually name and number of qubits as that's all that's
    used from the object for a lookup) and the name from the op count
    dictionary is not sufficient for a lookup. So this will need to be
    handled independently in a separate PR.
    
    * Remove duplicated check in unitary synthesis
    
    * Unify hardcoded sets of control flow op names

--- qiskit/dagcircuit/dagcircuit.py
+++ qiskit/dagcircuit/dagcircuit.py
@@ -36,7 +36,7 @@ from qiskit.circuit import (
     SwitchCaseOp,
     _classical_resource_map,
 )
-from qiskit.circuit.controlflow import condition_resources, node_resources
+from qiskit.circuit.controlflow import condition_resources, node_resources, CONTROL_FLOW_OP_NAMES
 from qiskit.circuit.quantumregister import QuantumRegister, Qubit
 from qiskit.circuit.classicalregister import ClassicalRegister, Clbit
 from qiskit.circuit.gate import Gate
@@ -916,9 +916,7 @@ class DAGCircuit:
         """
         length = len(self._multi_graph) - 2 * len(self._wires)
         if not recurse:
-            if any(
-                x in self._op_names for x in ("for_loop", "while_loop", "if_else", "switch_case")
-            ):
+            if any(x in self._op_names for x in CONTROL_FLOW_OP_NAMES):
                 raise DAGCircuitError(
                     "Size with control flow is ambiguous."
                     " You may use `recurse=True` to get a result,"
@@ -980,9 +978,7 @@ class DAGCircuit:
                 return node_lookup.get(target, 1)
 
         else:
-            if any(
-                x in self._op_names for x in ("for_loop", "while_loop", "if_else", "switch_case")
-            ):
+            if any(x in self._op_names for x in CONTROL_FLOW_OP_NAMES):
                 raise DAGCircuitError(
                     "Depth with control flow is ambiguous."
                     " You may use `recurse=True` to get a result,"
@@ -1978,7 +1974,7 @@ class DAGCircuit:
         Returns:
             Mapping[str, int]: a mapping of operation names to the number of times it appears.
         """
-        if not recurse:
+        if not recurse or not CONTROL_FLOW_OP_NAMES.intersection(self._op_names):
             return self._op_names.copy()
 
         # pylint: disable=cyclic-import
