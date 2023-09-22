d48912f52d14556d32c632bbcbf61cb7eb9a5555
Luciano Bello <bel@zurich.ibm.com>
2023-09-22 11:30:32 +0200

    Small doc formatting issues (#10574)
    * small doc fixes
    
    * more small changes
    
    * Update qiskit/extensions/quantum_initializer/squ.py
    
    Co-authored-by: Julien Gacon <gaconju@gmail.com>
    
    * improve gate docs
    
    * Apply suggestions from code review
    
    Co-authored-by: Julien Gacon <gaconju@gmail.com>
    
    * Update qiskit/converters/circuit_to_dag.py
    
    * Update qiskit/execute_function.py
    
    * Circuits as Directed Acyclic Graphs
    
    * a quantum circuit as a Directed Acyclic Graphs (DAG
    
    * __
    
    * Fix errant plural
    
    ---------
    
    Co-authored-by: Julien Gacon <gaconju@gmail.com>
    Co-authored-by: Jake Lishman <jake@binhbar.com>

--- qiskit/converters/circuit_to_dag.py
+++ qiskit/converters/circuit_to_dag.py
@@ -17,7 +17,7 @@ from qiskit.dagcircuit.dagcircuit import DAGCircuit
 
 
 def circuit_to_dag(circuit, copy_operations=True, *, qubit_order=None, clbit_order=None):
-    """Build a ``DAGCircuit`` object from a ``QuantumCircuit``.
+    """Build a :class:`.DAGCircuit` object from a :class:`.QuantumCircuit`.
 
     Args:
         circuit (QuantumCircuit): the input circuit.
