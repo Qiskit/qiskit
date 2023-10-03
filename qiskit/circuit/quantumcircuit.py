bebb247305864cac020e0b0d658a77e003584808
Pieter Eendebak <pieter.eendebak@gmail.com>
2023-10-03 14:16:22 +0200

    lint

--- qiskit/circuit/quantumcircuit.py
+++ qiskit/circuit/quantumcircuit.py
@@ -1587,7 +1587,7 @@ class QuantumCircuit:
         from qiskit.converters.dag_to_circuit import dag_to_circuit
 
         # do not copy operations, this is done in the conversion from dag_to_circuit
-        dag = circuit_to_dag(self,copy_operations=False)
+        dag = circuit_to_dag(self, copy_operations=False)
         dag = HighLevelSynthesis().run(dag)
         pass_ = Decompose(gates_to_decompose)
         for _ in range(reps):
