aa7b1f2d9b66a17f9f428cde45256a81182e0ff7
Julien Gacon <gaconju@gmail.com>
2023-10-03 13:39:46 +0200

    Rename alternative color tests
    - iqx -> iqp
    - add test for clifford

--- test/visual/mpl/circuit/test_circuit_matplotlib_drawer.py
+++ test/visual/mpl/circuit/test_circuit_matplotlib_drawer.py
@@ -884,7 +884,7 @@ class TestCircuitMatplotlibDrawer(QiskitTestCase):
     def test_alternative_colors(self):
         """Tests alternative color schemes"""
         ratios = []
-        for style in ["iqx", "iqx-dark", "textbook"]:
+        for style in ["iqp", "iqp-dark", "iqx", "iqx-dark", "textbook", "clifford"]:
             with self.subTest(style=style):
                 circuit = QuantumCircuit(7)
                 circuit.h(0)
@@ -913,11 +913,17 @@ class TestCircuitMatplotlibDrawer(QiskitTestCase):
                 circuit.reset(5)
 
                 fname = f"{style}_color.png"
+                # IQX has the same reference filename as IQP
+                if style[:3] == "iqx":
+                    ref_fname = "iqp" + style[3:] + "_color.png"
+                else:
+                    ref_fname = fname
+
                 self.circuit_drawer(circuit, style={"name": style}, filename=fname)
 
                 ratio = VisualTestUtilities._save_diff(
                     self._image_path(fname),
-                    self._reference_path(fname),
+                    self._reference_path(ref_fname),
                     fname,
                     FAILURE_DIFF_DIR,
                     FAILURE_PREFIX,
