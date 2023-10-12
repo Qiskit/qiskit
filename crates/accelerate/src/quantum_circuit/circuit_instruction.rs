177191b120a93781e1c2992e941ccd962211e971
Kevin Hartman <kevin@hart.mn>
2023-10-12 13:21:24 -0400

    Use freelist for CircuitInstruction class.

--- crates/accelerate/src/quantum_circuit/circuit_instruction.rs
+++ crates/accelerate/src/quantum_circuit/circuit_instruction.rs
@@ -15,7 +15,7 @@ use pyo3::prelude::*;
 use pyo3::types::PyTuple;
 use pyo3::{PyObject, PyResult};
 
-#[pyclass(sequence, get_all, module = "qiskit._accelerate.quantum_circuit")]
+#[pyclass(freelist = 100, sequence, get_all, module = "qiskit._accelerate.quantum_circuit")]
 #[derive(Clone, Debug)]
 pub struct CircuitInstruction {
     pub operation: PyObject,
