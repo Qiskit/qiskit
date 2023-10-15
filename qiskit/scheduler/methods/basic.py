2ce2ed599da3b0819bee43b0b6a9078e62f5f579
MozammilQ <54737489+MozammilQ@users.noreply.github.com>
2023-10-15 21:04:08 +0530

    Apply suggestion in docstring.
    Co-authored-by: Kento Ueda <38037695+to24toro@users.noreply.github.com>

--- qiskit/scheduler/methods/basic.py
+++ qiskit/scheduler/methods/basic.py
@@ -42,7 +42,7 @@ def as_soon_as_possible(
     Args:
         circuit: The quantum circuit to translate.
         schedule_config: Backend specific parameters used for building the Schedule.
-        backend: Pass in the backend used to build the Schedule, the backend could be BackendV1
+        backend: A backend used to build the Schedule, the backend could be BackendV1
                  or BackendV2.
 
     Returns:
