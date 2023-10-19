56643f0708b60c38a5d99b3e2237e55852e24c17
Matthew Treinish <mtreinish@kortar.org>
2023-10-19 16:10:38 -0400

    Set version number to 0.45.0rc1 for first release candidate (#11028)
    For the 0.45.0 release we're going to push release candidates prior to
    the release to enable testing before we cut the final release. In
    preparation for tagging the first release candidate this commit updates
    the version string to indicate it's a release candidate. This commit
    should be the final commit on main for 0.45.0rc1 and what gets tagged as
    0.45.0rc1 post-merge.

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ csp_requirements = ["python-constraint>=1.4"]
 
 setup(
     name="qiskit-terra",
-    version="0.45.0",
+    version="0.45.0rc1",
     description="Software for developing quantum computing programs",
     long_description=README,
     long_description_content_type="text/markdown",
