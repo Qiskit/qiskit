ba136ce537a92c69657e6fad2ce44b9c60616d23
Matthew Treinish <mtreinish@kortar.org>
2023-10-24 15:06:39 -0400

    Don't test forward compatibility in qpy compat tests
    The qpy compatibility tests are designed to test backwards
    compatibility. They generate QPY files using all historical releases
    of Qiskit with QPY support and test that all proposed changes are
    able to load the qpy files. There was an underlying assumption to how
    the tests were written that the version under test is always newer
    than all the historical releases. This has been true in the past because
    the release model for qiskit mean that we were maintaining at most one
    stable release at a time, and when we released a new minor version of
    qiskit we retired the previous minor version release series's branch.
    However, moving forward this assumption will no longer be true as we're
    moving to a model where we support > 1 branch at a time. When we start
    supporting > 1 release series at a time, PRs proposed to the older
    stable branches will no longer be able to test all releases because QPY
    only support backwards compatibility and will not be able to load QPY
    files generated from newer releases. The example of this was in the
    0.25.x series, there is overlap in the support of the stable/0.25 branch
    and the stable/0.45 branch, and after the release of 0.45.0rc1 the tests
    started to fail because you can't load QPY files generated with
    0.45.0rc1 with 0.25.x.
    
    This commit address this by adding a condition to the QPY compat scripts
    to ensure that the version under test is >= the QPY file source version.
    If we encounter that scenario we just skip the tests. This should
    prevent this situation from occurring for future releases as we expand
    our support windows.

--- test/qpy_compat/process_version.sh
+++ test/qpy_compat/process_version.sh
@@ -14,9 +14,23 @@
 set -e
 set -x
 
+# version is the source version, this is the release with which to generate
+# qpy files with to load with the version under test
 version=$1
 parts=( ${version//./ } )
-if [[ ${parts[1]} -lt 18 ]] ; then
+# qiskit_version is the version under test, We're testing that we can correctly
+# read the qpy files generated with source version with this version.
+qiskit_version=`../qiskit_venv/bin/python -c "import qiskit;print(qiskit.__version__`)"`
+qiskit_version_pars = ${qiskit_version//./ } )
+
+# If source version is less than 0.18 QPY didn't exist yet so exit fast
+if [[ ${parts[0]} -eq 0 && ${parts[1]} -lt 18 ]] ; then
+    exit 0
+fi
+# If the source version is newer than the version under test exit fast because
+# there is no QPY compatibility for loading a qpy file generated from a newer
+# release with an older release of Qiskit.
+if [[ ${parts[0]} -gt ${qiskit_parts[0]} || ${parts[1]} -gt ${qiskit_parts[1]} ]] ; then
     exit 0
 fi
 
