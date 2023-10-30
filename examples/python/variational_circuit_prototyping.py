a0d7551bebc047f2362eafde22cac3f70165c096
Shef Ashiru <sefunmi@vt.edu>
2023-10-30 13:11:49 -0400

    Add example on how to use ``NLocal`` Fixes #10480

--- examples/python/variational_circuit_prototyping.py
+++ examples/python/variational_circuit_prototyping.py
@@ -13,17 +13,48 @@
 
 """
 Example showing how to quickly prototype variational circuits using NLocal class.
+This example shows the most common workflows cases.
 
-This example shows the most common workflows and some special use cases. TODO: discribe the examples below
+Note: Remember to set the values and perform simulations or actual quantum computations based on your specific task.
 
-TODO: describe what the cfunction paramters are
+Variational or parametrized quantum circuits are quantum algorithms characterized by their reliance on freely 
+adjustable parameters. Similar to conventional quantum circuits, 
+they comprise 3 essential elements:
+"""
+from qiskit import QuantumCircuit, Parameter
+from qiskit.circuit.library import NLocal
 
 """
+1. Preparation of a specific initial quantum state:
 
-#TODO: add nessary imports
+You can create an initial state QuantumCircuit and prepend it to the NLocal circuit.
+"""
+initial_state = QuantumCircuit(3)
 
-#TODO: add examples in functions  
+# Define your initial state preparation here
+nlocal_circuit = NLocal(3, rotation_blocks=None, entanglement_blocks=None, initial_state=initial_state)
 
-#TODO: call functions and print out there output
+"""
+2. A quantum circuit U(θ) parameterized by a set of free parameters θ:
 
+You can specify the rotation and entanglement blocks that act on the circuit. 
+These blocks can contain parameters that you can adjust later.
+"""
+rotation_blocks = QuantumCircuit(3)
 
+# Add parameterized gates to the rotation_blocks
+theta = Parameter('θ')
+rotation_blocks.ry(theta, 0)
+entanglement_blocks = QuantumCircuit(3)
+
+# Add gates to the entanglement_blocks
+nlocal_circuit = NLocal(3, rotation_blocks, entanglement_blocks)
+
+"""
+3. Measurement of an observable ^B at the output:
+
+You can add measurement gates or observable gates to measure the final state of the circuit. 
+Here's an example of adding measurements to all qubits.
+"""
+for qubit in range(3):
+    nlocal_circuit.measure(qubit, qubit)
