2c8526d1ab472238dc0f75a1d7502702492b1f63
Matthew Treinish <mtreinish@kortar.org>
2023-10-30 13:39:24 -0400

    Fix return code with invalid version
    This commit fixes the return code generation if an invalid comparison is
    requested. We want to handle this with a 0 return code to treat it as a
    skip, but the previous logic would error before we evaluated the if
    statement.
    
    Co-authored-by: Jake Lishman <jake.lishman@ibm.com>

--- test/qpy_compat/process_version.sh
+++ test/qpy_compat/process_version.sh
@@ -23,8 +23,7 @@ parts=( ${version//./ } )
 qiskit_version=`./qiskit_venv/bin/python -c "import qiskit;print(qiskit.__version__)"`
 qiskit_parts=( ${qiskit_version//./ } )
 
-./qiskit_venv/bin/python ./compare_versions.py "$version" "$qiskit_version"
-valid_version=$?
+
 # If source version is less than 0.18 QPY didn't exist yet so exit fast
 if [[ ${parts[0]} -eq 0 && ${parts[1]} -lt 18 ]] ; then
     exit 0
@@ -33,7 +32,7 @@ fi
 # If the source version is newer than the version under test exit fast because
 # there is no QPY compatibility for loading a qpy file generated from a newer
 # release with an older release of Qiskit.
-if [[ $valid_version -ne 0 ]] ; then
+if ! ./qiskit_venv/bin/python ./compare_versions.py "$version" "$qiskit_version" ; then
     exit 0
 fi
 
