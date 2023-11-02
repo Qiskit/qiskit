a2c5412f870f017ea9e81b72a30ab05befcd3ddb
Jake Lishman <jake.lishman@ibm.com>
2023-11-02 19:29:08 +0000

    Fix build ordering of `BlueprintCircuit._append` (#11181)
    `QuantumCircuit._append` is the actual append primitive and is allowed
    in inner-loop appends (and for `QuantumCircuit` to assume that it's
    callable from other methods), so it's important that the "ensure built"
    check happens there, not in `append`.

--- qiskit/circuit/library/blueprintcircuit.py
+++ qiskit/circuit/library/blueprintcircuit.py
@@ -119,10 +119,10 @@ class BlueprintCircuit(QuantumCircuit, ABC):
             self._build()
         return super().qasm(formatted, filename, encoding)
 
-    def append(self, instruction, qargs=None, cargs=None):
+    def _append(self, instruction, _qargs=None, _cargs=None):
         if not self._is_built:
             self._build()
-        return super().append(instruction, qargs, cargs)
+        return super()._append(instruction, _qargs, _cargs)
 
     def compose(self, other, qubits=None, clbits=None, front=False, inplace=False, wrap=False):
         if not self._is_built:
