11925667a020ab72120dbf64bfd68c6636874200
Caleb Johnson <calebj1524@outlook.com>
2023-11-06 09:57:10 -0600

    test order

--- test/python/circuit/library/test_overlap.py
+++ test/python/circuit/library/test_overlap.py
@@ -82,6 +82,15 @@ class TestUnitaryOverlap(QiskitTestCase):
         overlap = UnitaryOverlap(unitary1, unitary2)
         self.assertEqual(overlap.num_parameters, unitary1.num_parameters)
 
+    def test_barrier(self):
+        """Test that barriers on input circuits are well handled"""
+        unitary1 = EfficientSU2(1, reps=0)
+        unitary1.barrier()
+        unitary2 = EfficientSU2(1, reps=1)
+        unitary2.barrier()
+        overlap = UnitaryOverlap(unitary1, unitary2)
+        self.assertEqual(overlap.num_parameters, unitary1.num_parameters + unitary2.num_parameters)
+
     def test_measurements(self):
         """Test that exception is thrown for measurements"""
         unitary1 = EfficientSU2(2)
@@ -121,15 +130,6 @@ class TestUnitaryOverlap(QiskitTestCase):
 
         with self.assertRaises(CircuitError):
             _ = UnitaryOverlap(unitary1, unitary2)
-
-    def test_barrier(self):
-        """Test that barriers on input circuits are well handled"""
-        unitary1 = EfficientSU2(1, reps=0)
-        unitary1.barrier()
-        unitary2 = EfficientSU2(1, reps=1)
-        unitary2.barrier()
-        overlap = UnitaryOverlap(unitary1, unitary2)
-        self.assertEqual(overlap.num_parameters, unitary1.num_parameters + unitary2.num_parameters)
 
 
 if __name__ == "__main__":
