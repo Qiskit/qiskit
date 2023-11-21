9cbb9978765411050718ab5b52e37a9948a8ebdc
Kevin J. Sung <kevjsung@umich.edu>
2023-11-21 18:57:24 -0500

    Improve "Custom Pass Managers" docs (#11288)
    * fix import in transpiler docs code example
    
    * other improvements
    
    * fix stage <-> pass manager mix up
    
    (cherry picked from commit 2935f0dd3a5d24843cd29dd1102157a7af1a1afe)

--- qiskit/transpiler/__init__.py
+++ qiskit/transpiler/__init__.py
@@ -145,15 +145,17 @@ Custom Pass Managers
 
 In addition to modifying preset pass managers, it is also possible to construct a pass
 manager to build an entirely custom pipeline for transforming input
-circuits. You can leverage the :class:`~.StagedPassManager` class directly to do
+circuits. You can use the :class:`~.StagedPassManager` class directly to do
 this. You can define arbitrary stage names and populate them with a :class:`~.PassManager`
-instance. For example::
+instance. For example, the following code creates a new :class:`~.StagedPassManager`
+that has 2 stages, ``init`` and ``translation``.::
 
     from qiskit.transpiler.passes import (
         UnitarySynthesis,
         Collect2qBlocks,
         ConsolidateBlocks,
         UnitarySynthesis,
+        Unroll3qOrMore,
     )
     from qiskit.transpiler import PassManager, StagedPassManager
 
@@ -171,13 +173,11 @@ instance. For example::
         stages=["init", "translation"], init=init, translation=translate
     )
 
-will create a new :class:`~.StagedPassManager` that has 2 stages ``init`` and ``translation``.
-There is no limit on the number of stages you can put in a custom :class:`~.StagedPassManager`
-instance.
+There is no limit on the number of stages you can put in a :class:`~.StagedPassManager`.
 
-The :ref:`stage_generators` functions may be useful for the construction of custom pass managers.
-They generate stages which provide common functionality used in many pass managers.
-For example, :func:`~.generate_embed_passmanager` can be used to generate a stage
+The :ref:`stage_generators` may be useful for the construction of custom :class:`~.StagedPassManager`s.
+They generate pass managers which provide common functionality used in many stages.
+For example, :func:`~.generate_embed_passmanager` generates a :class:`~.PassManager`
 to "embed" a selected initial :class:`~.Layout` from a layout pass to the specified target device.
 
 Representing Quantum Computers
