37a9a5219f7efbaa13ce57bdf8b1220c4b36d23f
Matthew Treinish <mtreinish@kortar.org>
2023-11-24 05:59:31 -0500

    Use stable on linux

--- tools/install_rust.sh
+++ tools/install_rust.sh
@@ -2,5 +2,5 @@
 if [ ! -d rust-installer ]; then
     mkdir rust-installer
     wget https://sh.rustup.rs -O rust-installer/rustup.sh
-    sh rust-installer/rustup.sh -y --default-toolchain 1.73
+    sh rust-installer/rustup.sh -y
 fi
