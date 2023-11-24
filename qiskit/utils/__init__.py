ba5a1a62f89b5e9f870a64a0b12a019bb38ffe0a
Elena Peña Tapia <57907331+ElePT@users.noreply.github.com>
2023-11-24 15:46:11 +0100

    Remove alg_globals and validation (#11252)

--- qiskit/utils/__init__.py
+++ qiskit/utils/__init__.py
@@ -36,7 +36,6 @@ Algorithm Utilities
 .. autofunction:: get_entangler_map
 .. autofunction:: validate_entangler_map
 .. autofunction:: name_args
-.. autodata:: algorithm_globals
 
 
 Optional Dependency Checkers (:mod:`qiskit.utils.optionals`)
@@ -63,8 +62,6 @@ from . import optionals
 from .circuit_utils import summarize_circuits
 from .entangler_map import get_entangler_map, validate_entangler_map
 from .name_unnamed_args import name_args
-from .algorithm_globals import algorithm_globals
-
 
 __all__ = [
     "LazyDependencyManager",
@@ -74,7 +71,6 @@ __all__ = [
     "get_entangler_map",
     "validate_entangler_map",
     "name_args",
-    "algorithm_globals",
     "add_deprecation_to_docstring",
     "deprecate_arg",
     "deprecate_arguments",
