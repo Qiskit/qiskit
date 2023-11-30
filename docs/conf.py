22ab6b09f1cebc76c451a19792a10779fb7819c1
Matthew Treinish <mtreinish@kortar.org>
2023-11-30 15:16:56 -0500

    Pivot to b1 from pre1

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ author = "Qiskit Development Team"
 # The short X.Y version
 version = "1.0"
 # The full version, including alpha/beta/rc tags
-release = "1.0.0pre1"
+release = "1.0.0b1"
 
 language = "en"
 
