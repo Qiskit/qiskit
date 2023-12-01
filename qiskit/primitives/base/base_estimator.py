90447be8569fb251555934a9c8bc858ba3dce15a
Ikko Hamamura <ikkoham@users.noreply.github.com>
2023-12-01 15:22:48 +0900

    Apply suggestions from code review
    Co-authored-by: Ian Hincks <ian.hincks@gmail.com>

--- qiskit/primitives/base/base_estimator.py
+++ qiskit/primitives/base/base_estimator.py
@@ -18,7 +18,7 @@ r"""
 Overview of EstimatorV2
 ========================
 
-:class:`~BaseEstimatorV2` estimates expectation values of quantum circuits for provided observables.
+:class:`~BaseEstimatorV2` is a primitive that estimates expectation values for provided quantum circuit and observable combinations.
 
 Following construction, and estimator is used by calling its :meth:`~.BaseEstimatorV2.run` method with a list of pubs (Primitive Unified Blocs).
 Each pub contains three values that, together, define a computation unit of work for the estimator to complete:
