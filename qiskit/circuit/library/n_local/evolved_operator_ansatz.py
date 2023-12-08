a02c2e9a83f946a266b82d24d9a773bcc4180df1
Caleb Smith <caleb.charles.smith23@gmail.com>
2023-12-08 10:10:15 -0600

    add release note fix import

--- qiskit/circuit/library/n_local/evolved_operator_ansatz.py
+++ qiskit/circuit/library/n_local/evolved_operator_ansatz.py
@@ -17,14 +17,12 @@ from collections.abc import Sequence
 
 import numpy as np
 
+from qiskit.circuit.library import NLocal, PauliEvolutionGate
 from qiskit.circuit.parameter import Parameter
 from qiskit.circuit.quantumregister import QuantumRegister
 from qiskit.circuit.quantumcircuit import QuantumCircuit
 from qiskit.quantum_info import Operator, Pauli, SparsePauliOp
 from qiskit.synthesis.evolution import LieTrotter
-
-from .pauli_evolution import PauliEvolutionGate
-from .n_local.n_local import NLocal
 
 
 class EvolvedOperatorAnsatz(NLocal):
