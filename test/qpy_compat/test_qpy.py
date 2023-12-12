2b66483b08627305309d271728094dc178a41cea
TsafrirA <tsafrir.armon@mail.huji.ac.il>
2023-12-12 20:21:36 +0200

    Yet another QPY compatibility fix

--- test/qpy_compat/test_qpy.py
+++ test/qpy_compat/test_qpy.py
@@ -432,9 +432,8 @@ def generate_schedule_blocks():
     current_version = current_version_str.split(".")
     for i in range(len(current_version[2])):
         if current_version[2][i].isalpha():
+            current_version[2] = current_version[2][:i]
             break
-    if i > 0:
-        current_version[2] = current_version[2][0:i]
     current_version = tuple(int(x) for x in current_version)
     # Parameterized schedule test is avoided.
     # Generated reference and loaded QPY object may induce parameter uuid mismatch.
