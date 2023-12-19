373122c9a2de7481dbd24babbb618b25d0ce492e
Sebastian Brandhofer <148463728+sbrandhsn@users.noreply.github.com>
2023-12-20 00:22:28 +0100

    cache open_ctrl field in controlled gates

--- qiskit/circuit/controlledgate.py
+++ qiskit/circuit/controlledgate.py
@@ -104,6 +104,7 @@ class ControlledGate(Gate):
         self.num_ctrl_qubits = num_ctrl_qubits
         self.definition = copy.deepcopy(definition)
         self._ctrl_state = None
+        self.open_ctrl = None
         self.ctrl_state = ctrl_state
         self._name = name
 
@@ -209,6 +210,7 @@ class ControlledGate(Gate):
             CircuitError: ctrl_state is invalid.
         """
         self._ctrl_state = _ctrl_state_to_int(ctrl_state, self.num_ctrl_qubits)
+        self._open_ctrl = self.ctrl_state < 2 ** self.num_ctrl_qubits - 1
 
     @property
     def params(self):
@@ -253,7 +255,11 @@ class ControlledGate(Gate):
     @property
     def _open_ctrl(self) -> bool:
         """Return whether gate has any open controls"""
-        return self.ctrl_state < 2**self.num_ctrl_qubits - 1
+        return self.open_ctrl
+
+    @_open_ctrl.setter
+    def _open_ctrl(self, open_ctrl):
+        self.open_ctrl = open_ctrl
 
     def __eq__(self, other) -> bool:
         return (
