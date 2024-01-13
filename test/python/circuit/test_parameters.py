9e36d1872b068f6850836fd28bb889752e66c0f7
Jake Lishman <jake.lishman@ibm.com>
2024-01-13 02:50:13 +0000

    Fix unused import

--- test/python/circuit/test_parameters.py
+++ test/python/circuit/test_parameters.py
@@ -32,7 +32,6 @@ from qiskit.circuit import Gate, Instruction, Parameter, ParameterExpression, Pa
 from qiskit.circuit.parametertable import ParameterReferences, ParameterTable, ParameterView
 from qiskit.circuit.exceptions import CircuitError
 from qiskit.compiler import assemble, transpile
-from qiskit.execute_function import execute
 from qiskit import pulse
 from qiskit.quantum_info import Operator
 from qiskit.test import QiskitTestCase
