9d5482a842ebb967038fb323c3c8f7e27ef06e4e
Takashi Imamichi <imamichi@jp.ibm.com>
2024-01-15 17:38:52 +0900

    copy methods from JobV1

--- qiskit/primitives/base/base_primitive_job.py
+++ qiskit/primitives/base/base_primitive_job.py
@@ -26,12 +26,53 @@ Status = TypeVar("Status")
 class BasePrimitiveJob(ABC, Generic[Result, Status]):
     """Primitive job abstract base class."""
 
+    def __init__(self, job_id: str, **kwargs) -> None:
+        """Initializes the primitive job.
+
+        Args:
+            job_id: a unique id in the context of the primitive used to run the job.
+            kwargs: Any key value metadata to associate with this job.
+        """
+        self._job_id = job_id
+        self.metadata = kwargs
+
+    def job_id(self) -> str:
+        """Return a unique id identifying the job."""
+        return self._job_id
+
     @abstractmethod
     def result(self) -> Result:
         """Return the results of the job."""
-        pass
+        raise NotImplementedError("Subclass of BasePrimitiveJob must implement `result` method.")
 
     @abstractmethod
     def status(self) -> Status:
         """Return the status of the job."""
-        pass
+        raise NotImplementedError("Subclass of BasePrimitiveJob must implement `status` method.")
+
+    @abstractmethod
+    def done(self) -> bool:
+        """Return whether the job has successfully run."""
+        raise NotImplementedError("Subclass of BasePrimitiveJob must implement `done` method.")
+
+    @abstractmethod
+    def running(self) -> bool:
+        """Return whether the job is actively running."""
+        raise NotImplementedError("Subclass of BasePrimitiveJob must implement `running` method.")
+
+    @abstractmethod
+    def cancelled(self) -> bool:
+        """Return whether the job has been cancelled."""
+        raise NotImplementedError("Subclass of BasePrimitiveJob must implement `cancelled` method.")
+
+    @abstractmethod
+    def in_final_state(self) -> bool:
+        """Return whether the job is in a final job state such as ``DONE`` or ``ERROR``."""
+        raise NotImplementedError(
+            "Subclass of BasePrimitiveJob must implement `is_final_state` method."
+        )
+
+    @abstractmethod
+    def cancel(self):
+        """Attempt to cancel the job."""
+        raise NotImplementedError("Subclass of BasePrimitiveJob must implement `cancel` method.")
--- qiskit/primitives/primitive_job.py
+++ qiskit/primitives/primitive_job.py
@@ -1,6 +1,6 @@
 # This code is part of Qiskit.
 #
-# (C) Copyright IBM 2022.
+# (C) Copyright IBM 2022, 2024.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -13,11 +13,13 @@
 Job implementation for the reference implementations of Primitives.
 """
 
+import time
 import uuid
 from concurrent.futures import ThreadPoolExecutor
-from typing import TypeVar, Union
+from typing import Callable, Optional, TypeVar, Union
 
-from qiskit.providers import JobError, JobStatus
+from qiskit.providers import JobError, JobStatus, JobTimeoutError
+from qiskit.providers.jobstatus import JOB_FINAL_STATES
 from qiskit.utils.deprecation import deprecate_func
 
 from .base.base_primitive_job import BasePrimitiveJob
@@ -29,7 +31,7 @@ Result = TypeVar("Result", bound=Union[BasePrimitiveResult, PrimitiveResult])
 
 class PrimitiveJob(BasePrimitiveJob[Result, JobStatus]):
     """
-    PrimitiveJob class for the reference implementations of Primitives.
+    Primitive job class for the reference implementations of Primitives.
     """
 
     def __init__(self, function, *args, **kwargs):
@@ -37,11 +39,53 @@ class PrimitiveJob(BasePrimitiveJob[Result, JobStatus]):
         Args:
             function: a callable function to execute the job.
         """
-        self._job_id = str(uuid.uuid4())
+        super().__init__(str(uuid.uuid4()))
         self._future = None
         self._function = function
         self._args = args
         self._kwargs = kwargs
+
+    def _submit(self):
+        if self._future is not None:
+            raise JobError("Primitive job has been submitted already.")
+
+        executor = ThreadPoolExecutor(max_workers=1)  # pylint: disable=consider-using-with
+        self._future = executor.submit(self._function, *self._args, **self._kwargs)
+        executor.shutdown(wait=False)
+
+    def result(self) -> Result:
+        self._check_submitted()
+        return self._future.result()
+
+    def status(self) -> JobStatus:
+        self._check_submitted()
+        if self._future.running():
+            return JobStatus.RUNNING
+        elif self._future.cancelled():
+            return JobStatus.CANCELLED
+        elif self._future.done() and self._future.exception() is None:
+            return JobStatus.DONE
+        return JobStatus.ERROR
+
+    def _check_submitted(self):
+        if self._future is None:
+            raise JobError("Primitive Job has not been submitted yet.")
+
+    def cancel(self):
+        self._check_submitted()
+        return self._future.cancel()
+
+    def done(self) -> bool:
+        return self.status() == JobStatus.DONE
+
+    def running(self) -> bool:
+        return self.status() == JobStatus.RUNNING
+
+    def cancelled(self) -> bool:
+        return self.status() == JobStatus.CANCELLED
+
+    def in_final_state(self) -> bool:
+        return self.status() in JOB_FINAL_STATES
 
     @deprecate_func(since="0.46.0")
     def submit(self):
@@ -54,35 +98,40 @@ class PrimitiveJob(BasePrimitiveJob[Result, JobStatus]):
         """
         self._submit()
 
-    def _submit(self):
-        if self._future is not None:
-            raise JobError("Primitive job has already been submitted.")
+    @deprecate_func(since="0.46.0")
+    def wait_for_final_state(
+        self, timeout: Optional[float] = None, wait: float = 5, callback: Optional[Callable] = None
+    ) -> None:
+        """Poll the job status until it progresses to a final state such as ``DONE`` or ``ERROR``.
 
-        executor = ThreadPoolExecutor(max_workers=1)  # pylint: disable=consider-using-with
-        self._future = executor.submit(self._function, *self._args, **self._kwargs)
-        executor.shutdown(wait=False)
+        .. deprecated:: 0.46.0
+            ``wait_for_final_state`` method is deprecated as of Qiskit 0.46 and will be removed
+            no earlier than 3 months after the release date.
 
-    def result(self) -> Result:
-        """Return the results of the job."""
-        self._check_submitted()
-        return self._future.result()
+        Args:
+            timeout: Seconds to wait for the job. If ``None``, wait indefinitely.
+            wait: Seconds between queries.
+            callback: Callback function invoked after each query.
+                The following positional arguments are provided to the callback function:
 
-    def cancel(self):
-        """Cancel the job."""
-        self._check_submitted()
-        return self._future.cancel()
+                * job_id: Job ID
+                * job_status: Status of the job from the last query
+                * job: This BaseJob instance
 
-    def status(self) -> JobStatus:
-        """Return the status of the job."""
-        self._check_submitted()
-        if self._future.running():
-            return JobStatus.RUNNING
-        elif self._future.cancelled():
-            return JobStatus.CANCELLED
-        elif self._future.done() and self._future.exception() is None:
-            return JobStatus.DONE
-        return JobStatus.ERROR
+                Note: different subclass might provide different arguments to
+                the callback function.
 
-    def _check_submitted(self):
-        if self._future is None:
-            raise JobError("Job not submitted yet!. You have to .submit() first!")
+        Raises:
+            JobTimeoutError: If the job does not reach a final state before the
+                specified timeout.
+        """
+        start_time = time.time()
+        status = self.status()
+        while status not in JOB_FINAL_STATES:
+            elapsed_time = time.time() - start_time
+            if timeout is not None and elapsed_time >= timeout:
+                raise JobTimeoutError(f"Timeout while waiting for job {self.job_id()}.")
+            if callback:
+                callback(self.job_id(), status, self)
+            time.sleep(wait)
+            status = self.status()
