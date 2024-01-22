2f9264095c4be168e6687b2985ef4231ce774d49
Jake Lishman <jake.lishman@ibm.com>
2024-01-22 18:08:00 +0000

    Poison import if Qiskit 1.0+ is detected
    This uses `importlib.metadata` to search for installations of Qiskit 1.0
    at runtime, and poison the import if one is detected.  The two packages
    are incompatible in a way that we cannot express to `pip` without having
    poisoned the `qiskit-terra` package as a whole, which is something we're
    less willing to do in order to keep it easier to install old versions
    of Qiskit-dependent software, and to keep the Qiskit 1.0 packaging story
    clean for the coming years.
    
    It is not generally expected that a user specifically attempting to
    install Qiskit 0.45/0.46 will accidentally install Qiskit 1.0 as well.
    The intention of this poisoning in the "stable" branch is so that the
    case that a user installs Qiskit 1.0, but either also includes an old
    package with a dependency purely on `qiskit-terra`, or runs a subsequent
    `pip install` command whose version resolution causes `qiskit-terra` to
    become installed.  In both these cases, it could be the case that the
    `import qiskit` command finds the `qiskit-terra` `__init__.py` file,
    and so any error we also place in Qiskit 1.0 would have been skipped.

--- qiskit/__init__.py
+++ qiskit/__init__.py
@@ -14,9 +14,30 @@
 
 """Main Qiskit public functionality."""
 
+import os
 import pkgutil
 import sys
 import warnings
+import importlib.metadata
+
+try:
+    qiskit_version = importlib.metadata.version("qiskit")
+except importlib.metadata.PackageNotFoundError:
+    # Terra doesn't care if there's no metapackage installed.
+    pass
+else:
+    _major, _ = qiskit_version.split(".", 1)
+    _suppress_error = os.environ.get("QISKIT_SUPPRESS_1_0_IMPORT_ERROR", False) == "1"
+    if int(_major) > 0 and not _suppress_error:
+        raise ImportError(
+            "Qiskit is installed in an invalid environment that has both Qiskit 1.0+"
+            " and an earlier version."
+            " You should create a new virtual environment, and ensure that you do not mix"
+            " dependencies between Qiskit pre-1.0 and post-1.0."
+            " Any packages that depend on 'qiskit-terra' are not compatible with Qiskit 1.0 and"
+            " will need to be updated."
+            " Qiskit unfortunately cannot enforce this requirement during environment resolution."
+        )
 
 import qiskit._accelerate
 
