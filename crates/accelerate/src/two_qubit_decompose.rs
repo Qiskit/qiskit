fd864e84724fd5962ae8ffc14f51a0e62b51c0c2
John Lapeyre <jlapeyre@users.noreply.github.com>
2024-01-23 13:51:03 -0500

    Update crates/accelerate/src/two_qubit_decompose.rs
    Co-authored-by: Matthew Treinish <mtreinish@kortar.org>

--- crates/accelerate/src/two_qubit_decompose.rs
+++ crates/accelerate/src/two_qubit_decompose.rs
@@ -1,3 +1,15 @@
+// This code is part of Qiskit.
+//
+// (C) Copyright IBM 2023
+//
+// This code is licensed under the Apache License, Version 2.0. You may
+// obtain a copy of this license in the LICENSE.txt file in the root directory
+// of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+//
+// Any modifications or derivative works of this code must retain this
+// copyright notice, and modified files need to carry a notice indicating
+// that they have been altered from the originals.
+
 // In numpy matrices real and imaginary components are adjacent:
 //   np.array([1,2,3], dtype='complex').view('float64')
 //   array([1., 0., 2., 0., 3., 0.])
