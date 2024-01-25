2c8a0f265d41365920cbe2d758fdf0e4b846a19f
Elena Peña Tapia <epenatap@gmail.com>
2024-01-25 11:17:22 +0100

    Apply remaining review comments

--- test/python/providers/basic_provider/test_basic_provider_backends.py
+++ test/python/providers/basic_provider/test_basic_provider_backends.py
@@ -35,33 +35,6 @@ class TestBasicProviderBackends(QiskitTestCase):
         backend = self.provider.get_backend(name=self.backend_name)
         self.assertEqual(backend.name, self.backend_name)
 
-    def test_deprecated(self):
-        """Test that deprecated names map the same backends as the new names."""
-
-        def _get_first_available_backend(provider, backend_names):
-            """Gets the first available backend."""
-            if isinstance(backend_names, str):
-                backend_names = [backend_names]
-            for backend_name in backend_names:
-                try:
-                    return provider.get_backend(backend_name).name
-                except QiskitBackendNotFoundError:
-                    pass
-            return None
-
-        deprecated_names = BasicProvider()._deprecated_backend_names()
-        for oldname, newname in deprecated_names.items():
-            expected = (
-                "WARNING:qiskit.providers.providerutils:Backend '%s' is deprecated. "
-                "Use '%s'." % (oldname, newname)
-            )
-            with self.subTest(oldname=oldname, newname=newname):
-                with self.assertLogs("qiskit.providers.providerutils", level="WARNING") as context:
-                    resolved_newname = _get_first_available_backend(BasicProvider(), newname)
-                    real_backend = BasicProvider().get_backend(resolved_newname)
-                    self.assertEqual(BasicProvider().backends(oldname)[0].name, real_backend.name)
-                self.assertEqual(context.output, [expected])
-
     def test_aliases_fail(self):
         """Test a failing backend lookup."""
         self.assertRaises(QiskitBackendNotFoundError, BasicProvider().get_backend, "bad_name")
