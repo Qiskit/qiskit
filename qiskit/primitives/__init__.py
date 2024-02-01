bf0a6361f2bc20fbb1090c61217a6361a8b1dcb3
Ian Hincks <ian.hincks@ibm.com>
2024-01-31 21:49:04 -0500

    do a bit of reorganization and write a migration section

--- qiskit/primitives/__init__.py
+++ qiskit/primitives/__init__.py
@@ -17,23 +17,112 @@ Primitives (:mod:`qiskit.primitives`)
 
 .. currentmodule:: qiskit.primitives
 
+The primitives are computational building blocks to be used in larger applications whose input 
+units, called primitive unified blocs (PUBs), require quantum resources to efficiently produce
+outputs for.
+
+Currently there are two types of primitives, whose abstractions, in their latest versions, are
+defined by :class:`~.BaseSamplerV2` and :class:`~.BaseEstimatorV2`. Samplers are responsible for
+accepting quantum circuits (or sweeps of values over parameterized circuits) and sampling from their
+classical output registers. Estimators accept combinations of circuits and observables (or sweeps
+thereof) to estimate expectation values of the observables.
+
+Qiskit implements a reference implementation for each of these abstractions, 
+:class:`~.StatevectorSampler` and :class:`~.StatevectorEstimator`.
+
 .. automodule:: qiskit.primitives.base.base_estimator
 .. automodule:: qiskit.primitives.base.base_sampler
 
-.. currentmodule:: qiskit.primitives
+Migration from Primitives V1 to V2
+==================================
 
-Estimator
-=========
+The formal distinction between the Primitives V1 and V2 APIs are the base classes from which 
+primitives implementations inherit, which are all listed at the bottom of the page. At a conceptual 
+level, however, here are some notable differences keep in mind when migrating from V1 to V2:
 
-.. autosummary::
-   :toctree: ../stubs/
+1. The V2 primitives favour vectorized inputs, where single circuits can be grouped with 
+   vector-valued (or more generally, array-valued) specifications. Each group is called a 
+   primitive unified bloc (pub), and each pub gets its own result. For example, in the estimator, 
+   you can compare the following differences:
 
-   BaseEstimator
-   Estimator
-   BackendEstimator
+   .. code-block:: python
 
-EstimatorV2
-===========
+      # Favoured V2 pattern. There is only one pub here, but there could be more.
+      job = estimator_v2.run([(circuit, [obs1, obs2, obs3, obs4])])
+      evs = job.result()[0].data.evs
+
+      # V1 equivalent, where the same circuit must be provided four times.
+      job = estimator_v1.run([circuit, circuit, circuit, circuit], [obs1, obs2, obs3, obs4])
+      evs = job.result().values
+
+   Not shown in the above example, for brevity, is that the circuit can be parametric, with arrays
+   of parameter value sets broadcasted against the array of observables. The sampler is similar,
+   but with no observables:
+
+   .. code-block:: python
+
+      # Favoured V2 pattern. There is only one pub here, but there could be more.
+      job = sampler_v2.run([(circuit, [vals1, vals2, vals3])])
+      samples = job.result()[0].data
+
+      # V1 equivalent, where the same circuit must be provided three times.
+      sampler_v1.run([circuit, circuit, circuit], [vals1, vals2, vals3])
+      quasi_dists = job.result().quasi_dists
+
+2. The V2 sampler returns samples of classical outcomes, preserving the shot order in which they 
+   were measured. This is in contrast to the V1 sampler that outputs quasi-probability distributies
+   which are instead an *estimate of the distribution* over classical outcomes. Moreover, the V2
+   sampler result objects organize data in terms of their input circuits' classical register 
+   names, which provides natural compatibility with dynamic circuits.
+
+   .. code-block:: python
+
+      circuit = QuantumCircuit(QuantumRegister(2, "qreg"), ClassicalRegister(2, "alpha"))
+      circuit.h(0)
+      circuit.cx(0, 1)
+      circuit.measure([0, 1], [0, 1])
+
+      # V1 sampler usage
+      result = sampler_v1.run([circuit]).result()
+      quasi_dists = bitstrings.get_counts()
+
+      # V2 sampler usage
+      result = sampler_v2.run([circuit]).result()
+      # these are all the bitstrings from the alpha register
+      bitstrings = result[0].data.alpha
+      # we can use it to generate a Counts mapping, which is the most similar thing to a quasi dist
+      counts = bitstrings.get_counts()           
+
+3. The V2 primitives have brought the concept of sampling overhead, inherent to all quantum systems 
+   via their inherent probabilistic nature, out of the options and into the API itself. For the
+   sampler, this means that the ``shots`` argument is now part of the :meth:`~.BaseSamplerV2.run`
+   signature, and moreover that each pub is able to specify its own value for ``shots``, which takes
+   precedence over any value given to the method. The sampler has an analagous ``precision``
+   argument that specifies the error bars that the primitive implemention should target for
+   expectation values estimates.
+
+   This concept is not present in the API of the V1 primitives, though all implementations of the 
+   V1 primitives have related settings somewhere in their options.
+
+   .. code-block:: python
+
+      # Sample two circuits at 128 shots each.
+      sampler_v2.run([circuit1, circuit2], shots=128)
+
+      # Sample two cricuits at different amounts of shots. The "None"s are necessary as placeholders
+      # for the lack of parameter values in this example.
+      sampler_v2.run([(circuit1, None, 123), (circuit2, None, 456)])
+
+      # Estimate expectation values for two pubs, both with 0.05 precision.
+      estimator_v2.run([(circuit1, obs_array1), (circuit2, obs_array_2)], precision=0.05)
+
+
+
+Primitives API
+==============
+
+Primitives V2
+-------------
 
 .. autosummary::
    :toctree: ../stubs/
@@ -41,18 +130,8 @@ EstimatorV2
    BaseEstimatorV2
    StatevectorEstimator
 
-Sampler
-=======
-
-.. autosummary::
-   :toctree: ../stubs/
-
-   BaseSampler
-   Sampler
-   BackendSampler
-
-SamplerV2
-=========
+Sampler V2
+----------
 
 .. autosummary::
    :toctree: ../stubs/
@@ -60,24 +139,59 @@ SamplerV2
    BaseSamplerV2
    StatevectorSampler
 
-Results
-=======
+Containers V2
+-------------
 
 .. autosummary::
    :toctree: ../stubs/
 
-   EstimatorResult
-   SamplerResult
+   BitArray
+   DataBin
    PrimitiveResult
    PubResult
+
+Estimator V1
+------------
+
+.. autosummary::
+   :toctree: ../stubs/
+
+   BaseEstimator
+   BaseEstimatorV1
+   Estimator
+   BackendEstimator
+   EstimatorResult
+
+
+Sampler V1
+----------
+
+.. autosummary::
+   :toctree: ../stubs/
+
+   BaseSampler
+   BaseSamplerV1
+   Sampler
+   BackendSampler
+   SamplerResult
+
 """
 
 from .backend_estimator import BackendEstimator
 from .backend_sampler import BackendSampler
-from .base import BaseEstimator, BaseEstimatorV2, BaseSampler, BaseSamplerV2
+from .base import (
+    BaseEstimator,
+    BaseEstimatorV1,
+    BaseEstimatorV2,
+    BaseSampler,
+    BaseSamplerV1,
+    BaseSamplerV2,
+)
 from .base.estimator_result import EstimatorResult
 from .base.sampler_result import SamplerResult
 from .containers import (
+    BitArray,
+    DataBin,
     PrimitiveResult,
     PubResult,
     EstimatorPubLike,
--- qiskit/primitives/base/base_estimator.py
+++ qiskit/primitives/base/base_estimator.py
@@ -11,9 +11,6 @@
 # that they have been altered from the originals.
 
 r"""
-
-.. estimator-desc:
-
 ========================
 Overview of EstimatorV2
 ========================
@@ -83,69 +80,6 @@ Here is an example of how the estimator is used.
     job_result = job2.result()
     print(f"The primitive-job finished with result {job_result}")
 
-
-========================
-Overview of EstimatorV1
-========================
-
-Estimator class estimates expectation values of quantum circuits and observables.
-
-An estimator is initialized with an empty parameter set. The estimator is used to
-create a :class:`~qiskit.providers.JobV1`, via the
-:meth:`qiskit.primitives.Estimator.run()` method. This method is called
-with the following parameters
-
-* quantum circuits (:math:`\psi_i(\theta)`): list of (parameterized) quantum circuits
-  (a list of :class:`~qiskit.circuit.QuantumCircuit` objects).
-
-* observables (:math:`H_j`): a list of :class:`~qiskit.quantum_info.SparsePauliOp`
-  objects.
-
-* parameter values (:math:`\theta_k`): list of sets of values
-  to be bound to the parameters of the quantum circuits
-  (list of list of float).
-
-The method returns a :class:`~qiskit.providers.JobV1` object, calling
-:meth:`qiskit.providers.JobV1.result()` yields the
-a list of expectation values plus optional metadata like confidence intervals for
-the estimation.
-
-.. math::
-
-    \langle\psi_i(\theta_k)|H_j|\psi_i(\theta_k)\rangle
-
-Here is an example of how the estimator is used.
-
-.. code-block:: python
-
-    from qiskit.primitives import Estimator
-    from qiskit.circuit.library import RealAmplitudes
-    from qiskit.quantum_info import SparsePauliOp
-
-    psi1 = RealAmplitudes(num_qubits=2, reps=2)
-    psi2 = RealAmplitudes(num_qubits=2, reps=3)
-
-    H1 = SparsePauliOp.from_list([("II", 1), ("IZ", 2), ("XI", 3)])
-    H2 = SparsePauliOp.from_list([("IZ", 1)])
-    H3 = SparsePauliOp.from_list([("ZI", 1), ("ZZ", 1)])
-
-    theta1 = [0, 1, 1, 2, 3, 5]
-    theta2 = [0, 1, 1, 2, 3, 5, 8, 13]
-    theta3 = [1, 2, 3, 4, 5, 6]
-
-    estimator = Estimator()
-
-    # calculate [ <psi1(theta1)|H1|psi1(theta1)> ]
-    job = estimator.run([psi1], [H1], [theta1])
-    job_result = job.result() # It will block until the job finishes.
-    print(f"The primitive-job finished with result {job_result}"))
-
-    # calculate [ <psi1(theta1)|H1|psi1(theta1)>,
-    #             <psi2(theta2)|H2|psi2(theta2)>,
-    #             <psi1(theta3)|H3|psi1(theta3)> ]
-    job2 = estimator.run([psi1, psi2, psi1], [H1, H2, H3], [theta1, theta2, theta3])
-    job_result = job2.result()
-    print(f"The primitive-job finished with result {job_result}")
 """
 
 from __future__ import annotations
@@ -181,6 +115,63 @@ class BaseEstimatorV1(BasePrimitive, Generic[T]):
     """Estimator base class.
 
     Base class for Estimator that estimates expectation values of quantum circuits and observables.
+
+    An estimator is initialized with an empty parameter set. The estimator is used to
+    create a :class:`~qiskit.providers.JobV1`, via the
+    :meth:`qiskit.primitives.Estimator.run()` method. This method is called
+    with the following parameters
+
+    * quantum circuits (:math:`\psi_i(\theta)`): list of (parameterized) quantum circuits
+      (a list of :class:`~qiskit.circuit.QuantumCircuit` objects).
+
+    * observables (:math:`H_j`): a list of :class:`~qiskit.quantum_info.SparsePauliOp`
+      objects.
+
+    * parameter values (:math:`\theta_k`): list of sets of values
+      to be bound to the parameters of the quantum circuits
+      (list of list of float).
+
+    The method returns a :class:`~qiskit.providers.JobV1` object, calling
+    :meth:`qiskit.providers.JobV1.result()` yields the
+    a list of expectation values plus optional metadata like confidence intervals for
+    the estimation.
+
+    .. math::
+
+        \langle\psi_i(\theta_k)|H_j|\psi_i(\theta_k)\rangle
+
+    Here is an example of how the estimator is used.
+
+    .. code-block:: python
+
+        from qiskit.primitives import Estimator
+        from qiskit.circuit.library import RealAmplitudes
+        from qiskit.quantum_info import SparsePauliOp
+
+        psi1 = RealAmplitudes(num_qubits=2, reps=2)
+        psi2 = RealAmplitudes(num_qubits=2, reps=3)
+
+        H1 = SparsePauliOp.from_list([("II", 1), ("IZ", 2), ("XI", 3)])
+        H2 = SparsePauliOp.from_list([("IZ", 1)])
+        H3 = SparsePauliOp.from_list([("ZI", 1), ("ZZ", 1)])
+
+        theta1 = [0, 1, 1, 2, 3, 5]
+        theta2 = [0, 1, 1, 2, 3, 5, 8, 13]
+        theta3 = [1, 2, 3, 4, 5, 6]
+
+        estimator = Estimator()
+
+        # calculate [ <psi1(theta1)|H1|psi1(theta1)> ]
+        job = estimator.run([psi1], [H1], [theta1])
+        job_result = job.result() # It will block until the job finishes.
+        print(f"The primitive-job finished with result {job_result}"))
+
+        # calculate [ <psi1(theta1)|H1|psi1(theta1)>,
+        #             <psi2(theta2)|H2|psi2(theta2)>,
+        #             <psi1(theta3)|H3|psi1(theta3)> ]
+        job2 = estimator.run([psi1, psi2, psi1], [H1, H2, H3], [theta1, theta2, theta3])
+        job_result = job2.result()
+        print(f"The primitive-job finished with result {job_result}")
     """
 
     __hash__ = None
