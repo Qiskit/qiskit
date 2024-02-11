ee54e226dc6abb4ea63906ce476c150ef37a48c4
AlexanderIvrii <alexi@il.ibm.com>
2024-02-11 23:21:41 +0200

    fix?

--- qiskit/transpiler/passes/synthesis/plugin.py
+++ qiskit/transpiler/passes/synthesis/plugin.py
@@ -363,69 +363,19 @@ High Level Synthesis
 Clifford Synthesis
 ''''''''''''''''''
 
-.. list-table:: "clifford" (:class:`qiskit.quantum_info.Clifford`) plugins
-    :header-rows: 1
-
-    * - Plugin name
-      - Plugin class
-      - Targeted connectivity
-      - Description
-    * - "ag"
-      - ~qiskit.transpiler.passes.synthesis.high_level_synthesis.AGSynthesisClifford
-      - all-to-all
-      -
-    * - "bm"
-      - ~qiskit.transpiler.passes.synthesis.high_level_synthesis.BMSynthesisClifford
-      - all-to-all
-      -
-    * - "greedy"
-      - ~qiskit.transpiler.passes.synthesis.high_level_synthesis.GreedySynthesisClifford
-      - all-to-all
-      -
-    * - "layers"
-      - ~qiskit.transpiler.passes.synthesis.high_level_synthesis.LayerSynthesisClifford
-      - all-to-all
-      -
-    * - "lnn"
-      - ~qiskit.transpiler.passes.synthesis.high_level_synthesis.LayerLnnSynthesisClifford
-      - linear
-      -
-    * - "default"
-      - ~qiskit.transpiler.passes.synthesis.high_level_synthesis.DefaultSynthesisClifford
-      - all-to-all
-      -
-
 .. autosummary::
    :toctree: ../stubs/
 
+   ~qiskit.transpiler.passes.synthesis.high_level_synthesis.AGSynthesisClifford
    ~qiskit.transpiler.passes.synthesis.high_level_synthesis.BMSynthesisClifford
    ~qiskit.transpiler.passes.synthesis.high_level_synthesis.GreedySynthesisClifford
    ~qiskit.transpiler.passes.synthesis.high_level_synthesis.LayerSynthesisClifford
    ~qiskit.transpiler.passes.synthesis.high_level_synthesis.LayerLnnSynthesisClifford
    ~qiskit.transpiler.passes.synthesis.high_level_synthesis.DefaultSynthesisClifford
 
+
 Linear Function Synthesis
 '''''''''''''''''''''''''
-
-.. list-table:: "linear" (:class:`.LinearFunction`) plugins
-    :header-rows: 1
-
-    * - Plugin name
-      - Plugin class
-      - Targeted connectivity
-      - Description
-    * - "kms"
-      - ~qiskit.transpiler.passes.synthesis.high_level_synthesis:KMSSynthesisLinearFunction
-      - linear
-      -
-    * - "pmh"
-      - ~qiskit.transpiler.passes.synthesis.high_level_synthesis:PMHSynthesisLinearFunction
-      - all-to-all
-      -
-    * - "default"
-      - ~qiskit.transpiler.passes.synthesis.high_level_synthesis:DefaultSynthesisLinearFunction
-      - all-to-all
-      -
 
 .. autosummary::
    :toctree: ../stubs/
