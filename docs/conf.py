800b298f539c3a127489328b976343c7780c3a65
Jake Lishman <jake.lishman@ibm.com>
2024-02-22 17:27:11 +0000

    Prepare Qiskit 1.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = "Qiskit Development Team"
 # The short X.Y version
 version = "1.0"
 # The full version, including alpha/beta/rc tags
-release = "1.0.0"
+release = "1.0.1"
 
 language = "en"
 
