08bae9f91c17650f6f0f7eed5ef654e00b725813
Edwin Navarro <enavarro@comcast.net>
2024-03-05 10:12:19 -0800

    Cleanup test file import

--- test/python/converters/test_circuit_to_dagdependency.py
+++ test/python/converters/test_circuit_to_dagdependency.py
@@ -14,11 +14,11 @@
 dependency."""
 
 import unittest
-from test import QiskitTestCase
 
 from qiskit.converters.dagdependency_to_circuit import dagdependency_to_circuit
 from qiskit.converters.circuit_to_dagdependency import circuit_to_dagdependency
 from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+from test import QiskitTestCase  # pylint: disable=wrong-import-order
 
 
 class TestCircuitToDagCanonical(QiskitTestCase):
