ecc46681e239f70e5343961b61bf82ad9c34cc9c
Raynel Sanchez <87539502+raynelfss@users.noreply.github.com>
2024-04-05 09:42:26 -0400

    Add: Instructions property - Instructions property returns all added to the target. - Similar behavior to source.

--- crates/accelerate/src/target.rs
+++ crates/accelerate/src/target.rs
@@ -15,6 +15,7 @@
 use hashbrown::{HashMap, HashSet};
 use pyo3::{
     prelude::*,
+    pyclass,
     types::{PyDict, PyTuple},
 };
 
@@ -40,10 +41,11 @@ pub struct Target {
     #[pyo3(get)]
     pub concurrent_measurements: Vec<HashSet<usize>>,
     // Maybe convert PyObjects into rust representations of Instruction and Data
-    gate_map: HashMap<String, PyObject>,
+    gate_map: HashMap<String, HashMap<isize, PyObject>>,
     gate_name_map: HashMap<String, PyObject>,
     global_operations: HashMap<usize, HashSet<String>>,
-    qarg_gate_map: HashMap<Vec<usize>, HashSet<String>>,
+    qarg_gate_map: HashMap<isize, HashSet<String>>,
+    qarg_hash_table: HashMap<isize, Vec<usize>>,
 }
 
 #[pymethods]
@@ -83,17 +85,10 @@ impl Target {
             gate_name_map: HashMap::new(),
             global_operations: HashMap::new(),
             qarg_gate_map: HashMap::new(),
+            qarg_hash_table: HashMap::new(),
         }
     }
 
-    // num_qubits: num_qubits.unwrap_or(0),
-    // dt: dt.unwrap_or(0.0),
-    // granularity: granularity.unwrap_or(1),
-    // min_length: min_length.unwrap_or(1),
-    // pulse_alignment: pulse_alignment.unwrap_or(1),
-    // acquire_alignment: acquire_alignment.unwrap_or(0),
-    // qubit_properties: qubit_properties.unwrap_or(Vec::new()),
-    // concurrent_measurements: concurrent_measurements.unwrap_or(Vec::new()),
     #[pyo3(text_signature = "(/, instruction, properties=None, name=None")]
     fn add_instruction(
         &mut self,
@@ -102,11 +97,14 @@ impl Target {
         properties: Option<&PyDict>,
         name: Option<String>,
     ) {
+        // Unwrap properties
         let properties: &PyDict = properties.unwrap_or(PyDict::new(py));
+        // Unwrap instruction name
         let mut instruction_name: String = match name {
             Some(name) => name,
             None => "".to_string(),
         };
+        // Unwrap instruction num qubits
         let instruction_num_qubits = match instruction.getattr(py, "num_qubits") {
             Ok(number) => match number.extract::<usize>(py) {
                 Ok(num_qubits) => num_qubits,
@@ -120,6 +118,7 @@ impl Target {
                 e
             ),
         };
+        // Cont. unwrap instruction name
         if instruction_name.is_empty() {
             instruction_name = match instruction.getattr(py, "name") {
                 Ok(i_name) => match i_name.extract::<String>(py) {
@@ -135,15 +134,19 @@ impl Target {
                 Err(e) => panic!("A name must be specified when defining a supported global operation by class: {:?}", e)
             };
         }
+        // Check if instruction exists
         if self.gate_map.contains_key(&instruction_name) {
             panic!(
                 "Instruction {:?} is already in the target",
                 &instruction_name
             );
         }
+        // Add to gate name map
         self.gate_name_map
             .insert(instruction_name.clone(), instruction);
         // TODO: Introduce a similar case to is_class
+
+        // If no properties
         if properties.is_empty() {
             if self.global_operations.contains_key(&instruction_num_qubits) {
                 self.global_operations
@@ -157,42 +160,72 @@ impl Target {
                 );
             }
         }
-        let mut qargs_val: HashMap<Vec<usize>, &PyAny> = HashMap::new();
+
+        // Obtain nested qarg hashmap
+        let mut qargs_val: HashMap<isize, PyObject> = HashMap::new();
         for (qarg, values) in properties {
+            // Obtain source qargs
             let qarg = match qarg.downcast::<PyTuple>() {
-                Ok(tuple) => match tuple.extract::<Vec<usize>>() {
-                    Ok(vec) => vec,
-                    Err(e) => panic!(
-                        "Failed to extract q_args from the provided properties: {:?}.",
-                        e
-                    ),
-                },
+                Ok(tuple) => tuple,
+                Err(e) => panic!(
+                    "Failed to downcast q_args from the provided properties: {:?}.",
+                    e
+                ),
+            };
+            // Obtain qarg hash for mapping
+            let qarg_hash = match qarg.hash() {
+                Ok(vec) => vec,
+                Err(e) => panic!(
+                    "Failed to hash q_args from the provided properties: {:?}.",
+                    e
+                ),
+            };
+            // Obtain values of qargs
+            let qarg = match qarg.extract::<Vec<usize>>() {
+                Ok(vec) => vec,
                 Err(e) => panic!(
                     "Failed to extract q_args from the provided properties: {:?}.",
                     e
                 ),
             };
+            // Store qargs hash value.
+            self.qarg_hash_table.insert(qarg_hash, qarg.clone());
+            println!("{:?}: {:?}", qarg, qarg_hash);
             if !qarg.is_empty() && qarg.len() != instruction_num_qubits {
                 panic!("The number of qubits for {:?} does not match the number of qubits in the properties dictionary: {:?}", &instruction_name, qarg)
             }
             if !qarg.is_empty() {
                 self.num_qubits = self
                     .num_qubits
-                    .max(qarg.iter().cloned().fold(0, usize::max))
+                    .max(qarg.iter().cloned().fold(0, usize::max) + 1)
             }
-            qargs_val.insert(qarg.clone(), values);
-            if self.qarg_gate_map.contains_key(&qarg) {
+            qargs_val.insert(qarg_hash, values.to_object(py));
+            if self.qarg_gate_map.contains_key(&qarg_hash) {
                 self.qarg_gate_map
-                    .get_mut(&qarg)
+                    .get_mut(&qarg_hash)
                     .unwrap()
                     .insert(instruction_name.clone());
             } else {
                 self.qarg_gate_map
-                    .insert(qarg.clone(), HashSet::from([instruction_name.clone()]));
+                    .insert(qarg_hash, HashSet::from([instruction_name.clone()]));
             }
         }
-        self.gate_map
-            .insert(instruction_name, qargs_val.to_object(py));
+        self.gate_map.insert(instruction_name, qargs_val);
+    }
+
+    #[getter]
+    fn instructions(&self) -> PyResult<Vec<(PyObject, Vec<usize>)>> {
+        let mut instruction_list: Vec<(PyObject, Vec<usize>)> = vec![];
+        for op in self.gate_map.keys() {
+            for qarg in self.gate_map[op].keys() {
+                let instruction_pair = (
+                    self.gate_name_map[op].clone(),
+                    self.qarg_hash_table[qarg].clone(),
+                );
+                instruction_list.push(instruction_pair);
+            }
+        }
+        Ok(instruction_list)
     }
 }
 
