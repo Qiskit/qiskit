2e5648b01809c6f9ffd011b75a2ac03aaa4ac316
Hirmay Sandesara <56473003+Hirmay@users.noreply.github.com>
2024-04-09 15:01:33 +0530

    Update __init__.py

--- qiskit/circuit/library/data_preparation/__init__.py
+++ qiskit/circuit/library/data_preparation/__init__.py
@@ -41,7 +41,7 @@ of the complete initial data.
 from .pauli_feature_map import PauliFeatureMap
 from .z_feature_map import ZFeatureMap
 from .zz_feature_map import ZZFeatureMap
-from .state_preparation import StatePreparation
+from .state_preparation import StatePreparation, Generalized_Uniform_Superposition_Gate
 from .initializer import Initialize
 
-__all__ = ["PauliFeatureMap", "ZFeatureMap", "ZZFeatureMap", "StatePreparation", "Initialize"]
+__all__ = ["PauliFeatureMap", "ZFeatureMap", "ZZFeatureMap", "StatePreparation", "Generalized_Uniform_Superposition_Gate", "Initialize"]
