c0a1bf7f54d470c81408392bb7b7a748c68cf5aa
Joe Schulte <joesho112358@gmail.com>
2024-04-09 23:11:14 -0400

    removing lint exclusion statement in favor of unused variable

--- test/python/circuit/library/test_state_preparation.py
+++ test/python/circuit/library/test_state_preparation.py
@@ -98,10 +98,9 @@ class TestStatePreparation(QiskitTestCase):
 
     def test_incompatible_int_state_and_qubit_args(self):
         """Test error raised if number of qubits not compatible with  integer state arg"""
-        # pylint: disable=pointless-statement
         with self.assertRaises(QiskitError):
             stateprep = StatePreparation(5, num_qubits=2)
-            stateprep.definition
+            _ = stateprep.definition
 
     def test_int_state_and_no_qubit_args(self):
         """Test automatic determination of qubit number"""
