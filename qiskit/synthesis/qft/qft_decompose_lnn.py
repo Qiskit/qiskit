bda1ca45a6f376456ba72191ccc1cb8869b27973
ShellyGarion <shelly@il.ibm.com>
2024-04-15 05:59:17 -0500

    update other synthesis functions

--- qiskit/synthesis/qft/qft_decompose_lnn.py
+++ qiskit/synthesis/qft/qft_decompose_lnn.py
@@ -15,7 +15,7 @@ Circuit synthesis for a QFT circuit.
 
 import numpy as np
 from qiskit.circuit import QuantumCircuit
-from qiskit.synthesis.linear_phase.cz_depth_lnn import _append_cx_stage1, _append_cx_stage2
+from qiskit.synthesis.permutation import synth_permutation_reverse_lnn_kms
 
 
 def synth_qft_line(
@@ -65,10 +65,7 @@ def synth_qft_line(
     if not do_swaps:
         # Add a reversal network for LNN connectivity in depth 2*n+2,
         # based on Kutin at al., https://arxiv.org/abs/quant-ph/0701194, Section 5.
-        for _ in range((num_qubits + 1) // 2):
-            qc = _append_cx_stage1(qc, num_qubits)
-            qc = _append_cx_stage2(qc, num_qubits)
-        if (num_qubits % 2) == 0:
-            qc = _append_cx_stage1(qc, num_qubits)
+        qc_rev = synth_permutation_reverse_lnn_kms(num_qubits)
+        qc = qc.compose(qc_rev)
 
     return qc
