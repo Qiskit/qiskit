2f85d24af3febb10a92a021a402ad0e27604f935
Arnau Casau <47946624+arnaucasau@users.noreply.github.com>
2024-05-07 16:03:26 +0200

    add header

--- crates/accelerate/src/common.rs
+++ crates/accelerate/src/common.rs
@@ -1,3 +1,15 @@
+// This code is part of Qiskit.
+//
+// (C) Copyright IBM 2024
+//
+// This code is licensed under the Apache License, Version 2.0. You may
+// obtain a copy of this license in the LICENSE.txt file in the root directory
+// of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+//
+// Any modifications or derivative works of this code must retain this
+// copyright notice, and modified files need to carry a notice indicating
+// that they have been altered from the originals.
+
 use num_complex::Complex64;
 use numpy::ndarray::{array, Array2, ArrayView2};
 
