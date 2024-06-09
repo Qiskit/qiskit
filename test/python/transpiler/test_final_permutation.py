71225c3b8c9d66eca5fbb93237ac0f10220fb0fb
AlexanderIvrii <alexi@il.ibm.com>
2024-06-09 23:28:38 +0200

    cleanup of the test file

--- test/python/transpiler/test_final_permutation.py
+++ test/python/transpiler/test_final_permutation.py
@@ -10,27 +10,28 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""FinalPermutation in transpile flow pass testing"""
+"""Testing FinalPermutation in transpile flows."""
 
 import unittest
 
 from qiskit import transpile
 from qiskit.circuit import QuantumCircuit
 from qiskit.circuit.library import PermutationGate
+from qiskit.circuit.random import random_circuit
 from qiskit.providers.fake_provider import GenericBackendV2
 from qiskit.quantum_info import Operator
 from qiskit.transpiler import CouplingMap
 from qiskit.transpiler.passes import ElidePermutations, StarPreRouting
 from qiskit.transpiler.preset_passmanagers import generate_preset_pass_manager
+
 from test import QiskitTestCase  # pylint: disable=wrong-import-order
-from utils import random_circuit
 
 
 class TestFinalPermutationInTranspile(QiskitTestCase):
-    """Tests for FixedPoint pass."""
+    """Tests for FinalPermutation in transpile flows."""
 
     def test_sabre_1(self):
-        """SabreLayout"""
+        """Transpile flow includes SabreLayout pass."""
         qc = QuantumCircuit(6)
         qc.cx(0, 2)
         qc.cx(0, 4)
@@ -38,36 +39,27 @@ class TestFinalPermutationInTranspile(QiskitTestCase):
         qc.cx(1, 3)
         qc.cx(1, 5)
         qc.cx(3, 5)
-
         op = Operator(qc)
-
-        cm = CouplingMap.from_line(6)
-        cm.make_symmetric()
-        print(cm)
-
-        qct = transpile(qc, optimization_level=3, coupling_map=cm, basis_gates=["cx", "u"])
-        self.assertTrue(Operator.from_circuit(qct).equiv(op))
+        coupling_map = CouplingMap.from_line(6)
+        qct = transpile(qc, optimization_level=3, coupling_map=coupling_map, basis_gates=["cx", "u"])
         self.assertTrue(Operator._from_circuit_new(qct).equiv(op))
 
     def test_sabre_2(self):
-        """Sabre."""
+        """Transpile flow includes SabreLayout pass."""
         qc = QuantumCircuit(6)
         qc.cx(0, 1)
         qc.cx(0, 2)
         qc.cx(0, 3)
         qc.cx(0, 4)
         qc.cx(0, 5)
-
         op = Operator(qc)
-
-        cm = CouplingMap.from_line(6)
-
-        qct = transpile(qc, optimization_level=3, coupling_map=cm, basis_gates=["cx", "u"])
+        coupling_map = CouplingMap.from_line(6)
+        qct = transpile(qc, optimization_level=3, coupling_map=coupling_map, basis_gates=["cx", "u"])
         self.assertTrue(Operator.from_circuit(qct).equiv(op))
         self.assertTrue(Operator._from_circuit_new(qct).equiv(op))
 
     def test_sabre_3(self):
-        """Sabre."""
+        """Transpile flow includes SabreLayout pass."""
         qc = QuantumCircuit(6)
         qc.cx(0, 1)
         qc.cx(0, 2)
@@ -79,17 +71,13 @@ class TestFinalPermutationInTranspile(QiskitTestCase):
         qc.cx(0, 3)
         qc.cx(0, 4)
         qc.cx(0, 5)
-
         op = Operator(qc)
-
-        cm = CouplingMap.from_line(6)
-
-        qct = transpile(qc, optimization_level=3, coupling_map=cm, basis_gates=["cx", "u"])
-        self.assertTrue(Operator.from_circuit(qct).equiv(op))
+        coupling_map = CouplingMap.from_line(6)
+        qct = transpile(qc, optimization_level=3, coupling_map=coupling_map, basis_gates=["cx", "u"])
         self.assertTrue(Operator._from_circuit_new(qct).equiv(op))
 
     def test_elide_sabre_1(self):
-        """Circuit with PermutationGate. ElidePermutations."""
+        """Transpile flow includes ElidePermutations and SabreLayout passes."""
         qc = QuantumCircuit(6)
         qc.cx(0, 2)
         qc.cx(0, 4)
@@ -99,16 +87,12 @@ class TestFinalPermutationInTranspile(QiskitTestCase):
         qc.cx(3, 5)
         qc.append(PermutationGate([1, 2, 0]), [0, 1, 2])
         op = Operator(qc)
-
-        cm = CouplingMap.from_line(6)
-        cm.make_symmetric()
-
-        qct = transpile(qc, optimization_level=3, coupling_map=cm, basis_gates=["cx", "u"])
-        self.assertTrue(Operator.from_circuit(qct).equiv(op))
+        coupling_map = CouplingMap.from_line(6)
+        qct = transpile(qc, optimization_level=3, coupling_map=coupling_map, basis_gates=["cx", "u"])
         self.assertTrue(Operator._from_circuit_new(qct).equiv(op))
 
     def test_elide_sabre_2(self):
-        """This runs ElidePermutations + Sabre"""
+        """Transpile flow includes ElidePermutations and SabreLayout passes."""
         qc = QuantumCircuit(6)
         qc.cx(0, 2)
         qc.cx(0, 4)
@@ -118,41 +102,32 @@ class TestFinalPermutationInTranspile(QiskitTestCase):
         qc.cx(1, 5)
         qc.cx(3, 5)
         op = Operator(qc)
-
-        cm = CouplingMap.from_line(6)
-        cm.make_symmetric()
-        print(cm)
-
-        qct = transpile(qc, optimization_level=3, coupling_map=cm, basis_gates=["cx", "u"])
-        self.assertTrue(Operator.from_circuit(qct).equiv(op))
+        coupling_map = CouplingMap.from_line(6)
+        qct = transpile(qc, optimization_level=3, coupling_map=coupling_map, basis_gates=["cx", "u"])
         self.assertTrue(Operator._from_circuit_new(qct).equiv(op))
 
     def test_star_vf2(self):
-        "StarPreRouting + perfect map"
+        """Transpile flow includes StarPreRouting."""
         qc = QuantumCircuit(6)
         qc.cx(0, 1)
         qc.cx(0, 2)
         qc.cx(0, 3)
         qc.cx(0, 4)
         qc.cx(0, 5)
-
         op = Operator(qc)
-
-        cm = CouplingMap.from_line(6)
-
+        coupling_map = CouplingMap.from_line(6)
         spm = generate_preset_pass_manager(
             optimization_level=3,
             seed_transpiler=1234,
-            coupling_map=cm,
+            coupling_map=coupling_map,
             basis_gates=["u", "cz"],
         )
         spm.init += StarPreRouting()
         qct = spm.run(qc)
-        self.assertTrue(Operator.from_circuit(qct).equiv(op))
         self.assertTrue(Operator._from_circuit_new(qct).equiv(op))
 
     def test_elide_star_sabre(self):
-        """Both StarPreRouting+Elide."""
+        """Transpile flow includes ElidePermutations followed by StarPreRouting."""
         qc = QuantumCircuit(5)
         qc.h(0)
         qc.swap(0, 2)
@@ -164,10 +139,8 @@ class TestFinalPermutationInTranspile(QiskitTestCase):
         qc.cx(0, 4)
         qc.append(PermutationGate([0, 2, 1]), [0, 1, 2])
         qc.h(1)
-
         op = Operator(qc)
         coupling_map = CouplingMap.from_line(5)
-
         spm = generate_preset_pass_manager(
             optimization_level=3,
             seed_transpiler=1234,
@@ -177,11 +150,10 @@ class TestFinalPermutationInTranspile(QiskitTestCase):
         spm.init += ElidePermutations()
         spm.init += StarPreRouting()
         qct = spm.run(qc)
-        self.assertTrue(Operator.from_circuit(qct).equiv(op))
         self.assertTrue(Operator._from_circuit_new(qct).equiv(op))
 
-    def test_star_elide_sabre(self):
-        """Both StarPreRouting+Elide."""
+    def test_star_elide_sabre_1(self):
+        """Transpile flow includes StarPreRouting followed by ElidePermutations."""
         qc = QuantumCircuit(5)
         qc.h(0)
         qc.swap(0, 2)
@@ -193,10 +165,8 @@ class TestFinalPermutationInTranspile(QiskitTestCase):
         qc.cx(0, 4)
         qc.append(PermutationGate([0, 2, 1]), [0, 1, 2])
         qc.h(1)
-
         op = Operator(qc)
         coupling_map = CouplingMap.from_line(5)
-
         spm = generate_preset_pass_manager(
             optimization_level=3,
             seed_transpiler=1234,
@@ -206,18 +176,16 @@ class TestFinalPermutationInTranspile(QiskitTestCase):
         spm.init += StarPreRouting()
         spm.init += ElidePermutations()
         qct = spm.run(qc)
-        self.assertTrue(Operator.from_circuit(qct).equiv(op))
         self.assertTrue(Operator._from_circuit_new(qct).equiv(op))
 
     def test_star_elide_sabre_2(self):
-        """First StarPreRouting, then Elide."""
+        """Transpile flow includes StarPreRouting followed by ElidePermutations."""
         qc = QuantumCircuit(6)
         qc.cx(0, 1)
         qc.cx(0, 2)
         qc.cx(0, 3)
         qc.cx(0, 4)
         qc.cx(0, 5)
-
         op = Operator(qc)
         spm = generate_preset_pass_manager(
             optimization_level=3,
@@ -228,49 +196,17 @@ class TestFinalPermutationInTranspile(QiskitTestCase):
         spm.init += StarPreRouting()
         spm.init += ElidePermutations()
         qct = spm.run(qc)
-        # self.assertTrue(Operator.from_circuit(qct).equiv(op))
         self.assertTrue(Operator._from_circuit_new(qct).equiv(op))
 
-    def test_star_elide_3(self):
-        """First StarPreRouting, then Elide."""
-        qc = QuantumCircuit(5)
-        qc.h(0)
-        qc.swap(0, 2)
-        qc.cx(0, 1)
-        qc.swap(1, 0)
-        qc.cx(0, 1)
-        qc.cx(0, 2)
-        qc.cx(0, 3)
-        qc.cx(0, 4)
-        qc.append(PermutationGate([0, 2, 1]), [0, 1, 2])
-        qc.h(1)
-
-        op = Operator(qc)
-        spm = generate_preset_pass_manager(
-            optimization_level=3,
-            seed_transpiler=1234,
-            coupling_map=CouplingMap.from_line(5),
-            basis_gates=["u", "cz"],
-        )
-        spm.init += StarPreRouting()
-        spm.init += ElidePermutations()
-        qct = spm.run(qc)
-        self.assertTrue(Operator.from_circuit(qct).equiv(op))
-        self.assertTrue(Operator._from_circuit_new(qct).equiv(op))
-
-    def test_random_embed_1(self):
-        """More qubits in coupling map"""
+    def test_random_embed(self):
+        """Physical circuit has more qubits than the virtual circuit."""
         for seed in range(10):
-            qc = random_circuit(4, 5, 3, seed=seed)
-            nq = qc.num_qubits
-
-            coupling_map = CouplingMap.from_line(nq + 2)
-
-            qc2 = QuantumCircuit(nq + 2)
-            qc2 = qc2.compose(qc, range(nq))
-
-            extended_op = Operator(qc2)
-
+            num_qubits = 4
+            qc = random_circuit(num_qubits, 10, 3, seed=seed)
+            coupling_map = CouplingMap.from_line(6)
+            extended_qc = QuantumCircuit(6)
+            extended_qc = extended_qc.compose(qc, range(4))
+            extended_op = Operator(extended_qc)
             qct = transpile(
                 qc,
                 optimization_level=3,
@@ -278,41 +214,31 @@ class TestFinalPermutationInTranspile(QiskitTestCase):
                 basis_gates=["cx", "u"],
                 seed_transpiler=3,
             )
-
-            transpiled_op = Operator.from_circuit(qct)
             transpiled_op_new = Operator._from_circuit_new(qct)
-
-            self.assertTrue(transpiled_op.equiv(extended_op))
             self.assertTrue(transpiled_op_new.equiv(extended_op))
 
     def test_routing_methods(self):
-        """Stochastic Swap for mapping (sets final layout)"""
+        """Testing various routing methods."""
         qc = random_circuit(4, 5, 3, seed=1)
-        nq = qc.num_qubits
-        op = Operator(qc)
-        cm = CouplingMap.from_line(nq + 2)
-        qc2 = QuantumCircuit(nq + 2)
-        qc2 = qc2.compose(qc, range(nq))
-        extended_op = Operator(qc2)
+        coupling_map = CouplingMap.from_line(6)
+        extended_qc = QuantumCircuit(6)
+        extended_qc = extended_qc.compose(qc, range(4))
+        extended_op = Operator(extended_qc)
 
         for routing_method in ["stochastic", "lookahead", "basic", "sabre"]:
             qct = transpile(
                 qc,
                 optimization_level=3,
-                coupling_map=cm,
+                coupling_map=coupling_map,
                 basis_gates=["cx", "u"],
                 seed_transpiler=3,
                 routing_method=routing_method,
             )
-
-            transpiled_op = Operator.from_circuit(qct)
             transpiled_op_new = Operator._from_circuit_new(qct)
-
-            self.assertTrue(transpiled_op.equiv(extended_op))
             self.assertTrue(transpiled_op_new.equiv(extended_op))
 
     def test_elide_and_routing_methods(self):
-        """Stochastic Swap for mapping (sets final layout)"""
+        """Testing ElidePermutations followed by various routing methods."""
         qc = QuantumCircuit(6)
         qc.cx(0, 2)
         qc.cx(0, 4)
@@ -321,30 +247,25 @@ class TestFinalPermutationInTranspile(QiskitTestCase):
         qc.cx(1, 5)
         qc.cx(3, 5)
         qc.append(PermutationGate([1, 2, 0]), [0, 1, 2])
-        nq = qc.num_qubits
-        op = Operator(qc)
-        cm = CouplingMap.from_line(nq + 2)
-        qc2 = QuantumCircuit(nq + 2)
-        qc2 = qc2.compose(qc, range(nq))
-        extended_op = Operator(qc2)
+        coupling_map = CouplingMap.from_line(8)
+        extended_qc = QuantumCircuit(8)
+        extended_qc = extended_qc.compose(qc, range(6))
+        extended_op = Operator(extended_qc)
 
         for routing_method in ["stochastic", "lookahead", "basic", "sabre"]:
             qct = transpile(
                 qc,
                 optimization_level=3,
-                coupling_map=cm,
+                coupling_map=coupling_map,
                 basis_gates=["cx", "u"],
                 seed_transpiler=3,
                 routing_method=routing_method,
             )
-
-            transpiled_op = Operator.from_circuit(qct)
             transpiled_op_new = Operator._from_circuit_new(qct)
-
-            self.assertTrue(transpiled_op.equiv(extended_op))
             self.assertTrue(transpiled_op_new.equiv(extended_op))
 
     def test_with_post_layout(self):
+        """Test involving VF2PostLayout pass."""
         qc = QuantumCircuit(3)
         qc.cx(0, 1)
         qc.cx(1, 2)
@@ -352,12 +273,9 @@ class TestFinalPermutationInTranspile(QiskitTestCase):
         qc.cx(0, 1)
         qc.cx(1, 2)
 
-        op = Operator(qc)
-
-        qc2 = QuantumCircuit(5)
-        qc2 = qc2.compose(qc, range(3))
-        extended_op = Operator(qc2)
-
+        extended_qc = QuantumCircuit(5)
+        extended_qc = extended_qc.compose(qc, range(3))
+        extended_op = Operator(extended_qc)
         coupling_map = CouplingMap.from_line(5)
         backend = GenericBackendV2(
             num_qubits=5,
@@ -366,11 +284,7 @@ class TestFinalPermutationInTranspile(QiskitTestCase):
             seed=0,
         )
         qct = transpile(qc, backend=backend, seed_transpiler=4242)
-
-        transpiled_op = Operator.from_circuit(qct)
         transpiled_op_new = Operator._from_circuit_new(qct)
-
-        self.assertTrue(transpiled_op.equiv(extended_op))
         self.assertTrue(transpiled_op_new.equiv(extended_op))
 
 
