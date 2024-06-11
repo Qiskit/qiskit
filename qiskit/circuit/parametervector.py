4f75098e25db7bad620aeb22908862333094a875
Jim Garrison <garrison@ibm.com>
2024-06-11 09:40:57 -0400

    Fix `ParameterVector` methods: `params` and `index`
    This changes the `params` and `index` methods on `ParameterVector`
    to _not_ return historical parameters, following
    https://github.com/Qiskit/qiskit/issues/12541#issuecomment-2160631785.
    
    I also simplified a bit of the implementation and renamed `_params`
    to `_all_params` so it is more unlikely someone might misuse it
    and create a similar problem in the future.
    
    Fixes #12541.

--- qiskit/circuit/parametervector.py
+++ qiskit/circuit/parametervector.py
@@ -50,14 +50,14 @@ class ParameterVectorElement(Parameter):
 class ParameterVector:
     """ParameterVector class to quickly generate lists of parameters."""
 
-    __slots__ = ("_name", "_params", "_size", "_root_uuid")
+    __slots__ = ("_name", "_all_params", "_size", "_root_uuid")
 
     def __init__(self, name, length=0):
         self._name = name
         self._size = length
         self._root_uuid = uuid4()
         root_uuid_int = self._root_uuid.int
-        self._params = [
+        self._all_params = [
             ParameterVectorElement(self, i, UUID(int=root_uuid_int + i)) for i in range(length)
         ]
 
@@ -69,29 +69,23 @@ class ParameterVector:
     @property
     def params(self):
         """Returns the list of parameters in the ParameterVector."""
-        return self._params
+        return self._all_params[: self._size]
 
     def index(self, value):
         """Returns first index of value."""
-        return self._params.index(value)
+        return self.params.index(value)
 
     def __getitem__(self, key):
-        if isinstance(key, slice):
-            start, stop, step = key.indices(self._size)
-            return self.params[start:stop:step]
-
-        if key > self._size:
-            raise IndexError(f"Index out of range: {key} > {self._size}")
         return self.params[key]
 
     def __iter__(self):
-        return iter(self.params[: self._size])
+        return iter(self.params)
 
     def __len__(self):
         return self._size
 
     def __str__(self):
-        return f"{self.name}, {[str(item) for item in self.params[: self._size]]}"
+        return f"{self.name}, {[str(item) for item in self.params]}"
 
     def __repr__(self):
         return f"{self.__class__.__name__}(name={self.name}, length={len(self)})"
@@ -103,12 +97,12 @@ class ParameterVector:
         previous elements are cached and not re-generated if the vector is enlarged again.
         This is to ensure that the parameter instances do not change.
         """
-        if length > len(self._params):
+        if length > len(self._all_params):
             root_uuid_int = self._root_uuid.int
-            self._params.extend(
+            self._all_params.extend(
                 [
                     ParameterVectorElement(self, i, UUID(int=root_uuid_int + i))
-                    for i in range(len(self._params), length)
+                    for i in range(len(self._all_params), length)
                 ]
             )
         self._size = length
