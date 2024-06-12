fd2ee652dd1ffa65a15917a43f70bc48b74ab252
Jim Garrison <jim@garrison.cc>
2024-06-11 22:15:59 -0400

    Disable wrong-import-order in `__init__.py`

--- qiskit/__init__.py
+++ qiskit/__init__.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-# pylint: disable=wrong-import-position
+# pylint: disable=wrong-import-position,wrong-import-order
 
 """Main Qiskit public functionality."""
 
