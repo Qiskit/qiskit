3810391424228c88e57862bac84bf78d09a88e40
Elena Peña Tapia <epenatap@gmail.com>
2024-06-20 10:25:16 +0200

    Prepare 1.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ author = "Qiskit Development Team"
 # The short X.Y version
 version = "1.1"
 # The full version, including alpha/beta/rc tags
-release = "1.1.0"
+release = "1.1.1"
 
 language = "en"
 
