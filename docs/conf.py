2b2be0307ff17966e9687ecfa581b3df536f348b
Raynel Sanchez <87539502+raynelfss@users.noreply.github.com>
2024-06-20 18:05:32 +0200

    Prepare 0.46.2 release (#12616)

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = "Qiskit Development Team"
 # The short X.Y version
 version = "0.46"
 # The full version, including alpha/beta/rc tags
-release = "0.46.1"
+release = "0.46.2"
 
 language = "en"
 
--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ csp_requirements = ["python-constraint>=1.4"]
 
 setup(
     name="qiskit-terra",
-    version="0.46.1",
+    version="0.46.2",
     description="Software for developing quantum computing programs",
     long_description=README,
     long_description_content_type="text/markdown",
