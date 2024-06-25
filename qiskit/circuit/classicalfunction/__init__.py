5e47c14464159b4a6b88c3176c13d9613bab7664
Hwa <weechinghwa@gmail.com>
2024-06-26 07:47:56 +0800

    Update __init__.py
    line break to comply to pylint

--- qiskit/circuit/classicalfunction/__init__.py
+++ qiskit/circuit/classicalfunction/__init__.py
@@ -55,7 +55,9 @@ significant bit and the right-most bit (``d``) is the least significant bit.
 
     The functionality of `qiskit.circuit.classicalfunction` requires `tweedledum`, 
     which isn't available on all platforms (up to Python version 3.11).
-    See `tweedledum installation guide <https://github.com/boschmitt/tweedledum/tree/master?tab=readme-ov-file#installation>`_ for more details.
+    See `tweedledum installation guide 
+    <https://github.com/boschmitt/tweedledum/tree/master?tab=readme-ov-file#installation>`_ 
+    for more details.
 
 Supplementary Information
 =========================
