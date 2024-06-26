96d397100da8d3c3f3e03e4b53c1babb6d3e9ba0
Jason Saroni <77505813+jsaroni@users.noreply.github.com>
2024-06-26 13:03:36 -0400

    Update backend_estimator_v2.py
    Used 1-line replacement for coeff_sq_sum removing pauli. Had to use _preprocess_pub to get the right data type for using .values(). From the EstimatorV2 tutorial example bc_obs is of the form [{'II': 1.0, 'IZ': 2.0, 'XI': 3.0}] so bc_obs[0] gets all the dictionary terms {'II': 1.0, 'IZ': 2.0, 'XI': 3.0}.

--- qiskit/primitives/backend_estimator_v2.py
+++ qiskit/primitives/backend_estimator_v2.py
@@ -160,15 +160,12 @@ class BackendEstimatorV2(BaseEstimatorV2):
 
     def _run(self, pubs: list[EstimatorPub]) -> PrimitiveResult[PubResult]:
         pub_dict = defaultdict(list)
-        for pub in pubs:
-            data = self._preprocess_pub(pub)
-        bc_obs = data.observables
-        coeff_sq_sum = 0
-        for pauli, coeff in bc_obs[0].items():
-            coeff_sq_sum += coeff**2
         # consolidate pubs with the same number of shots
         for i, pub in enumerate(pubs):
-            shots = int(math.ceil(coeff_sq_sum / pub.precision**2))
+            data = self._preprocess_pub(pub)
+            bc_obs = data.observables
+            coeff_sq_sum = sum(coeff**2 for coeff in bc_obs[0].values())
+            shots = int(math.ceil(coeff_sq_sum/pub.precision**2))
             pub_dict[shots].append(i)
 
         results = [None] * len(pubs)
