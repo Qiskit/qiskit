ada3d38935897c53058ab732d6c48ade0165a65f
Luciano Bello <bel@zurich.ibm.com>
2024-07-02 11:17:57 +0200

    aer warnings

--- test/python/providers/test_fake_backends.py
+++ test/python/providers/test_fake_backends.py
@@ -105,17 +105,17 @@ class TestFakeBackends(QiskitTestCase):
         optimization_level=[0, 1, 2, 3],
     )
     def test_circuit_on_fake_backend_v2(self, backend, optimization_level):
-        if not optionals.HAS_AER and backend.num_qubits > 20:
-            self.skipTest(f"Unable to run fake_backend {backend.name} without qiskit-aer")
-        job = backend.run(
-            transpile(
-                self.circuit, backend, seed_transpiler=42, optimization_level=optimization_level
-            ),
-            seed_simulator=42,
-        )
         with warnings.catch_warnings():
             # TODO remove this catch once Aer stops using QobjDictField
             warnings.simplefilter("ignore", category=DeprecationWarning)
+            if not optionals.HAS_AER and backend.num_qubits > 20:
+                self.skipTest(f"Unable to run fake_backend {backend.name} without qiskit-aer")
+            job = backend.run(
+                transpile(
+                    self.circuit, backend, seed_transpiler=42, optimization_level=optimization_level
+                ),
+                seed_simulator=42,
+            )
             result = job.result()
         counts = result.get_counts()
         max_count = max(counts.items(), key=operator.itemgetter(1))[0]
