a71f09f2e348d5616d7f4a735967a36ab291aa8e
Abdalla  Elhendawy <abdall.elhendawy50@gmail.com>
2024-07-03 11:12:49 +0300

    Re-adding functions API docs

--- qiskit/synthesis/linear/cnot_synth.py
+++ qiskit/synthesis/linear/cnot_synth.py
@@ -5,6 +5,7 @@
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
 # of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
@@ -25,8 +26,36 @@ from qiskit.exceptions import QiskitError
 def synth_cnot_count_full_pmh(
     state: list[list[bool]] | np.ndarray[bool], section_size: int = 2
 ) -> QuantumCircuit:
-    """Synthesize linear reversible circuits using the Patel-Markov-Hayes algorithm with virtual padding."""
+    """
+    Synthesize linear reversible circuits for all-to-all architecture
+    using Patel, Markov and Hayes method.
 
+    This function is an implementation of the Patel, Markov and Hayes algorithm from [1]
+    for optimal synthesis of linear reversible circuits for all-to-all architecture,
+    as specified by an :math:`n \\times n` matrix.
+
+    Args:
+        state: :math:`n \\times n` boolean invertible matrix, describing
+            the state of the input circuit
+        section_size: The size of each section, used in the
+            Patel–Markov–Hayes algorithm [1]. ``section_size`` must be a factor of the number
+            of qubits.
+
+    Returns:
+        QuantumCircuit: a CX-only circuit implementing the linear transformation.
+
+    Raises:
+        QiskitError: when variable ``state`` isn't of type ``numpy.ndarray``
+
+    We used concept called virtual padding to generate marices filled by Zeros
+    when the algorithm fail to handle a section_size more than the number of qubits.
+
+    References:
+        1. Patel, Ketan N., Igor L. Markov, and John P. Hayes,
+           *Optimal synthesis of linear reversible circuits*,
+           Quantum Information & Computation 8.3 (2008): 282-294.
+           `arXiv:quant-ph/0302002 [quant-ph] <https://arxiv.org/abs/quant-ph/0302002>`_
+    """
     if not isinstance(state, (list, np.ndarray)):
         raise QiskitError(
             "state should be of type list or numpy.ndarray, "
@@ -60,7 +89,40 @@ def synth_cnot_count_full_pmh(
     return circ
 
 def _lwr_cnot_synth(state, section_size, num_qubits):
-    """Helper function for the Patel-Markov-Hayes algorithm with virtual padding."""
+
+    """
+    This function is a helper function of the algorithm for optimal synthesis
+    of linear reversible circuits (the Patel–Markov–Hayes algorithm). It works
+    like gaussian elimination, except that it works a lot faster, and requires
+    fewer steps (and therefore fewer CNOTs). It takes the matrix "state" and
+    splits it into sections of size section_size. Then it eliminates all non-zero
+    sub-rows within each section, which are the same as a non-zero sub-row
+    above. Once this has been done, it continues with normal gaussian elimination.
+    The benefit is that with small section sizes (m), most of the sub-rows will
+    be cleared in the first step, resulting in a factor m fewer row row operations
+    during Gaussian elimination.
+
+    The algorithm is described in detail in the following paper
+    "Optimal synthesis of linear reversible circuits."
+    Patel, Ketan N., Igor L. Markov, and John P. Hayes.
+    Quantum Information & Computation 8.3 (2008): 282-294.
+
+    Note:
+    This implementation tweaks the Patel, Markov, and Hayes algorithm by adding
+    a "back reduce" which adds rows below the pivot row with a high degree of
+    overlap back to it. The intuition is to avoid a high-weight pivot row
+    increasing the weight of lower rows.
+
+    Args:
+        state (ndarray): n x n matrix, describing a linear quantum circuit
+        section_size (int): the section size the matrix columns are divided into
+
+    Returns:
+        numpy.matrix: n by n matrix, describing the state of the output circuit
+        list: a k by 2 list of C-NOT operations that need to be applied
+    """
+
+    """Also used virtual padding."""
 
     circuit = []
     cutoff = 1
