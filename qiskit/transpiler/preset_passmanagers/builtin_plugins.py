a8c5b8cd846d226317810abfa6e9ae7a8b66e712
Sebastian Brandhofer <148463728+sbrandhsn@users.noreply.github.com>
2024-07-08 10:51:15 +0200

    Update builtin_plugins.py

--- qiskit/transpiler/preset_passmanagers/builtin_plugins.py
+++ qiskit/transpiler/preset_passmanagers/builtin_plugins.py
@@ -183,8 +183,8 @@ class DefaultInitPassManager(PassManagerStagePlugin):
                         op = stdgates.get(op, None)
                     if op is None or op.name in _discrete_skipped_ops:
                         continue
-                    params = op.params
-                    if len(params) > 0:
+
+                    if len(list(op.params)) > 0:
                         return True
                 return False
 
