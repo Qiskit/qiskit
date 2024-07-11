95bb321805b9b6d41e68d40e749218e41487d76a
Andrew Eddins <aeddins@ibm.com>
2024-07-11 10:16:53 -0400

    Revert ValueError to IndexError changes
    Reverting these changes as they will instead be made in a separate PR.
    
    This reverts commit 8f3217838c6632d30ef300445fcca1590454b536.
    
    Revert "update error type in slice_shots docstring"
    
    This reverts commit 50545efbf26f6fac72c7c00919ae0995b8464ba6.
    
    Revert "change ValueError to IndexError in slice_shots"
    
    This reverts commit c4becd9b0e4363797331157b176c1603dabc40c9.
    
    Revert "update slice_bits test to use IndexError"
    
    This reverts commit c2b00390da40b12d5821476de850803da2b72a69.

--- qiskit/primitives/containers/bit_array.py
+++ qiskit/primitives/containers/bit_array.py
@@ -424,13 +424,13 @@ class BitArray(ShapedMixin):
             A bit array sliced along the bit axis.
 
         Raises:
-            IndexError: If there are any invalid indices of the bit axis.
+            ValueError: If there are any invalid indices of the bit axis.
         """
         if isinstance(indices, int):
             indices = (indices,)
         for index in indices:
             if index < 0 or index >= self.num_bits:
-                raise IndexError(
+                raise ValueError(
                     f"index {index} is out of bounds for the number of bits {self.num_bits}."
                 )
         # This implementation introduces a temporary 8x memory overhead due to bit
@@ -451,13 +451,13 @@ class BitArray(ShapedMixin):
             A bit array sliced along the shots axis.
 
         Raises:
-            IndexError: If there are any invalid indices of the shots axis.
+            ValueError: If there are any invalid indices of the shots axis.
         """
         if isinstance(indices, int):
             indices = (indices,)
         for index in indices:
             if index < 0 or index >= self.num_shots:
-                raise IndexError(
+                raise ValueError(
                     f"index {index} is out of bounds for the number of shots {self.num_shots}."
                 )
         arr = self._array
