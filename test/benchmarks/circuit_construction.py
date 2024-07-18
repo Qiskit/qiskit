4446b195fa5dc003850fb3a424130b198eb4f434
Catherine Lozano <clozano1@usc.edu>
2024-07-18 11:36:48 -0400

    lint test

--- test/benchmarks/circuit_construction.py
+++ test/benchmarks/circuit_construction.py
@@ -16,7 +16,7 @@
 import os
 import itertools
 
-from qiskit.quantum_info import *
+from qiskit.quantum_info import random_clifford
 from qiskit import QuantumRegister, QuantumCircuit
 from qiskit.circuit import Parameter
 from qiskit.circuit.library import EfficientSU2, QuantumVolume
@@ -171,10 +171,12 @@ class QasmImport:
         assert ops.get("cx", 0) == 15000
         return ops
 
+
 class CliffordSynthesis:
     param_names = ["num_qubits"]
     params = [10, 50, 100]
-    def time_clifford_synthesis(self,num_qubits):
+
+    def time_clifford_synthesis(self, num_qubits):
         cliff = random_clifford(num_qubits)
         qc = cliff.to_circuit()
         return qc
