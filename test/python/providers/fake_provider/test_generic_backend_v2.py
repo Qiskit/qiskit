787090f4216271478d8ebcc2d95ecb00db7a44b6
Luciano Bello <bel@zurich.ibm.com>
2024-07-23 14:31:37 +0200

    Update test/python/providers/fake_provider/test_generic_backend_v2.py
    Co-authored-by: Elena Peña Tapia <57907331+ElePT@users.noreply.github.com>

--- test/python/providers/fake_provider/test_generic_backend_v2.py
+++ test/python/providers/fake_provider/test_generic_backend_v2.py
@@ -137,11 +137,7 @@ class TestGenericBackendV2(QiskitTestCase):
 
         backend = GenericBackendV2(num_qubits=5, basis_gates=["cx", "id", "rz", "sx", "x"])
         tqc = transpile(qc, backend=backend, optimization_level=3, seed_transpiler=42)
-        with warnings.catch_warnings():
-            # TODO remove this catch once Aer stops using Provider ABC
-            # https://github.com/Qiskit/qiskit-aer/pull/2184
-            warnings.filterwarnings("ignore", category=DeprecationWarning, module="qiskit")
-            result = backend.run(tqc, seed_simulator=42, shots=1000).result()
+        result = backend.run(tqc, seed_simulator=42, shots=1000).result()
         counts = result.get_counts()
 
         self.assertTrue(math.isclose(counts["00000"], 500, rel_tol=0.1))
