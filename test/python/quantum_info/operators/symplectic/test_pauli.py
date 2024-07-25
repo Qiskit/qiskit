7a761c12845500f49a4c6307279e373a4d12c5d8
Luciano Bello <bel@zurich.ibm.com>
2024-07-25 23:51:01 +0200

    I forgot a test

--- test/python/quantum_info/operators/symplectic/test_pauli.py
+++ test/python/quantum_info/operators/symplectic/test_pauli.py
@@ -550,9 +550,8 @@ class TestPauli(QiskitTestCase):
         thetas = list(range(len(psi.parameters)))
         transpiled_psi = transpile(psi, backend, optimization_level=3)
         permuted_op = op.apply_layout(transpiled_psi.layout)
-        with self.assertWarns(DeprecationWarning):
-            job = estimator.run(transpiled_psi, permuted_op, thetas)
-            res = job.result().values
+        job = estimator.run(transpiled_psi, permuted_op, thetas)
+        res = job.result().values
         if optionals.HAS_AER:
             np.testing.assert_allclose(res, [0.20898438], rtol=0.5, atol=0.2)
         else:
