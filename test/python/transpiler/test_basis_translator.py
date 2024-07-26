c8c53cc6f81f43f628f1c64ddf67f27a9d1fdb4c
Matthew Treinish <mtreinish@kortar.org>
2024-07-26 13:29:21 -0400

    Add a default optimization level to generate_preset_pass_manager (#12150)
    * Add a default optimization level to generate_preset_pass_manager
    
    This commit adds a default value to the generate_preset_pass_manager's
    optimization_level argument. If it's not specified optimization level 2
    will be used. After #12148 optimization level 2 is a better fit for an
    optimal tradeoff between heuristic effort and runtime that makes it
    well suited as a default optimization level.
    
    * Update transpile()'s default opt level to match
    
    This commit updates the transpile() function's optimization_level argument
    default value to match generate_preset_pass_manager's new default to use 2
    instead of 1. This is arguably a breaking API change, but since the
    semantics are equivalent with two minor edge cases with implicit behavior
    that were a side effect of the level 1 preset pass manager's construction
    (which are documented in the release notes) we're ok making it in this
    case. Some tests which we're relying on the implicit behavior of
    optimization level 1 are updated to explicitly set the optimization
    level argument which will retain this behavior.
    
    * Update more tests expecting optimization level 1
    
    * * Set optimization level to 1 in test_approximation_degree.
    
    * Replace use of transpile with specific pass in  HLS tests.
    
    * Set optimization_level=1 in layout-dependent tests.
    
    * Expand upgrade note explanation on benefits of level 2
    
    * Apply Elena's reno suggestions
    
    ---------
    
    Co-authored-by: Elena Peña Tapia <epenatap@gmail.com>
    Co-authored-by: Elena Peña Tapia <57907331+ElePT@users.noreply.github.com>

--- test/python/transpiler/test_basis_translator.py
+++ test/python/transpiler/test_basis_translator.py
@@ -1106,6 +1106,7 @@ class TestBasisExamples(QiskitTestCase):
             circ,
             basis_gates=["id", "rz", "sx", "x", "cx"],
             seed_transpiler=42,
+            optimization_level=1,
         )
         self.assertEqual(circ_transpiled.count_ops(), {"cx": 91, "rz": 66, "sx": 22})
 
