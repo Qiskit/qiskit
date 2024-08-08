a7e6f665d096c88457c40dab85393d008d49d64b
Kevin Hartman <kevin@hart.mn>
2024-08-08 18:28:32 -0400

    Prepare 1.1.2 (#12927)
    * Bump version to 1.1.2.
    
    * Add prelude.
    
    * Fix version parsing issue in bloch.py.
    
    * Fix release note typos.

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ author = "Qiskit Development Team"
 # The short X.Y version
 version = "1.1"
 # The full version, including alpha/beta/rc tags
-release = "1.1.1"
+release = "1.1.2"
 
 language = "en"
 
