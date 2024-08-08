2b6068779d919587a6a6b0c26d816a8cfd780fb4
Raynel Sanchez <raynelfss@hotmail.com>
2024-08-08 12:04:10 -0400

    Initial: Add `parameter_by_uuid` to `CircuitData` - This method receives a `uuid` instance from Python and returns the parameter object identified by said `uuid`.

--- crates/circuit/src/circuit_data.rs
+++ crates/circuit/src/circuit_data.rs
@@ -361,6 +361,12 @@ impl CircuitData {
             .map(|ob| ob.clone_ref(py))
     }
 
+    pub fn get_parameter_by_uuid(&self, py: Python, uuid: ParameterUuid) -> Option<Py<PyAny>> {
+        self.param_table
+            .py_parameter_by_uuid(uuid)
+            .map(|ob| ob.clone_ref(py))
+    }
+
     /// Return the width of the circuit. This is the number of qubits plus the
     /// number of clbits.
     ///
