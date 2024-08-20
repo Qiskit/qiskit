40228f0d99299c7d287efe5dff8a591afa50689e
Alexander Ivrii <alexi@il.ibm.com>
2024-08-20 17:22:27 +0300

    Update qiskit/circuit/library/standard_gates/x.py
    Co-authored-by: Julien Gacon <gaconju@gmail.com>

--- qiskit/circuit/library/standard_gates/x.py
+++ qiskit/circuit/library/standard_gates/x.py
@@ -1341,7 +1341,7 @@ class MCXRecursive(MCXGate):
             "It is recommended to use MCXGate and let HighLevelSynthesis choose "
             "the best synthesis method depending on the number of ancilla qubits available. "
             "If this specific synthesis method is required, one can specify it using the "
-            "high-level-synthesis plugin `mcx_1_clean` for MCX gates, or, alternatively, "
+            "high-level-synthesis plugin '1_clean_b95' for MCX gates, or, alternatively, "
             "one can use synth_mcx_1_clean to construct the gate directly."
         ),
         since="1.3",
