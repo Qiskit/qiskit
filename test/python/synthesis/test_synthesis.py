c582a16d8209ce5fda5cd7f751b2d6ed1d5eb7ab
Elena Peña Tapia <epenatap@gmail.com>
2024-08-22 11:06:26 +0200

    Get rid of encoded assumption in TwoQubitBasisDecomposer that the given KAK gate is a Rust-space StandardGate

--- test/python/synthesis/test_synthesis.py
+++ test/python/synthesis/test_synthesis.py
@@ -1270,6 +1270,15 @@ class TestTwoQubitDecompose(CheckDecompositions):
             requested_basis = set(oneq_gates + [kak_gate_name])
             self.assertTrue(decomposition_basis.issubset(requested_basis))
 
+    def test_non_std_gate(self):
+        """Test that the TwoQubitBasisDecomposer class can be correctly instantiated with a
+        non-standard gate.
+
+        Reproduce from: https://github.com/Qiskit/qiskit/issues/12998
+        """
+        # note that `CXGate(ctrl_state=0)` is not handled as a "standard" gate.
+        TwoQubitBasisDecomposer(CXGate(ctrl_state=0))(CXGate())
+
 
 @ddt
 class TestPulseOptimalDecompose(CheckDecompositions):
