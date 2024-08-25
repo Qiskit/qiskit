401abc311bf08cd05a72aaa4dd8be0d48882a4d4
Raynel Sanchez <raynelfss@hotmail.com>
2024-08-25 13:11:07 -0400

    Fix: leverage new methods in layers - Fix incorrect re-insertion of last_node.

--- crates/circuit/src/dag_circuit.rs
+++ crates/circuit/src/dag_circuit.rs
@@ -4343,7 +4343,7 @@ def _format(operand):
 
             let mut new_layer = self.copy_empty_like(py, vars_mode)?;
 
-            new_layer.extend(py, op_nodes.iter().map(|(inst, _)| (*inst).clone()))?;
+            new_layer.add_from_iter(py, op_nodes.iter().map(|(inst, _)| (*inst).clone()))?;
 
             let new_layer_op_nodes = new_layer.op_nodes(false).filter_map(|node_index| {
                 match new_layer.dag.node_weight(node_index) {
@@ -6515,7 +6515,8 @@ impl DAGCircuit {
                 };
                 qubit_last_nodes
                     .entry(*qubit)
-                    .and_modify(|val| *val = (new_node, qubit_last_node.1.clone()));
+                    .and_modify(|val| *val = (new_node, qubit_last_node.1.clone()))
+                    .or_insert((new_node, qubit_last_node.1.clone()));
                 nodes_to_connect.insert(qubit_last_node);
             }
 
@@ -6536,7 +6537,8 @@ impl DAGCircuit {
                 };
                 clbit_last_nodes
                     .entry(clbit)
-                    .and_modify(|val| *val = (new_node, clbit_last_node.1.clone()));
+                    .and_modify(|val| *val = (new_node, clbit_last_node.1.clone()))
+                    .or_insert((new_node, clbit_last_node.1.clone()));
                 nodes_to_connect.insert(clbit_last_node);
             }
 
