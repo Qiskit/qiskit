8011fdf547ab1a6539cc3920136fa9746cab9ba5
Raynel Sanchez <raynelfss@hotmail.com>
2024-09-04 12:58:38 -0400

    Fix: Remove duplicate vars check

--- crates/circuit/src/dag_circuit.rs
+++ crates/circuit/src/dag_circuit.rs
@@ -6447,10 +6447,6 @@ impl DAGCircuit {
                 };
 
                 vars_last_nodes.set_item(var, new_node.index())?;
-                if var_last_node == new_node {
-                    // TODO: Fix instances of duplicate nodes for Vars
-                    continue;
-                }
                 self.dag
                     .add_edge(var_last_node, new_node, Wire::Var(var.clone_ref(py)));
             }
