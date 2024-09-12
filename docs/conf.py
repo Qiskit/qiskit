cb486e6a312dccfcbb4d88e8f21d93455d1ddf82
Julien Gacon <jules.gacon@googlemail.com>
2024-09-12 12:19:59 +0200

    Prepare 1.2.1 release (#13088)
    * Prepare 1.2.1 release
    
    * update renos
    
    * move remaining renos
    
    * Update releasenotes/notes/1.2/fix-2q-basis-decomposer-non-std-kak-gate-edc69ffb5d9ef302.yaml
    
    ---------
    
    Co-authored-by: Elena Peña Tapia <57907331+ElePT@users.noreply.github.com>

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ author = "Qiskit Development Team"
 # The short X.Y version
 version = "1.2"
 # The full version, including alpha/beta/rc tags
-release = "1.2.0"
+release = "1.2.1"
 
 language = "en"
 
