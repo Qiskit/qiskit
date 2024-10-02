d9f56f842491c3139864011076c028733caa08c3
Shivansh Mittal <88429611+Shivansh20128@users.noreply.github.com>
2024-10-02 11:43:11 +0530

    Doc fix ZZ_feature_maps (#13231)
    * initial formatting changes
    
    * .compose used for concatination of two circuits
    
    * updated docstring data property
    
    * updated entanglement_blocks docstring
    
    * updated docstring of num_parameters
    
    * updates docstring of parameters
    
    * update _zz_feature_map.py documentation
    
    * update changes to ZZ_Feature_Map
    
    * update doc strings matched with parent class
    
    * _zz_feature_map.py reformatted
    
    (cherry picked from commit f1b25806e97674428b8d687bd5886f23fd6cbe80)
    
    # Conflicts:
    #	qiskit/circuit/library/data_preparation/zz_feature_map.py

--- qiskit/circuit/library/data_preparation/pauli_feature_map.py
+++ qiskit/circuit/library/data_preparation/pauli_feature_map.py
@@ -209,6 +209,11 @@ class PauliFeatureMap(NLocal):
 
     @property
     def entanglement_blocks(self):
+        """The blocks in the entanglement layers.
+
+        Returns:
+            The blocks in the entanglement layers.
+        """
         return [self.pauli_block(pauli) for pauli in self._paulis]
 
     @entanglement_blocks.setter
