1443081c912739040ff9729682900e9495b5bd86
Luciano Bello <bel@zurich.ibm.com>
2024-10-17 08:46:28 +0200

    Custom gates and parametrized circuits

--- test/benchmarks/qasm3_exporter.py
+++ test/benchmarks/qasm3_exporter.py
@@ -13,12 +13,14 @@
 # pylint: disable=missing-docstring
 # pylint: disable=attribute-defined-outside-init
 
+from qiskit.circuit import Parameter
+from qiskit import QuantumCircuit
 from qiskit import qasm3
 
 from .utils import random_circuit
 
 
-class QASM3ExporterBenchmarks:
+class RandomBenchmarks:
 
     params = ([20], [256, 1024], [0, 42])
 
@@ -36,5 +38,46 @@ class QASM3ExporterBenchmarks:
             max_operands=3,
         )
 
+    def time_dumps(self, _, __, ___):
+        qasm3.dumps(self.circuit)
+
+
+class CustomGateBenchmarks:
+
+    params = ([200], [100])
+
+    param_names = ["n_qubits", "depth"]
+    timeout = 300
+
+    def setup(self, n_qubits, depth):
+        custom_gate = QuantumCircuit(2, name="custom_gate")
+        custom_gate.h(0)
+        custom_gate.x(1)
+
+        qc = QuantumCircuit(n_qubits)
+        for _ in range(depth):
+            for i in range(n_qubits - 1):
+                qc.append(custom_gate.to_gate(), [i, i + 1])
+        self.circuit = qc
+
     def time_dumps(self, _, __):
-        ___ = qasm3.dumps(self.circuit)
+        qasm3.dumps(self.circuit)
+
+
+class ParameterizedBenchmarks:
+
+    params = ([20, 50], [1, 5, 10])
+
+    param_names = ["n_qubits", "n_params"]
+    timeout = 300
+
+    def setup(self, n_qubits, n_params):
+        qc = QuantumCircuit(n_qubits)
+        params = [Parameter(f"angle{i}") for i in range(n_params)]
+        for n in range(n_qubits - 1):
+            for i in params:
+                qc.rx(i, n)
+        self.circuit = qc
+
+    def time_dumps(self, _, __):
+        qasm3.dumps(self.circuit)
