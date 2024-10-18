9b8b3fb42648790f6baab4381f3fb4018d04d62d
ABBY CROSS <across@us.ibm.com>
2024-10-18 13:51:32 -0400

    first attempt to add global_phase examples

--- qiskit/circuit/quantumcircuit.py
+++ qiskit/circuit/quantumcircuit.py
@@ -236,6 +236,47 @@ class QuantumCircuit:
 
     .. autoattribute:: global_phase
 
+    Example:
+    
+    .. plot::
+       :include-source:
+       :nofigs:
+       
+       from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+
+       qreg_q = QuantumRegister(2, 'q')
+       creg_c = ClassicalRegister(2, 'c')
+       circuit = QuantumCircuit(qreg_q, creg_c)
+
+       circuit.reset(qreg_q[0])
+       circuit.h(qreg_q[0])
+       circuit.reset(qreg_q[1])
+       circuit.cx(qreg_q[0], qreg_q[1])
+       
+       print(circuit.global_phase) # find the global phase of the current circuit
+       
+    .. code-block:: text
+    
+       0.0
+    
+    .. plot::
+       :include-source:
+       :nofigs:
+       
+       from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
+
+       qreg_q = QuantumRegister(2, 'q')
+       creg_c = ClassicalRegister(2, 'c')
+       circuit = QuantumCircuit(qreg_q, creg_c)
+       from numpy import pi
+       
+       circuit.global_phase = pi/4 # set the global phase of the circuit to pi/4
+       print(circuit.global_phase)
+       
+    .. code-block:: text
+    
+       0.7853981633974483
+
     The :attr:`name` of a circuit becomes the name of the :class:`~.circuit.Instruction` or
     :class:`.Gate` resulting from :meth:`to_instruction` and :meth:`to_gate` calls, which can be
     handy for visualizations.
