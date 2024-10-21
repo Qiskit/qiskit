0f110c08762e2221a55d9b1ad7330eea9b2242bd
ABBY CROSS <across@us.ibm.com>
2024-10-21 14:53:57 -0400

    add :context:

--- qiskit/circuit/quantumcircuit.py
+++ qiskit/circuit/quantumcircuit.py
@@ -241,6 +241,7 @@ class QuantumCircuit:
     .. plot::
        :include-source:
        :nofigs:
+       :context: reset
        
        from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
 
@@ -248,9 +249,7 @@ class QuantumCircuit:
        creg_c = ClassicalRegister(2, 'c')
        circuit = QuantumCircuit(qreg_q, creg_c)
 
-       circuit.reset(qreg_q[0])
        circuit.h(qreg_q[0])
-       circuit.reset(qreg_q[1])
        circuit.cx(qreg_q[0], qreg_q[1])
        
        print(circuit.global_phase) # find the global phase of the current circuit
@@ -262,12 +261,8 @@ class QuantumCircuit:
     .. plot::
        :include-source:
        :nofigs:
+       :context:
        
-       from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
-
-       qreg_q = QuantumRegister(2, 'q')
-       creg_c = ClassicalRegister(2, 'c')
-       circuit = QuantumCircuit(qreg_q, creg_c)
        from numpy import pi
        
        circuit.global_phase = pi/4 # set the global phase of the circuit to pi/4
