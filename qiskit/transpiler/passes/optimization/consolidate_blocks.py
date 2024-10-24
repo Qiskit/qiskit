d3e900b2f8c37694eebb2d5e9b7e3560b00b2d91
Matthew Treinish <mtreinish@kortar.org>
2024-10-24 14:33:53 -0400

    Fix lint

--- qiskit/transpiler/passes/optimization/consolidate_blocks.py
+++ qiskit/transpiler/passes/optimization/consolidate_blocks.py
@@ -13,22 +13,12 @@
 """Replace each block of consecutive gates by a single Unitary node."""
 from __future__ import annotations
 
-import numpy as np
-
-from qiskit.circuit.classicalregister import ClassicalRegister
-from qiskit.circuit.quantumregister import QuantumRegister
-from qiskit.circuit.quantumcircuit import QuantumCircuit
-from qiskit.dagcircuit.dagnode import DAGOpNode
-from qiskit.quantum_info import Operator
 from qiskit.synthesis.two_qubit import TwoQubitBasisDecomposer
-from qiskit.circuit.library.generalized_gates.unitary import UnitaryGate
 from qiskit.circuit.library.standard_gates import CXGate, CZGate, iSwapGate, ECRGate
 from qiskit.transpiler.basepasses import TransformationPass
 from qiskit.transpiler.passmanager import PassManager
-from qiskit.transpiler.passes.synthesis import unitary_synthesis
 from qiskit.circuit.controlflow import CONTROL_FLOW_OP_NAMES
 from qiskit._accelerate.consolidate_blocks import consolidate_blocks
-from qiskit.exceptions import QiskitError
 
 from .collect_1q_runs import Collect1qRuns
 from .collect_2q_blocks import Collect2qBlocks
