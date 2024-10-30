50e9363d83612be9c723da6b9c65f04e7d5b1e88
Julien Gacon <jules.gacon@googlemail.com>
2024-10-30 12:09:38 +0100

    IQP as circuit library function (and in Rust) (#13241)
    * make IQP a function
    
    * Make IQP fast
    
    * Try fixing type on windows Py 3.9
    
    * rm trailing comment
    
    * review comments v1
    
    * fix test comment
    
    * split into iqp and random_iqp
    
    * fix indentation?
    
    i hope this was the cause of the error, lets see
    
    * fix merge artifact
    
    -- the diagonal entries on the diagonal got lost!
    
    Co-authored-by: Alexander Ivrii <alexi@il.ibm.com>
    
    ---------
    
    Co-authored-by: Alexander Ivrii <alexi@il.ibm.com>

--- crates/accelerate/src/circuit_library/mod.rs
+++ crates/accelerate/src/circuit_library/mod.rs
@@ -13,12 +13,15 @@
 use pyo3::prelude::*;
 
 mod entanglement;
+mod iqp;
 mod pauli_feature_map;
 mod quantum_volume;
 
 pub fn circuit_library(m: &Bound<PyModule>) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(pauli_feature_map::pauli_feature_map))?;
     m.add_wrapped(wrap_pyfunction!(entanglement::get_entangler_map))?;
+    m.add_wrapped(wrap_pyfunction!(iqp::py_iqp))?;
+    m.add_wrapped(wrap_pyfunction!(iqp::py_random_iqp))?;
     m.add_wrapped(wrap_pyfunction!(quantum_volume::quantum_volume))?;
     Ok(())
 }
