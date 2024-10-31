40e605fefe1835d857632997141ff85f6513ad7d
Alexander Ivrii <alexi@il.ibm.com>
2024-10-31 11:35:10 +0200

    Boolean circuits as gates (#13333)
    * AND -> AndGate
    
    * more AND tests
    
    * more AND tests
    
    * OR -> OrGate
    
    * fix
    
    * XOR -> BitwiseXorGate
    
    * InnerProduct -> InnerProductGate
    
    * reno
    
    * applying review comments
    
    * adding missing eq and inverse methods + tests
    
    * docstring fix
    
    * docstring and API ref
    
    * fix reno
    
    * docstring fix

--- qiskit/circuit/library/__init__.py
+++ qiskit/circuit/library/__init__.py
@@ -211,9 +211,15 @@ or of a set of qubit states.
    :template: autosummary/class_no_inherited_members.rst
 
    AND
+   AndGate
    OR
+   OrGate
    XOR
+   BitwiseXorGate
    InnerProduct
+   InnerProductGate
+
+.. autofunction:: random_bitwise_xor
 
 Basis Change Circuits
 =====================
@@ -526,9 +532,14 @@ from .pauli_evolution import PauliEvolutionGate
 from .hamiltonian_gate import HamiltonianGate
 from .boolean_logic import (
     AND,
+    AndGate,
     OR,
+    OrGate,
     XOR,
+    BitwiseXorGate,
+    random_bitwise_xor,
     InnerProduct,
+    InnerProductGate,
 )
 from .basis_change import QFT, QFTGate
 from .arithmetic import (
