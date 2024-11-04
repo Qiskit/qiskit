2a1fd08406a4d491d9dcc42f31c28c1e4c813d35
Eli Arbel <46826214+eliarbel@users.noreply.github.com>
2024-11-04 19:35:59 +0300

    Oxidize GateDirection pass (#13069)
    * First working implementation
    
    * Rebase on main with CheckGateDirection ported
    
    * Port GateDirection to Rust
    
    Move GateDirection._run_coupling_map and GateDirection._run_target to
    Rust. Also moving the code to create replacement DAGs to Rust. Both
    GateDirection and CheckGateDirection will reside in gate_direction.rs.
    
    * First working implementation
    
    * Fix minor build issues
    
    * Complete implementation
    
    * Address review comments
    
    Mainly replace going through Python with Rust-native calls in various places,
    most prominently when building replacement DAGs.
    
    * Rename functions and other minor changes

--- crates/circuit/src/dag_circuit.rs
+++ crates/circuit/src/dag_circuit.rs
@@ -963,7 +963,7 @@ def _format(operand):
     /// case, the operation does not need to be translated to the device basis.
     ///
     /// DEPRECATED since Qiskit 1.3.0 and will be removed in Qiskit 2.0.0
-    fn has_calibration_for(&self, py: Python, node: PyRef<DAGOpNode>) -> PyResult<bool> {
+    pub fn has_calibration_for(&self, py: Python, node: PyRef<DAGOpNode>) -> PyResult<bool> {
         emit_pulse_dependency_deprecation(
             py,
             "method ``qiskit.dagcircuit.dagcircuit.DAGCircuit.has_calibration_for``",
