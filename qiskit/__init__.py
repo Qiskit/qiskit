640957a159f9319ebe5782dd7ea385a474512545
Alexander Ivrii <alexi@il.ibm.com>
2024-11-06 23:11:55 +0200

    Oxidize parts of HLS (#13369)
    * code from private developer branch
    
    * lint
    
    * changing the internals of QubitContext to be a list rathen than dict
    
    * temporarily disabling a failing test
    
    * renaming to _local_to_global following review comments
    
    * pretty print
    
    * fixing test
    
    * oxidizing qubit tracker
    
    * fix from parent branch
    
    * oxidizing qubit context
    
    * lint
    
    * renaming

--- qiskit/__init__.py
+++ qiskit/__init__.py
@@ -106,6 +106,7 @@ sys.modules["qiskit._accelerate.gate_direction"] = _accelerate.gate_direction
 sys.modules["qiskit._accelerate.inverse_cancellation"] = _accelerate.inverse_cancellation
 sys.modules["qiskit._accelerate.check_map"] = _accelerate.check_map
 sys.modules["qiskit._accelerate.filter_op_nodes"] = _accelerate.filter_op_nodes
+sys.modules["qiskit._accelerate.high_level_synthesis"] = _accelerate.high_level_synthesis
 sys.modules["qiskit._accelerate.remove_identity_equiv"] = _accelerate.remove_identity_equiv
 
 from qiskit.exceptions import QiskitError, MissingOptionalLibraryError
