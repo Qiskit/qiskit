24fb7d88d410d8ad2cf1fc044dd93d69078531b7
Julien Gacon <jules.gacon@googlemail.com>
2024-12-04 13:06:00 +0100

    Uppercase `Qiskit` in deprecation warnings (#13452)
    * uppercase Qiskit
    
    * uppercase Qiskit in Rust
    
    * forgot `deprecate_arg`
    
    * Fix tests, update docstring

--- crates/circuit/src/dag_circuit.rs
+++ crates/circuit/src/dag_circuit.rs
@@ -414,7 +414,7 @@ impl DAGCircuit {
                 py,
                 concat!(
                     "The property ``qiskit.dagcircuit.dagcircuit.DAGCircuit.duration`` is ",
-                    "deprecated as of qiskit 1.3.0. It will be removed in Qiskit 2.0.0.",
+                    "deprecated as of Qiskit 1.3.0. It will be removed in Qiskit 2.0.0.",
                 )
             ),
             py.get_type_bound::<PyDeprecationWarning>(),
@@ -433,7 +433,7 @@ impl DAGCircuit {
                 py,
                 concat!(
                     "The property ``qiskit.dagcircuit.dagcircuit.DAGCircuit.unit`` is ",
-                    "deprecated as of qiskit 1.3.0. It will be removed in Qiskit 2.0.0.",
+                    "deprecated as of Qiskit 1.3.0. It will be removed in Qiskit 2.0.0.",
                 )
             ),
             py.get_type_bound::<PyDeprecationWarning>(),
@@ -7039,7 +7039,7 @@ fn emit_pulse_dependency_deprecation(py: Python, msg: &str) {
         PyString::new_bound(
             py,
             &format!(
-                "The {} is deprecated as of qiskit 1.3.0. It will be removed in Qiskit 2.0.0. \
+                "The {} is deprecated as of Qiskit 1.3.0. It will be removed in Qiskit 2.0.0. \
                 The entire Qiskit Pulse package is being deprecated \
                 and this is a dependency on the package.",
                 msg
