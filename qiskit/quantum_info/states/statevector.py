5dc23335c276fe5abb96c6633887f48768b5bb32
J. Harte <13206585+boonware@users.noreply.github.com>
2024-12-13 14:48:16 +0000

    clarify method purpose

--- qiskit/quantum_info/states/statevector.py
+++ qiskit/quantum_info/states/statevector.py
@@ -254,7 +254,7 @@ class Statevector(QuantumState, TolerancesMixin):
         return Operator(mat, input_dims=self.dims(), output_dims=self.dims())
 
     def conjugate(self) -> Statevector:
-        """Return the complex conjugate of the state vector."""
+        """Return the complex conjugate of the state vector with respect to the basis of the vector components returned by :meth:`data`"""
         return Statevector(np.conj(self.data), dims=self.dims())
 
     def trace(self) -> np.float64:
