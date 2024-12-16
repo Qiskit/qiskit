077ecb97d40641f023ee11c60e89596906ced555
ShellyGarion <shelly@il.ibm.com>
2024-12-16 06:58:19 -0600

    update (temporarily) basis gates in test

--- test/python/compiler/test_transpiler.py
+++ test/python/compiler/test_transpiler.py
@@ -1277,7 +1277,7 @@ class TestTranspile(QiskitTestCase):
         basis_gates=[
             ["u3", "cx"],
             ["rx", "rz", "iswap"],
-            ["rx", "ry", "rxx"],
+            ["ry", "rz", "rxx"],
         ],
     )
     def test_translation_method_synthesis(self, optimization_level, basis_gates):
