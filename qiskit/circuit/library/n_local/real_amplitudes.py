ca7d96cd3110aac904fa6ea5df5abaf66e4a888e
Elena Peña Tapia <57907331+ElePT@users.noreply.github.com>
2025-01-08 18:48:39 +0100

    Close previous figures in docs plots. (#13635)
    (cherry picked from commit 8b2f6bd83ce8839c3efcb24d784dbb5f888db307)

--- qiskit/circuit/library/n_local/real_amplitudes.py
+++ qiskit/circuit/library/n_local/real_amplitudes.py
@@ -78,21 +78,21 @@ def real_amplitudes(
 
         .. plot::
            :include-source:
-           :context:
+           :context: close-figs
 
            ansatz = real_amplitudes(3, entanglement="full", reps=2)  # it is the same unitary as above
            ansatz.draw("mpl")
 
         .. plot::
            :include-source:
-           :context:
+           :context: close-figs
 
            ansatz = real_amplitudes(3, entanglement="linear", reps=2, insert_barriers=True)
            ansatz.draw("mpl")
 
         .. plot::
            :include-source:
-           :context:
+           :context: close-figs
 
            ansatz = real_amplitudes(4, reps=2, entanglement=[[0,3], [0,2]], skip_unentangled_qubits=True)
            ansatz.draw("mpl")
