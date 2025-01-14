843c22024b6f332969bc5ee31231218d7100cbe1
MozammilQ <qsigmozammil@gmail.com>
2025-01-14 07:39:57 +0000

    renamed variables; added comments

--- crates/accelerate/src/synthesis/linear/pmh.rs
+++ crates/accelerate/src/synthesis/linear/pmh.rs
@@ -158,8 +158,7 @@ pub fn synth_cnot_count_full_pmh(
     matrix: PyReadonlyArray2<bool>,
     section_size: Option<i64>,
 ) -> PyResult<CircuitData> {
-    let arrayview = matrix.as_array();
-    let mat: Array2<bool> = arrayview.to_owned();
+    let mat: Array2<bool> = matrix.as_array().to_owned();
     let num_qubits = mat.nrows();
     let section_size: Option<usize> =
         section_size.and_then(|num| if num >= 0 { Some(num as usize) } else { None });
--- crates/accelerate/src/synthesis/linear_phase/cnot_phase_synth.rs
+++ crates/accelerate/src/synthesis/linear_phase/cnot_phase_synth.rs
@@ -77,18 +77,18 @@ fn get_instr(angle: String, qubit_idx: usize) -> Option<Instruction> {
 ///           Quantum Science and Technology 4.1 (2018): 015002.
 ///           `arXiv:1712.01859 <https://arxiv.org/abs/1712.01859>`_
 #[pyfunction]
-#[pyo3(signature = (cnots, angles, section_size=2))]
+#[pyo3(signature = (binary_string, angles, section_size=2))]
 pub fn synth_cnot_phase_aam(
     py: Python,
-    cnots: PyReadonlyArray2<bool>,
+    binary_string: PyReadonlyArray2<bool>,
     angles: &Bound<PyList>,
     section_size: Option<i64>,
 ) -> PyResult<CircuitData> {
     // converting to Option<usize>
     let section_size: Option<usize> =
         section_size.and_then(|num| if num >= 0 { Some(num as usize) } else { None });
-    let cnots = cnots.as_array().to_owned();
-    let num_qubits = cnots.nrows();
+    let binary_string = binary_string.as_array().to_owned();
+    let num_qubits = binary_string.nrows();
 
     let mut angles = angles
         .iter()
@@ -101,17 +101,16 @@ pub fn synth_cnot_phase_aam(
 
     let mut state: Array2<bool> = Array2::from_shape_fn((num_qubits, num_qubits), |(i, j)| i == j);
 
-    let mut cnots_cpy = cnots.clone();
-    let mut q = vec![(
-        cnots.clone(),
-        (0..num_qubits).collect::<Vec<usize>>(),
-        num_qubits,
-    )];
-    let mut _cnots = cnots;
-    let mut _i = (0..num_qubits).collect::<Vec<usize>>();
-    let mut _ep = num_qubits;
+    let mut binary_str_cpy = binary_string.clone();
+    let num_qubits_range = (0..num_qubits).collect::<Vec<usize>>();
+    let mut data_stack = vec![(binary_string.clone(), num_qubits_range.clone(), num_qubits)];
+    let mut bin_str_ = binary_string;
+    let mut qubits_range_ = num_qubits_range;
+    let mut qubits_ = num_qubits;
 
-    // variables keeping the state of iteration
+    // The aim is to keep the instructions as iterables
+    // Below variables are keeping the state of iteration in
+    // `std::iter::from_fn`
     let mut keep_iterating: bool = true;
     let mut cx_gate_done: bool = false;
     let mut phase_loop_on: bool = false;
@@ -122,22 +121,39 @@ pub fn synth_cnot_phase_aam(
     let mut pmh_init_done: bool = false;
     let mut synth_pmh_iter: Option<Box<dyn Iterator<Item = Instruction>>> = None;
 
+    // The instructions are stored as iterables in the variable.
     let cx_phase_iter = std::iter::from_fn(move || {
+        // For now, the `gate_instr` stores `None`, but as soon as
+        // it gets any instruction, the current loop breaks, and
+        // the variable is returned to be stored in `cx_phase_iter`
+        // as an iterable.
         let mut gate_instr: Option<Instruction> = None;
 
+        // This while loop corresponds to the first block of code where
+        // different phases are applied to the qubits.
+        // The variable `phase_done` is a bool switch which is initially
+        // set to `false`, so that this while loop gets executed, different
+        // phases are applied to the qubits, and then `phase_done` is set
+        // to `true` indicating the phases have been applied to the qubits, so
+        // the control doesn't enter the while loop again in the execution.
         while !phase_done {
-            let icnot = cnots_cpy.column(index);
+            let bin_str_subset_ = binary_str_cpy.column(index);
             index += 1;
             let target_state = state.row(qubit_idx);
 
-            if icnot == target_state {
+            if bin_str_subset_ == target_state {
                 index -= 1;
-                cnots_cpy.remove_index(numpy::ndarray::Axis(1), index);
+                binary_str_cpy.remove_index(numpy::ndarray::Axis(1), index);
                 let angle = angles.remove(index);
+                // instruction applied on the `qubit_idx`
+                // according to the `angle` passed.
                 gate_instr = get_instr(angle, qubit_idx);
+                // as soon, as `gate_instr` gets an instruction, the
+                // loop is broken, so that `gate_instr` returns the
+                // instruction as iterable to `cx_phase_iter`.
                 break;
             }
-            if index == cnots_cpy.ncols() {
+            if index == binary_str_cpy.ncols() {
                 qubit_idx += 1;
                 index = 0;
             }
@@ -146,163 +162,236 @@ pub fn synth_cnot_phase_aam(
                 index = 0;
                 qubit_idx = 0;
             }
-        } // end phase_apply loop
+        } // end phase applying loop
 
+        // This while loop corresponds to the second block of code
+        // where CNOT gate with different phases are applied to the qubits.
+        // After the first block is executed `phase_done` is set to true,
+        // similarly, `cx_phase_done` is a bool variable indicating if the
+        // second block of code which applies CNOTs is complete.
         'outer_loop: while phase_done && !cx_phase_done {
-            if !phase_loop_on && q.is_empty() {
+            // Wherever, `gate_instr` gets any instruciton, to return
+            // the instruction, the loop has to be broken prematurely,
+            // and, the control again strats from line after `std::iter::from_fn`
+            // the variable `phase_loop_on` indicates that the loop has been
+            // broken prematurely, so that the code blocks which are supposed
+            // to get executed after the completion of the loop are not executed.
+            if !phase_loop_on && data_stack.is_empty() {
+                // If `data_stack` is empty, this marks successfull
+                // application of CNOTs, so `cx_phase_done` is set to indicate
+                // the second code block applying CNOTs is complete, and, thus
+                // the loop gets broken finally.
                 cx_phase_done = true;
                 break 'outer_loop;
             }
             if !phase_loop_on {
-                (_cnots, _i, _ep) = q.pop().unwrap();
+                (bin_str_, qubits_range_, qubits_) = data_stack.pop().unwrap();
             }
-            if !phase_loop_on && _cnots.is_empty() {
+            if !phase_loop_on && bin_str_.is_empty() {
                 continue 'outer_loop;
             }
 
-            while keep_iterating && (_ep < num_qubits) {
+            // For every qubit less than `num_qubits`, the condition that all
+            // bits of the row corresponding to `qubit_idx` in `bin_str_` is true
+            // is checked, even if the condition is fulfilled once, the varibale
+            // `keep_iterating` is set, so that after `qubit_idx` reaches `num_qubits`.
+            // The entire loop runs at-least once more.
+            while keep_iterating && (qubits_ < num_qubits) {
                 if !phase_loop_on {
                     keep_iterating = false;
                 }
                 while qubit_idx < num_qubits {
-                    if (qubit_idx != _ep) && !_cnots.row(qubit_idx).iter().any(|&b| !b) {
+                    if (qubit_idx != qubits_) && !bin_str_.row(qubit_idx).into_iter().any(|&b| !b) {
+                        // To return the `cx` gate stored in `gate_instr` the loop has to be
+                        // broke prematurely, the bool `cx_gate_done` indicates that `cx` has been
+                        // applied for this particular iteration of loop, so that, when the control
+                        // comes back again at this spot, furthur contents of the loop is executed,
+                        // and no more `cx` is applied now for this iteration of loop.
                         if !cx_gate_done && !phase_loop_on {
                             keep_iterating = true;
                             cx_gate_done = true;
                             gate_instr = Some((
                                 StandardGate::CXGate,
                                 smallvec![],
-                                smallvec![Qubit((qubit_idx) as u32), Qubit(_ep as u32)],
+                                smallvec![Qubit((qubit_idx) as u32), Qubit(qubits_ as u32)],
                             ));
                             index = 0;
                             phase_loop_on = true;
-                            for _k in 0..state.ncols() {
-                                state[(_ep, _k)] ^= state[(qubit_idx, _k)];
+                            for col_idx in 0..state.ncols() {
+                                state[(qubits_, col_idx)] ^= state[(qubit_idx, col_idx)];
                             }
+                            // `gate_instr` contains `cx` gate now, and the loop has to
+                            // be borken prematurely to return this gate.
                             break 'outer_loop;
                         }
 
                         cx_gate_done = false;
+
+                        // This while loop is to apply the phases to qubits after the
+                        // `cx` has been applied, again the bool `phase_loop_on` is merely
+                        // an indication that the loop is supposed to break prematurely
+                        // and whenever the control returns to this block of code it keep
+                        // on applying phase until `phase_loop_on` is set to false, which
+                        // indicates that all the phases associated to this iteration of loop
+                        // has been applied.
                         while phase_loop_on {
-                            if index == cnots_cpy.ncols() {
+                            if index == binary_str_cpy.ncols() {
                                 phase_loop_on = false;
                                 break;
                             }
-                            let icnot = cnots_cpy.column(index);
+                            let bin_str_subset_ = binary_str_cpy.column(index);
                             index += 1;
-                            let target_state = state.row(_ep);
-                            if icnot == target_state {
+                            let target_state = state.row(qubits_);
+                            if bin_str_subset_ == target_state {
                                 index -= 1;
-                                cnots_cpy.remove_index(numpy::ndarray::Axis(1), index);
+                                binary_str_cpy.remove_index(numpy::ndarray::Axis(1), index);
                                 let angle = angles.remove(index);
-                                gate_instr = get_instr(angle, _ep);
+                                gate_instr = get_instr(angle, qubits_);
+                                // loop has to be broken, to return the instruction stored in
+                                // `gate_instr`.
                                 break 'outer_loop;
                             }
                         }
 
-                        q.push((_cnots.clone(), _i.clone(), _ep));
-                        let mut unique_q = vec![];
+                        data_stack.push((bin_str_.clone(), qubits_range_.clone(), qubits_));
+                        let mut uniq_ele_dat_stack = vec![];
 
-                        for data in q.iter() {
-                            if !unique_q.contains(data) {
-                                let d = (*data).clone();
-                                unique_q.push(d);
+                        for data in data_stack.iter() {
+                            if !uniq_ele_dat_stack.contains(data) {
+                                let dat_ = (*data).clone();
+                                uniq_ele_dat_stack.push(dat_);
                             }
                         }
-                        q = unique_q;
+                        data_stack = uniq_ele_dat_stack;
 
-                        for data in &mut q {
-                            let (ref mut _temp_cnots, _, _) = data;
-                            if _temp_cnots.is_empty() {
+                        for data in &mut data_stack {
+                            let (ref mut temp_bin_str_, _, _) = data;
+                            if temp_bin_str_.is_empty() {
                                 continue;
                             }
 
-                            for idx in 0.._temp_cnots.row(qubit_idx).len() {
-                                _temp_cnots[(qubit_idx, idx)] ^= _temp_cnots[(_ep, idx)];
+                            for idx in 0..temp_bin_str_.row(qubit_idx).len() {
+                                temp_bin_str_[(qubit_idx, idx)] ^= temp_bin_str_[(qubits_, idx)];
                             }
                         }
 
-                        (_cnots, _i, _ep) = q.pop().unwrap();
-                    } // end of if _ep < num_qubits ...
+                        (bin_str_, qubits_range_, qubits_) = data_stack.pop().unwrap();
+                    } // end of if qubits_ < num_qubits ...
                     qubit_idx += 1;
                 } // end of while check qubit_idx < num_qubits ...
                 qubit_idx = 0;
             } // end of while keep iterating ...
             keep_iterating = true;
 
-            if _i.is_empty() {
+            if qubits_range_.is_empty() {
                 continue 'outer_loop;
             }
 
-            let maxes: Vec<usize> = _cnots
+            let bin_str_max_0_1: Vec<usize> = bin_str_
                 .axis_iter(numpy::ndarray::Axis(0))
                 .map(|row| {
                     std::cmp::max(
                         row.iter().filter(|&&x| !x).count(),
-                        row.iter().filter(|&&x| x).count(),
+                        row.into_iter().filter(|&&x| x).count(),
                     )
                 })
                 .collect();
 
-            let maxes2: Vec<usize> = _i.iter().map(|&_i_idx| maxes[_i_idx]).collect();
-
-            let _temp_argmax = maxes2
+            let bin_str_mx_qbt_rng: Vec<usize> = qubits_range_
                 .iter()
+                .map(|&q_idx| bin_str_max_0_1[q_idx])
+                .collect();
+
+            let argmax_ = bin_str_mx_qbt_rng
+                .into_iter()
                 .enumerate()
                 .max_by(|(_, x), (_, y)| x.cmp(y))
                 .map(|(idx, _)| idx)
                 .unwrap();
 
-            let _j = _i[_temp_argmax];
+            let argmax_qubit_idx = qubits_range_[argmax_];
 
-            let mut cnots0_t = vec![];
-            let mut cnots1_t = vec![];
+            let mut bin_str_subset_0_t = vec![];
+            let mut bin_str_subset_1_t = vec![];
 
-            let mut cnots0_t_shape = (0_usize, _cnots.column(0).len());
-            let mut cnots1_t_shape = (0_usize, 0_usize);
-            cnots1_t_shape.1 = cnots0_t_shape.1;
-            for cols in _cnots.columns() {
-                if !cols[_j] {
-                    cnots0_t_shape.0 += 1;
-                    cnots0_t.append(&mut cols.to_vec());
+            let mut bin_str_subset_0_t_shape = (0_usize, bin_str_.column(0).len());
+            let mut bin_str_subset_1_t_shape = (0_usize, 0_usize);
+            bin_str_subset_1_t_shape.1 = bin_str_subset_0_t_shape.1;
+            for cols in bin_str_.columns() {
+                if !cols[argmax_qubit_idx] {
+                    bin_str_subset_0_t_shape.0 += 1;
+                    bin_str_subset_0_t.append(&mut cols.to_vec());
                 } else {
-                    cnots1_t_shape.0 += 1;
-                    cnots1_t.append(&mut cols.to_vec());
+                    bin_str_subset_1_t_shape.0 += 1;
+                    bin_str_subset_1_t.append(&mut cols.to_vec());
                 }
             }
 
-            let cnots0 = Array2::from_shape_vec((cnots0_t_shape.0, cnots0_t_shape.1), cnots0_t)
-                .unwrap()
-                .reversed_axes();
-            let cnots1 = Array2::from_shape_vec((cnots1_t_shape.0, cnots1_t_shape.1), cnots1_t)
-                .unwrap()
-                .reversed_axes();
+            let bin_str_subset_0 = Array2::from_shape_vec(
+                (bin_str_subset_0_t_shape.0, bin_str_subset_0_t_shape.1),
+                bin_str_subset_0_t,
+            )
+            .unwrap()
+            .reversed_axes();
+            let bin_str_subset_1 = Array2::from_shape_vec(
+                (bin_str_subset_1_t_shape.0, bin_str_subset_1_t_shape.1),
+                bin_str_subset_1_t,
+            )
+            .unwrap()
+            .reversed_axes();
 
-            if _ep == num_qubits {
-                q.push((
-                    cnots1,
-                    _i.clone().into_iter().filter(|&x| x != _j).collect(),
-                    _j,
+            if qubits_ == num_qubits {
+                data_stack.push((
+                    bin_str_subset_1,
+                    qubits_range_
+                        .clone()
+                        .into_iter()
+                        .filter(|&x| x != argmax_qubit_idx)
+                        .collect(),
+                    argmax_qubit_idx,
                 ));
             } else {
-                q.push((
-                    cnots1,
-                    _i.clone().into_iter().filter(|&x| x != _j).collect(),
-                    _ep,
+                data_stack.push((
+                    bin_str_subset_1,
+                    qubits_range_
+                        .clone()
+                        .into_iter()
+                        .filter(|&x| x != argmax_qubit_idx)
+                        .collect(),
+                    qubits_,
                 ));
             }
-            q.push((
-                cnots0,
-                _i.clone().into_iter().filter(|&x| x != _j).collect(),
-                _ep,
+            data_stack.push((
+                bin_str_subset_0,
+                qubits_range_
+                    .clone()
+                    .into_iter()
+                    .filter(|&x| x != argmax_qubit_idx)
+                    .collect(),
+                qubits_,
             ));
         } // end 'outer_loop
 
+        // After the phases are applied, then `cx` with phases are applied,
+        // now, this is the third block of code which corresponds to appending
+        // the output of `synth_pmh` to the iterables stored in `cx_phase_iter`.
+        // The bool `pmh_init_done` indicates if the output of `synth_pmh` has been
+        // obtained. Since, the size of output from `synth_pmh` depends on the
+        // contents of `state` left after applying phases and CNOTs, so the memory
+        // required to store the output of `synth_pmh` is not known at compile
+        // time, that's why this output which is an iterable has to be stored on the heap!
         if phase_done && cx_phase_done && !pmh_init_done {
             synth_pmh_iter = Some(Box::new(synth_pmh(state.clone(), section_size).rev()));
+            // Once, the output has been stored on the heap, setting this
+            // bool makes sure the control doesn't enter this if block again.
             pmh_init_done = true;
         }
 
+        // Now, that the output of `synth_pmh` has been stored
+        // in `synth_pmh_iter` on the heap, now, all we have to do
+        // is to keep calling `next()`, and returning to `cx_phase_iter`.
+        // When `next()` yeilds `None` the same is passed to `cx_phase_iter`
+        // and that marks the completion of the whole `std::iter::from_fn` logic!
         if pmh_init_done {
             if let Some(ref mut data) = synth_pmh_iter {
                 gate_instr = data.next();
