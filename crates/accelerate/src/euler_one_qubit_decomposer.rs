b43327d133372380d8435af270962ffd3abee135
Jake Lishman <jake.lishman@ibm.com>
2025-01-22 15:47:09 +0000

    Reformat modified imports

--- crates/accelerate/src/euler_one_qubit_decomposer.rs
+++ crates/accelerate/src/euler_one_qubit_decomposer.rs
@@ -33,8 +33,7 @@ use pyo3::pybacked::PyBackedStr;
 use rustworkx_core::petgraph::stable_graph::NodeIndex;
 
 use qiskit_circuit::circuit_data::CircuitData;
-use qiskit_circuit::dag::{DAGCircuit, NodeType};
-use qiskit_circuit::dag::DAGOpNode;
+use qiskit_circuit::dag::{DAGCircuit, DAGOpNode, NodeType};
 use qiskit_circuit::operations::{Operation, Param, StandardGate};
 use qiskit_circuit::slice::{PySequenceIndex, SequenceIndex};
 use qiskit_circuit::util::c64;
