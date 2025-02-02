413d35c31b01aa278f86fc5fdc1f7c7a83e0e69a
Ak-ash22 <14akash2000@gmail.com>
2025-02-02 21:54:22 +0100

    Included prefix r to the qasm string

--- test/python/circuit/test_circuit_qasm.py
+++ test/python/circuit/test_circuit_qasm.py
@@ -418,7 +418,7 @@ mcx q[0],q[1],q[2],q[3];"""
         # qasm output doesn't support parameterized gate yet.
         # param0 for "gate mcuq(param0) is not used inside the definition
         expected_qasm = re.compile(
-            """OPENQASM 2.0;
+            r"""OPENQASM 2.0;
 include "qelib1.inc";
 gate mcx_vchain_dg q0,q1,q2 { ccx q0,q1,q2; }
 gate mcx_vchain q0,q1,q2,q3,q4 { h q3; p\(pi/8\) q0; p\(pi/8\) q1; p\(pi/8\) q2; p\(pi/8\) q3; cx q0,q1; p\(-pi/8\) q1; cx q0,q1; cx q1,q2; p\(-pi/8\) q2; cx q0,q2; p\(pi/8\) q2; cx q1,q2; p\(-pi/8\) q2; cx q0,q2; cx q2,q3; p\(-pi/8\) q3; cx q1,q3; p\(pi/8\) q3; cx q2,q3; p\(-pi/8\) q3; cx q0,q3; p\(pi/8\) q3; cx q2,q3; p\(-pi/8\) q3; cx q1,q3; p\(pi/8\) q3; cx q2,q3; p\(-pi/8\) q3; cx q0,q3; h q3; }
