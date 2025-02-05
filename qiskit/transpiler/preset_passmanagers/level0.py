fa26efb17043a52c584f9b4474d51b549317fa3d
Jake Lishman <jake.lishman@ibm.com>
2025-02-05 18:39:47 +0000

    Add `default` translation stage plugin
    This is added to give us semantic room to play with the default
    translation algorithm during the 2.x series.  It starts life as an alias
    to `translator`, but may change to improve support for fractional-gate
    translation, more optimized synthesis routines, or discrete-basis
    translations (like a SK-based 1q translator for logical backends).
    
    This makes it programmatically possible to distinguish the "default"
    plugin choice from an explicit "translator" choice, even when (as right
    now) those are the same thing.  Note that `pass_manager_config` uses
    `None` as an implicit default, but we active suggest using the
    plugin-stage getters, which need an actual plugin name.

--- qiskit/transpiler/preset_passmanagers/level0.py
+++ qiskit/transpiler/preset_passmanagers/level0.py
@@ -49,7 +49,7 @@ def level_0_pass_manager(pass_manager_config: PassManagerConfig) -> StagedPassMa
     init_method = pass_manager_config.init_method or "default"
     layout_method = pass_manager_config.layout_method or "default"
     routing_method = pass_manager_config.routing_method or "sabre"
-    translation_method = pass_manager_config.translation_method or "translator"
+    translation_method = pass_manager_config.translation_method or "default"
     optimization_method = pass_manager_config.optimization_method or "default"
     scheduling_method = pass_manager_config.scheduling_method or "default"
     target = pass_manager_config.target
