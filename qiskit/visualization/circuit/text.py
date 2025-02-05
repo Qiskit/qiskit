1ef0d7957cde5d6ad7df640632b73371c118b265
atharva-satpute <55058959+atharva-satpute@users.noreply.github.com>
2025-02-05 19:20:51 +0530

    Fix barrier label position when bits are reversed (#13780)
    * qa: Fix barrier label position when bits are reversed
    
    * Consider qubit with min index as top qubit
    
    * Fix lint issues
    
    * Fix typo
    
    ---------
    
    Co-authored-by: Jake Lishman <jake@binhbar.com>

--- qiskit/visualization/circuit/text.py
+++ qiskit/visualization/circuit/text.py
@@ -1153,9 +1153,10 @@ class TextDrawing:
             if not self.plotbarriers:
                 return layer, current_cons, current_cons_cond, connection_label
 
-            for i, qubit in enumerate(node.qargs):
+            top_qubit = min(node.qargs, key=lambda q: self._wire_map.get(q, float("inf")))
+            for qubit in node.qargs:
                 if qubit in self.qubits:
-                    label = op.label if i == 0 else ""
+                    label = op.label if qubit == top_qubit else ""
                     layer.set_qubit(qubit, Barrier(label))
 
         elif isinstance(op, SwapGate):
