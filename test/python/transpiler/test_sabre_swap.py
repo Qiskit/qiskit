820905ab4d16f2b64db60c201417c3d8601af84e
Jake Lishman <jake.lishman@ibm.com>
2025-02-07 20:57:09 +0000

    Fix mistaken test

--- test/python/transpiler/test_sabre_swap.py
+++ test/python/transpiler/test_sabre_swap.py
@@ -760,9 +760,9 @@ class TestSabreSwapControlFlow(QiskitTestCase):
         expected.if_else((creg[0], 0), etrue_body, efalse_body, qreg[[1, 2, 3, 4]], creg[[0]])
         expected.h(3)
         expected.swap(2, 3)
-        expected.cx(3, 2)
+        expected.cx(2, 1)
         expected.barrier()
-        expected.measure(qreg[[2, 0, 1, 3, 4]], creg)
+        expected.measure(qreg[[1, 0, 3, 2, 4]], creg)
         self.assertEqual(dag_to_circuit(cdag), expected)
 
     def test_if_expr(self):
