07cd06a7398fd40c0183fdfdc629bd6b4fda2454
Matthew Treinish <mtreinish@kortar.org>
2025-02-07 14:54:10 -0500

    Try to fix equality failure in qpy tests

--- test/qpy_compat/test_qpy.py
+++ test/qpy_compat/test_qpy.py
@@ -123,8 +123,11 @@ def generate_random_circuits(version):
         for j in range(i):
             qc.reset(j)
             if version >= (2, 0, 0):
-                with qc.if_test((qc.cregs[0], i)):
-                    qc.x(0)
+                from qiskit.circuit.controlflow import IfElseOp
+                condition = (qc.cregs[0], i)
+                body = QuantumCircuit(qc.qubits[0])
+                body.x(0)
+                qc.append(IfElseOp((condition, body), [qc.qubits[0]]))
             else:
                 qc.x(0).c_if(qc.cregs[0], i)
         for j in range(i):
@@ -293,8 +296,11 @@ def generate_single_clbit_condition_teleportation(version):  # pylint: disable=i
     teleport_qc.x(0)
     teleport_qc.measure(0, cr[0])
     if version >= (2, 0, 0):
-        with teleport_qc.if_test((cr[0], 1)):
-            teleport_qc.x(0)
+        from qiskit.circuit.controlflow import IfElseOp
+        condition = (cr[0], 1)
+        body = QuantumCircuit(teleport_qc.qubits[0])
+        body.x(0)
+        teleport_qc.append(IfElseOp((condition, body), [teleport_qc.qubits[0]]))
     else:
         teleport_qc.x(0).c_if(cr[0], 1)
     teleport_qc.measure(0, cr[1])
