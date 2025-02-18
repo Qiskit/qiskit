457ad8bf366ef24adcb0eff80d28dfb29e6837c3
Luciano Bello <bel@zurich.ibm.com>
2025-02-18 12:15:55 +0100

    Change the register name for `measure_active` to `meas` (#13866)
    * change the register name for measure_active
    
    * Tweak cross-references
    
    ---------
    
    Co-authored-by: Jake Lishman <jake@binhbar.com>

--- qiskit/circuit/quantumcircuit.py
+++ qiskit/circuit/quantumcircuit.py
@@ -3953,7 +3953,7 @@ class QuantumCircuit:
             circ = self.copy()
         dag = circuit_to_dag(circ)
         qubits_to_measure = [qubit for qubit in circ.qubits if qubit not in dag.idle_wires()]
-        new_creg = circ._create_creg(len(qubits_to_measure), "measure")
+        new_creg = circ._create_creg(len(qubits_to_measure), "meas")
         circ.add_register(new_creg)
         circ.barrier()
         circ.measure(qubits_to_measure, new_creg)
