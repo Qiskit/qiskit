4e0f2dfb20465f17bccbd60a2166ae832f87375c
Kevin Hartman <kevin@hart.mn>
2025-02-18 13:09:53 -0500

    Add serialization testing.

--- test/python/compiler/test_transpiler.py
+++ test/python/compiler/test_transpiler.py
@@ -2240,7 +2240,8 @@ class TestPostTranspileIntegration(QiskitTestCase):
             base.append(CustomCX(), [2, 4])
             base.ry(a, 4)
             base.measure(4, 2)
-        with base.switch(expr.bit_and(base.cregs[0], 2)) as case_:
+        # Use a const Uint RHS to make sure we QPY can serialize it.
+        with base.switch(expr.bit_and(base.cregs[0], expr.lift(2, try_const=True))) as case_:
             with case_(0, 1):
                 base.cz(3, 5)
             with case_(case_.DEFAULT):
