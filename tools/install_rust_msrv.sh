46b218f5e78b82af2befb080999810af8732046f
Matthew Treinish <mtreinish@kortar.org>
2025-02-21 17:11:48 -0500

    Split out aarch64 tier 1 job to separate job
    The aarch64 wheel builds are failing with PGO on latest stable (and
    stable - 1). This is likely either an issue in rustc or the llvm tools
    component from rustup on the later releases. But it seems to build fine
    with our MSRV. We build with latest stable for release jobs typically to
    enable the improvements from the latest compiler for what we publish.
    But PGO has enough benefit that using our MSRV is fine if that is what's
    necessary.

---
+++ tools/install_rust_msrv.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+if [ ! -d rust-installer ]; then
+    mkdir rust-installer
+    wget https://sh.rustup.rs -O rust-installer/rustup.sh
+    sh rust-installer/rustup.sh -y -c llvm-tools -t 1.79
+fi
+. "$HOME/.cargo/env"
