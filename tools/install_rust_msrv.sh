47c1b0ccfaea04a9be0fe1bd955587522e61b8ff
Matthew Treinish <mtreinish@kortar.org>
2025-02-24 13:44:51 -0500

    Promote linux aarch64 to tier 1 support (#13737)
    * Promote linux aarch64 to tier 1 support
    
    With native linux aarch64 runners available from github now available
    [1] we can run tests on every PR on the platform and promote it to tier
    1 support [2]. This commit adds a github action test job that runs on
    PRs to test that the platform works on every commit. Then the wheel job
    is promoted to the tier 1 job which includes PGO which will improve the
    performance on the wheels we publish for the platform.
    
    While there is a larger effort to migrate our entire CI to github
    actions in #13474 expanding our test matrix prior to that won't cause
    a conflict as we can easily expand the test matrix in that PR if this
    merges first.
    
    [1]: https://github.blog/changelog/2025-01-16-linux-arm64-hosted-runners-now-available-for-free-in-public-repositories-public-preview/
    [2]: https://docs.quantum.ibm.com/guides/install-qiskit#operating-system-support
    
    * Update job name for test job
    
    * Try latest manylinux image for arm
    
    * Use url for image path
    
    * Try without explicit latest tag
    
    * Tweak rust install script
    
    * Revert "Try without explicit latest tag"
    
    This reverts commit d39b6ee74916babd99ecd6d292648b703ee8bcbc.
    
    * Split out aarch64 tier 1 job to separate job
    
    The aarch64 wheel builds are failing with PGO on latest stable (and
    stable - 1). This is likely either an issue in rustc or the llvm tools
    component from rustup on the later releases. But it seems to build fine
    with our MSRV. We build with latest stable for release jobs typically to
    enable the improvements from the latest compiler for what we publish.
    But PGO has enough benefit that using our MSRV is fine if that is what's
    necessary.
    
    * Remove duplicate tier 1 arm job
    
    * Try different target string to fix profile version mismatch
    
    * Tweak msrv install again
    
    * Only install a single rust toolchain in aarch64 env
    
    * Deduplicate job definitions
    
    * Add comment about msrv location
    
    * Remove stale comment
    
    * Fix release notes typo
    
    * Adjust run script
    
    * Set rust toolchain to msrv in build

--- tools/install_rust_msrv.sh
+++ tools/install_rust_msrv.sh
@@ -3,5 +3,8 @@ if [ ! -d rust-installer ]; then
     mkdir rust-installer
     wget https://sh.rustup.rs -O rust-installer/rustup.sh
     sh rust-installer/rustup.sh -y -c llvm-tools
+    rustup default 1.79
+    rustup component add llvm-tools
+    rustup uninstall stable
 fi
 . "$HOME/.cargo/env"
