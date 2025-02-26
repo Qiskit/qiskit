2b8dad05009bb36397accdf78a568e1c1c1782f4
Vyom Patel <vyom.patel@uwaterloo.ca>
2025-02-26 11:31:34 -0500

    add test for cx count

--- test/python/synthesis/test_mcx_synthesis.py
+++ test/python/synthesis/test_mcx_synthesis.py
@@ -4,7 +4,12 @@ from ddt import ddt, data
 
 from qiskit import QuantumCircuit, transpile
 from qiskit.quantum_info.states import Statevector
-from qiskit.synthesis.multi_controlled import synth_mcx_1_clean_kg24, synth_mcx_2_clean_kg24, synth_mcx_1_dirty_kg24, synth_mcx_2_dirty_kg24
+from qiskit.synthesis.multi_controlled import (
+    synth_mcx_1_clean_kg24,
+    synth_mcx_2_clean_kg24,
+    synth_mcx_1_dirty_kg24,
+    synth_mcx_2_dirty_kg24,
+)
 from test import QiskitTestCase  # pylint: disable=wrong-import-order
 
 
@@ -30,11 +35,11 @@ class TestMCXSynth(QiskitTestCase):
 
                 corrected = np.zeros(2 ** (num_ctrl_qubits + 1), dtype=complex)
                 for i, statevector_amplitude in enumerate(statevector):
-                    i = int(bin(i)[2:].zfill(qc.num_qubits)[1 :], 2)    # 1 ancilla qubit
+                    i = int(bin(i)[2:].zfill(qc.num_qubits)[1:], 2)  # 1 ancilla qubit
                     corrected[i] += statevector_amplitude
                 statevector = corrected
             np.testing.assert_array_almost_equal(statevector.real, reference)
-    
+
     @data(3, 5, 8, 10, 15)
     def test_synth_mcx_2_ancilla(self, num_ctrl_qubits):
         """Test mcx synthesis with 2 ancilla."""
@@ -53,12 +58,35 @@ class TestMCXSynth(QiskitTestCase):
 
                 corrected = np.zeros(2 ** (num_ctrl_qubits + 1), dtype=complex)
                 for i, statevector_amplitude in enumerate(statevector):
-                    i = int(bin(i)[2:].zfill(qc.num_qubits)[2 :], 2)    # 2 ancilla qubits
+                    i = int(bin(i)[2:].zfill(qc.num_qubits)[2:], 2)  # 2 ancilla qubits
                     corrected[i] += statevector_amplitude
                 statevector = corrected
             np.testing.assert_array_almost_equal(statevector.real, reference)
 
-    
+    @data(3, 5, 10, 15)
+    def test_synth_mcx_clean_kg24_count(self, num_ctrl_qubits):
+        """Test if cx count of the synth_mcx_clean_kg24 is correct."""
+        for mcx in [
+            synth_mcx_1_clean_kg24(num_ctrl_qubits),
+            synth_mcx_2_clean_kg24(num_ctrl_qubits),
+        ]:
+            with self.subTest(gate_circuit=mcx):
+                tr_mcx = transpile(mcx, basis_gates=["u", "cx"])
+                cx_count = tr_mcx.count_ops()["cx"]
+                self.assertLessEqual(cx_count, 12 * num_ctrl_qubits - 18)
+
+    @data(3, 5, 10, 15)
+    def test_synth_mcx_dirty_kg24_count(self, num_ctrl_qubits):
+        """Test if cx count of synth_mcx_dirty_kg24 is correct."""
+        for mcx in [
+            synth_mcx_1_dirty_kg24(num_ctrl_qubits),
+            synth_mcx_2_dirty_kg24(num_ctrl_qubits),
+        ]:
+            with self.subTest(gate_circuit=mcx):
+                tr_mcx = transpile(mcx, basis_gates=["u", "cx"])
+                cx_count = tr_mcx.count_ops()["cx"]
+                self.assertLessEqual(cx_count, 24 * num_ctrl_qubits - 48)
+
+
 if __name__ == "__main__":
     unittest.main()
-
