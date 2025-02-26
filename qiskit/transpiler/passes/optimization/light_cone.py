f02a7b9b857f462630fd1a4c80144592c9bae460
Marc Sanz Drudis <93275620+MarcDrudis@users.noreply.github.com>
2025-02-26 10:01:31 +0100

    Apply suggestions from code review
    Applied suggestions
    
    Co-authored-by: Julien Gacon <gaconju@gmail.com>

--- qiskit/transpiler/passes/optimization/light_cone.py
+++ qiskit/transpiler/passes/optimization/light_cone.py
@@ -35,10 +35,10 @@ class LightCone(TransformationPass):
     def __init__(self, bit_terms: str | None = None, indices: list[int] | None = None) -> None:
         """
         Args:
-            bit_terms: If `None` the light-cone will be computed for the set of measurements
-            in the circuit. If a string is specified, the light-cone will correspond to the
-            reduced circuit with the same expectation value for the observable.
-            indices: list of non-trivial indices corresponding to the observable in `bit_terms`.
+            bit_terms: If ``None`` the light-cone will be computed for the set of measurements
+                in the circuit. If a string is specified, the light-cone will correspond to the
+                reduced circuit with the same expectation value for the observable.
+            indices: list of non-trivial indices corresponding to the observable in ``bit_terms``.
         """
         super().__init__()
         valid_characters = {"X", "Y", "Z", "+", "-", "l", "r", "0", "1"}
