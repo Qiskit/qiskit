962a2fcd90814720ad275b270af745c47906295b
Luciano Bello <bel@zurich.ibm.com>
2025-02-26 15:52:04 +0100

    test.python.circuit.test_controlled_gate.TestControlledGate.test_mcxrecursive_clean_ancilla_cx_count

--- qiskit/circuit/library/standard_gates/x.py
+++ qiskit/circuit/library/standard_gates/x.py
@@ -1117,7 +1117,7 @@ class MCXGate(ControlledGate):
         _base_label=None,
     ):
         """Create new MCX gate."""
-        num_ancilla_qubits = self.get_num_ancilla_qubits(num_ctrl_qubits)
+        num_ancilla_qubits = self.__class__.get_num_ancilla_qubits(num_ctrl_qubits)
         super().__init__(
             _name,
             num_ctrl_qubits + 1 + num_ancilla_qubits,
