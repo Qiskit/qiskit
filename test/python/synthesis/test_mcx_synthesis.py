0ce5f9fecb086d7be8060a57c5839b7805180d62
Vyom Patel <vyom.patel@uwaterloo.ca>
2025-02-27 02:31:45 -0500

    add test for depth

--- test/python/synthesis/test_mcx_synthesis.py
+++ test/python/synthesis/test_mcx_synthesis.py
@@ -71,7 +71,7 @@ class TestMCXSynth(QiskitTestCase):
             synth_mcx_2_clean_kg24(num_ctrl_qubits),
         ]:
             with self.subTest(gate_circuit=mcx):
-                tr_mcx = transpile(mcx, basis_gates=["u", "cx"])
+                tr_mcx = transpile(mcx, basis_gates=["u", "cx"], optimization_level=3)
                 cx_count = tr_mcx.count_ops()["cx"]
                 self.assertLessEqual(cx_count, 12 * num_ctrl_qubits - 18)
 
@@ -83,9 +83,25 @@ class TestMCXSynth(QiskitTestCase):
             synth_mcx_2_dirty_kg24(num_ctrl_qubits),
         ]:
             with self.subTest(gate_circuit=mcx):
-                tr_mcx = transpile(mcx, basis_gates=["u", "cx"])
+                tr_mcx = transpile(mcx, basis_gates=["u", "cx"], optimization_level=3)
                 cx_count = tr_mcx.count_ops()["cx"]
                 self.assertLessEqual(cx_count, 24 * num_ctrl_qubits - 48)
+
+    @data(3, 5, 10, 15)
+    def test_synth_mcx_1_clean_kg24_depth(self, num_ctrl_qubits):
+        """Test if depth of the synth_mcx_1_clean_kg24 is correct."""
+        mcx = synth_mcx_1_clean_kg24(num_ctrl_qubits)
+        tr_mcx = transpile(mcx, basis_gates=["u", "cx"], optimization_level=3)
+        depth = tr_mcx.depth()
+        self.assertLessEqual(depth, 18 * num_ctrl_qubits - 22)  # Found via regression fit
+
+    @data(3, 5, 10, 15)
+    def test_synth_mcx_1_dirty_kg24_depth(self, num_ctrl_qubits):
+        """Test if depth of the synth_mcx_1_dirty_kg24 is correct."""
+        mcx = synth_mcx_1_dirty_kg24(num_ctrl_qubits)
+        tr_mcx = transpile(mcx, basis_gates=["u", "cx"], optimization_level=3)
+        depth = tr_mcx.depth()
+        self.assertLessEqual(depth, 36 * num_ctrl_qubits - 63)  # Found via regression fit
 
 
 if __name__ == "__main__":
