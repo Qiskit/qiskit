Qiskit/qiskit

commit a00279bc7d6005bfc5ea783cd1fe336784160518

Circuits with control flow now raise a proper error when `circuit_to_instruction` is called (#13921)
* Circuits with control flow now raise a proper error when circuit_to_instruction is called

* test

* cyclic import

* Update qiskit/converters/circuit_to_instruction.py

Co-authored-by: Elena Peña Tapia <57907331+ElePT@users.noreply.github.com>

---------

Co-authored-by: Elena Peña Tapia <57907331+ElePT@users.noreply.github.com>

qiskit/converters/circuit_to_instruction.py
@@ -11,6 +11,7 @@
 # that they have been altered from the originals.
 
 """Helper function for converting a circuit to an instruction."""
+from qiskit.circuit.controlflow.control_flow import ControlFlowOp
 from qiskit.exceptions import QiskitError
 from qiskit.circuit.instruction import Instruction
 from qiskit.circuit.quantumregister import QuantumRegister
@@ -83,6 +84,13 @@ def circuit_to_instruction(circuit, parameter_map=None, equivalence_library=None
             "Circuits with internal variables cannot yet be converted to instructions."
             " You may be able to use `QuantumCircuit.compose` to inline this circuit into another."
         )
+
+    for inst in circuit.data:
+        if isinstance(inst.operation, ControlFlowOp):
+            raise QiskitError(
+                f"Circuits with control flow operations ({type(inst.operation)}) "
+                "cannot be converted to an instruction."
+            )
 
     if parameter_map is None:
         parameter_dict = {p: p for p in circuit.parameters}

releasenotes/notes/fixes_11379-4c4d6d71213a1c8c.yaml
@@ -1,0 +1,5 @@
+---
+upgrade_circuits:
+  - |
+    Converting a quantum circuit to a gate with :func:`.converters.circuit_to_instruction` fails properly when given
+    circuit contains control flow instructions.

test/python/converters/test_circuit_to_instruction.py
@@ -143,6 +143,17 @@ class TestCircuitToInstruction(QiskitTestCase):
             QiskitError, circuit_to_instruction, qc, {theta: gamma, phi: phi, delta: delta}
         )
 
+    def test_control_flow_raises(self):
+        """Raising if a circuit to convert has control flow.
+        See https://github.com/Qiskit/qiskit/issues/11379"""
+        qc = QuantumCircuit(2)
+        qc.h(0)
+        qc.cx(0, 1)
+        qc.measure_all()
+        with qc.if_test((qc.clbits[0], 0)):
+            qc.x(0)
+        self.assertRaises(QiskitError, circuit_to_instruction, qc)
+
     def test_parameter_map(self):
         """Verify alternate parameter specification"""
         qr = QuantumRegister(3, "qr")

commit 4e4eb9bb1b8ffef3d8f068e6710e9cb3825d2bdc

Add: Add method has_control_flow_op(), to `QuantumCircuit`
This method was added to fix a performance regression introduced by #13921. This check should be quicker than the multiple calls to `isinstance`.

crates/circuit/src/circuit_data.rs
@@ -15,7 +15,7 @@ use std::sync::OnceLock;
 
 use crate::bit_data::BitData;
 use crate::circuit_instruction::{CircuitInstruction, OperationFromPython};
-use crate::dag_circuit::add_global_phase;
+use crate::dag_circuit::{add_global_phase, CONTROL_FLOW_OP_NAMES};
 use crate::imports::{ANNOTATED_OPERATION, CLBIT, QUANTUM_CIRCUIT, QUBIT};
 use crate::interner::{Interned, Interner};
 use crate::operations::{Operation, OperationRef, Param, StandardGate};
@@ -421,6 +421,13 @@ impl CircuitData {
             }
         }
         Ok(())
+    }
+
+    /// Checks whether the circuit has an instance of :class:`.ControlFlowOp`
+    /// present amongst its operations.
+    pub fn has_control_flow_op(&self) -> bool {
+        let operations = self.count_ops();
+        CONTROL_FLOW_OP_NAMES.iter().any(|name| operations.contains_key(name))
     }
 
     /// Replaces the bits of this container with the given ``qubits``

crates/circuit/src/dag_circuit.rs
@@ -70,7 +70,7 @@ use std::f64::consts::PI;
 #[cfg(feature = "cache_pygates")]
 use std::sync::OnceLock;
 
-static CONTROL_FLOW_OP_NAMES: [&str; 4] = ["for_loop", "while_loop", "if_else", "switch_case"];
+pub(crate) static CONTROL_FLOW_OP_NAMES: [&str; 4] = ["for_loop", "while_loop", "if_else", "switch_case"];
 static SEMANTIC_EQ_SYMMETRIC: [&str; 4] = ["barrier", "swap", "break_loop", "continue_loop"];
 
 /// An opaque key type that identifies a variable within a [DAGCircuit].

qiskit/circuit/quantumcircuit.py
@@ -4341,6 +4341,11 @@ class QuantumCircuit:
         )
         return None if inplace else target
 
+    def has_control_flow_op(self) -> bool:
+        """Checks whether the circuit has an instance of :class:`.ControlFlowOp` 
+        present amongst its operations."""
+        return self._data.has_control_flow_op()
+    
     def _unroll_param_dict(
         self, parameter_binds: Mapping[Parameter, ParameterValueType]
     ) -> Mapping[Parameter, ParameterValueType]:

qiskit/converters/circuit_to_instruction.py
@@ -85,12 +85,10 @@ def circuit_to_instruction(circuit, parameter_map=None, equivalence_library=None
             " You may be able to use `QuantumCircuit.compose` to inline this circuit into another."
         )
 
-    for inst in circuit.data:
-        if isinstance(inst.operation, ControlFlowOp):
-            raise QiskitError(
-                f"Circuits with control flow operations ({type(inst.operation)}) "
-                "cannot be converted to an instruction."
-            )
+    if circuit.has_control_flow_op():
+        raise QiskitError(
+            f"Circuits with control flow operations cannot be converted to an instruction."
+        )
 
     if parameter_map is None:
         parameter_dict = {p: p for p in circuit.parameters}