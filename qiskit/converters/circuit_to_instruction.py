Qiskit/qiskit

commit a00279bc7d6005bfc5ea783cd1fe336784160518

Circuits with control flow now raise a proper error when `circuit_to_instruction` is called (#13921)
* Circuits with control flow now raise a proper error when circuit_to_instruction is called

* test

* cyclic import

* Update qiskit/converters/circuit_to_instruction.py

Co-authored-by: Elena Peña Tapia <57907331+ElePT@users.noreply.github.com>

---------

Co-authored-by: Elena Peña Tapia <57907331+ElePT@users.noreply.github.com>

qiskit/converters/circuit_to_instruction.py
@@ -11,6 +11,7 @@
 # that they have been altered from the originals.
 
 """Helper function for converting a circuit to an instruction."""
+from qiskit.circuit.controlflow.control_flow import ControlFlowOp
 from qiskit.exceptions import QiskitError
 from qiskit.circuit.instruction import Instruction
 from qiskit.circuit.quantumregister import QuantumRegister
@@ -83,6 +84,13 @@ def circuit_to_instruction(circuit, parameter_map=None, equivalence_library=None
             "Circuits with internal variables cannot yet be converted to instructions."
             " You may be able to use `QuantumCircuit.compose` to inline this circuit into another."
         )
+
+    for inst in circuit.data:
+        if isinstance(inst.operation, ControlFlowOp):
+            raise QiskitError(
+                f"Circuits with control flow operations ({type(inst.operation)}) "
+                "cannot be converted to an instruction."
+            )
 
     if parameter_map is None:
         parameter_dict = {p: p for p in circuit.parameters}

releasenotes/notes/fixes_11379-4c4d6d71213a1c8c.yaml
@@ -1,0 +1,5 @@
+---
+upgrade_circuits:
+  - |
+    Converting a quantum circuit to a gate with :func:`.converters.circuit_to_instruction` fails properly when given
+    circuit contains control flow instructions.

test/python/converters/test_circuit_to_instruction.py
@@ -143,6 +143,17 @@ class TestCircuitToInstruction(QiskitTestCase):
             QiskitError, circuit_to_instruction, qc, {theta: gamma, phi: phi, delta: delta}
         )
 
+    def test_control_flow_raises(self):
+        """Raising if a circuit to convert has control flow.
+        See https://github.com/Qiskit/qiskit/issues/11379"""
+        qc = QuantumCircuit(2)
+        qc.h(0)
+        qc.cx(0, 1)
+        qc.measure_all()
+        with qc.if_test((qc.clbits[0], 0)):
+            qc.x(0)
+        self.assertRaises(QiskitError, circuit_to_instruction, qc)
+
     def test_parameter_map(self):
         """Verify alternate parameter specification"""
         qr = QuantumRegister(3, "qr")

commit 53b142e93acc8820869ebf2ac19e29fb958abb26

Fix performance regression in circuit_to_instruction
In #13921 a check was added to circuit_to_instruction() for determining
if the circuit has a control flow op. This check was adding significant
overhead to the function, nightly asv benchmarks are showing up to a ~9x
slowdown. #13921 attempted to fix this regression but did so by adding a
new API which isn't applicable at this point for 2.0. That PR could/should
still be used in 2.1. In the meantime this commit seeks to solve the
performance regression by adjusting how the check is performed to
mitigate the runtime regression for 2.0.

qiskit/converters/circuit_to_instruction.py
@@ -11,7 +11,7 @@
 # that they have been altered from the originals.
 
 """Helper function for converting a circuit to an instruction."""
-from qiskit.circuit.controlflow.control_flow import ControlFlowOp
+from qiskit.circuit.controlflow import CONTROL_FLOW_OP_NAMES
 from qiskit.exceptions import QiskitError
 from qiskit.circuit.instruction import Instruction
 from qiskit.circuit.quantumregister import QuantumRegister
@@ -84,13 +84,10 @@ def circuit_to_instruction(circuit, parameter_map=None, equivalence_library=None
             "Circuits with internal variables cannot yet be converted to instructions."
             " You may be able to use `QuantumCircuit.compose` to inline this circuit into another."
         )
-
-    for inst in circuit.data:
-        if isinstance(inst.operation, ControlFlowOp):
-            raise QiskitError(
-                f"Circuits with control flow operations ({type(inst.operation)}) "
-                "cannot be converted to an instruction."
-            )
+    if CONTROL_FLOW_OP_NAMES.intersection(circuit.count_ops()):
+        raise QiskitError(
+            "Circuits with control flow operations cannot be converted to an instruction."
+        )
 
     if parameter_map is None:
         parameter_dict = {p: p for p in circuit.parameters}