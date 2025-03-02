08e305922a5dd3aff3267afd866c54bab9fc4f3f
John Lapeyre <jlapeyre@users.noreply.github.com>
2025-03-01 19:11:41 -0500

    Fix qpy serialization of substitution of type `ParameterExpression` (#13890)
    * Fix qpy serialization of substitution of type ParameterExpression
    
    When substitution history `ParameterExpression._qpy_replay` is serialized,
    there was no branch for the case that the substituted value is of type
    `ParameterExpression`.
    This commit fixes this oversight.
    
    * Fix deserializing qpy written by previous writing fix
    
    * Added a branch for reading `ParameterExpression` in _qpy_replay
    
    * Added a missing argument (version) in an existing call in code path that
      was previously untested
    
    * Remove a bit of useless code introduced in last commit
    
    * run black
    
    * Add tests
    
    * Revert renaming extra_symbols to extra_expressions
    
    Reverting this to minimize changes in order to make the PR safer to
    backport.
    
    * Revert cleaning up logic in _encode_replay_subs
    
    This was a good change. But not necessary for the bug fix, which is
    the main point of this PR.
    
    * Add qpy compat test
    
    * Bind parameter when checking for equality
    
    * Run black after merge in browser
    
    ---------
    
    Co-authored-by: Luciano Bello <bel@zurich.ibm.com>
    (cherry picked from commit a6fa6f87d4cdea40b8776c50240e5d1e28fdb741)
    
    # Conflicts:
    #	test/qpy_compat/test_qpy.py

--- test/qpy_compat/test_qpy.py
+++ test/qpy_compat/test_qpy.py
@@ -820,8 +820,41 @@ def generate_v12_expr():
     return [index, shift]
 
 
+<<<<<<< HEAD
 def generate_circuits(version_parts):
     """Generate reference circuits."""
+=======
+def generate_replay_with_expression_substitutions():
+    """Circuits with parameters that have substituted expressions in the replay"""
+    a = Parameter("a")
+    b = Parameter("b")
+    a1 = a * 2
+    a2 = a1.subs({a: 3 * b})
+    qc = QuantumCircuit(1)
+    qc.rz(a2, 0)
+
+    return [qc]
+
+
+def generate_v14_expr():
+    """Circuits that contain expressions and types new in QPY v14."""
+    from qiskit.circuit.classical import expr, types
+
+    float_expr = QuantumCircuit(name="float_expr")
+    with float_expr.if_test(expr.less(1.0, 2.0)):
+        pass
+
+    return [float_expr]
+
+
+def generate_circuits(version_parts, current_version, load_context=False):
+    """Generate reference circuits.
+
+    If load_context is True, avoid generating Pulse-based reference
+    circuits. For those circuits, load_qpy only checks that the cached
+    circuits can be loaded without erroring."""
+
+>>>>>>> a6fa6f87d (Fix qpy serialization of substitution of type `ParameterExpression` (#13890))
     output_circuits = {
         "full.qpy": [generate_full_circuit()],
         "unitary.qpy": [generate_unitary_gate_circuit()],
@@ -871,6 +904,16 @@ def generate_circuits(version_parts):
     if version_parts >= (1, 1, 0):
         output_circuits["standalone_vars.qpy"] = generate_standalone_var()
         output_circuits["v12_expr.qpy"] = generate_v12_expr()
+<<<<<<< HEAD
+=======
+    if version_parts >= (1, 4, 1):
+        output_circuits["replay_with_expressions.qpy"] = (
+            generate_replay_with_expression_substitutions()
+        )
+
+    if version_parts >= (2, 0, 0):
+        output_circuits["v14_expr.qpy"] = generate_v14_expr()
+>>>>>>> a6fa6f87d (Fix qpy serialization of substitution of type `ParameterExpression` (#13890))
     return output_circuits
 
 
@@ -968,6 +1011,8 @@ def load_qpy(qpy_files, version_parts):
                 bind = np.linspace(1.0, 2.0, 22)
             elif path == "parameter_vector_expression.qpy":
                 bind = np.linspace(1.0, 2.0, 15)
+            elif path == "replay_with_expressions.qpy":
+                bind = [2.0]
 
             assert_equal(
                 circuit, qpy_circuits[i], i, version_parts, bind=bind, equivalent=equivalent
