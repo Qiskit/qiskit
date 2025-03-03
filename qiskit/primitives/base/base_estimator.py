5e4dfe17285e194e6959473bbfcc7efd6b63a0e7
Elena Peña Tapia <epenatap@gmail.com>
2025-03-03 10:55:25 +0100

    Fix lint, remove duplicated reno

--- qiskit/primitives/base/base_estimator.py
+++ qiskit/primitives/base/base_estimator.py
@@ -49,12 +49,12 @@ class BaseEstimatorV2(ABC):
      * pubs: list of pubs (Primitive Unified Blocs). Each pub contains three values that, together,
         define a computation unit of work for the estimator to complete:
 
-            * a single :class:`~qiskit.circuit.QuantumCircuit`, possibly parametrized, whose final state we
-            define as :math:`\psi(\theta)`,
+            * a single :class:`~qiskit.circuit.QuantumCircuit`, possibly parametrized, 
+            whose final state we define as :math:`\psi(\theta)`,
 
             * one or more observables (specified as any :class:`~.ObservablesArrayLike`, including
-            :class:`~.Pauli`, :class:`~.SparsePauliOp`, ``str``) that specify which expectation values to
-            estimate, denoted :math:`H_j`, and
+            :class:`~.Pauli`, :class:`~.SparsePauliOp`, ``str``) that specify which expectation 
+            values to estimate, denoted :math:`H_j`, and
 
             * a collection parameter value sets to bind the circuit against, :math:`\theta_k`.
 
