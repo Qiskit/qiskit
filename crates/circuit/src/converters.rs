adb5982747b56e232566a0679c387fc78c4804bd
Matthew Treinish <mtreinish@kortar.org>
2025-03-04 07:12:14 -0500

    Fix lint

--- crates/circuit/src/converters.rs
+++ crates/circuit/src/converters.rs
@@ -13,8 +13,8 @@
 #[cfg(feature = "cache_pygates")]
 use std::sync::OnceLock;
 
-use pyo3::prelude::*;
 use pyo3::intern;
+use pyo3::prelude::*;
 
 use crate::circuit_data::CircuitData;
 use crate::dag_circuit::{DAGCircuit, NodeType};
