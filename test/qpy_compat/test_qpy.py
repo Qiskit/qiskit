ae08faed911080381452e24efe8b96fc1c9c651e
Kevin Hartman <kevin@hart.mn>
2025-03-04 16:31:47 -0500

    Fix qpy compat test.

--- test/qpy_compat/test_qpy.py
+++ test/qpy_compat/test_qpy.py
@@ -15,6 +15,7 @@
 """Test cases to verify qpy backwards compatibility."""
 
 import argparse
+
 import itertools
 import random
 import re
@@ -847,7 +848,8 @@ def generate_replay_with_expression_substitutions():
 
 def generate_v14_expr():
     """Circuits that contain expressions and types new in QPY v14."""
-    from qiskit.circuit.classical import expr, types
+    import uuid
+    from qiskit.circuit.classical import expr
     from qiskit.circuit import Duration
 
     float_expr = QuantumCircuit(name="float_expr")
@@ -885,7 +887,8 @@ def generate_v14_expr():
         pass
 
     stretch_expr = QuantumCircuit(name="stretch_expr")
-    stretch = stretch_expr.add_stretch("a")
+    s = expr.Stretch(uuid.UUID(bytes=b"hello, qpy world", version=4), "a")
+    stretch = stretch_expr.add_stretch(s)
     with stretch_expr.if_test(expr.equal(stretch, Duration.dt(100))):
         pass
 
