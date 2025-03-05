c19a5d9b7619501b8eebae350cbba5a89b283f0e
Shelly Garion <46566946+ShellyGarion@users.noreply.github.com>
2025-03-05 13:32:19 +0200

    minor fix to consolidate blocks with RZZ and CZ gates (#13958)

--- test/python/transpiler/test_consolidate_blocks.py
+++ test/python/transpiler/test_consolidate_blocks.py
@@ -654,12 +654,13 @@ class TestConsolidateBlocks(QiskitTestCase):
         self.assertEqual({"unitary": 1}, res.count_ops())
         self.assertEqual(Operator.from_circuit(qc), Operator(res.data[0].operation.params[0]))
 
-    def test_collect_rzz(self):
+    @data(["rzz", "rx", "rz"], ["rzz", "rx", "rz", "cz"])
+    def test_collect_rzz(self, basis_gates):
         """Collect blocks with RZZ gates."""
         qc = QuantumCircuit(2)
         qc.rzz(0.1, 0, 1)
         qc.rzz(0.2, 0, 1)
-        consolidate_pass = ConsolidateBlocks(basis_gates=["rzz", "rx", "rz"])
+        consolidate_pass = ConsolidateBlocks(basis_gates=basis_gates)
         res = consolidate_pass(qc)
         self.assertEqual({"unitary": 1}, res.count_ops())
         self.assertEqual(Operator.from_circuit(qc), Operator(res.data[0].operation.params[0]))
