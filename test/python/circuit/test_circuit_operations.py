78063c39c8d58308b67ca5eb274459c0ccc1b03a
Raynel Sanchez <87539502+raynelfss@users.noreply.github.com>
2025-03-17 14:14:06 -0400

    Fix: Lint error

--- test/python/circuit/test_circuit_operations.py
+++ test/python/circuit/test_circuit_operations.py
@@ -869,14 +869,14 @@ class TestCircuitOperations(QiskitTestCase):
         self.assertEqual(len(circuit.cregs), 2)  # Now two cregs
         self.assertTrue(all(len(reg) == 1 for reg in circuit.cregs))  # All of length 1
         # Check that no name is the same
-        self.assertEqual(len(set(reg.name for reg in circuit.cregs)), 2)
+        self.assertEqual(len({reg.name for reg in circuit.cregs}), 2)
 
         # Third call should also create a new register
         circuit.measure_all()
         self.assertEqual(len(circuit.cregs), 3)  # Now two cregs
         self.assertTrue(all(len(reg) == 1 for reg in circuit.cregs))  # All of length 1
         # Check that no name is the same
-        self.assertEqual(len(set(reg.name for reg in circuit.cregs)), 3)
+        self.assertEqual(len({reg.name for reg in circuit.cregs}), 3)
 
     def test_remove_final_measurements(self):
         """Test remove_final_measurements
