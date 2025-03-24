bbb0324544f81b16b65bda9b8a828387c8857a8c
Kevin Hartman <kevin@hart.mn>
2025-03-24 17:27:15 -0400

    Add more plumbing for Expr.

--- crates/circuit/src/classical/expr.rs
+++ crates/circuit/src/classical/expr.rs
@@ -13,8 +13,11 @@
 use crate::bit::{ClassicalRegister, ShareableClbit};
 use crate::classical::Type;
 use crate::duration::Duration;
+use crate::imports::UUID;
 use pyo3::prelude::*;
-use pyo3::IntoPyObjectExt;
+use pyo3::types::IntoPyDict;
+use pyo3::{intern, BoundObject, IntoPyObjectExt};
+use uuid::Uuid;
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum Expr {
@@ -25,6 +28,40 @@ pub enum Expr {
     Var(Var),
     Stretch(Stretch),
     Index(Box<Index>),
+}
+
+impl Expr {
+    fn is_const(&self) -> bool {
+        match self {
+            Expr::Unary(u) => u.constant,
+            Expr::Binary(b) => b.constant,
+            Expr::Cast(c) => c.constant,
+            Expr::Value(_) => true,
+            Expr::Var(_) => false,
+            Expr::Stretch(_) => true,
+            Expr::Index(i) => i.constant,
+        }
+    }
+
+    fn ty(&self) -> Type {
+        match self {
+            Expr::Unary(u) => u.ty,
+            Expr::Binary(b) => b.ty,
+            Expr::Cast(c) => c.ty,
+            Expr::Value(v) => match v {
+                Value::Duration(_) => Type::Duration,
+                Value::Float { ty, .. } => *ty,
+                Value::Uint { ty, .. } => *ty,
+            },
+            Expr::Var(v) => match v {
+                Var::Standalone { ty, .. } => *ty,
+                Var::Bit { .. } => Type::Bool,
+                Var::Register { ty, .. } => *ty,
+            },
+            Expr::Stretch(_) => Type::Duration,
+            Expr::Index(i) => i.ty,
+        }
+    }
 }
 
 impl From<Unary> for Expr {
@@ -69,10 +106,14 @@ impl From<Index> for Expr {
     }
 }
 
-/// The base class of all Python expressions.
+/// Root base class of all nodes in the expression tree.  The base case should never be
+/// instantiated directly.
 ///
-/// We store a [ExprKind] enum inside to quickly determine which Python expression
-/// we're looking at during extraction.
+/// This must not be subclassed by users; subclasses form the internal data of the representation of
+/// expressions, and it does not make sense to add more outside of Qiskit library code.
+///
+/// All subclasses are responsible for setting their ``type`` attribute in their ``__init__``, and
+/// should not call the parent initializer."""
 #[pyclass(
     eq,
     hash,
@@ -82,7 +123,28 @@ impl From<Index> for Expr {
     module = "qiskit._accelerate.circuit"
 )]
 #[derive(PartialEq, Clone, Copy, Debug, Hash)]
-struct PyExpr(ExprKind);
+struct PyExpr(ExprKind); // ExprKind is used for fast extraction from Python
+
+#[pymethods]
+impl PyExpr {
+    /// Call the relevant ``visit_*`` method on the given :class:`ExprVisitor`.  The usual entry
+    /// point for a simple visitor is to construct it, and then call :meth:`accept` on the root
+    /// object to be visited.  For example::
+    ///
+    ///     expr = ...
+    ///     visitor = MyVisitor()
+    ///     visitor.accept(expr)
+    ///
+    /// Subclasses of :class:`Expr` should override this to call the correct virtual method on the
+    /// visitor.  This implements double dispatch with the visitor."""
+    /// return visitor.visit_generic(self)
+    fn accept<'py>(
+        slf: PyRef<'py, Self>,
+        visitor: &Bound<'py, PyAny>,
+    ) -> PyResult<Bound<'py, PyAny>> {
+        visitor.call_method1(intern!(visitor.py(), "visit_generic"), (slf,))
+    }
+}
 
 #[repr(u8)]
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
@@ -158,40 +220,6 @@ impl<'py> FromPyObject<'py> for Expr {
     }
 }
 
-impl Expr {
-    fn is_const(&self) -> bool {
-        match self {
-            Expr::Unary(u) => u.constant,
-            Expr::Binary(b) => b.constant,
-            Expr::Cast(c) => c.constant,
-            Expr::Value(_) => true,
-            Expr::Var(_) => false,
-            Expr::Stretch(_) => true,
-            Expr::Index(i) => i.constant,
-        }
-    }
-
-    fn ty(&self) -> Type {
-        match self {
-            Expr::Unary(u) => u.ty,
-            Expr::Binary(b) => b.ty,
-            Expr::Cast(c) => c.ty,
-            Expr::Value(v) => match v {
-                Value::Duration(_) => Type::Duration,
-                Value::Float { ty, .. } => *ty,
-                Value::Uint { ty, .. } => *ty,
-            },
-            Expr::Var(v) => match v {
-                Var::Standalone { ty, .. } => *ty,
-                Var::Bit { .. } => Type::Bool,
-                Var::Register { ty, .. } => *ty,
-            },
-            Expr::Stretch(_) => Type::Duration,
-            Expr::Index(i) => i.ty,
-        }
-    }
-}
-
 #[derive(Clone, Debug, PartialEq)]
 pub struct Unary {
     op: UnaryOp,
@@ -201,13 +229,31 @@ pub struct Unary {
 }
 
 #[repr(u8)]
-#[derive(Copy, Clone, Debug, PartialEq)]
+#[derive(Copy, Hash, Clone, Debug, PartialEq)]
+#[pyclass(
+    eq,
+    hash,
+    frozen,
+    name = "Unary",
+    module = "qiskit._accelerate.circuit"
+)]
 pub enum UnaryOp {
     BitNot = 1,
     LogicNot = 2,
 }
 
-#[pyclass(eq, extends = PyExpr, name = "Unary", module = "qiskit._accelerate.circuit")]
+#[pymethods]
+impl UnaryOp {
+    fn __str__(&self) -> String {
+        format!("Unary.Op.{:?}", self)
+    }
+
+    fn __repr__(&self) -> String {
+        format!("Unary.Op.{:?}", self)
+    }
+}
+
+#[pyclass(eq, extends = PyExpr, name = "Unary", module = "qiskit._accelerate.circuit.classical")]
 #[derive(PartialEq, Clone, Debug)]
 struct PyUnary(Unary);
 
@@ -216,6 +262,13 @@ impl PyUnary {
     #[getter]
     fn get_type(&self, py: Python) -> PyResult<Py<PyAny>> {
         self.0.ty.clone().into_py_any(py)
+    }
+
+    fn accept<'py>(
+        slf: PyRef<'py, Self>,
+        visitor: &Bound<'py, PyAny>,
+    ) -> PyResult<Bound<'py, PyAny>> {
+        visitor.call_method1(intern!(visitor.py(), "visit_unary"), (slf,))
     }
 }
 
@@ -230,6 +283,13 @@ pub struct Binary {
 
 #[repr(u8)]
 #[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+#[pyclass(
+    eq,
+    hash,
+    frozen,
+    name = "Unary",
+    module = "qiskit._accelerate.circuit.classical.Binary"
+)]
 pub enum BinaryOp {
     BitAnd = 1,
     BitOr = 2,
@@ -250,7 +310,18 @@ pub enum BinaryOp {
     Div = 17,
 }
 
-#[pyclass(eq, extends = PyExpr, name = "Binary", module = "qiskit._accelerate.circuit")]
+#[pymethods]
+impl BinaryOp {
+    fn __str__(&self) -> String {
+        format!("Binary.Op.{:?}", self)
+    }
+
+    fn __repr__(&self) -> String {
+        format!("Binary.Op.{:?}", self)
+    }
+}
+
+#[pyclass(eq, extends = PyExpr, name = "Binary", module = "qiskit._accelerate.circuit.classical")]
 #[derive(PartialEq, Clone, Debug)]
 struct PyBinary(Binary);
 
@@ -259,6 +330,13 @@ impl PyBinary {
     #[getter]
     fn get_type(&self, py: Python) -> PyResult<Py<PyAny>> {
         self.0.ty.clone().into_py_any(py)
+    }
+
+    fn accept<'py>(
+        slf: PyRef<'py, Self>,
+        visitor: &Bound<'py, PyAny>,
+    ) -> PyResult<Bound<'py, PyAny>> {
+        visitor.call_method1(intern!(visitor.py(), "visit_binary"), (slf,))
     }
 }
 
@@ -270,7 +348,9 @@ pub struct Cast {
     implicit: bool,
 }
 
-#[pyclass(eq, extends = PyExpr, name = "Cast", module = "qiskit._accelerate.circuit")]
+/// A cast from one type to another, implied by the use of an expression in a different
+/// context.
+#[pyclass(eq, extends = PyExpr, name = "Cast", module = "qiskit._accelerate.circuit.classical")]
 #[derive(PartialEq, Clone, Debug)]
 struct PyCast(Cast);
 
@@ -279,6 +359,13 @@ impl PyCast {
     #[getter]
     fn get_type(&self, py: Python) -> PyResult<Py<PyAny>> {
         self.0.ty.clone().into_py_any(py)
+    }
+
+    fn accept<'py>(
+        slf: PyRef<'py, Self>,
+        visitor: &Bound<'py, PyAny>,
+    ) -> PyResult<Bound<'py, PyAny>> {
+        visitor.call_method1(intern!(visitor.py(), "visit_cast"), (slf,))
     }
 }
 
@@ -289,7 +376,7 @@ pub enum Value {
     Uint { raw: u64, ty: Type },
 }
 
-#[pyclass(eq, extends = PyExpr, name = "Value", module = "qiskit._accelerate.circuit")]
+#[pyclass(eq, extends = PyExpr, name = "Value", module = "qiskit._accelerate.circuit.classical")]
 #[derive(PartialEq, Clone, Debug)]
 struct PyValue(Value);
 
@@ -301,6 +388,13 @@ impl PyValue {
             Value::Duration(_) => Type::Duration.into_py_any(py),
             Value::Float { ty, .. } | Value::Uint { ty, .. } => ty.into_py_any(py),
         }
+    }
+
+    fn accept<'py>(
+        slf: PyRef<'py, Self>,
+        visitor: &Bound<'py, PyAny>,
+    ) -> PyResult<Bound<'py, PyAny>> {
+        visitor.call_method1(intern!(visitor.py(), "visit_value"), (slf,))
     }
 }
 
@@ -320,8 +414,56 @@ pub enum Var {
     },
 }
 
+/// A classical variable.
+///
+/// These variables take two forms: a new-style variable that owns its storage location and has an
+/// associated name; and an old-style variable that wraps a :class:`.Clbit` or
+/// :class:`.ClassicalRegister` instance that is owned by some containing circuit.  In general,
+/// construction of variables for use in programs should use :meth:`Var.new` or
+/// :meth:`.QuantumCircuit.add_var`.
+///
+/// Variables are immutable after construction, so they can be used as dictionary keys."""
+#[pyclass(eq, hash, frozen, extends = PyExpr, name = "Var", module = "qiskit._accelerate.circuit.classical")]
+#[derive(PartialEq, Clone, Debug, Hash)]
+struct PyVar(Var);
+
 #[pymethods]
 impl PyVar {
+    #[new]
+    #[pyo3(signature = (var, ty, *, name = None), text_signature="(var, type, *, name=None)")]
+    fn new0(var: &Bound<PyAny>, ty: Type, name: Option<String>) -> PyResult<Py<Self>> {
+        let v = if let Some(name) = name {
+            Var::Standalone {
+                uuid: var.getattr(intern!(var.py(), "int"))?.extract()?,
+                name,
+                ty,
+            }
+        } else if let Ok(register) = var.extract::<ClassicalRegister>() {
+            Var::Register { register, ty }
+        } else {
+            Var::Bit {
+                bit: var.extract()?,
+            }
+        };
+        Py::new(var.py(), (PyVar(v), PyExpr(ExprKind::Var)))
+    }
+
+    /// Generate a new named variable that owns its own backing storage.
+    #[classmethod]
+    fn new(cls: &Bound<'_, pyo3::types::PyType>, name: String, ty: Type) -> PyResult<Py<Self>> {
+        Py::new(
+            cls.py(),
+            (
+                PyVar(Var::Standalone {
+                    uuid: Uuid::new_v4().as_u128(),
+                    name,
+                    ty,
+                }),
+                PyExpr(ExprKind::Var),
+            ),
+        )
+    }
+
     #[getter]
     fn get_type(&self, py: Python) -> PyResult<Py<PyAny>> {
         match self.0 {
@@ -329,11 +471,65 @@ impl PyVar {
             Var::Standalone { ty, .. } | Var::Register { ty, .. } => ty.into_py_any(py),
         }
     }
-}
 
-#[pyclass(eq, hash, frozen, extends = PyExpr, name = "Var", module = "qiskit._accelerate.circuit")]
-#[derive(PartialEq, Clone, Debug, Hash)]
-struct PyVar(Var);
+    /// A reference to the backing data storage of the :class:`Var` instance.  When lifting
+    /// old-style :class:`.Clbit` or :class:`.ClassicalRegister` instances into a :class:`Var`,
+    /// this is exactly the :class:`.Clbit` or :class:`.ClassicalRegister`.  If the variable is a
+    /// new-style classical variable (one that owns its own storage separate to the old
+    /// :class:`.Clbit`/:class:`.ClassicalRegister` model), this field will be a :class:`~uuid.UUID`
+    /// to uniquely identify it."""
+    #[getter]
+    fn get_var(&self, py: Python) -> PyResult<Py<PyAny>> {
+        match &self.0 {
+            Var::Standalone { uuid, .. } => {
+                let kwargs = [("int", uuid)].into_py_dict(py)?;
+                Ok(UUID.get_bound(py).call((), Some(&kwargs))?.unbind())
+            }
+            Var::Bit { bit } => bit.into_py_any(py),
+            Var::Register { register, .. } => register.into_py_any(py),
+        }
+    }
+
+    /// The name of the variable.  This is required to exist if the backing :attr:`var` attribute
+    /// is a :class:`~uuid.UUID`, i.e. if it is a new-style variable, and must be ``None`` if it is
+    /// an old-style variable."""
+    #[getter]
+    fn get_name(&self, py: Python) -> PyResult<Py<PyAny>> {
+        match &self.0 {
+            Var::Standalone { name, .. } => Some(name),
+            Var::Bit { .. } | Var::Register { .. } => None,
+        }
+        .into_py_any(py)
+    }
+
+    /// Whether this :class:`Var` is a standalone variable that owns its storage
+    /// location, if applicable. If false, this is a wrapper :class:`Var` around a
+    /// pre-existing circuit object."""
+    #[getter]
+    fn get_standalone(&self) -> bool {
+        match self.0 {
+            Var::Standalone { .. } => true,
+            Var::Bit { .. } | Var::Register { .. } => false,
+        }
+    }
+
+    fn accept<'py>(
+        slf: PyRef<'py, Self>,
+        visitor: &Bound<'py, PyAny>,
+    ) -> PyResult<Bound<'py, PyAny>> {
+        visitor.call_method1(intern!(visitor.py(), "visit_var"), (slf,))
+    }
+
+    fn __copy__(slf: PyRef<Self>) -> PyRef<Self> {
+        // I am immutable...
+        slf
+    }
+
+    fn __deepcopy__<'py>(slf: PyRef<'py, Self>, _memo: Bound<'py, PyAny>) -> PyRef<'py, Self> {
+        // ... as are all my constituent parts.
+        slf
+    }
+}
 
 #[derive(Clone, Debug, PartialEq, Hash)]
 pub struct Stretch {
@@ -341,15 +537,61 @@ pub struct Stretch {
     name: String,
 }
 
-#[pyclass(eq, hash, frozen, extends = PyExpr, name = "Stretch", module = "qiskit._accelerate.circuit")]
+#[pyclass(eq, hash, frozen, extends = PyExpr, name = "Stretch", module = "qiskit._accelerate.circuit.classical")]
 #[derive(PartialEq, Clone, Debug, Hash)]
 struct PyStretch(Stretch);
 
 #[pymethods]
 impl PyStretch {
+    #[new]
+    fn new0(var: &Bound<PyAny>, name: String) -> PyResult<Py<Self>> {
+        Py::new(
+            var.py(),
+            (
+                PyStretch(Stretch {
+                    uuid: var.getattr(intern!(var.py(), "int"))?.extract()?,
+                    name,
+                }),
+                PyExpr(ExprKind::Stretch),
+            ),
+        )
+    }
+
+    /// Generate a new named stretch variable.
+    #[classmethod]
+    fn new(cls: &Bound<'_, pyo3::types::PyType>, name: String) -> PyResult<Py<Self>> {
+        Py::new(
+            cls.py(),
+            (
+                PyStretch(Stretch {
+                    uuid: Uuid::new_v4().as_u128(),
+                    name,
+                }),
+                PyExpr(ExprKind::Stretch),
+            ),
+        )
+    }
+
     #[getter]
     fn get_type(&self, py: Python) -> PyResult<Py<PyAny>> {
         Type::Duration.into_py_any(py)
+    }
+
+    fn accept<'py>(
+        slf: PyRef<'py, Self>,
+        visitor: &Bound<'py, PyAny>,
+    ) -> PyResult<Bound<'py, PyAny>> {
+        visitor.call_method1(intern!(visitor.py(), "visit_stretch"), (slf,))
+    }
+
+    fn __copy__(slf: PyRef<Self>) -> PyRef<Self> {
+        // I am immutable...
+        slf
+    }
+
+    fn __deepcopy__<'py>(slf: PyRef<'py, Self>, _memo: Bound<'py, PyAny>) -> PyRef<'py, Self> {
+        // ... as are all my constituent parts.
+        slf
     }
 }
 
@@ -361,7 +603,7 @@ pub struct Index {
     constant: bool,
 }
 
-#[pyclass(eq, extends = PyExpr, name = "Index", module = "qiskit._accelerate.circuit")]
+#[pyclass(eq, extends = PyExpr, name = "Index", module = "qiskit._accelerate.circuit.classical")]
 #[derive(PartialEq, Clone, Debug)]
 struct PyIndex(Index);
 
@@ -370,6 +612,13 @@ impl PyIndex {
     #[getter]
     fn get_type(&self, py: Python) -> PyResult<Py<PyAny>> {
         self.0.ty.clone().into_py_any(py)
+    }
+
+    fn accept<'py>(
+        slf: PyRef<'py, Self>,
+        visitor: &Bound<'py, PyAny>,
+    ) -> PyResult<Bound<'py, PyAny>> {
+        visitor.call_method1(intern!(visitor.py(), "visit_index"), (slf,))
     }
 }
 
