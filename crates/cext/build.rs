ae3628419c003c8e3fe74c588f04b52e161ce1b4
Julien Gacon <jules.gacon@googlemail.com>
2025-03-28 16:56:40 +0100

    store pyo3 build config during compilation
    maybe this should be set behind a flag that we enable if we detect  we're running on windows, to avoid the dependency on unix

---
+++ crates/cext/build.rs
@@ -0,0 +1,29 @@
+// This code is part of Qiskit.
+//
+// (C) Copyright IBM 2025
+//
+// This code is licensed under the Apache License, Version 2.0. You may
+// obtain a copy of this license in the LICENSE.txt file in the root directory
+// of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+//
+// Any modifications or derivative works of this code must retain this
+// copyright notice, and modified files need to carry a notice indicating
+// that they have been altered from the originals.
+
+use pyo3_build_config;
+
+fn main() {
+    // Get the Python library directory that PyO3 is using, and store it into a
+    // configuration file.
+    let interpreter_config = pyo3_build_config::get();
+    let python_lib_dir = format!(
+        "QISKIT_PYTHON_LIB_DIR={}\n",
+        interpreter_config.lib_dir.clone().unwrap_or("".to_string())
+    );
+    // This path is relative to the current file, i.e. we write into the root's target dir.
+    let pyo3_config_file = "../../target/qiskit_pyo3_python.config";
+    match ::std::fs::write(pyo3_config_file, python_lib_dir) {
+        Ok(_) => (),
+        Err(_) => println!("cargo:warning=Failed to write Python config."),
+    }
+}
