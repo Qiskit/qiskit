d1e3f951557b5d73088f8f2495e613c70ec46f98
Raynel Sanchez <87539502+raynelfss@users.noreply.github.com>
2025-04-03 18:26:00 -0400

    Initial: Move the source of truth of the `Target` gate map to Rust. - Create two parallel structs `GateMap` and `PropsMap` to represent the complex mapping happening in `GateMap` and achieve two things:     - Make the python side mapping no longer be exposed publicly.     - Make the mapping python independent and rebuildable upon request. - This commit is built on several branches that perform additions to the target and this branch will either be superceded or heavily rebased upon further review.

--- crates/accelerate/src/target_transpiler/gate_map.rs
+++ crates/accelerate/src/target_transpiler/gate_map.rs
@@ -10,18 +10,26 @@
 // copyright notice, and modified files need to carry a notice indicating
 // that they have been altered from the originals.
 
-use std::{ops::{Deref, DerefMut}, sync::OnceLock};
+use std::{
+    ops::{Deref, DerefMut},
+    sync::OnceLock,
+};
 
-use indexmap::IndexMap;
-use pyo3::{prelude::*, sync::OnceLockExt, types::{PyDict, PyTuple}};
 use super::{instruction_properties::InstructionProperties, Qargs};
+use indexmap::{map::IntoIter, IndexMap};
+use pyo3::{
+    exceptions::PyRuntimeError,
+    prelude::*,
+    sync::OnceLockExt,
+    types::{IntoPyDict, PyDict},
+};
 
-/// This structure is supposed to mimic the behavior of a [PyDict] without
-/// strictly requiring any python interaction whatsoever.
-#[derive(Debug, Clone, Default)]
-pub(crate) struct GateMap {
-    map: IndexMap<String, IndexMap<Qargs, Option<InstructionProperties>>>,
-    cached: OnceLock<Py<PyDict>>
+/// This structure is supposed to mimic the behavior of a [PyDict]
+/// for a `Target's` gate map without requiring to
+#[derive(Debug, Default)]
+pub struct GateMap {
+    map: IndexMap<String, PropsMap>,
+    cached: OnceLock<Py<PyDict>>,
 }
 
 impl GateMap {
@@ -29,6 +37,8 @@ impl GateMap {
         Default::default()
     }
 
+    // TODO: Remove once `Target` is built from rust.
+    #[allow(dead_code)]
     pub fn with_capacity(capacity: usize) -> Self {
         Self {
             map: IndexMap::with_capacity(capacity),
@@ -36,18 +46,56 @@ impl GateMap {
         }
     }
 
-    pub fn cached_py_dict(&self, py: Python) -> &PyDict {
-
+    pub fn cached_py_dict(&self, py: Python) -> &Py<PyDict> {
+        self.cached
+            .get_or_init(|| self.map.clone().into_py_dict(py).unwrap().into())
     }
+}
 
-    fn generate_py_dict(&self, py: Python) -> Bound<PyDict> {
-        let dict = PyDict::new(py);
+impl Clone for GateMap {
+    fn clone(&self) -> Self {
+        GateMap {
+            map: self.map.clone(),
+            cached: Default::default(),
+        }
+    }
+}
 
+impl<'py> IntoPyObject<'py> for GateMap {
+    type Target = PyDict;
+
+    type Output = Bound<'py, PyDict>;
+
+    type Error = PyErr;
+
+    fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
+        Ok(self.cached_py_dict(py).clone_ref(py).into_bound(py))
+    }
+}
+
+impl<'py, 'a> IntoPyObject<'py> for &'a GateMap {
+    type Target = PyDict;
+
+    type Output = Borrowed<'a, 'py, PyDict>;
+
+    type Error = PyErr;
+
+    fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
+        Ok(self.cached_py_dict(py).bind_borrowed(py))
+    }
+}
+
+impl<'py> FromPyObject<'py> for GateMap {
+    fn extract_bound(ob: &Bound<'py, PyAny>) -> PyResult<Self> {
+        Ok(Self {
+            map: ob.extract()?,
+            cached: ob.downcast::<PyDict>()?.clone().unbind().into(),
+        })
     }
 }
 
 impl Deref for GateMap {
-    type Target = IndexMap<String, IndexMap<Qargs, Option<InstructionProperties>>>;
+    type Target = IndexMap<String, PropsMap>;
 
     fn deref(&self) -> &Self::Target {
         &self.map
@@ -56,18 +104,20 @@ impl Deref for GateMap {
 
 impl DerefMut for GateMap {
     fn deref_mut(&mut self) -> &mut Self::Target {
+        self.cached.take();
         &mut self.map
     }
 }
 
-
-#[derive(Debug, Clone, Default)]
-pub(crate) struct PropsMap {
+#[derive(Debug, Default)]
+pub struct PropsMap {
     map: IndexMap<Qargs, Option<InstructionProperties>>,
-    cached: OnceLock<Py<PyDict>>
+    cached: OnceLock<Py<PyDict>>,
 }
 
 impl PropsMap {
+    // TODO: Remove once `Target` can be created python free.
+    #[allow(dead_code)]
     pub fn new() -> Self {
         Default::default()
     }
@@ -80,27 +130,99 @@ impl PropsMap {
     }
 
     pub fn cached_py_dict(&self, py: Python) -> PyResult<&Py<PyDict>> {
-        let res = self.cached.get_or_init_py_attached(py, || self.generate_py_dict(py).unwrap());
+        let res = self
+            .cached
+            .get_or_init_py_attached(py, || self.generate_py_dict(py).unwrap());
         let res_bound = res.bind(py);
+        let res_bound_len = res_bound.len();
         if res_bound.len() == self.map.len() {
             Ok(res)
         } else if res_bound.len() < self.map.len() {
-            let len_diff = self.map.len() - res_bound.len();
-            for idx in (self.map.len()-len_diff)..self.map.len() {
-                let (key,val) = self.map
-                res_bound.set_item(key, value)
+            for idx in res_bound_len..self.map.len() {
+                let (key, val) = self.map.get_index(idx).unwrap();
+                res_bound.set_item(key.as_ref(), val.clone())?;
             }
             Ok(res)
+        } else {
+            Err(PyRuntimeError::new_err("The python and rust counterparts of this gatemap are out of sync, perhaps something modified it?"))
         }
-
     }
 
     fn generate_py_dict(&self, py: Python) -> PyResult<Py<PyDict>> {
         let dict = PyDict::new(py);
         for (qargs, inst_prop) in self.map.iter() {
-            dict.set_item(qargs.map(|items| PyTuple::new(py, items)).transpose()?, inst_prop.clone())?;
+            dict.set_item(qargs.as_ref(), inst_prop.clone())?;
         }
         Ok(dict.unbind())
+    }
+}
+
+impl IntoIterator for PropsMap {
+    type Item = (Qargs, Option<InstructionProperties>);
+
+    type IntoIter = IntoIter<Qargs, Option<InstructionProperties>>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.map.into_iter()
+    }
+}
+
+impl From<IndexMap<Qargs, Option<InstructionProperties>>> for PropsMap {
+    fn from(value: IndexMap<Qargs, Option<InstructionProperties>>) -> Self {
+        Self {
+            map: value,
+            ..Default::default()
+        }
+    }
+}
+
+impl Clone for PropsMap {
+    fn clone(&self) -> Self {
+        PropsMap {
+            map: self.map.clone(),
+            cached: Default::default(),
+        }
+    }
+}
+
+impl<'py> IntoPyObject<'py> for PropsMap {
+    type Target = PyDict;
+
+    type Output = Bound<'py, PyDict>;
+
+    type Error = PyErr;
+
+    fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
+        Ok(self.cached_py_dict(py)?.clone_ref(py).into_bound(py))
+    }
+}
+
+impl<'py, 'a> IntoPyObject<'py> for &'a PropsMap {
+    type Target = PyDict;
+
+    type Output = Borrowed<'a, 'py, PyDict>;
+
+    type Error = PyErr;
+
+    fn into_pyobject(self, py: Python<'py>) -> Result<Self::Output, Self::Error> {
+        Ok(self.cached_py_dict(py)?.bind_borrowed(py))
+    }
+}
+
+impl<'py> FromPyObject<'py> for PropsMap {
+    fn extract_bound(ob: &Bound<'py, PyAny>) -> PyResult<Self> {
+        let ob = ob.downcast::<PyDict>()?;
+        let mut map: IndexMap<Qargs, Option<InstructionProperties>> =
+            IndexMap::with_capacity(ob.len() - 1);
+        for (key, val) in ob.iter() {
+            let key: Qargs = key.extract()?;
+            let val: Option<InstructionProperties> = val.extract()?;
+            map.insert(key, val);
+        }
+        Ok(Self {
+            map,
+            cached: ob.clone().unbind().into(),
+        })
     }
 }
 
