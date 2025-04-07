eefcedc8011c7e7791e4ff51cf3a16fb6a24c118
Abde Manaaf Ghadiali <abdemanaaf.ghadiali.1998@gmail.com>
2025-04-07 12:51:46 -0400

    Issue #14188 - Added Test Cases for testing is_entangled method

--- test/python/quantum_info/states/test_statevector.py
+++ test/python/quantum_info/states/test_statevector.py
@@ -260,6 +260,31 @@ class TestStatevector(QiskitTestCase):
             state = Statevector(self.rand_vec(4, normalize=True))
             self.assertTrue(state.is_valid())
 
+    def test_is_entangled(self):
+        """Test is_entangled method."""
+        qc_2 = QuantumCircuit(2)
+        qc_3 = QuantumCircuit(3)
+
+        qc_2.h(0)
+        qc_2.cx(0, 1)
+
+        qc_3.h(0)
+        qc_3.cx(0, 1)
+        qc_3.cx(0, 2)
+
+        ne_state_2 = Statevector.from_label("++")
+        ne_state_3 = Statevector.from_label("+++")
+
+        self.assertFalse(ne_state_2.is_entangled())
+        self.assertFalse(ne_state_3.is_entangled())
+
+        e_state_2 = Statevector.from_instruction(qc_2)
+        e_state_3 = Statevector.from_instruction(qc_3)
+
+        self.assertTrue(e_state_2.is_entangled())
+        self.assertTrue(e_state_3.is_entangled())
+
+
     def test_to_operator(self):
         """Test to_operator method for returning projector."""
         for _ in range(10):
