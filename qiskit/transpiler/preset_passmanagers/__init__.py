ef161c0a60306d84a48b96101cca5276b546bd63
Alexander Ivrii <alexi@il.ibm.com>
2025-04-20 11:47:59 +0300

    adding generate_discrete_passmanager to init files

--- qiskit/transpiler/preset_passmanagers/__init__.py
+++ qiskit/transpiler/preset_passmanagers/__init__.py
@@ -69,7 +69,10 @@ from .common import (
     generate_translation_passmanager,
     generate_scheduling,
 )
-from .generate_preset_pass_manager import generate_preset_pass_manager
+from .generate_preset_pass_manager import (
+    generate_preset_pass_manager,
+    generate_discrete_pass_manager,
+)
 from .level0 import level_0_pass_manager
 from .level1 import level_1_pass_manager
 from .level2 import level_2_pass_manager
