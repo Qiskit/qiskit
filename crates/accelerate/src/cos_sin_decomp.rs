c003ad22cd946c6e9ba3c9c4573166e13e7ae422
Alexander Ivrii <alexi@il.ibm.com>
2025-04-22 17:25:26 +0300

    Finishing the implementation of the cos-sin decomposition

--- crates/accelerate/src/cos_sin_decomp.rs
+++ crates/accelerate/src/cos_sin_decomp.rs
@@ -28,11 +28,45 @@ type CosSinDecompReturn = (
     DMatrix<Complex64>,
     DMatrix<Complex64>,
     DMatrix<Complex64>,
-    DMatrix<f64>,
-    DMatrix<f64>,
+    Vec<f64>,
 );
 
-/// Comute the cosine-sin decomposition of a Unitary matrix
+/// Reverses rows in-place.
+fn reverse_rows(mat: &mut DMatrix<Complex64>) {
+    // Row-wise reverse u
+    let mut start = 0;
+    let mut end = mat.nrows() - 1;
+    while start < end {
+        mat.swap_rows(start, end);
+        start += 1;
+        end -= 1;
+    }
+}
+
+/// Reverses columns in-place.
+fn reverse_columns(mat: &mut DMatrix<Complex64>) {
+    // Row-wise reverse u
+    let mut start = 0;
+    let mut end = mat.ncols() - 1;
+    while start < end {
+        mat.swap_columns(start, end);
+        start += 1;
+        end -= 1;
+    }
+}
+
+/// Reverses elements in-place.
+fn reverse_vec(vec: &mut DVector<f64>) {
+    let mut start = 0;
+    let mut end = vec.len() - 1;
+    while start < end {
+        vec.swap_rows(start, end);
+        start += 1;
+        end -= 1;
+    }
+}
+
+/// Computes the cosine-sin decomposition (CSD) of a unitary matrix.
 ///
 /// # Args
 ///
@@ -40,16 +74,22 @@ type CosSinDecompReturn = (
 ///
 /// # Returns
 ///
-/// The tuple (l0, l1, r0, r1, c, s) which represents the decomposition
+/// The tuple `(l0, l1, r0, r1, theta)` representing the CSD:
 ///
+/// ```text
 /// [l0   ] [c -s] [r0   ]
 /// [   l1] [s  c] [   r1]
+/// ```
 ///
-/// where l0, l1, r0 and r1 are unitaries of equal size, c and s are diagonal
-/// matrices with non-negative entries, the diagonal entries of c are in
-/// non-decreasimg order, and
+/// - `l0`, `l1`, `r0`, and `r1` are unitaries of equal size,
+/// - `theta` is the array of angles, with each angle in in the interval :math:`[0, \pi/2]`.
 ///
-/// c^2 + s^2 = I
+/// The diagonal matrices `c` and `s` contain the cosines and sines of angles in `theta`,
+/// respectively. In particular, `c^2 + s^2 = I`.
+///
+/// Furthermore, the angles in `theta` are sorted in descending order, so:
+/// - cosines are in ascending order,
+/// - sines are in descending order.
 pub fn cos_sin_decomposition(u: DMatrix<Complex64>) -> CosSinDecompReturn {
     let shape = u.shape();
     let n = shape.0 / 2;
@@ -58,61 +98,56 @@ pub fn cos_sin_decomposition(u: DMatrix<Complex64>) -> CosSinDecompReturn {
     // Upper right corner
     let u01 = u.view((0, n), (n, n));
     // Lower left corner
-    let u10 = u.view((n, 0), (n, n)).to_owned();
+    let u10 = u.view((n, 0), (n, n));
     // Lower right corner
     let u11 = u.view((n, n), (n, n));
 
+    // Apply SVD to u00
     let svd = u00.svd(true, true);
     let mut l0 = svd.u.unwrap();
-    // Row-wise reverse u
-    let mut start = 0;
-    let mut end = n - 1;
-    while start < end {
-        l0.swap_rows(start, end);
-        start += 1;
-        end -= 1;
-    }
-    let mut r0_dag = svd.v_t.unwrap();
-    // Row-wise reverse v
-    start = 0;
-    end = 0;
-    while start < end {
-        r0_dag.swap_rows(start, end);
-        start += 1;
-        end -= 1;
-    }
-    let singular_values = svd.singular_values;
-    let values_diag: DVector<f64> =
-        DVector::from_iterator(singular_values.len(), singular_values.iter().rev().copied());
-    let c = DMatrix::from_diagonal(&values_diag);
-    let r0 = r0_dag.adjoint();
+    let mut r0 = svd.v_t.unwrap();
+    let mut c: DVector<f64> = svd.singular_values.column(0).into_owned();
 
-    // Now u00 = l0 c r0; l0 and r0 are unitary, and c is diagonal with positive
-    // non-decreasing entries. Because u00 is a submatrix of a unitary matrix, its
-    // singular values (the entries of c) are all <= 1.
+    // We have u00 = l0 c r0, where l0 and r0 are unitary, and c is a diagonal matrix
+    // with positive non-decreasing entries. However, we want the entries of c to be
+    // in the ascending order instead. Fortunately, it is easy to modify l0, c, and r0,
+    // so that this becomes true.
+    reverse_rows(&mut r0);
+    reverse_columns(&mut l0);
+    reverse_vec(&mut c);
 
+    // Compute the angles theta. Because u00 is a submatrix of a unitary matrix, its
+    // singular values (the entries of c) are all <= 1. Due to floating-point precision
+    // errors, it is possible that some of the c-values are just a tiny bit larger than 1,
+    // so we correct for that.
+    let thetas: Vec<f64> = c.iter().map(|f| f.min(1.0).acos()).collect();
+
+    // Apply QR to u10_r0_dag.
+    // We have u10 r0* = l1 s, where l1 is unitary and S is upper-triangular.
+    // Equivalently, u10 = l1 s r0.
+    let r0_dag = r0.adjoint();
     let u10_r0_dag = u10 * r0_dag;
-    let qr = u10_r0_dag.qr();
+    let qr = u10_r0_dag.clone().qr();
     let mut l1 = qr.q();
     let mut s = qr.unpack_r();
-    // Now u10 r0* = l1 S; l1 is unitary, and S is upper triangular.
-    //
-    // Claim: S is diagonal.
+
+    // Claim: s is diagonal.
     // Proof: Since u is unitary, we have
     //     I = u00* u00 + u10* u10
-    //       = (l0 c r0)* (l0 c r0) + (l1 S r0)* (l1 S r0)
-    //       = r0* c l0* l0 c r0 + r0* S* l1* l1 S r0
-    //       = r0* c^2 r0 + r0* S* S r0
-    //       = r0* (c^2 + S* S) r0
+    //       = (l0 c r0)* (l0 c r0) + (l1 s r0)* (l1 s r0)
+    //       = r0* c* l0* l0 c r0 + r0* S* l1* l1 s r0
+    //       = r0* c* c r0 + r0* s* s r0
+    //       = r0* (c^2 + s* s) r0
     // So I = c^2 + (S* S), so (S* S) = I - c^2 is a diagonal matrix with non-
-    // increasing entries in the range [0,1). As S is upper triangular, this
-    // implies that S must be diagonal. (Proof by induction over the dimension n
-    // of S: consider the two cases S_00 = 0 and S_00 != 0 and reduce to the n-1
-    // case.)
-    //
-    // We want S to be real. This is not guaranteed, though since it is diagonal
-    // it can be made so by adjusting l1.
-    if s.iter().any(|x| x.im != 0.) {
+    // increasing entries in the range [0,1). As s is upper triangular, this
+    // implies that s must be diagonal. (Proof by induction over the dimension n
+    // of S: consider the two cases s_00 = 0 and s_00 != 0 and reduce to the n-1
+    // case. Note: it is important that the entries of s are in descending order).
+
+    // We want s to be real. This is not guaranteed, though it seems to be always
+    // true in practice. In either case, it can be made possible by suitable adjusting
+    // both S and l1 together.
+    if s.diagonal().iter().any(|x| x.im != 0.) {
         for j in 0..n {
             let z = s[(j, j)];
             let r = z.abs();
@@ -124,40 +159,38 @@ pub fn cos_sin_decomposition(u: DMatrix<Complex64>) -> CosSinDecompReturn {
         }
     }
 
-    // Now s is real and diagonal, and c^2 + S^2 = I
-    let mut s_real: DMatrix<f64> = s.map(|x| x.re);
-    // Make all entries in s_real non-negative.
+    // Now s is real and diagonal, and c^2 + s^2 = I.
+    let mut s: DVector<f64> = s.diagonal().map(|x| x.re);
+
+    // Additionally, adjust l1 and s so that all entries in s are non-negative.
+    // Again, this seems to be never needed in practice.
     for j in 0..n {
-        let val = s_real[(j, j)];
+        let val = s[j];
         if val < 0. {
-            s_real[(j, j)] = -val;
+            s[j] = -s[j];
             l1.column_mut(j).iter_mut().for_each(|x| *x = -(*x));
         }
     }
+
     // Finally compute r1, being careful not to divide by small things.
+    // r1 should satisfy u01 = -l0 s r1 and u11 = l1 c r1, with these two
+    // conditions being equivalent due to u being unitary.
     let mut r1: DMatrix<Complex64> = DMatrix::zeros(n, n);
-    let l0_adjoint = l0.adjoint();
-    let l1_adjoint = l1.adjoint();
+    let l0_dag_u01 = l0.adjoint() * u01;
+    let l1_dag_u11 = l1.adjoint() * u11;
     for i in 0..n {
-        let mut row = r1.row_mut(i);
-        if s_real[(i, i)] > c[(i, i)] {
-            let mut new_mat = &l0_adjoint * u01;
-            let mut new_row = new_mat.row_mut(i);
-            new_row.iter_mut().for_each(|x| *x = -(*x) / s[(i, i)]);
-            row.iter_mut()
-                .zip(new_row.into_iter())
-                .for_each(|(x, y)| *x = *y);
+        if s[i] > c[i] {
+            for j in 0..n {
+                r1[(i, j)] = -l0_dag_u01[(i, j)] / s[i];
+            }
         } else {
-            let mut new_mat = &l1_adjoint * u11;
-            let mut new_row = new_mat.row_mut(i);
-            new_row.iter_mut().for_each(|x| *x /= c[(i, i)]);
-            row.iter_mut()
-                .zip(new_row.into_iter())
-                .for_each(|(x, y)| *x = *y);
+            for j in 0..n {
+                r1[(i, j)] = l1_dag_u11[(i, j)] / c[i];
+            }
         }
     }
 
-    (l0.to_owned(), l1, r0.to_owned(), r1, c, s_real)
+    (l0, l1, r0, r1, thetas)
 }
 
 // TODO: Remove this function and all the python interface when QSD is ported to rust
@@ -169,7 +202,7 @@ pub fn cossin<'py>(py: Python<'py>, u: PyReadonlyArray2<Complex64>) -> PyResult<
     let res = cos_sin_decomposition(mat);
     Ok((
         (res.0.to_pyarray(py), res.1.to_pyarray(py)),
-        res.5.to_pyarray(py),
+        res.4.to_pyarray(py),
         (res.2.to_pyarray(py), res.3.to_pyarray(py)),
     )
         .into_pyobject(py)?
