b38c2a7acf62119536efecb75afbabf58df23fe6
Raynel Sanchez <87539502+raynelfss@users.noreply.github.com>
2025-04-28 16:45:25 -0400

    Add: `add_instruction` function

--- crates/cext/src/transpiler/target.rs
+++ crates/cext/src/transpiler/target.rs
@@ -10,8 +10,12 @@
 // copyright notice, and modified files need to carry a notice indicating
 // that they have been altered from the originals.
 
-use crate::pointers::const_ptr_as_ref;
-use qiskit_accelerate::target_transpiler::{InstructionProperties, Target};
+use crate::pointers::{const_ptr_as_ref, mut_ptr_as_ref};
+use qiskit_accelerate::{
+    nlayout::PhysicalQubit,
+    target_transpiler::{InstructionProperties, Qargs, Target},
+};
+use qiskit_circuit::operations::{Operation, Param, StandardGate};
 
 /// @ingroup QkTarget
 /// Construct a new Target with the given number of qubits
@@ -75,6 +79,82 @@ pub extern "C" fn qk_instruction_properties_new(
     )))
 }
 
-// #[no_mangle]
-// #[cfg(feature = "cbinding")]
-// pub unsafe extern "C" fn qk_target_add_instruction(target: *mut Target, operation: )
+/// Represents the mapping between qargs and `InstructionProperties`
+pub struct PropertyMap(Vec<(Qargs, Option<InstructionProperties>)>);
+
+pub extern "C" fn qk_propety_map_new() -> *const PropertyMap {
+    Box::into_raw(Box::new(PropertyMap(vec![])))
+}
+
+pub unsafe extern "C" fn qk_property_map_add(
+    property_map: *mut PropertyMap,
+    qargs: *const u32,
+    num_qubits: u32,
+    instruction_properties: *const InstructionProperties,
+) {
+    // SAFETY: Per documentation, the pointer is non-null and aligned.
+    let prop_map = unsafe { mut_ptr_as_ref(property_map) };
+    // SAFETY: Per the documentation the qubits pointer is an array of num_qubits elements
+    let qubits: Qargs = unsafe {
+        (0..num_qubits)
+            .map(|idx| PhysicalQubit(*qargs.wrapping_add(idx as usize)))
+            .collect()
+    };
+    // SAFETY: Per documentation, the pointer is non-null and aligned.
+    let instruction_properties = unsafe { const_ptr_as_ref(instruction_properties) };
+    prop_map.0.push((qubits, Some(*instruction_properties)));
+}
+
+#[no_mangle]
+#[cfg(feature = "cbinding")]
+pub unsafe extern "C" fn qk_target_add_instruction(
+    target: *mut Target,
+    operation: StandardGate,
+    params: *const f64,
+    property_map: *const PropertyMap,
+) {
+    // SAFETY: Per documentation, the pointer is non-null and aligned.
+    let target = unsafe { mut_ptr_as_ref(target) };
+
+    // SAFETY: Per documentation, the pointer is non-null and aligned.
+    let property_map = unsafe { const_ptr_as_ref(property_map) };
+    // SAFETY: Per the documentation the params pointers are arrays of num_params() elements.
+    let parsed_params: &[Param] = unsafe {
+        match operation.num_params() {
+            0 => &[],
+            1 => &[(*params.wrapping_add(0)).into()],
+            2 => &[
+                (*params.wrapping_add(0)).into(),
+                (*params.wrapping_add(1)).into(),
+            ],
+            3 => &[
+                (*params.wrapping_add(0)).into(),
+                (*params.wrapping_add(1)).into(),
+                (*params.wrapping_add(2)).into(),
+            ],
+            4 => &[
+                (*params.wrapping_add(0)).into(),
+                (*params.wrapping_add(1)).into(),
+                (*params.wrapping_add(2)).into(),
+                (*params.wrapping_add(3)).into(),
+            ],
+            // There are no standard gates that take > 4 params
+            _ => unreachable!(),
+        }
+    };
+    let props_map = if property_map.0.is_empty() {
+        None
+    } else {
+        Some(
+            property_map
+                .0
+                .iter()
+                .map(|(q, i)| (q.clone(), *i))
+                .collect(),
+        )
+    };
+
+    target
+        .add_instruction(operation.into(), parsed_params, None, props_map)
+        .unwrap();
+}
