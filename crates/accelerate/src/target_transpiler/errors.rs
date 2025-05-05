6c7586e902da73861ebe634b81e558ae561367f0
Raynel Sanchez <87539502+raynelfss@users.noreply.github.com>
2025-05-05 12:38:15 -0400

    Enable rust native `Target` creation. (#14228)
    * Add: Rust native `add_instruction` to `Target`
    - Add rust native way of adding an instruction to the `Target` without any need for Python.
    - Additions are guaranteed to be reflected in Rust and Python.
    
    * Add: rust native `update_instruction_properties`
    
    * Fix: Add Rust native testing for new methods.
    - Remove `PyResult` from the output of the rust native functions.
    - Add quotation marks to the output of error messages for gate names.
    - Add rust native unit testing for the new methods to make sure they're working properly.
    
    * Add: Use `this_error` to implement `Target` errors.
    - Use rustdoc format for docstrings
    - Removce generic type for property map in `add_instruction`.
    - Add `QargsRef::as_owned_qargs` to return an owned version of Qargs.
    
    * Update crates/accelerate/src/target_transpiler/errors.rs
    
    Co-authored-by: Matthew Treinish <mtreinish@kortar.org>
    
    * Fix: Address review comments.
    - Remove `as_owned_qargs`.
    - Use `String` instead of owned `Qargs` for `TargetError`.
    
    * Apply suggestions from code review
    
    Co-authored-by: Matthew Treinish <mtreinish@kortar.org>
    
    * Docs: Fix docstring formatting for `update` method
    
    ---------
    
    Co-authored-by: Matthew Treinish <mtreinish@kortar.org>

--- crates/accelerate/src/target_transpiler/errors.rs
+++ crates/accelerate/src/target_transpiler/errors.rs
@@ -10,25 +10,33 @@
 // copyright notice, and modified files need to carry a notice indicating
 // that they have been altered from the originals.
 
-use std::{error::Error, fmt::Display};
+use thiserror::Error;
 
-/// Error thrown when operation key is not present in the Target
-#[derive(Debug)]
-pub struct TargetKeyError {
-    pub message: String,
+/// A collection of the Errors possible in the [Target].
+#[derive(Debug, Error)]
+pub enum TargetError {
+    /// An invalid instruction name being queried into the [Target].
+    #[error["Provided instruction: '{0}' not in this Target."]]
+    InvalidKey(String),
+    /// An already existing instruction name being queried into the [Target].
+    #[error["Instruction '{0}' is already in the target."]]
+    AlreadyExists(String),
+    /// An attempt to add collection of qargs to the [Target] that does
+    /// not match the source instruction's number of qubits.
+    #[error["The number of qubits for {instruction} does not match the number of qubits in the properties dictionary: {arguments}."]]
+    QargsMismatch {
+        instruction: String,
+        arguments: String,
+    },
+    /// An attempt to query collection of qargs to the [Target] that are
+    /// not operated on by the specified instruction.
+    #[error["Provided qarg {arguments} not in this Target for '{instruction}'."]]
+    InvalidQargsKey {
+        instruction: String,
+        arguments: String,
+    },
+    /// An attempt to query collection of qargs to the [Target] that are
+    /// not operated on by any instruction.
+    #[error["{0} not in Target."]]
+    QargsWithoutInstruction(String),
 }
-
-impl TargetKeyError {
-    /// Initializes the new error
-    pub fn new_err(message: String) -> Self {
-        Self { message }
-    }
-}
-
-impl Display for TargetKeyError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.message)
-    }
-}
-
-impl Error for TargetKeyError {}
