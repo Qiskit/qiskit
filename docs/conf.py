e9831519be7049535e4878d2f7d1908ef097ac75
Elena Peña Tapia <epenatap@gmail.com>
2025-05-09 09:46:08 +0200

    Bump version and add prelude

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ author = "Qiskit Development Team"
 # The short X.Y version
 version = "1.4"
 # The full version, including alpha/beta/rc tags
-release = "1.4.2"
+release = "1.4.3"
 
 language = "en"
 
