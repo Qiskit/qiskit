30344100ff39079a9dccea750f0214031f157e9f
ShellyGarion <shelly@il.ibm.com>
2025-05-12 03:02:56 -0500

    update tollerance

--- test/python/synthesis/test_qsd_synthesis.py
+++ test/python/synthesis/test_qsd_synthesis.py
@@ -357,7 +357,7 @@ class TestQuantumShannonDecomposer(QiskitTestCase):
         qc2 = qsd.qs_decomposition(hidden_mat, opt_a2=False)
         cqc2 = transpile(qc2, basis_gates=["u", "cx"], optimization_level=0)
         op2 = Operator(qc2)
-        self.assertTrue(matrix_equal(hidden_op.data, op2.data, atol=1e-5))
+        self.assertTrue(matrix_equal(hidden_op.data, op2.data, atol=1e-3))
         self.assertLessEqual(
             cqc2.count_ops().get("cx", 0),
             2 * self._qsd_l2_cx_count(num_qubits - 1) + self._qsd_ucrz(num_qubits),
@@ -381,7 +381,7 @@ class TestQuantumShannonDecomposer(QiskitTestCase):
         qc2 = qsd.qs_decomposition(hidden_mat, opt_a2=False)
         cqc2 = transpile(qc2, basis_gates=["u", "cx"], optimization_level=0)
         op2 = Operator(qc2)
-        self.assertTrue(matrix_equal(hidden_op.data, op2.data, atol=1e-5))
+        self.assertTrue(matrix_equal(hidden_op.data, op2.data, atol=1e-3))
         self.assertLessEqual(
             cqc2.count_ops().get("cx", 0),
             2 * self._qsd_l2_cx_count(num_qubits - 1) + self._qsd_ucrz(num_qubits),
