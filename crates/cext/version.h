11ff3caa0344f223a3db45f5c7ae3eeaa0e8309a
velocityraptor7085 <drarchitch@gmail.com>
2025-05-21 19:50:36 +0530

    Added support for querying version information in the Qiskit C API.

--- crates/cext/version.h
+++ crates/cext/version.h
@@ -2,5 +2,6 @@
 #define QISKIT_VERSION_MINOR 1
 #define QISKIT_VERSION_PATCH 0
 
-#define QISKIT_VERSION_NUMERIC(M,m,p) ((M)<<16|(m)<<8|(p))
-#define QISKIT_VERSION (QISKIT_VERSION_MAJOR<<16 | QISKIT_VERSION_MINOR<<8 | QISKIT_VERSION_PATCH)
+#define QISKIT_VERSION_NUMERIC(M, m, p) ((M) << 16 | (m) << 8 | (p))
+#define QISKIT_VERSION                                                                             \
+    (QISKIT_VERSION_MAJOR << 16 | QISKIT_VERSION_MINOR << 8 | QISKIT_VERSION_PATCH)
