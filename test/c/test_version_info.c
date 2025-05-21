fc298f4f29b5e0cd337d78a2c5f5923465ba783b
velocityraptor7085 <drarchitch@gmail.com>
2025-05-21 18:45:44 +0530

    Added support for querying version information in the Qiskit C API.

--- test/c/test_version_info.c
+++ test/c/test_version_info.c
@@ -1,9 +1,9 @@
 #include "common.h"
 
 // Test that QISKIT_VERSION_MAJOR, _MINOR, _PATCH are defined and print them
-int test_version_macros()
-{
-    printf("Qiskit version: %d.%d.%d\n", QISKIT_VERSION_MAJOR, QISKIT_VERSION_MINOR, QISKIT_VERSION_PATCH);
+int test_version_macros() {
+    printf("Qiskit version: %d.%d.%d\n", QISKIT_VERSION_MAJOR, QISKIT_VERSION_MINOR,
+           QISKIT_VERSION_PATCH);
     if (QISKIT_VERSION_MAJOR < 0 || QISKIT_VERSION_MINOR < 0 || QISKIT_VERSION_PATCH < 0) {
         return EqualityError;
     }
@@ -11,8 +11,7 @@ int test_version_macros()
 }
 
 // Main test function
-int test_version_info()
-{
+int test_version_info() {
     int num_failed = 0;
     num_failed += RUN_TEST(test_version_macros);
 
