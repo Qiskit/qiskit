Qiskit/qiskit

commit a00279bc7d6005bfc5ea783cd1fe336784160518

Circuits with control flow now raise a proper error when `circuit_to_instruction` is called (#13921)
* Circuits with control flow now raise a proper error when circuit_to_instruction is called

* test

* cyclic import

* Update qiskit/converters/circuit_to_instruction.py

Co-authored-by: Elena Peña Tapia <57907331+ElePT@users.noreply.github.com>

---------

Co-authored-by: Elena Peña Tapia <57907331+ElePT@users.noreply.github.com>

qiskit/converters/circuit_to_instruction.py
@@ -11,6 +11,7 @@
 # that they have been altered from the originals.
 
 """Helper function for converting a circuit to an instruction."""
+from qiskit.circuit.controlflow.control_flow import ControlFlowOp
 from qiskit.exceptions import QiskitError
 from qiskit.circuit.instruction import Instruction
 from qiskit.circuit.quantumregister import QuantumRegister
@@ -83,6 +84,13 @@ def circuit_to_instruction(circuit, parameter_map=None, equivalence_library=None
             "Circuits with internal variables cannot yet be converted to instructions."
             " You may be able to use `QuantumCircuit.compose` to inline this circuit into another."
         )
+
+    for inst in circuit.data:
+        if isinstance(inst.operation, ControlFlowOp):
+            raise QiskitError(
+                f"Circuits with control flow operations ({type(inst.operation)}) "
+                "cannot be converted to an instruction."
+            )
 
     if parameter_map is None:
         parameter_dict = {p: p for p in circuit.parameters}

releasenotes/notes/fixes_11379-4c4d6d71213a1c8c.yaml
@@ -1,0 +1,5 @@
+---
+upgrade_circuits:
+  - |
+    Converting a quantum circuit to a gate with :func:`.converters.circuit_to_instruction` fails properly when given
+    circuit contains control flow instructions.

test/python/converters/test_circuit_to_instruction.py
@@ -143,6 +143,17 @@ class TestCircuitToInstruction(QiskitTestCase):
             QiskitError, circuit_to_instruction, qc, {theta: gamma, phi: phi, delta: delta}
         )
 
+    def test_control_flow_raises(self):
+        """Raising if a circuit to convert has control flow.
+        See https://github.com/Qiskit/qiskit/issues/11379"""
+        qc = QuantumCircuit(2)
+        qc.h(0)
+        qc.cx(0, 1)
+        qc.measure_all()
+        with qc.if_test((qc.clbits[0], 0)):
+            qc.x(0)
+        self.assertRaises(QiskitError, circuit_to_instruction, qc)
+
     def test_parameter_map(self):
         """Verify alternate parameter specification"""
         qr = QuantumRegister(3, "qr")

commit 2ce6863aeec0b01fe373735f8f25551518b51836

Add method `has_control_flow_op()` to `QuantumCircuit` (#13936)
* Add: Add method has_control_flow_op(), to `QuantumCircuit`

This method was added to fix a performance regression introduced by #13921. This check should be quicker than the multiple calls to `isinstance`.

* Lint: Fix formatting

* Lint: Removed unused import and f-string

* Docs: Add release note
- Add `has_control_flow_op` to autogenerate docs.

* Fix: Addressing review comments

Co-authored-by: Matthew Treinish <mtreinish@kortar.org>

* Docs: Fix formatting issue in `QuantumCircuit` docstring.

* Lint: Fix formatting bug in `QuantumCircuit`.

* Test: Add testcases
- One test case with a regular control flow operation and another with a custom one.

* Test: Remove unsupported test and extend original test.
- Implement a combinatorics based test for control flow operations.

* Fix: Import order in `test_circuit_operations`

* Update test/python/circuit/test_circuit_operations.py

---------

Co-authored-by: Matthew Treinish <mtreinish@kortar.org>

crates/circuit/src/circuit_data.rs
@@ -642,6 +642,12 @@ impl CircuitData {
         Ok(())
     }
 
+    /// Checks whether the circuit has an instance of :class:`.ControlFlowOp`
+    /// present amongst its operations.
+    pub fn has_control_flow_op(&self) -> bool {
+        self.data.iter().any(|inst| inst.op.control_flow())
+    }
+
     /// Replaces the bits of this container with the given ``qubits``
     /// and/or ``clbits``.
     ///

qiskit/circuit/quantumcircuit.py
@@ -827,6 +827,9 @@ class QuantumCircuit:
     a block, even though it has no operations defined.  In this case, you can use the :meth:`noop`
     method.
 
+    To check whether a circuit contains a :class:`.ControlFlowOp` you can use the helper method
+    :meth:`.QuantumCircuit.has_control_flow_op`.
+
     ..
         TODO: expand the examples of the builder interface.
 
@@ -839,6 +842,7 @@ class QuantumCircuit:
     .. automethod:: switch
     .. automethod:: while_loop
     .. automethod:: noop
+    .. automethod:: has_control_flow_op
 
 
     Converting circuits to single objects
@@ -4819,6 +4823,11 @@ class QuantumCircuit:
             target._data.assign_parameters_iterable(parameters)
 
         return None if inplace else target
+
+    def has_control_flow_op(self) -> bool:
+        """Checks whether the circuit has an instance of :class:`.ControlFlowOp`
+        present amongst its operations."""
+        return self._data.has_control_flow_op()
 
     def _unroll_param_dict(
         self, parameter_binds: Mapping[Parameter, ParameterValueType]

qiskit/converters/circuit_to_instruction.py
@@ -11,7 +11,6 @@
 # that they have been altered from the originals.
 
 """Helper function for converting a circuit to an instruction."""
-from qiskit.circuit.controlflow import CONTROL_FLOW_OP_NAMES
 from qiskit.exceptions import QiskitError
 from qiskit.circuit.instruction import Instruction
 from qiskit.circuit import QuantumRegister
@@ -84,7 +83,8 @@ def circuit_to_instruction(circuit, parameter_map=None, equivalence_library=None
             "Circuits with internal variables cannot yet be converted to instructions."
             " You may be able to use `QuantumCircuit.compose` to inline this circuit into another."
         )
-    if CONTROL_FLOW_OP_NAMES.intersection(circuit.count_ops()):
+
+    if circuit.has_control_flow_op():
         raise QiskitError(
             "Circuits with control flow operations cannot be converted to an instruction."
         )

releasenotes/notes/add_has_control_flow-8b4f4e8170a52235.yaml
@@ -1,0 +1,4 @@
+features_circuits:
+  - |
+    Added :meth:`.QuantumCircuit.has_control_flow` to check if a circuit
+    contains any control flow operations.

test/python/circuit/test_circuit_operations.py
@@ -15,6 +15,7 @@
 """Test Qiskit's QuantumCircuit class."""
 import copy
 import pickle
+from itertools import combinations
 
 import numpy as np
 from ddt import data, ddt
@@ -23,6 +24,10 @@ from qiskit import ClassicalRegister, QuantumCircuit, QuantumRegister
 from qiskit.circuit import Gate, Instruction, Measure, Parameter, Barrier, AnnotatedOperation
 from qiskit.circuit.classical import expr, types
 from qiskit.circuit import Clbit
+from qiskit.circuit.controlflow.box import BoxOp
+from qiskit.circuit.controlflow.for_loop import ForLoopOp
+from qiskit.circuit.controlflow.switch_case import SwitchCaseOp
+from qiskit.circuit.controlflow.while_loop import WhileLoopOp
 from qiskit.circuit.exceptions import CircuitError
 from qiskit.circuit.controlflow import IfElseOp
 from qiskit.circuit.library import CXGate, HGate
@@ -1575,6 +1580,55 @@ class TestCircuitOperations(QiskitTestCase):
 
         self.assertEqual(circuit, expected)
         self.assertEqual(circuit.name, "test")
+
+    def test_circuit_has_control_flow_op(self):
+        """Test `has_control_flow_op` method"""
+        circuit_1 = QuantumCircuit(2, 1)
+        circuit_1.x(0)
+        circuit_2 = QuantumCircuit(2, 1)
+        circuit_2.y(0)
+
+        # Build a circuit
+        circ = QuantumCircuit(2, 2)
+        circ.h(0)
+        circ.cx(0, 1)
+        circ.measure(0, 0)
+        circ.measure(1, 1)
+
+        # Check if circuit has any control flow operations
+        self.assertFalse(circ.has_control_flow_op())
+
+        # Create examples of all control flow operations
+        control_flow_ops = [
+            IfElseOp((circ.clbits[1], 1), circuit_1, circuit_2),
+            WhileLoopOp((circ.clbits[1], 1), circuit_1),
+            ForLoopOp((circ.clbits[1], 1), None, body=circuit_1),
+            SwitchCaseOp(circ.clbits[1], [(0, circuit_1), (1, circuit_2)]),
+            BoxOp(circuit_1),
+        ]
+
+        # Create combinations of all control flow operations for the
+        # circuit.
+        op_combinations = [
+            comb_list
+            for comb_n in range(5)
+            for comb_list in combinations(control_flow_ops, comb_n + 1)
+        ]
+
+        # Use combinatorics to test all combinations of control flow operations
+        # to see if we can detect all of them.
+        for op_combination in op_combinations:
+            # Build a circuit
+            circ = QuantumCircuit(2, 2)
+            circ.h(0)
+            circ.cx(0, 1)
+            circ.measure(0, 0)
+            circ.measure(1, 1)
+            self.assertFalse(circ.has_control_flow_op())
+            for op in op_combination:
+                circ.append(op, [0, 1], [1])
+            # Check if circuit has any control flow operation
+            self.assertTrue(circ.has_control_flow_op())
 
 
 class TestCircuitPrivateOperations(QiskitTestCase):