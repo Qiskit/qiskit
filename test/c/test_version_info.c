522b24f4fa80a9fc32a9ae971dda3a768d06cb70
velocityraptor7085 <drarchitch@gmail.com>
2025-05-30 09:40:15 +0530

    Removed printf statement in the test file

--- test/c/test_version_info.c
+++ test/c/test_version_info.c
@@ -2,8 +2,6 @@
 
 // Test that QISKIT_VERSION_MAJOR, _MINOR, _PATCH are defined and print them
 int test_version_macros() {
-    printf("Qiskit version: %d.%d.%d\n", QISKIT_VERSION_MAJOR, QISKIT_VERSION_MINOR,
-           QISKIT_VERSION_PATCH);
     if (QISKIT_VERSION_MAJOR < 0 || QISKIT_VERSION_MINOR < 0 || QISKIT_VERSION_PATCH < 0) {
         return EqualityError;
     }
