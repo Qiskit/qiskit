f42a8be2b47cf385164b955447fdb7c92d3732af
Archit Chadalawada <drarchitch@gmail.com>
2025-06-02 13:13:21 +0530

    Update sampled_exp_val.rs

--- crates/accelerate/src/sampled_exp_val.rs
+++ crates/accelerate/src/sampled_exp_val.rs
@@ -14,9 +14,9 @@ use num_complex::Complex64;
 
 use hashbrown::HashMap;
 use numpy::PyReadonlyArray1;
+use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
-use pyo3::exceptions::PyValueError;
 
 use crate::pauli_exp_val::fast_sum;
 use qiskit_circuit::util::c64;
