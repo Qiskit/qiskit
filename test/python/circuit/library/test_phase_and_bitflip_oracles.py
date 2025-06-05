25034fb4ecfc05b4a17d4b24a51c41a556d880b7
AsadullahGalib007 <abrgalib@gmail.com>
2025-06-05 17:28:13 +0600

    updated tests to work with latest gate-functions

--- test/python/circuit/library/test_phase_and_bitflip_oracles.py
+++ test/python/circuit/library/test_phase_and_bitflip_oracles.py
@@ -33,76 +33,126 @@ class TestPhaseOracleAndGate(QiskitTestCase):
         ("xx & xxx | ( ~z ^ zz)", "0111", True),
     )
     @unpack
-    def test_evaluate_bitstring(self, expression, input_bitstring, expected):
-        """PhaseOracle(...).evaluate_bitstring"""
-        oracle = PhaseOracle(expression)
+    def test_phase_oracle_evaluate_bitstring_deprecated(
+        self, expression, input_bitstring, expected
+    ):
+        """Deprecated PhaseOracle(...).evaluate_bitstring"""
+        with self.assertWarns(DeprecationWarning):
+            oracle = PhaseOracle(expression)
         result = oracle.evaluate_bitstring(input_bitstring)
         self.assertEqual(result, expected)
 
-    @data(
+    # Test data for statevector tests
+    statevector_test_data = [
         ("x | x", "01"),
         ("~x", "10"),
         ("x & y", "0001"),
         ("x & ~y", "0100"),
         ("(x0 & x1 | ~x2) ^ x4", "1111000100001110"),
         ("x & y ^ ( ~z1 | z2)", "1110000111101110"),
-    )
+    ]
+
+    @data(*statevector_test_data)
     @unpack
-    def test_statevector(self, expression, truth_table):
-        """Circuit generation"""
-        for use_gate in [True, False]:
-            oracle = PhaseOracleGate(expression) if use_gate else PhaseOracle(expression)
-            num_qubits = oracle.num_qubits
-            circuit = QuantumCircuit(num_qubits)
-            circuit.h(range(num_qubits))
-            circuit.compose(oracle, inplace=True)
-            statevector = Statevector.from_instruction(circuit)
+    def test_phase_oracle_gate_statevector(self, expression, truth_table):
+        """Circuit generation with PhaseOracleGate"""
+        oracle = PhaseOracleGate(expression)
+        num_qubits = oracle.num_qubits
+        circuit = QuantumCircuit(num_qubits)
+        circuit.h(range(num_qubits))
+        circuit.compose(oracle, inplace=True)
+        statevector = Statevector.from_instruction(circuit)
 
-            valid_state = -1 / sqrt(2**num_qubits)
-            invalid_state = 1 / sqrt(2**num_qubits)
+        valid_state = -1 / sqrt(2**num_qubits)
+        invalid_state = 1 / sqrt(2**num_qubits)
 
-            states = list(range(2**num_qubits))
-            good_states = [i for i in range(len(states)) if truth_table[i] == "1"]
-            expected_valid = [state in good_states for state in states]
-            result_valid = [isclose(statevector.data[state], valid_state) for state in states]
+        states = list(range(2**num_qubits))
+        good_states = [i for i in range(len(states)) if truth_table[i] == "1"]
+        expected_valid = [state in good_states for state in states]
+        result_valid = [isclose(statevector.data[state], valid_state) for state in states]
 
-            expected_invalid = [state not in good_states for state in states]
-            result_invalid = [isclose(statevector.data[state], invalid_state) for state in states]
-            with self.subTest(use_gate=use_gate):
-                self.assertListEqual(expected_valid, result_valid)
-                self.assertListEqual(expected_invalid, result_invalid)
+        expected_invalid = [state not in good_states for state in states]
+        result_invalid = [isclose(statevector.data[state], invalid_state) for state in states]
+        self.assertListEqual(expected_valid, result_valid)
+        self.assertListEqual(expected_invalid, result_invalid)
 
-    @data(
+    @data(*statevector_test_data)
+    @unpack
+    def test_phase_oracle_statevector_deprecated(self, expression, truth_table):
+        """Circuit generation with deprecated PhaseOracle"""
+        with self.assertWarns(DeprecationWarning):
+            oracle = PhaseOracle(expression)
+        num_qubits = oracle.num_qubits
+        circuit = QuantumCircuit(num_qubits)
+        circuit.h(range(num_qubits))
+        circuit.compose(oracle, inplace=True)
+        statevector = Statevector.from_instruction(circuit)
+
+        valid_state = -1 / sqrt(2**num_qubits)
+        invalid_state = 1 / sqrt(2**num_qubits)
+
+        states = list(range(2**num_qubits))
+        good_states = [i for i in range(len(states)) if truth_table[i] == "1"]
+        expected_valid = [state in good_states for state in states]
+        result_valid = [isclose(statevector.data[state], valid_state) for state in states]
+
+        expected_invalid = [state not in good_states for state in states]
+        result_invalid = [isclose(statevector.data[state], invalid_state) for state in states]
+        self.assertListEqual(expected_valid, result_valid)
+        self.assertListEqual(expected_invalid, result_invalid)
+
+    # Test data for variable order tests
+    variable_order_test_data = [
         ("((A & C) | (B & D)) & ~(C & D)", None, [3, 7, 12, 13]),
         ("((A & C) | (B & D)) & ~(C & D)", ["A", "B", "C", "D"], [5, 7, 10, 11]),
-    )
+    ]
+
+    @data(*variable_order_test_data)
     @unpack
-    def test_variable_order(self, expression, var_order, good_states):
-        """Circuit generation"""
-        for use_gate in [True, False]:
-            oracle = (
-                PhaseOracleGate(expression, var_order=var_order)
-                if use_gate
-                else PhaseOracle(expression, var_order=var_order)
-            )
-            num_qubits = oracle.num_qubits
-            circuit = QuantumCircuit(num_qubits)
-            circuit.h(range(num_qubits))
-            circuit.compose(oracle, inplace=True)
-            statevector = Statevector.from_instruction(circuit)
+    def test_phase_oracle_gate_variable_order(self, expression, var_order, good_states):
+        """Circuit generation with PhaseOracleGate and variable order"""
+        oracle = PhaseOracleGate(expression, var_order=var_order)
+        num_qubits = oracle.num_qubits
+        circuit = QuantumCircuit(num_qubits)
+        circuit.h(range(num_qubits))
+        circuit.compose(oracle, inplace=True)
+        statevector = Statevector.from_instruction(circuit)
 
-            valid_state = -1 / sqrt(2**num_qubits)
-            invalid_state = 1 / sqrt(2**num_qubits)
+        valid_state = -1 / sqrt(2**num_qubits)
+        invalid_state = 1 / sqrt(2**num_qubits)
 
-            states = list(range(2**num_qubits))
-            expected_valid = [state in good_states for state in states]
-            result_valid = [isclose(statevector.data[state], valid_state) for state in states]
+        states = list(range(2**num_qubits))
+        expected_valid = [state in good_states for state in states]
+        result_valid = [isclose(statevector.data[state], valid_state) for state in states]
 
-            expected_invalid = [state not in good_states for state in states]
-            result_invalid = [isclose(statevector.data[state], invalid_state) for state in states]
-            with self.subTest(use_gate=use_gate):
-                self.assertListEqual(expected_valid, result_valid)
-                self.assertListEqual(expected_invalid, result_invalid)
+        expected_invalid = [state not in good_states for state in states]
+        result_invalid = [isclose(statevector.data[state], invalid_state) for state in states]
+        self.assertListEqual(expected_valid, result_valid)
+        self.assertListEqual(expected_invalid, result_invalid)
+
+    @data(*variable_order_test_data)
+    @unpack
+    def test_phase_oracle_variable_order_deprecated(self, expression, var_order, good_states):
+        """Circuit generation with deprecated PhaseOracle and variable order"""
+        with self.assertWarns(DeprecationWarning):
+            oracle = PhaseOracle(expression, var_order=var_order)
+        num_qubits = oracle.num_qubits
+        circuit = QuantumCircuit(num_qubits)
+        circuit.h(range(num_qubits))
+        circuit.compose(oracle, inplace=True)
+        statevector = Statevector.from_instruction(circuit)
+
+        valid_state = -1 / sqrt(2**num_qubits)
+        invalid_state = 1 / sqrt(2**num_qubits)
+
+        states = list(range(2**num_qubits))
+        expected_valid = [state in good_states for state in states]
+        result_valid = [isclose(statevector.data[state], valid_state) for state in states]
+
+        expected_invalid = [state not in good_states for state in states]
+        result_invalid = [isclose(statevector.data[state], invalid_state) for state in states]
+        self.assertListEqual(expected_valid, result_valid)
+        self.assertListEqual(expected_invalid, result_invalid)
 
 
 @ddt
