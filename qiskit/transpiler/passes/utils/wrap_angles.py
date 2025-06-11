31791e3fd39fe6356910267fca37b6b60695da98
Matthew Treinish <mtreinish@kortar.org>
2025-06-11 15:57:51 -0400

    Improve WrapAngles pass documentation

--- qiskit/transpiler/passes/utils/wrap_angles.py
+++ qiskit/transpiler/passes/utils/wrap_angles.py
@@ -27,8 +27,51 @@ class WrapAngles(TransformationPass):
     bound specified in the target. If any gates outside the bound are identified the callback in
     the target will be called to substitute the gate outside the bound with an equivalent subcircuit.
     This pass does not run on gates that are parameterized, even if the gate has unparameterized
-    parameters outside a specified bound. If there are parameterized gates they must be bound for
-    the angles to be treated as inside the bounds
+    parameters outside a specified bound. If there are parameterized gates in the circuit they will
+    be ignored by this pass as bound angles are neceessary to transform the gate. For example:
+
+    .. plot::
+       :include-source:
+
+       from qiskit.circuit import Gate, Parameter, Qubit, QuantumCircuit
+       from qiskit.circuit.library import RZGate
+       from qiskit.dagcircuit import DAGCircuit
+       from qiskit.transpiler.passes import WrapAngles
+       from qiskit.transpiler.target import Target
+       from qiskit.transpiler import WrapAngleRegistry
+
+       param = Parameter("a")
+       circuit = QuantumCircuit(1)
+       circuit.rz(6.8, 0)
+       target = Target(num_qubits=1)
+       target.add_instruction(RZGate(param), angle_bounds=[(0, 0.5)])
+
+       def callback(angles, _qubits):
+           angle = angles[0]
+           if angle > 0:
+               number_of_gates = angle / 0.5
+           else:
+               number_of_gates = (6.28 - angle) / 0.5
+           dag = DAGCircuit()
+           dag.add_qubits([Qubit()])
+           for _ in range(int(number_of_gates)):
+               dag.apply_operation_back(RZGate(0.5), [dag.qubits[0]])
+           return dag
+
+       registry = WrapAngleRegistry()
+       registry.add_wrapper("rz", callback)
+       wrap_pass = WrapAngles(target, registry)
+       res = wrap_pass(circuit)
+       res.draw("mpl")
+
+    Args:
+        target(Target): The :class:`.Target` that
+        registry(WrapAngleRegistry): The registry of wrapping functions used
+            by the pass to wrap the angles of a gate. If not specified the
+            global ``WRAP_ANGLE_REGISTRY`` object defined in this module will be used.
+            Unless you are planning to run this pass standalone or are building a
+            custom :class:`~.transpiler.PassManager` including this pass you will want
+            to rely on ``WRAP_ANGLE_REGISTRY``.
     """
 
     def __init__(self, target, registry=None):
