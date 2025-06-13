6c80065f37a7b8d8aa477a155e79afbc1159815b
Archit Chadalawada <drarchitch@gmail.com>
2025-06-13 11:10:52 +0530

    Update __init__.py
    Co-authored-by: Kevin Hartman <kevin@hart.mn>

--- qiskit/qpy/__init__.py
+++ qiskit/qpy/__init__.py
@@ -441,7 +441,7 @@ circuits in the data.
 Version 16
 ----------
 
-Version 16 adds a circuit start table to the QPY file format. It contiains the set of
+Version 16 adds a circuit start table to the QPY file format. It serves as an index of the
 byte offsets of each circuit payload in the file. The motivation for this change is to
 enable a more efficient loading of circuits using multi-threading in a future rust
 implementation of the QPY deserializer.
