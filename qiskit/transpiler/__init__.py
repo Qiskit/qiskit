bcfdea6554f508eb3825508bd3fb5820a2ed34f1
Raynel Sanchez <raynelfss@hotmail.com>
2025-06-17 10:08:40 -0400

    Fix: Revert changes in `__init__` file.

--- qiskit/transpiler/__init__.py
+++ qiskit/transpiler/__init__.py
@@ -1331,6 +1331,7 @@ from qiskit.passmanager import (
     DoWhileController,
 )
 from qiskit.passmanager.compilation_status import PropertySet
+
 from .passmanager import PassManager, StagedPassManager
 from .passmanager_config import PassManagerConfig
 from .exceptions import (
