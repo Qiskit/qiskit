19eeb418b808cb3c6a2f43ee70ecb7314482636a
Matthew Treinish <mtreinish@kortar.org>
2025-06-17 07:19:06 -0400

    Prepare 2.0.3 release (#14626)
    * Prepare 2.0.3 release
    
    This commit prepares the 2.0.2 bugfix release. It updates the release notes
    and bumps all the necessary version numbers. Once the commit is merged
    the resulting merged commit should be tagged as 2.0.3.
    
    * Touch up release notes
    
    * Update releasenotes/notes/fix-hls-clbits-6ddf1f8d1542eed2.yaml
    
    Co-authored-by: Jake Lishman <jake@binhbar.com>
    
    ---------
    
    Co-authored-by: Jake Lishman <jake@binhbar.com>

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ author = "Qiskit Development Team"
 # The short X.Y version
 version = "2.0"
 # The full version, including alpha/beta/rc tags
-release = "2.0.2"
+release = "2.0.3"
 
 language = "en"
 
