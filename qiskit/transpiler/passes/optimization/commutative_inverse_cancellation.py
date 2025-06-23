c9dd735ed84fc323ae82ce44c3ec388399cd2d40
Alexander Ivrii <alexi@il.ibm.com>
2025-06-23 10:32:26 +0300

    some internal refactoring

--- qiskit/transpiler/passes/optimization/commutative_inverse_cancellation.py
+++ qiskit/transpiler/passes/optimization/commutative_inverse_cancellation.py
@@ -70,28 +70,28 @@ class CommutativeInverseCancellation(TransformationPass):
             inverse = None
         return inverse
 
-    def _check_inverse(self, node1, node2):
-        """Checks whether op1 and op2 are inverse up to a phase, that is whether
-        ``op2 = e^{i * d} op1^{-1})`` for some phase difference ``d``.
-        If this is the case, we can replace ``op2 * op1`` by `e^{i * d} I``.
-        The input to this function is a pair of DAG nodes.
-        The output is a tuple representing whether the two nodes
-        are inverse up to a phase and that phase difference.
+    def _check_equal_upto_phase(self, op1, op2, matrix_based):
+        """
+        Checks whether op1 and op2 are equal up to a phase, that is whether
+        ``op2 = e^{i * d} op1)`` for some phase difference ``d``.
+
+        If this is the case, we can replace ``op2 * op1^{-1}`` by `e^{i * d} I``.
+
+        The output is a tuple representing whether the two ops
+        are equal up to a phase and that phase difference.
         """
         phase_difference = 0
-        if not self._matrix_based:
-            is_inverse = node1.op.inverse() == node2.op
-        elif len(node2.qargs) > self._max_qubits:
-            is_inverse = False
+        if not matrix_based:
+            are_equal = op1 == op2
         else:
-            mat1 = Operator(node1.op.inverse()).data
-            mat2 = Operator(node2.op).data
+            mat1 = Operator(op1).data
+            mat2 = Operator(op2).data
             props = {}
-            is_inverse = matrix_equal(mat1, mat2, ignore_phase=True, props=props)
-            if is_inverse:
+            are_equal = matrix_equal(mat1, mat2, ignore_phase=True, props=props)
+            if are_equal:
                 # mat2 = e^{i * phase_difference} mat1
                 phase_difference = props["phase_difference"]
-        return is_inverse, phase_difference
+        return are_equal, phase_difference
 
     def run(self, dag: DAGCircuit):
         """
@@ -126,8 +126,14 @@ class CommutativeInverseCancellation(TransformationPass):
                     and topo_sorted_nodes[idx2].qargs == topo_sorted_nodes[idx1].qargs
                     and topo_sorted_nodes[idx2].cargs == topo_sorted_nodes[idx1].cargs
                 ):
-                    is_inverse, phase = self._check_inverse(
-                        topo_sorted_nodes[idx1], topo_sorted_nodes[idx2]
+                    matrix_based = (
+                        self._matrix_based
+                        and len(topo_sorted_nodes[idx2].qargs) <= self._max_qubits
+                    )
+                    is_inverse, phase = self._check_equal_upto_phase(
+                        topo_sorted_nodes[idx1].op.inverse(),
+                        topo_sorted_nodes[idx2].op,
+                        matrix_based,
                     )
                     if is_inverse:
                         phase_update += phase
