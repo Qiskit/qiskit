cbf09e3b869506e524e7d857753441036a9bd5cf
WhimsyHippo <hippowiseman789@gmail.com>
2025-06-26 18:18:22 +0200

    Update test/benchmarks/__init__.py with final integrated content

--- test/benchmarks/__init__.py
+++ test/benchmarks/__init__.py
@@ -1,3 +1,8 @@
+import sys
+import warnings
+import unittest
+from unittest import mock
+from qiskit import __init__ as qiskit_init
 # This code is part of Qiskit.
 #
 # (C) Copyright IBM 2023
@@ -9,3 +14,12 @@
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
+
+class TestDeprecationWarnings(unittest.TestCase):
+    @mock.patch('sys.version_info', new=(3, 9))
+    def test_deprecation_warning_for_python_3_9(self):
+        with warnings.catch_warnings(record=True) as w:
+            warnings.simplefilter('always')
+            # Re-trigger the import to check for warnings after mocking
+            import qiskit.__init__ as qiskit_init
+            self.assertTrue(any(issubclass(warn.category, DeprecationWarning) for warn in w))
