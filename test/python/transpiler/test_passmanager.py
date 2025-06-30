ea36eeef65ea901e30ac83cbd4d3770fdf03b847
Mostafizur <rahaman1@ibm.com>
2025-06-30 17:42:02 +0530

    Added test function for multi-circuit-manager-callback

--- test/python/transpiler/test_passmanager.py
+++ test/python/transpiler/test_passmanager.py
@@ -81,6 +81,64 @@ class TestPassManager(QiskitTestCase):
         self.assertEqual(calls[0]["property_set"], PropertySet())
         self.assertEqual("MyCircuit", calls[1]["dag"].name)
 
+    def test_callback_multi_circuits(self):
+        """Test callback with multiple circuits."""
+        import copy
+
+        qr = QuantumRegister(1, "qr")
+
+        circuit1 = QuantumCircuit(qr, name="Circuit1")
+        circuit1.h(qr[0])
+        circuit1.h(qr[0])
+        circuit1.h(qr[0])
+        expected_start_1 = QuantumCircuit(qr)
+        expected_start_1.append(U2Gate(0, np.pi), [qr[0]])
+        expected_start_1.append(U2Gate(0, np.pi), [qr[0]])
+        expected_start_1.append(U2Gate(0, np.pi), [qr[0]])
+        expected_start_dag_1 = circuit_to_dag(expected_start_1)
+
+        expected_end_1 = QuantumCircuit(qr)
+        expected_end_1.append(U2Gate(0, np.pi), [qr[0]])
+        expected_end_dag_1 = circuit_to_dag(expected_end_1)
+
+        circuit2 = circuit1.copy(name="Circuit2")
+        expected_start_dag_2 = copy.deepcopy(expected_start_dag_1)
+        expected_end_dag_2 = copy.deepcopy(expected_end_dag_1)
+
+        calls = []
+
+        def callback(**kwargs):
+            out_dict = kwargs
+            out_dict["dag"] = copy.deepcopy(kwargs["dag"])
+            calls.append(out_dict)
+
+        passmanager = PassManager()
+        passmanager.append(BasisTranslator(std_eqlib, ["u2"]))
+        passmanager.append(Optimize1qGates())
+
+        passmanager.run([circuit1, circuit2], callback=callback)
+
+        # We expect 2 passes × 2 circuits = 4 callback calls
+        self.assertEqual(len(calls), 4)
+
+        # Validate callback for first circuit (index 0,1)
+        self.assertEqual(calls[0]["pass_"].name(), "BasisTranslator")
+        self.assertEqual(calls[0]["dag"].name, "Circuit1")
+        self.assertEqual(expected_start_dag_1, calls[0]["dag"])
+
+        self.assertEqual(calls[1]["pass_"].name(), "Optimize1qGates")
+        self.assertEqual(calls[1]["dag"].name, "Circuit1")
+        self.assertEqual(expected_end_dag_1, calls[1]["dag"])
+
+        # Validate callback for second circuit (index 2,3)
+        self.assertEqual(calls[2]["pass_"].name(), "BasisTranslator")
+        self.assertEqual(calls[2]["dag"].name, "Circuit2")
+        self.assertEqual(expected_start_dag_2, calls[2]["dag"])
+
+        self.assertEqual(calls[3]["pass_"].name(), "Optimize1qGates")
+        self.assertEqual(calls[3]["dag"].name, "Circuit2")
+        self.assertEqual(expected_end_dag_2, calls[3]["dag"])
+
     def test_callback_with_pass_requires(self):
         """Test the callback with a pass with pass requirements."""
         qr = QuantumRegister(3, "qr")
