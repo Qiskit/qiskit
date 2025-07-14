71c91f1e4f5ba7936c8b0bb7b8c94901750e91a7
Matthew Treinish <mtreinish@kortar.org>
2025-07-14 16:12:11 -0400

    Add negative test where no layout can be found

--- test/c/test_vf2_layout.c
+++ test/c/test_vf2_layout.c
@@ -20,67 +20,46 @@
 #include <stdio.h>
 #include <string.h>
 
-/**
- * Test adding an instruction to the Target.
- */
-int test_vf2_layout_line(void) {
-    const uint32_t num_qubits = 5;
-    // Let's create a target with one qubit for now
-    QkTarget *target = qk_target_new(num_qubits);
-    int result = Ok;
-
-    // Add an X Gate.
-    // This operation is global, no property map is provided
+int build_target(QkTarget *target, uint32_t num_qubits) {
+    // Create a target with cx connectivity in a line.
     QkExitCode result_x = qk_target_add_instruction(target, qk_target_entry_new(QkGate_X));
     if (result_x != QkExitCode_Success) {
         printf("Unexpected error occurred when adding a global X gate.");
-        result = EqualityError;
-        goto cleanup;
+        return EqualityError;
     }
-
-    // Re-add same gate, check if it fails
-    QkExitCode result_x_readded = qk_target_add_instruction(target, qk_target_entry_new(QkGate_X));
-    if (result_x_readded != QkExitCode_TargetInstAlreadyExists) {
-        printf("The addition of a repeated gate did not fail as expected.");
-        result = EqualityError;
-        goto cleanup;
-    }
-
-    // Number of qubits of the target should not change.
-    uint32_t current_num_qubits = qk_target_num_qubits(target);
-
-    size_t current_size = qk_target_num_instructions(target);
-    if (current_size != 1) {
-        printf("The size of this target is not correct: Expected 1, got %zu", current_size);
-        result = EqualityError;
-        goto cleanup;
-    }
-
-    // Add a CX Gate.
-    // Create prop_map for the instruction
-    // Add property for (0, 1)
     QkTargetEntry *cx_entry = qk_target_entry_new(QkGate_CX);
-    for (uint32_t i = 0; i < current_num_qubits - 1; i++) {
+    for (uint32_t i = 0; i < num_qubits - 1; i++) {
         uint32_t qargs[2] = {i, i + 1};
-        double inst_error = 0.0090393 * (current_num_qubits - i);
+        double inst_error = 0.0090393 * (num_qubits - i);
         double inst_duration = 0.020039;
 
         QkExitCode result_cx_props =
             qk_target_entry_add_property(cx_entry, qargs, 2, inst_duration, inst_error);
         if (result_cx_props != QkExitCode_Success) {
             printf("Unexpected error occurred when adding property to a CX gate entry.");
-            result = EqualityError;
-            goto cleanup;
+            return EqualityError;
         }
     }
-
     QkExitCode result_cx = qk_target_add_instruction(target, cx_entry);
     if (result_cx != QkExitCode_Success) {
         printf("Unexpected error occurred when adding a CX gate.");
-        result = EqualityError;
+        return EqualityError;
+    }
+    return Ok;
+}
+
+/**
+ * Test running VF2Layout on a line connectivity
+ */
+int test_vf2_layout_line(void) {
+    const uint32_t num_qubits = 5;
+    QkTarget *target = qk_target_new(num_qubits);
+    int result = Ok;
+    result = build_target(target, num_qubits);
+    if (result != Ok) {
         goto cleanup;
     }
-
+    // Create a circuit with line connectivity.
     QkCircuit *qc = qk_circuit_new(5, 0);
     for (uint32_t i = 0; i < qk_circuit_num_qubits(qc) - 1; i++) {
         uint32_t qargs[2] = {i, i + 1};
@@ -89,7 +68,7 @@ int test_vf2_layout_line(void) {
         }
     }
     QkVF2LayoutResult *layout_result =
-        qk_transpiler_pass_standalone_vf2_layout(qc, target, false, -1, NAN, -1);
+        qk_transpiler_pass_standalone_vf2_layout(qc, target, false, -1, 0.0, -1);
     if (!qk_vf2_layout_has_match(layout_result)) {
         printf("No layout was found");
         result = EqualityError;
@@ -124,9 +103,47 @@ cleanup:
     return result;
 }
 
+/**
+ * Test VF2Layout where a solution isn't possible
+ */
+int test_vf2_no_layout_found(void) {
+    const uint32_t num_qubits = 5;
+    QkTarget *target = qk_target_new(num_qubits);
+    int result = Ok;
+    result = build_target(target, num_qubits);
+    if (result != Ok) {
+        goto cleanup;
+    }
+    QkCircuit *qc = qk_circuit_new(5, 0);
+    for (uint32_t i = 0; i < qk_circuit_num_qubits(qc); i++) {
+        for (uint32_t j = 0; j < qk_circuit_num_qubits(qc); j++) {
+            if (i == j) {
+                continue;
+            }
+            uint32_t qargs[2] = {i, j};
+            qk_circuit_gate(qc, QkGate_CX, qargs, NULL);
+        }
+    }
+    QkVF2LayoutResult *layout_result =
+        qk_transpiler_pass_standalone_vf2_layout(qc, target, false, -1, 0.0, -1);
+    if (qk_vf2_layout_has_match(layout_result)) {
+        printf("Unexpected layout found when one shouldn't be possible");
+        result = EqualityError;
+        goto layout_cleanup;
+    }
+layout_cleanup:
+    qk_vf2_layout_free(layout_result);
+circuit_cleanup:
+    qk_circuit_free(qc);
+cleanup:
+    qk_target_free(target);
+    return result;
+}
+
 int test_vf2_layout(void) {
     int num_failed = 0;
     num_failed += RUN_TEST(test_vf2_layout_line);
+    num_failed += RUN_TEST(test_vf2_no_layout_found);
 
     fflush(stderr);
     fprintf(stderr, "=== Number of failed subtests: %i\n", num_failed);
