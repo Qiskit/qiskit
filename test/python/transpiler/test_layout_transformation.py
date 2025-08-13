2567ffe53a04eee1f5c6d2d90e1e2576445d172d
Alexander Ivrii <alexi@il.ibm.com>
2025-08-13 12:08:09 +0300

    fixing failing CI tests

--- test/python/transpiler/test_layout_transformation.py
+++ test/python/transpiler/test_layout_transformation.py
@@ -86,10 +86,10 @@ class TestLayoutTransformation(QiskitTestCase):
         self.assertEqual(circuit_to_dag(expected), output_dag)
 
     def test_full_connected_coupling_map(self):
-        """Test if the permutation {0->3,1->0,2->1,3->2} in a fully connected map."""
+        """Test if the permutation {0->3,1->2,2->1,3->0} is implemented correctly in a fully connected map."""
         v = QuantumRegister(4, "v")  # virtual qubits
         from_layout = Layout({v[0]: 0, v[1]: 1, v[2]: 2, v[3]: 3})
-        to_layout = Layout({v[0]: 3, v[1]: 0, v[2]: 1, v[3]: 2})
+        to_layout = Layout({v[0]: 3, v[1]: 2, v[2]: 1, v[3]: 0})
         ltpass = LayoutTransformation(
             coupling_map=None, from_layout=from_layout, to_layout=to_layout, seed=42
         )
@@ -98,9 +98,8 @@ class TestLayoutTransformation(QiskitTestCase):
         output_dag = ltpass.run(dag)
 
         expected = QuantumCircuit(4)
-        expected.swap(1, 0)
-        expected.swap(2, 1)
-        expected.swap(3, 2)
+        expected.swap(0, 3)
+        expected.swap(1, 2)
 
         self.assertEqual(circuit_to_dag(expected), output_dag)
 
