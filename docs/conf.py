030e0430167230fe2727e6361a166a497d60768b
Kevin Hartman <kevin@hart.mn>
2025-08-14 15:10:53 -0400

    Prepare 1.4.4.

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ author = "Qiskit Development Team"
 # The short X.Y version
 version = "1.4"
 # The full version, including alpha/beta/rc tags
-release = "1.4.3"
+release = "1.4.4"
 
 language = "en"
 
