c12428039a91b0abd1d180d70646b8793f9722df
Matthew Treinish <mtreinish@kortar.org>
2025-08-15 09:44:50 -0400

    Rework C interface to write arrays to user provided pointers
    This commit reworks the C API for accessing the layout details to
    writing the output to a user provided pointer rather than allocating the
    output array and giving them the pointer and size of it.

--- crates/cext/src/transpiler/transpile_layout.rs
+++ crates/cext/src/transpiler/transpile_layout.rs
@@ -18,6 +18,8 @@ use qiskit_transpiler::transpile_layout::TranspileLayout;
 ///
 /// @param layout A pointer to the ``QkTranspileLayout``.
 ///
+/// @return The number of input qubits
+///
 /// # Safety
 ///
 /// Behavior is undefined if ``layout`` is not a valid, non-null pointer to a
@@ -36,6 +38,8 @@ pub unsafe extern "C" fn qk_transpile_layout_num_input_qubits(
 ///
 /// @param layout A pointer to the ``QkTranspileLayout``.
 ///
+/// @return The number of output qubits
+///
 /// # Safety
 ///
 /// Behavior is undefined if ``layout`` is not a valid, non-null pointer to a
@@ -49,128 +53,166 @@ pub unsafe extern "C" fn qk_transpile_layout_num_output_qubits(
     layout.num_output_qubits()
 }
 
-/// A layout array object that represent the initial layout, routing permutation
-/// or final layout depending on which function was used to generate it.
-#[repr(C)]
-pub struct QkLayoutArray {
-    /// The array size
-    size: usize,
-    /// A pointer to an array of ``uint32_t`` representing the physical qubit
-    /// indices.
-    array: *mut u32,
-}
-
 /// @ingroup QkTranspileLayout
 /// Return the initial layout of a ``QkTranspileLayout``.
+///
+/// The output array from this function represents the mapping from the virutal qubits in the
+/// original input circuit to the physical qubit in the output circuit. The
+/// index in the array is the virtual qubit and the value is the physical qubit. For example an
+/// output array of:
+///
+/// ```
+/// [1, 0, 2]
+/// ```
+///
+/// indicates that the layout maps virtual qubit 0 -> physical qubit 1, virtual qubit 1 -> physical
+/// qubit -> 0, and virtual qubit 2 -> physical qubit 2.
 ///
 /// @param layout A pointer to the ``QkTranspileLayout``.
 /// @param filter_ancillas If set to true the output array will not include any indicies for any
 /// ancillas added by the transpiler.
+/// @param initial_layout A pointer to the array where this function will write the initial layout
+/// to. This must have sufficient space for the full array which will either be
+/// ``qk_transpile_layout_num_input_qubits()`` or ``qk_transpile_layout_num_output_qubits()`` for
+/// ``filter_ancillas`` being true or false respectively.
 ///
-/// @returns An QkLayoutArray object which is a size and pointer to an array of the initial layout. This
-/// will need to be passed to ``qk_layout_array_free()`` to free the array allocation. A
-/// QkLayoutArray with a size 0 and null pointer for the array is returned if there is no initial layout
-/// present in the provided ``QkTranspileLayout``.
+/// @returns True if there was a initial_layout written to ``initial_layout`` and false if there
+/// is no initial layout.
 ///
 /// # Safety
 /// Behavior is undefined if ``layout`` is not a valid, non-null pointer to a
-/// ``QkTranspileLayout``.
+/// ``QkTranspileLayout``. ``initial_layout`` must be a valid, non-null pointer with a large enough
+/// allocation to store the size necessary for the initial layout. If ``filter_ancillas`` is true
+/// this will be number of input qubits (which can be checked with
+/// ``qk_transpile_layout_num_input_qubits()``) or the number of output qubits if ``filter_ancillas``
+/// is false (which can be queried with ``qk_transpile_layout_num_output_qubits()``).
 #[no_mangle]
 #[cfg(feature = "cbinding")]
 pub unsafe extern "C" fn qk_transpile_layout_initial_layout(
     layout: *const TranspileLayout,
     filter_ancillas: bool,
-) -> QkLayoutArray {
+    initial_layout: *mut u32,
+) -> bool {
+    // SAFETY: Per the documentation layout must be a valid pointer to a TranspileLayout
     let layout = unsafe { const_ptr_as_ref(layout) };
-    let initial_layout = layout.initial_layout(filter_ancillas);
-    if let Some(initial_layout) = initial_layout {
-        let mut box_slice = initial_layout.into_boxed_slice();
-        let array = box_slice.as_mut_ptr() as *mut u32;
-        let size = box_slice.len();
-        let _ = Box::into_raw(box_slice);
-        QkLayoutArray { size, array }
+    let out_initial_layout = layout.initial_layout(filter_ancillas);
+    if let Some(out_initial_layout) = out_initial_layout {
+        // SAFETY: Per the documentation initial_layout must be a valid pointer with a sufficient
+        // allocation for the output array
+        unsafe {
+            let out_slice =
+                std::slice::from_raw_parts_mut(initial_layout, out_initial_layout.len());
+            out_slice
+                .iter_mut()
+                .zip(out_initial_layout.iter())
+                .for_each(|(dest, src)| *dest = src.0);
+        };
+        true
     } else {
-        QkLayoutArray {
-            size: 0,
-            array: std::ptr::null_mut(),
-        }
+        false
     }
 }
 
 /// @ingroup QkTranspileLayout
-/// Return the routing permutation of a ``QkTranspileLayout``
+/// Return the output permutation of a ``QkTranspileLayout``
+///
+/// The output array from this function represents the permutation induced by the transpiler where
+/// the index indicates the qubit at the start of the circuit and the value is the position of the
+/// qubit at the end of the circuit. For example an output array of:
+///
+/// ```
+/// [1, 2, 0]
+/// ```
+///
+/// indicates that qubit 0 from the start of the circuit is at qubit 1 at the end of the circuit,
+/// 1 -> 2, and 2 -> 0.
 ///
 /// @param layout A pointer to the ``QkTranspileLayout``.
+/// @param output_permutation A pointer to the array where this function will write the output permutation
+/// to. This must have sufficient space for the output which will be the number of output qubits in
+/// the layout. This can be queried with ``qk_transpile_layout_num_output_qubits``.
 ///
-/// @returns A QkLayoutArray object which is a size and pointer to an array of the routing
-/// permutation. This will need to be passed to ``qk_layout_array_free()`` to free the array allocation.
+/// @returns True if there is an output permutation that was written to ``output_permutation``
+/// false if the ``QkTranspileLayout`` does not contain an output permutation.
 ///
 /// # Safety
 /// Behavior is undefined if ``layout`` is not a valid, non-null pointer to a
-/// ``QkTranspileLayout``.
+/// ``QkTranspileLayout``. ``output_permutation`` must be a valid, non-null pointer with a large enough
+/// allocation to store the size necessary for the output_permutation. This will always be the number
+/// of output qubits in the ``QkTranspileLayout`` which can be queried with
+/// ``qk_transpile_layout_num_output_qubits()``.
 #[no_mangle]
 #[cfg(feature = "cbinding")]
 pub unsafe extern "C" fn qk_transpile_layout_output_permutation(
     layout: *const TranspileLayout,
-) -> QkLayoutArray {
+    output_permutation: *mut u32,
+) -> bool {
+    // SAFETY: Per the documentation layout must be a valid pointer to a TranspileLayout
     let layout = unsafe { const_ptr_as_ref(layout) };
-    let permutation = layout.explicit_output_permutation().to_vec();
-    let mut box_slice = permutation.into_boxed_slice();
-    let array = box_slice.as_mut_ptr() as *mut u32;
-    let size = box_slice.len();
-    let _ = Box::into_raw(box_slice);
-    QkLayoutArray { size, array }
+    let permutation = layout.output_permutation();
+    if let Some(permutation) = permutation {
+        // SAFETY: Per the documentation output_permutation must be a valid pointer with a sufficient
+        // allocation for the output array
+        unsafe {
+            let out_slice = std::slice::from_raw_parts_mut(output_permutation, permutation.len());
+            out_slice
+                .iter_mut()
+                .zip(permutation.iter())
+                .for_each(|(dest, src)| *dest = src.0);
+        };
+        true
+    } else {
+        false
+    }
 }
 
 /// @ingroup QkTranspileLayout
 /// Return the final layout of a ``QkTranspileLayout``
 ///
+/// The output array represents the mapping from the virtual qubit in the original input circuit to
+/// the physical qubit at the end of the transpile circuit that has that qubit's state. The array
+/// index represents the virtual qubit and the value represents the physical qubit at the end of
+/// the transpiled circuit which has that virtual qubit's state. For example, an output array of:
+///
+/// ```
+/// [2, 0, 1]
+/// ```
+///
+/// indicates that virtual qubit 0's state in the original circuit is on
+/// physical qubit 2 at the end of the transpiled circuit, 1 -> 0, and 2 -> 1.
+///
 /// @param layout A pointer to the ``QkTranspileLayout``.
 /// @param filter_ancillas If set to true the output array will not include any indicies for any
 /// ancillas added by the transpiler.
-///
-/// @returns A QkLayoutArray object which is a size and pointer to an array of the final layout. This
-/// will need to be passed to ``qk_layout_array_free()`` to free the array allocation.
+/// @param final_layout A pointer to the array where this function will write the final layout to.
+/// This must have sufficient space for the output which will either be the number of input or
+/// output qubits depending on the value of filter_ancillas.
 ///
 /// # Safety
 /// Behavior is undefined if ``layout`` is not a valid, non-null pointer to a
-/// ``QkTranspileLayout``.
+/// ``QkTranspileLayout``. ``final_layout`` must be a valid, non-null pointer with a large enough
+/// allocation to store the size necessary for the final layout. If ``filter_ancillas`` is true
+/// this will be number of input qubits (which can be checked with
+/// ``qk_transpile_layout_num_input_qubits()``) or the number of output qubits if ``filter_ancillas``
+/// is false (which can be queried with ``qk_transpile_layout_num_output_qubits()``).
 #[no_mangle]
 #[cfg(feature = "cbinding")]
 pub unsafe extern "C" fn qk_transpile_layout_final_layout(
     layout: *const TranspileLayout,
     filter_ancillas: bool,
-) -> QkLayoutArray {
+    final_layout: *mut u32,
+) {
+    // SAFETY: Per the documentation layout must be a valid pointer to a TranspileLayout
     let layout = unsafe { const_ptr_as_ref(layout) };
-    let final_layout = layout.final_index_layout(filter_ancillas);
-    let mut box_slice = final_layout.into_boxed_slice();
-    let array = box_slice.as_mut_ptr() as *mut u32;
-    let size = box_slice.len();
-    let _ = Box::into_raw(box_slice);
-    QkLayoutArray { size, array }
-}
-
-/// @ingroup QkTranspileLayout
-/// Free a QkLayoutArray
-///
-/// This function will free the memory allocation for an array pointed to by a ``QkLayoutArray``.
-///
-/// @param layout_array The QkLayoutArray object to free the array allocation of.
-///
-/// # Safety
-///
-/// Behavior is undefined if ``layout_array`` is not an QkLayoutArray object created by either
-/// ``qk_transpile_layout_final_layout()``, ``qk_transpile_layout_output_permutation()``, or
-/// ``qk_transpile_layout_initial_layout()``.
-#[no_mangle]
-#[cfg(feature = "cbinding")]
-pub unsafe extern "C" fn qk_layout_array_free(layout_array: QkLayoutArray) {
-    if layout_array.size == 0 && layout_array.array.is_null() {
-        return;
-    }
+    let result = layout.final_index_layout(filter_ancillas);
+    // SAFETY: Per the documentation final_layout must be a valid pointer with a sufficient
+    // allocation for the output array
     unsafe {
-        let data = std::slice::from_raw_parts_mut(layout_array.array, layout_array.size);
-        let _: Box<[u32]> = Box::from_raw(data);
+        let out_slice = std::slice::from_raw_parts_mut(final_layout, result.len());
+        out_slice
+            .iter_mut()
+            .zip(result.iter())
+            .for_each(|(dest, src)| *dest = src.0);
     }
 }
 
@@ -219,13 +261,64 @@ mod test {
         let result = layout.final_index_layout(true);
         let expected = vec![PhysicalQubit(3), PhysicalQubit(5), PhysicalQubit(2)];
         assert_eq!(expected, result);
-        let array_result = unsafe { qk_transpile_layout_final_layout(&layout, true) };
-        let slice = unsafe { std::slice::from_raw_parts(array_result.array, array_result.size) };
+        let mut result = vec![u32::MAX; layout.num_input_qubits() as usize];
+        unsafe { qk_transpile_layout_final_layout(&layout, true, result.as_mut_ptr()) };
         let expected = expected.into_iter().map(|x| x.0).collect::<Vec<_>>();
-        assert_eq!(expected.as_slice(), slice);
-        unsafe {
-            qk_layout_array_free(array_result);
-        }
+        assert_eq!(expected.as_slice(), result.as_slice());
+    }
+
+    #[test]
+    fn test_final_layout_no_filter() {
+        let initial_layout_vec = vec![
+            PhysicalQubit(9),
+            PhysicalQubit(4),
+            PhysicalQubit(0),
+            PhysicalQubit(1),
+            PhysicalQubit(2),
+            PhysicalQubit(3),
+            PhysicalQubit(5),
+            PhysicalQubit(6),
+            PhysicalQubit(7),
+            PhysicalQubit(8),
+        ];
+        let initial_layout = NLayout::from_virtual_to_physical(initial_layout_vec).unwrap();
+        let routing_permutation = vec![
+            Qubit(2),
+            Qubit(0),
+            Qubit(1),
+            Qubit(4),
+            Qubit(5),
+            Qubit(6),
+            Qubit(7),
+            Qubit(8),
+            Qubit(9),
+            Qubit(3),
+        ];
+        let input_qubits = vec![ShareableQubit::new_anonymous(); 10];
+        let layout = TranspileLayout::new(
+            Some(initial_layout),
+            Some(routing_permutation),
+            input_qubits,
+            3,
+        );
+        let result = layout.final_index_layout(false);
+        let expected = vec![
+            PhysicalQubit(3),
+            PhysicalQubit(5),
+            PhysicalQubit(2),
+            PhysicalQubit(0),
+            PhysicalQubit(1),
+            PhysicalQubit(4),
+            PhysicalQubit(6),
+            PhysicalQubit(7),
+            PhysicalQubit(8),
+            PhysicalQubit(9),
+        ];
+        assert_eq!(expected, result);
+        let mut result = vec![u32::MAX; layout.num_output_qubits() as usize];
+        unsafe { qk_transpile_layout_final_layout(&layout, false, result.as_mut_ptr()) };
+        let expected = expected.into_iter().map(|x| x.0).collect::<Vec<_>>();
+        assert_eq!(expected.as_slice(), result.as_slice());
     }
 
     #[test]
@@ -266,14 +359,64 @@ mod test {
             .into_iter()
             .map(|x| x.0)
             .collect();
-        let array_result = unsafe { qk_transpile_layout_initial_layout(&layout, true) };
-        let slice = unsafe { std::slice::from_raw_parts(array_result.array, array_result.size) };
-        assert_eq!(expected.as_slice(), slice);
-        unsafe { qk_layout_array_free(array_result) };
+        let mut result: Vec<u32> = vec![u32::MAX; layout.num_input_qubits() as usize];
+        assert!(unsafe { qk_transpile_layout_initial_layout(&layout, true, result.as_mut_ptr()) });
+        assert_eq!(expected.as_slice(), result.as_slice());
     }
 
     #[test]
-    fn test_routing_permutation() {
+    fn test_initial_layout_no_filter() {
+        let initial_layout_vec = vec![
+            PhysicalQubit(9),
+            PhysicalQubit(4),
+            PhysicalQubit(0),
+            PhysicalQubit(1),
+            PhysicalQubit(2),
+            PhysicalQubit(3),
+            PhysicalQubit(5),
+            PhysicalQubit(6),
+            PhysicalQubit(7),
+            PhysicalQubit(8),
+        ];
+        let expected: Vec<u32> = initial_layout_vec
+            .iter()
+            .map(|x| x.0)
+            .collect();
+        let initial_layout = NLayout::from_virtual_to_physical(initial_layout_vec).unwrap();
+        let routing_permutation = vec![
+            Qubit(2),
+            Qubit(0),
+            Qubit(1),
+            Qubit(4),
+            Qubit(5),
+            Qubit(6),
+            Qubit(7),
+            Qubit(8),
+            Qubit(9),
+            Qubit(3),
+        ];
+        let input_qubits = vec![ShareableQubit::new_anonymous(); 10];
+        let layout = TranspileLayout::new(
+            Some(initial_layout),
+            Some(routing_permutation),
+            input_qubits,
+            3,
+        );
+        let mut result: Vec<u32> = vec![u32::MAX; layout.num_output_qubits() as usize];
+        assert!(unsafe { qk_transpile_layout_initial_layout(&layout, false, result.as_mut_ptr()) });
+        assert_eq!(expected.as_slice(), result.as_slice());
+    }
+
+    #[test]
+    fn test_initial_layout_no_layout() {
+        let input_qubits = vec![ShareableQubit::new_anonymous(); 10000];
+        let layout = TranspileLayout::new(None, None, input_qubits, 10000);
+        let mut result: Vec<u32> = vec![u32::MAX; layout.num_input_qubits() as usize];
+        assert!(!unsafe { qk_transpile_layout_initial_layout(&layout, true, result.as_mut_ptr()) });
+    }
+
+    #[test]
+    fn test_output_permutation() {
         let initial_layout_vec = vec![
             PhysicalQubit(9),
             PhysicalQubit(4),
@@ -307,10 +450,17 @@ mod test {
             input_qubits,
             3,
         );
-        let array_result = unsafe { qk_transpile_layout_output_permutation(&layout) };
-        let slice = unsafe { std::slice::from_raw_parts(array_result.array, array_result.size) };
-        assert_eq!(expected.as_slice(), slice);
-        unsafe { qk_layout_array_free(array_result) };
+        let mut result: Vec<u32> = vec![u32::MAX; layout.num_output_qubits() as usize];
+        assert!(unsafe { qk_transpile_layout_output_permutation(&layout, result.as_mut_ptr()) });
+        assert_eq!(expected.as_slice(), result.as_slice());
+    }
+
+    #[test]
+    fn test_output_permutation_not_set() {
+        let input_qubits = vec![ShareableQubit::new_anonymous(); 10000];
+        let layout = TranspileLayout::new(None, None, input_qubits, 10000);
+        let mut result: Vec<u32> = vec![u32::MAX; layout.num_output_qubits() as usize];
+        assert!(!unsafe { qk_transpile_layout_output_permutation(&layout, result.as_mut_ptr()) });
     }
 
     #[test]
