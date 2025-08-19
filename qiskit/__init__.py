b3566ac547a379a81791c3e677e4d69e33b51627
Alexander Ivrii <alexi@il.ibm.com>
2025-08-19 18:25:57 +0300

    lint

--- qiskit/__init__.py
+++ qiskit/__init__.py
@@ -131,9 +131,7 @@ sys.modules["qiskit._accelerate.high_level_synthesis"] = _accelerate.high_level_
 sys.modules["qiskit._accelerate.remove_identity_equiv"] = _accelerate.remove_identity_equiv
 sys.modules["qiskit._accelerate.circuit_duration"] = _accelerate.circuit_duration
 sys.modules["qiskit._accelerate.cos_sin_decomp"] = _accelerate.cos_sin_decomp
-sys.modules["qiskit._accelerate.litinski_transformation"] = (
-    _accelerate.litinski_transformation
-)
+sys.modules["qiskit._accelerate.litinski_transformation"] = _accelerate.litinski_transformation
 sys.modules["qiskit._accelerate.unroll_3q_or_more"] = _accelerate.unroll_3q_or_more
 
 from qiskit.exceptions import QiskitError, MissingOptionalLibraryError
