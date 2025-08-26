8a91f98c2e7b8a3037c470a540f86cf8271f8033
Alexander Ivrii <alexi@il.ibm.com>
2025-08-26 18:50:21 +0300

    Burn inverse cancellation (#14840)
    * Add Python free path to inverse_cancellation
    
    This commit adds a Python free path to the inverse cancellation
    transpiler pass. Previously, the only way to run the pass required Python
    because the pass handles arbitrary gates to cancel which can include
    custom gates from Python. This meant to validate the gates were equal we
    would need the Python interpreter. This commit opts to add a new
    entrypoint for Rust as working under the constraints that we're only
    having StandardGates can lead to a more efficient implementation.
    Additionally, for the preset pass managers we always used the same fixed
    set of standard gates which don't take parameters to cancel. Since the
    initial goal of the C API is to expose the preset pass managers this new
    entrypoint only uses this set which simplifies the checking necessary.
    This provides an optimized path for the preset pass managers too. For the
    standalone C function in #14444 we don't need to make the pass
    configurable. If we decide eventually to expand this it is trivial to add
    an updated entrypoint, but for the first MVP it's not necessary.
    
    Since this new entrypoint is also more efficient for the use case of the
    preset pass managers the Python side pass is updated to use it in the
    default case. This enables the preset pass managers to run the pass
    faster.
    
    Related to #14444
    
    * Fix copy paste error in inverse pair path
    
    The inverse pair path was incorrectly checking the same gate twice
    instead of the gate and the next one in a run. This was just a copy and
    paste error where code was supposed to operate on n and n+1 but it was
    doing n twice. This commit fixes this and expands the testing to ensure
    we have direct coverage of the new code path instead of relying on the
    transpiler tests testing indirectly.
    
    * Apply suggestions from code review
    
    Co-authored-by: Alexander Ivrii <alexi@il.ibm.com>
    
    * Rename collect_1q_runs_by() -> collect_runs_by()
    
    * Fix short circuit conditions for inverse pair loop
    
    * adding inverse cancellation pass to the C API
    
    * Addressing review comments
    
    * applying review suggestions
    
    * typo
    
    ---------
    
    Co-authored-by: Matthew Treinish <mtreinish@kortar.org>

--- crates/cext/src/transpiler/passes/mod.rs
+++ crates/cext/src/transpiler/passes/mod.rs
@@ -12,6 +12,7 @@
 
 pub mod elide_permutations;
 pub mod gate_direction;
+pub mod inverse_cancellation;
 pub mod remove_diagonal_gates_before_measure;
 pub mod remove_identity_equiv;
 pub mod sabre_layout;
