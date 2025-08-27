edcfe321936c3c9d75cb213df6415b385aebfa7b
Phalak <133593335+phalakbh@users.noreply.github.com>
2025-08-27 20:38:52 +0530

    Fix lint

--- crates/transpiler/src/passes/mod.rs
+++ crates/transpiler/src/passes/mod.rs
@@ -22,8 +22,8 @@
 //! for building Python submodules.
 
 mod alap_schedule_analysis;
-mod asap_schedule_analysis;
 mod apply_layout;
+mod asap_schedule_analysis;
 mod barrier_before_final_measurement;
 mod basis_translator;
 mod check_map;
@@ -50,8 +50,8 @@ mod unroll_3q_or_more;
 mod vf2;
 
 pub use alap_schedule_analysis::{alap_schedule_analysis_mod, run_alap_schedule_analysis};
-pub use asap_schedule_analysis::{asap_schedule_analysis_mod, run_asap_schedule_analysis};
 pub use apply_layout::{apply_layout, apply_layout_mod, update_layout};
+pub use asap_schedule_analysis::{asap_schedule_analysis_mod, run_asap_schedule_analysis};
 pub use barrier_before_final_measurement::{
     barrier_before_final_measurements_mod, run_barrier_before_final_measurements,
 };
