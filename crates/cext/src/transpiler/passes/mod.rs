34e9f03933eb6db31a41846a51f1ece36d271690
Matthew Treinish <mtreinish@kortar.org>
2025-08-29 05:37:16 -0400

    Add CommutativeCancellation standalone transpiler pass to C API (#14831)
    * Add CommutativeCancellation standalone transpiler pass to C API
    
    This commit adds a standalone transpiler pass function to the C API for
    running the `CommutativeCancellation` transpiler pass. This pass mutates
    the dag in place if there are any gates to be cancelled.
    
    Fixes #14446
    
    * Apply suggestions from code review
    
    Co-authored-by: Julien Gacon <gaconju@gmail.com>
    
    * Use ExitCode instead of returning raw int
    
    * Move approximation_degree check before dag conversion
    
    * Update example with valid C
    
    * Fix rust test
    
    * Apply suggestions from code review
    
    Co-authored-by: Julien Gacon <gaconju@gmail.com>
    
    ---------
    
    Co-authored-by: Julien Gacon <gaconju@gmail.com>
    Co-authored-by: Alexander Ivrii <alexi@il.ibm.com>

--- crates/cext/src/transpiler/passes/mod.rs
+++ crates/cext/src/transpiler/passes/mod.rs
@@ -10,6 +10,7 @@
 // copyright notice, and modified files need to carry a notice indicating
 // that they have been altered from the originals.
 
+pub mod commutative_cancellation;
 pub mod elide_permutations;
 pub mod gate_direction;
 pub mod inverse_cancellation;
