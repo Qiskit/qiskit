bc1b103bb8d423f608856ba43893c600029bd06a
Kevin Hartman <kevin@hart.mn>
2025-09-26 15:06:49 -0400

    Remove InstructionView and friends.

--- crates/circuit/src/instruction.rs
+++ crates/circuit/src/instruction.rs
@@ -10,17 +10,11 @@
 // copyright notice, and modified files need to carry a notice indicating
 // that they have been altered from the originals.
 
-use crate::circuit_data::CircuitData;
-use crate::operations::{
-    BoxDuration, CaseSpecifier, Condition, DelayUnit, OperationRef, Param, PyGate, PyInstruction,
-    PyOperation, StandardGate, SwitchTarget, UnitaryGate,
-};
-use nalgebra::Matrix2;
+use crate::operations::{BoxDuration, CaseSpecifier, Condition, OperationRef, Param, SwitchTarget};
 use ndarray::Array2;
 use num_complex::Complex64;
 use pyo3::PyObject;
 use smallvec::SmallVec;
-use std::ops::Deref;
 
 // TODO: rename this to PythonOperation
 /// Represents an instruction that is directly convertible to our Python API
@@ -121,58 +115,6 @@ impl<T> Parameters<T> {
 }
 
 #[derive(Clone, Debug)]
-pub enum InstructionView<'a, T> {
-    ControlFlow(ControlFlowView<'a, T>),
-    StandardGate(StandardGateView<'a>),
-    StandardInstruction(StandardInstructionView<'a>),
-    Gate(&'a PyGate),
-    Instruction(&'a PyInstruction),
-    Operation(&'a PyOperation),
-    Unitary(UnitaryGateView<'a>),
-}
-
-impl<T> InstructionView<'_, T> {
-    pub fn try_matrix(&self) -> Option<Array2<Complex64>> {
-        match self {
-            InstructionView::StandardGate(g) => g.matrix(),
-            InstructionView::Gate(g) => g.matrix(),
-            InstructionView::Unitary(u) => u.matrix(),
-            _ => None,
-        }
-    }
-
-    /// Returns a static matrix for 1-qubit gates. Will return `None` when the gate is not 1-qubit.
-    #[inline]
-    pub fn try_matrix_as_static_1q(&self) -> Option<[[Complex64; 2]; 2]> {
-        match self {
-            Self::StandardGate(standard) => standard.matrix_as_static_1q(),
-            Self::Gate(gate) => gate.matrix_as_static_1q(),
-            Self::Unitary(unitary) => unitary.matrix_as_static_1q(),
-            _ => None,
-        }
-    }
-
-    pub fn try_matrix_as_nalgebra_1q(&self) -> Option<Matrix2<Complex64>> {
-        match self {
-            InstructionView::Unitary(u) => u.matrix_as_nalgebra_1q(),
-            // default implementation
-            _ => self
-                .try_matrix_as_static_1q()
-                .map(|arr| Matrix2::new(arr[0][0], arr[0][1], arr[1][0], arr[1][1])),
-        }
-    }
-
-    pub fn try_definition(&self) -> Option<CircuitData> {
-        match self {
-            InstructionView::StandardGate(g) => g.definition(),
-            InstructionView::Gate(g) => g.definition(),
-            InstructionView::Instruction(i) => i.definition(),
-            _ => None,
-        }
-    }
-}
-
-#[derive(Clone, Debug)]
 pub enum ControlFlowView<'a, T> {
     Box(Option<&'a BoxDuration>, &'a T),
     BreakLoop,
@@ -221,61 +163,5 @@ impl<'a, T> ControlFlowView<'a, T> {
             ControlFlowView::While { body, .. } => vec![*body],
         }
         .into_iter()
-    }
-}
-
-#[derive(Clone, Debug)]
-pub enum StandardInstructionView<'a> {
-    Barrier(u32),
-    Delay {
-        duration: &'a Param,
-        unit: DelayUnit,
-    },
-    Measure,
-    Reset,
-}
-
-#[derive(Clone, Debug)]
-pub struct StandardGateView<'a>(pub StandardGate, pub &'a [Param]);
-
-impl<'a> StandardGateView<'a> {
-    #[inline]
-    pub fn gate(&self) -> &StandardGate {
-        &self.0
-    }
-
-    #[inline]
-    pub fn params(&self) -> &'a [Param] {
-        self.1
-    }
-
-    #[inline]
-    pub fn matrix(&self) -> Option<Array2<Complex64>> {
-        self.0.matrix(self.1)
-    }
-
-    #[inline]
-    fn matrix_as_static_1q(&self) -> Option<[[Complex64; 2]; 2]> {
-        self.0.matrix_as_static_1q(self.1)
-    }
-
-    #[inline]
-    pub fn definition(&self) -> Option<CircuitData> {
-        self.0.definition(self.1)
-    }
-
-    #[inline]
-    pub fn inverse(&self) -> Option<(StandardGate, SmallVec<[Param; 3]>)> {
-        self.0.inverse(self.1)
-    }
-}
-
-#[derive(Clone, Debug)]
-pub struct UnitaryGateView<'a>(pub &'a UnitaryGate);
-
-impl Deref for UnitaryGateView<'_> {
-    type Target = UnitaryGate;
-    fn deref(&self) -> &Self::Target {
-        self.0
     }
 }
