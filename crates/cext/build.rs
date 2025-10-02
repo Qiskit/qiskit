76ce79c92e874825f2d84df6dec98d8367087bfa
Julien Gacon <jules.gacon@googlemail.com>
2025-10-02 17:04:54 +0200

    Use tmp buffer
    and assume we can store the full header file as String (which should be fine as long as it doesn't get humongous)

--- crates/cext/build.rs
+++ crates/cext/build.rs
@@ -30,42 +30,29 @@ fn generate_qiskit_header() {
     // Ensure target path exists and then set the full filename of qiskit.h.
     let mut path = ::std::path::PathBuf::from_str("../../target/").unwrap();
     ::std::fs::create_dir_all(&path).expect("Could not create target directory.");
-    // We use a temporary file for cbindgen, which we post-process to fix renamings we
-    // don't want (like PyObject being renamed for QkPyObject).
-    let mut tmp_header = path.clone();
-    tmp_header.push("__tmp_qiskit.h");
-
     path.push("qiskit.h");
 
     // Build the header.
+    let mut buffer: Vec<u8> = Vec::new();
     cbindgen::Builder::new()
         .with_crate(".")
         .with_config(config)
         .generate()
         .expect("Unable to generate C bindings.")
-        .write_to_file(&tmp_header);
+        .write(&mut buffer);
 
-    // Undo cbindgen's renaming of QkPyObject to PyObject
-    let final_file = File::create(&path).expect("The qiskit.h path should exist.");
-    let mut final_buffer = BufWriter::new(final_file);
-    let tmp_file = File::open(&tmp_header).expect("The tmp path should exist.");
-    let tmp_buffer = BufReader::new(tmp_file);
-
-    // we currently only have a single target-replacement pair, this could be promoted to a
-    // HashMap if we have more than that
+    // Undo cbindgen's renaming of QkPyObject to PyObject. We currently only have a single
+    // target-replacement pair, this could be promoted to a HashMap if we have more than that.
     let target = "QkPyObject";
     let replacement = "PyObject";
+    let header = String::from_utf8(buffer)
+        .expect("Unable to parse C bindings.")
+        .replace(target, replacement);
 
-    // Check each line and replace the target words
-    for line in tmp_buffer.lines() {
-        let mut line = line.expect("The tmp header file should be readable");
-        if line.contains(target) {
-            line = line.replace(target, replacement);
-        }
-        writeln!(final_buffer, "{}", line).expect("Failed writing to qiskit.h");
-    }
-    final_buffer.flush().expect("Failed writing to qiskit.h");
-    ::std::fs::remove_file(tmp_header).expect("Failed deleting tmp header.");
+    let mut header_file = File::create(&path).expect("The qiskit.h path should exist.");
+    header_file
+        .write(header.as_bytes())
+        .expect("Unable to write header.");
 }
 
 fn main() {
