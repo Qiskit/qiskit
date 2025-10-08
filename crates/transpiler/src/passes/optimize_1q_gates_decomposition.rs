83b762d31c8127b77636b38fd0ef91cd3124f472
Matthew Treinish <mtreinish@kortar.org>
2025-10-08 11:22:43 -0400

    Check target gates are parameterized in optimize1qgatesdecomposition (#15131)
    * Check target gates are parameterized in optimize1qgatesdecomposition
    
    This commit updates the Optimize1qGatesDecomposition transpiler pass to
    check the defined operation in the target to ensure it's a valid gate to
    use for the decomposition. Previously the pass would just use the gate
    name for each qubit and treat that as a basis gate list. However this
    ignored the extra details that the target provides as a target gate can
    match the name but not be valid for use in synthesis. This commit fixes
    that oversight by checking the gates directly and ensuring they're valid
    for use in the decomposition.
    
    Fixes #14743
    
    * Fix release note typos
    
    Co-authored-by: Jake Lishman <jake@binhbar.com>
    
    * Simplify logic in pass
    
    Since this PR is intended to be backported to 2.2.x and 1.4.x we should
    minimize the diff. The previous version of this PR changed the behavior
    of the pass to normalize on using standard gates even if there were
    custom names for gates in the target. Longer term I think this behavior
    is better for the pass, but there is discussions around exactly the
    semantics of the Target's data model around names vs operations here and
    also that this might change the output pretty noticeably in some cases.
    For a bugfix intended for backport it's better to be conservative here,
    so this commit reverts the logic to just use the name in the target and
    only filter operations if the target doesn't actually support arbitrary
    angles on them. We can revisit the improvements in a follow up PR.
    
    ---------
    
    Co-authored-by: Jake Lishman <jake@binhbar.com>

--- crates/transpiler/src/passes/optimize_1q_gates_decomposition.rs
+++ crates/transpiler/src/passes/optimize_1q_gates_decomposition.rs
@@ -20,9 +20,9 @@ use ndarray::prelude::*;
 use rustworkx_core::petgraph::stable_graph::NodeIndex;
 
 use qiskit_circuit::dag_circuit::{DAGCircuit, NodeType};
-use qiskit_circuit::operations::{Operation, Param};
+use qiskit_circuit::operations::{Operation, OperationRef, Param};
 
-use crate::target::Target;
+use crate::target::{Target, TargetOperation};
 use qiskit_circuit::{PhysicalQubit, gate_matrix};
 use qiskit_synthesis::euler_one_qubit_decomposer::{
     EULER_BASES, EULER_BASIS_NAMES, EulerBasis, EulerBasisSet, OneQubitGateSequence,
@@ -76,7 +76,30 @@ pub fn run_optimize_1q_gates_decomposition(
         };
         if basis_gates_per_qubit[qubit.index()].is_none() {
             let basis_gates = match target {
-                Some(target) => Some(target.operation_names_for_qargs(&[qubit])?),
+                Some(target) => Some(
+                    target
+                        .operation_names_for_qargs(&[qubit])?
+                        .into_iter()
+                        .filter(|gate_name| {
+                            let target_op = target.operation_from_name(gate_name).unwrap();
+                            let TargetOperation::Normal(gate) = target_op else {
+                                return false;
+                            };
+                            if let OperationRef::StandardGate(_) = gate.operation.view() {
+                                // For standard gates check that the target entry accepts any
+                                // params and if so then we can use the gate in the pass
+                                // else filter the operation since arbitrary angles are not
+                                // supported
+                                gate.params
+                                    .iter()
+                                    .all(|x| matches!(x, Param::ParameterExpression(_)))
+                            } else {
+                                // For all other gates pass it through
+                                true
+                            }
+                        })
+                        .collect(),
+                ),
                 None => {
                     let basis = basis_gates.as_ref();
                     basis.map(|basis| basis.iter().map(|x| x.as_str()).collect())
