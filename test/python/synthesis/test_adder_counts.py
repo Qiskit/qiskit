6f8cc0313808fc5088916c35e7029382abff2524
Jake Lishman <jake.lishman@ibm.com>
2025-10-20 22:31:09 +0100

    Update to Black 2025 (#15225)
    We're only 9 months late on this one...
    
    The vast majority of changes are just because `black` is now better at
    fixing up spacing in docstrings.  The other couple of changes are
    removing unnecessary parentheses and being more consistent with trailing
    commas on split function-call parameter lists.
    
    There's fairly little in here that should cause any significant problems
    with existing PRs.

--- test/python/synthesis/test_adder_counts.py
+++ test/python/synthesis/test_adder_counts.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-""" Test gate counts of synthesis algorithms for adder circuits."""
+"""Test gate counts of synthesis algorithms for adder circuits."""
 
 from __future__ import annotations
 import unittest
