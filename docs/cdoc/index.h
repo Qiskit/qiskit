932aedde83c35de4806b283652fb299dacdb56a0
Jake Lishman <jake.lishman@ibm.com>
2025-10-28 15:44:03 +0000

    Fail on warnings in Doxygen (#15238)
    * Fail on warnings in Doxygen
    
    We were already pretty clean from within Doxygen, but this did uncover a
    couple of places where we're misnamed the parameters.
    
    * Fix configuration typo
    
    Co-authored-by: Raynel Sanchez <87539502+raynelfss@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Raynel Sanchez <87539502+raynelfss@users.noreply.github.com>

--- docs/cdoc/index.h
+++ docs/cdoc/index.h
@@ -1,71 +1,25 @@
-/**
- * @defgroup QkObs QkObs
- */
-
-/**
- * @defgroup QkObsTerm QkObsTerm
- */
+// The syntax is
+//      @defgroup <groupname> <description of the group>
+// but we don't usually bother much with the description.
 
 /**
  * @defgroup QkBitTerm QkBitTerm
- */
-
-/**
- * @defgroup QkComplex64 QkComplex64
- */
-
-/**
  * @defgroup QkCircuit QkCircuit
- */
-
-/**
- * @defgroup QkQuantumRegister QkQuantumRegister
- */
-
-/**
- * @defgroup QkClassicalRegister QkClassicalRegister
- */
-
-/**
- * @defgroup QkTarget QkTarget
- */
-
-/**
- * @defgroup QkTargetEntry QkTargetEntry
- */
-
-/**
- * @defgroup QkNeighbors QkNeighbors
- */
-
-/**
- * @defgroup QkDag QkDag
- */
-
-/**
- * @defgroup QkTranspilerPasses QkTranspilerPasses
- */
-
-/**
- * @defgroup QkVF2LayoutResult QkVF2LayoutResult
- */
-
-/**
- * @defgroup QkVF2LayoutConfiguration QkVF2LayoutConfiguration
- */
-
-/**
- * @defgroup QkTranspileLayout QkTranspileLayout
- */
-
-/**
- * @defgroup QkSabreLayoutOptions QkSabreLayoutOptions
- */
-
-/**
- * @defgroup QkTranspiler QkTranspiler
- */
-
-/**
  * @defgroup QkCircuitLibrary QkCircuitLibrary
+ * @defgroup QkClassicalRegister QkClassicalRegister
+ * @defgroup QkComplex64 QkComplex64
+ * @defgroup QkDag QkDag
+ * @defgroup QkNeighbors QkNeighbors
+ * @defgroup QkObs QkObs
+ * @defgroup QkObsTerm QkObsTerm
+ * @defgroup QkParam QkParam
+ * @defgroup QkQuantumRegister QkQuantumRegister
+ * @defgroup QkSabreLayoutOptions QkSabreLayoutOptions
+ * @defgroup QkTarget QkTarget
+ * @defgroup QkTargetEntry QkTargetEntry
+ * @defgroup QkTranspileLayout QkTranspileLayout
+ * @defgroup QkTranspiler QkTranspiler
+ * @defgroup QkTranspilerPasses QkTranspilerPasses
+ * @defgroup QkVF2LayoutConfiguration QkVF2LayoutConfiguration
+ * @defgroup QkVF2LayoutResult QkVF2LayoutResult
  */
