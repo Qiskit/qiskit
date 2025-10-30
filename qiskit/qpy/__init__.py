02cd80fe5ad79f1a2dbc853b9dbbcb6a64a63602
kaldag <kalyan.dasgupta@gmail.com>
2025-10-31 00:22:53 +0530

    version correction

--- qiskit/qpy/__init__.py
+++ qiskit/qpy/__init__.py
@@ -199,9 +199,12 @@ of QPY in qiskit-terra 0.18.0.
    * - Qiskit (qiskit-terra for < 1.0.0) version
      - :func:`.dump` format(s) output versions
      - :func:`.load` maximum supported version (older format versions can always be read)
-   * - 2.2.3
+   * - 2.3.0.dev0
      - 13, 14, 15, 16, 17
      - 17
+   * - 2.2.3
+     - 13, 14, 15, 16
+     - 16
    * - 2.2.2
      - 13, 14, 15, 16
      - 16
