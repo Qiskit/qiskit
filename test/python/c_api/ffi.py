160792f9214ccc4b5e15b6946daccff1e3e08d67
Matthew Treinish <mtreinish@kortar.org>
2025-11-06 13:39:11 -0500

    Update test/python/c_api/ffi.py
    Co-authored-by: Julien Gacon <gaconju@gmail.com>

--- test/python/c_api/ffi.py
+++ test/python/c_api/ffi.py
@@ -153,7 +153,7 @@ def build_circuit_from_python(circuit: qiskit.circuit.QuantumCircuit) -> ctypes.
 
 
 def build_homogenous_target(
-    cmap: qiskit.transpiler.CouplingMap, basis_gates: list[str], seed: int, ideal_gates=False
+    cmap: qiskit.transpiler.CouplingMap, basis_gates: list[str], seed: int, ideal_gates: bool = False
 ) -> QkTarget:
     """Build a target with a homogenous gate set."""
 
