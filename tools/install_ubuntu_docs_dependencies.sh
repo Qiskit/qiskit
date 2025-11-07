11bf86cd34f9fe0dcfde34b8623a9e397e235bd1
Raynel Sanchez <raynelfss@hotmail.com>
2025-11-07 15:43:04 -0500

    FIx: Try to cd correctly

--- tools/install_ubuntu_docs_dependencies.sh
+++ tools/install_ubuntu_docs_dependencies.sh
@@ -5,22 +5,22 @@
 if [ $GITHUB_TOKEN ]; then
     set -e
 
-    python -m pip install --upgrade pip setuptools wheel
-    python -m pip install --upgrade tox
+    # python -m pip install --upgrade pip setuptools wheel
+    # python -m pip install --upgrade tox
 
-    sudo apt-get update
-    sudo apt-get install -y graphviz pandoc 
+    # sudo apt-get update
+    # sudo apt-get install -y graphviz pandoc 
 
     # This command fetches the latest release of doxygen and its linux binaries
-    curl -H "Authorization: token $GITHUB_TOKEN" \
-        https://api.github.com/repos/doxygen/doxygen/releases/latest | \
-        jq '.["assets"][2]["browser_download_url"]' | \
-        xargs -I {} wget {} -O ./doxygen.tar.gz
+    # curl -H "Authorization: token $GITHUB_TOKEN" \
+    #     https://api.github.com/repos/doxygen/doxygen/releases/latest | \
+    #     jq '.["assets"][2]["browser_download_url"]' | \
+    #     xargs -I {} wget {} -O ./doxygen.tar.gz
 
     # The following commands install the binaries for doxygen
     tar -zxvf ./doxygen.tar.gz
     # Find the doxygen folder and pick the first option
-    find . -type d | grep doxygen -m 1 | xargs -I {} cd {}
+    cd "$(find . -type d | grep doxygen -m 1)"
     # Run the remainder of the setup process
     ./configure
     make
