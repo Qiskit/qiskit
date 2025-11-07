a041e0f6b8a96360f16129bd695d873b5d9ce263
Raynel Sanchez <raynelfss@hotmail.com>
2025-11-07 14:20:27 -0500

    Fix: Handle receipt of github token.

--- tools/install_ubuntu_docs_dependencies.sh
+++ tools/install_ubuntu_docs_dependencies.sh
@@ -2,26 +2,28 @@
 #
 # Prepare an Ubuntu CI machine for running 'tox -e docs'.  Assumes that Python is available.
 
-set -e
+if [[ -n "$1" ]]
+then
+    set -e
 
-python -m pip install --upgrade pip setuptools wheel
-python -m pip install --upgrade tox
+    python -m pip install --upgrade pip setuptools wheel
+    python -m pip install --upgrade tox
 
-sudo apt-get update
-sudo apt-get install -y graphviz pandoc 
+    sudo apt-get update
+    sudo apt-get install -y graphviz pandoc 
 
-# This command fetches the latest release of doxygen and its linux binaries
-curl -H "Authorization: token $$0" \
-    https://api.github.com/repos/doxygen/doxygen/releases/latest | \
-    jq '.["assets"][2]["browser_download_url"]' | \
-    xargs -I {} wget {} -O ./doxygen.tar.gz
+    # This command fetches the latest release of doxygen and its linux binaries
+    curl -H "Authorization: token $$1" \
+        https://api.github.com/repos/doxygen/doxygen/releases/latest | \
+        jq '.["assets"][2]["browser_download_url"]' | \
+        xargs -I {} wget {} -O ./doxygen.tar.gz
 
-# The following commands install the binaries for doxygen
-tar -zxvf ./doxygen.tar.gz
-cd ./doxygen
-./configure
-make
-sudo make install
-
-
-
+    # The following commands install the binaries for doxygen
+    tar -zxvf ./doxygen.tar.gz
+    cd ./doxygen
+    ./configure
+    make
+    sudo make install
+else
+    echo "No github token provided"
+fi
