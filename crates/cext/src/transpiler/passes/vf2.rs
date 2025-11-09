70ef8e85f642557d3e559840ea87c3341540d80d
Jake Lishman <jake.lishman@ibm.com>
2025-11-09 14:31:25 +0100

    Split VF2 `call_limit` to before and after first layout (#14862)
    * Split VF2 `call_limit` to before and after first layout
    
    With the new VF2 scoring algorithm, the `max_trials` option is no longer
    appropriate as a limit, but is one that we functionally relied on for
    good runtimes in the face of high numbers of isomorphisms.  This splits
    the `call_limit` functionality in two, so that a separate (likely lower)
    limit can be placed on the time spent improving the layout, while
    retaining a high call limit for the initial search.
    
    * Fix call-limit unbound conversion
    
    * Remove unnecessary pub
    
    * Revert separation of O1 and O2 VF2 configuration
    
    That shouldn't be being done in this PR, if at all.
    
    * Clarify `call_limit` meaning
    
    Previously it was not clear that it's just two limits and we swap from
    one to the other at the point of the first match.  The previous switch
    was also not entirely correct, since swapping from a `None` to a `Some`
    limit would not have counted calls already made.

--- crates/cext/src/transpiler/passes/vf2.rs
+++ crates/cext/src/transpiler/passes/vf2.rs
@@ -162,10 +162,18 @@ pub unsafe extern "C" fn qk_vf2_layout_configuration_free(config: *mut VF2Layout
     }
 }
 /// @ingroup QkVF2LayoutConfiguration
-/// Limit the numbers of times that the VF2 algorithm will attempt to extend its mapping.
+/// Limit the numbers of times that the VF2 algorithm will attempt to extend its mapping before and
+/// after it finds the first match.
+///
+/// The VF2 algorithm keeps track of the number of steps it has taken, and terminates when it
+/// reaches the limit.  After the first match is found, the limit swaps from the "before" limit to
+/// the "after" limit without resetting the number of steps taken.
 ///
 /// @param config The configuration to update.
-/// @param limit The number of attempts to allow.  Set to a negative number to have no bound.
+/// @param before The number of attempts to allow before the first match is found.  Set to a
+///     negative number to have no bound.
+/// @param after The number of attempts to allow after the first match (if any) is found.  Set to a
+///     negative number to have no bound.
 ///
 /// # Safety
 ///
@@ -175,13 +183,12 @@ pub unsafe extern "C" fn qk_vf2_layout_configuration_free(config: *mut VF2Layout
 #[cfg(feature = "cbinding")]
 pub unsafe extern "C" fn qk_vf2_layout_configuration_set_call_limit(
     config: *mut VF2LayoutConfiguration,
-    limit: i64,
+    before: i64,
+    after: i64,
 ) {
-    let lift = |limit: i64| -> Option<usize> {
-        (limit > 0).then(|| limit.try_into().unwrap_or(usize::MAX))
-    };
+    let lift = |limit: i64| usize::try_from(limit).ok();
     // SAFETY: per documentation this is a valid configuration pointer.
-    unsafe { (*config).0.call_limit = lift(limit) };
+    unsafe { (*config).0.call_limit = (lift(before), lift(after)) };
 }
 /// @ingroup QkVF2LayoutConfiguration
 /// Limit the runtime of the VF2 search.
@@ -311,7 +318,7 @@ pub unsafe extern "C" fn qk_vf2_layout_configuration_set_shuffle_seed(
 ///         }
 ///     }
 ///     QkVF2LayoutConfiguration *config = qk_vf2_layout_configuration_new();
-///     qk_vf2_layout_configuration_set_call_limit(config, 10000);
+///     qk_vf2_layout_configuration_set_call_limit(config, 10000, 10000);
 ///     QkVF2LayoutResult *layout_result = qk_transpiler_pass_standalone_vf2_layout(qc, target, config, false);
 ///     qk_vf2_layout_result_free(layout_result);
 ///     qk_vf2_layout_configuration_free(config);
