40064654f019d7a95774c978a3245994a678b543
Raynel Sanchez <raynelfss@hotmail.com>
2025-11-10 08:30:02 -0500

    Fix: Review comments - Hardcode binaries from script - Remove unnecessary indentation in favor of a quick exit. - Remove duplicate calls to make.

--- tools/install_ubuntu_docs_dependencies.sh
+++ tools/install_ubuntu_docs_dependencies.sh
@@ -2,30 +2,25 @@
 #
 # Prepare an Ubuntu CI machine for running 'tox -e docs'.  Assumes that Python is available.
 
-if [ $GITHUB_TOKEN ]; then
-    set -e
-
-    python -m pip install --upgrade pip setuptools wheel
-    python -m pip install --upgrade tox
-
-    sudo apt-get update
-    sudo apt-get install -y graphviz pandoc 
-
-    # This command fetches the latest release of doxygen and its linux binaries
-    curl -H "Authorization: token $GITHUB_TOKEN" \
-        https://api.github.com/repos/doxygen/doxygen/releases/latest | \
-        jq '.["assets"][2]["browser_download_url"]' | \
-        xargs -I {} wget {} -O ./doxygen.tar.gz
-
-    # The following commands install the binaries for doxygen
-    tar -zxvf ./doxygen.tar.gz
-    # Find the doxygen folder
-    DOXYGEN_DIR=$(find . -maxdepth 1 -type d  -name "doxygen*")
-    cd $DOXYGEN_DIR
-    # Run the remainder of the setup process
-    sudo make
-    sudo make install
-else
+set -e
+if [ -z $GITHUB_TOKEN ]; then
     echo "No github token provided"
     exit 1
 fi
+
+python -m pip install --upgrade pip setuptools wheel
+python -m pip install --upgrade tox
+
+sudo apt-get update
+sudo apt-get install -y graphviz pandoc 
+
+# This command fetches the latest release of doxygen and its linux binaries
+wget --header "Authorization: token $GITHUB_TOKEN" \
+    https://github.com/doxygen/doxygen/releases/download/Release_1_15_0/doxygen-1.15.0.linux.bin.tar.gz
+
+# The following commands install the binaries for doxygen
+tar -zxvf ./doxygen-1.15.0.linux.bin.tar.gz
+cd ./doxygen-1.15.0
+
+# Run the remainder of the setup process
+sudo make install
