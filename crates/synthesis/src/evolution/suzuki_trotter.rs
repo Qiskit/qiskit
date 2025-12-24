abff9020534f9c30acb57bed8ded33d93b9387e0
estebanrg21 <estebanrg21@icloud.com>
2025-12-24 13:23:24 -0600

    Fix evolution parameter type and change terms type to SparseTermView

--- crates/synthesis/src/evolution/suzuki_trotter.rs
+++ crates/synthesis/src/evolution/suzuki_trotter.rs
@@ -13,40 +13,19 @@
 use ahash::HashSet;
 use indexmap::IndexMap;
 use itertools::Itertools;
-use num_complex::Complex64;
-use qiskit_quantum_info::sparse_observable::BitTerm;
+use qiskit_quantum_info::sparse_observable::SparseTermView;
 use rustworkx_core::coloring::{ColoringStrategy, greedy_node_color_with_coloring_strategy};
 use rustworkx_core::petgraph::graph::NodeIndex;
 use rustworkx_core::petgraph::{Graph, Undirected};
 use std::convert::Infallible;
 use std::usize;
 
-// Helper struct for pauli evolution ease of use
-#[derive(Clone, Debug)]
-pub struct StrSparseTerm<'a> {
-    pub terms: String,
-    pub coeff: Complex64,
-    pub indices: &'a [u32],
-}
-
-pub fn bit_term_as_char(bit_term: &BitTerm) -> char {
-    match bit_term {
-        BitTerm::X => 'X',
-        BitTerm::Plus => '+',
-        BitTerm::Minus => '-',
-        BitTerm::Y => 'Y',
-        BitTerm::Right => 'r',
-        BitTerm::Left => 'l',
-        BitTerm::Z => 'Z',
-        BitTerm::Zero => '0',
-        BitTerm::One => '1',
-    }
-}
-
-pub fn evolution(order: u32, mut paulis: Vec<StrSparseTerm>) -> Vec<StrSparseTerm> {
+pub fn evolution<'a, 'b>(order: u32, paulis: &'b [SparseTermView<'a>]) -> Vec<SparseTermView<'a>> {
     return match order {
-        1 => paulis,
+        1 => paulis.to_vec(),
         2 => {
+            let mut paulis = paulis.to_vec();
+
             let last = paulis.pop();
 
             paulis.iter_mut().for_each(|view| {
@@ -66,24 +45,25 @@ pub fn evolution(order: u32, mut paulis: Vec<StrSparseTerm>) -> Vec<StrSparseTer
         }
         _ => {
             let reduction = 1.0 / (4.0 - 4_f64.powf(1.0 / (order as f64 - 1.0)));
+            let mut paulis = paulis.to_vec();
             let mut inner = paulis.clone();
 
             paulis.iter_mut().for_each(|view| {
                 view.coeff.re = view.coeff.re * reduction as f64;
             });
-            paulis = evolution(order - 2, paulis);
+            let mut paulis = evolution(order - 2, &paulis);
             paulis = paulis
                 .iter()
                 .cycle()
                 .take(paulis.len() * 2)
-                .map(|v| v.clone())
+                .cloned()
                 .collect();
             let mut outer = paulis.clone();
 
             inner.iter_mut().for_each(|view| {
                 view.coeff.re = view.coeff.re * (1.0 - 4.0 * reduction) as f64;
             });
-            inner = evolution(order - 2, inner);
+            let mut inner = evolution(order - 2, &inner);
 
             paulis.append(&mut inner);
             paulis.append(&mut outer);
@@ -93,13 +73,13 @@ pub fn evolution(order: u32, mut paulis: Vec<StrSparseTerm>) -> Vec<StrSparseTer
     };
 }
 
-pub fn reorder_terms<'a>(terms: &'a Vec<StrSparseTerm<'a>>) -> Vec<StrSparseTerm<'a>> {
-    let sorted: Vec<&StrSparseTerm> = terms
+pub fn reorder_terms<'a>(terms: &'a Vec<SparseTermView<'a>>) -> Vec<SparseTermView<'a>> {
+    let sorted: Vec<&SparseTermView> = terms
         .iter()
-        .sorted_by_key(|view| (view.indices, view.terms.clone()))
+        .sorted_by_key(|view| (view.indices, view.bit_terms))
         .collect();
 
-    let mut graph: Graph<&StrSparseTerm, Option<u8>, Undirected> = Graph::new_undirected();
+    let mut graph: Graph<&SparseTermView, Option<u8>, Undirected> = Graph::new_undirected();
 
     for term in sorted {
         graph.add_node(term);
