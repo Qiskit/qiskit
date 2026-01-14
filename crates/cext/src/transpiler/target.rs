e8ae1d961f82e31075f729c9a5bfc1ce72fe12e6
Raynel Sanchez <raynelfss@hotmail.com>
2026-01-14 13:31:07 -0500

    Fix: Adapt to c changes

--- crates/cext/src/transpiler/target.rs
+++ crates/cext/src/transpiler/target.rs
@@ -45,18 +45,7 @@ use smallvec::{SmallVec, smallvec};
 ///
 #[unsafe(no_mangle)]
 pub extern "C" fn qk_target_new(num_qubits: u32) -> *mut Target {
-    let target = Target::new(
-        None,
-        Some(num_qubits),
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-        None,
-    )
-    .unwrap();
+    let target = Target::new().with_num_qubits(num_qubits);
     Box::into_raw(Box::new(target))
 }
 
