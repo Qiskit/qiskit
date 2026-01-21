642fb0e6a82858ac8f53be6a997333a786f40dcc
Shelly Garion <shelly@il.ibm.com>
2026-01-21 11:52:03 +0200

    fix comment

--- test/python/transpiler/test_pbc_transformation.py
+++ test/python/transpiler/test_pbc_transformation.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Test Substitute Pi/4-Rotations optimization pass"""
+"""Test PBCTransformation optimization pass"""
 
 from ddt import ddt
 
