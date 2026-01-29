5b65ac4726e34cb07f54c929fe7f485938f67070
Shelly Garion <shelly@il.ibm.com>
2026-01-29 19:03:41 +0200

    simplify tests using get_standard_gate_name_mapping

--- test/python/transpiler/test_pbc_transformation.py
+++ test/python/transpiler/test_pbc_transformation.py
@@ -19,53 +19,13 @@ from qiskit.transpiler import TranspilerError
 from qiskit.transpiler.passes import PBCTransformation
 from qiskit.quantum_info import Operator
 from qiskit.circuit.random import random_circuit
+from qiskit.circuit.library.standard_gates import get_standard_gate_name_mapping
 from qiskit.circuit.library import (
-    RXGate,
-    RYGate,
-    RZGate,
-    PhaseGate,
-    U1Gate,
-    RZZGate,
-    RXXGate,
-    RZXGate,
-    RYYGate,
-    CPhaseGate,
-    CU1Gate,
-    CRZGate,
-    CRXGate,
-    CRYGate,
-    IGate,
-    XGate,
-    YGate,
-    ZGate,
-    SGate,
-    SdgGate,
-    TGate,
-    TdgGate,
-    HGate,
-    CXGate,
-    CYGate,
-    CZGate,
-    CHGate,
-    CSGate,
-    CSdgGate,
-    CSXGate,
-    SwapGate,
-    iSwapGate,
-    DCXGate,
-    ECRGate,
-    UGate,
-    U3Gate,
-    U2Gate,
-    RGate,
-    CUGate,
-    CU3Gate,
-    XXPlusYYGate,
-    XXMinusYYGate,
     CCXGate,
     C3XGate,
     C4XGate,
     MCXGate,
+    GlobalPhaseGate,
 )
 from test import combine, QiskitTestCase  # pylint: disable=wrong-import-order
 
@@ -74,99 +34,26 @@ from test import combine, QiskitTestCase  # pylint: disable=wrong-import-order
 class TestPBCTransformation(QiskitTestCase):
     """Test the PBC Transformation optimization pass."""
 
-    @combine(
-        gate=[
-            RXGate,
-            RYGate,
-            RZGate,
-            PhaseGate,
-            U1Gate,
-            RZZGate,
-            RXXGate,
-            RZXGate,
-            RYYGate,
-            CPhaseGate,
-            CU1Gate,
-            CRZGate,
-            CRXGate,
-            CRYGate,
-        ],
-        angle=[0.1, -0.2],
-        global_phase=[0, 1.0, -3.0],
-    )
-    def test_single_param_gates_transpiled(self, gate, angle, global_phase):
-        """Test that standard 1-qubit and 2-qubit single parametric gates are translated into
-        Pauli product rotatations correctly."""
-        num_qubits = gate(angle).num_qubits
-        qc = QuantumCircuit(num_qubits)
-        qc.global_phase = global_phase
-        qc.append(gate(angle), range(num_qubits))
-        qct = PBCTransformation()(qc)
-        ops_names = set(qct.count_ops().keys())
-        self.assertEqual(ops_names, {"PauliEvolution"})
-        self.assertEqual(Operator(qct), Operator(qc))
+    def setUp(self):
+        super().setUp()
+        self.standard_gates = get_standard_gate_name_mapping()
 
-    @combine(
-        gate=[
-            IGate,
-            XGate,
-            YGate,
-            ZGate,
-            SGate,
-            SdgGate,
-            TGate,
-            TdgGate,
-            HGate,
-            CXGate,
-            CYGate,
-            CZGate,
-            CHGate,
-            CSGate,
-            CSdgGate,
-            CSXGate,
-            SwapGate,
-            iSwapGate,
-            DCXGate,
-            ECRGate,
-        ],
-        global_phase=[0, 1.0, -3.0],
-    )
-    def test_non_params_gates_transpiled(self, gate, global_phase):
-        """Test that standard 1-qubit and 2-qubit non-parametric gates are translated into
+    @combine(angle=[0.12, -0.43], global_phase=[0, 1.0, -3.0])
+    def test_standard_gates_transpiled(self, angle, global_phase):
+        """Test that standard 1-qubit and 2-qubit gates are translated into
         Pauli product rotatations correctly."""
-        num_qubits = gate().num_qubits
-        qc = QuantumCircuit(num_qubits)
-        qc.global_phase = global_phase
-        qc.append(gate(), range(num_qubits))
-        qct = PBCTransformation()(qc)
-        ops_names = set(qct.count_ops().keys())
-        self.assertEqual(ops_names, {"PauliEvolution"})
-        self.assertEqual(Operator(qct), Operator(qc))
-
-    @combine(
-        gate=[
-            UGate(0.12, -0.34, 0.56),
-            U3Gate(0.13, -0.24, 0.65),
-            RGate(0.12, -0.34),
-            U2Gate(0.12, -0.34),
-            CUGate(0.12, -0.34, 0.56, -0.78),
-            CU3Gate(0.13, -0.24, 0.67),
-            XXPlusYYGate(0.12, -0.34),
-            XXMinusYYGate(0.12, -0.34),
-        ],
-        global_phase=[0, 1.0, -3.0],
-    )
-    def test_many_param_gates_transpiled(self, gate, global_phase):
-        """Test that standard 1-qubit and 2-qubit gates with several paramteres are translated into
-        Pauli product rotatations correctly."""
-        num_qubits = gate.num_qubits
-        qc = QuantumCircuit(num_qubits)
-        qc.global_phase = global_phase
-        qc.append(gate, range(num_qubits))
-        qct = PBCTransformation()(qc)
-        ops_names = set(qct.count_ops().keys())
-        self.assertEqual(ops_names, {"PauliEvolution"})
-        self.assertEqual(Operator(qct), Operator(qc))
+        for gate in self.standard_gates.values():
+            if getattr(gate, "_standard_gate", None):
+                num_qubits = gate.num_qubits
+                if num_qubits in [1, 2]:
+                    params = [angle * (i + 1) for i in range(len(gate.params))]
+                    qc = QuantumCircuit(num_qubits)
+                    qc.global_phase = global_phase
+                    qc.append(gate.base_class(*params), range(num_qubits))
+                    qct = PBCTransformation()(qc)
+                    ops_names = set(qct.count_ops().keys())
+                    self.assertEqual(ops_names, {"PauliEvolution"})
+                    self.assertEqual(Operator(qct), Operator(qc))
 
     def test_random_circuit(self):
         """Test that a pesudo-random circuit with 1-qubit and 2-qubit gates
@@ -180,7 +67,7 @@ class TestPBCTransformation(QiskitTestCase):
         self.assertEqual(ops_names, {"PauliEvolution"})
         self.assertEqual(Operator(qct), Operator(qc))
 
-    def test_random_circuit_measure_barrier(self):
+    def test_random_circuit_measure_barrier_delay_reset(self):
         """Test that a pesudo-random circuit with 1-qubit and 2-qubit gates,
         measurements, delays, resets and barriers,
         is tranlated into Pauli product rotatations correctly."""
@@ -201,45 +88,37 @@ class TestPBCTransformation(QiskitTestCase):
             ops_names, {"PauliEvolution", "pauli_product_measurement", "delay", "reset", "barrier"}
         )
 
-    @combine(
-        gate=[
-            RXGate(Parameter("theta")),
-            RYGate(Parameter("theta")),
-            RZGate(Parameter("theta")),
-            PhaseGate(Parameter("theta")),
-            U1Gate(Parameter("theta")),
-            RZZGate(Parameter("theta")),
-            RXXGate(Parameter("theta")),
-            RZXGate(Parameter("theta")),
-            RYYGate(Parameter("theta")),
-            CPhaseGate(Parameter("theta")),
-            CU1Gate(Parameter("theta")),
-            CRZGate(Parameter("theta")),
-            CRXGate(Parameter("theta")),
-            CRYGate(Parameter("theta")),
-            UGate(Parameter("theta"), Parameter("phi"), Parameter("lam")),
-            U3Gate(Parameter("theta"), Parameter("phi"), Parameter("lam")),
-            RGate(Parameter("theta"), Parameter("phi")),
-            U2Gate(Parameter("theta"), Parameter("phi")),
-            CUGate(Parameter("theta"), Parameter("phi"), Parameter("lam"), Parameter("gamma")),
-            CU3Gate(Parameter("theta"), Parameter("phi"), Parameter("lam")),
-            XXPlusYYGate(Parameter("theta"), Parameter("phi")),
-            XXMinusYYGate(Parameter("theta"), Parameter("phi")),
-        ],
-    )
-    def test_parametrized_gates(self, gate):
+    def test_parametrized_gates(self):
         """Test that a circuit with 1-qubit and 2-qubit parametrized gates
         is tranlated into Pauli product rotatations correctly."""
-        num_qubits = gate.num_qubits
-        num_params = len(gate.params)
-        qc = QuantumCircuit(num_qubits)
-        qc.append(gate, range(num_qubits))
-        qct = PBCTransformation()(qc)
-        ops_names = set(qct.count_ops().keys())
-        self.assertEqual(ops_names, {"PauliEvolution"})
-        qc_bound = qc.assign_parameters([0.123] * num_params)
-        qct_bound = qct.assign_parameters([0.123] * num_params)
-        self.assertEqual(Operator(qct_bound), Operator(qc_bound))
+        for gate in self.standard_gates.values():
+            if getattr(gate, "_standard_gate", None):
+                num_qubits = gate.num_qubits
+                num_params = len(gate.params)
+                if num_qubits in [1, 2]:
+                    params = []
+                    if num_params == 1:
+                        params = [Parameter("theta")]
+                    elif num_params == 2:
+                        params = [Parameter("theta"), Parameter("phi")]
+                    elif num_params == 3:
+                        params = [Parameter("theta"), Parameter("phi"), Parameter("lam")]
+                    elif num_params == 4:
+                        params = [
+                            Parameter("theta"),
+                            Parameter("phi"),
+                            Parameter("lam"),
+                            Parameter("gamma"),
+                        ]
+
+                    qc = QuantumCircuit(num_qubits)
+                    qc.append(gate.base_class(*params), range(num_qubits))
+                    qct = PBCTransformation()(qc)
+                    ops_names = set(qct.count_ops().keys())
+                    self.assertEqual(ops_names, {"PauliEvolution"})
+                    qc_bound = qc.assign_parameters([0.123] * num_params)
+                    qct_bound = qct.assign_parameters([0.123] * num_params)
+                    self.assertEqual(Operator(qct_bound), Operator(qc_bound))
 
     @combine(
         gate=[
@@ -247,12 +126,13 @@ class TestPBCTransformation(QiskitTestCase):
             C3XGate(),
             C4XGate(),
             MCXGate(5),
+            GlobalPhaseGate(0.1),
         ]
     )
     def test_unsupported_gates_raise_error(self, gate):
         """Test that unsupported gates raise a trasnpiler error."""
         num_qubits = gate.num_qubits
-        qc = QuantumCircuit(num_qubits)
+        qc = QuantumCircuit(num_qubits + 2)
         qc.h(0)
         qc.cx(0, 1)
         qc.append(gate, range(num_qubits))
