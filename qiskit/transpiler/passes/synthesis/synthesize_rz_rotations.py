847cbac52056d3a4164c273757c60a3b5e9c6419
jan-an <jananisarana@gmail.com>
2026-02-11 21:13:38 +0530

    documentation added and epsilon modified to approximation degree

--- qiskit/transpiler/passes/synthesis/synthesize_rz_rotations.py
+++ qiskit/transpiler/passes/synthesis/synthesize_rz_rotations.py
@@ -8,35 +8,77 @@ class SynthesizeRZRotations(TransformationPass):
 
     This pass replaces all single-qubit RZ rotation gates with sequences
     of Clifford+T gates. We first canonicalize angles based on the 4π cyclicity
-    of RZ gates, and further utilize the property RZ(θ+2π) = e^{iπ} RZ(θ) to map
-    angles to the [0, 2π) range to limit the number of distinct angles synthesized.
+    of RZ gates, and further utilize properties of RZ(θ) to limit the number of
+    distinct angles synthesized to [0, π/2) and a u8 bit.
+
+    This pass performs a two-fold optimization for implementing RZ synthesis.
+    For QFT-like circuits with repeating angles, we want to avoid doing redundant
+    syntheses of the same angle multiple times. The mechanism for this is to first
+    collect all the angles of RZ gates from the DAG and sort them. If we implement
+    a cache-like mechanism where if the synthesis angle already exists, we reuse it,
+    if it does not, then we synthesize them. We further extend this by allowing
+    approximations of angles and doing the synthesis with a lower epsilon.
+    Because RZ is  4π-cyclic, we can come up with canonical representations of angles
+    from  (−∞, ∞) → [0, 4π) . Further, we can exploit properties of  RZ  such as:
+    RZ(θ+π/2) = RZ(θ)⋅S , RZ(θ+π) = RZ(θ)⋅Z, RZ(θ+2π) = −RZ(θ) to partition the [0, 4π)
+    domain as ∪ [nπ/2, π(n+1)/2] , n ∈ Z + : [0, 7]. The canonical angle representation
+    reduces to range [0 ,π/2) for RZ synthesis, but we make updates to the global phase
+    and gates to add to the synthesised  RZ sequence.
+    .
+    Hence, this mapping further reduces as:
+    [0, 4π) → {{r : r ∈ Z + , [0, 7]} , (0, π/2]}, where r corresponds to a specific
+    phase and gate update (from a static look-up table) to be made to the DAG.
+
     We then iterate over the dag to identify RZ gates and replace them with their
     Clifford+T approximations.
 
+    For example::
+
+      from qiskit.circuit import QuantumCircuit
+      from qiskit.transpiler.passes import SynthesizeRZRotations
+      from qiskit.quantum_info import Operator
+      from numpy import pi
+
+      # The following quantum circuit consists of 5 Clifford gates
+      # and three single-qubit RZ rotations gates
+
+      qc = QuantumCircuit(4)
+      qc.cx(0, 1)
+      qc.rz(9*pi/4, 0)
+      qc.cz(0, 1)
+      qc.rz(3*pi/8, 1)
+      qc.h(1)
+      qc.s(2)
+      qc.rz(13*pi/2, 2)
+      qc.cz(2, 0)
+      qc.rz(5*pi/3, 3)
+
+      qct = SynthesizeRZRotations()(qc)
+
+      # The transformed circuit consists of Clifford, T and Tdg gates
+      clifford_t_names = get_clifford_gate_names() + ["t"] + ["tdg"]
+      assert(set(qct.count_ops().keys()).issubset(set(clifford_t_names)))
+
+      # The circuits before and after the transformation are equivalent
+      assert Operator(qc) == Operator(qct)
+
     Args:
-        [Change this to approximation degree in later iteration to maintain
-        consistency across other passes]
+        approximation_degree: float = 0.9999999999
 
-        epsilon: Precision parameter for gridsynth approximation.
-        Default: 1e-10.
-
-        [need to elaborate more, WIP]
     """
 
-    def __init__(self, epsilon: float = 1e-10):
+    def __init__(self, approximation_degree: float = 0.9999999999):
         super().__init__()
-        self.epsilon = epsilon
+        self.approximation_degree = approximation_degree
 
     def run(self, dag: DAGCircuit) -> DAGCircuit:
         """Run the pass on a DAG.
-
         Args:
             dag: the input DAG.
-
         Returns:
             The output DAG.
         """
-        new_dag = synthesize_rz_rotations(dag, self.epsilon)
+        new_dag = synthesize_rz_rotations(dag, self.approximation_degree)
 
         if new_dag is None:
             return dag
