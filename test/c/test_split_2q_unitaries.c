b5473647657be3252a8b1c7f43b02f862c985557
Bruno Caballero <bruno.caballero@gmail.com>
2026-02-17 16:57:32 +0100

    Update test/c/test_split_2q_unitaries.c
    Co-authored-by: Eli Arbel <46826214+eliarbel@users.noreply.github.com>

--- test/c/test_split_2q_unitaries.c
+++ test/c/test_split_2q_unitaries.c
@@ -243,7 +243,7 @@ static int test_split_2q_unitaries_x_y_unitary(void) {
     int result = Ok;
     if (split_result != NULL) {
         result = EqualityError;
-        printf("Permutation returned for a circuit that shouldn't isn't a swap equivalent\n");
+        printf("Permutation returned for a circuit that isn't swap-equivalent\n");
         qk_transpile_layout_free(split_result);
         goto cleanup;
     }
