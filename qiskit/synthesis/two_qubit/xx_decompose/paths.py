22f6b1cfea9a10c4a3cd5d2b9174e7ebbc2a844a
Ian Hincks <ian.hincks@ibm.com>
2026-03-06 08:53:48 -0500

    Fix typos in `qiskit.utils`, `qiskit.synth`, and `qiskit.visualization` (#15771)
    * Fix typos in qiskit.synthesis
    
    * Fix typos in qiskit.utils
    
    * Fix typos in qiskit.visualization
    
    (cherry picked from commit 019a0a6bf8069cba0b23223734a15b043b74f607)
    
    # Conflicts:
    #	qiskit/utils/optionals.py

--- qiskit/synthesis/two_qubit/xx_decompose/paths.py
+++ qiskit/synthesis/two_qubit/xx_decompose/paths.py
@@ -138,7 +138,7 @@ xx_region_polytope = PolytopeData(
             equalities=[],
             name=(
                 "I ∩ A alcove ∩ "
-                "A unreflected ∩ ah slant ∩ al frustrum ∩ B alcove ∩ B unreflected ∩ AF=B3"
+                "A unreflected ∩ ah slant ∩ al frustum ∩ B alcove ∩ B unreflected ∩ AF=B3"
             ),
         ),
         ConvexPolytopeData(
@@ -162,7 +162,7 @@ xx_region_polytope = PolytopeData(
             equalities=[],
             name=(
                 "I ∩ A alcove ∩ "
-                "A reflected ∩ ah strength ∩ al frustrum ∩ B alcove ∩ B reflected ∩ AF=B3"
+                "A reflected ∩ ah strength ∩ al frustum ∩ B alcove ∩ B reflected ∩ AF=B3"
             ),
         ),
         ConvexPolytopeData(
@@ -187,7 +187,7 @@ xx_region_polytope = PolytopeData(
             equalities=[],
             name=(
                 "I ∩ A alcove ∩ "
-                "A unreflected ∩ af slant ∩ al frustrum ∩ B alcove ∩ B unreflected ∩ AF=B1"
+                "A unreflected ∩ af slant ∩ al frustum ∩ B alcove ∩ B unreflected ∩ AF=B1"
             ),
         ),
         ConvexPolytopeData(
@@ -212,7 +212,7 @@ xx_region_polytope = PolytopeData(
             equalities=[],
             name=(
                 "I ∩ A alcove ∩ "
-                "A reflected ∩ af strength ∩ al frustrum ∩ B alcove ∩ B reflected ∩ AF=B1"
+                "A reflected ∩ af strength ∩ al frustum ∩ B alcove ∩ B reflected ∩ AF=B1"
             ),
         ),
     ]
@@ -267,7 +267,7 @@ xx_lift_polytope = PolytopeData(
             equalities=[[0, 0, 0, 1, 0, 0, -1, 0, 0, 0, 0]],
             name=(
                 "I ∩ A alcove ∩ "
-                "A unreflected ∩ ah slant ∩ al frustrum ∩ B alcove ∩ B unreflected ∩ AF=B3"
+                "A unreflected ∩ ah slant ∩ al frustum ∩ B alcove ∩ B unreflected ∩ AF=B3"
             ),
         ),
         ConvexPolytopeData(
@@ -305,7 +305,7 @@ xx_lift_polytope = PolytopeData(
             equalities=[[0, 0, 0, 1, -1, 0, 0, 0, 0, 0, 0]],
             name=(
                 "I ∩ A alcove ∩ "
-                "A unreflected ∩ af slant ∩ al frustrum ∩ B alcove ∩ B unreflected ∩ AF=B1"
+                "A unreflected ∩ af slant ∩ al frustum ∩ B alcove ∩ B unreflected ∩ AF=B1"
             ),
         ),
         ConvexPolytopeData(
@@ -343,7 +343,7 @@ xx_lift_polytope = PolytopeData(
             equalities=[[0, 0, 0, 1, -1, 0, 0, 0, 0, 0, 0]],
             name=(
                 "I ∩ A alcove ∩ "
-                "A reflected ∩ af strength ∩ al frustrum ∩ B alcove ∩ B reflected ∩ AF=B1"
+                "A reflected ∩ af strength ∩ al frustum ∩ B alcove ∩ B reflected ∩ AF=B1"
             ),
         ),
         ConvexPolytopeData(
@@ -381,7 +381,7 @@ xx_lift_polytope = PolytopeData(
             equalities=[[0, 0, 0, 1, 0, 0, -1, 0, 0, 0, 0]],
             name=(
                 "I ∩ A alcove ∩ "
-                "A reflected ∩ ah strength ∩ al frustrum ∩ B alcove ∩ B reflected ∩ AF=B3"
+                "A reflected ∩ ah strength ∩ al frustum ∩ B alcove ∩ B reflected ∩ AF=B3"
             ),
         ),
     ]
