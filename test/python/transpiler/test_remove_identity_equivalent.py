Fix PauliEvo trace and dim calculation
The dimension should be `2^num_qubits`, not just `num_qubits`!

test/python/transpiler/test_remove_identity_equivalent.py
@@ -28,8 +28,9 @@ from qiskit.circuit.library import (
     XXPlusYYGate,
     GlobalPhaseGate,
     UnitaryGate,
+    PauliEvolutionGate,
 )
-from qiskit.quantum_info import Operator
+from qiskit.quantum_info import Operator, Pauli
 from qiskit.transpiler.passes import RemoveIdentityEquivalent
 from qiskit.transpiler.target import Target, InstructionProperties
 
@@ -209,3 +210,33 @@ class TestRemoveIdentityEquivalent(QiskitTestCase):
         qc.append(GlobalPhaseGate(theta), [])
         transpiled = RemoveIdentityEquivalent()(qc)
         self.assertEqual(qc, transpiled)
+
+    def test_pauli_evo_equals_stdgate(self):
+        """Test the Pauli evolution gate is consistent with std gates."""
+
+        def get_rz(angle):
+            qc = QuantumCircuit(1)
+            qc.rz(angle, 0)
+            return qc
+
+        def get_paulievo(angle, n):
+            evo = PauliEvolutionGate(Pauli("Z" + (n - 1) * "I"), time=angle / 2)
+            qc = QuantumCircuit(evo.num_qubits)
+            qc.append(evo, qc.qubits)
+            return qc
+
+        pass_ = RemoveIdentityEquivalent()
+
+        # it is important to cover a small enough grid around the threshold where we
+        # start removing gates, to ensure all cases are handled consistently
+        for angle in np.linspace(1e-5, 1e-6, num=25):
+            # number of expected instructions
+            rz = get_rz(angle)
+            expected = pass_(rz).count_ops().get("rz", 0)
+
+            # check the number matches for different `n`
+            for n in [1, 10]:
+                with self.subTest(n=n):
+                    evo = get_paulievo(angle, n)
+                    num_inst = pass_(evo).count_ops().get("PauliEvolution", 0)
+                    self.assertEqual(expected, num_inst)