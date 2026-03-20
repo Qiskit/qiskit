5825af7c479f7a8621413361268db50bba464c1b
Jake Lishman <jake.lishman@ibm.com>
2026-03-20 15:07:11 +0000

    Fix stack-overflow segfault in OpenQASM 2 expression parsers
    The current recursive implementation of the operator-precedence
    expression parsers is susceptible to overflowing the stack on
    excessively deep expressions.  The simplest way to trigger this is to
    enter an arbitrary number of parentheses.
    
    This patch puts a simple recursion-depth limit on the expression parser
    to safely error out before it comes close to the stack depth.  We use
    `sys.getrecursionlimit` on input simply as a compromise between not
    wanting to define new keyword arguments in bugfix patches and still
    wanting to allow some amount of control over the limit.
    
    Future patches could consider replacing the recursive components of the
    expression parser with iterative state-tracking parsing.  The outer
    recursive-descent parser that handles the rest of the OpenQASM 2 grammar
    has a bounded recursion depth by nature of the grammar it is parsing; it
    is only the expression subparser that can reach arbitrary depths.

--- test/python/qasm2/test_expression.py
+++ test/python/qasm2/test_expression.py
@@ -134,6 +134,28 @@ class TestSimple(QiskitTestCase):
         parameters = list(parsed.data[0].operation.params)
         self.assertEqual([bigint, -bigint, 2 * bigint], parameters)
 
+    def test_excessive_depth_unary(self):
+        """We should either succeed in the evaluation or raise a Python-space error. We should _not_
+        segfault."""
+        expr = "-" * 100_000 + "2.0"
+        program = f"qreg q[1]; U({expr}, 0.0, 0.0) q[0];"
+        # This can be changed to an assertion that the expression evaluates to precisely `2.0` if we
+        # modify the parser to be non-recursive.
+        with self.assertRaises(RecursionError):
+            qiskit.qasm2.loads(program)
+
+    def test_excessive_depth_binary(self):
+        """We should either succeed in the evaluation or raise a Python-space error. We should _not_
+        segfault."""
+        # We have to include the parentheses or the operator-precedence parser will just decay to
+        # iterative anyway.
+        expr = "1.0+(" * 100_000 + "1.0" + ")" * 100_000
+        program = f"qreg q[1]; U({expr}, 0.0, 0.0) q[0];"
+        # This can be changed to an assertion that the expression evaluates to precisely `100001.0`
+        # if we modify the parser to be non-recursive.
+        with self.assertRaises(RecursionError):
+            qiskit.qasm2.loads(program)
+
 
 class TestPrecedenceAssociativity(QiskitTestCase):
     def test_precedence(self):
