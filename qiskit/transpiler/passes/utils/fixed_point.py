ce141dd821b2b2a3978ea67e1be8e987157bb7ca
Aarush <112254386+Hadar01@users.noreply.github.com>
2026-03-26 11:28:43 +0530

    Refactor imports and docstrings in fixed_point.py

--- qiskit/transpiler/passes/utils/fixed_point.py
+++ qiskit/transpiler/passes/utils/fixed_point.py
@@ -9,14 +9,13 @@
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
-
 """Check if a property reached a fixed point."""
-
-from __future__ import annotations
-
 from copy import deepcopy
+from typing import TYPE_CHECKING
 
-from qiskit.dagcircuit import DAGCircuit
+if TYPE_CHECKING:
+    from qiskit.dagcircuit import DAGCircuit
+
 from qiskit.transpiler.basepasses import AnalysisPass
 
 
@@ -29,8 +28,7 @@ class FixedPoint(AnalysisPass):
     """
 
     def __init__(self, property_to_check: str) -> None:
-        """Initialize a ``FixedPoint`` pass.
-
+        """
         Args:
             property_to_check: The property to check if a fixed point was reached.
         """
@@ -38,14 +36,12 @@ class FixedPoint(AnalysisPass):
         self._property = property_to_check
 
     def run(self, dag: DAGCircuit) -> None:
-        """Run the FixedPoint pass on *dag*."""
+        """Run the FixedPoint pass on ``dag``."""
         current_value = self.property_set[self._property]
         fixed_point_previous_property = f"_fixed_point_previous_{self._property}"
-
         if self.property_set[fixed_point_previous_property] is None:
             self.property_set[f"{self._property}_fixed_point"] = False
         else:
             fixed_point_reached = self.property_set[fixed_point_previous_property] == current_value
             self.property_set[f"{self._property}_fixed_point"] = fixed_point_reached
-
         self.property_set[fixed_point_previous_property] = deepcopy(current_value)
