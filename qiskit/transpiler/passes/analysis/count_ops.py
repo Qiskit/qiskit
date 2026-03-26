5dd8d152baecfff32e364453e3ae051e22c93c68
Aarush <112254386+Hadar01@users.noreply.github.com>
2026-03-26 13:46:56 +0530

    Add copyright notice and docstring to count_ops.py

--- qiskit/transpiler/passes/analysis/count_ops.py
+++ qiskit/transpiler/passes/analysis/count_ops.py
@@ -9,7 +9,9 @@
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
+
 """Count the operations in a DAG circuit."""
+
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
