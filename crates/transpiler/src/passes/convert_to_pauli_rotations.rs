967ca88e8f436b51767c25a1734b1b935fd3edf1
jan-an <jananisarana@gmail.com>
2026-04-07 14:53:56 +0530

    rewrite name-based barrier/reset/delay check

--- crates/transpiler/src/passes/convert_to_pauli_rotations.rs
+++ crates/transpiler/src/passes/convert_to_pauli_rotations.rs
@@ -19,8 +19,8 @@ use std::f64::consts::{FRAC_PI_2, FRAC_PI_4, FRAC_PI_8, PI};
 use qiskit_circuit::dag_circuit::{DAGCircuit, NodeType};
 use qiskit_circuit::instruction::Parameters;
 use qiskit_circuit::operations::{
-    Operation, OperationRef, Param, PauliProductMeasurement, PauliProductRotation, StandardGate,
-    add_param, multiply_param, radd_param,
+    Operation, OperationRef, Param, PauliProductMeasurement, PauliProductRotation,
+    StandardInstruction, StandardGate, add_param, multiply_param, radd_param, 
 };
 use qiskit_circuit::{BlocksMode, Qubit, VarsMode};
 use qiskit_quantum_info::sparse_observable::BitTerm;
@@ -478,11 +478,15 @@ pub fn py_convert_to_pauli_rotations(dag: &DAGCircuit) -> PyResult<DAGCircuit> {
         let NodeType::Operation(inst) = &dag[node_index] else {
             unreachable!("dag.topological_op_nodes only returns Operations");
         };
-        if ["barrier", "reset", "delay"].contains(&inst.op.name())
-            || matches!(inst.op.view(), OperationRef::ControlFlow(_))
+        if matches!(
+        inst.op.view(),
+        OperationRef::ControlFlow(_)
+            | OperationRef::StandardInstruction(StandardInstruction::Barrier(_))
+            | OperationRef::StandardInstruction(StandardInstruction::Reset)
+            | OperationRef::StandardInstruction(StandardInstruction::Delay(_))
+        ) 
         {
             new_dag.push_back(inst.clone())?;
-            continue;
         } else if inst.op.name() == "measure" {
             let z = vec![true];
             let x = vec![false];
