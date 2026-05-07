5bc02c651b833aacd7882fe6917246ffc4e711d5
Matthew Treinish <mtreinish@kortar.org>
2026-05-07 12:09:30 -0400

    Add release notes and docstring updates

--- crates/cext/src/transpiler/passes/commutative_cancellation.rs
+++ crates/cext/src/transpiler/passes/commutative_cancellation.rs
@@ -24,6 +24,12 @@ use qiskit_transpiler::target::Target;
 ///
 /// This pass cancels the redundant (self-adjoint) gates through commutation relations.
 ///
+/// This function is multithreaded and will potentially launch a thread pool
+/// with threads equal to the number of CPUs by default. You can tune the
+/// number of threads with the ``RAYON_NUM_THREADS`` environment variable.
+/// For example, setting ``RAYON_NUM_THREADS=4`` would limit the thread pool
+/// to 4 threads.
+///
 /// @param circuit A pointer to the circuit to run CommutativeCancellation on. This circuit
 /// pointer to will be updated with the modified circuit if the pass is able to remove any gates.
 /// @param target This pass will attempt to accumulate all Z rotations into either
