77583b20b1b09c0843b219a0a469b95f87d6edce
Gadi Aleksandrowicz <gadial@gmail.com>
2026-05-20 10:44:27 +0300

    code simplification

--- test/qpy_compat/get_forward_compat_versions.py
+++ test/qpy_compat/get_forward_compat_versions.py
@@ -19,45 +19,29 @@ import packaging.version
 from qpy_version_utils import fetch_installable_qiskit_versions
 
 # Minimum Qiskit minor series that can read each supported QPY format version.
+MIN_VERSION_STR = "1.4"
 QPY_MIN_COMPAT_VERSION = {
-    13: "1.4",
-    14: "2.0",
-    15: "2.1",
-    16: "2.2",
-    17: "2.3",
+    13: (1, 4),
+    14: (2, 0),
+    15: (2, 1),
+    16: (2, 2),
+    17: (2, 3),
 }
 
 
 def get_qiskit_versions():
     """For each minor series referenced in QPY_MIN_COMPAT_VERSION, find the latest
     available patch release on PyPI that is installable on this platform."""
-    minimum_minor_by_qpy = {
-        qpy_version: packaging.version.parse(minor_key)
-        for qpy_version, minor_key in QPY_MIN_COMPAT_VERSION.items()
-    }
-    needed_minors = set(QPY_MIN_COMPAT_VERSION.values())
-
-    # Fetch all qiskit versions from the minimum supported version onwards
-    min_qpy_version = min(minimum_minor_by_qpy.values())
-    min_version_str = f"{min_qpy_version.major}.{min_qpy_version.minor}"
-
-    results = {qpy_version: [] for qpy_version in QPY_MIN_COMPAT_VERSION.keys()}
-
+    results = {qpy_version: [] for qpy_version in QPY_MIN_COMPAT_VERSION}
     for version, python_version in fetch_installable_qiskit_versions(
         package="qiskit",
-        min_version=min_version_str,
+        min_version=MIN_VERSION_STR,
         max_version=None,
     ):
-        minor_version = packaging.version.parse(f"{version.major}.{version.minor}")
-        minor_key = f"{version.major}.{version.minor}"
-
-        # Skip versions that are not in needed_minors unless they're >= minimum
-        if minor_key not in needed_minors and minor_version < min(minimum_minor_by_qpy.values()):
-            continue
-
+        found_minor_version = (version.major, version.minor)
         # Add this version to all QPY versions it can read
-        for qpy_version, minimum_minor in minimum_minor_by_qpy.items():
-            if minor_version >= minimum_minor:
+        for qpy_version, minimum_minor in QPY_MIN_COMPAT_VERSION.items():
+            if found_minor_version >= minimum_minor:
                 results[qpy_version].append(version)
 
     return results
