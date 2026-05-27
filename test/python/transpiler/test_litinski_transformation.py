38c884e01827e66231a1560cab53597517fc7f6b
Shelly Garion <shelly@il.ibm.com>
2026-05-27 16:06:45 +0300

    minor update in test following review

--- test/python/transpiler/test_litinski_transformation.py
+++ test/python/transpiler/test_litinski_transformation.py
@@ -182,7 +182,7 @@ class TestLitinskiTransformation(QiskitTestCase):
         qc_litinski_no_fix = LitinskiTransformation(fix_clifford=False, use_ppr=True)(qc)
 
         # make sure the transform was applied
-        self.assertEqual(list(qc_litinski_no_fix.count_ops().keys()), ["pauli_product_rotation"])
+        self.assertEqual(list(qc_litinski_no_fix.count_ops()), ["pauli_product_rotation"])
         self.assertEqual(
             qc_litinski_no_fix.count_ops()["pauli_product_rotation"],
             qc_litinski.count_ops()["pauli_product_rotation"],
