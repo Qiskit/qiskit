a79486f5456c83d2b3affe0cf1bcd48860ea06d8
Matthew Treinish <mtreinish@kortar.org>
2026-05-27 10:23:03 -0400

    Use try_for_each to propogate add_global_phase error

--- crates/transpiler/src/passes/optimize_1q_gates_decomposition.rs
+++ crates/transpiler/src/passes/optimize_1q_gates_decomposition.rs
@@ -417,14 +417,14 @@ pub fn run_optimize_1q_gates_decomposition(
         runs.into_iter()
             .zip(sequences)
             .filter_map(|(raw_run, sequence)| sequence.map(|x| (raw_run, x)))
-            .for_each(|(raw_run, sequence)| {
+            .try_for_each(|(raw_run, sequence)| -> PyResult<()> {
                 for gate in sequence.gates {
                     dag.insert_1q_on_incoming_qubit((gate.0, &gate.1), raw_run[0]);
                 }
-                dag.add_global_phase(&Param::Float(sequence.global_phase))
-                    .unwrap();
+                dag.add_global_phase(&Param::Float(sequence.global_phase))?;
                 dag.remove_1q_sequence(&raw_run);
-            });
+                Ok(())
+            })?;
     } else {
         for raw_run in runs {
             let sequence = process_run(&raw_run, dag)?;
