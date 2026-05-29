90e81d5c57fdd961e0663d50bf0a7998c7a09bf0
Esteban Ramirez <estebanrg21@icloud.com>
2026-05-29 17:45:08 -0600

    Add test for entanglement strategy

--- test/c/test_nlocal.c
+++ test/c/test_nlocal.c
@@ -19,23 +19,94 @@
  * Test setting the entanglement of the layers by entanglement strategy
  */
 static int test_entanglement_by_strategy(void) {
-    // int result = (compare_circuits(qc, expected)) ? Ok : EqualityError;
-    char *paramenter_prefix = "";
+    int result = Ok;
+    int reps = 3;
 
-    QkGate rotation_blocks[1] = {QkGate_X};
     QkGate entanglement_blocks[1] = {QkGate_CCX};
 
-    QkEntanglement *entanglement = qk_get_entanglement_with_strategy(
-        5, 3, QkEntanglementStrategy_Linear, entanglement_blocks, 1);
+    uint32_t expected_connections_full[10][3] = {
+        {0, 1, 2}, {0, 1, 3}, {0, 1, 4}, {0, 2, 3}, {0, 2, 4},
+        {0, 3, 4}, {1, 2, 3}, {1, 2, 4}, {1, 3, 4}, {2, 3, 4},
+    };
+    uint32_t expected_connections_linear[3][3] = {{0, 1, 2}, {1, 2, 3}, {2, 3, 4}};
+    uint32_t expected_connections_reverse_linear[3][3] = {{2, 3, 4}, {1, 2, 3}, {0, 1, 2}};
+    uint32_t expected_connections_circular[5][3] = {
+        {3, 4, 0}, {4, 0, 1}, {0, 1, 2}, {1, 2, 3}, {2, 3, 4}};
+    uint32_t expected_connections_sca[5][3] = {{0}};
 
-    QkCircuit *qc = qk_n_local(rotation_blocks, 1, entanglement_blocks, 1, entanglement, 5, 3,
-                               paramenter_prefix, false, false);
+    for (size_t strategy = 0; strategy < 5; strategy++) {
 
-    
+        QkEntanglement *entanglement =
+            qk_get_entanglement_with_strategy(5, reps, strategy, entanglement_blocks, 1);
 
-    qk_circuit_free(qc);
+        if (qk_get_entanglement_layers_quantity(entanglement) != (size_t)reps) {
+            result = EqualityError;
+        }
 
-    return Ok;
+        if (qk_get_entanglement_layer_blocks_quantity(entanglement, 0) != (size_t)1) {
+            result = EqualityError;
+        }
+
+        bool break_loop = false;
+        for (size_t i = 0; i < (size_t)reps; i++) {
+            if (break_loop)
+                break;
+
+            uint32_t (*expected_connections)[3] = NULL;
+            uint32_t expected_connections_length;
+            switch (strategy) {
+            case QkEntanglementStrategy_Full:
+                expected_connections = expected_connections_full;
+                expected_connections_length = 10;
+                break;
+            case QkEntanglementStrategy_Linear:
+                expected_connections = expected_connections_linear;
+                expected_connections_length = 3;
+                break;
+            case QkEntanglementStrategy_ReverseLinear:
+                expected_connections = expected_connections_reverse_linear;
+                expected_connections_length = 3;
+                break;
+            case QkEntanglementStrategy_Sca:
+                expected_connections_length = 5;
+                for (size_t k = 0; k < expected_connections_length; k++) {
+                    int offset = ((int)k) - ((int)i);
+                    int ind = offset >= 0 ? offset : expected_connections_length + offset;
+                    for (size_t m = 0; m < 3; m++) {
+                        expected_connections_sca[k][m] =
+                            expected_connections_circular[ind][i % 2 == 1 ? 2 - m : m];
+                    }
+                }
+                expected_connections = expected_connections_sca;
+                break;
+            case QkEntanglementStrategy_Circular:
+                expected_connections = expected_connections_circular;
+                expected_connections_length = 5;
+                break;
+            }
+
+            if (qk_get_entanglement_qubit_connections_quantity(entanglement, i, 0) !=
+                expected_connections_length) {
+                result = EqualityError;
+                break_loop = true;
+                break;
+            }
+
+            for (size_t j = 0; j < expected_connections_length; j++) {
+                uint32_t *connections = *(expected_connections + j);
+                QkQubitConnection *expected_connection = qk_qubit_connection_new(3, connections);
+                QkQubitConnection *connection =
+                    qk_get_entanglement_qubit_connections(entanglement, i, 0, j);
+
+                if (!qk_qubit_connection_equal(connection, expected_connection)) {
+                    result = EqualityError;
+                    break_loop = true;
+                    break;
+                }
+            }
+        }
+    }
+    return result;
 }
 
 int test_nlocal(void) {
