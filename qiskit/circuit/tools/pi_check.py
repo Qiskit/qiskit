accf46f2da14f00e47f1218d27572b5ea5153425
ツンデレ <vpeilivanidis@gmail.com>
2026-06-01 16:14:50 +0300

    Update qiskit/circuit/tools/pi_check.py
    Co-authored-by: Julien Gacon <gaconju@gmail.com>

--- qiskit/circuit/tools/pi_check.py
+++ qiskit/circuit/tools/pi_check.py
@@ -18,7 +18,10 @@ from qiskit.circuit.parameterexpression import ParameterExpression
 from qiskit.exceptions import QiskitError
 
 MAX_FRAC = 16
-# Max denominator k for pi/k in the fourth check (2-digit denominators; see pi_check).
+# Max denominator k for pi/k in the fourth check (2-digit denominators).
+# The different threshold compared to MAX_FRAC is historic, since previous versions
+# of Qiskit used a slightly different, inconsistent check that led to higher fractions 
+# being detected.
 MAX_PI_FRAC = 99
 N, D = np.meshgrid(np.arange(1, MAX_FRAC + 1), np.arange(1, MAX_FRAC + 1))
 FRAC_MESH = N / D * np.pi
