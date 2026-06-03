66cadb6b194f0b6eccbb0d3b68fc72de6abfed5d
Esteban Ramirez <estebanrg21@icloud.com>
2026-06-02 23:59:39 -0600

    More lint fixes

--- test/c/test_qdrift.c
+++ test/c/test_qdrift.c
@@ -1,20 +1,15 @@
-/**
- * This code is part of Qiskit.
- *
- * (C) Copyright IBM 2025
- *
- * This code is licensed under the Apache License, Version 2.0. You may
- * obtain a copy of this license in the LICENSE.txt file in the root directory
- * of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
- *
- * Any modifications or derivative works of this code must retain this
- * copyright notice, and modified files need to carry a notice indicating
- * that they have been altered from the originals.
- **/
-// Required for M_PI using MSVC toolchain
-#ifdef _WIN32
-#define _USE_MATH_DEFINES
-#endif
+// This code is part of Qiskit.
+//
+// (C) Copyright IBM 2025.
+//
+// This code is licensed under the Apache License, Version 2.0. You may
+// obtain a copy of this license in the LICENSE.txt file in the root directory
+// of this source tree or at https://www.apache.org/licenses/LICENSE-2.0.
+//
+// Any modifications or derivative works of this code must retain this
+// copyright notice, and modified files need to carry a notice indicating
+// that they have been altered from the originals.
+
 #include "common.h"
 #include <math.h>
 #include <qiskit.h>
