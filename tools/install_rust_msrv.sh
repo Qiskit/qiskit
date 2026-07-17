1f64c390ac4fe1966fb5a650c5a0a6163f6424f7
Jake Lishman <jake.lishman@ibm.com>
2026-07-17 13:19:34 +0100

    Infer MSRV in `tools/install_rust_msrv.sh`
    Since this is an executable, and Python 3.11+ has `tomllib` in the
    standard library, this just makes one fewer place to update on MSRV
    bumps.

--- tools/install_rust_msrv.sh
+++ tools/install_rust_msrv.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
+set -e
+
 if [ ! -d rust-installer ]; then
     mkdir rust-installer
     wget https://sh.rustup.rs -O rust-installer/rustup.sh
-    sh rust-installer/rustup.sh -y -c llvm-tools
-    rustup default 1.87
-    rustup component add llvm-tools
-    rustup uninstall stable
+    msrv="$(python3 <<EOF
+from pathlib import Path
+import tomllib
+
+cargo_toml_file = Path("$0").absolute().parents[1] / "Cargo.toml"
+manifest = tomllib.load(open(cargo_toml_file, 'rb'))
+print(manifest["workspace"]["package"]["rust-version"])
+EOF
+)"
+    sh rust-installer/rustup.sh -y --default-toolchain "$msrv" --component llvm-tools
 fi
 . "$HOME/.cargo/env"
