e7b12c16197d8f4cc52da600c1eb283ee894dbd8
David Cohen <me@davidfcohen.com>
2026-07-28 17:41:53 -0400

    improve release notes language

---
+++ create-registers.py
@@ -0,0 +1,33 @@
+import time
+
+from qiskit.circuit import ClassicalRegister, QuantumCircuit
+
+start = time.time()
+size = 10
+qc = QuantumCircuit(*(ClassicalRegister(5, f"cr_{i:05}") for i in range(size)))
+end = time.time()
+elapsed = (end - start) * 1000
+print("n=10: ", elapsed)
+
+start = time.time()
+size = 100
+qc = QuantumCircuit(*(ClassicalRegister(5, f"cr_{i:05}") for i in range(size)))
+end = time.time()
+elapsed = (end - start) * 1000
+print("n=100: ", elapsed)
+
+start = time.time()
+size = 1000
+qc = QuantumCircuit(*(ClassicalRegister(5, f"cr_{i:05}") for i in range(size)))
+end = time.time()
+elapsed = (end - start) * 1000
+print("n=1000: ", elapsed)
+
+start = time.time()
+size = 10_000
+qc = QuantumCircuit(*(ClassicalRegister(5, f"cr_{i:05}") for i in range(size)))
+end = time.time()
+elapsed = (end - start) * 1000
+print("n=10000: ", elapsed)
+
+qc = QuantumCircuit(ClassicalRegister(5, "a"), ClassicalRegister(5, "b"))
