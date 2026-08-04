89086f2dd5d1c5a1bfc059049917f8662faa8d36
ella <ella@il.ibm.com>
2026-08-04 14:43:23 +0300

    Migrate synth_mcx_n_clean_m15 to Rust

--- crates/synthesis/src/multi_controlled/mcx.rs
+++ crates/synthesis/src/multi_controlled/mcx.rs
@@ -461,6 +461,64 @@ pub fn synth_mcx_noaux_v24(
     }
 }
 
+/// Synthesize a multi-controlled X gate with :math:`k\ge 3` controls using :math:`k - 2`
+///     clean ancillary qubits with producing a circuit with :math:`2 * k - 1` qubits
+///     and at most :math:`6 * k - 6` CX gates, by Maslov [1].
+///     For :math:`k\le 2`, the returned circuit consists of a single X, CX or CCX gate
+///     (corresponding to :math:`k = 0, 1, 2`, respectively) and uses no ancillary qubits.
+///
+///     Args:
+///         num_controls: The number of control qubits.
+///
+///     Returns:
+///         The synthesized quantum circuit.
+///
+///     Raises:
+///         QiskitError: if ``num_ctrl_qubits`` is illegal.
+///
+///     References:
+///         1. Maslov., Phys. Rev. A 93, 022311 (2016),
+///            `arXiv:1508.03273 <https://arxiv.org/pdf/1508.03273>`_
+pub fn synth_mcx_n_clean_m15(num_controls: usize) -> Result<CircuitData, CircuitDataError> {
+    if num_controls == 0 {
+        let mut circuit = CircuitData::with_capacity(1, 0, 1, Param::Float(0.0))?;
+        circuit.x(0)?;
+        Ok(circuit)
+    } else if num_controls == 1 {
+        let mut circuit = CircuitData::with_capacity(2, 0, 1, Param::Float(0.0))?;
+        circuit.cx(0, 1)?;
+        Ok(circuit)
+    } else if num_controls == 2 {
+        Ok(ccx())
+    } else {
+        let num_qubits = 2 * num_controls - 1;
+
+        let mut circuit = CircuitData::with_capacity(num_qubits as u32, 0, 0, Param::Float(0.0))?;
+
+        let controls: Vec<u32> = (0..num_controls).map(|q| q as u32).collect();
+        let target = num_controls as u32;
+        let ancillas: Vec<u32> = ((num_controls + 1)..num_qubits).map(|q| q as u32).collect();
+
+        circuit.rccx(controls[0], controls[1], ancillas[0])?;
+
+        let mut i = 0;
+        for &ctrl in controls.iter().skip(2).take(num_controls - 3) {
+            circuit.rccx(ctrl, ancillas[i], ancillas[i + 1])?;
+            i += 1;
+        }
+        circuit.ccx(controls[num_controls - 1], ancillas[i], target)?;
+
+        for &ctrl in controls.iter().skip(2).take(num_controls - 3).rev() {
+            circuit.rccx(ctrl, ancillas[i - 1], ancillas[i])?;
+            i -= 1;
+        }
+
+        circuit.rccx(controls[0], controls[1], ancillas[i])?;
+
+        Ok(circuit)
+    }
+}
+
 // The following synth_mcx_noaux_hp24 algorithm is based on the work by Huang and Palsberg.
 //
 // # References
