a2074ee27fd10fe6dba36d1cbc7b2b49e945b716
Raynel Sanchez <raynelfss@hotmail.com>
2026-08-18 13:54:09 -0400

    Fix: Resolved endian in QPY

--- crates/qpy/src/params.rs
+++ crates/qpy/src/params.rs
@@ -582,7 +582,7 @@ pub(crate) fn pack_param_obj(
         Param::Obj(py_object) => {
             Python::attach(|py| py_pack_param(py_object.bind(py), qpy_data, endian))?
         }
-        Param::Int(int) => match endian {
+        Param::Int(int) => match resolved {
             Endian::Little => formats::GenericDataPack {
                 type_key: ValueType::Integer,
                 data: int.to_le_bytes().into(),
