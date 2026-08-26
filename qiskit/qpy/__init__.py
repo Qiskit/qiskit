c65a7b47ba02d63ce11e918160ebe698327e9f02
Gadi Aleksandrowicz <gadial@gmail.com>
2026-08-26 14:35:28 +0300

    Update the QPY format doc

--- qiskit/qpy/__init__.py
+++ qiskit/qpy/__init__.py
@@ -512,8 +512,8 @@ The register name needs no length of its own because the enclosing field already
 payload: ``conditional_reg_name_size`` for a condition, and the ``INSTRUCTION_PARAM`` header's
 ``size`` for a parameter.
 
-Changes to REGISTER_PACK
-~~~~~~~~~~~~~~~~~~~~~~~~
+Changes to REGISTERS
+~~~~~~~~~~~~~~~~~~~~
 
 The representation of registers defined in :ref:`qpy_registers` and updated in :ref:`qpy_version_4`)
 in QPY has changed in Version 18. The first change is the type of register
@@ -522,6 +522,11 @@ change to ``int64_t`` was done to enable using -1 as a sentinel value for a bit 
 is not actually needed as we can use the max value of a ``uint32_t`` (4294967295) as the sentinel value.
 In version 18 values of 4294967295 should be treated as -1 was in previous QPY and the bit in that array
 position is not in the circuit.
+
+Version 18 also adds ``'a'`` as a value for the ``type`` field to represent an
+:class:`.AncillaRegister`. Previously, ancilla registers were encoded as ``'q'`` and were
+therefore deserialized as ordinary :class:`.QuantumRegister` objects. The existing ``'q'`` and
+``'c'`` values continue to represent quantum and classical registers, respectively.
 
 The :ref:`qpy_registers` header format has also been updated to
 
