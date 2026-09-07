9699b78e8789bc83d083916e4bc054da6580fd5f
Julien Gacon <jules.gacon@googlemail.com>
2026-09-07 14:06:15 +0200

    Fix an edge case in sub_opt
    Nested sub-binary expression with numeric values as RHS values could lead to a -1 multiplier. E.g. ((x+y) - 1) - 2) would become 3 - (x+y) instead of (x+y) - 3.
    
    This has no release note since the bug is not yet released.

--- crates/circuit/src/parameter/symbol_expr.rs
+++ crates/circuit/src/parameter/symbol_expr.rs
@@ -1948,8 +1948,8 @@ impl SymbolExpr {
                                 // case: l_lhs - (lv + rv)
                                 (_, SymbolExpr::Value(lv)) => {
                                     return Some(_sub(
-                                        SymbolExpr::Value(rv + lv),
                                         l_lhs.as_ref().clone(),
+                                        SymbolExpr::Value(rv + lv),
                                     ));
                                 }
                                 (_, _) => (),
