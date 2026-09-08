754edd777eb5dd79ad57cc4288bf1fe4783f8286
Vyom Patel <vyom.patel@uwaterloo.ca>
2026-09-08 12:28:25 -0400

    Document reduced-ancilla MCRY selection
    Mention the newly enabled basic-mode selection in the release note and update its test to cover four controls with one clean ancilla.

--- test/python/circuit/test_controlled_gate.py
+++ test/python/circuit/test_controlled_gate.py
@@ -738,10 +738,10 @@ class TestControlledGate(QiskitTestCase):
 
     def test_mcry_defaults_to_vchain(self):
         """Test mcry defaults to the v-chain mode if sufficient work qubits are provided."""
-        circuit = QuantumCircuit(5)
-        control_qubits = circuit.qubits[:3]
-        target_qubit = circuit.qubits[3]
-        additional_qubits = circuit.qubits[4:]
+        circuit = QuantumCircuit(6)
+        control_qubits = circuit.qubits[:4]
+        target_qubit = circuit.qubits[4]
+        additional_qubits = circuit.qubits[5:]
         circuit.mcry(0.2, control_qubits, target_qubit, additional_qubits)
 
         # If the v-chain mode is selected, all qubits are used. If the noancilla mode would be
