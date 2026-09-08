jakelishman/qiskit-terra

Move deeply recursive `SymbolExpr` parse trees to heap
The parser is implemented as a recursive descent, which makes it liable
to overflowing the stack in deep nesting, such as `-(-(-(... a )))...`.
This doesn't rewrite the parser to be non-recursive, but since all
recursion cycles go through a single function, it's easy to add in a
"is the stack getting out of hand?" check into it, and spill to the heap
if so.

The entire `SymbolExpr` class has recursion trouble in its `*_opt`
functions still, so the resulting expressions can still cause overflow
problems when used.

test/python/circuit/test_parameter_expression.py
@@ -984,3 +984,12 @@ class TestParameterExpression(QiskitTestCase):
         b = Parameter("b")
         expr = a + b
         self.assertEqual(expr, expr.simplify())
+
+    def test_deep_string_parse(self):
+        """Test that the string parser can handle very deep expressions."""
+        a = Parameter("a")
+        expr = "-(" * 1_000_000 + "a" + ")" * 1_000_000
+        # This is an explicitly private constructor, but the purpose of the test is for _any_ string
+        # constructor; we can change it over to a new API if/when we expose one.
+        out = ParameterExpression({"a": a}, expr)
+        self.assertEqual(out, a)

Qiskit/qiskit

test/python/circuit/test_parameter_expression.py
@@ -1071,6 +1071,22 @@ class TestParameterExpression(QiskitTestCase):
         expected = x + y - 3.0
         self.assertEqual(expected, sub2)
 
+    def test_deep_string_parse(self):
+        """Test that the string parser can handle very deep expressions."""
+        n = 100_000
+        a = Parameter("a")
+        expr_str = "a" + " ** a" * n
+        # This is an explicitly private constructor, but the purpose of the test is for _any_ string
+        # constructor; we can change it over to a new API if/when we expose one.
+        out = ParameterExpression({"a": a}, expr_str)
+        expected = a
+        for _ in range(n):
+            # TODO: actually this seems like a weirdness in the parser: ` ** ` should be
+            # right-associative (so it should be `a**expected`).  We have to use `**` in the test to
+            # avoid complexity explosion via attempted simplification.
+            expected = expected**a
+        self.assertStructurallyEqual(out, expected)
+
     def test_structurally_equal_simplification(self):
         x = Parameter("x")
         y = Parameter("y")