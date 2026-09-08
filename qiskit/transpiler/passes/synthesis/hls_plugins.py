c37ac4a3c286e24dd4cc2aa063a02f98d7fc9691
Alexander Ivrii <alexi@il.ibm.com>
2026-09-08 08:53:05 +0300

    return error directly, without hiding it into a try block

--- qiskit/transpiler/passes/synthesis/hls_plugins.py
+++ qiskit/transpiler/passes/synthesis/hls_plugins.py
@@ -2143,10 +2143,8 @@ class PauliEvolutionSynthesisDefault(HighLevelSynthesisPlugin):
         if "preserve_order" in options and isinstance(algo, ProductFormula):
             algo.preserve_order = options["preserve_order"]
 
-        try:
-            synth_object = algo.synthesize(high_level_object)
-        finally:
-            algo.preserve_order = original_preserve_order
+        synth_object = algo.synthesize(high_level_object)
+        algo.preserve_order = original_preserve_order
         return synth_object
 
 
@@ -2237,18 +2235,17 @@ class PauliEvolutionSynthesisRustiq(HighLevelSynthesisPlugin):
         upto_phase = options.get("upto_phase", False)
         resynth_clifford_method = options.get("resynth_clifford_method", 1)
 
-        try:
-            synth_object = synth_pauli_network_rustiq(
-                num_qubits=num_qubits,
-                pauli_network=pauli_network,
-                optimize_count=optimize_count,
-                preserve_order=preserve_order,
-                upto_clifford=upto_clifford,
-                upto_phase=upto_phase,
-                resynth_clifford_method=resynth_clifford_method,
-            )
-        finally:
-            algo.preserve_order = original_preserve_order
+        synth_object = synth_pauli_network_rustiq(
+            num_qubits=num_qubits,
+            pauli_network=pauli_network,
+            optimize_count=optimize_count,
+            preserve_order=preserve_order,
+            upto_clifford=upto_clifford,
+            upto_phase=upto_phase,
+            resynth_clifford_method=resynth_clifford_method,
+        )
+
+        algo.preserve_order = original_preserve_order
         return synth_object
 
 
@@ -2338,18 +2335,16 @@ class PauliEvolutionSynthesisMcts(HighLevelSynthesisPlugin):
         num_simulations = options.get("num_simulations", 1)
         max_parallel_simulations = options.get("max_parallel_simulations", None)
 
-        try:
-            synth_object = synth_pauli_network_mcts(
-                num_qubits=num_qubits,
-                pauli_network=pauli_network,
-                preserve_order=preserve_order,
-                upto_clifford=upto_clifford,
-                upto_phase=upto_phase,
-                num_simulations=num_simulations,
-                max_parallel_simulations=max_parallel_simulations,
-            )
-        finally:
-            algo.preserve_order = original_preserve_order
+        synth_object = synth_pauli_network_mcts(
+            num_qubits=num_qubits,
+            pauli_network=pauli_network,
+            preserve_order=preserve_order,
+            upto_clifford=upto_clifford,
+            upto_phase=upto_phase,
+            num_simulations=num_simulations,
+            max_parallel_simulations=max_parallel_simulations,
+        )
+        algo.preserve_order = original_preserve_order
         algo.preserve_order = original_preserve_order
         return synth_object
 
