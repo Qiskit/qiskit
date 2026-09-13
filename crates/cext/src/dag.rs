8dedcb6f067a36dda90c1d0116c39489467b4362
Julien Gacon <jules.gacon@gmail.com>
2026-09-13 09:09:10 -0400

    Fix clippy

--- crates/cext/src/dag.rs
+++ crates/cext/src/dag.rs
@@ -19,14 +19,14 @@ use crate::exit_codes::ExitCode;
 use crate::transpiler::target::parse_params;
 use qiskit_circuit::bit::{ClassicalRegister, QuantumRegister};
 use qiskit_circuit::circuit_data::CircuitData;
+#[cfg(feature = "python_binding")]
+use qiskit_circuit::dag_circuit::PyDAGCircuit;
 use qiskit_circuit::dag_circuit::{DAGCircuit, DAGError, NodeIndex, NodeType};
 use qiskit_circuit::instruction::Parameters;
 use qiskit_circuit::operations::{
     ArrayType, Operation, OperationRef, Param, StandardGate, StandardInstruction, UnitaryGate,
 };
 use qiskit_circuit::{Clbit, Qubit};
-#[cfg(feature = "python_binding")]
-use qiskit_circuit::dag_circuit::PyDAGCircuit;
 
 use crate::circuit::{CBlocksMode, CInstruction, CVarsMode};
 
