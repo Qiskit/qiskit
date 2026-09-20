ef7f30ecec84a0b7a4efad55c3bf53100bf90114
Gadi Aleksandrowicz <gadial@gmail.com>
2026-09-20 17:08:48 +0300

    update the python test to handle multiple circuits

--- test/python/capi/test_qpy.py
+++ test/python/capi/test_qpy.py
@@ -10,6 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
+import ctypes
 import os
 from pathlib import Path
 import tempfile
@@ -20,21 +21,27 @@ from test import QiskitTestCase
 
 class TestQpyCAPI(QiskitTestCase):
     def test_dump_python_circuit_data(self):
-        """The Python ctypes binding can dump Python-owned CircuitData as QPY."""
+        """The Python ctypes binding can dump multiple Python-owned CircuitData objects."""
         circuit = QuantumCircuit(2, 2)
         circuit.h(0)
         circuit.cx(0, 1)
         circuit.measure([0, 1], [0, 1])
 
-        circuit_ptr = capi.qk_circuit_borrow_from_python(circuit._data)
+        second_circuit = QuantumCircuit(1)
+        second_circuit.x(0)
+
+        circuit_ptrs = (ctypes.POINTER(capi.QkCircuit) * 2)(
+            capi.qk_circuit_borrow_from_python(circuit._data),
+            capi.qk_circuit_borrow_from_python(second_circuit._data),
+        )
 
         with tempfile.TemporaryDirectory() as tmp_dir:
             filename = Path(tmp_dir) / "circuit.qpy"
-            result = capi.qk_qpy_dump_file(circuit_ptr, os.fsencode(filename), 18)
+            result = capi.qk_qpy_dump_file(circuit_ptrs, len(circuit_ptrs), os.fsencode(filename))
 
             self.assertEqual(result, capi.QkExitCode.Success.value.value)
             self.assertTrue(filename.is_file())
             with filename.open("rb") as qpy_file:
                 loaded = qpy.load(qpy_file)
 
-        self.assertEqual(loaded, [circuit])
+        self.assertEqual(loaded, [circuit, second_circuit])
