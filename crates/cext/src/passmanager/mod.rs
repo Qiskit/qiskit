48e70c4a3cc2be2b9a1f6f0375a46b73d965249c
Jake Lishman <jake.lishman@ibm.com>
2026-09-22 15:26:52 +0100

    WIP: refactor for changed base trait

--- crates/cext/src/passmanager/mod.rs
+++ crates/cext/src/passmanager/mod.rs
@@ -10,105 +10,282 @@
 // copyright notice, and modified files need to carry a notice indicating
 // that they have been altered from the originals.
 
-use qiskit_passmanager::{DynTypeId, Pass, PassContext, PassManager, PassManagerContext};
+use qiskit_passmanager::{
+    DynTypeId, Pass, PassContext, PassError, PassManager, PassManagerContext,
+};
 use std::{
     any::Any,
     ffi::{CStr, c_char, c_void},
-    ptr::null_mut,
+    mem, ptr,
+    sync::Arc,
 };
 
 use crate::{
     ExitCode,
-    pointers::{const_ptr_as_ref, mut_ptr_as_ref},
+    pointers::{arc_clone_from_raw, const_ptr_as_ref, mut_ptr_as_ref},
 };
 
+type CFuncPtr = unsafe extern "C" fn() -> c_void;
+
+// TODO: docs
 /// @ingroup QkPassManager
-/// A slot in a vtable, with an `index` and a `ptr` to the function pointer.
-///
-/// This is a generic slot that can be used for different vtables.
+/// An entry in a vtable for defining objects with custom behavior.
 #[repr(C)]
-pub struct VTableSlot {
-    index: usize,
-    ptr: *mut c_void,
-}
-
-/// The pass' run method with signature
-/// void* run(void *self, void *ir, QkPassContext *context);
-type RunFunctionPtr = extern "C" fn(*mut c_void, *mut c_void, *mut PassContext) -> *mut c_void;
-
-/// The pass vtable.
-struct PassVTable {
-    run_ptr: RunFunctionPtr,
-}
-
-impl PassVTable {
-    /// # Safety
+pub struct VTableEntry {
+    /// The "slot" of the function, or the sentinel `(uint32_t)-1` to mark the final array entry.
     ///
-    /// Behavior is undefined if `slots` is not a pointer to `num_slots` consecutive, valid,
-    /// non-null [VTableSlot]s, or if any of the slots does not point to a function with
-    /// the signature documented in [PassFromC].
-    unsafe fn from_slots(slots: *const VTableSlot, num_slots: usize) -> Result<Self, &'static str> {
-        // SAFETY: Per documentation, the pointer points to `num_slots` valid slots.
-        let slots = unsafe { ::std::slice::from_raw_parts(slots, num_slots) };
-
-        let run_ptr = if let Some(run_slot) = slots.first() {
-            // SAFETY: Per documentation, the slot has the right type.
-            unsafe { ::std::mem::transmute::<*mut c_void, RunFunctionPtr>(run_slot.ptr) }
-        } else {
-            return Err("Expected slot 0 to contain the `run_ptr`, but slot is empty.");
-        };
-
-        Ok(Self { run_ptr })
-    }
+    /// This is typically set to some `enum` value, where the particular `enum` varies depending on
+    /// which vtable you are defining.
+    pub slot: u32,
+    /// Any additional "flags" for the particular table entry.  These will typically be or'd (`|`)
+    /// together, and the valid set of flags will be documented by the table user.
+    pub flags: u32,
+    /// A function pointer implementing the correct signature for the combination of the `slot` and
+    /// `flags`.
+    ///
+    /// This can be `NULL` only in the case of `slots` being the sentinel `-1`.
+    pub ptr: Option<CFuncPtr>,
 }
 
-/// A struct representing a Qiskit pass in C.
-///
-/// The pass is exposed as opaque pointer to keep the ability of adding more fields
-/// without breaking the ABI.
-///
-/// | Slot   | Signature                                                   | Required |
-/// |--------|-------------------------------------------------------------|----------|
-/// | 0      | `void (*run)(void *self, void *ir, QkPassContext *context)` |    Yes   |
-pub struct PassFromC {
-    /// The pass object, aka `self`.
-    self_ptr: *mut c_void,
-    /// The pass' vtable.
-    vtable: PassVTable,
-}
+// TODO: docs
+pub struct Error(anyhow::Error);
 
-impl Pass for PassFromC {
-    type InputIR = *mut c_void;
-    type OutputIR = *mut c_void;
-
-    fn run(&self, ir: Self::InputIR, context: &mut PassContext) -> anyhow::Result<Self::OutputIR> {
-        Ok((self.vtable.run_ptr)(self.self_ptr, ir, context))
-    }
-}
-
-/// @ingroup QkPassManager
-/// Create a new pass.
+/// @ingroup QkError
+/// Create a new error message.
 ///
-/// @param self_ptr A `void *` to a config struct, or `self`. Can be `NULL`.
-/// @param slots A pointer to the vtable slots specifying the pass methods.
-/// @param num_slots The number of slots.
+/// @param msg A borrowed pointer to a nul-terminated string.
 ///
 /// # Safety
 ///
-/// Behavior is undefined if `slots` is not a pointer to `num_slots` consecutive, valid,
-/// non-null [VTableSlot]s, or if any of the slots does not point to a function with
-/// the signature documented in [PassFromC].
+/// `msg` must point to nul-terminated bytes.
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn qk_pass_new(
-    self_ptr: *mut c_void,
-    slots: *const VTableSlot,
-    num_slots: usize,
-) -> *mut PassFromC {
-    let Ok(vtable) = (unsafe { PassVTable::from_slots(slots, num_slots) }) else {
-        return null_mut();
-    };
+pub unsafe extern "C" fn qk_error_new(msg: *const c_char) -> ptr::NonNull<Error> {
+    // SAFETY: per documentation, `msg` points to nul-terminated bytes.
+    let msg = unsafe { CStr::from_ptr(msg) }.to_string_lossy();
+    let ptr = Box::into_raw(Box::new(Error(anyhow::Error::msg(msg))));
+    // SAFETY: `Box` is always non-null.
+    unsafe { ptr::NonNull::new_unchecked(ptr) }
+}
 
-    Box::into_raw(Box::new(PassFromC { self_ptr, vtable }))
+// TODO: docs
+#[derive(Clone, Copy, derive_more::TryFrom, Debug)]
+#[try_from(repr)]
+#[repr(u32)]
+pub enum PassSlot {
+    /// Run the pass on an IR of the correct type. *Required*.
+    ///
+    /// Signature:
+    /// ```c
+    /// void *run(void *this, void *ir, QkPassContext *context, QkError **error);
+    /// ```
+    ///
+    /// # Implementation
+    ///
+    /// This implements the pass behavior.  `this` will be equal to a value passed to `qk_pass_new`
+    /// with this vtable.  `ir` will point to data of the type specified by the vtable's `ir_in`
+    /// field.
+    // TODO: ownership of `ir`?
+    Run = 0,
+    /// A destructor for the `this` argument of a pass, at the time that the pass is destructed.
+    /// *Optional*.
+    ///
+    /// Signature:
+    /// ```c
+    /// void delete(void *this);
+    /// ```
+    ///
+    /// # Safety
+    ///
+    /// This must be safe to call from any thread.
+    Delete = 1,
+}
+
+// TODO: docs
+pub struct PassVTable {
+    name: String,
+    ir_in: Arc<IRVTable>,
+    ir_out: Arc<IRVTable>,
+    run: unsafe extern "C" fn(
+        *mut c_void,
+        *mut c_void,
+        *mut PassContext,
+        *mut *mut Error,
+    ) -> *mut c_void,
+    delete: Option<unsafe extern "C" fn(*mut c_void) -> c_void>,
+}
+impl TryFrom<PassVTablePartial> for PassVTable {
+    /// The first slot encountered that's required but absent.
+    type Error = PassSlot;
+
+    fn try_from(partial: PassVTablePartial) -> Result<Self, Self::Error> {
+        Ok(Self {
+            name: partial.name,
+            ir_in: partial.ir_in,
+            ir_out: partial.ir_out,
+            run: partial.run.ok_or(PassSlot::Run)?,
+            delete: partial.delete,
+        })
+    }
+}
+
+// TODO: docs
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn qk_pass_vtable_new(
+    name: *const c_char,
+    ir_in: *const IRVTable,
+    ir_out: *const IRVTable,
+    mut table: *const VTableEntry,
+) -> *const PassVTable {
+    // SAFETY: per documentation `name` is a pointer to nul-terminated `char`s.
+    let name = unsafe { CStr::from_ptr(name) }
+        .to_string_lossy()
+        .into_owned();
+    // SAFETY: per documentation, `ir_in` is the valid output of an `IRVTable` constructor.
+    let ir_in = unsafe { arc_clone_from_raw(ir_in) };
+    // SAFETY: per documentation, `ir_out` is the valid output of an `IRVTable` constructor.
+    let ir_out = unsafe { arc_clone_from_raw(ir_out) };
+    let mut partial = PassVTablePartial::new(name, ir_in, ir_out);
+    loop {
+        // SAFETY: per documentation, `table` is valid for reads until we see the sentinel all-ones
+        // pattern in a `slot`.
+        let entry = unsafe { table.read() };
+        table = if entry.slot == u32::MAX {
+            break;
+        } else {
+            table.wrapping_add(1)
+        };
+        let Ok(slot) = PassSlot::try_from(entry.slot) else {
+            // We assume this is a slot from a later version of Qiskit.
+            // TODO: add an envvar / global to turn on debug information in these cases?
+            continue;
+        };
+        // SAFETY: per documentation, `entry.ptr` is not null because `entry.slot` was not all ones.
+        let ptr = unsafe { entry.ptr.unwrap_unchecked() };
+        // SAFETY: per documentation, `ptr` is of the expected function-pointer type.
+        unsafe { partial.set(slot, ptr) };
+    }
+    let vtable = PassVTable::try_from(partial).unwrap();
+    Arc::into_raw(Arc::new(vtable))
+}
+
+// TODO: we can very likely do a bit of macro trickery to simplify the creation of vtable objects,
+// taking care that the filled public one needs to be visible to `cbindgen`.  For initial
+// implementation, hard-coding is good enough.
+#[derive(Debug)]
+struct PassVTablePartial {
+    name: String,
+    ir_in: Arc<IRVTable>,
+    ir_out: Arc<IRVTable>,
+    run: Option<
+        unsafe extern "C" fn(
+            *mut c_void,
+            *mut c_void,
+            *mut PassContext,
+            *mut *mut Error,
+        ) -> *mut c_void,
+    >,
+    delete: Option<unsafe extern "C" fn(*mut c_void) -> c_void>,
+}
+impl PassVTablePartial {
+    fn new(name: String, ir_in: Arc<IRVTable>, ir_out: Arc<IRVTable>) -> Self {
+        Self {
+            name,
+            ir_in,
+            ir_out,
+            run: None,
+            delete: None,
+        }
+    }
+
+    /// Set the `slot` to the corresponding function `ptr`.
+    ///
+    /// # Safety
+    ///
+    /// `ptr` must be a valid function pointer of the type expected by the corresponding method in
+    /// [`PassVTable`].
+    unsafe fn set(&mut self, slot: PassSlot, ptr: CFuncPtr) -> bool {
+        match slot {
+            PassSlot::Run => {
+                // SAFETY: per documentation, caller ensures pointer type validity.
+                let ptr = unsafe { mem::transmute::<CFuncPtr, _>(ptr) };
+                self.run.replace(ptr).is_some()
+            }
+            PassSlot::Delete => {
+                // SAFETY: per documentation, caller ensures pointer type validity.
+                let ptr = unsafe { mem::transmute::<CFuncPtr, _>(ptr) };
+                self.delete.replace(ptr).is_some()
+            }
+        }
+    }
+}
+
+// TODO: docs
+pub struct CPass {
+    /// The pass object, aka `self`.
+    this: *mut c_void,
+    vtable: Arc<PassVTable>,
+}
+impl CPass {
+    /// Create a new instance of the pass.
+    ///
+    /// # Safety
+    ///
+    /// 1. `this` must point ot data of the type that is expected by all "`this`" arguments in
+    ///    functions in the `vtable`.
+    /// 2. the data pointed to by `this` must be safe to share and send between threads.
+    unsafe fn new(this: *mut c_void, vtable: Arc<PassVTable>) -> Self {
+        Self { this, vtable }
+    }
+}
+impl Pass for CPass {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+    fn ir_id_in(&self) -> DynTypeId<'_> {
+        self.vtable.ir_in.dyn_type_id()
+    }
+    fn ir_id_out(&self) -> DynTypeId<'_> {
+        self.vtable.ir_out.dyn_type_id()
+    }
+    fn name(&self) -> &str {
+        &self.vtable.name
+    }
+    fn run(&self, ir: Box<dyn Any>, context: &mut PassContext) -> Result<Box<dyn Any>, PassError> {
+        let mut error = None::<ptr::NonNull<Error>>;
+        let error_ptr = (&raw mut error).cast::<*mut Error>();
+        let ir_out = unsafe { (self.vtable.run)(self.this, ir, context, error_ptr) };
+        match error {
+            Some(error) => {
+                let error = unsafe { Box::from_raw(error.as_ptr()) };
+                Err(PassError::Runtime(error.0))
+            }
+            None => Ok(ir_out),
+        }
+    }
+}
+impl Drop for CPass {
+    fn drop(&mut self) {
+        if let Some(delete) = self.vtable.delete {
+            // SAFETY: per documentation of `PassVTable`, if the `delete` method is set, it is valid
+            // to be passed `self.this` from any thread.
+            unsafe { delete(self.this) };
+        }
+    }
+}
+// SAFETY: per struct documentation, the `this` pointer must be safe to send between threads.
+unsafe impl Send for CPass {}
+// SAFETY: per struct documentation, the `this` pointer must be safe to share between threads.
+unsafe impl Sync for CPass {}
+
+// TODO: docs
+#[unsafe(no_mangle)]
+pub unsafe extern "C" fn qk_pass_new(this: *mut c_void, vtable: *const PassVTable) -> *mut CPass {
+    // SAFETY: per documentation, `vtable` is the still-valid result of `qk_pass_vtable_new`, which
+    // returns the result of `Arc::into_raw`.
+    let vtable = unsafe { arc_clone_from_raw(vtable) };
+    // SAFETY: per documentation, `this` points to data of the type expected by `vtable` methods.
+    let pass = unsafe { CPass::new(this, vtable) };
+    Box::into_raw(Box::new(pass))
 }
 
 /// @ingroup QkPassManager
@@ -121,17 +298,11 @@ pub unsafe extern "C" fn qk_pass_new(
 ///
 /// Behavior is undefined if ``pass`` is not either null or a valid pointer to a ``QkPass``.
 #[unsafe(no_mangle)]
-pub unsafe extern "C" fn qk_pass_free(pass: *mut PassFromC) {
-    if !pass.is_null() {
-        if !pass.is_aligned() {
-            panic!("Attempted to free a non-aligned pointer.")
-        }
-
-        // SAFETY: We have verified the pointer is non-null and aligned, so it should be
-        // readable by Box.
-        unsafe {
-            let _ = Box::from_raw(pass);
-        }
+pub unsafe extern "C" fn qk_pass_free(pass: Option<ptr::NonNull<CPass>>) {
+    if let Some(ptr) = pass {
+        // SAFETY: per documentation, `pass` points to the result of `qk_pass_new`, which returns a
+        // leaked boxed `CPass`.
+        _ = unsafe { Box::from_raw(ptr.as_ptr()) };
     }
 }
 
